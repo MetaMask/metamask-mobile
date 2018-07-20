8d35ef66d534c0cf8ce515878ec19bd174676cd8
brunobar79 <brunobar79@gmail.com>
2018-07-20 16:28:27 -0400

    replace specific line

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,6 +3,5 @@ echo "PostInstall script:"
 
 echo "1 - Fixing WKWebView..."
 WKWEBVIEW_FILE=$(find . -name RCTWKWebView.m);
-sed -i '' -e 's/injectionTime:WKUserScriptInjectionTimeAtDocumentEnd/injectionTime:WKUserScriptInjectionTimeAtDocumentStart/g' $WKWEBVIEW_FILE;
-
+sed -i '' -e '143s/WKUserScriptInjectionTimeAtDocumentEnd/WKUserScriptInjectionTimeAtDocumentStart/' $WKWEBVIEW_FILE;
 echo "Done"
