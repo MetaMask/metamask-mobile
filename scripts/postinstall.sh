98aac9f11d178f24d0df0742bc36e4d7cc85e5a9
Bruno Barbieri <bruno.barbieri@consensys.net>
2018-07-25 19:08:05 -0400

    Add MetaMaskController (#6)

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 echo "PostInstall script:"
 
-echo "1 - Fixing WKWebView..."
+echo "1. React Native nodeify..."
+node_modules/.bin/rn-nodeify --install 'crypto,buffer,react-native-randombytes,vm,stream' --hack
+echo "Done"
+
+echo "2. Fixing WKWebView..."
 WKWEBVIEW_FILE=$(find . -name RCTWKWebView.m);
 sed -i '' -e '143s/WKUserScriptInjectionTimeAtDocumentEnd/WKUserScriptInjectionTimeAtDocumentStart/' $WKWEBVIEW_FILE;
+echo "Done"
+
+echo "3. Fixing ethereumjs-wallet dependency (aes-js)..."
+TARGET="node_modules/ethereumjs-wallet/node_modules/aes-js/index.js"
+sed -i '' -e 's/var previous_mymodule/\/\/var previous_mymodule/' $TARGET;
 echo "Done"
