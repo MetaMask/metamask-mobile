0244c0ee7704102a3c447d73ca13b00f1c8e741d
Bruno Barbieri <bruno.barbieri@consensys.net>
2018-08-02 00:51:33 -0400

    Bottom navbar + icons (#13)
    * navbar with icons
    
    * fix statusbar color

--- app/components/App/index.js
+++ app/components/App/index.js
@@ -1,15 +1,40 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation';
 import BrowserScreen from '../BrowserScreen';
 import WalletScreen from '../WalletScreen';
+import { createIconSetFromFontello } from 'react-native-vector-icons';
+import fontelloConfig from '../../fonts/config.json';
+const Icon = createIconSetFromFontello(fontelloConfig);
 
+import { colors } from '../../styles/common';
 /**
  * Root application component responsible for configuring the tab navigator
  */
-export default createBottomTabNavigator({
-	Home: {
-		screen: BrowserScreen
+export default createBottomTabNavigator(
+	{
+		Home: {
+			screen: BrowserScreen,
+			navigationOptions: () => ({
+				title: 'ÐApps',
+				tabBarIcon: ico => <Icon name="dapp" size={18} color={ico.tintColor} /> // eslint-disable-line react/display-name
+			})
+		},
+		Wallet: {
+			screen: WalletScreen,
+			navigationOptions: () => ({
+				title: 'Wallet',
+				tabBarIcon: ico => <Icon name="wallet" size={20} color={ico.tintColor} /> // eslint-disable-line react/display-name
+			})
+		}
 	},
-	Wallet: {
-		screen: WalletScreen
+	{
+		tabBarOptions: {
+			activeTintColor: colors.primary,
+			inactiveTintColor: colors.inactive,
+			style: {
+				borderTopWidth: StyleSheet.hairlineWidth
+			}
+		}
 	}
-});
+);
---
+++ scripts/copy-android-assets.sh
@@ -0,0 +1,2 @@
+yes | cp -rf ./app/entry.js ./android/app/src/main/assets/entry.js
+yes | cp -rf ./app/fonts/metamask.ttf ./android/app/src/main/assets/metamask.ttf
