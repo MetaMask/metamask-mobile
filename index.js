c9a79109b700f579bb6f728096e0dadb20eeb929
brunobar79 <brunobar79@gmail.com>
2018-08-08 23:22:25 -0400

    update

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-import { decode, encode } from 'base-64';
-if (!global.btoa) {
-	global.btoa = encode;
-}
-
-if (!global.atob) {
-	global.atob = decode;
-}
-
 import './shim.js';
 
 import crypto from 'crypto'; // eslint-disable-line import/no-nodejs-modules, no-unused-vars
