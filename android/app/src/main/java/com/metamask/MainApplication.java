66ac2631aa3c580d48196fced521f65c6fb2eb6d
brunobar79 <brunobar79@gmail.com>
2018-08-23 18:07:32 -0400

    Crashlytics added for android

--- android/app/src/main/java/com/metamask/MainApplication.java
+++ android/app/src/main/java/com/metamask/MainApplication.java
@@ -2,6 +2,7 @@ package com.metamask;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.facebook.react.ReactApplication;
 import com.tectiv3.aes.RCTAesPackage;
 import com.oblador.keychain.KeychainPackage;
@@ -16,6 +17,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import io.fabric.sdk.android.Fabric;
 import java.util.Arrays;
 import java.util.List;
 
@@ -59,6 +61,7 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
   @Override
   public void onCreate() {
     super.onCreate();
+    Fabric.with(this, new Crashlytics());
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
