ffe45e017a7abcccf6ab7b77dc25540ba7989e08
Esteban Miño <efmino@uc.cl>
2018-09-05 17:49:15 -0300

    fix last line copy android assets

--- scripts/copy-android-assets.sh
+++ scripts/copy-android-assets.sh
@@ -3,3 +3,4 @@ yes | cp -rf app/core/InpageBridge.js android/app/src/main/assets/.
 yes | cp -rf app/core/InpageBridgeWeb3.js android/app/src/main/assets/.
 # Copy fonts with iconset
 yes | cp -rf ./app/fonts/Metamask.ttf ./android/app/src/main/assets/fonts/Metamask.ttf
+
