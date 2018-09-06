86c07af8130a8269f87a4b139b3df18bb1435195
Esteban MIno <efmino@uc.cl>
2018-09-05 22:12:09 -0300

    wallet tokens logos path

--- app/util/assets.js
+++ app/util/assets.js
@@ -1,9 +1,6 @@
 export default function getAssetLogoPath(logo) {
 	if (!logo) return;
-	const pngPath = 'https://github.com/MetaMask/eth-contract-metadata/raw/master/images/';
-	const svgPath = 'https://raw.githubusercontent.com/MetaMask/eth-contract-metadata/master/images/';
-	const extension = logo.split('.')[-1];
-	const svgLogo = extension === 'svg';
-	const uri = svgLogo ? svgPath + logo : pngPath + logo;
+	const path = 'https://raw.githubusercontent.com/MetaMask/eth-contract-metadata/master/images/';
+	const uri = path + logo;
 	return uri;
 }
