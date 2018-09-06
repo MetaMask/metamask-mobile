2dd55e1bd3b0612687ce6f35cf1c98334fb9b960
Esteban Miño <efmino@uc.cl>
2018-09-05 21:38:03 -0300

    fix assets util

--- app/util/assets.js
+++ app/util/assets.js
@@ -2,7 +2,7 @@ export default function getAssetLogoPath(logo) {
 	if (!logo) return;
 	const pngPath = 'https://github.com/MetaMask/eth-contract-metadata/raw/master/images/';
 	const svgPath = 'https://raw.githubusercontent.com/MetaMask/eth-contract-metadata/master/images/';
-	const extension = logo.split('.')[1];
+	const extension = logo.split('.')[-1];
 	const svgLogo = extension === 'svg';
 	const uri = svgLogo ? svgPath + logo : pngPath + logo;
 	return uri;
