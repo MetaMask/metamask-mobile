0f90311dacb5e7f8ce7c49e54f0d83bd6b279bd8
brunobar79 <brunobar79@gmail.com>
2018-09-13 19:46:50 -0400

    update unit tests

--- app/components/SendScreen/index.test.js
+++ app/components/SendScreen/index.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import SendScreen from './';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 
-describe('SendScreen', () => {
+const mockStore = configureMockStore();
+const store = mockStore({});
+
+describe('Accounts', () => {
 	it('should render correctly', () => {
-		const wrapper = shallow(<SendScreen />);
+		const wrapper = shallow(
+			<Provider store={store}>
+				<SendScreen />
+			</Provider>
+		);
 		expect(wrapper).toMatchSnapshot();
 	});
 });
