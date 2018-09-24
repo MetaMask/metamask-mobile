a69c3a1c2114a57385b560b629b992c7c6910bb7
brunobar79 <brunobar79@gmail.com>
2018-09-23 22:19:07 -0400

    fix ropsten links

--- app/components/TransactionSubmitted/index.js
+++ app/components/TransactionSubmitted/index.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import { InteractionManager, Platform, Text, ActivityIndicator, View, StyleSheet } from 'react-native';
+import { Animated, InteractionManager, Platform, Text, ActivityIndicator, View, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { strings } from '../../../locales/i18n';
 import { colors, fontStyles } from '../../styles/common';
 import Screen from '../Screen';
 import { getModalNavbarOptions } from '../Navbar';
 import StyledButton from '../StyledButton';
-import Engine from '../../core/Engine';
 
 const styles = StyleSheet.create({
 	wrapper: {
 		backgroundColor: colors.white,
 		flex: 1,
-		padding: 30
+		padding: 20
 	},
 	loader: {
 		backgroundColor: colors.white,
@@ -50,15 +50,19 @@ const styles = StyleSheet.create({
 /**
  * Copmonent that provides ability to add a bookmark
  */
-export default class TransactionSubmitted extends Component {
+class TransactionSubmitted extends Component {
 	static navigationOptions = ({ navigation }) =>
 		getModalNavbarOptions(strings('transactionSubmitted.title'), navigation);
 
 	static propTypes = {
 		/**
-		/* navigation object required to push new views
+		/* navigation object required to push and pop other views
 		*/
-		navigation: PropTypes.object
+		navigation: PropTypes.object,
+		/**
+		/* String that represents the current network
+		*/
+		network: PropTypes.string
 	};
 
 	renderLoader() {
@@ -69,14 +73,29 @@ export default class TransactionSubmitted extends Component {
 		);
 	}
 
+	iconSpringVal = new Animated.Value(0.6);
+
+	componentDidMount() {
+		this.animateIcon();
+	}
+
+	animateIcon() {
+		InteractionManager.runAfterInteractions(() => {
+			Animated.spring(this.iconSpringVal, {
+				toValue: 1,
+				friction: 2.5,
+				useNativeDriver: true
+			}).start();
+		});
+	}
+
 	goToEtherscan = () => {
+		const { navigation } = this.props;
+		const hash = navigation.getParam('hash', null);
+		const isRopsten = this.props.network === 'ropsten';
+		const url = `https://${isRopsten ? 'ropsten.' : ''}etherscan.io/tx/${hash}`;
 		this.props.navigation.pop();
 		InteractionManager.runAfterInteractions(() => {
-			const { navigation } = this.props;
-			const hash = navigation.getParam('hash', null);
-			const { NetworkController } = Engine.context;
-			const isRopsten = NetworkController.provider.type === 'ropsten';
-			const url = `https://${isRopsten ? 'ropsten.' : ''}etherscan.io/tx/${hash}`;
 			this.props.navigation.navigate('BrowserView', {
 				url
 			});
@@ -88,12 +107,17 @@ export default class TransactionSubmitted extends Component {
 			<View style={styles.content}>
 				<Text style={styles.subtitle}>{strings('transactionSubmitted.yourTxHashIs')}</Text>
 				<Text style={styles.hash}>{hash}</Text>
-				<Icon name="check-circle" size={150} style={styles.icon} />
-				<StyledButton
-					type={'normal'}
-					onPress={this.goToEtherscan}
-					containerStyle={[styles.button, styles.cancel]}
+				<Animated.View
+					style={[
+						styles.iconWrapper,
+						{
+							transform: [{ scale: this.iconSpringVal }]
+						}
+					]}
 				>
+					<Icon name="check-circle" size={150} style={styles.icon} />
+				</Animated.View>
+				<StyledButton type={'normal'} onPress={this.goToEtherscan} containerStyle={styles.button}>
 					{strings('transactionSubmitted.viewOnEtherscan')}
 				</StyledButton>
 			</View>
@@ -113,3 +137,6 @@ export default class TransactionSubmitted extends Component {
 		);
 	}
 }
+
+const mapStateToProps = state => ({ network: state.backgroundState.NetworkController.provider.type });
+export default connect(mapStateToProps)(TransactionSubmitted);
