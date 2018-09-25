030c8246af1f0f4ef04894f81a3311c4df008769
Bruno Barbieri <bruno.barbieri@consensys.net>
2018-09-25 13:34:42 -0400

    support non https sites via dapp protocol

--- app/core/DeeplinkManager.js
+++ app/core/DeeplinkManager.js
@@ -32,7 +32,14 @@ export default class DeeplinkManager {
 			// Specific to the browser screen
 			// For ex. navigate to a specific dapp
 			case 'dapp':
-				urlObj.set('protocol', 'https:');
+
+				// if ?secure=false then default to http
+				if(urlObj.query && urlObj.query.secure === "false"){
+					urlObj.set('protocol', 'http:');
+				} else {
+					urlObj.set('protocol', 'https:');
+				}
+				
 				this.navigation.navigate('BrowserView', {
 					url: urlObj.href
 				});
