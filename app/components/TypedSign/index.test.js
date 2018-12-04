760818941668919c66cb9e2bb9dfb95fcf499c77
Esteban MIno <efmino@uc.cl>
2018-12-04 12:19:28 -0300

    update snapshots

--- app/components/TypedSign/index.test.js
+++ app/components/TypedSign/index.test.js
@@ -1,32 +1,15 @@
 import React from 'react';
-import TypedSign from './';
-import configureMockStore from 'redux-mock-store';
 import { shallow } from 'enzyme';
-
-const mockStore = configureMockStore();
+import TypedSign from './';
 
 describe('TypedSign', () => {
 	it('should render correctly', () => {
-		const initialState = {
-			backgroundState: {
-				AccountTrackerController: {
-					accounts: { '0x2': { balance: '0' } }
-				},
-				PreferencesController: {
-					selectedAddress: '0x2',
-					identities: { '0x2': { address: '0x2', name: 'Account 1' } }
-				}
-			}
-		};
 		const wrapper = shallow(
 			<TypedSign
-				navigation={{ state: { params: { messageParams: { from: '0x2', message: '0x879' } } } }}
+				currentPageInformation={{ title: 'title', url: 'url' }}
 				messageParams={{ data: { type: 'string', name: 'Message', value: 'Hi, Alice!' } }}
-			/>,
-			{
-				context: { store: mockStore(initialState) }
-			}
+			/>
 		);
-		expect(wrapper.dive()).toMatchSnapshot();
+		expect(wrapper).toMatchSnapshot();
 	});
 });
