63b9b9e0e8b25fa55b7a34f13c248a7138823ec2
brunobar79 <brunobar79@gmail.com>
2018-12-17 11:36:55 -0500

    fix typo for the second time

--- app/components/Browser/index.js
+++ app/components/Browser/index.js
@@ -289,7 +289,7 @@ export class Browser extends Component {
 				});
 				if (!privacyMode || ((!params || !params.force) && approvedHosts[hostname])) {
 					this.approvalRequest.resolve([selectedAddress]);
-					this.enableAccounts();
+					this.backgroundBridge.enableAccounts();
 				} else {
 					this.setState({ showApprovalDialog: true });
 				}
