c7d70df8e7e2fb3b430bab724877a8c23749875d
Bruno Barbieri <bruno.barbieri@consensys.net>
2019-01-11 19:32:50 -0500

    Fix url parsing (#308)
    * fix with tests
    
    * undo package-lock.json changes

--- app/util/browser.js
+++ app/util/browser.js
@@ -1,14 +1,15 @@
 /**
- * Calculates gas fee in wei
+ * Returns a sanitized url, which could be a search engine url if
+ * a keyword is detected instead of a url
  *
  * @param {string} input - String corresponding to url input
  * @param {string} searchEngine - Protocol string to append to URLs that have none
  * @param {string} defaultProtocol - Protocol string to append to URLs that have none
  * @returns {string} - String corresponding to sanitized input depending if it's a search or url
  */
-export default function onUrlSubmit(input, searchEngine, defaultProtocol = 'https://') {
+export default function onUrlSubmit(input, searchEngine = 'Google', defaultProtocol = 'https://') {
 	//Check if it's a url or a keyword
-	const res = input.match(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!&',;=.]+$/g);
+	const res = input.match(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!&',;=.+]+$/g);
 	if (res === null) {
 		// In case of keywords we default to google search
 		let searchUrl = 'https://www.google.com/search?q=' + escape(input);
