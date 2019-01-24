74353ed318d5d9d4787582ef8f3226d5cb2261db
Esteban MIno <efmino@uc.cl>
2019-01-24 17:56:13 -0300

    add AssetActionButtons

--- app/components/CollectibleContractOverview/index.js
+++ app/components/CollectibleContractOverview/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert, StyleSheet, Text, View, Platform } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
 import Identicon from '../Identicon';
 import LinearGradient from 'react-native-linear-gradient';
@@ -7,9 +7,7 @@ import { colors, fontStyles } from '../../styles/common';
 import { strings } from '../../../locales/i18n';
 import CollectibleImage from '../CollectibleImage';
 import { renderShortAddress } from '../../util/address';
-import StyledButton from '../StyledButton';
-import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
-import FoundationIcon from 'react-native-vector-icons/Foundation';
+import AssetActionButtons from '../AssetActionButtons';
 
 const styles = StyleSheet.create({
 	wrapper: {
@@ -55,43 +53,6 @@ const styles = StyleSheet.create({
 		fontSize: 12,
 		color: colors.fontSecondary,
 		...fontStyles.bold
-	},
-	buttons: {
-		flex: 1,
-		flexDirection: 'row',
-		marginTop: 30
-	},
-	button: {
-		flex: 1,
-		height: 50,
-		flexDirection: 'row'
-	},
-	leftButton: {
-		marginRight: 10
-	},
-	rightButton: {
-		marginLeft: 10
-	},
-	buttonText: {
-		marginLeft: 8,
-		marginTop: Platform.OS === 'ios' ? 0 : -2,
-		fontSize: 15,
-		color: colors.white,
-		...fontStyles.bold
-	},
-	buttonContent: {
-		flex: 1,
-		flexDirection: 'row',
-		alignItems: 'flex-start',
-		justifyContent: 'center'
-	},
-	buttonIcon: {
-		width: 15,
-		height: 15,
-		marginTop: 0
-	},
-	flexRow: {
-		flexDirection: 'row'
 	}
 });
 
@@ -136,30 +97,13 @@ export default class CollectibleContractOverview extends Component {
 					<Text style={styles.totalSupply}>Total Supply: {total_supply}</Text>
 					<Text style={styles.address}>{renderShortAddress(address)}</Text>
 				</View>
-				<View style={styles.buttons}>
-					<StyledButton
-						type={'confirm'}
-						onPress={this.onSend}
-						containerStyle={[styles.button, styles.leftButton]}
-						style={styles.buttonContent}
-						childGroupStyle={styles.flexRow}
-					>
-						<MaterialIcon name={'send'} size={15} color={colors.white} style={styles.buttonIcon} />
-						<Text style={styles.buttonText}>{strings('asset_overview.send_button').toUpperCase()}</Text>
-					</StyledButton>
-					<StyledButton
-						type={'confirm'}
-						onPress={this.onAdd}
-						containerStyle={[styles.button, styles.rightButton]}
-						style={styles.buttonContent}
-						childGroupStyle={styles.flexRow}
-					>
-						<FoundationIcon name={'download'} size={20} color={colors.white} style={styles.buttonIcon} />
-						<Text style={styles.buttonText}>
-							{strings('asset_overview.add_collectible_button').toUpperCase()}
-						</Text>
-					</StyledButton>
-				</View>
+
+				<AssetActionButtons
+					leftText={strings('asset_overview.send_button').toUpperCase()}
+					rightText={strings('asset_overview.add_collectible_button').toUpperCase()}
+					onRightPress={this.onSend}
+					onLeftPress={this.onAdd}
+				/>
 			</LinearGradient>
 		);
 	};
