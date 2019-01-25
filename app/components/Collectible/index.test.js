62d2d6ff39ccabf3030d679be1ca36e439da8bf6
Esteban MIno <efmino@uc.cl>
2019-01-24 21:03:54 -0300

    snapshots

---
+++ app/components/Collectible/index.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Collectible from './';
+import configureMockStore from 'redux-mock-store';
+
+const mockStore = configureMockStore();
+
+describe('Collectible', () => {
+	it('should render correctly', () => {
+		const initialState = {
+			engine: {
+				backgroundState: {
+					AssetsController: {
+						collectibles: [{ address: '0x0', name: 'collectible', tokenId: 0, image: 'image' }]
+					}
+				}
+			}
+		};
+
+		const wrapper = shallow(<Collectible navigation={{ state: { params: { address: '0x1' } } }} />, {
+			context: { store: mockStore(initialState) }
+		});
+		expect(wrapper.dive()).toMatchSnapshot();
+	});
+});
--- app/components/CollectibleView/index.test.js
+++ app/components/CollectibleView/index.test.js
@@ -4,7 +4,12 @@ import CollectibleView from './';
 
 describe('CollectibleView', () => {
 	it('should render correctly', () => {
-		const wrapper = shallow(<CollectibleView asset={{ name: 'Leopard', tokenId: 6904, address: '0x123' }} />);
+		const wrapper = shallow(
+			<CollectibleView
+				navigation={{ state: { params: { address: '0x1' } } }}
+				asset={{ name: 'Leopard', tokenId: 6904, address: '0x123' }}
+			/>
+		);
 		expect(wrapper).toMatchSnapshot();
 	});
 });
