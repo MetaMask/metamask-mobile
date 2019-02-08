2c6d6d32697bf76efbf93c8927ba2df9135e0e8c
brunobar79 <brunobar79@gmail.com>
2019-02-08 18:33:19 -0500

    license shit

--- scripts/build.sh
+++ scripts/build.sh
@@ -145,8 +145,6 @@ buildAndroidRelease(){
 		TARGET="android/app/build.gradle"
 		sed -i'' -e 's/getPassword("mm","mm-upload-key")/"ANDROID_KEY"/' $TARGET;
 		sed -i'' -e "s/ANDROID_KEY/$ANDROID_KEY/" $TARGET;
-		# required by react-native-aes-crypto
-		echo “Y” | /opt/android/sdk/tools/bin/sdkmanager “platforms;android-26”
 	fi
 
 	cd android &&
