c9e69f5358a39ca74eecf51056a77f277d12358a
Bruno Barbieri <brunobar79@gmail.com>
2019-02-28 11:45:05 -0500

    missing condition

--- app/components/Views/Wallet/index.js
+++ app/components/Views/Wallet/index.js
@@ -189,7 +189,7 @@ class Wallet extends Component {
 			const newPendingTxs = txs.filter(tx => tx.status === 'pending');
 			// To avoid extra re-renders we want to set the new txs only when
 			// there's a new tx in the history or the status of one of the existing txs changed
-			if (this.txs.length !== txs.length || this.didTxStatusesChange(newPendingTxs)) {
+			if (this.txs.length === 0 || this.txs.length !== txs.length || this.didTxStatusesChange(newPendingTxs)) {
 				this.txs = txs;
 				this.txsPending = newPendingTxs;
 			}
