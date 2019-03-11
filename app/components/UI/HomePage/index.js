9effdd8cd079ba52da4d622d283f07f3ba15104d
Bruno Barbieri <brunobar79@gmail.com>
2019-03-11 19:33:51 -0400

    fix snapshots

--- app/components/UI/HomePage/index.js
+++ app/components/UI/HomePage/index.js
@@ -153,7 +153,7 @@ const styles = StyleSheet.create({
 	},
 	urlAutocomplete: {
 		position: 'absolute',
-		marginTop: 60,
+		marginTop: 50,
 		backgroundColor: colors.white,
 		width: '100%',
 		height: '100%'
