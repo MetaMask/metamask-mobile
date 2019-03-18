c31c8a45b1ff555e8350fe5c7dda83ac9ceab79d
Esteban Mino <efmino@uc.cl>
2019-03-18 15:46:37 -0300

    remove border

--- app/components/UI/HomePage/index.js
+++ app/components/UI/HomePage/index.js
@@ -105,7 +105,7 @@ const styles = StyleSheet.create({
 	},
 	searchInputOnFocus: {
 		backgroundColor: colors.white,
-		borderColor: colors.primary
+		borderColor: colors.white
 	},
 	searchIcon: {
 		position: 'absolute',
