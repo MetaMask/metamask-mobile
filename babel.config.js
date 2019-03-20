f983dea39eb58179eab517041fcfba0295dde1b8
Bruno Barbieri <brunobar79@gmail.com>
2019-03-20 05:16:25 -0400

    fix env variables

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-	presets: ['module:metro-react-native-babel-preset']
+	presets: ['module:metro-react-native-babel-preset'],
+	plugins: [
+		"transform-inline-environment-variables"
+	],
 };
