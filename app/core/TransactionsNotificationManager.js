8f1275e6619416e2816c845da20f578d053df1dc
Bruno Barbieri <bruno.barbieri@consensys.net>
2019-04-09 13:20:27 -0400

    Two step  push notification prompt (#589)
    * two step notification prompt
    
    * clean up
    
    * fix typo

--- app/core/TransactionsNotificationManager.js
+++ app/core/TransactionsNotificationManager.js
@@ -7,7 +7,9 @@ import Networks, { isKnownNetwork } from '../util/networks';
 import { toChecksumAddress } from 'ethereumjs-util';
 import { hexToBN, renderFromWei } from '../util/number';
 import { strings } from '../../locales/i18n';
-import { AppState, Platform } from 'react-native';
+import { Alert, AppState, Platform } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import AppConstants from './AppConstants';
 
 /**
  * Singleton class responsible for managing all the transaction
@@ -107,6 +109,41 @@ class TransactionsNotificationManager {
 	}
 
 	/**
+	 * Handles the push notification prompt
+	 * with a custom set of rules, like max. number of attempts
+	 */
+	requestPushNotificationsPermission = async () => {
+		const promptCount = await AsyncStorage.getItem('@MetaMask:pushNotificationsPromptCount');
+		if (!promptCount || Number(promptCount) < AppConstants.MAX_PUSH_NOTIFICATION_PROMPT_TIMES) {
+			PushNotification.checkPermissions(permissions => {
+				if (!permissions || !permissions.alert) {
+					Alert.alert(
+						strings('notifications.prompt_title'),
+						strings('notifications.prompt_desc'),
+						[
+							{
+								text: strings('notifications.prompt_cancel'),
+								onPress: () => false,
+								style: 'default'
+							},
+							{
+								text: strings('notifications.prompt_ok'),
+								onPress: () => PushNotification.requestPermissions()
+							}
+						],
+						{ cancelable: false }
+					);
+
+					const times = (promptCount && Number(promptCount) + 1) || 1;
+					AsyncStorage.setItem('@MetaMask:pushNotificationsPromptCount', times.toString());
+					// In case we want to prompt again after certain time.
+					AsyncStorage.setItem('@MetaMask:pushNotificationsPromptTime', Date.now().toString());
+				}
+			});
+		}
+	};
+
+	/**
 	 * Returns the id of the transaction that should
 	 * be displayed and removes it from memory
 	 */
@@ -173,7 +210,7 @@ class TransactionsNotificationManager {
 
 				Platform.OS === 'ios' &&
 					setTimeout(() => {
-						PushNotification.requestPermissions();
+						this.requestPushNotificationsPermission();
 					}, 7000);
 			}, 500);
 		});
@@ -271,5 +308,8 @@ export default {
 	},
 	gotIncomingTransaction(lastBlock) {
 		return instance.gotIncomingTransaction(lastBlock);
+	},
+	requestPushNotificationsPermission() {
+		return instance.requestPushNotificationsPermission();
 	}
 };
