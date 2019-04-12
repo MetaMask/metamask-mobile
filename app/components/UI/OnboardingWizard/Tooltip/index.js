d9baad900c651bbac9f0aa2f278c60a0964527e8
Esteban Mino <efmino@uc.cl>
2019-04-12 18:00:18 -0400

    indicator style

--- app/components/UI/OnboardingWizard/Tooltip/index.js
+++ app/components/UI/OnboardingWizard/Tooltip/index.js
@@ -6,8 +6,10 @@ import StyledButton from '../../StyledButton';
 
 const styles = StyleSheet.create({
 	root: {
+		marginHorizontal: 55
+	},
+	tooltip: {
 		backgroundColor: colors.primary,
-		marginHorizontal: 55,
 		borderRadius: 5,
 		padding: 18
 	},
@@ -32,19 +34,39 @@ const styles = StyleSheet.create({
 		...fontStyles.bold,
 		color: colors.white,
 		fontSize: 18
+	},
+	triangle: {
+		width: 0,
+		height: 0,
+		backgroundColor: colors.transparent,
+		borderStyle: 'solid',
+		borderLeftWidth: 10,
+		borderRightWidth: 10,
+		borderBottomWidth: 20,
+		borderLeftColor: colors.transparent,
+		borderRightColor: colors.transparent,
+		borderBottomColor: colors.primary,
+		position: 'absolute'
+	},
+	topCenter: {
+		marginBottom: 12,
+		alignItems: 'center'
+	},
+	topLeft: {
+		marginBottom: 12,
+		alignItems: 'flex-start',
+		marginLeft: 12
 	}
 });
 
 export default class Tooltip extends Component {
 	static propTypes = {
-		/**
-		 * Object that represents the navigator
-		 */
 		content: PropTypes.string,
 		onClose: PropTypes.func,
 		onNext: PropTypes.func,
 		onBack: PropTypes.func,
-		title: PropTypes.string
+		title: PropTypes.string,
+		indicatorPosition: PropTypes.oneOf(['topCenter', 'topLeft'])
 	};
 
 	onNext = () => {
@@ -62,23 +84,37 @@ export default class Tooltip extends Component {
 		onClose && onClose();
 	};
 
+	getIndicatorStyle = indicatorPosition => {
+		const positions = {
+			topCenter: styles.topCenter,
+			topLeft: styles.topLeft,
+			[undefined]: styles.topCenter
+		};
+		return positions[indicatorPosition];
+	};
+
 	render() {
-		const { content, title } = this.props;
+		const { content, title, indicatorPosition } = this.props;
 		return (
 			<View style={styles.root}>
-				<View style={styles.titleContainer}>
-					<Text style={styles.title}>{title}</Text>
+				<View style={this.getIndicatorStyle(indicatorPosition)}>
+					<View style={styles.triangle} />
 				</View>
-				<View style={styles.contentContainer}>
-					<Text style={styles.content}>{content}</Text>
-				</View>
-				<View style={styles.actions}>
-					<StyledButton containerStyle={styles.button} type={'orange'} onPress={this.onBack}>
-						Back
-					</StyledButton>
-					<StyledButton containerStyle={styles.button} type={'warning'} onPress={this.onNext}>
-						Next
-					</StyledButton>
+				<View style={styles.tooltip}>
+					<View style={styles.titleContainer}>
+						<Text style={styles.title}>{title}</Text>
+					</View>
+					<View style={styles.contentContainer}>
+						<Text style={styles.content}>{content}</Text>
+					</View>
+					<View style={styles.actions}>
+						<StyledButton containerStyle={styles.button} type={'orange'} onPress={this.onBack}>
+							Back
+						</StyledButton>
+						<StyledButton containerStyle={styles.button} type={'warning'} onPress={this.onNext}>
+							Next
+						</StyledButton>
+					</View>
 				</View>
 			</View>
 		);
