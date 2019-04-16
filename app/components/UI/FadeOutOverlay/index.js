81653459aee91ca88ef81314f3899953c2beb8c2
Bruno Barbieri <bruno.barbieri@consensys.net>
2019-04-16 19:57:46 -0400

    Animations on App launch & resume (#606)
    * lottie working
    
    * added fadeIn / out effect
    
    * finally fix crash in debug mode
    
    * added fadeIn to other views
    
    * update unit tests
    
    * fix lock screen
    
    * adjust timing
    
    * more timing stuff
    
    * fix timing stuff
    
    * use optimized bounce animation
    
    * fix for android
    
    * update snapshots
    
    * use exact versions

---
+++ app/components/UI/FadeOutOverlay/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Animated, StyleSheet } from 'react-native';
+import { colors } from '../../../styles/common';
+
+const styles = StyleSheet.create({
+	view: {
+		backgroundColor: colors.white,
+		position: 'absolute',
+		top: 0,
+		bottom: 0,
+		left: 0,
+		right: 0
+	}
+});
+
+/**
+ * View that is displayed to first time (new) users
+ */
+export default class FadeOutOverlay extends Component {
+	static propTypes = {
+		style: PropTypes.any,
+		duration: PropTypes.number
+	};
+
+	state = {
+		done: false
+	};
+
+	opacity = new Animated.Value(1);
+
+	componentDidMount() {
+		Animated.timing(this.opacity, {
+			toValue: 0,
+			duration: this.props.duration,
+			useNativeDriver: true,
+			isInteraction: false
+		}).start(() => {
+			this.setState({ done: true });
+		});
+	}
+
+	render() {
+		if (this.state.done) return null;
+		return <Animated.View style={[{ opacity: this.opacity }, styles.view, this.props.style]} />;
+	}
+}
+
+FadeOutOverlay.defaultProps = {
+	style: null,
+	duration: 600
+};
