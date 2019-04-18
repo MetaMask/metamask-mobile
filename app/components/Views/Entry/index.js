884a20fb8ef8392412eb1da02754f75fe41ce9f9
Bruno Barbieri <bruno.barbieri@consensys.net>
2019-04-18 17:55:36 -0400

    update animation (#619)
    * update animation
    
    * update snapshots

--- app/components/Views/Entry/index.js
+++ app/components/Views/Entry/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Animated, Dimensions, StyleSheet, Image, View } from 'react-native';
+import { Animated, Dimensions, StyleSheet, View } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import Engine from '../../../core/Engine';
 import LottieView from 'lottie-react-native';
@@ -13,28 +13,37 @@ import { baseStyles } from '../../../styles/common';
  * while showing the fox
  */
 const LOGO_SIZE = 194;
-const metamask_name = require('../../../images/metamask-name.png'); // eslint-disable-line
 const styles = StyleSheet.create({
 	metamaskName: {
-		marginLeft: 6,
+		marginTop: 10,
 		height: 30,
 		width: 190,
-		alignSelf: 'center'
+		alignSelf: 'center',
+		alignItems: 'center',
+		justifyContent: 'center'
 	},
 	logoWrapper: {
 		marginTop: Dimensions.get('window').height / 2 - LOGO_SIZE / 2,
 		height: LOGO_SIZE
 	},
+	foxAndName: {
+		alignSelf: 'center',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
 	animation: {
-		width: 400,
-		height: 400
+		width: 125,
+		height: 125,
+		alignSelf: 'center',
+		alignItems: 'center',
+		justifyContent: 'center'
 	},
 	fox: {
 		width: 125,
 		height: 125,
 		alignSelf: 'center',
-		justifyContent: 'center',
-		alignItems: 'center'
+		alignItems: 'center',
+		justifyContent: 'center'
 	}
 });
 
@@ -51,6 +60,7 @@ export default class Entry extends Component {
 	};
 
 	animation = React.createRef();
+	animationName = React.createRef();
 	opacity = new Animated.Value(1);
 
 	componentDidMount() {
@@ -61,12 +71,13 @@ export default class Entry extends Component {
 			} else {
 				this.animateAndGoTo('OnboardingRootNav');
 			}
-		}, 1900);
+		}, 800);
 	}
 
 	animateAndGoTo(view) {
 		this.setState({ viewToGo: view }, () => {
 			this.animation.play();
+			this.animationName.play();
 		});
 	}
 
@@ -74,13 +85,13 @@ export default class Entry extends Component {
 		setTimeout(() => {
 			Animated.timing(this.opacity, {
 				toValue: 0,
-				duration: 400,
+				duration: 300,
 				useNativeDriver: true,
 				isInteraction: false
 			}).start(() => {
 				this.props.navigation.navigate(this.state.viewToGo);
 			});
-		}, 1750);
+		}, 100);
 	};
 
 	async unlockKeychain() {
@@ -114,16 +125,27 @@ export default class Entry extends Component {
 		}
 
 		return (
-			<LottieView
-				// eslint-disable-next-line react/jsx-no-bind
-				ref={animation => {
-					this.animation = animation;
-				}}
-				style={styles.animation}
-				loop={false}
-				source={require('../../../animations/fox-in-out.json')}
-				onAnimationFinish={this.onAnimationFinished}
-			/>
+			<View style={styles.foxAndName}>
+				<LottieView
+					// eslint-disable-next-line react/jsx-no-bind
+					ref={animation => {
+						this.animation = animation;
+					}}
+					style={styles.animation}
+					loop={false}
+					source={require('../../../animations/fox-in.json')}
+					onAnimationFinish={this.onAnimationFinished}
+				/>
+				<LottieView
+					// eslint-disable-next-line react/jsx-no-bind
+					ref={animation => {
+						this.animationName = animation;
+					}}
+					style={styles.metamaskName}
+					loop={false}
+					source={require('../../../animations/wordmark.json')}
+				/>
+			</View>
 		);
 	}
 
@@ -132,7 +154,6 @@ export default class Entry extends Component {
 			<View style={baseStyles.flexGrow}>
 				<Animated.View style={[styles.logoWrapper, { opacity: this.opacity }]}>
 					<View style={styles.fox}>{this.renderAnimations()}</View>
-					<Image source={metamask_name} style={styles.metamaskName} resizeMethod={'auto'} />
 				</Animated.View>
 			</View>
 		);
