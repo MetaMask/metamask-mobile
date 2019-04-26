345d84aeda549ca9424f45368c41f7368cf10a53
Esteban Mino <efmino@uc.cl>
2019-04-26 16:26:37 -0400

    sync from extension success

--- app/components/Views/SyncWithExtensionSuccess/index.test.js
+++ app/components/Views/SyncWithExtensionSuccess/index.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import SyncWIthExtensionSuccess from './';
+import SyncWithExtensionSuccess from './';
+import configureMockStore from 'redux-mock-store';
 
-describe('SyncWIthExtensionSuccess', () => {
+const mockStore = configureMockStore();
+
+describe('SyncWithExtensionSuccess', () => {
 	it('should render correctly', () => {
-		const wrapper = shallow(<SyncWIthExtensionSuccess />);
+		const initialState = {};
+
+		const wrapper = shallow(<SyncWithExtensionSuccess />, {
+			context: { store: mockStore(initialState) }
+		});
 		expect(wrapper).toMatchSnapshot();
 	});
 });
