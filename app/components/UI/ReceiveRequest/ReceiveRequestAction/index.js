6d49c0983dc1e16efa723a2cf10b00b2219c5b57
Esteban Miño <efmino@uc.cl>
2019-05-14 12:57:03 -0400

    Feature: payment request (#641)
    * modal payment request
    
    * add qr address modal
    
    * basic stricture done
    
    * rename
    
    * select asset payment request
    
    * basic amount input
    
    * handle payment request navigation
    
    * handle amount
    
    * handle primary currency
    
    * handle primary currency switch
    
    * get crypto amount
    
    * divide primary currency methods
    
    * add buttons
    
    * generate link
    
    * success UI
    
    * better ui
    
    * share and copy to clipboard
    
    * show and close qer modal
    
    * consolidate ui and shgow user tokens
    
    * payment request flow from asset
    
    * handle network assets and link
    
    * handle empty search and empty tokens
    
    * handle not conversion rate
    
    * handle fiat primary currency with no conversion nor exchange rate
    
    * handle decimal places
    
    * consolidate primary currency methods
    
    * fix condition
    
    * docs
    
    * more docs
    
    * qr codes update
    
    * locales
    
    * onbuy coming soon
    
    * fix android icions
    
    * update icons
    
    * snapshots
    
    * on submit next
    
    * handle payment request from qrscanner
    
    * update receive icon
    
    * snapshots
    
    * update qr codes
    
    * update qr and android
    
    * android
    
    * comments

---
+++ app/components/UI/ReceiveRequest/ReceiveRequestAction/index.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { TouchableOpacity, StyleSheet, View, Text } from 'react-native';
+import { fontStyles, colors } from '../../../../styles/common';
+
+const styles = StyleSheet.create({
+	wrapper: {
+		margin: 8,
+		borderWidth: 1,
+		borderColor: colors.blue,
+		borderRadius: 8,
+		padding: 10,
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	title: {
+		...fontStyles.bold,
+		fontSize: 14,
+		padding: 5
+	},
+	description: {
+		...fontStyles.normal,
+		fontSize: 12,
+		padding: 5,
+		textAlign: 'center',
+		color: colors.grey500
+	},
+	row: {
+		alignSelf: 'center'
+	},
+	icon: {
+		marginBottom: 5
+	}
+});
+
+/**
+ * Component that renders a receive action
+ */
+export default class ReceiveRequestAction extends Component {
+	static propTypes = {
+		/**
+		 * The navigator object
+		 */
+		icon: PropTypes.object,
+		/**
+		 * Action title
+		 */
+		actionTitle: PropTypes.string,
+		/**
+		 * Action description
+		 */
+		actionDescription: PropTypes.string,
+		/**
+		 * Custom style
+		 */
+		style: PropTypes.object,
+		/**
+		 * Callback on press action
+		 */
+		onPress: PropTypes.func
+	};
+
+	render() {
+		const { icon, actionTitle, actionDescription, style, onPress } = this.props;
+		return (
+			<TouchableOpacity onPress={onPress} style={[styles.wrapper, style]}>
+				<View style={[styles.row, styles.icon]}>{icon}</View>
+				<View style={styles.row}>
+					<Text style={styles.title}>{actionTitle}</Text>
+				</View>
+				<View style={styles.row}>
+					<Text numberOfLines={2} style={styles.description}>
+						{actionDescription}
+					</Text>
+				</View>
+			</TouchableOpacity>
+		);
+	}
+}
