fd779892b70f7877af44c06b12806cd1b89bf358
Bruno Barbieri <brunobar79@gmail.com>
2019-05-24 00:32:20 -0400

    fix approval screen

--- app/components/Nav/Main/index.js
+++ app/components/Nav/Main/index.js
@@ -397,7 +397,13 @@ class Main extends Component {
 		transactionMeta.transaction.gas = hexToBN(gas);
 		transactionMeta.transaction.gasPrice = hexToBN(gasPrice);
 		this.props.setTransactionObject({
-			...{ symbol: 'ETH', type: 'ETHER_TRANSACTION', assetType: 'ETH', id: transactionMeta.id },
+			...{
+				symbol: 'ETH',
+				selectedAsset: { isETH: true, symbol: 'ETH' },
+				type: 'ETHER_TRANSACTION',
+				assetType: 'ETH',
+				id: transactionMeta.id
+			},
 			...transactionMeta.transaction
 		});
 		this.props.navigation.push('ApprovalView');
