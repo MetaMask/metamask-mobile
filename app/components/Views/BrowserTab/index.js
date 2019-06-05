d1b3820fdcc4f5cd3d472c98286790d23ce4937b
Esteban Miño <efmino@uc.cl>
2019-06-05 18:51:00 -0400

    Bugfix: bottom bar workaround (#704)
    * fix height script
    
    * avoid rendering at bottom

--- app/components/Views/BrowserTab/index.js
+++ app/components/Views/BrowserTab/index.js
@@ -1213,10 +1213,8 @@ export class BrowserTab extends PureComponent {
 
 		// We need to get the title of the page and the height
 		const { current } = this.webview;
-
-		Platform.OS === 'ios'
-			? current.evaluateJavaScript(JS_WINDOW_INFORMATION_HEIGHT)
-			: current.injectJavaScript(JS_WINDOW_INFORMATION_HEIGHT);
+		const js = JS_WINDOW_INFORMATION_HEIGHT(Platform.OS);
+		Platform.OS === 'ios' ? current.evaluateJavaScript(js) : current.injectJavaScript(js);
 		clearTimeout(this.timeoutHandler);
 	};
 
@@ -1316,7 +1314,7 @@ export class BrowserTab extends PureComponent {
 	handleScroll = e => {
 		if (Platform.OS === 'android') return;
 
-		if (e.contentSize.height < Dimensions.get('window').height) {
+		if (e.contentSize.height < Dimensions.get('window').height - BOTTOM_NAVBAR_HEIGHT) {
 			return;
 		}
 
@@ -1328,6 +1326,11 @@ export class BrowserTab extends PureComponent {
 
 		// Avoid wrong position at the beginning
 		if ((this.state.scrollAnim._value === 0 && newOffset > BOTTOM_NAVBAR_HEIGHT) || newOffset <= 0) {
+			return;
+		}
+
+		// Avoid blocking bottom content
+		if (this.state.contentHeight - e.layoutMeasurement.height - newOffset < BOTTOM_NAVBAR_HEIGHT) {
 			return;
 		}
 
