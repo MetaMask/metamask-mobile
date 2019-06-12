e7e032bc7187378669781fae74e34ed4284d758b
Bruno Barbieri <brunobar79@gmail.com>
2019-06-12 18:28:13 -0400

    Feature: Universal deeplinks (iOS) / App Links (Android) (#715)
    * more deeplinking fixes
    
    * update manifest
    
    * android is now working
    
    * update ios settings
    
    * clean up
    
    * fix
    
    * move domain to a constant
    
    * ios fix

--- app/core/DeeplinkManager.js
+++ app/core/DeeplinkManager.js
@@ -2,24 +2,87 @@
 
 import URL from 'url-parse';
 import qs from 'qs';
+import { InteractionManager, Alert } from 'react-native';
 import { parse } from 'eth-url-parser';
 import WalletConnect from '../core/WalletConnect';
 import PaymentChannelsClient from '../core/PaymentChannelsClient';
 
-export default class DeeplinkManager {
+const MM_UNIVERSAL_LINK_HOST = 'metamask.app.link';
+
+class DeeplinkManager {
 	constructor(_navigation) {
 		this.navigation = _navigation;
 	}
 
-	parse(url) {
+	handleEthereumUrl(url) {
+		let action;
+		const ethUrl = parse(url);
+		action = 'send-eth';
+		if (ethUrl.function_name === 'transfer') {
+			// Send erc20 token
+			action = 'send-token';
+		} else if (ethUrl.function_name) {
+			// Other smart contract interaction
+			// That involves txs
+			action = 'smart-contract-interaction';
+		}
+		this.navigation.navigate('SendView', { txMeta: { ...ethUrl, action, source: url } });
+	}
+
+	handleBrowserUrl(url, callback) {
+		this.navigation.navigate('BrowserTabHome');
+		InteractionManager.runAfterInteractions(() => {
+			callback(url);
+		});
+	}
+
+	handlePaymentChannelsUrl(to, params) {
+		PaymentChannelsClient.hub.emit('payment::request', {
+			to,
+			...params
+		});
+	}
+
+	parse(url, browserCallBack = null) {
 		const urlObj = new URL(url);
-		let ethUrl, action, params;
+		let params;
 
 		if (urlObj.query.length) {
 			params = qs.parse(urlObj.query.substring(1));
 		}
-
 		switch (urlObj.protocol.replace(':', '')) {
+			case 'http':
+			case 'https':
+				// Universal links
+				if (urlObj.hostname === MM_UNIVERSAL_LINK_HOST) {
+					// action is the first parth of the pathname
+					const action = urlObj.pathname.split('/')[1];
+					switch (action) {
+						case 'dapp':
+							this.handleBrowserUrl(
+								urlObj.href.replace(`https://${MM_UNIVERSAL_LINK_HOST}/dapp/`, 'https://'),
+								browserCallBack
+							);
+							break;
+						case 'send':
+							this.handleEthereumUrl(
+								urlObj.href.replace(`https://${MM_UNIVERSAL_LINK_HOST}/send/`, 'ethereum:')
+							);
+							break;
+						case 'payment':
+							this.handlePaymentChannelsUrl(urlObj.pathname.replace('/payment/', ''), params);
+							break;
+
+						default:
+							Alert.alert('Error', 'invalid deeplink');
+					}
+				} else {
+					// Normal links (same as dapp)
+					urlObj.set('protocol', 'https:');
+					this.handleBrowserUrl(urlObj.href, browserCallBack);
+				}
+				break;
+
 			// walletconnect related deeplinks
 			// address, transactions, etc
 			case 'wc':
@@ -35,17 +98,7 @@ export default class DeeplinkManager {
 				}
 				break;
 			case 'ethereum':
-				ethUrl = parse(url);
-				action = 'send-eth';
-				if (ethUrl.function_name === 'transfer') {
-					// Send erc20 token
-					action = 'send-token';
-				} else if (ethUrl.function_name) {
-					// Other smart contract interaction
-					// That involves txs
-					action = 'smart-contract-interaction';
-				}
-				this.navigation.navigate('SendView', { txMeta: { ...ethUrl, action, source: url } });
+				this.handleEthereumUrl(url);
 
 				break;
 
@@ -54,22 +107,35 @@ export default class DeeplinkManager {
 			case 'dapp':
 				// Enforce https
 				urlObj.set('protocol', 'https:');
-
-				this.navigation.navigate('BrowserView', {
-					url: urlObj.href
-				});
+				this.handleBrowserUrl(urlObj.href, browserCallBack);
 				break;
 
 			// Specific to the MetaMask app
 			// For ex. go to settings
 			case 'metamask':
 				if (urlObj.hostname === 'payment') {
-					PaymentChannelsClient.hub.emit('payment::request', {
-						to: urlObj.pathname.replace('/', ''),
-						...params
-					});
+					this.handlePaymentChannelsUrl(urlObj.pathname.replace('/', ''), params);
 				}
 				break;
 		}
 	}
 }
+
+let instance = null;
+let pendingDeeplink = null;
+
+const SharedDeeplinkManager = {
+	init: navigation => {
+		instance = new DeeplinkManager(navigation);
+	},
+	parse: (url, browserCallback) => instance.parse(url, browserCallback),
+	setDeeplink: url => {
+		pendingDeeplink = url;
+	},
+	getPendingDeeplink: () => pendingDeeplink,
+	expireDeeplink: () => {
+		pendingDeeplink = null;
+	}
+};
+
+export default SharedDeeplinkManager;
