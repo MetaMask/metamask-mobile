3c43a8ffd5924fa50dc73febc614c0033e9b4ed7
Esteban Mino <efmino@uc.cl>
2019-06-26 11:51:21 -0400

    card image

--- app/components/Views/AssetCard/index.js
+++ app/components/Views/AssetCard/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, ImageBackground } from 'react-native';
 import { colors, fontStyles } from '../../../styles/common';
 import PropTypes from 'prop-types';
 import ElevatedView from 'react-native-elevated-view';
@@ -28,8 +28,20 @@ const styles = StyleSheet.create({
 		...fontStyles.normal,
 		fontSize: 12,
 		color: colors.grey200
+	},
+	watermarkWrapper: {
+		flex: 1
+	},
+	watermarkImage: {
+		flex: 1,
+		borderRadius: 8,
+		width: '70%',
+		left: '30%',
+		opacity: 0.5
 	}
 });
+
+const paymentChannelWatermark = require('../../../images/payment-channel-watermark.png'); // eslint-disable-line
 
 /**
  * View that displays an asset card
@@ -37,20 +49,24 @@ const styles = StyleSheet.create({
 export default class AssetCard extends Component {
 	static propTypes = {
 		balance: PropTypes.string,
-		balanceFiat: PropTypes.string,
-		children: PropTypes.object
+		balanceFiat: PropTypes.string
 	};
 
 	render() {
 		const { balance, balanceFiat } = this.props;
 		return (
 			<ElevatedView elevation={10} style={styles.wrapper}>
-				<View style={styles.contentWrapper}>
-					<Text style={styles.title}>Balance</Text>
-					<Text style={styles.balance}>{balance}</Text>
-					<Text style={styles.balanceFiat}>{balanceFiat}</Text>
-					{this.props.children}
-				</View>
+				<ImageBackground
+					source={paymentChannelWatermark}
+					style={styles.watermarkWrapper}
+					imageStyle={styles.watermarkImage}
+				>
+					<View style={styles.contentWrapper}>
+						<Text style={styles.title}>Balance</Text>
+						<Text style={styles.balance}>{balance}</Text>
+						<Text style={styles.balanceFiat}>{balanceFiat}</Text>
+					</View>
+				</ImageBackground>
 			</ElevatedView>
 		);
 	}
