2e632db8945f0c4465918100272bb761ffd0b1a9
Bruno Barbieri <bruno.barbieri@consensys.net>
2019-07-15 19:48:31 -0400

    Release 0.1.9 to master (#823)
    * attempt to fix and get info
    
    * enable android app bundles
    
    * update homepage url
    
    * Update navigation deps to latest versions (#818)
    
    * update deps
    
    * update snapshots
    
    * Bugfix: improve payment deposit
    
    * Bugfix: onboarding android (#820)
    
    * fix skip
    
    * snaps
    
    * rm
    
    * min height
    
    * release 0.1.9 (#822)

--- app/components/UI/OnboardingWizard/index.js
+++ app/components/UI/OnboardingWizard/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Platform, TouchableOpacity, View, StyleSheet, Text } from 'react-native';
+import { Platform, TouchableOpacity, View, StyleSheet, Text, Dimensions } from 'react-native';
 import { colors, fontStyles } from '../../../styles/common';
 import { connect } from 'react-redux';
 import Step1 from './Step1';
@@ -18,6 +18,7 @@ import ElevatedView from 'react-native-elevated-view';
 import Modal from 'react-native-modal';
 import DeviceSize from '../../../util/DeviceSize';
 
+const MIN_HEIGHT = Dimensions.get('window').height;
 const styles = StyleSheet.create({
 	root: {
 		top: 0,
@@ -26,7 +27,8 @@ const styles = StyleSheet.create({
 		right: 0,
 		flex: 1,
 		margin: 0,
-		position: 'absolute'
+		position: 'absolute',
+		backgroundColor: colors.transparent
 	},
 	main: {
 		flex: 1,
@@ -118,7 +120,7 @@ class OnboardingWizard extends Component {
 				backdropOpacity={0}
 				disableAnimation
 				transparent
-				style={[styles.root, { backgroundColor: colors.transparent }]}
+				style={[styles.root, Platform.OS === 'android' ? { minHeight: MIN_HEIGHT } : {}]}
 			>
 				<View style={styles.main}>{this.onboardingWizardNavigator(step)}</View>
 				{step !== 1 && (
