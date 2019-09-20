ebdc7553efd06de1a677df90f267fddf9f1c1cb1
Esteban Mino <efmino@uc.cl>
2019-09-20 14:20:32 -0300

    done

--- app/components/Views/AddressQRCode/index.js
+++ app/components/Views/AddressQRCode/index.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Platform, TouchableOpacity, Dimensions, StyleSheet, View, Text, Clipboard } from 'react-native';
+import { TouchableOpacity, Dimensions, StyleSheet, View, Text, Clipboard } from 'react-native';
 import { colors, fontStyles } from '../../../styles/common';
 import { toChecksumAddress } from 'ethereumjs-util';
 import { connect } from 'react-redux';
@@ -11,52 +11,51 @@ import DeviceSize from '../../../util/DeviceSize';
 import { showAlert } from '../../../actions/alert';
 import GlobalAlert from '../../UI/GlobalAlert';
 
+const WIDTH = Dimensions.get('window').width - 88;
+
 const styles = StyleSheet.create({
-	detailsWrapper: {
-		padding: 10,
+	root: {
+		flex: 1,
+		flexDirection: 'row',
+		alignItems: 'center',
+		marginTop: DeviceSize.isSmallDevice() ? -30 : -50
+	},
+	wrapper: {
+		flex: 1,
 		alignItems: 'center'
 	},
 	qrCode: {
 		marginBottom: 16,
 		alignItems: 'center',
 		justifyContent: 'center',
-		paddingHorizontal: 36,
-		paddingBottom: 24,
-		paddingTop: 16,
+		padding: 36,
 		backgroundColor: colors.grey000,
 		borderRadius: 8
-	},
-	qrCodeWrapper: {
-		borderColor: colors.grey300,
-		borderRadius: 8,
-		borderWidth: 1,
-		padding: 15
 	},
 	addressWrapper: {
 		alignItems: 'center',
 		justifyContent: 'center',
-		paddingHorizontal: 16,
-		paddingTop: 16,
-		marginTop: 10,
-		borderRadius: 5,
-		backgroundColor: colors.grey000
-	},
-	titleQr: {
-		flexDirection: 'row'
+		width: WIDTH,
+		borderRadius: 8,
+		backgroundColor: colors.grey000,
+		paddingVertical: 12
 	},
 	closeIcon: {
-		position: 'absolute',
-		right: DeviceSize.isSmallDevice() ? (Platform.OS === 'ios' ? -30 : -30) : Platform.OS === 'ios' ? -40 : -50,
-		bottom: Platform.OS === 'ios' ? 8 : 10
+		width: WIDTH + 40,
+		paddingBottom: DeviceSize.isSmallDevice() ? 30 : 50,
+		flexDirection: 'row-reverse'
 	},
 	addressTitle: {
 		fontSize: 16,
-		marginBottom: 16,
+		paddingHorizontal: 28,
+		paddingVertical: 4,
 		...fontStyles.normal
 	},
 	address: {
 		...fontStyles.normal,
-		fontSize: Platform.OS === 'ios' ? 14 : 20,
+		paddingHorizontal: 28,
+		paddingVertical: 4,
+		fontSize: 16,
 		textAlign: 'center'
 	}
 });
@@ -74,6 +73,9 @@ class AddressQRCode extends PureComponent {
 		/* Triggers global alert
 		*/
 		showAlert: PropTypes.func,
+		/**
+		/* Callback to close the modal
+		*/
 		closeQrModal: PropTypes.func
 	};
 
@@ -96,25 +98,34 @@ class AddressQRCode extends PureComponent {
 		});
 	};
 
+	processAddress = () => {
+		const { selectedAddress } = this.props;
+		const processedAddress = `${selectedAddress.slice(0, 2)} ${selectedAddress
+			.slice(2)
+			.match(/.{1,4}/g)
+			.join(' ')}`;
+		return processedAddress;
+	};
+
 	render() {
 		return (
-			<View style={styles.detailsWrapper}>
-				<View style={styles.qrCode}>
-					<View style={styles.titleQr}>
-						<Text style={styles.addressTitle}>{strings('receive_request.public_address_qr_code')}</Text>
-						<TouchableOpacity style={styles.closeIcon} onPress={this.closeQrModal}>
-							<IonicIcon name={'ios-close'} size={28} color={colors.black} />
-						</TouchableOpacity>
-					</View>
-					<View style={styles.qrCodeWrapper}>
+			<View style={styles.root}>
+				<View style={styles.wrapper}>
+					<TouchableOpacity style={styles.closeIcon} onPress={this.closeQrModal}>
+						<IonicIcon name={'ios-close'} size={38} color={colors.white} />
+					</TouchableOpacity>
+					<View style={styles.qrCode}>
 						<QRCode
 							value={`ethereum:${this.props.selectedAddress}`}
 							size={Dimensions.get('window').width - 160}
 						/>
 					</View>
-					<TouchableOpacity style={styles.addressWrapper} onPress={this.copyAccountToClipboard}>
-						<Text style={styles.address}>{this.props.selectedAddress}</Text>
-					</TouchableOpacity>
+					<View style={styles.addressWrapper}>
+						<Text style={styles.addressTitle}>{strings('receive_request.public_address_qr_code')}</Text>
+						<TouchableOpacity onPress={this.copyAccountToClipboard}>
+							<Text style={styles.address}>{this.processAddress()}</Text>
+						</TouchableOpacity>
+					</View>
 				</View>
 				<GlobalAlert />
 			</View>
