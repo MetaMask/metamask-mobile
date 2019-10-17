b6af3fcd3c946a0c0e5f299000b976f00dad1b71
Esteban Mino <efmino@uc.cl>
2019-10-17 18:40:39 +0900

    snaps

--- app/store/index.js
+++ app/store/index.js
@@ -1,13 +1,31 @@
 import { createStore } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer, createMigrate } from 'redux-persist';
 import AsyncStorage from '@react-native-community/async-storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import rootReducer from '../reducers';
 
+const migrations = {
+	// Needed after https://github.com/MetaMask/gaba/pull/152
+	0: state => {
+		const addressBook = state.engine.backgroundState.AddressBookController.addressBook;
+		const migratedAddressBook = {};
+		Object.keys(addressBook).forEach(address => {
+			const chainId = addressBook[address].chainId;
+			migratedAddressBook[chainId]
+				? (migratedAddressBook[chainId] = { ...migratedAddressBook[chainId], [address]: addressBook[address] })
+				: (migratedAddressBook[chainId] = { [address]: addressBook[address] });
+		});
+		state.engine.backgroundState.AddressBookController.addressBook = migratedAddressBook;
+		return state;
+	}
+};
+
 const persistConfig = {
 	key: 'root',
+	version: 0,
 	storage: AsyncStorage,
-	stateReconciler: autoMergeLevel2 // see "Merge Process" section for details.
+	stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
+	migrate: createMigrate(migrations, { debug: true })
 };
 
 const pReducer = persistReducer(persistConfig, rootReducer);
