df453e252c32d8ad6b6cf2fe7d4517ef6654965d
Bruno Barbieri <brunobar79@gmail.com>
2019-11-13 17:15:12 -0500

    updates

--- app/store/index.js
+++ app/store/index.js
@@ -3,6 +3,7 @@ import { persistStore, persistReducer, createMigrate } from 'redux-persist';
 import AsyncStorage from '@react-native-community/async-storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import rootReducer from '../reducers';
+import AppConstants from '../core/AppConstants';
 
 const migrations = {
 	// Needed after https://github.com/MetaMask/gaba/pull/152
@@ -17,6 +18,20 @@ const migrations = {
 		});
 		state.engine.backgroundState.AddressBookController.addressBook = migratedAddressBook;
 		return state;
+	},
+	// MakerDAO DAI => SAI
+	1: state => {
+		const tokens = state.engine.backgroundState.AssetsController.tokens;
+		const migratedTokens = [];
+		tokens.forEach(token => {
+			if (token.symbol === 'DAI' && token.address.toLowerCase() === AppConstants.SAI_ADDRESS.toLowerCase()) {
+				token.symbol = 'SAI';
+			}
+			migratedTokens.push(token);
+		});
+		state.engine.backgroundState.AssetsController.tokens = migratedTokens;
+
+		return state;
 	}
 };
 
@@ -25,7 +40,7 @@ const persistConfig = {
 	version: 0,
 	storage: AsyncStorage,
 	stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
-	migrate: createMigrate(migrations, { debug: false })
+	migrate: createMigrate(migrations, { debug: true })
 };
 
 const pReducer = persistReducer(persistConfig, rootReducer);
