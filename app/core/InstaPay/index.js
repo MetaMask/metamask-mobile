84de610b016721f6b6fc14600fe30dad0d03bcd9
Bruno Barbieri <brunobar79@gmail.com>
2020-01-24 16:59:00 -0500

    fix compatibility issue with connext

--- app/core/InstaPay/index.js
+++ app/core/InstaPay/index.js
@@ -208,13 +208,6 @@ class InstaPay {
 			username: null
 		});
 
-		try {
-			await this.addDefaultPaymentProfile();
-		} catch (e) {
-			Logger.error('InstaPay :: Error adding default payment profile...', e);
-		}
-
-		// await channel.restoreState();
 		await this.startPoller();
 	};
 
