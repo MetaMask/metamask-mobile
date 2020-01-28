1f8c99a161a56515ba13e0be3a543eb3620deb0c
Erik Marks <rekmarks@protonmail.com>
2020-01-27 19:31:46 -0800

    init git submodules in postinstall

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -148,3 +148,6 @@ rm -rf node_modules/react-native/React/React.xcodeproj/project.pbxproj.bak
 echo "15. Remove Branch examples"
 rm -rf node_modules/react-native-branch/examples/
 rm -rf node_modules/react-native-branch/native-tests/
+
+echo "16. Init git submodules"
+git submodule update --init
