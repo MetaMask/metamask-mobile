79b87e2278a8325dfa98e82a495615292047b737
Esteban Miño <efmino@uc.cl>
2020-01-30 19:54:18 -0300

    Feature: approve screen (#1251)
    * approve, tx direction & pass origin
    
    * basic info section
    
    * tx fee section
    
    * other details
    
    * add icons
    
    * consolidate styles
    
    * more icons
    
    * gas modal
    
    * total gas and gas fiat
    
    * total gas and gas fiat
    
    * spend permission ui
    
    * handle keyboard on custom spend limit
    
    * onspendlimtchange
    
    * handl eapprove data generation
    
    * oncancel
    
    * text input key
    
    * locales
    
    * gas validation
    
    * minor
    
    * validate gas
    
    * style
    
    * copy contract address
    
    * more validations
    
    * approve success
    
    * enable save permission
    
    * gas validation
    
    * send the tx
    
    * android text color
    
    * add balance
    
    * snaps
    
    * snaps
    
    * fix build
    
    * fix gethost
    
    * fix limit
    
    * snaps
    
    * fix legacy transaction state issue
    
    * parseint
    
    * validate decimal

---
+++ app/components/Views/ApproveView/Approve/index.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Approve from './';
+import configureMockStore from 'redux-mock-store';
+import { BN } from 'gaba';
+
+const mockStore = configureMockStore();
+
+describe('Approve', () => {
+	it('should render correctly', () => {
+		const initialState = {
+			transaction: {},
+			engine: {
+				backgroundState: {
+					AccountTrackerController: {
+						accounts: { '0x2': { balance: '0' } }
+					},
+					NetworkController: {
+						provider: {
+							ticker: 'ETH'
+						}
+					},
+					TransactionController: {
+						transactions: []
+					},
+					CurrencyRateController: {
+						currentCurrency: 'USD',
+						conversionRate: 100
+					},
+					PreferencesController: {
+						selectedAddress: '0x1',
+						identities: { '0x1': { name: 'Account 1' } }
+					},
+					TokenBalancesController: {
+						contractBalances: { '0x2': new BN(0) }
+					}
+				}
+			}
+		};
+
+		const wrapper = shallow(<Approve />, {
+			context: { store: mockStore(initialState) }
+		});
+		expect(wrapper).toMatchSnapshot();
+	});
+});
