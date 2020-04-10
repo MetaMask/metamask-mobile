a52d8f315b97b314ba6a51bd27ebe1b3a03b974f
andrepimenta <andrepimenta7@gmail.com>
2020-04-10 19:18:13 +0100

    android running but with RN errors

--- android/app/src/main/java/io/metamask/MainApplication.java
+++ android/app/src/main/java/io/metamask/MainApplication.java
@@ -4,33 +4,10 @@ import com.facebook.react.ReactApplication;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactInstanceManager;
-import io.sentry.RNSentryPackage;
-import com.sensors.RNSensorsPackage;
-import com.swmansion.reanimated.ReanimatedPackage;
-import com.reactnativecommunity.webview.RNCWebViewPackage;
-import com.reactnativecommunity.netinfo.NetInfoPackage;
-import fr.greweb.reactnativeviewshot.RNViewShotPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
-import com.ocetnik.timer.BackgroundTimerPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.horcrux.svg.SvgPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import io.branch.rnbranch.RNBranchPackage;
 import io.branch.rnbranch.RNBranchModule;
-import io.metamask.nativeModules.RCTAnalyticsPackage;
-import io.metamask.nativeModules.PreventScreenshotPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import cl.json.RNSharePackage;
-import com.bitgo.randombytes.RandomBytesPackage;
-import com.peel.react.rnos.RNOSModule;
-import com.oblador.keychain.KeychainPackage;
-import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
-import com.rnfs.RNFSPackage;
-import org.reactnative.camera.RNCameraPackage;
-import com.tectiv3.aes.RCTAesPackage;
-import com.swmansion.rnscreens.RNScreensPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -53,34 +30,10 @@ public class MainApplication extends MultiDexApplication implements ShareApplica
 		protected List<ReactPackage> getPackages() {
 			@SuppressWarnings("UnnecessaryLocalVariable")
 			List<ReactPackage> packages = new PackageList(this).getPackages();
-				packages.add(new MainReactPackage());
-				packages.add(new RNSentryPackage());
-				packages.add(new RNSensorsPackage());
-				packages.add(new ReanimatedPackage());
-				packages.add(new RNCWebViewPackage());
-				packages.add(new NetInfoPackage());
-				packages.add(new RNViewShotPackage());
-				packages.add(new LottiePackage());
-				packages.add(new AsyncStoragePackage());
-				packages.add(new ReactNativePushNotificationPackage());
-				packages.add(new BackgroundTimerPackage());
-				packages.add(new RNDeviceInfo());
-				packages.add(new SvgPackage());
-				packages.add(new RNGestureHandlerPackage());
-				packages.add(new RNScreensPackage());
-				packages.add(new RNBranchPackage());
-				packages.add(new KeychainPackage());
-				packages.add(new RandomBytesPackage());
-				packages.add(new RCTAesPackage());
-				packages.add(new RNCameraPackage());
-				packages.add(new RNFSPackage());
-				packages.add(new RNI18nPackage());
-				packages.add(new RNOSModule());
-				packages.add(new RNSharePackage());
-				packages.add(new VectorIconsPackage());
-				packages.add(new RCTAnalyticsPackage());
+			packages.add(new LottiePackage());
+			packages.add(new RNGestureHandlerPackage());
 
-				return packages;
+			return packages;
 		}
 
 		@Override
@@ -97,9 +50,9 @@ public class MainApplication extends MultiDexApplication implements ShareApplica
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		SoLoader.init(this, /* native exopackage */ false);
 
 		RNBranchModule.getAutoInstance(this);
-		SoLoader.init(this, /* native exopackage */ false);
 		initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     }
     /**
@@ -130,7 +83,8 @@ public class MainApplication extends MultiDexApplication implements ShareApplica
     		} catch (InvocationTargetException e) {
     		  e.printStackTrace();
     		}
-    	}
+		}
+	}
 
 	@Override
 	public String getFileProviderAuthority() {
--- react-native.config.js
+++ react-native.config.js
@@ -21,6 +21,11 @@ module.exports = {
 			platforms: {
 				ios: null // disable Android platform, other platforms will still autolink if provided
 			}
+		},
+		'react-native-gesture-handler': {
+			platforms: {
+				android: null // disable Android platform, other platforms will still autolink if provided
+			}
 		}
 	}
 };
