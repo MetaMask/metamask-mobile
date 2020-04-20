ed2b81d11a1a2eda11a9d15d03163caedd60f3d6
Etienne Dusseault <etienne.dusseault@gmail.com>
2020-04-20 14:04:50 +0800

    TypedSign: use ExpandedMessage component

--- app/components/UI/TypedSign/index.js
+++ app/components/UI/TypedSign/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, View, Text, TouchableOpacity, ScrollView } from 'react-native';
-import { colors, fontStyles, baseStyles } from '../../../styles/common';
+import { StyleSheet, View, Text } from 'react-native';
+import { colors, fontStyles } from '../../../styles/common';
 import Engine from '../../../core/Engine';
 import SignatureRequest from '../SignatureRequest';
-import WebsiteIcon from '../WebsiteIcon';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import ExpandedMessage from '../SignatureRequest/ExpandedMessage';
 import { strings } from '../../../../locales/i18n';
 import Device from '../../../util/Device';
-import { getHost } from '../../../util/browser';
 
 const styles = StyleSheet.create({
 	root: {
@@ -17,54 +15,6 @@ const styles = StyleSheet.create({
 		borderTopLeftRadius: 10,
 		borderTopRightRadius: 10,
 		paddingBottom: Device.isIphoneX() ? 20 : 0
-	},
-	expandedRoot: {
-		...baseStyles.flexGrow,
-		backgroundColor: colors.white,
-		maxHeight: '75%',
-		borderTopLeftRadius: 10,
-		borderTopRightRadius: 10,
-		paddingHorizontal: 16,
-		paddingTop: 16,
-		paddingBottom: Device.isIphoneX() ? 20 : 0
-	},
-	expandedMessageHeader: {
-		width: '100%',
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		marginBottom: 20
-	},
-	arrowIcon: {
-		...baseStyles.flexGrow,
-		color: colors.grey200
-	},
-	iconHidden: {
-		...baseStyles.flexGrow
-	},
-	messageLabelTextExpanded: {
-		...baseStyles.flexGrow,
-		textAlign: 'center',
-		...fontStyles.bold,
-		fontSize: 16
-	},
-	messageIntroWrapper: {
-		alignItems: 'center',
-		marginBottom: 20
-	},
-	domainLogo: {
-		width: 40,
-		height: 40,
-		borderRadius: 20,
-		marginBottom: 20
-	},
-	messageFromLabel: {
-		textAlign: 'center',
-		...fontStyles.bold,
-		fontSize: 16
-	},
-	scrollView: {
-		flex: 1
 	},
 	informationCol: {
 		width: '75%',
@@ -199,27 +149,16 @@ export default class TypedSign extends React.Component {
 
 	renderRoot = () => {
 		const { messageParams, currentPageInformation } = this.props;
-		const url = currentPageInformation.url;
-		const title = getHost(url);
 		let domain;
 		if (messageParams.version === 'V3') {
 			domain = JSON.parse(messageParams.data).domain;
 		}
 		const rootView = this.state.showExpandedMessage ? (
-			<View style={styles.expandedRoot}>
-				<TouchableOpacity style={styles.expandedMessageHeader} onPress={this.toggleExpandedMessage}>
-					<Ionicons name={'ios-arrow-back'} size={30} style={styles.arrowIcon} />
-					<Text style={styles.messageLabelTextExpanded}>{strings('signature_request.message')}</Text>
-					<View style={styles.iconHidden} />
-				</TouchableOpacity>
-				<View style={styles.messageIntroWrapper}>
-					<WebsiteIcon style={styles.domainLogo} title={title} url={url} />
-					<Text style={styles.messageFromLabel}>
-						{strings('signature_request.message_from')} {title}
-					</Text>
-				</View>
-				<ScrollView style={styles.scrollView}>{this.renderTypedMessage()}</ScrollView>
-			</View>
+			<ExpandedMessage
+				currentPageInformation={currentPageInformation}
+				renderMessage={this.renderTypedMessage}
+				toggleExpandedMessage={this.toggleExpandedMessage}
+			/>
 		) : (
 			<View style={styles.root}>
 				<SignatureRequest
