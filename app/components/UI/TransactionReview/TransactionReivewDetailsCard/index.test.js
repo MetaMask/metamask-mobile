927e49f6bbe311a8a06732f9a2edbea7f7f069ef
rickycodes <ricky.miller@gmail.com>
2020-05-25 11:34:17 -0400

    Update test

--- app/components/UI/TransactionReview/TransactionReivewDetailsCard/index.test.js
+++ app/components/UI/TransactionReview/TransactionReivewDetailsCard/index.test.js
@@ -13,6 +13,25 @@ describe('TransactionReviewDetailsCard', () => {
 				address="0xcE25...233a"
 				allowance="70000"
 				data="0x095ea7b30000000000000000000000009bc5baf874d2da8d216ae9f137804184ee5afef40000000000000000000000000000000000000000000000000000000000011170"
+				displayViewData
+				host="metamask.github.io"
+				method="Approve"
+				tokenSymbol="TST"
+			/>,
+			{
+				context: { store: mockStore(initialState) }
+			}
+		);
+		expect(wrapper).toMatchSnapshot();
+	});
+	it('should render correctly if displayViewData is false', () => {
+		const initialState = {};
+
+		const wrapper = shallow(
+			<TransactionReviewDetailsCard
+				address="0xcE25...233a"
+				allowance="70000"
+				data="0x095ea7b30000000000000000000000009bc5baf874d2da8d216ae9f137804184ee5afef40000000000000000000000000000000000000000000000000000000000011170"
 				displayViewData={false}
 				host="metamask.github.io"
 				method="Approve"
