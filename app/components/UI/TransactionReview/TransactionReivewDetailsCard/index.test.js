92cb816bdd22fe21f72f75c7ed3a7315bba741d7
rickycodes <ricky.miller@gmail.com>
2020-05-25 11:16:53 -0400

    Add snapshot test for TransactionReivewDetailsCard

---
+++ app/components/UI/TransactionReview/TransactionReivewDetailsCard/index.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import TransactionReviewDetailsCard from './';
+import configureMockStore from 'redux-mock-store';
+
+describe('TransactionReviewDetailsCard', () => {
+	const mockStore = configureMockStore();
+	it('should render correctly', () => {
+		const initialState = {
+			engine: {
+				backgroundState: {
+					TokenRatesController: {
+						contractExchangeRates: {}
+					},
+					CurrencyRateController: {
+						currentCurrency: 'usd',
+						conversionRate: 0.1
+					}
+				}
+			},
+			transaction: {
+				value: '',
+				data: '',
+				from: '0x1',
+				gas: '',
+				gasPrice: '',
+				to: '0x2',
+				selectedAsset: undefined,
+				assetType: undefined
+			}
+		};
+
+		const wrapper = shallow(
+			<TransactionReviewDetailsCard
+				address="0xcE25...233a"
+				allowance="70000"
+				data="0x095ea7b30000000000000000000000009bc5baf874d2da8d216ae9f137804184ee5afef40000000000000000000000000000000000000000000000000000000000011170"
+				displayViewData={false}
+				host="metamask.github.io"
+				method="Approve"
+				tokenSymbol="TST"
+			/>,
+			{
+				context: { store: mockStore(initialState) }
+			}
+		);
+		expect(wrapper).toMatchSnapshot();
+	});
+});
