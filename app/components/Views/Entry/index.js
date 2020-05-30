e0c446a887f9c3c829c722fd969999888193196d
Andre Pimenta <andrepimenta7@gmail.com>
2020-05-30 17:50:09 +0100

    remove set timeouts (#1555)
    Co-authored-by: Ibrahim Taveras <ibrahimtaveras00@gmail.com>

--- app/components/Views/Entry/index.js
+++ app/components/Views/Entry/index.js
@@ -87,18 +87,15 @@ class Entry extends PureComponent {
 	animationName = React.createRef();
 	opacity = new Animated.Value(1);
 
-	componentDidMount() {
+	async componentDidMount() {
 		DeeplinkManager.init(this.props.navigation);
 		this.unsubscribeFromBranch = Branch.subscribe(this.handleDeeplinks);
-
-		setTimeout(async () => {
-			const existingUser = await AsyncStorage.getItem('@MetaMask:existingUser');
-			if (existingUser !== null) {
-				await this.unlockKeychain();
-			} else {
-				this.animateAndGoTo('OnboardingRootNav');
-			}
-		}, 100);
+		const existingUser = await AsyncStorage.getItem('@MetaMask:existingUser');
+		if (existingUser !== null) {
+			await this.unlockKeychain();
+		} else {
+			this.animateAndGoTo('OnboardingRootNav');
+		}
 	}
 
 	handleDeeplinks = async ({ error, params }) => {
@@ -121,12 +118,8 @@ class Entry extends PureComponent {
 	animateAndGoTo(view) {
 		this.setState({ viewToGo: view }, () => {
 			if (Device.isAndroid()) {
-				setTimeout(() => {
-					this.animation ? this.animation.play(0, 25) : this.onAnimationFinished();
-					setTimeout(() => {
-						this.animationName && this.animationName.play();
-					}, 1);
-				}, 50);
+				this.animation ? this.animation.play(0, 25) : this.onAnimationFinished();
+				this.animationName && this.animationName.play();
 			} else {
 				this.animation.play();
 				this.animationName.play();
@@ -136,30 +129,24 @@ class Entry extends PureComponent {
 
 	onAnimationFinished = () => {
 		const { viewToGo } = this.state;
-		setTimeout(() => {
-			Animated.timing(this.opacity, {
-				toValue: 0,
-				duration: 300,
-				useNativeDriver: true,
-				isInteraction: false
-			}).start(() => {
-				if (viewToGo !== 'WalletView' || viewToGo !== 'Onboarding') {
-					this.props.navigation.navigate(viewToGo);
-				} else if (viewToGo === 'Onboarding') {
-					this.props.navigation.navigate(
-						'OnboardingRootNav',
-						{},
-						NavigationActions.navigate({ routeName: 'Oboarding' })
-					);
-				} else {
-					this.props.navigation.navigate(
-						'HomeNav',
-						{},
-						NavigationActions.navigate({ routeName: 'WalletView' })
-					);
-				}
-			});
-		}, 100);
+		Animated.timing(this.opacity, {
+			toValue: 0,
+			duration: 300,
+			useNativeDriver: true,
+			isInteraction: false
+		}).start(() => {
+			if (viewToGo !== 'WalletView' || viewToGo !== 'Onboarding') {
+				this.props.navigation.navigate(viewToGo);
+			} else if (viewToGo === 'Onboarding') {
+				this.props.navigation.navigate(
+					'OnboardingRootNav',
+					{},
+					NavigationActions.navigate({ routeName: 'Oboarding' })
+				);
+			} else {
+				this.props.navigation.navigate('HomeNav', {}, NavigationActions.navigate({ routeName: 'WalletView' }));
+			}
+		});
 	};
 
 	async unlockKeychain() {
