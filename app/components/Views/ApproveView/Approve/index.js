6d292b4ca225a87313da19281c426d99e7de6463
rickycodes <ricky.miller@gmail.com>
2020-06-01 13:22:57 -0400

    Include getMethodData

--- app/components/Views/ApproveView/Approve/index.js
+++ app/components/Views/ApproveView/Approve/index.js
@@ -29,7 +29,8 @@ import {
 	getTicker,
 	decodeTransferData,
 	generateApproveData,
-	getNormalizedTxState
+	getNormalizedTxState,
+	getMethodData
 } from '../../../../util/transactions';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import ErrorMessage from '../../SendFlow/ErrorMessage';
@@ -421,8 +422,11 @@ class Approve extends PureComponent {
 		}
 		const originalApproveAmount = decodeTransferData('transfer', data)[1];
 		const totalGas = gas.mul(gasPrice);
+		const { name: method } = await getMethodData(data);
+
 		this.setState({
 			host,
+			method,
 			originalApproveAmount,
 			tokenDecimals,
 			tokenSymbol,
