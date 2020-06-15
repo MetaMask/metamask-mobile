a277fe12596391b92a5db6c4ebdf13862cf8579b
ricky <ricky.miller@gmail.com>
2020-06-15 17:05:26 -0400

    Fix network color indicator on transactionHeader for connect screens (#1628)
    * Use network colors

--- app/components/UI/NavbarBrowserTitle/index.js
+++ app/components/UI/NavbarBrowserTitle/index.js
@@ -119,7 +119,7 @@ class NavbarBrowserTitle extends PureComponent {
 					</Text>
 				</View>
 				<View style={styles.network}>
-					<View style={[styles.networkIcon, color ? { backgroundColor: color } : styles.otherNetworkIcon]} />
+					<View style={[styles.networkIcon, { backgroundColor: color || colors.red }]} />
 					<Text style={styles.networkName} testID={'navbar-title-network'}>
 						{name}
 					</Text>
