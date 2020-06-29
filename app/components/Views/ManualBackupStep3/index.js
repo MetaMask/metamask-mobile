b5dfb012f0d853adf8c2d7593c65a2cecfe3ad21
Etienne Dusseault <etienne.dusseault@gmail.com>
2020-06-29 15:26:12 +0800

    ManualBackupStep3: implement layout

--- app/components/Views/ManualBackupStep3/index.js
+++ app/components/Views/ManualBackupStep3/index.js
@@ -1,14 +1,12 @@
 import React, { PureComponent } from 'react';
-import { ScrollView, Text, Image, View, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native';
-import Clipboard from '@react-native-community/clipboard';
+import { ScrollView, Text, View, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import Emoji from 'react-native-emoji';
-import Pager from '../../UI/Pager';
 import { colors, fontStyles } from '../../../styles/common';
+import Emoji from 'react-native-emoji';
+import OnboardingProgress from '../../UI/OnboardingProgress';
 import StyledButton from '../../UI/StyledButton';
 import { strings } from '../../../../locales/i18n';
-import CustomAlert from '../../UI/CustomAlert';
 import { showAlert } from '../../../actions/alert';
 import AndroidBackHandler from '../AndroidBackHandler';
 import Device from '../../../util/Device';
@@ -18,72 +16,54 @@ const styles = StyleSheet.create({
 		backgroundColor: colors.white,
 		flex: 1
 	},
+	scrollviewWrapper: {
+		flex: 1,
+		paddingTop: 12
+	},
 	wrapper: {
-		flexGrow: 1,
-		padding: 30,
-		paddingTop: 0
+		flex: 1,
+		paddingHorizontal: 50
 	},
-	content: {
-		alignItems: 'flex-start'
+	onBoardingWrapper: {
+		paddingHorizontal: 20,
+		marginBottom: 12
 	},
-	title: {
+	congratulations: {
 		fontSize: 32,
-		marginTop: 20,
-		marginBottom: 20,
+		marginBottom: 12,
 		color: colors.fontPrimary,
 		justifyContent: 'center',
-		textAlign: 'left',
-		...fontStyles.normal
+		textAlign: 'center',
+		...fontStyles.extraBold
 	},
-	text: {
-		marginBottom: 10,
-		justifyContent: 'center'
-	},
-	label: {
+	baseText: {
 		fontSize: 16,
-		lineHeight: 23,
 		color: colors.fontPrimary,
-		textAlign: 'left',
+		textAlign: 'center',
 		...fontStyles.normal
+	},
+	successText: {
+		marginBottom: 32
+	},
+	hintText: {
+		marginBottom: 26,
+		color: colors.blue
+	},
+	learnText: {
+		color: colors.blue
+	},
+	recoverText: {
+		marginBottom: 26
 	},
 	emoji: {
-		textAlign: 'left',
-		fontSize: 75
+		textAlign: 'center',
+		fontSize: 65,
+		marginBottom: 16
 	},
 	buttonWrapper: {
-		flex: 1,
-		justifyContent: 'flex-end'
-	},
-	bullet: {
-		marginRight: 5
-	},
-	bulletPoint: {
-		flexDirection: 'row',
-		marginBottom: 15
-	},
-	copy: {
-		flexDirection: 'row',
-		marginBottom: 15,
-		marginLeft: 10
-	},
-	tips: {
-		marginTop: 20
-	},
-	disclaimer: {
-		marginTop: 20
-	},
-	singleBold: {
-		...fontStyles.bold
-	},
-	foxBadge: {
-		marginTop: 40,
-		width: 75,
-		height: 75
-	},
-	succesModalText: {
-		textAlign: 'center',
-		fontSize: 13,
-		...fontStyles.normal
+		flexShrink: 1,
+		justifyContent: 'flex-end',
+		paddingHorizontal: 32
 	}
 });
 
@@ -91,112 +71,70 @@ const styles = StyleSheet.create({
  * View that's shown during the last step of
  * the backup seed phrase flow
  */
-class AccountBackupStep6 extends PureComponent {
+class ManualBackupStep3 extends PureComponent {
 	static propTypes = {
 		/**
 		/* navigation object required to push and pop other views
 		*/
-		navigation: PropTypes.object,
-		/**
-		/* Triggers global alert
-		*/
-		showAlert: PropTypes.func
+		navigation: PropTypes.object
 	};
 
 	state = {
-		showSuccessModal: false
+		currentStep: 4,
+		showHint: false
 	};
 
-	showModal = () => {
-		this.setState({ showSuccessModal: true });
+	componentDidMount() {
+		this.steps = this.props.navigation.getParam('steps');
+	}
+
+	toggleHint = () => {
+		this.setState({ showHint: !this.state.showHint });
 	};
 
-	onSuccesModalAction = () => {
-		this.setState({ showSuccessModal: false });
-		this.props.navigation.popToTop();
-		this.props.navigation.goBack(null);
-	};
-
-	onCopySeedPhrase = async () => {
-		const words = this.props.navigation.getParam('words', []);
-		await Clipboard.setString(words.join(' '));
-		this.props.showAlert({
-			isVisible: true,
-			autodismiss: 1500,
-			content: 'clipboard-alert',
-			data: { msg: strings(`reveal_credential.seed_phrase_copied`) }
+	learnMore = () =>
+		this.props.navigation.navigate('Webview', {
+			url: 'https://support.metamask.io',
+			title: strings('drawer.metamask_support')
 		});
-	};
 
 	render() {
 		return (
 			<SafeAreaView style={styles.mainWrapper}>
-				<Pager pages={5} selected={5} />
 				<ScrollView
-					contentContainerStyle={styles.wrapper}
+					contentContainerStyle={styles.scrollviewWrapper}
 					style={styles.mainWrapper}
 					testID={'account-backup-step-6-screen'}
 				>
-					<View style={styles.content}>
-						<Image
-							source={require('../../../images/fox-badge.png')}
-							style={styles.foxBadge}
-							resizeMethod={'auto'}
-						/>
-						<Text style={styles.title}>{strings('account_backup_step_6.title')}</Text>
-						<View style={styles.text}>
-							<Text style={styles.label}>{strings('account_backup_step_6.info_text')}</Text>
-						</View>
-						<View style={[styles.text, styles.tips]}>
-							<View style={styles.bulletPoint}>
-								<Text style={styles.bullet}>{'\u2022'}</Text>
-								<Text style={styles.label}>{strings('account_backup_step_6.tip_1')}</Text>
-							</View>
-							<View style={styles.bulletPoint}>
-								<Text style={styles.bullet}>{'\u2022'}</Text>
-								<Text style={styles.label}>{strings('account_backup_step_6.tip_2')}</Text>
-							</View>
-							<TouchableOpacity style={styles.copy} onPress={this.onCopySeedPhrase}>
-								<Text style={[styles.label, { color: colors.blue }]}>
-									{strings('account_backup_step_6.copy_seed_phrase')}
-								</Text>
-							</TouchableOpacity>
-							<View style={styles.bulletPoint}>
-								<Text style={styles.bullet}>{'\u2022'}</Text>
-								<Text style={styles.label}>{strings('account_backup_step_6.tip_3')}</Text>
-							</View>
-						</View>
-
-						<View style={[styles.text, styles.disclaimer]}>
-							<Text style={styles.label}>
-								{strings('account_backup_step_6.disclaimer')}
-								<Text style={styles.singleBold}>
-									{strings('account_backup_step_6.disclaimer_bold')}
-								</Text>
+					<View style={styles.onBoardingWrapper}>
+						<OnboardingProgress currentStep={this.state.currentStep} stepWords={this.steps} />
+					</View>
+					<View style={styles.wrapper}>
+						<Emoji name="tada" style={styles.emoji} />
+						<Text style={styles.congratulations}>{strings('manual_backup_step_3.congratulations')}</Text>
+						<Text style={[styles.baseText, styles.successText]}>
+							{strings('manual_backup_step_3.success')}
+						</Text>
+						<TouchableOpacity>
+							<Text style={[styles.baseText, styles.hintText]}>
+								{strings('manual_backup_step_3.hint')}
 							</Text>
-						</View>
+						</TouchableOpacity>
+						<Text style={[styles.baseText, styles.recoverText]}>
+							{strings('manual_backup_step_3.recover')}
+						</Text>
+						<TouchableOpacity onPress={this.learnMore}>
+							<Text style={[styles.baseText, styles.learnText]}>
+								{strings('manual_backup_step_3.learn')}
+							</Text>
+						</TouchableOpacity>
 					</View>
-					<View style={styles.buttonWrapper}>
-						<StyledButton
-							containerStyle={styles.button}
-							type={'confirm'}
-							onPress={this.showModal}
-							testID={'submit-button'}
-						>
-							{strings('account_backup_step_6.cta_text')}
-						</StyledButton>
-					</View>
-					<CustomAlert
-						headerStyle={{ backgroundColor: colors.grey200 }}
-						headerContent={<Emoji name="tada" style={styles.emoji} />}
-						titleText={strings('account_backup_step_6.modal_title')}
-						buttonText={strings('account_backup_step_6.modal_button')}
-						onPress={this.onSuccesModalAction}
-						isVisible={this.state.showSuccessModal}
-					>
-						<Text style={styles.succesModalText}>{strings('account_backup_step_6.modal_text')}</Text>
-					</CustomAlert>
 				</ScrollView>
+				<View style={styles.buttonWrapper}>
+					<StyledButton containerStyle={styles.button} type={'confirm'} testID={'submit-button'}>
+						{strings('manual_backup_step_3.done')}
+					</StyledButton>
+				</View>
 				{Device.isAndroid() && <AndroidBackHandler customBackPress={this.props.navigation.pop} />}
 			</SafeAreaView>
 		);
@@ -210,4 +148,4 @@ const mapDispatchToProps = dispatch => ({
 export default connect(
 	null,
 	mapDispatchToProps
-)(AccountBackupStep6);
+)(ManualBackupStep3);
