4de93db3eb7e1938336569eb64c1e080b47ee671
Andre Pimenta <andrepimenta7@gmail.com>
2020-07-07 18:04:07 +0100

    Merge pull request from GHSA-w5xj-p53v-pg3f
    * improve browser
    
    * Update package.json

--- app/components/Views/BrowserTab/index.js
+++ app/components/Views/BrowserTab/index.js
@@ -1287,16 +1287,7 @@ export class BrowserTab extends PureComponent {
 				}
 
 				case 'NAV_CHANGE': {
-					const { url, title } = data.payload;
-					this.setState({
-						inputValue: url,
-						autocompletInputValue: url,
-						currentPageTitle: title,
-						forwardEnabled: false
-					});
-					this.setState({ lastUrlBeforeHome: null });
-					this.props.navigation.setParams({ url: data.payload.url, silent: true, showUrlModal: false });
-					this.updateTabInfo(data.payload.url);
+					// This event is not necessary since it is handled by the onLoadEnd now
 					break;
 				}
 
@@ -1326,7 +1317,7 @@ export class BrowserTab extends PureComponent {
 		return true;
 	};
 
-	onPageChange = ({ url }) => {
+	onPageChange = url => {
 		if (this.isHomepage(url)) {
 			this.refreshHomeScripts();
 		}
@@ -1386,6 +1377,7 @@ export class BrowserTab extends PureComponent {
 
 		this.updateTabInfo(inputValue);
 		this.setState({
+			url,
 			fullHostname,
 			inputValue,
 			autocompleteInputValue: inputValue,
@@ -1395,7 +1387,7 @@ export class BrowserTab extends PureComponent {
 	};
 
 	formatHostname(hostname) {
-		return hostname.toLowerCase().replace('www.', '');
+		return hostname.toLowerCase().replace(/^www./, '');
 	}
 
 	onURLChange = inputValue => {
@@ -1406,7 +1398,9 @@ export class BrowserTab extends PureComponent {
 		this.setState({ progress });
 	};
 
-	onLoadEnd = () => {
+	onLoadEnd = ({ nativeEvent }) => {
+		if (nativeEvent.loading) return;
+
 		// Wait for the title, then store the visit
 		setTimeout(() => {
 			this.props.addToBrowserHistory({
@@ -1424,14 +1418,33 @@ export class BrowserTab extends PureComponent {
 
 		const { current } = this.webview;
 		// Inject favorites on the homepage
-		if (this.isHomepage() && current) {
+		if (this.isHomepage(nativeEvent.url) && current) {
 			const js = this.state.homepageScripts;
 			current.injectJavaScript(js);
+		}
+
+		// Onloadstart does not fire when a website url has changes, e.g. example.com/ex#user1 to example.com/ex#user2. So this is needed for those cases.
+		const urlObj = new URL(nativeEvent.url);
+		if (urlObj.hostname === this.state.fullHostname && nativeEvent.url !== this.state.inputValue) {
+			const { url, title } = nativeEvent;
+			this.setState({
+				url,
+				inputValue: url,
+				autocompletInputValue: url,
+				currentPageTitle: title,
+				forwardEnabled: false
+			});
+			this.setState({ lastUrlBeforeHome: null });
+			this.props.navigation.setParams({ url: nativeEvent.url, silent: true, showUrlModal: false });
+			this.updateTabInfo(nativeEvent.url);
 		}
 	};
 
 	onError = ({ nativeEvent: errorInfo }) => {
 		Logger.log(errorInfo);
+		this.props.navigation.setParams({
+			error: true
+		});
 		this.setState({ lastError: errorInfo });
 	};
 
@@ -1814,6 +1827,7 @@ export class BrowserTab extends PureComponent {
 	onLoadStart = async ({ nativeEvent }) => {
 		// Handle the scenario when going back
 		// from an ENS name
+		this.props.navigation.setParams({ error: false });
 		if (nativeEvent.navigationType === 'backforward' && nativeEvent.url === this.state.inputValue) {
 			setTimeout(() => this.goBack(), 500);
 		} else if (nativeEvent.url.indexOf(this.props.ipfsGateway) !== -1) {
@@ -1843,6 +1857,8 @@ export class BrowserTab extends PureComponent {
 			const origin = new URL(nativeEvent.url).origin;
 			this.initializeBackgroundBridge(origin, true);
 		}
+
+		this.onPageChange(nativeEvent.url);
 	};
 
 	canGoForward = () => this.state.forwardEnabled;
@@ -1912,7 +1928,6 @@ export class BrowserTab extends PureComponent {
 							onLoadEnd={this.onLoadEnd}
 							onError={this.onError}
 							onMessage={this.onMessage}
-							onNavigationStateChange={this.onPageChange}
 							ref={this.webview}
 							source={{ uri: url }}
 							style={styles.webview}
