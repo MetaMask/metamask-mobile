464b24127346f34f608c804abee3f2d0cf77954f
Esteban Mino <efmino@uc.cl>
2020-07-24 17:46:14 -0400

    modal

--- app/components/Views/Onboarding/index.js
+++ app/components/Views/Onboarding/index.js
@@ -30,13 +30,15 @@ const PUB_KEY = process.env.MM_PUBNUB_PUB_KEY;
 
 const styles = StyleSheet.create({
 	scroll: {
-		flexGrow: 1
+		flex: 1
 	},
 	wrapper: {
-		paddingHorizontal: 40,
-		paddingVertical: 30,
 		flex: 1,
 		alignItems: 'center'
+	},
+	modalWrapper: {
+		flexGrow: 1,
+		paddingHorizontal: 24
 	},
 	foxWrapper: {
 		width: Device.isIos() ? 90 : 45,
@@ -82,12 +84,11 @@ const styles = StyleSheet.create({
 		marginVertical: 24
 	},
 	createWrapper: {
-		position: 'absolute',
-		width: '100%',
-		bottom: 0
+		flex: 1,
+		justifyContent: 'flex-end',
+		marginBottom: 24
 	},
 	buttonWrapper: {
-		flexGrow: 1,
 		marginBottom: 16
 	},
 	scanTitle: {
@@ -95,13 +96,12 @@ const styles = StyleSheet.create({
 		color: colors.fontPrimary,
 		fontWeight: 'bold'
 	},
-	steps: {
-		paddingVertical: 24
-	},
+	steps: {},
 	step: {
 		fontSize: 16,
 		color: colors.fontPrimary,
-		fontWeight: 'normal'
+		fontWeight: 'normal',
+		lineHeight: 32
 	},
 	loader: {
 		marginTop: 180,
@@ -116,22 +116,18 @@ const styles = StyleSheet.create({
 		...fontStyles.normal
 	},
 	column: {
-		flexDirection: 'column',
-		alignItems: 'flex-start',
-		width: 200
+		marginVertical: 24,
+		alignItems: 'flex-start'
 	},
 	row: {
 		flexDirection: 'row',
 		alignItems: 'flex-start',
-		flexWrap: 'wrap',
-		flex: 1
+		flexWrap: 'wrap'
 	},
 	bullet: {
 		width: 20
 	},
-	bulletText: {
-		flex: 1
-	}
+	bulletText: {}
 });
 
 /**
@@ -573,7 +569,7 @@ class Onboarding extends PureComponent {
 					confirmText="Scan"
 					confirmButtonMode="confirm"
 				>
-					<View style={styles.wrapper}>
+					<View style={styles.modalWrapper}>
 						<Text style={styles.scanTitle}>{strings('onboarding.scan_title')}</Text>
 						<View style={styles.column}>
 							{[1, 2, 3, 4].map(val => (
