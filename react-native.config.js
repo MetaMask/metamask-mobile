f9eff49e5f59397ac296fd116ae6b886b2196d07
andrepimenta <andrepimenta7@gmail.com>
2020-08-04 15:57:24 +0100

    Working on iOS

--- react-native.config.js
+++ react-native.config.js
@@ -2,11 +2,6 @@
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
 	dependencies: {
-		'react-native-aes-crypto': {
-			platforms: {
-				ios: null // disable Android platform, other platforms will still autolink if provided
-			}
-		},
 		'react-native-gesture-handler': {
 			platforms: {
 				android: null // disable Android platform, other platforms will still autolink if provided
