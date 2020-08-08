1a2a0b33f25b435639004db214cf89f8aecaf15d
rickycodes <ricky.miller@gmail.com>
2020-08-08 02:40:57 -0400

    Update test for Identicon to inclue mock store

--- app/components/UI/Identicon/index.test.js
+++ app/components/UI/Identicon/index.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Identicon from './';
+import configureMockStore from 'redux-mock-store';
+
+const mockStore = configureMockStore();
 
 describe('Identicon', () => {
 	it('should render correctly', () => {
-		const wrapper = shallow(<Identicon />);
-		expect(wrapper).toMatchSnapshot();
+		const initialState = {
+			settings: { useBlockieIcon: false }
+		};
+
+		const wrapper = shallow(<Identicon />, {
+			context: { store: mockStore(initialState) }
+		});
+		expect(wrapper.dive()).toMatchSnapshot();
 	});
 });
