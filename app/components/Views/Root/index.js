36caa71d3c44168816adc5cc86f61207287718f7
andrepimenta <andrepimenta7@gmail.com>
2020-10-08 15:58:26 +0100

    error boundary init

--- app/components/Views/Root/index.js
+++ app/components/Views/Root/index.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
+import ErrorBoundary from 'react-native-error-boundary';
 
 import { store, persistor } from '../../../store/';
 import SplashScreen from 'react-native-splash-screen';
@@ -8,12 +10,25 @@ import SplashScreen from 'react-native-splash-screen';
 import App from '../../Nav/App';
 import SecureKeychain from '../../../core/SecureKeychain';
 import EntryScriptWeb3 from '../../../core/EntryScriptWeb3';
+import Logger from '../../../util/Logger';
 
 /**
  * Top level of the component hierarchy
  * App component is wrapped by the provider from react-redux
  */
 export default class Root extends PureComponent {
+	static propTypes = {
+		foxCode: PropTypes.string
+	};
+
+	static defaultProps = {
+		foxCode: 'null'
+	};
+
+	errorHandler = (error, stackTrace) => {
+		Logger.error(error, stackTrace);
+	};
+
 	constructor(props) {
 		super(props);
 		SecureKeychain.init(props.foxCode); // eslint-disable-line
@@ -25,7 +40,9 @@ export default class Root extends PureComponent {
 	render = () => (
 		<Provider store={store}>
 			<PersistGate persistor={persistor}>
-				<App />
+				<ErrorBoundary onError={this.errorHandler}>
+					<App />
+				</ErrorBoundary>
 			</PersistGate>
 		</Provider>
 	);
