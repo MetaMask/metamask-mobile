e0f9ab94a2d2be47558bea325e5d595bd00e1341
Esteban Mino <efmino@uc.cl>
2020-10-08 21:53:50 -0300

    CURRENT_PROJECT_VERSION

--- app/util/transactions.js
+++ app/util/transactions.js
@@ -282,7 +282,7 @@ export async function getTransactionActionKey(transaction) {
  * @returns {string} - Transaction type message
  */
 export async function getActionKey(tx, selectedAddress, ticker, paymentChannelTransaction) {
-	if (tx.isTransfer) {
+	if (tx && tx.isTransfer) {
 		const selfSent = safeToChecksumAddress(tx.transaction.from) === selectedAddress;
 		const translationKey = selfSent ? 'transactions.self_sent_unit' : 'transactions.received_unit';
 		// Third party sending wrong token symbol
