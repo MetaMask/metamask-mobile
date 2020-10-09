c7967632dcdd81c47fbde31368e72a48834a367a
Esteban Mino <efmino@uc.cl>
2020-10-09 16:03:41 -0300

    Revert "REVERT THIS"
    This reverts commit a417b67a109096b82c25cdad5cc0135790cfb348.

--- app/components/Views/Wallet/index.js
+++ app/components/Views/Wallet/index.js
@@ -241,7 +241,6 @@ class Wallet extends PureComponent {
 	render = () => (
 		<ErrorBoundary view="Wallet">
 			<View style={baseStyles.flexGrow} testID={'wallet-screen'}>
-				{''}
 				<ScrollView
 					style={styles.wrapper}
 					refreshControl={<RefreshControl refreshing={this.state.refreshing} onRefresh={this.onRefresh} />}
