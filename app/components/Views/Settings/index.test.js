d4fef69a69bb4560621341dd5d9cbbdb5575a960
rickycodes <ricky.miller@gmail.com>
2020-10-25 23:28:21 -0400

    Update Settings test

--- app/components/Views/Settings/index.test.js
+++ app/components/Views/Settings/index.test.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Settings from './';
+import configureMockStore from 'redux-mock-store';
 
 describe('Settings', () => {
+	const mockStore = configureMockStore();
 	it('should render correctly', () => {
-		const wrapper = shallow(<Settings />);
-		expect(wrapper).toMatchSnapshot();
+		const initialState = {
+			privacy: { approvedHosts: [], privacyMode: true },
+			browser: { history: [] },
+			settings: { lockTime: 1000, searchEngine: 'DuckDuckGo', useBlockieIcon: true },
+			engine: {
+				backgroundState: {
+					CurrencyRateController: { currentCurrency: 'USD' },
+					NetworkController: {
+						provider: {
+							type: 'mainnet'
+						}
+					},
+					PreferencesController: { selectedAddress: '0x0' }
+				}
+			}
+		};
+		const wrapper = shallow(
+			<Settings
+				navigation={{
+					state: { params: {} }
+				}}
+			/>,
+			{
+				context: { store: mockStore(initialState) }
+			}
+		);
+		expect(wrapper.dive()).toMatchSnapshot();
 	});
 });
