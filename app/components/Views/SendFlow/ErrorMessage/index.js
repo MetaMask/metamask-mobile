43a5f82bf95de522995cefc28e64d3d1571fd607
Andre Pimenta <andrepimenta7@gmail.com>
2020-11-06 00:49:05 +0000

    Fix QR send (#1916)
    * Fix QR send
    
    * Working for more types
    
    * More comments
    
    * Remove unnecessary export
    
    * Warning for smart and token address
    
    * Fixes and new warning info adjustments
    
    * Remove comment
    
    * Add comments
    
    * Show error for erc721 and erc20
    
    * Fix bold style

--- app/components/Views/SendFlow/ErrorMessage/index.js
+++ app/components/Views/SendFlow/ErrorMessage/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { colors, fontStyles } from '../../../../styles/common';
 import PropTypes from 'prop-types';
+import { strings } from '../../../../../locales/i18n';
 
 const styles = StyleSheet.create({
-	wrapper: {
+	wrapperError: {
 		backgroundColor: colors.red000,
 		borderWidth: 1,
 		borderColor: colors.red,
@@ -17,21 +18,61 @@ const styles = StyleSheet.create({
 		color: colors.red,
 		flexDirection: 'row',
 		alignItems: 'center'
+	},
+	button: {
+		marginTop: 27
+	},
+	buttonMessage: {
+		...fontStyles.normal,
+		fontSize: 12,
+		color: colors.blue,
+		textAlign: 'center'
+	},
+	wrapperWarning: {
+		backgroundColor: colors.blue000,
+		borderWidth: 1,
+		borderColor: colors.blue,
+		borderRadius: 4,
+		padding: 15
+	},
+	warningMessage: {
+		...fontStyles.normal,
+		fontSize: 12,
+		color: colors.blue,
+		flexDirection: 'row',
+		alignItems: 'center'
 	}
 });
 
 export default function ErrorMessage(props) {
-	const { errorMessage } = props;
+	const { errorMessage, errorContinue, onContinue, isOnlyWarning } = props;
 	return (
-		<View style={styles.wrapper} testID={'error-message-warning'}>
-			<Text style={styles.errorMessage}>{errorMessage}</Text>
+		<View style={isOnlyWarning ? styles.wrapperWarning : styles.wrapperError} testID={'error-message-warning'}>
+			<Text style={isOnlyWarning ? styles.warningMessage : styles.errorMessage}>{errorMessage}</Text>
+			{errorContinue && (
+				<TouchableOpacity onPress={onContinue} style={styles.button}>
+					<Text style={styles.buttonMessage}>{strings('transaction.continueError')}</Text>
+				</TouchableOpacity>
+			)}
 		</View>
 	);
 }
 
 ErrorMessage.propTypes = {
 	/**
-	 * Error message to display
+	 * Error message to display, can be a string or a Text component
 	 */
-	errorMessage: PropTypes.string
+	errorMessage: PropTypes.oneOfType([PropTypes.object, PropTypes.array, PropTypes.string]),
+	/**
+	 * Show continue button when it is a contract address
+	 */
+	errorContinue: PropTypes.bool,
+	/**
+	 * Function that is called when continue button is pressed
+	 */
+	onContinue: PropTypes.func,
+	/**
+	 * Show a warning info instead of an error
+	 */
+	isOnlyWarning: PropTypes.bool
 };
