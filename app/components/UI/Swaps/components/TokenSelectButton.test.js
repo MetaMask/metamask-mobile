f9ba8af6185514da7eb5c5fa211c880981672d7f
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2020-11-16 13:43:14 -0300

    Add TokenSelectButton label prop to test

--- app/components/UI/Swaps/components/TokenSelectButton.test.js
+++ app/components/UI/Swaps/components/TokenSelectButton.test.js
@@ -5,14 +5,15 @@ import TokenSelectButton from './TokenSelectButton';
 describe('TokenSelectButton component', () => {
 	it('should Render correctly', () => {
 		const dummyHandler = jest.fn();
-		const empty = shallow(<TokenSelectButton />);
+		const empty = shallow(<TokenSelectButton label="Select a token" />);
 		expect(empty).toMatchSnapshot();
-		const eth = shallow(<TokenSelectButton symbol="ETH" />);
+		const eth = shallow(<TokenSelectButton label="Select a token" symbol="ETH" />);
 		expect(eth).toMatchSnapshot();
-		const symbol = shallow(<TokenSelectButton symbol="cDAI" />);
+		const symbol = shallow(<TokenSelectButton label="Select a token" symbol="cDAI" />);
 		expect(symbol).toMatchSnapshot();
 		const icon = shallow(
 			<TokenSelectButton
+				label="Select a token"
 				symbol="DAI"
 				icon="https://cloudflare-ipfs.com/ipfs/QmNYVMm3iC7HEoxfvxsZbRoapdjDHj9EREFac4BPeVphSJ"
 			/>
@@ -20,6 +21,7 @@ describe('TokenSelectButton component', () => {
 		expect(icon).toMatchSnapshot();
 		const onPress = shallow(
 			<TokenSelectButton
+				label="Select a token"
 				symbol="DAI"
 				icon="https://cloudflare-ipfs.com/ipfs/QmNYVMm3iC7HEoxfvxsZbRoapdjDHj9EREFac4BPeVphSJ"
 				onPress={dummyHandler}
