7d0cd152175f3a48fedfde9fea9ed62aff8fb8af
Esteban Miño <efmino@uc.cl>
2020-11-18 19:27:34 -0300

    Bugfix/android choose password (#2003)
    * hack
    
    * circleci
    
    * circleci2
    
    * resetGenericPassword
    
    * revertworkaround
    
    * whatsnew
    
    * 2003
    
    * Revert "circleci"
    
    This reverts commit 991eec5412a6779dda1f924104c670dc84047cb1.
    
    * Revert "circleci2"
    
    This reverts commit f3433ef875daa0b08e40e21f76438b32228eeb14.
    
    * CURRENT_PROJECT_VERSION

--- app/components/UI/WhatsNewModal/whatsNewList.js
+++ app/components/UI/WhatsNewModal/whatsNewList.js
@@ -2,10 +2,10 @@ import { strings } from '../../../../locales/i18n';
 
 export const whatsNew = [
 	{
-		// All users that have <1.0.6 and are updating to >=1.0.6 should see
+		// All users that have <1.0.7 and are updating to >=1.0.7 should see
 		onlyUpdates: true, // Only users who updated the app will see this, not newly installs
-		maxLastAppVersion: '1.0.6', // Only users who had a previous version <1.0.6 version will see this
-		minAppVersion: '1.0.6', // Only users who updated to a version >= 1.0.6 will see this
+		maxLastAppVersion: '1.0.7', // Only users who had a previous version <1.0.7 version will see this
+		minAppVersion: '1.0.7', // Only users who updated to a version >= 1.0.7 will see this
 		features: [
 			{
 				title: strings('whats_new.feature_security_settings_title'),
