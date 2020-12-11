4cd66a0752923b5ef1cd78f99b7dd78bfd80f709
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2020-12-10 23:23:51 -0300

    Add initial Alert component

---
+++ app/components/Base/Alert.js
@@ -0,0 +1,80 @@
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { colors } from '../../styles/common';
+import Text from './Text';
+
+const styles = StyleSheet.create({
+	base: {
+		paddingHorizontal: 12,
+		paddingVertical: 12,
+		borderWidth: 1,
+		borderRadius: 4,
+		flexDirection: 'row'
+	},
+	baseSmall: {
+		paddingVertical: 8
+	},
+	info: {
+		backgroundColor: colors.blue100,
+		borderColor: colors.blue
+	},
+	warning: {
+		backgroundColor: colors.yellow100,
+		borderColor: colors.yellow
+	},
+	error: {
+		backgroundColor: colors.red000,
+		borderColor: colors.red
+	},
+	textInfo: { color: colors.blue },
+	textWarning: { color: colors.yellow700 },
+	textError: { color: colors.red },
+	iconWrapper: {
+		alignItems: 'center'
+	}
+});
+
+function getStyles(type) {
+	switch (type) {
+		case 'warning': {
+			return [styles.warning, styles.textWarning];
+		}
+		case 'error': {
+			return [styles.error, styles.textError];
+		}
+		case 'info':
+		default: {
+			return [styles.info, styles.textInfo];
+		}
+	}
+}
+
+function Alert({ type = 'info', small, renderIcon, style, onPress, children }) {
+	const [wrapperStyle, textStyle] = useMemo(() => getStyles(type), [type]);
+
+	const Wrapper = onPress ? TouchableOpacity : View;
+	return (
+		<Wrapper style={[styles.base, small && styles.baseSmall, wrapperStyle, style]} onPress={onPress}>
+			{renderIcon && typeof renderIcon === 'function' && <View style={styles.iconWrapper}>{renderIcon()}</View>}
+			{typeof children === 'function' ? (
+				children(textStyle)
+			) : (
+				<Text small={small} style={textStyle}>
+					{children}
+				</Text>
+			)}
+		</Wrapper>
+	);
+}
+
+Alert.propTypes = {
+	type: PropTypes.oneOf(['info', 'warning', 'error']),
+	style: PropTypes.object,
+	small: PropTypes.boolean,
+	renderIcon: PropTypes.func,
+	onPress: PropTypes.func,
+	children: PropTypes.node
+};
+
+export default Alert;
