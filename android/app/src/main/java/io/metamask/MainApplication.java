5c02e00556539f5be2074adc794775216ef7df28
rickycodes <ricky.miller@gmail.com>
2021-01-06 22:47:19 -0500

    Add rn-fetch-blob dep

--- android/app/src/main/java/io/metamask/MainApplication.java
+++ android/app/src/main/java/io/metamask/MainApplication.java
@@ -1,6 +1,7 @@
 package io.metamask;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.cmcewen.blurview.BlurViewPackage;
 import android.content.Context;
 import com.facebook.react.PackageList;
