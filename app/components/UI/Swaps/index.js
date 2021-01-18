7ab3dc72a54b2c07392d4875005bf7b241080da7
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2021-01-18 18:21:07 -0300

    Add sourceToken order by balance in selector modal

--- app/components/UI/Swaps/index.js
+++ app/components/UI/Swaps/index.js
@@ -8,6 +8,7 @@ import BigNumber from 'bignumber.js';
 import { toChecksumAddress } from 'ethereumjs-util';
 import { swapsUtils } from '@estebanmino/controllers';
 
+import { swapsTokensWithBalanceSelector } from '../../../reducers/swaps';
 import Engine from '../../../core/Engine';
 import AppConstants from '../../../core/AppConstants';
 import useModalHandler from '../../Base/hooks/useModalHandler';
@@ -101,8 +102,7 @@ const styles = StyleSheet.create({
 
 // Grab this from SwapsController.utils
 const SWAPS_ETH_ADDRESS = swapsUtils.ETH_SWAPS_TOKEN_ADDRESS;
-
-function SwapsAmountView({ tokens, accounts, selectedAddress, balances }) {
+function SwapsAmountView({ tokens, accounts, selectedAddress, balances, tokensWithBalance }) {
 	const navigation = useContext(NavigationContext);
 	const initialSource = navigation.getParam('sourceToken', SWAPS_ETH_ADDRESS);
 	const [amount, setAmount] = useState('0');
@@ -237,8 +237,9 @@ function SwapsAmountView({ tokens, accounts, selectedAddress, balances }) {
 						dismiss={toggleSourceModal}
 						title={strings('swaps.convert_from')}
 						tokens={tokens}
+						initialTokens={tokensWithBalance}
 						onItemPress={handleSourceTokenPress}
-						exclude={[destinationToken?.symbol]}
+						exclude={[destinationToken?.address]}
 					/>
 				</View>
 				<View style={styles.amountContainer}>
@@ -293,7 +294,7 @@ function SwapsAmountView({ tokens, accounts, selectedAddress, balances }) {
 						title={strings('swaps.convert_to')}
 						tokens={tokens}
 						onItemPress={handleDestinationTokenPress}
-						exclude={[sourceToken?.symbol]}
+						exclude={[sourceToken?.address]}
 					/>
 				</View>
 			</View>
@@ -341,6 +342,7 @@ SwapsAmountView.navigationOptions = ({ navigation }) => getSwapsAmountNavbar(nav
 
 SwapsAmountView.propTypes = {
 	tokens: PropTypes.arrayOf(PropTypes.object),
+	tokensWithBalance: PropTypes.arrayOf(PropTypes.object),
 	/**
 	 * Map of accounts to information objects including balances
 	 */
@@ -359,7 +361,8 @@ const mapStateToProps = state => ({
 	tokens: state.engine.backgroundState.SwapsController.tokens,
 	accounts: state.engine.backgroundState.AccountTrackerController.accounts,
 	selectedAddress: state.engine.backgroundState.PreferencesController.selectedAddress,
-	balances: state.engine.backgroundState.TokenBalancesController.contractBalances
+	balances: state.engine.backgroundState.TokenBalancesController.contractBalances,
+	tokensWithBalance: swapsTokensWithBalanceSelector(state)
 });
 
 export default connect(mapStateToProps)(SwapsAmountView);
