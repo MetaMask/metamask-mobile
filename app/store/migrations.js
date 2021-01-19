e3621d77e6d2fd1a4209a38b4e3ef410101d9c97
Andre Pimenta <andrepimenta7@gmail.com>
2021-01-19 19:39:45 +0000

    Merge pull request from GHSA-996m-jhjg-3chr
    * require chain id
    
    * Update unit tests
    
    * Implement eth_chainId and net_version rpc methods
    
    * Improve chainId validation
    
    * Trying to revive the advisory
    
    * Fix validations and return chainId rpc as hex
    
    * Controllers update to 5.1.0
    
    * Validate chainId number (MAX_SAFE_CHAIN_ID)
    
    * Update getDecimalChainIdNumber comment
    
    * BigNumber cleanup
    
    * Update controllers
    
    * Update test

---
+++ app/store/migrations.js
@@ -0,0 +1,53 @@
+import AppConstants from '../core/AppConstants';
+import { getAllNetworks, isSafeChainId } from '../util/networks';
+
+export const migrations = {
+	// Needed after https://github.com/MetaMask/controllers/pull/152
+	0: state => {
+		const addressBook = state.engine.backgroundState.AddressBookController.addressBook;
+		const migratedAddressBook = {};
+		Object.keys(addressBook).forEach(address => {
+			const chainId = addressBook[address].chainId.toString();
+			migratedAddressBook[chainId]
+				? (migratedAddressBook[chainId] = { ...migratedAddressBook[chainId], [address]: addressBook[address] })
+				: (migratedAddressBook[chainId] = { [address]: addressBook[address] });
+		});
+		state.engine.backgroundState.AddressBookController.addressBook = migratedAddressBook;
+		return state;
+	},
+	// MakerDAO DAI => SAI
+	1: state => {
+		const tokens = state.engine.backgroundState.AssetsController.tokens;
+		const migratedTokens = [];
+		tokens.forEach(token => {
+			if (token.symbol === 'DAI' && token.address.toLowerCase() === AppConstants.SAI_ADDRESS.toLowerCase()) {
+				token.symbol = 'SAI';
+			}
+			migratedTokens.push(token);
+		});
+		state.engine.backgroundState.AssetsController.tokens = migratedTokens;
+
+		return state;
+	},
+	2: state => {
+		const provider = state.engine.backgroundState.NetworkController.provider;
+
+		// Check if the current network is one of the initial networks
+		const isInitialNetwork = provider.type && getAllNetworks().includes(provider.type);
+
+		// Check if the current network has a valid chainId
+		const chainIdNumber = parseInt(provider.chainId, 10);
+		const isCustomRpcWithInvalidChainId = !isSafeChainId(chainIdNumber);
+
+		if (!isInitialNetwork && isCustomRpcWithInvalidChainId) {
+			// If the current network does not have a chainId, switch to testnet.
+			state.engine.backgroundState.NetworkController.provider = {
+				ticker: 'ETH',
+				type: 'rinkeby'
+			};
+		}
+		return state;
+	}
+};
+
+export const version = 2;
