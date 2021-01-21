6c68bbade489013f251bac9bb598bb7b0163e3ed
andrepimenta <andrepimenta7@gmail.com>
2021-01-21 17:49:31 +0000

    translations and update script

--- locales/i18n.js
+++ locales/i18n.js
@@ -4,16 +4,35 @@ import I18n from 'react-native-i18n';
 import { LANGUAGE } from '../app/constants/storage';
 
 // Import all locales
-import en from './en.json';
-import es from './es.json';
-
+import en from './languages/en.json';
+import esXl from './languages/es-xl.json';
+import es from './languages/es.json';
+import hi from './languages/hi-in.json';
+import id from './languages/id-id.json';
+import ja from './languages/ja-jp.json';
+import ko from './languages/ko-kr.json';
+import pt from './languages/pt-br.json';
+import ru from './languages/ru-ru.json';
+import tl from './languages/tl.json';
+import vi from './languages/vi-vn.json';
+import zh from './languages/zh-cn.json';
 // Should the app fallback to English if user locale doesn't exists
 I18n.fallbacks = true;
 I18n.defaultLocale = 'en';
 // Define the supported translations
 I18n.translations = {
 	en,
-	es
+	'es-419': esXl, // Spanish - Latin America
+	es,
+	hi,
+	id,
+	ja,
+	ko,
+	pt,
+	ru,
+	tl,
+	vi,
+	zh
 };
 // If language selected get locale
 getUserPreferableLocale();
@@ -21,7 +40,7 @@ getUserPreferableLocale();
 const currentLocale = I18n.currentLocale();
 
 // Is it a RTL language?
-export const isRTL = currentLocale.indexOf('he') === 0 || currentLocale.indexOf('ar') === 0;
+export const isRTL = currentLocale.indexOf('jaJp') === 0;
 
 // Set locale
 export async function setLocale(locale) {
@@ -31,7 +50,20 @@ export async function setLocale(locale) {
 
 // Get languages
 export function getLanguages() {
-	return { en: 'English', es: 'Spanish' };
+	return {
+		en: 'English',
+		es: 'Spanish',
+		'es-419': 'Spanish Latin',
+		hi: 'Hindi',
+		id: 'Bahasa Indonesian',
+		ja: 'Japanese',
+		ko: 'Korean',
+		pt: 'Portuguese - Brazil',
+		ru: 'Russian',
+		tl: 'Filipino',
+		vi: 'Vietnamese',
+		zh: 'Chinese - China'
+	};
 }
 
 // Allow RTL alignment in RTL languages
