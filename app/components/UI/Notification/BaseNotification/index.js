096fc2a0049bbe901f39022b11872a8836220d8a
Ricky Miller <ricky.miller@gmail.com>
2021-02-08 10:12:06 -0500

    Clean up BaseNotification component

--- app/components/UI/Notification/BaseNotification/index.js
+++ app/components/UI/Notification/BaseNotification/index.js
@@ -55,114 +55,115 @@ const styles = StyleSheet.create({
 	}
 });
 
+const getIcon = status => {
+	switch (status) {
+		case 'pending':
+		case 'pending_withdrawal':
+		case 'pending_deposit':
+		case 'speedup':
+			return <AnimatedSpinner size={36} />;
+		case 'success_deposit':
+		case 'success_withdrawal':
+		case 'success':
+		case 'received':
+		case 'received_payment':
+			return <IonicIcon color={colors.green500} size={36} name="md-checkmark" style={styles.checkIcon} />;
+		case 'cancelled':
+		case 'error':
+			return <MaterialIcon color={colors.red} size={36} name="alert-circle-outline" style={styles.checkIcon} />;
+		case 'simple_notification_rejected':
+			return <AntIcon color={colors.red} size={36} name="closecircleo" style={styles.checkIcon} />;
+		case 'simple_notification':
+			return <AntIcon color={colors.green500} size={36} name="checkcircleo" style={styles.checkIcon} />;
+	}
+};
+
+const getTitle = (status, { nonce, amount, assetType }) => {
+	switch (status) {
+		case 'pending':
+			return strings('notifications.pending_title');
+		case 'pending_deposit':
+			return strings('notifications.pending_deposit_title');
+		case 'pending_withdrawal':
+			return strings('notifications.pending_withdrawal_title');
+		case 'success':
+			return strings('notifications.success_title', { nonce: parseInt(nonce) });
+		case 'success_deposit':
+			return strings('notifications.success_deposit_title');
+		case 'success_withdrawal':
+			return strings('notifications.success_withdrawal_title');
+		case 'received':
+			return strings('notifications.received_title', {
+				amount,
+				assetType
+			});
+		case 'speedup':
+			return strings('notifications.speedup_title', { nonce: parseInt(nonce) });
+		case 'received_payment':
+			return strings('notifications.received_payment_title');
+		case 'cancelled':
+			return strings('notifications.cancelled_title');
+		case 'error':
+			return strings('notifications.error_title');
+	}
+};
+
+const getDescription = (status, { amount = null }) => {
+	if (amount) {
+		return strings(`notifications.${status}_message`, { amount });
+	}
+	return strings(`notifications.${status}_message`);
+};
+
 /**
  * BaseNotification component used to render in-app notifications
  */
-export default function BaseNotification({ status, data = {}, onPress, onHide, closeButtonDisabled }) {
-	const { description, title, amount = null, nonce, assetType } = data;
-	// eslint-disable-next-line
-	_getIcon = () => {
-		switch (status) {
-			case 'pending':
-			case 'pending_withdrawal':
-			case 'pending_deposit':
-			case 'speedup':
-				return <AnimatedSpinner size={36} />;
-			case 'success_deposit':
-			case 'success_withdrawal':
-			case 'success':
-			case 'received':
-			case 'received_payment':
-				return <IonicIcon color={colors.green500} size={36} name="md-checkmark" style={styles.checkIcon} />;
-			case 'cancelled':
-			case 'error':
-				return (
-					<MaterialIcon color={colors.red} size={36} name="alert-circle-outline" style={styles.checkIcon} />
-				);
-			case 'simple_notification_rejected':
-				return <AntIcon color={colors.red} size={36} name="closecircleo" style={styles.checkIcon} />;
-			case 'simple_notification':
-				return <AntIcon color={colors.green500} size={36} name="checkcircleo" style={styles.checkIcon} />;
-		}
-	};
-
-	// eslint-disable-next-line no-undef
-	_getTitle = () => {
-		switch (status) {
-			case 'pending':
-				return strings('notifications.pending_title');
-			case 'pending_deposit':
-				return strings('notifications.pending_deposit_title');
-			case 'pending_withdrawal':
-				return strings('notifications.pending_withdrawal_title');
-			case 'success':
-				return strings('notifications.success_title', { nonce: parseInt(nonce) });
-			case 'success_deposit':
-				return strings('notifications.success_deposit_title');
-			case 'success_withdrawal':
-				return strings('notifications.success_withdrawal_title');
-			case 'received':
-				return strings('notifications.received_title', {
-					amount,
-					assetType
-				});
-			case 'speedup':
-				return strings('notifications.speedup_title', { nonce: parseInt(nonce) });
-			case 'received_payment':
-				return strings('notifications.received_payment_title');
-			case 'cancelled':
-				return strings('notifications.cancelled_title');
-			case 'error':
-				return strings('notifications.error_title');
-		}
-	};
-
-	// eslint-disable-next-line no-undef
-	_getDescription = () => {
-		if (amount) {
-			return strings(`notifications.${status}_message`, { amount });
-		}
-		return strings(`notifications.${status}_message`);
-	};
-
-	return (
-		<View style={baseStyles.flexGrow}>
-			<TouchableOpacity
-				style={styles.defaultFlashFloating}
-				testID={'press-notification-button'}
-				onPress={onPress}
-				activeOpacity={0.8}
-			>
-				<View style={styles.flashIcon}>{this._getIcon()}</View>
-				<View style={styles.flashLabel}>
-					<Text
-						style={[styles.flashTitle, !description && styles.flashTitleNoDesc]}
-						testID={'notification-title'}
-					>
-						{!title ? this._getTitle() : title}
-					</Text>
-					<Text style={styles.flashText}>{!title ? this._getDescription() : description}</Text>
-				</View>
-				<View>
-					{!closeButtonDisabled && (
-						<TouchableOpacity style={styles.closeTouchable} onPress={onHide}>
-							<IonicIcon name="ios-close" size={36} style={styles.closeIcon} />
-						</TouchableOpacity>
-					)}
-				</View>
-			</TouchableOpacity>
-		</View>
-	);
-}
+const BaseNotification = ({
+	status,
+	data = null,
+	data: { description = null, title = null },
+	onPress,
+	onHide,
+	autoDismiss
+}) => (
+	<View style={baseStyles.flexGrow}>
+		<TouchableOpacity
+			style={styles.defaultFlashFloating}
+			testID={'press-notification-button'}
+			onPress={onPress}
+			activeOpacity={0.8}
+		>
+			<View style={styles.flashIcon}>{getIcon(status)}</View>
+			<View style={styles.flashLabel}>
+				<Text
+					style={[styles.flashTitle, !description && styles.flashTitleNoDesc]}
+					testID={'notification-title'}
+				>
+					{!title ? getTitle(status, data) : title}
+				</Text>
+				<Text style={styles.flashText}>{!title ? getDescription(status, data) : description}</Text>
+			</View>
+			<View>
+				{autoDismiss && (
+					<TouchableOpacity style={styles.closeTouchable} onPress={onHide}>
+						<IonicIcon name="ios-close" size={36} style={styles.closeIcon} />
+					</TouchableOpacity>
+				)}
+			</View>
+		</TouchableOpacity>
+	</View>
+);
 
 BaseNotification.propTypes = {
 	status: PropTypes.string,
 	data: PropTypes.object,
 	onPress: PropTypes.func,
 	onHide: PropTypes.func,
-	closeButtonDisabled: PropTypes.bool
+	autoDismiss: PropTypes.bool
 };
 
 BaseNotification.defaultProps = {
-	closeButtonDisabled: false
+	autoDismiss: false
 };
+
+export default BaseNotification;
