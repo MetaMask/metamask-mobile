251b221823e6f6b6b0ed9186cb15dfd9dc379cef
Ricky Miller <ricky.miller@gmail.com>
2021-02-09 20:28:56 -0500

    Only use this style for error case

--- app/components/UI/Tokens/index.js
+++ app/components/UI/Tokens/index.js
@@ -209,7 +209,9 @@ class Tokens extends PureComponent {
 				<View style={styles.balances} testID={'balance'}>
 					<Text style={styles.balance}>{mainBalance}</Text>
 					{secondaryBalance ? (
-						<Text style={[styles.balanceFiat, styles.balanceFiatTokenError]}>{secondaryBalance}</Text>
+						<Text style={[styles.balanceFiat, asset?.balanceError && styles.balanceFiatTokenError]}>
+							{secondaryBalance}
+						</Text>
 					) : null}
 				</View>
 			</AssetElement>
