4b97f84e79bf585ed4d4221df9d5f88be9662168
Ricky Miller <ricky.miller@gmail.com>
2021-02-25 15:06:08 -0500

    re-add migration

--- app/store/index.js
+++ app/store/index.js
@@ -6,10 +6,48 @@ import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import rootReducer from '../reducers';
 import { migrations, version } from './migrations';
 
+import Logger from '../util/Logger';
+
+/*
+ * At some point in the not so distant future it'll be safe to delete this one time migration
+ */
+const MigratedStorage = {
+	async getItem(key) {
+		try {
+			const res = await FileSystemStorage.getItem(key);
+			if (res) {
+				// Using new storage system
+				return res;
+			}
+		} catch (e) {
+			//
+		}
+
+		// Using old storage system, should only happen once
+		try {
+			const res = await AsyncStorage.getItem(key);
+			AsyncStorage.setItem(key, ''); // clear old storage
+			return res;
+		} catch (e) {
+			Logger.log('Failed to run migration', e);
+		}
+	},
+	setItem(key, value) {
+		return FileSystemStorage.setItem(key, value);
+	},
+	removeItem(key) {
+		try {
+			return FileSystemStorage.removeItem(key);
+		} catch (e) {
+			//
+		}
+	}
+};
+
 const persistConfig = {
 	key: 'root',
 	version,
-	storage: AsyncStorage,
+	storage: MigratedStorage,
 	stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
 	migrate: createMigrate(migrations, { debug: false })
 };
