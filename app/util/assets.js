5a809a25a8480d90d61bb80c8ea944f14ac6e901
Esteban Mino <efmino@uc.cl>
2021-03-11 16:22:58 -0300

    getversionfrompackage

--- app/util/assets.js
+++ app/util/assets.js
@@ -1,11 +1,14 @@
+const pack = require('../../package.json'); // eslint-disable-line
+
 /**
  * Utility function to return corresponding @metamask/contract-metadata logo
  *
  * @param {string} logo - Logo path from @metamask/contract-metadata
  */
 export default function getAssetLogoPath(logo) {
+	const version = pack.dependencies['@metamask/contract-metadata']?.replace('^', '');
 	if (!logo) return;
-	const path = 'https://raw.githubusercontent.com/metamask/contract-metadata/v1.23.0/images/';
+	const path = `https://raw.githubusercontent.com/metamask/contract-metadata/v${version}/images/`;
 	const uri = path + logo;
 	return uri;
 }
