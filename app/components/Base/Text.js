48d4ea052e07b8cae97a884114281d3eb09bb796
Ricky Miller <ricky.miller@gmail.com>
2021-03-11 11:26:16 -0500

    Add colors to Base/Text component

--- app/components/Base/Text.js
+++ app/components/Base/Text.js
@@ -17,6 +17,12 @@ const style = StyleSheet.create({
 		textAlign: 'right'
 	},
 	bold: fontStyles.bold,
+	black: {
+		color: colors.black
+	},
+	blue: {
+		color: colors.blue
+	},
 	green: {
 		color: colors.green400
 	},
@@ -55,6 +61,8 @@ const Text = ({
 	right,
 	bold,
 	green,
+	black,
+	blue,
 	primary,
 	small,
 	upper,
@@ -73,6 +81,8 @@ const Text = ({
 			right && style.right,
 			bold && style.bold,
 			green && style.green,
+			black && style.black,
+			blue && style.blue,
 			primary && style.primary,
 			disclaimer && [style.small, style.disclaimer],
 			small && style.small,
@@ -93,6 +103,8 @@ Text.defaultProps = {
 	right: false,
 	bold: false,
 	green: false,
+	black: false,
+	blue: false,
 	primary: false,
 	disclaimer: false,
 	modal: false,
@@ -125,6 +137,14 @@ Text.propTypes = {
 	 * Makes text green
 	 */
 	green: PropTypes.bool,
+	/**
+	 * Makes text black
+	 */
+	black: PropTypes.bool,
+	/**
+	 * Makes text blue
+	 */
+	blue: PropTypes.bool,
 	/**
 	 * Makes text fontPrimary color
 	 */
