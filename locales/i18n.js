1653f9b1add3b388e480be39baa79bd9933a2948
Andre Pimenta <andrepimenta7@gmail.com>
2021-03-22 15:37:10 +0000

    Translations with update script (#2156)
    * translations and update script
    
    * Using new translation for es
    
    * Update gitignore
    
    * save old
    
    * Remove RTL
    
    * Fix qa issues
    
    * Update es.json
    
    * Update tests
    
    * Update vi-vn.json

--- locales/i18n.js
+++ locales/i18n.js
@@ -4,24 +4,42 @@ import I18n from 'react-native-i18n';
 import { LANGUAGE } from '../app/constants/storage';
 
 // Import all locales
-import en from './en.json';
-import es from './es.json';
-
+import en from './languages/en.json';
+import es from './languages/es.json';
+import hi from './languages/hi-in.json';
+import id from './languages/id-id.json';
+import ja from './languages/ja-jp.json';
+import ko from './languages/ko-kr.json';
+import pt from './languages/pt-br.json';
+import ru from './languages/ru-ru.json';
+import tl from './languages/tl.json';
+import vi from './languages/vi-vn.json';
+import zh from './languages/zh-cn.json';
 // Should the app fallback to English if user locale doesn't exists
 I18n.fallbacks = true;
 I18n.defaultLocale = 'en';
 // Define the supported translations
 I18n.translations = {
 	en,
-	es
+	es,
+	hi,
+	id,
+	ja,
+	ko,
+	pt,
+	ru,
+	tl,
+	vi,
+	zh
 };
 // If language selected get locale
 getUserPreferableLocale();
 
-const currentLocale = I18n.currentLocale();
+// Uncomment this for using RTL
+//const currentLocale = I18n.currentLocale();
 
 // Is it a RTL language?
-export const isRTL = currentLocale.indexOf('he') === 0 || currentLocale.indexOf('ar') === 0;
+export const isRTL = false; // currentLocale.indexOf('jaJp') === 0;
 
 // Set locale
 export async function setLocale(locale) {
@@ -31,7 +49,19 @@ export async function setLocale(locale) {
 
 // Get languages
 export function getLanguages() {
-	return { en: 'English', es: 'Spanish' };
+	return {
+		en: 'English',
+		es: 'Spanish',
+		hi: 'Hindi',
+		id: 'Bahasa Indonesian',
+		ja: 'Japanese',
+		ko: 'Korean',
+		pt: 'Portuguese - Brazil',
+		ru: 'Russian',
+		tl: 'Filipino',
+		vi: 'Vietnamese',
+		zh: 'Chinese - China'
+	};
 }
 
 // Allow RTL alignment in RTL languages
