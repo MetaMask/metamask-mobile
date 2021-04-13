9be58715ba0b4e01290a969a3e9175d922fa2cb0
Esteban Mino <efmino@uc.cl>
2021-04-13 14:15:11 -0400

    componentUI

--- app/components/Views/OfflineMode/index.js
+++ app/components/Views/OfflineMode/index.js
@@ -1,52 +1,44 @@
 'use strict';
 import React from 'react';
-import { SafeAreaView, Image, Text, View, StyleSheet } from 'react-native';
+import { SafeAreaView, Image, View, StyleSheet } from 'react-native';
+import Text from '../../Base/Text';
 import NetInfo from '@react-native-community/netinfo';
-import { colors, fontStyles } from '../../../styles/common';
+import { baseStyles, colors, fontStyles } from '../../../styles/common';
 import PropTypes from 'prop-types';
 import { strings } from '../../../../locales/i18n';
 import StyledButton from '../../UI/StyledButton';
 import { getOfflineModalNavbar } from '../../UI/Navbar';
 import AndroidBackHandler from '../AndroidBackHandler';
 import Device from '../../../util/Device';
+import AppConstants from '../../../core/AppConstants';
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		backgroundColor: colors.white
-	},
-	innerView: {
 		flex: 1
 	},
 	frame: {
 		width: 200,
 		height: 200,
 		alignSelf: 'center',
-		justifyContent: 'center',
-		marginTop: 80,
-		marginBottom: 30
+		marginTop: 60
 	},
 	content: {
 		flex: 1,
 		marginHorizontal: 18,
 		alignSelf: 'center',
-		justifyContent: 'center'
-	},
-	text: {
-		flex: 1,
-		fontSize: 12,
-		color: colors.fontPrimary,
-		textAlign: 'center',
 		justifyContent: 'center',
-		...fontStyles.normal
+		marginVertical: 30
 	},
 	title: {
 		fontSize: 18,
 		color: colors.fontPrimary,
-		textAlign: 'center',
-		justifyContent: 'center',
 		marginBottom: 10,
 		...fontStyles.bold
+	},
+	text: {
+		fontSize: 12,
+		color: colors.fontPrimary,
+		...fontStyles.normal
 	}
 });
 
@@ -61,27 +53,32 @@ const OfflineMode = ({ navigation }) => {
 		});
 	};
 
+	const learnMore = () => navigation.navigate('SimpleWebview', { url: AppConstants.URLS.CONNECTIVITY_ISSUES });
+
 	return (
-		<View style={styles.container}>
-			<SafeAreaView style={styles.innerView}>
-				<Image source={astronautImage} style={styles.frame} />
-				<View style={styles.content}>
-					<Text style={styles.title}>{strings('offline_mode.title')}</Text>
+		<SafeAreaView style={styles.container}>
+			<Image source={astronautImage} style={styles.frame} />
+			<View style={styles.content}>
+				<View style={baseStyles.flexGrow}>
+					<Text bold centered style={styles.title}>
+						{strings('offline_mode.title')}
+					</Text>
 					<Text
+						centered
 						style={styles.text}
 					>{`MetaMask is unable to reach our hosted blockchain connection. Visit this guide to review possible reasons.`}</Text>
-					<View>
-						<StyledButton type={'normal'} onPress={tryAgain}>
-							{strings('offline_mode.try_again')}
-						</StyledButton>
-						<StyledButton type={'blue'} onPress={tryAgain}>
-							{'Learn more'}
-						</StyledButton>
-					</View>
 				</View>
-			</SafeAreaView>
+				<View>
+					<StyledButton type={'normal'} onPress={tryAgain}>
+						{strings('offline_mode.try_again')}
+					</StyledButton>
+					<StyledButton type={'blue'} onPress={learnMore}>
+						{'Learn more'}
+					</StyledButton>
+				</View>
+			</View>
 			{Device.isAndroid() && <AndroidBackHandler customBackPress={tryAgain} />}
-		</View>
+		</SafeAreaView>
 	);
 };
 
