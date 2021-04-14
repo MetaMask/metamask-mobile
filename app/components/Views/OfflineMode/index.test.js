d7951ac3728336b4a818f254e44a33639b989db3
Esteban Miño <efmino@uc.cl>
2021-04-14 16:12:52 -0400

    Check infura availability (#2520)
    * addcheck
    
    * component
    
    * componentUI
    
    * checkProviderStatus
    
    * log
    
    * finalstyle
    
    * locales
    
    * tryagain
    
    * snapshots
    
    * checkInfuraAvailability
    
    * updatecopy
    
    * handleinfura
    
    * tests
    
    * CONNECTIVITY_ISSUESlink
    
    * copy
    
    * copy
    
    * translations
    
    * e
    
    * check
    
    * test

--- app/components/Views/OfflineMode/index.test.js
+++ app/components/Views/OfflineMode/index.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import configureMockStore from 'redux-mock-store';
 import OfflineMode from './';
+
+const mockStore = configureMockStore();
 
 describe('OfflineMode', () => {
 	it('should render correctly', () => {
-		const wrapper = shallow(<OfflineMode navigation={{ getParam: () => false }} />);
-		expect(wrapper).toMatchSnapshot();
+		const initialState = {
+			infuraAvailability: {
+				isBlocked: false
+			}
+		};
+		const wrapper = shallow(<OfflineMode navigation={{ getParam: () => false }} />, {
+			context: { store: mockStore(initialState) }
+		});
+		expect(wrapper.dive()).toMatchSnapshot();
 	});
 });
