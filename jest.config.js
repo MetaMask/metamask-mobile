f430eaa0937c9d0d46ee693480610a5e21138ad3
Ricky Miller <ricky.miller@gmail.com>
2021-04-15 11:40:56 -0400

    Add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line import/no-commonjs
+module.exports = {
+	preset: 'react-native',
+	setupFiles: ['<rootDir>/app/util/testSetup.js'],
+	transform: {
+		'^.+\\.js$': '<rootDir>jest.preprocessor.js'
+	},
+	transformIgnorePatterns: [
+		'node_modules/(?!(react-native|rn-fetch|redux-persist-filesystem|@react-navigation|@react-native|react-navigation|react-navigation-redux-helpers|@sentry))'
+	],
+	snapshotSerializers: ['enzyme-to-json/serializer'],
+	collectCoverage: false,
+	collectCoverageFrom: [
+		'<rootDir>/app/**/*.{js,jsx}',
+		'!<rootDir>/node_modules/',
+		'!<rootDir>/app/util/*.{js,jsx}',
+		'!<rootDir>/app/entry*.js'
+	]
+};
