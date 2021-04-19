c9ad43b385e768152e3bd757cb8ed5120638b9a5
Esteban Mino <efmino@uc.cl>
2021-04-19 12:23:10 -0400

    newscript

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
 
-set -e
-set -u
-set -o pipefail
-
 versionName="$(awk '/VERSION_NAME: /{print $2}' bitrise.yml)"
 
 MAJOR=$(awk -F. '{print $1}' <<< $versionName)
 MINOR=$(awk -F. '{print $2}' <<< $versionName)
 PATCH=$(awk -F. '{print $3}' <<< $versionName)
 
-RELEASE_TYPE=$1
+branchName=$(git rev-parse --symbolic --abbrev-ref HEAD)
 
-if [ "$1" ==  "major" ] ; then
+if [ $branchName == "--major" ] ; then
     major=$(($MAJOR +  1));
     minor="0";
     patch="0";
     version=$major'.'$minor'.'$patch
-elif [ "$1" ==  "minor" ] ; then
+elif [ $branchName == "--minor" ] ; then
     minor=$(($MINOR +  1));
     patch="0";
     version=$MAJOR'.'$minor'.'$patch
-elif [ "$1" ==  "patch" ] ; then
+elif [ $branchName == "--patch" ] ; then
     patch=$(($PATCH +  1));
     version=$MAJOR'.'$MINOR'.'$patch
+else
+    version=$MAJOR'.'$MINOR'.'$PATCH
 fi
 sed -i '' -e 's/VERSION_NAME: [0-9]\{1,\}.[0-9]\{1,\}.[0-9]\{1,\}/VERSION_NAME: '$version'/' bitrise.yml
 echo $version
+echo $branchName
