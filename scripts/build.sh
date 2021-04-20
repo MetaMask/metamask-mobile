dd591a666203a5150b6613e2134ca5019d4f0709
sethkfman <seth.kaufman@consensys.net>
2021-04-20 15:10:20 -0600

    uploaded keystore file android test

--- scripts/build.sh
+++ scripts/build.sh
@@ -228,20 +228,20 @@ buildAndroidRelease(){
 	cd android && ./gradlew assembleRelease --no-daemon --max-workers 2
 
 	# # GENERATE BUNDLE
-	# if [ "$GENERATE_BUNDLE" = true ] ; then
-	# 	./gradlew bundleDebug
-	# fi
+	if [ "$GENERATE_BUNDLE" = true ] ; then
+		./gradlew bundleDebug
+	fi
 
-	# if [ "$PRE_RELEASE" = true ] ; then
-	# 	# Generate sourcemaps
-	# 	yarn sourcemaps:android
-	# 	# Generate checksum
-	# 	yarn build:android:checksum
-	# fi
+	if [ "$PRE_RELEASE" = true ] ; then
+		# Generate sourcemaps
+		yarn sourcemaps:android
+		# Generate checksum
+		yarn build:android:checksum
+	fi
 
-	# if [ "$PRE_RELEASE" = false ] ; then
-	# 	adb install app/build/outputs/apk/release/app-release.apk
-	# fi
+	if [ "$PRE_RELEASE" = false ] ; then
+		adb install app/build/outputs/apk/release/app-release.apk
+	fi
 }
 
 buildAndroidReleaseE2E(){
