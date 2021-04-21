218ce9985898391ffd03c6cb652850c0f13b0896
Esteban Mino <efmino@uc.cl>
2021-04-21 16:11:08 -0400

    branchasinput

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -9,26 +9,23 @@ versionName="$(awk '/VERSION_NAME: /{print $2}' bitrise.yml)"
 MAJOR=$(awk -F. '{print $1}' <<< $versionName)
 MINOR=$(awk -F. '{print $2}' <<< $versionName)
 PATCH=$(awk -F. '{print $3}' <<< $versionName)
-version='a'
-branchName=$(git rev-parse --symbolic --abbrev-ref HEAD)
-# branchName='release/aaa-patch'
 version=$MAJOR'.'$MINOR'.'$PATCH
 
-if [[ $branchName == *"release/"* ]] ; then
-    if [[ $branchName == *"-major"* ]] ; then
+if [[ $1 == *"release/"* ]] ; then
+    if [[ $1 == *"-major"* ]] ; then
         major=$(($MAJOR +  1));
         minor="0";
         patch="0";
         version=$major'.'$minor'.'$patch
-    elif [[ $branchName == *"-minor"* ]] ; then
+    elif [[ $1 == *"-minor"* ]] ; then
         minor=$(($MINOR +  1));
         patch="0";
         version=$MAJOR'.'$minor'.'$patch
-    elif [[ $branchName == *"-patch"* ]] ; then
+    elif [[ $1 == *"-patch"* ]] ; then
         patch=$(($PATCH +  1));
         version=$MAJOR'.'$MINOR'.'$patch
     fi
 fi
 sed -i '' -e 's/VERSION_NAME: [0-9]\{1,\}.[0-9]\{1,\}.[0-9]\{1,\}/VERSION_NAME: '$version'/' bitrise.yml
 echo $version
-echo $branchName
+echo $1
