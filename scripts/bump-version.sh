204ff6ab7948ab2d31a9c9eefded2cef7455fadd
Esteban Miño <efmino@uc.cl>
2021-04-21 17:05:46 -0400

    Logs (#9)
    * a
    
    * Revert "a"
    
    This reverts commit 5df3682b72954f3698d2cc8dd981de109f0f6746.
    
    * logs

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -26,6 +26,9 @@ if [[ $1 == *"release/"* ]] ; then
         version=$MAJOR'.'$MINOR'.'$patch
     fi
 fi
-sed -i '' -e 's/VERSION_NAME: [0-9]\{1,\}.[0-9]\{1,\}.[0-9]\{1,\}/VERSION_NAME: '$version'/' bitrise.yml
+
+echo 'GOT THE VERSION'
 echo $version
+echo "BRANCH"
 echo $1
+sed -i '' -e 's/VERSION_NAME: [0-9]\{1,\}.[0-9]\{1,\}.[0-9]\{1,\}/VERSION_NAME: '$version'/' bitrise.yml
