3a2387f4b4e79f7d83fd12db784621e5d8c13534
andrepimenta <andrepimenta7@gmail.com>
2021-04-22 12:29:21 +0100

    Improve rpc errors logging and removing user rejected errors

--- app/util/middlewares.js
+++ app/util/middlewares.js
@@ -1,6 +1,18 @@
 import Logger from './Logger';
 
-const REJECTED_TRANSACTION_ERROR = 'User rejected the transaction';
+/**
+ * List of rpc errors caused by the user rejecting a certain action.
+ * Errors that include these phrases should not be logged to Sentry.
+ * Examples of these errors include:
+ * - User rejected the transaction
+ * - User cancelled the transaction
+ * - User rejected the request.
+ * - MetaMask Message Signature: User denied message signature.
+ * - MetaMask Personal Message Signature: User denied message signature.
+ */
+const USER_REJECTED_ERRORS = ['user rejected', 'user denied', 'user cancelled'];
+
+const USER_REJECTED_ERROR_CODE = 4001;
 
 /**
  * Returns a middleware that appends the DApp origin to request
@@ -22,6 +34,29 @@ export function createOriginMiddleware(opts) {
 }
 
 /**
+ * Checks if the error code or message contains a user rejected error
+ * @param {String} errorMessage
+ * @returns {boolean}
+ */
+function containsUserRejectedError(errorMessage, errorCode) {
+	try {
+		if (!errorMessage || !errorMessage.includes || !errorMessage.toLowerCase) return false;
+
+		const userRejectedErrorMessage = USER_REJECTED_ERRORS.some(userRejectedError =>
+			errorMessage.toLowerCase().includes(userRejectedError.toLowerCase())
+		);
+
+		if (userRejectedErrorMessage) return true;
+
+		if (errorCode === USER_REJECTED_ERROR_CODE) return true;
+
+		return false;
+	} catch (e) {
+		return false;
+	}
+}
+
+/**
  * Returns a middleware that logs RPC activity
  * @param {{ origin: string }} opts - The middleware options
  * @returns {Function}
@@ -31,8 +66,20 @@ export function createLoggerMiddleware(opts) {
 		next((/** @type {Function} */ cb) => {
 			if (res.error) {
 				const { error, ...resWithoutError } = res;
-				if (error && error.message !== REJECTED_TRANSACTION_ERROR) {
-					Logger.error(error, { message: 'Error in RPC response', res: resWithoutError });
+				if (error) {
+					if (containsUserRejectedError(error.message, error.code)) {
+						//TODO After merging PR 2529, use return trackErrorAsAnalytics(`Error in RPC response: User rejected`, error.message);
+					} else {
+						let errorToLog = error;
+						if (error.data && error.data.message) {
+							errorToLog = new Error(error.data.message);
+						}
+						Logger.error(errorToLog, {
+							message: error.message,
+							res: resWithoutError,
+							data: error.data
+						});
+					}
 				}
 			}
 			if (req.isMetamaskInternal) {
