ff74582f3af8e5949c429d36e3ff97716c60c1c1
Ricky Miller <ricky.miller@gmail.com>
2021-04-23 13:10:41 -0400

    useRef instead of useMemo

--- app/components/Views/Entry/index.js
+++ app/components/Views/Entry/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import { withNavigation } from 'react-navigation';
 import PropTypes from 'prop-types';
 import { Animated, Dimensions, StyleSheet, View } from 'react-native';
@@ -76,7 +76,7 @@ const Entry = props => {
 
 	const animation = useRef(null);
 	const animationName = useRef(null);
-	const opacity = useMemo(() => new Animated.Value(1), []);
+	const opacity = useRef(new Animated.Value(1)).current;
 
 	const onAnimationFinished = useCallback(() => {
 		Animated.timing(opacity, {
