01f3554529b6a432a6bcc00db1e87ec64936308a
sethkfman <seth.kaufman@consensys.net>
2021-04-23 13:18:37 -0600

    updated slack announce script

--- scripts/metamask-bot-build-announce-bitrise.js
+++ scripts/metamask-bot-build-announce-bitrise.js
@@ -1,25 +1,45 @@
 #!/usr/bin/env node
 /* eslint-disable import/no-commonjs */
 const request = require('request-promise');
+const github = require('octonode');
 
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const ANDROID_APK_LINK = process.env.BITRISE_PUBLIC_INSTALL_PAGE_URL;
 const ANDROID_AAB_LINK = process.env.BITRISE_PUBLIC_INSTALL_PAGE_URL;
-// const IOS_APP_LINK = process.env.IOS_APP_LINK;
+const GIT_PROJECT_REPONAME = 'metamask-mobile';
+const BITRISEIO_GIT_REPOSITORY_OWNER = process.env.BITRISEIO_GIT_REPOSITORY_OWNER;
 const SLACK_TOKEN = process.env.MM_SLACK_TOKEN;
 const SLACK_SECRET = process.env.MM_SLACK_SECRET;
 const SLACK_ROOM = process.env.MM_SLACK_ROOM;
-const BITRISEIO_PULL_REQUEST_REPOSITORY_URL = process.env.BITRISEIO_PULL_REQUEST_REPOSITORY_URL;
-const BITRISE_PULL_REQUEST = process.env.BITRISEIO_PULL_REQUEST;
-const BITRISE_GIT_MESSAGE = process.env.BITRISEIO_GIT_MESSAGE;
+const BITRISE_GIT_COMMIT = process.env.BITRISE_GIT_COMMIT;
 
 start().catch(console.error);
 
+async function getPRInfo() {
+	const client = github.client(GITHUB_TOKEN);
+	const REPO = client.repo(`${BITRISEIO_GIT_REPOSITORY_OWNER}/${GIT_PROJECT_REPONAME}`);
+	const response = await REPO.prsAsync({ state: 'closed' });
+	const PR = response[0].find(obj => obj.merge_commit_sha === BITRISE_GIT_COMMIT);
+	console.log('BITRISE_GIT_COMMIT', BITRISE_GIT_COMMIT);
+	console.log('RESPONSE', response[0]);
+
+	if (PR) {
+		return { title: PR.title, number: PR.number, url: PR.html_url };
+	}
+}
+
 async function start() {
+	const PR_INFO = await getPRInfo();
+	console.log('PR_URL', PR_INFO.url);
+	console.log('BITRISE_GIT_COMMIT', BITRISE_GIT_COMMIT);
+
 	const content = {
-		text: `THIS IS A TEST FOR THE NEW BITRISE INTEGRATION - NEW BUILDS AVAILABLE! Including <${BITRISEIO_PULL_REQUEST_REPOSITORY_URL}|#${BITRISE_PULL_REQUEST} - ${BITRISE_GIT_MESSAGE}>`,
+		text: `THIS IS A TEST FOR THE NEW BITRISE INTEGRATION - NEW BUILDS AVAILABLE! Including <${PR_INFO.url}|#${
+			PR_INFO.number
+		} - ${PR_INFO.title}>`,
 		attachments: [
 			{
-				title_link: 'TEST',
+				title_link: 'itms-beta://beta.itunes.apple.com/v1/app/1438144202',
 				title: 'iOS',
 				text: 'Install via Bitrise',
 				thumb_url:
