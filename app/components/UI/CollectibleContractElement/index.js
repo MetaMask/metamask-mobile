b5f9c6ae490331bf11bf08e500e5cd5f56aa5e26
Esteban Mino <efmino@uc.cl>
2021-04-27 18:40:56 -0400

    styles

--- app/components/UI/CollectibleContractElement/index.js
+++ app/components/UI/CollectibleContractElement/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, View, Text } from 'react-native';
 import { colors, fontStyles } from '../../../styles/common';
 import CollectibleImage from '../CollectibleImage';
 import Icon from 'react-native-vector-icons/Ionicons';
+import Device from '../../../util/Device';
+
+const DEVICE_WIDTH = Device.getDeviceWidth();
 
 const styles = StyleSheet.create({
 	itemWrapper: {
 		paddingHorizontal: 15,
 		paddingTop: 15
 	},
-	collectibleIcon: { width: 30, height: 30 },
-	collectibleIconContainer: { width: 30, height: 30, marginHorizontal: 8 },
+	collectibleContractIcon: { width: 30, height: 30 },
+	collectibleContractIconContainer: { marginHorizontal: 8 },
 	titleContainer: {
 		flex: 1,
 		flexDirection: 'row'
@@ -24,24 +27,41 @@ const styles = StyleSheet.create({
 		fontSize: 16,
 		color: colors.grey500,
 		...fontStyles.normal
+	},
+	collectibleIcon: {
+		width: (DEVICE_WIDTH - 30 - 16) / 3,
+		height: (DEVICE_WIDTH - 30 - 16) / 3
+	},
+	collectiblesRowContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		marginTop: 15,
+		justifyContent: 'space-between'
+	},
+	collectibleBox: {
+		flex: 1,
+		flexDirection: 'row'
 	}
 });
 
 /**
  * Customizable view to render assets in lists
  */
-export default function CollectibleContractElement({ asset, onPress, contractCollectibles }) {
-	const handleOnPress = () => onPress(asset);
+export default function CollectibleContractElement({ asset, contractCollectibles }) {
+	const [collectiblesToRender, setCollectiblesToRender] = useState([]);
+	useEffect(() => {
+		setCollectiblesToRender(contractCollectibles.concat(contractCollectibles));
+	}, [contractCollectibles, setCollectiblesToRender]);
 
 	return (
-		<View onPress={handleOnPress} style={styles.itemWrapper}>
+		<View style={styles.itemWrapper}>
 			<View style={styles.titleContainer}>
 				<View style={styles.verticalAlignedContainer}>
 					<Icon name="ios-arrow-down" size={12} color={colors.fontTertiary} style={styles.arrowIcon} />
 				</View>
 				<CollectibleImage
-					iconStyle={styles.collectibleIcon}
-					containerStyle={styles.collectibleIconContainer}
+					iconStyle={styles.collectibleContractIcon}
+					containerStyle={[styles.collectibleContractIcon, styles.collectibleContractIconContainer]}
 					collectible={{ ...asset, image: asset.logo }}
 				/>
 				<View style={styles.verticalAlignedContainer}>
@@ -49,6 +69,17 @@ export default function CollectibleContractElement({ asset, onPress, contractCol
 						{asset.name}
 					</Text>
 				</View>
+			</View>
+			<View style={styles.collectiblesRowContainer}>
+				{collectiblesToRender.map(({ name, address, tokenId, image }) => (
+					<View key={address + tokenId} styles={styles.collectibleBox}>
+						<CollectibleImage
+							iconStyle={styles.collectibleIcon}
+							containerStyle={styles.collectibleIcon}
+							collectible={{ name, address, tokenId, image }}
+						/>
+					</View>
+				))}
 			</View>
 		</View>
 	);
@@ -59,10 +90,6 @@ CollectibleContractElement.propTypes = {
 	 * Object being rendered
 	 */
 	asset: PropTypes.object,
-	/**
-	 * Callback triggered on long press
-	 */
-	onPress: PropTypes.func,
 	/**
 	 * Array of collectibles
 	 */
