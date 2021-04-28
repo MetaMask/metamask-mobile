b7f0d89e18ef0adb5accd2f65c1230580dcb993f
Esteban Mino <efmino@uc.cl>
2021-04-28 11:42:06 -0400

    muted

--- app/components/Views/MediaReproductor/index.js
+++ app/components/Views/MediaReproductor/index.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Video from 'react-native-video';
 import PropTypes from 'prop-types';
+import { View, TouchableWithoutFeedback, StyleSheet } from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
+import { colors } from '../../../styles/common';
+
+const styles = StyleSheet.create({
+	mutedIcon: {
+		marginTop: -20
+	}
+});
 
 function MediaReproductor({ uri, style }) {
+	const [muted, setMuted] = useState(true);
+
 	const onBuffer = () => console.log('s');
 	const onError = () => console.log('s');
+	const toggleMuted = () => setMuted(!muted);
 	return (
-		<Video
-			source={{ uri }}
-			ref={ref => {
-				this.player = ref;
-			}}
-			onBuffer={onBuffer}
-			onError={onError}
-			style={style}
-		/>
+		<View>
+			<TouchableWithoutFeedback onPress={toggleMuted}>
+				<View>
+					<Video
+						source={{ uri }}
+						ref={ref => {
+							this.player = ref;
+						}}
+						onBuffer={onBuffer}
+						onError={onError}
+						style={style}
+						muted={muted}
+					/>
+					<Entypo
+						name={`sound${muted ? '-mute' : ''}`}
+						size={20}
+						color={colors.orange}
+						style={styles.mutedIcon}
+					/>
+				</View>
+			</TouchableWithoutFeedback>
+		</View>
 	);
 }
 
