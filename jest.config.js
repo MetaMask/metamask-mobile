650d2ef2ce8d4cdf4e5692887a58c453bae2b091
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2021-04-28 15:05:07 -0700

    Update Jest config (#53)
    Updates the Jest config to:
    
    - Include files ending in `.test.js`, not just `.test.ts`
    - Reset mocks
    - Restore mocks
    - Decrease test timeout from 5000 to 2500 ms

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,14 @@ module.exports = {
   },
   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
   preset: 'ts-jest',
+  // "resetMocks" resets all mocks, including mocked modules, to jest.fn(),
+  // between each test case.
+  resetMocks: true,
+  // "restoreMocks" restores all mocks created using jest.spyOn to their
+  // original implementations, between each test. It does not affect mocked
+  // modules.
+  restoreMocks: true,
   testEnvironment: 'node',
-  testRegex: ['\\.test\\.ts$'],
-  testTimeout: 5000,
+  testRegex: ['\\.test\\.(ts|js)$'],
+  testTimeout: 2500,
 };
