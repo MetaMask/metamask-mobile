f884416eeaf31b54a4ef16a5aa9c75e90c32f6ef
Ricky Miller <ricky.miller@gmail.com>
2021-04-29 13:00:48 -0400

    Add cover to video

--- app/components/UI/SeedPhraseVideo/index.js
+++ app/components/UI/SeedPhraseVideo/index.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Image, TouchableOpacity } from 'react-native';
 import { TextTrackType } from 'react-native-video';
 import VideoPlayer from 'react-native-video-controls';
+import { colors } from '../../../styles/common';
 import Logger from '../../../util/Logger';
+import scaling from '../../../util/scaling';
 
 const styles = StyleSheet.create({
 	videoContainer: {
+		justifyContent: 'center',
+		alignItems: 'center',
 		position: 'relative',
 		borderRadius: 8,
 		overflow: 'hidden',
 		width: '100%',
-		height: 180,
-		flex: 1
+		height: 180
+	},
+	image: {
+		zIndex: 0,
+		width: scaling.scale(138),
+		height: scaling.scale(162)
+	},
+	cover: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		zIndex: 1,
+		position: 'absolute',
+		left: 0,
+		top: 0,
+		backgroundColor: colors.grey,
+		opacity: 0.3,
+		width: '100%',
+		height: '100%'
 	}
 });
+
+// eslint-disable-next-line import/no-commonjs
+const explain_backup_seedphrase = require('../../../images/explain-backup-seedphrase.png');
 
 // eslint-disable-next-line import/no-commonjs
 const vid = require('../../../../app/videos/recovery-phrase.mp4');
@@ -21,32 +44,50 @@ const VTT_URI =
 	'https://github.com/MetaMask/metamask-extension/blob/develop/app/images/videos/recovery-onboarding/subtitles-en.vtt';
 
 const SeedPhraseVideo = () => {
+	const [isPlaying, setPlaying] = useState(false);
 	const onError = e => Logger.error(e, 'Video failed');
-	const onPlay = () => Logger.log('User clicked play');
+	const onPlay = () => {
+		Logger.log('User clicked play');
+		setPlaying(true);
+	};
+
 	return (
 		<View style={styles.videoContainer}>
-			<VideoPlayer
-				paused // paused to start
-				disableFullscreen
-				disableBack
-				source={vid}
-				textTracks={[
-					{
-						title: 'English CC',
-						language: 'en',
-						type: TextTrackType.VTT,
-						uri: VTT_URI
-					}
-				]}
-				selectedTextTrack={{
-					type: 'language',
-					value: 'en'
-				}}
-				style={StyleSheet.absoluteFill}
-				onError={onError}
-				onPlay={onPlay}
-				resizeMode="cover"
-			/>
+			{!isPlaying ? (
+				<>
+					<TouchableOpacity style={styles.cover} onPress={onPlay}>
+						<></>
+					</TouchableOpacity>
+					<Image
+						source={explain_backup_seedphrase}
+						style={styles.image}
+						resizeMethod="auto"
+						testID={'carousel-one-image'}
+					/>
+				</>
+			) : (
+				<VideoPlayer
+					disableFullscreen
+					disableBack
+					source={vid}
+					textTracks={[
+						{
+							title: 'English CC',
+							language: 'en',
+							type: TextTrackType.VTT,
+							uri: VTT_URI
+						}
+					]}
+					selectedTextTrack={{
+						type: 'language',
+						value: 'en'
+					}}
+					style={StyleSheet.absoluteFill}
+					onError={onError}
+					onPlay={onPlay}
+					resizeMode="cover"
+				/>
+			)}
 		</View>
 	);
 };
