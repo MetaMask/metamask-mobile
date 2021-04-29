5ca20395484e2116365f381815379cae6563e97d
Ricky Miller <ricky.miller@gmail.com>
2021-04-29 11:44:35 -0400

    Remove placeholder video and add recovery-phrase

--- app/components/UI/SeedPhraseVideo/index.js
+++ app/components/UI/SeedPhraseVideo/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Video, { TextTrackType } from 'react-native-video';
+import Logger from '../../../util/Logger';
 
 const styles = StyleSheet.create({
 	videoContainer: {
+		position: 'relative',
 		borderRadius: 8,
 		overflow: 'hidden',
 		width: '100%',
@@ -13,32 +15,36 @@ const styles = StyleSheet.create({
 });
 
 // eslint-disable-next-line import/no-commonjs
-const vid = require('../../../../app/videos/placeholder.mp4');
+const vid = require('../../../../app/videos/recovery-phrase.mp4');
 const VTT_URI =
 	// TODO: can we load this asset as local resource? (like above)
 	'https://github.com/MetaMask/metamask-extension/blob/develop/app/images/videos/recovery-onboarding/subtitles-en.vtt';
 
-const SeedPhraseVideo = () => (
-	<View style={styles.videoContainer}>
-		<Video
-			source={vid}
-			textTracks={[
-				{
-					title: 'English CC',
-					language: 'en',
-					type: TextTrackType.VTT,
-					uri: VTT_URI
-				}
-			]}
-			selectedTextTrack={{
-				type: 'language',
-				value: 'en'
-			}}
-			style={StyleSheet.absoluteFill}
-			controls
-			resizeMode="cover"
-		/>
-	</View>
-);
+const SeedPhraseVideo = () => {
+	const onError = e => Logger.error(e, 'Video failed.');
+	return (
+		<View style={styles.videoContainer}>
+			<Video
+				source={vid}
+				textTracks={[
+					{
+						title: 'English CC',
+						language: 'en',
+						type: TextTrackType.VTT,
+						uri: VTT_URI
+					}
+				]}
+				selectedTextTrack={{
+					type: 'language',
+					value: 'en'
+				}}
+				style={StyleSheet.absoluteFill}
+				controls
+				onError={onError}
+				resizeMode="cover"
+			/>
+		</View>
+	);
+};
 
 export default SeedPhraseVideo;
