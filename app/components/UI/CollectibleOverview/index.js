1b9807d944257145be82e2b708fdeb19a10e8355
Esteban Mino <efmino@uc.cl>
2021-05-05 12:43:53 -0400

    functional

--- app/components/UI/CollectibleOverview/index.js
+++ app/components/UI/CollectibleOverview/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import PropTypes from 'prop-types';
 import { colors } from '../../../styles/common';
@@ -97,40 +97,13 @@ const styles = StyleSheet.create({
 /**
  * View that displays the information of a specific ERC-721 Token
  */
-export default class CollectibleOverview extends PureComponent {
-	static propTypes = {
-		/**
-		 * Object that represents the collectible to be displayed
-		 */
-		collectible: PropTypes.object,
-		/**
-		 * Represents if the collectible is tradable (can be sent)
-		 */
-		tradable: PropTypes.bool,
-		/**
-		 * Function called when user presses the Send button
-		 */
-		onSend: PropTypes.func,
-		/**
-		 * Object that represents the navigator
-		 */
-		navigation: PropTypes.object
-	};
+const CollectibleOverview = ({ collectible, tradable, onSend, navigation }) => {
+	const renderImage = () => <CollectibleMedia big renderAnimation collectible={collectible} />;
 
-	renderImage = () => {
-		const { collectible } = this.props;
-		return <CollectibleMedia big renderAnimation collectible={collectible} />;
-	};
+	const openLink = url => navigation.navigate('SimpleWebview', { url });
 
-	openLink = url => {
-		this.props.navigation.navigate('BrowserView', {
-			newTabUrl: url
-		});
-	};
-
-	renderCollectibleInfoRow = (key, value, isLink) => {
+	const renderCollectibleInfoRow = (key, value, isLink) => {
 		if (!value) return null;
-
 		return (
 			<View style={styles.collectibleInfoContainer} key={key}>
 				<Text noMargin black bold big style={styles.collectibleInfoKey}>
@@ -145,7 +118,7 @@ export default class CollectibleOverview extends PureComponent {
 					style={styles.collectibleInfoValue}
 					numberOfLines={1}
 					ellipsizeMode="middle"
-					onPress={isLink ? () => this.openLink(value) : null}
+					onPress={isLink ? () => openLink(value) : null}
 				>
 					{value}
 				</Text>
@@ -153,122 +126,130 @@ export default class CollectibleOverview extends PureComponent {
 		);
 	};
 
-	renderCollectibleInfo = () => {
-		const { collectible } = this.props;
+	const renderCollectibleInfo = () => [
+		renderCollectibleInfoRow(
+			strings('collectible.collectible_last_sold'),
+			collectible?.lastSale?.event_timestamp &&
+				toLocaleDate(new Date(collectible?.lastSale?.event_timestamp)).toString()
+		),
+		renderCollectibleInfoRow(
+			strings('collectible.collectible_last_price_sold'),
+			collectible?.lastSale?.total_price && `${renderFromWei(collectible?.lastSale?.total_price)} ETH`
+		),
+		renderCollectibleInfoRow(strings('collectible.collectible_source'), collectible?.imageOriginal, true),
+		renderCollectibleInfoRow(strings('collectible.collectible_link'), collectible?.externalLink, true),
+		renderCollectibleInfoRow(strings('collectible.collectible_asset_contract'), collectible?.address, true)
+	];
 
-		return [
-			this.renderCollectibleInfoRow(
-				strings('collectible.collectible_last_sold'),
-				collectible?.lastSale?.event_timestamp &&
-					toLocaleDate(new Date(collectible?.lastSale?.event_timestamp)).toString()
-			),
-			this.renderCollectibleInfoRow(
-				strings('collectible.collectible_last_price_sold'),
-				collectible?.lastSale?.total_price && `${renderFromWei(collectible?.lastSale?.total_price)} ETH`
-			),
-			this.renderCollectibleInfoRow(strings('collectible.collectible_source'), collectible?.imageOriginal, true),
-			this.renderCollectibleInfoRow(strings('collectible.collectible_link'), collectible?.externalLink, true),
-			this.renderCollectibleInfoRow(strings('collectible.collectible_asset_contract'), collectible?.address, true)
-		];
-	};
-
-	addCollectibleToFavorites = () => {
+	const addCollectibleToFavorites = () => {
 		// TODO: Add collectible to favorites was pressed
 	};
 
-	shareCollectible = () => {
+	const shareCollectible = () => {
 		// TODO: Share collectible was pressed
 	};
 
-	render = () => {
-		const {
-			collectible: { name, tokenId },
-			collectible,
-			tradable,
-			onSend
-		} = this.props;
+	// TODO: Get favorited status here or directly from props
+	const favorited = false;
 
-		// TODO: Get favorited status here or directly from props
-		const favorited = false;
-
-		return (
-			<View style={styles.wrapper}>
-				<View style={styles.basicsWrapper}>
-					<View>{this.renderImage()}</View>
-				</View>
-
-				<View style={styles.informationWrapper}>
-					{collectible?.creator && (
-						<View style={styles.userContainer}>
-							<RemoteImage
-								fadeIn
-								placeholderStyle={{ backgroundColor: colors.white }}
-								source={{ uri: collectible.creator.profile_img_url }}
-								style={styles.userImage}
-							/>
-							<View style={styles.userInfoContainer}>
-								{collectible.creator.user?.username && (
-									<Text black bold noMargin big style={styles.username}>
-										{collectible.creator.user.username}
-									</Text>
-								)}
-								<Text black noMargin small>
-									{collectible.contractName}
-								</Text>
-							</View>
-						</View>
-					)}
-					<Text bold primary noMargin style={styles.name}>
-						{name}
-					</Text>
-					<Text primary noMargin style={styles.tokenId}>
-						{strings('unit.token_id')}
-						{tokenId}
-					</Text>
-
-					<View style={styles.buttonContainer}>
-						{tradable && (
-							<StyledButton onPress={onSend} type={'rounded-normal'} containerStyle={styles.sendButton}>
-								<Text link big bold noMargin>
-									{strings('asset_overview.send_button')}
-								</Text>
-							</StyledButton>
-						)}
-						<StyledButton
-							type={'rounded-normal'}
-							containerStyle={styles.iconButtons}
-							onPress={this.shareCollectible}
-						>
-							<Text bold link noMargin>
-								<EvilIcons name={Device.isIos() ? 'share-apple' : 'share-google'} size={32} />
-							</Text>
-						</StyledButton>
-						<StyledButton
-							type={'rounded-normal'}
-							containerStyle={styles.iconButtons}
-							onPress={this.addCollectibleToFavorites}
-						>
-							<Text link noMargin>
-								<AntIcons name={favorited ? 'star' : 'staro'} size={24} />
-							</Text>
-						</StyledButton>
-					</View>
-
-					{collectible?.description && (
-						<View style={styles.information}>
-							<View style={styles.row}>
-								<Text noMargin black bold big>
-									{strings('collectible.collectible_description')}
-								</Text>
-								<Text noMargin black style={styles.content}>
-									{collectible.description}
-								</Text>
-							</View>
-						</View>
-					)}
-					<View style={styles.information}>{this.renderCollectibleInfo()}</View>
-				</View>
+	return (
+		<View style={styles.wrapper}>
+			<View style={styles.basicsWrapper}>
+				<View>{renderImage()}</View>
 			</View>
-		);
-	};
-}
+
+			<View style={styles.informationWrapper}>
+				{collectible?.creator && (
+					<View style={styles.userContainer}>
+						<RemoteImage
+							fadeIn
+							placeholderStyle={{ backgroundColor: colors.white }}
+							source={{ uri: collectible.creator.profile_img_url }}
+							style={styles.userImage}
+						/>
+						<View style={styles.userInfoContainer}>
+							{collectible.creator.user?.username && (
+								<Text black bold noMargin big style={styles.username}>
+									{collectible.creator.user.username}
+								</Text>
+							)}
+							<Text black noMargin small>
+								{collectible.contractName}
+							</Text>
+						</View>
+					</View>
+				)}
+				<Text bold primary noMargin style={styles.name}>
+					{collectible.name}
+				</Text>
+				<Text primary noMargin style={styles.tokenId}>
+					{strings('unit.token_id')}
+					{collectible.tokenId}
+				</Text>
+
+				<View style={styles.buttonContainer}>
+					{tradable && (
+						<StyledButton onPress={onSend} type={'rounded-normal'} containerStyle={styles.sendButton}>
+							<Text link big bold noMargin>
+								{strings('asset_overview.send_button')}
+							</Text>
+						</StyledButton>
+					)}
+					<StyledButton
+						type={'rounded-normal'}
+						containerStyle={styles.iconButtons}
+						onPress={shareCollectible}
+					>
+						<Text bold link noMargin>
+							<EvilIcons name={Device.isIos() ? 'share-apple' : 'share-google'} size={32} />
+						</Text>
+					</StyledButton>
+					<StyledButton
+						type={'rounded-normal'}
+						containerStyle={styles.iconButtons}
+						onPress={addCollectibleToFavorites}
+					>
+						<Text link noMargin>
+							<AntIcons name={favorited ? 'star' : 'staro'} size={24} />
+						</Text>
+					</StyledButton>
+				</View>
+
+				{collectible?.description && (
+					<View style={styles.information}>
+						<View style={styles.row}>
+							<Text noMargin black bold big>
+								{strings('collectible.collectible_description')}
+							</Text>
+							<Text noMargin black style={styles.content}>
+								{collectible.description}
+							</Text>
+						</View>
+					</View>
+				)}
+				<View style={styles.information}>{renderCollectibleInfo()}</View>
+			</View>
+		</View>
+	);
+};
+
+CollectibleOverview.propTypes = {
+	/**
+	 * Object that represents the collectible to be displayed
+	 */
+	collectible: PropTypes.object,
+	/**
+	 * Represents if the collectible is tradable (can be sent)
+	 */
+	tradable: PropTypes.bool,
+	/**
+	 * Function called when user presses the Send button
+	 */
+	onSend: PropTypes.func,
+	/**
+	 * Object that represents the navigator
+	 */
+	navigation: PropTypes.object
+};
+
+export default CollectibleOverview;
