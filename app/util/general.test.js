f769ef06bdc9fe90519ef9c6151d5750d1702d80
Ricky Miller <ricky.miller@gmail.com>
2021-05-06 10:16:03 -0400

    Update toLowerCaseCompare test

--- app/util/general.test.js
+++ app/util/general.test.js
@@ -18,14 +18,15 @@ describe('tlc', () => {
 });
 
 describe('toLowerCaseCompare', () => {
+	const o = {};
 	it('compares two things', () => {
 		expect(toLowerCaseCompare('A', 'A')).toEqual(true);
 		expect(toLowerCaseCompare('aBCDefH', 'abcdefh')).toEqual(true);
 		expect(toLowerCaseCompare('A', 'B')).toEqual(false);
 		expect(toLowerCaseCompare('aBCDefH', 'abcdefi')).toEqual(false);
 		// cases where a or b are undefined
-		expect(toLowerCaseCompare({}.undefined, 'A')).toEqual(false);
-		expect(toLowerCaseCompare('A', {}.undefined)).toEqual(false);
+		expect(toLowerCaseCompare(o.p, 'A')).toEqual(false);
+		expect(toLowerCaseCompare('A', o.p)).toEqual(false);
 		expect(toLowerCaseCompare(undefined, 'A')).toEqual(false);
 		expect(toLowerCaseCompare('A', undefined)).toEqual(false);
 	});
