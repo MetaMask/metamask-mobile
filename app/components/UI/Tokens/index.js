0b599ca3f52e600441f69b3d7ef0e80d42a8af5c
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2021-05-07 08:11:30 -0400

    Exclude native asset from hiding when balance is zero (#2639)

--- app/components/UI/Tokens/index.js
+++ app/components/UI/Tokens/index.js
@@ -265,8 +265,8 @@ class Tokens extends PureComponent {
 		const { tokens, hideZeroBalanceTokens, tokenBalances } = this.props;
 		const tokensToDisplay = hideZeroBalanceTokens
 			? tokens.filter(token => {
-					const { address, symbol } = token;
-					return (tokenBalances[address] && !tokenBalances[address]?.isZero?.()) || symbol === 'ETH';
+					const { address, isETH } = token;
+					return (tokenBalances[address] && !tokenBalances[address]?.isZero?.()) || isETH;
 					// eslint-disable-next-line no-mixed-spaces-and-tabs
 			  })
 			: tokens;
