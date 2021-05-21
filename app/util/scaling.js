5587646bf24537c09d7e828c5c52dd775f9faa34
Esteban Miño <efmino@uc.cl>
2021-05-20 21:04:26 -0400

    Collectibles experience (#2618)
    * Collectibles under wallet (#2602)
    
    * functionalcomponent
    
    * raw
    
    * sections
    
    * styles
    
    * splitIntoSubArrays
    
    * gotocollectible
    
    * styles
    
    * collectiblesVisible
    
    * collectiblesVisible
    
    * tests
    
    * rmconcat
    
    * renderCollectible
    
    * comments
    
    * collectiblesVisible
    
    * noconcat
    
    * tests
    
    * rightprops
    
    * MediaPlayer component (#2612)
    
    * init
    
    * muted
    
    * CollectibleImage
    
    * CollectibleMediaReproductor
    
    * Upgrade swaps-controller v4.0.0
    
    * imagesize
    
    * styles.container
    
    * imagePreview
    
    * CollectibleImage
    
    * collectiblemedia
    
    * mediaplayer
    
    * raw
    
    * onProgress
    
    * progress
    
    * progress
    
    * cleanup
    
    * cleanup2
    
    * cleanup3
    
    * works
    
    * props
    
    * working
    
    * workingyes
    
    * cleanup
    
    * onScreenTouch
    
    * backgroundColor
    
    * styles
    
    * fixandroidcrash
    
    * resetControlsTimeout
    
    * onScreenTouch
    
    * styles
    
    * styles
    
    * addpackage
    
    * tests
    
    * startmuted
    
    * text
    
    * comments
    
    * inline
    
    * NFTs
    
    Co-authored-by: Pedro Pablo Aste Kompen <wachunei@gmail.com>
    
    * Feature/collectible info component (#2622)
    
    * Ui finished
    
    * Strings
    
    * Add price and date
    
    * Fix collectibles list design
    
    * Update tests and controllers zip
    
    * Forgot to remove this file
    
    * Missed a console log
    
    * Favorite collectibles (#2624)
    
    * redux
    
    * tests
    
    * functional
    
    * openlink
    
    * changes
    
    * working
    
    * styles
    
    * favoritessectiuon
    
    * boolean
    
    * tests
    
    * naming
    
    * naming
    
    * tests
    
    * reselect
    
    * Feature/collectible simple modal (#2638)
    
    * Collectible simple modal
    
    * Update tests
    
    Co-authored-by: Esteban Mino <efmino@uc.cl>
    
    * Empty collectibles list (#2636)
    
    * renderempty
    
    * locales
    
    * tests
    
    * tests
    
    * Fix open link on collectibles (#2661)
    
    * Feature/share collectible (#2666)
    
    * Share collectible
    
    * Put strings on locales
    
    * Update tests
    
    * Collectibles modal (#2680)
    
    * noimage
    
    * workswithmargin
    
    * iosworks
    
    * almostthere
    
    * works
    
    * callbacks
    
    * ref
    
    * works
    
    * styles
    
    * works
    
    * SafeAreaView
    
    * zindex
    
    * android
    
    * buttons
    
    * tests
    
    * android
    
    * overflo
    
    * works
    
    * finallyworking
    
    * swipe
    
    * fixdescription
    
    * ANIMATION_OFFSET
    
    * scroll
    
    * yeah
    
    * length
    
    * tests
    
    * progress
    
    * close
    
    * closebutton
    
    * loader
    
    * locales
    
    * onclose
    
    * overlapping
    
    * addnft
    
    * mock
    
    * test
    
    * controllers@9.1.0
    
    * Collectibles experience QA feedback (#2700)
    
    * checkmp4
    
    * fixheight
    
    * contain
    
    * removecollectible
    
    * ignoreSilentSwitch
    
    * tests
    
    * contractname
    
    * onpressout
    
    * onPressCollectible
    
    * arrow
    
    * tests
    
    * margings
    
    * ignore
    
    * whoops
    
    * tests
    
    * iphone11
    
    * scrollable
    
    * TouchableNativeFeedback
    
    * shouldwork
    
    * Revert "shouldwork"
    
    This reverts commit 516582bd9a8ffbef71a468295585eb52cd62316d.
    
    * tada
    
    * MEDIA_WIDTH_MARGIN
    
    * tests
    
    * onPressOut
    
    * blockremovalonfavorites
    
    * rmfavorite
    
    * comtrollers
    
    * comtrollers
    
    * snaps
    
    * actionkey
    
    * actionkey
    
    * removeFavoriteCollectible
    
    * somelogs
    
    * collectible
    
    * Revert "somelogs"
    
    This reverts commit 331987f4ff582a62a8177955d140726eb9aeee6a.
    
    Co-authored-by: Pedro Pablo Aste Kompen <wachunei@gmail.com>
    Co-authored-by: Andre Pimenta <andrepimenta7@gmail.com>

--- app/util/scaling.js
+++ app/util/scaling.js
@@ -8,9 +8,15 @@ const IPHONE_6_HEIGHT = 667;
 const IPHONE_11_PRO_WIDTH = 375;
 const IPHONE_11_PRO_HEIGHT = 812;
 
+//baseModel 2
+const IPHONE_11_PRO_MAX_WIDTH = 414;
+const IPHONE_11_PRO_MAX_HEIGHT = 896;
+
 const getBaseModel = baseModel => {
 	if (baseModel === 1) {
 		return { width: IPHONE_11_PRO_WIDTH, height: IPHONE_11_PRO_HEIGHT };
+	} else if (baseModel === 2) {
+		return { width: IPHONE_11_PRO_MAX_WIDTH, height: IPHONE_11_PRO_MAX_HEIGHT };
 	}
 
 	return { width: IPHONE_6_WIDTH, height: IPHONE_6_HEIGHT };
