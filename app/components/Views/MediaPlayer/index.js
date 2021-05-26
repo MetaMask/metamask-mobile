fd15b3dc73e38796d01b7da2f9b9633f0fe53aec
Esteban Mino <efmino@uc.cl>
2021-05-26 10:54:56 -0400

    747

---
+++ app/components/Views/MediaPlayer/index.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { ViewPropTypes, View, StyleSheet } from 'react-native';
+import AndroidMediaPlayer from './AndroidMediaPlayer';
+import Video from 'react-native-video';
+import Device from '../../../util/Device';
+import Loader from './Loader';
+
+const styles = StyleSheet.create({
+	loaderContainer: {
+		position: 'absolute',
+		zIndex: 999,
+		width: '100%'
+	}
+});
+
+function MediaPlayer({ uri, style, onClose }) {
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+
+	const onLoad = () => setLoading(false);
+	const onError = () => setError(true);
+
+	return (
+		<View style={style}>
+			{loading && (
+				<View style={[styles.loaderContainer, style]}>
+					<Loader error={error} onClose={onClose} />
+				</View>
+			)}
+			{Device.isAndroid() ? (
+				<AndroidMediaPlayer onLoad={onLoad} onError={onError} onClose={onClose} source={{ uri }} />
+			) : (
+				<Video
+					onLoad={onLoad}
+					onError={onError}
+					style={style}
+					muted
+					source={{ uri }}
+					controls
+					ignoreSilentSwitch="ignore"
+				/>
+			)}
+		</View>
+	);
+}
+
+MediaPlayer.propTypes = {
+	/**
+	 * Media URI
+	 */
+	uri: PropTypes.string,
+	/**
+	 * Custom style object
+	 */
+	style: ViewPropTypes.style,
+	/**
+	 * On close callback
+	 */
+	onClose: PropTypes.func
+};
+
+MediaPlayer.defaultProps = {
+	onError: () => null
+};
+
+export default MediaPlayer;
