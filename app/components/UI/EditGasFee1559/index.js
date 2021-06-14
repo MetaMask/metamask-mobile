73e1a13bbc6820d7b41651f3409cc5e4ee88ee30
andrepimenta <andrepimenta7@gmail.com>
2021-06-14 16:51:26 +0100

    [EIP1559] Base for Edit Gas Fee screen

---
+++ app/components/UI/EditGasFee1559/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View } from 'react-native';
+import Text from '../../Base/Text';
+
+const EditGasFee1559 = () => (
+	<View>
+		<View>
+			<Text>HEADER</Text>
+		</View>
+		<View>
+			<Text>SELECTOR</Text>
+		</View>
+		<View>
+			<Text>ADVANCED AND RANGE INPUTS</Text>
+		</View>
+		<View>
+			<Text>SAVE BUTTON</Text>
+		</View>
+	</View>
+);
+
+export default EditGasFee1559;
