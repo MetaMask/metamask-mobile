f03a62f6534dab2706b2698659b07f363beb653f
andrepimenta <andrepimenta7@gmail.com>
2021-06-15 16:08:05 +0100

    Initial design

--- app/components/UI/EditGasFee1559/index.js
+++ app/components/UI/EditGasFee1559/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import Text from '../../Base/Text';
+import StyledButton from '../StyledButton';
 
 const styles = StyleSheet.create({
 	root: {
 		paddingHorizontal: 24
+	},
+	headerContainer: {
+		alignItems: 'center'
+	},
+	headerText: {
+		fontSize: 48
+	},
+	headerTitle: {
+		flexDirection: 'row'
+	},
+	headerTitleSide: {
+		flex: 1
+	},
+	saveButton: {
+		marginBottom: 20
 	}
 });
 
 const EditGasFee1559 = () => (
 	<View style={styles.root}>
-		<View>
-			<Text>HEADER</Text>
+		<View style={styles.headerContainer}>
+			<View style={styles.headerTitle}>
+				<View style={styles.headerTitleSide}>
+					<Text right black style={styles.headerText}>
+						~
+					</Text>
+				</View>
+				<Text black style={styles.headerText}>
+					$6.32
+				</Text>
+				<View style={styles.headerTitleSide} />
+			</View>
+			<Text big black>
+				Up to <Text bold>$6.32</Text>
+			</Text>
+			<Text red>Unknown processing time</Text>
 		</View>
 		<View>
 			<Text>SELECTOR</Text>
@@ -20,7 +50,12 @@ const EditGasFee1559 = () => (
 			<Text>ADVANCED AND RANGE INPUTS</Text>
 		</View>
 		<View>
-			<Text>SAVE BUTTON</Text>
+			<TouchableOpacity style={styles.saveButton}>
+				<Text link centered>
+					How should I choose?
+				</Text>
+			</TouchableOpacity>
+			<StyledButton type={'confirm'}>Save</StyledButton>
 		</View>
 	</View>
 );
