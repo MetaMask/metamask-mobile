608d0641b1a360a34a82bedb587801178f151c4a
gantunesr <gantunes@uc.cl>
2021-07-13 07:29:41 -0400

    fix(ENSUtils.test): Typo

--- app/util/ENSUtils.test.js
+++ app/util/ENSUtils.test.js
@@ -2,11 +2,11 @@ import { isDefaultAccountName } from './ENSUtils';
 
 describe('isDefaultAccountName', () => {
 	const accountNameDefaultOne = 'Account 1';
-	it('should match RegEx if name "Account 1" have default pattern', () => {
+	it('should match RegEx if name "Account 1" has default pattern', () => {
 		expect(isDefaultAccountName(accountNameDefaultOne)).toEqual(true);
 	});
 	const accountNameDefaultTwo = 'Account 99999';
-	it('should match RegEx if name "Account 99999" have default pattern', () => {
+	it('should match RegEx if name "Account 99999" has default pattern', () => {
 		expect(isDefaultAccountName(accountNameDefaultTwo)).toEqual(true);
 	});
 	const accountNameEmpty = '';
@@ -18,7 +18,7 @@ describe('isDefaultAccountName', () => {
 		expect(isDefaultAccountName(accountNameUndefined)).toEqual(false);
 	});
 	const accountNameNotDefault = 'Johns Wallet';
-	it('should not match RegEx if name does not have default pattern', () => {
+	it('should not match RegEx if name does not has default pattern', () => {
 		expect(isDefaultAccountName(accountNameNotDefault)).toEqual(false);
 	});
 });
