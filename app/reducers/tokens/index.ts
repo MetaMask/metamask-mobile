8dd3312d626b9fdfe87a7f3f398f8c04fd233974
Cal Leung <cleun007@gmail.com>
2021-07-13 13:46:27 -0700

    Add comments to token reducers

--- app/reducers/tokens/index.ts
+++ app/reducers/tokens/index.ts
@@ -1,20 +1,21 @@
 import { createSelector } from 'reselect';
 import { tokenListToArray } from '../../util/tokens';
 
-// TODO: Provide types
 const tokenListSelector = (state: any) => state.engine.backgroundState.TokenListController.tokenList;
 
 /**
  * Return token list from TokenListController.
  * Can pass directly into useSelector.
- * @param {Object} state - Redux State
- * @return {Object} - tokenList from TokenListController
  */
 export const getTokenList = createSelector(
 	tokenListSelector,
 	tokenList => tokenList
 );
 
+/**
+ * Return token list array from TokenListController.
+ * Can pass directly into useSelector.
+ */
 export const getTokenListArray = createSelector(
 	tokenListSelector,
 	tokenListToArray
