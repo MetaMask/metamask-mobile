06e10192309f429ef42309dcf2aed797bffcd0bd
sethkfman <seth.kaufman@consensys.net>
2021-07-21 10:29:53 -0600

    removed fastlane, updated version bump to use a variable

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -5,6 +5,7 @@ set -u
 set -o pipefail
 
 versionName="$(awk '/VERSION_NAME: /{print $2}' bitrise.yml)"
+versionNumber="$(awk '/VERSION_NUMBER: /{print $2}' bitrise.yml)"
 
 MAJOR=$(awk -F. '{print $1}' <<< "$versionName")
 MINOR=$(awk -F. '{print $2}' <<< "$versionName")
@@ -25,10 +26,17 @@ if [[ $1 == *"release/"* ]] ; then
         patch=$((PATCH +  1));
         version=$MAJOR'.'$MINOR'.'$patch
     fi
+    
+    echo "$versionNumber"
+    let "versionNumber+=1"
 
     echo "Bumping versionName to"
     echo "$version"
 
+    echo "Bumping versionNumber to"
+    echo "$versionNumber"
+
+    sed -i -e 's/VERSION_NUMBER: [0-9]\{1,\}.[0-9]\{1,\}.[0-9]\{1,\}/VERSION_NUMBER: '"$versionNumber"'/' bitrise.yml
     sed -i -e 's/VERSION_NAME: [0-9]\{1,\}.[0-9]\{1,\}.[0-9]\{1,\}/VERSION_NAME: '"$version"'/' bitrise.yml
     sed -i -e 's/"version": "[0-9]\{1,\}.[0-9]\{1,\}.[0-9]\{1,\}"/"version": "'"$version"'"/' package.json
 
