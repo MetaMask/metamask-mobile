4fe89d3bc50b7d30fbb1007c55d9c7033dac1e6e
gantunesr <gantunes@uc.cl>
2021-07-30 14:50:05 -0400

    fix(TokenIconBug): Add useRemoteResourceExists custom hook

---
+++ app/components/Base/hooks/useRemoteResourceExists.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+/**
+ * @typedef {Boolean} resourceExists boolean value that represent wether the remote resource exists or not
+ * @typedef {Boolean} isLoading boolean value that indicates when the promise is completed
+ */
+
+/**
+ * Hook to handle the remote state of a resource
+ * @param {String} uri Resource URI
+ * @return {Boolean} resourceExists
+ */
+const useRemoteResourceExists = (uri: string): boolean[] => {
+	const [resourceExists, setReourceExists] = useState<boolean>(false);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+
+	const fetchStatus = async (uri: string): Promise<void> => {
+		fetch(uri, { method: 'HEAD' }).then(res => {
+			setReourceExists(res.status === 200);
+			setIsLoading(false);
+		});
+	};
+
+	useEffect(() => {
+		fetchStatus(uri);
+	}, [uri]);
+
+	return [resourceExists, isLoading];
+};
+
+export default useRemoteResourceExists;
