7c9e4fd7128c2beae33fccedf349d8b6a3cb162e
sethkfman <10342624+sethkfman@users.noreply.github.com>
2021-08-12 10:39:05 -0600

    [FEATURE] - Storage Limit - Reduce Txs Being Stored (#2972)
    * added controller test tarball and updated path
    
    * added DefaultPreference to save OPT_IN METRICS data
    
    * added react native default prefs to pod
    
    * added view on chain footer to list view, dis-allow list for onboarding and analytics in store
    
    * updated yarn.lock file
    
    * updated copy on footer
    
    * updated tarball for latest controller PR
    
    * yarn.lock update
    
    * disable cache
    
    * enable cache
    
    * added migration for new values being stored in default preferences
    
    * trigger test build
    
    * [FIX] Account for `txParams.data` when we sync (#2999)
    
    * Account for txParams.data when we sync
    
    * Remove "Bummer!" wording
    
    Co-authored-by: sethkfman <10342624+sethkfman@users.noreply.github.com>
    
    * Fix/ci release update (#3004)
    
    * removed release build trigger
    
    * updated GH action tigger on pull requests not pushes
    
    * updated to v14.2.0 controllers
    
    Co-authored-by: ricky <ricky.miller@gmail.com>

--- app/components/Views/Login/index.js
+++ app/components/Views/Login/index.js
@@ -45,6 +45,7 @@ import WarningExistingUserModal from '../../UI/WarningExistingUserModal';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { trackErrorAsAnalytics } from '../../../util/analyticsV2';
 import { tlc, toLowerCaseEquals } from '../../../util/general';
+import DefaultPreference from 'react-native-default-preference';
 
 const isTextDelete = text => tlc(text) === 'delete';
 const deviceHeight = Device.getDeviceHeight();
@@ -298,9 +299,9 @@ class Login extends PureComponent {
 			}
 
 			// Get onboarding wizard state
-			const onboardingWizard = await AsyncStorage.getItem(ONBOARDING_WIZARD);
+			const onboardingWizard = await DefaultPreference.get(ONBOARDING_WIZARD);
 			// Check if user passed through metrics opt-in screen
-			const metricsOptIn = await AsyncStorage.getItem(METRICS_OPT_IN);
+			const metricsOptIn = await DefaultPreference.get(METRICS_OPT_IN);
 			if (!metricsOptIn) {
 				this.props.navigation.navigate('OnboardingRootNav', {
 					screen: 'OnboardingNav',
