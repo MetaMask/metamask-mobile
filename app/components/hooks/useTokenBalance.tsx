609837cf984c5b732629548ec87348e94a297654
gantunesr <gantunes@uc.cl>
2021-08-18 18:51:09 -0400

    fix(TokenBalance): Add hook to fetch token balances

---
+++ app/components/hooks/useTokenBalance.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect, Dispatch, SetStateAction } from 'react';
+import Engine from '../../core/Engine';
+
+const useTokenBalance = (tokenAddress: string, userAddress: string): [number, boolean, boolean] => {
+	// This hook should be only used with ERC20 tokens
+	const [balance, setBalance]: [number, Dispatch<SetStateAction<number>>] = useState<number>(0);
+	const [loading, setLoading]: [boolean, Dispatch<SetStateAction<boolean>>] = useState<boolean>(true);
+	const [error, setError]: [boolean, Dispatch<SetStateAction<boolean>>] = useState<boolean>(false);
+	const { TokenBalancesController }: any = Engine.context;
+
+	const fetchBalance = async (tokenAddress: string, userAddress: string): Promise<void> => {
+		TokenBalancesController.getBalanceOf(tokenAddress, userAddress)
+			.then((balance: string) => setBalance(Number(balance)))
+			.catch(() => setError(true))
+			.finally(() => setLoading(false));
+	};
+
+	useEffect(() => {
+		fetchBalance(tokenAddress, userAddress);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [tokenAddress, userAddress]);
+
+	return [balance, loading, error];
+};
+
+export default useTokenBalance;
