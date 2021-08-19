eac3b5ef16270b5044cac11de6e21a4c870e7310
Matthew Epps <mepps32@gmail.com>
2021-08-19 12:45:01 -0700

    chore: update manually_pasting copy

--- app/store/index.js
+++ app/store/index.js
@@ -55,5 +55,8 @@ const persistConfig = {
 
 const pReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(pReducer);
+export const store = createStore(
+	pReducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 export const persistor = persistStore(store);
