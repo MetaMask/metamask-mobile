d08ef59568a18cccce2f3e20f373733a4b7a6ecf
sethkfman <10342624+sethkfman@users.noreply.github.com>
2021-08-25 12:15:29 -0600

    updated slack script

--- scripts/metamask-bot-build-announce-bitrise.js
+++ scripts/metamask-bot-build-announce-bitrise.js
@@ -1,12 +1,8 @@
 #!/usr/bin/env node
 /* eslint-disable import/no-commonjs */
 const request = require('request-promise');
-const github = require('octonode');
 
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const ANDROID_APK_LINK = process.env.BITRISE_PUBLIC_INSTALL_PAGE_URL;
-const GIT_PROJECT_REPONAME = 'metamask-mobile';
-const BITRISEIO_GIT_REPOSITORY_OWNER = process.env.BITRISEIO_GIT_REPOSITORY_OWNER;
 const SLACK_TOKEN = process.env.MM_SLACK_TOKEN;
 const SLACK_SECRET = process.env.MM_SLACK_SECRET;
 const SLACK_ROOM = process.env.MM_SLACK_ROOM;
@@ -14,28 +10,9 @@ const BITRISE_GIT_COMMIT = process.env.BITRISE_GIT_COMMIT;
 
 start().catch(console.error);
 
-async function getPRInfo() {
-	const client = github.client(GITHUB_TOKEN);
-	try {
-		const REPO = client.repo(`${BITRISEIO_GIT_REPOSITORY_OWNER}/${GIT_PROJECT_REPONAME}`);
-		const response = await REPO.prsAsync({ state: 'closed' });
-		const PR = response[0].find(obj => obj.merge_commit_sha === BITRISE_GIT_COMMIT);
-
-		if (PR) {
-			return { title: PR.title, number: PR.number, url: PR.html_url };
-		}
-	} catch (e) {
-		return {};
-	}
-}
-
 async function start() {
-	const PR_INFO = await getPRInfo();
 	const content = {
-		text:
-			PR_INFO !== {}
-				? `NEW BUILDS AVAILABLE! Including <${PR_INFO.url}|#${PR_INFO.number} - ${PR_INFO.title}>`
-				: `NEW BUILDS AVAILABLE!`,
+		text: `THIS IS A TEST _ NEW BUILDS AVAILABLE! Commit - <${BITRISE_GIT_COMMIT}>`,
 		attachments: [
 			{
 				title_link: 'itms-beta://beta.itunes.apple.com/v1/app/1438144202',
