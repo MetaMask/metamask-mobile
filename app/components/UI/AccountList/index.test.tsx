c6505b3ebb37eec87a710a54a391163c44045ecf
Cal Leung <cleun007@gmail.com>
2021-08-26 19:38:56 -0700

    Begin converting tests to TS

--- app/components/UI/AccountList/index.test.tsx
+++ app/components/UI/AccountList/index.test.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import AccountList from './';
 import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
-import AccountRightButton from './';
 
 const mockStore = configureMockStore();
-const store = mockStore({});
+const address = '0xe7E125654064EEa56229f273dA586F10DF96B0a1';
+const store = mockStore({
+	engine: {
+		backgroundState: {
+			AccountTrackerController: {
+				accounts: {
+					[address]: { name: 'account 1', address, balance: 0 }
+				}
+			}
+		}
+	}
+});
 
-describe('AccountRightButton', () => {
+describe('Accounts', () => {
 	it('should render correctly', () => {
-		const address = '0xe7E125654064EEa56229f273dA586F10DF96B0a1';
-
 		const wrapper = shallow(
 			<Provider store={store}>
-				<AccountRightButton address={address} />
+				<AccountList selectedAddress={address} />
 			</Provider>
 		);
 		expect(wrapper).toMatchSnapshot();
--- app/components/UI/AccountRightButton/index.test.tsx
+++ app/components/UI/AccountRightButton/index.test.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import AccountList from './';
 import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
+import AccountRightButton from './';
 
 const mockStore = configureMockStore();
-const store = mockStore({});
+const store = mockStore({
+	engine: {
+		backgroundState: {
+			PreferencesController: {
+				selectedAddress: '0xe7E125654064EEa56229f273dA586F10DF96B0a1'
+			}
+		}
+	}
+});
 
-describe('Accounts', () => {
+describe('AccountRightButton', () => {
 	it('should render correctly', () => {
-		const address = '0xe7E125654064EEa56229f273dA586F10DF96B0a1';
-		const accounts = {};
-		accounts[address] = { name: 'account 1', address, balance: 0 };
 		const wrapper = shallow(
 			<Provider store={store}>
-				<AccountList accounts={accounts} selectedAddress={address} />
+				<AccountRightButton />
 			</Provider>
 		);
 		expect(wrapper).toMatchSnapshot();
