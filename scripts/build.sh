654245ce560a30ff04b4d074e7f3f4bcbbc42518
sethkfman <10342624+sethkfman@users.noreply.github.com>
2021-08-27 13:51:59 -0600

    updated ios to not release sentry sourcemaps on e2e tests

--- scripts/build.sh
+++ scripts/build.sh
@@ -296,6 +296,13 @@ startWatcher() {
 checkAuthToken() {
 	local propertiesFileName="$1"
 
+	if [ -n "${MM_SENTRY_AUTH_TOKEN}" ]; then
+		sed -i'' -e "s/auth.token.*/auth.token=${MM_SENTRY_AUTH_TOKEN}/" "./${propertiesFileName}";
+	elif ! grep -qE '^auth.token=[[:alnum:]]+$' "./${propertiesFileName}"; then
+		printError "Missing auth token in '${propertiesFileName}'; add the token, or set it as MM_SENTRY_AUTH_TOKEN"
+		exit 1
+	fi
+
 	if [ ! -e "./${propertiesFileName}" ]; then
 		if [ -n "${MM_SENTRY_AUTH_TOKEN}" ]; then
 			cp "./${propertiesFileName}.example" "./${propertiesFileName}"
@@ -303,13 +310,6 @@ checkAuthToken() {
 			printError "Missing '${propertiesFileName}' file (see '${propertiesFileName}.example' or set MM_SENTRY_AUTH_TOKEN to generate)"
 			exit 1
 		fi
-	fi
-
-	if [ -n "${MM_SENTRY_AUTH_TOKEN}" ]; then
-		sed -i'' -e "s/auth.token.*/auth.token=${MM_SENTRY_AUTH_TOKEN}/" "./${propertiesFileName}";
-	elif ! grep -qE '^auth.token=[[:alnum:]]+$' "./${propertiesFileName}"; then
-		printError "Missing auth token in '${propertiesFileName}'; add the token, or set it as MM_SENTRY_AUTH_TOKEN"
-		exit 1
 	fi
 }
 
