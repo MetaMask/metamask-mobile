924590ca5c2e44489b575bfcd3b74c91452f82bc
Ricky Miller <ricky.miller@gmail.com>
2021-08-30 11:37:16 -0400

    clean up types

--- app/core/Engine.tsx
+++ app/core/Engine.tsx
@@ -513,15 +513,33 @@ class Engine {
 			PreferencesController.setSelectedAddress(accounts.hd[0]);
 		}
 
-		const mapTx = (tx: { txParams: Transaction; id: any; metamaskNetworkId: string; origin: string; status: string; time: any; hash: string; rawTx: string; }) => ({
-			id: tx.id,
-			networkID: tx.metamaskNetworkId,
-			origin: tx.origin,
-			status: tx.status,
-			time: tx.time,
-			transactionHash: tx.hash,
-			rawTx: tx.rawTx,
-			transaction: { ...tx.txParams }
+		const mapTx = ({
+			id,
+			metamaskNetworkId,
+			origin,
+			status,
+			time,
+			hash,
+			rawTx,
+			txParams
+		}: {
+			id: any;
+			metamaskNetworkId: string;
+			origin: string;
+			status: string;
+			time: any;
+			hash: string;
+			rawTx: string;
+			txParams: Transaction;
+		}) => ({
+			id,
+			networkID: metamaskNetworkId,
+			origin,
+			status,
+			time,
+			transactionHash: hash,
+			rawTx,
+			transaction: { ...txParams }
 		});
 
 		await TransactionController.update({
