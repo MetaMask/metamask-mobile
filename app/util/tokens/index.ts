8343603f97c90b255c893b272ecc22049865a280
Cal Leung <cleun007@gmail.com>
2021-09-01 13:05:08 -0700

    No need to set address on tokenlist array

--- app/util/tokens/index.ts
+++ app/util/tokens/index.ts
@@ -4,10 +4,5 @@ import { TokenListMap } from '@metamask/controllers';
  * Convert token list object to token list array
  */
 export const tokenListToArray = (tokenList: TokenListMap) => {
-	return Object.entries(tokenList).map(([address, tokenData]) => {
-		// TODO: Have the TokenListController return the same Token type regardless of static or dynamic
-		// We need to set the token address here since static token list will not supply it by default
-		tokenData.address = address;
-		return tokenData;
-	});
+	return Object.values(tokenList).map(tokenData => tokenData);
 };
