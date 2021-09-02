5d8d850ae74b81b913a489a2376db7df84f7da22
Alex Donesky <adonesky@gmail.com>
2021-09-02 11:02:44 -0500

    Integrate changes from controllers v15.0.2 (#3071)
    * ignored tokens updates
    
    * add migration
    
    * cleanup
    
    * bump @metamask/controllers to 15.0.2

--- app/store/migrations.js
+++ app/store/migrations.js
@@ -155,7 +155,50 @@ export const migrations = {
 		DefaultPreference.set(ONBOARDING_WIZARD, EXPLORED);
 
 		return state;
+	},
+	7: state => {
+		const allTokens = state.engine.backgroundState.TokensController.allTokens;
+		const newAllTokens = {};
+		if (allTokens) {
+			Object.keys(allTokens).forEach(accountAddress => {
+				Object.keys(allTokens[accountAddress]).forEach(chainId => {
+					const tokensArray = allTokens[accountAddress][chainId];
+					if (newAllTokens[chainId] === undefined) {
+						newAllTokens[chainId] = { [accountAddress]: tokensArray };
+					} else {
+						newAllTokens[chainId] = {
+							...newAllTokens[chainId],
+							[accountAddress]: tokensArray
+						};
+					}
+				});
+			});
+		}
+
+		const ignoredTokens = state.engine.backgroundState.TokensController.ignoredTokens;
+		const newAllIgnoredTokens = {};
+		Object.keys(allTokens).forEach(accountAddress => {
+			Object.keys(allTokens[accountAddress]).forEach(chainId => {
+				if (newAllIgnoredTokens[chainId] === undefined) {
+					newAllIgnoredTokens[chainId] = {
+						[accountAddress]: ignoredTokens
+					};
+				} else {
+					newAllIgnoredTokens[chainId] = {
+						...newAllIgnoredTokens[chainId],
+						[accountAddress]: ignoredTokens
+					};
+				}
+			});
+		});
+
+		state.engine.backgroundState.TokensController = {
+			allTokens: newAllTokens,
+			allIgnoredTokens: newAllIgnoredTokens
+		};
+
+		return state;
 	}
 };
 
-export const version = 6;
+export const version = 7;
