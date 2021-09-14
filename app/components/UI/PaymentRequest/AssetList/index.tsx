5998b4b5d226ff82106623087f2c8f1d079a4e02
Cal Leung <cleun007@gmail.com>
2021-09-14 14:34:57 -0700

    Showing icons for payment request flow

--- app/components/UI/PaymentRequest/AssetList/index.tsx
+++ app/components/UI/PaymentRequest/AssetList/index.tsx
@@ -1,11 +1,12 @@
-import React, { PureComponent } from 'react';
+import React, { useCallback } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import PropTypes from 'prop-types';
 import StyledButton from '../../StyledButton';
 import AssetIcon from '../../AssetIcon';
 import { colors, fontStyles } from '../../../../styles/common';
 import Identicon from '../../Identicon';
 import NetworkMainAssetLogo from '../../NetworkMainAssetLogo';
+import { useSelector } from 'react-redux';
+import { getTokenList } from '../../../../reducers/tokens';
 
 const styles = StyleSheet.create({
 	item: {
@@ -21,13 +22,13 @@ const styles = StyleSheet.create({
 		alignItems: 'flex-start',
 	},
 	text: {
-		...fontStyles.normal,
+		...(fontStyles.normal as any),
 	},
 	textSymbol: {
 		...fontStyles.normal,
 		paddingBottom: 4,
 		fontSize: 16,
-	},
+	} as any,
 	assetInfo: {
 		flex: 1,
 		flexDirection: 'column',
@@ -45,66 +46,70 @@ const styles = StyleSheet.create({
 	},
 });
 
-/**
- * PureComponent that provides ability to search assets.
- */
-export default class AssetList extends PureComponent {
-	static propTypes = {
-		/**
-		 * Array of assets objects returned from the search
-		 */
-		searchResults: PropTypes.array,
-		/**
-		 * Callback triggered when a token is selected
-		 */
-		handleSelectAsset: PropTypes.func,
-		/**
-		 * Message string to display when searchResults is empty
-		 */
-		emptyMessage: PropTypes.string,
-	};
+interface Props {
+	/**
+	 * Array of assets objects returned from the search
+	 */
+	searchResults: any;
+	/**
+	 * Callback triggered when a token is selected
+	 */
+	handleSelectAsset: any;
+	/**
+	 * Message string to display when searchResults is empty
+	 */
+	emptyMessage: string;
+}
+
+const AssetList = ({ searchResults, handleSelectAsset, emptyMessage }: Props) => {
+	const tokenList = useSelector(getTokenList);
 
 	/**
 	 * Render logo according to asset. Could be ETH, Identicon or contractMap logo
 	 *
 	 * @param {object} asset - Asset to generate the logo to render
 	 */
-	renderLogo = (asset) => {
-		const { iconUrl, address, isETH } = asset;
-		if (!iconUrl && !isETH) {
-			return <Identicon address={address} />;
-		} else if (isETH) {
-			return <NetworkMainAssetLogo big style={styles.ethLogo} />;
-		}
-		return <AssetIcon logo={iconUrl} />;
-	};
+	const renderLogo = useCallback(
+		(asset: any) => {
+			const { address, isETH } = asset;
+			if (isETH) {
+				return <NetworkMainAssetLogo big style={styles.ethLogo} />;
+			}
+			const token = tokenList?.[(address || '')?.toLowerCase?.()];
+			const iconUrl = token?.iconUrl;
+			if (!iconUrl) {
+				return <Identicon address={address} />;
+			}
+			return <AssetIcon logo={iconUrl} />;
+		},
+		[tokenList]
+	);
 
-	render = () => {
-		const { searchResults, handleSelectAsset } = this.props;
-
-		return (
-			<View style={styles.rowWrapper} testID={'add-searched-token-screen'}>
-				{searchResults.slice(0, 6).map((_, i) => {
-					const { symbol, name } = searchResults[i] || {};
-					return (
-						<StyledButton
-							type={'normal'}
-							containerStyle={styles.item}
-							onPress={() => handleSelectAsset(searchResults[i])} // eslint-disable-line
-							key={i}
-						>
-							<View style={styles.assetListElement}>
-								<View style={styles.assetIcon}>{this.renderLogo(searchResults[i])}</View>
-								<View style={styles.assetInfo}>
-									<Text style={styles.textSymbol}>{symbol}</Text>
-									{!!name && <Text style={styles.text}>{name}</Text>}
-								</View>
+	return (
+		<View testID={'add-searched-token-screen'}>
+			{searchResults.slice(0, 6).map((_: any, i: number) => {
+				console.log(searchResults);
+				const { symbol, name } = searchResults[i] || {};
+				return (
+					<StyledButton
+						type={'normal'}
+						containerStyle={styles.item}
+						onPress={() => handleSelectAsset(searchResults[i])} // eslint-disable-line
+						key={i}
+					>
+						<View style={styles.assetListElement}>
+							<View style={styles.assetIcon}>{renderLogo(searchResults[i])}</View>
+							<View style={styles.assetInfo}>
+								<Text style={styles.textSymbol}>{symbol}</Text>
+								{!!name && <Text style={styles.text}>{name}</Text>}
 							</View>
-						</StyledButton>
-					);
-				})}
-				{searchResults.length === 0 && <Text style={styles.text}>{this.props.emptyMessage}</Text>}
-			</View>
-		);
-	};
-}
+						</View>
+					</StyledButton>
+				);
+			})}
+			{searchResults.length === 0 && <Text style={styles.text}>{emptyMessage}</Text>}
+		</View>
+	);
+};
+
+export default AssetList;
