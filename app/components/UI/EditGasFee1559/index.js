e6856af567ac03adebf67c8935b77d00b7bba53d
Cal Leung <cleun007@gmail.com>
2021-09-21 15:25:26 -0700

    Feature/3140 token detection eth mainnet (#3148)
    * Update AssetDetectionController to use TokenDetectionController
    
    * Update redux with token list controller data
    
    * Replace contract map with token list from TokenListController. Add typescript support
    
    * - Update to use iconUrl from token list
    - Create token list selectors
    - Minor refactor into typescript
    - Update copies
    
    * Hook up preferences controller to handle static token list
    
    * Use controllers v12.1.0. Set up last bit for token list controller constructor
    
    * Remove controllers tgz
    
    * Add comments to token reducers
    
    * Remove all contract metadata imports
    
    * Upgrade nodeify library to fix pbkdf2 library crasher
    
    * Remove comment and lock library version
    
    * Remove unused field on engine reducer
    
    * Add missing blue100
    
    * Change Alert to use enums. Add jest types. Remove comments. Write test for token util.
    
    * Handle undefined passed to token util
    
    * Update enzyme to support jest diving redux connected components
    
    * Fix all unit tests that uses Redux. Update snapshots.
    
    * Update app to support latest controllers.
    
    * Provide TokensController with provider on initialization.
    
    * Clean up code to display asset logos
    
    * Create static asset generation script. Generate on build + watch.
    
    * Update static logos
    
    * Properly format static logos file
    
    * Update controller package
    
    * Mock static-logos.js for testing
    
    * ignored tokens updates
    
    * add migration
    
    * cleanup
    
    * Upgrade controller version
    
    * Use latest controller version that includes abort controller polyfill
    
    * Use TokenListController types
    
    * Remove unused EthInput
    
    * Remove commented code in QuotesView.js
    
    * No need to set address on tokenlist array
    
    * Update controller version
    
    * Remove TransactionDirection unused file. ESLint ignore static assets file
    
    * Refactor wallet to tsx
    
    * Add missing deps array
    
    * Add missing tab label
    
    * Don't lint generated static logos file. Fix crasher for ipfs logos.
    
    * Fix unit test
    
    * Update title and cta label on tokens and nft pages
    
    * Fix unit test
    
    * Fix unit test
    
    * Rename asset list extension
    
    * Showing icons for payment request flow
    
    * Fix showing icon in payments. Fix tests
    
    * Rename experimental settings from js to tsx
    
    * Move token detection toggle to experimental
    
    * Turn off token detection by default
    
    * Hide token detection on networks other than Ethereum mainnet
    
    * Rename search autocomplete file from js to tsx
    
    * Add token detection banner to search autocomplete. Conditionally render X close button on experimental screen
    
    * Hide token detection banner onFocus, non mainnets, and when it is on
    
    * Hide token detection banner onFocus, non mainnets, and when it is on
    
    * Update token removal confirmation copy
    
    * Add description for onBlur and onFocus events
    
    * Remove token toggle from security
    
    * Update test
    
    * Fix image crasher. Show same image on asset overview
    
    Co-authored-by: Alex <adonesky@gmail.com>
    Co-authored-by: sethkfman <10342624+sethkfman@users.noreply.github.com>

--- app/components/UI/EditGasFee1559/index.js
+++ app/components/UI/EditGasFee1559/index.js
@@ -9,7 +9,7 @@ import { colors } from '../../../styles/common';
 import InfoModal from '../Swaps/components/InfoModal';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { strings } from '../../../../locales/i18n';
-import Alert from '../../Base/Alert';
+import Alert, { AlertType } from '../../Base/Alert';
 import HorizontalSelector from '../../Base/HorizontalSelector';
 import Device from '../../../util/device';
 import { isMainnetByChainId } from '../../../util/networks';
@@ -466,7 +466,7 @@ const EditGasFee1559 = ({
 			return (
 				<Alert
 					small
-					type="warning"
+					type={AlertType.Warning}
 					renderIcon={() => <MaterialCommunityIcon name="information" size={20} color={colors.yellow} />}
 					style={styles.warningContainer}
 				>
@@ -489,7 +489,7 @@ const EditGasFee1559 = ({
 			return (
 				<Alert
 					small
-					type="error"
+					type={AlertType.Error}
 					renderIcon={() => <MaterialCommunityIcon name="information" size={20} color={colors.red} />}
 					style={styles.warningContainer}
 				>
