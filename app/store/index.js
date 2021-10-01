ff698689e0cb3fb95745ba2849b313055427525a
ricky <ricky.miller@gmail.com>
2021-10-01 11:27:05 -0400

    Revert fix issues 2100 (#3081) (#3248)

--- app/store/index.js
+++ app/store/index.js
@@ -50,7 +50,6 @@ const persistConfig = {
 	storage: MigratedStorage,
 	stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
 	migrate: createMigrate(migrations, { debug: false }),
-	timeout: 30000,
 	writeFailHandler: (error) => Logger.error(error, { message: 'Error persisting data' }), // Log error if saving state fails
 };
 
