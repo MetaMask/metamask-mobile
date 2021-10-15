eb8774ff7b0dbf2df5c21b30430bc6cd704620c5
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2021-10-15 11:04:24 -0300

    Add more RemoteImage tests cases
    I removed the Provider and just mocked the selector function the component uses

--- app/components/Base/RemoteImage/index.test.tsx
+++ app/components/Base/RemoteImage/index.test.tsx
@@ -1,29 +1,42 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import RemoteImage from './';
-import configureMockStore from 'redux-mock-store';
-import { Provider } from 'react-redux';
 
-const mockStore = configureMockStore();
-const initialState = {
-	engine: {
-		backgroundState: {
-			PreferencesController: { ipfsGateway: 'https://cloudflare-ipfs.com/ipfs/' },
-		},
-	},
-};
-const store = mockStore(initialState);
+jest.mock('react-redux', () => ({
+	...jest.requireActual('react-redux'),
+	useSelector: jest.fn().mockImplementation(() => 'https://cloudflare-ipfs.com/ipfs/'),
+}));
 
 describe('RemoteImage', () => {
-	it('should render correctly', () => {
+	it('should render svg correctly', () => {
 		const wrapper = shallow(
-			<Provider store={store}>
-				<RemoteImage
-					source={{
-						uri: `https://raw.githubusercontent.com/MetaMask/contract-metadata/master/images/dai.svg`,
-					}}
-				/>
-			</Provider>
+			<RemoteImage
+				source={{
+					uri: 'https://raw.githubusercontent.com/MetaMask/contract-metadata/master/images/dai.svg',
+				}}
+			/>
+		);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('should render static sources', () => {
+		const wrapper = shallow(
+			<RemoteImage
+				source={{
+					uri: 'https://s3.amazonaws.com/airswap-token-images/OXT.png',
+				}}
+			/>
+		);
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('should render ipfs sources', () => {
+		const wrapper = shallow(
+			<RemoteImage
+				source={{
+					uri: 'ipfs://QmeE94srcYV9WwJb1p42eM4zncdLUai2N9zmMxxukoEQ23',
+				}}
+			/>
 		);
 		expect(wrapper).toMatchSnapshot();
 	});
