8fa4fe445a888a6925cd65d5548d9c89222f9879
André Fatia <andre@thecodeventure.com>
2021-11-08 17:30:47 +0000

    Added alert back, fixed typo

--- app/components/Views/Settings/SecuritySettings/index.js
+++ app/components/Views/Settings/SecuritySettings/index.js
@@ -465,6 +465,10 @@ class Settings extends PureComponent {
 			await this.trackOptInEvent('Metrics Opt Out');
 			Analytics.disable();
 			this.setState({ analyticsEnabled: false });
+			Alert.alert(
+				strings('app_settings.metametrics_opt_out'),
+				strings('app_settings.metametrics_restart_required')
+			);
 		}
 	};
 
