2cc63222983afd3b284bbe802d1a6f04018743e3
Curtis <Curtis.David7@gmail.com>
2021-11-15 17:46:28 -0500

    updated test name

--- e2e/specs/start-exploring.spec.js
+++ e2e/specs/start-exploring.spec.js
@@ -54,7 +54,7 @@ describe('Start Exploring', () => {
 		await CreatePasswordView.isVisible();
 	});
 
-	it('should be able to opt-out of the onboarding-wizard', async () => {
+	it('should be able to create a new wallet', async () => {
 		await CreatePasswordView.enterPassword(PASSWORD);
 		await CreatePasswordView.reEnterPassword(PASSWORD);
 		await CreatePasswordView.tapIUnderstandCheckBox();
