eab43e798326ef3feaeda2ef9f97761792d379ac
Ricky Miller <ricky.miller@gmail.com>
2021-11-30 15:46:50 -0500

    add test-ids constants file

---
+++ app/constants/test-ids.js
@@ -0,0 +1,7 @@
+export const DELETE_WALLET_CONTAINER_ID = 'delete-wallet-modal-container';
+export const DELETE_WALLET_INPUT_BOX_ID = 'delete-wallet-inputbox';
+
+export const LOGIN_CONTAINER_ID = 'login';
+export const PASSWORD_INPUT_BOX_ID = 'login-password-input';
+export const LOGIN_PASSWORD_ERROR = 'invalid-password-error';
+export const RESET_WALLET_ID = 'reset-wallet-button';
--- e2e/pages/LoginView.js
+++ e2e/pages/LoginView.js
@@ -1,9 +1,10 @@
 import TestHelpers from '../helpers';
-
-const LOGIN_CONTAINER_ID = 'login';
-const PASSWORD_INPUT_BOX_ID = 'login-password-input';
-const LOGIN_PASSWORD_ERROR = 'invalid-password-error';
-const RESET_WALLET_ID = 'reset-wallet-button';
+import {
+	PASSWORD_INPUT_BOX_ID,
+	RESET_WALLET_ID,
+	LOGIN_CONTAINER_ID,
+	LOGIN_PASSWORD_ERROR,
+} from 'app/constants/test-ids';
 export default class LoginView {
 	static async enterPassword(password) {
 		await TestHelpers.typeTextAndHideKeyboard(PASSWORD_INPUT_BOX_ID, password);
