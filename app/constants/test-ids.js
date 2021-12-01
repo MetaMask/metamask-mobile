8adb00e235156feee472576b9a0f8654c05da269
Ricky Miller <ricky.miller@gmail.com>
2021-12-01 11:22:28 -0500

    add test-ids for AddAddressModal.js

--- app/constants/test-ids.js
+++ app/constants/test-ids.js
@@ -9,3 +9,6 @@ export const RESET_WALLET_ID = 'reset-wallet-button';
 export const ACCOUNT_APROVAL_MODAL_CONTAINER_ID = 'account-approval-modal-container';
 export const CANCEL_BUTTON_ID = 'connect-cancel-button';
 export const CONNECT_BUTTON_ID = 'connect-approve-button';
+
+export const ADD_ADDRESS_MODAL_CONTAINER_ID = 'add-address-modal';
+export const ENTER_ALIAS_INPUT_BOX_ID = 'address-alias-input';
