8b0e915067d76f977f21a1e25c01845e83a530da
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2021-12-03 15:52:07 -0300

    Fix collectibles autodetection (#3458)
    Co-authored-by: Alex <adonesky@gmail.com>
    Co-authored-by: gantunesr <gantunes@uc.cl>

--- app/components/UI/CollectibleContractElement/index.js
+++ app/components/UI/CollectibleContractElement/index.js
@@ -12,6 +12,7 @@ import ActionSheet from 'react-native-actionsheet';
 import { strings } from '../../../../locales/i18n';
 import Engine from '../../../core/Engine';
 import { removeFavoriteCollectible } from '../../../actions/collectibles';
+import { collectibleContractsSelector } from '../../../reducers/collectibles';
 
 const DEVICE_WIDTH = Device.getDeviceWidth();
 const COLLECTIBLE_WIDTH = (DEVICE_WIDTH - 30 - 16) / 3;
@@ -244,7 +245,7 @@ CollectibleContractElement.propTypes = {
 };
 
 const mapStateToProps = (state) => ({
-	collectibleContracts: state.engine.backgroundState.CollectiblesController.collectibleContracts,
+	collectibleContracts: collectibleContractsSelector(state),
 	chainId: state.engine.backgroundState.NetworkController.provider.chainId,
 	selectedAddress: state.engine.backgroundState.PreferencesController.selectedAddress,
 });
--- app/components/UI/DrawerView/index.js
+++ app/components/UI/DrawerView/index.js
@@ -42,6 +42,7 @@ import { findRouteNameFromNavigatorState } from '../../../util/general';
 import AnalyticsV2, { ANALYTICS_EVENTS_V2 } from '../../../util/analyticsV2';
 import { isDefaultAccountName, doENSReverseLookup } from '../../../util/ENSUtils';
 import ClipboardManager from '../../../core/ClipboardManager';
+import { collectiblesSelector } from '../../../reducers/collectibles';
 
 const styles = StyleSheet.create({
 	wrapper: {
@@ -1140,7 +1141,7 @@ const mapStateToProps = (state) => ({
 	ticker: state.engine.backgroundState.NetworkController.provider.ticker,
 	tokens: state.engine.backgroundState.TokensController.tokens,
 	tokenBalances: state.engine.backgroundState.TokenBalancesController.contractBalances,
-	collectibles: state.engine.backgroundState.CollectiblesController.collectibles,
+	collectibles: collectiblesSelector(state),
 	seedphraseBackedUp: state.user.seedphraseBackedUp,
 });
 
