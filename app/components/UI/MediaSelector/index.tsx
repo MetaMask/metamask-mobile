b775ebd7788176140790858af45b8faaaf6e0058
Andrea Salvatore <andrea.salvatore@consensys.net>
2021-12-14 13:12:54 +0100

    Adds media selection and upload button

--- app/components/UI/MediaSelector/index.tsx
+++ app/components/UI/MediaSelector/index.tsx
@@ -1,7 +1,9 @@
 import React, { useRef, useState } from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { ImagePickerResponse, launchCamera, launchImageLibrary } from 'react-native-image-picker';
 import ActionSheet from 'react-native-actionsheet';
+import LinearGradient from 'react-native-linear-gradient';
+import FontAwesomeIcon5 from 'react-native-vector-icons/FontAwesome5';
 import PropTypes from 'prop-types';
 import Text from '../../Base/Text';
 import { strings } from '../../../../locales/i18n';
@@ -10,29 +12,41 @@ import CollectibleMedia from '../CollectibleMedia';
 
 const styles = StyleSheet.create({
 	mediaButton: {
-		borderWidth: 1,
+		borderWidth: 2,
 		borderColor: colors.blue,
-		padding: 30,
-		borderRadius: 5,
+		borderStyle: 'dashed',
+		borderRadius: 6,
 		marginBottom: 20,
+		height: 180,
 	},
-	buttonText: {
-		color: colors.blue,
-		fontSize: 16,
-		textAlign: 'center',
+	gradient: {
+		paddingHorizontal: 60,
+		paddingVertical: 30,
+		justifyContent: 'center',
+		alignItems: 'center',
+		flex: 1,
+	},
+	indicator: {
+		marginBottom: 5,
 	},
 });
 
 const MediaSelector = ({ setMediaToSend }) => {
 	const [media, setMedia] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
 	const actionSheetRef = useRef();
 
 	const handleChooseMedia = (index: number) => {
+		setIsLoading(true);
+
 		switch (index) {
 			case 1:
 				launchCamera({ mediaType: 'photo' }, (response: ImagePickerResponse) => {
+					// eslint-disable-next-line no-console
 					console.log(response);
+
 					if (response?.assets) {
+						setIsLoading(false);
 						setMedia(response.assets[0]);
 						setMediaToSend(response.assets[0]);
 					}
@@ -40,8 +54,11 @@ const MediaSelector = ({ setMediaToSend }) => {
 				break;
 			case 2:
 				launchCamera({ mediaType: 'video' }, (response: ImagePickerResponse) => {
+					// eslint-disable-next-line no-console
 					console.log(response);
+
 					if (response?.assets) {
+						setIsLoading(false);
 						setMedia(response.assets[0]);
 						setMediaToSend(response.assets[0]);
 					}
@@ -49,8 +66,11 @@ const MediaSelector = ({ setMediaToSend }) => {
 				break;
 			case 3:
 				launchImageLibrary({ mediaType: 'mixed' }, (response: ImagePickerResponse) => {
+					// eslint-disable-next-line no-console
 					console.log(response);
+
 					if (response?.assets) {
+						setIsLoading(false);
 						setMedia(response.assets[0]);
 						setMediaToSend(response.assets[0]);
 					}
@@ -75,7 +95,28 @@ const MediaSelector = ({ setMediaToSend }) => {
 				/>
 			) : (
 				<TouchableOpacity style={styles.mediaButton} onPress={() => actionSheetRef.current?.show()}>
-					<Text style={styles.buttonText}>Choose NFT Media</Text>
+					<LinearGradient colors={['#FFFFFF', '#EAF6FF']} style={styles.gradient}>
+						{isLoading ? (
+							<React.Fragment>
+								<ActivityIndicator color={colors.blue} style={styles.indicator} />
+								<Text small blue centered>
+									{'Uploading...'}
+								</Text>
+							</React.Fragment>
+						) : (
+							<React.Fragment>
+								<FontAwesomeIcon5
+									name="file-image"
+									size={36}
+									color={colors.blue}
+									style={styles.indicator}
+								/>
+								<Text small blue centered>
+									{'Take a photo, video or select from your device’s media.'}
+								</Text>
+							</React.Fragment>
+						)}
+					</LinearGradient>
 				</TouchableOpacity>
 			)}
 			<ActionSheet
