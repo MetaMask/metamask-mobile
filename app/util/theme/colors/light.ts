8fe0a70ec46609bdf28480e380b32a126e47059d
georgewrmarshall <george.marshall@consensys.net>
2021-12-16 11:00:19 +1300

    Updating theme files

--- app/util/theme/colors/light.ts
+++ app/util/theme/colors/light.ts
@@ -8,6 +8,26 @@ const colors: Colors = {
 	backgroundAlternative: baseColors.grey000,
 	borderDefault: baseColors.grey100,
 	muted: baseColors.grey100,
+	primary: baseColors.blue500,
+	onPrimary: baseColors.white,
+	info: baseColors.blue000,
+	onInfo: baseColors.blue500,
+	error: baseColors.red000,
+	onError: baseColors.red500,
+	success: baseColors.green100,
+	onSuccess: baseColors.green500,
+	warning: baseColors.yellow000,
+	onWarning: baseColors.yellow600,
+	inverse: baseColors.grey800,
+	onInverse: baseColors.white,
+	alert: 'rgba(0,0,0,.75)',
+	onAlert: baseColors.white,
+	// UI escape hatches for grey colors with no general purpose
+	ui4: baseColors.grey500,
+	onUi4: baseColors.white,
+	shadowColor: baseColors.gray900,
+	// needs auditing
+	transparent: 'transparent',
 };
 
 export default colors;
