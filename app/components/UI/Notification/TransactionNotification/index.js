75cdb51834748187b835ed2b8f736332c1aa9ebf
Andrea Salvatore <andrea.salvatore@consensys.net>
2022-01-05 13:25:31 +0100

    Fixes potential decimal number calculated for gasFee

--- app/components/UI/Notification/TransactionNotification/index.js
+++ app/components/UI/Notification/TransactionNotification/index.js
@@ -209,7 +209,8 @@ function TransactionNotification(props) {
 			const existingGasPrice = new BigNumber(tx?.transaction?.gasPrice || '0x0');
 			const gasFee = existingGasPrice
 				.times(transactionAction === ACTION_CANCEL ? CANCEL_RATE : SPEED_UP_RATE)
-				.toString();
+				.toString()
+				.split('.')[0];
 			setGasFee(gasFee);
 			setTx(tx);
 			setTransactionElement(transactionElement);
