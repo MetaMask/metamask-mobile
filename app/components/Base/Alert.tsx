d932ea7cbf82e1a77e4d90564b943825f33e42be
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2022-01-27 10:12:44 -0300

    Add tsx to lint files

--- app/components/Base/Alert.tsx
+++ app/components/Base/Alert.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import {
 	View,
 	StyleSheet,
@@ -7,7 +7,7 @@ import {
 	StyleProp,
 	TouchableOpacityProps,
 	ViewProps,
-	TextStyle
+	TextStyle,
 } from 'react-native';
 import { colors } from '../../styles/common';
 import CustomText from './Text';
@@ -17,37 +17,71 @@ const Text = CustomText as any;
 export enum AlertType {
 	Info = 'Info',
 	Warning = 'Warning',
-	Error = 'Error'
+	// eslint-disable-next-line @typescript-eslint/no-shadow
+	Error = 'Error',
 }
 
-type Props = {
+interface Props {
 	type: AlertType;
 	style?: StyleProp<ViewStyle>;
 	small?: boolean;
 	renderIcon?: () => ReactNode;
 	onPress?: () => void;
 	children?: ReactNode;
+}
+
+const styles = StyleSheet.create({
+	base: {
+		paddingHorizontal: 12,
+		paddingVertical: 8,
+		borderWidth: 1,
+		borderRadius: 8,
+		flexDirection: 'row',
+	},
+	baseSmall: {
+		paddingVertical: 8,
+	},
+	info: {
+		backgroundColor: colors.blue100,
+		borderColor: colors.blue,
+	},
+	warning: {
+		backgroundColor: colors.yellow100,
+		borderColor: colors.yellowWarningBorder,
+	},
+	error: {
+		backgroundColor: colors.red000,
+		borderColor: colors.red,
+	},
+	baseTextStyle: { fontSize: 14, flex: 1, lineHeight: 17 },
+	textInfo: { color: colors.blue },
+	textWarning: { color: colors.black },
+	textError: { color: colors.red },
+	textIconStyle: { marginRight: 12 },
+	iconWrapper: {
+		alignItems: 'center',
+	},
+});
+
+const getAlertStyles: (alertType: AlertType) => [StyleProp<ViewStyle>, StyleProp<TextStyle>] = (alertType) => {
+	switch (alertType) {
+		case AlertType.Warning: {
+			return [styles.warning, { ...styles.textWarning, ...styles.baseTextStyle }];
+		}
+		case AlertType.Error: {
+			return [styles.error, { ...styles.textError, ...styles.baseTextStyle }];
+		}
+		case AlertType.Info:
+		default: {
+			return [styles.info, { ...styles.textInfo, ...styles.baseTextStyle }];
+		}
+	}
 };
 
 const Alert = ({ type = AlertType.Info, small, renderIcon, style, onPress, children, ...props }: Props) => {
 	const Wrapper: React.ComponentClass<TouchableOpacityProps | ViewProps> = onPress ? TouchableOpacity : View;
 
-	const getStyles: (type: AlertType) => [StyleProp<ViewStyle>, StyleProp<TextStyle>] = useCallback(type => {
-		switch (type) {
-			case AlertType.Warning: {
-				return [styles.warning, { ...styles.textWarning, ...styles.baseTextStyle }];
-			}
-			case AlertType.Error: {
-				return [styles.error, { ...styles.textError, ...styles.baseTextStyle }];
-			}
-			case AlertType.Info:
-			default: {
-				return [styles.info, { ...styles.textInfo, ...styles.baseTextStyle }];
-			}
-		}
-	}, []);
-
-	const [wrapperStyle, textStyle] = getStyles(type);
+	const [wrapperStyle, textStyle] = getAlertStyles(type);
 
 	return (
 		<Wrapper style={[styles.base, small && styles.baseSmall, wrapperStyle, style]} onPress={onPress} {...props}>
@@ -62,38 +96,5 @@ const Alert = ({ type = AlertType.Info, small, renderIcon, style, onPress, child
 		</Wrapper>
 	);
 };
-
-const styles = StyleSheet.create({
-	base: {
-		paddingHorizontal: 12,
-		paddingVertical: 8,
-		borderWidth: 1,
-		borderRadius: 8,
-		flexDirection: 'row'
-	},
-	baseSmall: {
-		paddingVertical: 8
-	},
-	info: {
-		backgroundColor: colors.blue100,
-		borderColor: colors.blue
-	},
-	warning: {
-		backgroundColor: colors.yellow100,
-		borderColor: colors.yellowWarningBorder
-	},
-	error: {
-		backgroundColor: colors.red000,
-		borderColor: colors.red
-	},
-	baseTextStyle: { fontSize: 14, flex: 1, lineHeight: 17 },
-	textInfo: { color: colors.blue },
-	textWarning: { color: colors.black },
-	textError: { color: colors.red },
-	textIconStyle: { marginRight: 12 },
-	iconWrapper: {
-		alignItems: 'center'
-	}
-});
 
 export default Alert;
