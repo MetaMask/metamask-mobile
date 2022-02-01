fbcf04e26ee79d9a48f9990f4cdcb380715097e0
Tomás <tommasini15@gmail.com>
2022-02-01 23:21:24 +0000

    pipeline error fixed

--- app/components/Views/SendFlow/Confirm/index.js
+++ app/components/Views/SendFlow/Confirm/index.js
@@ -1192,7 +1192,7 @@ class Confirm extends PureComponent {
 				onToSelectedAddressChange={this.onToSelectedAddressChange}
 				confusableCollection={(!existingContact && confusableCollection) || []}
 				displayExclamation={displayExclamation}
-				confirmScreen={true}
+				confirmScreen
 			/>
 		);
 
