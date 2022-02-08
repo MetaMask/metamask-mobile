f79317f03a50bb62d5bcd5111006a51de0457571
Ricky Miller <ricky.miller@gmail.com>
2022-02-08 15:34:20 -0500

    update DetoxTest.java

--- android/app/src/androidTest/java/com/metamask/DetoxTest.java
+++ android/app/src/androidTest/java/com/metamask/DetoxTest.java
@@ -1,6 +1,7 @@
 package io.metamask;
 
 import com.wix.detox.Detox;
+import com.wix.detox.config.DetoxConfig;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,6 +20,11 @@ public class DetoxTest {
 
 	@Test
 	public void runDetoxTests() {
-		Detox.runTests(mActivityRule);
+		DetoxConfig detoxConfig = new DetoxConfig();
+		detoxConfig.idlePolicyConfig.masterTimeoutSec = 90;
+		detoxConfig.idlePolicyConfig.idleResourceTimeoutSec = 60;
+		detoxConfig.rnContextLoadTimeoutSec = (BuildConfig.DEBUG ? 180 : 60);
+
+		Detox.runTests(mActivityRule, detoxConfig);
 	}
 }
