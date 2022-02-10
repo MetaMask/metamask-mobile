d22795c9dcf972bb1819fc3e74f7113105be1808
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2022-02-10 16:09:39 -0300

    Refactor AddressQRCode

--- app/components/Views/AddressQRCode/index.js
+++ app/components/Views/AddressQRCode/index.js
@@ -1,149 +1,133 @@
-import React, { PureComponent } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import PropTypes from 'prop-types';
-import { TouchableOpacity, Dimensions, StyleSheet, View, Text } from 'react-native';
-import { colors, fontStyles } from '../../../styles/common';
-import { connect } from 'react-redux';
+import { StyleSheet, TouchableOpacity, useWindowDimensions, View } from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
-import { strings } from '../../../../locales/i18n';
 import IonicIcon from 'react-native-vector-icons/Ionicons';
-import Device from '../../../util/device';
+import { connect } from 'react-redux';
+
+import { strings } from '../../../../locales/i18n';
 import { showAlert } from '../../../actions/alert';
-import GlobalAlert from '../../UI/GlobalAlert';
 import { protectWalletModalVisible } from '../../../actions/user';
 import ClipboardManager from '../../../core/ClipboardManager';
-
-const WIDTH = Dimensions.get('window').width - 88;
+import { colors } from '../../../styles/common';
+import Text from '../../Base/Text';
+import GlobalAlert from '../../UI/GlobalAlert';
 
 const styles = StyleSheet.create({
-	root: {
-		flex: 1,
-		flexDirection: 'row',
-		alignItems: 'center',
-		marginTop: Device.isSmallDevice() ? -30 : -50,
-	},
 	wrapper: {
 		flex: 1,
 		alignItems: 'center',
+		justifyContent: 'center',
 	},
 	qrCode: {
-		marginBottom: 16,
-		alignItems: 'center',
-		justifyContent: 'center',
 		padding: 36,
 		backgroundColor: colors.grey000,
 		borderRadius: 8,
+		marginVertical: 16,
 	},
 	addressWrapper: {
-		alignItems: 'center',
-		justifyContent: 'center',
-		width: WIDTH,
 		borderRadius: 8,
 		backgroundColor: colors.grey000,
-		paddingVertical: 12,
+		padding: 12,
 	},
 	closeIcon: {
-		width: WIDTH + 40,
-		paddingBottom: Device.isSmallDevice() ? 30 : 50,
-		flexDirection: 'row-reverse',
-	},
-	addressTitle: {
-		fontSize: 16,
-		paddingHorizontal: 28,
-		paddingVertical: 4,
-		...fontStyles.normal,
-	},
-	address: {
-		...fontStyles.normal,
-		paddingHorizontal: 28,
-		paddingVertical: 4,
-		fontSize: 16,
-		textAlign: 'center',
+		alignItems: 'flex-end',
 	},
 });
 
 /**
- * PureComponent that renders a public address view
+ * Component that renders a public address view
  */
-class AddressQRCode extends PureComponent {
-	static propTypes = {
-		/**
-		 * Selected address as string
-		 */
-		selectedAddress: PropTypes.string,
-		/**
-		/* Triggers global alert
-		*/
-		showAlert: PropTypes.func,
-		/**
-		/* Callback to close the modal
-		*/
-		closeQrModal: PropTypes.func,
-		/**
-		 * Prompts protect wallet modal
-		 */
-		protectWalletModalVisible: PropTypes.func,
-		/**
-		 * redux flag that indicates if the user
-		 * completed the seed phrase backup flow
-		 */
-		seedphraseBackedUp: PropTypes.bool,
-	};
-
+const AddressQRCode = ({ closeQrModal, seedphraseBackedUp, protectWalletModalVisible, selectedAddress, showAlert }) => {
+	const { width: windowWidth, height: windowHeight } = useWindowDimensions();
+	const contentStyle = useMemo(() => {
+		const isPortrait = windowHeight > windowWidth;
+		if (isPortrait) {
+			return {
+				size: Math.min(windowWidth - 160, 420),
+			};
+		}
+		return {
+			size: 420,
+		};
+	}, [windowWidth, windowHeight]);
 	/**
 	 * Closes QR code modal
 	 */
-	closeQrModal = () => {
-		this.props.closeQrModal();
-		!this.props.seedphraseBackedUp && setTimeout(() => this.props.protectWalletModalVisible(), 1000);
-	};
+	const closeModal = useCallback(() => {
+		closeQrModal();
+		!seedphraseBackedUp && setTimeout(() => protectWalletModalVisible(), 1000);
+	}, [closeQrModal, protectWalletModalVisible, seedphraseBackedUp]);
 
-	copyAccountToClipboard = async () => {
-		const { selectedAddress } = this.props;
+	const copyAccountToClipboard = useCallback(async () => {
 		await ClipboardManager.setString(selectedAddress);
-		this.props.showAlert({
+		showAlert({
 			isVisible: true,
 			autodismiss: 1500,
 			content: 'clipboard-alert',
 			data: { msg: strings('account_details.account_copied_to_clipboard') },
 		});
-	};
+	}, [selectedAddress, showAlert]);
 
-	processAddress = () => {
-		const { selectedAddress } = this.props;
-		const processedAddress = `${selectedAddress.slice(0, 2)} ${selectedAddress
-			.slice(2)
-			.match(/.{1,4}/g)
-			.join(' ')}`;
-		return processedAddress;
-	};
+	const address = useMemo(
+		() =>
+			`${selectedAddress.slice(0, 2)} ${selectedAddress
+				.slice(2)
+				.match(/.{1,4}/g)
+				.join(' ')}`,
+		[selectedAddress]
+	);
 
-	render() {
-		return (
-			<View style={styles.root}>
-				<View style={styles.wrapper}>
-					<TouchableOpacity style={styles.closeIcon} onPress={this.closeQrModal} testID={'close-qr-modal'}>
-						<IonicIcon name={'ios-close'} size={38} color={colors.white} />
-					</TouchableOpacity>
-					<View style={styles.qrCode}>
-						<QRCode
-							value={`ethereum:${this.props.selectedAddress}`}
-							size={Dimensions.get('window').width - 160}
-						/>
-					</View>
-					<View style={styles.addressWrapper}>
-						<Text style={styles.addressTitle}>{strings('receive_request.public_address_qr_code')}</Text>
-						<TouchableOpacity onPress={this.copyAccountToClipboard}>
-							<Text style={styles.address} testID={'public-address-input'}>
-								{this.processAddress()}
-							</Text>
-						</TouchableOpacity>
-					</View>
-				</View>
-				<GlobalAlert />
+	return (
+		<View style={[styles.wrapper]}>
+			<TouchableOpacity
+				style={[styles.closeIcon, { width: contentStyle.size + 36 * 2 }]}
+				onPress={closeModal}
+				testID={'close-qr-modal'}
+			>
+				<IonicIcon name={'ios-close'} size={38} color={colors.white} />
+			</TouchableOpacity>
+			<View style={styles.qrCode}>
+				<QRCode value={`ethereum:${selectedAddress}`} size={contentStyle.size} />
 			</View>
-		);
-	}
-}
+			<View style={[styles.addressWrapper, { width: contentStyle.size + 36 * 2 }]}>
+				<Text big black centered>
+					{strings('receive_request.public_address_qr_code')}
+				</Text>
+				<TouchableOpacity onPress={copyAccountToClipboard}>
+					<Text big black centered testID={'public-address-input'}>
+						{address}
+					</Text>
+				</TouchableOpacity>
+			</View>
+			<GlobalAlert />
+		</View>
+	);
+};
+
+AddressQRCode.propTypes = {
+	/**
+	 * Selected address as string
+	 */
+	selectedAddress: PropTypes.string,
+	/**
+		/* Triggers global alert
+		*/
+	showAlert: PropTypes.func,
+	/**
+		/* Callback to close the modal
+		*/
+	closeQrModal: PropTypes.func,
+	/**
+	 * Prompts protect wallet modal
+	 */
+	protectWalletModalVisible: PropTypes.func,
+	/**
+	 * redux flag that indicates if the user
+	 * completed the seed phrase backup flow
+	 */
+	seedphraseBackedUp: PropTypes.bool,
+};
 
 const mapStateToProps = (state) => ({
 	selectedAddress: state.engine.backgroundState.PreferencesController.selectedAddress,
