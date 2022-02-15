27e28202fb1a42d46850fb4c2165bb08d5713957
georgewrmarshall <george.marshall@consensys.net>
2022-02-16 08:42:26 +1030

    Updating story and removing excluded archs

--- app/components/UI/ButtonReveal/ButtonReveal.stories.tsx
+++ app/components/UI/ButtonReveal/ButtonReveal.stories.tsx
@@ -2,18 +2,11 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react-native';
 import { action } from '@storybook/addon-actions';
-import { text, boolean, select } from '@storybook/addon-knobs';
+import { text } from '@storybook/addon-knobs';
 import ButtonReveal from './index';
 
 storiesOf('UI / ButtonReveal', module)
 	.addDecorator((getStory) => getStory())
-	.add('Default', () => {
-		return (
-			<ButtonReveal
-				label={'Hold to reveal SRP'}
-				onLongPress={() => {
-					console.log('Revealing SRP');
-				}}
-			/>
-		);
-	});
+	.add('Default', () => (
+		<ButtonReveal label={text('label', 'Hold to reveal SRP')} onLongPress={action('onLongPress')} />
+	));
