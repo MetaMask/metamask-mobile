7ce35a32ff67ed141a2ab1f4575a6881d58f800d
Andre Pimenta <andrepimenta7@gmail.com>
2022-02-22 14:14:41 +0000

    Fix margin when account is selected but not connected

--- app/components/UI/AccountListPermissions/AccountElement/index.tsx
+++ app/components/UI/AccountListPermissions/AccountElement/index.tsx
@@ -54,6 +54,7 @@ const styles = StyleSheet.create({
 		backgroundColor: colors.yellow100,
 		borderColor: colors.yellow,
 		borderWidth: 1,
+		marginBottom: 8,
 	},
 	accountNameContainer: {
 		flexDirection: 'row',
