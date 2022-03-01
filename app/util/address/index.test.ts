0864b2738f26cb6d88523c5907b54e25bc53ea81
tommasini <46944231+tommasini@users.noreply.github.com>
2022-03-01 12:40:43 +0000

    Fix issue #2829 - Lack of full ENS namespace support (#3825)

--- app/util/address/index.test.ts
+++ app/util/address/index.test.ts
@@ -4,8 +4,8 @@ describe('isENS', () => {
 	it('should return false by default', () => {
 		expect(isENS()).toBe(false);
 	});
-	it('should return false for normal domain', () => {
-		expect(isENS('ricky.codes')).toBe(false);
+	it('should return true for normal domain', () => {
+		expect(isENS('ricky.codes')).toBe(true);
 	});
 	it('should return true for ens', () => {
 		expect(isENS('rickycodes.eth')).toBe(true);
