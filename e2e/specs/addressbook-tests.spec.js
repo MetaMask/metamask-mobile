6a1970169d63e06abf7ec2b017bc8d46ae0b04cc
Curtis <Curtis.David7@gmail.com>
2022-03-10 13:00:03 -0500

    move contract nickname into it's own file.

--- e2e/specs/addressbook-tests.spec.js
+++ e2e/specs/addressbook-tests.spec.js
@@ -1,61 +1,61 @@
 'use strict';
-import ImportWalletView from '../pages/Onboarding/ImportWalletView';
 import OnboardingView from '../pages/Onboarding/OnboardingView';
 import OnboardingCarouselView from '../pages/Onboarding/OnboardingCarouselView';
+import ProtectYourWalletView from '../pages/Onboarding/ProtectYourWalletView';
+import CreatePasswordView from '../pages/Onboarding/CreatePasswordView';
 
-import ContractNickNameView from '../pages/ContractNickNameView';
 import SendView from '../pages/SendView';
 
-import DrawerView from '../pages/Drawer/DrawerView';
 import MetaMetricsOptIn from '../pages/Onboarding/MetaMetricsOptInView';
 import WalletView from '../pages/WalletView';
+import DrawerView from '../pages/Drawer/DrawerView';
 
-import AddContactView from '../pages/Drawer/Settings/Contacts/AddContactView';
-import ContactsView from '../pages/Drawer/Settings/Contacts/ContactsView';
 import SettingsView from '../pages/Drawer/Settings/SettingsView';
+import ContactsView from '../pages/Drawer/Settings/Contacts/ContactsView';
+import AddContactView from '../pages/Drawer/Settings/Contacts/AddContactView';
 
 import AddAddressModal from '../pages/modals/AddAddressModal';
-import ApprovalModal from '../pages/modals/ApprovalModal';
-import NetworkListModal from '../pages/modals/NetworkListModal';
+import SkipAccountSecurityModal from '../pages/modals/SkipAccountSecurityModal';
 import OnboardingWizardModal from '../pages/modals/OnboardingWizardModal';
+import ProtectYourWalletModal from '../pages/modals/ProtectYourWalletModal';
 
 import TestHelpers from '../helpers';
-
-const SECRET_RECOVERY_PHRASE = 'fold media south add since false relax immense pause cloth just raven';
-const PASSWORD = `12345678`;
 
 const INVALID_ADDRESS = '0xB8B4EE5B1b693971eB60bDa15211570df2dB221L';
 const TETHER_ADDRESS = '0xdac17f958d2ee523a2206206994597c13d831ec7';
 const MYTH_ADDRESS = '0x1FDb169Ef12954F20A15852980e1F0C122BfC1D6';
 const MEMO = 'Test adding ENS';
-const RINKEBY = 'Rinkeby Test Network';
-const APPROVAL_DEEPLINK_URL =
-	'https://metamask.app.link/send/0x01BE23585060835E02B77ef475b0Cc51aA1e0709@4/approve?address=0x178e3e6c9f547A00E33150F7104427ea02cfc747&uint256=5e8';
-const CONTRACT_NICK_NAME_TEXT = 'Ace RoMaIn';
+const PASSWORD = '12345678';
 
 describe('Addressbook Tests', () => {
 	beforeEach(() => {
 		jest.setTimeout(150000);
 	});
 
-	it('should import via seed phrase and validate in settings', async () => {
+	it('should create new wallet', async () => {
 		await OnboardingCarouselView.isVisible();
 		await OnboardingCarouselView.tapOnGetStartedButton();
 
 		await OnboardingView.isVisible();
-		await OnboardingView.tapImportWalletFromSeedPhrase();
+		await OnboardingView.tapCreateWallet();
 
 		await MetaMetricsOptIn.isVisible();
 		await MetaMetricsOptIn.tapAgreeButton();
 
-		await ImportWalletView.isVisible();
+		await CreatePasswordView.isVisible();
+		await CreatePasswordView.enterPassword(PASSWORD);
+		await CreatePasswordView.reEnterPassword(PASSWORD);
+		await CreatePasswordView.tapIUnderstandCheckBox();
+		await CreatePasswordView.tapCreatePasswordButton();
 	});
 
-	it('should attempt to import wallet with invalid secret recovery phrase', async () => {
-		await ImportWalletView.toggleRememberMe();
-		await ImportWalletView.enterSecretRecoveryPhrase(SECRET_RECOVERY_PHRASE);
-		await ImportWalletView.enterPassword(PASSWORD);
-		await ImportWalletView.reEnterPassword(PASSWORD);
+	it('Should skip backup check', async () => {
+		// Check that we are on the Secure your wallet screen
+		await ProtectYourWalletView.isVisible();
+		await ProtectYourWalletView.tapOnRemindMeLaterButton();
+
+		await SkipAccountSecurityModal.tapIUnderstandCheckBox();
+		await SkipAccountSecurityModal.tapSkipButton();
 		await WalletView.isVisible();
 	});
 
@@ -69,6 +69,18 @@ describe('Addressbook Tests', () => {
 		} catch {
 			//
 		}
+	});
+
+	it('should dismiss the protect your wallet modal', async () => {
+		await ProtectYourWalletModal.isCollapsedBackUpYourWalletModalVisible();
+		await TestHelpers.delay(1000);
+
+		await ProtectYourWalletModal.tapRemindMeLaterButton();
+
+		await SkipAccountSecurityModal.tapIUnderstandCheckBox();
+		await SkipAccountSecurityModal.tapSkipButton();
+
+		await WalletView.isVisible();
 	});
 
 	it('should go to send view', async () => {
@@ -97,7 +109,6 @@ describe('Addressbook Tests', () => {
 		await AddAddressModal.tapSaveButton();
 
 		await SendView.removeAddress();
-		await TestHelpers.delay(1000);
 		await SendView.isSavedAliasVisible('Myth'); // Check that the new account is on the address list
 	});
 
@@ -177,94 +188,5 @@ describe('Addressbook Tests', () => {
 		await DrawerView.tapSendButton();
 
 		await SendView.isSavedAliasVisible('Ibrahim');
-	});
-	it('should swtich to rinkeby', async () => {
-		await SendView.tapcancelButton();
-		await WalletView.tapNetworksButtonOnNavBar();
-		await NetworkListModal.changeNetwork(RINKEBY);
-
-		await WalletView.isNetworkNameVisible(RINKEBY);
-	});
-
-	it('should deep link to the approval modal', async () => {
-		await TestHelpers.openDeepLink(APPROVAL_DEEPLINK_URL);
-		await TestHelpers.delay(3000);
-		await ApprovalModal.isVisible();
-	});
-	it('should add a nickname to the contract', async () => {
-		await ApprovalModal.tapAddNickName();
-
-		await ContractNickNameView.isVisible();
-		await ContractNickNameView.typeContractNickName(CONTRACT_NICK_NAME_TEXT);
-		await ContractNickNameView.isContractNickNameInInputBoxVisible(CONTRACT_NICK_NAME_TEXT);
-		await ContractNickNameView.tapConfirmButton();
-
-		await ApprovalModal.isContractNickNameVisible(CONTRACT_NICK_NAME_TEXT);
-	});
-
-	it('should edit the contract nickname', async () => {
-		await ApprovalModal.tapEditNickName();
-
-		await ContractNickNameView.isContractNickNameInInputBoxVisible(CONTRACT_NICK_NAME_TEXT);
-		await ContractNickNameView.typeContractNickName('Ace');
-		await ContractNickNameView.tapConfirmButton();
-
-		await ApprovalModal.isContractNickNameVisible('Ace');
-		await ApprovalModal.tapToCopyContractAddress();
-		await ApprovalModal.tapRejectButton();
-	});
-
-	it('should return to the send view', async () => {
-		// Open Drawer
-		await WalletView.tapDrawerButton();
-
-		await DrawerView.isVisible();
-		await DrawerView.tapSendButton();
-		// Make sure view with my accounts visible
-		await SendView.isTransferBetweenMyAccountsButtonVisible();
-	});
-
-	it('should verify the contract nickname does not appear in send flow', async () => {
-		await SendView.isSavedAliasIsNotVisible('Ace');
-	});
-
-	it('should verify contract does not appear in contacts view', async () => {
-		await SendView.tapcancelButton();
-
-		// Check that we are on the wallet screen
-		await WalletView.isVisible();
-		await WalletView.tapDrawerButton();
-
-		await DrawerView.isVisible();
-		await DrawerView.tapSettings();
-
-		await SettingsView.tapContacts();
-
-		await ContactsView.isVisible();
-		await ContactsView.isContactAliasVisible('Ace');
-	});
-
-	it('should deep link to the approval modal and approve transaction', async () => {
-		await TestHelpers.openDeepLink(APPROVAL_DEEPLINK_URL);
-		await TestHelpers.delay(3000);
-		await ApprovalModal.isVisible();
-		await ApprovalModal.isContractNickNameVisible('Ace');
-
-		await ApprovalModal.tapApproveButton();
-		await ApprovalModal.isNotVisible();
-	});
-
-	it('should go to the send view again', async () => {
-		// Open Drawer
-		await WalletView.tapDrawerButton();
-
-		await DrawerView.isVisible();
-		await DrawerView.tapSendButton();
-		// Make sure view with my accounts visible
-		await SendView.isTransferBetweenMyAccountsButtonVisible();
-	});
-
-	it('should verify the contract nickname does not appear in recents', async () => {
-		await SendView.isSavedAliasIsNotVisible('Ace');
 	});
 });
