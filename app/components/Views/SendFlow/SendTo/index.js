65aec18520921dc8db7a4ed74dd4d4ab3e1015b9
Ricky Miller <ricky.miller@gmail.com>
2022-03-16 14:07:00 -0400

    add .

--- app/components/Views/SendFlow/SendTo/index.js
+++ app/components/Views/SendFlow/SendTo/index.js
@@ -690,7 +690,7 @@ class SendFlow extends PureComponent {
 											{strings('transaction.confusable_title')}
 										</Text>
 										<Text style={styles.confusableMsg}>
-											{strings('transaction.confusable_msg')} {explanations.join(', ')}
+											{strings('transaction.confusable_msg')} {explanations.join(', ')}.
 										</Text>
 									</View>
 								</View>
