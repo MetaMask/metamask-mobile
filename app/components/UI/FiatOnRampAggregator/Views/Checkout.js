a5cf96b54ae5b85aea302d24ea7cf411bb6f53d6
Alaa Hadad <alaahd@gmail.com>
2022-03-28 14:13:49 +0400

    basic setup for order processing

--- app/components/UI/FiatOnRampAggregator/Views/Checkout.js
+++ app/components/UI/FiatOnRampAggregator/Views/Checkout.js
@@ -5,6 +5,7 @@ import { WebView } from 'react-native-webview';
 import { baseStyles } from '../../../../styles/common';
 import { useTheme } from '../../../../util/theme';
 import { getFiatOnRampAggNavbar } from '../../Navbar';
+import { allowedCallbackBaseUrls } from '../orderProcessor';
 
 const CheckoutWebView = () => {
 	const navigation = useNavigation();
@@ -15,10 +16,22 @@ const CheckoutWebView = () => {
 		navigation.setOptions(getFiatOnRampAggNavbar(navigation, { title: 'Checkout' }, colors));
 	}, [navigation, colors]);
 
-	if (params?.buyURL) {
+	const handleNavigationStateChange = async (navState) => {
+		if (allowedCallbackBaseUrls.some((callbackBaseUrl) => navState?.url.startsWith(callbackBaseUrl))) {
+			// eslint-disable-next-line no-console
+			console.log('redirectURL--------', navState?.url);
+		}
+	};
+
+	if (uri) {
 		return (
 			<View style={baseStyles.flexGrow}>
-				<WebView source={{ uri }} allowInlineMediaPlayback mediaPlaybackRequiresUserAction={false} />
+				<WebView
+					source={{ uri }}
+					allowInlineMediaPlayback
+					mediaPlaybackRequiresUserAction={false}
+					onNavigationStateChange={handleNavigationStateChange}
+				/>
 			</View>
 		);
 	}
