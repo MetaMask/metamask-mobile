6f68f1472a2ffccccf601be73422f41f46a38e3f
gantunesr <gantunes@uc.cl>
2022-04-10 23:05:10 -0400

    Add new component AccountDetails

---
+++ app/components/Views/ConnectQRHardware/AccountDetails/index.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/EvilIcons';
+import Device from '../../../../util/device';
+import { getEtherscanAddressUrl } from '../../../../util/etherscan';
+import { renderFromWei } from '../../../../util/number';
+import { mockTheme, useAppThemeFromContext } from '../../../../util/theme';
+import EthereumAddress from '../../../UI/EthereumAddress';
+
+interface IAccountDetailsProps {
+	item: any;
+	network: string;
+}
+
+const createStyle = (colors: any) =>
+	StyleSheet.create({
+		rowContainer: {
+			flex: 1,
+			height: 65,
+			flexDirection: 'row',
+			justifyContent: 'space-between',
+			paddingLeft: Device.isIphoneX() ? 20 : 10,
+		},
+		accountDetails: {
+			justifyContent: 'flex-start',
+		},
+		linkIcon: {
+			height: '100%',
+			fontSize: 36,
+			textAlignVertical: 'center',
+		},
+		index: {
+			fontSize: 22,
+			color: colors.text.default,
+		},
+		information: {
+			color: colors.text.muted,
+		},
+	});
+
+const AccountDetails = (props: IAccountDetailsProps) => {
+	const { colors } = useAppThemeFromContext() || mockTheme;
+	const styles = createStyle(colors);
+	const navigation = useNavigation();
+	const { item } = props;
+
+	const toEtherscan = (address: string) => {
+		const { network } = props;
+		const accountLink = getEtherscanAddressUrl(network, address);
+		navigation.navigate('Webview', {
+			screen: 'SimpleWebview',
+			params: {
+				url: accountLink,
+			},
+		});
+	};
+
+	return (
+		<View style={styles.rowContainer}>
+			<View style={styles.accountDetails}>
+				<Text style={styles.index}>{item.index}</Text>
+				<EthereumAddress style={styles.information} address={item.address} type={'short'} />
+				<Text style={styles.information}>{renderFromWei(item.balance)} ETH</Text>
+			</View>
+			<Icon
+				size={18}
+				name={'external-link'}
+				onPress={() => toEtherscan(item.address)}
+				style={styles.linkIcon}
+				color={colors.text.default}
+			/>
+		</View>
+	);
+};
+
+export default AccountDetails;
