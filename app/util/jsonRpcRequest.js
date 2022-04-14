b85b0b9303fd868ff470f6ce16031ec8ea62ecfc
Daisuke Ogawa <d-ogawa@plathome.co.jp>
2022-04-14 11:41:04 +0900

    fix custom network addtion with basic auth

--- app/util/jsonRpcRequest.js
+++ app/util/jsonRpcRequest.js
@@ -18,12 +18,12 @@ export async function jsonRpcRequest(rpcUrl, rpcMethod, rpcParams = []) {
 	};
 
 	// Convert basic auth URL component to Authorization header
-	const { origin, pathname, username, password, search } = new URL(rpcUrl);
+	const { origin, pathname, username, password, query } = new URL(rpcUrl);
 	// URLs containing username and password needs special processing
 	if (username && password) {
 		const encodedAuth = Buffer.from(`${username}:${password}`).toString('base64');
 		headers.Authorization = `Basic ${encodedAuth}`;
-		fetchUrl = `${origin}${pathname}${search}`;
+		fetchUrl = `${origin}${pathname}${query}`;
 	}
 
 	const jsonRpcResponse = await fetch(fetchUrl, {
