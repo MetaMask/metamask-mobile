f759c421b6ec0c2bd599d057374cb9b0da089f61
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2022-04-25 16:28:24 -0500

    Handle lock time when using apple pay

--- app/components/UI/FiatOnRampAggregator/containers/ApplePayButton.tsx
+++ app/components/UI/FiatOnRampAggregator/containers/ApplePayButton.tsx
@@ -1,8 +1,5 @@
 import React, { useCallback } from 'react';
-import {
-	useDispatch,
-	//useSelector
-} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 import { protectWalletModalNotVisible } from '../../../../actions/user';
 import { addFiatOrder } from '../../../../reducers/fiatOrders';
@@ -11,22 +8,21 @@ import useApplePay from '../hooks/applePay';
 import Logger from '../../../../util/Logger';
 import NotificationManager from '../../../../core/NotificationManager';
 import { strings } from '../../../../../locales/i18n';
-
-// TODO(wachunei): Handle lock time
+import { setLockTime } from '../../../../actions/settings';
 
 const ApplePayButton = ({ quote, label }: { quote: any; label: string }) => {
 	const navigation = useNavigation();
 	const dispatch = useDispatch();
 	const [pay, ABORTED] = useApplePay(quote) as [() => Promise<any>, string];
-	// const lockTime = useSelector((state) => state.settings.lockTime);
+	const lockTime = useSelector((state: any) => state.settings.lockTime);
 
 	// const setLockTime = useCallback((time) => dispatch(setLockTime(time)), [dispatch]);
 	const addOrder = useCallback((order) => dispatch(addFiatOrder(order)), [dispatch]);
 	const protectWalletModalVisible = useCallback(() => dispatch(protectWalletModalNotVisible()), [dispatch]);
 
 	const handlePress = useCallback(async () => {
-		// const prevLockTime = lockTime;
-		// setLockTime(-1);
+		const prevLockTime = lockTime;
+		setLockTime(-1);
 		try {
 			const order = await pay();
 			if (order !== ABORTED) {
@@ -50,9 +46,9 @@ const ApplePayButton = ({ quote, label }: { quote: any; label: string }) => {
 			});
 			Logger.error(error, 'FiatOrders::WyreApplePayProcessor Error');
 		} finally {
-			// setLockTime(prevLockTime);
+			setLockTime(prevLockTime);
 		}
-	}, [ABORTED, addOrder, navigation, pay, protectWalletModalVisible, quote.crypto.symbol]);
+	}, [ABORTED, addOrder, lockTime, navigation, pay, protectWalletModalVisible, quote.crypto.symbol]);
 
 	return <ApplePayButtonComponent label={label} onPress={handlePress} />;
 };
