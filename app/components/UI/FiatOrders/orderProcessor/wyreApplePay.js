205304f9ed5cadac054175fb161c4fcd00a0f869
Gustavo Antunes <gantunes@uc.cl>
2022-04-25 20:33:55 -0400

    Revert #4151 (#4155)

--- app/components/UI/FiatOrders/orderProcessor/wyreApplePay.js
+++ app/components/UI/FiatOrders/orderProcessor/wyreApplePay.js
@@ -532,7 +532,7 @@ const paymentOptions = {
 	requestPayerPhone: true,
 	requestPayerEmail: true,
 	requestBilling: true,
-	merchantCapabilities: ['debit'],
+	merchantCapabilities: ['debit', 'credit'],
 };
 
 const createPayload = (network, amount, address, currency, paymentDetails) => {
