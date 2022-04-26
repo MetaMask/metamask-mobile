94a447a016c25415cd2ae74b2839fc6142478e26
Ricky Miller <ricky.miller@gmail.com>
2022-04-26 15:09:57 -0400

    update config

--- wdio/config/android.config.js
+++ wdio/config/android.config.js
@@ -1,19 +1,19 @@
-const {config} = require('../wdio.conf');
+const { config } = require('../wdio.conf');
 
 // Appium capabilities
 config.capabilities = [
-    {
-        platformName: 'Android',
-        noReset: true,
-        fullReset: false,
-        maxInstances: 1,
-        deviceName: "Pixel 3 API 29",
-        platformVersion: "10",
-        app: "/Users/chriswilcox/projects/wdio/resources/ApiDemos-debug.apk",
-        appPackage: "io.appium.android.apis",
-        appActivity: ".view.TextFields",
-        automationName: "UiAutomator2"
-    }
+	{
+		platformName: 'Android',
+		noReset: true,
+		fullReset: false,
+		maxInstances: 1,
+		deviceName: 'Pixel 3 API 29',
+		platformVersion: '10',
+		app: '../android/app/build/outputs/apk/debug/app-debug.apk',
+		appPackage: 'io.metamask',
+		appActivity: '.MainActivity',
+		automationName: 'UiAutomator2',
+	},
 ];
 
 config.cucumberOpts.tagExpression = '@androidApp'; // pass tag to run tests specific to android
--- wdio/demo_index.js
+++ wdio/demo_index.js
@@ -1,20 +1,7 @@
 const wdio = require("webdriverio");
 const assert = require("assert");
 
-const opts = {
-    path: '/wd/hub',
-    port: 4723,
-    capabilities: {
-        platformName: "Android",
-        platformVersion: "10",
-        deviceName: "Pixel 3 API 29",
-        app: "/Users/chriswilcox/projects/wdio/resources/ApiDemos-debug.apk",
-        // app: __dirname + "/projects/wdio/resources/ApiDemos-debug.apk",
-        appPackage: "io.appium.android.apis",
-        appActivity: ".view.TextFields",
-        automationName: "UiAutomator2"
-    }
-};
+const opts = {};
 
 async function main() {
     const client = await wdio.remote(opts);
