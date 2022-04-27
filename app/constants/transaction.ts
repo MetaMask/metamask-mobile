d31fa774d44287fd48ca74acd582d2105a5de2ec
Gustavo Antunes <gantunes@uc.cl>
2022-04-27 18:27:31 -0400

    [FIX] Issue #207 (#4035)

--- app/constants/transaction.ts
+++ app/constants/transaction.ts
@@ -1,3 +1,6 @@
+import { BN } from 'ethereumjs-util';
+
+// Transaction Status
 export const TX_UNAPPROVED = 'unapproved';
 export const TX_SUBMITTED = 'submitted';
 export const TX_SIGNED = 'signed';
@@ -5,4 +8,5 @@ export const TX_PENDING = 'pending';
 export const TX_CONFIRMED = 'confirmed';
 
 // Values
+export const UINT256_BN_MAX_VALUE = new BN(2).pow(new BN(256)).sub(new BN(1));
 export const UINT256_HEX_MAX_VALUE = 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
