6f29368194f391a641d31293b4abf06891dc6ebb
João Loureiro <joao.loureiro@consensys.net>
2022-05-06 15:02:28 +0100

    remove eslint-disable comment

--- app/components/Base/BaseAvatar/BaseAvatar.test.tsx
+++ app/components/Base/BaseAvatar/BaseAvatar.test.tsx
@@ -7,13 +7,13 @@ import { Image } from 'react-native';
 describe('BaseAvatar', () => {
   it('should match the snapshot', () => {
     const stubAddress = '0x310ff9e227946749ca32aC146215F352183F556b';
+    const blockieStyles = {
+      width: '100%',
+      height: '100%',
+    };
     const wrapper = shallow(
       <BaseAvatar size={AvatarSize.ExtraLarge}>
-        <Image
-          source={{ uri: toDataUrl(stubAddress) }}
-          // eslint-disable-next-line react-native/no-inline-styles
-          style={{ width: '100%', height: '100%' }}
-        />
+        <Image source={{ uri: toDataUrl(stubAddress) }} style={blockieStyles} />
       </BaseAvatar>,
     );
     expect(wrapper).toMatchSnapshot();
