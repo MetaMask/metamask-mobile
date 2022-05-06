e68da6aaceb0c4265c7dfe3e6766f60a48d97b60
João Loureiro <joao.loureiro@consensys.net>
2022-05-06 15:01:26 +0100

    add useStyles jsdoc comment. inject theme context

--- app/components/hooks/useStyles.ts
+++ app/components/hooks/useStyles.ts
@@ -1,3 +1,4 @@
+import { useTheme } from '@react-navigation/native';
 import { useMemo } from 'react';
 
 type UseStylesType = <ParamsType, StyleSheetType>(
@@ -5,7 +6,18 @@ type UseStylesType = <ParamsType, StyleSheetType>(
   params: ParamsType,
 ) => StyleSheetType;
 
-const useStyles: UseStylesType = (createStyles, params) =>
-  useMemo(() => createStyles(params), [createStyles, params]);
+/**
+ * useStyles, a custom hook that memoizes the stylesheet object with the supplied parameters
+ * @param createStyles function which creates a stylesheet
+ * @param params parameters that are used inside the stylesheet
+ * @returns memoized stylesheet object
+ */
+const useStyles: UseStylesType = (createStyles, params) => {
+  const theme = useTheme();
+  return useMemo(
+    () => createStyles({ theme, ...params }),
+    [createStyles, params, theme],
+  );
+};
 
 export default useStyles;
