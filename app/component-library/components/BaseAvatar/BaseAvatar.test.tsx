7651ca39fb77b90e13d82fc7caf32d04074791e4
João Loureiro <joao.loureiro@consensys.net>
2022-05-09 21:59:53 +0100

    fix tests

--- app/component-library/components/BaseAvatar/BaseAvatar.test.tsx
+++ app/component-library/components/BaseAvatar/BaseAvatar.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import BaseAvatar, { AvatarSize } from '.';
-import { toDataUrl } from 'app/util/blockies';
+import { toDataUrl } from '../../../util/blockies';
 import { Image } from 'react-native';
 
 describe('BaseAvatar', () => {
-  it('should match the snapshot', () => {
+  it('should render correctly', () => {
     const stubAddress = '0x310ff9e227946749ca32aC146215F352183F556b';
     const blockieStyles = {
       width: '100%',
