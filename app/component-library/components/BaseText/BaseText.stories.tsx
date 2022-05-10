880f3767b315eb4cc73069c5cdaf52c9cae8dd3f
Cal Leung <cleun007@gmail.com>
2022-05-10 13:47:35 -0700

    Update BaseText storybook

--- app/component-library/components/BaseText/BaseText.stories.tsx
+++ app/component-library/components/BaseText/BaseText.stories.tsx
@@ -4,6 +4,9 @@ import BaseText, { BaseTextVariant } from './';
 
 storiesOf('Component Library / BaseText', module)
   .addDecorator((getStory) => getStory())
-  .add('Simple', () => (
-    <BaseText variant={BaseTextVariant.lBodyMD}>{`I'm Text!`}</BaseText>
+  .add('Small Display MD', () => (
+    <BaseText variant={BaseTextVariant.sDisplayMD}>{`I'm Text!`}</BaseText>
+  ))
+  .add('Small Body MD', () => (
+    <BaseText variant={BaseTextVariant.sBodyMD}>{`I'm Text!`}</BaseText>
   ));
