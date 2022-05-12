45f8f5028f0965d6f2ad5faf88765cca8ceb8bf5
Vik Chawla <vik.chawla2@gmail.com>
2022-05-12 17:53:53 -0400

    new getstarted screen based off previous implementation and pr by Alaa

--- app/components/UI/FiatOnRampAggregator/Views/GetStarted.tsx
+++ app/components/UI/FiatOnRampAggregator/Views/GetStarted.tsx
@@ -1,10 +1,7 @@
 import React, { useCallback, useEffect } from 'react';
 import { StyleSheet, Image, View, Dimensions } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-
 import TextJS from '../../../Base/Text';
-import ListItemJS from '../../../Base/ListItem';
 import StyledButton from '../../StyledButton';
 import ScreenLayout from '../components/ScreenLayout';
 import { getFiatOnRampAggNavbar } from '../../Navbar';
@@ -13,15 +10,12 @@ import { useTheme } from '../../../../util/theme';
 import { useFiatOnRampSDK } from '../sdk';
 import ErrorViewWithReportingJS from '../components/ErrorViewWithReporting';
 
-const IMG_PADDING = 80;
-const DEVICE_WIDTH = Dimensions.get('window').width;
-
 /* eslint-disable import/no-commonjs, @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports */
-const getStartedIcon = require('../components/images/get-started.png');
+const getStartedIcon = require('../components/images/WalletInfo.png');
 
 // TODO: Convert into typescript and correctly type optionals
 const Text = TextJS as any;
-const ListItem = ListItemJS as any;
+
 const ErrorViewWithReporting = ErrorViewWithReportingJS as any;
 
 const styles = StyleSheet.create({
@@ -42,46 +36,19 @@ const styles = StyleSheet.create({
   },
   getStartedImageWrapper: { flexDirection: 'row', justifyContent: 'center' },
   getStartedImage: {
-    width: DEVICE_WIDTH - IMG_PADDING,
-    height: 250,
-    marginTop: 30,
+    marginTop: 80,
   },
   ctaWrapper: {
     marginBottom: 30,
     marginTop: 20,
   },
+  marginTop: {
+    marginTop: 15,
+  },
+  caption: {
+    marginVertical: 22,
+  },
 });
-
-const whatToExpectList = [
-  {
-    id: 1,
-    title: strings('fiat_on_ramp_aggregator.onboarding.save_time_money'),
-    description: strings(
-      'fiat_on_ramp_aggregator.onboarding.save_time_money_description',
-    ),
-    icon: 'clock-outline',
-  },
-  {
-    id: 2,
-    title: strings(
-      'fiat_on_ramp_aggregator.onboarding.full_control_at_your_hands',
-    ),
-    description: strings(
-      'fiat_on_ramp_aggregator.onboarding.full_control_at_your_hands_description',
-    ),
-    icon: 'tune',
-  },
-  {
-    id: 3,
-    title: strings(
-      'fiat_on_ramp_aggregator.onboarding.growing_collection_of_tokens',
-    ),
-    description: strings(
-      'fiat_on_ramp_aggregator.onboarding.growing_collection_of_tokens_description',
-    ),
-    icon: 'format-list-bulleted',
-  },
-];
 
 const GetStarted: React.FC = () => {
   const navigation = useNavigation();
@@ -107,15 +74,6 @@ const GetStarted: React.FC = () => {
     setGetStarted(true);
   }, [navigation, setGetStarted]);
 
-  useEffect(() => {
-    if (getStarted) {
-      navigation.reset({
-        index: 0,
-        routes: [{ name: 'Region_hasStarted' }],
-      });
-    }
-  }, [getStarted, navigation]);
-
   if (sdkError) {
     return (
       <ScreenLayout>
@@ -139,19 +97,12 @@ const GetStarted: React.FC = () => {
           </View>
         </ScreenLayout.Content>
         <ScreenLayout.Content>
-          {whatToExpectList.map(({ id, title, description, icon }) => (
-            <ListItem.Content key={id} style={styles.listItem}>
-              <ListItem.Icon style={styles.icon}>
-                <MaterialCommunityIcons name={icon} style={styles.icon} />
-              </ListItem.Icon>
-              <ListItem.Body>
-                <ListItem.Title bold style={styles.title}>
-                  {title}
-                </ListItem.Title>
-                <Text style={styles.description}>{description}</Text>
-              </ListItem.Body>
-            </ListItem.Content>
-          ))}
+          <Text centered bold style={styles.marginTop}>
+            {strings('fiat_on_ramp_aggregator.onboarding.best_quotes')}
+          </Text>
+          <Text centered bold style={styles.caption}>
+            {strings('fiat_on_ramp_aggregator.onboarding.benefits')}
+          </Text>
         </ScreenLayout.Content>
       </ScreenLayout.Body>
 
