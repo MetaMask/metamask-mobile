8d96293207c302b225e39146c8e913eccf6012d4
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2022-05-13 12:58:40 -0500

    Fix chevron icon color

--- app/components/UI/FiatOnRampAggregator/components/CurrencyChevron.tsx
+++ app/components/UI/FiatOnRampAggregator/components/CurrencyChevron.tsx
@@ -1,32 +1,39 @@
-/* eslint react/prop-types: 0 */
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Entypo from 'react-native-vector-icons/Entypo';
+import BaseText from '../../../Base/Text';
+import { useTheme } from '../../../../util/theme';
+import { Colors } from '../../../../util/theme/models';
 
-import CustomText from '../../../Base/Text';
 // TODO: Convert into typescript and correctly type optionals
-const Text = CustomText as any;
+const Text = BaseText as any;
 
-const styles = StyleSheet.create({
-  chevron: {
-    marginLeft: 10,
-  },
-});
+const createStyles = (colors: Colors) =>
+  StyleSheet.create({
+    chevron: {
+      marginLeft: 10,
+      color: colors.icon.default,
+    },
+  });
 
 interface Props {
   currency?: string;
 }
 
-const CurrencyChevron = ({ currency, ...props }: Props) => (
-  <View {...props}>
-    <Text black>
-      <Text black bold>
-        {currency}
+const CurrencyChevron = ({ currency, ...props }: Props) => {
+  const { colors } = useTheme();
+  const styles = createStyles(colors);
+  return (
+    <View {...props}>
+      <Text black>
+        <Text black bold>
+          {currency}
+        </Text>
+        {'  '}
+        <Entypo name="chevron-down" size={16} style={styles.chevron} />
       </Text>
-      {'  '}
-      <Entypo name="chevron-down" size={16} style={styles.chevron} />
-    </Text>
-  </View>
-);
+    </View>
+  );
+};
 
 export default CurrencyChevron;
