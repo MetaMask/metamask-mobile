20e6cfe2533a6674c7e0514d568a528638e73b59
owencraston <owen.craston@consensys.net>
2022-05-13 13:42:14 -0400

    Hide remember me
    - remove remember me from login options by default
    - allow users to enable it in security settings
    - remember me is lower priority than biometrics and biometrics will
      allways show up if it is enabled

--- app/actions/user/index.js
+++ app/actions/user/index.js
@@ -104,3 +104,10 @@ export function checkedAuth(initialScreen) {
     },
   };
 }
+
+export function setIsUsingRememberMe(isUsingRememberMe) {
+  return {
+    type: 'SET_IS_USING_REMEMBER_ME',
+    payload: { isUsingRememberMe },
+  };
+}
