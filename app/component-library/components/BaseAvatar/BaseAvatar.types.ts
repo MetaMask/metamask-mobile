aaa7fb0505b735856a02e224f2f89b34051b83b9
João Loureiro <joao.loureiro@consensys.net>
2022-05-17 17:15:05 +0100

    (BaseAvatar) add jsdoc comments to component types

--- app/component-library/components/BaseAvatar/BaseAvatar.types.ts
+++ app/component-library/components/BaseAvatar/BaseAvatar.types.ts
@@ -1,5 +1,8 @@
 import { ViewProps } from 'react-native';
 
+/**
+ * BaseAvatar Avatar sizes
+ */
 export enum BaseAvatarSize {
   Xs = '16',
   Sm = '24',
@@ -8,8 +11,17 @@ export enum BaseAvatarSize {
   Xl = '48',
 }
 
+/**
+ * BaseAvatar component props.
+ */
 export interface BaseAvatarProps extends ViewProps {
+  /**
+   * Enum to select between Avatar sizes.
+   */
   size: BaseAvatarSize;
 }
 
+/**
+ * Style sheet input parameters.
+ */
 export type BaseAvatarStyleSheetVars = Pick<BaseAvatarProps, 'size' | 'style'>;
