49342fbc2cbdddbba6ca16415964dca1f5fc3b7a
João Loureiro <1649425+jpcloureiro@users.noreply.github.com>
2022-05-18 16:01:49 +0100

    [PS UI] BaseAvatar component (#4260)
    * feat: first iteration of BaseAvatar
    
    * add useStyles hook
    
    * add useStyles jsdoc comment. inject theme context
    
    * remove eslint-disable comment
    
    * add Xs avatar dimension
    
    * add markdown doc to base avatar component
    
    * export getStrippedEnum function
    
    * rename markdown doc
    
    * cleanup duplicate types / interfaces
    
    * reorg folder structure
    
    * change enums to strings & remove conversion helper function
    
    * fix tests
    
    * rename stylesheet builder function
    
    * pass in style prop inside stylesheet arrow func
    
    * (BaseAvatar): rename AvatarSize enum
    
    to BaseAvatarSize
    
    * (BaseAvatar) update component story
    
    rename createStylesheet
    
    * (BaseAvatar) add jsdoc comments to component types
    
    * Clean up BaseAvatar
    
    Co-authored-by: Cal Leung <cleun007@gmail.com>

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -8,6 +8,7 @@ function loadStories() {
   require('../app/components/Base/ListItem.stories');
   require('../app/components/Base/Text.stories');
   require('../app/components/Base/Title.stories');
+	require('../app/component-library/components/BaseAvatar/BaseAvatar.stories');
   require('../app/components/UI/ButtonReveal/ButtonReveal.stories');
   require('../app/components/UI/Fox/Fox.stories');
   require('../app/components/UI/StyledButton/StyledButton.stories');
@@ -20,6 +21,7 @@ const stories = [
   '../app/components/Base/ListItem.stories',
   '../app/components/Base/Text.stories',
   '../app/components/Base/Title.stories',
+	'../app/components/Base/BaseAvatar/BaseAvatar.stories',
   '../app/components/UI/ButtonReveal/ButtonReveal.stories',
   '../app/components/UI/Fox/Fox.stories',
   '../app/components/UI/StyledButton/StyledButton.stories',
