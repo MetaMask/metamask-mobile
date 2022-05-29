41bfd6cf6d0a1eb6e087fe443345373a3bc6ae8e
gantunesr <gantunes@uc.cl>
2022-05-29 19:44:03 -0400

    Keep move styles to new file

--- app/components/UI/SettingsButtonSection/index.tsx
+++ app/components/UI/SettingsButtonSection/index.tsx
@@ -1,9 +1,9 @@
 import React, { ReactNode, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import StyledButton from '../StyledButton';
 import ActionModal from '../ActionModal';
-import { fontStyles } from '../../../styles/common';
 import { mockTheme, useAppThemeFromContext } from '../../../util/theme';
+import createStyles from './styles';
 
 interface ISettingsButtonSectionProps {
   sectionTitle: string;
@@ -18,51 +18,6 @@ interface ISettingsButtonSectionProps {
   onPress?: () => void;
   modalOnConfirm?: () => void;
 }
-
-const createStyles = (colors: any) =>
-  StyleSheet.create({
-    setting: {
-      marginTop: 50,
-    },
-    title: {
-      ...fontStyles.normal,
-      color: colors.text.default,
-      fontSize: 20,
-      lineHeight: 20,
-      paddingTop: 4,
-      marginTop: -4,
-    },
-    desc: {
-      ...fontStyles.normal,
-      color: colors.text.alternative,
-      fontSize: 14,
-      lineHeight: 20,
-      marginTop: 12,
-    },
-    confirmButton: {
-      marginTop: 18,
-    },
-    modalView: {
-      alignItems: 'center',
-      flex: 1,
-      flexDirection: 'column',
-      justifyContent: 'center',
-      padding: 20,
-    },
-    modalTitle: {
-      ...fontStyles.bold,
-      fontSize: 22,
-      textAlign: 'center',
-      marginBottom: 20,
-      color: colors.text.default,
-    },
-    modalText: {
-      ...fontStyles.normal,
-      fontSize: 18,
-      textAlign: 'center',
-      color: colors.text.default,
-    },
-  });
 
 const SettingsButtonSection = ({
   sectionTitle,
