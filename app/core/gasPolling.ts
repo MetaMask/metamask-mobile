6b52a956d034aa0b11c02e742fe92c4ac10257f1
Sylva Elendu <iamsylvaelendu@gmail.com>
2022-05-31 12:59:28 +0300

    refactor:sendflow

--- app/core/gasPolling.ts
+++ app/core/gasPolling.ts
@@ -5,7 +5,10 @@ import AppConstants from './AppConstants';
 import { GAS_ESTIMATE_TYPES } from '@metamask/controllers';
 
 import { fromWei } from '../util/number';
-import { parseTransactionEIP1559 } from '../util/transactions';
+import {
+  parseTransactionEIP1559,
+  parseTransactionLegacy,
+} from '../util/transactions';
 
 export const startGasPolling = async (token: undefined) => {
   const { GasFeeController }: any = Engine.context;
@@ -20,11 +23,7 @@ export const stopGasPolling = (token: string) => {
   return GasFeeController.stopPolling(token);
 };
 
-export const getGasFeeEstimates = () => {
-  const [gasEstimateTypeChange, updateGasEstimateTypeChange] = useState('');
-  const [stopUpdateGas, updateStopUpdateGas] = useState(false);
-  const [advancedGasInserted, updateAdvancedGasInserted] = useState(false);
-
+export const useDataStore = () => {
   const gasFeeEstimates = useSelector(
     (state: any) =>
       state.engine.backgroundState.GasFeeController.gasFeeEstimates,
@@ -50,111 +49,151 @@ export const getGasFeeEstimates = () => {
     (state: any) =>
       state.engine.backgroundState.CurrencyRateController.nativeCurrency,
   );
+  return {
+    gasFeeEstimates,
+    transactionState,
+    gasEstimateType,
+    contractExchangeRates,
+    conversionRate,
+    currentCurrency,
+    nativeCurrency,
+  };
+};
+
+interface GetEIP1559TransactionDataProps {
+  transactionGas: string;
+  gas: {
+    maxWaitTimeEstimate: number;
+    minWaitTimeEstimate: number;
+    suggestedMaxFeePerGas: string;
+    suggestedMaxPriorityFeePerGas: string;
+  };
+  selectedOption: string;
+  gasFeeEstimates: any;
+  transactionState: any;
+  contractExchangeRates: any;
+  conversionRate: number;
+  currentCurrency: 'usd' | string;
+  nativeCurrency: string;
+}
+
+interface LegacyProps {
+  contractExchangeRates: any;
+  conversionRate: number;
+  currentCurrency: 'usd' | string;
+  transactionState: any;
+  ticker: 'ETH';
+  suggestedGasPrice: any;
+  suggestedGasLimit: any;
+}
+
+export const getEIP1559TransactionData = ({
+  transactionGas,
+  gas,
+  selectedOption,
+  gasFeeEstimates,
+  transactionState,
+  contractExchangeRates,
+  conversionRate,
+  currentCurrency,
+  nativeCurrency,
+}: GetEIP1559TransactionDataProps) => {
+  const suggestedGasLimit = fromWei(transactionGas, 'wei');
+  const parsedTransactionEIP1559 = parseTransactionEIP1559({
+    contractExchangeRates,
+    conversionRate,
+    currentCurrency,
+    nativeCurrency,
+    transactionState,
+    gasFeeEstimates,
+    swapsParams: undefined,
+    selectedGasFee: {
+      ...gas,
+      suggestedGasLimit,
+      selectedOption,
+      estimatedBaseFee: gasFeeEstimates.estimatedBaseFee,
+    },
+  });
+
+  return parsedTransactionEIP1559;
+};
+
+export const getLegacyTransactionData = ({
+  contractExchangeRates,
+  conversionRate,
+  currentCurrency,
+  transactionState,
+  ticker,
+  suggestedGasPrice,
+  suggestedGasLimit,
+}: LegacyProps) => {
+  const parsedTransationData = parseTransactionLegacy({
+    contractExchangeRates,
+    conversionRate,
+    currentCurrency,
+    transactionState,
+    ticker,
+    selectedGasFee: { suggestedGasLimit, suggestedGasPrice },
+  });
+  return parsedTransationData;
+};
+
+export const useGasFeeEstimates = () => {
+  const [gasEstimateTypeChange, updateGasEstimateTypeChange] = useState('');
+
+  const {
+    gasFeeEstimates,
+    transactionState,
+    gasEstimateType,
+    contractExchangeRates,
+    conversionRate,
+    currentCurrency,
+    nativeCurrency,
+  } = useDataStore();
 
   useEffect(() => {
     if (gasEstimateType !== gasEstimateTypeChange) {
       updateGasEstimateTypeChange(gasEstimateType);
     }
-  }, [gasEstimateType]);
-
-  enum SelectedOptions {
-    low = 'low',
-    medium = 'medium',
-    high = 'high',
-  }
-
-  interface GasFee {
-    maxWaitTimeEstimate: number;
-    minWaitTimeEstimate: number;
-    selectedOption: SelectedOptions;
-    suggestedGasLimit: number;
-    suggestedMaxFeePerGas: number;
-    suggestedMaxPriorityFeePerGas: number;
-  }
-
-  const parseTransactionDataEIP1559 = (gasFee: GasFee) => {
-    const parsedTransactionEIP1559 = parseTransactionEIP1559({
-      contractExchangeRates,
-      conversionRate,
-      currentCurrency,
-      nativeCurrency,
-      transactionState,
-      gasFeeEstimates,
-      swapsParams: undefined,
-      selectedGasFee: {
-        ...gasFee,
-        estimatedBaseFee: gasFeeEstimates.estimatedBaseFee,
-      },
-    });
-    const { transaction } = transactionState;
-
-    // TODO: rewrite this
-    // parsedTransactionEIP1559.error = this.validateAmount({
-    //   transaction,
-    //   total: parsedTransactionEIP1559.totalMaxHex,
-    // });
-
-    return parsedTransactionEIP1559;
-  };
+  }, [gasEstimateType, gasEstimateTypeChange]);
 
   const gasSelected = gasEstimateTypeChange
     ? AppConstants.GAS_OPTIONS.MEDIUM
     : AppConstants.GAS_OPTIONS.MEDIUM;
 
-  const gasSelectedTemp = gasEstimateTypeChange
-    ? AppConstants.GAS_OPTIONS.MEDIUM
-    : AppConstants.GAS_OPTIONS.MEDIUM;
+  const {
+    transaction: { gas: transactionGas },
+  } = transactionState;
 
-  if ((!stopUpdateGas && !advancedGasInserted) || gasEstimateTypeChange) {
+  if (gasEstimateTypeChange) {
     if (gasEstimateType === GAS_ESTIMATE_TYPES.FEE_MARKET) {
-      const {
-        transaction: { gas },
-      } = transactionState;
-      const suggestedGasLimit = fromWei(gas, 'wei');
-      const EIP1559TransactionData = parseTransactionDataEIP1559({
-        ...gasFeeEstimates[gasSelected],
-        suggestedGasLimit,
+      const EIP1559TransactionData = getEIP1559TransactionData({
+        transactionGas,
+        gas: gasFeeEstimates[gasSelected],
         selectedOption: gasSelected,
+        gasFeeEstimates,
+        transactionState,
+        contractExchangeRates,
+        conversionRate,
+        currentCurrency,
+        nativeCurrency,
       });
-
-      let EIP1559TransactionDataTemp;
-      if (gasSelected === gasSelectedTemp) {
-        EIP1559TransactionDataTemp = EIP1559TransactionData;
-      } else {
-        EIP1559TransactionDataTemp = parseTransactionDataEIP1559({
-          ...gasFeeEstimates[gasSelectedTemp],
-          suggestedGasLimit,
-          selectedOption: gasSelectedTemp,
-        });
-      }
-
-      const {
-        renderableTotalMinNative,
-        renderableTotalMinConversion,
-        renderableTotalMaxNative,
-        renderableGasFeeMinNative,
-        renderableGasFeeMinConversion,
-        renderableGasFeeMaxNative,
-        renderableGasFeeMaxConversion,
-        timeEstimate,
-        timeEstimateColor,
-        timeEstimateId,
-        error,
-      } = EIP1559TransactionData;
-
-      return {
-        renderableTotalMinNative,
-        renderableTotalMinConversion,
-        renderableTotalMaxNative,
-        renderableGasFeeMinNative,
-        renderableGasFeeMinConversion,
-        renderableGasFeeMaxNative,
-        renderableGasFeeMaxConversion,
-        timeEstimate,
-        timeEstimateColor,
-        timeEstimateId,
-        error,
-      };
+      return EIP1559TransactionData;
+    } else if (gasEstimateType !== GAS_ESTIMATE_TYPES.NONE) {
+      const suggestedGasLimit = fromWei(transactionGas, 'wei');
+      const LegacyTransactionData = getLegacyTransactionData({
+        contractExchangeRates,
+        conversionRate,
+        currentCurrency,
+        transactionState,
+        ticker: 'ETH',
+        suggestedGasPrice:
+          gasEstimateType === GAS_ESTIMATE_TYPES.LEGACY
+            ? gasFeeEstimates[gasSelected]
+            : gasFeeEstimates.gasPrice,
+        suggestedGasLimit,
+      });
+      return LegacyTransactionData;
     }
   }
 
