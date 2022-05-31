a8cb30b4e721cc4e28db4697fc2d5bf2bf2ed2dc
Suhail Ranger <ranger@sardine.ai>
2022-05-31 16:16:55 +0530

    yarn format

--- app/components/UI/FiatOrders/PaymentMethodSelector/sardine.js
+++ app/components/UI/FiatOrders/PaymentMethodSelector/sardine.js
@@ -12,62 +12,68 @@ import { NETWORKS_CHAIN_ID } from '../../../../constants/on-ramp';
 import { useAssetFromTheme } from '../../../../util/theme';
 
 const styles = StyleSheet.create({
-	title: {
-		flex: 1,
-		flexWrap: 'wrap',
-	},
-	logo: {
-		marginVertical: 5,
-		aspectRatio: 95 / 25,
-		width: Device.isIphone5() ? 80 : 95,
-		height: Device.isIphone5() ? 25 : 35,
-		marginLeft: 5,
-	},
+  title: {
+    flex: 1,
+    flexWrap: 'wrap',
+  },
+  logo: {
+    marginVertical: 5,
+    aspectRatio: 95 / 25,
+    width: Device.isIphone5() ? 80 : 95,
+    height: Device.isIphone5() ? 25 : 35,
+    marginLeft: 5,
+  },
 });
 
 // eslint-disable-next-line import/no-commonjs
 const SardineLogoLightIcon = require('../../../../images/SardineLogo.svg');
 const SardineLogoDarkIcon = require('../../../../images/SardineLogoDark.svg');
 const SardineLogo = () => {
-	const sardinePayLogoIcon = useAssetFromTheme(SardineLogoLightIcon, SardineLogoDarkIcon);
-	return <Image source={sardinePayLogoIcon} style={styles.logo} />;
-} 
+  const sardinePayLogoIcon = useAssetFromTheme(
+    SardineLogoLightIcon,
+    SardineLogoDarkIcon,
+  );
+  return <Image source={sardinePayLogoIcon} style={styles.logo} />;
+};
 
 const hasStablecoins = (chainId) =>
-	[NETWORKS_CHAIN_ID.MAINNET, NETWORKS_CHAIN_ID.BSC, NETWORKS_CHAIN_ID.POLYGON, NETWORKS_CHAIN_ID.CELO].includes(
-		chainId
-	);
+  [
+    NETWORKS_CHAIN_ID.MAINNET,
+    NETWORKS_CHAIN_ID.BSC,
+    NETWORKS_CHAIN_ID.POLYGON,
+    NETWORKS_CHAIN_ID.CELO,
+  ].includes(chainId);
 
 const SardinePaymentMethod = ({ onPress, ticker, chainId }) => (
-	<PaymentMethod onPress={onPress}>
-		<PaymentMethod.Content>
-			<PaymentMethod.Details>
-				<PaymentMethod.Title>
-					<Title style={styles.title}>
-						{hasStablecoins(chainId)
-							? strings('fiat_on_ramp.buy_ticker_stablecoins', { ticker })
-							: strings('fiat_on_ramp.buy_ticker', { ticker })}
-					</Title>
-					<SardineLogo />
-				</PaymentMethod.Title>
-				<Text bold>{strings('fiat_on_ramp.sardine_tagline')}</Text>
-				<Text>{strings('fiat_on_ramp.sardine_features')}</Text>
+  <PaymentMethod onPress={onPress}>
+    <PaymentMethod.Content>
+      <PaymentMethod.Details>
+        <PaymentMethod.Title>
+          <Title style={styles.title}>
+            {hasStablecoins(chainId)
+              ? strings('fiat_on_ramp.buy_ticker_stablecoins', { ticker })
+              : strings('fiat_on_ramp.buy_ticker', { ticker })}
+          </Title>
+          <SardineLogo />
+        </PaymentMethod.Title>
+        <Text bold>{strings('fiat_on_ramp.sardine_tagline')}</Text>
+        <Text>{strings('fiat_on_ramp.sardine_features')}</Text>
 
-				<PaymentMethod.InfoIconLine>
-					<Text small>{strings('fiat_on_ramp.options_fees_vary')}</Text>
-				</PaymentMethod.InfoIconLine>
-			</PaymentMethod.Details>
-		</PaymentMethod.Content>
-	</PaymentMethod>
+        <PaymentMethod.InfoIconLine>
+          <Text small>{strings('fiat_on_ramp.options_fees_vary')}</Text>
+        </PaymentMethod.InfoIconLine>
+      </PaymentMethod.Details>
+    </PaymentMethod.Content>
+  </PaymentMethod>
 );
 
 SardinePaymentMethod.propTypes = {
-	onPress: PropTypes.func,
-	ticker: PropTypes.string,
-	chainId: PropTypes.string,
+  onPress: PropTypes.func,
+  ticker: PropTypes.string,
+  chainId: PropTypes.string,
 };
 SardinePaymentMethod.defaultProps = {
-	onPress: undefined,
+  onPress: undefined,
 };
 
 export default SardinePaymentMethod;
--- app/components/UI/FiatOrders/orderProcessor/sardine.js
+++ app/components/UI/FiatOrders/orderProcessor/sardine.js
@@ -4,18 +4,20 @@ import qs from 'query-string';
 import AppConstants from '../../../../core/AppConstants';
 import Logger from '../../../../util/Logger';
 import {
-	FIAT_ORDER_PROVIDERS,
-	FIAT_ORDER_STATES,
-	NETWORKS_CHAIN_ID,
-	SARDINE_NETWORK_PARAMETERS,
+  FIAT_ORDER_PROVIDERS,
+  FIAT_ORDER_STATES,
+  NETWORKS_CHAIN_ID,
+  SARDINE_NETWORK_PARAMETERS,
 } from '../../../../constants/on-ramp';
 
 //* env vars
 
 const SARDINE_API_KEY_STAGING = process.env.SARDINE_API_KEY_STAGING;
-const SARDINE_API_KEY_SECRET_STAGING = process.env.SARDINE_API_KEY_SECRET_STAGING;
+const SARDINE_API_KEY_SECRET_STAGING =
+  process.env.SARDINE_API_KEY_SECRET_STAGING;
 const SARDINE_API_KEY_PRODUCTION = process.env.SARDINE_API_KEY_PRODUCTION;
-const SARDINE_API_KEY_SECRET_PRODUCTION = process.env.SARDINE_API_KEY_SECRET_PRODUCTION;
+const SARDINE_API_KEY_SECRET_PRODUCTION =
+  process.env.SARDINE_API_KEY_SECRET_PRODUCTION;
 
 //* typedefs
 
@@ -78,52 +80,68 @@ const SARDINE_API_KEY_SECRET_PRODUCTION = process.env.SARDINE_API_KEY_SECRET_PRO
 
 //* Functions
 const SARDINE_ALLOWED_NETWORKS = [
-	NETWORKS_CHAIN_ID.MAINNET,
-	NETWORKS_CHAIN_ID.BSC,
-	NETWORKS_CHAIN_ID.POLYGON,
-	NETWORKS_CHAIN_ID.AVAXCCHAIN,
-	NETWORKS_CHAIN_ID.CELO,
-	NETWORKS_CHAIN_ID.FANTOM,
+  NETWORKS_CHAIN_ID.MAINNET,
+  NETWORKS_CHAIN_ID.BSC,
+  NETWORKS_CHAIN_ID.POLYGON,
+  NETWORKS_CHAIN_ID.AVAXCCHAIN,
+  NETWORKS_CHAIN_ID.CELO,
+  NETWORKS_CHAIN_ID.FANTOM,
 ];
-export const isSardineAllowedToBuy = (chainId) => SARDINE_ALLOWED_NETWORKS.includes(chainId);
+export const isSardineAllowedToBuy = (chainId) =>
+  SARDINE_ALLOWED_NETWORKS.includes(chainId);
 
 //* Constants
 
-const { SARDINE_URL, SARDINE_URL_STAGING, SARDINE_API_URL_STAGING, SARDINE_API_URL_PRODUCTION, SARDINE_REDIRECT_URL } =
-	AppConstants.FIAT_ORDERS;
+const {
+  SARDINE_URL,
+  SARDINE_URL_STAGING,
+  SARDINE_API_URL_STAGING,
+  SARDINE_API_URL_PRODUCTION,
+  SARDINE_REDIRECT_URL,
+} = AppConstants.FIAT_ORDERS;
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
-const SARDINE_API_BASE_URL = `${isDevelopment ? SARDINE_API_URL_STAGING : SARDINE_API_URL_PRODUCTION}`;
-const SARDINE_API_KEY = isDevelopment ? SARDINE_API_KEY_STAGING : SARDINE_API_KEY_PRODUCTION;
-const SARDINE_API_KEY_SECRET = isDevelopment ? SARDINE_API_KEY_SECRET_STAGING : SARDINE_API_KEY_SECRET_PRODUCTION;
+const SARDINE_API_BASE_URL = `${
+  isDevelopment ? SARDINE_API_URL_STAGING : SARDINE_API_URL_PRODUCTION
+}`;
+const SARDINE_API_KEY = isDevelopment
+  ? SARDINE_API_KEY_STAGING
+  : SARDINE_API_KEY_PRODUCTION;
+const SARDINE_API_KEY_SECRET = isDevelopment
+  ? SARDINE_API_KEY_SECRET_STAGING
+  : SARDINE_API_KEY_SECRET_PRODUCTION;
 
 /**
  * https://integrate.sardine.com/69a2474c8d8d40daa04bd5bbe804fb6d?v=48a0c9fd98854078a4eaf5ec9a0a4f65
  * @enum {string}
  */
 const SARDINE_ORDER_STATES = {
-	AWAITING_PAYMENT_FROM_USER: 'AWAITING_PAYMENT_FROM_USER',
-	PAYMENT_DONE_MARKED_BY_USER: 'PAYMENT_DONE_MARKED_BY_USER',
-	PROCESSING: 'PROCESSING',
-	PENDING_DELIVERY_FROM_SARDINE: 'PENDING_DELIVERY_FROM_SARDINE',
-	COMPLETED: 'COMPLETED',
-	EXPIRED: 'EXPIRED',
-	FAILED: 'FAILED',
-	CANCELLED: 'CANCELLED',
+  AWAITING_PAYMENT_FROM_USER: 'AWAITING_PAYMENT_FROM_USER',
+  PAYMENT_DONE_MARKED_BY_USER: 'PAYMENT_DONE_MARKED_BY_USER',
+  PROCESSING: 'PROCESSING',
+  PENDING_DELIVERY_FROM_SARDINE: 'PENDING_DELIVERY_FROM_SARDINE',
+  COMPLETED: 'COMPLETED',
+  EXPIRED: 'EXPIRED',
+  FAILED: 'FAILED',
+  CANCELLED: 'CANCELLED',
 };
 
 //* API
 
 const sardineApi = axios.create({
-	baseURL: SARDINE_API_BASE_URL,
+  baseURL: SARDINE_API_BASE_URL,
 });
 
 // const getPartnerStatus = () => sardineApi.get(`partners/${SARDINE_API_KEY}`);
 const getClientToken = (orderId) =>
-	sardineApi.get(`auth/client-token`, { params: { partnerAPISecret: SARDINE_API_KEY_SECRET } });
+  sardineApi.get(`auth/client-token`, {
+    params: { partnerAPISecret: SARDINE_API_KEY_SECRET },
+  });
 const getOrderStatus = (orderId) =>
-	sardineApi.get(`trade/${orderId}`, { params: { partnerAPISecret: SARDINE_API_KEY_SECRET } });
+  sardineApi.get(`trade/${orderId}`, {
+    params: { partnerAPISecret: SARDINE_API_KEY_SECRET },
+  });
 
 //* Helpers
 
@@ -132,25 +150,25 @@ const getOrderStatus = (orderId) =>
  * @param {SARDINE_ORDER_STATES} sardineOrderState
  */
 const sardineOrderStateToFiatOrderState = (sardineOrderState) => {
-	switch (sardineOrderState) {
-		case SARDINE_ORDER_STATES.COMPLETED: {
-			return FIAT_ORDER_STATES.COMPLETED;
-		}
-		case SARDINE_ORDER_STATES.EXPIRED:
-		case SARDINE_ORDER_STATES.FAILED: {
-			return FIAT_ORDER_STATES.FAILED;
-		}
-		case SARDINE_ORDER_STATES.CANCELLED: {
-			return FIAT_ORDER_STATES.CANCELLED;
-		}
-		case SARDINE_ORDER_STATES.AWAITING_PAYMENT_FROM_USER:
-		case SARDINE_ORDER_STATES.PAYMENT_DONE_MARKED_BY_USER:
-		case SARDINE_ORDER_STATES.PROCESSING:
-		case SARDINE_ORDER_STATES.PENDING_DELIVERY_FROM_SARDINE:
-		default: {
-			return FIAT_ORDER_STATES.PENDING;
-		}
-	}
+  switch (sardineOrderState) {
+    case SARDINE_ORDER_STATES.COMPLETED: {
+      return FIAT_ORDER_STATES.COMPLETED;
+    }
+    case SARDINE_ORDER_STATES.EXPIRED:
+    case SARDINE_ORDER_STATES.FAILED: {
+      return FIAT_ORDER_STATES.FAILED;
+    }
+    case SARDINE_ORDER_STATES.CANCELLED: {
+      return FIAT_ORDER_STATES.CANCELLED;
+    }
+    case SARDINE_ORDER_STATES.AWAITING_PAYMENT_FROM_USER:
+    case SARDINE_ORDER_STATES.PAYMENT_DONE_MARKED_BY_USER:
+    case SARDINE_ORDER_STATES.PROCESSING:
+    case SARDINE_ORDER_STATES.PENDING_DELIVERY_FROM_SARDINE:
+    default: {
+      return FIAT_ORDER_STATES.PENDING;
+    }
+  }
 };
 
 /**
@@ -159,20 +177,20 @@ const sardineOrderStateToFiatOrderState = (sardineOrderState) => {
  * @returns {FiatOrder} Fiat order object to store in the state
  */
 const sardineOrderToFiatOrder = (sardineOrder) => ({
-	id: sardineOrder.order_id,
-	provider: FIAT_ORDER_PROVIDERS.SARDINE,
-	createdAt: new Date(sardineOrder.created_at).getTime(),
-	amount: sardineOrder.fiat_price,
-	fee: sardineOrder.network_fee,
-	cryptoAmount: sardineOrder.asset_price,
-	cryptoFee: sardineOrder.transaction_fee,
-	currency: sardineOrder.fiat_type,
-	cryptocurrency: sardineOrder.asset_type,
-	amountInUSD: sardineOrder.asset_total,
-	state: sardineOrderStateToFiatOrderState(sardineOrder.status),
-	account: sardineOrder.wallet_id,
-	txHash: sardineOrder.tx_hash || null,
-	data: sardineOrder,
+  id: sardineOrder.order_id,
+  provider: FIAT_ORDER_PROVIDERS.SARDINE,
+  createdAt: new Date(sardineOrder.created_at).getTime(),
+  amount: sardineOrder.fiat_price,
+  fee: sardineOrder.network_fee,
+  cryptoAmount: sardineOrder.asset_price,
+  cryptoFee: sardineOrder.transaction_fee,
+  currency: sardineOrder.fiat_type,
+  cryptocurrency: sardineOrder.asset_type,
+  amountInUSD: sardineOrder.asset_total,
+  state: sardineOrderStateToFiatOrderState(sardineOrder.status),
+  account: sardineOrder.wallet_id,
+  txHash: sardineOrder.tx_hash || null,
+  data: sardineOrder,
 });
 
 /**
@@ -180,18 +198,19 @@ const sardineOrderToFiatOrder = (sardineOrder) => ({
  * @param {SardineRedirectOrder} sardineRedirectOrder Sardine order object
  * @returns {FiatOrder} Fiat order object to store in the state
  */
- export const sardineCallbackOrderToFiatOrder = (sardineRedirectOrder) => ({
-	id: sardineRedirectOrder.trade_id,
-	provider: FIAT_ORDER_PROVIDERS.SARDINE,
-	createdAt: Date.now(),
-	amount: Number(sardineRedirectOrder.total),
-	fee: Number(sardineRedirectOrder.network_fee),
-	currency: sardineRedirectOrder.asset_type || sardineRedirectOrder.fiat_type,
-	cryptoAmount: sardineRedirectOrder.price,
-	cryptocurrency: sardineRedirectOrder.fiat_type || sardineRedirectOrder.asset_type,
-	state: SARDINE_ORDER_STATES.PROCESSING,
-	account: "",
-	data: sardineRedirectOrder,
+export const sardineCallbackOrderToFiatOrder = (sardineRedirectOrder) => ({
+  id: sardineRedirectOrder.trade_id,
+  provider: FIAT_ORDER_PROVIDERS.SARDINE,
+  createdAt: Date.now(),
+  amount: Number(sardineRedirectOrder.total),
+  fee: Number(sardineRedirectOrder.network_fee),
+  currency: sardineRedirectOrder.asset_type || sardineRedirectOrder.fiat_type,
+  cryptoAmount: sardineRedirectOrder.price,
+  cryptocurrency:
+    sardineRedirectOrder.fiat_type || sardineRedirectOrder.asset_type,
+  state: SARDINE_ORDER_STATES.PROCESSING,
+  account: '',
+  data: sardineRedirectOrder,
 });
 
 //* Handlers
@@ -206,11 +225,11 @@ const sardineOrderToFiatOrder = (sardineOrder) => ({
  * @returns {FiatOrder}
  */
 export const handleSardineRedirect = (url, network) => {
-	/** @type {SardineRedirectOrder} */
-	console.log(url);
-	const data = qs.parse(url.split(SARDINE_REDIRECT_URL)[1]);
-	const order = { ...sardineCallbackOrderToFiatOrder(data), network };
-	return order;
+  /** @type {SardineRedirectOrder} */
+  console.log(url);
+  const data = qs.parse(url.split(SARDINE_REDIRECT_URL)[1]);
+  const order = { ...sardineCallbackOrderToFiatOrder(data), network };
+  return order;
 };
 
 /**
@@ -220,39 +239,45 @@ export const handleSardineRedirect = (url, network) => {
  * @returns {FiatOrder} Fiat order to update in the state
  */
 export async function processSardineOrder(order) {
-	try {
-		const {
-			data: { response },
-		} = await getOrderStatus(order.id);
-		if (!response) {
-			throw new Error('Payment Request Failed: empty sardine response');
-		}
-		return {
-			...order,
-			...sardineOrderToFiatOrder(response),
-		};
-	} catch (error) {
-		Logger.error(error, { message: 'FiatOrders::SardineProcessor error while processing order', order });
-		return order;
-	}
+  try {
+    const {
+      data: { response },
+    } = await getOrderStatus(order.id);
+    if (!response) {
+      throw new Error('Payment Request Failed: empty sardine response');
+    }
+    return {
+      ...order,
+      ...sardineOrderToFiatOrder(response),
+    };
+  } catch (error) {
+    Logger.error(error, {
+      message: 'FiatOrders::SardineProcessor error while processing order',
+      order,
+    });
+    return order;
+  }
 }
 
 //* Hooks
 
 export const useSardineFlowURL = (address, chainId) => {
-	const params = useMemo(() => {
-		const selectedChainId = isSardineAllowedToBuy(chainId) ? chainId : NETWORKS_CHAIN_ID.MAINNET;
-		const [network, defaultCryptoCurrency, cryptoCurrencyList] = SARDINE_NETWORK_PARAMETERS[selectedChainId];
-		return qs.stringify({
-			apiKey: SARDINE_API_KEY,
-			defaultCryptoCurrency,
-			cryptoCurrencyList,
-			network,
-			themeColor: '037dd6',
-			walletAddress: address,
-			source: 'app',
-			redirectURL: SARDINE_REDIRECT_URL,
-		});
-	}, [address, chainId]);
-	return `${isDevelopment ? SARDINE_URL_STAGING : SARDINE_URL}?${params}`;
+  const params = useMemo(() => {
+    const selectedChainId = isSardineAllowedToBuy(chainId)
+      ? chainId
+      : NETWORKS_CHAIN_ID.MAINNET;
+    const [network, defaultCryptoCurrency, cryptoCurrencyList] =
+      SARDINE_NETWORK_PARAMETERS[selectedChainId];
+    return qs.stringify({
+      apiKey: SARDINE_API_KEY,
+      defaultCryptoCurrency,
+      cryptoCurrencyList,
+      network,
+      themeColor: '037dd6',
+      walletAddress: address,
+      source: 'app',
+      redirectURL: SARDINE_REDIRECT_URL,
+    });
+  }, [address, chainId]);
+  return `${isDevelopment ? SARDINE_URL_STAGING : SARDINE_URL}?${params}`;
 };
