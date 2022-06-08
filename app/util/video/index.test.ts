c384689f4d54b38f3910fa72447ab2e735aaf207
Ricky Miller <ricky.miller@gmail.com>
2022-06-08 10:03:51 -0400

    update

--- app/util/video/index.test.ts
+++ app/util/video/index.test.ts
@@ -2,9 +2,8 @@ import { getSubtitleUri } from '.';
 import { getLanguages } from '../../../locales/i18n';
 
 describe('getSubtitleUri', () => {
-  const languages = getLanguages();
-  const keys = Object.keys(languages);
-  keys.forEach((key) => {
+  const languageKeys = Object.keys(getLanguages());
+  languageKeys.forEach((key) => {
     it(`should return ${key} for ${key}`, () => {
       expect(getSubtitleUri(key)).toStrictEqual(
         `https://github.com/MetaMask/metamask-mobile/blob/main/app/videos/subtitles/secretPhrase/subtitles-${key}.vtt?raw=true`,
