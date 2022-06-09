b99b8eae9e341caf844821d47a693989d10c7229
Sylva Elendu <iamsylvaelendu@gmail.com>
2022-06-09 13:22:23 +0300

    rebased to main

--- app/components/UI/Swaps/components/TokenIcon.js
+++ app/components/UI/Swaps/components/TokenIcon.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, View } from 'react-native';
 
