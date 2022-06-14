1b7e109978bac6cb39748eea0e318a960e31f6a3
Ricky Miller <ricky.miller@gmail.com>
2022-06-14 15:39:30 -0400

    allow for user to customise apk location

--- scripts/browserstack-upload.js
+++ scripts/browserstack-upload.js
@@ -8,6 +8,7 @@ const file = './android/app/build/outputs/apk/debug/app-debug.apk';
 
 const BROWSERSTACK_USERNAME = process.env.MM_BROWSERSTACK_USERNAME;
 const BROWSERSTACK_ACCESS_KEY = process.env.MM_BROWSERSTACK_ACCESS_KEY;
+const APK_LOCATION = process.env.MM_APK_LOCATION;
 
 if (!BROWSERSTACK_USERNAME || !BROWSERSTACK_ACCESS_KEY) {
   console.log(
@@ -19,7 +20,9 @@ if (!BROWSERSTACK_USERNAME || !BROWSERSTACK_ACCESS_KEY) {
 async function upload() {
   await new Promise((resolve, reject) => {
     exec(
-      `curl -u "${BROWSERSTACK_USERNAME}:${BROWSERSTACK_ACCESS_KEY}" -X POST "${endpoint}" -F "file=@${file}"`,
+      `curl -u "${BROWSERSTACK_USERNAME}:${BROWSERSTACK_ACCESS_KEY}" -X POST "${endpoint}" -F "file=@${
+        file || APK_LOCATION
+      }"`,
       (error, stdout, stderr) => {
         if (error) reject(new Error(error));
         console.log({ stdout });
