a12ad2c4ffc7e8b2194bbc148e598e229e5d8f66
Chris Wilcox <chrisleewilcox@outlook.com>
2022-06-17 12:53:42 -0700

    add proper formatting with Prettier

--- app/core/PreventScreenshot.js
+++ app/core/PreventScreenshot.js
@@ -1,8 +1,8 @@
-import { NativeModules, Platform } from 'react-native';
-
-const isAndroid = Platform.OS === 'android';
-
 export default {
-  forbid:()=>{ return true},
-	allow:()=>{ return true}
+  forbid: () => {
+    true;
+  },
+  allow: () => {
+    true;
+  },
 };
