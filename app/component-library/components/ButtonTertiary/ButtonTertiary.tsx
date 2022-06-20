3fff09724a4e4f5e5cc8609c3910c17bbc0d334d
Cal Leung <cleun007@gmail.com>
2022-06-20 13:35:04 -0700

    Add pressed state

--- app/component-library/components/ButtonTertiary/ButtonTertiary.tsx
+++ app/component-library/components/ButtonTertiary/ButtonTertiary.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
+import { GestureResponderEvent } from 'react-native';
 import { useStyles } from '../../hooks';
 import BaseButton from '../BaseButton';
 import styleSheet from './ButtonTertiary.styles';
@@ -10,24 +11,55 @@ import {
 
 const ButtonTertiary = ({
   style,
+  onPressIn,
+  onPressOut,
   variant,
   ...props
 }: ButtonTertiaryProps): JSX.Element => {
+  const [pressed, setPressed] = useState(false);
   const { styles, theme } = useStyles(styleSheet, { style, variant });
   const labelColor = useMemo(() => {
     let color: string;
     switch (variant) {
       case ButtonTertiaryVariant.Normal:
-        color = theme.colors.primary.default;
+        color = pressed
+          ? theme.colors.primary.alternative
+          : theme.colors.primary.default;
         break;
       case ButtonTertiaryVariant.Danger:
-        color = theme.colors.error.default;
+        color = pressed
+          ? theme.colors.error.alternative
+          : theme.colors.error.default;
         break;
     }
     return color;
-  }, [theme, variant]);
+  }, [theme, variant, pressed]);
 
-  return <BaseButton {...props} style={styles.base} labelColor={labelColor} />;
+  const triggerOnPressedIn = useCallback(
+    (e: GestureResponderEvent) => {
+      setPressed(true);
+      onPressIn?.(e);
+    },
+    [setPressed, onPressIn],
+  );
+
+  const triggerOnPressedOut = useCallback(
+    (e: GestureResponderEvent) => {
+      setPressed(false);
+      onPressOut?.(e);
+    },
+    [setPressed, onPressOut],
+  );
+
+  return (
+    <BaseButton
+      style={styles.base}
+      labelColor={labelColor}
+      onPressIn={triggerOnPressedIn}
+      onPressOut={triggerOnPressedOut}
+      {...props}
+    />
+  );
 };
 
 export default ButtonTertiary;
