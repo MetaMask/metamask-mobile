3c6535693f9a9fe1ec162c05c40f9a8b23b5c7dd
Cal Leung <cleun007@gmail.com>
2022-06-21 14:04:46 -0700

    Update JS odc

--- app/actions/recents/index.js
+++ app/actions/recents/index.js
@@ -3,7 +3,7 @@ import { safeToChecksumAddress } from '../../util/address';
 /**
  * Action creator for adding account addresses to recents list.
  *
- * @param {string} recent Account address in mix case format. Will be converted to checksum before being applied to the reducer.
+ * @param {string} recent Account address in any case format. Will be converted to checksum before being applied to the reducer.
  * @returns Action object that is recognized and handled by recents reducer.
  */
 export default function addRecent(recent) {
