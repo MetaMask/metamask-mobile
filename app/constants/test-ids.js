aa793587ac92067d5049a8a76b8771446a415e08
Curtis David <Curtis.David7@gmail.com>
2022-06-23 16:09:53 -0400

    [Detox] Add coverage for whats new modal (#4581)
    * add test step to dismiss what's new modal.
    
    * delete wallet fix

--- app/constants/test-ids.js
+++ app/constants/test-ids.js
@@ -61,3 +61,7 @@ export const RPC_VIEW_CONTAINER_ID = 'new-rpc-screen';
 export const NEW_NETWORK_ADDED_CLOSE_BUTTON_ID = 'close-network-button';
 export const NEW_NETWORK_ADDED_SWITCH_TO_NETWORK_BUTTON_ID =
   'switch-to-network-button';
+
+export const WHATS_NEW_MODAL_CONTAINER_ID = 'whats-new-modal-container';
+export const WHATS_NEW_MODAL_CLOSE_BUTTON_ID = 'whats-new-modal-close-button';
+export const WHATS_NEW_MODAL_GOT_IT_BUTTON_ID = 'whats-new-modal-got-it-button';
