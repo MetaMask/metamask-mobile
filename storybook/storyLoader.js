d86d23aa653420deba6c94b5ddd7f28dbfe4bf5c
Cal Leung <cleun007@gmail.com>
2022-06-28 15:13:36 -0700

    Component/4517 button secondary (#4524)
    * [WIP] Push up icon changes
    
    * Introduce assets
    
    * Create script to generate Icon types based on icons in assets directory
    
    * Change Icon component to use SVGs. Create script to auto generate SVG types. Use an svg transformer to allow for importing SVGs as components
    
    * Update ignores on Icon.assets
    
    * Update generate-assets script to add ignores
    
    * Remove icon test - SVG mock issue. Update Icon docs with color prop
    
    * Fix useStyles for other components
    
    * Create BaseButton component
    
    * Update Icon README
    
    * Clean up BaseButton
    
    * Create ButtomPrimary
    
    * Add more stories to BaseButton
    
    * Finalize ButtonPrimary
    
    * Simplify ButtonPrimary types
    
    * Clean up BaseButton
    
    * Create secondary button
    
    * Update test
    
    * Update ButtonPrimary
    
    * Create snapshot
    
    * Add variant
    
    * Add vvariant
    
    * Update test
    
    * Update BaseButton type to extend TouchableOpacity
    
    * Add pressed color
    
    * Add pressed state
    
    * Clean up types
    
    * Update test

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -15,6 +15,7 @@ function loadStories() {
   require('../app/component-library/components/BaseAvatar/BaseAvatar.stories');
   require('../app/component-library/components/BaseButton/BaseButton.stories');
   require('../app/component-library/components/ButtonPrimary/ButtonPrimary.stories');
+  require('../app/component-library/components/ButtonSecondary/ButtonSecondary.stories');
   require('../app/component-library/components/BaseText/BaseText.stories');
   require('../app/component-library/components/Icon/Icon.stories');
   require('../app/component-library/components/Link/Link.stories');
@@ -34,6 +35,7 @@ const stories = [
   '../app/component-library/components/BaseAvatar/BaseAvatar.stories',
   '../app/component-library/components/BaseButton/BaseButton.stories',
   '../app/component-library/components/ButtonPrimary/ButtonPrimary.stories',
+  '../app/component-library/components/ButtonSecondary/ButtonSecondary.stories',
   '../app/component-library/components/BaseText/BaseText.stories',
   '../app/component-library/components/Icon/Icon.stories',
   '../app/component-library/components/Link/Link.stories',
