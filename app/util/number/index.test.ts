cac5cd596234158fe0dc79869205fad9e8bbcbc5
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-07-06 16:10:57 -0400

    Add tests to safeNumberToBN

--- app/util/number/index.test.ts
+++ app/util/number/index.test.ts
@@ -598,4 +598,22 @@ describe('Number utils :: safeNumberToBN', () => {
     expect(result.negative).toEqual(expected.negative);
     expect(result.length).toEqual(expected.length);
   });
+
+  it('should safe convert decimal zero to BN', () => {
+    const result: any = safeNumberToBN('0');
+    const expected: any = new BN('0');
+    expect(result.words[0]).toEqual(expected.words[0]);
+    expect(result.words[1]).toEqual(expected.words[1]);
+    expect(result.negative).toEqual(expected.negative);
+    expect(result.length).toEqual(expected.length);
+  });
+
+  it('should safe convert hex zero to BN', () => {
+    const result: any = safeNumberToBN('0x0');
+    const expected: any = new BN('0');
+    expect(result.words[0]).toEqual(expected.words[0]);
+    expect(result.words[1]).toEqual(expected.words[1]);
+    expect(result.negative).toEqual(expected.negative);
+    expect(result.length).toEqual(expected.length);
+  });
 });
