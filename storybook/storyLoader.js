32d6ace11c13d2f13b983401c005acb74148bee3
Cal Leung <cleun007@gmail.com>
2022-07-18 18:12:30 -0700

    Create PickerItem component

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -26,6 +26,7 @@ function loadStories() {
   require('../app/component-library/components/Link/Link.stories');
   require('../app/component-library/components/MultiselectListItem/MultiselectListItem.stories');
   require('../app/component-library/components/NetworkAvatar/NetworkAvatar.stories');
+  require('../app/component-library/components/PickerItem/PickerItem.stories');
   require('../app/component-library/components/SelectableListItem/SelectableListItem.stories');
   require('../app/component-library/components/TabBar/TabBar.stories');
   require('../app/component-library/components/TabBarItem/TabBarItem.stories');
@@ -57,12 +58,13 @@ const stories = [
   '../app/component-library/components/Link/Link.stories',
   '../app/component-library/components/MultiselectListItem/MultiselectListItem.stories',
   '../app/component-library/components/NetworkAvatar/NetworkAvatar.stories',
+  '../app/component-library/components/PickerItem/PickerItem.stories',
   '../app/component-library/components/SelectableListItem/SelectableListItem.stories',
   '../app/component-library/components/TabBar/TabBar.stories',
   '../app/component-library/components/TabBarItem/TabBarItem.stories',
   '../app/component-library/components/Tag/Tag.stories',
   '../app/component-library/components/TagUrl/TagUrl.stories',
-  '../app/component-library/components/TokenAvatar/TokenAvatar.stories'
+  '../app/component-library/components/TokenAvatar/TokenAvatar.stories',
 ];
 
 module.exports = {
