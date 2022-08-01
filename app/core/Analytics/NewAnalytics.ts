3d0c3072f678fe2a88e63e12aed36b3dbf0c39a1
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-08-01 10:42:31 -0400

    Progress...

--- app/core/Analytics/NewAnalytics.ts
+++ app/core/Analytics/NewAnalytics.ts
@@ -1,5 +1,15 @@
 import { createClient } from '@segment/analytics-react-native';
+import DefaultPreference from 'react-native-default-preference';
 import { ANALYTICS_EVENTS_V2 } from '../../util/analyticsV2';
+import Logger from '../../util/Logger';
+import {
+  METRICS_OPT_IN,
+  AGREED,
+  DENIED,
+  ANALYTICS_DATA_DELETION_TASK_ID,
+  ANALYTICS_DATA_DELETION_DATE,
+  ANALYTICS_DATA_RECORDED,
+} from '../../constants/storage';
 
 enum STATES {
   enabled = 'ENABLED',
@@ -28,21 +38,34 @@ class Analytics implements IAnalytics {
       writeKey: (__DEV__
         ? process.env.SEGMENT_DEV_KEY
         : process.env.SEGMENT_PROD_KEY) as string,
+      debug: __DEV__,
     });
 
     this.state = STATES.enabled;
   }
+
+  private _trackEvent() {
+    // eslint-disable-next-line no-console
+    console.log('trackEvent');
+  }
+
+  private __storeMetricsOptInPreference = async () => {
+    try {
+      await DefaultPreference.set(
+        METRICS_OPT_IN,
+        this.state === STATES.enabled ? AGREED : DENIED,
+      );
+    } catch (e: any) {
+      const errorMsg = 'Error storing Metrics OptIn flag in user preferences';
+      Logger.error(e, errorMsg);
+    }
+  };
 
   public static getInstance(): Analytics {
     if (!Analytics._instance) {
       Analytics._instance = new Analytics();
     }
     return Analytics._instance;
-  }
-
-  private _trackEvent() {
-    // eslint-disable-next-line no-console
-    console.log('trackEvent');
   }
 
   public enable() {
