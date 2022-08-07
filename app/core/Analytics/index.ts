446c39c6a8e6429c016ddfd26766e2965fa24bcb
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-08-07 01:07:16 -0400

    Add index file to Analytics dir

---
+++ app/core/Analytics/index.ts
@@ -0,0 +1,5 @@
+import MetaMetrics from './MetaMetrics';
+import Analytics from './Analytics';
+import { DeletionTaskStatus, ResponseStatus } from './constants';
+
+export { MetaMetrics, Analytics, DeletionTaskStatus, ResponseStatus };
