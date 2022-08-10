d2c06fa54a7cc47ffd56dc95899616460544cd4b
Sylva Elendu <iamsylvaelendu@gmail.com>
2022-08-10 20:12:18 +0100

    refactoring transaction sendflow

--- app/components/Views/SendFlow/Confirm/index.js
+++ app/components/Views/SendFlow/Confirm/index.js
@@ -850,6 +850,12 @@ class Confirm extends PureComponent {
     this.review();
   };
 
+  existingGas = {
+    isEIP1559Transaction: false,
+    maxFeePerGas: '123',
+    maxPriorityFeePerGas: '123',
+  };
+
   renderCustomGasModalEIP1559 = () => {
     const { primaryCurrency, chainId, gasFeeEstimates } = this.props;
     const {
