d4b10538d745387bedc285d20bf2739dde3f609f
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-08-10 01:06:24 -0400

    Add Segment regulations endpoint

--- app/core/Analytics/constants.ts
+++ app/core/Analytics/constants.ts
@@ -20,3 +20,6 @@ export enum States {
 }
 
 export const METAMETRICS_ANONYMOUS_ID = '0x0000000000000000';
+
+export const SEGMENT_REGULATIONS_ENDPOINT =
+  'https://platform.segmentapis.com/v1beta/workspaces/myworkspace/regulations';
