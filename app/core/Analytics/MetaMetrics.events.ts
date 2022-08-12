e9d1f8b23afbdcd03d55c64d43b9ae931db75b1e
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-08-11 22:55:07 -0400

    Add events

--- app/core/Analytics/MetaMetrics.events.ts
+++ app/core/Analytics/MetaMetrics.events.ts
@@ -1,3 +1,14 @@
-const MetaMetricsEvents = {};
+const generateOpt = (name: string, anonymous?: boolean) => ({
+  category: name,
+  anonymous: anonymous || false,
+});
+
+const MetaMetricsEvents = {
+  WALLET: {
+    OPEN: generateOpt('Wallet Opened'),
+    TOKEN_ADDED: generateOpt('Token Added'),
+    COLLECTIBLE_ADDED: generateOpt('Collectible Added'),
+  },
+};
 
 export default MetaMetricsEvents;
