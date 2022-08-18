e71f2218a0bdb1353717c7cff5597380ec09fc2b
Brian August Nguyen <brianacnguyen@gmail.com>
2022-08-18 10:29:05 -0700

    Component/4080-badge (#4841)
    * Badge work
    
    * Finish Badge component
    
    * Add Badge component into Avatar Base
    
    * Update snapshots for Avatar Base and Badge
    
    * Update component based on code review
    
    * Rename Badge to BadgeWrapper
    
    * Commit broken out badge
    
    * Add Badge Base
    
    * Add BadgeNetwork
    
    * Add Badge
    
    * Add Badge Wrapper
    
    * Resolve code review conversations
    
    Co-authored-by: Cal Leung <cleun007@gmail.com>

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -18,6 +18,10 @@ function loadStories() {
   require('../app/component-library/components/Avatars/AvatarIcon/AvatarIcon.stories');
   require('../app/component-library/components/Avatars/AvatarNetwork/AvatarNetwork.stories');
   require('../app/component-library/components/Avatars/AvatarToken/AvatarToken.stories');
+  require('../app/component-library/components/Badges/Badge/Badge.stories');
+  require('../app/component-library/components/Badges/Badge/foundation/BadgeBase/BadgeBase.stories');
+  require('../app/component-library/components/Badges/Badge/variants/BadgeNetwork/BadgeNetwork.stories');
+  require('../app/component-library/components/Badges/BadgeWrapper/BadgeWrapper.stories');
   require('../app/component-library/components/Buttons/ButtonBase/ButtonBase.stories');
   require('../app/component-library/components/Buttons/ButtonIcon/ButtonIcon.stories');
   require('../app/component-library/components/Buttons/ButtonLink/ButtonLink.stories');
@@ -63,6 +67,10 @@ const stories = [
   '../app/component-library/components/Avatars/AvatarIcon/AvatarIcon.stories',
   '../app/component-library/components/Avatars/AvatarNetwork/AvatarNetwork.stories',
   '../app/component-library/components/Avatars/AvatarToken/AvatarToken.stories',
+  '../app/component-library/components/Badges/Badge/Badge.stories',
+  '../app/component-library/components/Badges/Badge/foundation/BadgeBase/BadgeBase.stories',
+  '../app/component-library/components/Badges/Badge/variants/BadgeNetwork/BadgeNetwork.stories',
+  '../app/component-library/components/Badges/BadgeWrapper/BadgeWrapper.stories',
   '../app/component-library/components/Buttons/ButtonBase/ButtonBase.stories',
   '../app/component-library/components/Buttons/ButtonIcon/ButtonIcon.stories',
   '../app/component-library/components/Buttons/ButtonLink/ButtonLink.stories',
