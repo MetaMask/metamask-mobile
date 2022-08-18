35b33e6c68bbacab61695a89dca9cc4f7dda0d99
Tomás SANTOS <tommasini15@gmail.com>
2022-08-18 21:47:02 +0100

    update testing

--- app/components/Nav/App/index.test.tsx
+++ app/components/Nav/App/index.test.tsx
@@ -2,20 +2,40 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './';
 import configureMockStore from 'redux-mock-store';
+import { MAINNET } from '../../../constants/network';
+import { Provider } from 'react-redux';
 
 const mockStore = configureMockStore();
 
+const initialState = {
+  engine: {
+    backgroundState: {
+      NetworkController: {
+        provider: {
+          type: MAINNET,
+        },
+      },
+    },
+  },
+};
+const store = mockStore(initialState);
+
 describe('App', () => {
   it('should render correctly when logged in', () => {
-    const initialState = {
+    const state = {
       user: {
         loggedIn: true,
       },
     };
 
-    const wrapper = shallow(<App />, {
-      context: { store: mockStore(initialState) },
-    });
+    const wrapper = shallow(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      {
+        context: { store: mockStore(state) },
+      },
+    );
     expect(wrapper).toMatchSnapshot();
     expect(wrapper).toMatchSnapshot();
   });
