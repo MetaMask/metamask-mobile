9a320f6f0e3de2659161e7e86523a00631cad583
Sylva Elendu <iamsylvaelendu@gmail.com>
2022-08-22 18:58:28 +0100

    some improvements

--- app/components/Views/SendFlow/Confirm/index.js
+++ app/components/Views/SendFlow/Confirm/index.js
@@ -1101,7 +1101,6 @@ class Confirm extends PureComponent {
 
   updateGasSelected = (selected) => {
     this.setState({
-      newGasValue: gas,
       stopUpdateGas: !selected,
       gasSelectedTemp: selected,
       gasSelected: selected,
