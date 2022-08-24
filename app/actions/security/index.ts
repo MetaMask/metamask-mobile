3c04b529fbc66a6e7f14027b01e0d76cf36f7a04
Owen Craston <owen.craston@consensys.net>
2022-08-24 15:06:20 -0700

    [Force upgrade] Automatic security checks settings (#4897)
    * redux state and settings toggle
    
    * only show in dev

--- app/actions/security/index.ts
+++ app/actions/security/index.ts
@@ -3,6 +3,7 @@ import type { Action } from 'redux';
 
 export enum ActionType {
   SET_ALLOW_LOGIN_WITH_REMEMBER_ME = 'SET_ALLOW_LOGIN_WITH_REMEMBER_ME',
+  SET_AUTOMATIC_SECURITY_CHECKS = 'SET_AUTOMATIC_SECURITY_CHECKS',
 }
 
 export interface AllowLoginWithRememberMeUpdated
@@ -10,11 +11,23 @@ export interface AllowLoginWithRememberMeUpdated
   enabled: boolean;
 }
 
-export type Action = AllowLoginWithRememberMeUpdated;
+export interface AutomaticSecurityChecks
+  extends Action<ActionType.SET_AUTOMATIC_SECURITY_CHECKS> {
+  enabled: boolean;
+}
+
+export type Action = AllowLoginWithRememberMeUpdated | AutomaticSecurityChecks;
 
 export const setAllowLoginWithRememberMe = (
   enabled: boolean,
 ): AllowLoginWithRememberMeUpdated => ({
   type: ActionType.SET_ALLOW_LOGIN_WITH_REMEMBER_ME,
+  enabled,
+});
+
+export const setAutomaticSecurityChecks = (
+  enabled: boolean,
+): AutomaticSecurityChecks => ({
+  type: ActionType.SET_AUTOMATIC_SECURITY_CHECKS,
   enabled,
 });
