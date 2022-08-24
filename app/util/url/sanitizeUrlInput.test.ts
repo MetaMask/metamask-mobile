d39ddc2f695f9d48ed94897a60a814193b01c749
tommasini <46944231+tommasini@users.noreply.github.com>
2022-08-24 11:40:03 +0100

    fix test sanitizeUrlInput error (#4898)

--- app/util/url/sanitizeUrlInput.test.ts
+++ app/util/url/sanitizeUrlInput.test.ts
@@ -2,8 +2,7 @@ import SanitizeUrlInput from './sanitizeUrlInput';
 
 describe('SanitizeInputUrl', () => {
   it('should sanitize url passed to the browser', () => {
-    const urlString =
-      "https://${Date.now()}.xyz/#'+eval(atob('${btoa(payload)}'))+'";
+    const urlString = "https://random.xyz/#'+eval(atob('${btoa(payload)}'))+'";
     const sanitizedUrl = SanitizeUrlInput(urlString);
     expect(sanitizedUrl).toEqual(
       'https://random.xyz/#%27+eval(atob(%27${btoa(payload)}%27))+%27',
