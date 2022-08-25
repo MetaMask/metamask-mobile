dabcbb44d32383932a3a5de61cce00431771abd3
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-08-25 17:05:53 -0400

    Revert ENS CCIP Wildcard Support

--- app/util/ENSUtils.js
+++ app/util/ENSUtils.js
@@ -1,12 +1,7 @@
-// eslint-disable-next-line import/no-unresolved
-import '@ethersproject/shims';
-
 import Engine from '../core/Engine';
-import ensNetworkMap from 'ethereum-ens-network-map';
-import { ethers } from 'ethers';
+import networkMap from 'ethjs-ens/lib/network-map.json';
+import ENS from 'ethjs-ens';
 import { toLowerCaseEquals } from '../util/general';
-import { getEthersNetworkTypeById } from './networks';
-import Logger from './Logger';
 
 /**
  * Utility class with the single responsibility
@@ -16,29 +11,21 @@ class ENSCache {
   static cache = {};
 }
 
-export function getEnsProvider(network, provider) {
-  const ensAddress = ensNetworkMap[network];
-  if (ensAddress) {
-    const networkType = getEthersNetworkTypeById(network);
-    return new ethers.providers.Web3Provider(provider, {
-      chainId: parseInt(network, 10),
-      name: networkType,
-      ensAddress,
-    });
-  }
-}
-
 export async function doENSReverseLookup(address, network) {
   const cache = ENSCache.cache[network + address];
-  const { provider } = Engine.context.NetworkController;
   if (cache) {
     return Promise.resolve(cache);
   }
-  const ensProvider = await getEnsProvider(network, provider);
-  if (ensProvider) {
+
+  const { provider } = Engine.context.NetworkController;
+
+  const networkHasEnsSupport = Boolean(networkMap[network]);
+
+  if (networkHasEnsSupport) {
+    this.ens = new ENS({ provider, network });
     try {
-      const name = await ensProvider.lookupAddress(address);
-      const resolvedAddress = await ensProvider.resolveName(name);
+      const name = await this.ens.reverse(address);
+      const resolvedAddress = await this.ens.lookup(name);
       if (toLowerCaseEquals(address, resolvedAddress)) {
         ENSCache.cache[network + address] = name;
         return name;
@@ -50,14 +37,16 @@ export async function doENSReverseLookup(address, network) {
 
 export async function doENSLookup(ensName, network) {
   const { provider } = Engine.context.NetworkController;
-  const ensProvider = await getEnsProvider(network, provider);
-  if (ensProvider) {
+
+  const networkHasEnsSupport = Boolean(networkMap[network]);
+
+  if (networkHasEnsSupport) {
+    this.ens = new ENS({ provider, network });
     try {
-      const resolvedAddress = await ensProvider.resolveName(ensName);
+      const resolvedAddress = await this.ens.lookup(ensName);
       return resolvedAddress;
-    } catch (e) {
-      Logger.error(e);
-    }
+      // eslint-disable-next-line no-empty
+    } catch (e) {}
   }
 }
 
