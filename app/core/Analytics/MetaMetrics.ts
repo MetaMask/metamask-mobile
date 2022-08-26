899503a3b1ddf0946cdca908f3de028fbf9b19cf
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-08-26 14:15:51 -0400

    Test version

--- app/core/Analytics/MetaMetrics.ts
+++ app/core/Analytics/MetaMetrics.ts
@@ -75,7 +75,7 @@ class MetaMetrics implements IMetaMetrics {
           ),
         ),
       );
-      await DefaultPreference.set(METAMETRICS_ID, metametricsId);
+      await DefaultPreference.set(METAMETRICS_ID, metametricsId as string);
     }
     if (__DEV__) Logger.log(`Current MetaMatrics ID: ${metametricsId}`);
     return metametricsId;
@@ -221,9 +221,7 @@ class MetaMetrics implements IMetaMetrics {
   public static getInstance(): IMetaMetrics {
     if (!MetaMetrics.#instance) {
       const segmentClient = createClient({
-        writeKey: (__DEV__
-          ? process.env.SEGMENT_DEV_KEY
-          : process.env.SEGMENT_PROD_KEY) as string,
+        writeKey: process.env.SEGMENT_DEV_KEY as string,
         debug: __DEV__,
       });
       MetaMetrics.#instance = new MetaMetrics(segmentClient);
