aed2e548a9773aca321e904ef3f309a100814b74
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-08-26 12:24:55 -0400

    Update testing

--- app/util/testSetup.js
+++ app/util/testSetup.js
@@ -179,6 +179,7 @@ jest.mock('../util/theme', () => ({
 }));
 
 jest.mock('@segment/analytics-react-native', () => ({
+  ...jest.requireActual('@segment/analytics-react-native'),
   createClient: () => ({
     identify: jest.fn(),
     track: jest.fn(),
