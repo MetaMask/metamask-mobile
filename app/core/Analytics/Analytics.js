0cb388c520f2d37abfea77822ca372db0ed5eb28
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-08-26 16:03:34 -0400

    Revert "Redirect events to Segment"
    This reverts commit 54cef155eb6c4a7b65ad1a35a4bfc6b855c998f0.

--- app/core/Analytics/Analytics.js
+++ app/core/Analytics/Analytics.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import AUTHENTICATION_TYPE from '../../constants/userProperties';
 import DefaultPreference from 'react-native-default-preference';
 import Logger from '../../util/Logger';
-// import analyticsV2, { ANALYTICS_EVENTS_V2 } from '../../util/analyticsV2';
+import { ANALYTICS_EVENTS_V2 } from '../../util/analyticsV2';
 import { store } from '../../store';
 import { MIXPANEL_ENDPOINT_BASE_URL } from '../../constants/urls';
 import {
@@ -21,7 +21,6 @@ import {
   MixPanelResponseStatus,
   MixPanelDeletionTaskStatus,
 } from './MetaMetrics.types';
-import MetaMetrics from './MetaMetrics';
 
 const RCTAnalytics = NativeModules.Analytics;
 
@@ -129,43 +128,39 @@ class Analytics {
     name,
     { event, params = {}, value, info, anonymously = false },
   ) {
-    MetaMetrics.trackEvent(event.category, anonymously, {
-      ...params,
-      ...event,
-    });
-    // const isAnalyticsPreferenceSelectedEvent =
-    //   ANALYTICS_EVENTS_V2.ANALYTICS_PREFERENCE_SELECTED === event;
-    // if (!this.enabled && !isAnalyticsPreferenceSelectedEvent) return;
-    // this._setUserProfileProperties();
-    // if (!__DEV__) {
-    //   if (!anonymously) {
-    //     RCTAnalytics.trackEvent({
-    //       ...event,
-    //       ...params,
-    //       value,
-    //       info,
-    //     });
-    //   } else {
-    //     RCTAnalytics.trackEventAnonymously({
-    //       ...event,
-    //       ...params,
-    //       value,
-    //       info,
-    //     });
-    //   }
+    const isAnalyticsPreferenceSelectedEvent =
+      ANALYTICS_EVENTS_V2.ANALYTICS_PREFERENCE_SELECTED === event;
+    if (!this.enabled && !isAnalyticsPreferenceSelectedEvent) return;
+    this._setUserProfileProperties();
+    if (!__DEV__) {
+      if (!anonymously) {
+        RCTAnalytics.trackEvent({
+          ...event,
+          ...params,
+          value,
+          info,
+        });
+      } else {
+        RCTAnalytics.trackEventAnonymously({
+          ...event,
+          ...params,
+          value,
+          info,
+        });
+      }
 
-    //   if (!this.isDataRecorded) {
-    //     this.isDataRecorded = true;
-    //     await DefaultPreference.set(
-    //       ANALYTICS_DATA_DELETION_TASK_ID,
-    //       this.dataDeletionTaskId,
-    //     );
+      if (!this.isDataRecorded) {
+        this.isDataRecorded = true;
+        await DefaultPreference.set(
+          ANALYTICS_DATA_DELETION_TASK_ID,
+          this.dataDeletionTaskId,
+        );
 
-    //     await DefaultPreference.set(ANALYTICS_DATA_RECORDED, 'true');
-    //   }
-    // } else {
-    //   Logger.log(`Analytics '${name}' -`, event, params, value, info);
-    // }
+        await DefaultPreference.set(ANALYTICS_DATA_RECORDED, 'true');
+      }
+    } else {
+      Logger.log(`Analytics '${name}' -`, event, params, value, info);
+    }
   }
 
   /**
