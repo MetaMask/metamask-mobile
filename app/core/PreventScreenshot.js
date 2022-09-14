15a8036405010e669fa05323c20f5a813b78746c
Andre Pimenta <andrepimenta7@gmail.com>
2022-09-14 17:57:45 +0100

    Don't go back if there is another request

--- app/core/PreventScreenshot.js
+++ app/core/PreventScreenshot.js
@@ -1,8 +1,8 @@
+import { NativeModules, Platform } from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
+
 export default {
-  forbid: () => {
-    true;
-  },
-  allow: () => {
-    true;
-  },
+  forbid: isAndroid ? NativeModules.PreventScreenshot.forbid : () => true,
+  allow: isAndroid ? NativeModules.PreventScreenshot.allow : () => true,
 };
