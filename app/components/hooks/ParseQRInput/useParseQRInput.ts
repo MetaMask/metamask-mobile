695572c959ab10ab64c490072eed27cc2bd4574c
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-09-27 10:12:03 -0300

    Add hook for QR Reader

---
+++ app/components/hooks/ParseQRInput/useParseQRInput.ts
@@ -0,0 +1,12 @@
+import { useCallback } from 'react';
+
+export enum QRError {
+  ERROR_ADDRESS_INPUT = 'error_address_input',
+}
+
+const useParseQRInput = () => {
+  const isValidSendInput
+  return { error };
+};
+
+export default useParseQRInput;
