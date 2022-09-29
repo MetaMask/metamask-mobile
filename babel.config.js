abbd790de78c8df75cfd6f2cd468d6e720d3f198
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-09-28 22:52:18 -0300

    Update babel.config

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,21 @@
 // eslint-disable-next-line import/no-commonjs
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    'transform-inline-environment-variables',
-    'react-native-reanimated/plugin',
-  ],
-  env: {
-    production: {
-      plugins: ['transform-remove-console'],
+module.exports = (api) => {
+  if (api) {
+    api.cache(true);
+    api.debug = process.env.NODE_ENV === 'development' || false;
+  }
+
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      './plugins/remove-flask-code.js',
+      'transform-inline-environment-variables',
+      'react-native-reanimated/plugin',
+    ],
+    env: {
+      production: {
+        plugins: ['transform-remove-console'],
+      },
     },
-  },
+  };
 };
