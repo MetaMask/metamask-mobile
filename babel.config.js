de3f10e7d8c5f3e6b212caa2c1b22e0c88352247
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-09-29 19:40:04 -0300

    Update babel configuration

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    './plugins/remove-flask-code',
     'transform-inline-environment-variables',
     'react-native-reanimated/plugin',
   ],
