1d7dfc27083841512925e69f8845252ce8f6a0f8
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-10-01 18:51:20 -0300

    Update segment regulations endpoint

--- app/core/Analytics/MetaMetrics.constants.ts
+++ app/core/Analytics/MetaMetrics.constants.ts
@@ -1,4 +1,4 @@
 export const METAMETRICS_ANONYMOUS_ID = '0x0000000000000000';
 
 export const SEGMENT_REGULATIONS_ENDPOINT =
-  'https://platform.segmentapis.com/v1beta/workspaces/myworkspace/regulations';
+  'https://api.segmentapis.com/regulations';
