2c30085d79c47f27bf43b069fb6d5563c5c57311
Sylva Elendu <iamsylvaelendu@gmail.com>
2022-10-10 14:08:36 +0100

    fix to legacy transactions

--- app/core/GasPolling/GasPolling.ts
+++ app/core/GasPolling/GasPolling.ts
@@ -145,6 +145,8 @@ export const getLegacyTransactionData = ({
   gas,
   onlyGas,
 }: LegacyProps) => {
+  // hack: selectedAsset becomes an empty object when transaction is submitted and it breaks the app. See Line 1241 in util/transactions.js
+  transactionState.selectedAsset.isETH = true;
   const parsedTransationData = parseTransactionLegacy(
     {
       contractExchangeRates,
