ae4e5e2800262e20753fcd78e77edbe2248d5bb7
Brian Nguyen <brianacnguyen@gmail.com>
2022-10-11 17:48:26 +0100

    Added AvatarAsset

--- app/component-library/components/Avatars/Avatar2/foundation/AvatarAsset/AvatarAsset.tsx
+++ app/component-library/components/Avatars/Avatar2/foundation/AvatarAsset/AvatarAsset.tsx
@@ -2,15 +2,24 @@
 import React from 'react';
 
 // External dependencies.
+import AvatarAssetIcon from './variants/AvatarAssetIcon';
+import AvatarAssetImage from './variants/AvatarAssetImage';
 import AvatarAssetInitial from './variants/AvatarAssetInitial';
+import AvatarAssetJazzIcon from './variants/AvatarAssetJazzIcon';
 
 // Internal dependencies.
 import { AvatarAssetProps, AvatarAssetVariants } from './AvatarAsset.types';
 
 const AvatarAsset = (avatarAssetProps: AvatarAssetProps) => {
   switch (avatarAssetProps.variant) {
+    case AvatarAssetVariants.Icon:
+      return <AvatarAssetIcon {...avatarAssetProps} />;
+    case AvatarAssetVariants.Image:
+      return <AvatarAssetImage {...avatarAssetProps} />;
     case AvatarAssetVariants.Initial:
       return <AvatarAssetInitial {...avatarAssetProps} />;
+    case AvatarAssetVariants.JazzIcon:
+      return <AvatarAssetJazzIcon {...avatarAssetProps} />;
     default:
       throw new Error('Invalid Avatar Asset Variant');
   }
--- app/component-library/components/Avatars/Avatar2/foundation/AvatarAsset/AvatarAsset.types.ts
+++ app/component-library/components/Avatars/Avatar2/foundation/AvatarAsset/AvatarAsset.types.ts
@@ -1,17 +1,24 @@
 // External dependencies.
+import { AvatarAssetIconProps } from './variants/AvatarAssetIcon/AvatarAssetIcon.types';
+import { AvatarAssetImageProps } from './variants/AvatarAssetImage/AvatarAssetImage.types';
 import { AvatarAssetInitialProps } from './variants/AvatarAssetInitial/AvatarAssetInitial.types';
+import { AvatarAssetJazzIconProps } from './variants/AvatarAssetJazzIcon/AvatarAssetJazzIcon.types';
 
 /**
  * Avatar Asset variants.
  */
 export enum AvatarAssetVariants {
-  JazzIcon = 'JazzIcon',
   Icon = 'Icon',
   Image = 'Image',
   Initial = 'Initial',
+  JazzIcon = 'JazzIcon',
 }
 
 /**
  * Avatar Asset component props.
  */
-export type AvatarAssetProps = AvatarAssetInitialProps;
+export type AvatarAssetProps =
+  | AvatarAssetIconProps
+  | AvatarAssetImageProps
+  | AvatarAssetInitialProps
+  | AvatarAssetJazzIconProps;
