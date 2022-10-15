a2114a6ecc1269d069e69c1e04a99fb630346a07
tommasini <46944231+tommasini@users.noreply.github.com>
2022-10-15 04:42:31 +0100

    [FIX] Delete contact on android fixed (#4235)
    * fix delete contact on android and improve performance of the address input when is ens
    
    * Refactored validate function to a util files, missing tests
    
    * refactor of validateAddressOrEns
    
    * refactor and implmentation of warning smart contract message
    
    * update test
    
    * Warning message updated
    
    * refactor to constants
    
    * refactor variables
    
    * changed network id number

--- app/constants/test-ids.js
+++ app/constants/test-ids.js
@@ -69,6 +69,8 @@ export const WHATS_NEW_MODAL_GOT_IT_BUTTON_ID = 'whats-new-modal-got-it-button';
 
 export const INPUT_NETWORK_NAME = 'input-network-name';
 
+export const ADDRESS_BOOK_NEXT_BUTTON = 'address-book-next-button';
+
 // Design System test ids
 export const FAVICON_AVATAR_IMAGE_ID = 'favicon-avatar-image';
 export const NETWORK_AVATAR_IMAGE_ID = 'network-avatar-image';
