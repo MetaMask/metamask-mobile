63e9acac84ca08e2302208d826450d61bdb596ee
Sylva Elendu <iamsylvaelendu@gmail.com>
2022-10-27 23:23:13 +0300

    Refactor ApprovalTransaction Component (#4868)
    * updateTxn
    
    * fix broken advanced setting
    
    * advanced screen blocker fixed
    
    * make this easy to read, will squash this commit later
    
    * suggestedGasLimit name
    
    * some improvements
    
    * improve comment on types
    
    * refactoring transaction sendflow
    
    * more refactoring
    
    * some improvements
    
    * draft PR for approval screen
    
    * removed eip1559
    
    * rebased to main
    
    * rebased to main
    
    * legacy transaction update
    
    * legacy transaction fix
    
    * legacyObject prop validation

--- app/components/Views/SendFlow/Confirm/index.js
+++ app/components/Views/SendFlow/Confirm/index.js
@@ -68,8 +68,8 @@ import InfoModal from '../../../UI/Swaps/components/InfoModal';
 import { addHexPrefix, toChecksumAddress } from 'ethereumjs-util';
 import { removeFavoriteCollectible } from '../../../../actions/collectibles';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import TransactionReviewEIP1559Update from '../../../UI/TransactionReview/TransactionReviewEIP1559Update';
-import EditGasFee1559Update from '../../../UI/EditGasFee1559Update';
+import TransactionReview from '../../../UI/TransactionReview/TransactionReviewEIP1559Update';
+import EditGasFee1559 from '../../../UI/EditGasFee1559Update';
 import EditGasFeeLegacy from '../../../UI/EditGasFeeLegacyUpdate';
 import CustomNonce from '../../../UI/CustomNonce';
 import AppConstants from '../../../../core/AppConstants';
@@ -893,7 +893,7 @@ class Confirm extends PureComponent {
         <KeyboardAwareScrollView
           contentContainerStyle={styles.keyboardAwareWrapper}
         >
-          <EditGasFee1559Update
+          <EditGasFee1559
             selectedGasValue={gasSelected}
             initialSuggestedGasLimit={this.state.suggestedGasLimit}
             gasOptions={gasFeeEstimates}
@@ -1261,7 +1261,7 @@ class Confirm extends PureComponent {
               </View>
             </View>
           )}
-          <TransactionReviewEIP1559Update
+          <TransactionReview
             gasSelected={this.state.gasSelected}
             primaryCurrency={primaryCurrency}
             onEdit={() => this.edit(!showFeeMarket ? EDIT : EDIT_EIP1559)}
@@ -1278,6 +1278,7 @@ class Confirm extends PureComponent {
             }
             updateTransactionState={this.updateTransactionState}
             legacy={!showFeeMarket}
+            onlyGas={false}
           />
           {showCustomNonce && (
             <CustomNonce
