ef045937d81c3f4bb95bd894d3585e1bd1fae63c
Owen Craston <owengc12@gmail.com>
2022-10-27 16:37:12 -0700

    fix prompting user every second

--- app/components/hooks/EnableAutomaticSecurityChecks/useEnableAutomaticSecurityChecks.tsx
+++ app/components/hooks/EnableAutomaticSecurityChecks/useEnableAutomaticSecurityChecks.tsx
@@ -16,7 +16,7 @@ const useEnableAutomaticSecurityChecks = () => {
         ...createEnableAutomaticSecurityChecksModalNavDetails(),
       );
     }
-  });
+  }, [hasUserSelectedAutomaticSecurityCheckOption, navigation]);
 };
 
 export default useEnableAutomaticSecurityChecks;
