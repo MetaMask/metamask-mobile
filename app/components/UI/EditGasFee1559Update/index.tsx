c392c5c23775e53715d08136c6c5784fa23d914a
Sylva Elendu <iamsylvaelendu@gmail.com>
2022-10-27 23:23:13 +0300

    Refactor ApprovalTransaction Component (#4868)
    * updateTxn
    
    * fix broken advanced setting
    
    * advanced screen blocker fixed
    
    * make this easy to read, will squash this commit later
    
    * suggestedGasLimit name
    
    * some improvements
    
    * improve comment on types
    
    * refactoring transaction sendflow
    
    * more refactoring
    
    * some improvements
    
    * draft PR for approval screen
    
    * removed eip1559
    
    * rebased to main
    
    * rebased to main
    
    * legacy transaction update
    
    * legacy transaction fix
    
    * legacyObject prop validation

--- app/components/UI/EditGasFee1559Update/index.tsx
+++ app/components/UI/EditGasFee1559Update/index.tsx
@@ -211,7 +211,7 @@ const EditGasFee1559Update = ({
           strings('edit_gas_fee_eip1559.max_priority_fee_high'),
         );
       } else {
-        setMaxPriorityFeeError('');
+        setMaxPriorityFeeError(null);
       }
 
       const newGas = {
