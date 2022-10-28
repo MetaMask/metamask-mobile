62dc55ee1214d30c6eb86a2f0ecae3b198c592af
CW <chris.wilcox@consensys.net>
2022-10-27 22:38:17 -0700

    clean up

--- wdio/features/step-definitions/drawer-view.js
+++ wdio/features/step-definitions/drawer-view.js
@@ -3,10 +3,8 @@ import DrawerViewScreen from '../screen-objects/DrawerViewScreen';
 
 When(/^I tap Lock menu item/, async () => {
     await DrawerViewScreen.tapNavBarItemLock();
-    await driver.pause(2000);
 });
 
 When(/^I tap Yes on alert/, async () => {
     await DrawerViewScreen.tapYesOnDeviceAlert();
-    await driver.pause(2000);
 });
