20cd2779ecfce32b6310f05603eaf315ad7fba4b
Curtis <Curtis.David7@gmail.com>
2022-10-27 20:11:35 -0400

    add testids for delete modal

--- wdio/features/testIDs/Screens/LoginScreen.testIds.js
+++ wdio/features/testIDs/Screens/LoginScreen.testIds.js
@@ -1,2 +1,3 @@
 export const LOGIN_VIEW_RESET_WALLET_ID = 'login-view-reset-wallet';
 export const DELETE_MODAL_UNDERSTAND_CONTINUE_ID = 'delete-modal-understand-continue';
+export const DELETE_MODAL_CANCEL_BUTTON = 'delete-modal-cancel-button'
