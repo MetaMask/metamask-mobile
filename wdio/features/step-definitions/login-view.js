73aa375dc40852a23544cfab7f9da7a964302795
CW <chris.wilcox@consensys.net>
2022-10-27 22:14:25 -0700

    +Lock & Reset wallet scenarios

--- wdio/features/step-definitions/login-view.js
+++ wdio/features/step-definitions/login-view.js
@@ -1,17 +1,23 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
+import { dirname } from 'path';
 import LoginScreen from '../screen-objects/LoginScreen';
 
 Then(/^Login screen is displayed/, async () => {
     await LoginScreen.isLoginScreenVisible();
-    await driver.pause(2000);
 });
 
 When(/^I tap Reset Wallet on Login screen/, async () => {
     await LoginScreen.tapResetWallet();
 });
 
-When(/^I tap "([^"]*)?" on Delete wallet modal/, async (text) => {
-    console.log(`## tap on ${text}`);
+When(/^I tap I understand, continue on Delete wallet modal/, async () => {
     await LoginScreen.tapIUnderstandContinue();
-    await driver.pause(5000);
+});
+
+When(/^I type "([^"]*)?" on Delete wallet modal permanently/, async (text) => {
+    await LoginScreen.typedelete(text);
+});
+
+When(/^I tap Delete my wallet on Delete wallet modal permanently/, async () => {
+    await LoginScreen.tapDeleteMyWallet();
 });
