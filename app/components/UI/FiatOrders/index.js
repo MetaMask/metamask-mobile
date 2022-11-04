40b21430d0737e0b66a70f0f600252e20ee74647
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2022-11-04 11:58:04 -0300

    On-Ramp: allow Harmony ONE (#5202)

--- app/components/UI/FiatOrders/index.js
+++ app/components/UI/FiatOrders/index.js
@@ -42,6 +42,7 @@ export const allowedToBuy = (chainId) =>
     NETWORKS_CHAIN_ID.ARBITRUM,
     NETWORKS_CHAIN_ID.CELO,
     NETWORKS_CHAIN_ID.AVAXCCHAIN,
+    NETWORKS_CHAIN_ID.HARMONY,
   ].includes(chainId);
 
 const baseNotificationDetails = {
