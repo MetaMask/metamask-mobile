d7a9740d0ab4579f2dd2adf1fa8bd0f7ae174e24
yande <110056475+Andepande@users.noreply.github.com>
2022-11-09 17:32:26 +0000

    Networks-flow appium feature file (#5188)
    * WIP draft PR
    
    * removed trailing space
    
    * Merge conflict resolution
    
    * Remove IOS tag
    
    * Addressed PR comments and merged with master
    
    * Final commit
    
    * Update index.js
    
    * Addressed PR comments
    
    * Pr comments
    
    * EsLint fixes
    
    * Pr comment
    
    * remove remaining duplicate testID
    
    * Change to test structure
    
    * Small pr comment changes
    
    * Pr comments

--- app/components/UI/Navbar/index.js
+++ app/components/UI/Navbar/index.js
@@ -30,10 +30,12 @@ import { importAccountFromPrivateKey } from '../../../util/address';
 import Device from '../../../util/device';
 import { isGatewayUrl } from '../../../lib/ens-ipfs/resolver';
 import { getHost } from '../../../util/browser';
-import { BACK_ARROW_BUTTON_ID } from '../../../constants/test-ids';
 import generateTestId from '../../../../wdio/utils/generateTestId';
 import { WALLET_VIEW_BURGER_ICON_ID } from '../../../../wdio/features/testIDs/Screens/WalletView.testIds';
-
+import {
+  NAV_ANDROID_BACK_BUTTON,
+  NETWORK_BACK_ARROW_BUTTON_ID,
+} from '../../../../wdio/features/testIDs/Screens/NetworksScreen.testids';
 const { HOMEPAGE_URL } = AppConstants;
 
 const trackEvent = (event) => {
@@ -213,7 +215,7 @@ export function getNavigationOptionsTitle(
         <TouchableOpacity
           onPress={navigationPop}
           style={styles.backButton}
-          testID={BACK_ARROW_BUTTON_ID}
+          {...generateTestId(Platform, NETWORK_BACK_ARROW_BUTTON_ID)}
         >
           <IonicIcon
             name={Device.isAndroid() ? 'md-arrow-back' : 'ios-arrow-back'}
@@ -870,7 +872,7 @@ export function getClosableNavigationOptions(
         <TouchableOpacity
           onPress={navigationPop}
           style={styles.backButton}
-          testID={'nav-android-back'}
+          {...generateTestId(Platform, NAV_ANDROID_BACK_BUTTON)}
         >
           <IonicIcon
             name={'md-arrow-back'}
