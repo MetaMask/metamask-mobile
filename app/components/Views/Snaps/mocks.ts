a41ee3657fbccf3db73b735dd5b63b9a96e697d0
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-11-17 14:39:51 -0300

    Move mockss to new file

---
+++ app/components/Views/Snaps/mocks.ts
@@ -0,0 +1,14 @@
+const mockIframe = `<iframe \
+    src="https://metamask.github.io/iframe-execution-environment/0.9.1" \
+    title="iframe Example 1" \
+    width="400" height="300">\
+  </iframe>`;
+
+const mockHTML = `
+  <body>
+    <p>Hello World</p>
+  </body>
+`;
+
+// eslint-disable-next-line import/prefer-default-export
+export { mockIframe, mockHTML };
