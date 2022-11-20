fa02890a2c5d5b7b8a5e7f1b28704fc6f75ee76e
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-11-19 22:46:59 -0300

    Add snap permissions

--- app/constants/permissions.ts
+++ app/constants/permissions.ts
@@ -1,11 +1,7 @@
-enum USER_INTENT {
-  None,
-  Create,
-  CreateMultiple,
-  Confirm,
-  Cancel,
-  Import,
-  ConnectHW,
-}
-
-export default USER_INTENT;
+// eslint-disable-next-line import/prefer-default-export
+export const EndowmentPermissions = Object.freeze({
+  'endowment:network-access': 'endowment:network-access',
+  'endowment:long-running': 'endowment:long-running',
+  'endowment:transaction-insight': 'endowment:transaction-insight',
+  'endowment:cronjob': 'endowment:cronjob',
+} as const);
