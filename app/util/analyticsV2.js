4970abdb466f28eb607b45958b272c71b8d336c8
Owen Craston <owen.craston@consensys.net>
2022-11-22 14:39:00 -0800

    Trigger UpdateNeeded screen (#4917)
    * Automatic security checks
    
    - prompt user to enable this feature after onboarding
    - if enabled, prompt user to update their app version if they are using
      an unsupported version
    - prompt the user every time they kill the app and open it again
    
    * fix prompting user every second
    
    * render alert after onboarding
    
    * track events
    
    * automatic security checks ananlytics
    
    * better meta data
    
    * ternary metrics value
    
    * cleanup tests
    
    * remind me later event
    
    * run after interaction
    
    * translations
    
    * make button grow
    
    * cleanup scrollview
    
    * make UpdateNeeded more dynamic
    
    * add test:
    
    * remove test builds
    
    * use production endpoint
    
    * add tests back

--- app/util/analyticsV2.js
+++ app/util/analyticsV2.js
@@ -181,6 +181,33 @@ export const ANALYTICS_EVENTS_V2 = {
   ONRAMP_EXTERNAL_LINK_CLICKED: generateOpt('External Link Clicked'),
   ONRAMP_QUOTE_ERROR: generateOpt('On-ramp Quote Error'),
   ONRAMP_ERROR: generateOpt('On-ramp Error'),
+
+  // force upgrade/automatic security checks
+  FORCE_UPGRADE_UPDATED_NEEDED_PROMPT_VIEWED: generateOpt(
+    'Force Upgrade Update Needed Prompt Viewed',
+  ),
+  FORCE_UPGRADE_UPDATE_TO_THE_LATEST_VERSION_CLICKED: generateOpt(
+    'Force Upgrade Clicked Update to Latest Version',
+  ),
+  FORCE_UPGRADE_REMIND_ME_LATER_CLICKED: generateOpt(
+    'Force Upgrade Clicked Remind Me Later',
+  ),
+
+  AUTOMATIC_SECURITY_CHECKS_ENABLED_FROM_PROMPT: generateOpt(
+    'Automatic Security Checks Enabled From Prompt',
+  ),
+
+  AUTOMATIC_SECURITY_CHECKS_DISABLED_FROM_PROMPT: generateOpt(
+    'Automatic Security Checks Disabled From Prompt',
+  ),
+
+  AUTOMATIC_SECURITY_CHECKS_ENABLED_FROM_SETTINGS: generateOpt(
+    'Automatic Security Checks Enabled From Settings',
+  ),
+
+  AUTOMATIC_SECURITY_CHECKS_DISABLED_FROM_SETTINGS: generateOpt(
+    'Automatic Security Checks Disabled From Settings',
+  ),
 };
 
 /**
