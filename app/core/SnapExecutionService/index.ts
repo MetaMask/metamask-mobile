13af572d073d825ff1be7ccbd71d651356260352
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-12-15 11:34:28 -0300

    Upadte Snaps core index

--- app/core/SnapExecutionService/index.ts
+++
@@ -1,5 +0,0 @@
-import SnapExecutionService from './SnapExecutionService';
-import SnapBridge from './SnapBridge';
-
-// eslint-disable-next-line import/prefer-default-export
-export { SnapExecutionService, SnapBridge };
---
+++ app/core/Snaps/index.ts
@@ -0,0 +1,13 @@
+import SnapBridge from './SnapBridge';
+import SnapDuplex from './SnapDuplex';
+import WebviewExecutionService from './WebviewExecutionService';
+import WebviewPostMessageStream from './WebviewPostMessageStream';
+import SnapWebviewPostMessageStream from './SnapWebviewPostMessageStream';
+
+export {
+  SnapDuplex,
+  SnapBridge,
+  WebviewExecutionService,
+  WebviewPostMessageStream,
+  SnapWebviewPostMessageStream,
+};
