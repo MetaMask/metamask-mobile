c89affd38ae878bce8102eabb3f95f7de754636c
Sylva Elendu <iamsylvaelendu@gmail.com>
2022-12-15 15:23:29 +0100

    fix bug with updating gas price for legacy transactions

--- app/core/GasPolling/GasPolling.ts
+++ app/core/GasPolling/GasPolling.ts
@@ -205,8 +205,8 @@ export const useGasTransaction = ({
         suggestedGasLimit: gasObject?.legacyGasLimit || suggestedGasLimit,
         suggestedGasPrice:
           gasFeeEstimates[gasSelected] ||
-          gasFeeEstimates?.gasPrice ||
-          gasObject?.suggestedGasPrice,
+          gasObject?.suggestedGasPrice ||
+          gasFeeEstimates?.gasPrice,
       },
       contractExchangeRates,
       conversionRate,
