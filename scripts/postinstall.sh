fcdf7e5b5322f48a0d6797b64db8b3d256cf4dbe
Tomás SANTOS <tommasini15@gmail.com>
2022-12-20 11:50:17 +0000

    stopped at codegen error

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -22,12 +22,12 @@ echo "6. Setup react-native-codegen"
 # Since we've patched React Native, Android requires us to build from source (ReactAndroid).
 # ReactAndroid needs to reference react-native-codegen's android directory, which we are manually fetching here
 
-REACT_NATIVE_VERSION="0.66.0"
+REACT_NATIVE_VERSION="0.67.0"
 
 rm -rf node_modules/react-native/packages
 mkdir -p node_modules/react-native/packages && cd node_modules/react-native/packages
 curl --proto '=https' --tlsv1.2 -LJO https://github.com/facebook/react-native/archive/refs/tags/v${REACT_NATIVE_VERSION}.tar.gz
 tar -zxvf react-native-${REACT_NATIVE_VERSION}.tar.gz react-native-${REACT_NATIVE_VERSION}/packages/react-native-codegen/
-mv react-native-0.66.0/packages/react-native-codegen/ .
+mv ./react-native-0.67.0/packages/react-native-codegen/ .
 # cleanup
 rm react-native-${REACT_NATIVE_VERSION}.tar.gz
