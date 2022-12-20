7be8ee1edc74c8d1ae090581d8837c8a0c2c6215
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2022-12-20 14:22:50 -0300

    Remove useMemo from timer

--- app/components/UI/FiatOnRampAggregator/Views/GetQuotes.tsx
+++ app/components/UI/FiatOnRampAggregator/Views/GetQuotes.tsx
@@ -541,46 +541,32 @@ const GetQuotes = () => {
     trackEvent,
   ]);
 
-  const QuotesPolling = useMemo(
-    () => (
-      <View style={styles.timerWrapper}>
-        {isFetchingQuotes ? (
-          <>
-            <ActivityIndicator size="small" />
-            <Text>
-              {' '}
-              {strings('fiat_on_ramp_aggregator.fetching_new_quotes')}
-            </Text>
-          </>
-        ) : (
-          <Text primary centered>
-            {pollingCyclesLeft > 0
-              ? strings('fiat_on_ramp_aggregator.new_quotes_in')
-              : strings('fiat_on_ramp_aggregator.quotes_expire_in')}{' '}
-            <Text
-              bold
-              primary
-              style={[
-                styles.timer,
-                remainingTime <= appConfig.POLLING_INTERVAL_HIGHLIGHT &&
-                  styles.timerHiglight,
-              ]}
-            >
-              {new Date(remainingTime).toISOString().substring(15, 19)}
-            </Text>
+  const QuotesPolling = () => (
+    <View style={styles.timerWrapper}>
+      {isFetchingQuotes ? (
+        <>
+          <ActivityIndicator size="small" />
+          <Text> {strings('fiat_on_ramp_aggregator.fetching_new_quotes')}</Text>
+        </>
+      ) : (
+        <Text primary centered>
+          {pollingCyclesLeft > 0
+            ? strings('fiat_on_ramp_aggregator.new_quotes_in')
+            : strings('fiat_on_ramp_aggregator.quotes_expire_in')}{' '}
+          <Text
+            bold
+            primary
+            style={[
+              styles.timer,
+              remainingTime <= appConfig.POLLING_INTERVAL_HIGHLIGHT &&
+                styles.timerHiglight,
+            ]}
+          >
+            {new Date(remainingTime).toISOString().substring(15, 19)}
           </Text>
-        )}
-      </View>
-    ),
-    [
-      appConfig.POLLING_INTERVAL_HIGHLIGHT,
-      isFetchingQuotes,
-      pollingCyclesLeft,
-      remainingTime,
-      styles.timer,
-      styles.timerHiglight,
-      styles.timerWrapper,
-    ],
+        </Text>
+      )}
+    </View>
   );
 
   if (sdkError) {
@@ -665,7 +651,7 @@ const GetQuotes = () => {
   return (
     <ScreenLayout>
       <ScreenLayout.Header>
-        {isInPolling && QuotesPolling}
+        {isInPolling && <QuotesPolling />}
         <ScreenLayout.Content style={styles.withoutVerticalPadding}>
           <Text centered grey>
             {strings('fiat_on_ramp_aggregator.buy_from_vetted', {
