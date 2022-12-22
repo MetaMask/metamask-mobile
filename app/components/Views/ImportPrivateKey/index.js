ffa4086f986d7ed35e60a3a93e2bbcac0bd5d051
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2022-12-21 23:59:46 -0300

    Move styles to new file

--- app/components/Views/ImportPrivateKey/index.js
+++ app/components/Views/ImportPrivateKey/index.js
@@ -6,12 +6,10 @@ import {
   TextInput,
   Text,
   View,
-  StyleSheet,
   InteractionManager,
 } from 'react-native';
 
 import PropTypes from 'prop-types';
-import { fontStyles } from '../../../styles/common';
 import StyledButton from '../../UI/StyledButton';
 import Icon from 'react-native-vector-icons/Feather';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
@@ -21,108 +19,7 @@ import Device from '../../../util/device';
 import { importAccountFromPrivateKey } from '../../../util/address';
 import PreventScreenshot from '../../../core/PreventScreenshot';
 import { ThemeContext, mockTheme } from '../../../util/theme';
-
-const createStyles = (colors) =>
-  StyleSheet.create({
-    mainWrapper: {
-      flex: 1,
-      backgroundColor: colors.background.default,
-    },
-    topOverlay: {
-      flex: 1,
-      backgroundColor: colors.primary.muted,
-    },
-    wrapper: {
-      flexGrow: 1,
-    },
-    content: {
-      alignItems: 'flex-start',
-    },
-    title: {
-      fontSize: 32,
-      marginTop: 20,
-      marginBottom: 40,
-      color: colors.text.default,
-      justifyContent: 'center',
-      textAlign: 'left',
-      ...fontStyles.normal,
-    },
-    dataRow: {
-      marginBottom: 10,
-    },
-    label: {
-      fontSize: 14,
-      color: colors.text.default,
-      textAlign: 'left',
-      ...fontStyles.normal,
-    },
-    subtitleText: {
-      fontSize: 18,
-      ...fontStyles.bold,
-      color: colors.text.default,
-    },
-    scanPkeyRow: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginTop: 20,
-    },
-    scanPkeyText: {
-      fontSize: 14,
-      color: colors.primary.default,
-    },
-    icon: {
-      textAlign: 'left',
-      fontSize: 50,
-      marginTop: 0,
-      marginLeft: 0,
-      color: colors.icon.alternative,
-    },
-    buttonWrapper: {
-      flex: 1,
-      justifyContent: 'flex-end',
-      padding: 20,
-      backgroundColor: colors.background.default,
-    },
-    button: {
-      marginBottom: Device.isIphoneX() ? 20 : 0,
-    },
-    top: {
-      paddingTop: 0,
-      padding: 30,
-    },
-    bottom: {
-      width: '100%',
-      padding: 30,
-      backgroundColor: colors.background.default,
-    },
-    input: {
-      marginTop: 20,
-      marginBottom: 10,
-      backgroundColor: colors.background.default,
-      paddingTop: 20,
-      paddingBottom: 20,
-      paddingLeft: 20,
-      paddingRight: 20,
-      fontSize: 15,
-      borderRadius: 4,
-      height: 120,
-      borderWidth: StyleSheet.hairlineWidth,
-      borderColor: colors.border.default,
-      ...fontStyles.normal,
-      color: colors.text.default,
-    },
-    navbarRightButton: {
-      alignSelf: 'flex-end',
-      paddingHorizontal: 22,
-      paddingTop: 20,
-      paddingBottom: 10,
-      marginTop: Device.isIphoneX() ? 40 : 20,
-    },
-    closeIcon: {
-      fontSize: 28,
-      color: colors.text.default,
-    },
-  });
+import { createStyles } from './styles';
 
 /**
  * View that's displayed the first time a user receives funds
