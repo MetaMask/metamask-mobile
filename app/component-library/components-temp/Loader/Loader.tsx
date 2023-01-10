b7c5f7c1343564b0179f04d05e51629b20baf358
João Loureiro <joao.loureiro@consensys.net>
2023-01-10 15:22:09 +0000

    (Loader): add color prop

--- app/component-library/components-temp/Loader/Loader.tsx
+++ app/component-library/components-temp/Loader/Loader.tsx
@@ -9,13 +9,15 @@ import { useStyles } from '../../hooks';
 import styleSheet from './Loader.styles';
 import { LoaderProps } from './Loader.types';
 
-const Loader = ({ size = 'large' }: LoaderProps) => {
+const Loader = ({ size = 'large', color }: LoaderProps) => {
   const { styles, theme } = useStyles(styleSheet, {});
   const { colors } = theme;
 
+  const indicatorColor = color ?? colors.primary.default;
+
   return (
     <View style={styles.base}>
-      <ActivityIndicator size={size} color={colors.primary.default} />
+      <ActivityIndicator size={size} color={indicatorColor} />
     </View>
   );
 };
