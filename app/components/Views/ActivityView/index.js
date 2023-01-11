23468a833dac30d9a4803ea1a42d143b81e42527
Cal Leung <cleun007@gmail.com>
2023-01-10 19:04:35 -0800

    Fix analytics

--- app/components/Views/ActivityView/index.js
+++ app/components/Views/ActivityView/index.js
@@ -12,7 +12,7 @@ import FiatOrdersView from '../FiatOrdersView';
 import ErrorBoundary from '../ErrorBoundary';
 import { useTheme } from '../../../util/theme';
 import Routes from '../../../constants/navigation/Routes';
-import AnalyticsV2 from '../../../util/analyticsV2';
+import { MetaMetricsEvents } from '../../../core/Analytics';
 
 const styles = StyleSheet.create({
   wrapper: {
@@ -37,12 +37,9 @@ const ActivityView = () => {
       screen: Routes.SHEET.ACCOUNT_SELECTOR,
     });
     // Track Event: "Opened Acount Switcher"
-    AnalyticsV2.trackEvent(
-      AnalyticsV2.ANALYTICS_EVENTS.BROWSER_OPEN_ACCOUNT_SWITCH,
-      {
-        number_of_accounts: Object.keys(accounts ?? {}).length,
-      },
-    );
+    AnalyticsV2.trackEvent(MetaMetricsEvents.BROWSER_OPEN_ACCOUNT_SWITCH, {
+      number_of_accounts: Object.keys(accounts ?? {}).length,
+    });
   }, [navigation, accounts]);
 
   useEffect(
