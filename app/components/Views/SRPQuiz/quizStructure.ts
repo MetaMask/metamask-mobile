467b5d49200c5476015a9b4ca4ac2a4becc996c5
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2023-01-19 18:18:49 -0300

    Add QuizStage enum

---
+++ app/components/Views/SRPQuiz/quizStructure.ts
@@ -0,0 +1,12 @@
+/* eslint-disable import/prefer-default-export */
+// import { strings } from '../../../../locales/i18n';
+
+export enum QuizStage {
+  introduction = 'introduction',
+  questionOne = 'question-one',
+  wrongAnswerQuestionOne = 'wrong-answer-questoin-one',
+  rightAnswerQuestionOne = 'right-answer-questoin-one',
+  questionTwo = 'question-two',
+  wrongAnswerQuestionTwo = 'wrong-answer-questoin-two',
+  rightAnswerQuestionTwo = 'right-answer-questoin-two',
+}
