4ff8ce9bc0920073537ab340cfdee906d832330b
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2023-01-19 18:19:52 -0300

    Update quiz components

--- app/components/Views/SRPQuiz/QuizInformation/QuizInformation.tsx
+++ app/components/Views/SRPQuiz/QuizInformation/QuizInformation.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { IQuizInformationProps } from '../types'
 import Button, {
   ButtonSize,
   ButtonVariants,
@@ -12,23 +13,40 @@ import { useStyles } from '../../../hooks/useStyles';
 import { strings } from '../../../../../locales/i18n';
 import stylesheet from './styles';
 
-const QuizInformation = ({ styles }) => {
+const QuizInformation = ({
+  title,
+  content,
+  btnLabel,
+  onContinuePress,
+}: IQuizInformationProps) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const navigation = useNavigation();
+  const { styles } = useStyles(stylesheet, {});
+
+  const openSupportArticle = () => {
+    // eslint-disable-next-line no-console
+    console.log('execute openSupportArticle method');
+  };
 
   return (
-    <View style={styles}>
-      <Text variant={TextVariants.lHeadingLG}>
-        {strings('srp_security_quiz.introduction')}
+    <View style={styles.bodyContainer}>
+      <Text variant={TextVariants.sHeadingLG} style={styles.title}>
+        {title}
       </Text>
+      {content ? (
+        <Text variant={TextVariants.sBodyMD} style={styles.content}>
+          {content}
+        </Text>
+      ) : null}
       <Button
         variant={ButtonVariants.Primary}
-        onPress={() => console.log('hello')}
-        label={strings('srp_security_quiz.get_started')}
+        onPress={onContinuePress}
+        label={btnLabel}
+        style={styles.button}
       />
       <Button
         variant={ButtonVariants.Tertiary}
-        onPress={() => console.log('hello')}
+        onPress={openSupportArticle}
         label={strings('srp_security_quiz.learn_more')}
       />
     </View>
--- app/components/Views/SRPQuiz/QuizModal/QuizModal.tsx
+++ app/components/Views/SRPQuiz/QuizModal/QuizModal.tsx
@@ -1,22 +1,105 @@
-import React, { useRef } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import { View } from 'react-native';
 // import { useNavigation } from '@react-navigation/native';
 import ReusableModal, { ReusableModalRef } from '../../../UI/ReusableModal';
+import Text, {
+  TextVariants,
+} from '../../../../component-library/components/Texts/Text';
 import Button, { ButtonSize, ButtonVariants } from '../../../../component-library/components/Buttons/Button';
+import Icon, {
+  IconSize,
+  IconName,
+} from '../../../../component-library/components/Icon';
 import { useStyles } from '../../../hooks/useStyles';
+import { strings } from '../../../../../locales/i18n';
 
+import { QuizStage } from '../quizStructure';
 import { QuizInformation } from '../QuizInformation';
 import stylesheet from './styles';
 
 const QuizModal = () => {
   const modalRef = useRef<ReusableModalRef>(null);
-  const { styles } = useStyles(stylesheet, {});
+  const [stage, setStage] = useState<QuizStage>(QuizStage.introduction);
+  const { styles, theme } = useStyles(stylesheet, {});
+  const { colors } = theme;
   // const navigation = useNavigation();
+
+  const wrongAnswerIcon = () => (
+    <Icon
+      size={IconSize.Xl}
+      name={IconName.DangerFilled}
+      color={colors.error.default}
+      // style={styles.icon}
+    />
+  );
+
+  const rightAnswerIcon = () => (
+    <Icon
+      size={IconSize.Xl}
+      name={IconName.CheckCircleOnFilled}
+      color={colors.success.default}
+      // style={styles.icon}
+    />
+  );
+
+  const quizPage = useCallback(() => {
+    switch (stage) {
+      case QuizStage.introduction:
+        return (
+          <QuizInformation
+            styles={styles.bodyContainer}
+            header={'Header 1'}
+            title={strings('srp_security_quiz.introduction')}
+            content={'Content 1'}
+            btnLabel={strings('srp_security_quiz.get_started')}
+            onContinuePress={() => setStage(QuizStage.questionOne)}
+          />
+        );
+      case QuizStage.questionOne:
+        return (
+          <QuizInformation
+            styles={styles.bodyContainer}
+            header={'Header 2'}
+            title={'Title 2'}
+            content={'Content 2'}
+            btnLabel={strings('srp_security_quiz.get_started')}
+            onContinuePress={() => setStage(QuizStage.questionTwo)}
+          />
+        );
+      case QuizStage.rightAnswerQuestionOne:
+        return <View />;
+      case QuizStage.wrongAnswerQuestionOne:
+        return <View />;
+      case QuizStage.questionTwo:
+        return <View />;
+      case QuizStage.rightAnswerQuestionTwo:
+        return <View />;
+      case QuizStage.wrongAnswerQuestionTwo:
+        return <View />;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [stage]);
 
   return (
     <ReusableModal ref={modalRef} style={styles.screen}>
       <View style={styles.modal}>
-        <QuizInformation styles={styles.bodyContainer} />
+        <View style={styles.header}>
+          <Text
+            variant={TextVariants.sHeadingSMRegular}
+            style={styles.headerText}
+          >
+            header
+          </Text>
+          <Icon
+            size={IconSize.Xs}
+            name={IconName.CloseOutline}
+            color={colors.icon.default}
+            style={styles.icon}
+          />
+        </View>
+        {wrongAnswerIcon()}
+        {rightAnswerIcon()}
+        {quizPage()}
       </View>
     </ReusableModal>
   );
