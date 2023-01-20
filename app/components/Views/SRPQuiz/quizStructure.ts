b6cd28b1e57e01f9fe9d812fc71d120cad4c8888
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2023-01-20 19:33:32 -0300

    Add quiz structure

--- app/components/Views/SRPQuiz/quizStructure.ts
+++ app/components/Views/SRPQuiz/quizStructure.ts
@@ -1,12 +1,59 @@
 /* eslint-disable import/prefer-default-export */
-// import { strings } from '../../../../locales/i18n';
+import { strings } from '../../../../locales/i18n';
 
-export enum QuizStage {
-  introduction = 'introduction',
-  questionOne = 'question-one',
-  wrongAnswerQuestionOne = 'wrong-answer-questoin-one',
-  rightAnswerQuestionOne = 'right-answer-questoin-one',
-  questionTwo = 'question-two',
-  wrongAnswerQuestionTwo = 'wrong-answer-questoin-two',
-  rightAnswerQuestionTwo = 'right-answer-questoin-two',
-}
+const headers = {
+  introduction: strings('srp_security_quiz.title'),
+  questionOne: `1 ${strings('srp_security_quiz.of')} 2`,
+  questionTwo: `2 ${strings('srp_security_quiz.of')} 2`,
+};
+
+export const quiz = {
+  introduction: {
+    header: headers.introduction,
+    title: strings('srp_security_quiz.introduction'),
+  },
+  questionOne: {
+    header: headers.questionOne,
+    content: strings('srp_security_quiz.question_one.question'),
+    answers: {
+      correct: strings('srp_security_quiz.question_one.right_answer'),
+      incorrect: strings('srp_security_quiz.question_one.wrong_answer'),
+    },
+  },
+  questionOneCorrectInformation: {
+    header: headers.questionOne,
+    title: strings('srp_security_quiz.question_one.right_answer_title'),
+    description: strings(
+      'srp_security_quiz.question_one.right_answer_description',
+    ),
+  },
+  questionOneIncorrectInformation: {
+    header: headers.questionOne,
+    title: strings('srp_security_quiz.question_one.wrong_answer_title'),
+    description: strings(
+      'srp_security_quiz.question_one.wrong_answer_description',
+    ),
+  },
+  questionTwo: {
+    header: headers.questionTwo,
+    content: strings('srp_security_quiz.question_one.question'),
+    answers: {
+      correct: strings('srp_security_quiz.question_two.right_answer'),
+      incorrect: strings('srp_security_quiz.question_two.wrong_answer'),
+    },
+  },
+  questionTwoCorrectInformation: {
+    header: headers.questionTwo,
+    title: strings('srp_security_quiz.question_two.right_answer_title'),
+    description: strings(
+      'srp_security_quiz.question_two.right_answer_description',
+    ),
+  },
+  questionTwoIncorrectInformation: {
+    header: headers.questionTwo,
+    title: strings('srp_security_quiz.question_two.wrong_answer_title'),
+    description: strings(
+      'srp_security_quiz.question_two.wrong_answer_description',
+    ),
+  },
+};
