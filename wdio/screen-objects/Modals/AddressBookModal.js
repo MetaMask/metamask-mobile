fd2ecb2671238b802bf53cb4d6771948d4e74920
Yande <andepandy@outlook.com>
2023-01-27 20:21:04 +0000

    Added missing Address Input ID

--- wdio/screen-objects/Modals/AddressBookModal.js
+++ wdio/screen-objects/Modals/AddressBookModal.js
@@ -3,6 +3,10 @@ import Selectors from '../../helpers/Selectors';
 import { ENTER_ALIAS_INPUT_BOX_ID } from '../testIDs/Screens/AddressBook.testids';
 
 class AddressBookModal {
+  get addressInputField() {
+    return Selectors.getElementByPlatform(ENTER_ALIAS_INPUT_BOX_ID);
+ }
+ 
   async fillAddressAliasField(text) {
     await Gestures.typeText(this.addressInputField, text);
   }
