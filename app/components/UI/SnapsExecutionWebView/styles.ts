56712f85350dde3d956b9528a34f8f815955ca6a
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2023-01-29 02:02:25 -0300

    Snaps developer UI

--- app/components/UI/SnapsExecutionWebView/styles.ts
+++ app/components/UI/SnapsExecutionWebView/styles.ts
@@ -5,7 +5,6 @@ import { StyleSheet } from 'react-native';
 export const createStyles = () =>
   StyleSheet.create({
     webview: {
-<<<<<<< HEAD
       height: 0,
       // marginBottom: 50,
       // borderWidth: 1,
@@ -17,17 +16,5 @@ export const createStyles = () =>
       // borderWidth: 1,
       // borderStyle: 'dashed',
       // borderColor: 'green',
-=======
-      height: 150,
-      borderWidth: 1,
-      borderStyle: 'dashed',
-      borderColor: 'red',
-    },
-    container: {
-      flex: 1,
-      borderWidth: 1,
-      borderStyle: 'dashed',
-      borderColor: 'green',
->>>>>>> 2c98d4f88 (Add styles to SnapsExecutionWebView for debugging)
     },
   });
