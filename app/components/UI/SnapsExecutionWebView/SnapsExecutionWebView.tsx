85c7ea3f9d1856890b9b8cc3effce39028838dc5
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2023-01-29 23:09:12 -0300

    Update UI

--- app/components/UI/SnapsExecutionWebView/SnapsExecutionWebView.tsx
+++ app/components/UI/SnapsExecutionWebView/SnapsExecutionWebView.tsx
@@ -1,14 +1,8 @@
 import React, { useRef } from 'react';
-import { View, ScrollView, Alert } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import WebView from 'react-native-webview';
 import { snapsState, WebviewPostMessageStream } from '../../../core/Snaps';
 import { createStyles } from './styles';
-
-import Engine from '../../../core/Engine';
-
-const INSTALL_SUCCESS_MESSAGE = (id: string) => `Snap ${id} installed 🎉🎉🎉`;
-const INSTALL_FAILED_MESSAGE = (id: string) =>
-  `Snap ${id} failed to install 💀💀💀`;
 
 let stream: any;
 
@@ -16,36 +10,6 @@ const SnapsExecutionWebView = () => {
   const styles = createStyles();
 
   const webviewRef = useRef();
-
-  const installSnap = async (snapId: string): Promise<void> => {
-    const mockOrigin = 'origin';
-    const { SnapController } = Engine.context as any;
-    await SnapController.installSnaps(mockOrigin, { [snapId]: {} });
-    // await snapController.terminateSnap(snapId);
-  };
-
-  const executeTestSnap = async (snapId: string) => {
-    // eslint-disable-next-line no-console
-    const { SnapController } = Engine.context as any;
-    const localSnap = snapId;
-    const origin = 'origin';
-    const result = await SnapController.handleRequest({
-      snapId: localSnap,
-      origin,
-      handler: 'onRpcRequest',
-      request: {
-        method: 'hello',
-      },
-    });
-    // eslint-disable-next-line no-console
-    console.log(result);
-  };
-
-  const getInstalledSnaps = () => {
-    const { SnapController } = Engine.context as any;
-    // eslint-disable-next-line no-console
-    console.log(SnapController.internalState.snaps);
-  };
 
   const setWebviewPostMessage = () => {
     stream = new WebviewPostMessageStream({
