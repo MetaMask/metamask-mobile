ff9b159beb81cc8c8d680c8a94c8ee323bc800e4
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2023-01-30 14:23:17 -0300

    Update permissions

--- app/constants/permissions.ts
+++ app/constants/permissions.ts
@@ -3,6 +3,8 @@ export const EndowmentPermissions = Object.freeze({
   'endowment:long-running': 'endowment:long-running',
   'endowment:transaction-insight': 'endowment:transaction-insight',
   'endowment:cronjob': 'endowment:cronjob',
+  'endowment:ethereum-provider': 'endowment:ethereum-provider',
+  'endowment:rpc': 'endowment:rpc',
 } as const);
 
 export enum USER_INTENT {
