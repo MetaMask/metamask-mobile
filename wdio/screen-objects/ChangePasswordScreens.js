33aa8f3f9ea71e57a0f896c9d5f6ee83db1bb92b
CW <chris.wilcox@consensys.net>
2023-02-02 19:08:10 -0800

    change password IDs

--- wdio/screen-objects/ChangePasswordScreens.js
+++ wdio/screen-objects/ChangePasswordScreens.js
@@ -1,15 +1,21 @@
 import Gestures from '../helpers/Gestures';
 import Selectors from '../helpers/Selectors';
+import {
+  RESET_PASSWORD_INPUT_ID,
+  RESET_PASSWORD_CONFIRM_BUTTON_ID,
+  RESET_PASSWORD_TERM_CHECKBOX_ID,
+} from './testIDs/Screens/ChangePasswordScreensIDs.testIds';
+
 
 class ChangePasswordScreens {
   get passwordInput() {
-    return Selectors.getElementByPlatform('private-credential-password-input-field');
+    return Selectors.getElementByPlatform(RESET_PASSWORD_INPUT_ID);
   }
   get buttonCONFIRM(){
-    return Selectors.getElementByPlatform('submit-button');
+    return Selectors.getElementByPlatform(RESET_PASSWORD_CONFIRM_BUTTON_ID);
   }
   get termsAndConditionCheckBox() {
-    return Selectors.getElementByPlatform('i-understand-text');
+    return Selectors.getElementByPlatform(RESET_PASSWORD_TERM_CHECKBOX_ID);
   }
   async typePassword(text) {
     const elem = await (this.passwordInput);
---
+++ wdio/screen-objects/testIDs/Screens/ChangePasswordScreensIDs.testIds.js
@@ -0,0 +1,3 @@
+export const RESET_PASSWORD_INPUT_ID = 'private-credential-password-input-field';
+export const RESET_PASSWORD_CONFIRM_BUTTON_ID = 'submit-button';
+export const RESET_PASSWORD_TERM_CHECKBOX_ID = 'i-understand-text';
