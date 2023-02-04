82f063c9612c877f96d9690516ada5c7955c651e
Brian August Nguyen <brianacnguyen@gmail.com>
2023-02-04 13:58:01 -0800

    Componentize TextInput (#5682)
    * Added TextInput to morph
    
    * Updated focused state
    
    * Addressed comments

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -28,7 +28,7 @@ function loadStories() {
   require('../app/component-library/components/Cells/Cell/variants/CellMultiselect/CellMultiselect.stories');
   require('../app/component-library/components/Cells/Cell/variants/CellSelect/CellSelect.stories');
   require('../app/component-library/components/Checkbox/Checkbox.stories');
-  require('../app/component-library/components-temp/CustomSpendCap/CustomSpendCap.stories');
+  require('../app/component-library/components/Form/TextInput/TextInput.stories');
   require('../app/component-library/components/Icon/Icon.stories');
   require('../app/component-library/components/Modals/ModalConfirmation/ModalConfirmation.stories');
   require('../app/component-library/components/Navigation/TabBar/TabBar.stories');
@@ -56,6 +56,7 @@ function loadStories() {
   require('../app/components/UI/WarningAlert/WarningAlert.stories');
   require('../app/component-library/components-temp/Accounts/AccountBalance/AccountBalance.stories');
   require('../app/component-library/components-temp/Contracts/ContractBox/ContractBox.stories');
+  require('../app/component-library/components-temp/CustomSpendCap/CustomSpendCap.stories');
 }
 
 const stories = [
@@ -83,8 +84,7 @@ const stories = [
   '../app/component-library/components/Cells/Cell/variants/CellMultiselect/CellMultiselect.stories',
   '../app/component-library/components/Cells/Cell/variants/CellSelect/CellSelect.stories',
   '../app/component-library/components/Checkbox/Checkbox.stories',
-  '../app/component-library/components-temp/ContractBox/ContractBox.stories',
-  '../app/component-library/components-temp/CustomSpendCap/CustomSpendCap.stories',
+  '../app/component-library/components/Form/TextInput/TextInput.stories',
   '../app/component-library/components/Icon/Icon.stories',
   '../app/component-library/components/Modals/ModalConfirmation/ModalConfirmation.stories',
   '../app/component-library/components/Navigation/TabBar/TabBar.stories',
@@ -103,7 +103,7 @@ const stories = [
   '../app/component-library/components/Toast/Toast.stories',
   '../app/components/Base/Alert.stories',
   '../app/components/Base/ListItem.stories',
-  '../app/components/Base/Text.stories',
+  '../app/components/Base/Text/Text.stories',
   '../app/components/Base/Title.stories',
   '../app/components/UI/ButtonReveal/ButtonReveal.stories',
   '../app/components/UI/Fox/Fox.stories',
@@ -112,6 +112,7 @@ const stories = [
   '../app/components/UI/WarningAlert/WarningAlert.stories',
   '../app/component-library/components-temp/Accounts/AccountBalance/AccountBalance.stories',
   '../app/component-library/components-temp/Contracts/ContractBox/ContractBox.stories',
+  '../app/component-library/components-temp/CustomSpendCap/CustomSpendCap.stories',
 ];
 
 module.exports = {
