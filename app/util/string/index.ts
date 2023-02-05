6726464d2e978e9d447d3341989e58efa26ae450
Jyoti Puri <jyotipuri@gmail.com>
2023-02-05 05:37:27 +0530

    Improve signature request message

---
+++ app/util/string/index.ts
@@ -0,0 +1,14 @@
+/**
+ * The method escape RTL character in string
+ *
+ * @param {string} str
+ * @returns {(string|*)} escaped string or original param value
+ */
+export const sanitizeString = (str: string): string => {
+    if (!str) {
+        return str;
+    }
+    const regex = /\u202E/giu;
+    // @ts-ignore
+    return str.replaceAll(regex, '\\u202E');
+};
