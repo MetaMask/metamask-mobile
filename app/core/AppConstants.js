ada4f3c02c30e4d2c673f12a5453eb87b96c924d
Tomás SANTOS <tommasini15@gmail.com>
2023-02-09 12:46:28 +0000

    remove margin bottom

--- app/core/AppConstants.js
+++ app/core/AppConstants.js
@@ -131,10 +131,9 @@ export default {
   NETWORK_STATE_CHANGE_EVENT: 'NetworkController:stateChange',
   ETH_SIGN_ERROR: 'eth_sign requires 32 byte message hash',
   TERMS_OF_USE: {
-    SCROLL_END_BOTTOM_MARGIN: 1050,
     TERMS_DISPLAYED: 'ToU Displayed',
     TERMS_ACCEPTED: 'ToU Accepted',
     TERMS_OF_USE_URL_WITHOUT_COOKIES:
-      'https://consensys.net/terms-of-use?gdpr=disable',
+      'https://consensys.net/terms-of-use?standalone=true',
   },
 };
--- app/util/termsOfUse/termsOfUse.ts
+++ app/util/termsOfUse/termsOfUse.ts
@@ -40,8 +40,6 @@ export default async function navigateTermsOfUse(
         },
         onRender: useTermsDisplayed,
         isScrollToEndNeeded: true,
-        scrollEndBottomMargin:
-          AppConstants.TERMS_OF_USE.SCROLL_END_BOTTOM_MARGIN,
       },
     });
   }
