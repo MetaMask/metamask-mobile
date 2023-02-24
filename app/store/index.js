b0dbd86e088a9a8dbc48788b96bca12b211ebfc1
João Loureiro <joao.loureiro@consensys.net>
2023-02-24 11:33:45 +0000

    TESTcomment unused vars

--- app/store/index.js
+++ app/store/index.js
@@ -5,57 +5,57 @@ import {
   createMigrate,
   createTransform,
 } from 'redux-persist';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import FilesystemStorage from 'redux-persist-filesystem-storage';
+/* import AsyncStorage from '@react-native-async-storage/async-storage'; */
+/* import FilesystemStorage from 'redux-persist-filesystem-storage'; */
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import rootReducer from '../reducers';
 import { migrations, version } from './migrations';
 import Logger from '../util/Logger';
 import EngineService from '../core/EngineService';
-import Device from '../util/device';
+/* import Device from '../util/device'; */
 import { MMKVLoader } from 'react-native-mmkv-storage';
 
 const TIMEOUT = 40000;
 
-const MigratedStorage = {
-  async getItem(key) {
-    try {
-      const res = await FilesystemStorage.getItem(key);
-      if (res) {
-        // Using new storage system
-        return res;
-      }
-    } catch {
-      //Fail silently
-    }
-
-    // Using old storage system, should only happen once
-    try {
-      const res = await AsyncStorage.getItem(key);
-      if (res) {
-        // Using old storage system
-        return res;
-      }
-    } catch (error) {
-      Logger.error(error, { message: 'Failed to run migration' });
-      throw new Error('Failed async storage storage fetch.');
-    }
-  },
-  async setItem(key, value) {
-    try {
-      return await FilesystemStorage.setItem(key, value, Device.isIos());
-    } catch (error) {
-      Logger.error(error, { message: 'Failed to set item' });
-    }
-  },
-  async removeItem(key) {
-    try {
-      return await FilesystemStorage.removeItem(key);
-    } catch (error) {
-      Logger.error(error, { message: 'Failed to remove item' });
-    }
-  },
-};
+/* const MigratedStorage = { */
+/*   async getItem(key) { */
+/*     try { */
+/*       const res = await FilesystemStorage.getItem(key); */
+/*       if (res) { */
+/*         // Using new storage system */
+/*         return res; */
+/*       } */
+/*     } catch { */
+/*       //Fail silently */
+/*     } */
+/**/
+/*     // Using old storage system, should only happen once */
+/*     try { */
+/*       const res = await AsyncStorage.getItem(key); */
+/*       if (res) { */
+/*         // Using old storage system */
+/*         return res; */
+/*       } */
+/*     } catch (error) { */
+/*       Logger.error(error, { message: 'Failed to run migration' }); */
+/*       throw new Error('Failed async storage storage fetch.'); */
+/*     } */
+/*   }, */
+/*   async setItem(key, value) { */
+/*     try { */
+/*       return await FilesystemStorage.setItem(key, value, Device.isIos()); */
+/*     } catch (error) { */
+/*       Logger.error(error, { message: 'Failed to set item' }); */
+/*     } */
+/*   }, */
+/*   async removeItem(key) { */
+/*     try { */
+/*       return await FilesystemStorage.removeItem(key); */
+/*     } catch (error) { */
+/*       Logger.error(error, { message: 'Failed to remove item' }); */
+/*     } */
+/*   }, */
+/* }; */
 
 /**
  * Transform middleware that blacklists fields from redux persist that we deem too large for persisted storage
