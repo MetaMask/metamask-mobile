c2c5d27196ace9858311fa93864020b85417c90e
João Loureiro <joao.loureiro@consensys.net>
2023-03-01 23:46:24 +0000

    remove react-native patch for now. deal with that later on

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -17,17 +17,3 @@ echo "" > ios/release.xcconfig
 echo "5. Init git submodules"
 echo "This may take a while..."
 git submodule update --init
-
-echo "6. Setup react-native-codegen"
-# Since we've patched React Native, Android requires us to build from source (ReactAndroid).
-# ReactAndroid needs to reference react-native-codegen's android directory, which we are manually fetching here
-
-REACT_NATIVE_VERSION="0.66.0"
-
-rm -rf node_modules/react-native/packages
-mkdir -p node_modules/react-native/packages && cd node_modules/react-native/packages
-curl --proto '=https' --tlsv1.2 -LJO https://github.com/facebook/react-native/archive/refs/tags/v${REACT_NATIVE_VERSION}.tar.gz
-tar -zxvf react-native-${REACT_NATIVE_VERSION}.tar.gz react-native-${REACT_NATIVE_VERSION}/packages/react-native-codegen/
-mv react-native-0.66.0/packages/react-native-codegen/ .
-# cleanup
-rm react-native-${REACT_NATIVE_VERSION}.tar.gz
