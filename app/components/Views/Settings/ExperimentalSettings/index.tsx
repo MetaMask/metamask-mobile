b714e29a72cc3a2263a7b1b0e6b4a3a600b69333
Arthur Breton <arthur.breton@consensys.net>
2023-03-02 22:50:17 +0800

    feat: remove sdk from experimental settings

--- app/components/Views/Settings/ExperimentalSettings/index.tsx
+++ app/components/Views/Settings/ExperimentalSettings/index.tsx
@@ -1,14 +1,10 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { StyleSheet, Text, ScrollView, View } from 'react-native';
-import StyledButton from '../../../UI/StyledButton';
-import { fontStyles } from '../../../../styles/common';
-import { getNavigationOptionsTitle } from '../../../UI/Navbar';
+import React, { useCallback, useEffect } from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { strings } from '../../../../../locales/i18n';
-import ActionModal from '../../../../components/UI/ActionModal';
-import SDKConnect from '../../../../core/SDKConnect/SDKConnect';
+import { fontStyles } from '../../../../styles/common';
 import { useTheme } from '../../../../util/theme';
-import DefaultPreference from 'react-native-default-preference';
-import AppConstants from '../../../../core/AppConstants';
+import { getNavigationOptionsTitle } from '../../../UI/Navbar';
+import StyledButton from '../../../UI/StyledButton';
 
 const createStyles = (colors: any) =>
   StyleSheet.create({
@@ -80,9 +76,6 @@ interface Props {
  * Main view for app Experimental Settings
  */
 const ExperimentalSettings = ({ navigation, route }: Props) => {
-  const [showClearMMSDKConnectionsModal, setshowClearMMSDKConnectionsModal] =
-    useState(false);
-
   const isFullScreenModal = route?.params?.isFullScreenModal;
   const { colors } = useTheme();
   const styles = createStyles(colors);
@@ -107,61 +100,8 @@ const ExperimentalSettings = ({ navigation, route }: Props) => {
     navigation.navigate('WalletConnectSessionsView');
   }, [navigation]);
 
-  const toggleClearMMSDKConnectionModal = () => {
-    setshowClearMMSDKConnectionsModal((show) => !show);
-  };
-
-  const clearMMSDKConnections = async () => {
-    SDKConnect.getInstance().removeAll();
-    await DefaultPreference.set(
-      AppConstants.MM_SDK.SDK_CONNECTIONS,
-      JSON.stringify({}),
-    );
-    await DefaultPreference.set(
-      AppConstants.MM_SDK.SDK_APPROVEDHOSTS,
-      JSON.stringify({}),
-    );
-    toggleClearMMSDKConnectionModal();
-  };
-  const renderMMSDKConnectionsModal = () => (
-    <ActionModal
-      modalVisible={showClearMMSDKConnectionsModal}
-      confirmText={strings('app_settings.clear')}
-      cancelText={strings('app_settings.reset_account_cancel_button')}
-      onCancelPress={toggleClearMMSDKConnectionModal}
-      onRequestClose={toggleClearMMSDKConnectionModal}
-      onConfirmPress={clearMMSDKConnections}
-    >
-      <View style={styles.modalView}>
-        <Text style={styles.modalTitle}>
-          {strings('app_settings.clear_sdk_connections_title')}
-        </Text>
-        <Text style={styles.modalText}>
-          {strings('app_settings.clear_sdk_connections_text')}
-        </Text>
-      </View>
-    </ActionModal>
-  );
-
   return (
     <ScrollView style={styles.wrapper}>
-      <View style={styles.setting}>
-        <View>
-          <Text style={styles.title}>
-            {strings('app_settings.clear_sdk_connections_title')}
-          </Text>
-          <Text style={styles.desc}>
-            {strings('app_settings.clear_sdk_connections_text')}
-          </Text>
-          <StyledButton
-            type="signingCancel"
-            onPress={toggleClearMMSDKConnectionModal}
-            containerStyle={styles.clearHistoryConfirm}
-          >
-            {strings('app_settings.clear_sdk_connections_title')}
-          </StyledButton>
-        </View>
-      </View>
       <View style={styles.setting}>
         <View>
           <Text style={styles.title}>
@@ -179,7 +119,6 @@ const ExperimentalSettings = ({ navigation, route }: Props) => {
           </StyledButton>
         </View>
       </View>
-      {renderMMSDKConnectionsModal()}
     </ScrollView>
   );
 };
