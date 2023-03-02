0ab615a1988912d7d08556b2ce0cfe2ae518f02a
Tomás SANTOS <tommasini15@gmail.com>
2023-03-02 00:48:22 +0000

    fix track ecent

--- app/util/termsOfUse/termsOfUse.ts
+++ app/util/termsOfUse/termsOfUse.ts
@@ -1,20 +1,20 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import AppConstants from '../../core/AppConstants';
 import { MetaMetricsEvents } from '../../core/Analytics';
-import AnalyticsV2 from '../analyticsV2';
+import { trackEvent } from '../analyticsV2';
 import { TRUE, USE_TERMS } from '../../constants/storage';
 import Routes from '../../constants/navigation/Routes';
 import { strings } from '../../../locales/i18n';
 
 const onConfirmUseTerms = async () => {
   await AsyncStorage.setItem(USE_TERMS, TRUE);
-  AnalyticsV2.trackEvent(MetaMetricsEvents.USER_TERMS, {
+  trackEvent(MetaMetricsEvents.USER_TERMS, {
     value: AppConstants.TERMS_OF_USE.TERMS_ACCEPTED,
   });
 };
 
 const useTermsDisplayed = () => {
-  AnalyticsV2.trackEvent(MetaMetricsEvents.USER_TERMS, {
+  trackEvent(MetaMetricsEvents.USER_TERMS, {
     value: AppConstants.TERMS_OF_USE.TERMS_DISPLAYED,
   });
 };
