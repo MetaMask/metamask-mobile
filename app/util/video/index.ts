708874522b96b505533176aaa7521eeb65e0cf1d
Nicolas MASSART <nicolas.massart@consensys.net>
2023-03-06 13:11:13 +0100

    Move SRP subtitles remote path template to constants file

--- app/util/video/index.ts
+++ app/util/video/index.ts
@@ -1,22 +1,14 @@
 import _ from 'lodash';
 import videoSourceUri from '../../videos/recovery-phrase-mobile-2pass-optimized.mp4';
-
-// Remote path for subtitles that can't be bundled with the app
-// because only remote subtitles are supported by react-native-video
-const baseRemoteSubtitlesPath =
-  'https://github.com/MetaMask/metamask-mobile/blob/main/app/';
-const baseSubtitlesPath = 'videos/subtitles/secretPhrase/';
-const subtitleFileFormat = 'subtitles-${language}.vtt';
-const gitHubRawPathParam = '?raw=true';
-const subtitlePathTemplate = `${baseRemoteSubtitlesPath}${baseSubtitlesPath}${subtitleFileFormat}${gitHubRawPathParam}`;
+import { SRP_SUBTITLES_URL_TEMPLATE } from '../../constants/urls';
 
 /**
  * Returns the subtitle URI for a given language
  * @param language
  */
 export function getSubtitleUri(language: string): string {
-  return _.template(subtitlePathTemplate)({ language });
+  return _.template(SRP_SUBTITLES_URL_TEMPLATE)({ language });
 }
 
-// Bundled recovery phrase video local path
+// Bundled recovery phrase video resource
 export const video_source_uri = videoSourceUri;
