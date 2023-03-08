676d478aad8e129f261e9cf5da0e259f58b080c1
sethkfman <Seth.Kaufman@consensys.net>
2023-03-08 15:36:17 -0700

    update securekeychain path

--- app/components/Views/ChoosePassword/index.js
+++ app/components/Views/ChoosePassword/index.js
@@ -69,7 +69,7 @@ import {
 import { LoginOptionsSwitch } from '../../UI/LoginOptionsSwitch';
 import generateTestId from '../../../../wdio/utils/generateTestId';
 import { scale } from 'react-native-size-matters';
-import SecureKeychain from 'app/core/SecureKeychain';
+import SecureKeychain from '../../../core/SecureKeychain';
 
 const createStyles = (colors) =>
   StyleSheet.create({
