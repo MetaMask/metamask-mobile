c4fbb4f5f2f693a7223f7aca6ed490c4eb1a86c4
Brian Nguyen <brianacnguyen@gmail.com>
2023-03-07 20:32:34 -0800

    Fixed lint issue

--- app/component-library/components/Form/TextFieldSearch/TextFieldSearch.test.tsx
+++ app/component-library/components/Form/TextFieldSearch/TextFieldSearch.test.tsx
@@ -4,11 +4,7 @@ import { shallow } from 'enzyme';
 
 // Internal dependencies.
 import TextFieldSearch from './TextFieldSearch';
-import {
-  TEXTFIELDSEARCH_TEST_ID,
-  TEXTFIELDSEARCH_SEARCHICON_TEST_ID,
-  TEXTFIELDSEARCH_CLEARBUTTON_TEST_ID,
-} from './TextFieldSearch.constants';
+import { TEXTFIELDSEARCH_TEST_ID } from './TextFieldSearch.constants';
 
 describe('TextFieldSearch', () => {
   it('should render default settings correctly', () => {
@@ -19,20 +15,6 @@ describe('TextFieldSearch', () => {
     const wrapper = shallow(<TextFieldSearch />);
     const textFieldSearchComponent = wrapper.findWhere(
       (node) => node.prop('testID') === TEXTFIELDSEARCH_TEST_ID,
-    );
-    expect(textFieldSearchComponent.exists()).toBe(true);
-  });
-  it('should render the search icon', () => {
-    const wrapper = shallow(<TextFieldSearch />);
-    const textFieldSearchIconComponent = wrapper.findWhere(
-      (node) => node.prop('testID') === TEXTFIELDSEARCH_SEARCHICON_TEST_ID,
-    );
-    expect(textFieldSearchIconComponent.exists()).toBe(true);
-  });
-  it('should render the clearButton if showClearButton=true', () => {
-    const wrapper = shallow(<TextFieldSearch showClearButton />);
-    const textFieldSearchComponent = wrapper.findWhere(
-      (node) => node.prop('testID') === TEXTFIELDSEARCH_CLEARBUTTON_TEST_ID,
     );
     expect(textFieldSearchComponent.exists()).toBe(true);
   });
