d214becee9c0f679f11ffdb664160ad1a3b58748
Sylva Elendu <iamsylvaelendu@gmail.com>
2023-03-14 15:36:42 +0100

    update the verify transaction details for custom spend cap

--- app/components/UI/ApproveTransactionReview/index.js
+++ app/components/UI/ApproveTransactionReview/index.js
@@ -301,6 +301,8 @@ class ApproveTransactionReview extends PureComponent {
     const { chainId } = this.props;
     const {
       transaction: { origin, to, data, from },
+      transaction,
+      setTransactionObject,
       tokenList,
     } = this.props;
     const { AssetsContractController, TokenBalancesController } =
@@ -369,6 +371,21 @@ class ApproveTransactionReview extends PureComponent {
     const { name: method } = await getMethodData(data);
     const minTokenAllowance = minimumTokenAllowance(tokenDecimals);
 
+    const newApprovalTransaction = generateTxWithNewTokenAllowance(
+      '0',
+      tokenDecimals,
+      spenderAddress,
+      transaction,
+    );
+
+    setTransactionObject({
+      ...newApprovalTransaction,
+      transaction: {
+        ...newApprovalTransaction.transaction,
+        data: newApprovalTransaction.data,
+      },
+    });
+
     this.setState(
       {
         host,
@@ -400,6 +417,32 @@ class ApproveTransactionReview extends PureComponent {
         this.fetchEstimatedL1Fee,
         POLLING_INTERVAL_ESTIMATED_L1_FEE,
       );
+    }
+  };
+
+  componentDidUpdate = (_, prevState) => {
+    const { transaction, setTransactionObject } = this.props;
+    const {
+      tokenSpendValue,
+      spenderAddress,
+      token: { tokenDecimals },
+    } = this.state;
+
+    if (prevState?.tokenSpendValue !== tokenSpendValue) {
+      const newApprovalTransaction = generateTxWithNewTokenAllowance(
+        tokenSpendValue,
+        tokenDecimals,
+        spenderAddress,
+        transaction,
+      );
+
+      setTransactionObject({
+        ...newApprovalTransaction,
+        transaction: {
+          ...newApprovalTransaction.transaction,
+          data: newApprovalTransaction.data,
+        },
+      });
     }
   };
 
@@ -892,17 +935,12 @@ class ApproveTransactionReview extends PureComponent {
       host,
       method,
       viewData,
-      originalApproveAmount,
-      spendLimitUnlimitedSelected,
-      spendLimitCustomValue,
+      tokenSpendValue,
       token: { tokenStandard, tokenSymbol, tokenValue, tokenName },
     } = this.state;
     const {
       transaction: { to, data },
     } = this.props;
-    const allowance =
-      (!spendLimitUnlimitedSelected && spendLimitCustomValue) ||
-      originalApproveAmount;
     return (
       <TransactionReviewDetailsCard
         toggleViewDetails={this.toggleViewDetails}
@@ -912,7 +950,7 @@ class ApproveTransactionReview extends PureComponent {
         nicknameExists={nicknameExists}
         address={to}
         host={host}
-        allowance={allowance}
+        tokenSpendValue={tokenSpendValue}
         tokenSymbol={tokenSymbol}
         data={data}
         tokenValue={tokenValue}
