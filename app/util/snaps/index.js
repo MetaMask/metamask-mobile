73e314590a4071429aad91935320cd2ec01f0696
Owen Craston <owengc12@gmail.com>
2023-03-17 12:17:40 -0700

    remove fetchNPMPackage

--- app/util/snaps/index.js
+++ app/util/snaps/index.js
@@ -1,5 +1,4 @@
 import { SNAP_BLOCKLIST } from './snap-blocklist';
 import checkSnapsBlockList from './checkSnapsBlockList';
-import { fetchNPMPackage } from './fetchNPMPackage';
 
-export { checkSnapsBlockList, SNAP_BLOCKLIST, fetchNPMPackage };
+export { checkSnapsBlockList, SNAP_BLOCKLIST };
