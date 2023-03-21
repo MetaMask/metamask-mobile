f59c4e6d682e3c12835371bc78dc5640903b3f35
Arthur Breton <arthur.breton@consensys.net>
2023-03-21 08:16:16 +0800

    feat: replace navigation.goback with sheetRef.hide

--- app/components/Views/SDKFeedbackModal/SDKFeedbackModal.tsx
+++ app/components/Views/SDKFeedbackModal/SDKFeedbackModal.tsx
@@ -1,6 +1,5 @@
 import SDKFeedback from '../../../../app/components/UI/SDKFeedback';
 import React, { useRef } from 'react';
-import { useNavigation } from '@react-navigation/native';
 
 import SheetBottom, {
   SheetBottomRef,
@@ -8,13 +7,12 @@ import SheetBottom, {
 
 const SDKLoadingModal = () => {
   const sheetRef = useRef<SheetBottomRef>(null);
-  const navigation = useNavigation();
 
   return (
     <SheetBottom ref={sheetRef}>
       <SDKFeedback
         onConfirm={() => {
-          navigation.goBack();
+          sheetRef.current?.hide();
         }}
       />
     </SheetBottom>
