d7e4d514f20f58de8f0c89a94231b35bde1c4c1e
Yande <andepandy@outlook.com>
2023-03-23 14:21:39 +0000

    Revert "temp removal of all tests to check bitrise run"
    This reverts commit 24c99c2e6b37f278847cb42b7b23b999228f51ff.

---
+++ e2e/specs/confirmations/send-eth-flow.spec.js
@@ -0,0 +1,53 @@
+'use strict';
+
+import AmountView from '../../pages/AmountView';
+import SendView from '../../pages/SendView';
+import TransactionConfirmationView from '../../pages/TransactionConfirmView';
+import WalletView from '../../pages/WalletView';
+import {
+  importWalletWithRecoveryPhrase,
+  switchToGoreliNetwork,
+} from '../../viewHelper';
+
+const MYTH_ADDRESS = '0x1FDb169Ef12954F20A15852980e1F0C122BfC1D6';
+
+describe('Send ETH Tests', () => {
+  beforeEach(() => {
+    jest.setTimeout(150000);
+  });
+
+  it('should go to send view', async () => {
+    await importWalletWithRecoveryPhrase();
+    await switchToGoreliNetwork();
+    // Navigate to send flow
+    await WalletView.tapSendIcon();
+    // Make sure view with my accounts visible
+    await SendView.isTransferBetweenMyAccountsButtonVisible();
+  });
+
+  it('should input a valid address to send to', async () => {
+    await SendView.inputAddress(MYTH_ADDRESS);
+    await SendView.noEthWarningMessageIsVisible();
+    await SendView.tapNextButton();
+    // Check that we are on the amount view
+    await AmountView.isVisible();
+  });
+
+  it('should input and validate amount', async () => {
+    // Input acceptable value
+    await AmountView.typeInTransactionAmount('0');
+    await AmountView.tapNextButton();
+
+    // Check that we are on the confirm view
+    await TransactionConfirmationView.isVisible();
+  });
+
+  it('should send ETH to Account 2', async () => {
+    // Check that the amount is correct
+    await TransactionConfirmationView.isTransactionTotalCorrect('0 GoerliETH');
+    // Tap on the Send CTA
+    await TransactionConfirmationView.tapConfirmButton();
+    // Check that we are on the wallet screen
+    await WalletView.isVisible();
+  });
+});
