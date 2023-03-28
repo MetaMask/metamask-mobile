3d6b6a0e4d60a3b8450c2b455d0be46cc540b86f
Mark Stacey <markjstacey@gmail.com>
2023-03-28 15:43:37 -0230

    Inline functions from `@metamask/utils`
    The two functions taken from `@metamask/utils` have been inlined as a
    workaround to an error that gets thrown upon import on Android builds.
    
    I'm still actively investigating that failure, so hopefully we can
    replace these inlined functions soon.

--- app/core/RPCMethods/eth_sendTransaction.ts
+++ app/core/RPCMethods/eth_sendTransaction.ts
@@ -3,8 +3,47 @@ import type {
   JsonRpcRequest,
   PendingJsonRpcResponse,
 } from 'json-rpc-engine';
-import { isObject, hasProperty } from '@metamask/utils';
 import { ethErrors } from 'eth-json-rpc-errors';
+
+/**
+ * A JavaScript object that is not `null`, a function, or an array.
+ *
+ * TODO: replace this with `RuntimeObject` from `@metamask/utils`
+ */
+type RuntimeObject = Record<PropertyKey, unknown>;
+
+/**
+ * A type guard for {@link RuntimeObject}.
+ *
+ * TODO: replace this with `isObject` from `@metamask/utils`
+ *
+ * @param value - The value to check.
+ * @returns Whether the specified value has a runtime type of `object` and is
+ * neither `null` nor an `Array`.
+ */
+function isObject(value: unknown): value is RuntimeObject {
+  return Boolean(value) && typeof value === 'object' && !Array.isArray(value);
+}
+
+/**
+ * A type guard for ensuring an object has a property.
+ *
+ * TODO: replace this with `hasProperty` from `@metamask/utils`
+ *
+ * @param objectToCheck - The object to check.
+ * @param name - The property name to check for.
+ * @returns Whether the specified object has an own property with the specified
+ * name, regardless of whether it is enumerable or not.
+ */
+const hasProperty = <
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  ObjectToCheck extends Object,
+  Property extends PropertyKey,
+>(
+  objectToCheck: ObjectToCheck,
+  name: Property,
+): objectToCheck is ObjectToCheck & Record<Property, unknown> =>
+  Object.hasOwnProperty.call(objectToCheck, name);
 
 /**
  * Handle a `eth_sendTransaction` request.
