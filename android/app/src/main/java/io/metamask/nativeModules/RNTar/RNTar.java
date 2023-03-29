bfc7f3e5adffb7d01a3fc71d217f388954d97c7c
Owen Craston <owengc12@gmail.com>
2023-03-29 09:42:21 -0700

    use apache compress and return value

--- android/app/src/main/java/io/metamask/nativeModules/RNTar/RNTar.java
+++ android/app/src/main/java/io/metamask/nativeModules/RNTar/RNTar.java
@@ -16,9 +16,8 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.zip.GZIPInputStream;
-import java.util.zip.TarEntry;
-import java.util.zip.TarInputStream;
-
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 
 public class RNTar extends ReactContextBaseJavaModule {
   private static String MODULE_NAME = "RNTar";
@@ -34,51 +33,53 @@ public class RNTar extends ReactContextBaseJavaModule {
   }
 
 
-  private String decompressTgzFile(String sourceFilePath, String destinationDirPath) {
-  try {
-      FileInputStream fis = new FileInputStream(sourceFilePath);
-      GZIPInputStream gzis = new GZIPInputStream(fis);
-      TarInputStream tis = new TarInputStream(gzis);
+  private String extractTgzFile(String sourceFilePath, String destinationDirPath) throws Exception {
+      try {
+          FileInputStream fis = new FileInputStream(sourceFilePath);
+          GZIPInputStream gzis = new GZIPInputStream(fis);
+          TarArchiveInputStream tis = new TarArchiveInputStream(gzis);
 
-      File destinationDir = new File(destinationDirPath);
-      if (!destinationDir.exists()) {
-          destinationDir.mkdirs();
-      }
-
-      TarEntry entry;
-      while ((entry = tis.getNextEntry()) != null) {
-          String fileName = entry.getName();
-          File outputFile = new File(destinationDirPath + File.separator + fileName);
-
-          if (entry.isDirectory()) {
-              if (!outputFile.exists()) {
-                  outputFile.mkdirs();
-              }
-          } else {
-              byte[] buffer = new byte[1024];
-              FileOutputStream fos = new FileOutputStream(outputFile);
-              int len;
-              while ((len = tis.read(buffer)) > 0) {
-                  fos.write(buffer, 0, len);
-              }
-              fos.close();
+          File destinationDir = new File(destinationDirPath);
+          if (!destinationDir.exists()) {
+              destinationDir.mkdirs();
           }
+
+        TarArchiveEntry entry;
+          while ((entry = tis.getCurrentEntry()) != null) {
+              String fileName = entry.getName();
+              File outputFile = new File(destinationDirPath + File.separator + fileName);
+
+              if (entry.isDirectory()) {
+                  if (!outputFile.exists()) {
+                      outputFile.mkdirs();
+                  }
+              } else {
+                  byte[] buffer = new byte[1024];
+                  FileOutputStream fos = new FileOutputStream(outputFile);
+                  int len;
+                  while ((len = tis.read(buffer)) > 0) {
+                      fos.write(buffer, 0, len);
+                  }
+                  fos.close();
+              }
+          }
+
+          tis.close();
+          gzis.close();
+          fis.close();
+        return destinationDir.getAbsolutePath();
+      } catch (IOException e) {
+          Log.e("DecompressTgzFile", "Error decompressing tgz file", e);
+          throw new Exception("Error decompressing tgz file");
       }
-
-      tis.close();
-      gzis.close();
-      fis.close();
-
-  } catch (IOException e) {
-      Log.e("DecompressTgzFile", "Error decompressing tgz file", e);
-  }
-}
+};
   @ReactMethod
   public void unTar(String pathToRead, String pathToWrite, final Promise promise) {
     Log.d(MODULE_NAME, "Create event called with name: " + pathToRead
       + " and location: " + pathToWrite);
     try {
-      promise.resolve("source/code");
+      String decompressedPath = extractTgzFile(pathToRead, pathToWrite);
+      promise.resolve(decompressedPath);
     } catch(Exception e) {
       promise.reject("Error uncompressing file:", e);
     }
