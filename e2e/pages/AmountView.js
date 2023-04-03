92b0550e47459e89b2b3cfd78fbc32717194e4cb
CW <chris.wilcox@consensys.net>
2023-04-02 19:46:00 -0700

    waitAndTap currency switch

--- e2e/pages/AmountView.js
+++ e2e/pages/AmountView.js
@@ -33,7 +33,7 @@ export default class AmountView {
   }
 
   static async tapCurrencySwitch() {
-    await TestHelpers.tap(CURRENCY_SWITCH);
+    await TestHelpers.waitAndTap(CURRENCY_SWITCH);
   }
 
   static async isVisible() {
