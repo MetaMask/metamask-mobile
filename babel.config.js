dc34b4f24bb421379a4b503d16ab636db5f01afd
LeoTM <1881059+leotm@users.noreply.github.com>
2023-04-06 15:56:48 +0100

    Revert "Add commented Babel config from debugging SVGs"
    This reverts commit d8f7279198bcc4c964a3afddfa39b43b82944c4a.

--- babel.config.js
+++ babel.config.js
@@ -4,12 +4,6 @@ module.exports = {
   plugins: [
     'transform-inline-environment-variables',
     'react-native-reanimated/plugin',
-    // [
-    //   '@babel/plugin-transform-react-jsx',
-    //   {
-    //     // throwIfNamespace: false,
-    //   },
-    // ],
   ],
   env: {
     production: {
