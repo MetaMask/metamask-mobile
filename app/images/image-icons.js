64ba7ee484fc5a56f04274358939ef88abbd955e
Tomás SANTOS <tommasini15@gmail.com>
2023-04-07 17:26:27 +0100

    remove unused image icons

--- app/images/image-icons.js
+++ app/images/image-icons.js
@@ -7,9 +7,6 @@ import FTM from './fantom.png';
 import ETHEREUM from './eth-logo-new.png';
 import BNB from './binance.png';
 import AVAX from './avalanche.png';
-import ROPSTEN from './ropsten-logo-dark.png';
-import KOVAN from './kovan-logo-dark.png';
-import RINKEBY from './rinkeby-logo-dark.png';
 import GOERLI from './goerli-logo-dark.png';
 import LINEATESTNET from './linea-logo-dark.png';
 
@@ -23,9 +20,6 @@ export default {
   BNB,
   AETH,
   AVAX,
-  ROPSTEN,
-  KOVAN,
-  RINKEBY,
   GOERLI,
   LINEATESTNET,
 };
