49fd559cb18e9dab104c8c3626d345421035f66e
Brian Nguyen <brianacnguyen@gmail.com>
2023-04-12 11:53:43 -0700

    Componentized Overlay Component

---
+++ app/component-library/components/Overlay/Overlay.styles.ts
@@ -0,0 +1,36 @@
+// Third party dependencies.
+import { StyleSheet, ViewStyle } from 'react-native';
+
+// External dependencies.
+import { Theme } from '../../../util/theme/models';
+
+// Internal dependencies.
+import { OverlayStyleSheetVars } from './Overlay.types';
+/**
+ * Style sheet function for Overlay component.
+ *
+ * @param params Style sheet params.
+ * @param params.theme App theme from ThemeContext.
+ * @param params.vars Inputs that the style sheet depends on.
+ * @returns StyleSheet object.
+ */
+const styleSheet = (params: { theme: Theme; vars: OverlayStyleSheetVars }) => {
+  const { theme, vars } = params;
+  const { style, color } = vars;
+  return StyleSheet.create({
+    base: Object.assign(
+      {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        backgroundColor: color || theme.colors.overlay.default,
+      } as ViewStyle,
+      style,
+    ) as ViewStyle,
+    fill: {
+      flex: 1,
+    },
+  });
+};
+
+export default styleSheet;
