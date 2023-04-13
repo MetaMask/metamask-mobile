5187097159cd09c2ef66c56f218c4b7f893695ec
witmicko <mjogrodniczak@gmail.com>
2023-04-13 09:53:51 +0100

    more tests

--- app/util/linkCheck.test.ts
+++ app/util/linkCheck.test.ts
@@ -15,6 +15,8 @@ jest.mock('../core/Engine', () => ({
 
 describe('linkCheck', () => {
   it('should correctly check links for safety', () => {
+    expect(isLinkSafe('htps://ww.example.com/')).toEqual(false);
+    expect(isLinkSafe('https://ww.example.com/')).toEqual(true);
     expect(isLinkSafe('http://example com/page?id=123')).toEqual(false);
     expect(isLinkSafe('https://www.example.com/')).toEqual(true);
     expect(isLinkSafe('http://phishing.com')).toEqual(false);
