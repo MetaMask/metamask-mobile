3a0eb4b67bf047c95a560b9a425d4eedce6cc2a4
Nicolas MASSART <nicolas.massart@consensys.net>
2023-04-14 11:30:43 +0200

    fix conflict with main

--- e2e/config.js
+++ e2e/config.js
@@ -4,11 +4,8 @@ require('dotenv').config({ path: '.e2e.env' });
 const config = {
   rootDir: '../',
   setupFilesAfterEnv: ['./e2e/init.js'],
-  preset: 'react-native',
+  testEnvironment: 'node',
   forceExit: true,
-  transform: {
-    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
-  },
   reporters: [
     'default',
     [
