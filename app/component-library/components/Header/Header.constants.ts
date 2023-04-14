dc48b08aec8d3735a1505259ff0e3e6ff31b2d5b
Brian August Nguyen <brianacnguyen@gmail.com>
2023-04-14 10:33:00 -0700

    Componentize Header Component (#6181)
    * Componentized Header Component
    
    * Updated story title to use string
    
    * Removed unused Text component from Header story
    
    * Fixed flashing problem
    
    * Addressed comments
    
    * Updated tests for Header

---
+++ app/component-library/components/Header/Header.constants.ts
@@ -0,0 +1,11 @@
+/* eslint-disable import/prefer-default-export */
+
+// External dependencies.
+import { TextVariant } from '../Texts/Text';
+
+// Defaults
+export const DEFAULT_HEADER_TITLE_TEXTVARIANT = TextVariant.HeadingSM;
+
+// Test IDs
+export const HEADER_TEST_ID = 'header';
+export const HEADER_TITLE_TEST_ID = 'header-title';
