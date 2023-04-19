0b6ce50110aced3dc774676d8f2fde51ed537570
LeoTM <1881059+leotm@users.noreply.github.com>
2023-04-19 18:17:46 +0100

    Revert "Test BigInt(2) + BigInt(3)"
    This reverts commit 521417a4f64cd2878188976c237e33ebd856807a.

--- index.js
+++ index.js
@@ -78,5 +78,3 @@ if (IGNORE_BOXLOGS_DEVELOPMENT === 'true') {
  * Application entry point responsible for registering root component
  */
 AppRegistry.registerComponent(name, () => Sentry.wrap(Root));
-
-console.log(BigInt(2) + BigInt(3)); // 5, expected 5n
