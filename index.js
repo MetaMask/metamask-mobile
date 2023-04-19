17d37881cf6d93d55d41c27d828042a1db3890ee
LeoTM <1881059+leotm@users.noreply.github.com>
2023-04-19 18:14:47 +0100

    Test 1n
    [SyntaxError: No identifiers allowed directly after numeric literal]
    
    Emu stacktrace: No identifiers allowed after numeric literal (x2), no stack (x2)

--- index.js
+++ index.js
@@ -78,3 +78,5 @@ if (IGNORE_BOXLOGS_DEVELOPMENT === 'true') {
  * Application entry point responsible for registering root component
  */
 AppRegistry.registerComponent(name, () => Sentry.wrap(Root));
+
+console.log(1n); // [SyntaxError: No identifiers allowed directly after numeric literal]
