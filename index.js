d867bc88bfc709e4c5fbc9a99d67415677bffae5
LeoTM <1881059+leotm@users.noreply.github.com>
2023-04-19 18:14:57 +0100

    Revert "Test 1n"
    This reverts commit 17d37881cf6d93d55d41c27d828042a1db3890ee.

--- index.js
+++ index.js
@@ -78,5 +78,3 @@ if (IGNORE_BOXLOGS_DEVELOPMENT === 'true') {
  * Application entry point responsible for registering root component
  */
 AppRegistry.registerComponent(name, () => Sentry.wrap(Root));
-
-console.log(1n); // [SyntaxError: No identifiers allowed directly after numeric literal]
