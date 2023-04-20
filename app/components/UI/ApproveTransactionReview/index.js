9b8971137e15bb24c6bcc132942a20846f7167cb
Sylva Elendu <iamsylvaelendu@gmail.com>
2023-04-20 10:55:07 +0100

    rebased to main

--- app/components/UI/ApproveTransactionReview/index.js
+++ app/components/UI/ApproveTransactionReview/index.js
@@ -63,8 +63,6 @@ import { ThemeContext, mockTheme } from '../../../util/theme';
 import withQRHardwareAwareness from '../QRHardware/withQRHardwareAwareness';
 import QRSigningDetails from '../QRHardware/QRSigningDetails';
 import Routes from '../../../constants/navigation/Routes';
-import formatNumber from '../../../util/formatNumber';
-import { allowedToBuy } from '../FiatOnRampAggregator';
 import createStyles from './styles';
 import {
   selectChainId,
@@ -81,9 +79,6 @@ import VerifyContractDetails from './VerifyContractDetails/VerifyContractDetails
 import ShowBlockExplorer from './ShowBlockExplorer';
 import { isNetworkBuyNativeTokenSupported } from '../FiatOnRampAggregator/utils';
 import { getRampNetworks } from '../../../reducers/fiatOrders';
-import CustomSpendCap from '../../../component-library/components-temp/CustomSpendCap';
-import {getAccountBalance} from '../../../util/dappTransactions';
-
 import SkeletonText from '../FiatOnRampAggregator/components/SkeletonText';
 
 const { ORIGIN_DEEPLINK, ORIGIN_QR_CODE } = AppConstants.DEEPLINKS;
@@ -1110,7 +1105,6 @@ const mapStateToProps = (state) => ({
     selectChainId(state),
     getRampNetworks(state),
   ),
-  tokenBalances: state.engine.backgroundState.TokenBalancesController.contractBalances,
 });
 
 const mapDispatchToProps = (dispatch) => ({
