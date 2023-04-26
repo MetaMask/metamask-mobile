c94cc70cab18dc0f164ff99df341a43ec6a551a2
Mark Stacey <markjstacey@gmail.com>
2023-04-26 17:33:50 -0230

    Add additional tests

--- app/core/SanitizationMiddleware.test.ts
+++ app/core/SanitizationMiddleware.test.ts
@@ -7,7 +7,47 @@ const noop = () => {
 };
 
 describe('createSanitizationMiddleware', () => {
-  it('does nothing when the first param is not an object', () => {
+  it('does nothing if the request is something other than eth_call', () => {
+    const sanitizationMiddleware = createSanitizationMiddleware();
+    const req = {
+      jsonrpc: '2.0' as const,
+      id: '1',
+      method: 'something_else',
+      params: [
+        {
+          foo: '123',
+        },
+      ],
+    };
+
+    sanitizationMiddleware(req, { jsonrpc: '2.0', id: 'any' }, noop, noop);
+
+    expect(req.params).toStrictEqual([
+      {
+        foo: '123',
+      },
+    ]);
+  });
+
+  it('does nothing if the params are an object', () => {
+    const sanitizationMiddleware = createSanitizationMiddleware();
+    const req = {
+      jsonrpc: '2.0' as const,
+      id: '1',
+      method: 'something_else',
+      params: {
+        foo: '123',
+      },
+    };
+
+    sanitizationMiddleware(req, { jsonrpc: '2.0', id: 'any' }, noop, noop);
+
+    expect(req.params).toStrictEqual({
+      foo: '123',
+    });
+  });
+
+  it('does nothing if the first param is not an object', () => {
     const sanitizationMiddleware = createSanitizationMiddleware();
     const req = {
       jsonrpc: '2.0' as const,
@@ -18,10 +58,38 @@ describe('createSanitizationMiddleware', () => {
 
     sanitizationMiddleware(req, { jsonrpc: '2.0', id: 'any' }, noop, noop);
 
-    expect(req.params[0]).toBe('FOO');
+    expect(req.params).toStrictEqual(['FOO']);
   });
 
-  it('removes any property that is not transaction-like or topic-like', () => {
+  it('does nothing if the first param is an array', () => {
+    const sanitizationMiddleware = createSanitizationMiddleware();
+    const req = {
+      jsonrpc: '2.0' as const,
+      id: '1',
+      method: 'eth_call',
+      params: [['FOO']],
+    };
+
+    sanitizationMiddleware(req, { jsonrpc: '2.0', id: 'any' }, noop, noop);
+
+    expect(req.params).toStrictEqual([['FOO']]);
+  });
+
+  it('does nothing if the first param is null', () => {
+    const sanitizationMiddleware = createSanitizationMiddleware();
+    const req = {
+      jsonrpc: '2.0' as const,
+      id: '1',
+      method: 'eth_call',
+      params: [null],
+    };
+
+    sanitizationMiddleware(req, { jsonrpc: '2.0', id: 'any' }, noop, noop);
+
+    expect(req.params).toStrictEqual([null]);
+  });
+
+  it('removes any properties that are not transaction-like or topic-like', () => {
     const sanitizationMiddleware = createSanitizationMiddleware();
     const testTxLike = {
       jsonrpc: '2.0' as const,
@@ -64,6 +132,52 @@ describe('createSanitizationMiddleware', () => {
     );
     expect(testTxLike.params[0]).toStrictEqual({ to: '0x123' });
   });
+
+  it('does nothing to non-string permitted properties', () => {
+    const sanitizationMiddleware = createSanitizationMiddleware();
+    const testTxLike = {
+      jsonrpc: '2.0' as const,
+      id: '1',
+      method: 'eth_call',
+      params: [
+        {
+          to: 100,
+        },
+      ],
+    };
+
+    sanitizationMiddleware(
+      testTxLike,
+      { jsonrpc: '2.0', id: 'any' },
+      noop,
+      noop,
+    );
+    expect(testTxLike.params[0]).toStrictEqual({ to: 100 });
+  });
+
+  for (const blockRef of ['latest', 'pending', 'earliest']) {
+    it(`does nothing to permitted properties set to ${blockRef}`, () => {
+      const sanitizationMiddleware = createSanitizationMiddleware();
+      const testTxLike = {
+        jsonrpc: '2.0' as const,
+        id: '1',
+        method: 'eth_call',
+        params: [
+          {
+            from: blockRef,
+          },
+        ],
+      } as any;
+
+      sanitizationMiddleware(
+        testTxLike,
+        { jsonrpc: '2.0', id: 'any' },
+        noop,
+        noop,
+      );
+      expect(testTxLike.params[0].from).toBe(blockRef);
+    });
+  }
 
   it('hex prefixes and lowercases any tx param that is missing one', () => {
     const sanitizationMiddleware = createSanitizationMiddleware();
