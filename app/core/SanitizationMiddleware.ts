15968c87a09c9a4b225a89292ad866c689d3fd90
Mark Stacey <markjstacey@gmail.com>
2023-04-28 19:06:47 -0230

    Add docs and improve function names

--- app/core/SanitizationMiddleware.ts
+++ app/core/SanitizationMiddleware.ts
@@ -6,8 +6,11 @@ import { addHexPrefix } from 'ethereumjs-util';
 // We use this to clean any custom params from the txParams
 // TODO: Update this to allow `blockhash` (used by `eth_getLogs`) and EIP-1559
 // transaction parameters
-// TODO: Consider making this middleware method-specific
+// TODO: Consider making this middleware method-specific so that we can more
+// easily understand what it does
 // TODO: Consider rejecting invalid inputs, rather than silently dropping them
+// TODO: Consider deleting this middleware; we can let the RPC handle
+// validation
 const permittedKeys = [
   'from',
   'to',
@@ -22,11 +25,19 @@ const permittedKeys = [
   'topics',
 ];
 
-function cloneTxParams(txParams: Record<PropertyKey, unknown>) {
+/**
+ * Sanitize an RPC parameter object. The object is expected to be either
+ * transaction parameters or an `eth_getLogs` filter object. A new object
+ * with just the permitted keys is returned.
+ *
+ * @param parameter - The parameter to sanitize.
+ * @returns The given parameter containing just permitted keys.
+ */
+function sanitizeRpcParameter(parameter: Record<PropertyKey, unknown>) {
   const sanitized = permittedKeys.reduce<Record<string, unknown>>(
     (copy, permitted) => {
-      if (permitted in txParams) {
-        const value = txParams[permitted];
+      if (permitted in parameter) {
+        const value = parameter[permitted];
         if (Array.isArray(value)) {
           copy[permitted] = value.map((item: unknown) => sanitize(item));
         } else {
@@ -41,6 +52,15 @@ function cloneTxParams(txParams: Record<PropertyKey, unknown>) {
   return sanitized;
 }
 
+/**
+ * Sanitize a single property. If it's a string other than a named block
+ * reference, it is formatted as a 0x-prefixed lowercase string.
+ *
+ * Non-string properties are returned unaltered.
+ *
+ * @param value - The property to sanitize.
+ * @returns The sanitized property.
+ */
 function sanitize(value: unknown) {
   switch (value) {
     case 'latest':
@@ -57,6 +77,13 @@ function sanitize(value: unknown) {
   }
 }
 
+/**
+ * Create RPC middleware for sanitizing method parameters. This middleware
+ * only sanitizes the first parameter, and only if it's an object.
+ *
+ * @returns RPC middleware that will sanitize parameters before passing the
+ * request along.
+ */
 // eslint-disable-next-line import/prefer-default-export
 export function createSanitizationMiddleware(): JsonRpcMiddleware<
   unknown,
@@ -75,7 +102,7 @@ export function createSanitizationMiddleware(): JsonRpcMiddleware<
       !Array.isArray(txParams) &&
       txParams !== null
     ) {
-      const sanitized = cloneTxParams(txParams);
+      const sanitized = sanitizeRpcParameter(txParams);
       req.params[0] = sanitized;
     }
 
