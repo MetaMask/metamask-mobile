ff94a165520c5a54ea29bbef7098d9976e857ace
Samuel Salas <samuel.salas.reyes@gmail.com>
2023-04-28 07:48:30 -0600

    Fix flaky test

--- wdio/screen-objects/TokenOverviewScreen.js
+++ wdio/screen-objects/TokenOverviewScreen.js
@@ -20,7 +20,15 @@ class TokenOverviewScreen {
   }
 
   async tapBackButton() {
-    await Gestures.tap(this.backButtonTokenOverview);
+    const backButton = await this.backButtonTokenOverview;
+    await backButton.waitForDisplayed();
+    let isDisplayed = backButton.isDisplayed();
+
+    while (isDisplayed) {
+      await backButton.click();
+      await driver.pause(2000);
+      isDisplayed = await backButton.isDisplayed();
+    }
   }
 
   async isTokenOverviewVisible() {
