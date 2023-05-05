6636fc3bd224b177c21f70b929ede73d73e8bca5
Owen Craston <owengc12@gmail.com>
2023-05-05 14:26:22 -0700

    use nio

--- android/app/src/main/java/io/metamask/nativeModules/RNTar/RNTar.java
+++ android/app/src/main/java/io/metamask/nativeModules/RNTar/RNTar.java
@@ -19,7 +19,10 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
-
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import android.os.Build;
 
 public class RNTar extends ReactContextBaseJavaModule {
   private static String MODULE_NAME = "RNTar";
@@ -34,27 +37,52 @@ public class RNTar extends ReactContextBaseJavaModule {
     return MODULE_NAME;
   }
 
+  private void createDirectories(String path) throws IOException {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      Files.createDirectories(Paths.get(path));
+    } else {
+      File dir = new File(path);
+      if (!dir.exists()) {
+        dir.mkdirs();
+      }
+    }
+  }
+
+  private boolean isReadableWritable(String path) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      Path dirPath = Paths.get(path);
+      return Files.isReadable(dirPath) && Files.isWritable(dirPath);
+    } else {
+      File dir = new File(path);
+      return dir.canRead() && dir.canWrite();
+    }
+  }
+
+  private boolean exists(String path) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      return Files.exists(Paths.get(path));
+    } else {
+      return new File(path).exists();
+    }
+  }
+
   private String extractTgzFile(String tgzPath, String outputPath) throws IOException {
     try {
       // Check if .tgz file exists
-      File tgzFile = new File(tgzPath);
-      if (!tgzFile.exists()) {
+      if (!exists(tgzPath)) {
         throw new IOException("The specified .tgz file does not exist.");
       }
 
       // Create output directory if it doesn't exist
-      File outputDirectory = new File(outputPath);
-      if (!outputDirectory.exists()) {
-        outputDirectory.mkdirs();
-      }
+      createDirectories(outputPath);
 
       // Check if the output directory is readable and writable
-      if (!outputDirectory.canRead() || !outputDirectory.canWrite()) {
+      if (!isReadableWritable(outputPath)) {
         throw new IOException("The output directory is not readable and/or writable.");
       }
 
       // Set up the input streams for reading the .tgz file
-      try (FileInputStream fileInputStream = new FileInputStream(tgzFile);
+      try (FileInputStream fileInputStream = new FileInputStream(tgzPath);
            GZIPInputStream gzipInputStream = new GZIPInputStream(fileInputStream);
            TarArchiveInputStream tarInputStream = new TarArchiveInputStream(new BufferedInputStream(gzipInputStream))) {
 
@@ -62,22 +90,19 @@ public class RNTar extends ReactContextBaseJavaModule {
 
         // Loop through the entries in the .tgz file
         while ((entry = (TarArchiveEntry) tarInputStream.getNextEntry()) != null) {
-          File outputFile = new File(outputDirectory, entry.getName());
+          File outputFile = new File(outputPath, entry.getName());
           System.out.println("Snaps/ entry " + entry.getName());
 
           // If it is a directory, create the output directory
           if (entry.isDirectory()) {
-            outputFile.mkdirs();
+            createDirectories(outputFile.getAbsolutePath());
           } else {
             // Create parent directories if they don't exist
-            outputFile.getParentFile().mkdirs();
+            createDirectories(outputFile.getParent());
 
             // Set up the output streams for writing the file
-            FileOutputStream fos = null;
-            BufferedWriter dest = null;
-            try {
-              fos = new FileOutputStream(outputFile);
-              dest = new BufferedWriter(new OutputStreamWriter(fos, StandardCharsets.UTF_8));
+            try (FileOutputStream fos = new FileOutputStream(outputFile);
+                 BufferedWriter dest = new BufferedWriter(new OutputStreamWriter(fos, StandardCharsets.UTF_8))) {
 
               // Set up a BufferedReader for reading the file from the .tgz file
               BufferedReader tarReader = new BufferedReader(new InputStreamReader(tarInputStream, StandardCharsets.UTF_8));
@@ -88,20 +113,12 @@ public class RNTar extends ReactContextBaseJavaModule {
                 dest.write(line);
                 dest.newLine();
               }
-            } finally {
-              if (dest != null) {
-                dest.flush();
-                dest.close();
-              }
-              if (fos != null) {
-                fos.close();
-              }
             }
           }
         }
       }
       // Return the output directory path
-      return new File(outputDirectory, "package").getAbsolutePath();
+      return new File(outputPath, "package").getAbsolutePath();
     } catch (IOException e) {
       Log.e("DecompressTgzFile", "Error decompressing tgz file", e);
       throw new IOException("Error decompressing tgz file: " + e.getMessage(), e);
