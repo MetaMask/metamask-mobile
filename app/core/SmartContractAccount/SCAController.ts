aac6b3f8dc815de8cd6917ee4b71e2ed665d5732
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2023-05-10 12:54:52 -0400

    More changes from SCAController

--- app/core/SmartContractAccount/SCAController.ts
+++ app/core/SmartContractAccount/SCAController.ts
@@ -2,10 +2,22 @@ import { Contract } from '@ethersproject/contracts';
 import { BaseController } from '@metamask/base-controller';
 import type { KeyringController } from '@metamask/keyring-controller';
 import { BigNumber, ethers } from 'ethers';
+import { Web3Provider } from '@ethersproject/providers';
 // import { randomBytes } from 'ethers/lib/utils';
 import { SCAConfig, SCAState } from './types';
-import { POLYGON_MUMBAI_ADDRESS, POLYGON_MUMBAI_NAME } from './constants';
+import {
+  POLYGON_MUMBAI_ADDRESS,
+  POLYGON_MUMBAI_CHAIN_ID,
+  POLYGON_MUMBAI_NAME,
+} from './constants';
 import FactoryABI from './ABIs/FactoryABI';
+import { PreferencesController } from '@metamask/preferences-controller';
+
+const ALCHEMY_API_KEY = 'ADD API KEY HERE';
+
+const debugLog = (args: any): void =>
+  // eslint-disable-next-line no-console
+  console.log(`[HACKATON DEBUG] - ${args}`);
 
 class SCAController extends BaseController<SCAConfig, SCAState> {
   override name = 'SCAController';
@@ -33,26 +45,44 @@ class SCAController extends BaseController<SCAConfig, SCAState> {
   }
 
   async createSCAccount(signerPublicAddress: string): Promise<void> {
-    const provider = new ethers.providers.JsonRpcProvider(
-      `https://polygon-mumbai.infura.io/v3/${process.env.MM_INFURA_PROJECT_ID}`,
+    const alchemyProvider = new ethers.providers.AlchemyProvider(
+      POLYGON_MUMBAI_NAME,
+      ALCHEMY_API_KEY,
     );
+    const infuraProvider = new ethers.providers.InfuraProvider(
+      POLYGON_MUMBAI_NAME,
+      process.env.MM_INFURA_PROJECT_ID,
+    );
+    debugLog('AlchemyProvider provider created');
+
     const privateKey = await this.exportAccount(
-      'fake password',
+      'password',
       signerPublicAddress,
     );
-    const signer = new ethers.Wallet(privateKey, provider);
+
+    const signer = new ethers.Wallet(privateKey, alchemyProvider);
+    debugLog('Signer created');
+
     const contract = new Contract(POLYGON_MUMBAI_ADDRESS, FactoryABI, signer);
-    const salt = BigNumber.from(1); // randomBytes(16);
-    const methodArgs = [signerPublicAddress, salt, [signerPublicAddress]];
-    // const method = await contract.createAccount(...methodArgs);
+    debugLog('Contract instance created');
 
-    console.log({ signerPublicAddress, privateKey, methodArgs, contract });
+    const options = {
+      gasLimit: 200000,
+    };
+    const salt = BigNumber.from(29378417934); // randomBytes(16)
+    try {
+      const receipt = await contract.createAccount(
+        signerPublicAddress,
+        salt,
+        [],
+        options,
+      );
 
-    // const ethersProvider = new ethers.providers.AlchemyProvider(
-    //   POLYGON_MUMBAI_NAME,
-    // );
-    // const createAccountMethod = contract.createAccount(...methodArgs);
-    // console.log({ createAccountMethod, methodArgs });
+      console.log(`Transaction hash: ${receipt.transactionHash}`);
+      console.log(`Block number: ${receipt.blockNumber}`);
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   // constructUserOp() {};
