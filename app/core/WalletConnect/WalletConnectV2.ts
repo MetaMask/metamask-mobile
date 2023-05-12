5fdb24107d660ae1aa209b00953377e17a76aed0
abretonc7s <arthur.breton@consensys.net>
2023-05-12 09:40:47 +0800

    feat: force bigint big-integer polyfill on android

--- app/core/WalletConnect/WalletConnectV2.ts
+++ app/core/WalletConnect/WalletConnectV2.ts
@@ -33,6 +33,16 @@ import WalletConnect from './WalletConnect';
 import parseWalletConnectUri from './wc-utils';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import METHODS_TO_REDIRECT from './wc-config';
+import { Platform } from 'react-native';
+
+if (Platform.OS === 'android') {
+  // eslint-disable-next-line
+  const BigInt = require('big-integer');
+  // Force big-integer / BigInt polyfill on android.
+  Object.assign(global, {
+    BigInt,
+  });
+}
 
 const ERROR_MESSAGES = {
   INVALID_CHAIN: 'Invalid chainId',
