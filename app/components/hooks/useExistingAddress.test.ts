c648a80010e52d757541eedfd7eb891b84a631ce
Jyoti Puri <jyotipuri@gmail.com>
2023-05-12 14:42:26 +0530

    fix build

--- app/components/hooks/useExistingAddress.test.ts
+++ app/components/hooks/useExistingAddress.test.ts
@@ -1,5 +1,4 @@
-import { renderHook } from '@testing-library/react-native';
-
+import { renderHookWithProvider } from '../../util/test/renderWithProvider';
 import useExistingAddress from './useExistingAddress';
 
 jest.mock('react-redux', () => ({
@@ -38,17 +37,56 @@ jest.mock('react-redux', () => ({
     }),
 }));
 
+const initialState = {
+  settings: {},
+  engine: {
+    backgroundState: {
+      PreferencesController: {
+        selectedAddress: '0x0',
+        identities: {
+          '0x0': {
+            address: '0x0',
+            name: 'Account 1',
+          },
+        },
+      },
+      NetworkController: {
+        network: 1,
+        provider: {
+          ticker: 'eth',
+        },
+      },
+      AddressBookController: {
+        addressBook: {
+          1: {
+            '0x1': {
+              address: '0x1',
+              name: 'Account 2',
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
 describe('useExistingAddress', () => {
   it('should return existing address from identities', async () => {
-    const { result } = renderHook(() => useExistingAddress('0x0'));
+    const { result } = renderHookWithProvider(() => useExistingAddress('0x0'), {
+      state: initialState,
+    });
     expect(result?.current?.name).toEqual('Account 1');
   });
   it('should return existing address from address book', async () => {
-    const { result } = renderHook(() => useExistingAddress('0x1'));
+    const { result } = renderHookWithProvider(() => useExistingAddress('0x1'), {
+      state: initialState,
+    });
     expect(result?.current?.name).toEqual('Account 2');
   });
   it('should return undefined address not in identities or address book', async () => {
-    const { result } = renderHook(() => useExistingAddress('0x2'));
+    const { result } = renderHookWithProvider(() => useExistingAddress('0x2'), {
+      state: initialState,
+    });
     expect(result?.current).toBeUndefined();
   });
 });
