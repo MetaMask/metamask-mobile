affafb03dd679c9918b69f8eb6145b8d01df21f4
Olusegun Akintayo <akintayo.segun@gmail.com>
2023-05-12 01:44:42 +0100

    updated test
    Signed-off-by: Olusegun Akintayo <akintayo.segun@gmail.com>

--- app/components/UI/TransactionReview/TransactionReviewEIP1559/index.test.tsx
+++ app/components/UI/TransactionReview/TransactionReviewEIP1559/index.test.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import TransactionReviewEIP1559 from './';
+import configureMockStore from 'redux-mock-store';
 import renderWithProvider from '../../../..//util/test/renderWithProvider';
 
+const mockStore = configureMockStore();
 const initialState = {
   engine: {
     backgroundState: {
@@ -20,20 +22,10 @@ const initialState = {
 };
 
 describe('TransactionReviewEIP1559', () => {
-<<<<<<< HEAD
-  it('should render correctly', () => {
-    const wrapper = shallow(
-      <Provider store={store}>
-        <TransactionReviewEIP1559 />
-      </Provider>,
-    );
-    expect(wrapper).toMatchSnapshot();
-=======
   it('should match snapshot', async () => {
     const container = renderWithProvider(<TransactionReviewEIP1559 />, {
       state: initialState,
     });
     expect(container).toMatchSnapshot();
->>>>>>> 7e2256282 (When the dapp suggested gas fees is too high, change the color of gas estimate times to orange and show an icon)
   });
 });
