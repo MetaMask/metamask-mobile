3102a13172ee03e4503bd2f13326660254745555
Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>
2023-05-13 23:02:45 -0400

    I cannot believe that it took this long to write a test for this.

--- app/core/SmartContractAccount/SCKeyring.ts
+++ app/core/SmartContractAccount/SCKeyring.ts
@@ -1,9 +1,15 @@
 /* eslint-disable no-console */
-import { BigNumber } from 'ethers';
+import { ethers, BigNumber } from 'ethers';
+import { hexlify } from 'ethers/lib/utils';
 import Web3 from 'web3';
 
-import { POLYGON_MUMBAI_ADDRESS, POLYGON_MUMBAI_CHAIN_ID } from './constants';
-import { FactoryABI, WalletABI } from './ABIs';
+import { fillAndSign, fillUserOpDefaults, getUserOpHash } from './utils';
+import {
+  POLYGON_MUMBAI_ADDRESS,
+  POLYGON_MUMBAI_CHAIN_ID,
+  ENTRY_POINT_ADDRESS,
+} from './constants';
+import { EntryPointABI, FactoryABI, WalletABI } from './ABIs';
 import { SCAccount } from './types';
 
 class SCKeyring {
@@ -57,13 +63,62 @@ class SCKeyring {
     return this.#accounts.map((account) => account.address);
   }
 
-  public signTransaction(transaction: any, from: string) {
-    console.log({ transaction, from });
-    throw new Error('Calling the signing method');
-  }
+  public async signTransaction(from: string, transaction: any) {
+    const web3 = new Web3();
+    const entryPointContract = new this.#provider.eth.Contract(
+      EntryPointABI,
+      ENTRY_POINT_ADDRESS.mumbai,
+    );
 
-  public newSmartWalletInstance(walletAddress: string) {
-    return this.#provider.eth.Contract(WalletABI, walletAddress);
+    const walletContract = new this.#provider.eth.Contract(WalletABI, from);
+    const nonce = await walletContract.methods.nonce().call();
+    console.log(typeof nonce);
+    console.log(web3.utils.toHex(nonce));
+
+    const executionSig = web3.eth.abi.encodeFunctionSignature(
+      'execute(address,uint256,bytes)',
+    );
+
+    const executionData =
+      executionSig +
+      web3.eth.abi
+        .encodeParameters(
+          ['address', 'uint256', 'bytes'],
+          [
+            transaction.to
+              ? web3.utils.toHex(transaction.to.toString())
+              : ethers.constants.AddressZero,
+            transaction.value,
+            transaction.data ? web3.utils.toHex(transaction.data) : '0x',
+          ],
+        )
+        .substr(2);
+
+    console.log({ executionSig, executionData });
+
+    const userOp = await fillAndSign(
+      fillUserOpDefaults({
+        sender: from,
+        callData: executionData,
+        paymasterAndData: '0x', // opts.usePaymaster ? hexConcat(['0x1Fc92037a8236AfFB3328cbEf71dd986c4a373dD', hexZeroPad('0x3de9210F3D577272Cdb8404Cd5276C4B5dBC5b91', 32), hexZeroPad(hexlify(1), 32)]) : '0x',
+        nonce: web3.utils.toHex(nonce),
+        callGasLimit: hexlify(2000000),
+        verificationGasLimit: hexlify(1000000),
+        maxFeePerGas: hexlify(3e9),
+      }),
+      this.#signer,
+      entryPointContract,
+    );
+
+    const userOpHash = getUserOpHash(
+      userOp,
+      ENTRY_POINT_ADDRESS.mumbai,
+      web3.utils.toHex(POLYGON_MUMBAI_CHAIN_ID),
+    );
+
+    console.log({ userOp, userOpHash });
+
+    return { ...transaction, userOp, userOpHash };
   }
 
   public async addAccount() {
