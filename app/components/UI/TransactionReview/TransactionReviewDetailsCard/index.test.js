d4a554e777feafbb4ade1a89ba65d561a48eef59
Sylva Elendu <iamsylvaelendu@gmail.com>
2023-05-16 16:18:30 +0100

    Confirmations: Empty SiteURL and Null TagURL in modal (#6274)
    * hide tagurl and siteurl if not present
    
    * some test coverage
    
    * rebased to main
    
    * failing lint and snapshot
    
    * removed build change
    
    * fix snapshot

---
+++ app/components/UI/TransactionReview/TransactionReviewDetailsCard/index.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import TransactionReviewDetailsCard from '.';
+import { shallow } from 'enzyme';
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+
+const mockStore = configureMockStore();
+const initialState = {
+  engine: {
+    backgroundState: {
+      CurrencyRateController: {
+        currentCurrency: 'usd',
+        conversionRate: 0.1,
+      },
+      NetworkController: {
+        providerConfig: {
+          ticker: 'ETH',
+          chainId: '1',
+        },
+      },
+    },
+  },
+};
+const store = mockStore(initialState);
+
+describe('TransactionReviewDetailsCard', () => {
+  it('should render correctly', () => {
+    const wrapper = shallow(
+      <Provider store={store}>
+        <TransactionReviewDetailsCard />
+      </Provider>,
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});
