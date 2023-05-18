631191ee244860a2245b9abcee89b999ca1b2ef5
Cal Leung <cleun007@gmail.com>
2023-05-17 19:16:41 -0700

    Update test IDs

--- wdio/screen-objects/testIDs/Common.testIds.js
+++ wdio/screen-objects/testIDs/Common.testIds.js
@@ -1,3 +1,4 @@
 export const TOAST_ID = 'toast';
 export const ANDROID_PROGRESS_BAR = 'android.widget.ProgressBar';
 export const EDIT_BUTTON = 'edit-button';
+export const NAV_BACK_BUTTON_ID = 'nav-back-button-id';
---
+++ wdio/screen-objects/testIDs/Screens/Regions.testIds.js
@@ -0,0 +1,3 @@
+export const CONTINUE_NAVIGATION_BUTTON_ID = 'continue-navigation-button-id';
+
+export const SELECT_REGION_BUTTON_ID = 'select-region-button-id';
