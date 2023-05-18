106fcd335f582e111b31fe66233ec48d8477e6bb
Cal Leung <cleun007@gmail.com>
2023-05-18 14:54:47 -0700

    Add comment to useMetaMetrics test

--- app/components/hooks/MetaMetrics/useMetaMetrics.test.tsx
+++ app/components/hooks/MetaMetrics/useMetaMetrics.test.tsx
@@ -1,6 +1,5 @@
 import { renderHook } from '@testing-library/react-hooks';
 import useMetaMetrics from './useMetaMetrics';
-// import { MetaMetrics } from '../../../core/Analytics';
 
 jest.mock('../../../core/Analytics', () => ({
   MetaMetrics: {
@@ -9,27 +8,10 @@ jest.mock('../../../core/Analytics', () => ({
   },
 }));
 
+// TODO - Add more tests when useMetaMetrics hook is actually used
 describe('useMetaMetrics', () => {
   test('it should return a function object from hook useMetaMetrics', () => {
     const { result } = renderHook(() => useMetaMetrics());
     expect(typeof result.current).toBe('function');
   });
-
-  // test('it should call MetaMetrics.trackAnonymousEvent for anonymous event', () => {
-  //   const { result } = renderHook(() => useMetaMetrics());
-  //   const trackEventSpy = jest.spyOn(MetaMetrics, 'trackAnonymousEvent');
-  //   act(() => {
-  //     result.current({ name: 'Mock Anonymous Event' }, true, {});
-  //   });
-  //   expect(trackEventSpy).toHaveBeenCalled();
-  // });
-
-  // test('it should call MetaMetrics.trackEvent for non-anonymous event', () => {
-  //   const { result } = renderHook(() => useMetaMetrics());
-  //   const trackEventSpy = jest.spyOn(MetaMetrics, 'trackEvent');
-  //   act(() => {
-  //     result.current({ name: 'Mock Non-Anonymous Event' }, false, {});
-  //   });
-  //   expect(trackEventSpy).toHaveBeenCalled();
-  // });
 });
