b4927db7e03deeb57de22d35ed7fb2f32c443c30
João Loureiro <joao.loureiro@consensys.net>
2023-05-24 18:39:05 +0100

    remove proptype error log from device screen.
    revert this commit once https://github.com/MetaMask/metamask-mobile/issues/6458
    gets closed

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ LogBox.ignoreLogs([
   'Module TcpSockets requires main queue setup',
   'Module RCTSearchApiManager requires main queue setup',
   'PushNotificationIOS has been extracted', // RNC PushNotification iOS issue - https://github.com/react-native-push-notification/ios/issues/43
+   "ViewPropTypes will be removed from React Native, along with all other PropTypes. We recommend that you migrate away from PropTypes and switch to a type system like TypeScript. If you need to continue using ViewPropTypes, migrate to the 'deprecated-react-native-prop-types' package.",
 ]);
 
 const IGNORE_BOXLOGS_DEVELOPMENT = process.env.IGNORE_BOXLOGS_DEVELOPMENT;
