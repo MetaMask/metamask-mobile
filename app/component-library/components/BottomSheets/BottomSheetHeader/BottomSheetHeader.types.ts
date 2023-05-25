aae909112a559af457a9d457754594f2de912150
Brian Nguyen <brianacnguyen@gmail.com>
2023-05-25 09:13:05 -0700

    Added BottomSheetHeader

---
+++ app/component-library/components/BottomSheets/BottomSheetHeader/BottomSheetHeader.types.ts
@@ -0,0 +1,16 @@
+// External dependencies.
+import { HeaderProps } from '../../Header/Header.types';
+
+/**
+ * BottomSheetHeader component props.
+ */
+export interface BottomSheetHeaderProps extends HeaderProps {
+  /**
+   * Optional function to trigger when pressing the back button.
+   */
+  onBack?: () => void;
+  /**
+   * Optional function to trigger when pressing the close button.
+   */
+  onClose?: () => void;
+}
