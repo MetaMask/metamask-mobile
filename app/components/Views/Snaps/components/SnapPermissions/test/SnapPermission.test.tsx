777608d30855cf6c2de07e74c2481cb65c810db4
Owen Craston <owengc12@gmail.com>
2023-05-26 17:52:34 -0700

    more permissions test

--- app/components/Views/Snaps/components/SnapPermissions/test/SnapPermission.test.tsx
+++ app/components/Views/Snaps/components/SnapPermissions/test/SnapPermission.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import SnapPermissions from '../SnapPermissions';
 import { SNAP_PERMISSION_CELL } from '../../../../../../constants/test-ids';
+import { Bip32PublicKeyArgsStruct } from '@metamask/rpc-methods/dist/restricted/getBip32PublicKey';
 
 describe('SnapPermissions', () => {
   const mockDate = 1684964145490;
@@ -25,41 +26,104 @@ describe('SnapPermissions', () => {
   });
 
   it('renders permissions correctly', () => {
-    const permissions = {
+    const mockPermissions: SnapPermissionsType = {
+      'endowment:long-running': {},
       'endowment:network-access': {},
+      'endowment:transaction-insight': {
+        allowTransactionOrigin: true,
+      },
+      'endowment:cronjob': {
+        jobs: [
+          {
+            expression: '* * * * *',
+            request: {
+              method: 'GET',
+              params: {},
+              id: '1',
+              jsonrpc: '2.0',
+            },
+          },
+        ],
+      },
       'endowment:rpc': {
         dapps: true,
         snaps: true,
       },
       snap_confirm: {},
+      snap_manageState: {},
+      snap_notify: {},
+      snap_getBip32Entropy: [
+        {
+          path: ['m', '44', '0', '0', '0'],
+          curve: 'ed25519',
+        },
+      ],
+      snap_getBip32PublicKey: [
+        {
+          path: ['m', '44', '0', '0', '0'],
+          curve: 'secp256k1',
+        },
+      ],
       snap_getBip44Entropy: [
         {
           coinType: 1,
         },
-        {
-          coinType: 461,
-        },
       ],
-      snap_manageState: {},
+      snap_getEntropy: {},
+      'endowment:keyring': {
+        namespaces: {
+          mockNamespace: {
+            chains: [
+              {
+                name: 'Mock Chain',
+                id: 'mock-chain-id',
+              },
+            ],
+            methods: ['mockMethod1', 'mockMethod2'],
+            events: ['mockEvent1', 'mockEvent2'],
+          },
+        },
+      },
     };
+
     const { getAllByTestId } = render(
-      <SnapPermissions permissions={permissions} installedAt={mockDate} />,
+      <SnapPermissions permissions={mockPermissions} installedAt={mockDate} />,
     );
     const permissionCells = getAllByTestId(SNAP_PERMISSION_CELL);
 
-    expect(permissionCells.length).toBe(5);
+    expect(permissionCells.length).toBe(13);
     expect(permissionCells[0].props.children.props.title).toBe(
+      longRunningTitle,
+    );
+    expect(permissionCells[1].props.children.props.title).toBe(
       networkAccessTitle,
     );
-    expect(permissionCells[1].props.children.props.title).toBe(rpcTitle);
     expect(permissionCells[2].props.children.props.title).toBe(
+      transactionInsightTitle,
+    );
+    expect(permissionCells[3].props.children.props.title).toBe(cronjobTitle);
+    expect(permissionCells[4].props.children.props.title).toBe(rpcTitle);
+    expect(permissionCells[5].props.children.props.title).toBe(
       snapConfirmTitle,
     );
-    expect(permissionCells[3].props.children.props.title).toBe(
+    expect(permissionCells[6].props.children.props.title).toBe(
+      snapManageStateTitle,
+    );
+    expect(permissionCells[7].props.children.props.title).toBe(snapNotifyTitle);
+    expect(permissionCells[8].props.children.props.title).toBe(
+      snapGetBip32EntropyTitle,
+    );
+    expect(permissionCells[9].props.children.props.title).toBe(
+      snapGetBip32PublicKeyTitle,
+    );
+    expect(permissionCells[10].props.children.props.title).toBe(
       snapGetBip44EntropyTitle,
     );
-    expect(permissionCells[4].props.children.props.title).toBe(
-      snapManageStateTitle,
+    expect(permissionCells[11].props.children.props.title).toBe(
+      snapGetEntropyTitle,
+    );
+    expect(permissionCells[12].props.children.props.title).toBe(
+      endowmentKeyringTitle,
     );
   });
 
