0b07521906e2ee1b31af3598c6c029d2f91449f6
Owen Craston <owen.craston@consensys.net>
2023-06-01 17:43:00 -0400

    [Feature][Flask] Snaps settings UX v1 (#6443)
    * wire up settings view
    
    * proper navigation options
    
    * check dev to show install input
    
    * properly type props
    
    * wire up settings for a specific snap
    
    * info container
    
    * snap version
    
    * create components folder
    
    * componentize everything
    
    * add a v before version
    
    * remove section finished
    
    * description section
    
    * description section
    
    * format date
    
    * rename main snaps settings page
    
    * get permission data
    
    * permissions cells
    
    * test snapdetails
    
    * test version badge
    
    * snap decsription test
    
    * test SnapElement
    
    * change folder to not be ignored
    
    * testing snap settins
    
    * fix header
    
    * snap permissions test
    
    * snaps settings list tests
    
    * strings
    
    * snap details strings
    
    * snap permissions strings
    
    * snap settings string
    
    * version strings
    
    * snaps permission titles
    
    * more permissions test
    
    * cleanup
    
    * custom component
    
    * fix tests
    
    * render different permissions for rpc
    
    * render different permissions for different protocols
    
    * fix tests
    
    * derive title from path
    
    * handle invalid paths
    
    * test all the bip32 options
    
    * test invalid bip32 public key
    
    * document functions
    
    * memoize
    
    * SnapPermissions readme
    
    * cleanup
    
    * reset MainNavigator.js
    
    * move SnapsSettingsList into its own folder
    
    * migrate snappermissions to useStyles pattern
    
    * fully migrate to use styles
    
    * disable snap
    
    * SnapVersionTag
    
    * use label
    
    * wrap icon in background
    
    * cleanup

---
+++ app/components/Views/Snaps/components/SnapVersionTag/test/SnapVersionTag.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import { SemVerVersion } from '@metamask/snaps-utils';
+import SnapVersionBadge from '../SnapVersionTag';
+import {
+  SNAP_VERSION_BADGE,
+  SNAP_VERSION_BADGE_VALUE,
+} from '../../../../../../constants/test-ids';
+
+describe('SnapVersionBadge', () => {
+  it('renders the version in the correct format', async () => {
+    const { getByTestId } = render(
+      <SnapVersionBadge version={'2.3.13' as SemVerVersion} />,
+    );
+    const versionBadge = await getByTestId(SNAP_VERSION_BADGE);
+    const versionBadgeValue = await getByTestId(SNAP_VERSION_BADGE_VALUE);
+    expect(versionBadge).toBeTruthy();
+    expect(versionBadgeValue.props.children).toBe('v2.3.13');
+  });
+});
