e1120535180b5c622871263782262e021119393b
Nicolas MASSART <nicolas.massart@consensys.net>
2023-06-06 15:35:52 +0200

    fix useSafeAreaFrame mocks

--- app/components/Views/Settings/AdvancedSettings/EthSignFriction/EthSignFriction.test.tsx
+++ app/components/Views/Settings/AdvancedSettings/EthSignFriction/EthSignFriction.test.tsx
@@ -15,12 +15,14 @@ jest.mock('../../../../../core/Engine', () => ({
 
 jest.mock('react-native-safe-area-context', () => {
   const inset = { top: 0, right: 0, bottom: 0, left: 0 };
+  const frame = { width: 0, height: 0, x: 0, y: 0 };
   return {
     SafeAreaProvider: jest.fn().mockImplementation(({ children }) => children),
     SafeAreaConsumer: jest
       .fn()
       .mockImplementation(({ children }) => children(inset)),
     useSafeAreaInsets: jest.fn().mockImplementation(() => inset),
+    useSafeAreaFrame: jest.fn().mockImplementation(() => frame),
   };
 });
 
