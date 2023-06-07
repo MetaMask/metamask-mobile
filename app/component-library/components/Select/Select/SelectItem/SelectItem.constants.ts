f10ab92b8d8b75e553edda692ca386e0e522fb72
Brian Nguyen <brianacnguyen@gmail.com>
2023-06-07 10:36:36 -0700

    Updated SelectItem to use ListItem

--- app/component-library/components/Select/Select/SelectItem/SelectItem.constants.ts
+++ app/component-library/components/Select/Select/SelectItem/SelectItem.constants.ts
@@ -1,3 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 
+// Defaults
+export const DEFAULT_SELECTITEM_PADDING = 16;
+export const DEFAULT_SELECTITEM_BORDERRADIUS = 4;
+
+// Test IDs
 export const SELECTABLE_ITEM_UNDERLAY_ID = 'selectable-item-underlay';
