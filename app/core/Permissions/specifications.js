d5147b9ddbc6ad4e8284167d0e776a64549704cc
Jiexi Luan <jiexiluan@gmail.com>
2023-06-07 13:09:22 -0700

    Remove sort

--- app/core/Permissions/specifications.js
+++ app/core/Permissions/specifications.js
@@ -54,15 +54,10 @@ export const getCaveatSpecifications = ({ getIdentities }) => ({
 
     decorator: (method, caveat) => async (args) => {
       const allAccounts = await method(args);
-      const res = caveat.value
-        .filter(({ address }) => {
-          const addressToCompare = address.toLowerCase();
-          return allAccounts.includes(addressToCompare);
-        })
-        .sort((a, b) =>
-          a.lastUsed < b.lastUsed ? -1 : a.lastUsed > b.lastUsed ? 1 : 0,
-        )
-        .reverse();
+      const res = caveat.value.filter(({ address }) => {
+        const addressToCompare = address.toLowerCase();
+        return allAccounts.includes(addressToCompare);
+      });
 
       return res;
     },
