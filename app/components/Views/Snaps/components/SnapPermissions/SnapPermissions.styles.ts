aca212ab2e6f0c21c9459703a5b8870d35f6b3d9
Owen Craston <owen.craston@consensys.net>
2023-06-07 18:07:16 -0400

    [Flask] Use permissions controller as source of truth for Snaps Permissions UI (#6533)
    * create custom component for snaps cell
    
    * rendering permissions from permissions controller
    
    * bip32
    
    * testing
    
    * use a switch statement
    
    * dont check for the types
    
    * handler functions
    
    * fix snapsettings test
    
    * cleanup
    
    * function to check the types
    
    * readme update

--- app/components/Views/Snaps/components/SnapPermissions/SnapPermissions.styles.ts
+++ app/components/Views/Snaps/components/SnapPermissions/SnapPermissions.styles.ts
@@ -1,5 +1,4 @@
 import { StyleSheet } from 'react-native';
-import { Theme } from '../../../../../util/theme/models';
 
 /**
  *
@@ -8,37 +7,10 @@ import { Theme } from '../../../../../util/theme/models';
  * @param params.vars Inputs that the style sheet depends on.
  * @returns StyleSheet object.
  */
-const styleSheet = (params: { theme: Theme }) => {
-  const { theme } = params;
-  const { colors } = theme;
-  return StyleSheet.create({
+const styleSheet = () =>
+  StyleSheet.create({
     section: {
       paddingTop: 32,
     },
-    permissionCell: {
-      borderRadius: 10,
-      borderWidth: 0,
-    },
-    cellBase: {
-      flexDirection: 'row',
-    },
-    cellBaseInfo: {
-      flex: 1,
-      alignItems: 'flex-start',
-    },
-    secondaryText: {
-      color: colors.text.alternative,
-    },
-    iconWrapper: {
-      marginTop: 16,
-      marginRight: 16,
-      width: 32,
-      height: 32,
-      borderRadius: 16,
-      backgroundColor: colors.background.alternative,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
   });
-};
 export default styleSheet;
