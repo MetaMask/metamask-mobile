2c0668d5eabefbdcfd4e859e2aea4361050d2ece
Jiexi Luan <jiexiluan@gmail.com>
2023-06-07 13:19:52 -0700

    Fix specs

--- app/core/Permissions/specifications.test.js
+++ app/core/Permissions/specifications.test.js
@@ -17,19 +17,18 @@ describe('PermissionController specifications', () => {
 
     describe('restrictReturnedAccounts', () => {
       describe('decorator', () => {
-        it('returns array members included in the caveat value sorted by most recently used first', async () => {
+        it('returns array members included in the caveat value', async () => {
           const getIdentities = jest.fn();
           const caveatValues = [
             { address: '0x1', lastUsed: '1' },
-            { address: '0x2', lastUsed: '3' },
-            { address: '0x3', lastUsed: '2' },
-            { address: '0x5', lastUsed: '4' },
+            { address: '0x2', lastUsed: '2' },
+            { address: '0x3', lastUsed: '3' },
           ];
           const { decorator } = getCaveatSpecifications({ getIdentities })[
             CaveatTypes.restrictReturnedAccounts
           ];
 
-          const method = async () => ['0x1', '0x2', '0x3', '0x4'];
+          const method = async () => ['0x1', '0x2', '0x4'];
           const caveat = {
             type: CaveatTypes.restrictReturnedAccounts,
             value: caveatValues,
@@ -37,9 +36,8 @@ describe('PermissionController specifications', () => {
           const decorated = decorator(method, caveat);
 
           expect(await decorated()).toStrictEqual([
-            caveatValues[1],
-            caveatValues[2],
             caveatValues[0],
+            caveatValues[1],
           ]);
         });
 
