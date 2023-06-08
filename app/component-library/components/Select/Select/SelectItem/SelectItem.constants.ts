9c76042f87bb7edf2eb75a1666ab376d28860a1b
Brian Nguyen <brianacnguyen@gmail.com>
2023-06-08 10:15:41 -0700

    Added sample const

--- app/component-library/components/Select/Select/SelectItem/SelectItem.constants.ts
+++ app/component-library/components/Select/Select/SelectItem/SelectItem.constants.ts
@@ -1,4 +1,9 @@
 /* eslint-disable import/prefer-default-export */
+// External dependencies.
+import { SAMPLE_LISTITEM_PROPS } from '../../../List/ListItem/ListItem.constants';
+
+// Internal dependencies.
+import { SelectItemProps } from './SelectItem.types';
 
 // Defaults
 export const DEFAULT_SELECTITEM_PADDING = 16;
@@ -6,3 +11,10 @@ export const DEFAULT_SELECTITEM_BORDERRADIUS = 4;
 
 // Test IDs
 export const SELECTABLE_ITEM_UNDERLAY_ID = 'selectable-item-underlay';
+
+// Sample consts
+export const SAMPLE_SELECTITEM_PROPS: SelectItemProps = {
+  isSelected: true,
+  isDisabled: false,
+  ...SAMPLE_LISTITEM_PROPS,
+};
