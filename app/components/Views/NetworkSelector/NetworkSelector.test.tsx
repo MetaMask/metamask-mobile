7e0fe7e96ed016ae4dc0e0535d89b095896780ea
tommasini <tommasini15@gmail.com>
2023-06-08 23:06:55 +0100

    add test when testnet is selected

--- app/components/Views/NetworkSelector/NetworkSelector.test.tsx
+++ app/components/Views/NetworkSelector/NetworkSelector.test.tsx
@@ -182,4 +182,113 @@ describe('Network Selector', () => {
 
     expect(setShowTestNetworksSpy).toBeCalled();
   });
+  it('toggle test network is disabled and is on when a testnet is selected', () => {
+    const { getByTestId } = renderComponent({
+      navigation: { currentBottomNavRoute: 'Wallet' },
+      privacy: { thirdPartyApiMode: true },
+      settings: {
+        primaryCurrency: 'usd',
+      },
+      engine: {
+        backgroundState: {
+          SwapsController: {
+            tokens: [],
+          },
+          AccountTrackerController: {
+            accounts: {
+              '0x': {
+                name: 'account 1',
+                address: '0x',
+                balance: 0,
+              },
+            },
+          },
+          NetworkController: {
+            providerConfig: {
+              type: 'mainnet',
+              nickname: 'Goerli mainnet',
+              ticket: 'eth',
+              chainId: '5',
+            },
+          },
+          CurrencyRateController: {
+            conversionRate: 5,
+            currentCurrency: 'usd',
+          },
+          TokensController: {
+            tokens: [],
+          },
+          PreferencesController: {
+            showTestNetworks: false,
+            selectedAddress: '0x',
+            identities: {
+              '0x': { name: 'Account 1', address: '0x' },
+            },
+            frequentRpcList: [
+              {
+                chainId: '43114',
+                nickname: 'Avalanche Mainnet C-Chain',
+                rpcPrefs: { blockExplorerUrl: 'https://snowtrace.io' },
+                rpcUrl: 'https://api.avax.network/ext/bc/C/rpc',
+                ticker: 'AVAX',
+              },
+              {
+                chainId: '137',
+                nickname: 'Polygon Mainnet',
+                rpcPrefs: { blockExplorerUrl: 'https://polygonscan.com' },
+                rpcUrl:
+                  'https://polygon-mainnet.infura.io/v3/cda392a134014865ad3c273dc7ddfff3',
+                ticker: 'MATIC',
+              },
+              {
+                chainId: '10',
+                nickname: 'Optimism',
+                rpcPrefs: {
+                  blockExplorerUrl: 'https://optimistic.etherscan.io',
+                },
+                rpcUrl:
+                  'https://optimism-mainnet.infura.io/v3/cda392a134014865ad3c273dc7ddfff3',
+                ticker: 'ETH',
+              },
+              {
+                chainId: '59140',
+                nickname: 'Linea Goerli Test Network',
+                rpcPrefs: {
+                  blockExplorerUrl: 'https://explorer.goerli.linea.build',
+                },
+                rpcUrl: 'https://rpc.goerli.linea.build/',
+                ticker: 'LineaETH',
+              },
+              {
+                chainId: '100',
+                nickname: 'Gnosis Chain',
+                rpcPrefs: {
+                  blockExplorerUrl: 'https://blockscout.com/xdai/mainnet/',
+                },
+                rpcUrl: 'https://rpc.gnosischain.com/',
+                ticker: 'XDAI',
+              },
+            ],
+          },
+          TokenBalancesController: {
+            contractBalances: {},
+          },
+          TokenListController: {
+            tokenList: {},
+          },
+          TokenRatesController: {
+            contractExchangeRates: {},
+          },
+          NftController: {
+            allNfts: { '0x': { '1': [] } },
+            allNftContracts: { '0x': { '1': [] } },
+          },
+        },
+      },
+    });
+    const testNetworksSwitch = getByTestId('test-network-switch-id');
+
+    expect(testNetworksSwitch.props.value).toBeTruthy();
+    expect(testNetworksSwitch.props.disabled).toBeTruthy();
+  });
 });
