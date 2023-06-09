294ff381b75ed6effba1224ca669f1bf61dec37f
tommasini <tommasini15@gmail.com>
2023-06-09 11:46:09 +0100

    add new translation to the Permissions screen

--- app/components/Views/AccountPermissions/AccountPermissionsRevoke/AccountPermissionsRevoke.tsx
+++ app/components/Views/AccountPermissions/AccountPermissionsRevoke/AccountPermissionsRevoke.tsx
@@ -33,6 +33,9 @@ import styleSheet from './AccountPermissionsRevoke.styles';
 import { useSelector } from 'react-redux';
 import generateTestId from '../../../../../wdio/utils/generateTestId';
 import { CONNECTED_ACCOUNTS_MODAL_REVOKE_BUTTON_ID } from '../../../../../wdio/screen-objects/testIDs/Components/ConnectedAccountsModal.testIds';
+import { IconName } from '../../../../component-library/components/Icons/Icon';
+import Avatar from '../../../../component-library/components/Avatars/Avatar/Avatar';
+import { AvatarVariants } from '../../../../component-library/components/Avatars/Avatar';
 
 const AccountPermissionsRevoke = ({
   ensByAccountAddress,
@@ -92,10 +95,9 @@ const AccountPermissionsRevoke = ({
         <SheetActions
           actions={[
             {
-              label: strings('accounts.revoke_all'),
+              label: strings('accounts.disconnect_all_accounts'),
               onPress: revokeAllAccounts,
               disabled: isLoading,
-              isDanger: true,
             },
           ]}
         />
@@ -107,7 +109,7 @@ const AccountPermissionsRevoke = ({
   return (
     <>
       <SheetHeader
-        title={strings('accounts.connected_accounts_title')}
+        title={strings('accounts.permissions')}
         onBack={() =>
           onSetPermissionsScreen(AccountPermissionsScreens.Connected)
         }
@@ -119,14 +121,25 @@ const AccountPermissionsRevoke = ({
           iconName={secureIcon}
         />
         <Text style={styles.description}>
-          {strings('accounts.connect_description')}
+          {strings('accounts.site_permission_to')}
         </Text>
+        <View style={styles.permissionContainer}>
+          <Avatar variant={AvatarVariants.Icon} name={IconName.Eye} />
+          <Text style={styles.permissionDescription}>
+            {strings('accounts.address_balance_activity_permission')}
+          </Text>
+        </View>
+        <View style={styles.securityContainer}>
+          <Avatar variant={AvatarVariants.Icon} name={IconName.SecurityTick} />
+          <Text style={styles.permissionDescription}>
+            {strings('accounts.suggest_transactions')}
+          </Text>
+        </View>
       </View>
       <AccountSelectorList
         renderRightAccessory={(address, name) => (
           <Button
             variant={ButtonVariants.Secondary}
-            isDanger
             onPress={() => {
               if (permittedAddresses.length === 1) {
                 // Dismiss and show toast
@@ -137,7 +150,7 @@ const AccountPermissionsRevoke = ({
                     label: `${name} `,
                     isBold: true,
                   },
-                  { label: strings('toast.revoked') },
+                  { label: strings('toast.disconnected') },
                 ];
                 if (activeAddress === address) {
                   const nextActiveAddress = permittedAddresses[1];
@@ -178,7 +191,7 @@ const AccountPermissionsRevoke = ({
                 );
               }
             }}
-            label={strings('accounts.revoke')}
+            label={strings('accounts.disconnect')}
             size={ButtonSize.Sm}
             style={styles.disconnectButton}
             {...generateTestId(
