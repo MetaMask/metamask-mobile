a31f36e8d0c08c78563ff6d806c7cdb9f875fa0e
Owen Craston <owengc12@gmail.com>
2023-06-13 15:36:33 -0700

    test InstallSnapConnectionRequest

---
+++ app/components/UI/InstallSnapApprovalFlow/components/InstallSnapConnectionRequest/test/InstallSnapConnectionRequest.test.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import InstallSnapConnectionRequest from '../InstallSnapConnectionRequest';
+import { prefixUrlWithProtocol } from '../../../../../../util/browser';
+import {
+  SNAP_INSTALL_CANCEL,
+  SNAP_INSTALL_CONNECT,
+  SNAP_INSTALL_CONNECTION_REQUEST,
+} from '../../../../../../constants/test-ids';
+
+describe('InstallSnapConnectionRequest', () => {
+  const requestData = {
+    requestData: {
+      metadata: {
+        id: 'uNadWHqPnwOM4NER3mERI',
+        origin: 'npm:@lavamoat/tss-snap',
+        dappOrigin: 'tss.ac',
+      },
+      permissions: {
+        snap_manageState: {},
+        'endowment:rpc': {
+          caveats: [
+            {
+              type: 'rpcOrigin',
+              value: {
+                dapps: true,
+                snaps: true,
+              },
+            },
+          ],
+        },
+      },
+      snapId: 'npm:@lavamoat/tss-snap',
+    },
+    id: 'uNadWHqPnwOM4NER3mERI',
+  };
+
+  const onConfirm = jest.fn();
+  const onCancel = jest.fn();
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders correctly', () => {
+    const { getByTestId } = render(
+      <InstallSnapConnectionRequest
+        requestData={requestData}
+        onConfirm={onConfirm}
+        onCancel={onCancel}
+      />,
+    );
+    expect(getByTestId(SNAP_INSTALL_CONNECTION_REQUEST)).toBeDefined();
+  });
+
+  it('calls onConfirm when the connect button is pressed', () => {
+    const { getByTestId } = render(
+      <InstallSnapConnectionRequest
+        requestData={requestData}
+        onConfirm={onConfirm}
+        onCancel={onCancel}
+      />,
+    );
+
+    fireEvent.press(getByTestId(SNAP_INSTALL_CONNECT));
+    expect(onConfirm).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls onCancel when the cancel button is pressed', () => {
+    const { getByTestId } = render(
+      <InstallSnapConnectionRequest
+        requestData={requestData}
+        onConfirm={onConfirm}
+        onCancel={onCancel}
+      />,
+    );
+
+    fireEvent.press(getByTestId(SNAP_INSTALL_CANCEL));
+    expect(onCancel).toHaveBeenCalledTimes(1);
+  });
+
+  it('correctly prefixes dappOrigin with protocol', () => {
+    const { getByText } = render(
+      <InstallSnapConnectionRequest
+        requestData={requestData}
+        onConfirm={onConfirm}
+        onCancel={onCancel}
+      />,
+    );
+
+    const expectedUrl = 'https://tss.ac';
+    expect(getByText(expectedUrl)).toBeTruthy();
+  });
+});
