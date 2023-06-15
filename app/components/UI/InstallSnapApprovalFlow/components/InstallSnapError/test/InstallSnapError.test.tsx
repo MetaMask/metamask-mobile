d050769a4d750be1a4010b5b68abe066be67d2ae
Owen Craston <owen.craston@consensys.net>
2023-06-15 13:12:49 -0700

    [Flask] Snaps install UX (#6561)

---
+++ app/components/UI/InstallSnapApprovalFlow/components/InstallSnapError/test/InstallSnapError.test.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import InstallSnapError from '../InstallSnapError';
+import {
+  SNAP_INSTALL_ERROR,
+  SNAP_INSTALL_OK,
+} from '../../../../../../constants/test-ids';
+
+describe('InstallSnapError', () => {
+  const requestData = {
+    metadata: {
+      id: 'uNadWHqPnwOM4NER3mERI',
+      origin: 'npm:@lavamoat/tss-snap',
+      dappOrigin: 'tss.ac',
+    },
+    permissions: {
+      snap_manageState: {},
+      'endowment:rpc': {
+        caveats: [
+          {
+            type: 'rpcOrigin',
+            value: {
+              dapps: true,
+              snaps: true,
+            },
+          },
+        ],
+      },
+    },
+    snapId: 'npm:@lavamoat/tss-snap',
+  };
+
+  const onConfirm = jest.fn();
+  const onCancel = jest.fn();
+  const error = new Error('Installation failed');
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders correctly', () => {
+    const { getByTestId } = render(
+      <InstallSnapError
+        requestData={requestData}
+        onConfirm={onConfirm}
+        error={error}
+        onCancel={onCancel}
+      />,
+    );
+
+    expect(getByTestId(SNAP_INSTALL_ERROR)).toBeTruthy();
+  });
+
+  it('calls onConfirm when the OK button is pressed', () => {
+    const { getByTestId } = render(
+      <InstallSnapError
+        requestData={requestData}
+        onConfirm={onConfirm}
+        error={error}
+        onCancel={onCancel}
+      />,
+    );
+
+    fireEvent.press(getByTestId(SNAP_INSTALL_OK));
+    expect(onConfirm).toHaveBeenCalledTimes(1);
+  });
+
+  it('displays the correct snap name', () => {
+    const { getByText } = render(
+      <InstallSnapError
+        requestData={requestData}
+        onConfirm={onConfirm}
+        error={error}
+        onCancel={onCancel}
+      />,
+    );
+
+    const expectedSnapName = '@lavamoat/tss-snap';
+    expect(getByText(expectedSnapName)).toBeTruthy();
+  });
+
+  it('displays the correct error title', () => {
+    const { getByText } = render(
+      <InstallSnapError
+        requestData={requestData}
+        onConfirm={onConfirm}
+        error={error}
+        onCancel={onCancel}
+      />,
+    );
+
+    expect(getByText('Installation failed')).toBeTruthy();
+  });
+});
