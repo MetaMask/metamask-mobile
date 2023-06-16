132c0cfbf643acfe5ae622c8f733a7b404a443c5
Victorien Gauch <85494462+VGau@users.noreply.github.com>
2023-06-16 23:42:17 +0200

    fix: refactor linea testnet implementation (#6491)
    * fix: refactor linea testnet implementation
    
    * feat: add linea testnet logo
    
    * update linea goerli block explorer url
    
    * fix: image icon issue for linea goerli network
    
    * fix: update transaction controller patch
    
    ---------
    
    Co-authored-by: Curtis David <Curtis.David7@gmail.com>

--- app/constants/network.js
+++ app/constants/network.js
@@ -2,12 +2,11 @@ export const MAINNET = 'mainnet';
 export const HOMESTEAD = 'homestead';
 export const GOERLI = 'goerli';
 export const SEPOLIA = 'sepolia';
+export const LINEA_GOERLI = 'linea-goerli';
 export const RPC = 'rpc';
 export const NO_RPC_BLOCK_EXPLORER = 'NO_BLOCK_EXPLORER';
 export const PRIVATENETWORK = 'PRIVATENETWORK';
 export const DEFAULT_MAINNET_CUSTOM_NAME = 'Ethereum Main Custom';
-export const LINEA_TESTNET_NICKNAME = 'Linea Goerli Test Network';
-export const LINEA_TESTNET_TICKER = 'LineaETH';
 
 /**
  * @enum {string}
@@ -23,6 +22,6 @@ export const NETWORKS_CHAIN_ID = {
   CELO: '42220',
   HARMONY: '1666600000',
   SEPOLIA: '11155111',
-  LINEA_TESTNET: '59140',
+  LINEA_GOERLI: '59140',
   GOERLI: '5',
 };
