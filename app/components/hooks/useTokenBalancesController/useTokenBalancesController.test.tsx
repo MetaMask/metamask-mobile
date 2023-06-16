f370e1d545b6555241c94895b077b61d70f56366
Nicolas MASSART <nicolas.massart@consensys.net>
2023-06-16 09:43:05 +0200

    revamped test with actual store and dummy test controller

--- app/components/hooks/useTokenBalancesController/useTokenBalancesController.test.tsx
+++ app/components/hooks/useTokenBalancesController/useTokenBalancesController.test.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import configureMockStore from 'redux-mock-store';
+import React, { useEffect } from 'react';
+import { Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { renderHook } from '@testing-library/react-native';
-import lodash from 'lodash';
+import { createStore } from 'redux';
+import { act, render, waitFor } from '@testing-library/react-native';
 import useTokenBalancesController from './useTokenBalancesController';
 
 interface TokenBalance {
@@ -13,9 +13,8 @@ const initialBalances: TokenBalance = {
   '0x326836cc6cd09B5aa59B81A7F72F25FcC0136b95': '0x2a',
 };
 
-// initial state for the mock store
-// without contract balances
-const mockState = {
+// initial state for the test store
+const testState = {
   engine: {
     backgroundState: {
       TokenBalancesController: { contractBalances: initialBalances },
@@ -23,84 +22,80 @@ const mockState = {
   },
 };
 
-let mockStore: any;
-let renderResult: any;
-let spyOnIsEqual: any;
+// test reducer for the test store
+const testBalancesReducer = (state: any, action: any) => {
+  if (action.type === 'add-balances') {
+    const newBalances = {
+      ...state.engine.backgroundState.TokenBalancesController.contractBalances,
+      ...action.value,
+    };
+    state.engine.backgroundState.TokenBalancesController.contractBalances =
+      newBalances;
+    return {
+      ...state,
+      value: newBalances,
+    };
+  }
+  return testState;
+};
+
+const testStore = createStore(testBalancesReducer);
 
 const Wrapper = ({ children }: any) => (
-  <Provider store={mockStore}>{children}</Provider>
+  <Provider store={testStore}>{children}</Provider>
 );
 
-describe('useTokenBalancesController', () => {
-  beforeEach(() => {
-    mockState.engine.backgroundState.TokenBalancesController.contractBalances =
-      initialBalances;
-    mockStore = configureMockStore()(() => mockState);
-    // spy on lodash isEqual function as this is the only way to check if the hook is correctly checking for equality
-    spyOnIsEqual = jest.spyOn(lodash, 'isEqual');
-    // render the hook with initial empty token balances
-    renderResult = renderHook(() => useTokenBalancesController(), {
-      wrapper: Wrapper,
+const spyOnDummyUseEffect = jest.fn();
+// Dummy component to test the useTokenBalancesController hook
+// and be able to detect the rerenders using the useEffect spy
+const DummyTestComponent = () => {
+  const balances = useTokenBalancesController();
+
+  useEffect(() => {
+    spyOnDummyUseEffect();
+  }, [balances]);
+
+  return <Text>{JSON.stringify(balances)}</Text>;
+};
+
+describe('useTokenBalancesController()', () => {
+  it('should only rerender when state changed', async () => {
+    // render the component with the test store
+    const { toJSON } = render(<DummyTestComponent />, { wrapper: Wrapper });
+
+    // check the useEffect is called once and initial balances are returned
+    await waitFor(async () => {
+      // check that the initial balances are returned
+      expect(spyOnDummyUseEffect).toHaveBeenCalledTimes(1);
+      expect(toJSON()).toMatchSnapshot();
     });
-  });
 
-  afterEach(() => {
-    jest.clearAllMocks();
-    renderResult.unmount();
-  });
+    // add a new balance to the store
+    await act(async () => {
+      testStore.dispatch({
+        type: 'add-balances',
+        value: { '0x326836cc6cd09B5aa59B81A7F72F25FcC0136b96': '0x539' },
+      });
+    });
 
-  it('should return stored data when state changed', async () => {
-    const { result, rerender } = renderResult;
+    // check that the useEffect is called again and the new updated balances are returned
+    await waitFor(() => {
+      expect(spyOnDummyUseEffect).toHaveBeenCalledTimes(2);
+      expect(toJSON()).toMatchSnapshot();
+    });
 
-    // useSelector hook equality check returns true as the storage state haven't changed yet
-    expect(spyOnIsEqual).toHaveNthReturnedWith(1, true);
+    // add the same balance to the store again, should override the previous one
+    await act(async () => {
+      testStore.dispatch({
+        type: 'add-balances',
+        value: { '0x326836cc6cd09B5aa59B81A7F72F25FcC0136b96': '0x539' },
+      });
+    });
 
-    // check that the initial balances are returned
-    expect(result.current.data).toStrictEqual(initialBalances);
-
-    // add a new token balance to the expected result
-    const newBalance: TokenBalance = {
-      '0x326836cc6cd09B5aa59B81A7F72F25FcC0136b96': '0x539',
-    };
-    const expectedBalances: TokenBalance = {
-      ...initialBalances,
-      ...newBalance,
-    };
-
-    // add the new token balances in the state
-    mockState.engine.backgroundState.TokenBalancesController.contractBalances =
-      expectedBalances;
-
-    // rerender the hook to get the new balances
-    rerender({ wrapper: Wrapper });
-
-    // check that the new balances are returned
-    expect(result.current.data).toEqual(expectedBalances);
-
-    // useSelector hook equality check returns false as the storage state have been updated
-    expect(spyOnIsEqual).toHaveNthReturnedWith(2, false);
-  });
-
-  it('should return memoised when state is unchanged', async () => {
-    const { result, rerender } = renderResult;
-
-    // useSelector hook equality check returns true as the storage state haven't changed yet
-    expect(spyOnIsEqual).toHaveNthReturnedWith(1, true);
-
-    // check that the initial balances are returned
-    expect(result.current.data).toStrictEqual(initialBalances);
-
-    // update token balances with identical object but different reference
-    mockState.engine.backgroundState.TokenBalancesController.contractBalances =
-      lodash.cloneDeep(initialBalances);
-
-    // rerender the hook to get the new balances
-    rerender({ wrapper: Wrapper });
-
-    // check that the same balances are returned
-    expect(result.current.data).toStrictEqual(initialBalances);
-
-    // useSelector hook equality check returns true as the storage state object content is similar to the previous one
-    expect(spyOnIsEqual).toHaveNthReturnedWith(2, true);
+    // check that the useEffect is not called again and the returned balances are unchanged
+    await waitFor(async () => {
+      expect(spyOnDummyUseEffect).toHaveBeenCalledTimes(2);
+      expect(toJSON()).toMatchSnapshot();
+    });
   });
 });
