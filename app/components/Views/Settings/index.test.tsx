4d1da84234c5969172eace22fa106e0c685202f5
tommasini <46944231+tommasini@users.noreply.github.com>
2023-06-16 10:09:20 +0100

    feat: [MC 0.5] Remove drawer and add remain options to settings tab (#6539)
    Co-authored-by: SamuelSalas <samuel.salas.reyes@gmail.com>

--- app/components/Views/Settings/index.test.tsx
+++ app/components/Views/Settings/index.test.tsx
@@ -3,10 +3,11 @@ import { shallow } from 'enzyme';
 import Settings from './';
 import configureMockStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
-
+import renderWithProvider from '../../../util/test/renderWithProvider';
+jest.unmock('react-redux');
 const mockStore = configureMockStore();
 const initialState = {
-  user: { seedphraseBackedUp: true },
+  user: { seedphraseBackedUp: true, passwordSet: true },
   privacy: { approvedHosts: [] },
   browser: { history: [] },
   settings: {
@@ -26,6 +27,20 @@ const initialState = {
     },
   },
 };
+
+const mockSetOptions = jest.fn();
+const mockNavigate = jest.fn();
+
+jest.mock('@react-navigation/native', () => {
+  const actualReactNavigation = jest.requireActual('@react-navigation/native');
+  return {
+    ...actualReactNavigation,
+    useNavigation: () => ({
+      navigate: mockNavigate,
+      setOptions: mockSetOptions,
+    }),
+  };
+});
 const store = mockStore(initialState);
 
 describe('Settings', () => {
@@ -36,5 +51,60 @@ describe('Settings', () => {
       </Provider>,
     );
     expect(wrapper).toMatchSnapshot();
+  });
+  it('should render general settings button', () => {
+    const { getByTestId } = renderWithProvider(<Settings />);
+    const generalSettings = getByTestId('general-settings');
+    expect(generalSettings).toBeDefined();
+  });
+  it('should render security settings button', () => {
+    const { getByTestId } = renderWithProvider(<Settings />);
+    const securitySettings = getByTestId('security-settings');
+    expect(securitySettings).toBeDefined();
+  });
+  it('should render advanced settings button', () => {
+    const { getByTestId } = renderWithProvider(<Settings />);
+    const advancedSettings = getByTestId('advanced-settings');
+    expect(advancedSettings).toBeDefined();
+  });
+  it('should render contacts settings button', () => {
+    const { getByTestId } = renderWithProvider(<Settings />);
+    const contactsSettings = getByTestId('contacts-settings');
+    expect(contactsSettings).toBeDefined();
+  });
+  it('should render network settings button', () => {
+    const { getByTestId } = renderWithProvider(<Settings />);
+    const networksSettings = getByTestId('networks-settings');
+    expect(networksSettings).toBeDefined();
+  });
+  it('should render feature request button', () => {
+    const { getByTestId } = renderWithProvider(<Settings />);
+    const onRampSettings = getByTestId('on-ramp-settings');
+    expect(onRampSettings).toBeDefined();
+  });
+  it('should render experimental settings button', () => {
+    const { getByTestId } = renderWithProvider(<Settings />);
+    const experimentalSettings = getByTestId('experimental-settings');
+    expect(experimentalSettings).toBeDefined();
+  });
+  it('should render about metamask button', () => {
+    const { getByTestId } = renderWithProvider(<Settings />);
+    const aboutMetamask = getByTestId('about-metamask-settings');
+    expect(aboutMetamask).toBeDefined();
+  });
+  it('should render request feature button', () => {
+    const { getByTestId } = renderWithProvider(<Settings />);
+    const requestFeature = getByTestId('request-feature');
+    expect(requestFeature).toBeDefined();
+  });
+  it('should render contact support button', () => {
+    const { getByTestId } = renderWithProvider(<Settings />);
+    const contactSupport = getByTestId('contact-support');
+    expect(contactSupport).toBeDefined();
+  });
+  it('should render lock button', () => {
+    const { getByTestId } = renderWithProvider(<Settings />);
+    const lock = getByTestId('lock');
+    expect(lock).toBeDefined();
   });
 });
