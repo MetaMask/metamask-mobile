44440c2f84fac700f7d4e98ba04d5ed5013769f4
Nicolas MASSART <nicolas.massart@consensys.net>
2023-06-21 21:39:36 +0200

    fix duplicated act call
    use multiple tests and init store on beforeAll

--- app/components/hooks/useTokenBalancesController/useTokenBalancesController.test.tsx
+++ app/components/hooks/useTokenBalancesController/useTokenBalancesController.test.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 import { Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, Store } from 'redux';
 import { act, render, waitFor } from '@testing-library/react-native';
 import useTokenBalancesController from './useTokenBalancesController';
 import { BN } from 'ethereumjs-util';
+import { cloneDeep } from 'lodash';
 
 // initial state for the test store
 const initialState = {
@@ -26,17 +27,9 @@ const testBalancesReducer = (state: any, action: any) => {
       ...state.engine.backgroundState.TokenBalancesController.contractBalances,
       ...action.value,
     };
-    return state;
   }
-  return initialState;
+  return state;
 };
-
-// create a test store, not a mock, as we need to test for content changes
-const testStore = createStore(testBalancesReducer);
-
-const Wrapper = ({ children }: any) => (
-  <Provider store={testStore}>{children}</Provider>
-);
 
 const spyOnDummyUseEffect = jest.fn();
 // Dummy component to test the useTokenBalancesController hook
@@ -53,19 +46,38 @@ const DummyTestComponent = () => {
 };
 
 describe('useTokenBalancesController()', () => {
-  it('should only rerender when state changed', async () => {
-    // render the component with the test store
-    const { toJSON } = render(<DummyTestComponent />, { wrapper: Wrapper });
+  let toJSON: any;
+  let testStore: Store<any, any>;
 
-    // check the useEffect is called once and initial balances are returned
+  beforeEach(async () => {
+    // create a test store, not a mock, as we need to test for content changes
+    // Cloning as the store is mutated by the reducer and prevents the test from
+    // starting from fresh store with the initial state
+    testStore = createStore(testBalancesReducer, cloneDeep(initialState));
+
+    // console.log('testStore', JSON.stringify(testStore.getState()));
+
+    const Wrapper = ({ children }: any) => (
+      <Provider store={testStore}>{children}</Provider>
+    );
+
+    // render the component with the test store
+    toJSON = render(<DummyTestComponent />, { wrapper: Wrapper }).toJSON;
+
     await waitFor(async () => {
       // check that the initial balances are returned
       expect(spyOnDummyUseEffect).toHaveBeenCalledTimes(1);
       expect(toJSON()).toMatchSnapshot();
     });
+  });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should rerender when state changed', async () => {
     // add a new balance to the store
-    await act(async () => {
+    act(() => {
       testStore.dispatch({
         type: 'add-balances',
         value: {
@@ -79,20 +91,22 @@ describe('useTokenBalancesController()', () => {
       expect(spyOnDummyUseEffect).toHaveBeenCalledTimes(2);
       expect(toJSON()).toMatchSnapshot();
     });
+  });
 
+  it('should not rerender when state is not changed', async () => {
     // add the same balance to the store again, should override the previous one
-    await act(async () => {
+    act(() => {
       testStore.dispatch({
         type: 'add-balances',
         value: {
-          '0x326836cc6cd09B5aa59B81A7F72F25FcC0136b96': new BN(0x539),
+          '0x326836cc6cd09B5aa59B81A7F72F25FcC0136b95': new BN(0x2a),
         },
       });
     });
 
     // check that the useEffect is not called again and the returned balances are unchanged
     await waitFor(async () => {
-      expect(spyOnDummyUseEffect).toHaveBeenCalledTimes(2);
+      expect(spyOnDummyUseEffect).toHaveBeenCalledTimes(1);
       expect(toJSON()).toMatchSnapshot();
     });
   });
