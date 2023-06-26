801c3d0dc76bba7ed432ee8f958f3132eee02b2a
Tiberiu Lepadatu <tiberiulepadatu14@gmail.com>
2023-06-26 15:51:08 -0300

    change regex import

--- jest.preprocessor.js
+++ jest.preprocessor.js
@@ -24,7 +24,7 @@
 
 'use strict';
 
-const REGEX_NODE_FILES = require('./app/util/regex');
+const { REGEX_NODE_FILES } = require('./app/util/regex');
 const { transformSync: babelTransformSync } = require('@babel/core');
 /* $FlowFixMe(>=0.54.0 site=react_native_oss) This comment suppresses an error
  * found when Flow v0.54 was deployed. To see the error delete this comment and
