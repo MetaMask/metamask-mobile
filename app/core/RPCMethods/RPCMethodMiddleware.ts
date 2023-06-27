2a68c6a5262e7657e2ee0660cdc5662922c268f4
tommasini <tommasini15@gmail.com>
2023-06-27 17:10:21 +0100

    send chain id to signature controller in hexadecimal format

--- app/core/RPCMethods/RPCMethodMiddleware.ts
+++ app/core/RPCMethods/RPCMethodMiddleware.ts
@@ -124,6 +124,8 @@ export const checkActiveAccountAndChainId = async ({
     }
 
     if (activeChainId !== chainIdRequest) {
+      console.log('ENTER HEREEEEEEEEEE');
+
       Alert.alert(
         `Active chainId is ${activeChainId} but received ${chainIdRequest}`,
       );
@@ -549,6 +551,7 @@ export const getRpcMethodMiddleware = ({
 
         const data = JSON.parse(req.params[1]);
         const chainId = data.domain.chainId;
+        console.log('ENTER CHZAIN ID', chainId);
 
         const pageMeta = {
           meta: {
@@ -570,6 +573,9 @@ export const getRpcMethodMiddleware = ({
           checkSelectedAddress: isMMSDK || isWalletConnect,
         });
 
+        console.log('ENTER HEREEE d', req.params[0]);
+        console.log('ENTER HEREEE d1', req.params[1]);
+
         const rawSig = await SignatureController.newUnsignedTypedMessage(
           {
             data: req.params[1],
@@ -580,6 +586,8 @@ export const getRpcMethodMiddleware = ({
           req,
           'V4',
         );
+
+        console.log('ENTER Hereee raw sig', rawSig);
 
         res.result = rawSig;
       },
