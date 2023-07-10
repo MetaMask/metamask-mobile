de52e2ab3394bbb37d68c0e3c803aa96b0a9d726
Matthew Walsh <matthew.walsh@consensys.net>
2023-07-10 10:30:36 +0100

    Add unit tests for selectors and hooks

---
+++ app/components/hooks/useApprovalFlow.test.ts
@@ -0,0 +1,46 @@
+import { ApprovalControllerState } from '@metamask/approval-controller';
+import { useSelector } from 'react-redux';
+import useApprovalFlow from './useApprovalFlow';
+
+jest.mock(
+  'react-redux',
+  () => ({
+    useSelector: jest.fn(),
+  }),
+  { virtual: true },
+);
+
+const APPROVAL_FLOW_MOCK = {
+  id: 'testId1',
+  loadingText: 'testLoadingText1',
+};
+
+const APPROVAL_FLOW_MOCK_2 = {
+  id: 'testId2',
+  loadingText: 'testLoadingText2',
+};
+
+const mockSelector = (
+  approvalFlows: ApprovalControllerState['approvalFlows'],
+) => {
+  (useSelector as jest.MockedFn<typeof useSelector>).mockReturnValue(
+    approvalFlows,
+  );
+};
+
+describe('useApprovalFlow', () => {
+  it('returns the approval flow if one exists', () => {
+    mockSelector([APPROVAL_FLOW_MOCK]);
+    expect(useApprovalFlow()).toEqual({ approvalFlow: APPROVAL_FLOW_MOCK });
+  });
+
+  it('returns the last approval flow if multiple exist', () => {
+    mockSelector([APPROVAL_FLOW_MOCK, APPROVAL_FLOW_MOCK_2]);
+    expect(useApprovalFlow()).toEqual({ approvalFlow: APPROVAL_FLOW_MOCK_2 });
+  });
+
+  it('returns undefined if no approval flows exist', () => {
+    mockSelector([]);
+    expect(useApprovalFlow()).toEqual({ approvalFlow: undefined });
+  });
+});
