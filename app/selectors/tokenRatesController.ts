dd66e58208aa727a94733f2d8ac6c4250263b47d
Salah <salah-eddine.saakoun@consensys.net>
2023-07-13 22:59:25 +0100

    fix: remove optional chaining operator

--- app/selectors/tokenRatesController.ts
+++ app/selectors/tokenRatesController.ts
@@ -4,10 +4,10 @@ import { TokenRatesState } from '@metamask/assets-controllers';
 import { EngineState } from './types';
 
 const selectTokenRatesControllerState = (state: EngineState) =>
-  state?.engine?.backgroundState?.TokenRatesController;
+  state.engine.backgroundState.TokenRatesController;
 
 export const selectContractExchangeRates = createSelector(
   selectTokenRatesControllerState,
   (tokenRatesControllerState: TokenRatesState) =>
-    tokenRatesControllerState?.contractExchangeRates,
+    tokenRatesControllerState.contractExchangeRates,
 );
