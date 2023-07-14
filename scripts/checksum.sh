80a8d6cd889e75facb6993296717438629e93a74
Owen Craston <owen.craston@consensys.net>
2023-07-14 11:20:38 -0700

    [Flask] bitrise pipelines (#6645)
    * basic pipeline scaffold
    
    * flask release ios
    
    * android release scripts
    
    * check for sentry props on flask build
    
    * bitrise certificate
    
    * debug/release provisioning profiles
    
    * basic build steps
    
    * flask pipelines
    
    * fix yaml
    
    * remap flask variables
    
    * remove pubnub keys
    
    * add deploy pipelines
    
    * fix file paths and undo edits to qa build
    
    * new flask release pipelines
    
    * update set-versions script and try to fix sourcemaps for flask
    
    * try a different sourcemp filepath
    
    * upgrade ruby version
    
    * update signing
    
    * set env to flask
    
    * square icons
    
    * manually remove icon alpha
    
    * add archive path var
    
    * introduce build types in build pipeline
    
    * remove echo
    
    * pass params to derive sentry environment and tests
    
    * new build numbers
    
    * fix: update cookie-tough dependency (#6772)
    
    * move versions into product flavours
    
    * test prod build with flask variables
    
    * cleanup deriveSentryEnvironment function
    
    * cleanup comment
    
    * reset build numbers
    
    * enable deploy to android and pass in env variable
    
    * new syntax
    
    * is expand true
    
    ---------

--- scripts/checksum.sh
+++ scripts/checksum.sh
@@ -8,7 +8,12 @@ FILE=./android/app/build/outputs/apk/qa/release/app-qa-release.apk
   if test -f "$FILE"; then
     shasum -a 512 "$FILE" > ./android/app/build/outputs/apk/qa/release/sha512sums.txt
   fi
+elif [ "$MODE" == "Flask" ]; then
+FILE=./android/app/build/outputs/apk/flask/release/app-flask-release.apk
 
+  if test -f "$FILE"; then
+    shasum -a 512 "$FILE" > ./android/app/build/outputs/apk/flask/release/sha512sums.txt
+  fi
 else
 FILE=./android/app/build/outputs/apk/prod/release/app-prod-release.apk
 
