5a66492e8d73c1ca4e0cc0fce21916fc15d14c03
Brian August Nguyen <brianacnguyen@gmail.com>
2023-07-14 09:41:35 -0700

    Updated constants structure for button to remove (#6808)
    require cycles

--- app/component-library/components/Buttons/Button/foundation/ButtonBase/ButtonBase.constants.ts
+++ app/component-library/components/Buttons/Button/foundation/ButtonBase/ButtonBase.constants.ts
@@ -1,17 +1,13 @@
 /* eslint-disable no-console */
 /* eslint-disable import/prefer-default-export */
 // External dependencies.
-import {
-  DEFAULT_BUTTON_SIZE,
-  DEFAULT_BUTTON_WIDTH,
-} from '../../Button.constants';
 import { ButtonBaseProps } from './ButtonBase.types';
 import { IconName, IconSize } from '../../../../Icons/Icon';
 import { ButtonSize, ButtonWidthTypes } from '../../Button.types';
 
 // Defaults
-export const DEFAULT_BUTTONBASE_SIZE = DEFAULT_BUTTON_SIZE;
-export const DEFAULT_BUTTONBASE_WIDTH = DEFAULT_BUTTON_WIDTH;
+export const DEFAULT_BUTTONBASE_SIZE = ButtonSize.Md;
+export const DEFAULT_BUTTONBASE_WIDTH = ButtonWidthTypes.Auto;
 export const DEFAULT_BUTTONBASE_ICON_SIZE = IconSize.Sm;
 
 // Samples
