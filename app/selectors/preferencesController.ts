3623cd9ae09bfd14989bbe78c1c93309a9a6ebaf
Salah <salah-eddine.saakoun@consensys.net>
2023-07-17 16:03:50 +0100

    fix: remove optional chaining operator

--- app/selectors/preferencesController.ts
+++ app/selectors/preferencesController.ts
@@ -3,54 +3,54 @@ import { PreferencesState } from '@metamask/preferences-controller';
 import { EngineState } from './types';
 
 const selectPreferencesControllerState = (state: EngineState) =>
-  state?.engine?.backgroundState?.PreferencesController;
+  state.engine.backgroundState.PreferencesController;
 
 export const selectIdentities = createSelector(
   selectPreferencesControllerState,
   (preferencesControllerState: PreferencesState) =>
-    preferencesControllerState?.identities,
+    preferencesControllerState.identities,
 );
 
 export const selectIpfsGateway = createSelector(
   selectPreferencesControllerState,
   (preferencesControllerState: PreferencesState) =>
-    preferencesControllerState?.ipfsGateway,
+    preferencesControllerState.ipfsGateway,
 );
 
 export const selectSelectedAddress = createSelector(
   selectPreferencesControllerState,
   (preferencesControllerState: PreferencesState) =>
-    preferencesControllerState?.selectedAddress,
+    preferencesControllerState.selectedAddress,
 );
 
 export const selectFrequentRpcList = createSelector(
   selectPreferencesControllerState,
   (preferencesControllerState: PreferencesState) =>
-    preferencesControllerState?.frequentRpcList,
+    preferencesControllerState.frequentRpcList,
 );
 
 export const selectUseNftDetection = createSelector(
   selectPreferencesControllerState,
   (preferencesControllerState: PreferencesState) =>
-    preferencesControllerState?.useNftDetection,
+    preferencesControllerState.useNftDetection,
 );
 
 export const selectUseTokenDetection = createSelector(
   selectPreferencesControllerState,
   (preferencesControllerState: PreferencesState) =>
-    preferencesControllerState?.useTokenDetection,
+    preferencesControllerState.useTokenDetection,
 );
 
 export const selectOpenSeaEnabled = createSelector(
   selectPreferencesControllerState,
   (preferencesControllerState: PreferencesState) =>
-    preferencesControllerState?.openSeaEnabled,
+    preferencesControllerState.openSeaEnabled,
 );
 
 export const selectDisabledRpcMethodPreferences = createSelector(
   selectPreferencesControllerState,
   (preferencesControllerState: PreferencesState) =>
-    preferencesControllerState?.disabledRpcMethodPreferences,
+    preferencesControllerState.disabledRpcMethodPreferences,
 );
 
 // isMultiAccountBalancesEnabled is a patched property - ref patches/@metamask+preferences-controller+2.1.0.patch
@@ -61,7 +61,7 @@ export const selectIsMultiAccountBalancesEnabled = createSelector(
       preferencesControllerState as PreferencesState & {
         isMultiAccountBalancesEnabled: boolean;
       }
-    )?.isMultiAccountBalancesEnabled,
+    ).isMultiAccountBalancesEnabled,
 );
 
 // showTestNetworks is a patched property - ref patches/@metamask+preferences-controller+2.1.0.patch
@@ -72,5 +72,5 @@ export const selectShowTestNetworks = createSelector(
       preferencesControllerState as PreferencesState & {
         showTestNetworks: boolean;
       }
-    )?.showTestNetworks,
+    ).showTestNetworks,
 );
