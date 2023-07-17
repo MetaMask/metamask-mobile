79925c0a30f5ac370269715e47fd4d9f1461c928
Matthew Walsh <matthew.walsh@consensys.net>
2023-07-17 18:31:57 +0100

    Fix unit tests

--- app/components/Approvals/FlowLoaderModal/FlowLoaderModal.tsx
+++ app/components/Approvals/FlowLoaderModal/FlowLoaderModal.tsx
@@ -1,8 +1,8 @@
 import React, { useCallback } from 'react';
 import useApprovalRequest from '../../hooks/useApprovalRequest';
 import ApprovalModal from '../ApprovalModal';
-import ApprovalFlowLoader from '../../UI/ApprovalFlowLoader';
 import useApprovalFlow from '../../hooks/useApprovalFlow';
+import ApprovalFlowLoader from '../../UI/Approval/ApprovalFlowLoader';
 
 const FlowLoaderModal = () => {
   const { approvalRequest } = useApprovalRequest();
