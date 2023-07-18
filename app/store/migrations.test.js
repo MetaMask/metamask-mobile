a9005ff2dcf88aafedbdcb214df0707a0e7a4976
tommasini <tommasini15@gmail.com>
2023-07-18 21:51:11 +0100

    update test with mock data

--- app/store/migrations.test.js
+++ app/store/migrations.test.js
@@ -2,13 +2,28 @@ import { migrations, version } from './migrations';
 
 describe('Redux Persist Migrations', () => {
   it('should apply migration number 19 and return state', () => {
-    let currentState = {};
+    let currentState = {
+      recents: '0x1',
+    };
 
     const migration = migrations[19];
 
     currentState = migration(currentState);
 
-    expect(currentState).toBeDefined(); // Ensure state is defined after the migration
+    expect(currentState).toBeDefined();
+    expect(currentState.recents).toBeUndefined();
+  });
+  it('should apply last migration version and return state', () => {
+    let currentState = {
+      recents: '0x1',
+    };
+
+    const migration = migrations[version];
+
+    currentState = migration(currentState);
+
+    expect(currentState).toBeDefined();
+    expect(currentState.recents).toBeUndefined();
   });
   it('should have all migrations up to the latest version', () => {
     // Assert that the latest migration index matches the version constant
