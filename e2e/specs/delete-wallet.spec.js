32f340fd7cdf8cebb3db85c6dd3a3e43558e7d9e
Vinicius Stevam <vinicius.stevam@consensys.net>
2023-07-19 19:57:14 +0100

    use fixtures on delete wallet test

--- e2e/specs/delete-wallet.spec.js
+++ e2e/specs/delete-wallet.spec.js
@@ -13,89 +13,67 @@ import SecurityAndPrivacyView from '../pages/Drawer/Settings/SecurityAndPrivacy/
 import ChangePasswordView from '../pages/Drawer/Settings/SecurityAndPrivacy/ChangePasswordView';
 
 import DeleteWalletModal from '../pages/modals/DeleteWalletModal';
-import WhatsNewModal from '../pages/modals/WhatsNewModal';
-import { importWalletWithRecoveryPhrase } from '../viewHelper';
-import Accounts from '../../wdio/helpers/Accounts';
+import { skipOnboarding } from '../viewHelper';
 import TabBarComponent from '../pages/TabBarComponent';
+import FixtureBuilder from '../fixtures/fixture-builder';
+import { withFixtures } from '../fixtures/fixture-helper';
 
 describe(
-  Smoke(
-    'Import wallet with 24 word SRP, change password then delete wallet flow',
-  ),
+  Smoke('Log in into the app, change password then delete wallet flow'),
   () => {
-    let validAccount;
-
-    beforeAll(() => {
-      validAccount = Accounts.getValidAccount();
-    });
+    const PASSWORD = '123123123';
 
     beforeEach(() => {
       jest.setTimeout(150000);
     });
-    it('should import wallet and go to the wallet view', async () => {
-      await importWalletWithRecoveryPhrase();
-    });
 
-    it('should tap on "Got it" to dimiss the whats new modal', async () => {
-      // dealing with flakiness on bitrise.
-      await TestHelpers.delay(2500);
-      try {
-        await WhatsNewModal.isVisible();
-        await WhatsNewModal.tapCloseButton();
-      } catch {
-        //
-      }
-    });
+    it('should log in into the app, change password then delete wallet flow', async () => {
+      const fixture = new FixtureBuilder().build();
+      await withFixtures({ fixture, restartDevice: true }, async () => {
+        await skipOnboarding();
 
-    it('should go to settings then security & privacy', async () => {
-      await TabBarComponent.tapSettings();
-      await SettingsView.tapSecurityAndPrivacy();
-      await SecurityAndPrivacyView.scrollToChangePasswordView();
-      await SecurityAndPrivacyView.isChangePasswordSectionVisible();
-    });
+        // should go to settings then security & privacy
+        await TabBarComponent.tapSettings();
+        await SettingsView.tapSecurityAndPrivacy();
+        await SecurityAndPrivacyView.scrollToChangePasswordView();
+        await SecurityAndPrivacyView.isChangePasswordSectionVisible();
 
-    it('should confirm password before changing it', async () => {
-      await SecurityAndPrivacyView.tapChangePasswordButton();
+        // should confirm password before changing it
+        await SecurityAndPrivacyView.tapChangePasswordButton();
 
-      await ChangePasswordView.isVisible();
-      await ChangePasswordView.typeInConfirmPasswordInputBox(
-        validAccount.password,
-      );
-      //await ChangePasswordView.tapConfirmButton();
-    });
+        await ChangePasswordView.isVisible();
+        await ChangePasswordView.typeInConfirmPasswordInputBox(PASSWORD);
 
-    it('should change the password', async () => {
-      const NEW_PASSWORD = '11111111';
-      await ChangePasswordView.tapIUnderstandCheckBox();
-      await ChangePasswordView.enterPassword(NEW_PASSWORD);
-      await ChangePasswordView.reEnterPassword(NEW_PASSWORD);
+        // should change the password
+        const NEW_PASSWORD = '11111111';
+        await ChangePasswordView.tapIUnderstandCheckBox();
+        await ChangePasswordView.enterPassword(NEW_PASSWORD);
+        await ChangePasswordView.reEnterPassword(NEW_PASSWORD);
 
-      if ((await device.getPlatform) === 'ios') {
-        await ChangePasswordView.tapResetPasswordButton();
-      }
-    });
+        if ((await device.getPlatform) === 'ios') {
+          await ChangePasswordView.tapResetPasswordButton();
+        }
 
-    it('should lock wallet from Settings', async () => {
-      await device.disableSynchronization(); // because the SRP tutorial video prevents the test from moving forward
-      await SecurityAndPrivacyView.tapBackButton();
-      await device.enableSynchronization();
-      await SettingsView.tapLock();
-      await SettingsView.tapYesAlertButton();
-      await LoginView.isVisible();
-    });
+        // should lock wallet from Settings
+        await device.disableSynchronization(); // because the SRP tutorial video prevents the test from moving forward
+        await SecurityAndPrivacyView.tapBackButton();
+        await device.enableSynchronization();
+        await SettingsView.tapLock();
+        await SettingsView.tapYesAlertButton();
+        await LoginView.isVisible();
 
-    it('should tap reset wallet button', async () => {
-      await LoginView.tapResetWalletButton();
+        // should tap reset wallet button
+        await LoginView.tapResetWalletButton();
 
-      await DeleteWalletModal.isVisible();
-    });
+        await DeleteWalletModal.isVisible();
 
-    it('should delete wallet', async () => {
-      await DeleteWalletModal.tapIUnderstandButton();
-      await DeleteWalletModal.typeDeleteInInputBox();
-      await DeleteWalletModal.tapDeleteMyWalletButton();
-      await TestHelpers.delay(2000);
-      await OnboardingView.isVisible();
+        // should delete wallet
+        await DeleteWalletModal.tapIUnderstandButton();
+        await DeleteWalletModal.typeDeleteInInputBox();
+        await DeleteWalletModal.tapDeleteMyWalletButton();
+        await TestHelpers.delay(2000);
+        await OnboardingView.isVisible();
+      });
     });
   },
 );
