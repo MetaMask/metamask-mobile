0c0a38ea0b85eea624d30a1e2c5a1631f3b9e05f
Mark Stacey <markjstacey@gmail.com>
2023-07-24 20:20:02 -0230

    test: Improve migration unit tests
    The migration unit tests have been updated to group tests by the
    migration number. One additional test case has been added as well, to
    cover the case where the affected state doesn't exist.
    
    This change was done to simplify PR #6872, which is part of
    https://github.com/MetaMask/mobile-planning/issues/798

--- app/store/migrations.test.js
+++ app/store/migrations.test.js
@@ -1,7 +1,14 @@
+import { cloneDeep } from 'lodash';
 import { migrations, version } from './migrations';
 
 describe('Redux Persist Migrations', () => {
+  it('should have all migrations up to the latest version', () => {
+    // Assert that the latest migration index matches the version constant
+    expect(Object.keys(migrations).length - 1).toBe(version);
+  });
+
   it('should apply last migration version and return state', () => {
+    // update this state to be compatible with the most recent migration
     const oldState = {
       recents: '0x1',
     };
@@ -11,10 +18,31 @@ describe('Redux Persist Migrations', () => {
     const newState = migration(oldState);
 
     expect(newState).toBeDefined();
-    expect(newState.recents).toBeUndefined();
   });
-  it('should have all migrations up to the latest version', () => {
-    // Assert that the latest migration index matches the version constant
-    expect(Object.keys(migrations).length - 1).toBe(version);
+
+  describe('#19', () => {
+    it('should not change state if recents are missing', () => {
+      const oldState = {
+        foo: 'bar',
+      };
+
+      const migration = migrations[version];
+
+      const newState = migration(cloneDeep(oldState));
+
+      expect(newState).toStrictEqual(oldState);
+    });
+
+    it('should remove recents', () => {
+      const oldState = {
+        recents: '0x1',
+      };
+
+      const migration = migrations[version];
+
+      const newState = migration(oldState);
+
+      expect(newState).toStrictEqual({});
+    });
   });
 });
