13dbbc7b408e78cfc05fc9c1136fb38a26d5ebee
Vinicius Stevam <vinicius.stevam@consensys.net>
2023-07-26 19:59:50 +0100

    convert template renderer to typescript

--- app/components/UI/TemplateRenderer/SafeComponentList.ts
+++ app/components/UI/TemplateRenderer/SafeComponentList.ts
@@ -5,9 +5,10 @@ import Text from '../../../component-library/components/Texts/Text';
 import Icon from '../../../component-library/components/Icons/Icon';
 import BottomSheetFooter from '../../../component-library/components/BottomSheets/BottomSheetFooter';
 import { View } from 'react-native';
+import { SafeComponentList } from './types';
 
 // eslint-disable-next-line import/prefer-default-export
-export const safeComponentList = {
+export const safeComponentList: SafeComponentList = {
   BottomSheetFooter,
   Button,
   Icon,
