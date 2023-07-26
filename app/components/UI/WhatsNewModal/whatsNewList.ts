ad00504dd87ca8d34d325b317e4a91885614d425
Olusegun Akintayo <akintayo.segun@gmail.com>
2023-07-26 09:41:17 +0100

    image should be first fix podfile
    Signed-off-by: Olusegun Akintayo <akintayo.segun@gmail.com>

--- app/components/UI/WhatsNewModal/whatsNewList.ts
+++ app/components/UI/WhatsNewModal/whatsNewList.ts
@@ -4,9 +4,9 @@ import { WhatsNew } from './types';
 
 export const whatsNew: WhatsNew = {
   // All users that have <6.4.0 and are updating to >=6.4.0 should see
-  onlyUpdates: false, // Users who updated the app and new installs will see this.
-  maxLastAppVersion: '7.3.0', // Only users who had a previous version <6.4.0 version will see this
-  minAppVersion: '7.3.0', // Only users who updated to a version >= 6.4.0 will see this
+  onlyUpdates: false, // false: Users who updated the app and new installs will see this. true: only users who update will see it
+  maxLastAppVersion: '7.3.0', // Only users who had a previous version <7.3.0 version will see this
+  minAppVersion: '7.3.0', // Only users who updated to a version >= 7.3.0 will see this
   /**
    * Slides utilizes a templating system in the form of a 2D array, which is eventually rendered within app/components/UI/WhatsNewModal/index.js.
    * The root layer determines the number of slides. Ex. To display 3 slides, the root layer should contain 3 arrays.
@@ -16,6 +16,10 @@ export const whatsNew: WhatsNew = {
    */
   slides: [
     [
+      {
+        type: 'image',
+        image: require('../../../images/whats_new_blockaid.png'),
+      },
       {
         type: 'title',
         title: strings('whats_new.blockaid.title'),
@@ -31,10 +35,6 @@ export const whatsNew: WhatsNew = {
       {
         type: 'description',
         description: strings('whats_new.blockaid.description_3'),
-      },
-      {
-        type: 'image',
-        image: require('../../../images/whats_new_blockaid.png'),
       },
       {
         type: 'button',
