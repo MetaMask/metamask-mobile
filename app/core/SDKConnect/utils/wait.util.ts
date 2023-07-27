d895301ed05320b1f37f52432e90ee20899fe64d
abretonc7s <arthur.breton@consensys.net>
2023-07-27 18:17:50 +0800

    feat: temp commit for metamask

--- app/core/SDKConnect/utils/wait.util.ts
+++ app/core/SDKConnect/utils/wait.util.ts
@@ -1,9 +1,9 @@
 import { KeyringController } from '@metamask/keyring-controller';
-import RPCQueueManager from '../RPCQueueManager';
 import { AndroidClient } from '../AndroidSDK/android-sdk-types';
+import RPCQueueManager from '../RPCQueueManager';
+import { SDKConnect } from '../SDKConnect';
 
 const MAX_QUEUE_LOOP = 50; // 50 seconds
-
 export const wait = (ms: number) =>
   new Promise((resolve) => {
     setTimeout(resolve, ms);
@@ -38,13 +38,28 @@ export const waitForKeychainUnlocked = async ({
   }
 };
 
-const MAX_QUEUE_LOOP = 50; // 50 seconds
+export const waitForAndroidServiceBinding = async () => {
+  let i = 1;
+  while (SDKConnect.getInstance().isAndroidSDKBinded() === false) {
+    await wait(500);
+    i += 1;
+    if (i > 5 && i % 10 === 0) {
+      console.warn(`Waiting for Android service binding... ${i}s`);
+    }
+  }
+};
+
 export const waitForEmptyRPCQueue = async (manager: RPCQueueManager) => {
   let i = 0;
   let queue = Object.keys(manager.get());
   while (queue.length > 0) {
     queue = Object.keys(manager.get());
-    console.log(`RPC queue not empty [${queue.length}], waiting...`, queue);
+
+    // print the queue every 5 seconds
+    if (i % 5 === 0) {
+      console.warn(`RPC queue not empty after ${i}s`, queue);
+    }
+
     if (i++ > MAX_QUEUE_LOOP) {
       console.warn(`RPC queue not empty after ${MAX_QUEUE_LOOP} seconds`);
       break;
