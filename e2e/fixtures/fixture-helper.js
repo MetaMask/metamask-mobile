0cd1c98240380f7fac475adbceb60f07393096b6
Vinicius Stevam <vinicius.stevam@consensys.net>
2023-07-29 07:29:02 +0100

    add more documentation

--- e2e/fixtures/fixture-helper.js
+++ e2e/fixtures/fixture-helper.js
@@ -8,6 +8,7 @@ const fixtureServer = new FixtureServer();
 
 const FIXTURE_SERVER_URL = 'http://localhost:12345/state.json';
 
+// checks if server has already been started
 const isFixtureServerStarted = async () => {
   try {
     const response = await axios.get(FIXTURE_SERVER_URL);
@@ -17,7 +18,17 @@ const isFixtureServerStarted = async () => {
   }
 };
 
+/**
+ * Loads a fixture into the fixture server.
+ *
+ * @param {Object} options - An object containing the fixture to load.
+ * @param {Object} [options.fixture] - The fixture data to load. If not provided, a default fixture is created.
+ * @returns {Promise<void>} - A promise that resolves once the fixture is successfully loaded.
+ * @throws {Error} - Throws an error if the fixture fails to load or if the fixture server is not properly set up.
+ */
 export const loadFixture = async ({ fixture } = {}) => {
+  // If no fixture is provided, the `onboarding` option is set to `true` by default, which means
+  // the app will be loaded without any fixtures and will start and go through the onboarding process.
   const state = fixture || new FixtureBuilder({ onboarding: true }).build();
   await fixtureServer.loadJsonState(state);
   // Checks if state is loaded
@@ -50,6 +61,17 @@ export const stopFixtureServer = async () => {
   console.log('The fixture server is stopped');
 };
 
+/**
+ * Executes a test suite with fixtures by setting up a fixture server, loading a specified fixture,
+ * and running the test suite. After the test suite execution, it stops the fixture server.
+ *
+ * @param {Object} options - An object containing configuration options.
+ * @param {Object} options.fixture - The fixture to load.
+ * @param {boolean} [options.restartDevice=false] - If true, restarts the app to apply the loaded fixture.
+ * @param {Function} testSuite - The test suite function to execute after setting up the fixture.
+ * @returns {Promise<void>} - A promise that resolves once the test suite completes.
+ * @throws {Error} - Throws an error if an exception occurs during the test suite execution.
+ */
 export async function withFixtures(options, testSuite) {
   const { fixture, restartDevice = false } = options;
   let failed;
--- e2e/init.js
+++ e2e/init.js
@@ -10,7 +10,9 @@ beforeAll(async () => {
   if (!isRunningBitrise) {
     await device.reverseTcpPort(12345);
   }
+  // Start server cause all E2E test relies on the new infrastructure for fixtures once IS_TEST is `true`
   await startFixtureServer();
+  // Load an empty fixtures to go through the onboarding process
   await loadFixture();
   await device.launchApp();
 });
