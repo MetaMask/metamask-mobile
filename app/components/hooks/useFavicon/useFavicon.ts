b149005d7ba5400c08173bb8af36bda0ca8d7fc9
Nicolas MASSART <nicolas.massart@consensys.net>
2023-08-02 13:44:08 +0200

    making the code more readable

--- app/components/hooks/useFavicon/useFavicon.ts
+++ app/components/hooks/useFavicon/useFavicon.ts
@@ -1,28 +1,60 @@
 import RNFetchBlob, { FetchBlobResponse } from 'rn-fetch-blob';
 import { DOMParser } from 'xmldom';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { ImageSourcePropType } from 'react-native';
-import { logger } from 'ethers';
 import { forEach } from 'lodash';
+import Logger from '../../../util/Logger';
+
+//Empty value uset to trigger fallback favicon in the UI and prevent use of undefined
+const EMPTY_FAVICON_URI = {};
 
 const useFavicon = (origin: string) => {
-  const [faviconURI, setFaviconURI] = useState<ImageSourcePropType>({});
+  const [faviconURI, setFaviconURI] =
+    useState<ImageSourcePropType>(EMPTY_FAVICON_URI);
 
-  const fetchHtmlSource = async (url: string) =>
-    RNFetchBlob.config({ fileCache: true })
-      .fetch('GET', url)
-      .then((response: FetchBlobResponse) => response.text())
-      .catch(() => {
-        logger.warn('useFavicon fetchHtmlSource failed', url);
-        return null;
+  const fetchHtmlSource = useCallback(
+    async (url: string) =>
+      RNFetchBlob.config({ fileCache: true })
+        .fetch('GET', url)
+        .then((response: FetchBlobResponse) => response.text())
+        .catch(() => {
+          Logger.log('useFavicon fetchHtmlSource failed', url);
+        }),
+    [],
+  );
+
+  const parseHtmlSource = useCallback(
+    async (htmlSource: string) =>
+      // use a return statement for the error handler to avoid the console warning
+      // as any error will result in fallback favicon
+      new DOMParser({
+        errorHandler: (level, msg) => {
+          if (level === 'error') Logger.log(level, msg);
+        },
+      }).parseFromString(htmlSource, 'text/html'),
+    [],
+  );
+
+  const getFaviconUriFromLinks = useCallback(
+    (links: HTMLCollectionOf<Element>) => {
+      // use lodash forEach as the collection require iteration to prevent named items to be returned
+      let uri = EMPTY_FAVICON_URI;
+      forEach(links, (link) => {
+        const rel = link.getAttribute('rel');
+        if (rel && (rel === 'shortcut icon' || rel === 'icon')) {
+          const href = link.getAttribute('href');
+          if (href && href !== '') {
+            const faviconURL = new URL(href, origin);
+            uri = { uri: faviconURL.toString() };
+            return false; //stop at first favicon found, same behaviour as browser extension
+          }
+        }
       });
 
-  const parseHtmlSource = async (htmlSource: string) =>
-    // use a return statement for the error handler to avoid the console warning
-    // as any error will result in fallback favicon
-    new DOMParser({
-      errorHandler: (level, msg) => logger.debug(level, msg),
-    }).parseFromString(htmlSource, 'text/html');
+      return uri;
+    },
+    [origin],
+  );
 
   useEffect(() => {
     (async () => {
@@ -31,31 +63,16 @@ const useFavicon = (origin: string) => {
         // then it's probably an already registered origin
         // we should probably just return the cached favicon
         // using the origin as key
-        logger.debug('useFavicon useEffect origin without scheme', origin);
+        await Logger.log('useFavicon useEffect origin without scheme', origin);
       }
-      const htmlSource = await fetchHtmlSource(origin);
-      if (!htmlSource) {
-        return;
-      }
-      const htmlDoc = await parseHtmlSource(htmlSource);
-      if (!htmlDoc) {
-        return;
-      }
-      const links = await htmlDoc.getElementsByTagName('link');
-      // use lodash forEach as the collection require iteration to prevent named items to be returned
-      forEach(links, (link) => {
-        const rel = link.getAttribute('rel');
-        if (rel && (rel === 'shortcut icon' || rel === 'icon')) {
-          const href = link.getAttribute('href');
-          if (href && href !== '') {
-            const faviconURL = new URL(href, origin);
-            setFaviconURI({ uri: faviconURL.toString() });
-            return false; //stop at first favicon found, same behaviour as browser extension
-          }
-        }
-      });
+
+      fetchHtmlSource(origin)
+        .then((htmlSource) => parseHtmlSource(htmlSource))
+        .then((htmlDoc) => htmlDoc.getElementsByTagName('link'))
+        .then((links) => getFaviconUriFromLinks(links))
+        .then((uri) => setFaviconURI(uri));
     })();
-  }, [origin]);
+  }, [origin, fetchHtmlSource, parseHtmlSource, getFaviconUriFromLinks]);
 
   return faviconURI;
 };
