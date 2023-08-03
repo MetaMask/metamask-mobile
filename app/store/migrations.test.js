b4f1fb1df6a9dd3994554d18b29674d3a59e98b7
João Loureiro <1649425+jpcloureiro@users.noreply.github.com>
2023-08-03 15:32:53 +0100

    migration: replace outdated ipfs gateway with default one

--- app/store/migrations.test.js
+++ app/store/migrations.test.js
@@ -1,5 +1,7 @@
 import { cloneDeep } from 'lodash';
 import { migrations, version } from './migrations';
+import initialBackgroundState from '../util/test/initial-background-state.json';
+import { IPFS_DEFAULT_GATEWAY_URL } from '../../app/constants/network';
 
 describe('Redux Persist Migrations', () => {
   it('should have all migrations up to the latest version', () => {
@@ -43,6 +45,48 @@ describe('Redux Persist Migrations', () => {
       const newState = migration(oldState);
 
       expect(newState).toStrictEqual({});
+    });
+  });
+
+  describe('#20', () => {
+    it('should not change state if ipfs gateway in use is not outdated', () => {
+      const currentState = {
+        engine: {
+          backgroundState: initialBackgroundState,
+        },
+      };
+
+      const migration = migrations[20];
+
+      const newState = migration(currentState);
+
+      expect(newState).toStrictEqual(currentState);
+    });
+
+    it('should change outdated ipfs gateway to default one', () => {
+      const stateWithIpfsGateway = (ipfsGateway) => ({
+        engine: {
+          backgroundState: {
+            ...initialBackgroundState,
+            PreferencesController: {
+              ...initialBackgroundState.PreferencesController,
+              ipfsGateway,
+            },
+          },
+        },
+      });
+
+      // State with outdated ipfs gateway
+      const currentState = stateWithIpfsGateway('https://hardbin.com/ipfs/');
+
+      // State with default ipfs gateway
+      const newStateExpectation = stateWithIpfsGateway(
+        IPFS_DEFAULT_GATEWAY_URL,
+      );
+
+      const migration = migrations[20];
+      const newState = migration(currentState);
+      expect(newState).toStrictEqual(newStateExpectation);
     });
   });
 });
