2326d8707aeb01480d765bc417553b0517ab8411
João Loureiro <1649425+jpcloureiro@users.noreply.github.com>
2023-08-16 16:00:56 +0100

    fix: vault recovery & invalid password error (#6957)
    * ResetPassword: remove unused redux action passwordUnset
    
    * Authentication: set `state.user.passwordSet` boolean after successful auth
    
    * Vault Recovery: reset engine initialization
    
    set EngineService engineInitialized instance to false to ensure we
    dispatch INIT_BG_STATE_KEY. This will save KeyringController state object
    to be saved into redux store state
    
    * EngineService: add unit tests
    
    ---------
    
    Co-authored-by: Owen Craston <owengc12@gmail.com>
    Co-authored-by: Cal Leung <cleun007@gmail.com>

--- app/components/Views/ResetPassword/index.js
+++ app/components/Views/ResetPassword/index.js
@@ -18,11 +18,7 @@ import CheckBox from '@react-native-community/checkbox';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import AsyncStorage from '../../../store/async-storage-wrapper';
 import { connect } from 'react-redux';
-import {
-  passwordSet,
-  passwordUnset,
-  seedphraseNotBackedUp,
-} from '../../../actions/user';
+import { passwordSet, seedphraseNotBackedUp } from '../../../actions/user';
 import { setLockTime } from '../../../actions/settings';
 import StyledButton from '../../UI/StyledButton';
 import Engine from '../../../core/Engine';
@@ -837,7 +833,6 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   passwordSet: () => dispatch(passwordSet()),
-  passwordUnset: () => dispatch(passwordUnset()),
   setLockTime: (time) => dispatch(setLockTime(time)),
   seedphraseNotBackedUp: () => dispatch(seedphraseNotBackedUp()),
 });
