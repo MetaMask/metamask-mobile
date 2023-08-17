5197aef7b57ae5acbda25f80ef665df565bd385d
tommasini <tommasini15@gmail.com>
2023-08-17 01:44:20 +0100

    add test ids to constants

--- app/components/Views/Settings/SecuritySettings/SecuritySettings.test.tsx
+++ app/components/Views/Settings/SecuritySettings/SecuritySettings.test.tsx
@@ -4,6 +4,23 @@ import renderWithProvider from '../../../../util/test/renderWithProvider';
 import SecuritySettings from './SecuritySettings';
 import initialBackgroundState from '../../../../util/test/initial-background-state.json';
 import { CHANGE_PASSWORD_TITLE_ID } from '../../../../constants/test-ids';
+import { AUTO_LOCK_SECTION } from './Sections/AutoLock/constants';
+import {
+  BATCH_BALANCE_REQUESTS_SECTION,
+  CLEAR_BROWSER_HISTORY_SECTION,
+  CLEAR_PRIVACY_SECTION,
+  DELETE_METRICS_BUTTON,
+  IPFS_GATEWAY_SECTION,
+  LOGIN_OPTIONS,
+  META_METRICS_SECTION,
+  NFT_AUTO_DETECT_MODE_SECTION,
+  NFT_OPEN_SEA_MODE_SECTION,
+  REVEAL_PRIVATE_KEY_SECTION,
+  SDK_SECTION,
+  SECURITY_SETTINGS_DELETE_WALLET_BUTTON,
+  THIRD_PARTY_SECTION,
+  TURN_ON_REMEMBER_ME,
+} from './SecuritySettings.constants';
 
 const initialState = {
   privacy: { approvedHosts: {} },
@@ -72,21 +89,21 @@ describe('SecuritySettings', () => {
     );
     expect(getByText('Protect your wallet')).toBeTruthy();
     expect(getByTestId(CHANGE_PASSWORD_TITLE_ID)).toBeTruthy();
-    expect(getByTestId('auto-lock-section')).toBeTruthy();
-    expect(getByTestId('login-options')).toBeTruthy();
-    expect(getByTestId('turn-on-remember-me')).toBeTruthy();
-    expect(getByTestId('reveal-private-key-section')).toBeTruthy();
-    expect(getByTestId('sdk-section')).toBeTruthy();
-    expect(getByTestId('clear-privacy-section')).toBeTruthy();
-    expect(getByTestId('clear-browser-history-section')).toBeTruthy();
-    expect(getByTestId('metametrics-section')).toBeTruthy();
-    expect(getByTestId('delete-metrics-button')).toBeTruthy();
-    expect(getByTestId('security-settings-delete-wallet-buttons')).toBeTruthy();
-    expect(getByTestId('batch-balance-requests-section')).toBeTruthy();
-    expect(getByTestId('third-party-section')).toBeTruthy();
-    expect(getByTestId('nft-opensea-mode-section')).toBeTruthy();
-    expect(getByTestId('nft-opensea-autodetect-mode-section')).toBeTruthy();
-    expect(getByTestId('ipfs-gateway-section')).toBeTruthy();
+    expect(getByTestId(AUTO_LOCK_SECTION)).toBeTruthy();
+    expect(getByTestId(LOGIN_OPTIONS)).toBeTruthy();
+    expect(getByTestId(TURN_ON_REMEMBER_ME)).toBeTruthy();
+    expect(getByTestId(REVEAL_PRIVATE_KEY_SECTION)).toBeTruthy();
+    expect(getByTestId(SDK_SECTION)).toBeTruthy();
+    expect(getByTestId(CLEAR_PRIVACY_SECTION)).toBeTruthy();
+    expect(getByTestId(CLEAR_BROWSER_HISTORY_SECTION)).toBeTruthy();
+    expect(getByTestId(META_METRICS_SECTION)).toBeTruthy();
+    expect(getByTestId(DELETE_METRICS_BUTTON)).toBeTruthy();
+    expect(getByTestId(SECURITY_SETTINGS_DELETE_WALLET_BUTTON)).toBeTruthy();
+    expect(getByTestId(BATCH_BALANCE_REQUESTS_SECTION)).toBeTruthy();
+    expect(getByTestId(THIRD_PARTY_SECTION)).toBeTruthy();
+    expect(getByTestId(NFT_OPEN_SEA_MODE_SECTION)).toBeTruthy();
+    expect(getByTestId(NFT_AUTO_DETECT_MODE_SECTION)).toBeTruthy();
+    expect(getByTestId(IPFS_GATEWAY_SECTION)).toBeTruthy();
     expect(getByText('Automatic security checks')).toBeTruthy();
   });
 });
