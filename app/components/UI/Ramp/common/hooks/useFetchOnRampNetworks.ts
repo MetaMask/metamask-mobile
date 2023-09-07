8199b77414f5e35848bc4031521664dc8a38395e
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2023-09-07 12:27:09 -0300

    refactor(ramp): add isLoading and error to fetchOnRampNetworks

--- app/components/UI/Ramp/common/hooks/useFetchOnRampNetworks.ts
+++ app/components/UI/Ramp/common/hooks/useFetchOnRampNetworks.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { SDK } from '../sdk';
 import {
@@ -15,19 +15,31 @@ import Logger from '../../../../../util/Logger';
 function useFetchOnRampNetworks() {
   const dispatch = useDispatch();
   const chainId = useSelector(chainIdSelector);
-  useEffect(() => {
-    const getNetworks = async () => {
-      try {
-        const networks = await SDK.getNetworks();
-        dispatch(updateOnRampNetworks(networks));
-      } catch (error) {
-        Logger.error('useFetchOnRampNetworks::getNetworks', error as Error);
-      }
-    };
-    getNetworks();
-  }, [chainId, dispatch]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<Error>();
 
-  return null;
+  const fetchNetworks = useCallback(async () => {
+    try {
+      setError(undefined);
+      setIsLoading(true);
+      const networks = await SDK.getNetworks();
+      dispatch(updateOnRampNetworks(networks));
+    } catch (requestError) {
+      setError(requestError as Error);
+      Logger.error(
+        'useFetchOnRampNetworks::getNetworks',
+        requestError as Error,
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    fetchNetworks();
+  }, [chainId, fetchNetworks]);
+
+  return [isLoading, error, fetchNetworks] as const;
 }
 
 export default useFetchOnRampNetworks;
