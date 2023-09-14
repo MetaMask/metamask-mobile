d808893c5fbd81b87792562f9ac9c1c5b398efaf
Yande <andepandy@outlook.com>
2023-09-14 16:47:40 +0100

    dynamic maxworker approach using env variable

--- e2e/jest.e2e.config.js
+++ e2e/jest.e2e.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-commonjs */
 require('dotenv').config({ path: '.e2e.env' });
-
+console.log(process.env.CI, 'TTTTTTTTTTTTTT');
 module.exports = {
   rootDir: '..',
   testMatch: [
@@ -8,7 +8,7 @@ module.exports = {
     '<rootDir>/e2e/specs/*/*.spec.js',
   ],
   testTimeout: 220000,
-  maxWorkers: 3,
+  maxWorkers: process.env.CI ? 3 : 1,
   setupFilesAfterEnv: ['<rootDir>/e2e/init.js'],
   globalSetup: 'detox/runners/jest/globalSetup',
   globalTeardown: 'detox/runners/jest/globalTeardown',
