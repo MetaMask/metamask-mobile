e38b86ffe1649a597dadf0e1a37aed97ea8d1a89
Yande <andepandy@outlook.com>
2023-09-14 17:56:01 +0100

    removed console.log

--- e2e/jest.e2e.config.js
+++ e2e/jest.e2e.config.js
@@ -1,7 +1,5 @@
 /* eslint-disable import/no-commonjs */
 require('dotenv').config({ path: '.e2e.env' });
-// eslint-disable-next-line no-console
-console.log(process.env.CI, 'TTTTTTTTTTTTTT');
 module.exports = {
   rootDir: '..',
   testMatch: [
