ae29ffebdb1bf32590efc0f032be264bfd12e31b
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2023-09-15 11:36:48 -0300

    feat(ramp): add getNetworkDetails SDK method call

--- app/components/UI/Ramp/common/hooks/useRampNetworksDetail.ts
+++ app/components/UI/Ramp/common/hooks/useRampNetworksDetail.ts
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
-// import { SDK } from '../sdk';
+import { SDK } from '../sdk';
 
 import Logger from '../../../../../util/Logger';
 import { Network } from '../../../../Views/Settings/NetworksSettings/NetworkSettings/CustomNetworkView/CustomNetwork.types';
@@ -12,23 +12,8 @@ function useRampNetworksDetail() {
     try {
       setError(undefined);
       setIsLoading(true);
-      // TODO: change this mocked response to the real one once the SDK method is ready
-      // const networkDetails = await SDK.getNetworksDetails();
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      const networksDetailsResponse = [
-        {
-          chainId: '25',
-          nickname: 'Cronos Mainnet',
-          rpcUrl: 'https://evm.cronos.org',
-          ticker: 'CRO',
-          rpcPrefs: {
-            blockExplorerUrl: 'https://cronoscan.com',
-            imageUrl:
-              'https://static.metafi.codefi.network/api/v1/tokenIcons/1/0x0c9c7712c83b3c70e7c5e11100d33d9401bdf9dd.png',
-          },
-        },
-      ];
-      setNetworksDetails(networksDetailsResponse);
+      const networkDetails = await SDK.getNetworkDetails();
+      setNetworksDetails(networkDetails);
     } catch (requestError) {
       setError(requestError as Error);
       Logger.error(
