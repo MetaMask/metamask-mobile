dffe28754ccd07db4cb0b3b00530e57fa4465d29
CW <chris.wilcox@consensys.net>
2023-09-15 18:20:53 -0700

    remove withUnProtected

--- e2e/fixtures/fixture-builder.js
+++ e2e/fixtures/fixture-builder.js
@@ -525,15 +525,6 @@ class FixtureBuilder {
   }
 
   /**
-   * Set fixture wallet to be unprotected
-   * @returns {FixtureBuilder} - The FixtureBuilder instance for method chaining.
-   */
-  withUnProtectedWallet() {
-    this.fixture.state.user.seedphraseBackedUp = false;
-    return this;
-  }
-
-  /**
    * Merges provided data into the background state of the PermissionController.
    * @param {object} data - Data to merge into the PermissionController's state.
    * @returns {FixtureBuilder} - The FixtureBuilder instance for method chaining.
