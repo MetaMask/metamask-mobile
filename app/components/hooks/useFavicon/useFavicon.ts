72ffeef8cc2ce92368e06100561e9d65c9342f9d
Nicolas MASSART <nicolas.massart@consensys.net>
2023-09-18 14:00:51 +0200

    add error handling

--- app/components/hooks/useFavicon/useFavicon.ts
+++ app/components/hooks/useFavicon/useFavicon.ts
@@ -5,6 +5,7 @@ import {
   getFaviconFromCache,
   cacheFavicon,
 } from '../../../util/favicon';
+import Logger from '../../../util/Logger';
 
 //Empty value uset to trigger fallback favicon in the UI and prevent use of undefined
 const EMPTY_FAVICON_URI: ImageSourcePropType = {};
@@ -15,18 +16,22 @@ const useFavicon = (origin: string) => {
 
   useEffect(() => {
     (async () => {
-      // If the origin is null, we don't want to fetch a favicon
-      // This can happen when the site is unreachable (DNS error)
-      if (!origin || origin === 'null') {
-        return;
-      }
-      const cachedFaviconUrl = await getFaviconFromCache(origin);
-      if (cachedFaviconUrl) {
-        setFaviconURI({ uri: cachedFaviconUrl });
-      } else {
-        const fetchedFaviconUrl = await getFaviconURLFromHtml(origin);
-        await cacheFavicon(origin, fetchedFaviconUrl);
-        setFaviconURI({ uri: fetchedFaviconUrl });
+      try {
+        // If the origin is null, we don't want to fetch a favicon
+        // This can happen when the site is unreachable (DNS error)
+        if (!origin || origin === 'null') {
+          return;
+        }
+        const cachedFaviconUrl = await getFaviconFromCache(origin);
+        if (cachedFaviconUrl) {
+          setFaviconURI({ uri: cachedFaviconUrl });
+        } else {
+          const fetchedFaviconUrl = await getFaviconURLFromHtml(origin);
+          await cacheFavicon(origin, fetchedFaviconUrl);
+          setFaviconURI({ uri: fetchedFaviconUrl });
+        }
+      } catch (error) {
+        await Logger.log('Error fetching or caching favicon: ', error);
       }
     })();
   }, [origin]);
