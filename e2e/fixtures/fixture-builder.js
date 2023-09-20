a65421acbdaf1de09a97dc6da314f6c6ae47734a
seaona <54408225+seaona@users.noreply.github.com>
2023-09-20 13:46:16 +0200

    test:  Signatures refactor with fixtures (#7225)
    * Signatures refactor
    
    * Change temp to Smoke so we run test in ci
    
    * Change back test from Smoke to Regression

--- e2e/fixtures/fixture-builder.js
+++ e2e/fixtures/fixture-builder.js
@@ -593,6 +593,14 @@ class FixtureBuilder {
     return this;
   }
 
+  withPreferencesController(data) {
+    merge(
+      this.fixture.state.engine.backgroundState.PreferencesController,
+      data,
+    );
+    return this;
+  }
+
   /**
    * Build and return the fixture object.
    * @returns {Object} - The built fixture object.
