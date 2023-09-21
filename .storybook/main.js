ca093a6d0e794bb4149286cd21e772351aa83d43
Brian Nguyen <brianacnguyen@gmail.com>
2023-09-21 11:01:45 -0700

    Added controls to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ['./stories/**/*.stories.?(ts|tsx|js|jsx)'],
-  addons: [],
+  addons: ['@storybook/addon-ondevice-controls'],
   framework: '@storybook/react-native',
   core: {
     builder: 'webpack5',
