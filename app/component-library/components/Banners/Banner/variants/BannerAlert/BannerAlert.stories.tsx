a36d4e42eb877ac41da94df4eb915ab2dfb42c83
Brian August Nguyen <brianacnguyen@gmail.com>
2023-09-21 22:23:06 -0700

    feat: Upgrade Mobile Storybook to version 6.5 (#7263)
    ## **Description**
    - Upgrade Mobile Storybook to version 6.5
    - Currently only uses samples story as a sample
    
    ## **Screenshots/Recordings**
    ![Simulator Screen Shot - iPhone 14 Pro - 2023-09-21 at 11 56
    33](https://github.com/MetaMask/metamask-mobile/assets/14355083/425b8fdd-a647-48c3-bb4e-b1cee78c3938)
    ![Simulator Screen Shot - iPhone 14 Pro - 2023-09-21 at 11 56
    32](https://github.com/MetaMask/metamask-mobile/assets/14355083/da244304-b9ed-49ac-98d9-ef769474e54a)
    ![Simulator Screen Shot - iPhone 14 Pro - 2023-09-21 at 11 56
    30](https://github.com/MetaMask/metamask-mobile/assets/14355083/d176b0c6-65e0-4a88-b9b0-9d61adbf3307)
    
    
    ## **Related issues**
    
    _Fixes #???_
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've clearly explained:
      - [ ] What problem this PR is solving.
      - [ ] How this problem was solved.
      - [ ] How reviewers can test my changes.
    - [x] I’ve indicated what issue this PR is linked to: Fixes #???
    - [x] I’ve included tests if applicable.
    - [x] I’ve documented any added code.
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    - [x] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [ ] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/component-library/components/Banners/Banner/variants/BannerAlert/BannerAlert.stories.tsx
+++ app/component-library/components/Banners/Banner/variants/BannerAlert/BannerAlert.stories.tsx
@@ -1,56 +1,23 @@
 /* eslint-disable no-console */
 
 // Third party dependencies.
-import React from 'react';
-import { select, text } from '@storybook/addon-knobs';
 
 // External dependencies.
-import { storybookPropsGroupID } from '../../../../../constants/storybook.constants';
-import { ButtonVariants } from '../../../../Buttons/Button';
 
 // Internal dependencies.
-import BannerAlert from './BannerAlert';
-import { BannerAlertProps, BannerAlertSeverity } from './BannerAlert.types';
+import { default as BannerAlertComponent } from './BannerAlert';
 import {
-  DEFAULT_BANNERALERT_SEVERITY,
-  SAMPLE_BANNERALERT_TITLE,
-  SAMPLE_BANNERALERT_DESCRIPTION,
-  SAMPLE_BANNERALERT_ACTIONBUTTONLABEL,
+  SAMPLE_BANNERALERT_PROPS,
+  STORYBOOK_BANNERALERT_ARGTYPES,
 } from './BannerAlert.constants';
 
-export const getBannerAlertStoryProps = (): BannerAlertProps => {
-  const severitySelector = select(
-    'severity',
-    BannerAlertSeverity,
-    DEFAULT_BANNERALERT_SEVERITY,
-    storybookPropsGroupID,
-  );
-
-  const title = text('title', SAMPLE_BANNERALERT_TITLE, storybookPropsGroupID);
-  const description = text(
-    'description',
-    SAMPLE_BANNERALERT_DESCRIPTION,
-    storybookPropsGroupID,
-  );
-  const actionButtonLabel = text(
-    'actionButtonLabel',
-    SAMPLE_BANNERALERT_ACTIONBUTTONLABEL,
-    storybookPropsGroupID,
-  );
-
-  return {
-    severity: severitySelector,
-    title,
-    description,
-    actionButtonProps: {
-      variant: ButtonVariants.Link,
-      label: actionButtonLabel,
-      onPress: () => console.log('actionButton clicked!'),
-    },
-    onClose: () => console.log('closeButton clicked!'),
-  };
+const BannerAlertMeta = {
+  title: 'Component Library / Banners',
+  component: BannerAlertComponent,
+  argTypes: STORYBOOK_BANNERALERT_ARGTYPES,
 };
+export default BannerAlertMeta;
 
-const BannerAlertStory = () => <BannerAlert {...getBannerAlertStoryProps()} />;
-
-export default BannerAlertStory;
+export const BannerAlert = {
+  args: SAMPLE_BANNERALERT_PROPS,
+};
