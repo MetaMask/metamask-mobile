3635e729262bf8db08983e79f80bba7d67f5df99
Mark Stacey <markjstacey@gmail.com>
2023-09-26 17:45:41 -0230

    refactor: Update controller packages to core v49 (#7125)
    <!--
    Thanks for your contribution!
    
    Please ensure that any applicable requirements below are satisfied
    before submitting this pull request. This will help ensure a quick and
    efficient review cycle.
    -->
    
    **Development & PR Process**
    1. Follow MetaMask [Mobile Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/coding_guidelines/CODING_GUIDELINES.md)
    2. Add `release-xx` label to identify the PR slated for a upcoming
    release (will be used in release discussion)
    3. Add `needs-dev-review` label when work is completed
    4. Add the appropiate QA label when dev review is completed
        - `needs-qa`: PR requires manual QA.
    - `No QA/E2E only`: PR does not require any manual QA effort. Prior to
    merging, ensure that you have successful end-to-end test runs in
    Bitrise.
    - `Spot check on release build`: PR does not require feature QA but
    needs non-automated verification. In the description section, provide
    test scenarios. Add screenshots, and or recordings of what was tested.
    5. Add `QA Passed` label when QA has signed off (Only required if the PR
    was labeled with `needs-qa`)
    6. Add your team's label, i.e. label starting with `team-` (or
    `external-contributor` label if your not a MetaMask employee)
    
    **Description**
    
    The controller packages have been updated to match those present in the
    core monorepo release v49. See this changelog for details:
    https://github.com/MetaMask/core/releases/tag/v49.0.0
    
    The main breaking change is that the `NetworkController` state property
    `network` has been split into `networkId` and `networkStatus`.
    
    **Screenshots/Recordings**
    
    https://recordit.co/GXEdxk9uNp
    
    Wasn't sure exactly what to manually test, but I confirmed that balance
    updates and incoming transactions seem to be working well, as well as
    network switching.
    
    **Issue**
    
    Resolves https://github.com/MetaMask/mobile-planning/issues/1226
    
    **Checklist**
    
    * [x] There is a related GitHub issue
    * [x] Tests are included if applicable
    * [x] Any added code is fully documented

--- app/selectors/networkController.ts
+++ app/selectors/networkController.ts
@@ -1,6 +1,10 @@
 import { createSelector } from 'reselect';
 import { RootState } from '../reducers';
-import { ProviderConfig, NetworkState } from '@metamask/network-controller';
+import {
+  ProviderConfig,
+  NetworkState,
+  NetworkStatus,
+} from '@metamask/network-controller';
 
 const selectNetworkControllerState = (state: RootState) =>
   state?.engine?.backgroundState?.NetworkController;
@@ -32,13 +36,34 @@ export const selectRpcTarget = createSelector(
   selectProviderConfig,
   (providerConfig: ProviderConfig) => providerConfig.rpcTarget,
 );
-export const selectNetwork = createSelector(
+export const selectNetworkId = createSelector(
   selectNetworkControllerState,
-  (networkControllerState: NetworkState) => networkControllerState?.network,
+  (networkControllerState: NetworkState) => networkControllerState?.networkId,
+);
+
+export const selectNetworkStatus = createSelector(
+  selectNetworkControllerState,
+  (networkControllerState: NetworkState) =>
+    networkControllerState?.networkStatus,
 );
 
 export const selectNetworkConfigurations = createSelector(
   selectNetworkControllerState,
   (networkControllerState: NetworkState) =>
     networkControllerState.networkConfigurations,
+);
+
+/**
+ * Derive a value matching the now-removed property `network` that had been
+ * included in the NetworkController state. It's set to "loading" if the
+ * network is loading, but otherwise is set to the network ID.
+ *
+ * @deprecated Use networkStatus and networkId instead
+ */
+export const selectLegacyNetwork = createSelector(
+  selectNetworkControllerState,
+  (networkControllerState: NetworkState) => {
+    const { networkId, networkStatus } = networkControllerState;
+    return networkStatus !== NetworkStatus.Available ? 'loading' : networkId;
+  },
 );
