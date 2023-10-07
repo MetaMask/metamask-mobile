4667a821c5e01a8d6721c678525be1b8c0dd5b55
Brian Nguyen <brianacnguyen@gmail.com>
2023-10-07 16:52:02 -0700

    Converted Overlay stories

--- app/component-library/components/Overlay/Overlay.stories.tsx
+++ app/component-library/components/Overlay/Overlay.stories.tsx
@@ -1,33 +1,24 @@
-/* eslint-disable no-console, react-native/no-inline-styles */
-
-// Third party dependencies.
-import React from 'react';
-import { color } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/react-native';
-
+/* eslint-disable no-console */
 // External dependencies.
 import { mockTheme } from '../../../util/theme';
-import { storybookPropsGroupID } from '../../constants/storybook.constants';
 
 // Internal dependencies.
-import Overlay from './Overlay';
-import { OverlayProps } from './Overlay.types';
+import { default as OverlayComponent } from './Overlay';
 
-export const getOverlayStoryProps = (): OverlayProps => {
-  const colorSelector = color(
-    'color',
-    mockTheme.colors.overlay.default,
-    storybookPropsGroupID,
-  );
-
-  return {
-    color: colorSelector,
-    onPress: () => console.log("I'm clicked!"),
-  };
+const OverlayMeta = {
+  title: 'Component Library / Overlay',
+  component: OverlayComponent,
+  argTypes: {
+    color: {
+      control: { type: 'color' },
+      defaultValue: mockTheme.colors.overlay.default,
+    },
+  },
 };
+export default OverlayMeta;
 
-const OverlayStory = () => <Overlay {...getOverlayStoryProps()} />;
-
-storiesOf('Component Library / Overlay', module).add('Overlay', OverlayStory);
-
-export default OverlayStory;
+export const Overlay = {
+  args: {
+    onPress: () => console.log("I'm clicked!"),
+  },
+};
