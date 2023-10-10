077aa0753edd56f9d1517699a6aaa446e58c3f0f
tommasini <tommasini15@gmail.com>
2023-10-10 21:57:45 +0100

    missing re ad params on navigate

--- app/components/Views/BrowserTab/index.js
+++ app/components/Views/BrowserTab/index.js
@@ -1451,6 +1451,9 @@ export const BrowserTab = (props) => {
           onPress: () =>
             props.navigation.navigate(Routes.MODAL.ROOT_MODAL_FLOW, {
               screen: Routes.SHEET.SHOW_IPFS,
+              params: {
+                setIpfsBannerVisible: () => setIpfsBannerVisible(false),
+              },
             }),
           textVariant: TextVariant.BodyMD,
           label: 'Turn on IPFS gateway',
