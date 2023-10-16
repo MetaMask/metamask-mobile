39248ed0f4686a582db05d5139230fc36a385ced
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2023-10-16 13:07:25 -0600

    feat(ramp): add getStarted flag for sell (#7401)

--- app/reducers/fiatOrders/index.ts
+++ app/reducers/fiatOrders/index.ts
@@ -48,6 +48,10 @@ export const setFiatOrdersGetStartedAGG = (getStartedFlag: boolean) => ({
   type: ACTIONS.FIAT_SET_GETSTARTED_AGG,
   payload: getStartedFlag,
 });
+export const setFiatOrdersGetStartedSell = (getStartedFlag: boolean) => ({
+  type: ACTIONS.FIAT_SET_GETSTARTED_SELL,
+  payload: getStartedFlag,
+});
 export const addFiatCustomIdData = (customIdData: CustomIdData) => ({
   type: ACTIONS.FIAT_ADD_CUSTOM_ID_DATA,
   payload: customIdData,
@@ -145,6 +149,10 @@ export const fiatOrdersGetStartedAgg: (
   state: RootState,
 ) => FiatOrdersState['getStartedAgg'] = (state: RootState) =>
   state.fiatOrders.getStartedAgg;
+export const fiatOrdersGetStartedSell: (
+  state: RootState,
+) => FiatOrdersState['getStartedSell'] = (state: RootState) =>
+  state.fiatOrders.getStartedSell;
 
 export const getOrders = createSelector(
   ordersSelector,
@@ -236,6 +244,7 @@ export const initialState: FiatOrdersState = {
   selectedRegionAgg: null,
   selectedPaymentMethodAgg: null,
   getStartedAgg: false,
+  getStartedSell: false,
   authenticationUrls: [],
   activationKeys: [],
 };
@@ -308,6 +317,12 @@ const fiatOrderReducer: (
       return {
         ...state,
         getStartedAgg: action.payload,
+      };
+    }
+    case ACTIONS.FIAT_SET_GETSTARTED_SELL: {
+      return {
+        ...state,
+        getStartedSell: action.payload,
       };
     }
     case ACTIONS.FIAT_SET_REGION_AGG: {
