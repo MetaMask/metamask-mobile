7cebf0d903cd2a7b6b6c910f0d24f09ffb6d6932
Jiexi Luan <jiexiluan@gmail.com>
2023-10-17 10:22:39 -0700

    Move inpage bridge scripts to /scripts/inpage-bridge

--- scripts/build-inpage-bridge.sh
+++ scripts/build-inpage-bridge.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 rm app/core/InpageBridgeWeb3.js
-mkdir -p js/dist && rm -rf js/dist/*
-cd js/inpage
-../../node_modules/.bin/webpack --config webpack.config.js
+mkdir -p scripts/inpage-bridge/dist && rm -rf scripts/inpage-bridge/dist/*
+cd scripts/inpage-bridge/inpage
+../../../node_modules/.bin/webpack --config webpack.config.js
 cd ..
 node content-script/build.js
 cat dist/inpage-bundle.js content-script/index.js > dist/index-raw.js
-../node_modules/.bin/webpack --config webpack.config.js
-cd ..
-cp js/dist/index.js app/core/InpageBridgeWeb3.js
+../../node_modules/.bin/webpack --config webpack.config.js
+cd ../..
+cp scripts/inpage-bridge/dist/index.js app/core/InpageBridgeWeb3.js
