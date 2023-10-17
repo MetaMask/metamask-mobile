b2922b13498de98bf088a5d1230958a296078a39
dawnseeker8 <dawnseeker8@gmail.com>
2023-10-17 19:57:58 +0800

    feat: fix the looping problem of permission

--- app/components/UI/LedgerModals/LedgerConfirmationModal.tsx
+++ app/components/UI/LedgerModals/LedgerConfirmationModal.tsx
@@ -47,6 +47,7 @@ const LedgerConfirmationModal = ({
 }: LedgerConfirmationModalProps) => {
   const { colors } = useAppThemeFromContext() || mockTheme;
   const styles = useMemo(() => createStyles(colors), [colors]);
+  const [permissionErrorShown, setPermissionErrorShown] = useState(false);
   const {
     isSendingLedgerCommands,
     isAppLaunchConfirmationNeeded,
@@ -85,11 +86,13 @@ const LedgerConfirmationModal = ({
   };
 
   const onRetry = async () => {
+    setPermissionErrorShown(false);
     if (!hasBluetoothPermissions) {
       await checkPermissions();
     }
 
     if (hasBluetoothPermissions && bluetoothOn) {
+      setErrorDetails(undefined);
       connectLedger();
     }
   };
@@ -100,102 +103,112 @@ const LedgerConfirmationModal = ({
   }, [hasBluetoothPermissions, bluetoothOn]);
 
   useEffect(() => {
-    if (ledgerError) {
-      switch (ledgerError) {
-        case LedgerCommunicationErrors.FailedToOpenApp:
-          setErrorDetails({
-            title: strings('ledger.failed_to_open_eth_app'),
-            subtitle: strings('ledger.ethereum_app_open_error'),
-          });
-          break;
-        case LedgerCommunicationErrors.FailedToCloseApp:
-          setErrorDetails({
-            title: strings('ledger.running_app_close'),
-            subtitle: strings('ledger.running_app_close_error'),
-          });
-          break;
-        case LedgerCommunicationErrors.AppIsNotInstalled:
-          setErrorDetails({
-            title: strings('ledger.ethereum_app_not_installed'),
-            subtitle: strings('ledger.ethereum_app_not_installed_error'),
-          });
-          break;
-        case LedgerCommunicationErrors.LedgerIsLocked:
-          setErrorDetails({
-            title: strings('ledger.ledger_is_locked'),
-            subtitle: strings('ledger.unlock_ledger_message'),
-          });
-          break;
-        case LedgerCommunicationErrors.UserRefusedConfirmation:
-          onReject();
-          break;
-        case LedgerCommunicationErrors.LedgerHasPendingConfirmation:
-          setErrorDetails({
-            title: strings('ledger.ledger_pending_confirmation'),
-            subtitle: strings('ledger.ledger_pending_confirmation_error'),
-          });
-          break;
-        case LedgerCommunicationErrors.NotSupported:
-          setErrorDetails({
-            title: strings('ledger.not_supported'),
-            subtitle: strings('ledger.not_supported_error'),
-          });
-          break;
-        case LedgerCommunicationErrors.UnknownError:
-          setErrorDetails({
-            title: strings('ledger.unknown_error'),
-            subtitle: strings('ledger.unknown_error_message'),
-          });
-          break;
-        case LedgerCommunicationErrors.NonceTooLow:
-          setErrorDetails({
-            title: strings('ledger.nonce_too_low'),
-            subtitle: strings('ledger.nonce_too_low_error'),
-          });
-          break;
-        case LedgerCommunicationErrors.LedgerDisconnected:
-        default:
-          setErrorDetails({
-            title: strings('ledger.ledger_disconnected'),
-            subtitle: strings('ledger.ledger_disconnected_error'),
-          });
-          break;
+    if (!permissionErrorShown) {
+      if (ledgerError) {
+        switch (ledgerError) {
+          case LedgerCommunicationErrors.FailedToOpenApp:
+            setErrorDetails({
+              title: strings('ledger.failed_to_open_eth_app'),
+              subtitle: strings('ledger.ethereum_app_open_error'),
+            });
+            break;
+          case LedgerCommunicationErrors.FailedToCloseApp:
+            setErrorDetails({
+              title: strings('ledger.running_app_close'),
+              subtitle: strings('ledger.running_app_close_error'),
+            });
+            break;
+          case LedgerCommunicationErrors.AppIsNotInstalled:
+            setErrorDetails({
+              title: strings('ledger.ethereum_app_not_installed'),
+              subtitle: strings('ledger.ethereum_app_not_installed_error'),
+            });
+            break;
+          case LedgerCommunicationErrors.LedgerIsLocked:
+            setErrorDetails({
+              title: strings('ledger.ledger_is_locked'),
+              subtitle: strings('ledger.unlock_ledger_message'),
+            });
+            break;
+          case LedgerCommunicationErrors.UserRefusedConfirmation:
+            onReject();
+            break;
+          case LedgerCommunicationErrors.LedgerHasPendingConfirmation:
+            setErrorDetails({
+              title: strings('ledger.ledger_pending_confirmation'),
+              subtitle: strings('ledger.ledger_pending_confirmation_error'),
+            });
+            break;
+          case LedgerCommunicationErrors.NotSupported:
+            setErrorDetails({
+              title: strings('ledger.not_supported'),
+              subtitle: strings('ledger.not_supported_error'),
+            });
+            break;
+          case LedgerCommunicationErrors.UnknownError:
+            setErrorDetails({
+              title: strings('ledger.unknown_error'),
+              subtitle: strings('ledger.unknown_error_message'),
+            });
+            break;
+          case LedgerCommunicationErrors.NonceTooLow:
+            setErrorDetails({
+              title: strings('ledger.nonce_too_low'),
+              subtitle: strings('ledger.nonce_too_low_error'),
+            });
+            break;
+          case LedgerCommunicationErrors.LedgerDisconnected:
+          default:
+            setErrorDetails({
+              title: strings('ledger.ledger_disconnected'),
+              subtitle: strings('ledger.ledger_disconnected_error'),
+            });
+            break;
+        }
       }
-    }
 
-    if (bluetoothPermissionError) {
-      switch (bluetoothPermissionError) {
-        case BluetoothPermissionErrors.LocationAccessBlocked:
-          setErrorDetails({
-            title: strings('ledger.location_access_blocked'),
-            subtitle: strings('ledger.location_access_blocked_error'),
-          });
-          break;
-        case BluetoothPermissionErrors.BluetoothAccessBlocked:
-          setErrorDetails({
-            title: strings('ledger.bluetooth_access_blocked'),
-            subtitle: strings('ledger.bluetooth_access_blocked_message'),
-          });
-          break;
+      if (bluetoothPermissionError) {
+        switch (bluetoothPermissionError) {
+          case BluetoothPermissionErrors.LocationAccessBlocked:
+            setErrorDetails({
+              title: strings('ledger.location_access_blocked'),
+              subtitle: strings('ledger.location_access_blocked_error'),
+            });
+            break;
+          case BluetoothPermissionErrors.BluetoothAccessBlocked:
+            setErrorDetails({
+              title: strings('ledger.bluetooth_access_blocked'),
+              subtitle: strings('ledger.bluetooth_access_blocked_message'),
+            });
+            break;
+        }
+        setPermissionErrorShown(true);
       }
-    }
 
-    if (bluetoothConnectionError) {
-      setErrorDetails({
-        title: strings('ledger.bluetooth_off'),
-        subtitle: strings('ledger.bluetooth_off_message'),
-      });
+      if (bluetoothConnectionError) {
+        setErrorDetails({
+          title: strings('ledger.bluetooth_off'),
+          subtitle: strings('ledger.bluetooth_off_message'),
+        });
+      }
     }
 
     if (
+      !permissionErrorShown &&
       !ledgerError &&
       !bluetoothPermissionError &&
       !bluetoothConnectionError
     ) {
       setErrorDetails(undefined);
     }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ledgerError, bluetoothConnectionError, bluetoothPermissionError]);
+  }, [
+    ledgerError,
+    bluetoothConnectionError,
+    bluetoothPermissionError,
+    permissionErrorShown,
+  ]);
 
   if (errorDetails) {
     return (
@@ -207,7 +220,9 @@ const LedgerConfirmationModal = ({
             title={errorDetails?.title}
             subTitle={errorDetails?.subtitle}
             showViewSettings={
-              !!bluetoothConnectionError || !!bluetoothPermissionError
+              permissionErrorShown ||
+              !!bluetoothConnectionError ||
+              !!bluetoothPermissionError
             }
             isRetryHide={
               ledgerError === LedgerCommunicationErrors.UnknownError ||
