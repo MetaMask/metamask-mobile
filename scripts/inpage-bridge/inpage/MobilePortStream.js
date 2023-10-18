bf25c3e2c3fc942e918544fa93fcad1955435a06
jiexi <jiexiluan@gmail.com>
2023-10-18 14:20:58 -0700

    build: Pull `@metamask/mobile-provider` back into `metamask-mobile` (#7494)
    ## **Description**
    
    Currently the inpage injected scripts used in the in-app browser (for
    making the wallet provider object available to the dapp) are currently
    built in a separate `@metamask/mobile-provider` package. This PR brings
    the build process into the mobile repo itself. This is needed in order
    to support passing build time variables to the provider initialization
    (for EIP6963 support).
    
    ## **Manual testing steps**
    
    1. Pull in branch
    2. `yarn setup`
    3. `yarn watch`
    4. `yarn start:ios`
    5. Go to in-app browser
    6. Visit test dapp
    7. Verify that buttons are still functional
    8. Optionally: Open safari debug console on the test dapp webview
    9. check that `window.ethereum` is defined and `window.ethereum.chainId`
    logs a deprecation warning
    
    ## **Screenshots/Recordings**
    Note the deprecation warnings which indicates that provider v13.0.0 is
    now being used
    
    ![image](https://github.com/MetaMask/metamask-mobile/assets/918701/143a5f56-29d4-4b7c-8192-d4595689ebba)
    
    
    ## **Related issues**
    
    See: https://github.com/MetaMask/MetaMask-planning/issues/869
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've clearly explained:
      - [x] What problem this PR is solving.
      - [x] How this problem was solved.
      - [x] How reviewers can test my changes.
    - [x] I’ve indicated what issue this PR is linked to: Fixes #???
    - [ ] I’ve included tests if applicable.
    - [x] I’ve documented any added code.
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    - [x] I’ve properly set the pull request status:
      - [x] In case it's not yet "ready for review", I've set it to "draft".
    - [x] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Cal Leung <cleun007@gmail.com>

---
+++ scripts/inpage-bridge/inpage/MobilePortStream.js
@@ -0,0 +1,109 @@
+const { inherits } = require('util');
+const { Duplex } = require('readable-stream');
+
+const noop = () => undefined;
+
+module.exports = MobilePortStream;
+
+inherits(MobilePortStream, Duplex);
+
+/**
+ * Creates a stream that's both readable and writable.
+ * The stream supports arbitrary objects.
+ *
+ * @class
+ * @param {Object} port Remote Port object
+ */
+function MobilePortStream(port) {
+  Duplex.call(this, {
+    objectMode: true,
+  });
+  this._name = port.name;
+  this._targetWindow = window;
+  this._port = port;
+  this._origin = location.origin;
+  window.addEventListener('message', this._onMessage.bind(this), false);
+}
+
+/**
+ * Callback triggered when a message is received from
+ * the remote Port associated with this Stream.
+ *
+ * @private
+ * @param {Object} msg - Payload from the onMessage listener of Port
+ */
+MobilePortStream.prototype._onMessage = function (event) {
+  const msg = event.data;
+
+  // validate message
+  if (this._origin !== '*' && event.origin !== this._origin) {
+    return;
+  }
+  if (!msg || typeof msg !== 'object') {
+    return;
+  }
+  if (!msg.data || typeof msg.data !== 'object') {
+    return;
+  }
+  if (msg.target && msg.target !== this._name) {
+    return;
+  }
+  // Filter outgoing messages
+  if (msg.data.data && msg.data.data.toNative) {
+    return;
+  }
+
+  if (Buffer.isBuffer(msg)) {
+    delete msg._isBuffer;
+    const data = Buffer.from(msg);
+    this.push(data);
+  } else {
+    this.push(msg);
+  }
+};
+
+/**
+ * Callback triggered when the remote Port
+ * associated with this Stream disconnects.
+ *
+ * @private
+ */
+MobilePortStream.prototype._onDisconnect = function () {
+  this.destroy();
+};
+
+/**
+ * Explicitly sets read operations to a no-op
+ */
+MobilePortStream.prototype._read = noop;
+
+/**
+ * Called internally when data should be written to
+ * this writable stream.
+ *
+ * @private
+ * @param {*} msg Arbitrary object to write
+ * @param {string} encoding Encoding to use when writing payload
+ * @param {Function} cb Called when writing is complete or an error occurs
+ */
+MobilePortStream.prototype._write = function (msg, _encoding, cb) {
+  try {
+    if (Buffer.isBuffer(msg)) {
+      const data = msg.toJSON();
+      data._isBuffer = true;
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify({ ...data, origin: window.location.href }),
+      );
+    } else {
+      if (msg.data) {
+        msg.data.toNative = true;
+      }
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify({ ...msg, origin: window.location.href }),
+      );
+    }
+  } catch (err) {
+    return cb(new Error('MobilePortStream - disconnected'));
+  }
+  return cb();
+};
