e7bb1f4944dfca33d1a608da1ad227ebe772faeb
Jyoti Puri <jyotipuri@gmail.com>
2023-10-18 18:08:51 +0530

    Update scripts/build_ppom.sh
    Co-authored-by: LeoTM <1881059+leotm@users.noreply.github.com>

--- scripts/build_ppom.sh
+++ scripts/build_ppom.sh
@@ -1,5 +1,5 @@
 # The script invokes tool to generate ppom.html.js
 # This is integrated into "yarn setup"
 cd ./ppom
-yarn && yarn lint && yarn build
+yarn clean && yarn && yarn lint && yarn build
 cd ..
