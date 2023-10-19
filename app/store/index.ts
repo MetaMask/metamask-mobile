789c9f2dbf8e0d59228c379bca7455c16012ced9
Aslau Mario-Daniel <marioaslau@gmail.com>
2023-10-19 17:14:20 +0100

    feat: revert flipper

--- app/store/index.ts
+++ app/store/index.ts
@@ -29,15 +29,10 @@ const createStoreAndPersistor = async () => {
 
   // Create the store and apply middlewares. In E2E tests, an optional initialState
   // from fixtures can be provided to preload the store; otherwise, it remains undefined.
-  const middlewares = [sagaMiddleware];
-  if (__DEV__) {
-    const createDebugger = require('redux-flipper').default;
-    middlewares.push(createDebugger());
-  }
 
   store = configureStore({
     reducer: pReducer,
-    middleware: middlewares,
+    middleware: [sagaMiddleware],
     preloadedState: initialState,
   });
 
