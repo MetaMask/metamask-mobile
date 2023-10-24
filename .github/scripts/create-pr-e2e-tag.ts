c13ccb00f5a4e2ffb6cb4e9370b7a1270fc4f5d2
Cal Leung <cleun007@gmail.com>
2023-10-23 18:56:24 -0700

    Fetch using public api

--- .github/scripts/create-pr-e2e-tag.ts
+++ .github/scripts/create-pr-e2e-tag.ts
@@ -10,14 +10,12 @@ main().catch((error: Error): void => {
 });
 
 async function main(): Promise<void> {
-  // "GITHUB_TOKEN" is an automatically generated, repository-specific access token provided by GitHub Actions.
   const githubToken = process.env.GITHUB_TOKEN;
   if (!githubToken) {
     core.setFailed('GITHUB_TOKEN not found.');
     process.exit(1);
   }
 
-  // Initialise octokit, required to call Github GraphQL API
   const octokit: InstanceType<typeof GitHub> = getOctokit(githubToken);
 
   const { pull_request, label, repository } = context.payload;
@@ -30,17 +28,30 @@ async function main(): Promise<void> {
   if (label.name === E2E_TRIGGERED_LABEL) {
     const tagName = `pr-e2e-${pull_request.number}`;
 
-    const { data: ref } = await octokit.rest.git.getRef({
-      owner: repository.owner.login,
-      repo: repository.name,
-      ref: `heads/${pull_request.head.ref}`,
-    });
+    const response = await fetch(
+      'https://api.github.com/repos/MetaMask/metamask-mobile/pulls/7339',
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/vnd.github.v3+json',
+        },
+      },
+    );
+    const pullRequestInfo = await response.json();
+    const sha = pullRequestInfo.head.sha;
+    console.log('PR INFO', pullRequestInfo.head.sha);
+
+    // const { data: ref } = await octokit.rest.git.getRef({
+    //   owner: repository.owner.login,
+    //   repo: repository.name,
+    //   ref: `heads/${pull_request.head.ref}`,
+    // });
 
     await octokit.rest.git.createRef({
       owner: repository.owner.login,
       repo: repository.name,
       ref: `refs/tags/${tagName}`,
-      sha: ref.object.sha,
+      sha,
     });
 
     console.log(`Created tag ${tagName}.`);
