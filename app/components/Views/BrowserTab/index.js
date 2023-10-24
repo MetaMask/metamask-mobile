7d3681022e23428aa9a33b6b1e91cf1b1cc97cc4
tommasini <tommasini15@gmail.com>
2023-10-24 18:51:22 +0100

    miss change

--- app/components/Views/BrowserTab/index.js
+++ app/components/Views/BrowserTab/index.js
@@ -99,7 +99,7 @@ import { ButtonVariants } from '../../../component-library/components/Buttons/Bu
 import CLText from '../../../component-library/components/Texts/Text/Text';
 import { TextVariant } from '../../../component-library/components/Texts/Text';
 import { regex } from '../../../../app/util/regex';
-import { selectChainId } from 'app/selectors/networkController';
+import { selectChainId } from '../../../selectors/networkController';
 
 const { HOMEPAGE_URL, NOTIFICATION_NAMES } = AppConstants;
 const HOMEPAGE_HOST = new URL(HOMEPAGE_URL)?.hostname;
