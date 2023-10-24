1d895ea180f8a21fe345998dd834323e50f3715d
Cal Leung <cleun007@gmail.com>
2023-10-24 13:04:42 -0700

    Trigger Bitrise build in script

--- .github/scripts/create-pr-e2e-tag.ts
+++ .github/scripts/create-pr-e2e-tag.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core';
-import { context, getOctokit } from '@actions/github';
-import { GitHub } from '@actions/github/lib/utils';
+import { context } from '@actions/github';
+// import { GitHub } from '@actions/github/lib/utils';
 import axios from 'axios';
 
 const E2E_TRIGGERED_LABEL = 'Run E2E';
@@ -17,7 +17,7 @@ async function main(): Promise<void> {
     process.exit(1);
   }
 
-  const octokit: InstanceType<typeof GitHub> = getOctokit(githubToken);
+  //   const octokit: InstanceType<typeof GitHub> = getOctokit(githubToken);
 
   const { pull_request, label, repository } = context.payload;
 
@@ -27,34 +27,57 @@ async function main(): Promise<void> {
   }
 
   if (label.name === E2E_TRIGGERED_LABEL) {
-    const tagName = `pr-e2e-${pull_request.number}`;
+    const data = {
+      hook_info: {
+        type: 'bitrise',
+        build_trigger_token: process.env.BITRISE_BUILD_TRIGGER_TOKEN,
+      },
+      build_params: {
+        branch: process.env.GITHUB_HEAD_REF,
+        pipeline_id: 'pr_smoke_e2e_pipeline',
+      },
+      triggered_by: 'create-pr-e2e-tag',
+    };
 
-    const response = await axios.get(
-      'https://api.github.com/repos/MetaMask/metamask-mobile/pulls/7339',
+    const response = await axios.post(
+      `https://app.bitrise.io/app/${process.env.BITRISE_APP_ID}/build/start.json`,
+      data,
       {
         headers: {
-          Accept: 'application/vnd.github.v3+json',
+          'Content-Type': 'application/json',
         },
       },
     );
-    const pullRequestInfo = response.data;
-    const sha = pullRequestInfo.head.sha;
-    console.log('PR INFO', pullRequestInfo.head.sha);
+    console.log('kick off build', response.data);
 
-    // const { data: ref } = await octokit.rest.git.getRef({
+    // const tagName = `pr-e2e-${pull_request.number}`;
+
+    // const response = await axios.get(
+    //   'https://api.github.com/repos/MetaMask/metamask-mobile/pulls/7339',
+    //   {
+    //     headers: {
+    //       Accept: 'application/vnd.github.v3+json',
+    //     },
+    //   },
+    // );
+    // const pullRequestInfo = response.data;
+    // const sha = pullRequestInfo.head.sha;
+    // console.log('PR INFO', pullRequestInfo.head.sha);
+
+    // // const { data: ref } = await octokit.rest.git.getRef({
+    // //   owner: repository.owner.login,
+    // //   repo: repository.name,
+    // //   ref: `heads/${pull_request.head.ref}`,
+    // // });
+
+    // await octokit.rest.git.createRef({
     //   owner: repository.owner.login,
     //   repo: repository.name,
-    //   ref: `heads/${pull_request.head.ref}`,
+    //   ref: `refs/tags/${tagName}`,
+    //   sha,
     // });
 
-    await octokit.rest.git.createRef({
-      owner: repository.owner.login,
-      repo: repository.name,
-      ref: `refs/tags/${tagName}`,
-      sha,
-    });
-
-    console.log(`Created tag ${tagName}.`);
+    // console.log(`Created tag ${tagName}.`);
   } else {
     console.log(
       `Skipping E2E build on PR #${pull_request.number} since ${E2E_TRIGGERED_LABEL} label does not exist.`,
