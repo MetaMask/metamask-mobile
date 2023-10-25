7d3f7a62a73fbe19a9b6f367bf061ef1d7bbf566
Brian August Nguyen <brianacnguyen@gmail.com>
2023-10-25 12:50:19 -0700

    refactor: Convert Form stories (#7349)
    ## **Description**
    - Convert Label, HelpText, TextField, and TextFieldSearch stories
    - Add Input story
    
    
    ## **Screenshots/Recordings**
    ![Simulator Screen Shot - iPhone 14 Pro - 2023-10-02 at 08 46
    57](https://github.com/MetaMask/metamask-mobile/assets/14355083/2334e49c-61d5-40c8-8c4e-91e2892a2b8b)
    ![Simulator Screen Shot - iPhone 14 Pro - 2023-10-02 at 08 46
    58](https://github.com/MetaMask/metamask-mobile/assets/14355083/efe9cbf5-cca5-48b6-a799-1b87ec072cb8)
    ![Simulator Screen Shot - iPhone 14 Pro - 2023-10-02 at 08 47
    05](https://github.com/MetaMask/metamask-mobile/assets/14355083/5c9c4a04-1748-438a-b176-503f55643988)
    ![Simulator Screen Shot - iPhone 14 Pro - 2023-10-02 at 08 47
    13](https://github.com/MetaMask/metamask-mobile/assets/14355083/3f6f3a04-1698-43ed-a261-4a0561e00fb8)
    ![Simulator Screen Shot - iPhone 14 Pro - 2023-10-02 at 08 51
    22](https://github.com/MetaMask/metamask-mobile/assets/14355083/25f26abe-d78a-4e5a-93bb-a9d75dac5404)
    ![Simulator Screen Shot - iPhone 14 Pro - 2023-10-02 at 08 51
    25](https://github.com/MetaMask/metamask-mobile/assets/14355083/1f3efed7-d5cb-4059-aa4f-6a214a9b9b23)
    ![Simulator Screen Shot - iPhone 14 Pro - 2023-10-02 at 08 51
    41](https://github.com/MetaMask/metamask-mobile/assets/14355083/25fd4a2f-8fa5-403f-bbd2-2df39c6da547)
    ![Simulator Screen Shot - iPhone 14 Pro - 2023-10-02 at 08 51
    46](https://github.com/MetaMask/metamask-mobile/assets/14355083/2287b272-84a6-4837-aa79-ee9314ba9b5b)
    ![Simulator Screen Shot - iPhone 14 Pro - 2023-10-02 at 08 51
    57](https://github.com/MetaMask/metamask-mobile/assets/14355083/7a179261-f987-47cb-8834-ddcca56572d7)
    ![Simulator Screen Shot - iPhone 14 Pro - 2023-10-02 at 08 51
    58](https://github.com/MetaMask/metamask-mobile/assets/14355083/020e1807-024a-4126-af81-4a1aaa404645)
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've clearly explained:
      - [ ] What problem this PR is solving.
      - [ ] How this problem was solved.
      - [ ] How reviewers can test my changes.
    - [ ] I’ve indicated what issue this PR is linked to: Fixes #???
    - [ ] I’ve included tests if applicable.
    - [ ] I’ve documented any added code.
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    - [ ] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [ ] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: [
     '../app/component-library/components/Cards/Card/**/*.stories.?(ts|tsx|js|jsx)',
     '../app/component-library/components/Banners/Banner/variants/BannerAlert/**/*.stories.?(ts|tsx|js|jsx)',
+    '../app/component-library/components/Form/**/*.stories.?(ts|tsx|js|jsx)',
     '../app/component-library/components/List/**/*.stories.?(ts|tsx|js|jsx)',
     '../app/component-library/components/Texts/**/*.stories.?(ts|tsx|js|jsx)',
     '../app/component-library/components/Overlay/**/*.stories.?(ts|tsx|js|jsx)',
