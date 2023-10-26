b513f2dd880f81e82e3e68a564503442bb0a87ac
tommasini <tommasini15@gmail.com>
2023-10-26 13:40:49 +0100

    desconstruct engine state object when initializing the background state

--- app/reducers/engine/index.js
+++ app/reducers/engine/index.js
@@ -7,7 +7,7 @@ const initialState = {
 const engineReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'INIT_BG_STATE':
-      return { backgroundState: Engine.state };
+      return { backgroundState: { ...Engine.state } };
     case 'UPDATE_BG_STATE': {
       const newState = { ...state };
       newState.backgroundState[action.key] = Engine.state[action.key];
