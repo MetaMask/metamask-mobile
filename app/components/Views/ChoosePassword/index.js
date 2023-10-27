d3bcbbaec9c6ff43d1e021f457deb1e3d754511c
Jonathan Ferreira <44679989+Jonathansoufer@users.noreply.github.com>
2023-10-27 10:29:21 +0100

    refactor: [Part 4] - Ongoing Work for legacy <Text> comp replacement. (#7604)
    ## **Description**
    This PR replaces old usage of legacy <Text> component with the text
    component that standardizes on typography provided through theme via
    [@metamask/design-tokens](https://www.npmjs.com/package/@metamask/design-tokens)
    library. This will bring uniformity to all text elements on our app.
    
    ## **Manual testing steps**
    - Fresh Install MM app and Go Through Create a New Wallet up to Create
    Password screen.
    
    ## **Screenshots/Recordings**
    
    ### **Before**
    ![Screenshot 2023-10-25 at 12 46
    35](https://github.com/MetaMask/metamask-mobile/assets/44679989/d5a15182-248f-4e75-9f13-035efb0db467)
    ![Screenshot 2023-10-25 at 12 39
    00](https://github.com/MetaMask/metamask-mobile/assets/44679989/7211ccda-b9db-4cc9-8358-251f3f1567d9)
    
    ### **After**
    ![Screenshot 2023-10-25 at 13 52
    33](https://github.com/MetaMask/metamask-mobile/assets/44679989/61b3eaaa-08d3-44c5-936c-9d0173a62eca)
    ![Screenshot 2023-10-25 at 12 51
    15](https://github.com/MetaMask/metamask-mobile/assets/44679989/bb9cf1e0-21ec-4a50-9835-94860f1e23a8)
    
    ## **Related issues**
    
    This is **part** of ongoing tasks related to issue #6887
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've clearly explained:
      - [x] What problem this PR is solving.
      - [x] How this problem was solved.
      - [x] How reviewers can test my changes.
    - [x] I’ve indicated what issue this PR is linked to: Fixes #???
    - [x] I’ve included tests if applicable.
    - [x] I’ve documented any added code.
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    - [ ] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [x] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/Views/ChoosePassword/index.js
+++ app/components/Views/ChoosePassword/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import {
   ActivityIndicator,
   Alert,
-  Text,
   View,
   TextInput,
   SafeAreaView,
@@ -14,6 +13,10 @@ import {
 } from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import Text, {
+  TextColor,
+  TextVariant,
+} from '../../../component-library/components/Texts/Text';
 import AsyncStorage from '../../../store/async-storage-wrapper';
 import { connect } from 'react-redux';
 import {
@@ -68,7 +71,6 @@ import {
 } from '../../../constants/test-ids';
 import { LoginOptionsSwitch } from '../../UI/LoginOptionsSwitch';
 import generateTestId from '../../../../wdio/utils/generateTestId';
-import { scale } from 'react-native-size-matters';
 import navigateTermsOfUse from '../../../util/termsOfUse/termsOfUse';
 
 const createStyles = (colors) =>
@@ -110,20 +112,13 @@ const createStyles = (colors) =>
       alignItems: 'center',
     },
     title: {
-      fontSize: Device.isAndroid() ? 20 : 25,
       marginTop: 20,
       marginBottom: 20,
-      color: colors.text.default,
       justifyContent: 'center',
       textAlign: 'center',
-      ...fontStyles.bold,
     },
     subtitle: {
-      fontSize: 16,
-      lineHeight: 23,
-      color: colors.text.default,
       textAlign: 'center',
-      ...fontStyles.normal,
     },
     text: {
       marginBottom: 10,
@@ -145,14 +140,9 @@ const createStyles = (colors) =>
       marginTop: -5,
     },
     label: {
-      ...fontStyles.normal,
-      fontSize: 14,
-      color: colors.text.default,
       paddingHorizontal: 10,
-      lineHeight: 18,
     },
     learnMore: {
-      color: colors.primary.default,
       textDecorationLine: 'underline',
       textDecorationColor: colors.primary.default,
     },
@@ -175,10 +165,6 @@ const createStyles = (colors) =>
       marginTop: 20,
       paddingHorizontal: 10,
     },
-    errorMsg: {
-      color: colors.error.default,
-      ...fontStyles.normal,
-    },
     biometrics: {
       position: 'relative',
       marginTop: 20,
@@ -195,18 +181,8 @@ const createStyles = (colors) =>
       top: 0,
       right: 0,
     },
-    hintLabel: {
-      color: colors.text.default,
-      fontSize: 16,
-      marginBottom: 12,
-      ...fontStyles.normal,
-    },
     passwordStrengthLabel: {
-      height: 20,
       marginTop: 10,
-      fontSize: scale(10),
-      color: colors.text.default,
-      ...fontStyles.normal,
     },
     showPassword: {
       position: 'absolute',
@@ -636,14 +612,17 @@ class ChoosePassword extends PureComponent {
               )}
             </View>
             <ActivityIndicator size="large" color={colors.text.default} />
-            <Text style={styles.title}>
+            <Text variant={TextVariant.HeadingLG} style={styles.title}>
               {strings(
                 previousScreen === ONBOARDING
                   ? 'create_wallet.title'
                   : 'secure_your_wallet.creating_password',
               )}
             </Text>
-            <Text style={styles.subtitle}>
+            <Text
+              variant={TextVariant.HeadingSMRegular}
+              style={styles.subtitle}
+            >
               {strings('create_wallet.subtitle')}
             </Text>
           </View>
@@ -657,22 +636,26 @@ class ChoosePassword extends PureComponent {
             >
               <View testID={CREATE_PASSWORD_CONTAINER_ID}>
                 <View style={styles.content}>
-                  <Text style={styles.title}>
+                  <Text variant={TextVariant.HeadingLG} style={styles.title}>
                     {strings('choose_password.title')}
                   </Text>
                   <View style={styles.text}>
-                    <Text style={styles.subtitle}>
+                    <Text
+                      variant={TextVariant.HeadingSMRegular}
+                      style={styles.subtitle}
+                    >
                       {strings('choose_password.subtitle')}
                     </Text>
                   </View>
                 </View>
                 <View style={styles.field}>
-                  <Text style={styles.hintLabel}>
+                  <Text variant={TextVariant.BodySM}>
                     {strings('choose_password.password')}
                   </Text>
                   <Text
+                    variant={TextVariant.BodySM}
                     onPress={this.toggleShowHide}
-                    style={[styles.hintLabel, styles.showPassword]}
+                    style={styles.showPassword}
                   >
                     {strings(
                       `choose_password.${secureTextEntry ? 'show' : 'hide'}`,
@@ -692,19 +675,30 @@ class ChoosePassword extends PureComponent {
                     keyboardAppearance={themeAppearance}
                   />
                   {(password !== '' && (
-                    <Text style={styles.passwordStrengthLabel}>
+                    <Text
+                      variant={TextVariant.BodySM}
+                      style={styles.passwordStrengthLabel}
+                    >
                       {strings('choose_password.password_strength')}
-                      <Text style={styles[`strength_${passwordStrengthWord}`]}>
+                      <Text
+                        variant={TextVariant.BodySM}
+                        style={styles[`strength_${passwordStrengthWord}`]}
+                      >
                         {' '}
                         {strings(
                           `choose_password.strength_${passwordStrengthWord}`,
                         )}
                       </Text>
                     </Text>
-                  )) || <Text style={styles.passwordStrengthLabel} />}
+                  )) || (
+                    <Text
+                      variant={TextVariant.BodySM}
+                      style={styles.passwordStrengthLabel}
+                    />
+                  )}
                 </View>
                 <View style={styles.field}>
-                  <Text style={styles.hintLabel}>
+                  <Text variant={TextVariant.BodySM}>
                     {strings('choose_password.confirm_password')}
                   </Text>
                   <TextInput
@@ -731,7 +725,10 @@ class ChoosePassword extends PureComponent {
                       />
                     ) : null}
                   </View>
-                  <Text style={styles.passwordStrengthLabel}>
+                  <Text
+                    variant={TextVariant.BodySM}
+                    style={styles.passwordStrengthLabel}
+                  >
                     {strings('choose_password.must_be_at_least', {
                       number: MIN_PASSWORD_LENGTH,
                     })}
@@ -753,17 +750,23 @@ class ChoosePassword extends PureComponent {
                   />
                   <Text
                     style={styles.label}
+                    variant={TextVariant.BodySM}
                     onPress={this.setSelection}
                     testID={ANDROID_I_UNDERSTAND_BUTTON_ID}
                   >
                     {strings('choose_password.i_understand')}{' '}
-                    <Text onPress={this.learnMore} style={styles.learnMore}>
+                    <Text
+                      variant={TextVariant.BodySM}
+                      color={TextColor.Info}
+                      onPress={this.learnMore}
+                      style={styles.learnMore}
+                    >
                       {strings('choose_password.learn_more')}
                     </Text>
                   </Text>
                 </View>
 
-                {!!error && <Text style={styles.errorMsg}>{error}</Text>}
+                {!!error && <Text color={TextColor.Error}>{error}</Text>}
               </View>
 
               <View style={styles.ctaWrapper}>
