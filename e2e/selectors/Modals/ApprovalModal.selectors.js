7ef2d4f17dae4205039de2bfcbde55af23cecbac
CW <chris.wilcox@consensys.net>
2023-10-29 10:55:25 -0700

    fix int issue

--- e2e/selectors/Modals/ApprovalModal.selectors.js
+++ e2e/selectors/Modals/ApprovalModal.selectors.js
@@ -1,5 +1,6 @@
 import messages from '../../../locales/languages/en.json';
 
+// eslint-disable-next-line import/prefer-default-export
 export const ApprovalModalSelectors = {
   ADD_NICKNAME_TEXT: messages.nickname.add_nickname,
   EDIT_NICKNAME_TEXT: messages.nickname.edit_nickname,
