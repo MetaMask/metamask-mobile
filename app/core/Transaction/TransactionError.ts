e35a43d5954378ad6aba10cb1c6a3488e0cc17f2
stanleyyuen <stanley.yuen@consensys.net>
2023-10-30 16:33:23 +0800

    fix: lint style

--- app/core/Transaction/TransactionError.ts
+++ app/core/Transaction/TransactionError.ts
@@ -1,26 +1,26 @@
 class TransactionError extends Error {
-    constructor(message: string) {
-      super(message);
-  
-      Object.setPrototypeOf(this, TransactionError.prototype);
-  
-      this.name = this.constructor.name;
-  
-      if (Error.captureStackTrace) {
-        Error.captureStackTrace(this, this.constructor);
-      }
+  constructor(message: string) {
+    super(message);
+
+    Object.setPrototypeOf(this, TransactionError.prototype);
+
+    this.name = this.constructor.name;
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
     }
   }
-  
-  class ApproveTransactionError extends TransactionError {}
-  
-  class CancelTransactionError extends TransactionError {}
-  
-  class SpeedupTransactionError extends TransactionError {}
-  
-  export {
-    TransactionError,
-    CancelTransactionError,
-    ApproveTransactionError,
-    SpeedupTransactionError,
-  };
+}
+
+class ApproveTransactionError extends TransactionError {}
+
+class CancelTransactionError extends TransactionError {}
+
+class SpeedupTransactionError extends TransactionError {}
+
+export {
+  TransactionError,
+  CancelTransactionError,
+  ApproveTransactionError,
+  SpeedupTransactionError,
+};
