6199e21f88dcd7a6e5729ac9d0a64cc139a4a82c
cryptotavares <joao.tavares@consensys.net>
2023-11-01 17:07:51 +0000

    chore: add shared gas types
    Created a types dir at the root where we
    can start adding types for known entities,
    instead of repeating the same types on every
    component.

---
+++ app/types/gas.ts
@@ -0,0 +1,15 @@
+import AppConstants from '../core/AppConstants';
+import { GAS_ESTIMATE_TYPES } from '@metamask/gas-fee-controller';
+
+type GAS_OPTIONS = (typeof AppConstants)['GAS_OPTIONS'];
+
+export type GAS_TIME_OPTIONS =
+  (typeof AppConstants)['GAS_TIMES'][keyof (typeof AppConstants)['GAS_TIMES']];
+
+export type GAS_ESTIMATE_TYPES_OPTIONS =
+  (typeof GAS_ESTIMATE_TYPES)[keyof typeof GAS_ESTIMATE_TYPES];
+
+export type SELECT_GAS_OPTIONS = Omit<
+  GAS_OPTIONS,
+  'MARKET' | 'AGGRESSIVE'
+>[keyof Omit<GAS_OPTIONS, 'MARKET' | 'AGGRESSIVE'>];
