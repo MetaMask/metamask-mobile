06c1c9affd7581eb4a06888a93f175a14069dd78
Sylva Elendu <iamsylvaelendu@gmail.com>
2023-11-02 11:12:47 +0100

    rebased to main

--- app/components/UI/ApproveTransactionHeader/ApproveTransactionHeader.constants.ts
+++ app/components/UI/ApproveTransactionHeader/ApproveTransactionHeader.constants.ts
@@ -2,6 +2,3 @@ import AppConstants from '../../../core/AppConstants';
 export const { ORIGIN_DEEPLINK, ORIGIN_QR_CODE } = AppConstants.DEEPLINKS;
 export const APPROVE_TRANSACTION_ORIGIN_PILL =
   'approve_transaction_origin_pill';
-
-export const FAV_ICON_URL = (hostUrl: string) =>
-  `https://api.faviconkit.com/${hostUrl}/64`;
