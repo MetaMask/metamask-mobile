ee9817906cfa52969b7247d87abf58a7347c821b
SamuelSalas <samuel.salas.reyes@gmail.com>
2023-11-03 11:31:34 -0600

    test: Move testids from in SendView, WalletView, SendLinkView and AmountView (#7682)
    ## **Description**
    Relocating the TestIDs from each page object to their corresponding
    testID file within the testID folder. This pr is aimed at transferring
    the TestIDs from the page objects in the SendView, WalletView,
    SendLinkView AmountView files.
    
    ## **Related issues**
    
    Fixes: #7561
    
    ## **Manual testing steps**
    
    
    ## **Screenshots/Recordings**
    https://app.bitrise.io/build/14011d98-8b21-4b43-ab74-f29f77fb0fc6
    
    ### **Before**
    
    
    ### **After**
    
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [ ] I've linked related issues
    - [ ] I've included manual testing steps
    - [ ] I've included screenshots/recordings if applicable
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    - [ ] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [ ] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- e2e/specs/settings/addressbook-tests.spec.js
+++ e2e/specs/settings/addressbook-tests.spec.js
@@ -48,7 +48,7 @@ describe(Smoke('Addressbook Tests'), () => {
     await TabBarComponent.tapActions();
     await WalletActionsModal.tapSendButton();
     // Make sure view with my accounts visible
-    await SendView.isMyAccountsVisisble();
+    await SendView.isMyAccountsVisible();
   });
 
   it('should show invalid address error message', async () => {
