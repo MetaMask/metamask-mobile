faf0b14cdfc81ae1b9728ab4d19a08c542bb8255
abretonc7s <arthur.breton@consensys.net>
2023-11-06 08:12:02 +0800

    feat: display qeue before reset for debuging purpose

--- app/core/SDKConnect/RPCQueueManager.ts
+++ app/core/SDKConnect/RPCQueueManager.ts
@@ -9,7 +9,8 @@ export class RPCQueueManager {
     const queuLength = Object.keys(this.rpcQueue).length;
     if (queuLength > 0) {
       console.warn(
-        `RPCQueueManager: ${queuLength} RPCs are still in the queue`,
+        `RPCQueueManager: ${queuLength} RPCs still in the queue`,
+        this.rpcQueue,
       );
     }
     this.rpcQueue = {};
