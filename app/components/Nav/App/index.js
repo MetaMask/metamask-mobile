c706bfc226e22e9217eff94bc545e5fca6b758d4
abretonc7s <arthur.breton@consensys.net>
2023-11-06 14:08:00 +0800

    feat: multi-stage sdk connect initialization

--- app/components/Nav/App/index.js
+++ app/components/Nav/App/index.js
@@ -1,3 +1,5 @@
+import { CommonActions, NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React, {
   useCallback,
   useContext,
@@ -5,94 +7,92 @@ import React, {
   useRef,
   useState,
 } from 'react';
-import { CommonActions, NavigationContainer } from '@react-navigation/native';
 import { Animated, Linking } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Login from '../../Views/Login';
-import QRScanner from '../../Views/QRScanner';
-import Onboarding from '../../Views/Onboarding';
-import OnboardingCarousel from '../../Views/OnboardingCarousel';
-import ChoosePassword from '../../Views/ChoosePassword';
-import AccountBackupStep1 from '../../Views/AccountBackupStep1';
-import AccountBackupStep1B from '../../Views/AccountBackupStep1B';
-import ManualBackupStep1 from '../../Views/ManualBackupStep1';
-import ManualBackupStep2 from '../../Views/ManualBackupStep2';
-import ManualBackupStep3 from '../../Views/ManualBackupStep3';
-import ImportFromSecretRecoveryPhrase from '../../Views/ImportFromSecretRecoveryPhrase';
-import DeleteWalletModal from '../../../components/UI/DeleteWalletModal';
-import WhatsNewModal from '../../UI/WhatsNewModal/WhatsNewModal';
-import Main from '../Main';
-import OptinMetrics from '../../UI/OptinMetrics';
-import MetaMaskAnimation from '../../UI/MetaMaskAnimation';
-import SimpleWebview from '../../Views/SimpleWebview';
-import SharedDeeplinkManager from '../../../core/DeeplinkManager';
-import Engine from '../../../core/Engine';
 import branch from 'react-native-branch';
-import AppConstants from '../../../core/AppConstants';
-import Logger from '../../../util/Logger';
-import { trackErrorAsAnalytics } from '../../../util/analyticsV2';
-import { routingInstrumentation } from '../../../util/sentryUtils';
-import Analytics from '../../../core/Analytics/Analytics';
+import { getVersion } from 'react-native-device-info';
 import { connect, useDispatch } from 'react-redux';
+import AmbiguousAddressSheet from '../../../../app/components/Views/Settings/Contacts/AmbiguousAddressSheet/AmbiguousAddressSheet';
+import WC2Manager, {
+  isWC2Enabled,
+} from '../../../../app/core/WalletConnect/WalletConnectV2';
+import {
+  setCurrentBottomNavRoute,
+  setCurrentRoute,
+} from '../../../actions/navigation';
+import ModalConfirmation from '../../../component-library/components/Modals/ModalConfirmation';
+import ModalMandatory from '../../../component-library/components/Modals/ModalMandatory';
+import Toast, {
+  ToastContext,
+} from '../../../component-library/components/Toast';
+import DeleteWalletModal from '../../../components/UI/DeleteWalletModal';
+import { EnableAutomaticSecurityChecksModal } from '../../../components/UI/EnableAutomaticSecurityChecksModal';
+import { TurnOffRememberMeModal } from '../../../components/UI/TurnOffRememberMeModal';
+import { UpdateNeeded } from '../../../components/UI/UpdateNeeded';
+import AccountActions from '../../../components/Views/AccountActions';
+import AccountConnect from '../../../components/Views/AccountConnect';
+import AccountPermissions from '../../../components/Views/AccountPermissions';
+import AccountSelector from '../../../components/Views/AccountSelector';
+import NetworkSelector from '../../../components/Views/NetworkSelector';
+import EthSignFriction from '../../../components/Views/Settings/AdvancedSettings/EthSignFriction';
+import { AUTHENTICATION_APP_TRIGGERED_AUTH_NO_CREDENTIALS } from '../../../constants/error';
+import Routes from '../../../constants/navigation/Routes';
 import {
   CURRENT_APP_VERSION,
   EXISTING_USER,
   LAST_APP_VERSION,
 } from '../../../constants/storage';
-import { getVersion } from 'react-native-device-info';
-import {
-  setCurrentBottomNavRoute,
-  setCurrentRoute,
-} from '../../../actions/navigation';
-import { findRouteNameFromNavigatorState } from '../../../util/general';
 import { Authentication } from '../../../core/';
-import { isBlockaidFeatureEnabled } from '../../../util/blockaid';
-import { useTheme } from '../../../util/theme';
-import Device from '../../../util/device';
+import Analytics from '../../../core/Analytics/Analytics';
+import AppConstants from '../../../core/AppConstants';
+import SharedDeeplinkManager from '../../../core/DeeplinkManager';
+import Engine from '../../../core/Engine';
+import NavigationService from '../../../core/NavigationService';
 import SDKConnect from '../../../core/SDKConnect/SDKConnect';
+import { PPOMView } from '../../../lib/ppom/PPOMView';
+import AsyncStorage from '../../../store/async-storage-wrapper';
 import { colors as importedColors } from '../../../styles/common';
-import Routes from '../../../constants/navigation/Routes';
-import ModalConfirmation from '../../../component-library/components/Modals/ModalConfirmation';
-import Toast, {
-  ToastContext,
-} from '../../../component-library/components/Toast';
-import AccountSelector from '../../../components/Views/AccountSelector';
-import AccountConnect from '../../../components/Views/AccountConnect';
-import AccountPermissions from '../../../components/Views/AccountPermissions';
-import { SRPQuiz } from '../../Views/Quiz';
-import { TurnOffRememberMeModal } from '../../../components/UI/TurnOffRememberMeModal';
+import Logger from '../../../util/Logger';
+import { trackErrorAsAnalytics } from '../../../util/analyticsV2';
+import { isBlockaidFeatureEnabled } from '../../../util/blockaid';
+import Device from '../../../util/device';
+import { findRouteNameFromNavigatorState } from '../../../util/general';
+import { routingInstrumentation } from '../../../util/sentryUtils';
+import { useTheme } from '../../../util/theme';
+import MetaMaskAnimation from '../../UI/MetaMaskAnimation';
+import OptinMetrics from '../../UI/OptinMetrics';
+import WhatsNewModal from '../../UI/WhatsNewModal/WhatsNewModal';
+import AccountBackupStep1 from '../../Views/AccountBackupStep1';
+import AccountBackupStep1B from '../../Views/AccountBackupStep1B';
 import AssetHideConfirmation from '../../Views/AssetHideConfirmation';
+import AssetOptions from '../../Views/AssetOptions';
+import ChoosePassword from '../../Views/ChoosePassword';
+import ConnectQRHardware from '../../Views/ConnectQRHardware';
 import DetectedTokens from '../../Views/DetectedTokens';
 import DetectedTokensConfirmation from '../../Views/DetectedTokensConfirmation';
-import AssetOptions from '../../Views/AssetOptions';
+import EditAccountName from '../../Views/EditAccountName/EditAccountName';
+import ImportFromSecretRecoveryPhrase from '../../Views/ImportFromSecretRecoveryPhrase';
 import ImportPrivateKey from '../../Views/ImportPrivateKey';
 import ImportPrivateKeySuccess from '../../Views/ImportPrivateKeySuccess';
-import ConnectQRHardware from '../../Views/ConnectQRHardware';
-import { AUTHENTICATION_APP_TRIGGERED_AUTH_NO_CREDENTIALS } from '../../../constants/error';
-import { UpdateNeeded } from '../../../components/UI/UpdateNeeded';
-import { EnableAutomaticSecurityChecksModal } from '../../../components/UI/EnableAutomaticSecurityChecksModal';
-import NetworkSettings from '../../Views/Settings/NetworksSettings/NetworkSettings';
-import ModalMandatory from '../../../component-library/components/Modals/ModalMandatory';
-import { RestoreWallet } from '../../Views/RestoreWallet';
-import WalletRestored from '../../Views/RestoreWallet/WalletRestored';
-import WalletResetNeeded from '../../Views/RestoreWallet/WalletResetNeeded';
-import SDKLoadingModal from '../../Views/SDKLoadingModal/SDKLoadingModal';
-import SDKFeedbackModal from '../../Views/SDKFeedbackModal/SDKFeedbackModal';
-import AccountActions from '../../../components/Views/AccountActions';
-import EthSignFriction from '../../../components/Views/Settings/AdvancedSettings/EthSignFriction';
-import WalletActions from '../../Views/WalletActions';
-import NetworkSelector from '../../../components/Views/NetworkSelector';
-import EditAccountName from '../../Views/EditAccountName/EditAccountName';
-import WC2Manager, {
-  isWC2Enabled,
-} from '../../../../app/core/WalletConnect/WalletConnectV2';
-import { PPOMView } from '../../../lib/ppom/PPOMView';
-import NavigationService from '../../../core/NavigationService';
 import LockScreen from '../../Views/LockScreen';
-import AsyncStorage from '../../../store/async-storage-wrapper';
-import ShowIpfsGatewaySheet from '../../Views/ShowIpfsGatewaySheet/ShowIpfsGatewaySheet';
+import Login from '../../Views/Login';
+import ManualBackupStep1 from '../../Views/ManualBackupStep1';
+import ManualBackupStep2 from '../../Views/ManualBackupStep2';
+import ManualBackupStep3 from '../../Views/ManualBackupStep3';
+import Onboarding from '../../Views/Onboarding';
+import OnboardingCarousel from '../../Views/OnboardingCarousel';
+import QRScanner from '../../Views/QRScanner';
+import { SRPQuiz } from '../../Views/Quiz';
+import { RestoreWallet } from '../../Views/RestoreWallet';
+import WalletResetNeeded from '../../Views/RestoreWallet/WalletResetNeeded';
+import WalletRestored from '../../Views/RestoreWallet/WalletRestored';
+import SDKFeedbackModal from '../../Views/SDKFeedbackModal/SDKFeedbackModal';
+import SDKLoadingModal from '../../Views/SDKLoadingModal/SDKLoadingModal';
+import NetworkSettings from '../../Views/Settings/NetworksSettings/NetworkSettings';
 import ShowDisplayNftMediaSheet from '../../Views/ShowDisplayMediaNFTSheet/ShowDisplayNFTMediaSheet';
-import AmbiguousAddressSheet from '../../../../app/components/Views/Settings/Contacts/AmbiguousAddressSheet/AmbiguousAddressSheet';
+import ShowIpfsGatewaySheet from '../../Views/ShowIpfsGatewaySheet/ShowIpfsGatewaySheet';
+import SimpleWebview from '../../Views/SimpleWebview';
+import WalletActions from '../../Views/WalletActions';
+import Main from '../Main';
 
 const clearStackNavigatorOptions = {
   headerShown: false,
@@ -233,6 +233,8 @@ const App = ({ userLoggedIn }) => {
   const { toastRef } = useContext(ToastContext);
   const dispatch = useDispatch();
   const sdkInit = useRef(false);
+  const sdkPostInit = useRef(false);
+  const [postInitReady, setPostInitReady] = useState(false);
   const [onboarded, setOnboarded] = useState(false);
   const triggerSetCurrentRoute = (route) => {
     dispatch(setCurrentRoute(route));
@@ -285,13 +287,10 @@ const App = ({ userLoggedIn }) => {
     const deeplink = params?.['+non_branch_link'] || uri || null;
     try {
       if (deeplink) {
-        const { KeyringController } = Engine.context;
-        const isUnlocked = KeyringController.isUnlocked();
-        isUnlocked
-          ? SharedDeeplinkManager.parse(deeplink, {
-              origin: AppConstants.DEEPLINKS.ORIGIN_DEEPLINK,
-            })
-          : SharedDeeplinkManager.setDeeplink(deeplink);
+        SharedDeeplinkManager.setDeeplink(deeplink);
+        SharedDeeplinkManager.parse(deeplink, {
+          origin: AppConstants.DEEPLINKS.ORIGIN_DEEPLINK,
+        });
       }
     } catch (e) {
       Logger.error(e, `Deeplink: Error parsing deeplink`);
@@ -353,17 +352,41 @@ const App = ({ userLoggedIn }) => {
   }, []);
 
   useEffect(() => {
-    if (navigator?.getCurrentRoute && !sdkInit.current && onboarded) {
-      SDKConnect.getInstance()
-        .init({ navigation: navigator })
-        .then(() => {
+    // Init SDKConnect only if the navigator is ready, user is onboarded, and SDK is not initialized.
+    async function initSDKConnect() {
+      if (navigator?.getCurrentRoute && onboarded && !sdkInit.current) {
+        try {
+          const sdkConnect = SDKConnect.getInstance();
+          await sdkConnect.init({ navigation: navigator });
+          setPostInitReady(true);
           sdkInit.current = true;
-        })
-        .catch((err) => {
+        } catch (err) {
           console.error(`Cannot initialize SDKConnect`, err);
-        });
+        }
+      }
     }
+    initSDKConnect();
   }, [navigator, onboarded]);
+
+  useEffect(() => {
+    // Handle post-init process separately.
+    async function handlePostInit() {
+      if (
+        sdkInit.current &&
+        !sdkPostInit.current &&
+        postInitReady &&
+        userLoggedIn
+      ) {
+        try {
+          await SDKConnect.getInstance().postInit();
+          sdkPostInit.current = true;
+        } catch (err) {
+          console.error(`Cannot postInit SDKConnect`, err);
+        }
+      }
+    }
+    handlePostInit();
+  }, [userLoggedIn, postInitReady]);
 
   useEffect(() => {
     if (isWC2Enabled) {
