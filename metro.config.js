339974555b3faed739245733d28aa1795c7ccb9f
JSoufer <jonathan.ferreira@consensys.net>
2023-11-08 12:51:37 +0000

    new: fence code core transformation based on metro bundler

--- metro.config.js
+++ metro.config.js
@@ -20,7 +20,7 @@ module.exports = (async () => {
           inlineRequires: true,
         },
       }),
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      babelTransformerPath: require.resolve('./metro.transform.js'),
       assetPlugins: ['react-native-svg-asset-plugin'],
       svgAssetPlugin: {
         pngCacheDir: '.png-cache',
@@ -33,7 +33,6 @@ module.exports = (async () => {
     resolver: {
       assetExts: assetExts.filter((ext) => ext !== 'svg'),
       sourceExts: [...sourceExts, 'svg', 'cjs'],
-      resolverMainFields: ['sbmodern', 'react-native', 'browser', 'main'],
     },
     maxWorkers: 2,
   };
---
+++ metro.transform.js
@@ -0,0 +1,22 @@
+const svgTransformer = require("react-native-svg-transformer");
+const defaultTransformer = require("metro-react-native-babel-transformer");
+const path = require("path");
+const { removeFencedCode, lintTransformedFile } = require("./remove-fenced-code.js");
+
+module.exports.transform = async ({ src, filename, options }) => {
+    if (filename.endsWith(".svg")) {
+        return svgTransformer.transform({ src, filename, options });
+    }
+      /**
+       * params based on builds we're code splitting
+       * i.e: flavorDimensions "version" productFlavors from android/app/build.gradle
+       */
+    if (['.js', '.cjs', '.mjs', '.ts'].includes(path.extname(filename))) {
+        const [processedSource, didModify] = removeFencedCode(filename, { all: new Set(['flask']), active: new Set([]) }, src);
+        if (didModify) {
+            await lintTransformedFile(fileContent, filename)
+        }
+        return defaultTransformer.transform({ src: processedSource, filename, options });
+    }
+    return defaultTransformer.transform({ src, filename, options });
+};
