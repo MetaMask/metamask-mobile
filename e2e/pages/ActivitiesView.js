012727249dbed8e64f072c5dcae2b5c35d6a09ae
Davide Brocchetto <davide.brocchetto@consensys.net>
2023-11-08 18:44:41 +0800

    Split the Swap test in two so transaction list is checked at the end

--- e2e/pages/ActivitiesView.js
+++ e2e/pages/ActivitiesView.js
@@ -1,18 +1,27 @@
 import TestHelpers from '../helpers';
 import messages from '../../locales/languages/en.json';
+import { ActivityViewSelectorsIDs } from '../selectors/ActivityView.selectors';
 
 export default class ActivitiesView {
   static async isVisible() {
-    await TestHelpers.checkIfElementWithTextIsVisible(
+    await TestHelpers.checkIfElementByTextIsVisible(
       messages.transactions_view.title,
     );
   }
 
-  static async tapOnSwapActivity(sourceToken, destinationToken) {
-    let title = messages.swaps.transaction_label.swap;
-    title = title.replace('{{sourceToken}}', sourceToken);
-    title = title.replace('{{destinationToken}}', destinationToken);
-
+  static async tapActivity(title) {
     await TestHelpers.waitAndTapText(title);
+  }
+
+  static async checkActivityTitle(title, index) {
+    return expect(
+      element(by.id(ActivityViewSelectorsIDs.TITLE)).atIndex(index),
+    ).toHaveText(title);
+  }
+
+  static async checkActivityStatus(status, index) {
+    return expect(
+      element(by.id(ActivityViewSelectorsIDs.STATUS)).atIndex(index),
+    ).toHaveText(status);
   }
 }
