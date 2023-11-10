40f25a8a7464a3920a0611a37f71a24f747f29e6
Davide Brocchetto <davide.brocchetto@consensys.net>
2023-11-10 23:50:57 +0800

    chore: Split the Swap test into two parts so that transaction activity is checked at the end (#7726)
    ## **Description**
    
    I refactored the code so that all swaps are executed first and we check
    that all transaction activities at the at the end. This increases
    execution speed, reduce chance for flakiness and also fix the previous
    code that wasn't catching if the transaction was failing
    
    ## **Related issues**
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [ ] I've linked related issues
    - [ ] I've included manual testing steps
    - [ ] I've included screenshots/recordings if applicable
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    - [ ] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [ ] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

---
+++ e2e/selectors/Modals/DetailsModal.selectors.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/prefer-default-export
+export const DetailsModalSelectorsIDs = {
+  TITLE: 'details-modal-title',
+  TRANSACTION_STATUS: 'transaction-status',
+  CLOSE_ICON: 'details-modal-close-ico',
+};
