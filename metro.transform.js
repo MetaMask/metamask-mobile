93a44c51190e2fbf71c2935015d8aa9335f29146
JSoufer <jonathan.ferreira@consensys.net>
2023-11-13 12:41:15 +0000

    refactor: polish on prettier reqs

--- metro.transform.js
+++ metro.transform.js
@@ -1,22 +1,35 @@
-const svgTransformer = require("react-native-svg-transformer");
-const defaultTransformer = require("metro-react-native-babel-transformer");
-const path = require("path");
-const { removeFencedCode, lintTransformedFile } = require("./remove-fenced-code.js");
+/* eslint-disable import/no-nodejs-modules */
+/* eslint-disable import/no-commonjs */
+const svgTransformer = require('react-native-svg-transformer');
+const defaultTransformer = require('metro-react-native-babel-transformer');
+const path = require('path');
+const {
+  removeFencedCode,
+  lintTransformedFile,
+} = require('./remove-fenced-code.js');
 
 module.exports.transform = async ({ src, filename, options }) => {
-    if (filename.endsWith(".svg")) {
-        return svgTransformer.transform({ src, filename, options });
+  if (filename.endsWith('.svg')) {
+    return svgTransformer.transform({ src, filename, options });
+  }
+  /**
+   * params based on builds we're code splitting
+   * i.e: flavorDimensions "version" productFlavors from android/app/build.gradle
+   */
+  if (['.js', '.cjs', '.mjs', '.ts'].includes(path.extname(filename))) {
+    const [processedSource, didModify] = removeFencedCode(
+      filename,
+      { all: new Set(['flask', 'snaps']), active: new Set([]) },
+      src,
+    );
+    if (didModify) {
+      await lintTransformedFile(processedSource, filename);
     }
-      /**
-       * params based on builds we're code splitting
-       * i.e: flavorDimensions "version" productFlavors from android/app/build.gradle
-       */
-    if (['.js', '.cjs', '.mjs', '.ts'].includes(path.extname(filename))) {
-        const [processedSource, didModify] = removeFencedCode(filename, { all: new Set(['flask']), active: new Set([]) }, src);
-        if (didModify) {
-            await lintTransformedFile(fileContent, filename)
-        }
-        return defaultTransformer.transform({ src: processedSource, filename, options });
-    }
-    return defaultTransformer.transform({ src, filename, options });
+    return defaultTransformer.transform({
+      src: processedSource,
+      filename,
+      options,
+    });
+  }
+  return defaultTransformer.transform({ src, filename, options });
 };
