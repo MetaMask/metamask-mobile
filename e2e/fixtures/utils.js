47c741672e719e8d2b472f4fafd4f9921d09e754
Vinicius Stevam <vinicius.stevam@consensys.net>
2023-11-13 17:08:49 +0000

    test

--- e2e/fixtures/utils.js
+++ e2e/fixtures/utils.js
@@ -1,22 +1,35 @@
+/* eslint-disable no-console */
 import { DEFAULT_GANACHE_PORT } from '../../app/util/test/ganache';
 import { DEFAULT_FIXTURE_SERVER_PORT } from './fixture-server';
 import { DEFAULT_DAPP_SERVER_PORT } from './fixture-helper';
 
+function transformToValidPort(defaultPort, pid) {
+  // Improve uniqueness by using a simple transformation
+  const transformedPort = (parseInt(pid, 10) % 100000) + defaultPort;
+
+  // Ensure the transformed port falls within the valid port range (0-65535)
+  return transformedPort % 65536;
+}
+
 function getServerPort(defaultPort) {
   if (process.env.CI) {
-    return defaultPort + Number(process.env.JEST_WORKER_ID);
+    return transformToValidPort(defaultPort, process.pid);
   }
   return defaultPort;
 }
 
 export function getGanachePort() {
-  return getServerPort(DEFAULT_GANACHE_PORT);
+  const port = getServerPort(DEFAULT_GANACHE_PORT);
+  return port;
 }
 
 export function getFixturesServerPort() {
-  return getServerPort(DEFAULT_FIXTURE_SERVER_PORT);
+  const port = getServerPort(DEFAULT_FIXTURE_SERVER_PORT);
+  return port;
 }
 
 export function getLocalTestDappPort() {
-  return getServerPort(DEFAULT_DAPP_SERVER_PORT);
+  const port = getServerPort(DEFAULT_DAPP_SERVER_PORT);
+
+  return port;
 }
