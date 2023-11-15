9ab1dd17acccd1d6f2ef859c0e8ec594090e7c01
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2023-11-15 15:21:23 -0600

    fix: remove wrong property (#7834)

--- app/components/UI/AddressInputs/index.js
+++ app/components/UI/AddressInputs/index.js
@@ -100,7 +100,6 @@ const createStyles = (colors, layout = 'horizontal') => {
     accountNameLabelText: {
       marginLeft: 4,
       marginTop: 4,
-      horizontalAlign: 'center',
       textAlign: 'center',
       paddingHorizontal: 8,
       ...fontStyles.bold,
