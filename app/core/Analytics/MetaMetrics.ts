38397d6bae30cec0bcae1c590e3c5f9b0ef003fb
Nicolas MASSART <nicolas.massart@consensys.net>
2023-11-16 17:22:46 +0100

    update regulation and tests
    and add comment

--- app/core/Analytics/MetaMetrics.ts
+++ app/core/Analytics/MetaMetrics.ts
@@ -27,6 +27,50 @@ import {
 } from './MetaMetrics.constants';
 import generateMetametricsId from '../../util/metrics/MetaMetricsId';
 
+/**
+ * MetaMetrics using Segment as the analytics provider.
+ *
+ *
+ * ## Base tracking usage
+ * ```
+ * const metrics = await MetaMetrics.getInstance();
+ * metrics.trackEvent('event_name', { property: 'value' });
+ * ```
+ *
+ * ## Enabling MetaMetrics
+ * Enable the metrics when user agrees (optin or settings).
+ * ```
+ * const metrics = await MetaMetrics.getInstance();
+ * await metrics.enable();
+ *```
+ *
+ * ## Disabling MetaMetrics
+ * Disable the metrics when user refuses (optout or settings).
+ * ```
+ * const metrics = await MetaMetrics.getInstance();
+ * await metrics.enable(false);
+ * ```
+ *
+ * ## Identify user
+ * By default all metrics are anonymous using a single hardcoded anonymous ID.
+ * Until you identify the user, all events will be associated to this anonymous ID.
+ * ```
+ * const metrics = await MetaMetrics.getInstance();
+ * metrics.addTraitsToUser({ property: 'value' });
+ * ```
+ *
+ * This will associate the user to a new generated unique ID and all future events will be associated to this user.
+ *
+ * ## Reset user
+ * If you want to reset the user ID, you can call the reset method.
+ * This will revert the user to the anonymous ID and generate a new unique ID.
+ * ```
+ * const metrics = await MetaMetrics.getInstance();
+ * metrics.reset();
+ * ```
+ *
+ * @see METAMETRICS_ANONYMOUS_ID
+ */
 class MetaMetrics implements IMetaMetrics {
   // Singleton instance
   protected static instance: MetaMetrics | null;
@@ -72,6 +116,9 @@ class MetaMetrics implements IMetaMetrics {
     return this.metametricsId;
   };
 
+  /**
+   * reset the analytics user ID and Sgment SDK state.
+   */
   #resetMetaMetricsId = async (): Promise<void> => {
     await DefaultPreference.set(METAMETRICS_ID, '');
     this.metametricsId = await this.#getMetaMetricsId();
@@ -81,6 +128,8 @@ class MetaMetrics implements IMetaMetrics {
    * associate traits or properties to an user.
    * Check Segment documentation for more information.
    * https://segment.com/docs/connections/sources/catalog/libraries/mobile/react-native/#identify
+   *
+   * Here we we only take traits as parameter as we want to keep the user ID controlled by the class.
    *
    * @param userTraits - Object containing user relevant traits or properties (optional).
    */
@@ -113,7 +162,7 @@ class MetaMetrics implements IMetaMetrics {
     this.segmentClient?.track(event, properties);
 
   /**
-   * Method to clear the internal state of the library for the current user and group.
+   * clear the internal state of the library for the current user and group.
    * https://segment.com/docs/connections/sources/catalog/libraries/mobile/react-native/#reset
    */
   #reset = (): void => {
@@ -138,19 +187,16 @@ class MetaMetrics implements IMetaMetrics {
    */
   #storeDeleteRegulationCreationDate = async (): Promise<void> => {
     const currentDate = new Date();
-    const month = currentDate.getUTCMonth() + 1;
-    const day = currentDate.getUTCDate();
-    const year = currentDate.getUTCFullYear();
-
-    // store with format: DAY/MONTH/YEAR
     await DefaultPreference.set(
       ANALYTICS_DATA_DELETION_DATE,
-      `${day}/${month}/${year}`,
+      `${
+        currentDate.getUTCMonth() + 1
+      }/${currentDate.getUTCDate()}/${currentDate.getUTCFullYear()}`,
     );
   };
 
   /**
-   * store segment's Regulation ID.
+   * store Segment's Regulation ID.
    *
    * @param regulationId - Segment's Regulation ID.
    */
@@ -162,7 +208,7 @@ class MetaMetrics implements IMetaMetrics {
   };
 
   /**
-   * generate a new delete regulation for an user.
+   * generate a new delete regulation for the user.
    * This is necessary to respect the GDPR and CCPA regulations.
    * Check Segment documentation for more information.
    * https://segment.com/docs/privacy/user-deletion-and-suppression/
@@ -187,10 +233,10 @@ class MetaMetrics implements IMetaMetrics {
           subjectIds: [this.metametricsId],
         }),
       });
-      const { result, status } = response as any;
+      const { data, status } = response as any;
 
-      if (status === '200') {
-        const { regulateId } = result.data;
+      if (status === 200) {
+        const { regulateId } = data;
         await this.#storeDeleteRegulationId(regulateId);
         await this.#storeDeleteRegulationCreationDate();
         return { status: DataDeleteResponseStatus.ok };
@@ -203,6 +249,14 @@ class MetaMetrics implements IMetaMetrics {
     }
   };
 
+  /**
+   * get an instance of the MetaMetrics system
+   * @returns Promise containing the MetaMetrics instance.
+   * Await this method to ensure the instance is ready.
+   * ```
+   * const metrics = await MetaMetrics.getInstance();
+   * ```
+   */
   public static async getInstance(): Promise<IMetaMetrics> {
     if (!this.instance) {
       const config = {
@@ -224,15 +278,27 @@ class MetaMetrics implements IMetaMetrics {
     return this.instance;
   }
 
+  /**
+   * enable or disable MetaMetrics
+   * @param enable - Boolean indicating if MetaMetrics should be enabled or disabled.
+   */
   enable = async (enable = true): Promise<void> => {
     this.enabled = enable;
     await this.#storeMetricsOptInPreference(this.enabled);
   };
 
+  /**
+   * check if MetaMetrics is enabled
+   * @returns Boolean indicating if MetaMetrics is enabled or disabled.
+   */
   isEnabled() {
     return this.enabled;
   }
 
+  /**
+   * add traits to the user and identify them
+   * @param userTraits
+   */
   addTraitsToUser = (userTraits: UserTraits): Promise<void> => {
     if (this.enabled) {
       return this.#identify(userTraits);
@@ -240,6 +306,11 @@ class MetaMetrics implements IMetaMetrics {
     return Promise.resolve();
   };
 
+  /**
+   * add an user to a specific group
+   * @param groupId
+   * @param groupTraits
+   */
   group = (groupId: string, groupTraits?: GroupTraits): Promise<void> => {
     if (this.enabled) {
       this.#group(groupId, groupTraits);
@@ -247,6 +318,16 @@ class MetaMetrics implements IMetaMetrics {
     return Promise.resolve();
   };
 
+  /**
+   * track an anonymous event
+   *
+   * This will track the event twice: once with the anonymous ID and once with the user ID.
+   * The anynomous has properties set so you can know what but not who.
+   * The non-anonymous has no properties so you can know who but not what.
+   *
+   * @param event
+   * @param properties
+   */
   trackAnonymousEvent(event: string, properties: JsonMap = {}): void {
     if (this.enabled) {
       this.#trackEvent(event, { anonymous: true, ...properties });
@@ -254,19 +335,39 @@ class MetaMetrics implements IMetaMetrics {
     }
   }
 
+  /**
+   * track an event - the regular way
+   * @param event
+   * @param properties
+   */
   trackEvent = (event: string, properties: JsonMap = {}): void => {
     if (this.enabled) {
       this.#trackEvent(event, { anonymous: false, ...properties });
     }
   };
 
+  /**
+   * clear the internal state of the library for the current user and reset the user ID.
+   */
   reset = async (): Promise<void> => {
     this.#reset();
     await this.#resetMetaMetricsId();
   };
 
+  /**
+   * forces the Segment SDK to flush all events in the queue.
+   * This will send all events to Segment without waiting for the queue to be full or the timeout to be reached.
+   */
   flush = async (): Promise<void> => this.segmentClient?.flush();
 
+  /**
+   * create a new delete regulation for the user.
+   * This is necessary to respect the GDPR and CCPA regulations.
+   * Check Segment documentation for more information.
+   * https://segment.com/docs/privacy/user-deletion-and-suppression/
+   * @returns Promise containing the status of the request.
+   * Await this method to ensure the request is completed.
+   */
   createSegmentDeleteRegulation = async (): Promise<{
     status: string;
     error?: string;
