aa0a370abb9d9be5bcaea560d9de8582f522c05b
JSoufer <jonathan.ferreira@consensys.net>
2023-11-16 20:25:31 +0000

    refactor: add availableFeatures const

--- metro.transform.js
+++ metro.transform.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable import/no-nodejs-modules */
 /* eslint-disable import/no-commonjs */
 const svgTransformer = require('react-native-svg-transformer');
@@ -6,14 +7,30 @@ const path = require('path');
 const {
   removeFencedCode,
   lintTransformedFile,
-} = require('./app/build/transforms/remove-fenced-code');
+} = require('./app/transforms/remove-fenced-code');
 
 const filesExtToScan = ['.js', '.cjs', '.mjs', '.ts'];
+const availableFeatures = new Set(['flask', 'snaps', 'beta']);
+
+const mainFeatureSet = new Set([]);
+const flaskFeatureSet = new Set(['flask', 'snaps']);
 
 module.exports.transform = async ({ src, filename, options }) => {
   if (filename.endsWith('.svg')) {
     return svgTransformer.transform({ src, filename, options });
   }
+
+  function getBuildTypeFeatures() {
+    switch (process.env.BUILD_TYPE) {
+      case 'main':
+        return mainFeatureSet;
+      case 'flask':
+        return flaskFeatureSet;
+      default:
+        throw new Error('Invalid build type');
+    }
+  }
+
   /**
    * params based on builds we're code splitting
    * i.e: flavorDimensions "version" productFlavors from android/app/build.gradle
@@ -21,7 +38,10 @@ module.exports.transform = async ({ src, filename, options }) => {
   if (filesExtToScan.includes(path.extname(filename))) {
     const [processedSource, didModify] = removeFencedCode(
       filename,
-      { all: new Set(['flask', 'snaps']), active: new Set([]) },
+      {
+        all: availableFeatures,
+        active: getBuildTypeFeatures(),
+      },
       src,
     );
     if (didModify) {
