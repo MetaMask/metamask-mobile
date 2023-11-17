25a6d855f5131eccfed3dd4cb3057bbe70528768
Jonathan Ferreira <44679989+Jonathansoufer@users.noreply.github.com>
2023-11-17 18:43:10 +0000

    feat: enable code fence capabilities on mobile app.  (#7734)
    ## **Description**
    
    This PR implements code fence feature to allow flask and main code
    co-live in the same branch yet delivered accordantly with build type,
    meaning flask code will only be included in flask build and not main.
    
    
    [Here](https://github.com/MetaMask/metamask-extension/blob/a0e8ee14af00f9793349206909c44d7b68985c41/development/build/transforms/remove-fenced-code.js#L88)
    is the version of code fencing implemented on extension.
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/accounts-planning/issues/112
    
    ## **Manual testing steps**
    
    Add code fence directive (BEGIN and END) on any code block and run the
    build.
    
    ```
                  {
                   ///: BEGIN:ONLY_INCLUDE_IN(flask)
                  }
    
                  <View style={styles.foxWrapper}>
                    {Device.isAndroid() ? (
                      <Image
                        source={require('../../../images/fox.png')}
                        style={styles.image}
                        resizeMethod={'auto'}
                      />
                    ) : (
                      <AnimatedFox bgColor={colors.background.default} />
                    )}
                  </View>
                  {
                    ///: END:ONLY_INCLUDE_IN
                  }
    ``` 
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [x] I've linked related issues
    - [x] I've included manual testing steps
    - [x] I've included screenshots/recordings if applicable
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    - [x] I’ve properly set the pull request status:
      - [x] In case it's not yet "ready for review", I've set it to "draft".
    - [x] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ## **Next Steps**
    - [ ] Adjust `android/app/build.gradle` and add new iOS
    configurations/schemas for FLASK build.
    - [ ]  Add env variable to distinguish builds.
    
    ---------
    
    Co-authored-by: Owen Craston <owengc12@gmail.com>
    Co-authored-by: Chris Wilcox <chris.wilcox@consensys.net>

---
+++ app/__mocks__/different-fenced-code-result-comp.mock.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MockComponent = () => {
+  ///: BEGIN:ONLY_INCLUDE_IN(flask)
+  console.log('Hello from inside Flask fence');
+  ///: END:ONLY_INCLUDE_IN
+  console.log('Goodbye, from outside Flask and Snaps fence');
+  return (
+    <View>
+      <Text>Hello from outside Flask and Snaps fence!</Text>
+      {
+      }
+    </View>
+  );
+};
+
+export default MockComponent;
