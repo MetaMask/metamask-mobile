da443e31c30e4141928611b79fb9ef2bd4614352
Jyoti Puri <jyotipuri@gmail.com>
2023-11-17 13:53:04 +0530

    Change in build file to enable blockaid in IOS build

--- scripts/build.sh
+++ scripts/build.sh
@@ -126,6 +126,7 @@ prebuild_ios(){
 	if [ "$PRE_RELEASE" = true ] ; then
 		echo "" > ios/debug.xcconfig
 		echo "" > ios/release.xcconfig
+		echo "MM_BLOCKAID_UI_ENABLED=true" > $IOS_ENV_FILE
 	fi
 	# Required to install mixpanel dep
 	git submodule update --init --recursive
