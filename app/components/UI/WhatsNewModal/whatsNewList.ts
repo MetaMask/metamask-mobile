2c67a1a3e844bcfce5d4f2abb353435bce6ca206
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2023-11-17 16:04:56 -0600

    feat: update whats new modal version to 7.12.0

--- app/components/UI/WhatsNewModal/whatsNewList.ts
+++ app/components/UI/WhatsNewModal/whatsNewList.ts
@@ -7,8 +7,8 @@ import { WhatsNew } from './types';
 export const whatsNew: WhatsNew = {
   // All users that have <6.4.0 and are updating to >=6.4.0 should see
   onlyUpdates: false, // false: Users who updated the app and new installs will see this. true: only users who update will see it
-  maxLastAppVersion: '7.3.0', // Only users who had a previous version <7.3.0 version will see this
-  minAppVersion: '7.3.0', // Only users who updated to a version >= 7.3.0 will see this
+  maxLastAppVersion: '7.12.0', // Only users who had a previous version <7.12.0 version will see this
+  minAppVersion: '7.12.0', // Only users who updated to a version >= 7.3.0 will see this
   /**
    * Slides utilizes a templating system in the form of a 2D array, which is eventually rendered within app/components/UI/WhatsNewModal/index.js.
    * The root layer determines the number of slides. Ex. To display 3 slides, the root layer should contain 3 arrays.
