9fe75f32fd604eaaa7d63c898fe9dfa93ad2b169
metamaskbot <metamaskbot@users.noreply.github.com>
2023-11-20 12:50:12 -0700

    checkout code location

--- scripts/create-cherry-pick-pr.sh
+++ scripts/create-cherry-pick-pr.sh
@@ -18,16 +18,17 @@ PR_BODY="This PR cherry-picks #${3}"
 git config user.name metamaskbot
 git config user.email metamaskbot@users.noreply.github.com
 
-git fetch
-
 git checkout "${BASE_PR_BRANCH_NAME}"
 git pull
 git checkout -b "${PR_BRANCH_NAME}"
 git cherry-pick "${COMMIT_HASH_TO_CHERRY_PICK}"
 
 git push --set-upstream origin "${PR_BRANCH_NAME}"
+git checkout fix/cherry-pick-node-version
 
-echo "${BASE_PR_BRANCH_NAME}"
-echo "${PR_BRANCH_NAME}"
-
-gh pr create --base "${BASE_PR_BRANCH_NAME}" --head "${PR_BRANCH_NAME}" --title "${PR_TITLE}" --body "${PR_BODY}"; 
+gh pr create \
+  --draft \
+  --title "${PR_TITLE}" \
+  --body "${PR_BODY}" \
+  --head "${PR_BRANCH_NAME}" \
+  --base "${BASE_PR_BRANCH_NAME}"
