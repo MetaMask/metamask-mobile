3ba70c4d083ac01d63cdfc06c4257f47fa808cc2
SamuelSalas <samuel.salas.reyes@gmail.com>
2023-11-24 13:08:40 -0600

    test: Fix permission-system-revoking-multiple-accounts test (#7904)
    ## **Description**
    The permission-system-revoking-multiple-accounts starting failing
    because of a change to the test dapp UI layout.
    permission-system-revoking-multiple-accounts should pass on both iOS and
    Android platforms.
    
    ## **Related issues**
    
    Fixes: #1383
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    Regression Test Run:
    https://app.bitrise.io/app/be69d4368ee7e86d/pipelines/b306139d-5996-4d24-bf38-89013e3faed4
    Smoke Test Run:
    https://app.bitrise.io/app/be69d4368ee7e86d/pipelines/2d45b7b7-29c3-42af-a2ad-aad655d956e8
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [ ] I've linked related issues
    - [ ] I've included manual testing steps
    - [ ] I've included screenshots/recordings if applicable
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    - [ ] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [ ] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- e2e/selectors/AccountListView.selectors.js
+++ e2e/selectors/AccountListView.selectors.js
@@ -1,7 +1,7 @@
 import messages from '../../locales/languages/en.json';
 
 // eslint-disable-next-line import/prefer-default-export
-export const AccountListViewSelectors = {
+export const AccountListViewSelectorsText = {
   REMOVE_IMPORTED_ACCOUNT: messages.accounts.yes_remove_it,
   IMPORT_ACCOUNT: messages.account_actions.import_account,
   CREATE_ACCOUNT: messages.account_actions.add_new_account,
