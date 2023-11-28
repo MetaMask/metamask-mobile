be67a6f23685685d4f0d2e70a57285ee8e6e18c8
Brian Nguyen <brianacnguyen@gmail.com>
2023-11-28 10:58:41 -0800

    Added SelectButton and SelectValue

---
+++ app/component-library/components/Select/SelectButton/SelectButton.stories.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable react/display-name */
+// Third party dependencies.
+import React from 'react';
+
+// External
+import { SAMPLE_AVATAR_PROPS } from '../../Avatars/Avatar/Avatar.constants';
+
+// Internal dependencies.
+import { default as SelectButtonComponent } from './SelectButton';
+import { SelectButtonSize } from './SelectButton.types';
+import { SAMPLE_SELECTBUTTON_PROPS } from './SelectButton.constants';
+
+const SelectButtonStoryMeta = {
+  title: 'Component Library / Select',
+  component: SelectButtonComponent,
+  argTypes: {
+    size: {
+      options: SelectButtonSize,
+      control: {
+        type: 'select',
+      },
+      defaultValue: SAMPLE_SELECTBUTTON_PROPS.size,
+    },
+    label: {
+      control: { type: 'text' },
+      defaultValue: SAMPLE_SELECTBUTTON_PROPS.label,
+    },
+    description: {
+      control: { type: 'text' },
+      defaultValue: SAMPLE_SELECTBUTTON_PROPS.description,
+    },
+    isDisabled: {
+      control: { type: 'boolean' },
+      defaultValue: SAMPLE_SELECTBUTTON_PROPS.isDisabled,
+    },
+    isDanger: {
+      control: { type: 'boolean' },
+      defaultValue: SAMPLE_SELECTBUTTON_PROPS.isDanger,
+    },
+  },
+};
+
+export default SelectButtonStoryMeta;
+
+export const SelectButton = {
+  render: (args: any) => (
+    <SelectButtonComponent
+      {...args}
+      iconProps={SAMPLE_SELECTBUTTON_PROPS.iconProps}
+    />
+  ),
+};
