004e603bd06956f1c498e36cee46f53962e81cb9
Brian Nguyen <brianacnguyen@gmail.com>
2023-12-07 11:58:39 -0800

    Updated scripts fox logo

--- scripts/inpage-bridge/inpage/webpack.config.js
+++ scripts/inpage-bridge/inpage/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { readFileSync } = require('fs');
 
 const SVG_LOGO_PATH =
-  '../../../app/component-library/components/Icons/Icon/assets/fox.svg';
+  '../../../app/images/fox.svg';
 function getBuildIcon() {
   const svg = readFileSync(SVG_LOGO_PATH, 'utf8');
   return `data:image/svg+xml,${encodeURIComponent(svg)}`;
