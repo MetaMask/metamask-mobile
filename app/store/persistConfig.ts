f8db9153912c8b95a7bb6c9d90932e576ae2cb22
Aslau Mario-Daniel <marioaslau@gmail.com>
2023-12-08 20:39:15 +0000

    feat: PR cleanup

--- app/store/persistConfig.ts
+++ app/store/persistConfig.ts
@@ -6,24 +6,11 @@ import Logger from '../util/Logger';
 
 const TIMEOUT = 40000;
 
-// const persistUserTransform = createTransform(
-//   // TODO: Add types for the 'user' slice
-//   (inboundState: any) => {
-//     const { initialScreen, isAuthChecked, ...state } = inboundState;
-//     // Reconstruct data to persist
-//     return state;
-//   },
-//   null,
-//   { whitelist: ['user'] },
-// );
-
 const persistConfig = {
   key: 'root',
   version,
-  // blacklist: ['engine'],
   whitelist: [],
   storage: MigratedStorage,
-  // transforms: [persistUserTransform],
   stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
   migrate: createMigrate(migrations, { debug: false }),
   timeout: TIMEOUT,
