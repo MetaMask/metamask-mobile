9e052d57a732d35102092c1f1194def20e1095e1
Vinicius Stevam <vinicius.stevam@consensys.net>
2023-12-11 07:02:00 +0000

    test postinstall

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 echo "PostInstall script:"
 
 echo "1. Build Inpage Bridge..."
-./scripts/build-inpage-bridge.sh
+# ./scripts/build-inpage-bridge.sh
 
 echo "2. React Native nodeify..."
 node_modules/.bin/rn-nodeify --install 'crypto,buffer,react-native-randombytes,vm,stream,http,https,os,url,net,fs' --hack
