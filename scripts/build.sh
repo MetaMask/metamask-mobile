67a2d7d35083ec85a9a601231a196a53ca6991b3
Vinicius Stevam <vinicius.stevam@consensys.net>
2023-12-15 09:23:42 +0000

    test remove of unnecessary tasks in gradle

--- scripts/build.sh
+++ scripts/build.sh
@@ -467,7 +467,7 @@ buildAndroidRunE2E(){
 	then
 		source $ANDROID_ENV_FILE
 	fi
-	cd android && ./gradlew assembleProdDebug app:assembleAndroidTest -DtestBuildType=debug --build-cache --parallel --configure-on-demand --no-daemon --max-workers 8 && cd ..
+	cd android && ./gradlew assembleProdDebug app:assembleAndroidTest -DtestBuildType=debug -x app:createBundleQaDebugJsAndAssets -x app:createBundleFlaskDebugJsAndAssets --build-cache --parallel --configure-on-demand --daemon && cd ..
 }
 
 buildIos() {
