38bcc0884c2790000f7aadbec5d4ad0f7ba50af4
Aslau Mario-Daniel <marioaslau@gmail.com>
2023-12-15 17:41:57 +0000

    feat: removed unit test test case as RTK is no longer implemented in engine

--- app/redux/slices/engine/engineReducer.test.tsx
+++ app/redux/slices/engine/engineReducer.test.tsx
@@ -3,19 +3,12 @@ import engineReducer from '.';
 import { createAction } from '@reduxjs/toolkit';
 
 // Create an action to initialize the background state
-export const initBgState = createAction('INIT_BG_STATE');
-
-// Create an action to update the background state
-export const updateBgState = createAction('UPDATE_BG_STATE', (key) => ({
-  payload: key,
-}));
+const initBgState = createAction('INIT_BG_STATE');
 
 jest.mock('../../../core/Engine', () => ({
   init: () => jest.requireActual('../../../core/Engine').default.init({}),
   state: {},
 }));
-// importing Engine after mocking to avoid global mock from overwriting its values
-import Engine from '../../../core/Engine';
 
 const backgroundState = {
   AccountTrackerController: {},
@@ -60,36 +53,5 @@ describe('engineReducer', () => {
     const initialState = { backgroundState };
     const nextState = engineReducer(initialState, initBgState());
     expect(nextState).toEqual({ backgroundState });
-  });
-
-  it('should update backgroundState when dispatching UPDATE_BG_STATE action', () => {
-    const reduxInitialState = {
-      backgroundState,
-    };
-
-    const key = 'AccountTrackerController';
-    // changing the mock version to suit this test manually due to our current global mock
-    (Engine as any).state = {
-      AccountTrackerController: { accounts: 'testValue' },
-    };
-    const { backgroundState: engineBackgroundState } = engineReducer(
-      reduxInitialState,
-      updateBgState({ key }),
-    );
-    console.log(
-      'Engine STATE: ',
-      Engine.state,
-      'Engine BKSTATE:',
-      engineBackgroundState,
-      'EQUAL:',
-      {
-        ...reduxInitialState.backgroundState,
-        AccountTrackerController: Engine.state[key],
-      },
-    );
-    expect(engineBackgroundState).toEqual({
-      ...reduxInitialState.backgroundState,
-      AccountTrackerController: Engine.state[key],
-    });
   });
 });
