f91fb5c095910bd2a512781c5ce63d70f5fb15e0
metamaskbot <metamaskbot@users.noreply.github.com>
2023-12-22 15:31:21 -0700

    added correct url and deeplink opening to switf action

--- ios/ActionExtension/ActionViewController.swift
+++ ios/ActionExtension/ActionViewController.swift
@@ -18,22 +18,21 @@ class ActionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
       
-      
-      if let item = extensionContext?.inputItems.first as? NSExtensionItem {
-          if let itemProvider = item.attachments?.first as? NSItemProvider {
-              if itemProvider.hasItemConformingToTypeIdentifier("public.url") {
-                  itemProvider.loadItem(forTypeIdentifier: "public.url", options: nil, completionHandler: { (url, error) in
-                    if let shareURL = url as? NSURL {
-                      // Now you have the URL
-                      print(shareURL.absoluteString ?? "")
-                      self.stringProperty = shareURL.absoluteString
-                    }
-                  })
-              }
-          }
-      }
-      
-      
+        //Find url selected
+        if let item = extensionContext?.inputItems.first as? NSExtensionItem {
+            if let itemProvider = item.attachments?.first as? NSItemProvider {
+                if itemProvider.hasItemConformingToTypeIdentifier("public.url") {
+                    itemProvider.loadItem(forTypeIdentifier: "public.url", options: nil, completionHandler: { (url, error) in
+                      if let shareURL = url as? NSURL {
+                        // Now you have the URL
+                        print(shareURL.absoluteString ?? "")
+                        self.stringProperty = shareURL.host
+                      }
+                    })
+                }
+            }
+        }
+        
         // Create a button
         let button = UIButton(type: .system) // Use .system to get a system-styled button
 
@@ -44,55 +43,35 @@ class ActionViewController: UIViewController {
         button.addTarget(self, action: #selector(self.buttonClicked), for: .touchUpInside)
 
         // Set the button's frame
-        button.frame = CGRect(x: 100, y: 100, width: 100, height: 50)
-
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
         // Add the button to the view
         self.view.addSubview(button)
-      
-        // Get the item[s] we're handling from the extension context.
-        
-        // For example, look for an image and place it into an image view.
-        // Replace this with something appropriate for the type[s] your extension supports.
-
-      
-        var imageFound = false
-        for item in self.extensionContext!.inputItems as! [NSExtensionItem] {
-            for provider in item.attachments! {
-                if provider.hasItemConformingToTypeIdentifier(UTType.image.identifier) {
-                    // This is an image. We'll load it, then place it in our image view.
-                    weak var weakImageView = self.imageView
-                    provider.loadItem(forTypeIdentifier: UTType.image.identifier, options: nil, completionHandler: { (imageURL, error) in
-                        OperationQueue.main.addOperation {
-                            if let strongImageView = weakImageView {
-                                if let imageURL = imageURL as? URL {
-                                    strongImageView.image = UIImage(data: try! Data(contentsOf: imageURL))
-                                }
-                            }
-                        }
-                    })
-                    
-                    imageFound = true
-                    break
-                }
-            }
-            
-            if (imageFound) {
-                // We only handle one image, so stop looking for more.
-                break
-            }
-        }
     }
 
-    @objc func buttonClicked() {
-        if let string = self.stringProperty {
-            print("Button clicked with string: \(string)")
-            //Create Deeplink
-//            let url = "dapp://" + string
-            self.extensionContext?.open(URL(string: "https://metamask.app.link/pancakeswap.finance/")! , completionHandler: nil)
-           self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+
+    @objc private func openURL(_ url: URL) -> Bool {
+        var responder: UIResponder? = self
+        while responder != nil {
+            if let application = responder as? UIApplication {
+                return application.perform(#selector(openURL(_:)), with: url) != nil
+            }
+            responder = responder?.next
         }
+        return false
     }
     
+    @objc func buttonClicked() {
+      if let string = self.stringProperty {
+        self.extensionContext?.completeRequest(returningItems: nil, completionHandler: { _ in
+          guard let url = URL(string: "https://metamask.app.link/" + string) else {
+            return
+          }
+          _ = self.openURL(url)
+        })
+      }
+    }
+      
+  
     @IBAction func done() {
       let appURL = NSURL(string: "metamask://")
 
