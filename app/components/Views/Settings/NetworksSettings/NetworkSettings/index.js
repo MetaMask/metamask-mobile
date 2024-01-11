e668d93c6d8c56a33973ace443be48c845248004
Salimtb <salim.toubal@consensys.net>
2024-01-11 10:12:54 +0100

    fix: fix inconsistency in the popular tab

--- app/components/Views/Settings/NetworksSettings/NetworkSettings/index.js
+++ app/components/Views/Settings/NetworksSettings/NetworkSettings/index.js
@@ -1093,7 +1093,7 @@ class NetworkSettings extends PureComponent {
               }}
             >
               <View
-                tabLabel={strings('app_settings.popular').toUpperCase()}
+                tabLabel={strings('app_settings.popular')}
                 key={AppConstants.ADD_CUSTOM_NETWORK_POPULAR_TAB_ID}
                 style={styles.networksWrapper}
                 testID={POPULAR_NETWORKS_TAB_ID}
