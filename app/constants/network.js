227d591f2a99ef8ff21eebaf4586b714beaef9da
Jyoti Puri <jyotipuri@gmail.com>
2024-01-16 10:40:48 +0530

    update

--- app/constants/network.js
+++ app/constants/network.js
@@ -36,8 +36,9 @@ export const NETWORKS_CHAIN_ID = {
 export const NETWORK_NAMES = {
   [NETWORKS_CHAIN_ID.MAINNET]: 'Ethereum Mainnet',
   [NETWORKS_CHAIN_ID.BSC]: 'Binance Smart Chain',
+  [NETWORKS_CHAIN_ID.OPTIMISM]: 'Optimism',
   [NETWORKS_CHAIN_ID.POLYGON]: 'Polygon',
-  [NETWORKS_CHAIN_ID.AVAXCCHAIN]: 'Avalanche',
-  [NETWORKS_CHAIN_ID.CELO]: 'Celo',
-  [NETWORKS_CHAIN_ID.FANTOM]: 'Fantom',
+  [NETWORKS_CHAIN_ID.AVALANCHE]: 'Avalance',
+  [NETWORKS_CHAIN_ID.ARBITRUM]: 'Arbitrum',
+  [NETWORKS_CHAIN_ID.LINEA_MAINNET]: 'Linea',
 };
