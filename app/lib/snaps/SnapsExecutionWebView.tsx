599201ab09d21ab4de6183327c1839192540b8e0
JSoufer <jonathan.ferreira@consensys.net>
2024-01-22 14:43:18 +0000

    chore: types, plataform, and error enhancements

--- app/lib/snaps/SnapsExecutionWebView.tsx
+++ app/lib/snaps/SnapsExecutionWebView.tsx
@@ -1,15 +1,28 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable import/no-commonjs */
 ///: BEGIN:ONLY_INCLUDE_IF(snaps)
-import React, { Component, RefObject, useRef, useState } from 'react';
-import { View, ScrollView } from 'react-native';
+import React, { Component, RefObject } from 'react';
+import { View, ScrollView, Platform } from 'react-native';
 import WebView, { WebViewMessageEvent } from 'react-native-webview';
 import { createStyles } from './styles';
 
-const ENV_URI = 'http://localhost:6363';
+const sourceUri =
+  (Platform.OS === 'android' ? 'file:///android_asset/' : '') +
+  'Web.bundle/index.html';
+
+let EE = sourceUri;
+let attempts = 0;
 
 const styles = createStyles();
 
+interface SnapsExecutionWebViewProps {
+  injectJavaScript: (js: string) => void;
+  addEventListener: (_event: any, listener: any) => void;
+  removeEventListener: (_event: any, _listener: any) => void;
+}
 // This is a hack to allow us to asynchronously await the creation of the WebView.
-let resolveGetWebView;
+let resolveGetWebView: (arg0: SnapsExecutionWebViewProps) => void;
 export const getSnapsWebViewPromise = new Promise((resolve) => {
   resolveGetWebView = resolve;
 });
@@ -29,18 +42,27 @@ export class SnapsExecutionWebView extends Component {
 
   onWebViewLoad() {
     const api = {
-      injectJavaScript: (js) => {
-        this.webViewRef.injectJavaScript(js);
+      injectJavaScript: (js: string) => {
+        this.webViewRef?.injectJavaScript(js);
       },
-      addEventListener: (_event, listener) => {
+      addEventListener: (_event: any, listener: any) => {
         this.listener = listener;
       },
-      removeEventListener: (_event, _listener) => {
+      removeEventListener: (_event: any, _listener: any) => {
         this.listener = null;
       },
     };
 
     resolveGetWebView(api);
+  }
+
+  onWebViewError() {
+    attempts++;
+    if (attempts < 2) {
+      // TODO: This should point to a AWS S3 bucket as a fallback.
+      EE = 'https://jonathansoufer.github.io/';
+      this.onWebViewLoad();
+    }
   }
 
   onWebViewMessage(data: WebViewMessageEvent) {
@@ -55,11 +77,9 @@ export class SnapsExecutionWebView extends Component {
         <View style={styles.webview}>
           <WebView
             ref={this.setWebViewRef}
-            source={{
-              uri: ENV_URI,
-            }}
+            source={{ uri: EE }}
             onMessage={this.onWebViewMessage}
-            // TODO: Handle load error
+            onError={this.onWebViewError}
             onLoadEnd={this.onWebViewLoad}
             // TODO: This should probably change
             originWhitelist={['*']}
