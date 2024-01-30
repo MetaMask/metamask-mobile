8674b2846ebd6d71e8c417a4379ccdc2a71a453e
Cal Leung <cleun007@gmail.com>
2024-01-29 17:25:06 -0800

    Clean up iOS script

--- scripts/ios/bundle-js-and-sentry-upload.sh
+++ scripts/ios/bundle-js-and-sentry-upload.sh
@@ -33,6 +33,6 @@ BUNDLE_REACT_NATIVE="/bin/sh $SENTRY_XCODE $REACT_NATIVE_XCODE"
 
 # Upload Sentry debug symbols
 if [ "$SENTRY_DISABLE_AUTO_UPLOAD" == "false" ]; then
-    [ $SENTRY_INCLUDE_NATIVE_SOURCES == "true" ] && INCLUDE_SOURCES_FLAG="--include-sources" || INCLUDE_SOURCES_FLAG=""
+    [ "$SENTRY_INCLUDE_NATIVE_SOURCES" == "true" ] && INCLUDE_SOURCES_FLAG="--include-sources" || INCLUDE_SOURCES_FLAG=""
     ../node_modules/@sentry/cli/bin/sentry-cli debug-files upload "$INCLUDE_SOURCES_FLAG" "$DWARF_DSYM_FOLDER_PATH"
 fi
