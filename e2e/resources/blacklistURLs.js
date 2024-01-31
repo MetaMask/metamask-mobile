d0514ffaddc1638eea4d19eff8d7fe9d63ff81c8
CW <chris.wilcox@consensys.net>
2024-01-30 21:23:11 -0800

    blacklist URLs

---
+++ e2e/resources/blacklistURLs.js
@@ -0,0 +1,7 @@
+const blacklistURLs = [
+  'https://mainnet.infura.io/v3/d039103314584a379e33c21fbe89b6cb',
+  'https://cdn.branch.io/sdk/uriskiplist_v0.json',
+  'https://api2.branch.io/v1/install',
+];
+
+export default blacklistURLs;
