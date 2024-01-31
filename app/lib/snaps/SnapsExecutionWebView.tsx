3b2f4679c764805cd4f6f3eb29decadc549f2e62
JSoufer <jonathan.ferreira@consensys.net>
2024-01-31 10:12:01 +0000

    chore: refactor listener methods + URL

--- app/lib/snaps/SnapsExecutionWebView.tsx
+++ app/lib/snaps/SnapsExecutionWebView.tsx
@@ -3,26 +3,26 @@
 /* eslint-disable import/no-commonjs */
 ///: BEGIN:ONLY_INCLUDE_IF(snaps)
 import React, { Component, RefObject } from 'react';
-import { View, ScrollView, Platform, NativeSyntheticEvent } from 'react-native';
+import { View, ScrollView, NativeSyntheticEvent } from 'react-native';
 import WebView, { WebViewMessageEvent } from 'react-native-webview';
 import { createStyles } from './styles';
 import { WebViewInterface } from '@metamask/snaps-controllers/dist/types/services/webview/WebViewMessageStream';
 import { WebViewError } from 'react-native-webview/lib/WebViewTypes';
-
-const sourceUri =
-  (Platform.OS === 'android' ? 'file:///android_asset/' : '') +
-  'web.bundle/index.html';
+import { PostMessageEvent } from '@metamask/post-message-stream';
 
 const styles = createStyles();
 
 interface SnapsExecutionWebViewProps {
-  injectJavaScript: (js: string) => void;
-  addEventListener: (_event: any, listener: any) => void;
-  removeEventListener: (_event: any, _listener: any) => void;
+  injectJavaScript(js: string): void;
+  registerMessageListener(listener: (event: PostMessageEvent) => void): void;
+  unregisterMessageListener(listener: (event: PostMessageEvent) => void): void;
 }
 // This is a hack to allow us to asynchronously await the creation of the WebView.
 let resolveGetWebView: (arg0: SnapsExecutionWebViewProps) => void;
 let rejectGetWebView: (error: NativeSyntheticEvent<WebViewError>) => void;
+
+// TODO: This should be a config value pointing to the AWS S3 bucket URL.
+const SNAPS_EE_URL = 'http://localhost:6363';
 
 export const getSnapsWebViewPromise = new Promise<WebViewInterface>(
   (resolve, reject) => {
@@ -33,7 +33,7 @@ export const getSnapsWebViewPromise = new Promise<WebViewInterface>(
 
 // This is a class component because storing the references we are don't work in functional components.
 export class SnapsExecutionWebView extends Component {
-  webViewRef: RefObject<WebView> | null = null;
+  webViewRef: RefObject<WebView> | any = null;
   listener: any = null;
 
   constructor(props: any) {
@@ -49,10 +49,14 @@ export class SnapsExecutionWebView extends Component {
       injectJavaScript: (js: string) => {
         this.webViewRef?.injectJavaScript(js);
       },
-      addEventListener: (_event: any, listener: any) => {
+      registerMessageListener: (
+        listener: (event: PostMessageEvent) => void,
+      ) => {
         this.listener = listener;
       },
-      removeEventListener: (_event: any, _listener: any) => {
+      unregisterMessageListener: (
+        _listener: (event: PostMessageEvent) => void,
+      ) => {
         this.listener = null;
       },
     };
@@ -78,7 +82,7 @@ export class SnapsExecutionWebView extends Component {
             ref={
               this.setWebViewRef as unknown as React.RefObject<WebView> | null
             }
-            source={{ uri: sourceUri }}
+            source={{ uri: SNAPS_EE_URL }}
             onMessage={this.onWebViewMessage}
             onError={this.onWebViewError}
             onLoadEnd={this.onWebViewLoad}
