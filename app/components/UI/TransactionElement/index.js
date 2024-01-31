8833ecf2dec27e053da2d424990ee33465159989
OGPoyraz <omergoktugpoyraz@gmail.com>
2024-01-31 13:53:32 +0100

    fix import

--- app/components/UI/TransactionElement/index.js
+++ app/components/UI/TransactionElement/index.js
@@ -25,8 +25,7 @@ import DetailsModal from '../../Base/DetailsModal';
 import { isMainNet } from '../../../util/networks';
 import { weiHexToGweiDec } from '@metamask/controller-utils';
 import { WalletDevice } from '@metamask/transaction-controller';
-// TODO: Update after this function has been exported from the package
-import { isEIP1559Transaction } from '@metamask/transaction-controller/dist/utils';
+import { isEIP1559Transaction } from '@metamask/transaction-controller';
 import { ThemeContext, mockTheme } from '../../../util/theme';
 import {
   selectChainId,
