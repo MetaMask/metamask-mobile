d35c6f2c28695f8505964c0156283eb519522be9
dawnseeker8 <dawnseeker8@gmail.com>
2024-02-05 21:23:34 +0800

    feat: Add paddingHorizontal and remove height in the LedgerTransactionModal

--- app/components/UI/LedgerModals/styles.ts
+++ app/components/UI/LedgerModals/styles.ts
@@ -7,11 +7,11 @@ export const createStyles = (colors: Colors) =>
     modal: {
       justifyContent: 'flex-end',
       margin: 0,
-      height: 600,
       zIndex: 1000,
     },
     contentWrapper: {
       zIndex: 1000,
+      paddingHorizontal: 8,
       paddingBottom: 32,
       borderRadius: 20,
       backgroundColor: colors.background.default,
