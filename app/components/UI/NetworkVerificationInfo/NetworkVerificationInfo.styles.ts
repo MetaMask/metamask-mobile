c3d0d35b0689914016ec90ad0e01216c5926cab0
salimtb <salim.toubal@outlook.com>
2024-02-06 19:14:03 +0100

    feat: add privacy toggle (#8234)
    ## **Description**
    
    This task aims to add the ability to turn on and off the network
    verification feature. To do that, we will be adding both a toggleable
    section in the settings as well as a contextual banner and sheet for
    quickly toggling it on.
    
    ## **Related issues**
    
    Fixes: [#1269](https://github.com/MetaMask/mobile-planning/issues/1269)
    
    ## **Manual testing steps**
    
    1. Go to the setting page
    2. Go to Security and Privacy
    3. Enable/disable the network details check
    4. Go to add network
    5. if the toggle was disabled you should see a banner ( check the video
    below )
    
    ## **Screenshots/Recordings**
    
    
    ### **Before**
    
    ### **After**
    
    
    https://drive.google.com/drive/folders/1BeTFKHelGrzu5XPUfXLTa-om7inhtowU?usp=drive_link
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [x] I've linked related issues
    - [x] I've included manual testing steps
    - [x] I've included screenshots/recordings if applicable
    - [x] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    - [x] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [x] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/UI/NetworkVerificationInfo/NetworkVerificationInfo.styles.ts
+++ app/components/UI/NetworkVerificationInfo/NetworkVerificationInfo.styles.ts
@@ -1,10 +1,10 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TextStyle } from 'react-native';
 // External dependencies.
 import { Theme } from '../../../util/theme/models';
 
 const styleSheet = (params: { theme: Theme }) => {
   const { theme } = params;
-  const { colors } = theme;
+  const { colors, typography } = theme;
 
   return StyleSheet.create({
     root: {
@@ -22,7 +22,36 @@ const styleSheet = (params: { theme: Theme }) => {
     textSection: {
       marginBottom: 8,
     },
-
+    alertBar: {
+      width: '100%',
+      marginBottom: 15,
+    },
+    title: {
+      textAlign: 'center',
+      marginVertical: 10,
+      paddingHorizontal: 16,
+      ...typography.lBodyMDBold,
+    } as TextStyle,
+    bottomSpace: {
+      marginBottom: 10,
+    },
+    textContainer: {
+      paddingHorizontal: 16,
+      marginVertical: 10,
+    },
+    warningContainer: {
+      marginTop: 20,
+      marginHorizontal: 4,
+    },
+    errorContinue: {
+      marginVertical: 16,
+    },
+    textCentred: {
+      textAlign: 'center',
+    },
+    boldText: {
+      ...typography.sBodyMDBold,
+    } as TextStyle,
     networkSection: { marginBottom: 16 },
     nestedScrollContent: { paddingBottom: 24 },
   });
