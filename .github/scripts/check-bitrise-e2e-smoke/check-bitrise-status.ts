1fe01fd3195b72adcdc32dbe10b7a3d881d84f56
Cal Leung <cleun007@gmail.com>
2024-02-06 17:09:45 -0800

    Fix check-bitrise-status script

--- .github/scripts/check-bitrise-e2e-smoke/check-bitrise-status.ts
+++ .github/scripts/check-bitrise-e2e-smoke/check-bitrise-status.ts
@@ -10,7 +10,6 @@ main().catch((error: Error): void => {
 async function main(): Promise<void> {
   const githubToken = process.env.GITHUB_TOKEN;
   const e2eLabel = process.env.E2E_LABEL;
-  const pullRequestLink = `https://github.com/MetaMask/metamask-mobile/pull/${context.issue.number}`;
 
   if (!githubToken) {
     core.setFailed('GITHUB_TOKEN not found');
@@ -33,54 +32,15 @@ async function main(): Promise<void> {
     return;
   }
 
-//   console.log(`Workflow triggered by the event (${triggerAction})`);
+  // Check if the "Bitrise build passed!" comment is posted
+  const { data: comments } = await octokit.rest.issues.listComments({ owner, repo, issue_number });
+  const isBitriseSuccessStatus = comments.some(comment => {
+    return (comment.body || '').trim() === "Bitrise build passed!"
+  });
 
-//   switch (triggerAction) {
-//     case PullRequestTriggerType.ReadyForReview:
-//       shouldTriggerE2E = true;
-//       console.log(
-//         `Starting E2E smoke since PR has changed to ready for review.`,
-//       );
-//       break;
-//     case PullRequestTriggerType.Labeled:
-//       shouldTriggerE2E = e2eLabel === context.payload.label.name;
-//       if (!shouldTriggerE2E) {
-//         console.log(
-//           `Skipping E2E smoke since (${e2eLabel}) is not the label that triggered this workflow.`,
-//         );
-//       } else {
-//         console.log(
-//           `Starting E2E smoke since (${e2eLabel}) is the label that triggered this workflow.`,
-//         );
-//       }
-//       break;
-//   }
-
-  if (shouldTriggerE2E) {
-    // Apply the E2E smoke label
-    const { owner, repo, number: issue_number } = context.issue;
-    const octokit: InstanceType<typeof GitHub> = getOctokit(githubToken);
-    try {
-      const applyLabelResponse = await octokit.rest.issues.addLabels({
-        owner,
-        repo,
-        issue_number,
-        labels: [e2eLabel],
-      });
-      if (applyLabelResponse.status === 200) {
-        console.log(`Applied (${e2eLabel}) label to PR ${pullRequestLink}`);
-      } else {
-        core.setFailed(
-          `Failed to apply (${e2eLabel}) label to ${pullRequestLink}`,
-        );
-        process.exit(1);
-      }
-    } catch (error) {
-      core.setFailed(`Error occured when applying label: ${error}`);
-      process.exit(1);
-    }
+  if (!isBitriseSuccessStatus) {
+    core.setFailed("Bitrise has not passed yet.");
   }
 
-  // Set the output for the next step to use.
-  core.setOutput("shouldTriggerE2E", shouldTriggerE2E);
+  console.log("Bitrise build has passed!")
 }
