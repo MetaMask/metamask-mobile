bb0eb6f61ffe899a2b13da744fc9c5f71e459d9b
CW <chris.wilcox@consensys.net>
2024-02-08 13:05:12 -0800

    comment on flaky URL not consistently being blocked

--- e2e/resources/blacklistURLs.js
+++ e2e/resources/blacklistURLs.js
@@ -1,7 +1,7 @@
 const blacklistURLs = [
   '.*infura.io/*',
   '.*cdn.branch.io/*',
-  '.*api2.branch.io/*',
+  '.*api2.branch.io/*', //this url is not consistently being blocked
   '.*api.etherscan.io/*',
   '.*static.metafi.codefi.network/*',
 ];
