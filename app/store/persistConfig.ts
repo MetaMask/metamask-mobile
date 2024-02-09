12d9ee407ebe99e073f0d706ce3bd7c1365339ed
tommasini <tommasini15@gmail.com>
2024-02-09 03:42:43 +0000

    fix with custom state reconcilier

--- app/store/persistConfig.ts
+++ app/store/persistConfig.ts
@@ -3,6 +3,7 @@ import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import MigratedStorage from '../redux/storage/MigratedStorage';
 import { migrations, version } from './migrations';
 import Logger from '../util/Logger';
+import { merge } from 'lodash';
 
 const TIMEOUT = 40000;
 
@@ -26,6 +27,7 @@ interface PersistConfig {
   migrate?: any;
   timeout?: number;
   writeFailHandler?: (error: Error) => void;
+  customStateReconcilier?: any;
 }
 
 const createPersistConfig = ({
@@ -35,13 +37,14 @@ const createPersistConfig = ({
   migrate,
   timeout,
   writeFailHandler,
+  customStateReconcilier,
 }: PersistConfig) => ({
   key,
   version,
   blacklist,
   storage: MigratedStorage(key),
   transforms,
-  stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
+  stateReconciler: customStateReconcilier ?? autoMergeLevel2, // see "Merge Process" section for details.
   migrate,
   timeout,
   writeFailHandler,
@@ -55,6 +58,8 @@ export const rootPersistConfig = createPersistConfig({
   timeout: TIMEOUT,
   writeFailHandler: (error: Error) =>
     Logger.error(error, { message: 'Error persisting data' }), // Log error if saving state fails
+  customStateReconcilier: (inboundState: any, originalState: any) =>
+    merge({}, originalState, inboundState),
 });
 
 export default createPersistConfig;
