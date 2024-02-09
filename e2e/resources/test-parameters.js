5fcc8888c3ba7d5426c97dcffb74110894f9a022
CW <chris.wilcox@consensys.net>
2024-02-09 13:10:52 -0800

    add JSDocs

--- e2e/resources/test-parameters.js
+++ e2e/resources/test-parameters.js
@@ -1,3 +1,7 @@
+/**
+ * An array of endpoint patterns to be blacklisted.
+ * @type {string[]}
+ */
 const blacklistEndPoints = [
   '.*infura.io/*',
   '.*cdn.branch.io/*',
@@ -7,6 +11,14 @@ const blacklistEndPoints = [
   '.*rpc.tenderly.co/*',
 ];
 
+/**
+ * A string representation of the blacklisted endpoint patterns,
+ * formatted for use in a regular expression.
+ * @type {string}
+ */
 const blacklistURLs = `\\("${blacklistEndPoints.join('","')}"\\)`;
 
+/**
+ * Export the blacklistURLs string.
+ */
 export default blacklistURLs;
