01a3e1bf788a682065e883a15c187765962efbbe
CW <chris.wilcox@consensys.net>
2024-02-09 19:47:19 -0800

    remove setTimeout

--- e2e/init.js
+++ e2e/init.js
@@ -1,7 +1,6 @@
 import blacklistURLs from './resources/blacklistURLs';
 
 beforeAll(async () => {
-  jest.setTimeout(150000);
   device.appLaunchArgs.modify({
     detoxURLBlacklistRegex: blacklistURLs,
   });
