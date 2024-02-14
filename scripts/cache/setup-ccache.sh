ff767f67f445da433d641760f35af37bed42a8d3
Vinicius Stevam <vinicius.stevam@consensys.net>
2024-02-14 12:03:16 +0000

    ccache config-path only for ios

--- scripts/cache/setup-ccache.sh
+++ scripts/cache/setup-ccache.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+# Get the operating system name
+os_name=$(uname -s)
+echo "Operating system: $os_name"
 
+# Check if the OS is macOS
+if [ "$os_name" == "Darwin" ]; then
+  # Set up ccache configuration for iOS
+  ccache --config-path "$(pwd)/ccache.conf"
+fi
 # Set up ccache configuration
-ccache --config-path "$(pwd)/ccache.conf"
 ccache --set-config cache_dir="$(pwd)/ccache"
 
 # Source ccache configuration
