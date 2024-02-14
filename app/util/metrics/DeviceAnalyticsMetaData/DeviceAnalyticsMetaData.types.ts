78fb17d1bee3d77101a2d23f0092e63d7b1c8f9b
Nicolas MASSART <nicolas.massart@consensys.net>
2024-02-14 10:04:24 +0100

    remove dead code

--- app/util/metrics/DeviceAnalyticsMetaData/DeviceAnalyticsMetaData.types.ts
+++ app/util/metrics/DeviceAnalyticsMetaData/DeviceAnalyticsMetaData.types.ts
@@ -1,13 +1,5 @@
 import { JsonMap } from '@segment/analytics-react-native';
 
-// export interface DeviceMetaData extends JsonMap {
-//   platform: string;
-//   currentBuildNumber: string;
-//   applicationVersion: string;
-//   operatingSystemVersion: string;
-//   deviceBrand: string;
-// }
-
 export type DeviceMetaData = JsonMap & {
   platform: string;
   currentBuildNumber: string;
