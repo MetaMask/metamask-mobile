cd698b6197cf6cbe261c119453d2fd45d3987291
Vinicius Stevam <45455812+vinistevam@users.noreply.github.com>
2024-02-15 09:00:01 +0000

    fix: ensure edit nonce input only accepts numbers (#8227)
    ## **Description**
    
    This PR ensures the input field for edit nonce only accepts numbers.
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-mobile/issues/6857

--- app/components/UI/CustomNonceModal/index.test.tsx
+++ app/components/UI/CustomNonceModal/index.test.tsx
@@ -3,9 +3,9 @@ import { shallow } from 'enzyme';
 import CustomNonceModal from './';
 
 describe('CustomNonceModal', () => {
+  const proposedNonce = 26;
+  const customNonce = 28;
   it('should render correctly', () => {
-    const proposedNonce = 26;
-    const customNonce = 28;
     const noop = () => ({});
     const wrapper = shallow(
       <CustomNonceModal
@@ -16,5 +16,23 @@ describe('CustomNonceModal', () => {
       />,
     );
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should handle only numeric inputs', () => {
+    const saveMock = jest.fn();
+    const closeMock = jest.fn();
+    const wrapper = shallow(
+      <CustomNonceModal
+        save={saveMock}
+        close={closeMock}
+        proposedNonce={proposedNonce}
+        nonceValue={proposedNonce}
+      />,
+    );
+    const nonceTextInput = wrapper.find('TextInput');
+    nonceTextInput.simulate('changeText', '30c');
+    expect(wrapper.find('TextInput').prop('value')).toBe(String(proposedNonce));
+    nonceTextInput.simulate('changeText', '30');
+    expect(wrapper.find('TextInput').prop('value')).toBe('30');
   });
 });
