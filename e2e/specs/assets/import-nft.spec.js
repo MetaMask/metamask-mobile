a3c2382dbc1f476c73f88a91c6b0fab257adc9e0
sahar-fehri <sahar.fehri@consensys.net>
2024-02-16 11:57:27 +0100

    fix: fix e2e

--- e2e/specs/assets/import-nft.spec.js
+++ e2e/specs/assets/import-nft.spec.js
@@ -5,8 +5,13 @@ import WalletView from '../../pages/WalletView';
 import AddCustomTokenView from '../../pages/AddCustomTokenView';
 import NetworkListModal from '../../pages/modals/NetworkListModal';
 import NetworkEducationModal from '../../pages/modals/NetworkEducationModal';
-import { importWalletWithRecoveryPhrase } from '../../viewHelper';
-import Collectibles from '../../resources/collectibles.json';
+import { importWalletWithRecoveryPhrase, loginToApp } from '../../viewHelper';
+import {
+  withFixtures,
+  defaultGanacheOptions,
+} from '../../fixtures/fixture-helper';
+import { SMART_CONTRACTS } from '../../../app/util/test/smart-contracts';
+import FixtureBuilder from '../../fixtures/fixture-builder';
 
 describe(SmokeCore('Import NFT'), () => {
   const SEPOLIA = 'Sepolia Test Network';
@@ -36,29 +41,46 @@ describe(SmokeCore('Import NFT'), () => {
   });
 
   it('should add a collectible', async () => {
-    await WalletView.isVisible();
-    // Tap on COLLECTIBLES tab
-    await WalletView.tapNftTab();
-    await WalletView.scrollDownOnNFTsTab();
-    // Tap on the add collectibles button
-    await WalletView.tapImportNFTButton();
-    await AddCustomTokenView.isVisible();
-    // Input incorrect contract address
-    await AddCustomTokenView.typeInNFTAddress('1234');
-    await AddCustomTokenView.typeInNFTIdentifier('');
-    await AddCustomTokenView.isNFTAddressWarningVisible();
-    await AddCustomTokenView.tapBackButton();
-    await WalletView.tapImportNFTButton();
-    await AddCustomTokenView.isVisible();
-    await AddCustomTokenView.typeInNFTAddress(Collectibles.erc1155tokenAddress);
-    await AddCustomTokenView.typeInNFTIdentifier(Collectibles.erc1155tokenID);
-    await WalletView.isVisible();
-    // Wait for asset to load
-    await TestHelpers.delay(3000);
-    await WalletView.isNFTVisibleInWallet(Collectibles.erc1155collectionName);
-    // Tap on Collectible
-    await WalletView.tapOnNFTInWallet(Collectibles.erc1155collectionName);
-    await WalletView.isNFTNameVisible(Collectibles.erc1155tokenName);
-    await WalletView.scrollUpOnNFTsTab();
+    const SMART_CONTRACT = SMART_CONTRACTS.NFTS;
+    await withFixtures(
+      {
+        dapp: true,
+        fixture: new FixtureBuilder()
+          .withGanacheNetwork()
+          .withPermissionControllerConnectedToTestDapp()
+          .build(),
+        restartDevice: true,
+        ganacheOptions: defaultGanacheOptions,
+        smartContract: SMART_CONTRACT,
+      },
+      async ({ contractRegistry }) => {
+        const erc1155ContractAddress =
+          await contractRegistry.getContractAddress(SMART_CONTRACT);
+        await loginToApp();
+        // Tap on COLLECTIBLES tab
+        await WalletView.tapNftTab();
+        await WalletView.scrollDownOnNFTsTab();
+        // Tap on the add collectibles button
+        await WalletView.tapImportNFTButton();
+        await AddCustomTokenView.isVisible();
+        // Input incorrect contract address
+        await AddCustomTokenView.typeInNFTAddress('1234');
+        await AddCustomTokenView.typeInNFTIdentifier('');
+        await AddCustomTokenView.isNFTAddressWarningVisible();
+        await AddCustomTokenView.tapBackButton();
+        await WalletView.tapImportNFTButton();
+        await AddCustomTokenView.isVisible();
+        await AddCustomTokenView.typeInNFTAddress(erc1155ContractAddress);
+        await AddCustomTokenView.typeInNFTIdentifier('1');
+        await WalletView.isVisible();
+        // Wait for asset to load
+        await TestHelpers.delay(3000);
+        await WalletView.isNFTVisibleInWallet('TestDappNFTs');
+        // Tap on Collectible
+        await WalletView.tapOnNFTInWallet('TestDappNFTs');
+        await WalletView.isNFTNameVisible('TestDappNFTs #1');
+        await WalletView.scrollUpOnNFTsTab();
+      },
+    );
   });
 });
