116829bd920db4435e3a6bc4c5847c8d30e607bb
Curtis David <Curtis.David7@gmail.com>
2024-02-21 15:58:43 -0500

    test: Refactor settings page object (#8644)
    ## **Description**
    
    The purpose of this PR is to continue on with our work to refactor page
    objects in our e2e test code.
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    Smoke on main:
    https://app.bitrise.io/app/be69d4368ee7e86d/pipelines/53f88ce9-0360-494e-84aa-4c9798983ad7
    Regression on main:
    https://app.bitrise.io/app/be69d4368ee7e86d/pipelines/27b6e69b-1f15-40be-b5a4-2d4f07f6ac82
    
    ### **After**
    Smoke:
    https://app.bitrise.io/app/be69d4368ee7e86d/pipelines/39c38a2b-cdfd-4993-bdf3-14b3de741386
    Regression:
    https://app.bitrise.io/app/be69d4368ee7e86d/pipelines/d3903f4e-7f87-48d1-97cd-5eba0fb404d1
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [ ] I've linked related issues
    - [ ] I've included manual testing steps
    - [ ] I've included screenshots/recordings if applicable
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    - [ ] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [ ] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- e2e/selectors/Settings/SettingsView.selectors.js
+++ e2e/selectors/Settings/SettingsView.selectors.js
@@ -4,3 +4,18 @@ import messages from '../../../locales/languages/en.json';
 export const SettingsViewSelectorsText = {
   ADVANCE_TITLE_TEXT: messages.app_settings.advanced_title,
 };
+
+export const SettingsViewSelectorsIDs = {
+  SETTINGS_SCROLL_ID: 'settings-scroll',
+  GENERAL: 'general-settings',
+  SECURITY: 'security-settings',
+  ADVANCED: 'advanced-settings',
+  CONTACTS: 'contacts-settings',
+  NETWORKS: 'networks-settings',
+  ON_RAMP: 'on-ramp-settings',
+  EXPERIMENTAL: 'experimental-settings',
+  ABOUT_METAMASK: 'about-metamask-settings',
+  REQUEST: 'request-settings',
+  CONTACT: 'contact-settings',
+  LOCK: 'lock-settings',
+};
