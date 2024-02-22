24e13b98b37eb682076efce9ea4aa43de6620424
Cal Leung <cleun007@gmail.com>
2024-02-22 00:38:50 -0800

    Change comment trigger to edit

--- .github/scripts/check-bitrise-e2e-smoke/check-bitrise-status.ts
+++ .github/scripts/check-bitrise-e2e-smoke/check-bitrise-status.ts
@@ -45,9 +45,7 @@ async function main(): Promise<void> {
     issue_number,
   });
   const isBitriseSuccessStatus = comments.some((comment) => {
-    const includesPass = comment.body?.includes(
-      'Bitrise E2E smoke tests passed',
-    );
+    const includesPass = comment.body?.includes('pipeline passed on Bitrise');
     return includesPass;
   });
 
