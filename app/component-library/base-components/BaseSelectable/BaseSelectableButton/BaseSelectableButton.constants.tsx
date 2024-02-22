2464ed9b44f06f12762979f3534123d66885cc8a
Brian Nguyen <brianacnguyen@gmail.com>
2024-02-22 09:46:30 -0800

    Added BaseSelectableButton

---
+++ app/component-library/base-components/BaseSelectable/BaseSelectableButton/BaseSelectableButton.constants.tsx
@@ -0,0 +1,6 @@
+// Defaults
+export const DEFAULT_BASESELECTABLEBUTTON_PLACEHOLDER_STRING = 'Select an item';
+
+// Test IDs
+export const BASESELECTABLEBUTTON_CARETICON_TESTID =
+  'BaseSelectableButton-careticon';
