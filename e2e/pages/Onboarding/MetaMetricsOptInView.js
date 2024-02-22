067233a1b618efb96b6a50073f8ec3967e2bf9ac
CW <chris.wilcox@consensys.net>
2024-02-21 16:07:04 -0800

    update selectors for onboarding

--- e2e/pages/Onboarding/MetaMetricsOptInView.js
+++ e2e/pages/Onboarding/MetaMetricsOptInView.js
@@ -1,29 +1,30 @@
-import {
-  OPTIN_METRICS_I_AGREE_BUTTON_ID,
-  OPTIN_METRICS_NO_THANKS_BUTTON_ID,
-  OPTIN_METRICS_PRIVACY_POLICY_DESCRIPTION_CONTENT_1_ID,
-  METAMETRICS_OPT_IN_CONTAINER_ID,
-} from '../../../wdio/screen-objects/testIDs/Screens/OptinMetricsScreen.testIds';
+import { MetaMetricsOptInSelectorsIDs } from '../../selectors/Onboarding/MetaMetricsOptIn.selectors';
 import Matchers from '../../utils/Matchers';
 import Gestures from '../../utils/Gestures';
 
 class MetaMetricsOptIn {
   get container() {
-    return Matchers.getElementByID(METAMETRICS_OPT_IN_CONTAINER_ID);
+    return Matchers.getElementByID(
+      MetaMetricsOptInSelectorsIDs.METAMETRICS_OPT_IN_CONTAINER_ID,
+    );
   }
 
   get optInMetricsContent() {
     return Matchers.getElementByID(
-      OPTIN_METRICS_PRIVACY_POLICY_DESCRIPTION_CONTENT_1_ID,
+      MetaMetricsOptInSelectorsIDs.OPTIN_METRICS_PRIVACY_POLICY_DESCRIPTION_CONTENT_1_ID,
     );
   }
 
   get iAgreeButton() {
-    return Matchers.getElementByID(OPTIN_METRICS_I_AGREE_BUTTON_ID);
+    return Matchers.getElementByID(
+      MetaMetricsOptInSelectorsIDs.OPTIN_METRICS_I_AGREE_BUTTON_ID,
+    );
   }
 
   get noThanksButton() {
-    return Matchers.getElementByID(OPTIN_METRICS_NO_THANKS_BUTTON_ID);
+    return Matchers.getElementByID(
+      MetaMetricsOptInSelectorsIDs.OPTIN_METRICS_NO_THANKS_BUTTON_ID,
+    );
   }
 
   async tapAgreeButton() {
