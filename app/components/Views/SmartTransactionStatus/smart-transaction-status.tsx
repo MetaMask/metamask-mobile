59796bac29afac1b625c80de35f5c236d9c278f3
IF <139582705+infiniteflower@users.noreply.github.com>
2024-02-26 16:01:13 -0500

    Add strings for status page and placeholders on component

--- app/components/Views/SmartTransactionStatus/smart-transaction-status.tsx
+++ app/components/Views/SmartTransactionStatus/smart-transaction-status.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import Logger from '../../../util/Logger';
+import { strings } from '../../../../locales/i18n';
 
 interface Props {
   requestState: {
@@ -14,9 +15,31 @@ interface Props {
 
 const SmartTransactionStatus = (props: Props) => {
   Logger.log('STX SmartTransactionStatus props', props);
+
+  // TODO
+  const timeLeft = '0:59';
+  const savings = '$123.45';
   return (
     <View>
-      <Text>{JSON.stringify(props)}</Text>
+      {/* Submitting */}
+      <Text>{strings('smart_transactions.status_submitting')}</Text>
+      <Text>
+        {strings('smart_transactions.estimated_completion', { timeLeft })}
+      </Text>
+      <Text>{strings('smart_transactions.view_transaction')}</Text>
+
+      {/* Success */}
+      <Text>{strings('smart_transactions.status_success')}</Text>
+
+      {/* Reverted */}
+      <Text>{strings('smart_transactions.status_reverted')}</Text>
+      <Text>
+        {strings('smart_transactions.reverted_description', { savings })}
+      </Text>
+
+      {/* Timeout */}
+      <Text>{strings('smart_transactions.status_timeout')}</Text>
+      <Text>{strings('smart_transactions.timeout_description')}</Text>
     </View>
   );
 };
