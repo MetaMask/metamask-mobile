ee3e13caa863c1014d30d2f92d71099b43a096ce
IF <139582705+infiniteflower@users.noreply.github.com>
2024-02-26 18:17:37 -0500

    Add timer text

--- app/components/Views/SmartTransactionStatus/smart-transaction-status.tsx
+++ app/components/Views/SmartTransactionStatus/smart-transaction-status.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Logger from '../../../util/Logger';
 import { strings } from '../../../../locales/i18n';
@@ -35,14 +35,42 @@ const styles = StyleSheet.create({
   },
 });
 
+const stxStatusDeadline = 160; // TODO: Use a value from backend instead.
+
 const SmartTransactionStatus = ({
   requestState: {
-    smartTransaction: { status },
+    smartTransaction: { status, creationTime },
   },
 }: Props) => {
+  const [timeLeftForPendingStxInSec, setTimeLeftForPendingStxInSec] =
+    useState(stxStatusDeadline);
+
   // TODO
   const timeLeft = '0:59';
   const savings = '$123.45';
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+    if (status === 'pending') {
+      const calculateRemainingTime = () => {
+        const secondsAfterStxSubmission = Math.round(
+          (Date.now() - creationTime) / 1000,
+        );
+        if (secondsAfterStxSubmission > stxStatusDeadline) {
+          setTimeLeftForPendingStxInSec(0);
+          clearInterval(intervalId);
+          return;
+        }
+        setTimeLeftForPendingStxInSec(
+          stxStatusDeadline - secondsAfterStxSubmission,
+        );
+      };
+      intervalId = setInterval(calculateRemainingTime, 1000);
+      calculateRemainingTime();
+    }
+
+    return () => clearInterval(intervalId);
+  }, [creationTime, status]);
 
   let icon;
   let iconColor;
@@ -54,7 +82,7 @@ const SmartTransactionStatus = ({
     iconColor = IconColor.Primary;
     header = strings('smart_transactions.status_submitting');
     description = strings('smart_transactions.estimated_completion', {
-      timeLeft,
+      timeLeft: timeLeftForPendingStxInSec,
     });
   } else if (status === 'success') {
     icon = IconName.Check;
