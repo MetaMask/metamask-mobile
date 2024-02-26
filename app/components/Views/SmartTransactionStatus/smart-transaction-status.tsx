09f95a5235b5fa518e6c2d92ec454e2c47efe7dd
IF <139582705+infiniteflower@users.noreply.github.com>
2024-02-26 18:09:46 -0500

    style status page more

--- app/components/Views/SmartTransactionStatus/smart-transaction-status.tsx
+++ app/components/Views/SmartTransactionStatus/smart-transaction-status.tsx
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 import Logger from '../../../util/Logger';
 import { strings } from '../../../../locales/i18n';
 import Icon, {
+  IconColor,
   IconName,
   IconSize,
 } from '../../../component-library/components/Icons/Icon';
@@ -22,6 +23,7 @@ const styles = StyleSheet.create({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    gap: 10,
   },
   header: {
     textAlign: 'center',
@@ -43,24 +45,30 @@ const SmartTransactionStatus = ({
   const savings = '$123.45';
 
   let icon;
+  let iconColor;
   let header;
   let description;
 
   if (status === 'pending') {
     icon = IconName.Clock;
+    iconColor = IconColor.Primary;
     header = strings('smart_transactions.status_submitting');
     description = strings('smart_transactions.estimated_completion', {
       timeLeft,
     });
   } else if (status === 'success') {
     icon = IconName.Check;
+    iconColor = IconColor.Success;
     header = strings('smart_transactions.status_success');
+    description = undefined;
   } else if (status === 'cancelled') {
     icon = IconName.Warning;
+    iconColor = IconColor.Warning;
     header = strings('smart_transactions.status_timeout');
     description = strings('smart_transactions.timeout_description');
   } else {
     icon = IconName.Danger;
+    iconColor = IconColor.Error;
     header = strings('smart_transactions.status_reverted');
     description = strings('smart_transactions.reverted_description', {
       savings,
@@ -70,9 +78,9 @@ const SmartTransactionStatus = ({
   return (
     <View>
       <View style={styles.centered}>
-        <Icon name={icon} size={IconSize.Xl} />
+        <Icon name={icon} color={iconColor} size={IconSize.Xl} />
         <Text style={styles.header}>{header}</Text>
-        <Text>{description}</Text>
+        {description && <Text>{description}</Text>}
         <Text style={styles.desc}>
           {strings('smart_transactions.view_transaction')}
         </Text>
