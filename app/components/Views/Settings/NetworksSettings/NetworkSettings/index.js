23ba8b1476f39c58eb40d1c8c56f6fae4cd51ebe
sahar-fehri <sahar.fehri@consensys.net>
2024-02-27 13:58:53 +0100

    fix: deprecate goerli network (#8567)
    ## **Description**
    
    Pr to deprecate goerli network. 
    - Removes instances of goerli network. (From network dropdown and
    setting)
    - Updates e2e tests to use Sepolia instead.
    - Creates a new component
    (https://www.figma.com/file/KXgbxFucvk3FM5VULGZmzk/Assets%3A-Bug-Fixes?node-id=449%3A2210&mode=dev)
    for the scenario when a user clicks on a deepling for goerli.
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/mobile-planning/issues/1368
    
    ## **Manual testing steps**
    
    1. Click on network dropdown list (you should not see Goerli)
    2. Click on settings, then network list (you should not see Goerli)
    3. Click on a deeplink that is supposed to switch to goerli :
    https://metamask.app.link/send/0x4701Aa9471d7bfAc765D87dcb1Ea6BB23AD32733@5/approve?address=0x178e3e6c9f547A00E33150F7104427ea02cfc747&uint256=5e8.
    You should see a modal saying that the network is deprecated.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    
    https://github.com/MetaMask/metamask-mobile/assets/10994169/d748b713-5157-4d00-8b85-558c1e0164a7
    
    
    ### **After**
    
    When you try to click on a deeplink with goerli
    
    
    https://github.com/MetaMask/metamask-mobile/assets/10994169/622a47c3-5223-4420-84d8-a2aa3274973d
    
    When you try to add goerli network as custom network
    
    
    https://github.com/MetaMask/metamask-mobile/assets/10994169/0591d2b9-3299-44aa-9e6b-463ff66cd693
    
    Android:
    
    
    https://github.com/MetaMask/metamask-mobile/assets/10994169/13f4ef7f-adcc-4967-8036-1ceffbf0653a
    
    
    
    https://github.com/MetaMask/metamask-mobile/assets/10994169/aa6028b1-5ab0-4063-b00f-6c47f941880f
    
    
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [ ] I've linked related issues
    - [ ] I've included manual testing steps
    - [ ] I've included screenshots/recordings if applicable
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    - [ ] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [ ] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/Views/Settings/NetworksSettings/NetworkSettings/index.js
+++ app/components/Views/Settings/NetworksSettings/NetworkSettings/index.js
@@ -67,6 +67,8 @@ import { regex } from '../../../../../../app/util/regex';
 import { NetworksViewSelectorsIDs } from '../../../../../../e2e/selectors/Settings/NetworksView.selectors';
 import { updateIncomingTransactions } from '../../../../../util/transaction-controller';
 import { withMetricsAwareness } from '../../../../../components/hooks/useMetrics';
+import { CHAIN_IDS } from '@metamask/transaction-controller/dist/constants';
+import Routes from '../../../../../constants/navigation/Routes';
 
 const createStyles = (colors) =>
   StyleSheet.create({
@@ -1088,12 +1090,19 @@ class NetworkSettings extends PureComponent {
 
   toggleNetworkDetailsModal = async () => {
     const { rpcUrl, chainId: stateChainId } = this.state;
+    const { navigation } = this.props;
     const formChainId = stateChainId.trim().toLowerCase();
 
     // Ensure chainId is a 0x-prefixed, lowercase hex string
     let chainId = formChainId;
     if (!chainId.startsWith('0x')) {
       chainId = `0x${parseInt(chainId, 10).toString(16)}`;
+    }
+
+    // if chainId is goerli, show deprecation modal
+    if (chainId === CHAIN_IDS.GOERLI) {
+      navigation.navigate(Routes.DEPRECATED_NETWORK_DETAILS);
+      return;
     }
 
     if (!(await this.validateChainIdOnSubmit(formChainId, chainId, rpcUrl))) {
