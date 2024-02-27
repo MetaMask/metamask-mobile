442d9eeb50ce312f34139b91a285144d6ecf5392
Jonathan Ferreira <44679989+Jonathansoufer@users.noreply.github.com>
2024-02-27 00:48:13 +0000

    feat: Snaps new architecture mobile integration (#8700)
    ## **Description**
    
    This PR adds a refactored Snaps Webview approach and embedded execution
    environment to allow mobile app consume the new mobile snaps
    architecture.
    
    ## **Related issues**
    
    Fixes: N/A 
    
    ## **After Screenshot**
    
    https://github.com/MetaMask/metamask-mobile/assets/44679989/6e2308dd-4ad6-4f62-a875-fdd13b0c8528
    
    
    ## **Manual testing steps**
    
    1. Open the mobile app
    2. Navigate to explore session and access snaps example page
    3. add any snap
    4. go to snaps item on settings
    5. check if the installed snap is listed.
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [x] I've linked related issues
    - [x] I've included manual testing steps
    - [x] I've included screenshots/recordings if applicable
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    - [x] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [x] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ## **CI Related Section: DO NOT EDIT**
    
    Flag used by Bitrise to either run or skip E2E smoke tests
    
    - Bitrise Flag:[skip ci]
    
    ---------
    
    Co-authored-by: Owen Craston <owengc12@gmail.com>

--- app/core/Snaps/index.ts
+++ app/core/Snaps/index.ts
@@ -1,10 +1,5 @@
 ///: BEGIN:ONLY_INCLUDE_IF(snaps)
 import SnapBridge from './SnapBridge';
-import SnapDuplex from './SnapDuplex';
-import WebviewExecutionService from './WebviewExecutionService';
-import WebviewPostMessageStream from './WebviewPostMessageStream';
-import SnapWebviewPostMessageStream from './SnapWebviewPostMessageStream';
-import snapsState from './SnapsState';
 import {
   ExcludedSnapPermissions,
   ExcludedSnapEndowments,
@@ -16,12 +11,7 @@ import {
 } from './location';
 
 export {
-  snapsState,
-  SnapDuplex,
   SnapBridge,
-  WebviewExecutionService,
-  WebviewPostMessageStream,
-  SnapWebviewPostMessageStream,
   ExcludedSnapPermissions,
   ExcludedSnapEndowments,
   fetchFunction,
