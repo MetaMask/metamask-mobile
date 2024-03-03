9beb87dc666526c263d870dada946847d31b301d
gantunesr <gustavoantunesrod@gmail.com>
2024-03-03 17:39:30 -0300

    fix: update OLD_ITERATIONS_NUMBER value

--- app/core/Encryptor/constants.ts
+++ app/core/Encryptor/constants.ts
@@ -1,5 +1,5 @@
 export const SHA256_DIGEST_LENGTH = 256;
-export const OLD_ITERATIONS_NUMBER = 1_000;
+export const OLD_ITERATIONS_NUMBER = 5_000;
 export const MINIMUM_ITERATIONS_NUMBER = 600_000;
 export const ENCRYPTION_LIBRARY = {
   original: 'original',
