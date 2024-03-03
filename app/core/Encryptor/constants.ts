5a0032f0d90ab9c78b0ca6e491b10cb73eee3942
gantunesr <gustavoantunesrod@gmail.com>
2024-03-03 02:45:40 -0300

    refactor: rename constants

--- app/core/Encryptor/constants.ts
+++ app/core/Encryptor/constants.ts
@@ -1,6 +1,6 @@
 export const SHA256_DIGEST_LENGTH = 256;
-export const OLD_NUMBER_ITERATIONS = 1_000;
-export const MINIMUM_NUMBER_ITERATIONS = 600_000;
+export const OLD_ITERATIONS_NUMBER = 1_000;
+export const MINIMUM_ITERATIONS_NUMBER = 600_000;
 export const ENCRYPTION_LIBRARY = {
   original: 'original',
 };
