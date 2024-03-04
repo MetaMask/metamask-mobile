e290c763fb9f682ddffe1fb15204e7bb39327846
gantunesr <gustavoantunesrod@gmail.com>
2024-03-03 21:19:37 -0300

    chore: add DEFAULT_ITERATIONS_NUMBER

--- app/core/Encryptor/constants.ts
+++ app/core/Encryptor/constants.ts
@@ -2,6 +2,7 @@ export const BYTE_COUNT = 32;
 export const SHA256_DIGEST_LENGTH = 256;
 export const OLD_ITERATIONS_NUMBER = 5_000;
 export const MINIMUM_ITERATIONS_NUMBER = 600_000;
+export const DEFAULT_ITERATIONS_NUMBER = 900_000;
 export const ENCRYPTION_LIBRARY = {
   original: 'original',
 };
@@ -9,6 +10,6 @@ export const ENCRYPTION_LIBRARY = {
 export const DEFAULT_DERIVATION_PARAMS = {
   algorithm: 'PBKDF2',
   params: {
-    iterations: MINIMUM_ITERATIONS_NUMBER,
+    iterations: DEFAULT_ITERATIONS_NUMBER,
   },
 };
