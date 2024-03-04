d7a1e5951bf184774d653c5707f389dcb01c1377
Jiexi Luan <jiexiluan@gmail.com>
2024-03-04 13:43:45 -0800

    Add rpcMethodTracking middleware spec

---
+++ app/util/middlewares/rpcMethodTracking.test.ts
@@ -0,0 +1,229 @@
+import { MetaMetricsEvents } from '../../core/Analytics';
+import createRPCMethodTrackingMiddleware from './rpcMethodTracking';
+
+const mockMetrics = {
+  isEnabled: jest.fn(),
+  trackEvent: jest.fn(),
+};
+
+const handler = createRPCMethodTrackingMiddleware({
+  metrics: mockMetrics,
+  rateLimitSeconds: 1,
+});
+
+function getNext(timeout = 500) {
+  let deferred: { resolve: (value: unknown) => void };
+  const promise = new Promise((resolve) => {
+    deferred = {
+      resolve,
+    };
+  });
+  const cb = () => deferred.resolve(undefined);
+  let triggerNext: (value: unknown) => void;
+  setTimeout(() => {
+    deferred.resolve(undefined);
+  }, timeout);
+  return {
+    executeMiddlewareStack: async () => {
+      if (triggerNext) {
+        triggerNext(() => cb());
+      }
+      return await deferred.resolve(undefined);
+    },
+    promise,
+    next: (postReqHandler: (value: unknown) => void) => {
+      triggerNext = postReqHandler;
+    },
+  };
+}
+
+const waitForSeconds = async (seconds: number) =>
+  await new Promise((resolve) => setTimeout(resolve, 1000 * seconds));
+
+describe('createRPCMethodTrackingMiddleware', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+    mockMetrics.isEnabled.mockReturnValue(false);
+  });
+
+  describe('MetaMetrics is disabled', () => {
+    beforeEach(() => {
+      mockMetrics.isEnabled.mockReturnValue(false);
+    });
+
+    it('should not track any events', async () => {
+      const req = {
+        method: 'eth_sign',
+        origin: 'some.dapp',
+      };
+
+      const res = {
+        error: null,
+      };
+      const { executeMiddlewareStack, next } = getNext();
+      handler(req, res, next);
+      await executeMiddlewareStack();
+      expect(mockMetrics.trackEvent).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('MetaMetrics is enabled', () => {
+    beforeEach(() => {
+      mockMetrics.isEnabled.mockReturnValue(true);
+    });
+
+    it(`should never track blocked methods such as 'metamask_getProviderState'`, () => {
+      const req = {
+        method: 'metamask_getProviderState',
+        origin: 'some.dapp',
+      };
+
+      const res = {
+        error: null,
+      };
+      const { next, executeMiddlewareStack } = getNext();
+      handler(req, res, next);
+      expect(mockMetrics.trackEvent).not.toHaveBeenCalled();
+      executeMiddlewareStack();
+    });
+
+    it(`should always track non rate limited events such as 'eth_sign'`, async () => {
+      const req = {
+        method: 'eth_sign',
+        origin: 'some.dapp',
+      };
+
+      const res = {
+        error: null,
+      };
+
+      let callCount = 0;
+
+      while (callCount < 3) {
+        callCount += 1;
+        const { next, executeMiddlewareStack } = getNext();
+        handler(req, res, next);
+        await executeMiddlewareStack();
+        if (callCount !== 3) {
+          await waitForSeconds(0.6);
+        }
+      }
+
+      const expectedArgs = [
+        MetaMetricsEvents.PROVIDER_METHOD_CALLED,
+        {
+          referrer: {
+            url: 'some.dapp',
+          },
+          properties: {
+            method: 'eth_sign',
+          },
+        },
+      ];
+
+      expect(mockMetrics.trackEvent).toHaveBeenCalledTimes(3);
+      expect(mockMetrics.trackEvent).toHaveBeenNthCalledWith(
+        1,
+        ...expectedArgs,
+      );
+      expect(mockMetrics.trackEvent).toHaveBeenNthCalledWith(
+        2,
+        ...expectedArgs,
+      );
+      expect(mockMetrics.trackEvent).toHaveBeenNthCalledWith(
+        3,
+        ...expectedArgs,
+      );
+    });
+
+    it(`should only track rate limited events when under threshold such as 'eth_requestAccounts'`, async () => {
+      const req = {
+        method: 'eth_requestAccounts',
+        origin: 'some.dapp',
+      };
+
+      const res = {
+        error: null,
+      };
+
+      let callCount = 0;
+
+      while (callCount < 3) {
+        callCount += 1;
+        const { next, executeMiddlewareStack } = getNext();
+        handler(req, res, next);
+        await executeMiddlewareStack();
+        if (callCount !== 3) {
+          await waitForSeconds(0.6);
+        }
+      }
+
+      const expectedArgs = [
+        MetaMetricsEvents.PROVIDER_METHOD_CALLED,
+        {
+          referrer: {
+            url: 'some.dapp',
+          },
+          properties: {
+            method: 'eth_requestAccounts',
+          },
+        },
+      ];
+
+      expect(mockMetrics.trackEvent).toHaveBeenCalledTimes(2);
+      expect(mockMetrics.trackEvent).toHaveBeenNthCalledWith(
+        1,
+        ...expectedArgs,
+      );
+      expect(mockMetrics.trackEvent).toHaveBeenNthCalledWith(
+        2,
+        ...expectedArgs,
+      );
+    });
+
+    it(`should treat tracking methods as rate limited by default`, async () => {
+      const req = {
+        method: 'some_unknown_event',
+        origin: 'some.dapp',
+      };
+
+      const res = {
+        error: null,
+      };
+
+      let callCount = 0;
+
+      while (callCount < 3) {
+        callCount += 1;
+        const { next, executeMiddlewareStack } = getNext();
+        handler(req, res, next);
+        await executeMiddlewareStack();
+        if (callCount !== 3) {
+          await waitForSeconds(0.6);
+        }
+      }
+
+      const expectedArgs = [
+        MetaMetricsEvents.PROVIDER_METHOD_CALLED,
+        {
+          referrer: {
+            url: 'some.dapp',
+          },
+          properties: {
+            method: 'some_unknown_event',
+          },
+        },
+      ];
+
+      expect(mockMetrics.trackEvent).toHaveBeenCalledTimes(2);
+      expect(mockMetrics.trackEvent).toHaveBeenNthCalledWith(
+        1,
+        ...expectedArgs,
+      );
+      expect(mockMetrics.trackEvent).toHaveBeenNthCalledWith(
+        2,
+        ...expectedArgs,
+      );
+    });
+  });
+});
