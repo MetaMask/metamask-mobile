0861b46379dcdccffba24fe6ab5719c217e5518f
tommasini <tommasini15@gmail.com>
2024-03-06 22:15:46 +0000

    unnecessary ts ignore removed

--- app/core/Engine.ts
+++ app/core/Engine.ts
@@ -542,8 +542,6 @@ class Engine {
       onNetworkStateChange: (listener) =>
         this.controllerMessenger.subscribe(
           AppConstants.NETWORK_STATE_CHANGE_EVENT,
-          // @ts-expect-error network controller will be updated on this PR: https://github.com/MetaMask/metamask-mobile/pull/8812 and this type error will be addressed
-          // This is not a blocker because gas fee controller does not need ticker to be defined
           listener,
         ),
       getCurrentNetworkEIP1559Compatibility: async () =>
