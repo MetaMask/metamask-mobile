3a9c12cd1c49981438b0a079d984857694476af5
tommasini <tommasini15@gmail.com>
2024-03-06 17:39:39 +0000

    dedupe dependencies and ignore ts error on engine that needs network controller be updated

--- app/core/Engine.ts
+++ app/core/Engine.ts
@@ -1030,6 +1030,8 @@ class Engine {
         onNetworkStateChange: (listener) =>
           this.controllerMessenger.subscribe(
             AppConstants.NETWORK_STATE_CHANGE_EVENT,
+            // @ts-expect-error network controller will be updated on this PR: https://github.com/MetaMask/metamask-mobile/pull/8812 and this type error will be addressed
+            // This is not a blocker because gas fee controller does not need ticker to be defined
             listener,
           ),
         // @ts-expect-error at this point in time the provider will be defined by the `networkController.initializeProvider`
