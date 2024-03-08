8f54762d946dba105ccc2beb592d84cdbb2f54e4
Jiexi Luan <jiexiluan@gmail.com>
2024-03-07 16:16:17 -0800

    Fix trailing slash regex

--- app/core/RPCMethods/wallet_addEthereumChain.js
+++ app/core/RPCMethods/wallet_addEthereumChain.js
@@ -75,7 +75,9 @@ const wallet_addEthereumChain = async ({
     : null;
   // Remove trailing slashes
   const firstValidRPCUrl = dirtyFirstValidRPCUrl
-    ? dirtyFirstValidRPCUrl.replace(/\/+$/, '')
+    // This slightly roundabout regex prevents a ReDos attack
+    // See https://github.com/MetaMask/mobile-planning/issues/1589
+    ? dirtyFirstValidRPCUrl.replace(/([^\/])\/+$/g, '$1')
     : dirtyFirstValidRPCUrl;
 
   const firstValidBlockExplorerUrl =
