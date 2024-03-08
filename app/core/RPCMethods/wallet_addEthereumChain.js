8c89243cf74d024b0d8b1c1e71774f5ea548a30c
Jiexi Luan <jiexiluan@gmail.com>
2024-03-08 09:05:53 -0800

    lint

--- app/core/RPCMethods/wallet_addEthereumChain.js
+++ app/core/RPCMethods/wallet_addEthereumChain.js
@@ -75,9 +75,9 @@ const wallet_addEthereumChain = async ({
     : null;
   // Remove trailing slashes
   const firstValidRPCUrl = dirtyFirstValidRPCUrl
-    // This slightly roundabout regex prevents a ReDos attack
-    // See https://github.com/MetaMask/mobile-planning/issues/1589
-    ? dirtyFirstValidRPCUrl.replace(/([^\/])\/+$/g, '$1')
+    ? // This slightly roundabout regex prevents a ReDos attack
+      // See https://github.com/MetaMask/mobile-planning/issues/1589
+      dirtyFirstValidRPCUrl.replace(/([^\/])\/+$/g, '$1')
     : dirtyFirstValidRPCUrl;
 
   const firstValidBlockExplorerUrl =
