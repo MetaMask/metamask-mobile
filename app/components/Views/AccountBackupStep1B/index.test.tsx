72b8eb6df3b2070be71c264568aebf9a43f51c5c
Timi Tejumola <tejumoladavid@gmail.com>
2024-03-14 12:16:49 -0400

    chore: Reduce enzymes usage 1/3 (#8721)
    ## **Description**
    
    Convert the list of unit tests to use react-testing-library instead of
    enzyme for the following files:
    ```
    'app/components/Views/Root/index.test.tsx',
    'app/components/Views/NavigationUnitTest/TestScreen3.test.js',
    'app/components/Views/NavigationUnitTest/TestScreen2.test.js',
    'app/components/Views/NavigationUnitTest/TestScreen1.test.js',
    'app/components/Views/ImportPrivateKeySuccess/index.test.tsx',
    'app/components/Views/AccountBackupStep1B/index.test.tsx',
    'app/components/UI/WebviewProgressBar/index.test.tsx',
    'app/components/UI/SeedphraseModal/index.test.tsx',
    'app/components/UI/PhishingModal/index.test.tsx',
    'app/components/UI/FoxScreen/index.test.tsx',
    'app/components/UI/FadeView/index.test.tsx',
    'app/components/UI/CustomAlert/index.test.tsx',
    'app/components/UI/Confetti/index.test.tsx',
    'app/components/UI/Collectibles/index.test.tsx',
    'app/components/UI/Button/index.test.tsx',
    'app/components/UI/AnimatedSpinner/index.test.tsx',
    'app/components/UI/AddCustomToken/index.test.tsx',
    ```
    
    ## **Related issues**
    
    Fixes: #8616 
    
    ## **Manual testing steps**
    
    Not Applicable
    
    ## **Screenshots/Recordings**
    
    Not Applicable
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [x] I've linked related issues
    - [ ] I've included manual testing steps
    - [ ] I've included screenshots/recordings if applicable
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    - [x] I’ve properly set the pull request status:
      - [ ] In case it's not yet "ready for review", I've set it to "draft".
    - [x] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Cal Leung <cleun007@gmail.com>

--- app/components/Views/AccountBackupStep1B/index.test.tsx
+++ app/components/Views/AccountBackupStep1B/index.test.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import { shallow } from 'enzyme';
 import AccountBackupStep1B from './';
+import { renderScreen } from '../../../util/test/renderWithProvider';
 
 describe('AccountBackupStep1B', () => {
+  beforeEach(() => jest.useFakeTimers());
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.useRealTimers();
+  });
+
   it('should render correctly', () => {
-    const wrapper = shallow(<AccountBackupStep1B />);
-    expect(wrapper).toMatchSnapshot();
+    const { toJSON } = renderScreen(AccountBackupStep1B, {
+      name: 'AccountBackupStep1B',
+    });
+    expect(toJSON()).toMatchSnapshot();
   });
 });
