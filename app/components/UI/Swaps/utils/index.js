60454c163287fa5473de5d407af55bd6e5652f18
IF <139582705+infiniteflower@users.noreply.github.com>
2024-03-14 16:30:34 -0400

    Fix feature flags on testnet

--- app/components/UI/Swaps/utils/index.js
+++ app/components/UI/Swaps/utils/index.js
@@ -3,6 +3,7 @@ import BigNumber from 'bignumber.js';
 import { swapsUtils } from '@metamask/swaps-controller';
 import { strings } from '../../../../../locales/i18n';
 import AppConstants from '../../../../core/AppConstants';
+import { NETWORKS_CHAIN_ID } from '../../../../constants/network';
 
 const {
   ETH_CHAIN_ID,
@@ -27,9 +28,14 @@ const allowedChainIds = [
   LINEA_CHAIN_ID,
   SWAPS_TESTNET_CHAIN_ID,
 ];
+
+export const allowedTestnetChainIds = [
+  NETWORKS_CHAIN_ID.GOERLI,
+  NETWORKS_CHAIN_ID.SEPOLIA,
+];
+
 if (__DEV__) {
-  allowedChainIds.push(NETWORKS_CHAIN_ID.GOERLI);
-  allowedChainIds.push(NETWORKS_CHAIN_ID.SEPOLIA);
+  allowedChainIds.push(...allowedTestnetChainIds);
 }
 
 export function isSwapsAllowed(chainId) {
