d40005f42423801a7e8914acab5813c9c1d4a25c
Xiaoming Wang <dawnseeker8@gmail.com>
2024-03-15 10:44:59 +0000

    fix: to revert back to 0.0.9 version since 0.0.10 version break the build.

--- app/components/Views/confirmations/SendFlow/Confirm/index.js
+++ app/components/Views/confirmations/SendFlow/Confirm/index.js
@@ -831,6 +831,8 @@ class Confirm extends PureComponent {
 
       const { result, transactionMeta } = this.state;
 
+      await this.persistTransactionParameters(transaction);
+
       const isLedgerAccount = isHardwareAccount(transaction.from, [
         ExtendedKeyringTypes.ledger,
       ]);
@@ -860,7 +862,6 @@ class Confirm extends PureComponent {
         return;
       }
 
-      await this.persistTransactionParameters(transaction);
       await KeyringController.resetQRKeyringState();
       await ApprovalController.accept(transactionMeta.id, undefined, {
         waitForResult: true,
