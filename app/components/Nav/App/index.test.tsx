fa0a4e0a0e3580188c9717b6ec09425532359cbd
Nicolas MASSART <nicolas.massart@consensys.net>
2024-03-15 17:15:56 +0100

    unit test metametrics startup
    migrate unit test to RN testing
    update snapshot
    update test names to match guidelines

--- app/components/Nav/App/index.test.tsx
+++ app/components/Nav/App/index.test.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import { renderScreen } from '../../../util/test/renderWithProvider';
+import initialBackgroundState from '../../../util/test/initial-background-state.json';
 import App from './';
-import configureMockStore from 'redux-mock-store';
-import { Provider } from 'react-redux';
+import { MetaMetrics } from '../../../core/Analytics';
+import { waitFor } from '@testing-library/react-native';
 
 const initialState = {
   user: {
     loggedIn: true,
   },
+  engine: {
+    ...initialBackgroundState,
+  },
 };
-const mockStore = configureMockStore();
-const store = mockStore(initialState);
+
+jest.mock('../../../core/Analytics/MetaMetrics');
+
+const mockMetrics = {
+  configure: jest.fn(),
+  addTraitsToUser: jest.fn(),
+};
+
+// Need to mock this module since it uses store.getState, which interferes with the mocks from this test file.
+jest.mock(
+  '../../../util/metrics/UserSettingsAnalyticsMetaData/generateUserProfileAnalyticsMetaData',
+  () => jest.fn().mockReturnValue({ userProp: 'User value' }),
+);
+
+jest.mock(
+  '../../../util/metrics/DeviceAnalyticsMetaData/generateDeviceAnalyticsMetaData',
+  () => jest.fn().mockReturnValue({ deviceProp: 'Device value' }),
+);
+
+(MetaMetrics.getInstance as jest.Mock).mockReturnValue(mockMetrics);
 
 describe('App', () => {
-  it('should render correctly when logged in', () => {
-    const wrapper = shallow(
-      <Provider store={store}>
-        <App />
-      </Provider>,
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders according to latest snapshot', () => {
+    const { toJSON } = renderScreen(
+      App,
+      { name: 'App' },
+      { state: initialState },
     );
-    expect(wrapper).toMatchSnapshot();
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('configures MetaMetrics instance and identifies user on startup', async () => {
+    renderScreen(App, { name: 'App' }, { state: initialState });
+    await waitFor(() => {
+      expect(mockMetrics.configure).toHaveBeenCalledTimes(1);
+      expect(mockMetrics.addTraitsToUser).toHaveBeenNthCalledWith(1, {
+        deviceProp: 'Device value',
+        userProp: 'User value',
+      });
+    });
   });
 });
