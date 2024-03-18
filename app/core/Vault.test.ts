0dcf2eaecf97d8a5ad47eafd18a5c86a47e48c26
Xiaoming Wang <dawnseeker8@gmail.com>
2024-03-18 14:44:04 +0000

    fix: fix the unit tests broken in vault.test.ts

--- app/core/Vault.test.ts
+++ app/core/Vault.test.ts
@@ -11,8 +11,6 @@ jest.mock('./Engine', () => ({
       restoreQRKeyring: jest.fn(),
       addNewKeyring: jest.fn(),
       persistAllKeyrings: jest.fn(),
-      updateIdentities: jest.fn(),
-      getAccounts: jest.fn().mockReturnValue(['account']),
     },
   },
 }));
@@ -79,8 +77,6 @@ describe('Vault', () => {
       expect(getLedgerKeyring).toHaveBeenCalled();
       expect(mockDeserializedKeyring).toHaveBeenCalled();
       expect(KeyringController.persistAllKeyrings).toHaveBeenCalled();
-      expect(KeyringController.updateIdentities).toHaveBeenCalled();
-      expect(KeyringController.getAccounts).toHaveBeenCalled();
     });
 
     it('should not restore ledger keyring if it does not exist', async () => {
@@ -88,8 +84,6 @@ describe('Vault', () => {
 
       await restoreLedgerKeyring();
       expect(KeyringController.persistAllKeyrings).not.toHaveBeenCalled();
-      expect(KeyringController.updateIdentities).not.toHaveBeenCalled();
-      expect(KeyringController.getAccounts).not.toHaveBeenCalled();
     });
 
     it('should log error if an exception is thrown', async () => {
