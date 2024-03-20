04e35fbd5bd8d2e3350eac861b1eec6569338ec0
Kylan Hurt <kylanhurt@users.noreply.github.com>
2024-03-19 22:41:39 -0300

    Migrate another (3/3) batch of unit tests away from enzyme (#8959)
    ## **Description**
    
    The mobile platform team is pushing to reduce the usage of enzyme within
    the application. This PR puts us one step closer to accomplishing this
    goal. This batch includes the following files:
    
    ```
    'app/components/UI/LedgerModals/LedgerMessageSignModal.test.tsx',
      'app/components/UI/GlobalAlert/index.test.tsx',
      'app/components/UI/BrowserBottomBar/index.test.tsx',
      'app/component-library/components/Buttons/ButtonIcon/ButtonIcon.test.tsx',
      'app/components/Nav/App/index.test.tsx',
      'app/components/Views/OfflineMode/index.test.tsx',
      'app/components/Views/LockScreen/index.test.tsx',
      'app/components/UI/SearchTokenAutocomplete/index.test.tsx',
      'app/components/UI/OptinMetrics/index.test.tsx',
      'app/components/UI/AccountRightButton/index.test.tsx',
      'app/components/UI/AccountOverview/index.test.tsx',
      'app/component-library/components/Pickers/PickerAccount/PickerAccount.test.tsx',
      'app/component-library/components/Buttons/Button/variants/ButtonSecondary/ButtonSecondary.test.tsx',
      'app/component-library/components/Buttons/Button/variants/ButtonPrimary/ButtonPrimary.test.tsx',
      'app/components/Views/QRScanner/index.test.tsx',
      'app/components/Views/ImportFromSecretRecoveryPhrase/index.test.tsx',
      'app/components/Views/AccountBackupStep1/index.test.tsx',
      'app/components/UI/AssetSearch/index.test.tsx',
      'app/components/Views/Settings/NetworksSettings/index.test.tsx'
    ```
    
    ## **Related issues**
    
    Fixes: #8805 
    
    ## **Manual testing steps**
    
    `yarn test:unit`
    
    ## **Screenshots/Recordings**
    
    <img width="346" alt="image"
    src="https://github.com/MetaMask/metamask-mobile/assets/6249205/d78f26ac-a47a-460d-9285-c4765ec678c6">
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [X] I've clearly explained what problem this PR is solving and how it
    is solved.
    - [X] I've linked related issues
    - [X] I've included manual testing steps
    - [X] I've included screenshots/recordings if applicable
    - [X] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    - [X I’ve properly set the pull request status:
      - [X] In case it's not yet "ready for review", I've set it to "draft".
    - [ ] In case it's "ready for review", I've changed it from "draft" to
    "non-draft".
    
    ## **Pre-merge reviewer checklist**
    
    - [X] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Cal Leung <cleun007@gmail.com>

--- app/components/UI/UpdateNeeded/UpdateNeeded.test.tsx
+++ app/components/UI/UpdateNeeded/UpdateNeeded.test.tsx
@@ -1,12 +1,6 @@
 import { renderScreen } from '../../..//util/test/renderWithProvider';
 import { UpdateNeeded } from './';
 
-jest.mock('react-native-device-info', () => ({
-  getBrand: () => 'some brand',
-  getBuildNumber: () => 'some build number',
-  getVersion: () => 'some version',
-}));
-
 describe('UpdateNeeded', () => {
   it('should render correctly', () => {
     const { toJSON } = renderScreen(
