d822be6b40b849aa974c4cb0eb560294d9d9668e
Owen Craston <owengc12@gmail.com>
2024-03-26 18:37:36 -0700

    getSelectedInternalAccount

---
+++ app/selectors/accountsController.test.ts
@@ -0,0 +1,84 @@
+import { AccountsControllerState } from '@metamask/accounts-controller';
+import getSelectedInternalAccount from './accountsController';
+
+const ACCOUNTS_CONTROLLER_STATE_MOCK: AccountsControllerState = {
+  internalAccounts: {
+    accounts: {
+      '30313233-3435-4637-b839-383736353430': {
+        address: '0x0',
+        id: '30313233-3435-4637-b839-383736353430',
+        options: {},
+        metadata: {
+          name: 'Account 1',
+          keyring: {
+            type: 'HD Key Tree',
+          },
+        },
+        methods: [
+          'personal_sign',
+          'eth_sign',
+          'eth_signTransaction',
+          'eth_signTypedData_v1',
+          'eth_signTypedData_v3',
+          'eth_signTypedData_v4',
+        ],
+        type: 'eip155:eoa',
+      },
+      '30313233-3435-4637-b839-383736353431': {
+        address: '0x1',
+        id: '30313233-3435-4637-b839-383736353431',
+        options: {},
+        metadata: {
+          name: 'Account 2',
+          keyring: {
+            type: 'HD Key Tree',
+          },
+        },
+        methods: [
+          'personal_sign',
+          'eth_sign',
+          'eth_signTransaction',
+          'eth_signTypedData_v1',
+          'eth_signTypedData_v3',
+          'eth_signTypedData_v4',
+        ],
+        type: 'eip155:eoa',
+      },
+    },
+    selectedAccount: '30313233-3435-4637-b839-383736353431',
+  },
+};
+describe('Accounts Controller Selectors', () => {
+  describe('getSelectedInternalAccount', () => {
+    it('returns selected internal account', () => {
+      expect(
+        getSelectedInternalAccount({
+          engine: {
+            backgroundState: {
+              AccountsController: ACCOUNTS_CONTROLLER_STATE_MOCK,
+            },
+          },
+        } as any),
+      ).toEqual({
+        address: '0x1',
+        id: '30313233-3435-4637-b839-383736353431',
+        options: {},
+        metadata: {
+          name: 'Account 2',
+          keyring: {
+            type: 'HD Key Tree',
+          },
+        },
+        methods: [
+          'personal_sign',
+          'eth_sign',
+          'eth_signTransaction',
+          'eth_signTypedData_v1',
+          'eth_signTypedData_v3',
+          'eth_signTypedData_v4',
+        ],
+        type: 'eip155:eoa',
+      });
+    });
+  });
+});
