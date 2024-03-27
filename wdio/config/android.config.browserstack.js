0cac3837a29dfb02c3128ace42ed9c2b8395b9ac
Cal-L <cleun007@gmail.com>
2024-03-26 17:16:54 -0700

    Fix build-router-start format

--- wdio/config/android.config.browserstack.js
+++ wdio/config/android.config.browserstack.js
@@ -19,10 +19,10 @@ config.capabilities = [
     build: 'Android QA E2E Smoke Tests',
     device: process.env.BROWSERSTACK_DEVICE || 'Google Pixel 6',
     os_version: process.env.BROWSERSTACK_OS_VERSION || '12.0',
-    app: 'bs://8f1908ee7bfd6ad208b886016d2c33fd3b9148e5',//process.env.BROWSERSTACK_CURRENT_APP_URL || process.env.BROWSERSTACK_APP_URL,
+    app: process.env.BROWSERSTACK_CURRENT_APP_URL || process.env.BROWSERSTACK_APP_URL,
     'browserstack.debug': true,
     'browserstack.local': true,
-    'browserstack.midSessionInstallApps' : ['bs://8f1908ee7bfd6ad208b886016d2c33fd3b9148e5'],//[process.env.BROWSERSTACK_APP_URL],
+    'browserstack.midSessionInstallApps' : [process.env.BROWSERSTACK_APP_URL],
   },
 ];
 
