22887ba7932e3932ad620b44a47595a52c9fcf46
tommasini <tommasini15@gmail.com>
2024-03-27 11:14:17 +0000

    remove unusable import

--- app/store/migrations/033.ts
+++ app/store/migrations/033.ts
@@ -2,7 +2,6 @@ import { CHAIN_IDS } from '@metamask/transaction-controller/dist/constants';
 import { SEPOLIA } from '../../constants/network';
 import { captureException } from '@sentry/react-native';
 import { isObject } from '@metamask/utils';
-import { NetworkState } from '@metamask/network-controller';
 import NetworkList from '../../util/networks';
 
 export default async function migrate(stateAsync: unknown) {
