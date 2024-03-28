f0ec2dbff080205b16b5ffb79a0367bc9768e6c5
JSoufer <jonathan.ferreira@consensys.net>
2024-03-28 20:22:21 +0000

    chore: improve notification type

--- app/components/Views/Notifications/types.ts
+++ app/components/Views/Notifications/types.ts
@@ -1,42 +1,20 @@
-import type { Hex } from '@metamask/utils';
 import type { NotificationsActionsTypes } from '../Settings/NotificationsSettings/NotificationsSettings.constants';
 import NotificationTypes from '../../../util/notifications';
 import { IconName } from '../../../component-library/components/Icons/Icon';
 
-export interface Asset {
-  address?: string;
-  symbol?: string;
-  ticker?: string;
-  name?: string;
-  logo?: string;
-  isETH?: boolean;
-  isNFT?: boolean;
-  nftUri?: string;
+import type { TransactionMeta } from '@metamask/transaction-controller';
+
+interface FcmMeta {
+  title: string;
+  message: string;
+  imageUri?: string;
+
+  cta?: {
+    label: string;
+    onPress: () => void;
+    icon?: IconName;
+  };
 }
-
-export interface Transaction {
-  id: string;
-  chainId: Hex;
-  from: string;
-  to: string;
-  data?: string;
-  nonce: string;
-  value?: string;
-  type?: string;
-
-  gas?: string;
-  gasPrice?: string;
-  gasUsed?: string;
-  maxFeePerGas?: string;
-  maxPriorityFeePerGas?: string;
-  estimatedBaseFee?: string;
-  estimateGasError?: string;
-
-  time: string;
-  status?: string;
-  asset: Asset;
-}
-
 export interface Notification {
   id: string;
   isVisible: boolean;
@@ -44,16 +22,9 @@ export interface Notification {
   status: string;
   type: NotificationTypes;
   actionsType: NotificationsActionsTypes;
-  imageUri?: string;
-  title?: string;
-  message?: string;
-  cta?: {
-    label: string;
-    onPress: () => void;
-    icon?: IconName;
-  };
-  data?: {
-    transaction?: Transaction;
-  };
   timestamp: string;
+  data: {
+    transactionMeta?: TransactionMeta;
+    fcmMeta?: FcmMeta;
+  };
 }
