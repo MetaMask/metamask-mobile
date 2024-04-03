a9488f5474c26acbcb2f526340442ef3bd077161
Curtis <Curtis.David7@gmail.com>
2024-04-03 10:55:29 +0100

    boiler plate for general view

---
+++ e2e/pages/Settings/GeneralView.js
@@ -0,0 +1,4 @@
+import Matchers from '../../utils/Matchers';
+import Gestures from '../../utils/Gestures';
+class GeneralView {}
+export default new GeneralView();
---
+++ e2e/selectors/Settings/GeneralView.selectors.js
@@ -0,0 +1,5 @@
+import enContent from '../../../locales/languages/en.json';
+
+export const GeneralViewSelectorsIDs = {};
+
+export const GeneralViewSelectorsText = {};
