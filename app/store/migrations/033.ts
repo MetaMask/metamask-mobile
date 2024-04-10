4f830c83d26501f20310b25294693aea49bb0514
tommasini <46944231+tommasini@users.noreply.github.com>
2024-04-10 18:08:07 +0100

    feat: Update network controller to version 13.0.1 (#9065)
    ## **Description**
    
    Updated network controller to version 13.0.1. Also created migration to
    delete networkId property from the network controller state
    Added an inpageProvider property with a network id property to our
    internal property,
    Created the reducer and the action for that property with createSlice of
    redux toolkit
    Added dispatch to the engine event subscriber `networkdStateChange` to
    update networkId
    Added dispatch to the Engine file with subscribing an event
    `networkWillChange` to update networkId to "loading"
    Updated the patch of network controller
    Updated test cases and initial states with new data structure with the
    networkId property
    
    
    ## **Related issues**
    
    Fixes https://github.com/MetaMask/mobile-planning/issues/1624
    
    ## **Manual testing steps**
    QA Build:
    https://app.bitrise.io/app/be69d4368ee7e86d/pipelines/4b1752b4-2490-4eee-b0f9-68a347562ccd
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    
    https://github.com/MetaMask/metamask-mobile/assets/46944231/7d80d89e-8c4c-485e-8806-65e11419934e
    
    Latest recording with new actions (while updating migration from main
    with account imported on optimism)
    
    
    https://github.com/MetaMask/metamask-mobile/assets/46944231/d290680a-ae53-4f3e-b752-1ded27964954
    
    
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/store/migrations/033.ts
+++ app/store/migrations/033.ts
@@ -2,7 +2,6 @@ import { CHAIN_IDS } from '@metamask/transaction-controller/dist/constants';
 import { SEPOLIA } from '../../constants/network';
 import { captureException } from '@sentry/react-native';
 import { isObject } from '@metamask/utils';
-import { NetworkState } from '@metamask/network-controller';
 import NetworkList from '../../util/networks';
 
 export default async function migrate(stateAsync: unknown) {
@@ -32,12 +31,20 @@ export default async function migrate(stateAsync: unknown) {
     );
     return state;
   }
-  const networkControllerState = state.engine.backgroundState
-    .NetworkController as NetworkState;
+  const networkControllerState = state.engine.backgroundState.NetworkController;
   if (!isObject(networkControllerState)) {
     captureException(
       new Error(
         `Migration 33: Invalid NetworkController state error: '${typeof networkControllerState}'`,
+      ),
+    );
+    return state;
+  }
+
+  if (!isObject(networkControllerState.providerConfig)) {
+    captureException(
+      new Error(
+        `Migration 33: NetworkController providerConfig not found: '${networkControllerState.providerConfig}'`,
       ),
     );
     return state;
