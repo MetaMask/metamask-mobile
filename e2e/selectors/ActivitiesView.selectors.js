2a85a0255c04c8594a8b04a4557f2bef7e988319
Samuel Salas <samuel.salas.reyes@gmail.com>
2024-04-10 11:47:13 -0600

    Change instances of messages to enContent while importing from locales

--- e2e/selectors/ActivitiesView.selectors.js
+++ e2e/selectors/ActivitiesView.selectors.js
@@ -1,7 +1,7 @@
-import messages from '../../locales/languages/en.json';
+import enContent from '../../locales/languages/en.json';
 
 // eslint-disable-next-line import/prefer-default-export
 export const ActivitiesViewSelectorsText = {
-  TITLE: messages.transactions_view.title,
-  SWAP: messages.swaps.transaction_label.swap,
+  TITLE: enContent.transactions_view.title,
+  SWAP: enContent.swaps.transaction_label.swap,
 };
