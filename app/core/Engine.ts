00bf4ee523918692e90b0242214a37031a758447
IF <139582705+infiniteflower@users.noreply.github.com>
2024-04-11 18:45:14 -0400

    Refactor file names of stx hook class

--- app/core/Engine.ts
+++ app/core/Engine.ts
@@ -195,7 +195,7 @@ import { NETWORKS_CHAIN_ID } from '../../app/constants/network';
 import { getIsSmartTransaction } from '../selectors/smartTransactionsController';
 import { getSwapsChainFeatureFlags } from '../reducers/swaps';
 import { SmartTransactionStatuses } from '@metamask/smart-transactions-controller/dist/types';
-import { submitSmartTransactionHook } from '../util/smart-transactions/smart-tx2';
+import { submitSmartTransactionHook } from '../util/smart-transactions/smart-tx';
 
 const NON_EMPTY = 'NON_EMPTY';
 
