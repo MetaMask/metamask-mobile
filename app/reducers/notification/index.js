332ce44fed50a6a0b64db7e8dd7abcdedc4a5840
JSoufer <jonathan.ferreira@consensys.net>
2024-04-16 12:44:26 +0100

    chore: add redux structure for notifications settings

--- app/reducers/notification/index.js
+++ app/reducers/notification/index.js
@@ -3,6 +3,7 @@ const { TRANSACTION, SIMPLE } = notificationTypes;
 
 export const initialState = {
   notifications: [],
+  notification: {},
 };
 
 export const ACTIONS = {
@@ -17,6 +18,7 @@ export const ACTIONS = {
   REMOVE_NOT_VISIBLE_NOTIFICATIONS: 'REMOVE_NOT_VISIBLE_NOTIFICATIONS',
   SHOW_SIMPLE_NOTIFICATION: 'SHOW_SIMPLE_NOTIFICATION',
   SHOW_TRANSACTION_NOTIFICATION: 'SHOW_TRANSACTION_NOTIFICATION',
+  UPDATE_NOTIFICATION_STATUS: 'UPDATE_NOTIFICATION_STATUS',
 };
 
 const enqueue = (notifications, notification) => [
@@ -187,6 +189,12 @@ const notificationReducer = (state = initialState, action) => {
       return {
         ...state,
         notifications: visibleNotifications,
+      };
+    }
+    case ACTIONS.UPDATE_NOTIFICATION_STATUS: {
+      return {
+        ...state,
+        notificationsSettings: action.notificationsSettings,
       };
     }
     default:
