911bdbc34fea6c04ff6a8254997de13cee611c01
Frank von Hoven <frank.vonhoven@consensys.net>
2024-04-16 16:28:24 -0500

    fix: rm unused getters from e2e & refactor done button

--- e2e/pages/Onboarding/OnboardingSuccessView.js
+++ e2e/pages/Onboarding/OnboardingSuccessView.js
@@ -1,28 +1,10 @@
 import { OnboardingSuccessSelectorIDs } from '../../selectors/Onboarding/OnboardingSuccess.selectors';
 import Matchers from '../../utils/Matchers';
 import Gestures from '../../utils/Gestures';
-import TestHelpers from '../../helpers';
 
 class OnboardingSuccessView {
-  get container() {
-    return device.getPlatform() === 'android'
-      ? Matchers.getElementByLabel(OnboardingSuccessSelectorIDs.CONTAINER_ID)
-      : Matchers.getElementByID(OnboardingSuccessSelectorIDs.CONTAINER_ID);
-  }
-
-  static async isVisible() {
-    if (!device.getPlatform() === 'android') {
-      // Check that we are on the onboarding success screen
-      await TestHelpers.checkIfExists(
-        OnboardingSuccessSelectorIDs.CONTAINER_ID,
-      );
-    }
-  }
-
   get doneButton() {
-    return device.getPlatform() === 'android'
-      ? Matchers.getElementByLabel(OnboardingSuccessSelectorIDs.DONE_BUTTON)
-      : Matchers.getElementByID(OnboardingSuccessSelectorIDs.DONE_BUTTON);
+    return Matchers.getElementByID(OnboardingSuccessSelectorIDs.DONE_BUTTON);
   }
 
   async tapDone() {
