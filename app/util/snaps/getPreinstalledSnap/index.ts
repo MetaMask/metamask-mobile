a531faf93e80d0ec8e1633fd7956c022f8d9fdf9
JSoufer <jonathan.ferreira@consensys.net>
2024-04-17 22:08:39 +0100

    chore: fix snapId type

--- app/util/snaps/getPreinstalledSnap/index.ts
+++ app/util/snaps/getPreinstalledSnap/index.ts
@@ -1,10 +1,11 @@
+import type { SnapId } from '@metamask/snaps-sdk';
 export default function getPreinstalledSnap(
   npmPackage: string,
   manifest: string,
   files: { path: string; value: string }[],
 ) {
   return {
-    snapId: `npm:${npmPackage}`,
+    snapId: `npm:${npmPackage}` as SnapId,
     manifest: JSON.parse(manifest),
     files: files.map((file) => ({
       path: file.path,
