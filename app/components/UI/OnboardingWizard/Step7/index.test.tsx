874614da929fd05d01983eaba8f511510eb2d664
JSoufer <jonathan.ferreira@consensys.net>
2024-04-18 08:27:26 +0100

    chore: replaces testing lib from enzyme to @testing-library

--- app/components/UI/OnboardingWizard/Step7/index.test.tsx
+++ app/components/UI/OnboardingWizard/Step7/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react-native';
 import Step7 from '.';
 import configureMockStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
@@ -9,11 +9,11 @@ const store = mockStore({});
 
 describe('Step7', () => {
   it('should render correctly', () => {
-    const wrapper = shallow(
+    const { toJSON } = render(
       <Provider store={store}>
         <Step7 />
       </Provider>,
     );
-    expect(wrapper).toMatchSnapshot();
+    expect(toJSON()).toMatchSnapshot();
   });
 });
