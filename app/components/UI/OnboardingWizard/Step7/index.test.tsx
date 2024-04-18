bae05ea5aa59a2757e9f11cc496eea56a775c055
JSoufer <jonathan.ferreira@consensys.net>
2024-04-18 23:07:33 +0100

    chore: updates tests

--- app/components/UI/OnboardingWizard/Step7/index.test.tsx
+++ app/components/UI/OnboardingWizard/Step7/index.test.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import renderWithProvider from '../../../../util/test/renderWithProvider';
 import Step7 from '.';
-import configureMockStore from 'redux-mock-store';
-import { Provider } from 'react-redux';
-
-const mockStore = configureMockStore();
-const store = mockStore({});
 
 describe('Step7', () => {
   it('should render correctly', () => {
-    const { toJSON } = render(
-      <Provider store={store}>
-        <Step7 />
-      </Provider>,
-    );
-    expect(toJSON()).toMatchSnapshot();
+    const container = renderWithProvider(<Step7 />);
+    expect(container).toMatchSnapshot();
   });
 });
