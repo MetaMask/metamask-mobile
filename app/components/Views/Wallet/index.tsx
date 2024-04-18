b44478819e3543d08360b38ff47db98f9dfc93b5
JSoufer <jonathan.ferreira@consensys.net>
2024-04-18 23:54:50 +0100

    chore: add seventh step awareness

--- app/components/Views/Wallet/index.tsx
+++ app/components/Views/Wallet/index.tsx
@@ -366,7 +366,7 @@ const Wallet = ({ navigation }: any) => {
    */
   const renderOnboardingWizard = useCallback(
     () =>
-      [1, 2, 3].includes(wizardStep) && (
+      [1, 2, 3, 4, 5, 6].includes(wizardStep) && (
         <OnboardingWizard
           navigation={navigation}
           coachmarkRef={walletRef.current}
