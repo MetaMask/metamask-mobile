8fff059522e3b47b555a160fdbcfb89c4c54a1f0
Jonathan Ferreira <44679989+Jonathansoufer@users.noreply.github.com>
2024-04-18 14:55:00 +0100

    feat: add new notifications badge (#9238)
    ## **Description**
    
    This PR implements a new badge element to be used within Notifications
    feature.
    
    ## **Related issues**
    
    N/A
    
    ## **Manual testing steps**
    
    N/A
    
    ## **Screenshots/Recordings**
    
    N/A 
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/util/notifications/types.ts
+++ app/util/notifications/types.ts
@@ -1,7 +1,8 @@
 export const NotificationTypes = {
-  transaction: 'transaction',
-  marketing: 'marketing',
-} as const;
+  TRANSACTION: 'transaction',
+  SIMPLE: 'simple',
+  ANNOUCEMENTS: 'annoucements',
+};
 
 export type NotificationTypesType =
   (typeof NotificationTypes)[keyof typeof NotificationTypes];
@@ -18,7 +19,7 @@ export const NotificationTransactionTypes = {
   cancelled: 'cancelled',
   received: 'received',
   received_payment: 'received_payment',
-} as const;
+};
 
 export type NotificationTransactionTypesType =
   (typeof NotificationTransactionTypes)[keyof typeof NotificationTransactionTypes];
@@ -27,4 +28,9 @@ export interface MarketingNotificationData {
   type: 'marketing';
   route?: string;
   routeProps?: string;
+}
+
+export enum NotificationsKindTypes {
+  transaction = 'transaction',
+  announcements = 'announcements',
 }
