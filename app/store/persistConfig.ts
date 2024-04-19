99fc3b839380b4e9b40f681545a5dce9c897e7f7
Aslau Mario-Daniel <marioaslau@gmail.com>
2024-04-19 11:34:46 +0100

    feat: added keys to getItem/setItem/removeItem logs

--- app/store/persistConfig.ts
+++ app/store/persistConfig.ts
@@ -18,7 +18,7 @@ const MigratedStorage = {
         return res;
       }
     } catch (error) {
-      Logger.error(error as Error, { message: 'Failed to get item' });
+      Logger.error(error as Error, { message: `Failed to get item for ${key}` });
     }
 
     // Using old storage system, should only happen once
@@ -37,14 +37,14 @@ const MigratedStorage = {
     try {
       return await FilesystemStorage.setItem(key, value, Device.isIos());
     } catch (error) {
-      Logger.error(error as Error, { message: 'Failed to set item' });
+      Logger.error(error as Error, { message: `Failed to set item for ${key}` });
     }
   },
   async removeItem(key: string) {
     try {
       return await FilesystemStorage.removeItem(key);
     } catch (error) {
-      Logger.error(error as Error, { message: 'Failed to remove item' });
+      Logger.error(error as Error, { message: `Failed to remove item for ${key}` });
     }
   },
 };
