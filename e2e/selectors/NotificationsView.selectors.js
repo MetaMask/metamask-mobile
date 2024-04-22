2e178dd3dbf3732ae4f5759546165002beac3274
Jonathan Ferreira <44679989+Jonathansoufer@users.noreply.github.com>
2024-04-22 19:39:10 +0100

    feat: notifications settings UI (#9258)
    ## **Description**
    
    This PR implements Notifications settings and wizard opt in UI.
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to Settings page and click on Notifications
    2. Enable Notifications settings.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    
    https://github.com/MetaMask/metamask-mobile/assets/44679989/a1ecc2c0-ba65-4866-a902-1efc77ecb58a
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: SamuelSalas <samuel.salas.reyes@gmail.com>
    Co-authored-by: Cal Leung <cleun007@gmail.com>

---
+++ e2e/selectors/NotificationsView.selectors.js
@@ -0,0 +1,12 @@
+import messages from '../../locales/languages/en.json';
+
+// eslint-disable-next-line import/prefer-default-export
+export const NotificationsViewSelectorsText = {
+  ADVANCE_TITLE_TEXT: messages.app_settings.advanced_title,
+  CONTACT_SUPPORT_TITLE: messages.app_settings.contact_support,
+};
+
+export const NotificationsViewSelectorsIDs = {
+  NOTIFICATIONS_CONTAINER: 'notifications-container',
+  NO_NOTIFICATIONS_CONTAINER: 'no-notifications-container',
+};
