MetaMask/metamask-mobile

commit ba6c1dc3794477d3c3c624926680e98acdfd5579

chore: improve yarn setup logs (#9025)
### Contributor experience improvement

- Simplify `yarn setup` logs.
- Make errors evident by displaying which task failed and what was the
problem.
- Create a foundation to extend the setup script to help turn
environment setup into a builtin interactive process.

New Dependencies:
- update execa to v8.x.x. [New API
support](https://github.com/sindresorhus/execa/blob/main/docs/scripts.md)
- add listr2, a [task list for the terminal](https://listr2.kilic.dev/)

## **Screenshots/Recordings**

Success setup

![new-setup-ok](https://github.com/MetaMask/metamask-mobile/assets/1649425/897dd00a-f7ac-4bc7-8176-d3fa8d166334)


Setup with error

![new-setup-fail](https://github.com/MetaMask/metamask-mobile/assets/1649425/8682e03b-6866-47ee-a549-155125b6f8f0)

CI non-TTY output
<img width="358" alt="image"
src="https://github.com/MetaMask/metamask-mobile/assets/1649425/325fcc68-9e03-4338-be9e-5b9bd08be911">

Co-authored-by: sethkfman <10342624+sethkfman@users.noreply.github.com>

scripts/setup.mjs
@@ -1,0 +1,202 @@
+import fs from 'fs';
+import { $ } from 'execa';
+import { Listr } from 'listr2';
+
+const rendererOptions = {
+  collapseErrors: false,
+  showSkipMessage: false,
+  suffixSkips: true,
+  collapseSubtasks: false
+};
+
+
+/*
+ * FIXME: We shouldn't be making system wide installs without user consent.
+ * Should make it optional on non-CI environments
+*/
+const detoxGlobalInstallTask = {
+  title: 'Install Detox utils',
+  task: (_, task) => task.newListr([
+    {
+      title: 'Install detox-cli globally',
+      task: async () => {
+        await $`yarn global add detox-cli`;
+      }
+    },
+    {
+      title: 'Install applesimutils globally',
+      task: async (_, appSimTask) => {
+        const isOSX = process.platform === 'darwin';
+        if (!isOSX) {
+          appSimTask.skip('Not macOS.');
+        } else {
+          await $`brew tap wix/brew`;
+          await $`brew install applesimutils`;
+        }
+      }
+    }
+  ],
+    {
+     rendererOptions
+    }
+  )
+};
+
+/*
+ * TODO: parse example env file and add missing variables to existing .js.env
+ */
+const copyEnvVarsTask = {
+  title: 'Copy env vars',
+  task: async (_, task) => {
+    const isCI = process.env.CI;
+    if (isCI) {
+      task.skip('CI detected')
+    } else {
+      const envFiles = [
+        '.js.env',
+        '.ios.env',
+        '.android.env'];
+      envFiles.forEach((envFileName) => {
+        try {
+          fs.copyFileSync(`${envFileName}.example`, envFileName, fs.constants.COPYFILE_EXCL);
+        } catch (err) {
+          // Ignore if file already exists
+          return;
+
+        }
+      })
+    }
+  }
+};
+
+const ppomBuildTask = {
+  title: 'Build PPOM',
+  task: (_, task) => {
+
+    const $ppom = $({ cwd: 'ppom' });
+
+    return task.newListr([
+      {
+        title: 'Clean',
+        task: async () => {
+          await $ppom`yarn clean`;
+        }
+      },
+      {
+        title: 'Install deps',
+        task: async () => {
+          await $ppom`yarn`;
+        }
+      },
+      {
+        title: 'Lint',
+        task: async () => {
+          await $ppom`yarn lint`;
+        }
+      },
+      {
+        title: 'Build',
+        task: async () => {
+          await $ppom`yarn build`;
+        }
+      }
+    ], {
+      concurrent: false,
+      exitOnError: true,
+    })
+  }
+}
+
+const mainSetupTask = {
+  title: 'Dependencies setup',
+  task: (_, task) => task.newListr([
+    {
+      title: 'Install iOS Pods',
+      task: async (_, podInstallTask) => {
+        const isOSX = process.platform === 'darwin';
+        if (!isOSX) {
+          podInstallTask.skip('Not macOS.');
+        } else {
+          try {
+            await $`pod install --project-directory=ios`;
+          } catch (error) {
+            throw new Error(error);
+          }
+        }
+      },
+    },
+    {
+      title: 'Run lavamoat allow-scripts',
+      task: async () => {
+        await $`yarn allow-scripts`;
+      },
+    },
+    copyEnvVarsTask,
+    detoxGlobalInstallTask
+  ],
+    {
+      exitOnError: false,
+      concurrent: true,
+      rendererOptions
+    })
+};
+
+const patchModulesTask = {
+  title: 'Patch modules',
+  task: (_, task) => task.newListr([
+    {
+      title: 'Build Inpage Bridge',
+      task: async () => {
+        await $`./scripts/build-inpage-bridge.sh`;
+      }
+    },
+    // TODO: find a saner alternative to bring node modules into react native bundler. See ReactNativify
+    {
+      title: 'React Native nodeify',
+      task: async () => {
+        await $`node_modules/.bin/rn-nodeify --install crypto,buffer,react-native-randombytes,vm,stream,http,https,os,url,net,fs --hack`;
+      }
+    },
+    {
+      title: 'Jetify',
+      task: async () => {
+        await $`yarn jetify`;
+      }
+    },
+    {
+      title: 'Patch npm packages',
+      task: async () => {
+        await $`yarn patch-package`;
+      }
+    },
+    {
+      title: 'Create xcconfig files',
+      task: async () => {
+        fs.writeFileSync('ios/debug.xcconfig', '');
+        fs.writeFileSync('ios/release.xcconfig', '');
+      }
+    },
+    {
+      title: 'Init git submodules',
+      task: async () => {
+        await $`git submodule update --init`;
+      }
+    },
+  ],
+    {
+      concurrent: false,
+      exitOnError: true,
+    })
+}
+const tasks = new Listr([
+  mainSetupTask,
+  ppomBuildTask,
+  patchModulesTask
+],
+  {
+    exitOnError: true,
+    concurrent: true,
+  });
+
+await tasks.run();
+

scripts/build_ppom.sh
@@ -1,5 +1,0 @@
-# The script invokes tool to generate ppom.html.js
-# This is integrated into "yarn setup"
-cd ./ppom
-yarn clean && yarn && yarn lint && yarn build
-cd ..

scripts/detox-setup.sh
@@ -1,12 +1,0 @@
-#!/bin/bash
-
-# Install detox-cli
-echo "Installing detox-cli..."
-yarn global add detox-cli
-
-# Install applesimutils
-echo "Installing applesimutils..."
-brew tap wix/brew
-brew install applesimutils
-
-echo "Installation complete."

scripts/postinstall.sh
@@ -1,40 +1,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo "PostInstall script:"
-
-echo "1. Build Inpage Bridge..."
-./scripts/build-inpage-bridge.sh
-
-echo "2. React Native nodeify..."
-node_modules/.bin/rn-nodeify --install 'crypto,buffer,react-native-randombytes,vm,stream,http,https,os,url,net,fs' --hack
-
-echo "3. jetify"
-yarn jetify
-
-echo "4. Patch npm packages"
-yarn patch-package
-
-echo "5. Create xcconfig files..."
-echo "" > ios/debug.xcconfig
-echo "" > ios/release.xcconfig
-
-echo "6. Init git submodules"
-echo "This may take a while..."
-git submodule update --init
-
-if ! [ -n "${CI+isset}" ] ; then
-  echo "7. CI not detected, copying env variables..."
-  if [ ! -f .js.env ]; then
-      echo "Creating .js.env..."
-      cp .js.env.example .js.env
-  fi
-  if [ ! -f .android.env ]; then
-      echo "Creating .android.env..."
-      cp .android.env.example .android.env
-  fi
-  if [ ! -f .ios.env ]; then
-      echo "Creating .ios.env"
-      cp .ios.env.example .ios.env
-  fi
-fi

commit 2e680d2df04911e5a4bf5a31b19699d5fedc819c

fix: Fix intermittent install failures
A new setup script was introduced in #9025 that caused certain parts of
the `yarn setup` command to run in parallel. Since then we have been
seeing intermittent failures during setup, for example this failed
build: https://app.bitrise.io/build/f748e322-fc0b-4c1a-ae72-5a7ce642304a#c69dfbdb-778d-4ba1-b2a0-c71088b0f878-error

This failure is caused by the "React Native nodeify" step running in
parallel with the "allow-scripts" step, which runs install scripts
needed by later setup steps.

The setup script has been updated to run these steps sequentially, as
they were run before. There are opportunities to do some of these steps
in parallel, but we need to ensure that any steps that operate on
`node_modules` run in a predictable order and complete prior to any
later steps that rely on `node_modules`.

scripts/setup.mjs
@@ -195,7 +195,7 @@ const tasks = new Listr([
 ],
   {
     exitOnError: true,
-    concurrent: true,
+    concurrent: false,
   });
 
 await tasks.run();