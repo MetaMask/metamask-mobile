ed4c3cee084b0297e8ae803e3c538fbbce1672e7
Jonathan Ferreira <44679989+Jonathansoufer@users.noreply.github.com>
2024-04-23 17:40:57 +0100

    feat: add notifications onboarding wizard (#9263)
    ## **Description**
    
    This PR implements a new step on the onboarding wizard to teach users
    about notifications feature.
    
    ## **Related issues**
    
    N/A
    
    ## **Manual testing steps**
    
    Install a fresh app and accept the tour at beginning. 
    
    ## **Screenshots/Recordings**
    
    https://github.com/MetaMask/metamask-mobile/assets/44679989/ff6273cb-613c-4b4d-afe8-99fb63d5445f
    
    
    ### **After**
    ![Screenshot 2024-04-16 at 17 37
    21](https://github.com/MetaMask/metamask-mobile/assets/44679989/faf5526d-a0e5-4c92-8dd4-fa65c08573a9)
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/UI/OnboardingWizard/Step4/index.test.tsx
+++ app/components/UI/OnboardingWizard/Step4/index.test.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import Step4 from '.';
 import { shallow } from 'enzyme';
-import Step4 from './';
 import configureMockStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
+import initialBackgroundState from '../../../../util/test/initial-background-state.json';
 
 const mockStore = configureMockStore();
-const store = mockStore({});
+const initialState = {
+  engine: {
+    backgroundState: initialBackgroundState,
+  },
+};
+const store = mockStore(initialState);
 
 describe('Step4', () => {
   it('should render correctly', () => {
     const wrapper = shallow(
       <Provider store={store}>
-        <Step4 />
+        <Step4 coachmarkRef={{}} />
       </Provider>,
     );
     expect(wrapper).toMatchSnapshot();
