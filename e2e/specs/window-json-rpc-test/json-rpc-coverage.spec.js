d4f276bf112aeb17f257eb7d42a8603bba98b152
Thomas Huang <thomas.b.huang@gmail.com>
2024-04-23 10:34:44 -0700

    open rpc test coverage tool implement

--- e2e/specs/window-json-rpc-test/json-rpc-coverage.spec.js
+++ e2e/specs/window-json-rpc-test/json-rpc-coverage.spec.js
@@ -3,6 +3,7 @@ import { web } from 'detox';
 import testCoverage from '@open-rpc/test-coverage';
 import { parseOpenRPCDocument } from '@open-rpc/schema-utils-js';
 import JsonSchemaFakerRule from '@open-rpc/test-coverage/build/rules/json-schema-faker-rule';
+import HtmlReporter from '@open-rpc/test-coverage/build/reporters/html-reporter';
 import TestHelpers from '../../helpers';
 import { SmokeCore } from '../../tags';
 import Browser from '../../pages/Browser';
@@ -16,87 +17,103 @@ import ConnectModal from '../../pages/modals/ConnectModal';
 
 import Assertions from '../../utils/Assertions';
 
-describe(SmokeCore('OpenRPC method coverage'), () => {
+describe(SmokeCore(''), () => {
   beforeAll(async () => {
     jest.setTimeout(150000);
     await TestHelpers.reverseServerPort();
   });
 
-  it('asdf', async () => {
+  it('', async () => {
     await withFixtures(
       {
         dapp: true,
-        fixture: new FixtureBuilder()
-          // .withPermissionControllerConnectedToTestDapp()
-          .build(),
+        fixture: new FixtureBuilder().build(),
         restartDevice: true,
       },
       async () => {
+        const openrpcDocument = await parseOpenRPCDocument(
+          'https://metamask.github.io/api-specs/latest/openrpc.json',
+        );
+
         await loginToApp();
         await TabBarComponent.tapBrowser();
         await Browser.isVisible();
         await Browser.navigateToTestDApp();
 
-        const webElement = await web.element(by.web.tag('body'));
+        const webElement = await web.element(by.web.id('json-rpc-response'));
         await webElement.scrollToView();
 
-        // const pollResult = async () => {
-        //   let result = await webElement.runScript(`() => {
-        //     return window.response
-        //   }`);
+        const pollResult = async () => {
+          let result = await webElement.runScript(`(el) => {
+            return el.innerHTML
+          }`);
 
-        //   while (result === isEmpty) {
-        //     await TestHelpers.delay(50);
-        //     result = await webElement.runScript(`() => {
-        //       return window.response
-        //     }`);
-        //   }
+          while (result.result === '') {
+            await TestHelpers.delay(500);
+            result = await webElement.runScript(`(el) => {
+              return el.innerHTML
+            }`);
+          }
 
-        //   await webElement.runScript(`() => {
-        //     window.response = {}
-        //   }`);
+          // await web.element(by.web.id('json-rpc-response')).replaceText('');
 
-        //   return result;
-        // };
+          return result;
+        };
 
-        // await webElement.runScript(`(el) => {
-        //   window.response = {};
-        //   window.ethereum.request({
-        //     method: 'wallet_requestPermissions',
-        //     params: [{
-        //       eth_accounts: {}
-        //     }]
-        //   }).then((res) => {
-        //     window.response = { result: res };
-        //   }).catch((err) => {
-        //     windows.response = {
-        //       error: {
-        //         code: err.code,
-        //         message: err.message,
-        //         data: err.date
-        //       }
-        //     }
-        //   });
-        // }`);
+        const createDriverTransport = (driver) => async (_, method, params) => {
+          await driver.runScript(
+            (el, m, p) => {
+              window.ethereum
+                .request({ method: m, params: p })
+                .then((res) => (el.innerHTML = JSON.stringify(res)))
+                .catch((err) => (el.innerHTML = JSON.stringify(err)));
+            },
+            [method, params],
+          );
+          const response = await pollResult();
+          return response;
+        };
 
-        await webElement.runScript(`(el) => {
-          window.ethereum.request({
-            method: 'wallet_requestPermissions',
-            params: [{
-              eth_accounts: {}
-            }]
-          }).then((res) => {
-            el.innerHTML = JSON.stringify(res);
-          }).catch((err) => {
-            el.innerHTML = JSON.stringify({
-              error: {
-                code: err.code,
-                message: err.message,
-                data: err.date
-              }
-            })
-          });
-        }`);
+        const transport = createDriverTransport(webElement);
+        // await transport('', 'wallet_registerOnboarding', []);
+
+        const methodsWithConfirmations = [
+          'wallet_requestPermissions',
+          'eth_requestAccounts',
+          'wallet_watchAsset',
+          'personal_sign', // requires permissions for eth_accounts
+          'wallet_addEthereumChain',
+          'eth_signTypedData_v4', // requires permissions for eth_accounts
+          'wallet_switchEthereumChain',
+          'eth_getEncryptionPublicKey', // requires permissions for eth_accounts
+        ];
+
+        const filteredMethods = openrpcDocument.methods
+          .filter(
+            (m) =>
+              m.name.includes('snap') ||
+              m.name.includes('Snap') ||
+              m.name.toLowerCase().includes('account') ||
+              m.name.includes('crypt') ||
+              m.name.includes('blob') ||
+              m.name.includes('sendTransaction') ||
+              m.name.startsWith('wallet_scanQRCode') ||
+              methodsWithConfirmations.includes(m.name),
+          )
+          .map((m) => m.name);
+
+        await testCoverage({
+          openrpcDocument,
+          transport,
+          reporters: ['console'],
+          rules: [
+            new JsonSchemaFakerRule({
+              only: [],
+              skip: filteredMethods,
+              numCalls: 1,
+            }),
+          ],
+        });
       },
     );
   });
