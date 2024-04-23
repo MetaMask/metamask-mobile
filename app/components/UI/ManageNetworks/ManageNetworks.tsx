571c5f0d49d13f1cdb1e428b27aff58e48c0cc83
Frank von Hoven <frank.vonhoven@consensys.net>
2024-04-23 12:37:49 -0500

    fix: add styles, & snapshot for ManageNetworks Component

--- app/components/UI/ManageNetworks/ManageNetworks.tsx
+++ app/components/UI/ManageNetworks/ManageNetworks.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react';
-import { View, StyleSheet, Linking } from 'react-native';
+import { View, Linking } from 'react-native';
 import Text, {
   TextVariant,
   TextColor,
@@ -20,28 +20,7 @@ import { useMetrics } from '../../../components/hooks/useMetrics';
 import { MetaMetricsEvents } from '../../../core/Analytics';
 import { ConnectedAccountsSelectorsIDs } from '../../../../e2e/selectors/Modals/ConnectedAccountModal.selectors';
 import AppConstants from '../../../core/AppConstants';
-
-const styles = StyleSheet.create({
-  setting: {
-    marginVertical: 16,
-  },
-  heading: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  description: {
-    fontSize: 14,
-    textAlign: 'left',
-    marginTop: 10,
-    lineHeight: 22,
-    fontWeight: '400',
-  },
-  networkPicker: {
-    marginVertical: 16,
-    alignSelf: 'flex-start',
-  },
-});
+import styles from './ManageNetworks.styles';
 
 export default function ManageNetworksComponent() {
   const providerConfig: ProviderConfig = useSelector(selectProviderConfig);
