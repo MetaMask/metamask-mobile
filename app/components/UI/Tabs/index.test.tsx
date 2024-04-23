9dcb04b79d21db4a2503194ccc57a7777fb01552
EtherWizard33 <eric.lamontagne@consensys.net>
2024-04-23 16:07:00 -0400

    test: add missing mock state to tabs test

--- app/components/UI/Tabs/index.test.tsx
+++ app/components/UI/Tabs/index.test.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import renderWithProvider from '../../../util/test/renderWithProvider';
 import Tabs from './';
+import initialBackgroundState from '../../../util/test/initial-background-state.json';
+
+const mockInitialState = {
+  wizard: {
+    step: 1,
+  },
+  engine: {
+    backgroundState: initialBackgroundState,
+  },
+};
 
 describe('Tabs', () => {
   it('should render correctly', () => {
     const { toJSON } = renderWithProvider(
-      <Tabs tabs={[{ id: 1, url: 'about:blank', image: '' }]} />,
+      <Tabs tabs={[{ id: 1, url: 'about:blank', image: '', id: 123 }]} />,
+      { state: mockInitialState },
     );
     expect(toJSON()).toMatchSnapshot();
   });
