5512e8d8f50a028956638409fdddef68de6a475d
Derek Brans <dbrans@gmail.com>
2024-04-25 13:41:29 -0400

    AmountPill

---
+++ app/components/UI/SimulationDetails/AmountPill.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { BigNumber } from 'bignumber.js';
+import { AssetIdentifier, AssetType } from './types';
+import { formatAmount, formatAmountMaxPrecision } from './formatAmount';
+import styleSheet from './AmountPill.styles';
+import { View, ViewProps } from 'react-native';
+import Text, {
+  TextVariant,
+} from '../../../component-library/components/Texts/Text';
+import { useStyles } from '../../hooks/useStyles';
+import { hexToDecimal } from '../../../util/conversions';
+
+interface AmountPillProperties extends ViewProps {
+  asset: AssetIdentifier;
+  amount: BigNumber;
+}
+/**
+ * Displays a pill with an amount and a background color indicating whether the amount
+ * is an increase or decrease.
+ *
+ * @param props
+ * @param props.asset
+ * @param props.amount
+ */
+const AmountPill: React.FC<AmountPillProperties> = ({
+  asset,
+  amount,
+  style,
+  ...props
+}) => {
+  // DO NOT MERGE: This is a placeholder for the locale.
+  const locale = 'en-US';
+
+  const { styles } = useStyles(styleSheet, {
+    style,
+    isNegative: amount.isNegative(),
+  });
+  const amountParts: string[] = [amount.isNegative() ? '-' : '+'];
+  const tooltipParts: string[] = [];
+
+  // ERC721 amounts are always 1 and are not displayed.
+  if (asset.type !== AssetType.ERC721) {
+    const formattedAmount = formatAmount(locale, amount.abs());
+    const fullPrecisionAmount = formatAmountMaxPrecision(locale, amount.abs());
+
+    amountParts.push(formattedAmount);
+    tooltipParts.push(fullPrecisionAmount);
+  }
+
+  if (asset.tokenId) {
+    const tokenIdPart = `#${hexToDecimal(asset.tokenId)}`;
+
+    amountParts.push(tokenIdPart);
+    tooltipParts.push(tokenIdPart);
+  }
+
+  return (
+    <View
+      testID="simulation-details-amount-pill"
+      style={styles.base}
+      {...props}
+    >
+      <Text
+        ellipsizeMode="tail"
+        variant={TextVariant.BodyMD}
+        style={styles.label}
+      >
+        {amountParts.join(' ')}
+      </Text>
+    </View>
+  );
+};
+
+export default AmountPill;
