46b73ea61e181b196b9951a1206afca338e53cbd
JSoufer <jonathan.ferreira@consensys.net>
2024-04-26 15:35:41 +0100

    chore: add unit tests

---
+++ app/components/UI/Notification/Row/Actions.test.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Linking } from 'react-native';
+import { fireEvent } from '@testing-library/react-native';
+
+import renderWithProvider from '../../../../util/test/renderWithProvider';
+
+import NotificationActions from './Actions';
+import { ACTIONS, PREFIXES } from '../../../../constants/deeplinks';
+
+Linking.openURL = jest.fn(() => Promise.resolve('opened https://metamask.io!'));
+
+describe('NotificationActions', () => {
+  const styles = {
+    button: {},
+  };
+  const action = {
+    actionText: 'Send now!',
+    actionUrl: PREFIXES[ACTIONS.SEND],
+    isExternal: false,
+  };
+  const link = {
+    linkText: 'Learn more',
+    linkUrl: 'https://metamask.io',
+    isExternal: true,
+  };
+
+  it('renders correctly', () => {
+    const { toJSON } = renderWithProvider(
+      <NotificationActions action={action} link={link} styles={styles} />,
+    );
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders correctly with LINK only', () => {
+    const { toJSON } = renderWithProvider(
+      <NotificationActions link={link} styles={styles} />,
+    ).toJSON();
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders link title correctly', () => {
+    const { getByText } = renderWithProvider(
+      <NotificationActions link={link} styles={styles} />,
+    );
+
+    expect(getByText('Learn more')).toBeTruthy();
+  });
+
+  it('Calls Linking.openURL when link CTA is clicked', async () => {
+    const { getByTestId } = renderWithProvider(
+      <NotificationActions link={link} styles={styles} />,
+    );
+
+    fireEvent.press(getByTestId('notification-actions-button'));
+    expect(Linking.openURL).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ app/components/UI/Notification/Row/Icon.test.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Linking } from 'react-native';
+
+import renderWithProvider from '../../../../util/test/renderWithProvider';
+
+import NotificationIcon from './Icon';
+import { TRIGGER_TYPES } from '../../../../util/notifications';
+import { IconName } from '../../../../component-library/components/Icons/Icon';
+import initialBackgroundState from '../../../../util/test/initial-background-state.json';
+import { CommonSelectorsIDs } from '../../../../../e2e/selectors/Common.selectors';
+
+Linking.openURL = jest.fn(() => Promise.resolve('opened https://metamask.io!'));
+
+const mockInitialState = {
+  engine: {
+    backgroundState: {
+      ...initialBackgroundState,
+    },
+  },
+};
+
+describe('NotificationIcon', () => {
+  const styles = {
+    foxWrapper: {},
+    fox: {},
+    nftLogo: {},
+    nftPlaceholder: {},
+    assetLogo: {},
+    assetPlaceholder: {},
+    badgeWrapper: {},
+    ethLogo: {},
+  };
+
+  const announcementNotification = {
+    notificationType: TRIGGER_TYPES.FEATURES_ANNOUNCEMENT,
+    imageUrl:
+      'https://github.com/MetaMask/brand-resources/blob/master/SVG/SVG_MetaMask_Icon_Color.svg',
+  };
+
+  const walletNotification = {
+    notificationType: TRIGGER_TYPES.ERC20_SENT,
+    badgeIcon: IconName.Send2,
+    imageUrl:
+      'https://raw.githubusercontent.com/MetaMask/contract-metadata/master/images/usdc.svg',
+  };
+
+  it('renders correctly with Wallet type', () => {
+    const { toJSON } = renderWithProvider(
+      <NotificationIcon
+        notificationType={walletNotification.notificationType}
+        imageUrl={walletNotification.imageUrl}
+        styles={styles}
+      />,
+      {
+        state: mockInitialState,
+      },
+    );
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders correctly with Announcement type', () => {
+    const { toJSON } = renderWithProvider(
+      <NotificationIcon
+        notificationType={announcementNotification.notificationType}
+        imageUrl={announcementNotification.imageUrl}
+        styles={styles}
+      />,
+    );
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Fox icon when notification type is FEATURES_ANNOUNCEMENT', () => {
+    const { getByTestId } = renderWithProvider(
+      <NotificationIcon
+        notificationType={announcementNotification.notificationType}
+        imageUrl={announcementNotification.imageUrl}
+        styles={styles}
+      />,
+    );
+
+    const foxIcon = getByTestId(CommonSelectorsIDs.FOX_ICON);
+    expect(foxIcon).toBeTruthy();
+  });
+});
