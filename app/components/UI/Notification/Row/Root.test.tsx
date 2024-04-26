8e636cddc61ba9bd897100210d8703eabf7a4ee5
JSoufer <jonathan.ferreira@consensys.net>
2024-04-26 22:56:48 +0100

    chore: fix lint

--- app/components/UI/Notification/Row/Root.test.tsx
+++ app/components/UI/Notification/Row/Root.test.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { Linking } from 'react-native';
-import { fireEvent } from '@testing-library/react-native';
 
 import renderWithProvider from '../../../../util/test/renderWithProvider';
 
 import NotificationRoot from './Root';
-import { ACTIONS, PREFIXES } from '../../../../constants/deeplinks';
 
 const children = <></>;
 const styles = {
@@ -16,13 +13,15 @@ const onDismiss = jest.fn();
 const handleOnPress = jest.fn();
 
 describe('NotificationRoot', () => {
-
-
   it('renders correctly', () => {
     const { toJSON } = renderWithProvider(
-      <NotificationRoot handleOnPress={handleOnPress} onDismiss={onDismiss} styles={styles} children={children}/>,
+      <NotificationRoot
+        handleOnPress={handleOnPress}
+        onDismiss={onDismiss}
+        styles={styles}
+        children={children}
+      />,
     );
     expect(toJSON()).toMatchSnapshot();
   });
-
 });
