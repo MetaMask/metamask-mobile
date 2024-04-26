49d2248ec4874890bd6a4b59ce6026dd682b9e94
Curtis David <Curtis.David7@gmail.com>
2024-04-26 13:47:01 -0400

    test: fix flakey E2E tests (#9413)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    The purpose of this PR is to stabilize the e2e tests on main. What was
    done in this PR:
    
    - Detox environment setup time and tear down time was increased
    - Why? Detox was attempting to execute tests prior to the
    simulator/emulator booting up. This caused the tests to fail with the
    error: `Exceeded timeout of 30000ms while tearing down Detox
    environment` or `Exceeded timeout of 25000ms while setting up Detox
    environment`
    
    - There was a duplicate device.launchapp() step in the before hook for
    the NFT import test. This caused the test to double launch, perhaps a
    symptom of why the test is unstable.
    
    - The swap-regression test was flaking on Android as well. The app would
    fetch quotes when swapping ERC20 <> ETH during the test running, which
    introduced instability. We should follow up in a separate task to
    address this.
    
     
    - Sign tests in the confirmations folder were also unstable. Each sign
    spec file contains the following tests: one to sign a transaction and
    the other to cancel it. Each test used fixtures to set up the app in a
    specific state. However, this resulted in some instability in the
    confirmation signature tests. Specifically, a few tests attempted to use
    a fixture port that was already in use, leading to errors. This needs to
    be investigated further in a follow-up task. Lastly, The time it takes
    to run the signature tests was too lengthy.
    - I condense the test actions into one `it` block, which makes the tests
    run quicker and more stable.
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    Passing Smoke tests:
    
    https://app.bitrise.io/app/be69d4368ee7e86d/pipelines/0572909c-97af-40f3-addf-8a2b02da1cf2
    
    https://app.bitrise.io/app/be69d4368ee7e86d/pipelines/f6f1f081-4707-41bc-8c1a-955c6b2a6163
    
    https://app.bitrise.io/app/be69d4368ee7e86d/pipelines/ae68f7f2-34b9-47fa-a31e-47be939ea3ef
    
    Passing regression:
    
    https://app.bitrise.io/app/be69d4368ee7e86d/pipelines/46fe763c-1c62-401f-a6b8-b0b86484ce39
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- e2e/specs/confirmations/signatures/eth-sign.spec.js
+++ e2e/specs/confirmations/signatures/eth-sign.spec.js
@@ -45,39 +45,13 @@ describe(SmokeConfirmations('Eth Sign'), () => {
         await TestHelpers.retry(MAX_ATTEMPTS, async () => {
           await TestDApp.tapEthSignButton();
           await SigningModal.isEthRequestVisible();
-          await SigningModal.tapSignButton();
+          await SigningModal.tapCancelButton();
+
           await SigningModal.isNotVisible();
-        });
-      },
-    );
-  });
 
-  it('should cancel eth_sign message', async () => {
-    await withFixtures(
-      {
-        dapp: true,
-        fixture: new FixtureBuilder()
-          .withGanacheNetwork()
-          .withPreferencesController({
-            disabledRpcMethodPreferences: {
-              eth_sign: true,
-            },
-          })
-          .withPermissionControllerConnectedToTestDapp()
-          .build(),
-        restartDevice: true,
-        ganacheOptions: defaultGanacheOptions,
-      },
-      async () => {
-        await loginToApp();
-
-        await TabBarComponent.tapBrowser();
-        await Browser.navigateToTestDApp();
-
-        await TestHelpers.retry(MAX_ATTEMPTS, async () => {
           await TestDApp.tapEthSignButton();
           await SigningModal.isEthRequestVisible();
-          await SigningModal.tapCancelButton();
+          await SigningModal.tapSignButton();
           await SigningModal.isNotVisible();
         });
       },
