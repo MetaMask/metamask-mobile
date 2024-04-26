9f826e1f44e81c76974ac721a693da17f06c4464
IF <139582705+infiniteflower@users.noreply.github.com>
2024-04-26 12:35:25 -0400

    fix: lint errors

--- app/components/Views/SmartTransactionStatus/ProgressBar.tsx
+++ app/components/Views/SmartTransactionStatus/ProgressBar.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import Logger from '../../../util/Logger';
 import { useTheme } from '../../../util/theme';
 
 interface Props {
