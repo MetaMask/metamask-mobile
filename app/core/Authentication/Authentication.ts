ae46654bcc12f3f2dd5e52ee99a5a975db79185a
tommasini <tommasini15@gmail.com>
2024-04-29 23:38:02 +0100

    function created to initialize EncryptionLib key on the storage if does not exist

--- app/core/Authentication/Authentication.ts
+++ app/core/Authentication/Authentication.ts
@@ -42,6 +42,13 @@ export interface AuthData {
   availableBiometryType?: BIOMETRY_TYPE;
 }
 
+const initializeEncryptionLibSetting = async () => {
+  const encryptionLib = await AsyncStorage.getItem(ENCRYPTION_LIB);
+  if (!encryptionLib) {
+    await AsyncStorage.setItem(ENCRYPTION_LIB, ORIGINAL);
+  }
+};
+
 class AuthenticationService {
   private authData: AuthData = { currentAuthType: AUTHENTICATION_TYPE.UNKNOWN };
   private store: Store | undefined = undefined;
@@ -351,10 +358,7 @@ class AuthenticationService {
       await this.storePassword(password, authData?.currentAuthType);
       await AsyncStorage.setItem(EXISTING_USER, TRUE);
       await AsyncStorage.removeItem(SEED_PHRASE_HINTS);
-      const encryptionLib = await AsyncStorage.getItem(ENCRYPTION_LIB);
-      if (!encryptionLib) {
-        await AsyncStorage.setItem(ENCRYPTION_LIB, ORIGINAL);
-      }
+      await initializeEncryptionLibSetting();
       this.dispatchLogin();
       this.authData = authData;
     } catch (e: any) {
@@ -386,10 +390,7 @@ class AuthenticationService {
       await this.storePassword(password, authData.currentAuthType);
       await AsyncStorage.setItem(EXISTING_USER, TRUE);
       await AsyncStorage.removeItem(SEED_PHRASE_HINTS);
-      const encryptionLib = await AsyncStorage.getItem(ENCRYPTION_LIB);
-      if (!encryptionLib) {
-        await AsyncStorage.setItem(ENCRYPTION_LIB, ORIGINAL);
-      }
+      await initializeEncryptionLibSetting();
       this.dispatchLogin();
       this.authData = authData;
     } catch (e: any) {
