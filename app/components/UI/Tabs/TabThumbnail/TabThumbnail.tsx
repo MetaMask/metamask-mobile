90fedb6066529f83a5835d290d754063fc0f6a63
EtherWizard33 <eric.lamontagne@consensys.net>
2024-05-01 11:56:30 -0400

    feat: make use of localizations

--- app/components/UI/Tabs/TabThumbnail/TabThumbnail.tsx
+++ app/components/UI/Tabs/TabThumbnail/TabThumbnail.tsx
@@ -54,7 +54,7 @@ const TabThumbnail = ({
     <Container style={styles.checkWrapper} elevation={8}>
       <TouchableOpacity
         accessible
-        accessibilityLabel="Switch tab"
+        accessibilityLabel={strings('browser.switch_tab')}
         onPress={() => onSwitch(tab)}
         style={[styles.tabWrapper, isActiveTab && styles.activeTab]}
       >
@@ -76,7 +76,7 @@ const TabThumbnail = ({
           </View>
           <TouchableOpacity
             accessible
-            accessibilityLabel="Close tab"
+            accessibilityLabel={strings('browser.close_tab')}
             onPress={() => onClose(tab)}
             hitSlop={{ top: 10, left: 10, bottom: 10, right: 10 }}
           >
@@ -117,7 +117,9 @@ const TabThumbnail = ({
             numberOfLines={1}
             ellipsizeMode="tail"
           >
-            {selectedAccount?.name ?? 'undefined account'} - {networkName}
+            {`${selectedAccount?.name ?? strings('browser.undefined_account')}${
+              networkName ? ` - ${networkName}` : ''
+            }`}
           </Text>
         </View>
       </TouchableOpacity>
