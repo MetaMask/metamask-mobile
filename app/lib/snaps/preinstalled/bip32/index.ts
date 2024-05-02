48a76a655c9673a33446524aad0b1fac3494de3c
JSoufer <jonathan.ferreira@consensys.net>
2024-05-02 15:01:44 +0100

    chore: replace relative paths

--- app/lib/snaps/preinstalled/bip32/index.ts
+++ app/lib/snaps/preinstalled/bip32/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports, import/no-commonjs*/
 import { assert } from '@metamask/utils';
-const bundleJs = require('../../../../../node_modules/@metamask/bip32-example-snap/dist/bundle.js');
-const manifestJson = require('../../../../../node_modules/@metamask/bip32-example-snap/snap.manifest.json');
+const bundleJs = require('@metamask/bip32-example-snap/dist/bundle.js');
+const manifestJson = require('@metamask/bip32-example-snap/snap.manifest.json');
 
 assert(bundleJs, new TypeError(`BundleJs not found`));
 assert(manifestJson, new TypeError(`manifestJson not found`));
@@ -11,7 +11,7 @@ const iconPath = 'images/icon.svg',
   name = '@metamask/bip32-example-snap';
 try {
   // eslint-disable-next-line @typescript-eslint/no-require-imports
-  icon = require('../../../../../node_modules/@metamask/bip32-example-snap/images/icon.svg');
+  icon = require('@metamask/bip32-example-snap/images/icon.svg');
 } catch (error) {
   icon = '';
 }
