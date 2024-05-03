18fc6398d4a33a1a2b4fa54b6d88a4b916e7aa7a
EtherWizard33 <eric.lamontagne@consensys.net>
2024-05-03 12:46:06 -0400

    test: refactor to make more testable

---
+++ app/components/Views/Browser/utils.js
@@ -0,0 +1,17 @@
+import { getBrowserViewNavbarOptions } from '../../UI/Navbar';
+
+const setNavOptions =
+  (route, colors, handleRightTopButtonAnalyticsEvent, navigation) => () => {
+    const options = {
+      ...getBrowserViewNavbarOptions(
+        route,
+        colors,
+        handleRightTopButtonAnalyticsEvent,
+      ),
+      headerShown: !(route.params?.showTabs ?? false),
+    };
+
+    return navigation.setOptions(options);
+  };
+
+export default setNavOptions;
