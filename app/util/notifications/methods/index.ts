c1e7c9431e6642830fd6a11cb1e5a4855972a7e6
sethkfman <Seth.Kaufman@consensys.net>
2024-05-06 10:43:29 -0600

    lint fix

--- app/util/notifications/methods/index.ts
+++ app/util/notifications/methods/index.ts
@@ -161,9 +161,8 @@ export function getNetwork(chain_id: HalRawNotification['chain_id']) {
   return ChainId[chain_id];
 }
 
-export const isNotificationsFeatureEnabled = () => {
-  return process.env.MM_NOTIFICATIONS_UI_ENABLED === 'true';
-};
+export const isNotificationsFeatureEnabled = () =>
+  process.env.MM_NOTIFICATIONS_UI_ENABLED === 'true';
 
 export function formatNotificationTitle(rawTitle: string): string {
   const words = rawTitle.split('_');
