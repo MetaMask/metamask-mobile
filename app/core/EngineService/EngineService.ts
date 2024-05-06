a10eac7c0304d4e186b551d7b8499eef71963f5d
Aslau Mario-Daniel <marioaslau@gmail.com>
2024-05-06 14:52:42 +0300

    feat: fix logs

--- app/core/EngineService/EngineService.ts
+++ app/core/EngineService/EngineService.ts
@@ -111,7 +111,7 @@ class EngineService {
     }
 
     engine?.datamodel?.subscribe?.(() => {
-      if (engine.context.KeyringController.vault) {
+      if (!engine.context.KeyringController.vault) {
         Logger.message('keyringController missing for INIT_BG_STATE_KEY');
       }
       if (!this.engineInitialized) {
@@ -123,7 +123,7 @@ class EngineService {
     controllers.forEach((controller) => {
       const { name, key = undefined } = controller;
       const update_bg_state_cb = () => {
-        if (engine.context.KeyringController.vault) {
+        if (!engine.context.KeyringController.vault) {
           Logger.message('keyringController missing for UPDATE_BG_STATE_KEY');
         }
         store.dispatch({ type: UPDATE_BG_STATE_KEY, payload: { key: name } });
