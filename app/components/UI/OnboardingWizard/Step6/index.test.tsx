d4e8528db2b329b7e7616565f24a259cc212caaa
Curtis David <Curtis.David7@gmail.com>
2024-05-07 15:20:07 -0400

    chore: revert  feat: notifications onboarding wizard (#9392) (#9571)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    Due to the introduction of failing tests into main, This PR reverts
    https://github.com/MetaMask/metamask-mobile/pull/9392
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/UI/OnboardingWizard/Step6/index.test.tsx
+++ app/components/UI/OnboardingWizard/Step6/index.test.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import Step6 from './';
-import renderWithProvider from '../../../../util/test/renderWithProvider';
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 
-const navigationMock = {
-  navigate: jest.fn(),
-};
+const mockStore = configureMockStore();
+const store = mockStore({});
 
-const closeOnboardingWizard = jest.fn();
 describe('Step6', () => {
   it('should render correctly', () => {
-    const { toJSON } = renderWithProvider(
-      <Step6 navigation={navigationMock} onClose={closeOnboardingWizard} />,
+    const wrapper = shallow(
+      <Provider store={store}>
+        <Step6 />
+      </Provider>,
     );
-    expect(toJSON()).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 });
