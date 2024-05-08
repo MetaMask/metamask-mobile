301e2385c01f5570fa25f8dfb500733291527739
Mark Stacey <markjstacey@gmail.com>
2024-05-08 11:47:36 -0230

    fix: Fix Engine context types (#9566)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    The `context` property of `Engine` was typed as `any`, effectively
    disabling type checking for most controller interactions. The `any` has
    been removed, restoring controller type checking.
    
    ## **Related issues**
    
    This problem was introduced in #6749
    
    ## **Manual testing steps**
    
    N/A
    
    ## **Screenshots/Recordings**
    
    N/A
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/core/RPCMethods/utils.ts
+++ app/core/RPCMethods/utils.ts
@@ -2,8 +2,11 @@ import { query } from '@metamask/controller-utils';
 import Engine from '../Engine';
 
 export const polyfillGasPrice = async (method: string, params: any[] = []) => {
-  const { TransactionController } = Engine.context;
-  const data = await query(TransactionController.ethQuery, method, params);
+  const ethQuery = Engine.controllerMessenger.call(
+    'NetworkController:getEthQuery',
+  );
+  // @ts-expect-error TODO: Handle case where this is called prior to initialization
+  const data = await query(ethQuery, method, params);
 
   if (data?.maxFeePerGas && !data.gasPrice) {
     data.gasPrice = data.maxFeePerGas;
