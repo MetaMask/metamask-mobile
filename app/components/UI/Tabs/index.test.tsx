cde65252dd0b4205063d8dc3a731d521ed65eb5e
EtherWizard33 <165834542+EtherWizard33@users.noreply.github.com>
2024-05-08 18:30:45 -0400

    feat: add a footer to the in-app browser tab, containing information about the connected account and network (#9321)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    - Add a footer to the in-app browser tab, containing information about
    the connected account and network.
    
    - The requirements [are in this
    issue](https://github.com/MetaMask/mobile-planning/issues/1634).
    
    - The localizations originated from this PR
    https://github.com/MetaMask/metamask-mobile/pull/9474 I'm told another
    team picks it up and adds other languages [in this
    PR](https://github.com/MetaMask/metamask-mobile/pull/9428)
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    ## **Related issues**
    
    Fixes: It's a new feature not a fix
    
    ## **Manual testing steps**
    
    1. Go to the browser tab
    2. In the broswer's tab view, click on + to add a new tab
    3. Again in the browser's tab view, click on the button on the right of
    the + sign to see the list of tabs
    4. The tab should be displayed with the expected footer at the bottom
    like in the 'After' screenshot below.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    | Before       | After        |
    |--------------|--------------|
    | <img width="350" alt="Screenshot 2024-04-18 at 3 56 43 PM"
    src="https://github.com/MetaMask/metamask-mobile/assets/165834542/b35617ff-2afa-4dec-8a33-79984da04e6d">
    |<img width="350" alt="Screenshot 2024-04-18 at 3 56 43 PM"
    src="https://github.com/MetaMask/metamask-mobile/assets/165834542/10e13fa9-99cb-44af-b073-9d2da1fff617">
    |
    
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/UI/Tabs/index.test.tsx
+++ app/components/UI/Tabs/index.test.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import renderWithProvider from '../../../util/test/renderWithProvider';
 import Tabs from './';
+import initialBackgroundState from '../../../util/test/initial-background-state.json';
+
+const mockInitialState = {
+  wizard: {
+    step: 1,
+  },
+  engine: {
+    backgroundState: initialBackgroundState,
+  },
+};
 
 jest.mock('react-native-safe-area-context', () => {
   const inset = { top: 1, right: 2, bottom: 3, left: 4 };
@@ -14,7 +24,8 @@ jest.mock('react-native-safe-area-context', () => {
 describe('Tabs', () => {
   it('should render correctly', () => {
     const { toJSON } = renderWithProvider(
-      <Tabs tabs={[{ id: 1, url: 'about:blank', image: '' }]} />,
+      <Tabs tabs={[{ id: 1, url: 'about:blank', image: '', id: 123 }]} />,
+      { state: mockInitialState },
     );
     expect(toJSON()).toMatchSnapshot();
   });
