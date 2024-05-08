79b1caf1b9f67378e42d121e15fc73a9d531cb19
legobt <6wbvkn0j@anonaddy.me>
2024-05-08 10:13:55 +0000

    fixme: ignore lint

--- app/components/Nav/App/index.js
+++ app/components/Nav/App/index.js
@@ -287,6 +287,7 @@ const VaultRecoveryFlow = () => (
   </Stack.Navigator>
 );
 
+// eslint-disable-next-line react/prop-types
 const App = ({ userLoggedIn }) => {
   const animationRef = useRef(null);
   const animationNameRef = useRef(null);
