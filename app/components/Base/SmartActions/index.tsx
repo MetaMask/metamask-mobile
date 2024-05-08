9ec6db41657039bcc5791c1c9f45d99b31482892
JSoufer <jonathan.ferreira@consensys.net>
2024-05-08 17:00:10 +0100

    chore: fix merge conflicts

--- app/components/Base/SmartActions/index.tsx
+++ app/components/Base/SmartActions/index.tsx
@@ -1,7 +1,7 @@
-import React, { useCallback, useState } from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, { useCallback } from 'react';
 import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
-import Tooltip from 'react-native-walkthrough-tooltip';
 
 import Icon, {
   IconColor,
@@ -12,6 +12,7 @@ import Text, {
   TextColor,
   TextVariant,
 } from '../../../component-library/components/Texts/Text';
+import { useTheme } from '../../../util/theme';
 
 export interface Action {
   title: string;
@@ -28,49 +29,47 @@ enum POSITION_TYPE {
   BOTTOM = 'bottom',
 }
 
-const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-    alignSelf: 'center',
-    bottom: '-79%',
-    paddingBottom: 200,
-  },
-  button: {
-    height: 64,
-    width: 64,
-    borderRadius: 8,
-    padding: 4,
-    marginHorizontal: 6,
-    marginTop: 8,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#FFF', //theme.colors.background.alternative
-    shadowColor: 'rgba(0,0,0, .2)',
-    shadowOffset: { height: 0, width: 0 },
-    shadowOpacity: 1,
-    shadowRadius: 2,
-  },
-  label: {
-    height: 14,
-    width: 56,
-    borderRadius: 8,
-    alignItems: 'center',
-    alignSelf: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#F6851F',
-    position: 'absolute',
-  },
-  title: {},
-});
+const createStyles = (colors: any) =>
+  StyleSheet.create({
+    wrapper: {
+      flex: 1,
+      alignSelf: 'center',
+      bottom: '-79%',
+      paddingBottom: 200,
+    },
+    button: {
+      height: 64,
+      width: 64,
+      borderRadius: 8,
+      padding: 4,
+      marginHorizontal: 6,
+      marginTop: 8,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: colors.background.default,
+      shadowColor: colors.overlay.default,
+      shadowOpacity: 0.1,
+      shadowOffset: { height: 4, width: 0 },
+    },
+    label: {
+      height: 14,
+      width: 56,
+      borderRadius: 8,
+      alignItems: 'center',
+      alignSelf: 'center',
+      justifyContent: 'center',
+      backgroundColor: colors.background.hover,
+      position: 'absolute',
+    },
+    title: {},
+  });
 
 export function SmartActions({
   actions,
   hasTitles = true,
   titlePosition = POSITION_TYPE.TOP,
   lablePosition = POSITION_TYPE.TOP,
-  toolTipPosition = POSITION_TYPE.TOP,
   swipeable = false,
-  showToolTips = true,
   ...props
 }: {
   actions: Action[];
@@ -81,75 +80,56 @@ export function SmartActions({
   swipeable?: boolean;
   showToolTips?: boolean;
 }) {
-  const [toolTipVisible, setToolTipVisible] = useState(false);
+  const { colors } = useTheme();
+  const styles = createStyles(colors);
 
   const renderItem = useCallback(
     ({ item }: { item: Action }) => (
-      <Tooltip
-        isVisible={toolTipVisible}
-        content={
-          <Text variant={TextVariant.BodyXS} color={TextColor.Default}>
-            {item?.tooltip || ''}
-          </Text>
-        }
-        placement={toolTipPosition}
-        onClose={() => setToolTipVisible(!toolTipVisible)}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={item.onPress}
+        accessibilityRole="button"
+        accessible
+        disabled={item.disabled}
       >
-        <TouchableOpacity
-          style={styles.button}
-          onPress={item.onPress}
-          accessibilityRole="button"
-          accessible
-          onLongPress={() => setToolTipVisible(!toolTipVisible)}
-          disabled={item.disabled}
-        >
-          {/* {showToolTips && ()} */}
-          {item.label && (
-            <View
-              style={[
-                styles.label,
-                { top: lablePosition === POSITION_TYPE.BOTTOM ? 45 : -8 },
-              ]}
-            >
-              <Text variant={TextVariant.BodyXS} color={TextColor.Inverse}>
-                {item.label}
-              </Text>
-            </View>
-          )}
-          {item.iconName && (
-            <Icon
-              size={IconSize.Sm}
-              name={item.iconName}
-              color={IconColor.Primary}
-              style={{
-                position:
-                  titlePosition === POSITION_TYPE.TOP ? 'relative' : 'absolute',
-              }}
-            />
-          )}
-          {hasTitles && (
-            <Text
-              variant={TextVariant.BodyXS}
-              color={TextColor.Default}
-              style={{
-                bottom: titlePosition === POSITION_TYPE.TOP ? 0 : -32,
-              }}
-            >
-              {item.title}
+        {/* {showToolTips && ()} */}
+        {item.label && (
+          <View
+            style={[
+              styles.label,
+              { top: lablePosition === POSITION_TYPE.BOTTOM ? 45 : -8 },
+            ]}
+          >
+            <Text variant={TextVariant.BodyXS} color={TextColor.Inverse}>
+              {item.label}
             </Text>
-          )}
-        </TouchableOpacity>
-      </Tooltip>
+          </View>
+        )}
+        {item.iconName && (
+          <Icon
+            size={IconSize.Sm}
+            name={item.iconName}
+            color={IconColor.Primary}
+            style={{
+              position:
+                titlePosition === POSITION_TYPE.TOP ? 'relative' : 'absolute',
+            }}
+          />
+        )}
+        {hasTitles && (
+          <Text
+            variant={TextVariant.BodyXS}
+            color={TextColor.Default}
+            style={{
+              bottom: titlePosition === POSITION_TYPE.TOP ? 0 : -32,
+            }}
+          >
+            {item.title}
+          </Text>
+        )}
+      </TouchableOpacity>
     ),
-    [
-      toolTipVisible,
-      setToolTipVisible,
-      showToolTips,
-      hasTitles,
-      titlePosition,
-      lablePosition,
-      toolTipPosition,
-    ],
+    [styles.button, styles.label, lablePosition, titlePosition, hasTitles],
   );
 
   return (
--- app/components/Views/Wallet/index.tsx
+++ app/components/Views/Wallet/index.tsx
@@ -1,6 +1,10 @@
 import { withHook } from '../../../util/withHook';
 import useWallet from './hooks/useWallet';
+<<<<<<< HEAD
 import withUITheme from './hooks/withUITheme';
+=======
+import choseUITheme from './hooks/choseUITheme';
+>>>>>>> cd49389e2 (chore: various polishings)
 
 /**
  * The main UI componente for the Wallet view will be dependent of the chosen theme
@@ -8,6 +12,10 @@ import withUITheme from './hooks/withUITheme';
  * This way we can have as many "Views" we want, since the logic is separeted from the UI
  */
 
+<<<<<<< HEAD
 const Wallet = withUITheme('custom02');
+=======
+const Wallet = choseUITheme('custom01');
+>>>>>>> cd49389e2 (chore: various polishings)
 
 export default withHook(useWallet)(Wallet);
