14e186c077b434bc4bb528ef76f155aa6374201c
tommasini <tommasini15@gmail.com>
2024-05-09 20:33:19 +0100

    update selector to be sent unit on this case of the e2e Sent TST

--- e2e/selectors/ActivitiesView.selectors.js
+++ e2e/selectors/ActivitiesView.selectors.js
@@ -5,6 +5,6 @@ export const ActivitiesViewSelectorsText = {
   TITLE: enContent.transactions_view.title,
   SWAP: enContent.swaps.transaction_label.swap,
   SENT_COLLECTIBLE_MESSAGE_TEXT: enContent.transactions.sent_collectible,
-  SENT_TOKENS_MESSAGE_TEXT: enContent.transactions.sent_tokens,
+  SENT_TOKENS_MESSAGE_TEXT: enContent.transactions.sent_unit,
   CONFIRM_TEXT: enContent.transaction.confirmed,
 };
