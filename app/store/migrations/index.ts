73dd8c9684ae95a2ffedb4d9b04a7881168dffbd
Aslau Mario-Daniel <marioaslau@gmail.com>
2024-05-09 19:07:28 +0100

    feat: push migrations from 39-41 to 39-40

--- app/store/migrations/index.ts
+++ app/store/migrations/index.ts
@@ -38,9 +38,9 @@ import migration34 from './034';
 import migration35 from './035';
 import migration36 from './036';
 import migration37 from './037';
+import migration38 from './038';
 import migration39 from './039';
 import migration40 from './040';
-import migration41 from './041';
 
 type MigrationFunction = (state: unknown) => unknown;
 type AsyncMigrationFunction = (state: unknown) => Promise<unknown>;
@@ -91,9 +91,9 @@ export const migrationList: MigrationsList = {
   35: migration35,
   36: migration36,
   37: migration37,
-  39: migration39,
-  40: migration40,
-  41: migration41,
+  39: migration38,
+  40: migration39,
+  41: migration40,
 };
 
 // Enable both synchronous and asynchronous migrations
