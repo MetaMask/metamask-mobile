dcdb03a95203e43d50dbe1dbb4d06226ba77dcd1
Jonathan Ferreira <44679989+Jonathansoufer@users.noreply.github.com>
2024-05-10 23:11:33 +0100

    feat: notifications onboarding wizard - [8 of 10] (#9572)
    ## **Description**
    
    This PR implements a new step on the onboarding wizard to teach users
    about notifications feature.
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    Install a fresh app and accept the tour at beginning.
    
    ## **Screenshots/Recordings**
    
    ### **Before**
    
    ### **After**
    ![Screenshot 2024-05-07 at 12 58
    11](https://github.com/MetaMask/metamask-mobile/assets/44679989/aed859d6-a37b-4902-8136-eeb881684a7e)
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

---
+++ app/components/UI/OnboardingWizard/Step6/index.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Device from '../../../../util/device';
+import Coachmark from '../Coachmark';
+import setOnboardingWizardStep from '../../../../actions/wizard';
+import { strings } from '../../../../../locales/i18n';
+import { createBrowserNavDetails } from '../../../Views/Browser';
+
+import onboardingStyles from '../styles';
+import {
+  MetaMetricsEvents,
+  ONBOARDING_WIZARD_STEP_DESCRIPTION,
+} from '../../../../core/Analytics';
+import { useTheme } from '../../../../util/theme';
+import generateTestId from '../../../../../wdio/utils/generateTestId';
+import { ONBOARDING_WIZARD_SIXTH_STEP_CONTENT_ID } from '../../../../../wdio/screen-objects/testIDs/Components/OnboardingWizard.testIds';
+import { useMetrics } from '../../../hooks/useMetrics';
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+  },
+  coachmarkContainer: {
+    position: 'absolute',
+    alignSelf: 'center',
+    left: 0,
+    right: 0,
+    marginHorizontal: 16,
+    bottom: Device.isIphoneX() ? 80 : Device.isIos() ? 40 : 64,
+  },
+});
+
+interface Step6Props {
+  navigation: any;
+  onClose: () => Promise<void>;
+}
+
+const Step6 = ({ onClose, navigation }: Step6Props) => {
+  const { trackEvent } = useMetrics();
+  const { colors } = useTheme();
+  const dynamicOnboardingStyles = onboardingStyles(colors);
+  const dispatch = useDispatch();
+  /**
+   * Dispatches 'setOnboardingWizardStep' with next step
+   */
+  const onNext = () => {
+    dispatch(setOnboardingWizardStep?.(7));
+    navigation?.navigate(...createBrowserNavDetails());
+    trackEvent(MetaMetricsEvents.ONBOARDING_TOUR_STEP_COMPLETED, {
+      tutorial_step_count: 6,
+      tutorial_step_name: ONBOARDING_WIZARD_STEP_DESCRIPTION[6],
+    });
+  };
+
+  /**
+   * Dispatches 'setOnboardingWizardStep' with next step
+   */
+  const onBack = () => {
+    dispatch(setOnboardingWizardStep?.(5));
+    trackEvent(MetaMetricsEvents.ONBOARDING_TOUR_STEP_REVISITED, {
+      tutorial_step_count: 6,
+      tutorial_step_name: ONBOARDING_WIZARD_STEP_DESCRIPTION[6],
+    });
+  };
+
+  /**
+   * Returns content for this step
+   */
+  const content = () => (
+    <View style={dynamicOnboardingStyles.contentContainer}>
+      <Text
+        style={dynamicOnboardingStyles.content}
+        {...generateTestId(Platform, ONBOARDING_WIZARD_SIXTH_STEP_CONTENT_ID)}
+      >
+        {strings('onboarding_wizard_new.step6.content1')}
+      </Text>
+    </View>
+  );
+
+  return (
+    <View style={styles.main}>
+      <View style={styles.coachmarkContainer}>
+        <Coachmark
+          title={strings('onboarding_wizard_new.step6.title')}
+          content={content()}
+          onNext={onNext}
+          onBack={onBack}
+          currentStep={5}
+          topIndicatorPosition={false}
+          bottomIndicatorPosition={'bottomRight'}
+          onClose={onClose}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Step6;
