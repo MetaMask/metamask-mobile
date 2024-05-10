ef995ffb5a270da5661fd906c852d303afc94efc
CW <chris.wilcox@consensys.net>
2024-05-09 20:06:08 -0700

    tap import button on detected token sheet (not working)

---
+++ e2e/selectors/asseets/DetectedTokensView.selectors.js
@@ -0,0 +1,9 @@
+import enContent from '../../../locales/languages/en.json';
+
+export const DetectedTokensSelectorTexts = {
+  IMPORT_BUTTON_TEXT: enContent.detected_tokens.import_cta,
+};
+
+export const DetectedTokensSelectorIDs = {
+  IMPORT_BUTTON_ID: 'token-detection-import-button',
+};
