93c506d860fe84a9738a508ba324b5517caf4778
CW <chris.wilcox@consensys.net>
2024-05-09 21:02:41 -0700

    rename folder

--- app/components/Views/DetectedTokens/index.tsx
+++ app/components/Views/DetectedTokens/index.tsx
@@ -25,7 +25,7 @@ import BottomSheet, {
   BottomSheetRef,
 } from '../../../component-library/components/BottomSheets/BottomSheet';
 import { useMetrics } from '../../../components/hooks/useMetrics';
-import { DetectedTokensSelectorIDs } from '../../../../e2e/selectors/asseets/DetectedTokensView.selectors';
+import { DetectedTokensSelectorIDs } from '../../../../e2e/selectors/assets/DetectedTokensView.selectors';
 
 const createStyles = (colors: any) =>
   StyleSheet.create({
