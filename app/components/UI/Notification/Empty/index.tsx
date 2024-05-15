291a064ca80d92c4853c5886fb9bf767425f72ee
JSoufer <jonathan.ferreira@consensys.net>
2024-05-15 10:24:01 +0100

    chore: replace style usage

--- app/components/UI/Notification/Empty/index.tsx
+++ app/components/UI/Notification/Empty/index.tsx
@@ -4,30 +4,27 @@ import Text, {
   TextVariant,
 } from '../../../../component-library/components/Texts/Text';
 import { strings } from '../../../../../locales/i18n';
-import { createStyles } from './styles';
+import { styles } from './styles';
 import Icon, {
   IconColor,
   IconName,
   IconSize,
 } from '../../../../component-library/components/Icons/Icon';
-const Empty = ({ testID }: { testID?: string }) => {
-  const styles = createStyles();
-  return (
-    <View style={styles.wrapper} testID={testID}>
-      <Icon
-        name={IconName.Notification}
-        size={IconSize.Xl}
-        color={IconColor.Default}
-        style={styles.text}
-      />
-      <Text style={styles.text} variant={TextVariant.HeadingMD}>
-        {strings('notifications.empty.title')}
-      </Text>
-      <Text style={styles.text} variant={TextVariant.BodyMD}>
-        {strings('notifications.empty.message')}
-      </Text>
-    </View>
-  );
-};
+const Empty = ({ testID }: { testID?: string }) => (
+  <View style={styles.wrapper} testID={testID}>
+    <Icon
+      name={IconName.Notification}
+      size={IconSize.Xl}
+      color={IconColor.Default}
+      style={styles.text}
+    />
+    <Text style={styles.text} variant={TextVariant.HeadingMD}>
+      {strings('notifications.empty.title')}
+    </Text>
+    <Text style={styles.text} variant={TextVariant.BodyMD}>
+      {strings('notifications.empty.message')}
+    </Text>
+  </View>
+);
 
 export default Empty;
