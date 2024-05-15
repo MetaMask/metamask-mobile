a341f12208fa873019c2cf05f243d6975ba9d4c5
Jony Bursztyn <jonathan@bursztyn.io>
2024-05-15 08:58:40 -0700

    feat: Add new privacy policy alert toast (#9204)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    Changes:
    Adds a new privacy policy banner that will show up after May 6, 2024.
    [EDIT HB - date set for June 4, 2023]
    If the user closes it manually, the privacy policy banner will not show
    up again.
    If the user does not close the toast manually, the privacy policy banner
    will cease to show up after a day of being shown for the first time.
    Changes the Survey banner so that we can display multiple banners at the
    same time without overlapping each other.
    
    Considerations:
    - Make sure this doesn't show up during onboarding
    - This toast will show up even after years of the privacy policy being
    changed. We should set up a deadline for that or eventually revert this
    PR.
    - Check if copy is correct.
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to the home page
    2. Check if there's a new privacy banner there (date should be edited in
    the code so that the privacy policy shows up earlier than May 6, 2024)
    3. When clicking the link, it should take you to the Privacy Policy
    website
    4. When closing it, it shouldn't show up again
    5. If not closing it manually, the toast should not show up again after
    1 day of being shown for the first time. This data is saved in
    localStorage so editing the timestamp there should be enough to test
    this feature out.
    
    ## **Screenshots/Recordings**
    
    
    https://github.com/MetaMask/metamask-mobile/assets/11148144/ae6e74fc-0a7f-47ce-8744-4a292875f7b3
    
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: EtherWizard33 <eric.lamontagne@consensys.net>

---
+++ app/reducers/legalNotices/index.ts
@@ -0,0 +1,78 @@
+import { RootState } from '..';
+import { Action } from 'redux';
+import ACTIONS from './types';
+
+export const newPrivacyPolicyDate = new Date('2024-06-04T00:00:00Z');
+
+const initialState = {
+  newPrivacyPolicyToastClickedOrClosed: false,
+  newPrivacyPolicyToastShownDate: null,
+};
+
+export const storePrivacyPolicyShownDate = (timestamp: number) => ({
+  type: ACTIONS.STORE_PRIVACY_POLICY_SHOWN_DATE,
+  payload: timestamp,
+});
+
+export const storePrivacyPolicyClickedOrClosed = () => ({
+  type: ACTIONS.STORE_PRIVACY_POLICY_CLICKED_OR_CLOSED,
+});
+
+export const shouldShowNewPrivacyToastSelector = (
+  state: RootState,
+): boolean => {
+  const {
+    newPrivacyPolicyToastShownDate,
+    newPrivacyPolicyToastClickedOrClosed,
+  } = state.legalNotices;
+
+  if (newPrivacyPolicyToastClickedOrClosed) return false;
+
+  const currentDate = new Date();
+  const shownDate = new Date(newPrivacyPolicyToastShownDate);
+
+  const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
+  const isRecent =
+    currentDate.getTime() - shownDate.getTime() < oneDayInMilliseconds;
+
+  return (
+    currentDate.getTime() >= newPrivacyPolicyDate.getTime() &&
+    (!newPrivacyPolicyToastShownDate ||
+      (isRecent && !newPrivacyPolicyToastClickedOrClosed))
+  );
+};
+
+export interface LegalNoticesAction extends Action {
+  newPrivacyPolicyToastShownDate: boolean;
+  payload: number;
+}
+
+const legalNoticesReducer = (
+  state = initialState,
+  action: LegalNoticesAction = {
+    type: '',
+    newPrivacyPolicyToastShownDate: false,
+    payload: 0,
+  },
+) => {
+  switch (action.type) {
+    case ACTIONS.STORE_PRIVACY_POLICY_SHOWN_DATE: {
+      if (state.newPrivacyPolicyToastShownDate !== null) {
+        return state;
+      }
+
+      return {
+        ...state,
+        newPrivacyPolicyToastShownDate: action.payload,
+      };
+    }
+
+    case ACTIONS.STORE_PRIVACY_POLICY_CLICKED_OR_CLOSED: {
+      return { ...state, newPrivacyPolicyToastClickedOrClosed: true };
+    }
+
+    default:
+      return state;
+  }
+};
+export default legalNoticesReducer;
