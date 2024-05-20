d2d0588908e57e573cea47e19e323ecedc659a33
legobt <6wbvkn0j@anonaddy.me>
2024-05-20 07:47:39 +0000

    fixup: console.error in test

--- app/core/RPCMethods/RPCMethodMiddleware.test.ts
+++ app/core/RPCMethods/RPCMethodMiddleware.test.ts
@@ -1117,7 +1117,6 @@ describe('getRpcMethodMiddleware', () => {
 
       const response = await callMiddleware({ middleware, request });
 
-      console.error((response as JsonRpcFailure).error);
       expect((response as JsonRpcFailure).error.code).toBe(expectedError.code);
       expect((response as JsonRpcFailure).error.message).toBe(
         expectedError.message,
