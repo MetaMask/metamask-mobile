d25cf14791b19d4fc07f452d0762b8ed064261bf
Curtis <Curtis.David7@gmail.com>
2024-05-20 18:09:12 -0400

    move transaction confirmation view to send folder

--- e2e/specs/confirmations/advanced-gas-fees.spec.js
+++ e2e/specs/confirmations/advanced-gas-fees.spec.js
@@ -3,7 +3,7 @@ import { SmokeConfirmations } from '../../tags';
 import WalletView from '../../pages/WalletView';
 import AmountView from '../../pages/Send/AmountView';
 import SendView from '../../pages/Send/SendView';
-import TransactionConfirmationView from '../../pages/TransactionConfirmView';
+import TransactionConfirmationView from '../../pages/Send/TransactionConfirmView';
 import { loginToApp } from '../../viewHelper';
 import FixtureBuilder from '../../fixtures/fixture-builder';
 import {
