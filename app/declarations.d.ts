d99d5e7701a146d781bba8c438d436d9b7efa0da
JSoufer <jonathan.ferreira@consensys.net>
2024-05-22 18:04:25 +0100

    chore: fix tsc adding declaration

--- app/declarations.d.ts
+++ app/declarations.d.ts
@@ -269,3 +269,5 @@ declare module 'react-native-vector-icons/Zocial' {
    */
   export default class Zocial extends ZocialIconType {}
 }
+
+declare module '@metamask/snaps-controllers/react-native';
