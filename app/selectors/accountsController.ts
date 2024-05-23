140141f0ac4befc2bfc210c568c5fb4d7edfaa02
Cal-L <cleun007@gmail.com>
2024-05-23 11:28:16 -0700

    Use deep equal for selector

--- app/selectors/accountsController.ts
+++ app/selectors/accountsController.ts
@@ -2,11 +2,12 @@ import { AccountsControllerState } from '@metamask/accounts-controller';
 import { captureException } from '@sentry/react-native';
 import { createSelector } from 'reselect';
 import { RootState } from '../reducers';
+import { createDeepEqualSelector } from './util';
 
 const selectAccountsControllerState = (state: RootState) =>
   state.engine.backgroundState.AccountsController;
 
-export const selectInternalAccounts = createSelector(
+export const selectInternalAccounts = createDeepEqualSelector(
   selectAccountsControllerState,
   (accountControllerState) =>
     Object.values(accountControllerState.internalAccounts.accounts),
