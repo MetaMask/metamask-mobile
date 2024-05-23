5b10e34fb03b0916ca0000a63ee00803e9479975
EtherWizard33 <eric.lamontagne@consensys.net>
2024-05-23 18:54:34 -0400

    style: lint

--- e2e/selectors/Modals/ExperienceEnhancerModal.selectors.js
+++ e2e/selectors/Modals/ExperienceEnhancerModal.selectors.js
@@ -1,6 +1,6 @@
-// eslint-disable-next-line import/prefer-default-export
 import enContent from '../../../locales/languages/en.json';
 
+// eslint-disable-next-line import/prefer-default-export
 export const ExperienceEnhancerModalSelectorsText = {
   TITLE: enContent.experience_enhancer_modal.title,
   I_AGREE: enContent.experience_enhancer_modal.accept,
