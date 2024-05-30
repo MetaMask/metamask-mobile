5c9ab68911b8196618a8d1e4763ccd931f36b4ef
Jyoti Puri <jyotipuri@gmail.com>
2024-05-30 18:50:41 +0530

    Improve message display for typed sign messages

--- app/util/string/index.text.ts
+++ app/util/string/index.text.ts
@@ -1,12 +1,72 @@
-import sanitizeString from '.';
+import {
+  parseTypedSignDataMessage,
+  sanitizeString,
+  stripMultipleNewlines,
+} from '.';
 
-describe('sanitizeString', () => {
-  it('should escape all occurences of \u202E in text', async () => {
-    const result = sanitizeString('test \u202E test \u202E test');
-    expect(result).toEqual('test \\u202E test \\u202E test');
+describe('string utils', () => {
+  describe('sanitizeString', () => {
+    it('should escape all occurences of \u202E in text', async () => {
+      const result = sanitizeString('test \u202E test \u202E test');
+      expect(result).toEqual('test \\u202E test \\u202E test');
+    });
+    it('should return a non-string value as it is', async () => {
+      const result = sanitizeString({ test: 'test \u202E test \u202E test' });
+      expect(result.test).toEqual('test \u202E test \u202E test');
+    });
   });
-  it('should return a non-string value as it is', async () => {
-    const result = sanitizeString({ test: 'test \u202E test \u202E test' });
-    expect(result.test).toEqual('test \u202E test \u202E test');
+
+  describe('stripMultipleNewlines', () => {
+    it('replace multiple newline characters in string with single newline character', async () => {
+      const result = stripMultipleNewlines(
+        'Secure ✅ \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n',
+      );
+      expect(result).toEqual('Secure ✅ \n');
+    });
+    it('does nothing if passed value is not defined or not a string type', async () => {
+      expect(stripMultipleNewlines(123 as any)).toEqual(123);
+      expect(stripMultipleNewlines(undefined as any)).not.toBeDefined();
+    });
+  });
+
+  describe('parseTypedSignDataMessage', () => {
+    const typedDataMsg =
+      '{"domain":{"chainId":97,"name":"Ether Mail","verifyingContract":"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC","version":"1"},"message":{"contents":"Hello, Bob!","from":{"name":"Cow","wallets":["0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826","0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF","0x06195827297c7A80a443b6894d3BDB8824b43896"]},"to":[{"name":"Bob","wallets":["0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB","0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57","0xB0B0b0b0b0b0B000000000000000000000000000"]}]},"primaryType":"Mail","types":{"EIP712Domain":[{"name":"name","type":"string"},{"name":"version","type":"string"},{"name":"chainId","type":"uint256"},{"name":"verifyingContract","type":"address"}],"Mail":[{"name":"from","type":"Person"},{"name":"to","type":"Person[]"},{"name":"contents","type":"string"}],"Person":[{"name":"name","type":"string"},{"name":"wallets","type":"address[]"}]}}';
+
+    it('should throw an error if types is undefined', () => {
+      const typedDataMsgWithoutTypes =
+        '{"domain":{"chainId":97,"name":"Ether Mail","verifyingContract":"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC","version":"1"},"message":{"contents":"Hello, Bob!","from":{"name":"Cow","wallets":["0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826","0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF","0x06195827297c7A80a443b6894d3BDB8824b43896"]},"to":[{"name":"Bob","wallets":["0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB","0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57","0xB0B0b0b0b0b0B000000000000000000000000000"]}]},"primaryType":"Mail"}';
+      expect(() => parseTypedSignDataMessage(typedDataMsgWithoutTypes)).toThrow(
+        'Invalid types definition',
+      );
+    });
+
+    it('should throw an error if base type is not defined', () => {
+      const typedSignDataWithoutBaseType =
+        '{"domain":{"chainId":97,"name":"Ether Mail","verifyingContract":"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC","version":"1"},"message":{"contents":"Hello, Bob!","from":{"name":"Cow","wallets":["0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826","0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF","0x06195827297c7A80a443b6894d3BDB8824b43896"]},"to":[{"name":"Bob","wallets":["0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB","0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57","0xB0B0b0b0b0b0B000000000000000000000000000"]}]},"types":{"EIP712Domain":[{"name":"name","type":"string"},{"name":"version","type":"string"},{"name":"chainId","type":"uint256"},{"name":"verifyingContract","type":"address"}],"Mail":[{"name":"from","type":"Person"},{"name":"to","type":"Person[]"},{"name":"contents","type":"string"}],"Person":[{"name":"name","type":"string"},{"name":"wallets","type":"address[]"}]}}';
+      expect(() =>
+        parseTypedSignDataMessage(typedSignDataWithoutBaseType),
+      ).toThrow('Invalid primary type definition');
+    });
+
+    it('should return ignore message data with unknown types and trim new lines', () => {
+      const result = parseTypedSignDataMessage(typedDataMsg);
+      expect(result).toStrictEqual({
+        value: {
+          string: { value: 'Secure ✅ \n', type: 'string' },
+          object: {
+            value: {
+              address: {
+                value: '0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF',
+                type: 'address',
+              },
+              uint: { value: 42, type: 'uint256' },
+            },
+            type: 'object',
+          },
+        },
+        type: 'payload',
+      });
+    });
   });
 });
--- app/util/string/index.ts
+++ app/util/string/index.ts
@@ -6,7 +6,7 @@ import { isString } from '../lodash';
  * @param {any} str
  * @returns {(string|*)} escaped string or original param value
  */
-export default (str: any): any => {
+export const sanitizeString = (str: any): any => {
   if (!str) {
     return str;
   }
@@ -15,4 +15,128 @@ export default (str: any): any => {
   }
   // Ref: https://stackoverflow.com/questions/69297024/why-is-string-replaceall-not-a-function-on-android-react-native
   return str.split('\u202E').join('\\u202E');
+};
+
+export const stripMultipleNewlines = (str: string): string => {
+  if (!str || typeof str !== 'string') {
+    return str;
+  }
+  return str.replace(/\n+/g, '\n');
+};
+
+const solidityTypes = () => {
+  const types = [
+    'bool',
+    'address',
+    'string',
+    'bytes',
+    'int',
+    'uint',
+    'fixed',
+    'ufixed',
+  ];
+
+  const ints = Array.from(new Array(32)).map(
+    (_, index) => `int${(index + 1) * 8}`,
+  );
+  const uints = Array.from(new Array(32)).map(
+    (_, index) => `uint${(index + 1) * 8}`,
+  );
+  const bytes = Array.from(new Array(32)).map(
+    (_, index) => `bytes${index + 1}`,
+  );
+
+  const fixedM = Array.from(new Array(32)).map(
+    (_, index) => `fixed${(index + 1) * 8}`,
+  );
+  const ufixedM = Array.from(new Array(32)).map(
+    (_, index) => `ufixed${(index + 1) * 8}`,
+  );
+  const fixed = Array.from(new Array(80)).map((_, index) =>
+    fixedM.map((aFixedM) => `${aFixedM}x${index + 1}`),
+  );
+  const ufixed = Array.from(new Array(80)).map((_, index) =>
+    ufixedM.map((auFixedM) => `${auFixedM}x${index + 1}`),
+  );
+
+  return [
+    ...types,
+    ...ints,
+    ...uints,
+    ...bytes,
+    ...fixed.flat(),
+    ...ufixed.flat(),
+  ];
+};
+
+const SOLIDITY_TYPES = solidityTypes();
+
+const stripArrayType = (potentialArrayType: string) =>
+  potentialArrayType.replace(/\[[[0-9]*\]*/gu, '');
+
+const stripOneLayerofNesting = (potentialArrayType: string) =>
+  potentialArrayType.replace(/\[(\d*)\]/u, '');
+
+const isArrayType = (potentialArrayType: string) =>
+  potentialArrayType.match(/\[[[0-9]*\]*/u) !== null;
+
+const isSolidityType = (type: any) => SOLIDITY_TYPES.includes(type);
+
+const sanitizeMessage = (
+  message: any,
+  primaryType: string,
+  types: Record<string, any>,
+) => {
+  if (!types) {
+    throw new Error(`Invalid types definition`);
+  }
+
+  // Primary type can be an array.
+  const isArray = primaryType && isArrayType(primaryType);
+  if (isArray) {
+    return {
+      value: message.map((value: any) =>
+        sanitizeMessage(value, stripOneLayerofNesting(primaryType), types),
+      ),
+      type: primaryType,
+    };
+  } else if (isSolidityType(primaryType)) {
+    return { value: stripMultipleNewlines(message), type: primaryType };
+  }
+
+  // If not, assume to be struct
+  const baseType = isArray ? stripArrayType(primaryType) : primaryType;
+
+  const baseTypeDefinitions = types[baseType];
+  if (!baseTypeDefinitions) {
+    throw new Error(`Invalid primary type definition`);
+  }
+
+  const sanitizedStruct = {};
+  const msgKeys = Object.keys(message);
+  msgKeys.forEach((msgKey) => {
+    const definedType: any = Object.values(baseTypeDefinitions).find(
+      (baseTypeDefinition: any) => baseTypeDefinition.name === msgKey,
+    );
+
+    if (!definedType) {
+      return;
+    }
+
+    (sanitizedStruct as Record<string, any>)[msgKey] = sanitizeMessage(
+      message[msgKey],
+      definedType.type,
+      types,
+    );
+  });
+  return { value: sanitizedStruct, type: primaryType };
+};
+
+export const parseTypedSignDataMessage = (dataToParse: string) => {
+  const { message, primaryType, types } = JSON.parse(dataToParse);
+  console.log(
+    '******************',
+    JSON.stringify(sanitizeMessage(message, primaryType, types)),
+  );
+  return sanitizeMessage(message, primaryType, types);
 };
