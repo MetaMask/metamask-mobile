f729d6bb08574dab3acec112de23e6a88f75249c
tommasini <tommasini15@gmail.com>
2024-05-31 19:12:11 +0100

    re using the selectors instead, we should be able to use the deepEqualSelector

--- app/selectors/accountTrackerController.ts
+++ app/selectors/accountTrackerController.ts
@@ -3,43 +3,33 @@ import {
   AccountTrackerState,
   AccountInformation,
 } from '@metamask/assets-controllers';
-import { toChecksumHexAddress } from '@metamask/controller-utils';
 import { RootState } from '../reducers';
-
+import { createDeepEqualSelector } from './util';
+import { selectChainId } from './networkController';
+import { selectSelectedInternalAccountChecksummedAddress } from './accountsController';
 const selectAccountTrackerControllerState = (state: RootState) =>
   state.engine.backgroundState.AccountTrackerController;
-
-export const selectAccounts = createSelector(
+export const selectAccounts = createDeepEqualSelector(
   selectAccountTrackerControllerState,
   (accountTrackerControllerState: AccountTrackerState) =>
     accountTrackerControllerState.accounts,
 );
-
 export const selectAccountsByChainId = createSelector(
   selectAccountTrackerControllerState,
   (accountTrackerControllerState: AccountTrackerState) =>
     accountTrackerControllerState.accountsByChainId,
 );
-
 export const selectAccountsLength = createSelector(
   selectAccounts,
   (accounts: { [address: string]: AccountInformation }) =>
     Object.keys(accounts || {}).length,
 );
-
-export const selectAccountBalanceByChainId = createSelector(
-  (state: RootState) => state.engine.backgroundState,
+export const selectAccountBalanceByChainId = createDeepEqualSelector(
   selectAccountsByChainId,
-  (backgroundState, accountsByChainId) => {
-    const { NetworkController, AccountsController } = backgroundState;
-    const chainId = NetworkController.providerConfig.chainId;
-    const selectedAccountId =
-      AccountsController.internalAccounts?.selectedAccount;
-    const selectedAccountAddress =
-      AccountsController.internalAccounts?.accounts?.[selectedAccountId]
-        ?.address;
-    return selectedAccountAddress && chainId && accountsByChainId
-      ? accountsByChainId[chainId][toChecksumHexAddress(selectedAccountAddress)]
-      : undefined;
-  },
+  selectChainId,
+  selectSelectedInternalAccountChecksummedAddress,
+  (accountsByChainId, chainId, selectedInternalAccountChecksummedAddress) =>
+    selectedInternalAccountChecksummedAddress && chainId && accountsByChainId
+      ? accountsByChainId[chainId][selectedInternalAccountChecksummedAddress]
+      : undefined,
 );
