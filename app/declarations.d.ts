1c484ee0102fee43dd62f28064f5c249e3156286
JSoufer <jonathan.ferreira@consensys.net>
2024-06-06 18:38:05 +0100

    chore: remove unecessary type definition

--- app/declarations.d.ts
+++ app/declarations.d.ts
@@ -8,9 +8,6 @@ declare module 'react-native-fade-in-image';
 
 declare module 'react-native-minimizer';
 
-declare module '@metamask/snaps-controllers';
-declare module '@metamask/snaps-controllers/react-native';
-
 declare module 'react-native-scrollable-tab-view/DefaultTabBar' {
   const content: React.FC<any>;
   export default content;
