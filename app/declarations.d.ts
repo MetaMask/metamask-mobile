5b8f456624568b8f56c09fdfb2dc8b8a04c9d97b
legobt <6wbvkn0j@anonaddy.me>
2024-06-08 01:32:05 +0000

    chore: remove unused overrideXMLHttpRequest and xhr2

--- app/declarations.d.ts
+++ app/declarations.d.ts
@@ -8,7 +8,6 @@ declare module 'react-native-fade-in-image';
 
 declare module 'react-native-minimizer';
 
-declare module 'xhr2';
 declare module 'react-native-scrollable-tab-view/DefaultTabBar' {
   // TODO: Replace "any" with type
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
