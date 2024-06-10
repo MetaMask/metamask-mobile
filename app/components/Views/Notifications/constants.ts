e4eb901045bf1b5f3c70757a9bfe94993eeac3ab
JSoufer <jonathan.ferreira@consensys.net>
2024-06-10 14:10:56 +0100

    chore: add contants for testing

---
+++ app/components/Views/Notifications/constants.ts
@@ -0,0 +1,5 @@
+export const NotificationsDetailViewSelectorsIDs = {
+  NOTIFICATIONS_HEADER_CONTAINER_ID: 'notification-details-header',
+  NOTIFICATIONS_HEADER_TITLE_ID: 'notification-details-header-title',
+  NOTIFICATIONS_HEADER_SUBTITLE_ID: 'notification-details-header-subtitle',
+};
