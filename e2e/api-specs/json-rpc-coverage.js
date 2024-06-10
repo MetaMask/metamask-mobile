0326e494cbad18a2e085a3738f1996de848ede2f
Curtis <Curtis.David7@gmail.com>
2024-06-10 18:58:17 -0400

    only run watch_asset

--- e2e/api-specs/json-rpc-coverage.js
+++ e2e/api-specs/json-rpc-coverage.js
@@ -221,7 +221,7 @@ const main = async () => {
             only: methodsWithConfirmations,
           }),
         ],
-        skip,
+        only: ['wallet_watchAsset'],
       });
       const failing = results.filter((r) => !r.valid);
       await detox.cleanup();
