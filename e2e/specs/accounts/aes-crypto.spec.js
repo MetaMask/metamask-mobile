fff2eb2267ef8684d4237e4225999eba42a7adbd
gantunesr <17601467+gantunesr@users.noreply.github.com>
2024-06-10 20:08:44 -0400

    refactor: E2E test

--- e2e/specs/accounts/aes-crypto.spec.js
+++ e2e/specs/accounts/aes-crypto.spec.js
@@ -1,11 +1,21 @@
 import { SmokeAccounts } from '../../tags';
 import TestHelpers from '../../helpers';
-import FixtureBuilder from '../../fixtures/fixture-builder';
-import { withFixtures } from '../../fixtures/fixture-helper';
+
 import TabBarComponent from '../../pages/TabBarComponent';
 import SettingsView from '../../pages/Settings/SettingsView';
 import { loginToApp } from '../../viewHelper';
 import AesCryptoTestForm from '../../pages/Settings/AesCryptoTestForm';
+
+import FixtureBuilder from '../../fixtures/fixture-builder';
+import {
+  loadFixture,
+  startFixtureServer,
+  stopFixtureServer,
+} from '../../fixtures/fixture-helper';
+import { getFixturesServerPort } from '../../fixtures/utils';
+import FixtureServer from '../../fixtures/fixture-server';
+
+const fixtureServer = new FixtureServer();
 
 describe(SmokeAccounts('AES Crypto'), () => {
   const SALT_BYTES_COUNT = 32;
@@ -17,91 +27,78 @@ describe(SmokeAccounts('AES Crypto'), () => {
   const DATA_TO_ENCRYPT_TWO = 'more random data to encrypt';
 
   beforeAll(async () => {
-    jest.setTimeout(150000);
     await TestHelpers.reverseServerPort();
+    const fixture = new FixtureBuilder().build();
+    await startFixtureServer(fixtureServer);
+    await loadFixture(fixtureServer, { fixture });
+    await device.launchApp({
+      launchArgs: { fixtureServerPort: `${getFixturesServerPort()}` },
+    });
+    await loginToApp();
+  });
+
+  afterAll(async () => {
+    await stopFixtureServer(fixtureServer);
+  });
+
+  beforeEach(async () => {
+    jest.setTimeout(150000);
   });
 
   it('generates random salt', async () => {
-    const fixture = new FixtureBuilder().build();
-    await withFixtures({ fixture, restartDevice: true }, async () => {
-      await loginToApp();
+    await TabBarComponent.tapSettings();
+    await SettingsView.scrollToAesCryptoButton();
+    await SettingsView.tapAesCryptoTestForm();
 
-      await TabBarComponent.tapSettings();
-      await SettingsView.scrollToAesCryptoButton();
-      await SettingsView.tapAesCryptoTestForm();
+    await AesCryptoTestForm.generateSalt(SALT_BYTES_COUNT.toString());
 
-      await AesCryptoTestForm.generateSalt(SALT_BYTES_COUNT.toString());
-
-      await expect(await AesCryptoTestForm.generateSaltResponse).toExist();
-    });
+    await expect(await AesCryptoTestForm.generateSaltResponse).toExist();
   });
 
   it('encrypts and decrypts using password', async () => {
-    const fixture = new FixtureBuilder().build();
-    await withFixtures({ fixture, restartDevice: true }, async () => {
-      await loginToApp();
-      // should go to settings then security & privacy
-      await TabBarComponent.tapSettings();
-      await SettingsView.scrollToAesCryptoButton();
-      await SettingsView.tapAesCryptoTestForm();
+    // encrypt and decrypt with password first piece of data
+    await AesCryptoTestForm.encrypt(DATA_TO_ENCRYPT_ONE, PASSWORD_ONE);
+    await AesCryptoTestForm.decrypt(PASSWORD_ONE);
+    await expect(await AesCryptoTestForm.decryptResponse).toHaveLabel(
+      DATA_TO_ENCRYPT_ONE,
+    );
 
-      // encrypt and decrypt with password first piece of data
-      await AesCryptoTestForm.encrypt(DATA_TO_ENCRYPT_ONE, PASSWORD_ONE);
-      await AesCryptoTestForm.decrypt(PASSWORD_ONE);
-      await expect(await AesCryptoTestForm.decryptResponse).toHaveLabel(
-        DATA_TO_ENCRYPT_ONE,
-      );
-
-      // encrypt and decrypt with password second piece of data
-      await AesCryptoTestForm.encrypt(DATA_TO_ENCRYPT_TWO, PASSWORD_TWO);
-      await AesCryptoTestForm.decrypt(PASSWORD_TWO);
-      await expect(await AesCryptoTestForm.decryptResponse).toHaveLabel(
-        DATA_TO_ENCRYPT_TWO,
-      );
-    });
+    // encrypt and decrypt with password second piece of data
+    await AesCryptoTestForm.encrypt(DATA_TO_ENCRYPT_TWO, PASSWORD_TWO);
+    await AesCryptoTestForm.decrypt(PASSWORD_TWO);
+    await expect(await AesCryptoTestForm.decryptResponse).toHaveLabel(
+      DATA_TO_ENCRYPT_TWO,
+    );
   });
 
   it('encrypts and decrypts using encryption key', async () => {
-    const fixture = new FixtureBuilder().build();
-    await withFixtures({ fixture, restartDevice: true }, async () => {
-      await loginToApp();
-      // should go to settings then security & privacy
-      await TabBarComponent.tapSettings();
-      await SettingsView.scrollToAesCryptoButton();
-      await SettingsView.tapAesCryptoTestForm();
+    // should go to settings then security & privacy
 
-      let encryptionKey;
+    let encryptionKey;
 
-      // generate new encryption key
-      encryptionKey = await AesCryptoTestForm.generateEncryptionKey(
-        PASSWORD_ONE,
-        SALT_ONE,
-      );
-      // encrypt and decrypt with password first piece of data
-      await AesCryptoTestForm.encryptWithKey(
-        encryptionKey,
-        DATA_TO_ENCRYPT_ONE,
-      );
-      await AesCryptoTestForm.decryptWithKey(encryptionKey);
-      await expect(await AesCryptoTestForm.decryptWithKeyResponse).toHaveLabel(
-        DATA_TO_ENCRYPT_ONE,
-      );
+    // generate new encryption key
+    encryptionKey = await AesCryptoTestForm.generateEncryptionKey(
+      PASSWORD_ONE,
+      SALT_ONE,
+    );
+    // encrypt and decrypt with password first piece of data
+    await AesCryptoTestForm.encryptWithKey(encryptionKey, DATA_TO_ENCRYPT_ONE);
+    await AesCryptoTestForm.decryptWithKey(encryptionKey);
+    await expect(await AesCryptoTestForm.decryptWithKeyResponse).toHaveLabel(
+      DATA_TO_ENCRYPT_ONE,
+    );
 
-      // generate new encryption key
-      await AesCryptoTestForm.scrollUpToGenerateEncryptionKey();
-      // encrypt and decrypt with password second piece of data
-      encryptionKey = await AesCryptoTestForm.generateEncryptionKey(
-        PASSWORD_TWO,
-        SALT_TWO,
-      );
-      await AesCryptoTestForm.encryptWithKey(
-        encryptionKey,
-        DATA_TO_ENCRYPT_TWO,
-      );
-      await AesCryptoTestForm.decryptWithKey(encryptionKey);
-      await expect(await AesCryptoTestForm.decryptWithKeyResponse).toHaveLabel(
-        DATA_TO_ENCRYPT_TWO,
-      );
-    });
+    // generate new encryption key
+    await AesCryptoTestForm.scrollUpToGenerateEncryptionKey();
+    // encrypt and decrypt with password second piece of data
+    encryptionKey = await AesCryptoTestForm.generateEncryptionKey(
+      PASSWORD_TWO,
+      SALT_TWO,
+    );
+    await AesCryptoTestForm.encryptWithKey(encryptionKey, DATA_TO_ENCRYPT_TWO);
+    await AesCryptoTestForm.decryptWithKey(encryptionKey);
+    await expect(await AesCryptoTestForm.decryptWithKeyResponse).toHaveLabel(
+      DATA_TO_ENCRYPT_TWO,
+    );
   });
 });
