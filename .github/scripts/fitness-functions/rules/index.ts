1f706abe3894e5df42cec63bda95892ef99ab3cb
sethkfman <10342624+sethkfman@users.noreply.github.com>
2024-06-10 21:02:43 -0600

    chore: fitness quality gate to only allow TS & TSX files in app directory (#9723)
    ## **Description**
    
    This PR adds a fitness function to fail if a .js or .jsx file is added
    to the app/ directory.
    
    ## **Related issues**
    
    Fixes:
    https://github.com/orgs/MetaMask/projects/60/views/3?pane=issue&itemId=59597337
    
    ## **Manual testing steps**
    
    1. Attempt to push a .js or .jsx file  in the app directory
    2. See the fitness function fail
    3.
    
    ## **Screenshots/Recordings**
    
    NA See unit tests
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    - Commit [outside of app
    directory](https://github.com/MetaMask/metamask-mobile/actions/runs/9353695806/job/25744808362)
    - Commit [inside of app
    directory](https://github.com/MetaMask/metamask-mobile/actions/runs/9455916396/job/26046720818?pr=9723)
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Cal Leung <cleun007@gmail.com>

--- .github/scripts/fitness-functions/rules/index.ts
+++ .github/scripts/fitness-functions/rules/index.ts
@@ -1,9 +1,15 @@
+import { preventJavaScriptFileAdditions } from './javascript-additions';
 import { preventCodeBlocksRule } from './prevent-code-blocks';
 
 const RULES: IRule[] = [
   {
     name: 'Check for blacklisted code blocks',
     fn: preventCodeBlocksRule,
+    docURL: '[WIP] No documentation exists for this rule yet.',
+  },
+  {
+    name: 'Check for js or jsx file being added',
+    fn: preventJavaScriptFileAdditions,
     docURL: '[WIP] No documentation exists for this rule yet.',
   },
 ];
@@ -32,5 +38,5 @@ function runFitnessFunctionRule(rule: IRule, diff: string): void {
   }
 }
 
-export { RULES, runFitnessFunctionRule };
+export { RULES, runFitnessFunctionRule, preventJavaScriptFileAdditions };
 export type { IRule };
