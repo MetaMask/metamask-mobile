f34a1d9e07a5d9f26eef7620a20e96d08c3a0f61
gantunesr <17601467+gantunesr@users.noreply.github.com>
2024-06-12 20:35:48 -0400

    chore: comment out unused variables

--- e2e/specs/accounts/aes/encryption-with-key.spec.js
+++ e2e/specs/accounts/aes/encryption-with-key.spec.js
@@ -22,11 +22,11 @@ describe(
   SmokeAccounts('AES Crypto - Encryption and decryption with encryption key'),
   () => {
     const PASSWORD_ONE = '123123123';
-    const PASSWORD_TWO = '456456456';
+    // const PASSWORD_TWO = '456456456';
     const SALT_ONE = 'ZDuWAyf5kcDxVvMgVaoyzJNB9kP3Ykdq8DSx8rR/+ro=';
-    const SALT_TWO = 'avJ8b37znYTLyeCL0sNxkYxctQrfUdFKoK7SeqC3JSU=';
+    // const SALT_TWO = 'avJ8b37znYTLyeCL0sNxkYxctQrfUdFKoK7SeqC3JSU=';
     const DATA_TO_ENCRYPT_ONE = 'random data to encrypt';
-    const DATA_TO_ENCRYPT_TWO = 'more random data to encrypt';
+    // const DATA_TO_ENCRYPT_TWO = 'more random data to encrypt';
 
     beforeAll(async () => {
       jest.setTimeout(150000);
