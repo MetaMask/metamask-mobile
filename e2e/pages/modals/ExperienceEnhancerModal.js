5023fd6ebdb1b48d1778397dc06d265f7106420c
Jony Bursztyn <jonathan@bursztyn.io>
2024-06-13 11:39:33 +0100

    Update e2e/pages/modals/ExperienceEnhancerModal.js
    Co-authored-by: Curtis David <Curtis.David7@gmail.com>

--- e2e/pages/modals/ExperienceEnhancerModal.js
+++ e2e/pages/modals/ExperienceEnhancerModal.js
@@ -10,7 +10,7 @@ class ExperienceEnhancerModal {
   }
 
   get iAgree() {
-    return Matchers.getElementByText(
+    return Matchers.getElementByID(
       ExperienceEnhancerModalSelectorsText.I_AGREE,
     );
   }
