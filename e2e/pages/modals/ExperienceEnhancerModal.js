97063f020f2b7ae156ef728b230dfb4814ad86c2
Jony Bursztyn <jonathan@bursztyn.io>
2024-06-13 11:38:57 +0100

    Update e2e/pages/modals/ExperienceEnhancerModal.js
    Co-authored-by: Curtis David <Curtis.David7@gmail.com>

--- e2e/pages/modals/ExperienceEnhancerModal.js
+++ e2e/pages/modals/ExperienceEnhancerModal.js
@@ -1,4 +1,4 @@
-import { ExperienceEnhancerModalSelectorsText } from '../../selectors/Modals/ExperienceEnhancerModal.selectors';
+import { ExperienceEnhancerModalSelectorsIDs } from '../../selectors/Modals/ExperienceEnhancerModal.selectors';
 import Matchers from '../../utils/Matchers';
 import Gestures from '../../utils/Gestures';
 
