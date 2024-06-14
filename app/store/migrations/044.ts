d668f2a6a63f68db58d9368a38195672d51e0a9f
tommasini <tommasini15@gmail.com>
2024-06-14 15:38:41 +0100

    synchronize accounts name with migration 44

---
+++ app/store/migrations/044.ts
@@ -0,0 +1,134 @@
+import { hasProperty, isObject } from '@metamask/utils';
+import { ensureValidState } from './util';
+import { captureException } from '@sentry/react-native';
+import { Identity } from '@metamask/preferences-controller';
+import { toChecksumHexAddress } from '@metamask/controller-utils';
+import { AccountsControllerState } from '@metamask/accounts-controller';
+
+/**
+ * Synchronize `AccountsController` names with `PreferencesController` identities.
+ *
+ * There was a bug in versions below v7.23.0 that resulted in the account `name` state in the
+ * `AccountsController` being reset. However, users account names were preserved in the
+ * `identities` state in the `PreferencesController`. This migration restores the names to the
+ * `AccountsController` if they are found.
+ *
+ * @param state Persisted Redux state that is potentially corrupted
+ * @returns Valid persisted Redux state
+ */
+export default function migrate(state: unknown) {
+  if (!ensureValidState(state, 44)) {
+    return state;
+  }
+
+  const accountsControllerState =
+    state.engine.backgroundState.AccountsController;
+
+  if (!isObject(accountsControllerState)) {
+    captureException(
+      new Error(
+        `FATAL ERROR: Migration 44: Invalid AccountsController state error: '${JSON.stringify(
+          accountsControllerState,
+        )}'`,
+      ),
+    );
+    return state;
+  }
+
+  if (
+    !hasProperty(accountsControllerState, 'internalAccounts') ||
+    !isObject(accountsControllerState.internalAccounts)
+  ) {
+    captureException(
+      new Error(
+        `FATAL ERROR: Migration 44: Invalid AccountsController internalAccounts state error: '${accountsControllerState.internalAccounts}'`,
+      ),
+    );
+    return state;
+  }
+
+  if (
+    !hasProperty(accountsControllerState.internalAccounts, 'accounts') ||
+    !isObject(accountsControllerState.internalAccounts.accounts)
+  ) {
+    captureException(
+      new Error(
+        `FATAL ERROR: Migration 44: Invalid AccountsController internalAccounts accounts state error: '${accountsControllerState.internalAccounts.accounts}'`,
+      ),
+    );
+    return state;
+  }
+
+  if (
+    Object.values(accountsControllerState.internalAccounts.accounts).some(
+      (account) => !isObject(account),
+    )
+  ) {
+    const invalidEntry = Object.entries(
+      accountsControllerState.internalAccounts.accounts,
+    ).find(([_, account]) => !isObject(account));
+    captureException(
+      new Error(
+        `FATAL ERROR: Migration 44: Invalid AccountsController entry with id: '${
+          invalidEntry?.[0]
+        }', type: '${typeof invalidEntry?.[1]}'`,
+      ),
+    );
+    return state;
+  }
+
+  const preferencesControllerState =
+    state.engine.backgroundState.PreferencesController;
+
+  if (!isObject(preferencesControllerState)) {
+    captureException(
+      new Error(
+        `FATAL ERROR: Migration 44: Invalid PreferencesController state error: '${JSON.stringify(
+          preferencesControllerState,
+        )}'`,
+      ),
+    );
+    return state;
+  }
+
+  if (
+    !hasProperty(preferencesControllerState, 'identities') ||
+    !isObject(preferencesControllerState.identities)
+  ) {
+    captureException(
+      new Error(
+        `FATAL ERROR: Migration 44: Invalid PreferencesController identities state error: '${preferencesControllerState.identities}'`,
+      ),
+    );
+    return state;
+  }
+  const accounts = accountsControllerState.internalAccounts.accounts;
+  const identities = preferencesControllerState.identities;
+  Object.keys(accounts).forEach((accountId) => {
+    const account = accounts[accountId];
+    if (isObject(account) && isObject(account.metadata)) {
+      if (Object.keys(identities).length) {
+        Object.keys(identities).forEach((identityAddress) => {
+          if (
+            toChecksumHexAddress(identityAddress) ===
+            toChecksumHexAddress(account.address as string)
+          ) {
+            const identity = identities[identityAddress];
+            if (
+              isObject(identity) &&
+              isObject(account.metadata) &&
+              identity.name !== account.metadata.name
+            ) {
+              (
+                accountsControllerState as AccountsControllerState
+              ).internalAccounts.accounts[accountId].metadata.name =
+                identity.name as string;
+            }
+          }
+        });
+      }
+    }
+  });
+
+  return state;
+}
