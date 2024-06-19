7758d2009892ef76b5b5a7102d714c60b7af29c0
Vinicius Stevam <45455812+vinistevam@users.noreply.github.com>
2024-06-19 14:30:02 +0100

    feat: add set approve for all screen (#9888)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    This PR aims to reuse the token approve confirmation screen for
    `setapproveforall`.
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/MetaMask-planning/issues/2408
    
    ## **Manual testing steps**
    
    1. Go to test dapp
    2. Deploy ERC721
    3. Mint 
    4. Click `SET APPROVAL FOR ALL`
    5. Confirm and check in the activity view if the transaction was a `set
    approval for all` and it's confirmed.
    
    ## **Screenshots/Recordings**
    
    
    [set-approve-for-all.webm](https://github.com/MetaMask/metamask-mobile/assets/45455812/e64f5f27-b9c4-4c32-8573-52475a6f2ce4)
    
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Matthew Walsh <matthew.walsh@consensys.net>
    Co-authored-by: Nico MASSART <NicolasMassart@users.noreply.github.com>

--- e2e/selectors/Browser/TestDapp.selectors.js
+++ e2e/selectors/Browser/TestDapp.selectors.js
@@ -7,6 +7,7 @@ export const TestDappSelectorsWebIDs = {
   INCREASE_ALLOWANCE_BUTTON_ID: 'increaseTokenAllowance',
   NFT_TRANSFER_FROM_BUTTON_ID: 'transferFromButton',
   PERSONAL_SIGN: 'personalSign',
+  SET_APPROVAL_FOR_ALL_BUTTON_ID: 'setApprovalForAllButton',
   SIGN_TYPE_DATA: 'signTypedData',
   SIGN_TYPE_DATA_V3: 'signTypedDataV3',
   SIGN_TYPE_DATA_V4: 'signTypedDataV4',
