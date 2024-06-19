6fa3955b17cd905c75dd649541b2d509c326c485
IF <139582705+infiniteflower@users.noreply.github.com>
2024-06-19 14:25:53 -0400

    chore: make test data more realistic

--- app/components/Nav/Main/index.test.tsx
+++ app/components/Nav/Main/index.test.tsx
@@ -21,8 +21,45 @@ describe('Main', () => {
   });
 
   describe('useSwapConfirmedEvent', () => {
+    const txMetaId = '04541dc0-2e69-11ef-b995-33aef2c88d1e';
+    const swapsTransactions = {
+      [txMetaId]: {
+        action: 'swap',
+        analytics: {
+          available_quotes: 5,
+          best_quote_source: 'oneInchV5',
+          chain_id: '1',
+          custom_slippage: false,
+          network_fees_ETH: '0.00337',
+          network_fees_USD: '$12.04',
+          other_quote_selected: false,
+          request_type: 'Order',
+          token_from: 'ETH',
+          token_from_amount: '0.001254',
+          token_to: 'USDC',
+          token_to_amount: '4.440771',
+        },
+        destinationAmount: '4440771',
+        destinationToken: {
+          address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+          decimals: 6,
+        },
+        paramsForAnalytics: {
+          approvalTransactionMetaId: {},
+          ethAccountBalance: '0xedfffbea734a07',
+          gasEstimate: '0x33024',
+          sentAt: '0x66732203',
+        },
+        sourceAmount: '1254000000000000',
+        sourceAmountInFiat: '$4.47',
+        sourceToken: {
+          address: '0x0000000000000000000000000000000000000000',
+          decimals: 18,
+        },
+      },
+    };
+
     it('queues transactionMeta ids correctly', () => {
-      const txMetaId = 'foo';
       const eventEmitter = new EventEmitter();
 
       const { result } = renderHook(() =>
@@ -41,7 +78,6 @@ describe('Main', () => {
       expect(result.current.transactionMetaIdsForListening).toEqual([txMetaId]);
     });
     it('adds a listener for transaction confirmation on the TransactionController', () => {
-      const txMetaId = 'foo';
       const eventEmitter = new EventEmitter();
 
       const { result } = renderHook(() =>
@@ -65,15 +101,8 @@ describe('Main', () => {
       expect(eventEmitter.listenerCount(`${txMetaId}:confirmed`)).toBe(1);
     });
     it('tracks Swap Confirmed after transaction confirmed', () => {
-      const txMetaId = 'foo';
       const eventEmitter = new EventEmitter();
       const trackSwaps = jest.fn();
-      const swapsTransactions = {
-        [txMetaId]: {
-          analytics: 'hello',
-          paramsForAnalytics: 'world',
-        },
-      };
       const txMeta = {
         id: txMetaId,
       };
@@ -99,15 +128,8 @@ describe('Main', () => {
       );
     });
     it('removes transactionMeta id after tracking', () => {
-      const txMetaId = 'foo';
       const eventEmitter = new EventEmitter();
       const trackSwaps = jest.fn();
-      const swapsTransactions = {
-        [txMetaId]: {
-          analytics: 'hello',
-          paramsForAnalytics: 'world',
-        },
-      };
       const txMeta = {
         id: txMetaId,
       };
