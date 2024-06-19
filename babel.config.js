b38abf89697babdc914db7f157f1121fc2acac6a
JSoufer <jonathan.ferreira@consensys.net>
2024-06-19 18:24:48 +0100

    chore: fix babel

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,10 @@ module.exports = {
       test: './node_modules/marked',
       plugins: [['@babel/plugin-transform-private-methods', { loose: true }]],
     },
+    {
+      test: './node_modules/@metamask/profile-sync-controller',
+      plugins: [['@babel/plugin-transform-private-methods', { loose: true }]],
+    },
   ],
   env: {
     production: {
