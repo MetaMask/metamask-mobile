83ba0bae523adf5584ec9e1ba00c1809a09b2fce
tommasini <46944231+tommasini@users.noreply.github.com>
2024-06-21 19:26:16 +0100

    chore: Update accounts controller v14 (#9779)
    ## **Description**
    
    This PR updates accounts controller to v^14.
    
    qa build:
    https://app.bitrise.io/app/be69d4368ee7e86d/pipelines/96496314-9441-4329-bd43-fc9e4f1e3dcd
    
    This PR also includes this PR:
    https://github.com/MetaMask/metamask-mobile/pull/9832
    
    ## **Related issues**
    
    ## Blocked by
    
    - [x] https://github.com/MetaMask/metamask-mobile/pull/9718
    This PR is dependent on the snaps controller update
    [PR](https://github.com/MetaMask/metamask-mobile/pull/9718)
    - [x] https://github.com/MetaMask/metamask-mobile/pull/9570
    This PR is dependent on the keyring controller update
    [PR](https://github.com/MetaMask/metamask-mobile/pull/9570)
    
    ## Fixes
    - https://github.com/MetaMask/mobile-planning/issues/1777
    
    ## **Manual testing steps**
    
    1. Import account via SRP
    2. Import account via private key
    3. Import tokens 
    
    ## **Screenshots/Recordings**
    
    * Import with private key
    
    
    ![image](https://github.com/MetaMask/metamask-mobile/assets/46944231/fdfe0e97-bfc8-4a62-ae76-3aebc91fd8fe)
    
    
    * Importing with SRP
    
    
    
    https://github.com/MetaMask/metamask-mobile/assets/46944231/d829d4d6-6e28-404f-b1bb-e0ed3ec6f986
    
    
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: legobt <6wbvkn0j@anonaddy.me>
    Co-authored-by: Nico MASSART <NicolasMassart@users.noreply.github.com>

--- app/components/Views/DetectedTokens/components/Token.tsx
+++ app/components/Views/DetectedTokens/components/Token.tsx
@@ -111,7 +111,7 @@ const Token = ({ token, selected, toggleSelected }: Props) => {
   const tokenBalances = useSelector(selectContractBalances);
   const conversionRate = useSelector(selectConversionRate);
   const currentCurrency = useSelector(selectCurrentCurrency);
-  const exchangeRate = tokenExchangeRates[address];
+  const tokenMarketData = tokenExchangeRates?.[address] ?? null;
   const tokenBalance = renderFromTokenMinimalUnit(
     tokenBalances[address],
     decimals,
@@ -122,7 +122,7 @@ const Token = ({ token, selected, toggleSelected }: Props) => {
   const fiatBalance = balanceToFiat(
     tokenBalance,
     conversionRate,
-    exchangeRate,
+    tokenMarketData?.price || undefined,
     currentCurrency,
   );
 
