ff9d9961b1c71337555aef5c11dfcf7e70a5315c
tommasini <tommasini15@gmail.com>
2024-06-24 23:31:24 +0100

    fix lint issues

--- app/core/RPCMethods/createUnsupportedMethodMiddleware/index.ts
+++ app/core/RPCMethods/createUnsupportedMethodMiddleware/index.ts
@@ -2,14 +2,15 @@ import { rpcErrors } from '@metamask/rpc-errors';
 import type { JsonRpcMiddleware } from '@metamask/json-rpc-engine';
 
 import { UNSUPPORTED_RPC_METHODS } from '../utils';
+import { Json, JsonRpcParams } from '@metamask/utils';
 
 /**
  * Creates a middleware that rejects explicitly unsupported RPC methods with the
  * appropriate error.
  */
 const createUnsupportedMethodMiddleware = (): JsonRpcMiddleware<
-  unknown,
-  void
+  JsonRpcParams,
+  Json
 > =>
   async function unsupportedMethodMiddleware(req, _res, next, end) {
     if ((UNSUPPORTED_RPC_METHODS as Set<string>).has(req.method)) {
