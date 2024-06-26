6abaa206e09b55c095492f1c9e920e8bfa592a63
Thomas Huang <tmashuang@users.noreply.github.com>
2024-06-26 09:18:52 -0700

    feat: add api spec test infrastructure (#9356)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    This PR adds tests using
    [api-specs](https://github.com/MetaMask/api-specs) via the
    [@open-rpc/test-coverage](https://github.com/open-rpc/test-coverage)
    tool.
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    Fixes: https://github.com/MetaMask/MetaMask-planning/issues/2238
    
    1. `yarn setup`
    2. `yarn test:e2e:ios:debug:build`
    3. `yarn test:api-specs`
    
    ![Screenshot 2024-06-18 at 3 25
    26 PM](https://github.com/MetaMask/metamask-mobile/assets/364566/ecad8a8a-60ed-4f89-b85e-3e4ba34ef692)
    
    ![image](https://github.com/MetaMask/metamask-mobile/assets/364566/82e7bfc1-933b-4a14-80ca-ca7baf34904f)
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    - [x] I’ve followed [MetaMask Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Shane Jonas <jonas.shane@gmail.com>
    Co-authored-by: Curtis <Curtis.David7@gmail.com>

--- e2e/selectors/Modals/ConnectAccountModal.selectors.js
+++ e2e/selectors/Modals/ConnectAccountModal.selectors.js
@@ -3,9 +3,12 @@ import enContent from '../../../locales/languages/en.json';
 export const ConnectAccountModalSelectorsIDs = {
   CONTAINER: 'connect-account-modal',
   SELECT_MULTI_BUTTON: 'multiconnect-connect-button',
+  CONNECT_BUTTON: 'connect-button',
+  CANCEL_BUTTON: 'cancel-button',
 };
 
 export const ConnectAccountModalSelectorsText = {
+  CONNECT: enContent.accounts.connect,
   CONNECT_ACCOUNTS: enContent.accounts.connect_multiple_accounts,
   IMPORT_ACCOUNT: enContent.account_actions.add_account_or_hardware_wallet,
   SELECT_ALL: enContent.accounts.select_all,
