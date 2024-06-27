6a4cd815d91a41db0be29541eb705c5bd998415a
Samuel Salas <samuel.salas.reyes@gmail.com>
2024-06-27 16:18:29 -0600

    Move testIDs to object

--- e2e/selectors/wallet/WalletView.selectors.js
+++ e2e/selectors/wallet/WalletView.selectors.js
@@ -8,6 +8,8 @@ export const WalletViewSelectorsIDs = {
   WALLET_NOTIFICATIONS_BUTTON: 'wallet-notifications-button',
   WALLET_TOKEN_DETECTION_LINK_BUTTON: 'wallet-token-detection-link-button',
   PORTFOLIO_BUTTON: 'portfolio-button',
+  TOTAL_BALANCE_TEXT: 'total-balance-text',
+  STAKE_BUTTON: 'stake-button',
 };
 
 export const WalletViewSelectorsText = {
