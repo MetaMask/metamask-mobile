d39e8a7d00b80da3eea1bbb7cc7d19b27a76546a
Jonathan Ferreira <44679989+Jonathansoufer@users.noreply.github.com>
2024-06-27 14:19:28 +0100

    feat: Integrate Firebase libraries and initial config to enable Push Notifications FCM. (#10085)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    This PR aims to handle ONLY the addition of Firebase related libraries
    to our codebase as well implements iOS and Android specific setup to
    enable Push Notifications FCM on MetaMask Mobile. No changes on
    consuming Push Notifications will take place on THIS PR since we're
    breaking this implementation down. No visual changes are introduced as
    well nor ways to test it, since the video updated is just to increase
    the understanding of what the changes will empower.
    
    Documentation used for implementing it, [here](https://rnfirebase.io/)
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    
    https://github.com/MetaMask/metamask-mobile/assets/44679989/dd9f7570-a4cb-4831-9cb2-23bc5ce920a4
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

---
+++ app/util/notifications/methods/fcmHelper.ts
@@ -0,0 +1,99 @@
+import { utils } from '@react-native-firebase/app';
+import messaging, {
+  FirebaseMessagingTypes,
+} from '@react-native-firebase/messaging';
+import Logger from '../../../util/Logger';
+import { PERMISSIONS, request } from 'react-native-permissions';
+
+export async function checkPlayServices() {
+  const { status, isAvailable, hasResolution, isUserResolvableError } =
+    utils().playServicesAvailability;
+  if (isAvailable) return Promise.resolve();
+
+  if (isUserResolvableError || hasResolution) {
+    switch (status) {
+      case 1:
+        return utils().makePlayServicesAvailable();
+      case 2:
+        return utils().resolutionForPlayServices();
+      default:
+        if (isUserResolvableError) return utils().promptForPlayServices();
+        if (hasResolution) return utils().resolutionForPlayServices();
+    }
+  }
+  return Promise.reject(
+    new Error('Unable to find a valid play services version.'),
+  );
+}
+
+export async function registerAppWithFCM() {
+  Logger.log(
+    'registerAppWithFCM status',
+    messaging().isDeviceRegisteredForRemoteMessages,
+  );
+  if (!messaging().isDeviceRegisteredForRemoteMessages) {
+    await messaging()
+      .registerDeviceForRemoteMessages()
+      .then((status: unknown) => {
+        Logger.log('registerDeviceForRemoteMessages status', status);
+      })
+      .catch((error: unknown) => {
+        Logger.log('registerDeviceForRemoteMessages error ', error);
+      });
+  }
+}
+
+export async function unRegisterAppWithFCM() {
+  Logger.log(
+    'unRegisterAppWithFCM status',
+    messaging().isDeviceRegisteredForRemoteMessages,
+  );
+
+  if (messaging().isDeviceRegisteredForRemoteMessages) {
+    await messaging()
+      .unregisterDeviceForRemoteMessages()
+      .then((status: unknown) => {
+        Logger.log('unregisterDeviceForRemoteMessages status', status);
+      })
+      .catch((error: unknown) => {
+        Logger.log('unregisterDeviceForRemoteMessages error ', error);
+      });
+  }
+  await messaging().deleteToken();
+  Logger.log(
+    'unRegisterAppWithFCM status',
+    messaging().isDeviceRegisteredForRemoteMessages,
+  );
+}
+
+export const checkApplicationNotificationPermission = async () => {
+  const authStatus = await messaging().requestPermission();
+
+  const enabled =
+    authStatus === FirebaseMessagingTypes.AuthorizationStatus.AUTHORIZED ||
+    authStatus === FirebaseMessagingTypes.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    Logger.log('Authorization status:', authStatus);
+  }
+  request(PERMISSIONS.ANDROID.POST_NOTIFICATIONS)
+    .then((result) => {
+      Logger.log('POST_NOTIFICATIONS status:', result);
+    })
+    .catch((error: unknown) => {
+      Logger.log('POST_NOTIFICATIONS error ', error);
+    });
+};
+
+export const getFcmToken = async () => {
+  let token = null;
+  await checkApplicationNotificationPermission();
+  await registerAppWithFCM();
+  try {
+    token = await messaging().getToken();
+    Logger.log('getFcmToken-->', token);
+  } catch (error) {
+    Logger.log('getFcmToken Device Token error ', error);
+  }
+  return token;
+};
