MetaMask/metamask-mobile

commit 2384af6f675f82d3abbf13f3b31f6095922c0ac7

chore: Update `@metamask/keyring-controller` to v16 (#9570)
<!--
Please submit this PR as a draft initially.
Do not mark it as "Ready for review" until the template has been
completely filled out, and PR status checks have passed at least once.
-->

## **Description**

The package `@metamask/keyring-controller` has been updated from v13 to
v16. See here for the changelog:
https://github.com/MetaMask/core/blob/%40metamask/keyring-controller%4016.0.0/packages/keyring-controller/CHANGELOG.md#1600

### Breaking KeyringController changes

Most of the changes did not require any code changes in mobile. The
breaking changes in v14 were mainly type changes that didn't break
anything for us, and the changes in v15 only impacted the extension (it
changed the meaning of the optional `accountCount` parameter to
`addNewAccount`, which mobile doesn't use).

The main breaking change is that as of v16, a number of methods no
longer return a copy of the `KeyringController` state. We were never
relying on this return value, but we had to adjust how a few of these
methods were called or mocked to accommodate this change.

### withKeyring method

v16 of the KeyringController introduced a new method for performing
custom keyring operations, called `withKeyring`. This is meant as an
alternative approach to using `getKeyringForAccount` or
`getKeyringsByType` to get a keyring instance, then using that keyring
instance directly.

The `withKeyring` method helps with getting the keyring instance, and it
ensures that the KeyringController is locked while the operation is
performed, and it ensures that the KeyringController state is updated
when the operation is finished. This gets rid of a bunch of boilerplate
in our QR and Ledger integrations, and adds additional protection
against race conditions and non-atomic state changes.

It's not strictly required as part of this update, but this was

app/core/Engine.ts
@@ -243,6 +243,7 @@ type GlobalActions =
   | GetCurrencyRateState
   | GetGasFeeState
   | GetTokenListState
+  | KeyringControllerActions
   | NetworkControllerActions
   | PermissionControllerActions
   | SignatureControllerActions
@@ -259,6 +260,7 @@ type GlobalEvents =
   | ApprovalControllerEvents
   | CurrencyRateStateChange
   | GasFeeStateChange
+  | KeyringControllerEvents
   | TokenListStateChange
   | NetworkControllerEvents
   | PermissionControllerEvents
@@ -676,7 +678,6 @@ class Engine {
         preferencesController,
       ),
       encryptor,
-      //@ts-expect-error Misalign types because of Base Controller version
       messenger: this.controllerMessenger.getRestricted({
         name: 'KeyringController',
         allowedActions: [],
@@ -1329,7 +1330,7 @@ class Engine {
     for (const controller of controllers) {
       if (
         hasProperty(initialState, controller.name) &&
-        //@ts-expect-error Misalign types because of Base Controller version
+        hasProperty(controller, 'subscribe') &&
         controller.subscribe !== undefined
       ) {
         // The following type error can be addressed by passing initial state into controller constructors instead

app/components/Nav/Main/RootRPCMethodsUI.js
@@ -59,7 +59,7 @@ import TemplateConfirmationModal from '../../Approvals/TemplateConfirmationModal
 import { selectTokenList } from '../../../selectors/tokenListController';
 import { selectTokens } from '../../../selectors/tokensController';
 import { selectSelectedAddress } from '../../../selectors/preferencesController';
-import { getLedgerKeyring } from '../../../core/Ledger/Ledger';
+import { getDeviceId } from '../../../core/Ledger/Ledger';
 import { createLedgerTransactionModalNavDetails } from '../../UI/LedgerModals/LedgerTransactionModal';
 import ExtendedKeyringTypes from '../../../constants/keyringTypes';
 import { useMetrics } from '../../../components/hooks/useMetrics';
@@ -251,12 +251,12 @@ const RootRPCMethodsUI = (props) => {
 
         // For Ledger Accounts we handover the signing to the confirmation flow
         if (isLedgerAccount) {
-          const ledgerKeyring = await getLedgerKeyring();
+          const deviceId = await getDeviceId();
 
           props.navigation.navigate(
             ...createLedgerTransactionModalNavDetails({
               transactionId: transactionMeta.id,
-              deviceId: ledgerKeyring.deviceId,
+              deviceId,
               // eslint-disable-next-line no-empty-function
               onConfirmationComplete: () => {},
               type: 'signTransaction',

app/components/UI/QRHardware/withQRHardwareAwareness.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, ComponentClass } from 'react';
+import React, { useState, useEffect, ComponentClass } from 'react';
 import Engine from '../../../core/Engine';
 import { IQRState } from './types';
 
@@ -10,7 +10,6 @@ const withQRHardwareAwareness = (
   }>,
 ) => {
   const QRHardwareAwareness = (props: any) => {
-    const keyringState: any = useRef();
     const [QRState, SetQRState] = useState<IQRState>({
       sync: {
         reading: false,
@@ -23,15 +22,20 @@ const withQRHardwareAwareness = (
     };
 
     useEffect(() => {
-      const { KeyringController } = Engine.context as any;
-      KeyringController.getQRKeyringState().then((store: any) => {
-        keyringState.current = store;
-        keyringState.current.subscribe(subscribeKeyringState);
-      });
+      // This ensures that a QR keyring gets created if it doesn't already exist.
+      // This is intentionally not awaited (the subscription still gets setup correctly if called
+      // before the keyring is created).
+      // TODO: Stop automatically creating keyrings
+      Engine.context.KeyringController.getOrAddQRKeyring();
+      Engine.controllerMessenger.subscribe(
+        'KeyringController:qrKeyringStateChange',
+        subscribeKeyringState,
+      );
       return () => {
-        if (keyringState.current) {
-          keyringState.current.unsubscribe(subscribeKeyringState);
-        }
+        Engine.controllerMessenger.unsubscribe(
+          'KeyringController:qrKeyringStateChange',
+          subscribeKeyringState,
+        );
       };
     }, []);
 

app/components/UI/Transactions/index.js
@@ -65,7 +65,7 @@ import {
   CancelTransactionError,
   SpeedupTransactionError,
 } from '../../../core/Transaction/TransactionError';
-import { getLedgerKeyring } from '../../../core/Ledger/Ledger';
+import { getDeviceId } from '../../../core/Ledger/Ledger';
 import ExtendedKeyringTypes from '../../../constants/keyringTypes';
 import { TOKEN_OVERVIEW_TXN_SCREEN } from '../../../../wdio/screen-objects/testIDs/Screens/TokenOverviewScreen.testIds';
 import {
@@ -552,7 +552,7 @@ class Transactions extends PureComponent {
   };
 
   signLedgerTransaction = async (transaction) => {
-    const ledgerKeyring = await getLedgerKeyring();
+    const deviceId = await getDeviceId();
 
     const onConfirmation = (isComplete) => {
       if (isComplete) {
@@ -566,7 +566,7 @@ class Transactions extends PureComponent {
     this.props.navigation.navigate(
       ...createLedgerTransactionModalNavDetails({
         transactionId: transaction.id,
-        deviceId: ledgerKeyring.deviceId,
+        deviceId,
         onConfirmationComplete: onConfirmation,
         type: 'signTransaction',
         replacementParams: transaction?.replacementParams,

app/components/Views/AccountConnect/AccountConnect.tsx
@@ -361,7 +361,7 @@ const AccountConnect = (props: AccountConnectProps) => {
       const { KeyringController } = Engine.context;
       try {
         setIsLoading(true);
-        const { addedAccountAddress } = await KeyringController.addNewAccount();
+        const addedAccountAddress = await KeyringController.addNewAccount();
         const checksummedAddress = safeToChecksumAddress(
           addedAccountAddress,
         ) as string;

app/components/Views/AddAccountActions/AddAccountActions.tsx
@@ -40,7 +40,7 @@ const AddAccountActions = ({ onBack }: AddAccountActionsProps) => {
     try {
       setIsLoading(true);
 
-      const { addedAccountAddress } = await KeyringController.addNewAccount();
+      const addedAccountAddress = await KeyringController.addNewAccount();
       Engine.setSelectedAddress(addedAccountAddress);
       trackEvent(MetaMetricsEvents.ACCOUNTS_ADDED_NEW_ACCOUNT, {});
     } catch (e: any) {

app/components/Views/Asset/index.test.js
@@ -18,8 +18,11 @@ jest.mock('../../../core/Engine.ts', () => ({
   init: () => mockedEngine.init({}),
   context: {
     KeyringController: {
-      getQRKeyringState: async () => ({ subscribe: () => ({}) }),
+      getOrAddQRKeyring: async () => ({ subscribe: () => ({}) }),
     },
+  },
+  controllerMessenger: {
+    subscribe: jest.fn(),
   },
 }));
 

app/components/Views/ConnectHardware/SelectHardware/index.tsx
@@ -16,7 +16,7 @@ import Text, {
 } from '../../../../component-library/components/Texts/Text';
 import Routes from '../../../../constants/navigation/Routes';
 import { MetaMetricsEvents } from '../../../../core/Analytics';
-import { getLedgerKeyring } from '../../../../core/Ledger/Ledger';
+import { withLedgerKeyring } from '../../../../core/Ledger/Ledger';
 import { fontStyles } from '../../../../styles/common';
 import {
   mockTheme,
@@ -25,6 +25,7 @@ import {
 } from '../../../../util/theme';
 import { getNavigationOptionsTitle } from '../../../UI/Navbar';
 import { useMetrics } from '../../../../components/hooks/useMetrics';
+import type LedgerKeyring from '@consensys/ledgerhq-metamask-keyring';
 
 const createStyle = (colors: any) =>
   StyleSheet.create({
@@ -105,8 +106,9 @@ const SelectHardwareWallet = () => {
   };
 
   const navigateToConnectLedger = async () => {
-    const ledgerKeyring = await getLedgerKeyring();
-    const accounts = await ledgerKeyring.getAccounts();
+    const accounts = await withLedgerKeyring(async (keyring: LedgerKeyring) =>
+      keyring.getAccounts(),
+    );
 
     trackEvent(MetaMetricsEvents.CONNECT_LEDGER, {
       device_type: 'Ledger',

commit 9c5c461ecebb29ff8f34f4a87ac5fa2b37da3de1

fix: Fix BaseControllerV1 state rehydration
We were failing to properly restore persisted state for some
BaseControllerV1 controllers. This resulted in state being lost when
the application was restarted. The only confirmed affected controller
so far is the `NftController`.

The context here is that for some older controllers, rather than
passing the initial state into the controller directly when it is
constructed, we call the `update` function directly. This has always
been a bad practice (controllers should update their own state), and
we're forced to fix this for controllers that we update to
`BaseControllerV2` (because directly updating state like this is no
longer possible. But this method of rehydrating controller state was
still relied upon in some cases.

This was broken recently in #9570 when a condition was added to fix
a type error. The condition was meant to check that the controller had
a `subscribe` function. Unfortunately this `hasProperty` check only
looks at owned properties, not inherited properties, and the
`subscribe` function was inherited from the base class. It has been
updated to use the `in` operator instead, which does look up the
entire prototype chain.

Fixes #10057

app/core/Engine.ts
@@ -1340,7 +1340,8 @@ class Engine {
     for (const controller of controllers) {
       if (
         hasProperty(initialState, controller.name) &&
-        hasProperty(controller, 'subscribe') &&
+        // Use `in` operator here because the `subscribe` function is one level up the prototype chain
+        'subscribe' in controller &&
         controller.subscribe !== undefined
       ) {
         // The following type error can be addressed by passing initial state into controller constructors instead