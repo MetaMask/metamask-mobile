fb9ba802171d5ba84f8f1a983fbf49af919eeec8
JSoufer <jonathan.ferreira@consensys.net>
2024-06-28 14:26:35 +0100

    chore: preview babel handlers

--- babel.config.js
+++ babel.config.js
@@ -12,6 +12,14 @@ module.exports = {
       plugins: [['@babel/plugin-transform-private-methods', { loose: true }]],
     },
     {
+      test: './node_modules/@metamask/profile-sync-controller',
+      plugins: [['@babel/plugin-transform-private-methods', { loose: true }]],
+    },
+    {
+      test: './node_modules/@metamask/notification-services-controller',
+      plugins: [['@babel/plugin-transform-private-methods', { loose: true }]],
+    },
+    {
       test: './node_modules/@metamask-previews/profile-sync-controller',
       plugins: [['@babel/plugin-transform-private-methods', { loose: true }]],
     },
