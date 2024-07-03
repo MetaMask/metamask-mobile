e96b6b1ca66275913941418a9d3ac4644963ec8d
Jyoti Puri <jyotipuri@gmail.com>
2024-07-03 09:42:26 +0530

    update

--- app/lib/ppom/PPOMView.test.tsx
+++ app/lib/ppom/PPOMView.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { shallow } from 'enzyme';
 
 import { PPOMView } from './PPOMView';
 
 describe('PPOMView', () => {
-  it('should render correctly deeply', () => {
-    const wrapper = render(<PPOMView />);
+  it('should render correctly', () => {
+    const wrapper = shallow(<PPOMView />);
     expect(wrapper).toMatchSnapshot();
   });
 });
