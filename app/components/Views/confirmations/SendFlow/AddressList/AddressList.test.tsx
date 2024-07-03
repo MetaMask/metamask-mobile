27df76a173d8f85cfb6e42884535768e24216150
sahar-fehri <sahar.fehri@consensys.net>
2024-07-03 16:39:40 +0200

    fix: fix

--- app/components/Views/confirmations/SendFlow/AddressList/AddressList.test.tsx
+++ app/components/Views/confirmations/SendFlow/AddressList/AddressList.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import renderWithProvider from '../../../../../util/test/renderWithProvider';
 import initialBackgroundState from '../../../../../util/test/initial-background-state.json';
 import AddressList from '.';
+import { createMockAccountsControllerState } from '../../../../../util/test/accountsControllerTestUtils';
 
 jest.mock('../../../../../core/Engine', () => ({
   context: {
@@ -13,35 +14,61 @@ jest.mock('../../../../../core/Engine', () => ({
   },
 }));
 
+const BUSINESS_ACCOUNT = '0xC4955C0d639D99699Bfd7Ec54d9FaFEe40e4D272';
+const PERSONAL_ACCOUNT = '0xd018538C87232FF95acbCe4870629b75640a78E7';
+
+const MOCK_ACCOUNTS_CONTROLLER_STATE = createMockAccountsControllerState([
+  BUSINESS_ACCOUNT,
+  PERSONAL_ACCOUNT,
+]);
+
 const initialState = {
   engine: {
     backgroundState: {
       ...initialBackgroundState,
-      AddressBookController: {
-        addressBook: {
-          '0x1': {
-            '0xC4955C0d639D99699Bfd7Ec54d9FaFEe40e4D272': {
-              address: '0xC4955C0d639D99699Bfd7Ec54d9FaFEe40e4D272',
-              chainId: '0x1',
-              isEns: false,
-              memo: '',
-              name: 'aa',
-            },
-          },
+      NetworkController: {
+        network: '1',
+        providerConfig: {
+          ticker: 'ETH',
+          type: 'mainnet',
+          chainId: '0x1',
+        },
+      },
+      AccountsController: MOCK_ACCOUNTS_CONTROLLER_STATE,
+      AccountTrackerController: {
+        accounts: {
+          [BUSINESS_ACCOUNT]: { balance: '0xDE0B6B3A7640000' },
+          [PERSONAL_ACCOUNT]: { balance: '0x1BC16D674EC80000' },
         },
       },
       PreferencesController: {
+        isMultiAccountBalancesEnabled: true,
+        selectedAddress: BUSINESS_ACCOUNT,
         identities: {
-          '0xC4955C0d639D99699Bfd7Ec54d9FaFEe40e4D272': {
-            address: '0xC4955C0d639D99699Bfd7Ec54d9FaFEe40e4D272',
-            name: 'Account 1',
+          [BUSINESS_ACCOUNT]: {
+            address: BUSINESS_ACCOUNT,
+            name: 'Business Account',
+          },
+          [PERSONAL_ACCOUNT]: {
+            address: PERSONAL_ACCOUNT,
+            name: 'Personal Account',
+          },
+        },
+      },
+      CurrencyRateController: {
+        currentCurrency: 'usd',
+        currencyRates: {
+          ETH: {
+            conversionRate: 3200,
           },
         },
       },
     },
   },
+  settings: {
+    primaryCurrency: 'ETH',
+  },
 };
-
 // TODO: Replace "any" with type
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const renderComponent = (state: any) =>
