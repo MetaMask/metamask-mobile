0b8f3a62384afbb5289f8b8cfe1794f150307912
JSoufer <jonathan.ferreira@consensys.net>
2024-07-12 15:18:54 +0100

    chore: change file ext due CI fitness func

--- app/__mocks__/htmlMock.js
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export, import/no-unresolved
-export { default as html } from '../../node_modules/@metamask/snaps-execution-environments/dist/browserify/webview/index.html';
