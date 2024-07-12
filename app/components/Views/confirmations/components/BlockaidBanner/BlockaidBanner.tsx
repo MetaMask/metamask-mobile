5f0a5cecc9c50248f90b47027289b0ee4a6057ed
Jyoti Puri <jyotipuri@gmail.com>
2024-07-12 18:01:18 +0530

    fix: blockaid loader on confirmation pages (#10266)

--- app/components/Views/confirmations/components/BlockaidBanner/BlockaidBanner.tsx
+++ app/components/Views/confirmations/components/BlockaidBanner/BlockaidBanner.tsx
@@ -11,11 +11,6 @@ import Accordion from '../../../../../component-library/components/Accordions/Ac
 import { BannerAlertSeverity } from '../../../../../component-library/components/Banners/Banner';
 import { DEFAULT_BANNERBASE_DESCRIPTION_TEXTVARIANT } from '../../../../../component-library/components/Banners/Banner/foundation/BannerBase/BannerBase.constants';
 import BannerAlert from '../../../../../component-library/components/Banners/Banner/variants/BannerAlert/BannerAlert';
-import {
-  IconName,
-  IconSize,
-} from '../../../../../component-library/components/Icons/Icon';
-import Icon from '../../../../../component-library/components/Icons/Icon/Icon';
 import Text from '../../../../../component-library/components/Texts/Text/Text';
 import { useStyles } from '../../../../../component-library/hooks/useStyles';
 import {
@@ -41,9 +36,6 @@ import {
 import { BLOCKAID_SUPPORTED_NETWORK_NAMES } from '../../../../../util/networks';
 import { selectIsSecurityAlertsEnabled } from '../../../../../selectors/preferencesController';
 import BlockaidVersionInfo from '../../../../../lib/ppom/blockaid-version';
-import ButtonIcon, {
-  ButtonIconSizes,
-} from '../../../../../component-library/components/Buttons/ButtonIcon';
 import { WALLET_CONNECT_ORIGIN } from '../../../../../util/walletconnect';
 import AppConstants from '../../../../../core/AppConstants';
 
@@ -72,15 +64,8 @@ const BlockaidBanner = (bannerProps: BlockaidBannerProps) => {
     onContactUsClicked,
   } = bannerProps;
   const { styles, theme } = useStyles(styleSheet, { style });
-  const [displayPositiveResponse, setDisplayPositiveResponse] = useState(false);
   const [reportUrl, setReportUrl] = useState<string>('');
   const isSecurityAlertsEnabled = useSelector(selectIsSecurityAlertsEnabled);
-
-  useEffect(() => {
-    if (securityAlertResponse?.reason === Reason.requestInProgress) {
-      setDisplayPositiveResponse(true);
-    }
-  }, [securityAlertResponse]);
 
   useEffect(() => {
     if (!securityAlertResponse) {
@@ -128,33 +113,15 @@ const BlockaidBanner = (bannerProps: BlockaidBannerProps) => {
 
   if (securityAlertResponse.reason === Reason.requestInProgress) {
     return (
-      <View style={styles.bannerSectionSmall}>
-        <ActivityIndicator size="small" color={theme.colors.icon.default} />
-        <Text style={styles.infoText}>
-          {strings('blockaid_banner.loading_title')}
-        </Text>
-      </View>
+      <ActivityIndicator
+        style={styles.bannerSectionSmall}
+        size="small"
+        color={theme.colors.warning.default}
+      />
     );
   }
 
   if (result_type === ResultType.Benign) {
-    if (displayPositiveResponse) {
-      return (
-        <View style={styles.bannerSectionSmallSpaced}>
-          <View style={styles.flexRowSection}>
-            <Icon name={IconName.Info} size={IconSize.Lg} />
-            <Text style={styles.infoText}>
-              {strings('blockaid_banner.loading_complete_title')}
-            </Text>
-          </View>
-          <ButtonIcon
-            size={ButtonIconSizes.Sm}
-            onPress={() => setDisplayPositiveResponse(false)}
-            iconName={IconName.Close}
-          />
-        </View>
-      );
-    }
     return null;
   }
 
