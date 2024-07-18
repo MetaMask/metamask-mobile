7086bd7dd68ebe8f74beb6041a442ca14785691e
Andrea Salvatore <andrea.salvatore@consensys.net>
2024-07-18 11:30:33 +0200

    fix: fixed account number for unit test

--- app/components/UI/AccountApproval/index.test.tsx
+++ app/components/UI/AccountApproval/index.test.tsx
@@ -18,7 +18,7 @@ jest.mock('../../../core/Engine', () => ({
       state: {
         keyrings: [
           {
-            accounts: ['0xC4955C0d639D99699Bfd7Ec54d9FaFEe40e4D272'],
+            accounts: ['0xC4966c0D659D99699BFD7EB54D8fafEE40e4a756'],
           },
         ],
       },
@@ -33,10 +33,10 @@ const mockInitialState = {
       AccountsController: {
         ...MOCK_ACCOUNTS_CONTROLLER_STATE,
         accounts: {
-          '0xC4955C0d639D99699Bfd7Ec54d9FaFEe40e4D272': {
+          '0xC4966c0D659D99699BFD7EB54D8fafEE40e4a756': {
             balance: '0x0',
             name: 'Account 1',
-            address: '0xC4955C0d639D99699Bfd7Ec54d9FaFEe40e4D272',
+            address: '0xC4966c0D659D99699BFD7EB54D8fafEE40e4a756',
           },
         },
       },
