4299acadc1cc8c2cf0f10c97fb1bbd564c14ee45
JSoufer <jonathan.ferreira@consensys.net>
2024-07-22 12:50:13 +0100

    chore: remove unnecessary configs for Firebase

--- app/util/notifications/constants/config.ts
+++ app/util/notifications/constants/config.ts
@@ -1,28 +1,5 @@
-interface FirebaseAppOptions {
-  appId?: string;
-  apiKey?: string;
-  databaseURL?: string;
-  projectId?: string;
-  gaTrackingId?: string;
-  storageBucket?: string;
-  messagingSenderId?: string;
-  clientId?: string;
-  androidClientId?: string;
-  deepLinkURLScheme?: string;
-  authDomain?: string;
-  measurementId?: string;
-}
-
 const getEnvStr = (test: string, prod: string, override?: string) =>
   override ?? (process.env.NODE_ENV !== 'production' ? test : prod);
-
-const parseConfig = <T>(configStr: string): T | null => {
-  try {
-    return JSON.parse(configStr) as T;
-  } catch {
-    return null;
-  }
-};
 
 export const NOTIFICATION_AUTH_URL = getEnvStr(
   'https://authentication.uat-api.cx.metamask.io',
@@ -52,23 +29,6 @@ export const PUSH_NOTIFICATIONS_SERVICE_URL = getEnvStr(
 );
 
 export const VAPID_KEY = process.env.FCM_VAPID_KEY ?? '';
-
-function getFirebaseConfigEnv(): FirebaseAppOptions | null {
-  if (process.env.FCM_CONFIG)
-    return parseConfig<FirebaseAppOptions>(process.env.FCM_CONFIG);
-
-  return {
-    apiKey: process.env.FCM_CONFIG_API_KEY,
-    authDomain: process.env.FCM_CONFIG_AUTH_DOMAIN,
-    projectId: process.env.FCM_CONFIG_PROJECT_ID,
-    storageBucket: process.env.FCM_CONFIG_STORAGE_BUCKET,
-    messagingSenderId: process.env.FCM_CONFIG_MESSAGING_SENDER_ID,
-    appId: process.env.FCM_CONFIG_APP_ID,
-    measurementId: process.env.FCM_CONFIG_MEASUREMENT_ID,
-  };
-}
-
-export const FIREBASE_CONFIG = getFirebaseConfigEnv();
 
 export const isNotificationsFeatureEnabled = () =>
   process.env.MM_NOTIFICATIONS_UI_ENABLED === 'true';
