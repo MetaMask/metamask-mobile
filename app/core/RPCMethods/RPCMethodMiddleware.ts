e33f02d1fadfe4b9f45175cbac4a1adc9d06f74b
Jony Bursztyn <jonathan@bursztyn.io>
2024-07-24 12:19:43 +0100

    Update app/core/RPCMethods/RPCMethodMiddleware.ts
    Co-authored-by: Mircea Nistor <mirceanis@gmail.com>

--- app/core/RPCMethods/RPCMethodMiddleware.ts
+++ app/core/RPCMethods/RPCMethodMiddleware.ts
@@ -40,7 +40,7 @@ import { regex } from '../../../app/util/regex';
 import Logger from '../../../app/util/Logger';
 import DevLogger from '../SDKConnect/utils/DevLogger';
 import { addTransaction } from '../../util/transaction-controller';
-import Routes from '../../constants/navigation/Routes.js';
+import Routes from '../../constants/navigation/Routes';
 
 // TODO: Replace "any" with type
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
