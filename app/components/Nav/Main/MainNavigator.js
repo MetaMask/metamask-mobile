0ef2673b0a60a5b1b042ca09f59118b3ff774488
JSoufer <jonathan.ferreira@consensys.net>
2024-07-25 08:54:17 +0100

    chore: add navigation to new details screen

--- app/components/Nav/Main/MainNavigator.js
+++ app/components/Nav/Main/MainNavigator.js
@@ -14,6 +14,7 @@ import SecuritySettings from '../../Views/Settings/SecuritySettings';
 import ExperimentalSettings from '../../Views/Settings/ExperimentalSettings';
 import NetworksSettings from '../../Views/Settings/NetworksSettings';
 import NotificationsSettings from '../../Views/Settings/NotificationsSettings';
+import NotificationsDetails from '../../Views/Notifications/Details';
 import OptIn from '../../Views/Notifications/OptIn';
 import AppInformation from '../../Views/Settings/AppInformation';
 import Contacts from '../../Views/Settings/Contacts';
@@ -602,6 +603,12 @@ const NotificationsModeView = (props) => (
       name={Routes.SETTINGS.NOTIFICATIONS}
       component={NotificationsSettings}
       options={NotificationsSettings.navigationOptions}
+    />
+    <Stack.Screen
+      name={Routes.NOTIFICATIONS.DETAILS}
+      component={NotificationsDetails}
+      options={NotificationsDetails.navigationOptions}
+      initialParams={{ notification: props.route.params?.notification }}
     />
     <Stack.Screen
       mode={'modal'}
