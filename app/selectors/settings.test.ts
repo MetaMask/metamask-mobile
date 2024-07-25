9ba4946074f301a893b5d0784f395b5ed11e2748
tommasini <tommasini15@gmail.com>
2024-07-25 11:43:14 +0100

    add unit tests to new selectors

--- app/selectors/settings.test.ts
+++ app/selectors/settings.test.ts
@@ -1,6 +1,10 @@
 import { RootState } from '../reducers';
 
-import { selectShowFiatInTestnets } from './settings';
+import {
+  selectPrimaryCurrency,
+  selectShowCustomNonce,
+  selectShowFiatInTestnets,
+} from './settings';
 
 describe('selectShowFiatInTestnets', () => {
   it('returns showFiatOnTestnets from state', () => {
@@ -11,5 +15,29 @@ describe('selectShowFiatInTestnets', () => {
     };
 
     expect(selectShowFiatInTestnets(mockState as RootState)).toBe(true);
+  });
+});
+
+describe('selectPrimaryCurrency', () => {
+  it('returns primaryCurrency from state', () => {
+    const mockState = {
+      settings: {
+        primaryCurrency: 'USD',
+      },
+    };
+
+    expect(selectPrimaryCurrency(mockState as RootState)).toBe('USD');
+  });
+});
+
+describe('selectShowCustomNonce', () => {
+  it('returns showCustomNonce from state', () => {
+    const mockState = {
+      settings: {
+        showCustomNonce: false,
+      },
+    };
+
+    expect(selectShowCustomNonce(mockState as RootState)).toBe(false);
   });
 });
