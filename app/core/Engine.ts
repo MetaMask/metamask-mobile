9c9628b2042e133517b925caf279fbaab3126eb4
Jongsun Suh <jongsun.suh@icloud.com>
2024-07-26 12:30:23 -0400

    Apply `@ts-expect-error` directive for type error caused by mobile's `@metamask/base-controller` being a major version behind `@metamask/composable-controller`

--- app/core/Engine.ts
+++ app/core/Engine.ts
@@ -1482,6 +1482,8 @@ class Engine {
       Controllers[keyof Controllers]
     >({
       controllers,
+      // @ts-expect-error Resolve/patch mismatch between base-controller versions
+      // ts(2322) - Property '#private' in type 'RestrictedControllerMessenger' refers to a different member that cannot be accessed from within type 'RestrictedControllerMessenger'
       messenger: this.controllerMessenger.getRestricted({
         name: 'ComposableController',
         allowedActions: [],
