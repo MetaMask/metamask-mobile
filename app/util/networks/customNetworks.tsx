cdcee6483b209e7538cd83d93a6d806209f4e5db
Nicholas Gambino <nicholas.gambino@consensys.net>
2024-07-29 13:51:06 -0700

    Cleanup typo

--- app/util/networks/customNetworks.tsx
+++ app/util/networks/customNetworks.tsx
@@ -130,7 +130,7 @@ export const UnpopularNetworkList = [
   },
 ];
 
-export const CustomNetworkMapping = {
+export const CustomNetworkImgMapping = {
   [toHex('14')]: require('../../images/flare-mainnet.png'), // Flare Mainnet
   [toHex('19')]: require('../../images/songbird.png'), // Songbird Testnet
 };
