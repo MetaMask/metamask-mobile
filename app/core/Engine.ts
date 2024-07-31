203c34a39beee25050a07ca6fc479a6327c99afb
Jongsun Suh <jongsun.suh@icloud.com>
2024-07-31 19:27:09 -0400

    Remove `ts-expect-error` directive from `TransactionController:stateChange` due to `@metamask/transaction-controller` version upgrade

--- app/core/Engine.ts
+++ app/core/Engine.ts
@@ -1504,6 +1504,7 @@ class Engine {
           'SubjectMetadataController:stateChange',
           'TokenListController:stateChange',
           'TokensController:stateChange',
+          'TransactionController:stateChange',
 
           /**
            * V1/V2 controllers incorrectly defined with a `messagingSystem` that is missing its `stateChange` event.
@@ -1526,8 +1527,6 @@ class Engine {
           'SnapsRegistry:stateChange',
           // @ts-expect-error BaseControllerV2, `TokenBalancesControllerState` import error
           'TokenBalancesController:stateChange',
-          // @ts-expect-error BaseControllerV1, has `messagingSystem` but as private field, messenger defined without `stateChange` event type
-          'TransactionController:stateChange',
           // @ts-expect-error BaseControllerV2, messenger defined without `stateChange` event type
           'UserStorageController:stateChange',
 
