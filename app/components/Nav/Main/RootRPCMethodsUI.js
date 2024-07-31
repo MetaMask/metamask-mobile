ca89abf0305c2f4600b8bd5dbcdcc3ca2028ac30
tommasini <tommasini15@gmail.com>
2024-07-31 14:17:06 +0100

    remove unneded useMemo import

--- app/components/Nav/Main/RootRPCMethodsUI.js
+++ app/components/Nav/Main/RootRPCMethodsUI.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import { Alert } from 'react-native';
 import PropTypes from 'prop-types';
