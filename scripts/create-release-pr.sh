513846f120f2355e754a485d8a1f4f8bf8c0190a
metamaskbot <metamaskbot@users.noreply.github.com>
2024-08-01 15:39:45 -0600

    updated create release gh action to create 2 PRs

--- scripts/create-release-pr.sh
+++ scripts/create-release-pr.sh
@@ -35,9 +35,31 @@ gh pr create \
   --body "${RELEASE_BODY}" \
   --head "${RELEASE_BRANCH_NAME}";
 
+CHANGELOG_BRANCH_NAME="${RELEASE_BRANCH_NAME}-Changelog"
+
+git checkout -b "${CHANGELOG_BRANCH_NAME}"
+
+if ! (git add . && git commit -m "${NEW_VERSION}");
+then
+    echo "Error: No changes detected."
+    exit 1
+fi
+
 #Generate changelog and test plan csv
 node ./scripts/generate-rc-commits.mjs "${PREVIOUS_VERSION}" "${RELEASE_BRANCH_NAME}" 
 ./scripts/changelog-csv.sh  "${RELEASE_BRANCH_NAME}" 
+
 git add ./commits.csv
+
 git commit -am "updated changelog and generated feature test plan"
-git push
+
+PR_BODY="This is PR updateds the change log for ${NEW_VERSION} and generates the test plan here [commit.csv](https://github.com/MetaMask/metamask-mobile/blob/${RELEASE_BRANCH_NAME}/commits.csv)"
+
+git push --set-upstream origin "${CHANGELOG_BRANCH_NAME}"
+
+gh pr create \
+  --draft \
+  --title "chore: ${CHANGELOG_BRANCH_NAME}" \
+  --body "${PR_BODY}" \
+  --base "${RELEASE_BRANCH_NAME}" \
+  --head "${CHANGELOG_BRANCH_NAME}";
