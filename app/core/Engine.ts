141c010927b2a972345fb3d651bd61db0674f267
Jongsun Suh <jongsun.suh@icloud.com>
2024-08-02 00:37:53 -0400

    Revert "Remove non-controllers from `EngineState`"
    This reverts commit 6ba9da2278532539ee2005cd55696d7f0f1947e8.

--- app/core/Engine.ts
+++ app/core/Engine.ts
@@ -46,6 +46,7 @@ import {
   AddressBookControllerEvents,
   AddressBookControllerState,
 } from '@metamask/address-book-controller';
+import { BaseState } from '@metamask/base-controller';
 import { ComposableController } from '@metamask/composable-controller';
 import {
   KeyringController,
@@ -120,10 +121,10 @@ import {
   SnapsRegistryState,
   SnapControllerEvents,
   SnapControllerActions,
+  PersistedSnapControllerState,
   SnapsRegistryMessenger,
   SnapsRegistryActions,
   SnapsRegistryEvents,
-  PersistedSnapControllerState,
 } from '@metamask/snaps-controllers';
 
 import { WebViewExecutionService } from '@metamask/snaps-controllers/react-native';
@@ -346,6 +347,7 @@ type Permissions = PermissionsByRpcMethod[keyof PermissionsByRpcMethod];
 export type EngineState = {
   AccountTrackerController: AccountTrackerControllerState;
   AddressBookController: AddressBookControllerState;
+  AssetsContractController: BaseState;
   NftController: NftControllerState;
   TokenListController: TokenListState;
   CurrencyRateController: CurrencyRateState;
@@ -360,6 +362,8 @@ export type EngineState = {
   SwapsController: SwapsState;
   GasFeeController: GasFeeState;
   TokensController: TokensControllerState;
+  TokenDetectionController: BaseState;
+  NftDetectionController: BaseState;
   ///: BEGIN:ONLY_INCLUDE_IF(preinstalled-snaps,external-snaps)
   SnapController: PersistedSnapControllerState;
   SnapsRegistry: SnapsRegistryState;
@@ -2006,6 +2010,7 @@ export default {
     const {
       AccountTrackerController,
       AddressBookController,
+      AssetsContractController,
       NftController,
       TokenListController,
       CurrencyRateController,
@@ -2021,6 +2026,8 @@ export default {
       SwapsController,
       GasFeeController,
       TokensController,
+      TokenDetectionController,
+      NftDetectionController,
       ///: BEGIN:ONLY_INCLUDE_IF(preinstalled-snaps,external-snaps)
       SnapController,
       SubjectMetadataController,
