f9b7c8400a2dab4157552e19c8f998b2e4cbccb4
JSoufer <jonathan.ferreira@consensys.net>
2024-08-02 11:49:53 +0100

    chore: add profile sync enablement for new users

--- app/components/Views/ChoosePassword/index.js
+++ app/components/Views/ChoosePassword/index.js
@@ -63,7 +63,7 @@ import { LoginOptionsSwitch } from '../../UI/LoginOptionsSwitch';
 import navigateTermsOfUse from '../../../util/termsOfUse/termsOfUse';
 import { ChoosePasswordSelectorsIDs } from '../../../../e2e/selectors/Onboarding/ChoosePassword.selectors';
 import trackOnboarding from '../../../util/metrics/TrackOnboarding/trackOnboarding';
-
+import { enableProfileSyncing } from '../../../actions/notification/helpers';
 const createStyles = (colors) =>
   StyleSheet.create({
     mainWrapper: {
@@ -371,6 +371,7 @@ class ChoosePassword extends PureComponent {
         wallet_setup_type: 'new',
         new_wallet: true,
       });
+      await enableProfileSyncing();
     } catch (error) {
       try {
         await this.recreateVault('');
@@ -662,11 +663,11 @@ class ChoosePassword extends PureComponent {
                       </Text>
                     </Text>
                   )) || (
-                    <Text
-                      variant={TextVariant.BodySM}
-                      style={styles.passwordStrengthLabel}
-                    />
-                  )}
+                      <Text
+                        variant={TextVariant.BodySM}
+                        style={styles.passwordStrengthLabel}
+                      />
+                    )}
                 </View>
                 <View style={styles.field}>
                   <Text variant={TextVariant.BodySM}>
