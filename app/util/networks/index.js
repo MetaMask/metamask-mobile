a75830e5a4b0ffe49340a416a05ef60e011ffa0c
IF <139582705+infiniteflower@users.noreply.github.com>
2024-08-16 16:39:53 -0400

    chore: remove unused eth obj in fetchEstimatedMultiLayerL1Fee

--- app/util/networks/index.js
+++ app/util/networks/index.js
@@ -454,11 +454,10 @@ const buildOVMGasPriceOracleContract = (eth) => {
  * It returns an estimated L1 fee for a multi layer network.
  * Currently only for the Optimism network, but can be extended to other networks.
  *
- * @param {Object} eth
  * @param {Object} txMeta
  * @returns {String} Hex string gas fee, with no 0x prefix
  */
-export const fetchEstimatedMultiLayerL1Fee = async (eth, txMeta) => {
+export const fetchEstimatedMultiLayerL1Fee = async (txMeta) => {
   const chainId =
     Engine.context.NetworkController.state?.providerConfig?.chainId;
 
