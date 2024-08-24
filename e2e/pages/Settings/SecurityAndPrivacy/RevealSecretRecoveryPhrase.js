e757a0d2e776e75abba41b4dbbc294ec359d0715
Plasma Corral <32695229+plasmacorral@users.noreply.github.com>
2024-08-23 23:22:09 -0400

    Update fixtures and view helper

--- e2e/pages/Settings/SecurityAndPrivacy/RevealSecretRecoveryPhrase.js
+++ e2e/pages/Settings/SecurityAndPrivacy/RevealSecretRecoveryPhrase.js
@@ -9,6 +9,8 @@ class RevealSecretRecoveryPhrase {
     );
   }
 
+  // This is the password requested at login
+  // and should probably be moved eventually into LoginView.js
   get passwordInput() {
     return Matchers.getElementByID(RevealSeedViewSelectorsIDs.PASSWORD_INPUT);
   }
@@ -19,14 +21,49 @@ class RevealSecretRecoveryPhrase {
     );
   }
 
+  // This is the password requested to expose secret credentials
+  get passwordInputToRevealSecretCredential() {
+    return Matchers.getElementByID(
+      RevealSeedViewSelectorsIDs.PASSWORD_INPUT_BOX_ID,
+    );
+  }
+
   get recoveryPhrase() {
     return Matchers.getElementByText(
       RevealSeedViewSelectorsIDs.SECRET_RECOVERY_PHRASE_TEXT,
     );
   }
+  get revealSecretRecoveryPhraseButton() {
+    return Matchers.getElementByID(
+      RevealSeedViewSelectorsIDs.SECRET_CREDENTIAL_REVEAL_BUTTON_ID,
+    );
+  }
 
+  get copyPrivateCredentialToClipboardButton() {
+    return Matchers.getElementByID(
+      RevealSeedViewSelectorsIDs.COPY_PRIVATE_CREDENTIAL_TO_CLIPBOARD_BUTTON,
+    );
+  }
+
+  // This is the password requested at login view
+  // and should probably be moved eventually into LoginView.js
   async enterPassword(password) {
     await Gestures.typeTextAndHideKeyboard(this.passwordInput, password);
+  }
+  // This is the password requested to expose secret credentials
+  async enterPasswordToRevealSecretCredential(password) {
+    await Gestures.typeTextAndHideKeyboard(
+      this.passwordInputToRevealSecretCredential,
+      password,
+    );
+  }
+
+  async tapToReveal() {
+    await Gestures.waitAndTap(this.revealSecretRecoveryPhraseButton);
+  }
+
+  async tapToCopyPrivateCredentialToClipboard() {
+    await Gestures.tap(this.copyPrivateCredentialToClipboardButton);
   }
 }
 
--- e2e/specs/accounts/reveal_secret_recovery_phrase.spec.js
+++ e2e/specs/accounts/reveal_secret_recovery_phrase.spec.js
@@ -12,17 +12,20 @@ import {
   SecurityQuizQuestionTwoModalSelectorsText,
 } from '../../selectors/Modals/SecurityQuizModal.selectors.js';
 import RevealSecretRecoveryPhrase from '../../pages/Settings/SecurityAndPrivacy/RevealSecretRecoveryPhrase.js';
-import {
-  RevealSeedViewSelectorsIDs,
-  RevealSeedViewSelectorsText,
-} from '../../selectors/Settings/SecurityAndPrivacy/RevealSeedView.selectors.js';
+import { RevealSeedViewSelectorsText } from '../../selectors/Settings/SecurityAndPrivacy/RevealSeedView.selectors.js';
 
-import {
-  withFixtures,
-  defaultGanacheOptions,
-} from '../../fixtures/fixture-helper.js';
+import FixtureServer from '../../fixtures/fixture-server';
 import FixtureBuilder from '../../fixtures/fixture-builder.js';
+import {
+  defaultGanacheOptions,
+  loadFixture,
+  startFixtureServer,
+  stopFixtureServer,
+} from '../../fixtures/fixture-helper';
+import { getFixturesServerPort } from '../../fixtures/utils';
 import Assertions from '../../utils/Assertions.js';
+
+const fixtureServer = new FixtureServer();
 
 describe(Regression('Secret Recovery Phrase Reveal from Settings'), () => {
   const PASSWORD = '123123123';
@@ -31,105 +34,106 @@ describe(Regression('Secret Recovery Phrase Reveal from Settings'), () => {
     await TestHelpers.reverseServerPort();
   });
 
+  beforeEach(async () => {
+    const fixture = new FixtureBuilder().withDefaultFixture().build();
+    await startFixtureServer(fixtureServer);
+    await loadFixture(fixtureServer, { fixture });
+    await device.launchApp({
+      launchArgs: { fixtureServerPort: `${getFixturesServerPort()}` },
+    });
+    await loginToApp();
+  });
+
+  afterAll(async () => {
+    await stopFixtureServer(fixtureServer);
+  });
+
   it('reveals Secret Recovery Phrase after completing the security quiz', async () => {
-    await withFixtures(
-      {
-        fixture: new FixtureBuilder().withGanacheNetwork().build(),
-        restartDevice: true,
-        ganacheOptions: defaultGanacheOptions,
-      },
-      async () => {
-        await loginToApp();
+    // Navigate to Reveal SRP screen
+    await TabBarComponent.tapSettings();
+    await SettingsView.tapSecurityAndPrivacy();
+    await SecurityAndPrivacy.tapRevealSecretRecoveryPhraseButton();
 
-        // Navigate to Reveal SRP screen
-        await TabBarComponent.tapSettings();
-        await SettingsView.tapSecurityAndPrivacy();
-        await SecurityAndPrivacy.tapRevealSecretRecoveryPhraseButton();
+    // Start the quiz
+    await SecurityQuizModal.tapGetStartedButton();
 
-        // Start the quiz
-        await SecurityQuizModal.tapGetStartedButton();
-
-        // Question 1 answer wrong, acknowledge error, then retry with correct answer
-        await Assertions.checkIfVisible(SecurityQuizModal.getQuizQuestionOne);
-        await SecurityQuizModal.tapQuestionOneWrongAnswerButton();
-        await Assertions.checkIfTextIsDisplayed(
-          SecurityQuizQuestionOneModalSelectorsText.QUIZ_QUESTION_ONE_WRONG_ANSWER_RESPONSE_TITLE,
-        );
-        await Assertions.checkIfTextIsDisplayed(
-          SecurityQuizQuestionOneModalSelectorsText.QUIZ_QUESTION_ONE_WRONG_ANSWER_RESPONSE_DESCRIPTION,
-        );
-        await SecurityQuizModal.tapQuestionOneWrongAnswerTryAgainButton();
-        await Assertions.checkIfVisible(SecurityQuizModal.getQuizQuestionOne);
-        await SecurityQuizModal.tapQuestionOneRightAnswerButton();
-        await Assertions.checkIfTextIsDisplayed(
-          SecurityQuizQuestionOneModalSelectorsText.QUIZ_QUESTION_ONE_RIGHT_ANSWER_RESPONSE_TITLE,
-        );
-        await Assertions.checkIfTextIsDisplayed(
-          SecurityQuizQuestionOneModalSelectorsText.QUIZ_QUESTION_ONE_RIGHT_ANSWER_RESPONSE_DESCRIPTION,
-        );
-        await SecurityQuizModal.tapQuestionOneContinueButton();
-        await Assertions.checkIfNotVisible(
-          SecurityQuizModal.questionOneRightContinueButton,
-        );
-
-        // Question 2 answer wrong, acknowledge error, then retry with correct answer
-        await Assertions.checkIfVisible(SecurityQuizModal.getQuizQuestionTwo);
-        await SecurityQuizModal.tapQuestionTwoWrongAnswerButton();
-        await Assertions.checkIfTextIsDisplayed(
-          SecurityQuizQuestionTwoModalSelectorsText.QUIZ_QUESTION_TWO_WRONG_ANSWER_RESPONSE_TITLE,
-        );
-        await Assertions.checkIfTextIsDisplayed(
-          SecurityQuizQuestionTwoModalSelectorsText.QUIZ_QUESTION_TWO_WRONG_ANSWER_RESPONSE_DESCRIPTION,
-        );
-        await SecurityQuizModal.tapQuestionTwoWrongAnswerTryAgainButton();
-        await Assertions.checkIfVisible(SecurityQuizModal.getQuizQuestionTwo);
-        await SecurityQuizModal.tapQuestionTwoRightAnswerButton();
-        await Assertions.checkIfTextIsDisplayed(
-          SecurityQuizQuestionTwoModalSelectorsText.QUIZ_QUESTION_TWO_RIGHT_ANSWER_RESPONSE_TITLE,
-        );
-        await Assertions.checkIfTextIsDisplayed(
-          SecurityQuizQuestionTwoModalSelectorsText.QUIZ_QUESTION_TWO_RIGHT_ANSWER_RESPONSE_DESCRIPTION,
-        );
-        await SecurityQuizModal.tapQuestionTwoContinueButton();
-        await Assertions.checkIfNotVisible(
-          SecurityQuizModal.questionTwoRightContinueButton,
-        );
-
-        // Enter password after completing quiz
-        await TestHelpers.typeTextAndHideKeyboard(
-          RevealSeedViewSelectorsIDs.PASSWORD_INPUT_BOX_ID,
-          PASSWORD,
-        );
-        // Tap on reveal SRP button
-        await TestHelpers.waitAndTap(
-          RevealSeedViewSelectorsIDs.SECRET_CREDENTIAL_REVEAL_BUTTON_ID,
-        );
-        // Confirm that the SRP container is displayed
-        await Assertions.checkIfVisible(RevealSecretRecoveryPhrase.container);
-        // Confirm that SRP Title is displayed
-        await Assertions.checkIfTextIsDisplayed(
-          RevealSeedViewSelectorsText.REVEAL_SECRET_CREDENTIAL_TITLE_TEXT,
-        );
-
-        // Confirm that expected SRP text is displayed
-        await Assertions.checkIfTextIsDisplayed(defaultGanacheOptions.mnemonic);
-
-        // Copy to clipboard
-        // Android devices running OS version < 11 (API level 29) will not see the copy to clipboard button presented
-        // This will cause the following step to fail if e2e were being run on an older android OS prior to our minimum API level 29
-        // See details here: https://github.com/MetaMask/metamask-mobile/pull/4170
-        await TestHelpers.tap(
-          RevealSeedViewSelectorsIDs.COPY_PRIVATE_CREDENTIAL_TO_CLIPBOARD_BUTTON,
-        );
-        // Tap done
-        await TestHelpers.waitAndTapText(
-          RevealSeedViewSelectorsText.REVEAL_SECRET_CREDENTIAL_DONE,
-        );
-        // Confirm that the SRP container is not displayed
-        await Assertions.checkIfNotVisible(
-          RevealSecretRecoveryPhrase.container,
-        );
-      },
+    // Question 1 answer wrong, acknowledge error, then retry with correct answer
+    await Assertions.checkIfVisible(SecurityQuizModal.getQuizQuestionOne);
+    await SecurityQuizModal.tapQuestionOneWrongAnswerButton();
+    await Assertions.checkIfTextIsDisplayed(
+      SecurityQuizQuestionOneModalSelectorsText.QUIZ_QUESTION_ONE_WRONG_ANSWER_RESPONSE_TITLE,
     );
+    await Assertions.checkIfTextIsDisplayed(
+      SecurityQuizQuestionOneModalSelectorsText.QUIZ_QUESTION_ONE_WRONG_ANSWER_RESPONSE_DESCRIPTION,
+    );
+    await SecurityQuizModal.tapQuestionOneWrongAnswerTryAgainButton();
+    await Assertions.checkIfVisible(SecurityQuizModal.getQuizQuestionOne);
+    await SecurityQuizModal.tapQuestionOneRightAnswerButton();
+    await Assertions.checkIfTextIsDisplayed(
+      SecurityQuizQuestionOneModalSelectorsText.QUIZ_QUESTION_ONE_RIGHT_ANSWER_RESPONSE_TITLE,
+    );
+    await Assertions.checkIfTextIsDisplayed(
+      SecurityQuizQuestionOneModalSelectorsText.QUIZ_QUESTION_ONE_RIGHT_ANSWER_RESPONSE_DESCRIPTION,
+    );
+    await SecurityQuizModal.tapQuestionOneContinueButton();
+    await Assertions.checkIfNotVisible(
+      SecurityQuizModal.questionOneRightContinueButton,
+    );
+
+    // Question 2 answer wrong, acknowledge error, then retry with correct answer
+    await Assertions.checkIfVisible(SecurityQuizModal.getQuizQuestionTwo);
+    await SecurityQuizModal.tapQuestionTwoWrongAnswerButton();
+    await Assertions.checkIfTextIsDisplayed(
+      SecurityQuizQuestionTwoModalSelectorsText.QUIZ_QUESTION_TWO_WRONG_ANSWER_RESPONSE_TITLE,
+    );
+    await Assertions.checkIfTextIsDisplayed(
+      SecurityQuizQuestionTwoModalSelectorsText.QUIZ_QUESTION_TWO_WRONG_ANSWER_RESPONSE_DESCRIPTION,
+    );
+    await SecurityQuizModal.tapQuestionTwoWrongAnswerTryAgainButton();
+    await Assertions.checkIfVisible(SecurityQuizModal.getQuizQuestionTwo);
+    await SecurityQuizModal.tapQuestionTwoRightAnswerButton();
+    await Assertions.checkIfTextIsDisplayed(
+      SecurityQuizQuestionTwoModalSelectorsText.QUIZ_QUESTION_TWO_RIGHT_ANSWER_RESPONSE_TITLE,
+    );
+    await Assertions.checkIfTextIsDisplayed(
+      SecurityQuizQuestionTwoModalSelectorsText.QUIZ_QUESTION_TWO_RIGHT_ANSWER_RESPONSE_DESCRIPTION,
+    );
+    await SecurityQuizModal.tapQuestionTwoContinueButton();
+    await Assertions.checkIfNotVisible(
+      SecurityQuizModal.questionTwoRightContinueButton,
+    );
+
+    // Enter password after completing quiz
+    await RevealSecretRecoveryPhrase.enterPasswordToRevealSecretCredential(
+      PASSWORD,
+    );
+    // await TestHelpers.typeTextAndHideKeyboard(
+    //   RevealSeedViewSelectorsIDs.PASSWORD_INPUT_BOX_ID,
+    //   PASSWORD,
+    // );
+    // Tap on reveal SRP button
+    await RevealSecretRecoveryPhrase.tapToReveal();
+
+    // Confirm that the SRP container is displayed
+    await Assertions.checkIfVisible(RevealSecretRecoveryPhrase.container);
+    // Confirm that SRP Title is displayed
+    await Assertions.checkIfTextIsDisplayed(
+      RevealSeedViewSelectorsText.REVEAL_SECRET_CREDENTIAL_TITLE_TEXT,
+    );
+
+    // Confirm that expected SRP text is displayed
+    await Assertions.checkIfTextIsDisplayed(defaultGanacheOptions.mnemonic);
+
+    // Copy to clipboard
+    // Android devices running OS version < 11 (API level 29) will not see the copy to clipboard button presented
+    // This will cause the following step to fail if e2e were being run on an older android OS prior to our minimum API level 29
+    // See details here: https://github.com/MetaMask/metamask-mobile/pull/4170
+    await RevealSecretRecoveryPhrase.tapToCopyPrivateCredentialToClipboard();
+    // Tap done
+    await TestHelpers.waitAndTapText(
+      RevealSeedViewSelectorsText.REVEAL_SECRET_CREDENTIAL_DONE,
+    );
+    // Confirm that the SRP container is not displayed
+    await Assertions.checkIfNotVisible(RevealSecretRecoveryPhrase.container);
   });
 });
