ba9b4917c4c64fbe147949c9c59ffd95ecb18842
tommasini <tommasini15@gmail.com>
2024-08-27 00:31:59 +0100

    terms of use content fix on declarations fike

--- app/declarations/index.d.ts
+++ app/declarations/index.d.ts
@@ -277,7 +277,7 @@ declare module '@metamask/contract-metadata' {
   export default content;
 }
 
-declare module './util/termsOfUse/termsOfUseContent' {
+declare module 'app/util/termsOfUse/termsOfUseContent' {
   const content: string;
   export default content;
 }
