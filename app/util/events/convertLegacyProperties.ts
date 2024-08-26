64e5a3983fd16977a64e3af5c81d7022121f8f4a
Nicolas MASSART <nicolas.massart@consensys.net>
2024-08-26 10:08:01 +0200

    change properties param name
    add comment
    format

--- app/util/events/convertLegacyProperties.ts
+++ app/util/events/convertLegacyProperties.ts
@@ -17,35 +17,42 @@ function isEventProperties(
 /**
  * Convert legacy properties to the new EventProperties type if needed
  *
+ * There are two types of legacy properties:
+ * - properties with the new structure (properties and sensitiveProperties) but with anonymous properties inside properties
+ * - properties with the old structure (just a JsonMap) and possibly anonymous properties inside
+ *
  * If the properties are already of the new type, they are returned as is
- * @param properties
+ * @param propertiesParam the properties to check for conversion and convert if needed
  */
 function convertLegacyProperties(
-  properties: CombinedProperties,
+  propertiesParam: CombinedProperties,
 ): EventProperties {
-  if (isEventProperties(properties)) {
+  if (isEventProperties(propertiesParam)) {
     // EventProperties non-anonymous properties could have anonymous properties inside
     // so we need to process them separately
-    if (properties.properties && Object.keys(properties.properties).length) {
+    if (
+      propertiesParam.properties &&
+      Object.keys(propertiesParam.properties).length
+    ) {
       const [nonAnonymousProperties, anonymousProperties] =
-        preProcessAnalyticsEvent(properties.properties);
+        preProcessAnalyticsEvent(propertiesParam.properties);
       return {
         properties: nonAnonymousProperties,
         // and concatenate all the anon props in sensitiveProperties
         sensitiveProperties: {
           ...anonymousProperties,
-          ...properties.sensitiveProperties,
+          ...propertiesParam.sensitiveProperties,
         },
       };
     }
     // If there are no non-anonymous properties, we don't need to process them
     // and we can return the object as is
-    return properties;
+    return propertiesParam;
   }
 
   // if the properties are not of the new type, we need to process them
   const [nonAnonymousProperties, anonymousProperties] =
-    preProcessAnalyticsEvent(properties);
+    preProcessAnalyticsEvent(propertiesParam);
   return {
     properties: nonAnonymousProperties,
     sensitiveProperties: anonymousProperties,
