99be1de5de1f422c269c2669f177659cc113b6d1
Jonathan Bursztyn <jonathan@bursztyn.io>
2024-08-27 17:38:18 +0100

    Add mock

--- app/components/Views/QRAccountDisplay/QRAccountDisplay.test.tsx
+++ app/components/Views/QRAccountDisplay/QRAccountDisplay.test.tsx
@@ -30,6 +30,8 @@ const initialState = {
   },
 };
 
+jest.mock('../../../core/ClipboardManager');
+
 const TestWrapper = () => (
   <QRAccountDisplay
     accountAddress={'0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'}
