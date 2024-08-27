2afbacb8bd989151be198ade8754c27c92452056
tommasini <tommasini15@gmail.com>
2024-08-27 12:12:58 +0100

    change termsOfUseContent for a ts file

--- app/declarations/index.d.ts
+++ app/declarations/index.d.ts
@@ -277,7 +277,7 @@ declare module '@metamask/contract-metadata' {
   export default content;
 }
 
-declare module './util/termsOfUse/termsOfUseContent.js' {
+declare module './util/termsOfUse/termsOfUseContent.ts' {
   const content: string;
   export default content;
 }
