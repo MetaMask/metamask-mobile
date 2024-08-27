99022f37dc582f394a117526a0aca66ad6148411
Jonathan Bursztyn <jonathan@bursztyn.io>
2024-08-27 23:37:23 +0100

    Fix test

--- app/components/Views/QRAccountDisplay/QRAccountDisplay.test.tsx
+++ app/components/Views/QRAccountDisplay/QRAccountDisplay.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import QRAccountDisplay from './index';
-import { fireEvent, waitFor } from '@testing-library/react-native';
-import ClipboardManager from '../../../core/ClipboardManager';
+import { fireEvent } from '@testing-library/react-native';
 import { renderScreen } from '../../../util/test/renderWithProvider';
 import initialBackgroundState from '../../../util/test/initial-background-state.json';
+import ClipboardManager from '../../../core/ClipboardManager';
 
 const initialState = {
   engine: {
@@ -30,7 +30,16 @@ const initialState = {
   },
 };
 
-jest.mock('../../../core/ClipboardManager');
+jest.mock('../../../core/ClipboardManager', () => {
+  let clipboardContent = '';
+
+  return {
+    setString: jest.fn((str) => {
+      clipboardContent = str;
+    }),
+    getString: jest.fn(() => clipboardContent),
+  };
+});
 
 const TestWrapper = () => (
   <QRAccountDisplay
@@ -39,6 +48,10 @@ const TestWrapper = () => (
 );
 
 describe('QRAccountDisplay', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('render matches snapshot', () => {
     const { toJSON } = renderScreen(
       TestWrapper,
@@ -58,14 +71,12 @@ describe('QRAccountDisplay', () => {
     const copyButton = getByTestId('qr-account-display-copy-button');
     fireEvent.press(copyButton);
 
+    expect(ClipboardManager.setString).toHaveBeenCalledWith(
+      '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045',
+    );
+    expect(ClipboardManager.getString()).toBe(
+      '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045',
+    );
     expect(copyButton).toBeTruthy();
-    expect(ClipboardManager.setString).toHaveBeenCalledTimes(1);
-
-    await waitFor(async () => {
-      const clipboardContent = await ClipboardManager.getString();
-      expect(clipboardContent).toBe(
-        '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045',
-      );
-    });
   });
 });
