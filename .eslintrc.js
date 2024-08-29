9cf50dd57c1b5aa6ad86a87fcea2de44d9aa4185
tommasini <tommasini15@gmail.com>
2024-08-30 00:10:13 +0100

    remove @babel/react-preset

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,7 +44,7 @@ module.exports = {
       parserOptions: {
         requireConfigFile: false,
         babelOptions: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+          presets: ['@babel/preset-env'],
         },
       },
       rules: {
