c225aa50e71d7161c55be7569f12fbc4feaded63
Davide Brocchetto <davide.brocchetto@consensys.net>
2024-08-29 23:31:10 +0200

    Added Assertion call

--- e2e/pages/swaps/SwapView.js
+++ e2e/pages/swaps/SwapView.js
@@ -5,6 +5,7 @@ import {
 
 import Matchers from '../../utils/Matchers';
 import Gestures from '../../utils/Gestures';
+import Assertions from '../../utils/Assertions';
 import TestHelpers from '../../helpers';
 
 class SwapView {
@@ -36,9 +37,9 @@ class SwapView {
   }
 
   async swipeToSwap() {
-    const percentage = device.getPlatform() === 'ios' ? 0.72 : 0.85;
+    const percentage = device.getPlatform() === 'ios' ? 0.72 : 0.95;
     // Wait past the flashing quote to make sure swap button is enabled
-    await TestHelpers.checkIfElementWithTextIsVisible('New quotes in 0:10');
+    await Assertions.checkIfVisible('New quotes in 0:05')
     await Gestures.swipe(this.swipeToSwapButton, 'right', 'slow', percentage);
   }
 
