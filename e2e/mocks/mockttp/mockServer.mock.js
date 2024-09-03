016fb97662d60f782230c44fa3921a0598e90dce
Cal-L <cleun007@gmail.com>
2024-09-03 16:57:58 -0700

    Intercept selected requests

--- e2e/mocks/mockttp/mockServer.mock.js
+++ e2e/mocks/mockttp/mockServer.mock.js
@@ -1,28 +1,41 @@
 const mockttp = require('mockttp');
 
-Create and start a Mockttp server instance
-const mockServer = mockttp.getLocal();
+const startServer = async () => {
+  // Create and start a Mockttp server instance
+  const mockServer = mockttp.getLocal();
 
-mockServer.start(8000)
-  .then(() => {
-    console.log(`Mockttp proxy server running at http://localhost:${mockServer.port}`);
+  mockServer.enableDebug();
 
-    // Set up interception and response for a specific path
-    mockServer.forGet("/networks/1/suggestedGasFees").thenReply(500, "A mocked response");
+  await mockServer.start(8000);
 
-    // Forward all other requests
-    return mockServer.forAnyRequest().thenPassThrough({
-      beforeRequest: async ({ url, method }) => {
-        const originalUrl = new URL(url).searchParams.get('url');
-        console.log(`Forwarding request: ${method} ${originalUrl || url}`);
+  console.log(
+    `Mockttp proxy server running at http://localhost:${mockServer.port}`,
+  );
 
-        return { url: originalUrl || url };
-      }
-    });
-  })
-  .then(() => {
-    console.log('Mockttp server is set up to mock specific paths and forward other requests.');
-  })
-  .catch(err => {
-    console.error('Error starting the Mockttp server:', err);
+  // This is a rule where the path is /proxy and a query param is https://www.google.com
+  // Docs - https://httptoolkit.github.io/mockttp/interfaces/Mockttp.html
+  await mockServer
+    .forGet('/proxy')
+    .withQuery({ url: 'https://www.google.com' })
+    .thenReply(500);
+
+  // Add other rules here...
+  // await mockServer
+  //   .forGet('/proxy')
+  //   .withQuery({ url: 'https://www.website.com/networks/1/suggestedGasFees' })
+  //   .thenReply(500);
+
+  // Unmatched rules falls back to this and is passed through normally
+  await mockServer.forUnmatchedRequest().thenPassThrough({
+    beforeRequest: async ({ url, method }) => {
+      const originalUrl = new URL(url).searchParams.get('url');
+      console.log(`Forwarding request to: ${method} ${url}`);
+
+      return { url: originalUrl || url };
+    },
   });
+};
+
+startServer().catch((e) => {
+  console.log('Error starting server', e);
+});
