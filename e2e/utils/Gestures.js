3cccc35fd80ce44757a30101aa2689429825374a
Davide Brocchetto <davide.brocchetto@consensys.net>
2024-09-03 18:12:46 +0200

    Changed timeout to 10 sec for waitAndTap

--- e2e/utils/Gestures.js
+++ e2e/utils/Gestures.js
@@ -51,9 +51,9 @@ class Gestures {
    * Wait for an element to be visible and then tap it.
    *
    * @param {Promise<Detox.IndexableNativeElement>} elementID - ID of the element to tap
-   * @param {number} timeout - Timeout for waiting (default: 8000ms)
+   * @param {number} timeout - Timeout for waiting (default: 10000ms)
    */
-  static async waitAndTap(elementID, timeout = 15000) {
+  static async waitAndTap(elementID, timeout = 10000) {
     const element = await elementID;
     await waitFor(element).toBeVisible().withTimeout(timeout);
     await element.tap();
