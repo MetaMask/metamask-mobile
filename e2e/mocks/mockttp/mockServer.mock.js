e1454530289ef28bb38be22ebdf7f4b89e2e95d5
Yande <andepandy@outlook.com>
2024-09-03 23:57:13 +0100

    comment

--- e2e/mocks/mockttp/mockServer.mock.js
+++ e2e/mocks/mockttp/mockServer.mock.js
@@ -1,14 +1,19 @@
 const mockttp = require('mockttp');
 
-Create and start a Mockttp server instance
+// Create and start a Mockttp server instance
 const mockServer = mockttp.getLocal();
 
-mockServer.start(8000)
+mockServer
+  .start(8000)
   .then(() => {
-    console.log(`Mockttp proxy server running at http://localhost:${mockServer.port}`);
+    console.log(
+      `Mockttp proxy server running at http://localhost:${mockServer.port}`,
+    );
 
     // Set up interception and response for a specific path
-    mockServer.forGet("/networks/1/suggestedGasFees").thenReply(500, "A mocked response");
+    mockServer
+      .forGet('/networks/1/suggestedGasFees')
+      .thenReply(500, 'A mocked response');
 
     // Forward all other requests
     return mockServer.forAnyRequest().thenPassThrough({
@@ -17,12 +22,14 @@ mockServer.start(8000)
         console.log(`Forwarding request: ${method} ${originalUrl || url}`);
 
         return { url: originalUrl || url };
-      }
+      },
     });
   })
   .then(() => {
-    console.log('Mockttp server is set up to mock specific paths and forward other requests.');
+    console.log(
+      'Mockttp server is set up to mock specific paths and forward other requests.',
+    );
   })
-  .catch(err => {
+  .catch((err) => {
     console.error('Error starting the Mockttp server:', err);
   });
