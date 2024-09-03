a0da29a0939fbc6de689b377bf7c147b2a501549
Alex <adonesky@gmail.com>
2024-09-03 15:19:56 -0500

    adding e2e test

---
+++ e2e/specs/multichain/per-dapp-selected-network.spec.js
@@ -0,0 +1,86 @@
+'use strict';
+import { SmokeConfirmations } from '../../tags';
+import TestHelpers from '../../helpers';
+import { loginToApp } from '../../viewHelper';
+import FixtureBuilder from '../../fixtures/fixture-builder';
+import {
+  withFixtures,
+  defaultGanacheOptions,
+} from '../../fixtures/fixture-helper';
+
+import { SMART_CONTRACTS } from '../../../app/util/test/smart-contracts';
+import { ContractApprovalModalSelectorsText } from '../../selectors/Modals/ContractApprovalModal.selectors';
+import { ActivitiesViewSelectorsText } from '../../selectors/ActivitiesView.selectors';
+
+import ContractApprovalModal from '../../pages/modals/ContractApprovalModal';
+import Assertions from '../../utils/Assertions';
+import TabBarComponent from '../../pages/TabBarComponent';
+import TestDApp from '../../pages/Browser/TestDApp';
+
+const HST_CONTRACT = SMART_CONTRACTS.HST;
+const EXPECTED_TOKEN_AMOUNT = '7';
+
+describe(SmokeConfirmations('Per Dapp Selected Network'), () => {
+  beforeAll(async () => {
+    jest.setTimeout(170000);
+    if (device.getPlatform() === 'android') {
+      await TestHelpers.reverseServerPort();
+    }
+  });
+
+  it('approve default ERC20 token amount from a dapp', async () => {
+    await withFixtures(
+      {
+        dapp: true,
+        fixture: new FixtureBuilder()
+          .withGanacheNetwork()
+          .withPermissionControllerConnectedToTestDapp()
+          .build(),
+        restartDevice: true,
+        ganacheOptions: defaultGanacheOptions,
+        smartContract: HST_CONTRACT,
+      },
+      async ({ contractRegistry }) => {
+        const hstAddress = await contractRegistry.getContractAddress(
+          HST_CONTRACT,
+        );
+        await loginToApp();
+        // Navigate to the browser screen
+        await TabBarComponent.tapBrowser();
+        await TestDApp.navigateToTestDappWithContract({
+          contractAddress: hstAddress,
+        });
+        await TestDApp.tapApproveButton();
+
+        await Assertions.checkIfVisible(
+          ContractApprovalModal.approveTokenAmount,
+        );
+
+        await Assertions.checkIfElementToHaveText(
+          ContractApprovalModal.approveTokenAmount,
+          EXPECTED_TOKEN_AMOUNT,
+        );
+        // Tap next button
+        await Assertions.checkIfTextIsDisplayed(
+          ContractApprovalModalSelectorsText.NEXT,
+        );
+        await ContractApprovalModal.tapNextButton();
+
+        await Assertions.checkIfTextIsDisplayed(
+          ContractApprovalModalSelectorsText.APPROVE,
+        );
+        // Tap approve button
+        await ContractApprovalModal.tapApproveButton();
+
+        // Navigate to the activity screen
+        await TabBarComponent.tapActivity();
+
+        // Assert erc20 is approved
+
+        await Assertions.checkIfTextIsDisplayed(
+          ActivitiesViewSelectorsText.CONFIRM_TEXT,
+        );
+      },
+    );
+  });
+});
