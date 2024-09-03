fbccad12944acfb1e207a6d87538ec0789f68566
JSoufer <jonathan.ferreira@consensys.net>
2024-09-03 11:09:50 +0100

    fix lint issues on test

--- app/components/Views/Notifications/Details/Fields/NetworkFeeField.test.tsx
+++ app/components/Views/Notifications/Details/Fields/NetworkFeeField.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
-
+import { processNotification } from '@metamask/notification-services-controller/dist/types/NotificationServicesController/processors';
+import { Mocks } from '@metamask/notification-services-controller/dist/types/NotificationServicesController';
 import NetworkFeeField from './NetworkFeeField';
-import MOCK_NOTIFICATIONS from '../../../../../components/UI/Notification/__mocks__/mock_notifications';
+import { OnChainRawNotificationsWithNetworkFields } from '@metamask/notification-services-controller/dist/types/NotificationServicesController/types';
+import { ModalFieldType } from 'app/util/notifications';
 
 jest.mock('../../../../../util/notifications/methods/common', () => ({
   getNetworkFees: () =>
@@ -18,6 +20,10 @@ jest.mock('../../../../../util/notifications/methods/common', () => ({
     }),
 }));
 
+const MOCK_NOTIFICATION = processNotification(
+  Mocks.createMockNotificationEthReceived(),
+) as OnChainRawNotificationsWithNetworkFields;
+
 describe('NetworkFeeField', () => {
   const setIsCollapsed = jest.fn();
   const isCollapsed = false;
@@ -26,20 +32,21 @@ describe('NetworkFeeField', () => {
     jest.clearAllMocks();
   });
 
-  it('should renders correctly', () => {
+  it('should renders correctly when type has "ModalField-NetworkFee"', () => {
     const { toJSON } = render(
       <NetworkFeeField
-        {...MOCK_NOTIFICATIONS[1].data.network_fee}
+        type={ModalFieldType.NETWORK_FEE}
+        {...MOCK_NOTIFICATION.data.network_fee}
         isCollapsed={isCollapsed}
         setIsCollapsed={setIsCollapsed}
         getNetworkFees={() =>
           Promise.resolve({
-            gasUsed: '0',
-            gasLimit: '0',
-            baseFee: '0',
-            priorityFee: '0',
-            maxFeePerGas: '0',
-            effectiveGasPrice: '0',
+            gasUsed: 0,
+            gasLimit: 0,
+            baseFee: 0,
+            priorityFee: 0,
+            maxFeePerGas: 0,
+            effectiveGasPrice: 0,
             transactionFeeInEth: '0',
             transactionFeeInUsd: '0',
           })
