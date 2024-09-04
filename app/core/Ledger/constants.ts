f20484cbbea1e8a0598106121302ac0cd38a24c6
Xiaoming Wang <dawnseeker8@gmail.com>
2024-09-04 16:43:57 +0800

    fix: minor change to ledger/constants.ts

--- app/core/Ledger/constants.ts
+++ app/core/Ledger/constants.ts
@@ -1,6 +1,4 @@
-const LEDGER_LIVE_PATH = `m/44'/60'/0'/0/0`;
+export const LEDGER_LIVE_PATH = `m/44'/60'/0'/0/0`;
+export const LEDGER_BIP44_PATH = `m/44'/60'/0'/0`;
+export const LEDGER_LEGACY_PATH = `m/44'/60'/0'`;
 
-const LEDGER_BIP44_PATH = `m/44'/60'/0'/0`;
-const LEDGER_LEGACY_PATH = `m/44'/60'/0'`;
-
-export { LEDGER_LIVE_PATH, LEDGER_BIP44_PATH, LEDGER_LEGACY_PATH };
