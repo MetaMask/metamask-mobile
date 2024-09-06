5c4ea7a43aef2ff125b3e47e4cfbf6d979ab177a
Brendan Kirby <brendan.kirby@consensys.net>
2024-09-06 08:56:43 -0700

    test: initial ramps tests

---
+++ e2e/specs/ramps/onramp.spec.js
@@ -0,0 +1,51 @@
+'use strict';
+import { loginToApp } from '../../viewHelper';
+import TabBarComponent from '../../pages/TabBarComponent';
+import WalletActionsModal from '../../pages/modals/WalletActionsModal';
+import FixtureBuilder from '../../fixtures/fixture-builder';
+import {
+  loadFixture,
+  startFixtureServer,
+  stopFixtureServer,
+} from '../../fixtures/fixture-helper';
+import { CustomNetworks } from '../../resources/networks.e2e';
+import TestHelpers from '../../helpers';
+import FixtureServer from '../../fixtures/fixture-server';
+import { getFixturesServerPort } from '../../fixtures/utils';
+import { SmokeRamps } from '../../tags';
+import Assertions from '../../utils/Assertions';
+import BuyGetStartedView from '../../pages/Ramps/BuyGetStartedView';
+
+const fixtureServer = new FixtureServer();
+
+describe(SmokeRamps('OnRamp'), () => {
+  beforeAll(async () => {
+    await TestHelpers.reverseServerPort();
+    const fixture = new FixtureBuilder()
+      .withNetworkController(CustomNetworks.Tenderly)
+      .build();
+    await startFixtureServer(fixtureServer);
+    await loadFixture(fixtureServer, { fixture });
+    await device.launchApp({
+      permissions: { notifications: 'YES' },
+      launchArgs: { fixtureServerPort: `${getFixturesServerPort()}` },
+    });
+    await loginToApp();
+  });
+
+  afterAll(async () => {
+    await stopFixtureServer(fixtureServer);
+  });
+
+  beforeEach(async () => {
+    jest.setTimeout(150000);
+  });
+
+  it('should tap the Buy button', async () => {
+    await TabBarComponent.tapWallet();
+    await TabBarComponent.tapActions();
+    await WalletActionsModal.tapBuyButton();
+    await BuyGetStartedView.tapGetStartedButton();
+  });
+
+});
--- e2e/tags.js
+++ e2e/tags.js
@@ -6,6 +6,7 @@ const tags = {
   SmokeSwaps: 'SmokeSwaps',
   SmokeRest: 'SmokeRest',
   smokeAssets: 'smokeAssets',
+  SmokeRamps: 'SmokeRamps',
 };
 
 const Regression = (testName) => `${tags.regression} ${testName}`;
@@ -15,6 +16,7 @@ const SmokeConfirmations = (testName) =>
   `${tags.smokeConfirmations} ${testName}`;
 const SmokeSwaps = (testName) => `${tags.SmokeSwaps} ${testName}`;
 const SmokeAssets = (testName) => `${tags.smokeAssets} ${testName}`;
+const SmokeRamps = (testName) => `${tags.SmokeRamps} ${testName}`;
 
 export {
   Regression,
@@ -23,4 +25,5 @@ export {
   SmokeConfirmations,
   SmokeSwaps,
   SmokeAssets,
+  SmokeRamps,
 };
