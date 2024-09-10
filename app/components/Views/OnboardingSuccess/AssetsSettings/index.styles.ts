3f1173290e708e78bf7be2a1881e9456ebafe62e
vinnyhoward <vincenguyenhoward@gmail.com>
2024-09-09 21:25:48 -0600

    feat: added options within assets for displaying nft media, auto detect nft, enhanced token detection, and IPFS gateway

--- app/components/Views/OnboardingSuccess/AssetsSettings/index.styles.ts
+++ app/components/Views/OnboardingSuccess/AssetsSettings/index.styles.ts
@@ -1,35 +1,52 @@
 import { StyleSheet } from 'react-native';
+import { Colors } from '../../../../util/theme/models';
 
-const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    paddingHorizontal: 16,
-    paddingTop: 16,
-  },
-  backButton: {
-    padding: 10,
-  },
-  setting: {
-    marginTop: 32,
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  title: {
-    flex: 1,
-  },
-  toggle: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginLeft: 16,
-  },
-  switch: {
-    alignSelf: 'flex-start',
-  },
-  desc: {
-    marginTop: 8,
-  },
-});
+const createStyles = (colors: Colors) =>
+  StyleSheet.create({
+    root: {
+      flex: 1,
+      paddingHorizontal: 16,
+      paddingTop: 16,
+    },
+    backButton: {
+      padding: 10,
+    },
+    setting: {
+      marginTop: 32,
+    },
+    titleContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    title: {
+      flex: 1,
+    },
+    toggle: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginLeft: 16,
+    },
+    switch: {
+      alignSelf: 'flex-start',
+    },
+    desc: {
+      marginTop: 8,
+    },
+    halfSetting: {
+      marginTop: 16,
+    },
+    switchElement: {
+      marginLeft: 16,
+    },
+    accessory: {
+      marginTop: 16,
+    },
+    picker: {
+      borderColor: colors.border.default,
+      borderRadius: 5,
+      borderWidth: 2,
+      marginTop: 16,
+    },
+  });
 
-export default styles;
+export default createStyles;
--- app/components/Views/OnboardingSuccess/AssetsSettings/index.tsx
+++ app/components/Views/OnboardingSuccess/AssetsSettings/index.tsx
@@ -1,12 +1,27 @@
 // third party dependencies
-import React, { useCallback, useLayoutEffect } from 'react';
-import { ScrollView, TouchableOpacity, View, Switch } from 'react-native';
+import React, {
+  useCallback,
+  useLayoutEffect,
+  useState,
+  useEffect,
+} from 'react';
+import {
+  ScrollView,
+  TouchableOpacity,
+  View,
+  Switch,
+  ActivityIndicator,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { isTokenDetectionSupportedForNetwork } from '@metamask/assets-controllers';
 import { useSelector } from 'react-redux';
 
 // internal dependencies
 import Engine from '../../../../core/Engine';
+import SelectComponent from '../../../UI/SelectComponent';
+import ipfsGateways from '../../../../util/ipfs-gateways.json';
+import { timeoutFetch } from '../../../../util/general';
+import { MetaMetricsEvents, useMetrics } from '../../../hooks/useMetrics';
 import { selectUseTokenDetection } from '../../../../selectors/preferencesController';
 import { selectChainId } from '../../../../selectors/networkController';
 import Text, {
@@ -18,16 +33,37 @@ import Icon, {
   IconName,
 } from '../../../../component-library/components/Icons/Icon';
 import { strings } from '../../../../../locales/i18n';
-import styles from './index.styles';
+import createStyles from './index.styles';
 import { useTheme } from '../../../../util/theme';
+import { UserProfileProperty } from '../../../../util/metrics/UserSettingsAnalyticsMetaData/UserProfileAnalyticsMetaData.types';
+import {
+  selectDisplayNftMedia,
+  selectUseNftDetection,
+  selectIsIpfsGatewayEnabled,
+  selectIpfsGateway,
+} from '../../../../selectors/preferencesController';
+import { Gateway } from '../../Settings/SecuritySettings/SecuritySettings.types';
+import {
+  HASH_STRING,
+  HASH_TO_TEST,
+} from '../../Settings/SecuritySettings/SecuritySettings.constants';
 
 const AssetSettings = () => {
+  const { trackEvent, isEnabled, addTraitsToUser } = useMetrics();
   const theme = useTheme();
   const { colors } = theme;
+  const styles = createStyles(colors);
   const navigation = useNavigation();
+
+  const [gotAvailableGateways, setGotAvailableGateways] = useState(false);
+  const [onlineIpfsGateways, setOnlineIpfsGateways] = useState<Gateway[]>([]);
 
   const isTokenDetectionEnabled = useSelector(selectUseTokenDetection);
   const chainId = useSelector(selectChainId);
+  const displayNftMedia = useSelector(selectDisplayNftMedia);
+  const useNftDetection = useSelector(selectUseNftDetection);
+  const isIpfsGatewayEnabled = useSelector(selectIsIpfsGatewayEnabled);
+  const ipfsGateway = useSelector(selectIpfsGateway);
 
   const renderBackButton = useCallback(
     () => (
@@ -55,7 +91,8 @@ const AssetSettings = () => {
   };
 
   const renderTokenDetectionSection = () => {
-    // const { isTokenDetectionEnabled, chainId } = this.props;
+    // TODO: Remember to ask why we needed mock styles
+    // in "AdvancedSettings" component
     // const { styles, colors } = this.getStyles();
     // const theme = this.context || mockTheme;
     if (!isTokenDetectionSupportedForNetwork(chainId)) {
@@ -95,6 +132,209 @@ const AssetSettings = () => {
     );
   };
 
+  const toggleDisplayNftMedia = (value: boolean) => {
+    const { PreferencesController } = Engine.context;
+    PreferencesController?.setDisplayNftMedia(value);
+    if (!value) PreferencesController?.setUseNftDetection(value);
+  };
+
+  const renderDisplayNftMedia = useCallback(
+    () => (
+      <View
+        style={styles.halfSetting}
+        // testID={NFT_DISPLAY_MEDIA_MODE_SECTION}
+      >
+        <View style={styles.titleContainer}>
+          <Text variant={TextVariant.BodyLGMedium} style={styles.title}>
+            {strings('app_settings.display_nft_media')}
+          </Text>
+          <View style={styles.switchElement}>
+            <Switch
+              value={displayNftMedia}
+              onValueChange={toggleDisplayNftMedia}
+              trackColor={{
+                true: colors.primary.default,
+                false: colors.border.muted,
+              }}
+              thumbColor={theme.brandColors.white}
+              style={styles.switch}
+              ios_backgroundColor={colors.border.muted}
+              testID="display-nft-toggle"
+            />
+          </View>
+        </View>
+        <Text
+          variant={TextVariant.BodyMD}
+          color={TextColor.Alternative}
+          style={styles.desc}
+        >
+          {strings('app_settings.display_nft_media_desc_new')}
+        </Text>
+      </View>
+    ),
+    [colors, styles, displayNftMedia, theme],
+  );
+
+  const toggleNftAutodetect = useCallback(
+    (value) => {
+      const { PreferencesController } = Engine.context;
+      if (value) {
+        PreferencesController.setDisplayNftMedia(value);
+      }
+      PreferencesController.setUseNftDetection(value);
+      const traits = {
+        [UserProfileProperty.NFT_AUTODETECTION]: value
+          ? UserProfileProperty.ON
+          : UserProfileProperty.OFF,
+      };
+      addTraitsToUser(traits);
+      trackEvent(MetaMetricsEvents.NFT_AUTO_DETECTION_ENABLED, {
+        ...traits,
+        location: 'app_settings',
+      });
+    },
+    [addTraitsToUser, trackEvent],
+  );
+
+  const renderAutoDetectNft = useCallback(
+    () => (
+      <View
+        style={styles.setting}
+        // TODO: Why do we need ref
+        // ref={detectNftComponentRef}
+        // testID={NFT_AUTO_DETECT_MODE_SECTION}
+      >
+        <View style={styles.titleContainer}>
+          <Text variant={TextVariant.BodyLGMedium} style={styles.title}>
+            {strings('app_settings.nft_autodetect_mode')}
+          </Text>
+          <View style={styles.switchElement}>
+            <Switch
+              value={useNftDetection}
+              onValueChange={toggleNftAutodetect}
+              trackColor={{
+                true: colors.primary.default,
+                false: colors.border.muted,
+              }}
+              thumbColor={theme.brandColors.white}
+              style={styles.switch}
+              ios_backgroundColor={colors.border.muted}
+            />
+          </View>
+        </View>
+        <Text
+          variant={TextVariant.BodyMD}
+          color={TextColor.Alternative}
+          style={styles.desc}
+        >
+          {strings('app_settings.autodetect_nft_desc')}
+        </Text>
+      </View>
+    ),
+    [colors, styles, useNftDetection, theme, toggleNftAutodetect],
+  );
+
+  const setIsIpfsGatewayEnabled = (isIpfsGatewatEnabled: boolean) => {
+    const { PreferencesController } = Engine.context;
+    PreferencesController.setIsIpfsGatewayEnabled(isIpfsGatewatEnabled);
+  };
+
+  const setIpfsGateway = (gateway: string) => {
+    const { PreferencesController } = Engine.context;
+    PreferencesController.setIpfsGateway(gateway);
+  };
+
+  const renderIpfsGateway = () => (
+    <View
+      style={styles.setting}
+      // testID={IPFS_GATEWAY_SECTION}
+    >
+      <View style={styles.titleContainer}>
+        <Text variant={TextVariant.BodyLGMedium} style={styles.title}>
+          {strings('app_settings.ipfs_gateway')}
+        </Text>
+        <View style={styles.switchElement}>
+          <Switch
+            value={isIpfsGatewayEnabled}
+            onValueChange={setIsIpfsGatewayEnabled}
+            trackColor={{
+              true: colors.primary.default,
+              false: colors.border.muted,
+            }}
+            thumbColor={theme.brandColors.white}
+            style={styles.switch}
+            ios_backgroundColor={colors.border.muted}
+          />
+        </View>
+      </View>
+      <Text
+        variant={TextVariant.BodyMD}
+        color={TextColor.Alternative}
+        style={styles.desc}
+      >
+        {strings('app_settings.ipfs_gateway_content')}
+      </Text>
+      {isIpfsGatewayEnabled && (
+        <View style={styles.accessory}>
+          <Text
+            variant={TextVariant.BodyMD}
+            color={TextColor.Alternative}
+            style={styles.desc}
+          >
+            {strings('app_settings.ipfs_gateway_desc')}
+          </Text>
+          <View style={styles.picker}>
+            {gotAvailableGateways ? (
+              <SelectComponent
+                selectedValue={ipfsGateway}
+                defaultValue={strings('app_settings.ipfs_gateway_down')}
+                onValueChange={setIpfsGateway}
+                label={strings('app_settings.ipfs_gateway')}
+                options={onlineIpfsGateways}
+              />
+            ) : (
+              <View>
+                <ActivityIndicator size="small" />
+              </View>
+            )}
+          </View>
+        </View>
+      )}
+    </View>
+  );
+
+  const handleAvailableIpfsGateways = useCallback(async () => {
+    if (!isIpfsGatewayEnabled) return;
+    const ipfsGatewaysPromises = ipfsGateways.map(async (gateway: Gateway) => {
+      const testUrl =
+        gateway.value + HASH_TO_TEST + '#x-ipfs-companion-no-redirect';
+      try {
+        const res = await timeoutFetch(testUrl, 1200);
+        const text = await res.text();
+        const available = text.trim() === HASH_STRING.trim();
+        return { ...gateway, available };
+      } catch (e) {
+        const available = false;
+        return { ...gateway, available };
+      }
+    });
+    const ipfsGatewaysAvailability = await Promise.all(ipfsGatewaysPromises);
+    const onlineGateways = ipfsGatewaysAvailability.filter(
+      (gateway) => gateway.available,
+    );
+
+    const sortedOnlineIpfsGateways = [...onlineGateways].sort(
+      (a, b) => a.key - b.key,
+    );
+
+    setGotAvailableGateways(true);
+    setOnlineIpfsGateways(sortedOnlineIpfsGateways);
+  }, [isIpfsGatewayEnabled]);
+
+  useEffect(() => {
+    handleAvailableIpfsGateways();
+  }, [handleAvailableIpfsGateways]);
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerLeft: renderBackButton,
@@ -103,7 +343,12 @@ const AssetSettings = () => {
   }, [navigation, renderBackButton, renderTitle]);
 
   return (
-    <ScrollView style={styles.root}>{renderTokenDetectionSection()}</ScrollView>
+    <ScrollView style={styles.root}>
+      {renderTokenDetectionSection()}
+      {renderDisplayNftMedia()}
+      {renderAutoDetectNft()}
+      {renderIpfsGateway()}
+    </ScrollView>
   );
 };
 
