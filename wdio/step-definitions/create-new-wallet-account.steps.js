796092c65e2330c4f07da344032891176dacb7f0
yande <110056475+Andepande@users.noreply.github.com>
2024-09-10 17:30:47 +0100

    Update create-new-wallet-account.steps.js

--- wdio/step-definitions/create-new-wallet-account.steps.js
+++ wdio/step-definitions/create-new-wallet-account.steps.js
@@ -5,11 +5,20 @@ import AccountListComponent from '../screen-objects/AccountListComponent';
 import WalletAccountModal from '../screen-objects/Modals/WalletAccountModal.js';
 import CommonScreen from '../screen-objects/CommonScreen';
 import AddAccountModal from '../screen-objects/Modals/AddAccountModal';
+import ExperienceEnhancerModal from '../screen-objects/Modals/ExperienceEnhancerModal';
 
 Then(/^I tap Create a new account/, async () => {
   await AccountListComponent.tapAddAccountButton();
   await AddAccountModal.tapNewAccountButton();
-  await CommonScreen.tapOnText('No thanks');
+  try {
+    // Handle Marketing consent modal
+
+    await ExperienceEnhancerModal.waitForDisplay();
+    await ExperienceEnhancerModal.tapNoThanks();
+    await ExperienceEnhancerModal.waitForDisappear();
+  } catch {
+    console.log('The marketing consent modal is not visible');
+  }
 });
 
 When(/^A new account is created/, async () => {
