5868d3bfb879db85780f6d1816e13e8ad8635ea0
Jonathan Bursztyn <jonathan@bursztyn.io>
2024-09-11 01:45:50 +0100

    Add mock

--- app/components/Views/QRScanner/index.test.tsx
+++ app/components/Views/QRScanner/index.test.tsx
@@ -4,6 +4,20 @@ import renderWithProvider from '../../../util/test/renderWithProvider';
 import QrScanner from './';
 import { backgroundState } from '../../../util/test/initial-root-state';
 
+const mockNavigate = jest.fn();
+const mockGoBack = jest.fn();
+
+jest.mock('@react-navigation/native', () => {
+  const actualReactNavigation = jest.requireActual('@react-navigation/native');
+  return {
+    ...actualReactNavigation,
+    useNavigation: () => ({
+      navigate: mockNavigate,
+      goBack: mockGoBack,
+    }),
+  };
+});
+
 const initialState = {
   engine: {
     backgroundState,
