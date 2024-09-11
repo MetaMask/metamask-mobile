f2e22b368245801ecb20e2590d699a9b307a2211
Matthew Grainger <46547583+Matt561@users.noreply.github.com>
2024-09-11 11:35:42 -0400

    feat: build your earnings component stub in eth token details (#10964)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    This PR adds staking-related component stubs as part of our mobile
    pooled staking initiative.
    
    - Added `StakingEarnings` and `TooltipModal` components.
    - Added `useTooltipModal()` for easy reuse of tooltip modal.
    - Added "Your earnings" section on Ethereum asset details page.
    - Added necessary English translations in `en.json`.
    - Updated relevant `__snapshot__` files.
    
    Note: The `<StakingEarnings>` component uses mock data temporarily until
    we've connected it to the backend. This component will also be disabled
    via the `MM_POOLED_STAKING_UI_ENABLED` feature flag.
    
    ## **Related issues**
    
    [STAKE-782: [FE] Build ‘Your Earnings’ component in ETH token
    details](https://consensyssoftware.atlassian.net/browse/STAKE-782)
    
    ## **Manual testing steps**
    
    Getting to ETH earnings section
    1. Click `Ethereum` on main portfolio/tokens list page
    2. You should see the new `Your earnings` underneath the balances
    section (near the top).
    
    Testing `TooltipModal`
    1. In the `Your earnings` section, click on the info icon beside "Annual
    rate".
    2. You should see a bottom sheet modal appear with the "Annual rate"
    tooltip.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    
    ### **Before**
    <!-- [screenshots/recordings] -->
    
    
    https://github.com/user-attachments/assets/bd00a576-7c1a-42e0-b6ff-7e98c84de5e0
    
    ### **After**
    <!-- [screenshots/recordings] -->
    
    
    https://github.com/user-attachments/assets/3e89d88f-9651-41da-8de9-1906c9590b02
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/UI/AssetOverview/TokenDetails/TokenDetails.tsx
+++ app/components/UI/AssetOverview/TokenDetails/TokenDetails.tsx
@@ -1,5 +1,5 @@
 import { zeroAddress } from 'ethereumjs-util';
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import { useSelector } from 'react-redux';
 import i18n from '../../../../../locales/i18n';
@@ -21,6 +21,8 @@ import Logger from '../../../../util/Logger';
 import TokenDetailsList from './TokenDetailsList';
 import MarketDetailsList from './MarketDetailsList';
 import { Asset } from '../AssetOverview.types';
+import StakingEarnings from '../StakingEarnings';
+import { isPooledStakingFeatureEnabled } from '../../Stake/constants';
 
 export interface TokenDetails {
   contractAddress: string | null;
@@ -49,6 +51,9 @@ const TokenDetails: React.FC<TokenDetailsProps> = ({ asset }) => {
   const conversionRate = useSelector(selectConversionRate);
   const currentCurrency = useSelector(selectCurrentCurrency);
   const tokenContractAddress = safeToChecksumAddress(asset.address);
+
+  // TEMP: Remove once component has been implemented.
+  const [hasStakingPositions] = useState(true);
 
   let tokenMetadata;
   let marketData;
@@ -121,7 +126,10 @@ const TokenDetails: React.FC<TokenDetailsProps> = ({ asset }) => {
   };
 
   return (
-    <View style={styles.wrapper}>
+    <View style={styles.tokenDetailsContainer}>
+      {asset.isETH &&
+        hasStakingPositions &&
+        isPooledStakingFeatureEnabled() && <StakingEarnings />}
       {(asset.isETH || tokenMetadata) && (
         <TokenDetailsList tokenDetails={tokenDetails} />
       )}
