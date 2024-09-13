f93bd3db2283c189a18b08b9954e9d23e4312fcb
Cal-L <cleun007@gmail.com>
2024-09-13 15:43:54 -0700

    Add more TS

--- app/actions/alert/index.ts
+++ app/actions/alert/index.ts
@@ -1,15 +1,40 @@
-export function dismissAlert() {
+import type { Action as ReduxAction } from 'redux';
+
+export interface AlertData {
+  msg: string;
+  width?: string;
+}
+
+export interface ShowAlert extends ReduxAction<'SHOW_ALERT'> {
+  autodismiss: number;
+  content: string;
+  data: AlertData;
+}
+export type HideAlert = ReduxAction<'HIDE_ALERT'>;
+export type AlertAction = ShowAlert | HideAlert;
+
+export function dismissAlert(): HideAlert {
   return {
     type: 'HIDE_ALERT',
   };
 }
 
-export function showAlert({ isVisible, autodismiss, content, data }: any) {
+export function showAlert({
+  isVisible,
+  autodismiss,
+  content,
+  data,
+}: {
+  isVisible?: boolean;
+  autodismiss: number;
+  content: string;
+  data: AlertData;
+}): ShowAlert {
   return {
     type: 'SHOW_ALERT',
     isVisible,
     autodismiss,
     content,
     data,
-  };
+  } as ShowAlert;
 }
