508c7f767bab50fd115eed564f98caf77f3e3090
metamaskbot <metamaskbot@users.noreply.github.com>
2024-09-16 15:40:27 -0600

    Revert "commented out 1st PR creation no longer needed"
    This reverts commit bac0e7e3afb4f5f9117446e1a49476546d300650.

--- scripts/create-release-pr.sh
+++ scripts/create-release-pr.sh
@@ -15,26 +15,26 @@ fi
 
 RELEASE_BRANCH_NAME="${RELEASE_BRANCH_PREFIX}${NEW_VERSION}"
 CHANGELOG_BRANCH_NAME="chore/${NEW_VERSION}-Changelog"
-# RELEASE_BODY="This is the release candidate for version ${NEW_VERSION}. The changelog will be found in another PR ${CHANGELOG_BRANCH_NAME}."
+RELEASE_BODY="This is the release candidate for version ${NEW_VERSION}. The changelog will be found in another PR ${CHANGELOG_BRANCH_NAME}."
 
 git config user.name metamaskbot
 git config user.email metamaskbot@users.noreply.github.com
 
-git checkout "${RELEASE_BRANCH_NAME}"
+git checkout -b "${RELEASE_BRANCH_NAME}"
 
-# if ! (git add . && git commit -m "${NEW_VERSION}");
-# then
-#     echo "Error: No changes detected."
-#     exit 1
-# fi
+if ! (git add . && git commit -m "${NEW_VERSION}");
+then
+    echo "Error: No changes detected."
+    exit 1
+fi
 
-# git push --set-upstream origin "${RELEASE_BRANCH_NAME}"
+git push --set-upstream origin "${RELEASE_BRANCH_NAME}"
 
-# gh pr create \
-#   --draft \
-#   --title "feat: ${NEW_VERSION}" \
-#   --body "${RELEASE_BODY}" \
-#   --head "${RELEASE_BRANCH_NAME}";
+gh pr create \
+  --draft \
+  --title "feat: ${NEW_VERSION}" \
+  --body "${RELEASE_BODY}" \
+  --head "${RELEASE_BRANCH_NAME}";
 
 
 git checkout -b "${CHANGELOG_BRANCH_NAME}"
