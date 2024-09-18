2cf43036e96778d1b5a91d01937c984f8a696f11
yande <110056475+Andepande@users.noreply.github.com>
2024-09-18 22:45:26 +0100

    test: E2E Mocking Setup For Detox Tests (#11144)
    Consolidated Mocking Research into Current Branch
    
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    This PR introduces the initial E2E API mocking test. When IsTest is
    true, the iOS/Android app redirects traffic to a proxy within the e2e
    folder for tests that require mocking. The mock server proxy forwards
    all traffic to its destination unless it matches specific mock
    requirements. The first test verifies that the suggestedGasApi falls
    back to the legacy endpoint when the EIP1559 API is down, displaying a
    legacy modal. The test then reinstates the service and asserts the modal
    change immediately.
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    ## **Related issues**
    
    
    Fixes:https://github.com/orgs/MetaMask/projects/60/views/16?pane=issue&itemId=70977379
    
    ## **Manual testing steps**
    
    1. Run e2e Detox setup 
    2. Have IOS and Android sim ready
    3. For IOS run command `yarn detox test -c ios.sim.debug
    e2e/specs/wallet/suggestedGasApi.mock.spec.js;` for Android
    run `yarn detox test -c android.emu.debug
    e2e/specs/wallet/suggestedGasApi.mock.spec.js`
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    https://www.loom.com/share/64d322a3f50d4888b1b7a96107008918
    
    https://app.bitrise.io/app/be69d4368ee7e86d/pipelines/ef9aaa7c-6247-4bf9-81fd-09bbaa76d7c8
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: SamuelSalas <samuel.salas.reyes@gmail.com>

--- shim.js
+++ shim.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-nodejs-modules */
+/* global Platform */
 import { decode, encode } from 'base-64';
 import {
   FIXTURE_SERVER_PORT,
@@ -6,6 +7,7 @@ import {
   testConfig,
 } from './app/util/test/utils.js';
 import { LaunchArguments } from 'react-native-launch-arguments';
+import { defaultMockPort } from './e2e/mockServer/mockUrlCollection';
 
 // In a testing environment, assign the fixtureServerPort to use a deterministic port
 if (isTest) {
@@ -56,3 +58,26 @@ if (typeof localStorage !== 'undefined') {
 // If using the crypto shim, uncomment the following line to ensure
 // crypto is loaded first, so it can populate global.crypto
 // require('crypto')
+
+if (isTest) {
+  (async () => {
+    const { fetch: originalFetch } = global;
+    const MOCKTTP_URL = `http://${
+      Platform.OS === 'ios' ? 'localhost' : '10.0.2.2'
+    }:${defaultMockPort}`;
+
+    const isMockServerAvailable = await originalFetch(
+      `${MOCKTTP_URL}/health-check`,
+    )
+      .then((res) => res.ok)
+      .catch(() => false);
+    // if mockServer is off we route to original destination
+    global.fetch = async (url, options) =>
+      isMockServerAvailable
+        ? originalFetch(
+            `${MOCKTTP_URL}/proxy?url=${encodeURIComponent(url)}`,
+            options,
+          ).catch(() => originalFetch(url, options))
+        : originalFetch(url, options);
+  })();
+}
