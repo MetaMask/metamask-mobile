b4b107819102870ce1ade767bf94d96bd800a1d7
Xiaoming Wang <dawnseeker8@gmail.com>
2024-09-18 19:52:52 +0800

    fix: add link style to clickable instruction in SearchingForDeviceStep.tsx

--- app/components/UI/LedgerModals/Steps/SearchingForDeviceStep.tsx
+++ app/components/UI/LedgerModals/Steps/SearchingForDeviceStep.tsx
@@ -11,14 +11,15 @@ import { getSystemVersion } from 'react-native-device-info';
 import { strings } from '../../../../../locales/i18n';
 import { LEDGER_SUPPORT_LINK } from '../../../../constants/urls';
 import Device from '../../../../util/device';
-import { useAssetFromTheme } from '../../../../util/theme';
+import { useAssetFromTheme, useTheme } from '../../../../util/theme';
 import Text, { TextVariant } from '../../../../component-library/components/Texts/Text';
 
 import ledgerConnectDarkImage from '../../../../images/ledger-connect-dark.png';
 import ledgerConnectLightImage from '../../../../images/ledger-connect-light.png';
 import { SEARCHING_FOR_DEVICE_STEP } from './Steps.constants';
+import { Colors } from '../../../../util/theme/models';
 
-const createStyles = () =>
+const createStyles = (colors: Colors) =>
   StyleSheet.create({
     modalTitle: {
       marginTop: 30,
@@ -51,6 +52,8 @@ const createStyles = () =>
       flexWrap: 'wrap',
       marginLeft: 20,
       marginRight: 20,
+      color: colors.primary.default,
+
     },
     lookingForDeviceContainer: {
       flex: 1,
@@ -59,7 +62,8 @@ const createStyles = () =>
   });
 
 const SearchingForDeviceStep = () => {
-  const styles = useMemo(() => createStyles(), []);
+  const { colors } = useTheme();
+  const styles = useMemo(() => createStyles(colors), []);
   const navigation = useNavigation();
 
   const deviceOSVersion = Number(getSystemVersion()) || 0;
