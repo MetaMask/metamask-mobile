726ca826662ea6044cd5da671555c379e19a7864
Matteo Scurati <matteo.scurati@gmail.com>
2024-09-19 16:37:37 +0200

    Feat/notifications add analytics (#11285)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    In this pull request, I have integrated extensive analytics tracking
    across various components and functionalities. This includes adding
    events for enabling and disabling basic functionality, profile syncing,
    and notifications. Additionally, I have implemented tracking for user
    interactions with notifications, such as opening the notifications menu,
    marking notifications as read, and clicking on notification details.
    
    ## **Related issues**
    
    N/A
    
    ## **Manual testing steps**
    
    N/A
    
    ## **Screenshots/Recordings**
    
    N/A
    
    ### **Before**
    
    N/A
    
    ### **After**
    
    N/A
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Prithpal Sooriya <prithpal.sooriya@consensys.net>

--- app/components/Views/Wallet/index.tsx
+++ app/components/Views/Wallet/index.tsx
@@ -1,4 +1,10 @@
-import React, { useEffect, useRef, useCallback, useContext, useLayoutEffect } from 'react';
+import React, {
+  useEffect,
+  useRef,
+  useCallback,
+  useContext,
+  useLayoutEffect,
+} from 'react';
 import {
   ActivityIndicator,
   StyleSheet,
@@ -86,7 +92,9 @@ import { getCurrentRoute } from '../../../reducers/navigation';
 import { WalletViewSelectorsIDs } from '../../../../e2e/selectors/wallet/WalletView.selectors';
 import {
   getMetamaskNotificationsUnreadCount,
+  getMetamaskNotificationsReadCount,
   selectIsMetamaskNotificationsEnabled,
+  selectIsProfileSyncingEnabled,
 } from '../../../selectors/notifications';
 import { ButtonVariants } from '../../../component-library/components/Buttons/Button';
 import { useListNotifications } from '../../../util/notifications/hooks/useNotifications';
@@ -282,9 +290,13 @@ const Wallet = ({
     selectIsMetamaskNotificationsEnabled,
   );
 
+  const isProfileSyncingEnabled = useSelector(selectIsProfileSyncingEnabled);
+
   const unreadNotificationCount = useSelector(
     getMetamaskNotificationsUnreadCount,
   );
+
+  const readNotificationCount = useSelector(getMetamaskNotificationsReadCount);
 
   const networkName = useSelector(selectNetworkName);
 
@@ -437,7 +449,9 @@ const Wallet = ({
         navigation,
         colors,
         isNotificationEnabled,
+        isProfileSyncingEnabled,
         unreadNotificationCount,
+        readNotificationCount,
       ),
     );
     /* eslint-disable-next-line */
@@ -448,7 +462,9 @@ const Wallet = ({
     networkImageSource,
     onTitlePress,
     isNotificationEnabled,
+    isProfileSyncingEnabled,
     unreadNotificationCount,
+    readNotificationCount,
   ]);
 
   const renderTabBar = useCallback(
