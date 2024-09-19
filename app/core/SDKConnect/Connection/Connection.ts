05e56f124d385df4cfdcb27a1636a4e42a237e39
abretonc7s <arthur.breton@consensys.net>
2024-09-19 16:56:07 +0800

    feat: unit test

--- app/core/SDKConnect/Connection/Connection.ts
+++ app/core/SDKConnect/Connection/Connection.ts
@@ -317,7 +317,7 @@ export class Connection extends EventEmitter2 {
     this.trigger = trigger;
   }
 
-  disconnect({ terminate, context }: { terminate: boolean; context?: string }) {
+  disconnect({ terminate, context }: { terminate: boolean; context?: string }): Promise<boolean> {
     return disconnect({ instance: this, terminate, context });
   }
 
