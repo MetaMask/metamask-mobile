58f3990538db35435a3fcb83f70d1297a1064c1f
JSoufer <jonathan.ferreira@consensys.net>
2024-09-20 16:23:58 +0100

    revokechange

--- app/lib/snaps/SnapsExecutionWebView.tsx
+++ app/lib/snaps/SnapsExecutionWebView.tsx
@@ -6,7 +6,7 @@ import React, { Component, RefObject } from 'react';
 import { View, ScrollView, NativeSyntheticEvent } from 'react-native';
 import WebView, { WebViewMessageEvent } from '@metamask/react-native-webview';
 import { createStyles } from './styles';
-import { WebViewInterface } from '@metamask/snaps-controllers/react-native';
+import { WebViewInterface } from '@metamask/snaps-controllers/dist/services/webview/WebViewMessageStream.cjs';
 import { WebViewError } from '@metamask/react-native-webview/lib/WebViewTypes';
 import { PostMessageEvent } from '@metamask/post-message-stream';
 
