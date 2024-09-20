82bdb05a3ae2f767b32416d5e192e221d3d87eda
salimtb <salim.toubal@outlook.com>
2024-09-20 16:30:42 +0200

    fix: update mock network function

--- app/util/test/network.ts
+++ app/util/test/network.ts
@@ -1,13 +1,17 @@
 // TODO: This is intentionally the old network state, and could be
 
-import { NetworkMetadata, NetworkStatus } from '@metamask/network-controller';
+import {
+  NetworkMetadata,
+  NetworkState,
+  NetworkStatus,
+  RpcEndpointType,
+} from '@metamask/network-controller';
 import { Hex } from '@metamask/utils';
 import { v4 as uuidv4 } from 'uuid';
 
 export const mockNetworkState = (
   ...networks: {
     id?: string;
-    type?: string;
     chainId: Hex;
     rpcUrl?: string;
     nickname?: string;
@@ -15,42 +19,59 @@ export const mockNetworkState = (
     blockExplorerUrl?: string;
     metadata?: NetworkMetadata;
   }[]
-) => {
-  const networkConfigurations = networks.map((network) => ({
-    id: network.id ?? uuidv4(),
-    chainId: network.chainId,
-    rpcUrl:
+): NetworkState => {
+  if (
+    new Set(networks.map((network) => network.chainId)).size !== networks.length
+  ) {
+    throw 'mockNetworkState doesnt currently support multiple rpc urls per chain id';
+  }
+
+  const networkConfigurations = networks.map((network) => {
+    const blockExplorer =
+      !('blockExplorerUrl' in network) || network.blockExplorerUrl
+        ? network.blockExplorerUrl ??
+          `https://localhost/blockExplorer/${network.chainId}`
+        : undefined;
+
+    const rpc =
       'rpcUrl' in network
         ? network.rpcUrl
-        : `https://localhost/rpc/${network.chainId}`,
-    nickname: network.nickname ?? '',
+        : `https://localhost/rpc/${network.chainId}`;
 
-    ticker: network.ticker ?? '',
-
-    ...((!('blockExplorerUrl' in network) || network.blockExplorerUrl) && {
-      rpcPrefs: {
-        blockExplorerUrl:
-          network.blockExplorerUrl ??
-          `https://localhost/blockExplorer/${network.chainId}`,
-      },
-    }),
-  }));
+    return {
+      chainId: network.chainId,
+      blockExplorerUrls: blockExplorer ? [blockExplorer] : [],
+      defaultBlockExplorerUrlIndex: blockExplorer ? 0 : undefined,
+      rpcEndpoints: [
+        {
+          networkClientId: network.id ?? uuidv4(),
+          type: RpcEndpointType.Custom,
+          url: rpc,
+        },
+      ],
+      defaultRpcEndpointIndex: 0,
+      name: 'nickname' in network ? network.nickname : 'mainnet',
+      nativeCurrency: 'ticker' in network ? network.ticker : 'ETH',
+    };
+  });
 
   const networksMetadata = networks.reduce(
     (acc, network, i) => ({
       ...acc,
-      [networkConfigurations[i].id]: network.metadata ?? {
-        EIPS: {},
-        status: NetworkStatus.Available,
-      },
+      [networkConfigurations[i].rpcEndpoints[0].networkClientId]:
+        network.metadata ?? {
+          EIPS: {},
+          status: NetworkStatus.Available,
+        },
     }),
     {},
   );
 
   return {
-    selectedNetworkClientId: networkConfigurations[0].id,
-    networkConfigurations: networkConfigurations.reduce(
-      (acc, network) => ({ ...acc, [network.id]: network }),
+    selectedNetworkClientId:
+      networkConfigurations[0].rpcEndpoints[0].networkClientId,
+    networkConfigurationsByChainId: networkConfigurations.reduce(
+      (acc, network) => ({ ...acc, [network.chainId]: network }),
       {},
     ),
     networksMetadata,
