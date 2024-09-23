78ff64b368191f0472bff6669737a880d7a76237
Jongsun Suh <jongsun.suh@icloud.com>
2024-09-23 08:29:03 -0400

    Remove duplicate controller entries in `EngineService`

--- app/core/EngineService/EngineService.ts
+++ app/core/EngineService/EngineService.ts
@@ -152,18 +152,6 @@ class EngineService {
         name: 'PPOMController',
         key: `${engine.context.PPOMController.name}:stateChange`,
       },
-      {
-        name: 'AuthenticationController',
-        key: `AuthenticationController:stateChange`,
-      },
-      {
-        name: 'UserStorageController',
-        key: `UserStorageController:stateChange`,
-      },
-      {
-        name: 'NotificationServicesController',
-        key: `NotificationServicesController:stateChange`,
-      },
     ];
 
     engine?.datamodel?.subscribe?.(() => {
