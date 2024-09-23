70102dd5d4a21966f0fb92bc264152d8383c269e
JSoufer <jonathan.ferreira@consensys.net>
2024-09-23 11:14:21 +0100

    adapt e2e tests

--- e2e/pages/EnableDeviceNotificationsAlert.js
+++ e2e/pages/EnableDeviceNotificationsAlert.js
@@ -8,24 +8,19 @@ import {
 
 export default class EnableDeviceNotificationsAlert {
   static async tapNoThanks() {
-    await TestHelpers.waitAndTap(
-      ENABLE_DEVICE_NOTIFICATIONS_NO_THANKS_BUTTON_ID,
+    await TestHelpers.waitAndTapByLabel(
+      ENABLE_DEVICE_NOTIFICATIONS_NO_THANKS_BUTTON_ID
     );
   }
 
   static async tapYes() {
-    await TestHelpers.waitAndTap(
+    await TestHelpers.waitAndTapByLabel(
       ENABLE_DEVICE_NOTIFICATIONS_YES_BUTTON_ID,
     );
   }
 
   static async isVisible() {
-    await TestHelpers.checkIfVisible(
-      ENABLE_DEVICE_NOTIFICATIONS_CONTAINER_ID,
-    );
-  }
-  static async isNotVisible() {
-    await TestHelpers.checkIfNotVisible(
+    await TestHelpers.checkIfElementWithTextIsVisible(
       ENABLE_DEVICE_NOTIFICATIONS_CONTAINER_ID,
     );
   }
--- wdio/screen-objects/testIDs/Screens/EnableDeviceNotificationsChecksAlert.testIds.js
+++ wdio/screen-objects/testIDs/Screens/EnableDeviceNotificationsChecksAlert.testIds.js
@@ -1,5 +1,6 @@
 export const ENABLE_DEVICE_NOTIFICATION_NO_THANKS_BUTTON_ID =
-  'no-thanks-button';
+  "Don't Allow";
 export const ENABLE_DEVICE_NOTIFICATIONS_CONTAINER_ID =
-  'enable-device-notifications-alert-container';
-export const ENABLE_DEVICE_NOTIFICATIONS_YES_BUTTON_ID = 'yes-button';
+  'Would Like to Send You Notifications';
+export const ENABLE_DEVICE_NOTIFICATIONS_YES_BUTTON_ID = 'Allow';
+
