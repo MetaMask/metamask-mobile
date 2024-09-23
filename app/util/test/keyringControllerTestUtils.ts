7311dd00a9c487bcb93cdab9b2c22026aaf55fff
Kate Johnson <johnson.kat.g@gmail.com>
2024-09-23 19:40:43 -0400

    feat: debugging mocked testing

---
+++ app/util/test/keyringControllerTestUtils.ts
@@ -0,0 +1,28 @@
+import { KeyringTypes } from '@metamask/keyring-controller';
+
+export const MOCK_KEYRING_CONTROLLER_STATE = {
+  keyring: {
+    keyrings: [
+      {
+        mnemonic:
+          'one two three four five six seven eight nine ten eleven twelve',
+      },
+    ],
+  },
+  state: {
+    keyrings: [
+      {
+        accounts: ['0xd018538C87232FF95acbCe4870629b75640a78E7'],
+        type: KeyringTypes.simple,
+      },
+      {
+        accounts: ['0xB374Ca013934e498e5baD3409147F34E6c462389'],
+        type: KeyringTypes.qr,
+      },
+      {
+        accounts: ['0x71C7656EC7ab88b098defB751B7401B5f6d8976F'],
+        type: KeyringTypes.hd,
+      },
+    ],
+  },
+};
