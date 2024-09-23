cc84acde31156c7c62a7336fc72b47f9dc981308
JSoufer <jonathan.ferreira@consensys.net>
2024-09-23 22:07:36 +0100

    increase testing covergae

--- app/util/notifications/services/NotificationService.test.ts
+++ app/util/notifications/services/NotificationService.test.ts
@@ -2,6 +2,7 @@ import notifee, {
   AuthorizationStatus,
   AndroidChannel,
   AndroidImportance,
+  EventType,
 } from '@notifee/react-native';
 import { Linking } from 'react-native';
 import { ChannelId } from '../../../util/notifications/androidChannels';
@@ -80,5 +81,54 @@ describe('NotificationsService', () => {
     await NotificationsService.createChannel(channel);
 
     expect(notifee.createChannel).toHaveBeenCalledWith(channel);
+  });
+
+  it('should return authorized from getAllPermissions', async () => {
+    const result = await NotificationsService.getAllPermissions();
+    expect(result.permission).toBe('authorized');
+  });
+
+  it('should return authorized from requestPermission ', async () => {
+    const result = await NotificationsService.requestPermission();
+    expect(result).toBe('authorized');
+  });
+
+  it('should return denied from requestPermission', async () => {
+    (notifee.requestPermission as jest.Mock).mockResolvedValue({
+      authorizationStatus: AuthorizationStatus.DENIED,
+    });
+    const result = await NotificationsService.requestPermission();
+    expect(result).toBe('denied');
+  });
+
+  it('should handle notification event', async () => {
+    const callback = jest.fn();
+
+    await NotificationsService.handleNotificationEvent({
+      type: EventType.DELIVERED,
+      detail: {
+        notification: {
+          id: '123',
+        },
+      },
+      callback,
+    });
+
+    expect(NotificationsService.incrementBadgeCount).toBeInstanceOf(Function);
+
+    await NotificationsService.handleNotificationEvent({
+      type: EventType.PRESS,
+      detail: {
+        notification: {
+          id: '123',
+        },
+      },
+      callback,
+    });
+
+    expect(NotificationsService.decrementBadgeCount).toBeInstanceOf(Function);
+    expect(NotificationsService.cancelTriggerNotification).toBeInstanceOf(
+      Function,
+    );
   });
 });
