25f23cff1a3ef51fb28e06b4c8b9766ebd916103
AxelGes <axelges9@gmail.com>
2024-09-24 18:23:32 -0300

    feat: refactor PaymentMethodSelector to use component library

--- app/components/UI/Ramp/components/PaymentMethodSelector.tsx
+++ app/components/UI/Ramp/components/PaymentMethodSelector.tsx
@@ -3,15 +3,14 @@ import { View, StyleSheet } from 'react-native';
 import Box from './Box';
 import Entypo from 'react-native-vector-icons/Entypo';
 import { useTheme } from '../../../../util/theme';
-
-import ListItemBase from '../../../Base/ListItem';
-import Text from '../../../Base/Text';
 import { Colors } from '../../../../util/theme/models';
-
-// TODO: Convert into typescript and correctly type
-// TODO: Replace "any" with type
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const ListItem = ListItemBase as any;
+import ListItem from '../../../../component-library/components/List/ListItem';
+import ListItemColumn, {
+  WidthType,
+} from '../../../../component-library/components/List/ListItemColumn';
+import Text, {
+  TextVariant,
+} from '../../../../component-library/components/Texts/Text';
 
 const createStyles = (colors: Colors) =>
   StyleSheet.create({
@@ -43,17 +42,23 @@ const PaymentMethodSelector: React.FC<IProps> = ({
   return (
     <Box label={label} onPress={onPress} highlighted={highlighted}>
       <View>
-        <ListItem.Content>
-          {Boolean(icon) && <ListItem.Icon>{icon}</ListItem.Icon>}
-          <ListItem.Body>
-            <Text black bold numberOfLines={1} adjustsFontSizeToFit>
+        <ListItem style={{ padding: 0 }}>
+          {Boolean(icon) && <ListItemColumn>{icon}</ListItemColumn>}
+
+          <ListItemColumn widthType={WidthType.Fill}>
+            <Text
+              variant={TextVariant.BodyLGMedium}
+              numberOfLines={1}
+              adjustsFontSizeToFit
+            >
               {name}
             </Text>
-          </ListItem.Body>
-          <ListItem.Amount>
+          </ListItemColumn>
+
+          <ListItemColumn>
             <Entypo name="chevron-right" size={16} style={styles.chevron} />
-          </ListItem.Amount>
-        </ListItem.Content>
+          </ListItemColumn>
+        </ListItem>
       </View>
     </Box>
   );
