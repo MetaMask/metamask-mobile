6a1bc5417bf155a94fd7c107c393a6303fca5c12
Jyoti Puri <jyotipuri@gmail.com>
2024-09-26 15:25:48 +0530

    Adding very basic confirmation page

--- app/components/Views/confirmations/Confirm/Confirm.test.tsx
+++ app/components/Views/confirmations/Confirm/Confirm.test.tsx
@@ -1,48 +1,14 @@
 import React from 'react';
 
 import renderWithProvider from '../../../../util/test/renderWithProvider';
-import { backgroundState } from '../../../../util/test/initial-root-state';
+import { personalSignatureConfirmationState } from '../../../../util/test/confirm-data-helpers';
 import Confirm from './index';
-
-process.env.REDESIGNED_SIGNATURE_REQUEST = 'true';
-
-const initialState = {
-  engine: {
-    backgroundState: {
-      ...backgroundState,
-      ApprovalController: {
-        pendingApprovals: {
-          '76b33b40-7b5c-11ef-bc0a-25bce29dbc09': {
-            id: '76b33b40-7b5c-11ef-bc0a-25bce29dbc09',
-            origin: 'metamask.github.io',
-            type: 'personal_sign',
-            time: 1727282253048,
-            requestData: {
-              data: '0x4578616d706c652060706572736f6e616c5f7369676e60206d657373616765',
-              from: '0x935e73edb9ff52e23bac7f7e043a1ecd06d05477',
-              meta: {
-                url: 'https://metamask.github.io/test-dapp/',
-                title: 'E2E Test Dapp',
-                icon: { uri: 'https://metamask.github.io/metamask-fox.svg' },
-                analytics: { request_source: 'In-App-Browser' },
-              },
-              origin: 'metamask.github.io',
-              metamaskId: '76b33b40-7b5c-11ef-bc0a-25bce29dbc09',
-            },
-            requestState: null,
-            expectsResult: true,
-          },
-        },
-        pendingApprovalCount: 1,
-        approvalFlows: [],
-      },
-    },
-  },
-};
 
 describe('Confirm', () => {
   it('should match snapshot for personal sign', async () => {
-    const container = renderWithProvider(<Confirm />, { state: initialState });
+    const container = renderWithProvider(<Confirm />, {
+      state: personalSignatureConfirmationState,
+    });
     expect(container).toMatchSnapshot();
   });
 });
