3fa0b64bba70bae1616baf4bbe2784be0131aa2e
Matthew Grainger <matthewgrainger33@gmail.com>
2024-09-27 12:49:19 -0400

    fix: removed padding on tooltip modal

--- app/components/Views/TooltipModal/ToolTipModal.styles.ts
+++ app/components/Views/TooltipModal/ToolTipModal.styles.ts
@@ -2,9 +2,6 @@ import { StyleSheet } from 'react-native';
 
 const styleSheet = () =>
   StyleSheet.create({
-    container: {
-      paddingBottom: 16,
-    },
     content: {
       paddingBottom: 16,
       paddingHorizontal: 16,
