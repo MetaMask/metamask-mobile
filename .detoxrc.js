ac46e2dd5b30d93c66bddc3c1cdf02193e5948d0
Cal-L <cleun007@gmail.com>
2024-09-28 23:15:45 -0700

    Fix ios spec config

--- .detoxrc.js
+++ .detoxrc.js
@@ -26,7 +26,7 @@ module.exports = {
   configurations: {
     'ios.sim.apiSpecs': {
       device: 'ios.simulator',
-      app: 'ios.debug',
+      app: 'ios.qa',
       testRunner: {
         args: {
           "$0": "node e2e/api-specs/run-api-spec-tests.js",
