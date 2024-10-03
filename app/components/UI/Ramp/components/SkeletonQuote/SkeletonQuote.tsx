954b8acea90d873da555afd03da46f809e2c19d1
AxelGes <axelges9@gmail.com>
2024-10-03 11:49:28 -0300

    fix: add classname in SkeletonQuote

--- app/components/UI/Ramp/components/SkeletonQuote/SkeletonQuote.tsx
+++ app/components/UI/Ramp/components/SkeletonQuote/SkeletonQuote.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleProp, View, ViewStyle } from 'react-native';
+import { StyleProp, View, ViewStyle, StyleSheet } from 'react-native';
 import Box from '../Box';
 import SkeletonText from '../SkeletonText';
 import ListItem from '../../../../../component-library/components/List/ListItem';
@@ -7,39 +7,50 @@ import ListItemColumn, {
   WidthType,
 } from '../../../../../component-library/components/List/ListItemColumn';
 
+const createStyles = () =>
+  StyleSheet.create({
+    alignEnd: {
+      alignItems: 'flex-end',
+    },
+  });
+
 const SkeletonQuote = ({
   collapsed,
   style,
 }: {
   collapsed?: boolean;
   style?: StyleProp<ViewStyle>;
-}) => (
-  <Box style={style} compact>
-    <ListItem>
-      <ListItemColumn>
-        <SkeletonText title />
-      </ListItemColumn>
-      <ListItemColumn widthType={WidthType.Fill}>
-        <View style={{ alignItems: 'flex-end' }}>
-          <SkeletonText small />
-        </View>
-      </ListItemColumn>
-    </ListItem>
-    {!collapsed && (
-      <>
-        <ListItem>
-          <ListItemColumn widthType={WidthType.Fill}>
-            <SkeletonText thin />
-          </ListItemColumn>
-          <ListItemColumn widthType={WidthType.Fill}>
-            <View style={{ alignItems: 'flex-end' }}>
-              <SkeletonText thin small />
-            </View>
-          </ListItemColumn>
-        </ListItem>
-      </>
-    )}
-  </Box>
-);
+}) => {
+  const styles = createStyles();
+
+  return (
+    <Box style={style} compact>
+      <ListItem>
+        <ListItemColumn>
+          <SkeletonText title />
+        </ListItemColumn>
+        <ListItemColumn widthType={WidthType.Fill}>
+          <View style={styles.alignEnd}>
+            <SkeletonText small />
+          </View>
+        </ListItemColumn>
+      </ListItem>
+      {!collapsed && (
+        <>
+          <ListItem>
+            <ListItemColumn widthType={WidthType.Fill}>
+              <SkeletonText thin />
+            </ListItemColumn>
+            <ListItemColumn widthType={WidthType.Fill}>
+              <View style={styles.alignEnd}>
+                <SkeletonText thin small />
+              </View>
+            </ListItemColumn>
+          </ListItem>
+        </>
+      )}
+    </Box>
+  );
+};
 
 export default SkeletonQuote;
