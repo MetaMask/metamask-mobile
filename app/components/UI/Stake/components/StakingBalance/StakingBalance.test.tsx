749a4a67f5518259baa1ce9ec3141211eba8f98a
Matthew Grainger <matthewgrainger33@gmail.com>
2024-10-03 11:42:22 -0400

    fix: updated tests

--- app/components/UI/Stake/components/StakingBalance/StakingBalance.test.tsx
+++ app/components/UI/Stake/components/StakingBalance/StakingBalance.test.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import { fireEvent, screen } from '@testing-library/react-native';
-import { renderScreen } from '../../../../../util/test/renderWithProvider';
-import { backgroundState } from '../../../../../util/test/initial-root-state';
+import { fireEvent } from '@testing-library/react-native';
+import renderWithProvider from '../../../../../util/test/renderWithProvider';
 import StakingBalance from './StakingBalance';
 import { strings } from '../../../../../../locales/i18n';
 import Routes from '../../../../../constants/navigation/Routes';
+import { Image } from 'react-native';
 
-function render(Component: React.ComponentType) {
-  return renderScreen(
-    Component,
-    {
-      name: 'Asset',
-    },
-    {
-      state: {
-        engine: {
-          backgroundState,
-        },
-      },
-    },
-  );
-}
+jest.mock('../../../../hooks/useIpfsGateway', () => jest.fn());
+
+Image.getSize = jest.fn((_uri, success) => {
+  success(100, 100); // Mock successful response for ETH native Icon Image
+});
 
 const mockNavigate = jest.fn();
 
@@ -34,15 +24,17 @@ jest.mock('@react-navigation/native', () => {
   };
 });
 describe('StakingBalance', () => {
+  beforeEach(() => jest.resetAllMocks());
+
   it('render matches snapshot', () => {
-    render(StakingBalance);
-    expect(screen.toJSON()).toMatchSnapshot();
+    const { toJSON } = renderWithProvider(<StakingBalance />);
+    expect(toJSON()).toMatchSnapshot();
   });
 
   it('redirects to StakeInputView on stake button click', () => {
-    render(StakingBalance);
+    const { getByText } = renderWithProvider(<StakingBalance />);
 
-    fireEvent.press(screen.getByText(strings('stake.stake_more')));
+    fireEvent.press(getByText(strings('stake.stake_more')));
 
     expect(mockNavigate).toHaveBeenCalledTimes(1);
     expect(mockNavigate).toHaveBeenCalledWith('StakeScreens', {
