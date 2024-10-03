48df91c91ba155a138fbe35942840314c9c7d20e
Amitabh Aggarwal <aggarwal.amitabh@gmail.com>
2024-10-03 16:22:43 -0500

    refactor: use InputDisplay component and input handling hooks to reduce code duplication

--- app/components/UI/Stake/Views/StakeInputView/StakeInputView.tsx
+++ app/components/UI/Stake/Views/StakeInputView/StakeInputView.tsx
@@ -1,62 +1,50 @@
 import { useNavigation } from '@react-navigation/native';
-import { BN } from 'ethereumjs-util';
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { View } from 'react-native';
-import { useSelector } from 'react-redux';
 import { strings } from '../../../../../../locales/i18n';
 import Button, {
   ButtonSize,
   ButtonVariants,
   ButtonWidthTypes,
 } from '../../../../../component-library/components/Buttons/Button';
-import Text, {
-  TextColor,
-  TextVariant,
-} from '../../../../../component-library/components/Texts/Text';
+import { TextVariant } from '../../../../../component-library/components/Texts/Text';
 import {
-  selectConversionRate,
-  selectCurrentCurrency,
-} from '../../../../../selectors/currencyRateController';
-import {
-  fiatNumberToWei,
-  fromTokenMinimalUnitString,
   limitToMaximumDecimalPlaces,
   renderFiat,
-  renderFromTokenMinimalUnit,
-  toWei,
-  weiToFiatNumber,
 } from '../../../../../util/number';
 import Keypad from '../../../../Base/Keypad';
 import { useStyles } from '../../../../hooks/useStyles';
 import { getStakingNavbar } from '../../../Navbar';
 import ScreenLayout from '../../../Ramp/components/ScreenLayout';
-import CurrencyToggle from '../../components/CurrencySwitch';
 import QuickAmounts from '../../components/QuickAmounts';
-import useBalance from '../../hooks/useBalance';
-import styleSheet from './StakeInputView.styles';
 import EstimatedAnnualRewardsCard from '../../components/EstimatedAnnualRewardsCard';
 import Routes from '../../../../../constants/navigation/Routes';
+import styleSheet from './StakeInputView.styles';
+import useStakingInputHandlers from '../../hooks/useStakingInput';
+import useBalance from '../../hooks/useBalance';
+import InputDisplay from '../../components/InputDisplay';
 
 const StakeInputView = () => {
   const title = strings('stake.stake_eth');
   const navigation = useNavigation();
   const { styles, theme } = useStyles(styleSheet, {});
-
-  const [amount, setAmount] = useState('0');
-  const [amountBN, setAmountBN] = useState<BN>(new BN(0));
-  const { balance, balanceBN, balanceFiatNumber } = useBalance();
   const [estimatedAnnualRewards, setEstimatedAnnualRewards] = useState('-');
+  const { balance, balanceFiatNumber, balanceBN } = useBalance();
 
-  const isNonZeroAmount = useMemo(() => amountBN.gt(new BN(0)), [amountBN]);
-  const isOverMaximum = useMemo(() => {
-    const additionalFundsRequired = amountBN.sub(balanceBN || new BN(0));
-    return isNonZeroAmount && additionalFundsRequired.gt(new BN(0));
-  }, [amountBN, balanceBN, isNonZeroAmount]);
-
-  const [fiatAmount, setFiatAmount] = useState('0');
-  const [isEth, setIsEth] = useState<boolean>(true);
-  const currentCurrency = useSelector(selectCurrentCurrency);
-  const conversionRate = useSelector(selectConversionRate) || 1;
+  const {
+    isEth,
+    currentCurrency,
+    isNonZeroAmount,
+    amount,
+    amountBN,
+    fiatAmount,
+    isOverMaximum,
+    handleCurrencySwitch,
+    currencyToggleValue,
+    percentageOptions,
+    handleAmountPress,
+    handleKeypadChange,
+  } = useStakingInputHandlers(balanceBN);
 
   const navigateToLearnMoreModal = () => {
     navigation.navigate('StakeModals', {
@@ -64,13 +52,21 @@ const StakeInputView = () => {
     });
   };
 
-  const balanceText = isEth
+  const handleStakePress = useCallback(() => {
+    // TODO: Display the Review bottom sheet: STAKE-824
+  }, []);
+
+  const balanceText = strings('stake.balance');
+
+  const buttonLabel = !isNonZeroAmount
+    ? strings('stake.enter_amount')
+    : isOverMaximum
+    ? strings('stake.not_enough_eth')
+    : strings('stake.review');
+
+  const balanceValue = isEth
     ? `${balance} ETH`
     : `${balanceFiatNumber?.toString()} ${currentCurrency.toUpperCase()}`;
-
-  const currencyToggleValue = isEth
-    ? `${fiatAmount} ${currentCurrency.toUpperCase()}`
-    : `${amount} ETH`;
 
   const annualRewardRate = '0.026'; //TODO: Replace with actual value: STAKE-806
   const calculateEstimatedAnnualRewards = useCallback(() => {
@@ -101,129 +97,22 @@ const StakeInputView = () => {
 
   useEffect(() => {
     calculateEstimatedAnnualRewards();
-  }, [
-    amount,
-    amountBN,
-    isEth,
-    conversionRate,
-    calculateEstimatedAnnualRewards,
-  ]);
-
-  const handleEthInput = useCallback(
-    (value: string) => {
-      setAmount(value);
-      setAmountBN(toWei(value, 'ether'));
-      const fiatValue = weiToFiatNumber(
-        toWei(value, 'ether'),
-        conversionRate,
-        2,
-      ).toString();
-      setFiatAmount(fiatValue);
-    },
-    [conversionRate],
-  );
-
-  const handleFiatInput = useCallback(
-    (value: string) => {
-      setFiatAmount(value);
-      const ethValue = renderFromTokenMinimalUnit(
-        fiatNumberToWei(value, conversionRate).toString(),
-        18,
-        5,
-      );
-
-      setAmount(ethValue);
-      setAmountBN(toWei(ethValue, 'ether'));
-    },
-    [conversionRate],
-  );
-
-  /* Keypad Handlers */
-  const handleKeypadChange = useCallback(
-    ({ value }) => {
-      isEth ? handleEthInput(value) : handleFiatInput(value);
-    },
-    [handleEthInput, handleFiatInput, isEth],
-  );
-
-  const handleCurrencySwitch = useCallback(() => {
-    setIsEth(!isEth);
-  }, [isEth]);
-
-  const handleStakePress = useCallback(() => {
-    // TODO: Display the Review bottom sheet: STAKE-824
-  }, []);
-
-  const percentageOptions = [
-    { value: 0.25, label: '25%' },
-    { value: 0.5, label: '50%' },
-    { value: 0.75, label: '75%' },
-    { value: 1, label: 'Max' },
-  ];
-
-  const handleAmountPress = useCallback(
-    ({ value }: { value: number }) => {
-      if (!balanceBN) return;
-      const percentage = value * 100;
-      const amountPercentage = balanceBN
-        ?.mul(new BN(percentage))
-        .div(new BN(100));
-
-      const newAmountString = fromTokenMinimalUnitString(
-        amountPercentage.toString(10),
-        18,
-      );
-      const newEthAmount = limitToMaximumDecimalPlaces(
-        Number(newAmountString),
-        5,
-      );
-      setAmount(newEthAmount);
-      setAmountBN(amountPercentage);
-
-      const newFiatAmount = weiToFiatNumber(
-        toWei(newEthAmount.toString(), 'ether'),
-        conversionRate,
-        2,
-      ).toString();
-      setFiatAmount(newFiatAmount);
-    },
-    [balanceBN, conversionRate],
-  );
+  }, [amount, amountBN, isEth, calculateEstimatedAnnualRewards]);
 
   return (
     <ScreenLayout style={styles.container}>
-      <View style={styles.inputContainer}>
-        <View>
-          {isOverMaximum ? (
-            <Text variant={TextVariant.BodySM} color={TextColor.Error}>
-              {strings('stake.not_enough_eth')}
-            </Text>
-          ) : (
-            <Text variant={TextVariant.BodySM}>
-              {strings('stake.balance')}
-              {': '}
-              {balanceText}
-            </Text>
-          )}
-        </View>
-        <View style={styles.amountRow}>
-          <Text
-            color={isNonZeroAmount ? TextColor.Default : TextColor.Muted}
-            variant={TextVariant.DisplayMD}
-          >
-            {isEth ? amount : fiatAmount}
-          </Text>
-          <Text color={TextColor.Muted} variant={TextVariant.DisplayMD}>
-            {isEth ? 'ETH' : currentCurrency.toUpperCase()}
-          </Text>
-        </View>
-        <View>
-          <CurrencyToggle
-            onPress={handleCurrencySwitch}
-            value={currencyToggleValue}
-          />
-        </View>
-      </View>
+      <InputDisplay
+        isOverMaximum={isOverMaximum}
+        balanceText={balanceText}
+        balanceValue={balanceValue}
+        isNonZeroAmount={isNonZeroAmount}
+        amount={amount}
+        fiatAmount={fiatAmount}
+        isEth={isEth}
+        currentCurrency={currentCurrency}
+        handleCurrencySwitch={handleCurrencySwitch}
+        currencyToggleValue={currencyToggleValue}
+      />
       <View style={styles.rewardsRateContainer}>
         <EstimatedAnnualRewardsCard
           estimatedAnnualRewards={estimatedAnnualRewards}
@@ -243,13 +132,7 @@ const StakeInputView = () => {
       />
       <View style={styles.reviewButtonContainer}>
         <Button
-          label={
-            !isNonZeroAmount
-              ? strings('stake.enter_amount')
-              : isOverMaximum
-              ? strings('stake.not_enough_eth')
-              : strings('stake.review')
-          }
+          label={buttonLabel}
           size={ButtonSize.Lg}
           labelTextVariant={TextVariant.BodyMDMedium}
           variant={ButtonVariants.Primary}
--- app/components/UI/Stake/Views/UnstakeInputView/UnstakeInputView.tsx
+++ app/components/UI/Stake/Views/UnstakeInputView/UnstakeInputView.tsx
@@ -1,196 +1,90 @@
 import { useNavigation } from '@react-navigation/native';
+import React, { useCallback, useEffect } from 'react';
 import { BN } from 'ethereumjs-util';
-import React, { useCallback, useEffect, useState } from 'react';
-import { View } from 'react-native';
-import { useSelector } from 'react-redux';
+import UnstakeInputViewBanner from './UnstakeBanner';
 import { strings } from '../../../../../../locales/i18n';
 import Button, {
   ButtonSize,
   ButtonVariants,
   ButtonWidthTypes,
 } from '../../../../../component-library/components/Buttons/Button';
-import Text, {
-  TextVariant,
-  TextColor,
-} from '../../../../../component-library/components/Texts/Text';
-import {
-  selectCurrentCurrency,
-  selectConversionRate,
-} from '../../../../../selectors/currencyRateController';
-import {
-  limitToMaximumDecimalPlaces,
-  toWei,
-  weiToFiatNumber,
-  renderFromTokenMinimalUnit,
-  fiatNumberToWei,
-  fromTokenMinimalUnitString,
-  renderFromWei,
-} from '../../../../../util/number';
+import { TextVariant } from '../../../../../component-library/components/Texts/Text';
+import { renderFromWei, weiToFiatNumber } from '../../../../../util/number';
 import Keypad from '../../../../Base/Keypad';
 import { useStyles } from '../../../../hooks/useStyles';
 import { getStakingNavbar } from '../../../Navbar';
 import ScreenLayout from '../../../Ramp/components/ScreenLayout';
-import CurrencyToggle from '../../components/CurrencySwitch';
 import QuickAmounts from '../../components/QuickAmounts';
+import { View } from 'react-native';
+import useStakingInputHandlers from '../../hooks/useStakingInput';
 import styleSheet from './UnstakeInputView.styles';
-import UnstakeInputViewBanner from './UnstakeBanner';
+import InputDisplay from '../../components/InputDisplay';
 
 const UnstakeInputView = () => {
   const title = strings('stake.unstake_eth');
   const navigation = useNavigation();
   const { styles, theme } = useStyles(styleSheet, {});
 
-  const [unstakeAmount, setUnstakeAmount] = useState('0');
-  const [unstakeAmountBN, setUnstakeAmountBN] = useState<BN>(new BN(0));
-
-  const [fiatAmount, setFiatAmount] = useState('0');
-  const [isEth, setIsEth] = useState<boolean>(true);
-  const currentCurrency = useSelector(selectCurrentCurrency);
-  const conversionRate = useSelector(selectConversionRate) || 1;
-
   const stakeBalance = '4599964000000000000'; //TODO: Replace with actual balance - STAKE-806
+
+  const {
+    isEth,
+    currentCurrency,
+    isNonZeroAmount,
+    amount,
+    fiatAmount,
+    isOverMaximum,
+    handleCurrencySwitch,
+    currencyToggleValue,
+    percentageOptions,
+    handleAmountPress,
+    handleKeypadChange,
+    conversionRate,
+  } = useStakingInputHandlers(new BN(stakeBalance));
+
   const stakeBalanceInEth = renderFromWei(stakeBalance, 5);
   const stakeBalanceFiatNumber = weiToFiatNumber(stakeBalance, conversionRate);
 
-  const isNonZeroAmount: boolean = unstakeAmountBN.gt(new BN(0));
-  const isOverMaximum: boolean =
-    isNonZeroAmount && unstakeAmountBN.sub(new BN(stakeBalance)).gt(new BN(0));
-
-  const stakedBalanceText = isEth
+  const stakedBalanceText = strings('stake.staked_balance');
+  const stakedBalanceValue = isEth
     ? `${stakeBalanceInEth} ETH`
     : `${stakeBalanceFiatNumber?.toString()} ${currentCurrency.toUpperCase()}`;
 
-  const currencyToggleValue = isEth
-    ? `${fiatAmount} ${currentCurrency.toUpperCase()}`
-    : `${unstakeAmount} ETH`;
+  const buttonLabel = !isNonZeroAmount
+    ? strings('stake.enter_amount')
+    : isOverMaximum
+    ? strings('stake.not_enough_eth')
+    : strings('stake.review');
 
   useEffect(() => {
     navigation.setOptions(getStakingNavbar(title, navigation, theme.colors));
   }, [navigation, theme.colors, title]);
 
-  const handleEthInput = useCallback(
-    (value: string) => {
-      setUnstakeAmount(value);
-      setUnstakeAmountBN(toWei(value, 'ether'));
-      const fiatValue = weiToFiatNumber(
-        toWei(value, 'ether'),
-        conversionRate,
-        2,
-      ).toString();
-      setFiatAmount(fiatValue);
-    },
-    [conversionRate],
-  );
-
-  const handleFiatInput = useCallback(
-    (value: string) => {
-      setFiatAmount(value);
-      const ethValue = renderFromTokenMinimalUnit(
-        fiatNumberToWei(value, conversionRate).toString(),
-        18,
-        5,
-      );
-
-      setUnstakeAmount(ethValue);
-      setUnstakeAmountBN(toWei(ethValue, 'ether'));
-    },
-    [conversionRate],
-  );
-
-  /* Keypad Handlers */
-  const handleKeypadChange = useCallback(
-    ({ value }) => {
-      isEth ? handleEthInput(value) : handleFiatInput(value);
-    },
-    [handleEthInput, handleFiatInput, isEth],
-  );
-
-  const handleCurrencySwitch = useCallback(() => {
-    setIsEth(!isEth);
-  }, [isEth]);
-
-  const handleStakePress = useCallback(() => {
+  const handleUnstakePress = useCallback(() => {
     // TODO: Display the Review bottom sheet: STAKE-841
   }, []);
 
-  const percentageOptions = [
-    { value: 0.25, label: '25%' },
-    { value: 0.5, label: '50%' },
-    { value: 0.75, label: '75%' },
-    { value: 1, label: 'Max' },
-  ];
-
-  const handleAmountPress = useCallback(
-    ({ value }: { value: number }) => {
-      const stakeBalanceBN = new BN(stakeBalance);
-      if (!stakeBalanceBN) return;
-      const percentage = value * 100;
-      const amountPercentage = stakeBalanceBN
-        ?.mul(new BN(percentage))
-        .div(new BN(100));
-
-      const newAmountString = fromTokenMinimalUnitString(
-        amountPercentage.toString(10),
-        18,
-      );
-      const newEthAmount = limitToMaximumDecimalPlaces(
-        Number(newAmountString),
-        5,
-      );
-      setUnstakeAmount(newEthAmount);
-      setUnstakeAmountBN(amountPercentage);
-
-      const newFiatAmount = weiToFiatNumber(
-        toWei(newEthAmount.toString(), 'ether'),
-        conversionRate,
-        2,
-      ).toString();
-      setFiatAmount(newFiatAmount);
-    },
-    [conversionRate, stakeBalance],
-  );
-
   return (
     <ScreenLayout style={styles.container}>
-      <View style={styles.inputContainer}>
-        <View>
-          {isOverMaximum ? (
-            <Text variant={TextVariant.BodySM} color={TextColor.Error}>
-              {strings('stake.not_enough_eth')}
-            </Text>
-          ) : (
-            <Text variant={TextVariant.BodySM}>
-              {strings('stake.staked_balance')}
-              {': '}
-              {stakedBalanceText}
-            </Text>
-          )}
-        </View>
-        <View style={styles.amountRow}>
-          <Text
-            color={isNonZeroAmount ? TextColor.Default : TextColor.Muted}
-            variant={TextVariant.DisplayMD}
-          >
-            {isEth ? unstakeAmount : fiatAmount}
-          </Text>
-          <Text color={TextColor.Muted} variant={TextVariant.DisplayMD}>
-            {isEth ? 'ETH' : currentCurrency.toUpperCase()}
-          </Text>
-        </View>
-        <View>
-          <CurrencyToggle
-            onPress={handleCurrencySwitch}
-            value={currencyToggleValue}
-          />
-        </View>
-      </View>
+      <InputDisplay
+        isOverMaximum={isOverMaximum}
+        balanceText={stakedBalanceText}
+        balanceValue={stakedBalanceValue}
+        isNonZeroAmount={isNonZeroAmount}
+        amount={amount}
+        fiatAmount={fiatAmount}
+        isEth={isEth}
+        currentCurrency={currentCurrency}
+        handleCurrencySwitch={handleCurrencySwitch}
+        currencyToggleValue={currencyToggleValue}
+      />
       <UnstakeInputViewBanner style={styles.unstakeBanner} />
       <QuickAmounts
         amounts={percentageOptions}
         onAmountPress={handleAmountPress}
       />
       <Keypad
-        value={isEth ? unstakeAmount : fiatAmount}
+        value={isEth ? amount : fiatAmount}
         onChange={handleKeypadChange}
         style={styles.keypad}
         currency={'ETH'}
@@ -198,19 +92,13 @@ const UnstakeInputView = () => {
       />
       <View style={styles.reviewButtonContainer}>
         <Button
-          label={
-            !isNonZeroAmount
-              ? strings('stake.enter_amount')
-              : isOverMaximum
-              ? strings('stake.not_enough_eth')
-              : strings('stake.review')
-          }
+          label={buttonLabel}
           size={ButtonSize.Lg}
           labelTextVariant={TextVariant.BodyMDMedium}
           variant={ButtonVariants.Primary}
           isDisabled={isOverMaximum || !isNonZeroAmount}
           width={ButtonWidthTypes.Full}
-          onPress={handleStakePress}
+          onPress={handleUnstakePress}
         />
       </View>
     </ScreenLayout>
