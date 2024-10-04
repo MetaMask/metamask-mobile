b3befd8200d581ba26203f3242a35d752ce1fbe4
Nick Gambino <35090461+gambinish@users.noreply.github.com>
2024-10-03 22:47:36 -0700

    chore: Componetize `Tokens` screen (#11471)
    ## **Description**
    
    **Proposal**: Break up monolithic `Tokens/index.tsx` file into smaller,
    more maintainable components.
    
    As I began working on my [Token sorting
    task](https://consensyssoftware.atlassian.net/browse/MMASSETS-357), I
    noticed that the `Tokens` screen code structure felt overly flat, which
    led to some challenges navigating through the logic (~800 lines). The
    reliance on multiple internal callbacks created lots of complexity,
    making it difficult to reason through the flow, and unclear where to
    reliably and maintainably introduce my sorting logic.
    
    In this PR, I’m proposing a refactor of the screen by breaking it down
    into smaller, more manageable components, with a utility directory to
    handle non TSX related helpers (like sorting or handling balances). So,
    instead of relying heavily on internal callbacks, we can just pass props
    and utilize selectors where needed for each component’s specific
    function.
    
    While this may be a matter of personal preference, I believe that an
    over-reliance on internal callbacks can often result in a bit of an
    anti-pattern. I feel that this refactor will make these components more
    straightforward, reliable, and easier to maintain. Is there a reason why
    we wouldn't want to go with this approach instead?
    
    I also realize that we have other tasks in-flight and I don't want to be
    a blocker, but I feel pretty strongly that this refactor will remove
    lots of technical debt and set us up really well for the long run.
    
    Note: This isn't quite complete, and you'll notice a few areas where I
    can do things like tighten up TypeScript types. If you folks are fine
    with this approach, I can go ahead and polish this.
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to tokens page, ensure that all functionality remains unchanged.
    
    ## **Screenshots/Recordings**
    
    
    https://github.com/user-attachments/assets/3995a93b-8b8e-441c-902e-39b48fb9b76e
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Nico MASSART <NicolasMassart@users.noreply.github.com>

---
+++ app/components/UI/Tokens/util/sortAssets.ts
@@ -0,0 +1,81 @@
+import { get } from 'lodash';
+
+export type SortOrder = 'asc' | 'dsc';
+export interface SortCriteria {
+  key: string;
+  order?: 'asc' | 'dsc';
+  sortCallback: SortCallbackKeys;
+}
+
+export type SortingType = number | string | Date;
+type SortCallbackKeys = keyof SortingCallbacksT;
+
+export interface SortingCallbacksT {
+  numeric: (a: number, b: number) => number;
+  stringNumeric: (a: string, b: string) => number;
+  alphaNumeric: (a: string, b: string) => number;
+  date: (a: Date, b: Date) => number;
+}
+
+// All sortingCallbacks should be asc order, sortAssets function handles asc/dsc
+const sortingCallbacks: SortingCallbacksT = {
+  numeric: (a: number, b: number) => a - b,
+  stringNumeric: (a: string, b: string) => parseInt(a, 10) - parseInt(b, 10),
+  alphaNumeric: (a: string, b: string) => a.localeCompare(b),
+  date: (a: Date, b: Date) => a.getTime() - b.getTime(),
+};
+
+// Utility function to access nested properties by key path
+function getNestedValue<T>(obj: T, keyPath: string): SortingType {
+  return get(obj, keyPath) as SortingType;
+}
+
+export function sortAssets<T>(array: T[], criteria: SortCriteria): T[] {
+  const { key, order = 'asc', sortCallback } = criteria;
+
+  return [...array].sort((a, b) => {
+    const aValue = getNestedValue(a, key);
+    const bValue = getNestedValue(b, key);
+
+    // Always move undefined values to the end, regardless of sort order
+    if (aValue === undefined) {
+      return 1;
+    }
+
+    if (bValue === undefined) {
+      return -1;
+    }
+
+    let comparison: number;
+
+    switch (sortCallback) {
+      case 'stringNumeric':
+      case 'alphaNumeric':
+        comparison = sortingCallbacks[sortCallback](
+          aValue as string,
+          bValue as string,
+        );
+        break;
+      case 'numeric':
+        comparison = sortingCallbacks.numeric(
+          aValue as number,
+          bValue as number,
+        );
+        break;
+      case 'date':
+        comparison = sortingCallbacks.date(aValue as Date, bValue as Date);
+        break;
+      default:
+        if (aValue < bValue) {
+          comparison = -1;
+        } else if (aValue > bValue) {
+          comparison = 1;
+        } else {
+          comparison = 0;
+        }
+    }
+
+    // Modify to sort in ascending or descending order
+    return order === 'asc' ? comparison : -comparison;
+  });
+}
