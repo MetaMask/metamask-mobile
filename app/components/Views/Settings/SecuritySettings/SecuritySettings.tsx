bdc9043a016ca5abdb5f8afc2650188356f61409
Vince Howard <vincenguyenhoward@gmail.com>
2024-10-04 08:18:28 -0600

    refactor(1702-3): batch account balance component  (#11368)
    ## **Description**
    
    This PR is part of a series that breaks down the original [large PR
    #11127](https://github.com/MetaMask/metamask-mobile/pull/11127) into
    smaller, more manageable chunks.
    
    The `BatchAccountBalance` functionality is valuable beyond its original
    implementation. By converting it into a reusable component, we can:
    1. Improve code maintainability
    2. Reduce duplication
    3. Enhance consistency across the application and ease of use
    
    ### Changes
    - Extracted `BatchAccountBalance` logic from its original location
    `SecuritySettings` and into a separate component file
    - Included new tests
    - Updated the `SecuritySettings` to use the new component
    - Removed related tests/constants within `SecuritySettings` and moved
    them into the new component folder
    
    ### Impact
    This refactored component will be used in:
    - Its current original screen which is `SecuritySettings`
    - New screens coming from the enhanced onboarding settings
    [#1702](https://github.com/MetaMask/mobile-planning/issues/1702) that is
    currently under progress
    
    ## **Related issues**
    
    Feature:
    [#1702](https://github.com/MetaMask/mobile-planning/issues/1702)
    
    ## **Manual testing steps**
    
    1. Make sure to have at least 2 accounts with a balance
    2. On the wallet home screen click on the accounts drop down and you
    will see a bottom sheet modal appear with your account names as well as
    a balance for all of your accounts
    3. Goto the security settings screen, find the "Batch account balance
    requests" section, and tap to toggle off
    4. Go back to the wallet home screen and click on the account picker
    once again
    5. Your active account should show your balance in the bottom sheet
    however inactive account balances will not be shown
    6. Repeat steps to toggle on/off to see your inactive balance appear and
    disappear
    
    ## **Screenshots/Recordings**
    
    
    ![batch_account](https://github.com/user-attachments/assets/769a012e-f8ff-47da-baf4-dbc7d797dba3)
    
    ### **Before**
    
    NA
    
    ### **After**
    
    NA
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/Views/Settings/SecuritySettings/SecuritySettings.tsx
+++ app/components/Views/Settings/SecuritySettings/SecuritySettings.tsx
@@ -7,7 +7,6 @@ import {
   View,
   ActivityIndicator,
   Keyboard,
-  Platform,
   Linking,
 } from 'react-native';
 import StorageWrapper from '../../../../store/storage-wrapper';
@@ -56,18 +55,13 @@ import {
 import {
   selectIpfsGateway,
   selectIsIpfsGatewayEnabled,
-  selectIsMultiAccountBalancesEnabled,
   selectDisplayNftMedia,
   selectShowIncomingTransactionNetworks,
   selectShowTestNetworks,
   selectUseSafeChainsListValidation,
   selectUseTransactionSimulations,
 } from '../../../../selectors/preferencesController';
-import {
-  SECURITY_PRIVACY_MULTI_ACCOUNT_BALANCES_TOGGLE_ID,
-  SECURITY_PRIVACY_VIEW_ID,
-} from '../../../../../wdio/screen-objects/testIDs/Screens/SecurityPrivacy.testIds';
-import generateTestId from '../../../../../wdio/utils/generateTestId';
+import { SECURITY_PRIVACY_VIEW_ID } from '../../../../../wdio/screen-objects/testIDs/Screens/SecurityPrivacy.testIds';
 import ipfsGateways from '../../../../util/ipfs-gateways.json';
 import SelectComponent from '../../../UI/SelectComponent';
 import { timeoutFetch } from '../../../../util/general';
@@ -81,7 +75,6 @@ import {
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import { useParams } from '../../../../util/navigation/navUtils';
 import {
-  BATCH_BALANCE_REQUESTS_SECTION,
   BIOMETRY_CHOICE_STRING,
   CLEAR_BROWSER_HISTORY_SECTION,
   DISPLAY_SAFE_CHAINS_LIST_VALIDATION,
@@ -129,6 +122,7 @@ import { RootState } from '../../../../reducers';
 import { EtherscanSupportedHexChainId } from '@metamask/preferences-controller';
 import { useDisableNotifications } from '../../../../util/notifications/hooks/useNotifications';
 import { isNotificationsFeatureEnabled } from '../../../../util/notifications';
+import BatchAccountBalanceSettings from '../BatchAccountBalanceSettings';
 import AutoDetectNFTSettings from '../../Settings/AutoDetectNFTSettings';
 
 const Heading: React.FC<HeadingProps> = ({ children, first }) => {
@@ -207,9 +201,6 @@ const Settings: React.FC = () => {
     (state: any) => state.user.seedphraseBackedUp,
   );
   const type = useSelector(selectProviderType);
-  const isMultiAccountBalancesEnabled = useSelector(
-    selectIsMultiAccountBalancesEnabled,
-  );
   const ipfsGateway = useSelector(selectIpfsGateway);
   const isIpfsGatewayEnabled = useSelector(selectIsIpfsGatewayEnabled);
   const myNetworks = ETHERSCAN_SUPPORTED_NETWORKS;
@@ -499,48 +490,6 @@ const Settings: React.FC = () => {
     </View>
   );
 
-  const toggleIsMultiAccountBalancesEnabled = (
-    multiAccountBalancesEnabled: boolean,
-  ) => {
-    const { PreferencesController } = Engine.context;
-    PreferencesController.setIsMultiAccountBalancesEnabled(
-      multiAccountBalancesEnabled,
-    );
-  };
-
-  const renderMultiAccountBalancesSection = () => (
-    <View style={styles.halfSetting} testID={BATCH_BALANCE_REQUESTS_SECTION}>
-      <View style={styles.titleContainer}>
-        <Text variant={TextVariant.BodyLGMedium} style={styles.title}>
-          {strings('app_settings.batch_balance_requests_title')}
-        </Text>
-        <View style={styles.switchElement}>
-          <Switch
-            value={isMultiAccountBalancesEnabled}
-            onValueChange={toggleIsMultiAccountBalancesEnabled}
-            trackColor={{
-              true: colors.primary.default,
-              false: colors.border.muted,
-            }}
-            thumbColor={theme.brandColors.white}
-            style={styles.switch}
-            ios_backgroundColor={colors.border.muted}
-            {...generateTestId(
-              Platform,
-              SECURITY_PRIVACY_MULTI_ACCOUNT_BALANCES_TOGGLE_ID,
-            )}
-          />
-        </View>
-      </View>
-      <Text
-        variant={TextVariant.BodyMD}
-        color={TextColor.Alternative}
-        style={styles.desc}
-      >
-        {strings('app_settings.batch_balance_requests_description')}
-      </Text>
-    </View>
-  );
   const toggleEnableIncomingTransactions = (
     hexChainId: EtherscanSupportedHexChainId,
     value: boolean,
@@ -1081,7 +1030,7 @@ const Settings: React.FC = () => {
         >
           {strings('app_settings.transactions_subheading')}
         </Text>
-        {renderMultiAccountBalancesSection()}
+        <BatchAccountBalanceSettings />
         {renderShowIncomingTransactions()}
         {renderHistoryModal()}
         {renderUseTransactionSimulations()}
