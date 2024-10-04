2a38e20d5fde6a28a02ca0acc49520164c835ef2
Matthew Grainger <matthewgrainger33@gmail.com>
2024-10-04 12:49:01 -0400

    feat: removed tooltip from estimated changes in YouReceiveCard

--- app/components/UI/Stake/components/Confirmation/YouReceiveCard/YouReceiveCard.tsx
+++ app/components/UI/Stake/components/Confirmation/YouReceiveCard/YouReceiveCard.tsx
@@ -4,16 +4,10 @@ import TagBase, {
   TagSeverity,
   TagShape,
 } from '../../../../../../component-library/base-components/TagBase';
-import { TooltipSizes } from '../../../../../../component-library/components-temp/KeyValueRow';
 import Avatar, {
   AvatarVariant,
   AvatarSize,
 } from '../../../../../../component-library/components/Avatars/Avatar';
-import ButtonIcon from '../../../../../../component-library/components/Buttons/ButtonIcon';
-import {
-  IconColor,
-  IconName,
-} from '../../../../../../component-library/components/Icons/Icon';
 import Text, {
   TextVariant,
   TextColor,
@@ -21,7 +15,6 @@ import Text, {
 import Card from '../../../../../../component-library/components/Cards/Card';
 import { renderFromWei } from '../../../../../../util/number';
 import { useStyles } from '../../../../../hooks/useStyles';
-import useTooltipModal from '../../../../../hooks/useTooltipModal';
 import styleSheet from './YouReceiveCard.styles';
 import ethLogo from '../../../../../../images/ethereum.png';
 import { YouReceiveCardProps } from './YouReceiveCard.types';
@@ -30,30 +23,12 @@ import { strings } from '../../../../../../../locales/i18n';
 const YouReceiveCard = ({ wei, fiat }: YouReceiveCardProps) => {
   const { styles } = useStyles(styleSheet, {});
 
-  const { openTooltipModal } = useTooltipModal();
-
-  const handleDisplayEstimatedChangesTooltip = () =>
-    openTooltipModal(
-      'TODO',
-      'Aute commodo incididunt culpa aliquip adipisicing cupidatat veniam culpa veniam officia dolor. Consectetur elit ut adipisicing esse nisi duis dolor.',
-    );
-
   return (
     <Card style={styles.changesCard} disabled>
       <View style={styles.estimatedChangesWrapper}>
         <Text variant={TextVariant.BodyMDMedium}>
           {strings('stake.estimated_changes')}
         </Text>
-        <ButtonIcon
-          size={TooltipSizes.Sm}
-          iconColor={IconColor.Muted}
-          iconName={IconName.Question}
-          accessibilityRole="button"
-          accessibilityLabel={strings(
-            'stake.accessibility_labels.unstake_estimated_changes',
-          )}
-          onPress={handleDisplayEstimatedChangesTooltip}
-        />
       </View>
       <View style={styles.youReceiveWrapper}>
         <Text variant={TextVariant.BodyMDMedium}>
