cfc0143e6303388a10f78a0a141a002866f886fd
tommasini <tommasini15@gmail.com>
2024-10-07 18:36:35 +0100

    sentry tags completed without the tag for opening by data, if it was deeplink or tap

--- app/util/sentry/tags/index.ts
+++ app/util/sentry/tags/index.ts
@@ -4,29 +4,27 @@ import { selectInternalAccounts } from '../../../selectors/accountsController';
 import { selectAllTokensFlat } from '../../../selectors/tokensController';
 import { getNotificationsList } from '../../../selectors/notifications';
 import { selectTransactions } from '../../../selectors/transactionController';
+import { selectPendingApprovals } from '../../../selectors/approvalController';
 
 export function getTraceTags(state: RootState) {
+  // if it's cold app start or warm app start
   // Replace this to say how metamask was opened, deeplink, wallet connect, sdk, or just tap
   //const uiType = getEnvironmentType();
   const unlocked = state.user.userLoggedIn;
   const accountCount = selectInternalAccounts(state).length;
   const nftCount = selectAllNftsFlat(state).length;
   const notificationCount = getNotificationsList(state).length;
-  const tokenCount = selectAllTokensFlat(state).length as number;
+  const tokenCount = selectAllTokensFlat(state).length;
   const transactionCount = selectTransactions(state).length;
+  const pendingApprovals = Object.values(selectPendingApprovals(state));
 
-  /* Understand the pending approvals and implement those how mobile does it 
-  const pendingApprovals = getPendingApprovals(
-    state as unknown as ApprovalsMetaMaskState,
-  ); */
-
-  //const firstApprovalType = pendingApprovals?.[0]?.type;
+  const firstApprovalType = pendingApprovals?.[0]?.type;
 
   return {
     'wallet.account_count': accountCount,
     'wallet.nft_count': nftCount,
     'wallet.notification_count': notificationCount,
-    'wallet.pending_approval': null, //firstApprovalType,
+    'wallet.pending_approval': firstApprovalType,
     'wallet.token_count': tokenCount,
     'wallet.transaction_count': transactionCount,
     'wallet.unlocked': unlocked,
