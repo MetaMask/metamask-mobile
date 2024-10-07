6be2bd56311fb915e75b34c057709786b974fe99
Jyoti Puri <jyotipuri@gmail.com>
2024-10-07 17:17:27 +0530

    feat: adding a basic personal sign page re-designed (#11602)
    ## **Description**
    
    Re-designed personal sign page
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-mobile/issues/11529
    
    ## **Manual testing steps**
    
    1. Enable re-designed confirmations locally
    2. To to test dapp
    3. Submit personal sign request and check page that appears
    
    ## **Screenshots/Recordings**
    <img width="407" alt="Screenshot 2024-10-03 at 8 05 44 PM"
    src="https://github.com/user-attachments/assets/0bbf0562-26e1-47ce-a2aa-0a4abd81119c">
    
    ## **Pre-merge author checklist**
    
    - [X] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [X] I've completed the PR template to the best of my ability
    - [X] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [X] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/Views/confirmations/components/UI/Tooltip/Tooltip.tsx
+++ app/components/Views/confirmations/components/UI/Tooltip/Tooltip.tsx
@@ -1,23 +1,27 @@
 import React, { ReactNode, useState } from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
+import Modal from 'react-native-modal';
 
 import ButtonIcon, {
   ButtonIconSizes,
 } from '../../../../../../component-library/components/Buttons/ButtonIcon';
-import InfoModal from '../../../../../../components/UI/Swaps/components/InfoModal';
 import {
   IconColor,
   IconName,
 } from '../../../../../../component-library/components/Icons/Icon';
+import { useTheme } from '../../../../../../util/theme';
+import createStyles from './style';
 
 interface TooltipProps {
   content: ReactNode;
   title?: string;
-  testID?: string;
+  tooltipTestId?: string;
 }
 
-const Tooltip = ({ content, title, testID }: TooltipProps) => {
+const Tooltip = ({ content, title, tooltipTestId }: TooltipProps) => {
   const [open, setOpen] = useState(false);
+  const { colors, shadows } = useTheme();
+  const styles = createStyles(colors, shadows);
 
   return (
     <View>
@@ -26,16 +30,32 @@ const Tooltip = ({ content, title, testID }: TooltipProps) => {
         iconName={IconName.Info}
         onPress={() => setOpen(true)}
         size={ButtonIconSizes.Sm}
-        testID={testID ?? 'tooltipTestId'}
+        testID={tooltipTestId ?? 'tooltipTestId'}
       />
-      {open && (
-        <InfoModal
-          body={content}
-          isVisible={open}
-          title={title}
-          toggleModal={() => setOpen(false)}
-        />
-      )}
+      <Modal
+        isVisible={open}
+        onBackdropPress={() => setOpen(false)}
+        onBackButtonPress={() => setOpen(false)}
+        onSwipeComplete={() => setOpen(false)}
+        swipeDirection={'down'}
+        style={styles.modal}
+        propagateSwipe
+        backdropColor={colors.overlay.default}
+        backdropOpacity={1}
+      >
+        <View style={styles.modalView}>
+          <ButtonIcon
+            iconColor={IconColor.Default}
+            iconName={IconName.Close}
+            onPress={() => setOpen(false)}
+            size={ButtonIconSizes.Md}
+            style={styles.closeModalBtn}
+            testID={tooltipTestId ?? 'tooltipTestId'}
+          />
+            {title && <Text style={styles.modalTitle}>{title}</Text>}
+            <Text>{content}</Text>
+        </View>
+      </Modal>
     </View>
   );
 };
