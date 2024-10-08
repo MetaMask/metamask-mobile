0d25e66a2251194473c45fbcab753fdad0249f88
Daniel-Cross <hello@danielcross.dev>
2024-10-08 09:44:11 +0200

    moved styles out of file

--- app/components/UI/EditGasFee1559/index.js
+++ app/components/UI/EditGasFee1559/index.js
@@ -37,115 +37,7 @@ import {
 } from '../../../util/gasUtils';
 import { useMetrics } from '../../../components/hooks/useMetrics';
 import { getAnalyticsParams } from './utils';
-
-const createStyles = (colors) =>
-  StyleSheet.create({
-    root: {
-      backgroundColor: colors.background.default,
-      borderTopLeftRadius: 20,
-      borderTopRightRadius: 20,
-      minHeight: 200,
-      maxHeight: '95%',
-      paddingTop: 24,
-      paddingBottom: Device.isIphoneX() ? 32 : 24,
-    },
-    wrapper: {
-      paddingHorizontal: 24,
-    },
-    customGasHeader: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      width: '100%',
-      paddingBottom: 20,
-    },
-    newGasFeeHeader: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      width: '100%',
-      justifyContent: 'center',
-    },
-    headerContainer: {
-      alignItems: 'center',
-      marginBottom: 22,
-    },
-    headerText: {
-      fontSize: 48,
-      flex: 1,
-      textAlign: 'center',
-    },
-    headerTitle: {
-      flexDirection: 'row',
-    },
-    saveButton: {
-      marginBottom: 20,
-    },
-    labelTextContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-    },
-    hitSlop: {
-      top: 10,
-      left: 10,
-      bottom: 10,
-      right: 10,
-    },
-    labelInfo: {
-      color: colors.text.muted,
-    },
-    advancedOptionsContainer: {
-      marginTop: 25,
-      marginBottom: 30,
-    },
-    advancedOptionsInputsContainer: {
-      marginTop: 14,
-    },
-    rangeInputContainer: {
-      marginBottom: 20,
-    },
-    advancedOptionsButton: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    advancedOptionsIcon: {
-      paddingTop: 1,
-      marginLeft: 5,
-    },
-    learnMoreLabels: {
-      marginTop: 9,
-    },
-    /* Add when the learn more link is ready
-  learnMoreLink: {
-    marginTop: 14
-  },*/
-    warningTextContainer: {
-      lineHeight: 20,
-      paddingLeft: 4,
-      flex: 1,
-    },
-    warningText: {
-      lineHeight: 20,
-      flex: 1,
-      color: colors.text.default,
-    },
-    warningContainer: {
-      marginBottom: 20,
-    },
-    dappEditGasContainer: {
-      marginVertical: 20,
-    },
-    subheader: {
-      marginBottom: 6,
-    },
-    learnMoreModal: {
-      maxHeight: Device.getDeviceHeight() * 0.7,
-    },
-    redInfo: {
-      marginLeft: 2,
-      color: colors.error.default,
-    },
-  });
+import { editGasFee1559Styles } from './styles';
 
 /**
  * The EditGasFee1559 component will be deprecated in favor of EditGasFee1559Update as part of the gas polling refactor code that moves gas fee modifications to `app/core/GasPolling`. When the refactoring is completed, the EditGasFee1559Update will be renamed EditGasFee1559 and this component will be removed. The EditGasFee1559Update is currently being used in the Update Transaction(Speed Up/Cancel) flow.
@@ -203,7 +95,7 @@ const EditGasFee1559 = ({
   const { colors } = useTheme();
   const { trackEvent } = useMetrics();
 
-  const styles = createStyles(colors);
+  const styles = editGasFee1559Styles(colors);
 
   const toggleAdvancedOptions = () => {
     if (!showAdvancedOptions) {
--- app/components/UI/EditGasFeeLegacy/index.js
+++ app/components/UI/EditGasFeeLegacy/index.js
@@ -3,7 +3,6 @@
 import React, { useState } from 'react';
 import {
   View,
-  StyleSheet,
   TouchableOpacity,
   ScrollView,
   TouchableWithoutFeedback,
@@ -20,10 +19,10 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import { strings } from '../../../../locales/i18n';
 import Alert, { AlertType } from '../../Base/Alert';
 import HorizontalSelector from '../../Base/HorizontalSelector';
-import Device from '../../../util/device';
 import { isMainnetByChainId } from '../../../util/networks';
 import FadeAnimationView from '../FadeAnimationView';
 import { MetaMetricsEvents } from '../../../core/Analytics';
+import { editGasFeeLegacyStyles } from '../EditGasFee1559/styles';
 
 import AppConstants from '../../../core/AppConstants';
 import { useTheme } from '../../../util/theme';
@@ -35,83 +34,6 @@ import {
 } from '../../../util/gasUtils';
 import { useMetrics } from '../../../components/hooks/useMetrics';
 import { getAnalyticsParams } from '../EditGasFee1559/utils';
-
-const createStyles = (colors) =>
-  StyleSheet.create({
-    root: {
-      backgroundColor: colors.background.default,
-      borderTopLeftRadius: 20,
-      borderTopRightRadius: 20,
-      minHeight: 200,
-      maxHeight: '95%',
-      paddingTop: 24,
-      paddingBottom: Device.isIphoneX() ? 32 : 24,
-    },
-    wrapper: {
-      paddingHorizontal: 24,
-    },
-    customGasHeader: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      width: '100%',
-      paddingBottom: 20,
-    },
-    headerContainer: {
-      alignItems: 'center',
-      marginBottom: 22,
-    },
-    headerText: {
-      fontSize: 48,
-    },
-    headerTitle: {
-      flexDirection: 'row',
-    },
-    headerTitleSide: {
-      flex: 1,
-    },
-    labelTextContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-    },
-    hitSlop: {
-      top: 10,
-      left: 10,
-      bottom: 10,
-      right: 10,
-    },
-    labelInfo: {
-      color: colors.text.muted,
-    },
-    advancedOptionsContainer: {
-      marginTop: 25,
-      marginBottom: 30,
-    },
-    advancedOptionsInputsContainer: {
-      marginTop: 14,
-    },
-    rangeInputContainer: {
-      marginBottom: 20,
-    },
-    advancedOptionsButton: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    advancedOptionsIcon: {
-      paddingTop: 1,
-      marginLeft: 5,
-    },
-    warningTextContainer: {
-      paddingLeft: 4,
-      lineHeight: 20,
-      textAlign: 'center',
-    },
-    warningText: {
-      lineHeight: 20,
-      color: colors.text.default,
-    },
-  });
 
 /**
  * The EditGasFeeLegacy component will be deprecated in favor of EditGasFeeLegacyUpdate as part of the gas polling refactor code that moves gas fee modifications to `app/core/GasPolling`. When the refactoring is completed, the EditGasFeeLegacyUpdate will be renamed EditGasFeeLegacy and this component will be removed. The EditGasFeeLegacyUpdate is currently being used in the Update Transaction(Speed Up/Cancel) flow.
@@ -151,7 +73,7 @@ const EditGasFeeLegacy = ({
   const [gasPriceError, setGasPriceError] = useState();
   const { colors } = useTheme();
   const { trackEvent } = useMetrics();
-  const styles = createStyles(colors);
+  const styles = editGasFeeLegacyStyles(colors);
 
   const toggleAdvancedOptions = () => {
     if (!showAdvancedOptions) {
