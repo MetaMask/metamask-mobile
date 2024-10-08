4e71a3c1bca11ed3137c91b133075f521807c0df
tommasini <tommasini15@gmail.com>
2024-10-08 21:46:05 +0100

    add tests for getItem and setItem

---
+++ app/store/storage-wrapper.test.ts
@@ -0,0 +1,17 @@
+import StorageWrapper from './storage-wrapper';
+
+describe('StorageWrapper', () => {
+  it('should return the value from Storage Wrapper', async () => {
+    const setItemSpy = jest.spyOn(StorageWrapper, 'setItem');
+    const getItemSpy = jest.spyOn(StorageWrapper, 'getItem');
+
+    await StorageWrapper.setItem('test-key', 'test-value');
+    getItemSpy.mockResolvedValue('test-value');
+
+    const result = await StorageWrapper.getItem('test-key');
+
+    expect(setItemSpy).toHaveBeenCalledWith('test-key', 'test-value');
+    expect(getItemSpy).toHaveBeenCalledWith('test-key');
+    expect(result).toBe('test-value');
+  });
+});
