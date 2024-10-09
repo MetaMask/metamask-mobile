cc80fefd70b1862bd2623ed365d7e6eb04d76f8d
Owen Craston <owengc12@gmail.com>
2024-10-09 18:02:38 -0400

    add missing code fences

--- app/core/Engine.ts
+++ app/core/Engine.ts
@@ -2293,9 +2293,10 @@ export default {
     assertEngineExists(instance);
     return instance.getGlobalEthQuery();
   },
-
+  ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
   getSnapKeyring: () => {
     assertEngineExists(instance);
     return instance.getSnapKeyring();
   },
+  ///: END:ONLY_INCLUDE_IF
 };
