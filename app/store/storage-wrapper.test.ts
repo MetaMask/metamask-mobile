2b662aa9b58e2a367117ffd93e62c6ec34f3d0b3
tommasini <tommasini15@gmail.com>
2024-10-09 09:23:21 +0100

    add test for setItem error scenario

--- app/store/storage-wrapper.test.ts
+++ app/store/storage-wrapper.test.ts
@@ -1,7 +1,7 @@
 import StorageWrapper from './storage-wrapper';
 
 describe('StorageWrapper', () => {
-  it('should return the value from Storage Wrapper', async () => {
+  it('return the value from Storage Wrapper', async () => {
     const setItemSpy = jest.spyOn(StorageWrapper, 'setItem');
     const getItemSpy = jest.spyOn(StorageWrapper, 'getItem');
 
@@ -13,5 +13,21 @@ describe('StorageWrapper', () => {
     expect(setItemSpy).toHaveBeenCalledWith('test-key', 'test-value');
     expect(getItemSpy).toHaveBeenCalledWith('test-key');
     expect(result).toBe('test-value');
+  });
+  it('throw an error when setItem do not receives a string', async () => {
+    const setItemSpy = jest.spyOn(StorageWrapper, 'setItem');
+
+    try {
+      //@ts-expect-error - Expected to test non string scenario
+      await StorageWrapper.setItem('test-key', 123);
+    } catch (error) {
+      const e = error as unknown as Error;
+      expect(e).toBeInstanceOf(Error);
+      expect(e.message).toBe(
+        'MMKV value must be a string, received value 123 for key test-key',
+      );
+    }
+
+    expect(setItemSpy).toHaveBeenCalledWith('test-key', 123);
   });
 });
