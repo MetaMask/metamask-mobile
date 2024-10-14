f2aa755b2fc5a9ac801cb8a69dfb17767df60bed
JSoufer <jonathan.ferreira@consensys.net>
2024-10-14 15:46:53 +0100

    use string due icompatibility

--- app/util/notifications/androidChannels.ts
+++ app/util/notifications/androidChannels.ts
@@ -1,31 +1,26 @@
 import { AndroidChannel, AndroidImportance } from '@notifee/react-native';
 
-export enum ChannelId {
-  DEFAULT_NOTIFICATION_CHANNEL_ID = 'DEFAULT_NOTIFICATION_CHANNEL_ID',
-  ANNOUNCEMENT_NOTIFICATION_CHANNEL_ID = 'ANNOUNCEMENT_NOTIFICATION_CHANNEL_ID',
-}
-
 export interface MetaMaskAndroidChannel extends AndroidChannel {
-  id: ChannelId;
+  id: string;
   title: string;
   subtitle: string;
 }
 
 export const notificationChannels = [
   {
-    id: ChannelId.DEFAULT_NOTIFICATION_CHANNEL_ID,
+    id: 'DEFAULT_NOTIFICATION_CHANNEL_ID',
     name: 'Transaction Complete',
-    lights: false,
-    vibration: false,
+    lights: true,
+    vibration: true,
     importance: AndroidImportance.DEFAULT,
     title: 'Transaction',
     subtitle: 'Transaction Complete',
   } as MetaMaskAndroidChannel,
   {
-    id: ChannelId.ANNOUNCEMENT_NOTIFICATION_CHANNEL_ID,
+    id: 'ANNOUNCEMENT_NOTIFICATION_CHANNEL_ID',
     name: 'MetaMask Announcement',
-    lights: false,
-    vibration: false,
+    lights: true,
+    vibration: true,
     importance: AndroidImportance.DEFAULT,
     title: 'Announcement',
     subtitle: 'MetaMask Announcement',
