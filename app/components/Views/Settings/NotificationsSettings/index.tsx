8d653d4bf5287cd6ba329c5b0bbfd98ddf38f99b
JSoufer <jonathan.ferreira@consensys.net>
2024-10-15 18:39:01 +0100

    make usage of new account UI

--- app/components/Views/Settings/NotificationsSettings/index.tsx
+++ app/components/Views/Settings/NotificationsSettings/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable react/display-name */
-import React, { useEffect, useMemo, useCallback } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { ScrollView, Switch, View, Linking } from 'react-native';
 import { useSelector } from 'react-redux';
 import { NavigationProp, ParamListBase } from '@react-navigation/native';
@@ -13,6 +13,7 @@ import Text, {
   TextVariant,
   TextColor,
 } from '../../../../component-library/components/Texts/Text';
+import { AccountsList} from './AccountsList';
 import { useAccounts } from '../../../../components/hooks/useAccounts';
 import { useMetrics } from '../../../../components/hooks/useMetrics';
 import { AvatarAccountType } from '../../../../component-library/components/Avatars/Avatar';
@@ -21,9 +22,7 @@ import SwitchLoadingModal from '../../../UI/Notification/SwitchLoadingModal';
 import { Props } from './NotificationsSettings.types';
 import { useStyles } from '../../../../component-library/hooks';
 
-import NotificationOptionToggle from './NotificationOptionToggle';
 import CustomNotificationsRow from './CustomNotificationsRow';
-import { NotificationsToggleTypes } from './NotificationsSettings.constants';
 import {
   selectIsFeatureAnnouncementsEnabled,
   selectIsMetamaskNotificationsEnabled,
@@ -121,19 +120,6 @@ const NotificationsSettings = ({ navigation, route }: Props) => {
     selectIsUpdatingMetamaskNotificationsAccount,
   );
 
-  const accountAddresses = useMemo(
-    () => accounts.map((a) => a.address),
-    [accounts],
-  );
-
-  const { switchFeatureAnnouncements } = useSwitchNotifications();
-
-  // Account Settings
-  const accountSettingsProps = useAccountSettingsProps(accountAddresses);
-  const refetchAccountSettings = useCallback(async () => {
-    await accountSettingsProps.update(accountAddresses);
-  }, [accountAddresses, accountSettingsProps]);
-
   const {
     enableNotifications,
     loading: enableLoading,
@@ -146,6 +132,9 @@ const NotificationsSettings = ({ navigation, route }: Props) => {
     error: disablingError,
   } = useDisableNotifications();
 
+  const { switchFeatureAnnouncements } = useSwitchNotifications();
+  const { updateAndfetchAccountSettings } = useAccountSettingsProps(accounts);
+
   const accountAvatarType = useSelector((state: RootState) =>
     state.settings.useBlockieIcon
       ? AvatarAccountType.Blockies
@@ -157,7 +146,7 @@ const NotificationsSettings = ({ navigation, route }: Props) => {
   const [uiNotificationStatus, setUiNotificationStatus] = React.useState(false);
   const [platformAnnouncementsState, setPlatformAnnouncementsState] =
     React.useState(isFeatureAnnouncementsEnabled);
-
+  const accountSettingsData = useSelector((state: RootState) => state.notifications);
   const loading = enableLoading || disableLoading;
   const errorText = enablingError || disablingError;
   const loadingText = !uiNotificationStatus
@@ -220,37 +209,6 @@ const NotificationsSettings = ({ navigation, route }: Props) => {
     );
   }, [colors, isFullScreenModal, navigation]);
 
-  const renderAccounts = useCallback(
-    () =>
-      accounts.map((account) => {
-        const isEnabled =
-          accountSettingsProps.data?.[account.address.toLowerCase()];
-        return (
-          <NotificationOptionToggle
-            type={NotificationsToggleTypes.ACCOUNT}
-            icon={accountAvatarType}
-            key={account.address}
-            title={account.name}
-            address={account.address}
-            disabledSwitch={isUpdatingMetamaskNotificationsAccount.length > 0}
-            isLoading={accountSettingsProps.accountsBeingUpdated.includes(
-              account.address.toLowerCase(),
-            )}
-            isEnabled={isEnabled ?? false}
-            refetchAccountSettings={refetchAccountSettings}
-          />
-        );
-      }),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [
-      accountSettingsProps.data,
-      accountSettingsProps.accountsBeingUpdated,
-      accountAvatarType,
-      isUpdatingMetamaskNotificationsAccount.length,
-      refetchAccountSettings,
-    ],
-  );
-
   const renderResetNotificationsBtn = useCallback(() => (
         <Button
           variant={ButtonVariants.Primary}
@@ -299,7 +257,13 @@ const NotificationsSettings = ({ navigation, route }: Props) => {
             styles={styles}
           />
 
-          {renderAccounts()}
+          <AccountsList
+            accounts={accounts}
+            accountAvatarType={accountAvatarType}
+            accountSettingsData={accountSettingsData}
+            updateAndfetchAccountSettings={updateAndfetchAccountSettings}
+            isUpdatingMetamaskNotificationsAccount={isUpdatingMetamaskNotificationsAccount}
+          />
           {renderResetNotificationsBtn()}
         </>
       )}
