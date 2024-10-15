1f22362cf95ffc789391873660f97b7e0144d0e4
Yande <andepandy@outlook.com>
2024-10-15 16:00:08 +0100

    update file paths

--- e2e/specs/mock-data/mock-events.js
+++ e2e/specs/mock-data/mock-events.js
@@ -2,19 +2,19 @@ export const mockEvents = {
     suggestedGasApiSuccessResponse: {
       mockUrl: 'https://gas.api.cx.metamask.io/networks/1/suggestedGasFees',
       responseCode: 200,
-      responseBodyFile: '../responseBodies/suggestedGasApi',
+      responseBodyFile: '/mock-responses/suggestGasApi.js',
       responseType: 'success',
     },
     suggestedGasApiErrorResponse: {
       mockUrl: 'https://gas.api.cx.metamask.io/networks/1/suggestedGasFees',
       responseCode: 500,
-      responseBodyFile: '../responseBodies/suggestedGasApi',
+      responseBodyFile: '/mock-responses/suggestGasApi.js',
       responseType: 'error',
     },
     suggestedGasApiSlowResponse: {
       mockUrl: 'https://gas.api.cx.metamask.io/networks/1/suggestedGasFees',
       responseCode: 200,
-      responseBodyFile: '../responseBodies/suggestedGasApi',
+      responseBodyFile: '/mock-responses/suggestGasApi.js',
       responseType: 'slowResponse',
     },
   };
