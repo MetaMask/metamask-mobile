2d87bb8d6f73ddd5a08fe6c80186db1d77e9bb76
Owen Craston <owengc12@gmail.com>
2024-10-17 15:46:46 -0700

    remove individual snap accounts

--- e2e/selectors/Modals/AccountActionsModal.selectors.js
+++ e2e/selectors/Modals/AccountActionsModal.selectors.js
@@ -3,5 +3,8 @@ export const AccountActionsModalSelectorsIDs = {
   VIEW_ETHERSCAN: 'view-etherscan-action',
   SHARE_ADDRESS: 'share-address-action',
   SHOW_PRIVATE_KEY: 'show-private-key-action',
-  REMOVE_HARDWARE_ACCOUNT: 'remove-hardward-account-action',
+  REMOVE_HARDWARE_ACCOUNT: 'remove-hardware-account-action',
+  ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
+  REMOVE_SNAP_ACCOUNT: 'remove-snap-account-action',
+  ///: END:ONLY_INCLUDE_IF
 };
