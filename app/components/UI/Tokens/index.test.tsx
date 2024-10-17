f27c825958f24b48a217431021bb5f1880f5f6bb
siibars <9662464+siibars@users.noreply.github.com>
2024-10-17 14:54:34 +0200

    chore: remove stake analytic event mock

--- app/components/UI/Tokens/index.test.tsx
+++ app/components/UI/Tokens/index.test.tsx
@@ -11,7 +11,6 @@ import { strings } from '../../../../locales/i18n';
 import AppConstants from '../../../../app/core/AppConstants';
 import Routes from '../../../../app/constants/navigation/Routes';
 import { WalletViewSelectorsIDs } from '../../../../e2e/selectors/wallet/WalletView.selectors';
-import { MetaMetricsEvents, useMetrics } from '../../../../app/components/hooks/useMetrics';
 
 jest.mock('../../../core/Engine', () => ({
   getTotalFiatAccountBalance: jest.fn(),
@@ -105,14 +104,6 @@ jest.mock('@react-navigation/native', () => {
   };
 });
 
-jest.mock('../../../components/hooks/useMetrics', () => ({
-  useMetrics: jest.fn(),
-}));
-
-jest.mock('../../../components/hooks/useMetrics', () => ({
-  useMetrics: jest.fn(),
-}));
-
 const Stack = createStackNavigator();
 // TODO: Replace "any" with type
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -132,18 +123,6 @@ const renderComponent = (state: any = {}) =>
   );
 
 describe('Tokens', () => {
-  beforeEach(() => {
-    (useMetrics as jest.Mock).mockReturnValue({
-      trackEvent: jest.fn(),
-    });
-  });
-
-  beforeEach(() => {
-    (useMetrics as jest.Mock).mockReturnValue({
-      trackEvent: jest.fn(),
-    });
-  })
-
   afterEach(() => {
     mockNavigate.mockClear();
     mockPush.mockClear();
@@ -277,11 +256,6 @@ describe('Tokens', () => {
     expect(getByTestId(WalletViewSelectorsIDs.STAKE_BUTTON)).toBeDefined();
   });
   it('navigates to Portfolio Stake url when stake button is pressed', () => {
-    const trackEvent = jest.fn();
-    (useMetrics as jest.Mock).mockReturnValue({
-      trackEvent,
-    });
-
     const { getByTestId } = renderComponent(initialState);
 
     fireEvent.press(getByTestId(WalletViewSelectorsIDs.STAKE_BUTTON));
@@ -292,16 +266,5 @@ describe('Tokens', () => {
       },
       screen: Routes.BROWSER.VIEW,
     });
-    expect(trackEvent).toHaveBeenNthCalledWith(
-      1,
-      MetaMetricsEvents.STAKE_BUTTON_CLICKED,
-      {
-        chain_id: expect.any(String),
-        location: 'Home Screen',
-        text: 'Stake',
-        token_symbol: expect.any(String),
-        url: AppConstants.STAKE.URL,
-      }
-    );
   });
 });
