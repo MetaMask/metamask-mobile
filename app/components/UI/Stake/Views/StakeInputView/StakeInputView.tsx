308a0adb212e34ec0fe97a83e5c7e928be9be8f2
Amitabh Aggarwal <aggarwal.amitabh@gmail.com>
2024-10-18 23:54:36 -0500

    refactor: update StakeInputView to use new hooks and remove unused code

--- app/components/UI/Stake/Views/StakeInputView/StakeInputView.tsx
+++ app/components/UI/Stake/Views/StakeInputView/StakeInputView.tsx
@@ -19,7 +19,6 @@ import styleSheet from './StakeInputView.styles';
 import useStakingInputHandlers from '../../hooks/useStakingInput';
 import useBalance from '../../hooks/useBalance';
 import InputDisplay from '../../components/InputDisplay';
-import { useStakeContext } from '../../hooks/useStakeContext';
 
 const StakeInputView = () => {
   const title = strings('stake.stake_eth');
@@ -44,9 +43,6 @@ const StakeInputView = () => {
     estimatedAnnualRewards,
   } = useStakingInputHandlers(balanceWei);
 
-
-  const { sdkService } = useStakeContext();
-  
   const navigateToLearnMoreModal = () => {
     navigation.navigate('StakeModals', {
       screen: Routes.STAKING.MODALS.LEARN_MORE,
@@ -62,7 +58,7 @@ const StakeInputView = () => {
       },
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [amountWei, fiatAmount, navigation, sdkService]);
+  }, [amountWei, fiatAmount, navigation]);
 
   const balanceText = strings('stake.balance');
 
