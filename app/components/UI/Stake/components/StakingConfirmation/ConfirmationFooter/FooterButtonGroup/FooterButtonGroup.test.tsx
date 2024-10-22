f2f84ed9c7214d32a5e4e50be724bc4a1c4f1996
Matthew Grainger <46547583+Matt561@users.noreply.github.com>
2024-10-22 10:45:05 -0400

    feat: STAKE-805 integrate stake method (#11845)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    This PR enables staking on both Ethereum mainnet and Holesky.
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    ## **Related issues**
    
    Ticket: [STAKE-805: [FE] [Stake flow] Integrate stake sdk
    method](https://consensyssoftware.atlassian.net/browse/STAKE-805)
    
    ## **Manual testing steps**
    
    1. Add `export MM_POOLED_STAKING_UI_ENABLED=true` to your .js.env file.
    2. Select Ethereum in the asset list
    3. Scroll down a bit and click "Stake More"
    4. Enter an amount to stake and click "review"
    5. On the review screen, click "Continue"
    6. You should see the transaction confirmation screen appear. Click
    "Confirm".
    7. The transaction should be submitted and you should be redirected to
    the Ethereum asset details page
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    https://github.com/user-attachments/assets/f90dfc78-b51a-4a6d-aa93-e48cc4146d30
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    **Update:** On transaction submission, the user is now redirected to the
    Transactions View.
    
    
    https://github.com/user-attachments/assets/61752a3b-6a7d-4bec-ae3b-a2060ddc257a
    
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/UI/Stake/components/StakingConfirmation/ConfirmationFooter/FooterButtonGroup/FooterButtonGroup.test.tsx
+++ app/components/UI/Stake/components/StakingConfirmation/ConfirmationFooter/FooterButtonGroup/FooterButtonGroup.test.tsx
@@ -3,9 +3,34 @@ import renderWithProvider from '../../../../../../../util/test/renderWithProvide
 import { strings } from '../../../../../../../../locales/i18n';
 import FooterButtonGroup from './FooterButtonGroup';
 import { fireEvent } from '@testing-library/react-native';
+import {
+  FooterButtonGroupActions,
+  FooterButtonGroupProps,
+} from './FooterButtonGroup.types';
+import { createMockAccountsControllerState } from '../../../../../../../util/test/accountsControllerTestUtils';
+import { backgroundState } from '../../../../../../../util/test/initial-root-state';
+
+const MOCK_ADDRESS_1 = '0x0';
+const MOCK_ADDRESS_2 = '0x1';
+
+const MOCK_ACCOUNTS_CONTROLLER_STATE = createMockAccountsControllerState([
+  MOCK_ADDRESS_1,
+  MOCK_ADDRESS_2,
+]);
+
+const mockInitialState = {
+  settings: {},
+  engine: {
+    backgroundState: {
+      ...backgroundState,
+      AccountsController: MOCK_ACCOUNTS_CONTROLLER_STATE,
+    },
+  },
+};
 
 const mockCanGoBack = jest.fn();
 const mockGoBack = jest.fn();
+const mockNavigate = jest.fn();
 
 jest.mock('@react-navigation/native', () => {
   const actualReactNavigation = jest.requireActual('@react-navigation/native');
@@ -14,9 +39,19 @@ jest.mock('@react-navigation/native', () => {
     useNavigation: () => ({
       canGoBack: mockCanGoBack,
       goBack: mockGoBack,
+      navigate: mockNavigate,
     }),
   };
 });
+
+jest.mock('../../../../hooks/usePoolStakedDeposit', () => ({
+  __esModule: true,
+  default: () => ({
+    poolStakingContract: {},
+    estimateDepositGas: jest.fn(),
+    attemptDepositTransaction: jest.fn(),
+  }),
+}));
 
 describe('FooterButtonGroup', () => {
   beforeEach(() => {
@@ -24,7 +59,15 @@ describe('FooterButtonGroup', () => {
   });
 
   it('render matches snapshot', () => {
-    const { getByText, toJSON } = renderWithProvider(<FooterButtonGroup />);
+    const props: FooterButtonGroupProps = {
+      valueWei: '3210000000000000',
+      action: FooterButtonGroupActions.STAKE,
+    };
+
+    const { getByText, toJSON } = renderWithProvider(
+      <FooterButtonGroup {...props} />,
+      { state: mockInitialState },
+    );
 
     expect(getByText(strings('stake.cancel'))).toBeDefined();
     expect(getByText(strings('stake.confirm'))).toBeDefined();
@@ -32,15 +75,22 @@ describe('FooterButtonGroup', () => {
     expect(toJSON()).toMatchSnapshot();
   });
 
-  it('navigates to previous page when cancel button is pressed', () => {
+  it('navigates to Asset page when cancel is pressed', () => {
     mockCanGoBack.mockImplementationOnce(() => true);
+    const props: FooterButtonGroupProps = {
+      valueWei: '3210000000000000',
+      action: FooterButtonGroupActions.STAKE,
+    };
 
-    const { getByText, toJSON } = renderWithProvider(<FooterButtonGroup />);
+    const { getByText, toJSON } = renderWithProvider(
+      <FooterButtonGroup {...props} />,
+      { state: mockInitialState },
+    );
 
     fireEvent.press(getByText(strings('stake.cancel')));
 
-    expect(mockCanGoBack).toHaveBeenCalledTimes(1);
-    expect(mockGoBack).toHaveBeenCalledTimes(1);
+    expect(mockNavigate).toHaveBeenCalledTimes(1);
+    expect(mockNavigate).toHaveBeenCalledWith('Asset');
 
     expect(toJSON()).toMatchSnapshot();
   });
