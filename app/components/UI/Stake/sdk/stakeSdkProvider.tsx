b8820ca1a691a8d91ef58185ec5232ab05df51da
Matthew Grainger <matthewgrainger33@gmail.com>
2024-10-22 10:53:39 -0400

    feat: added chain compatibility check when initializing stake sdkService

--- app/components/UI/Stake/sdk/stakeSdkProvider.tsx
+++ app/components/UI/Stake/sdk/stakeSdkProvider.tsx
@@ -3,6 +3,8 @@ import {
   StakeSdk,
   PooledStakingContract,
   type StakingApiService,
+  isSupportedChain,
+  ChainId,
 } from '@metamask/stake-sdk';
 import React, {
   useState,
@@ -14,6 +16,9 @@ import { getProviderByChainId } from '../../../../util/notifications';
 import { useSelector } from 'react-redux';
 import { selectChainId } from '../../../../selectors/networkController';
 import { hexToDecimal } from '../../../../util/conversions';
+
+const hexToChainId = (hex: `0x${string}`): ChainId =>
+  hexToDecimal(hex).toString() as unknown as ChainId;
 
 export const SDK = StakeSdk.create({ stakingType: StakingType.POOLED });
 
@@ -37,7 +42,21 @@ export const StakeSDKProvider: React.FC<
   const chainId = useSelector(selectChainId);
 
   const sdkService = useMemo(() => {
+    if (!chainId || !isSupportedChain(hexToChainId(chainId))) {
+      console.error(
+        'Failed to initialize Staking SDK Service: chainId unsupported',
+      );
+      return;
+    }
+
     const provider = getProviderByChainId(chainId);
+
+    if (!provider) {
+      console.error(
+        'Failed to initialize Staking SDK Service: provider not found',
+      );
+      return;
+    }
 
     const sdk = StakeSdk.create({
       chainId: parseInt(hexToDecimal(chainId).toString()),
@@ -51,12 +70,12 @@ export const StakeSDKProvider: React.FC<
 
   const stakeContextValue = useMemo(
     (): Stake => ({
-      stakingContract: sdkService.pooledStakingContract,
-      stakingApiService: sdkService.stakingApiService,
+      stakingContract: sdkService?.pooledStakingContract,
+      stakingApiService: sdkService?.stakingApiService,
       sdkType,
       setSdkType,
     }),
-    [sdkService.pooledStakingContract, sdkService.stakingApiService, sdkType],
+    [sdkService?.pooledStakingContract, sdkService?.stakingApiService, sdkType],
   );
   return (
     <StakeContext.Provider value={stakeContextValue}>
