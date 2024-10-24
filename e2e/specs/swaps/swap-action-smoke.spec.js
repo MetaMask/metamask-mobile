dac3a988b24ff4979440a633a442034d8b7ec67a
Davide Brocchetto <davide.brocchetto@consensys.net>
2024-10-24 19:28:12 +0200

    Removed test for optimism

--- e2e/specs/swaps/swap-action-smoke.spec.js
+++ e2e/specs/swaps/swap-action-smoke.spec.js
@@ -76,6 +76,7 @@ describe(SmokeSwaps('Swap from Actions'), () => {
     await SuccessImportAccountView.tapCloseButton();
     await AccountListView.swipeToDismissAccountsModal();
     await Assertions.checkIfVisible(WalletView.container);
+    await TestHelpers.delay(2000);
   });
 
   it.each`
@@ -83,8 +84,6 @@ describe(SmokeSwaps('Swap from Actions'), () => {
     ${'native'}$     |${'.4'}   | ${'ETH'}          | ${'WETH'}       | ${CustomNetworks.Tenderly.Mainnet}
     ${'wrapped'}$    |${'.2'}   | ${'WETH'}         | ${'ETH'}        | ${CustomNetworks.Tenderly.Mainnet}
     ${'native'}$     |${'.1 '}  | ${'ETH'}          | ${'USDC'}       | ${CustomNetworks.Tenderly.Optimism}
-    ${'unapproved'}$ |${'50 '}  | ${'USDC'}         | ${'DAI'}        | ${CustomNetworks.Tenderly.Optimism}
-
   `(
     "should swap $type token '$sourceTokenSymbol' to '$destTokenSymbol' on '$network.providerConfig.nickname'",
     async ({ type, quantity, sourceTokenSymbol, destTokenSymbol, network }) => {
