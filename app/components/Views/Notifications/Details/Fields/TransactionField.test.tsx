bef4fccc161e49fb7bd7f2f0793ea0b6bdebf7f8
JSoufer <jonathan.ferreira@consensys.net>
2024-10-25 16:42:30 +0100

    increase test coverage

---
+++ app/components/Views/Notifications/Details/Fields/TransactionField.test.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable import/no-namespace */
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react-native';
+import TransactionField from '../TransactionField';
+import * as useMetricsModule from '../../../../../../components/hooks/useMetrics';
+import * as useCopyClipboardModule from '../../hooks/useCopyClipboard';
+import { ModalFieldType } from '../../../../../../util/notifications';
+import MOCK_NOTIFICATIONS from '../../../../../UI/Notification/__mocks__/mock_notifications';
+
+// Mock the required modules
+jest.mock('../../../../../../components/hooks/useMetrics');
+jest.mock('../../hooks/useCopyClipboard');
+jest.mock('../../../../../../../locales/i18n', () => ({
+  strings: jest.fn((key) => key),
+}));
+
+describe('TransactionField', () => {
+  // Arrange
+  const mockProps = {
+    txHash: '0x123456789abcdef',
+    notification: {
+      id: '1',
+      type: 'transaction',
+    },
+  };
+
+  const mockTrackEvent = jest.fn();
+  const mockCopyToClipboard = jest.fn();
+
+  beforeEach(() => {
+    jest.spyOn(useMetricsModule, 'useMetrics').mockReturnValue({ trackEvent: mockTrackEvent } as unknown as useMetricsModule.IUseMetricsHook);
+    jest.spyOn(useCopyClipboardModule, 'default').mockReturnValue(mockCopyToClipboard);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should copy transaction hash and track event when copy button is pressed', () => {
+    const { getByText } = render(
+      <TransactionField
+        type={ModalFieldType.TRANSACTION}
+        notification={MOCK_NOTIFICATIONS[0]}
+        txHash={mockProps.txHash}
+      />
+    );
+    const copyButton = getByText('transaction.transaction_id');
+
+    fireEvent.press(copyButton);
+
+    expect(mockTrackEvent).toHaveBeenCalledWith({'category': 'Notification Detail Clicked'}, {'chain_id': 1, 'clicked_item': 'tx_id', 'notification_id': '3fa85f64-5717-4562-b3fc-2c963f66afa7', 'notification_type': 'eth_sent'});
+  });
+});
