727375cde34d29b3468f492368c09e93b5765db3
Mathieu Artu <mathieu.artu@consensys.net>
2024-10-25 14:49:06 +0200

    fix: add SmokeNotifications tag

--- e2e/specs/notifications/account-syncing/sync-after-adding-custom-name-account.spec.js
+++ e2e/specs/notifications/account-syncing/sync-after-adding-custom-name-account.spec.js
@@ -20,90 +20,93 @@ import EditAccountNameView from '../../../pages/EditAccountNameView';
 import Gestures from '../../../utils/Gestures';
 import EditAccountNameSelectorIDs from '../../../selectors/EditAccountName.selectors';
 import { mockNotificationServices } from '../utils/mocks';
+import { SmokeNotifications } from '../../../tags';
 
-describe('Account syncing', () => {
-  const NEW_ACCOUNT_NAME = 'My third account';
+describe(
+  SmokeNotifications('Account syncing', () => {
+    const NEW_ACCOUNT_NAME = 'My third account';
 
-  it('syncs newly added accounts with custom names and retrieves them after importing the same SRP', async () => {
-    jest.setTimeout(200000);
-    await TestHelpers.reverseServerPort();
+    it('syncs newly added accounts with custom names and retrieves them after importing the same SRP', async () => {
+      jest.setTimeout(200000);
+      await TestHelpers.reverseServerPort();
 
-    const mockServer = await startMockServer({
-      mockUrl: 'https://user-storage.api.cx.metamask.io/api/v1/userstorage',
-    });
+      const mockServer = await startMockServer({
+        mockUrl: 'https://user-storage.api.cx.metamask.io/api/v1/userstorage',
+      });
 
-    const { userStorageMockttpControllerInstance } =
-      await mockNotificationServices(mockServer);
+      const { userStorageMockttpControllerInstance } =
+        await mockNotificationServices(mockServer);
 
-    userStorageMockttpControllerInstance.setupPath('accounts', mockServer, {
-      getResponse: accountsSyncMockResponse,
-    });
+      userStorageMockttpControllerInstance.setupPath('accounts', mockServer, {
+        getResponse: accountsSyncMockResponse,
+      });
 
-    const decryptedAccountNames = await Promise.all(
-      accountsSyncMockResponse.map(async (response) => {
-        const decryptedAccountName = await SDK.Encryption.decryptString(
-          response.Data,
-          NOTIFICATIONS_TEAM_STORAGE_KEY,
-        );
-        return JSON.parse(decryptedAccountName).n;
-      }),
-    );
-
-    await device.launchApp({
-      newInstance: true,
-      delete: true,
-    });
-
-    await importWalletWithRecoveryPhrase(
-      NOTIFICATIONS_TEAM_SEED_PHRASE,
-      NOTIFICATIONS_TEAM_PASSWORD,
-    );
-
-    await WalletView.tapIdenticon();
-    await Assertions.checkIfVisible(AccountListView.accountList);
-
-    for (const accountName of decryptedAccountNames) {
-      await Assertions.checkIfVisible(
-        element(by.text(accountName).and(by.id('cellbase-avatar-title'))),
+      const decryptedAccountNames = await Promise.all(
+        accountsSyncMockResponse.map(async (response) => {
+          const decryptedAccountName = await SDK.Encryption.decryptString(
+            response.Data,
+            NOTIFICATIONS_TEAM_STORAGE_KEY,
+          );
+          return JSON.parse(decryptedAccountName).n;
+        }),
       );
-    }
 
-    await AccountListView.tapAddAccountButton();
-    await AddAccountModal.tapCreateAccount();
+      await device.launchApp({
+        newInstance: true,
+        delete: true,
+      });
 
-    await AccountListView.swipeToDismissAccountsModal();
+      await importWalletWithRecoveryPhrase(
+        NOTIFICATIONS_TEAM_SEED_PHRASE,
+        NOTIFICATIONS_TEAM_PASSWORD,
+      );
 
-    await WalletView.tapMainWalletAccountActions();
-    await AccountActionsModal.tapEditAccount();
-    await Gestures.clearField(EditAccountNameView.accountNameInput);
-    await TestHelpers.typeTextAndHideKeyboard(
-      EditAccountNameSelectorIDs.ACCOUNT_NAME_INPUT,
-      NEW_ACCOUNT_NAME,
-    );
-    await EditAccountNameView.tapSave();
+      await WalletView.tapIdenticon();
+      await Assertions.checkIfVisible(AccountListView.accountList);
 
-    await Assertions.checkIfElementToHaveText(
-      WalletView.accountName,
-      NEW_ACCOUNT_NAME,
-    );
+      for (const accountName of decryptedAccountNames) {
+        await Assertions.checkIfVisible(
+          element(by.text(accountName).and(by.id('cellbase-avatar-title'))),
+        );
+      }
 
-    await device.launchApp({
-      newInstance: true,
-      delete: true,
+      await AccountListView.tapAddAccountButton();
+      await AddAccountModal.tapCreateAccount();
+
+      await AccountListView.swipeToDismissAccountsModal();
+
+      await WalletView.tapMainWalletAccountActions();
+      await AccountActionsModal.tapEditAccount();
+      await Gestures.clearField(EditAccountNameView.accountNameInput);
+      await TestHelpers.typeTextAndHideKeyboard(
+        EditAccountNameSelectorIDs.ACCOUNT_NAME_INPUT,
+        NEW_ACCOUNT_NAME,
+      );
+      await EditAccountNameView.tapSave();
+
+      await Assertions.checkIfElementToHaveText(
+        WalletView.accountName,
+        NEW_ACCOUNT_NAME,
+      );
+
+      await device.launchApp({
+        newInstance: true,
+        delete: true,
+      });
+
+      await importWalletWithRecoveryPhrase(
+        NOTIFICATIONS_TEAM_SEED_PHRASE,
+        NOTIFICATIONS_TEAM_PASSWORD,
+      );
+
+      await WalletView.tapIdenticon();
+      await Assertions.checkIfVisible(AccountListView.accountList);
+
+      await Assertions.checkIfVisible(
+        element(by.text(NEW_ACCOUNT_NAME).and(by.id('cellbase-avatar-title'))),
+      );
+
+      await stopMockServer();
     });
-
-    await importWalletWithRecoveryPhrase(
-      NOTIFICATIONS_TEAM_SEED_PHRASE,
-      NOTIFICATIONS_TEAM_PASSWORD,
-    );
-
-    await WalletView.tapIdenticon();
-    await Assertions.checkIfVisible(AccountListView.accountList);
-
-    await Assertions.checkIfVisible(
-      element(by.text(NEW_ACCOUNT_NAME).and(by.id('cellbase-avatar-title'))),
-    );
-
-    await stopMockServer();
-  });
-});
+  }),
+);
--- e2e/specs/notifications/account-syncing/sync-after-onboarding.spec.js
+++ e2e/specs/notifications/account-syncing/sync-after-onboarding.spec.js
@@ -15,52 +15,55 @@ import WalletView from '../../../pages/wallet/WalletView';
 import AccountListView from '../../../pages/AccountListView';
 import Assertions from '../../../utils/Assertions';
 import { mockNotificationServices } from '../utils/mocks';
+import { SmokeNotifications } from '../../../tags';
 
-describe('Account syncing', () => {
-  it('retrieves all previously synced accounts', async () => {
-    const decryptedAccountNames = await Promise.all(
-      accountsSyncMockResponse.map(async (response) => {
-        const decryptedAccountName = await SDK.Encryption.decryptString(
-          response.Data,
-          NOTIFICATIONS_TEAM_STORAGE_KEY,
-        );
-        return JSON.parse(decryptedAccountName).n;
-      }),
-    );
-
-    const mockServer = await startMockServer({
-      mockUrl: 'https://user-storage.api.cx.metamask.io/api/v1/userstorage',
-    });
-
-    const { userStorageMockttpControllerInstance } =
-      await mockNotificationServices(mockServer);
-
-    userStorageMockttpControllerInstance.setupPath('accounts', mockServer, {
-      getResponse: accountsSyncMockResponse,
-    });
-
-    jest.setTimeout(200000);
-    await TestHelpers.reverseServerPort();
-
-    await device.launchApp({
-      newInstance: true,
-      delete: true,
-    });
-
-    await importWalletWithRecoveryPhrase(
-      NOTIFICATIONS_TEAM_SEED_PHRASE,
-      NOTIFICATIONS_TEAM_PASSWORD,
-    );
-
-    await WalletView.tapIdenticon();
-    await Assertions.checkIfVisible(AccountListView.accountList);
-
-    for (const accountName of decryptedAccountNames) {
-      await Assertions.checkIfVisible(
-        element(by.text(accountName).and(by.id('cellbase-avatar-title'))),
+describe(
+  SmokeNotifications('Account syncing', () => {
+    it('retrieves all previously synced accounts', async () => {
+      const decryptedAccountNames = await Promise.all(
+        accountsSyncMockResponse.map(async (response) => {
+          const decryptedAccountName = await SDK.Encryption.decryptString(
+            response.Data,
+            NOTIFICATIONS_TEAM_STORAGE_KEY,
+          );
+          return JSON.parse(decryptedAccountName).n;
+        }),
       );
-    }
 
-    await stopMockServer();
-  });
-});
+      const mockServer = await startMockServer({
+        mockUrl: 'https://user-storage.api.cx.metamask.io/api/v1/userstorage',
+      });
+
+      const { userStorageMockttpControllerInstance } =
+        await mockNotificationServices(mockServer);
+
+      userStorageMockttpControllerInstance.setupPath('accounts', mockServer, {
+        getResponse: accountsSyncMockResponse,
+      });
+
+      jest.setTimeout(200000);
+      await TestHelpers.reverseServerPort();
+
+      await device.launchApp({
+        newInstance: true,
+        delete: true,
+      });
+
+      await importWalletWithRecoveryPhrase(
+        NOTIFICATIONS_TEAM_SEED_PHRASE,
+        NOTIFICATIONS_TEAM_PASSWORD,
+      );
+
+      await WalletView.tapIdenticon();
+      await Assertions.checkIfVisible(AccountListView.accountList);
+
+      for (const accountName of decryptedAccountNames) {
+        await Assertions.checkIfVisible(
+          element(by.text(accountName).and(by.id('cellbase-avatar-title'))),
+        );
+      }
+
+      await stopMockServer();
+    });
+  }),
+);
