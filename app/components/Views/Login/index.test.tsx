ce0bef34c79b7ea3386fac663dcebc9c3a726e10
tommasini <tommasini15@gmail.com>
2024-10-25 19:35:19 +0100

    remove  unit test that can be only applied once react native testing library is in place instead of enzyme

--- app/components/Views/Login/index.test.tsx
+++ app/components/Views/Login/index.test.tsx
@@ -4,8 +4,6 @@ import Login from './';
 import configureMockStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import { backgroundState } from '../../../util/test/initial-root-state';
-// eslint-disable-next-line import/no-namespace
-import * as traceObj from '../../../util/trace';
 
 const mockStore = configureMockStore();
 const initialState = {
@@ -20,15 +18,11 @@ const store = mockStore(initialState);
 
 describe('Login', () => {
   it('should render correctly', () => {
-    const spyFetch = jest
-      .spyOn(traceObj, 'trace')
-      .mockImplementation(() => undefined);
     const wrapper = shallow(
       <Provider store={store}>
         <Login />
       </Provider>,
     );
     expect(wrapper).toMatchSnapshot();
-    expect(spyFetch).toHaveBeenCalledTimes(1);
   });
 });
