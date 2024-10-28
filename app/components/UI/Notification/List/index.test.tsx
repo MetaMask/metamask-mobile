80b6be0c7cc248e119940bf74da0747ca48cf0c5
JSoufer <jonathan.ferreira@consensys.net>
2024-10-28 14:37:50 +0000

    updates snapshot

--- app/components/UI/Notification/List/index.test.tsx
+++ app/components/UI/Notification/List/index.test.tsx
@@ -1,24 +1,66 @@
 import React from 'react';
 import NotificationsList from './';
-import renderWithProvider from '../../../../util/test/renderWithProvider';
+import renderWithProvider, { DeepPartial } from '../../../../util/test/renderWithProvider';
 import MOCK_NOTIFICATIONS from '../__mocks__/mock_notifications';
-import { NavigationProp, ParamListBase } from '@react-navigation/native';
+import { backgroundState } from '../../../../util/test/initial-root-state';
+import { RootState } from '../../../../reducers';
+import { createNavigationProps } from '../../../../util/testUtils';
 
+const mockNavigation = createNavigationProps({});
 
-const navigationMock = {
-  navigate: jest.fn(),
-} as unknown as NavigationProp<ParamListBase>;
+jest.mock('@react-navigation/native', () => {
+  const navigation = {
+    params: {},
+  };
+  return {
+    // TODO: Replace "any" with type
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ...jest.requireActual<any>('@react-navigation/native'),
+    useRoute: jest.fn(() => ({ params: navigation.params })),
+    useNavigation: jest.fn(() => mockNavigation),
+  };
+});
 
+const mockInitialState: DeepPartial<RootState> = {
+  engine: {
+    backgroundState: {
+      ...backgroundState,
+      NotificationServicesController: {
+        metamaskNotificationsList: [],
+      },
+    },
+  },
+};
+
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  // TODO: Replace "any" with type
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  useSelector: (fn: any) => fn(mockInitialState),
+}));
 
 describe('NotificationsList', () => {
-  it('should render correctly', () => {
+  it('renders correctly', () => {
     const { toJSON } = renderWithProvider(
       <NotificationsList
-        navigation={navigationMock}
+        navigation={mockNavigation}
         allNotifications={MOCK_NOTIFICATIONS}
         walletNotifications={[MOCK_NOTIFICATIONS[1]]}
         web3Notifications={[MOCK_NOTIFICATIONS[0]]}
         loading
+      />,
+    );
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders empty state', () => {
+    const { toJSON } = renderWithProvider(
+      <NotificationsList
+        navigation={mockNavigation}
+        allNotifications={[]}
+        walletNotifications={[]}
+        web3Notifications={[]}
+        loading={false}
       />,
     );
     expect(toJSON()).toMatchSnapshot();
