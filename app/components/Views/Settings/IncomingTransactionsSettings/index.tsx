68d8956ad311b514c03bd73b3f24d51a71a97af2
vinnyhoward <vincenguyenhoward@gmail.com>
2024-10-29 16:02:20 -0600

    fix: render mainnet first

--- app/components/Views/Settings/IncomingTransactionsSettings/index.tsx
+++ app/components/Views/Settings/IncomingTransactionsSettings/index.tsx
@@ -19,6 +19,7 @@ import {
 import { selectNetworkConfigurations } from '../../../../selectors/networkController';
 import { EtherscanSupportedHexChainId } from '@metamask/preferences-controller';
 import { ETHERSCAN_SUPPORTED_NETWORKS } from '@metamask/transaction-controller';
+import { NetworkConfiguration } from '@metamask/network-controller';
 import styleSheet from './index.styles';
 import {
   INCOMING_TRANSACTIONS,
@@ -51,39 +52,61 @@ const IncomingTransactionsSettings = () => {
     );
   };
 
-  const renderRpcNetworks = () =>
-    Object.values(networkConfigurations).map(
-      ({ name: nickname, rpcEndpoints, chainId, defaultRpcEndpointIndex }) => {
-        const rpcUrl = rpcEndpoints[defaultRpcEndpointIndex].url;
-        if (!chainId || !Object.keys(supportedNetworks).includes(chainId))
-          return null;
-        const { name } = { name: nickname || rpcUrl };
+  const renderRpcNetworks = () => {
+    const networks = Object.values(networkConfigurations);
 
-        let testId = '';
-        if (chainId === Networks[MAINNET].chainId) {
-          testId = INCOMING_MAINNET_TOGGLE;
-        } else if (chainId === Networks[LINEA_MAINNET].chainId) {
-          testId = INCOMING_LINEA_MAINNET_TOGGLE;
-        }
-
-        //@ts-expect-error - The utils/network file is still JS and this function expects a networkType, and should be optional
-        const image = getNetworkImageSource({ chainId: chainId?.toString() });
-        const secondaryText =
-          supportedNetworks[chainId as keyof typeof supportedNetworks].domain;
-        return (
-          <NetworkCell
-            key={`${nickname}-${chainId}`}
-            name={name}
-            chainId={chainId as EtherscanSupportedHexChainId}
-            imageSource={image}
-            secondaryText={secondaryText}
-            showIncomingTransactionsNetworks={showIncomingTransactionsNetworks}
-            toggleEnableIncomingTransactions={toggleEnableIncomingTransactions}
-            testID={testId}
-          />
-        );
-      },
+    const mainnetNetworks = networks.filter(
+      ({ chainId }) =>
+        chainId === Networks[MAINNET].chainId ||
+        chainId === Networks[LINEA_MAINNET].chainId,
     );
+
+    const otherNetworks = networks.filter(
+      ({ chainId }) =>
+        chainId !== Networks[MAINNET].chainId &&
+        chainId !== Networks[LINEA_MAINNET].chainId,
+    );
+
+    const renderNetwork = ({
+      name: nickname,
+      rpcEndpoints,
+      chainId,
+      defaultRpcEndpointIndex,
+    }: NetworkConfiguration) => {
+      if (!chainId || !Object.keys(supportedNetworks).includes(chainId))
+        return null;
+
+      const rpcUrl = rpcEndpoints[defaultRpcEndpointIndex].url;
+      const { name } = { name: nickname || rpcUrl };
+
+      let testId = '';
+      if (chainId === Networks[MAINNET].chainId) {
+        testId = INCOMING_MAINNET_TOGGLE;
+      } else if (chainId === Networks[LINEA_MAINNET].chainId) {
+        testId = INCOMING_LINEA_MAINNET_TOGGLE;
+      }
+
+      //@ts-expect-error - The utils/network file is still JS and this function expects a networkType, and should be optional
+      const image = getNetworkImageSource({ chainId: chainId?.toString() });
+      const secondaryText =
+        supportedNetworks[chainId as keyof typeof supportedNetworks].domain;
+
+      return (
+        <NetworkCell
+          key={`${nickname}-${chainId}`}
+          name={name}
+          chainId={chainId as EtherscanSupportedHexChainId}
+          imageSource={image}
+          secondaryText={secondaryText}
+          showIncomingTransactionsNetworks={showIncomingTransactionsNetworks}
+          toggleEnableIncomingTransactions={toggleEnableIncomingTransactions}
+          testID={testId}
+        />
+      );
+    };
+
+    return [...mainnetNetworks, ...otherNetworks].map(renderNetwork);
+  };
 
   const renderOtherNetworks = () => {
     const NetworksTyped = Networks as NetworksI;
@@ -106,6 +129,9 @@ const IncomingTransactionsSettings = () => {
       );
     });
   };
+  console.log('incoming transactions state:', showIncomingTransactionsNetworks);
+  console.log('Ethereum Mainnet:', showIncomingTransactionsNetworks['0x1']);
+  console.log('Linea Mainnet:', showIncomingTransactionsNetworks['0xe708']);
 
   return (
     <View style={styles.setting} testID={INCOMING_TRANSACTIONS}>
