fa64ace5c2593db5c1d306163db8bb2bb19f321c
Aslau Mario-Daniel <marioaslau@gmail.com>
2024-10-29 16:01:56 -0700

    feat: upgrading Appium to v2 & iOS coverage

--- wdio/config/ios.config.browserstack.js
+++ wdio/config/ios.config.browserstack.js
@@ -9,27 +9,78 @@ const browserstack = require('browserstack-local');
 
 config.user = process.env.BROWSERSTACK_USERNAME;
 config.key = process.env.BROWSERSTACK_ACCESS_KEY;
-config.capabilities = [
+
+const defaultCapabilities = [
   {
-    platformName: 'iOS',
+    platformName: 'Android',
     noReset: false,
     fullReset: false,
     maxInstances: 1,
-    build: 'iOS QA E2E Tests',
-    automationName: 'XCUITest',
-    deviceName: 'iPhone 13 Pro',
-    platformVersion: '16',
-    app: process.env.BROWSERSTACK_APP_URL, // TODO: Add package ID when upload to BrowserStack
+    build: 'iOS App Launch Times Tests',
+    device: process.env.BROWSERSTACK_DEVICE || 'iPhone 13 Pro',
+    os_version: process.env.BROWSERSTACK_OS_VERSION || '16',
+    app: process.env.BROWSERSTACK_APP_URL,
     'browserstack.debug': true,
     'browserstack.local': true,
+    settings: {
+      snapshotMaxDepth: 100,
+    },
+  }
+];
+
+// Define capabilities for app upgrade tests
+const upgradeCapabilities = [
+  {
+    platformName: 'Android',
+    noReset: false,
+    fullReset: false,
+    maxInstances: 1,
+    build: 'iOS App Upgrade Tests',
+    device: process.env.BROWSERSTACK_DEVICE || 'iPhone 12',
+    os_version: process.env.BROWSERSTACK_OS_VERSION || '15',
+    app: process.env.PRODUCTION_APP_URL || process.env.BROWSERSTACK_APP_URL,
+    'browserstack.debug': true,
+    'browserstack.local': true,
+    'browserstack.midSessionInstallApps' : [process.env.BROWSERSTACK_APP_URL],
     settings: {
       snapshotMaxDepth: 100,
     },
   },
 ];
 
+// Determine test type based on command-line arguments
+const isAppUpgrade = process.argv.includes('--upgrade') || false;
+const isPerformance = process.argv.includes('--performance') || false;
+
+// Consolidating the conditional logic for capabilities and tag expression
+const { selectedCapabilities, defaultTagExpression } = (() => {
+  if (isAppUpgrade) {
+      return {
+          selectedCapabilities: upgradeCapabilities,
+          defaultTagExpression: '@upgrade and @iosApp',
+      };
+  } else if (isPerformance) {
+      return {
+          selectedCapabilities: defaultCapabilities,
+          defaultTagExpression: '@performance and @iosApp',
+      };
+  } else {
+      return {
+          selectedCapabilities: defaultCapabilities,
+          defaultTagExpression: '@smoke and @iosApp',
+      };
+  }
+})();
+
+// Apply the selected configuration
+config.cucumberOpts.tagExpression = selectedCapabilities; // pass tag to run tests specific to android
+config.cucumberOpts.tagExpression = process.env.BROWSERSTACK_TAG_EXPRESSION || defaultTagExpression;
+
+config.waitforTimeout = 10000; // do we need these here as well?
+config.connectionRetryTimeout = 90000; // do we need these here as well?
 config.connectionRetryCount = 3;
-config.cucumberOpts.tagExpression = '@performance and @iosApp'; // pass tag to run tests specific to android
+
+
 config.onPrepare = function (config, capabilities) {
   removeSync('./wdio/reports');
   console.log('Connecting local');
