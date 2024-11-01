7a0b3f4ea44fd3634a9513b45ed69e0f2744ba6a
Nicholas Smith <nick.smith@consensys.net>
2024-10-31 21:13:50 -0500

    chore: fix tsc lint errors

--- app/components/UI/Tokens/types.ts
+++ app/components/UI/Tokens/types.ts
@@ -21,6 +21,6 @@ export interface TokenI {
   balanceFiat: string;
   logo: string | undefined;
   isETH: boolean | undefined;
-  isStaked: boolean | undefined;
-  nativeAsset: TokenI | undefined;
+  isStaked?: boolean | undefined;
+  nativeAsset?: TokenI | undefined;
 }
