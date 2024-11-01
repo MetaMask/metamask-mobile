d85cf48fc71174280513eed9d00089a8f530855f
SamuelSalas <samuel.salas.reyes@gmail.com>
2024-10-31 20:08:34 -0600

    test: Move files to Settings folder (#12135)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    We currently have a few of our page object files lingering outside of a
    specific folder. As part of our ongoing efforts to improve the
    organization and maintainability of our test automation codebase, we
    need to move Page Object files into their respective folders. This will
    help streamline the project structure, making it easier for team members
    to locate and manage the files. The folder where each of the remaining
    files should be moved have to reflect the functionality or feature on
    the app. This same structure should be reflected on the selectors
    folder.
    
    Files to relocate to the Settings folder:
    
    - e2e/pages/modals/AutoLockModal.js
    - e2e/pages/modals/DeleteContactModal.js
    - e2e/pages/modals/DeleteWalletModal.js
    - e2e/pages/modals/FiatOnTestnetsModal.js
    - e2e/pages/modals/SrpQuizModal.js
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    Regression:
    https://app.bitrise.io/app/be69d4368ee7e86d/pipelines/69b40802-723a-4d8f-9282-19eef5a5ce22?tab=workflows
    Appium:
    https://app.bitrise.io/app/be69d4368ee7e86d/pipelines/74059856-9aca-41b0-8a45-058787f10b5c?tab=workflows
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- e2e/selectors/Settings/SecurityAndPrivacy/DeleteWalletModal.selectors.js
+++ e2e/selectors/Settings/SecurityAndPrivacy/DeleteWalletModal.selectors.js
@@ -1,4 +1,4 @@
-import enContent from '../../../locales/languages/en.json';
+import enContent from '../../../../locales/languages/en.json';
 
 export const DeleteWalletModalSelectorsIDs = {
   CONTAINER: 'delete-wallet-modal-container',
