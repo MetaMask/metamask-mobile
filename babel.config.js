e5a890c800fcee48733c43ca6039ed12cacf448b
Matthew Walsh <matthew.walsh@consensys.net>
2024-11-01 11:28:04 +0000

    Upgrade version

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
-  ignore: [/ses\.cjs/],
+  ignore: ['ses.cjs'],
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     'transform-inline-environment-variables',
