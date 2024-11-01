c537fa92b4d2296509247788d93526d17e5a7907
Owen Craston <owengc12@gmail.com>
2024-10-31 17:01:09 -0700

    feat: disable wallet buttons for accounts that cannot sign transactions

--- app/components/UI/WalletAction/WalletAction.tsx
+++ app/components/UI/WalletAction/WalletAction.tsx
@@ -1,6 +1,6 @@
 // Third party dependencies.
-import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import React, { useMemo } from 'react';
+import { TouchableOpacity, View } from 'react-native';
 import { lightTheme } from '@metamask/design-tokens';
 
 // External dependencies.
@@ -8,39 +8,58 @@ import Text, {
   TextVariant,
 } from '../../../component-library/components/Texts/Text';
 import { useStyles } from '../../../component-library/hooks';
+
 // Internal dependencies.
-import { WalletActionProps } from './WalletAction.types';
+import { walletActionDetails, WalletActionProps } from './WalletAction.types';
 import styleSheet from './WalletAction.styles';
 import Avatar, {
   AvatarVariant,
 } from '../../../component-library/components/Avatars/Avatar';
 
 const WalletAction = ({
-  actionTitle,
-  actionDescription,
+  actionType,
   iconName,
   iconSize,
   onPress,
   containerStyle,
   iconStyle,
   actionID,
+  disabled,
   ...props
 }: WalletActionProps) => {
+  const actionStrings = actionType ? walletActionDetails[actionType] : null;
+  const actionTitle = actionStrings?.title ?? '';
+  const actionDescription = actionStrings?.description;
   const { colors } = lightTheme;
   const { styles } = useStyles(styleSheet, {});
+
+  const avatarStyle = useMemo(
+    () => ({
+      ...iconStyle,
+      backgroundColor: disabled ? colors.primary.muted : colors.primary.default,
+    }),
+    [disabled, colors, iconStyle],
+  );
+
+  const touchableStyles = [
+    styles.base,
+    containerStyle,
+    disabled && styles.disabled,
+  ];
+
   return (
     <TouchableOpacity
-      style={{ ...styles.base, ...containerStyle }}
+      style={touchableStyles}
       onPress={onPress}
       testID={actionID}
+      disabled={disabled}
       {...props}
     >
       <Avatar
         variant={AvatarVariant.Icon}
-        style={iconStyle}
+        style={avatarStyle}
         size={iconSize}
         name={iconName}
-        backgroundColor={colors.primary.default}
         iconColor={colors.background.default}
       />
       <View>
