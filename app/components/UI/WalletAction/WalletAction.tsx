abc306aa38c81aacb207ddf5774d16bcb480979a
Owen Craston <owengc12@gmail.com>
2024-11-05 18:58:17 -0800

    move walletActionDetails into component

--- app/components/UI/WalletAction/WalletAction.tsx
+++ app/components/UI/WalletAction/WalletAction.tsx
@@ -8,9 +8,14 @@ import Text, {
   TextVariant,
 } from '../../../component-library/components/Texts/Text';
 import { useStyles } from '../../../component-library/hooks';
+import { strings } from '../../../../locales/i18n';
 
 // Internal dependencies.
-import { walletActionDetails, WalletActionProps } from './WalletAction.types';
+import {
+  WalletActionDetail,
+  WalletActionProps,
+  WalletActionType,
+} from './WalletAction.types';
 import styleSheet from './WalletAction.styles';
 import Avatar, {
   AvatarVariant,
@@ -27,6 +32,39 @@ const WalletAction = ({
   disabled,
   ...props
 }: WalletActionProps) => {
+  const walletActionDetails: Record<WalletActionType, WalletActionDetail> = {
+    [WalletActionType.Buy]: {
+      title: strings('asset_overview.buy_button'),
+      description: strings('asset_overview.buy_description'),
+      disabledDescription: strings('asset_overview.disabled_button.buy'),
+    },
+    [WalletActionType.Sell]: {
+      title: strings('asset_overview.sell_button'),
+      description: strings('asset_overview.sell_description'),
+      disabledDescription: strings('asset_overview.disabled_button.sell'),
+    },
+    [WalletActionType.Swap]: {
+      title: strings('asset_overview.swap'),
+      description: strings('asset_overview.swap_description'),
+      disabledDescription: strings('asset_overview.disabled_button.swap'),
+    },
+    [WalletActionType.Bridge]: {
+      title: strings('asset_overview.bridge'),
+      description: strings('asset_overview.bridge_description'),
+      disabledDescription: strings('asset_overview.disabled_button.bridge'),
+    },
+    [WalletActionType.Send]: {
+      title: strings('asset_overview.send_button'),
+      description: strings('asset_overview.send_description'),
+      disabledDescription: strings('asset_overview.disabled_button.send'),
+    },
+    [WalletActionType.Receive]: {
+      title: strings('asset_overview.receive_button'),
+      description: strings('asset_overview.receive_description'),
+      disabledDescription: strings('asset_overview.disabled_button.receive'),
+    },
+  };
+
   const actionStrings = actionType ? walletActionDetails[actionType] : null;
   const actionTitle = actionStrings?.title ?? '';
   const actionDescription = actionStrings?.description;
--- app/components/UI/WalletAction/WalletAction.types.ts
+++ app/components/UI/WalletAction/WalletAction.types.ts
@@ -1,7 +1,6 @@
 import { ImageStyle, ViewStyle } from 'react-native';
 import { IconName } from '../../../component-library/components/Icons/Icon';
 import { AvatarSize } from '../../../component-library/components/Avatars/Avatar';
-import { strings } from '../../../../locales/i18n';
 
 export enum WalletActionType {
   Buy = 'Buy',
@@ -17,40 +16,6 @@ export interface WalletActionDetail {
   description: string;
   disabledDescription: string;
 }
-
-export const walletActionDetails: Record<WalletActionType, WalletActionDetail> =
-  {
-    [WalletActionType.Buy]: {
-      title: strings('asset_overview.buy_button'),
-      description: strings('asset_overview.buy_description'),
-      disabledDescription: strings('asset_overview.disabled_button.buy'),
-    },
-    [WalletActionType.Sell]: {
-      title: strings('asset_overview.sell_button'),
-      description: strings('asset_overview.sell_description'),
-      disabledDescription: strings('asset_overview.disabled_button.sell'),
-    },
-    [WalletActionType.Swap]: {
-      title: strings('asset_overview.swap'),
-      description: strings('asset_overview.swap_description'),
-      disabledDescription: strings('asset_overview.disabled_button.swap'),
-    },
-    [WalletActionType.Bridge]: {
-      title: strings('asset_overview.bridge'),
-      description: strings('asset_overview.bridge_description'),
-      disabledDescription: strings('asset_overview.disabled_button.bridge'),
-    },
-    [WalletActionType.Send]: {
-      title: strings('asset_overview.send_button'),
-      description: strings('asset_overview.send_description'),
-      disabledDescription: strings('asset_overview.disabled_button.send'),
-    },
-    [WalletActionType.Receive]: {
-      title: strings('asset_overview.receive_button'),
-      description: strings('asset_overview.receive_description'),
-      disabledDescription: strings('asset_overview.disabled_button.receive'),
-    },
-  };
 
 export interface WalletActionProps {
   actionType?: WalletActionType;
