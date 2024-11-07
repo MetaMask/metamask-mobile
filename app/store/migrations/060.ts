8032bd603c188a49d9a655cc71ef522ee23e9670
kylanhurt <kylan.hurt@gmail.com>
2024-11-07 11:01:02 -0600

    Improve conditional for migration 60 (NotificationServicesController

--- app/store/migrations/060.ts
+++ app/store/migrations/060.ts
@@ -1,21 +1,28 @@
 import { ensureValidState } from "./util";
+import { hasProperty, isObject } from '@metamask/utils';
 
 export default function migrate(state: unknown) {
     if (!ensureValidState(state, 60)) {
         return state;
     }
-    if (state.engine.backgroundState.NotificationServicesController) return
-    state.engine.backgroundState.NotificationServicesController = {
-        isCheckingAccountsPresence: false,
-        isFeatureAnnouncementsEnabled: false,
-        isFetchingMetamaskNotifications: false,
-        isMetamaskNotificationsFeatureSeen: false,
-        isNotificationServicesEnabled: false,
-        isUpdatingMetamaskNotifications: false,
-        isUpdatingMetamaskNotificationsAccount: [],
-        metamaskNotificationsList: [],
-        metamaskNotificationsReadList: [],
-        subscriptionAccountsSeen: [],
-      }
+
+    if (
+        !hasProperty(state.engine.backgroundState, 'NotificationServicesController') ||
+        !isObject(state.engine.backgroundState.NotificationServicesController)
+    ) {
+        state.engine.backgroundState.NotificationServicesController = {
+            isCheckingAccountsPresence: false,
+            isFeatureAnnouncementsEnabled: false,
+            isFetchingMetamaskNotifications: false,
+            isMetamaskNotificationsFeatureSeen: false,
+            isNotificationServicesEnabled: false,
+            isUpdatingMetamaskNotifications: false,
+            isUpdatingMetamaskNotificationsAccount: [],
+            metamaskNotificationsList: [],
+            metamaskNotificationsReadList: [],
+            subscriptionAccountsSeen: [],
+        }
+        return
+    }
     return state;
 }
