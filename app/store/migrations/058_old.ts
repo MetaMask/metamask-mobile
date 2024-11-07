7da9caaaf5164237ed5019f8bcab4c5b94c86093
kylanhurt <kylan.hurt@gmail.com>
2024-11-07 12:10:16 -0600

    Add more checks to getTraceTags

--- app/store/migrations/058_old.ts
+++
@@ -1,5 +0,0 @@
-export default function migrate(state) {
-    state.engine.backgroundState.NotificationServicesController = undefined;
-    state.engine.backgroundState.NotificationServicesControllerState = undefined;
-    return state;
-}
