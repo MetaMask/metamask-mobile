c6ba80aa6d49ee9b310da2ed6ec31320199416cc
Nico MASSART <NicolasMassart@users.noreply.github.com>
2024-11-07 18:21:26 +0100

    fix timer error in unit test
    Fixes:
    ReferenceError: You are trying to access a property or method of the Jest environment after it has been torn down. From app/components/Views/QRTabSwitcher/QRTabSwitcher.test.tsx.
    
          at Timeout.now [as _onTimeout] (node_modules/react-native/jest/setup.js:28:41)

--- app/components/Views/QRTabSwitcher/QRTabSwitcher.test.tsx
+++ app/components/Views/QRTabSwitcher/QRTabSwitcher.test.tsx
@@ -35,6 +35,8 @@ jest.mock('@react-navigation/compat', () => {
 jest.mock('../QRScanner', () => jest.fn(() => null));
 jest.mock('../../UI/ReceiveRequest', () => jest.fn(() => null));
 
+jest.useFakeTimers();
+
 describe('QRTabSwitcher', () => {
   it('renders QRScanner by default', () => {
     const { getByText } = render(<QRTabSwitcher />);
