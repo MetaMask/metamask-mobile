88838616908a1aa0410e17e207ef14e16a2b5ea9
Brian Bergeron <brian.e.bergeron@gmail.com>
2024-11-07 15:20:31 -0800

    improve unit test

--- app/components/hooks/usePolling.test.ts
+++ app/components/hooks/usePolling.test.ts
@@ -4,13 +4,13 @@ import usePolling from './usePolling';
 
 describe('usePolling', () => {
 
-  it('Should call startPolling each input, and stopPollingByPollingToken on dismount', async () => {
+  it('Should start/stop polling when inputs are added/removed, and stop on dismount', async () => {
 
     const inputs = ['foo', 'bar'];
     const mockStartPolling = jest.fn().mockImplementation((input) => `${input}_token`);
     const mockStopPollingByPollingToken = jest.fn();
 
-    const { unmount } = renderHook(() =>
+    const { unmount, rerender } = renderHook(() =>
       usePolling({
         startPolling: mockStartPolling,
         stopPollingByPollingToken: mockStopPollingByPollingToken,
@@ -18,12 +18,19 @@ describe('usePolling', () => {
       })
     );
 
+    // All inputs should start polling
     for (const input of inputs) {
       expect(mockStartPolling).toHaveBeenCalledWith(input);
     }
 
-    unmount();
+    // Remove one input, and add another
+    inputs[0] = 'baz';
+    rerender({ input: inputs});
+    expect(mockStopPollingByPollingToken).toHaveBeenCalledWith('foo_token');
+    expect(mockStartPolling).toHaveBeenCalledWith('baz');
 
+    // All inputs should stop polling on dismount
+    unmount();
     for (const input of inputs) {
       expect(mockStopPollingByPollingToken).toHaveBeenCalledWith(`${input}_token`);
     }
