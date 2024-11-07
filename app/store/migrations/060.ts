30e14e5a032e7aa446e3d8cfb64a841ddc1e4413
kylanhurt <kylan.hurt@gmail.com>
2024-11-07 10:56:11 -0600

    Skip migration 60 if NotificationServicesController already exists

--- app/store/migrations/060.ts
+++ app/store/migrations/060.ts
@@ -3,7 +3,8 @@ import { ensureValidState } from "./util";
 export default function migrate(state: unknown) {
     if (!ensureValidState(state, 60)) {
         return state;
-      }
+    }
+    if (state.engine.backgroundState.NotificationServicesController) return
     state.engine.backgroundState.NotificationServicesController = {
         isCheckingAccountsPresence: false,
         isFeatureAnnouncementsEnabled: false,
