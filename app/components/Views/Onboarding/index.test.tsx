312474b01e61d00a0727258e58371f79fb5ba5eb
tommasini <tommasini15@gmail.com>
2024-11-08 16:06:31 +0000

    remove unused imports

--- app/components/Views/Onboarding/index.test.tsx
+++ app/components/Views/Onboarding/index.test.tsx
@@ -1,10 +1,6 @@
 import { renderScreen } from '../../../util/test/renderWithProvider';
 import Onboarding from './';
 import { backgroundState } from '../../../util/test/initial-root-state';
-import { OnboardingSelectorIDs } from '../../../../e2e/selectors/Onboarding/Onboarding.selectors';
-import { fireEvent } from '@testing-library/react-native';
-// eslint-disable-next-line import/no-namespace
-import * as traceObj from '../../../util/trace';
 
 const mockInitialState = {
   engine: {
