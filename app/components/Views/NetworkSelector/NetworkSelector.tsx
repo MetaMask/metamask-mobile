fe546bda58e507a8be8b9ac82068c027d771cd5f
tommasini <46944231+tommasini@users.noreply.github.com>
2024-11-11 20:35:20 +0000

    chore: Remove navigation instrumentation (#12174)
    ## **Description**
    
    This PR removes navigation instrumentation of Sentry configuration.
    
    The reason why we are removing navigation instrumentation is that
    transaction.durations were not showing accurate values, since if a user
    backgrounded the app and opened, that time would be added to the
    transaction.duration, creating issues on our measurements.
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-mobile/issues/12165
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Aslau Mario-Daniel <marioaslau@gmail.com>

--- app/components/Views/NetworkSelector/NetworkSelector.tsx
+++ app/components/Views/NetworkSelector/NetworkSelector.tsx
@@ -89,6 +89,14 @@ import { useNetworkInfo } from '../../../selectors/selectedNetworkController';
 import { NetworkConfiguration } from '@metamask/network-controller';
 import Logger from '../../../util/Logger';
 import RpcSelectionModal from './RpcSelectionModal/RpcSelectionModal';
+import {
+  TraceName,
+  TraceOperation,
+  endTrace,
+  trace,
+} from '../../../util/trace';
+import { getTraceTags } from '../../../util/sentry/tags';
+import { store } from '../../../store';
 
 interface infuraNetwork {
   name: string;
@@ -130,7 +138,11 @@ const NetworkSelector = () => {
 
   // origin is defined if network selector is opened from a dapp
   const origin = route.params?.hostInfo?.metadata?.origin || '';
-
+  const parentSpan = trace({
+    name: TraceName.NetworkSwitch,
+    tags: getTraceTags(store.getState()),
+    op: TraceOperation.NetworkSwitch,
+  });
   const {
     chainId: selectedChainId,
     rpcUrl: selectedRpcUrl,
@@ -234,7 +246,11 @@ const NetworkSelector = () => {
       NetworkController,
       SelectedNetworkController,
     } = Engine.context;
-
+    trace({
+      name: TraceName.SwitchCustomNetwork,
+      parentContext: parentSpan,
+      op: TraceOperation.SwitchCustomNetwork,
+    });
     if (networkConfiguration) {
       const {
         name: nickname,
@@ -261,6 +277,8 @@ const NetworkSelector = () => {
       }
 
       sheetRef.current?.onCloseBottomSheet();
+      endTrace({ name: TraceName.SwitchCustomNetwork });
+      endTrace({ name: TraceName.NetworkSwitch });
       trackEvent(MetaMetricsEvents.NETWORK_SWITCHED, {
         chain_id: getDecimalChainId(chainId),
         from_network: selectedNetworkName,
@@ -346,6 +364,11 @@ const NetworkSelector = () => {
 
   // The only possible value types are mainnet, linea-mainnet, sepolia and linea-sepolia
   const onNetworkChange = (type: InfuraNetworkType) => {
+    trace({
+      name: TraceName.SwitchBuiltInNetwork,
+      parentContext: parentSpan,
+      op: TraceOperation.SwitchBuiltInNetwork,
+    });
     const {
       NetworkController,
       CurrencyRateController,
@@ -383,7 +406,8 @@ const NetworkSelector = () => {
     }
 
     sheetRef.current?.onCloseBottomSheet();
-
+    endTrace({ name: TraceName.SwitchBuiltInNetwork });
+    endTrace({ name: TraceName.NetworkSwitch });
     trackEvent(MetaMetricsEvents.NETWORK_SWITCHED, {
       chain_id: getDecimalChainId(selectedChainId),
       from_network: selectedNetworkName,
