a042551f132033f05397c4156b71f943ef28f494
tommasini <tommasini15@gmail.com>
2024-11-14 11:50:09 +0000

    fix linter

--- app/core/Performance/UIStartup.ts
+++ app/core/Performance/UIStartup.ts
@@ -1,11 +1,9 @@
-import { getTraceTags } from '../../util/sentry/tags';
 import {
   TraceContext,
   TraceName,
   TraceOperation,
   trace,
 } from '../../util/trace';
-import { store } from '../../store';
 
 let UIStartupSpan: TraceContext;
 
