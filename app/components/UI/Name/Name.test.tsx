19c9a7aec764857f3ed62811c3727f12c2c73c26
OGPoyraz <omergoktugpoyraz@gmail.com>
2024-11-19 13:12:18 +0100

    Remove unnecessary comments

--- app/components/UI/Name/Name.test.tsx
+++ app/components/UI/Name/Name.test.tsx
@@ -15,11 +15,6 @@ jest.mock('../../hooks/DisplayName/useDisplayName', () => ({
   default: jest.fn(),
 }));
 
-// jest.mock('../Identicon', () => ({
-//   __esModule: true,
-//   default: () => 'Identicon',
-// }));
-
 const UNKNOWN_ADDRESS_CHECKSUMMED =
   '0x299007B3F9E23B8d432D5f545F8a4a2B3E9A5B4e';
 const EXPECTED_UNKNOWN_ADDRESS_CHECKSUMMED = '0x29900...A5B4e';
