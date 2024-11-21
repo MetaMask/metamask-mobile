0e0b8b004e4145ada375e4914aa6ae279e36aba9
Brian Bergeron <brian.e.bergeron@gmail.com>
2024-11-20 17:34:37 -0800

    feat: upgrade assets controllers to version 43 (#12340)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    Upgrades the assets controllers to versiom 43, which allows polling for
    token detection and token lists across chains.
    
    It also lets us remove the patch for staking.
    
    ## **Related issues**
    
    Depends on: https://github.com/MetaMask/metamask-mobile/pull/12270
    
    ## **Manual testing steps**
    
    With PORTFOLIO_VIEW=false and PORTFOLIO_VIEW=true:
    
    1. Onboard
    2. Verify erc20 tokens are detected correctly
    3. Switch chains
    4. Verify they're detected correctly on the new chain
    
    
    
    ## **Screenshots/Recordings**
    
    No visual changes
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    
    
    https://github.com/user-attachments/assets/709ca6ba-69b8-4ec8-8fce-d358c7f39b27
    
    
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/core/Engine.ts
+++ app/core/Engine.ts
@@ -1546,6 +1546,7 @@ export class Engine {
           ),
         platform: 'mobile',
         useAccountsAPI: true,
+        disabled: false
       }),
 
       new NftDetectionController({
@@ -1919,13 +1920,9 @@ export class Engine {
 
   startPolling() {
     const {
-      TokenDetectionController,
-      TokenListController,
       TransactionController,
     } = this.context;
 
-    TokenListController.start();
-    TokenDetectionController.start();
     // leaving the reference of TransactionController here, rather than importing it from utils to avoid circular dependency
     TransactionController.startIncomingTransactionPolling();
   }
