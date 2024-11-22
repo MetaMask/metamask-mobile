f31a2be2ddfc285c5abc469e4ab9a091e25b21a3
Owen Craston <owengc12@gmail.com>
2024-11-21 17:21:49 -0800

    code fence bitcoin snap

--- app/lib/snaps/preinstalled-snaps.ts
+++ app/lib/snaps/preinstalled-snaps.ts
@@ -4,7 +4,9 @@ import BitcoinSnap from '@metamask/bitcoin-wallet-snap/dist/preinstalled-snap.js
 
 const PREINSTALLED_SNAPS: readonly PreinstalledSnap[] = Object.freeze([
   MessageSigningSnap as PreinstalledSnap,
+  ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
   BitcoinSnap as unknown as PreinstalledSnap,
+  ///: END:ONLY_INCLUDE_IF
 ]);
 
 export default PREINSTALLED_SNAPS;
