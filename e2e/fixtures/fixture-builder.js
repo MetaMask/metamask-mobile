16fb43c4e6344f3f66430eef5073bf52730702a5
Matthew Walsh <matthew.walsh@consensys.net>
2024-11-25 20:44:17 +0000

    Add E2E tests

--- e2e/fixtures/fixture-builder.js
+++ e2e/fixtures/fixture-builder.js
@@ -698,9 +698,8 @@ class FixtureBuilder {
     const { providerConfig } = data;
 
     // Generate a unique key for the new network client ID
-    const newNetworkClientId = `networkClientId${
-      Object.keys(networkController.networkConfigurationsByChainId).length + 1
-    }`;
+    const newNetworkClientId = `networkClientId${Object.keys(networkController.networkConfigurationsByChainId).length + 1
+      }`;
 
     // Define the network configuration
     const networkConfig = {
@@ -780,10 +779,9 @@ class FixtureBuilder {
     const fixtures = this.fixture.state.engine.backgroundState;
 
     // Generate a unique key for the new network client ID
-    const newNetworkClientId = `networkClientId${
-      Object.keys(fixtures.NetworkController.networkConfigurationsByChainId)
-        .length + 1
-    }`;
+    const newNetworkClientId = `networkClientId${Object.keys(fixtures.NetworkController.networkConfigurationsByChainId)
+      .length + 1
+      }`;
 
     // Define the Ganache network configuration
     const ganacheNetworkConfig = {
@@ -819,10 +817,9 @@ class FixtureBuilder {
     const sepoliaConfig = CustomNetworks.Sepolia.providerConfig;
 
     // Generate a unique key for the new network client ID
-    const newNetworkClientId = `networkClientId${
-      Object.keys(fixtures.NetworkController.networkConfigurationsByChainId)
-        .length + 1
-    }`;
+    const newNetworkClientId = `networkClientId${Object.keys(fixtures.NetworkController.networkConfigurationsByChainId)
+      .length + 1
+      }`;
 
     // Define the Sepolia network configuration
     const sepoliaNetworkConfig = {
@@ -872,9 +869,8 @@ class FixtureBuilder {
       } = network.providerConfig;
 
       // Generate a unique key for the new network client ID
-      const newNetworkClientId = `networkClientId${
-        Object.keys(networkConfigurationsByChainId).length + 1
-      }`;
+      const newNetworkClientId = `networkClientId${Object.keys(networkConfigurationsByChainId).length + 1
+        }`;
 
       // Define the network configuration
       const networkConfig = {
@@ -944,6 +940,30 @@ class FixtureBuilder {
       ],
       vault:
         '{"cipher":"vxFqPMlClX2xjUidoCTiwazr43W59dKIBp6ihT2lX66q8qPTeBRwv7xgBaGDIwDfk4DpJ3r5FBety1kFpS9ni3HtcoNQsDN60Pa80L94gta0Fp4b1jVeP8EJ7Ho71mJ360aDFyIgxPBSCcHWs+l27L3WqF2VpEuaQonK1UTF7c3WQ4pyio4jMAH9x2WQtB11uzyOYiXWmiD3FMmWizqYZY4tHuRlzJZTWrgE7njJLaGMlMmw86+ZVkMf55jryaDtrBVAoqVzPsK0bvo1cSsonxpTa6B15A5N2ANyEjDAP1YVl17roouuVGVWZk0FgDpP82i0YqkSI9tMtOTwthi7/+muDPl7Oc7ppj9LU91JYH6uHGomU/pYj9ufrjWBfnEH/+ZDvPoXl00H1SmX8FWs9NvOg7DZDB6ULs4vAi2/5KGs7b+Td2PLmDf75NKqt03YS2XeRGbajZQ/jjmRt4AhnWgnwRzsSavzyjySWTWiAgn9Vp/kWpd70IgXWdCOakVf2TtKQ6cFQcAf4JzP+vqC0EzgkfbOPRetrovD8FHEFXQ+crNUJ7s41qRw2sketk7FtYUDCz/Junpy5YnYgkfcOTRBHAoOy6BfDFSncuY+08E6eiRHzXsXtbmVXenor15pfbEp/wtfV9/vZVN7ngMpkho3eGQjiTJbwIeA9apIZ+BtC5b7TXWLtGuxSZPhomVkKvNx/GNntjD7ieLHvzCWYmDt6BA9hdfOt1T3UKTN4yLWG0v+IsnngRnhB6G3BGjJHUvdR6Zp5SzZraRse8B3z5ixgVl2hBxOS8+Uvr6LlfImaUcZLMMzkRdKeowS/htAACLowVJe3pU544IJ2CGTsnjwk9y3b5bUJKO3jXukWjDYtrLNKfdNuQjg+kqvIHaCQW40t+vfXGhC5IDBWC5kuev4DJAIFEcvJfJgRrm8ua6LrzEfH0GuhjLwYb+pnQ/eg8dmcXwzzggJF7xK56kxgnA4qLtOqKV4NgjVR0QsCqOBKb3l5LQMlSktdfgp9hlW","iv":"b09c32a79ed33844285c0f1b1b4d1feb","keyMetadata":{"algorithm":"PBKDF2","params":{"iterations":5000}},"lib":"original","salt":"GYNFQCSCigu8wNp8cS8C3w=="}',
+    });
+    return this;
+  }
+
+  withToken(token) {
+    merge(this.fixture.state.engine.backgroundState.TokensController, {
+      tokens: [token],
+    });
+    return this;
+  }
+
+  withIncomingTransactionPreferences(incomingTransactionPreferences) {
+    merge(
+      this.fixture.state.engine.backgroundState.PreferencesController,
+      {
+        showIncomingTransactions: incomingTransactionPreferences,
+      },
+    );
+    return this;
+  }
+
+  withTransactions(transactions) {
+    merge(this.fixture.state.engine.backgroundState.TransactionController, {
+      transactions,
     });
     return this;
   }
--- e2e/specs/wallet/incoming-transactions.spec.js
+++ e2e/specs/wallet/incoming-transactions.spec.js
@@ -8,11 +8,13 @@ import { withFixtures } from '../../fixtures/fixture-helper';
 import FixtureBuilder, { DEFAULT_FIXTURE_ACCOUNT } from '../../fixtures/fixture-builder';
 import TabBarComponent from '../../pages/TabBarComponent';
 import ActivitiesView from '../../pages/Transactions/ActivitiesView';
-import Matchers from '../../utils/Matchers';
+
+const TOKEN_SYMBOL_MOCK = 'ABC';
+const TOKEN_ADDRESS_MOCK = '0x123';
 
 const RESPONSE_STANDARD_MOCK = {
-    hash: '0x1',
-    timestamp: new Date(123456789123).toISOString(),
+    hash: '0x123456',
+    timestamp: new Date().toISOString(),
     chainId: 1,
     blockNumber: 1,
     blockHash: '0x2',
@@ -32,16 +34,37 @@ const RESPONSE_STANDARD_MOCK = {
 
 const RESPONSE_STANDARD_2_MOCK = {
     ...RESPONSE_STANDARD_MOCK,
-    timestamp: new Date(987654321987).toISOString(),
+    timestamp: new Date().toISOString(),
     hash: '0x2',
     value: '2340000000000000000',
 };
 
-function mockAccountsApi() {
+const RESPONSE_TOKEN_TRANSFER_MOCK = {
+    ...RESPONSE_STANDARD_MOCK,
+    to: '0x2',
+    valueTransfers: [
+        {
+            contractAddress: TOKEN_ADDRESS_MOCK,
+            decimal: 18,
+            symbol: TOKEN_SYMBOL_MOCK,
+            from: '0x2',
+            to: DEFAULT_FIXTURE_ACCOUNT.toLowerCase(),
+            amount: '4560000000000000000',
+        },
+    ],
+};
+
+const RESPONSE_OUTGOING_TRANSACTION_MOCK = {
+    ...RESPONSE_STANDARD_MOCK,
+    to: '0x2',
+    from: DEFAULT_FIXTURE_ACCOUNT.toLowerCase(),
+};
+
+function mockAccountsApi(transactions) {
     return {
         urlEndpoint: `https://accounts.api.cx.metamask.io/v1/accounts/${DEFAULT_FIXTURE_ACCOUNT}/transactions?networks=0x1&sortDirection=ASC`,
         response: {
-            data: [RESPONSE_STANDARD_MOCK, RESPONSE_STANDARD_2_MOCK],
+            data: transactions ?? [RESPONSE_STANDARD_MOCK, RESPONSE_STANDARD_2_MOCK],
             pageInfo: {
                 count: 2,
                 hasNextPage: false
@@ -55,10 +78,9 @@ describe(SmokeCore('Incoming Transactions'), () => {
     beforeAll(async () => {
         jest.setTimeout(2500000);
         await TestHelpers.reverseServerPort();
-        await startMockServer({ GET: [mockAccountsApi()] });
     });
 
-    afterAll(async () => {
+    afterEach(async () => {
         try {
             await stopMockServer();
         } catch (error) {
@@ -67,7 +89,9 @@ describe(SmokeCore('Incoming Transactions'), () => {
         }
     });
 
-    it('should display incoming transaction', async () => {
+    it('displays standard incoming transaction', async () => {
+        await startMockServer({ GET: [mockAccountsApi()] });
+
         await withFixtures(
             {
                 fixture: new FixtureBuilder().build(),
@@ -80,6 +104,91 @@ describe(SmokeCore('Incoming Transactions'), () => {
                 await Assertions.checkIfTextIsDisplayed('Received ETH');
                 await Assertions.checkIfTextIsDisplayed(/.*1\.23 ETH.*/);
                 await Assertions.checkIfTextIsDisplayed(/.*2\.34 ETH.*/);
+            }
+        );
+    });
+
+    it('displays incoming token transfers', async () => {
+        await startMockServer({ GET: [mockAccountsApi([RESPONSE_TOKEN_TRANSFER_MOCK])] });
+
+        await withFixtures(
+            {
+                fixture: new FixtureBuilder().withToken({
+                    address: TOKEN_ADDRESS_MOCK, decimals: 18, symbol: TOKEN_SYMBOL_MOCK
+                }).build(),
+                restartDevice: true
+            },
+            async () => {
+                await loginToApp();
+                await TabBarComponent.tapActivity();
+                await ActivitiesView.swipeDown();
+                await Assertions.checkIfTextIsDisplayed('Received ABC');
+                await Assertions.checkIfTextIsDisplayed(/.*4\.56 ABC.*/);
+            }
+        );
+    });
+
+    it('displays outgoing transactions', async () => {
+        await startMockServer({ GET: [mockAccountsApi([RESPONSE_OUTGOING_TRANSACTION_MOCK])] });
+
+        await withFixtures(
+            {
+                fixture: new FixtureBuilder().build(),
+                restartDevice: true
+            },
+            async () => {
+                await loginToApp();
+                await TabBarComponent.tapActivity();
+                await ActivitiesView.swipeDown();
+                await Assertions.checkIfTextIsDisplayed('Sent ETH');
+                await Assertions.checkIfTextIsDisplayed(/.*1\.23 ETH.*/);
+            }
+        );
+    });
+
+    it('displays nothing if incoming transactions disabled', async () => {
+        await startMockServer({ GET: [mockAccountsApi()] });
+
+        await withFixtures(
+            {
+                fixture: new FixtureBuilder()
+                    .withIncomingTransactionPreferences({
+                        '0x1': false
+                    })
+                    .build(),
+                restartDevice: true
+            },
+            async () => {
+                await loginToApp();
+                await TabBarComponent.tapActivity();
+                await ActivitiesView.swipeDown();
+                await TestHelpers.delay(2000);
+                await Assertions.checkIfTextIsNotDisplayed('Received ETH');
+            }
+        );
+    });
+
+    it('displays nothing if incoming transaction is a duplicate', async () => {
+        await startMockServer({ GET: [mockAccountsApi([RESPONSE_STANDARD_MOCK])] });
+
+        await withFixtures(
+            {
+                fixture: new FixtureBuilder()
+                    .withTransactions([{
+                        hash: RESPONSE_STANDARD_MOCK.hash,
+                        txParams: {
+                            from: RESPONSE_STANDARD_MOCK.from
+                        }
+                    }])
+                    .build(),
+                restartDevice: true
+            },
+            async () => {
+                await loginToApp();
+                await TabBarComponent.tapActivity();
+                await ActivitiesView.swipeDown();
+                await TestHelpers.delay(2000);
+                await Assertions.checkIfTextIsNotDisplayed('Received ETH');
             }
         );
     });
