a8435c1629f904998ae5656ea20eb94480e209cf
Jongsun Suh <jongsun.suh@icloud.com>
2024-11-26 11:44:53 -0500

    Add missing `SnapsRegistry` property to Engine context

--- app/core/Engine/Engine.ts
+++ app/core/Engine/Engine.ts
@@ -1421,6 +1421,7 @@ export class Engine {
       LoggingController: loggingController,
       ///: BEGIN:ONLY_INCLUDE_IF(preinstalled-snaps,external-snaps)
       SnapController: this.snapController,
+      SnapsRegistry: snapsRegistry,
       SubjectMetadataController: this.subjectMetadataController,
       AuthenticationController: authenticationController,
       UserStorageController: userStorageController,
