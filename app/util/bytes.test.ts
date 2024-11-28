9fdc49b8e21dcb356d6c123cabb62a8411571454
tommasini <46944231+tommasini@users.noreply.github.com>
2024-11-28 15:33:30 +0000

    Update app/util/bytes.test.ts
    Co-authored-by: Nico MASSART <NicolasMassart@users.noreply.github.com>

--- app/util/bytes.test.ts
+++ app/util/bytes.test.ts
@@ -1,7 +1,7 @@
 import { bytesToBits } from './bytes';
 
 describe('bytesToBits', () => {
-  it('should correctly convert bytes to bits', () => {
+  it('converts bytes length to bits length', () => {
     expect(bytesToBits(1)).toBe(8);
     expect(bytesToBits(2)).toBe(16);
     expect(bytesToBits(32)).toBe(256);
