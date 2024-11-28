29082aa611de80e79fa355540dd6951d3df42b82
EtherWizard33 <eric.lamontagne@consensys.net>
2024-11-28 16:49:54 -0500

    test: e2e scenario where the user connects an in app browser dapp by granting permission to one account and one chain

---
+++ e2e/specs/multichain/permission-system-initial-permissions-grant-one-account-and-one-chain.spec.js
@@ -0,0 +1,45 @@
+'use strict';
+import TestHelpers from '../../helpers';
+import { SmokeCore } from '../../tags';
+import Browser from '../../pages/Browser/BrowserView';
+import TabBarComponent from '../../pages/TabBarComponent';
+import NetworkListModal from '../../pages/Network/NetworkListModal';
+import TestDApp from '../../pages/Browser/TestDApp';
+import ConnectedAccountsModal from '../../pages/Browser/ConnectedAccountsModal';
+import FixtureBuilder from '../../fixtures/fixture-builder';
+import { withFixtures } from '../../fixtures/fixture-helper';
+import { loginToApp } from '../../viewHelper';
+import Assertions from '../../utils/Assertions';
+import ConnectBottomSheet from '../../pages/Browser/ConnectBottomSheet';
+
+describe(SmokeCore('MultiChain Permissions System:'), () => {
+  beforeAll(async () => {
+    jest.setTimeout(150000);
+    await TestHelpers.reverseServerPort();
+  });
+
+  it('should grant initial permissions for one account and one chain', async () => {
+    await withFixtures(
+      {
+        dapp: true,
+        fixture: new FixtureBuilder()
+          .withPermissionController()
+          // .withChainPermission()
+          .build(),
+        restartDevice: true,
+      },
+      async () => {
+        await loginToApp();
+        await TabBarComponent.tapBrowser();
+        await Assertions.checkIfVisible(Browser.browserScreenID);
+
+        await Browser.navigateToTestDApp();
+        await TestDApp.connect();
+        await ConnectBottomSheet.tapConnectButton();
+
+        await Browser.tapNetworkAvatarButtonOnBrowser();
+        await Assertions.checkIfVisible(ConnectedAccountsModal.title);
+      },
+    );
+  });
+});
