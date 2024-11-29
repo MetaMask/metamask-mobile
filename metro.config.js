e54d05d551d5522926bd7309df34670be4a84736
João Loureiro <175489935+joaoloureirop@users.noreply.github.com>
2024-11-29 09:54:31 +0000

    Revert "setup metro bundler symlink resolution"
    This reverts commit 00a3bd164f163a714d4e15e921dbb300b1fc70fb.

--- metro.config.js
+++ metro.config.js
@@ -8,18 +8,6 @@
 
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-const path = require("path");
-
-const featureFlagModuleDir = path.resolve(__dirname, "../../core/feature-flags/packages/remote-feature-flag-controller");
-
-const extraNodeModules = {
-  "@metamask/remote-feature-flag-controller": featureFlagModuleDir,
-};
-
-const watchFolders = [
-  featureFlagModuleDir,
-];
-
 module.exports = function (baseConfig) {
   const defaultConfig = mergeConfig(baseConfig, getDefaultConfig(__dirname));
   const {
@@ -27,16 +15,10 @@ module.exports = function (baseConfig) {
   } = defaultConfig;
 
   return mergeConfig(defaultConfig, {
-    watchFolders,
     resolver: {
       assetExts: assetExts.filter((ext) => ext !== 'svg'),
       sourceExts: [...sourceExts, 'svg', 'cjs', 'mjs'],
       resolverMainFields: ['sbmodern', 'react-native', 'browser', 'main'],
-      extraNodeModules: new Proxy (extraNodeModules, {
-        get: (target, name) =>
-          name in target ? target[name] : path.join(process.cwd(), `node_modules/${name}`),
-      }),
-      unstable_enableSymlinks: true,
     },
     transformer: {
       babelTransformerPath: require.resolve('./metro.transform.js'),
