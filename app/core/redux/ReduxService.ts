c849f8a478488fcda06904e43eff8dcbc7c95acb
Cal-L <cleun007@gmail.com>
2024-12-02 14:01:20 -0800

    Create ReduxService to manage Redux store

---
+++ app/core/redux/ReduxService.ts
@@ -0,0 +1,44 @@
+import { ReduxStore } from './types';
+
+/**
+ * ReduxService class that manages the Redux store
+ */
+class ReduxService {
+  static reduxStore: ReduxStore;
+
+  static #assertReduxStoreType(store: ReduxStore) {
+    if (
+      typeof store.dispatch !== 'function' ||
+      typeof store.getState !== 'function'
+    ) {
+      throw new Error('Redux store is not a valid store!');
+    }
+    return this.reduxStore;
+  }
+
+  static #assertReduxStoreExists() {
+    if (!this.reduxStore) {
+      throw new Error('Redux store does not exist!');
+    }
+    return this.reduxStore;
+  }
+
+  /**
+   * Set the store in the Redux class
+   * @param store
+   */
+  static set store(store: ReduxStore) {
+    this.#assertReduxStoreType(store);
+    this.reduxStore = store;
+  }
+
+  /**
+   * Get the store from the Redux class
+   */
+  static get store() {
+    this.#assertReduxStoreExists();
+    return this.reduxStore;
+  }
+}
+
+export default ReduxService;
---
+++ app/core/redux/types.ts
@@ -0,0 +1,4 @@
+import { Store } from 'redux';
+import { RootState } from '../../reducers';
+
+export type ReduxStore = Store<RootState, any>;
