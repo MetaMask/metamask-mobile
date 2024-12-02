1a79d12683b842b7c5b39d77149c829ed18ce240
Jonathan Bursztyn <jonathan@bursztyn.io>
2024-12-02 15:57:28 +0000

    Add test

--- app/components/UI/ReceiveRequest/index.test.tsx
+++ app/components/UI/ReceiveRequest/index.test.tsx
@@ -1,10 +1,21 @@
 import { cloneDeep } from 'lodash';
 import { RpcEndpointType } from '@metamask/network-controller';
+import { fireEvent } from '@testing-library/react-native';
+import { StackNavigationOptions } from '@react-navigation/stack';
 import ReceiveRequest from './';
 import { renderScreen } from '../../../util/test/renderWithProvider';
 import { backgroundState } from '../../../util/test/initial-root-state';
 import { MOCK_ACCOUNTS_CONTROLLER_STATE } from '../../../util/test/accountsControllerTestUtils';
 import { mockNetworkState } from '../../../util/test/network';
+
+interface ScreenParams {
+  name: string;
+  options?: StackNavigationOptions;
+  metrics?: {
+    trackEvent: jest.Mock;
+    createEventBuilder: jest.Mock;
+  };
+}
 
 const initialState = {
   engine: {
@@ -54,6 +65,37 @@ describe('ReceiveRequest', () => {
       { state: initialState },
     );
     expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('should track metrics when onReceive is called', () => {
+    const mockTrackEvent = jest.fn();
+    const mockCreateEventBuilder = jest.fn().mockReturnValue({
+      addProperties: jest.fn().mockReturnValue({
+        build: jest.fn(),
+      }),
+      build: jest.fn(),
+    });
+
+    const { getByTestId } = renderScreen(
+      ReceiveRequest,
+      {
+        name: 'ReceiveRequest',
+        metrics: {
+          trackEvent: mockTrackEvent,
+          createEventBuilder: mockCreateEventBuilder,
+        },
+      } as ScreenParams,
+      { state: initialState },
+    );
+
+    const requestButton = getByTestId('request-payment-button');
+    fireEvent.press(requestButton);
+
+    expect(mockCreateEventBuilder).toHaveBeenCalledWith('ReceiveRequest');
+    expect(mockCreateEventBuilder).toHaveBeenCalledWith(
+      'RECEIVE_OPTIONS_PAYMENT_REQUEST',
+    );
+    expect(mockTrackEvent).toHaveBeenCalledTimes(2);
   });
 
   it('render with different ticker matches snapshot', () => {
