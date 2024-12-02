bf62cbaefca6a81ae4a7906c51081f6c74eb7b69
João Loureiro <175489935+joaoloureirop@users.noreply.github.com>
2024-12-02 18:18:51 +0000

    feat: implement remote feature flag controller (#12427)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    Introduction of `@metamask/remote-feature-flag-controller` library.
    
    Remote feature flag controller manages data flow, retry policy, and
    cache expiry.
    The controller consumer manages default values, data persistency, and
    data distribution.
    
    See
    [ADR](https://github.com/MetaMask/decisions/blob/b3094d47a568ac1e076a44fa704c2d29d1b59f35/decisions/wallet-platform/0001-remote-rollout-feature-flags.md)
    for a in-depth description
    
    ## Technical decisions
    
    ### Controller init on `Engine.ts` with feature flags fetching only on
    cold app starts.
    
    `@metamask/remote-feature-flag-controller` is only asked to fetch
    feature flags after its init in `Engine.ts`. Ensures feature flags are
    only fetched on cold app starts.
    
    ### Fallback values
    
    Default values are used when remote feature flags are undefined.
    The fallback mechanism is implemented by each feature flag selector
    `app/selectors/featureFlagsController/<featureFlagName>`
    
    In this PR we include `minimumAppVersion` selector, which manages the LD
    feature flag `mobile-minimum-versions`
    
    ### One selector per each feature flag
    
    [LD feature flags can be boolean, number, strings on JSON
    objects](https://docs.launchdarkly.com/sdk/concepts/flag-types#understanding-flag-types).
    We've decided to have each feature flag with its own selector
    
    A feature flag selector contains:
    - state selectors for each feature flag value.
    - business logic
    - defaults for when feature flags values are undefined.
    - TS types.
    - unit tests and mocked data.
    
    This architecture offers a clear separation between each feature flag
    and the logic behind it, allowing easier manipulation.
    Code owners are assigned to each feature flag.
    
    ### 
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/mobile-planning/issues/2054
    Fixes: https://github.com/MetaMask/mobile-planning/issues/1975
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Nico MASSART <NicolasMassart@users.noreply.github.com>
    Co-authored-by: tommasini <46944231+tommasini@users.noreply.github.com>

--- app/core/Engine/types.ts
+++ app/core/Engine/types.ts
@@ -159,6 +159,10 @@ import {
   AccountsControllerState,
 } from '@metamask/accounts-controller';
 import { getPermissionSpecifications } from '../Permissions/specifications.js';
+import {
+  RemoteFeatureFlagController,
+  RemoteFeatureFlagControllerState
+} from '@metamask/remote-feature-flag-controller';
 
 /**
  * Controllers that area always instantiated
@@ -292,6 +296,7 @@ export interface Controllers {
   SelectedNetworkController: SelectedNetworkController;
   PhishingController: PhishingController;
   PreferencesController: PreferencesController;
+  RemoteFeatureFlagController: RemoteFeatureFlagController;
   PPOMController: PPOMController;
   TokenBalancesController: TokenBalancesController;
   TokenListController: TokenListController;
@@ -330,6 +335,7 @@ export interface EngineState {
   KeyringController: KeyringControllerState;
   NetworkController: NetworkState;
   PreferencesController: PreferencesState;
+  RemoteFeatureFlagController: RemoteFeatureFlagControllerState;
   PhishingController: PhishingControllerState;
   TokenBalancesController: TokenBalancesControllerState;
   TokenRatesController: TokenRatesControllerState;
