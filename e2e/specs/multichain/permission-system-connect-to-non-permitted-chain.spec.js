c4c087bbd523939fafc683255771673cda95070b
EtherWizard33 <eric.lamontagne@consensys.net>
2024-12-02 16:15:44 -0500

    refactor: split non-permitted flow e2e test into 3 smaller tests

--- e2e/specs/multichain/permission-system-connect-to-non-permitted-chain.spec.js
+++ e2e/specs/multichain/permission-system-connect-to-non-permitted-chain.spec.js
@@ -1,4 +1,4 @@
-import { Regression } from '../../tags';
+import { SmokeCore } from '../../tags';
 import { loginToApp } from '../../viewHelper';
 import WalletView from '../../pages/wallet/WalletView';
 import NetworkListModal from '../../pages/Network/NetworkListModal';
@@ -15,9 +15,9 @@ import NetworkNonPemittedBottomSheet from '../../pages/Network/NetworkNonPemitte
 import ConnectedAccountsModal from '../../pages/Browser/ConnectedAccountsModal';
 
 const fixtureServer = new FixtureServer();
-const ETHEREUM = 'Ethereum Main Network';
+const SEPOLIA = CustomNetworks.Sepolia.providerConfig.nickname;
 
-describe(Regression('Connect to a Test Network'), () => {
+describe(SmokeCore('Network Permission System, non-permitted chain'), () => {
   beforeAll(async () => {
     await TestHelpers.reverseServerPort();
   });
@@ -30,7 +30,7 @@ describe(Regression('Connect to a Test Network'), () => {
     await stopFixtureServer(fixtureServer);
   });
 
-  it('should switch to test Network then dismiss the network education modal', async () => {
+  it('should show bottom sheet when switching to non-permitted network', async () => {
     await withFixtures(
       {
         dapp: true,
@@ -43,60 +43,90 @@ describe(Regression('Connect to a Test Network'), () => {
       async () => {
         await loginToApp();
         await TabBarComponent.tapBrowser();
-        await Assertions.checkIfVisible(Browser.browserScreenID);
         await Browser.navigateToTestDApp();
 
+        // Switch to non-permitted network
         await TabBarComponent.tapWallet();
-        await Assertions.checkIfVisible(WalletView.container);
         await WalletView.tapNetworksButtonOnNavBar();
         await NetworkListModal.scrollToBottomOfNetworkList();
-        await Assertions.checkIfVisible(NetworkListModal.networkScroll);
-        await Assertions.checkIfToggleIsOn(NetworkListModal.testNetToggle);
-        await NetworkListModal.changeNetworkTo(
-          CustomNetworks.Sepolia.providerConfig.nickname,
-        );
-        await Assertions.checkIfVisible(NetworkEducationModal.container);
-        await Assertions.checkIfElementToHaveText(
-          NetworkEducationModal.networkName,
-          CustomNetworks.Sepolia.providerConfig.nickname,
-        );
+        await NetworkListModal.changeNetworkTo(SEPOLIA);
         await NetworkEducationModal.tapGotItButton();
-        await Assertions.checkIfNotVisible(NetworkEducationModal.container);
-        await Assertions.checkIfVisible(WalletView.container);
 
-        const networkPicker = await WalletView.getNavbarNetworkPicker();
-        await Assertions.checkIfElementHasLabel(
-          networkPicker,
-          CustomNetworks.Sepolia.providerConfig.nickname,
-        );
-
+        // Verify bottom sheet appears
         await TabBarComponent.tapBrowser();
         await Assertions.checkIfVisible(
           NetworkNonPemittedBottomSheet.addThisNetworkTitle,
         );
+      },
+    );
+  });
+
+  it('should NOT show bottom sheet when permission was already granted', async () => {
+    await withFixtures(
+      {
+        dapp: true,
+        fixture: new FixtureBuilder()
+          .withPermissionControllerConnectedToTestDapp()
+          .withChainPermission(['0x1', '0xaa36a7'])
+          .build(),
+        restartDevice: true,
+      },
+      async () => {
+        await loginToApp();
+        await TabBarComponent.tapBrowser();
+        await Browser.navigateToTestDApp();
+
+        // Switch to already permitted network
+        await TabBarComponent.tapWallet();
+        await WalletView.tapNetworksButtonOnNavBar();
+        await NetworkListModal.scrollToBottomOfNetworkList();
+        await NetworkListModal.changeNetworkTo(SEPOLIA);
+        await NetworkEducationModal.tapGotItButton();
+
+        // Verify no bottom sheet appears
+        await TabBarComponent.tapBrowser();
+        await Assertions.checkIfNotVisible(
+          NetworkNonPemittedBottomSheet.addThisNetworkTitle,
+        );
+      },
+    );
+  });
+
+  it('should add network permission when requested', async () => {
+    await withFixtures(
+      {
+        dapp: true,
+        fixture: new FixtureBuilder()
+          .withPermissionControllerConnectedToTestDapp()
+          .withChainPermission()
+          .build(),
+        restartDevice: true,
+      },
+      async () => {
+        await loginToApp();
+        await TabBarComponent.tapBrowser();
+        await Browser.navigateToTestDApp();
+
+        // Switch to non-permitted network
+        await TabBarComponent.tapWallet();
+        await WalletView.tapNetworksButtonOnNavBar();
+        await NetworkListModal.scrollToBottomOfNetworkList();
+        await NetworkListModal.changeNetworkTo(SEPOLIA);
+        await NetworkEducationModal.tapGotItButton();
+
+        // Add network permission
+        await TabBarComponent.tapBrowser();
         await NetworkNonPemittedBottomSheet.tapAddThisNetworkButton();
 
+        // Verify permission was added by checking that disconnecting both networks shows disconnect all button
         await Browser.tapNetworkAvatarButtonOnBrowser();
         await ConnectedAccountsModal.tapManagePermissionsButton();
         await ConnectedAccountsModal.tapNavigateToEditNetworksPermissionsButton();
-        await Assertions.checkIfVisible(
-          NetworkNonPemittedBottomSheet.sepoliaNetworkName,
-        );
-
-        // Check the permission was added
-        // TODO: add isCheckboxChecked to test helpers,
-        // Until then, find a workaround to confirm the perm was indeed added
-        // For example, if we uncheck Sepolia, and mainnet, we expect no networks permissions to be checked, and the disconnect all button should show
         await NetworkNonPemittedBottomSheet.tapSepoliaNetworkName();
         await NetworkNonPemittedBottomSheet.tapEthereumMainNetNetworkName();
-        await ConnectedAccountsModal.tapDisconnectNetworksButton();
-        await ConnectedAccountsModal.tapConfirmDisconnectNetworksButton();
-
-        await Browser.tapNetworkAvatarButtonOnBrowser();
-        await Assertions.checkIfNotVisible(ConnectedAccountsModal.title);
-        await Assertions.checkIfVisible(NetworkListModal.networkScroll);
-        await NetworkListModal.swipeToDismissModal();
-        await Assertions.checkIfNotVisible(NetworkListModal.networkScroll);
+        await Assertions.checkIfVisible(
+          ConnectedAccountsModal.disconnectNetworksButton,
+        );
       },
     );
   });
