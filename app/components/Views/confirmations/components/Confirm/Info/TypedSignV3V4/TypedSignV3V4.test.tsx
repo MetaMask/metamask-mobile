c2b8851a040615a56a67b3e154fcb1f73f055884
Jyoti Puri <jyotipuri@gmail.com>
2024-12-02 18:21:57 +0530

    update

--- app/components/Views/confirmations/components/Confirm/Info/TypedSignV3V4/TypedSignV3V4.test.tsx
+++ app/components/Views/confirmations/components/Confirm/Info/TypedSignV3V4/TypedSignV3V4.test.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import renderWithProvider from '../../../../../../../util/test/renderWithProvider';
-import { typedSignV1ConfirmationState } from '../../../../../../../util/test/confirm-data-helpers';
+import { typedSignV3ConfirmationState } from '../../../../../../../util/test/confirm-data-helpers';
 import TypedSignV1 from './TypedSignV3V4';
 
 describe('TypedSignV3V4', () => {
   it('should contained required text', async () => {
     const { getByText } = renderWithProvider(<TypedSignV1 />, {
-      state: typedSignV1ConfirmationState,
+      state: typedSignV3ConfirmationState,
     });
     expect(getByText('Request from')).toBeDefined();
     expect(getByText('metamask.github.io')).toBeDefined();
