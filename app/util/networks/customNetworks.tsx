e757ad1ebd4d54271d08012f54ccd52e767c9e04
salimtb <salim.toubal@outlook.com>
2024-12-04 10:37:10 +0100

    feat: migrate Base network RPC from mainnet.base.org to base-mainnet.infura.io

--- app/util/networks/customNetworks.tsx
+++ app/util/networks/customNetworks.tsx
@@ -44,7 +44,7 @@ export const PopularList = [
   {
     chainId: toHex('8453'),
     nickname: 'Base',
-    rpcUrl: `https://mainnet.base.org`,
+    rpcUrl: `https://base-mainnet.infura.io/v3/${infuraProjectId}`,
     ticker: 'ETH',
     warning: true,
     rpcPrefs: {
@@ -98,6 +98,77 @@ export const PopularList = [
       imageSource: require('../../images/zk-sync.png'),
     },
   },
+];
+
+export const INFURA_TESTNET_CHAIN_IDS = {
+  SEPOLIA: '0xaa36a7',
+  HOLESKY: '0x4268',
+  LINEA_SEPOLIA: '0xe705',
+  AMOY: '0x13882',
+  BASE_SEPOLIA: '0x14a34',
+  OPTIMISM_SEPOLIA: '0xaa37dc',
+  ARBITRUM_SEPOLIA: '0x66eee',
+  PALM_TESTNET: '0x2a15c3083',
+  AVALANCHE_TESTNET: '0xa869',
+  CELO_TESTNET: '0xaef3',
+  ZK_SYNC_ERA_TESTNET: '0x12c',
+  BSC_TESTNET: '0x61',
+  MANTA_SEPOLIA: '0x138b',
+  OPBNB_TESTNET: '0x15eb',
+  SCROLL_SEPOLIA: '0x8274f',
+  UNICHAIN_SEPOLIA: '0x515',
+} as const;
+
+export const infuraChainIdsTestNets: string[] = [
+  INFURA_TESTNET_CHAIN_IDS.SEPOLIA,
+  INFURA_TESTNET_CHAIN_IDS.HOLESKY,
+  INFURA_TESTNET_CHAIN_IDS.LINEA_SEPOLIA,
+  INFURA_TESTNET_CHAIN_IDS.AMOY,
+  INFURA_TESTNET_CHAIN_IDS.BASE_SEPOLIA,
+  INFURA_TESTNET_CHAIN_IDS.OPTIMISM_SEPOLIA,
+  INFURA_TESTNET_CHAIN_IDS.ARBITRUM_SEPOLIA,
+  INFURA_TESTNET_CHAIN_IDS.PALM_TESTNET,
+  INFURA_TESTNET_CHAIN_IDS.AVALANCHE_TESTNET,
+  INFURA_TESTNET_CHAIN_IDS.CELO_TESTNET,
+  INFURA_TESTNET_CHAIN_IDS.ZK_SYNC_ERA_TESTNET,
+  INFURA_TESTNET_CHAIN_IDS.BSC_TESTNET,
+  INFURA_TESTNET_CHAIN_IDS.MANTA_SEPOLIA,
+  INFURA_TESTNET_CHAIN_IDS.OPBNB_TESTNET,
+  INFURA_TESTNET_CHAIN_IDS.SCROLL_SEPOLIA,
+  INFURA_TESTNET_CHAIN_IDS.UNICHAIN_SEPOLIA,
+];
+
+export const allowedInfuraHosts = [
+  // Ethereum
+  'mainnet.infura.io',
+  // Linea
+  'linea-mainnet.infura.io',
+  // Polygon
+  'polygon-mainnet.infura.io',
+  // Base
+  'base-mainnet.infura.io',
+  // Blast
+  'blast-mainnet.infura.io',
+  // Optimism
+  'optimism-mainnet.infura.io',
+  // Arbitrum
+  'arbitrum-mainnet.infura.io',
+  // Palm
+  'palm-mainnet.infura.io',
+  // Avalanche
+  'avalanche-mainnet.infura.io',
+  // Celo
+  'celo-mainnet.infura.io',
+  // ZKSync
+  'zksync-mainnet.infura.io',
+  // BSC
+  'bsc-mainnet.infura.io',
+  // Mantle
+  'mantle-mainnet.infura.io',
+  // OPBNB
+  'opbnb-mainnet.infura.io',
+  // Scroll
+  'scroll-mainnet.infura.io',
 ];
 
 /**
