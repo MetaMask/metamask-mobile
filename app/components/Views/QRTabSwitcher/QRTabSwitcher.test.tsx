7777df0ed1849456681bf8943f3f38b1ddca5384
Nico MASSART <NicolasMassart@users.noreply.github.com>
2024-12-05 11:46:41 +0000

    add timer restore

--- app/components/Views/QRTabSwitcher/QRTabSwitcher.test.tsx
+++ app/components/Views/QRTabSwitcher/QRTabSwitcher.test.tsx
@@ -35,9 +35,15 @@ jest.mock('@react-navigation/compat', () => {
 jest.mock('../QRScanner', () => jest.fn(() => null));
 jest.mock('../../UI/ReceiveRequest', () => jest.fn(() => null));
 
-jest.useFakeTimers();
-
 describe('QRTabSwitcher', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   it('renders QRScanner by default', () => {
     const { getByText } = render(<QRTabSwitcher />);
     expect(getByText(strings('qr_tab_switcher.scanner_tab'))).toBeTruthy();
