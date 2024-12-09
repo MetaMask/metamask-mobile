ede38cedda5c2921bcf826b1750755a6caac363e
Cal Leung <cal.leung@consensys.net>
2024-12-09 13:43:52 -0800

    chore: Chore/12435 mvp handle engine does not exist (#12538)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    The main purpose of these changes is to ensure that the navigation
    service is available when Engine is initialized. The reason that this is
    needed is because it allows us to redirect instances of failed
    controller instances to vault recovery where users may at least recover
    their keys as opposed to being bricked. This is an example of the
    bricked state - https://github.com/MetaMask/metamask-mobile/pull/12115.
    
    This PR reorganizes the initialization of services, including Engine to
    sagas, where it enables us to wait for dependencies to load first. The
    two dependencies that the Engine relies on are:
    - Persisted data loaded
    - Navigation loaded
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-mobile/issues/12435
    
    ## **Manual testing steps**
    
    While the underlying logic changes, the app behavior should remain the
    same
    
    1. Install the app on this branch
    2. Create a wallet
    3. Kill app, reopen and login
    4. Should not experience any issues
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    Simulates controller failed initialization
    
    
    https://github.com/user-attachments/assets/beb5e952-0fe3-4470-8aa6-2ff0947e3ffd
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    Simulates vault recovery when controller fails to initialize
    
    https://github.com/user-attachments/assets/76a853cd-34bc-465c-af03-d5c07609b8ee
    
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/actions/navigation/index.ts
+++ app/actions/navigation/index.ts
@@ -1,18 +1,28 @@
 /* eslint-disable import/prefer-default-export */
 import {
-  SET_CURRENT_ROUTE,
-  SET_CURRENT_BOTTOM_NAV_ROUTE,
-} from '../../reducers/navigation';
+  type OnNavigationReadyAction,
+  type SetCurrentRouteAction,
+  type SetCurrentBottomNavRouteAction,
+  NavigationActionType,
+} from './types';
 
-/**
- * Action Creators
- */
-export const setCurrentRoute = (route: string) => ({
-  type: SET_CURRENT_ROUTE,
+export * from './types';
+
+export const setCurrentRoute = (route: string): SetCurrentRouteAction => ({
+  type: NavigationActionType.SET_CURRENT_ROUTE,
   payload: { route },
 });
 
-export const setCurrentBottomNavRoute = (route: string) => ({
-  type: SET_CURRENT_BOTTOM_NAV_ROUTE,
+export const setCurrentBottomNavRoute = (
+  route: string,
+): SetCurrentBottomNavRouteAction => ({
+  type: NavigationActionType.SET_CURRENT_BOTTOM_NAV_ROUTE,
   payload: { route },
+});
+
+/**
+ * Action that is called when navigation is ready
+ */
+export const onNavigationReady = (): OnNavigationReadyAction => ({
+  type: NavigationActionType.ON_NAVIGATION_READY,
 });
