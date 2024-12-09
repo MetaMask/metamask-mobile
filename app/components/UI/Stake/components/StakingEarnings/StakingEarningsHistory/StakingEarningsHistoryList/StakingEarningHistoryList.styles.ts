ed9d1f6e459dcb91e77e25921c80dd36606ad97c
Nicholas Smith <nick.smith@consensys.net>
2024-12-09 14:45:56 -0600

    feat: add user earning history graph and list on history button click

---
+++ app/components/UI/Stake/components/StakingEarnings/StakingEarningsHistory/StakingEarningsHistoryList/StakingEarningHistoryList.styles.ts
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+
+const styleSheet = () =>
+  StyleSheet.create({
+    stakingEarningsHistoryListContainer: {
+      flex: 1,
+      paddingTop: 24,
+    },
+    lineItemContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'flex-start',
+      paddingVertical: 8,
+    },
+    rightLineItemContainer: {
+      flex: 1,
+      width: '50%',
+      justifyContent: 'space-between',
+      alignItems: 'flex-end',
+    },
+    rightLineItemBox: {
+      flex: 1,
+    },
+    leftLineItemBox: {
+      width: '50%',
+      justifyContent: 'center',
+      alignItems: 'flex-start',
+      flex: 1,
+      padding: 10,
+    },
+  });
+
+export default styleSheet;
