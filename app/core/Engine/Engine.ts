af3e5692d17b88eb8d8e3d2b7c652af9bd0e50e3
Cal-L <cleun007@gmail.com>
2024-12-11 15:25:53 -0800

    Remove passing navigation from Engine

--- app/core/Engine/Engine.ts
+++ app/core/Engine/Engine.ts
@@ -853,7 +853,6 @@ export class Engine {
           getRpcMethodMiddleware({
             hostname,
             getProviderState,
-            navigation: null,
             getApprovedHosts: () => null,
             setApprovedHosts: () => null,
             approveHost: () => null,
@@ -971,8 +970,8 @@ export class Engine {
         disableSnaps: !isBasicFunctionalityToggleEnabled(),
       }),
       clientCryptography: {
-        pbkdf2Sha512: pbkdf2
-      }
+        pbkdf2Sha512: pbkdf2,
+      },
     });
 
     const authenticationController = new AuthenticationController.Controller({
@@ -1297,7 +1296,9 @@ export class Engine {
               .addProperties({
                 token_standard: 'ERC20',
                 asset_type: 'token',
-                chain_id: getDecimalChainId(getGlobalChainId(networkController)),
+                chain_id: getDecimalChainId(
+                  getGlobalChainId(networkController),
+                ),
               })
               .build(),
           ),
@@ -1513,7 +1514,7 @@ export class Engine {
         if (
           state.networksMetadata[state.selectedNetworkClientId].status ===
             NetworkStatus.Available &&
-            getGlobalChainId(networkController) !== currentChainId
+          getGlobalChainId(networkController) !== currentChainId
         ) {
           // We should add a state or event emitter saying the provider changed
           setTimeout(() => {
@@ -1533,7 +1534,9 @@ export class Engine {
         } catch (error) {
           console.error(
             error,
-            `Network ID not changed, current chainId: ${getGlobalChainId(networkController)}`,
+            `Network ID not changed, current chainId: ${getGlobalChainId(
+              networkController,
+            )}`,
           );
         }
       },
