b466761badd46b2376cc2a10a9ad48171e14b6f1
Cal-L <cleun007@gmail.com>
2024-12-13 00:03:27 -0800

    Update unit tests to ensure that services are initialized on Root

--- app/components/Views/Root/index.test.tsx
+++ app/components/Views/Root/index.test.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import Root from './';
-import { render } from '@testing-library/react-native';
+import { render, waitFor } from '@testing-library/react-native';
+import SecureKeychain from '../../../core/SecureKeychain';
+import EntryScriptWeb3 from '../../../core/EntryScriptWeb3';
 
-// Mock PersistGate component
-jest.mock('redux-persist/lib/integration/react', () => ({
-  PersistGate: ({ children }: { children: React.ReactNode }) => children,
+jest.mock('../../../core/SecureKeychain', () => ({
+  init: jest.fn(),
+}));
+
+jest.mock('../../../core/EntryScriptWeb3', () => ({
+  init: jest.fn(),
 }));
 
 describe('Root', () => {
   it('should render correctly', () => {
     const { toJSON } = render(<Root foxCode="" />);
     expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('should initialize SecureKeychain', async () => {
+    render(<Root foxCode="" />);
+    await waitFor(() => {
+      expect(SecureKeychain.init).toHaveBeenCalled();
+    });
+  });
+
+  it('should initialize EntryScriptWeb3', async () => {
+    render(<Root foxCode="" />);
+    expect(EntryScriptWeb3.init).toHaveBeenCalled();
   });
 });
