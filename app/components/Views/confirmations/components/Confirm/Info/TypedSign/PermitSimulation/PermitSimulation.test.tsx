b65adf359baecfdb4760611dfe1d3a27586366dd
digiwand <20778143+digiwand@users.noreply.github.com>
2024-12-18 18:54:54 +0700

    test: PermitSimulation

---
+++ app/components/Views/confirmations/components/Confirm/Info/TypedSign/PermitSimulation/PermitSimulation.test.tsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+import renderWithProvider from '../../../../../../../../util/test/renderWithProvider';
+import { typedSignV4ConfirmationState } from '../../../../../../../../util/test/confirm-data-helpers';
+import PermitSimulation from './PermitSimulation';
+
+describe('PermitSimulation', () => {
+  it('should render correctly for personal sign', async () => {
+    const { getByText } = renderWithProvider(<PermitSimulation />, {
+      state: typedSignV4ConfirmationState,
+    });
+
+    expect(getByText('Estimated changes')).toBeDefined();
+    expect(getByText('You’re giving the spender permission to spend this many tokens from your account.')).toBeDefined();
+    expect(getByText('Spending cap')).toBeDefined();
+    expect(getByText('3,000')).toBeDefined();
+    expect(getByText('0xCcCCc...ccccC')).toBeDefined();
+  });
+});
