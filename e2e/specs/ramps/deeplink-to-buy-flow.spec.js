94f82c549ca46db32dcba2a28925ab1655b63f73
Curtis <Curtis.David7@gmail.com>
2024-12-18 16:28:11 -0500

    Revert "make ramps fail"
    This reverts commit c0cb335a392031cbc0f1b43c2a5838b496149a93.

--- e2e/specs/ramps/deeplink-to-buy-flow.spec.js
+++ e2e/specs/ramps/deeplink-to-buy-flow.spec.js
@@ -1,19 +1,18 @@
 'use strict';
-import TestHelpers from '../../helpers';
-
 import { loginToApp } from '../../viewHelper';
-import { withFixtures } from '../../fixtures/fixture-helper';
-import { SmokeAccounts } from '../../tags';
-import FixtureBuilder from '../../fixtures/fixture-builder';
 
+import FixtureBuilder from '../../fixtures/fixture-builder';
+import { withFixtures } from '../../fixtures/fixture-helper';
+
+import TestHelpers from '../../helpers';
 import SellGetStartedView from '../../pages/Ramps/SellGetStartedView';
+import { SmokeCore } from '../../tags';
 import BuyGetStartedView from '../../pages/Ramps/BuyGetStartedView';
 
 import BuildQuoteView from '../../pages/Ramps/BuildQuoteView';
-import TokenSelectBottomSheet from '../../pages/Ramps/TokenSelectBottomSheet';
 import Assertions from '../../utils/Assertions';
 
-describe(SmokeAccounts('Buy Crypto Deeplinks'), () => {
+describe(SmokeCore('Buy Crypto Deeplinks'), () => {
   beforeAll(async () => {
     await TestHelpers.reverseServerPort();
   });
@@ -45,16 +44,23 @@ describe(SmokeAccounts('Buy Crypto Deeplinks'), () => {
         await BuyGetStartedView.tapGetStartedButton();
         await Assertions.checkIfVisible(BuildQuoteView.getQuotesButton);
 
-        await TokenSelectBottomSheet.tapTokenByName('DAI');
-        await Assertions.checkIfTextIsDisplayed('Dai Stablecoin');
+        /* Uncomment this once the bug:
+              "when I update the token after deep linking"
+              is updated
+
+            // await BuildQuoteView.tapDefaultToken();
+            // await TokenSelectBottomSheet.tapTokenByName('LINK');
+            // await Assertions.checkIfTextIsDisplayed('LINK');
+            */
+        // await Assertions.checkIfTextIsDisplayed('XCD');
         await Assertions.checkIfTextIsDisplayed('$275');
       },
     );
   });
 
   it('should deep link to onramp on Linea network', async () => {
-    const queryParamsBuyDeepLink =
-      'metamask://buy?chainId=59144&address=0x176211869ca2b568f2a7d4ee941e073a821ee1ff';
+    const unaddedNetworkBuyDeepLink =
+      'metamask://buy?chainId=59144&address=0x176211869cA2b568f2A7D4EE941E073a821EE1ff&amount=270';
     await withFixtures(
       {
         fixture: new FixtureBuilder()
@@ -67,7 +73,7 @@ describe(SmokeAccounts('Buy Crypto Deeplinks'), () => {
         await loginToApp();
         await device.sendToHome();
         await device.launchApp({
-          url: queryParamsBuyDeepLink,
+          url: unaddedNetworkBuyDeepLink,
         });
         await Assertions.checkIfVisible(
           await SellGetStartedView.getStartedButton,
@@ -77,8 +83,6 @@ describe(SmokeAccounts('Buy Crypto Deeplinks'), () => {
         await Assertions.checkIfVisible(BuildQuoteView.getQuotesButton);
 
         await Assertions.checkIfTextIsDisplayed('$270');
-        await Assertions.checkIfTextIsDisplayed('USD coin');
-        await Assertions.checkIfTextIsDisplayed('USDC');
       },
     );
   });
