e866552f4ea82467933d2c0133c6ab8163c28d9a
Curtis <Curtis.David7@gmail.com>
2024-12-18 18:46:23 -0500

    seperate test

--- e2e/specs/accounts/deeplink-to-buy-flow-with-unsupported-network.spec.js
+++ e2e/specs/accounts/deeplink-to-buy-flow-with-unsupported-network.spec.js
@@ -24,34 +24,36 @@ describe(SmokeAccounts('Buy Crypto Deeplinks'), () => {
   beforeEach(async () => {
     jest.setTimeout(150000);
   });
-  it('should deep link to onramp ETH', async () => {
-    const buyLink = 'metamask://buy?chainId=1&amount=275';
+
+  it('should deep link to onramp on Base network', async () => {
+    const BuyDeepLink =
+      'metamask://buy?chainId=8453&address=0x833589fcd6edb6e08f4c7c32d4f71b54bda02913&amount=12';
 
     await withFixtures(
       {
-        fixture: new FixtureBuilder()
-          .withPopularNetworks()
-          .withRampsSelectedRegion()
-          .build(),
+        fixture: new FixtureBuilder().withRampsSelectedRegion().build(),
         restartDevice: true,
       },
       async () => {
         await loginToApp();
         await device.sendToHome();
         await device.launchApp({
-          url: buyLink,
+          url: BuyDeepLink,
         });
+
         await Assertions.checkIfVisible(
           await SellGetStartedView.getStartedButton,
         );
 
         await BuyGetStartedView.tapGetStartedButton();
-        await Assertions.checkIfVisible(BuildQuoteView.getQuotesButton);
-        await BuildQuoteView.tapDefaultToken('Ethereum');
 
-        await TokenSelectBottomSheet.tapTokenByName('DAI');
-        await Assertions.checkIfTextIsDisplayed('Dai Stablecoin');
-        await Assertions.checkIfTextIsDisplayed('$275');
+        await Assertions.checkIfVisible(NetworkApprovalBottomSheet.container);
+        await NetworkApprovalBottomSheet.tapApproveButton();
+        await NetworkAddedBottomSheet.tapSwitchToNetwork();
+        await Assertions.checkIfVisible(NetworkEducationModal.container);
+        await NetworkEducationModal.tapGotItButton();
+        // await Assertions.checkIfTextIsDisplayed('$12');
+        await Assertions.checkIfTextIsDisplayed('USD coin');
       },
     );
   });
