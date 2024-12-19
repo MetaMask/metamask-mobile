3188e229012157dd372b806cce59ac4c27c8b6e5
Iván <ivan.canete@consensys.net>
2024-12-19 12:23:03 +0100

    test: approve erc721 token e2e (#12767)
    ## **Description**
    
    This PR is within the scope of the Quality Quest.
    
    We're including an e2e test case to approve an ERC721 token.
    
    **Test Steps:**
    Given I am on the test dApp
    When I tap on the Approve From button under the ERC 721 section
    Then the transaction bottom sheet should appear
    When I submit the transaction
    Then the transaction should appear in the transaction history
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    
    ### **Before**
    
    
    ### **After**
    
    
    ## **Pre-merge author checklist**
    
    - [✓] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [✓] I've completed the PR template to the best of my ability
    - [✓] I’ve included tests if applicable
    - [✓] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [✓] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- e2e/selectors/Transactions/ActivitiesView.selectors.js
+++ e2e/selectors/Transactions/ActivitiesView.selectors.js
@@ -16,4 +16,5 @@ export const ActivitiesViewSelectorsText = {
   SET_APPROVAL_FOR_ALL_METHOD: enContent.transactions.set_approval_for_all,
   SWAP: enContent.swaps.transaction_label.swap,
   TITLE: enContent.transactions_view.title,
+
 };
