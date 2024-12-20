f7d909edda0f6d93229a62299e926ba69c4fe263
vinnyhoward <vincenguyenhoward@gmail.com>
2024-12-19 17:52:57 -0700

    fix: lint issues

--- app/components/UI/SettingsDrawer/index.stories.tsx
+++ app/components/UI/SettingsDrawer/index.stories.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable no-alert */
 // Third party dependencies.
 import React from 'react';
 import { View } from 'react-native';
