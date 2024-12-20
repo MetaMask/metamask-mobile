57fdfcc85db0888d304729eefbacf0b7128d5862
digiwand <20778143+digiwand@users.noreply.github.com>
2024-12-20 22:52:35 +0700

    feat: add Network URL non-ascii -> punycode warning

--- app/util/url/index.ts
+++ app/util/url/index.ts
@@ -1,4 +1,5 @@
 import AppConstants from '../../core/AppConstants';
+import punycode from 'punycode';
 
 export function isPortfolioUrl(url: string) {
   try {
@@ -24,6 +25,35 @@ export function isBridgeUrl(url: string) {
   }
 }
 
+export const isValidASCIIURL = (urlString?: string) => {
+  try {
+    return urlString?.includes(punycode.toASCII(new URL(urlString).host));
+  } catch (exp: unknown) {
+    console.error(exp);
+    return false;
+  }
+};
+
 function removePathTrailingSlash(path: string) {
   return path.endsWith('/') ? path.slice(0, -1) : path;
 }
+
+/** 
+ * Note: We use the punycode library here because the url library in react native doesn't support punycode encoding.
+ * It is supported in node.js which allows tests to pass, but behavior in react-native does not match the
+ * behavior in the tests. This differs from the toPunycodeURL util method in metamask-extension.
+ */
+export const toPunycodeURL = (urlString: string) => {
+  try {
+    const url = new URL(urlString);
+    const punycodeHostname = punycode.toASCII(url.hostname);
+    const { protocol, port, search, hash } = url;
+    const pathname =
+      url.pathname === '/' && !urlString.endsWith('/') ? '' : url.pathname;
+
+    return `${protocol}//${punycodeHostname}${port}${pathname}${search}${hash}`;
+  } catch (err: unknown) {
+    console.error(`Failed to convert URL to Punycode: ${err}`);
+    return urlString;
+  }
+};
