146c063312808804aa93efa3f131a1acc0db0cd9
jpuri <jyotipuri@gmail.com>
2025-01-14 16:25:39 +0530

    Adding remote feature flag for signature redesign

--- app/components/Views/confirmations/hooks/useConfirmationRedesignEnabled.ts
+++ app/components/Views/confirmations/hooks/useConfirmationRedesignEnabled.ts
@@ -1,10 +1,13 @@
 import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
 
 import { ApprovalTypes } from '../../../../core/RPCMethods/RPCMethodMiddleware';
+import { selectRemoteFeatureFlags } from '../../../../selectors/featureFlagController';
 import useApprovalRequest from './useApprovalRequest';
 
 const useConfirmationRedesignEnabled = () => {
   const { approvalRequest } = useApprovalRequest();
+  const { confirmation_redesign } = useSelector(selectRemoteFeatureFlags);
 
   const { type: approvalRequestType } = approvalRequest ?? {
     requestData: {},
@@ -12,12 +15,12 @@ const useConfirmationRedesignEnabled = () => {
 
   const isRedesignedEnabled = useMemo(
     () =>
+      (confirmation_redesign as Record<string, string>)?.signatures &&
       approvalRequestType &&
-      process.env.REDESIGNED_SIGNATURE_REQUEST === 'true' &&
       [ApprovalTypes.PERSONAL_SIGN, ApprovalTypes.ETH_SIGN_TYPED_DATA].includes(
         approvalRequestType as ApprovalTypes,
       ),
-    [approvalRequestType],
+    [approvalRequestType, confirmation_redesign],
   );
 
   return { isRedesignedEnabled };
