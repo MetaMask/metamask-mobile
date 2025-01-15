bf2bda64e1a9c0fd7b9c37082bbeece7ef2197f7
jpuri <jyotipuri@gmail.com>
2025-01-15 14:33:51 +0530

    update

--- app/components/Views/confirmations/hooks/useConfirmActions.ts
+++ app/components/Views/confirmations/hooks/useConfirmActions.ts
@@ -4,9 +4,6 @@ import { MetaMetricsEvents } from '../../../hooks/useMetrics';
 import { isSignatureRequest } from '../utils/confirm';
 import useApprovalRequest from './useApprovalRequest';
 import { useSignatureMetrics } from './useSignatureMetrics';
-import { isExternalHardwareAccount } from '../../../../util/address';
-import createExternalSignModelNav from '../../../../util/hardwareWallet/signatureUtils';
-import { useNavigation } from '@react-navigation/native';
 
 export const useConfirmActions = () => {
   const {
@@ -15,39 +12,20 @@ export const useConfirmActions = () => {
     approvalRequest,
   } = useApprovalRequest();
   const { captureSignatureMetrics } = useSignatureMetrics();
-  const navigation = useNavigation();
 
   const signatureRequest =
     approvalRequest?.type && isSignatureRequest(approvalRequest?.type);
 
   const onConfirm = useCallback(async () => {
-    if (isExternalHardwareAccount(approvalRequest?.requestData?.from)) {
-      navigation.navigate(
-        ...(await createExternalSignModelNav(
-          onRequestReject,
-          onRequestConfirm,
-          approvalRequest?.requestData,
-          'personal_sign',
-        )),
-      );
-    } else {
-      await onRequestConfirm({
-        waitForResult: true,
-        deleteAfterResult: true,
-        handleErrors: false,
-      });
-    }
+    await onRequestConfirm({
+      waitForResult: true,
+      deleteAfterResult: true,
+      handleErrors: false,
+    });
     if (signatureRequest) {
       captureSignatureMetrics(MetaMetricsEvents.SIGNATURE_APPROVED);
     }
-  }, [
-    captureSignatureMetrics,
-    onRequestConfirm,
-    navigation,
-    signatureRequest,
-    approvalRequest,
-    onRequestReject,
-  ]);
+  }, [captureSignatureMetrics, onRequestConfirm, signatureRequest]);
 
   const onReject = useCallback(() => {
     onRequestReject();
