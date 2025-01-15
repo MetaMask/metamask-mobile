9ae0c13cc9102a349b512b53f5d5b16d6cd1f0cb
salimtb <salim.toubal@outlook.com>
2025-01-15 11:10:08 +0100

    fix: use flash list

--- app/components/UI/Tokens/TokenList/index.tsx
+++ app/components/UI/Tokens/TokenList/index.tsx
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
-import { View, FlatList, RefreshControl } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { View, RefreshControl } from 'react-native';
 import { useSelector } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { FlashList } from '@shopify/flash-list';
+import {
+  useMetrics,
+  MetaMetricsEvents,
+} from '../../../../components/hooks/useMetrics';
 import { useTheme } from '../../../../util/theme';
+import { createDetectedTokensNavDetails } from '../../../Views/DetectedTokens';
+import { selectChainId } from '../../../../selectors/networkController';
+import { selectDetectedTokens } from '../../../../selectors/tokensController';
 import { selectPrivacyMode } from '../../../../selectors/preferencesController';
+import { getDecimalChainId } from '../../../../util/networks';
 import createStyles from '../styles';
 import Text from '../../../../component-library/components/Texts/Text';
 import { TokenI } from '../types';
@@ -18,6 +29,12 @@ interface TokenListProps {
   onRefresh: () => void;
   showRemoveMenu: (arg: TokenI) => void;
   goToAddToken: () => void;
+  setIsAddTokenEnabled: (arg: boolean) => void;
+}
+
+interface TokenListNavigationParamList {
+  AddAsset: { assetType: string };
+  [key: string]: undefined | object;
 }
 
 export const TokenList = ({
@@ -27,27 +44,62 @@ export const TokenList = ({
   onRefresh,
   showRemoveMenu,
   goToAddToken,
+  setIsAddTokenEnabled,
 }: TokenListProps) => {
+  const navigation =
+    useNavigation<
+      StackNavigationProp<TokenListNavigationParamList, 'AddAsset'>
+    >();
   const { colors } = useTheme();
+  const { trackEvent, createEventBuilder } = useMetrics();
+
+  const chainId = useSelector(selectChainId);
+  const detectedTokens = useSelector(selectDetectedTokens);
   const privacyMode = useSelector(selectPrivacyMode);
 
   const [showScamWarningModal, setShowScamWarningModal] = useState(false);
 
   const styles = createStyles(colors);
 
-  return tokens?.length ? (
-    <FlatList
+  const showDetectedTokens = () => {
+    navigation.navigate(...createDetectedTokensNavDetails());
+    trackEvent(
+      createEventBuilder(MetaMetricsEvents.TOKEN_IMPORT_CLICKED)
+        .addProperties({
+          source: 'detected',
+          chain_id: getDecimalChainId(chainId),
+          tokens: detectedTokens?.map(
+            (token) => `${token.symbol} - ${token.address}`,
+          ),
+        })
+        .build(),
+    );
+    setIsAddTokenEnabled(true);
+  };
+  const renderItem = useCallback(
+    ({ item }) => (
+      <TokenListItem
+        asset={item}
+        showRemoveMenu={showRemoveMenu}
+        showScamWarningModal={showScamWarningModal}
+        setShowScamWarningModal={setShowScamWarningModal}
+        privacyMode={privacyMode}
+      />
+    ),
+    [
+      showScamWarningModal,
+      privacyMode,
+      showRemoveMenu,
+      setShowScamWarningModal,
+    ],
+  );
+
+  return tokens?.length > 0 ? (
+    <FlashList
       testID={WalletViewSelectorsIDs.TOKENS_CONTAINER_LIST}
       data={tokens}
-      renderItem={({ item }) => (
-        <TokenListItem
-          asset={item}
-          showRemoveMenu={showRemoveMenu}
-          showScamWarningModal={showScamWarningModal}
-          setShowScamWarningModal={setShowScamWarningModal}
-          privacyMode={privacyMode}
-        />
-      )}
+      estimatedItemSize={50}
+      renderItem={renderItem}
       keyExtractor={(_, index) => index.toString()}
       ListFooterComponent={
         <TokenListFooter
