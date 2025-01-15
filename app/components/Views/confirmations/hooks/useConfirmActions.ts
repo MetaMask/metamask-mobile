19be28e09481cf9d02e3f32da92108e8fe6548f0
jpuri <jyotipuri@gmail.com>
2025-01-15 14:04:32 +0530

    fix

--- app/components/Views/confirmations/hooks/useConfirmActions.ts
+++ app/components/Views/confirmations/hooks/useConfirmActions.ts
@@ -4,6 +4,9 @@ import { MetaMetricsEvents } from '../../../hooks/useMetrics';
 import { isSignatureRequest } from '../utils/confirm';
 import useApprovalRequest from './useApprovalRequest';
 import { useSignatureMetrics } from './useSignatureMetrics';
+import { isExternalHardwareAccount } from '../../../../util/address';
+import createExternalSignModelNav from '../../../../util/hardwareWallet/signatureUtils';
+import { useNavigation } from '@react-navigation/native';
 
 export const useConfirmActions = () => {
   const {
@@ -12,20 +15,39 @@ export const useConfirmActions = () => {
     approvalRequest,
   } = useApprovalRequest();
   const { captureSignatureMetrics } = useSignatureMetrics();
+  const navigation = useNavigation();
 
   const signatureRequest =
     approvalRequest?.type && isSignatureRequest(approvalRequest?.type);
 
   const onConfirm = useCallback(async () => {
-    await onRequestConfirm({
-      waitForResult: true,
-      deleteAfterResult: true,
-      handleErrors: false,
-    });
+    if (isExternalHardwareAccount(approvalRequest?.requestData?.from)) {
+      navigation.navigate(
+        ...(await createExternalSignModelNav(
+          onRequestReject,
+          onRequestConfirm,
+          approvalRequest?.requestData,
+          'personal_sign',
+        )),
+      );
+    } else {
+      await onRequestConfirm({
+        waitForResult: true,
+        deleteAfterResult: true,
+        handleErrors: false,
+      });
+    }
     if (signatureRequest) {
       captureSignatureMetrics(MetaMetricsEvents.SIGNATURE_APPROVED);
     }
-  }, [captureSignatureMetrics, onRequestConfirm, signatureRequest]);
+  }, [
+    captureSignatureMetrics,
+    onRequestConfirm,
+    navigation,
+    signatureRequest,
+    approvalRequest,
+    onRequestReject,
+  ]);
 
   const onReject = useCallback(() => {
     onRequestReject();
