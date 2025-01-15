edf7bda2c29e53f7e08a812ffb45ce75f3faabda
digiwand <20778143+digiwand@users.noreply.github.com>
2025-01-15 13:55:56 -0800

    feat: Create Permit Simulation (#12606)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    Creates Permit Simulation UI to be shown on V3 and V4 signTypedData
    confirmations. Signature Decoding logic to be added following this PR.
    
    ### Add
    - create ButtonPill component
    - create PermitSimulation component
      - add to TypedSignV3V4 component
    - create constants/signatures.ts file
    - create hooks/useGetTokenStandardAndDetails files
    - create hooks/useTrackERC20WithoutDecimalInformation.ts
     file
    - create utils/signature.ts file
    - create utils/token.ts file
    
    ### Update
    - add style param to UI/Name component 
    - add noBorder param to Box component
    - add TokenStandard enum
    - add handleCloseOnBackdropClick param to confirmation BottomModal
    component
    - add skipCharacterInEnd param to shortenString common util. This mimics
    the same logic in extension
    
    ### Designs
    Simulations MVP
    https://www.figma.com/design/8DrinrQI4hs76Grm2F34xK/Simulations-MVP?node-id=1283-18713&node-type=canvas&t=FbRsIUjxMDxC3Get-0
    
    Confirmations Redesign V5
    https://www.figma.com/design/wcXUl6AH5KNFwKdAIv49kh/Confirmation-redesign-V5?node-id=4157-7776&node-type=canvas&m=dev
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-mobile/issues/12432
    Related: https://github.com/MetaMask/metamask-mobile/pull/12994
    (Signature Decoding Simulation - follow-up PR)
    
    
    ###: Follow-up
    - Related: https://github.com/MetaMask/metamask-mobile/issues/12910
    (Create storybook page + unit tests for ButtonPill)
    - Related: https://github.com/MetaMask/metamask-mobile/issues/13013 (Do
    not use Ramp/Box. Use View instead)
    - Related: https://github.com/MetaMask/metamask-mobile/issues/13016
    (Move shortenString helper location)
    
    ## **Manual testing steps**
    
    1. Test Permit test-dapp button
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <img width="320"
    src="https://github.com/user-attachments/assets/a7a478b0-6b05-47a6-b68c-c691af82c5ee">
    
    ![CleanShot 2025-01-15 at 12 41
    45](https://github.com/user-attachments/assets/76549a51-7254-41b1-8ab9-44cceedaefd7)
    
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

---
+++ app/components/Views/confirmations/components/Confirm/Info/TypedSignV3V4/Simulation/components/ValueDisplay/ValueDisplay.tsx
@@ -0,0 +1,208 @@
+import React, { useMemo, useState } from 'react';
+import { TouchableOpacity, View } from 'react-native';
+import { useSelector } from 'react-redux';
+import { NetworkClientId } from '@metamask/network-controller';
+import { Hex } from '@metamask/utils';
+
+import ButtonPill from '../../../../../../../../../../component-library/components-temp/Buttons/ButtonPill/ButtonPill';
+import { ButtonIconSizes } from '../../../../../../../../../../component-library/components/Buttons/ButtonIcon/ButtonIcon.types';
+import ButtonIcon from '../../../../../../../../../../component-library/components/Buttons/ButtonIcon/ButtonIcon';
+import { IconName , IconColor } from '../../../../../../../../../../component-library/components/Icons/Icon';
+import Text from '../../../../../../../../../../component-library/components/Texts/Text';
+
+import { IndividualFiatDisplay } from '../../../../../../../../../UI/SimulationDetails/FiatDisplay/FiatDisplay';
+import {
+  formatAmount,
+  formatAmountMaxPrecision,
+} from '../../../../../../../../../UI/SimulationDetails/formatAmount';
+
+import Box from '../../../../../../../../../UI/Ramp/components/Box';
+import Address from '../../../../../../UI/InfoRow/InfoValue/Address/Address';
+
+import { selectContractExchangeRates } from '../../../../../../../../../../selectors/tokenRatesController';
+
+import Logger from '../../../../../../../../../../util/Logger';
+import { shortenString } from '../../../../../../../../../../util/notifications/methods/common';
+import { useTheme } from '../../../../../../../../../../util/theme';
+import { calcTokenAmount } from '../../../../../../../../../../util/transactions';
+
+import useGetTokenStandardAndDetails from '../../../../../../../hooks/useGetTokenStandardAndDetails';
+import useTrackERC20WithoutDecimalInformation from '../../../../../../../hooks/useTrackERC20WithoutDecimalInformation';
+import { TokenDetailsERC20 } from '../../../../../../../utils/token';
+import BottomModal from '../../../../../../UI/BottomModal';
+
+import styleSheet from './ValueDisplay.styles';
+
+interface SimulationValueDisplayParams {
+  /** ID of the associated chain. */
+  chainId: Hex;
+
+  /** Change type to be displayed in value tooltip */
+  labelChangeType: string;
+
+  /** The network client ID */
+  networkClientId?: NetworkClientId;
+
+  /**
+   * The ethereum token contract address. It is expected to be in hex format.
+   * We currently accept strings since we have a patch that accepts a custom string
+   * {@see .yarn/patches/@metamask-eth-json-rpc-middleware-npm-14.0.1-b6c2ccbe8c.patch}
+   */
+  tokenContract: Hex | string | undefined;
+
+  // Optional
+
+  /** True if value is being credited to wallet */
+  credit?: boolean;
+
+  /** True if value is being debited to wallet */
+  debit?: boolean;
+
+  /** The primaryType of the typed sign message */
+  primaryType?: string;
+
+  /** The tokenId for NFT */
+  tokenId?: string;
+
+  /** The token amount */
+  value?: number | string;
+}
+
+const SimulationValueDisplay: React.FC<
+  SimulationValueDisplayParams
+> = ({
+  chainId,
+  labelChangeType,
+  networkClientId,
+  primaryType,
+  tokenContract,
+  tokenId,
+  value,
+  credit,
+  debit,
+}) => {
+    const [hasValueModalOpen, setHasValueModalOpen] = useState(false);
+
+    const { colors } = useTheme();
+    const styles = styleSheet(colors);
+
+    const contractExchangeRates = useSelector(selectContractExchangeRates);
+    const exchangeRate =
+      tokenContract && contractExchangeRates
+        ? contractExchangeRates[tokenContract as `0x${string}`]?.price
+        : undefined;
+
+    const tokenDetails = useGetTokenStandardAndDetails(tokenContract, networkClientId);
+    const { decimalsNumber: tokenDecimals } = tokenDetails;
+
+    useTrackERC20WithoutDecimalInformation(
+      chainId,
+      tokenContract,
+      tokenDetails as TokenDetailsERC20,
+    );
+
+    const fiatValue = useMemo(() => {
+      if (exchangeRate && value && !tokenId) {
+        const tokenAmount = calcTokenAmount(value, tokenDecimals);
+        return tokenAmount.multipliedBy(exchangeRate).toNumber();
+      }
+      return undefined;
+    }, [exchangeRate, tokenDecimals, tokenId, value]);
+
+    const { tokenValue, tokenValueMaxPrecision } = useMemo(() => {
+      if (!value || tokenId) {
+        return { tokenValue: null, tokenValueMaxPrecision: null };
+      }
+
+      const tokenAmount = calcTokenAmount(value, tokenDecimals);
+
+      return {
+        tokenValue: formatAmount('en-US', tokenAmount),
+        tokenValueMaxPrecision: formatAmountMaxPrecision('en-US', tokenAmount),
+      };
+    }, [tokenDecimals, tokenId, value]);
+
+    /** Temporary error capturing as we are building out Permit Simulations */
+    if (!tokenContract) {
+      Logger.error(
+        new Error(
+          `SimulationValueDisplay: Token contract address is missing where primaryType === ${primaryType}`,
+        ),
+      );
+      return null;
+    }
+
+    function handlePressTokenValue() {
+      setHasValueModalOpen(true);
+    }
+
+    return (
+      <Box style={styles.wrapper}>
+        <Box style={styles.flexRowTokenValueAndAddress}>
+          <View style={styles.valueAndAddress}>
+            <ButtonPill
+              onPress={handlePressTokenValue}
+              onPressIn={handlePressTokenValue}
+              onPressOut={handlePressTokenValue}
+              style={[credit && styles.valueIsCredit, debit && styles.valueIsDebit]}
+            >
+              <Text>
+                {credit && '+ '}
+                {debit && '- '}
+                {tokenValue !== null &&
+                  shortenString(tokenValue || '', {
+                    truncatedCharLimit: 15,
+                    truncatedStartChars: 15,
+                    truncatedEndChars: 0,
+                    skipCharacterInEnd: true,
+                  })}
+                {tokenId && `#${tokenId}`}
+              </Text>
+            </ButtonPill>
+            <Box compact noBorder style={styles.tokenAddress}>
+              <Address address={tokenContract} chainId={chainId} />
+            </Box>
+          </View>
+        </Box>
+        <Box compact noBorder>
+          {/*
+            TODO - add fiat shorten prop after tooltip logic has been updated
+            {@see {@link https://github.com/MetaMask/metamask-mobile/issues/12656}
+          */}
+          {fiatValue && <IndividualFiatDisplay fiatAmount={fiatValue} /* shorten*/ />}
+        </Box>
+        {hasValueModalOpen && (
+          /**
+           * TODO replace BottomModal instances with BottomSheet
+           * {@see {@link https://github.com/MetaMask/metamask-mobile/issues/12656}}
+           */
+          <BottomModal onClose={() => setHasValueModalOpen(false)}>
+            <TouchableOpacity
+              activeOpacity={1}
+              onPress={() => setHasValueModalOpen(false)}
+            >
+              <View style={styles.valueModal} >
+                <View style={styles.valueModalHeader}>
+                  <ButtonIcon
+                    iconColor={IconColor.Default}
+                    size={ButtonIconSizes.Sm}
+                    style={styles.valueModalHeaderIcon}
+                    onPress={() => setHasValueModalOpen(false)}
+                    iconName={IconName.ArrowLeft}
+                  />
+                  <Text style={styles.valueModalHeaderText}>
+                    {labelChangeType}
+                  </Text>
+                </View>
+                <Text style={styles.valueModalText}>
+                  {tokenValueMaxPrecision}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </BottomModal>
+        )}
+      </Box>
+    );
+  };
+
+export default SimulationValueDisplay;
