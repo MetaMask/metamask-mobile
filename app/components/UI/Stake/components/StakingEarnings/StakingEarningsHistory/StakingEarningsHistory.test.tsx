50c3e61fb2fcdb69708cd4bd91d6ad7faa242edc
Nicholas Smith <nick.smith@consensys.net>
2025-01-17 16:44:09 -0600

    chore: add unit tests

--- app/components/UI/Stake/components/StakingEarnings/StakingEarningsHistory/StakingEarningsHistory.test.tsx
+++ app/components/UI/Stake/components/StakingEarnings/StakingEarningsHistory/StakingEarningsHistory.test.tsx
@@ -52,4 +52,95 @@ describe('StakingEarningsHistory', () => {
     expect(getByText('6000.00 USD')).toBeTruthy();
     expect(getByText('+ 0.00054 ETH')).toBeTruthy();
   });
+
+  it('renders correctly with trailing zero values', () => {
+    (useStakingEarningsHistory as jest.Mock).mockReturnValue({
+      earningsHistory: [
+        {
+          dateStr: '2022-11-02',
+          dailyRewards: '0',
+          dailyRewardsUsd: '0',
+          sumRewards: '0',
+        },
+        {
+          dateStr: '2022-12-31',
+          dailyRewards: '442219562575615',
+          dailyRewardsUsd: '3000.00',
+          sumRewards: '442219562575615',
+        },
+        {
+          dateStr: '2023-01-01',
+          dailyRewards: '542219562575615',
+          dailyRewardsUsd: '6000.00',
+          sumRewards: '984439125151230',
+        },
+      ],
+      isLoading: false,
+      error: null,
+    });
+
+    const { getByText, queryByText } = render(
+      <StakingEarningsHistory asset={MOCK_STAKED_ETH_ASSET} />,
+    );
+
+    expect(getByText('Lifetime earnings')).toBeTruthy();
+    expect(getByText('0.00098 ETH')).toBeTruthy();
+    expect(getByText('December')).toBeTruthy();
+    expect(getByText('3000.00 USD')).toBeTruthy();
+    expect(getByText('+ 0.00044 ETH')).toBeTruthy();
+    expect(getByText('January')).toBeTruthy();
+    expect(getByText('6000.00 USD')).toBeTruthy();
+    expect(getByText('+ 0.00054 ETH')).toBeTruthy();
+    expect(queryByText('November')).toBeFalsy();
+  });
+
+  it('renders correctly with inner and trailing zero values', () => {
+    (useStakingEarningsHistory as jest.Mock).mockReturnValue({
+      earningsHistory: [
+        {
+          dateStr: '2022-10-02',
+          dailyRewards: '0',
+          dailyRewardsUsd: '0',
+          sumRewards: '0',
+        },
+        {
+          dateStr: '2022-11-30',
+          dailyRewards: '442219562575615',
+          dailyRewardsUsd: '3000.00',
+          sumRewards: '442219562575615',
+        },
+        {
+          dateStr: '2022-12-31',
+          dailyRewards: '0',
+          dailyRewardsUsd: '0',
+          sumRewards: '442219562575615',
+        },
+        {
+          dateStr: '2023-01-01',
+          dailyRewards: '542219562575615',
+          dailyRewardsUsd: '6000.00',
+          sumRewards: '984439125151230',
+        },
+      ],
+      isLoading: false,
+      error: null,
+    });
+
+    const { getByText, queryByText } = render(
+      <StakingEarningsHistory asset={MOCK_STAKED_ETH_ASSET} />,
+    );
+
+    expect(getByText('Lifetime earnings')).toBeTruthy();
+    expect(getByText('0.00098 ETH')).toBeTruthy();
+    expect(getByText('November')).toBeTruthy();
+    expect(getByText('3000.00 USD')).toBeTruthy();
+    expect(getByText('+ 0.00044 ETH')).toBeTruthy();
+    expect(getByText('December')).toBeTruthy();
+    expect(getByText('+ 0 ETH')).toBeTruthy();
+    expect(getByText('0.00 USD')).toBeTruthy();
+    expect(getByText('January')).toBeTruthy();
+    expect(getByText('6000.00 USD')).toBeTruthy();
+    expect(getByText('+ 0.00054 ETH')).toBeTruthy();
+    expect(queryByText('October')).toBeFalsy();
+  });
 });
