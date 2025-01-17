3c1e5e337632f0a8b8acdcb8085736fff51c00b2
digiwand <20778143+digiwand@users.noreply.github.com>
2025-01-17 09:55:47 -0800

    Revert "refactor: separate selector from hook useTypedSignSimulationEnabled"
    This reverts commit bccc48966e369f048475803fdfbd1a655a0a2232.

--- app/selectors/signatureController.ts
+++ app/selectors/signatureController.ts
@@ -1,8 +1,4 @@
-import { SignTypedDataVersion } from '@metamask/eth-sig-util';
-import { MessageParamsTyped, SignatureRequest, SignatureRequestType } from '@metamask/signature-controller';
-import { selectUseTransactionSimulations } from './preferencesController';
-import { isRecognizedPermit } from '../components/Views/confirmations/utils/signature';
-import { isNonPermitSupportedByDecodingAPI } from '../components/Views/confirmations/utils/signatureDecoding';
+import { SignatureRequest } from '@metamask/signature-controller';
 import { RootState } from '../reducers';
 import { createDeepEqualSelector } from './util';
 
@@ -19,31 +15,4 @@ export const selectSignatureRequestById = createDeepEqualSelector(
   (_: RootState, id: string) => id,
   (signatureRequests, id) =>
     signatureRequests[id] as SignatureRequest | undefined,
-);
-
-export const selectTypedSignSimulationEnabled = createDeepEqualSelector(
-  [selectSignatureRequestById, selectUseTransactionSimulations],
-  (signatureRequest, useTransactionSimulations) => {
-    if (!signatureRequest) {
-      return undefined;
-    }
-
-    const requestType = signatureRequest.type;
-    const signatureMethod = (signatureRequest.messageParams as MessageParamsTyped)?.version;
-
-    const isTypedSignV3V4 = requestType === SignatureRequestType.TypedSign && (
-      signatureMethod === SignTypedDataVersion.V3 ||
-      signatureMethod === SignTypedDataVersion.V4
-    );
-    const isPermit = isRecognizedPermit(signatureRequest);
-
-    const nonPermitSupportedByDecodingAPI: boolean =
-      isTypedSignV3V4 && isNonPermitSupportedByDecodingAPI(signatureRequest);
-
-    return (
-      useTransactionSimulations &&
-      isTypedSignV3V4 &&
-      (isPermit || nonPermitSupportedByDecodingAPI)
-    );
-  }
 );
