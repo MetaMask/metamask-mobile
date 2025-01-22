7a46fdfe28a0d0694c6695e5a1462db1fb8ac591
Bigshmow <devin.stewart@consensys.net>
2025-01-21 19:42:02 -0700

    feat: add-token-search-discovery-controller

---
+++ app/core/Engine/controllers/TokenSearchDiscoveryController/utils.ts
@@ -0,0 +1,37 @@
+import Logger from '../../../../util/Logger';
+import {
+  TokenSearchApiService,
+  TokenSearchDiscoveryController,
+} from '@metamask/token-search-discovery-controller';
+import { TokenSearchDiscoveryControllerParams } from './types';
+import { PORTFOLIO_API_URL } from './constants';
+
+const getPortfolioApiBaseUrl = () => {
+  const env = process.env.METAMASK_ENVIRONMENT;
+  switch (env) {
+    case 'local':
+      return PORTFOLIO_API_URL.dev;
+    case 'pre-release':
+    case 'production':
+      return PORTFOLIO_API_URL.prod;
+    default:
+      return PORTFOLIO_API_URL.dev;
+  }
+};
+
+export const createTokenSearchDiscoveryController = ({
+  state,
+  messenger,
+}: TokenSearchDiscoveryControllerParams) => {
+  try {
+    const controller = new TokenSearchDiscoveryController({
+      state,
+      messenger,
+      tokenSearchService: new TokenSearchApiService(getPortfolioApiBaseUrl()),
+    });
+    return controller;
+  } catch (error) {
+    Logger.error(error as Error);
+    throw error;
+  }
+};
