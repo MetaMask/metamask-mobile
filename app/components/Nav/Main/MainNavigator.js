f4e99ebb44d302843a9a317128494e6ed8b49ff7
Nicholas Gambino <nicholas.gambino@consensys.net>
2025-01-23 15:29:18 -0800

    chore: Remove dead code from navigation

--- app/components/Nav/Main/MainNavigator.js
+++ app/components/Nav/Main/MainNavigator.js
@@ -24,7 +24,6 @@ import Wallet from '../../Views/Wallet';
 import Asset from '../../Views/Asset';
 import AssetDetails from '../../Views/AssetDetails';
 import AddAsset from '../../Views/AddAsset';
-import Collectible from '../../Views/Collectible';
 import Send from '../../Views/confirmations/Send';
 import SendTo from '../../Views/confirmations/SendFlow/SendTo';
 import { RevealPrivateCredential } from '../../Views/RevealPrivateCredential';
@@ -166,11 +165,6 @@ const WalletTabStackFlow = () => (
       name="AddAsset"
       component={AddAsset}
       options={AddAsset.navigationOptions}
-    />
-    <Stack.Screen
-      name="Collectible"
-      component={Collectible}
-      options={Collectible.navigationOptions}
     />
     <Stack.Screen
       name="ConfirmAddAsset"
