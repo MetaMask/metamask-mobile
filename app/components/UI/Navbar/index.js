2c41309334a19e639afe78ebb44a980af2676ee3
Owen Craston <owengc12@gmail.com>
2025-01-23 14:51:44 -0800

    hardcode solana network

--- app/components/UI/Navbar/index.js
+++ app/components/UI/Navbar/index.js
@@ -60,7 +60,7 @@ import { RequestPaymentViewSelectors } from '../../../../e2e/selectors/Receive/R
 import { MetricsEventBuilder } from '../../../core/Analytics/MetricsEventBuilder';
 import { toChecksumHexAddress } from '@metamask/controller-utils';
 ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
-import { isBtcAccount } from '../../../core/Multichain/utils';
+import { isBtcAccount, isSolanaAccount } from '../../../core/Multichain/utils';
 ///: END:ONLY_INCLUDE_IF
 import { withMetaMetrics } from '../Stake/utils/metaMetrics/withMetaMetrics';
 
@@ -600,9 +600,10 @@ export function getSendFlowTitle(
       canGoBack ? (
         // eslint-disable-next-line react/jsx-no-bind
         <TouchableOpacity onPress={leftAction} style={styles.closeButton}>
-          <Text style={innerStyles.headerButtonText}
-          testID={SendViewSelectorsIDs.SEND_BACK_BUTTON}
-        >
+          <Text
+            style={innerStyles.headerButtonText}
+            testID={SendViewSelectorsIDs.SEND_BACK_BUTTON}
+          >
             {strings('transaction.back')}
           </Text>
         </TouchableOpacity>
@@ -660,7 +661,6 @@ export function getOnboardingNavbarOptions(
           source={metamask_name}
           style={innerStyles.metamaskName}
           resizeMethod={'auto'}
-
         />
       </View>
     ),
@@ -1018,6 +1018,18 @@ export function getWalletNavbarOptions(
     );
 
     ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
+    if (isSolanaAccount(selectedInternalAccount)) {
+      networkPicker = (
+        <PickerNetwork
+          label={'Solana'}
+          imageSource={require('../../../images/solana-logo.png')}
+          testID={WalletViewSelectorsIDs.NAVBAR_NETWORK_BUTTON}
+          hideNetworkName
+          isDisabled
+        />
+      );
+    }
+
     if (isBtcAccount(selectedInternalAccount)) {
       networkPicker = (
         <PickerNetwork
@@ -1029,6 +1041,7 @@ export function getWalletNavbarOptions(
         />
       );
     }
+
     ///: END:ONLY_INCLUDE_IF
 
     return <View style={styles.leftElementContainer}>{networkPicker}</View>;
