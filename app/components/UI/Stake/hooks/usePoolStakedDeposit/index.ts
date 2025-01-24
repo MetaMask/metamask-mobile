1e858998108fd91025de1c9412cb6d49b94cba2c
Amitabh Aggarwal <aggarwal.amitabh@gmail.com>
2025-01-24 13:28:08 -0600

    feat: hacking stablecoin lending functionality

--- app/components/UI/Stake/hooks/usePoolStakedDeposit/index.ts
+++ app/components/UI/Stake/hooks/usePoolStakedDeposit/index.ts
@@ -1,91 +1,139 @@
-import { ChainId, PooledStakingContract } from '@metamask/stake-sdk';
 import {
+  CHAIN_IDS,
   TransactionParams,
   TransactionType,
   WalletDevice,
 } from '@metamask/transaction-controller';
-import { ORIGIN_METAMASK, toHex } from '@metamask/controller-utils';
+import { ORIGIN_METAMASK } from '@metamask/controller-utils';
 import { addTransaction } from '../../../../../util/transaction-controller';
-import { formatEther } from 'ethers/lib/utils';
 import { useStakeContext } from '../useStakeContext';
 import trackErrorAsAnalytics from '../../../../../util/metrics/TrackError/trackErrorAsAnalytics';
-import { NetworkClientId } from '@metamask/network-controller';
 import { Stake } from '../../sdk/stakeSdkProvider';
+import USDTContractABI from './USDT COntractABI.json';
+import { ethers } from 'ethers';
+import Engine from '../../../../../core/Engine';
+import { Web3Provider } from '@ethersproject/providers';
 
-const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+export const attemptDepositTransaction = async (
+  receiver: string, // the address that can claim exited ETH
+) => {
+  try {
+    const receiver = '0x316BDE155acd07609872a56Bc32CcfB0B13201fA';
 
-const generateDepositTxParams = (
-  valueWei: string,
-  activeAccountAddress: string,
-  contractAddress: string,
-  encodedDepositTransactionData: string,
-  chainId: ChainId,
-): TransactionParams => ({
-  to: contractAddress,
-  from: activeAccountAddress,
-  chainId: `0x${chainId}`,
-  data: encodedDepositTransactionData,
-  value: toHex(valueWei.toString()),
-});
+    const MM_LENDING_CONTRACT = '0x8e59Abbd93A5822eac35dbB4DFCF1F6646e87b0F';
 
-const attemptDepositTransaction =
-  (
-    pooledStakingContract: PooledStakingContract,
-    networkClientId: NetworkClientId,
-  ) =>
-  async (
-    depositValueWei: string,
-    receiver: string, // the address that can claim exited ETH
-    referrer: string = ZERO_ADDRESS, // any address to track referrals or deposits from different interfaces (can use zero address if not needed)
-  ) => {
-    try {
-      const gasLimit = await pooledStakingContract.estimateDepositGas(
-        formatEther(depositValueWei),
-        receiver,
-        referrer,
+    const USDT_CONTRACT_SEPOLIA = '0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0';
+
+    const abi = USDTContractABI;
+
+    const networkClientId =
+      Engine.context.NetworkController.findNetworkClientIdByChainId(
+        CHAIN_IDS.SEPOLIA,
       );
-
-      const encodedDepositTransactionData =
-        await pooledStakingContract.encodeDepositTransactionData(
-          formatEther(depositValueWei),
-          receiver,
-          referrer,
-          {
-            gasLimit,
-          },
-        );
-
-      const { data, chainId } = encodedDepositTransactionData;
-
-      const txParams = generateDepositTxParams(
-        depositValueWei,
-        receiver,
-        pooledStakingContract.contract.address,
-        data,
-        chainId,
-      );
-
-      return await addTransaction(txParams, {
-        deviceConfirmedOn: WalletDevice.MM_MOBILE,
+    const provider = new Web3Provider(
+      Engine.context.NetworkController.getNetworkClientById(
         networkClientId,
-        origin: ORIGIN_METAMASK,
-        type: TransactionType.stakingDeposit,
-      });
-    } catch (e) {
-      const errorMessage = (e as Error).message;
-      trackErrorAsAnalytics('Pooled Staking Transaction Failed', errorMessage);
-    }
-  };
+      )?.provider,
+    );
+
+    const contract = new ethers.Contract(MM_LENDING_CONTRACT, abi, provider);
+
+    const approvaldata = contract.interface.encodeFunctionData('approve', [
+      // Spender must be the lending contract. You're giving permission to the lending contract to spend your USDT.
+      MM_LENDING_CONTRACT,
+      // USDT has 6 decimal points.
+      '100000000',
+    ]);
+
+    console.log('approvaldata: ', approvaldata);
+
+    // approve spending of an erc20 token amount
+    // const approvalTxParam: TransactionParams = {
+    //   to: USDT_CONTRACT_SEPOLIA,
+    //   from: receiver,
+    //   chainId: CHAIN_IDS.SEPOLIA,
+    //   data: approvaldata,
+    //   value: '0',
+    // };
+    // const approvalTx = await addTransaction(approvalTxParam, {
+    //   deviceConfirmedOn: WalletDevice.MM_MOBILE,
+    //   networkClientId,
+    //   origin: ORIGIN_METAMASK,
+    // });
+
+    // deposit amount to lend
+    // const transactionData = contract.interface.encodeFunctionData('deposit', [
+    //   '10000000',
+    //   receiver,
+    // ]);
+    // const depositTxParams: TransactionParams = {
+    //   to: MM_LENDING_CONTRACT, //
+    //   from: receiver,
+    //   chainId: CHAIN_IDS.SEPOLIA,
+    //   data: transactionData,
+    //   value: '0',
+    // };
+    // const depositTx = await addTransaction(depositTxParams, {
+    //   deviceConfirmedOn: WalletDevice.MM_MOBILE,
+    //   networkClientId,
+    //   origin: ORIGIN_METAMASK,
+    //   type: TransactionType.stakingDeposit,
+    // });
+
+    // redeem a lent amount
+    // const redeemTransactionData = contract.interface.encodeFunctionData(
+    //   'redeem',
+    //   ['9000000', receiver, receiver],
+    // );
+    // const redeemTxParams: TransactionParams = {
+    //   to: MM_LENDING_CONTRACT,
+    //   from: receiver,
+    //   chainId: CHAIN_IDS.SEPOLIA,
+    //   data: redeemTransactionData,
+    //   value: '0',
+    // };
+
+    // const redeemTx = await addTransaction(redeemTxParams, {
+    //   deviceConfirmedOn: WalletDevice.MM_MOBILE,
+    //   networkClientId,
+    //   origin: ORIGIN_METAMASK,
+    //   type: TransactionType.stakingUnstake,
+    // });
+
+    // withdraw a lent amount by asset value - Withdrawing 0.3 USDT
+    const withdrawTransactionData = contract.interface.encodeFunctionData(
+      'withdraw',
+      ['300000', receiver, receiver],
+    );
+    const withdrawTxParams: TransactionParams = {
+      to: MM_LENDING_CONTRACT,
+      from: receiver,
+      chainId: CHAIN_IDS.SEPOLIA,
+      data: withdrawTransactionData,
+      value: '0',
+    };
+
+    const withdrawTx = await addTransaction(withdrawTxParams, {
+      deviceConfirmedOn: WalletDevice.MM_MOBILE,
+      networkClientId,
+      origin: ORIGIN_METAMASK,
+      type: TransactionType.stakingUnstake,
+    });
+
+    return withdrawTx;
+  } catch (e) {
+    const errorMessage = (e as Error).message;
+    trackErrorAsAnalytics('Pooled Staking Transaction Failed', errorMessage);
+  }
+
+  // withdraw function takes in assets and maxRedeem would withdraw everything
+};
 
 const usePoolStakedDeposit = () => {
-  const { networkClientId, stakingContract } =
-    useStakeContext() as Required<Stake>;
+  const { networkClientId } = useStakeContext() as Required<Stake>;
 
   return {
-    attemptDepositTransaction: attemptDepositTransaction(
-      stakingContract,
-      networkClientId,
-    ),
+    attemptDepositTransaction,
   };
 };
 
