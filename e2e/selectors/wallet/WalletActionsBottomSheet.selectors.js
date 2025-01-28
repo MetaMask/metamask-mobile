096832ab3448e52fe52861cde56d98bea1479af4
Nicholas Smith <nick.smith@consensys.net>
2025-01-27 19:57:17 -0500

    feat: add earn button to wallet actions (#13018)
    ## **Description**
    
    1. What is the reason for the change?
    
    We want to add a wallet action button for Earn to make it easy for users
    to find
    
    2. What is the improvement/solution?
    
    Add Earn button to wallet actions and make sure that on click we switch
    to mainnet if we are on a staking unsupported network
    
    ## **Related issues**
    
    Fixes: https://consensyssoftware.atlassian.net/browse/STAKE-897
    
    ## **Manual testing steps**
    - Turn the MM_STABLECOIN_LENDING_UI_ENABLED flag on
    - Go to wallet actions on mainnet, click Earn and you should go to
    staking page for mainnet
    - Go to wallet actions on holesky, click Earn and you should go to
    staking page for holesky
    - Go to wallet actions on any other network, click Earn and you should
    go to staking page for mainnet
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <img width="290" alt="Screenshot 2025-01-15 at 6 42 58 PM"
    src="https://github.com/user-attachments/assets/ffd34338-e2c4-4f31-98ea-1e260f625346"
    />
    
    ### **After**
    
    
    https://github.com/user-attachments/assets/77eda484-9125-4a78-a913-740af06a0743
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- e2e/selectors/wallet/WalletActionsBottomSheet.selectors.js
+++ e2e/selectors/wallet/WalletActionsBottomSheet.selectors.js
@@ -5,4 +5,5 @@ export const WalletActionsBottomSheetSelectorsIDs = {
   BUY_BUTTON: 'wallet-buy-action',
   SELL_BUTTON: 'wallet-sell-action',
   BRIDGE_BUTTON: 'wallet-bridge-action',
+  EARN_BUTTON: 'wallet-earn-action',
 };
