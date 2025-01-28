8496bec40ee7c7dce740667bd1320a47e1eac7a7
Cal-L <cleun007@gmail.com>
2025-01-28 08:52:45 -0800

    Remove messenger mock

--- app/core/Engine/utils/test-utils.ts
+++ app/core/Engine/utils/test-utils.ts
@@ -2,27 +2,6 @@ import { ControllerInitRequest } from '../modular-controller.types';
 import { BaseControllerMessenger } from '../types';
 
 /**
- * Build a mock for the BaseControllerMessenger.
- *
- * @param recurse - Whether to recurse and mock the restricted messenger.
- * @returns A mocked BaseControllerMessenger.
- */
-export function buildControllerMessengerMock(
-  recurse = true,
-): jest.Mocked<BaseControllerMessenger> {
-  return {
-    call: jest.fn(),
-    getRestricted: jest
-      .fn()
-      .mockReturnValue(recurse ? buildControllerMessengerMock(false) : {}),
-    publish: jest.fn(),
-    registerActionHandler: jest.fn(),
-    registerInitialEventPayload: jest.fn(),
-    subscribe: jest.fn(),
-  } as unknown as jest.Mocked<BaseControllerMessenger>;
-}
-
-/**
  * Build a mock for the ControllerInitRequest.
  *
  * @param baseControllerMessenger - The base controller messenger.
