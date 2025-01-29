04a974b13911c02a14142197b995ac6adde0f23b
digiwand <20778143+digiwand@users.noreply.github.com>
2025-01-30 02:23:14 +0300

    refactor: rm background color form bottom sheet footer and header no need because BottomSheetDialog provides background color

--- app/component-library/components/HeaderBase/HeaderBase.styles.ts
+++ app/component-library/components/HeaderBase/HeaderBase.styles.ts
@@ -19,7 +19,7 @@ const styleSheet = (params: {
   theme: Theme;
   vars: HeaderBaseStyleSheetVars;
 }) => {
-  const { vars, theme } = params;
+  const { vars } = params;
   const { style, startAccessorySize, endAccessorySize } = vars;
   let accessoryWidth;
   if (startAccessorySize && endAccessorySize) {
@@ -29,7 +29,6 @@ const styleSheet = (params: {
   return StyleSheet.create({
     base: Object.assign(
       {
-        backgroundColor: theme.colors.background.default,
         flexDirection: 'row',
       } as ViewStyle,
       style,
