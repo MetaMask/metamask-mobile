f89c331faa04eebd5848308b2879a5202ede9033
Bigshmow <devin.stewart@consensys.net>
2025-01-30 10:39:47 -0700

    feat: introduce generic debounce hook to searchTokens

--- app/components/hooks/useTokenSearchDiscovery/useTokenSearchDiscovery.ts
+++ app/components/hooks/useTokenSearchDiscovery/useTokenSearchDiscovery.ts
@@ -1,15 +1,19 @@
 import { useCallback } from 'react';
 import { useSelector } from 'react-redux';
+import { useDebouncedValue } from '../useDebouncedValue';
 import Engine from '../../../core/Engine';
 import { selectRecentTokenSearches } from '../../../selectors/tokenSearchDiscoveryController';
-import { TokenSearchParams } from '@metamask/token-search-discovery-controller/dist/types.cjs';
+import type { TokenSearchParams } from '@metamask/token-search-discovery-controller/dist/types.d.cts';
+
+const SEARCH_DEBOUNCE_DELAY = 300;
 
 export const useTokenSearchDiscovery = () => {
   const recentSearches = useSelector(selectRecentTokenSearches);
 
   const searchTokens = useCallback(async (params: TokenSearchParams) => {
+    const debouncedParams = useDebouncedValue(params, SEARCH_DEBOUNCE_DELAY);
     const { TokenSearchDiscoveryController } = Engine.context;
-    return await TokenSearchDiscoveryController.searchTokens(params);
+    return await TokenSearchDiscoveryController.searchTokens(debouncedParams);
   }, []);
 
   return {
