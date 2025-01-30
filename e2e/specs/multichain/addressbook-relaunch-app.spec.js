52e8aa081240cc868db1ad6441f62643cca19389
Curtis David <Curtis.David7@gmail.com>
2025-01-30 12:36:55 -0500

    resturcture test

--- e2e/specs/multichain/addressbook-relaunch-app.spec.js
+++ e2e/specs/multichain/addressbook-relaunch-app.spec.js
@@ -6,61 +6,54 @@ import AddContactView from '../../pages/Settings/Contacts/AddContactView';
 import TabBarComponent from '../../pages/wallet/TabBarComponent';
 import { loginToApp } from '../../viewHelper';
 import FixtureBuilder from '../../fixtures/fixture-builder';
-import {
-  loadFixture,
-  startFixtureServer,
-  stopFixtureServer,
-} from '../../fixtures/fixture-helper';
+
 import TestHelpers from '../../helpers';
-import FixtureServer from '../../fixtures/fixture-server';
 import { getFixturesServerPort } from '../../fixtures/utils';
 import Assertions from '../../utils/Assertions';
+import { withFixtures } from '../../fixtures/fixture-helper';
 
-const fixtureServer = new FixtureServer();
 const MEMO = 'Address for testing 123123123';
-describe(SmokeMultiChainPermissions('Addressbook Relaunch'), () => {
-  beforeAll(async () => {
-    await TestHelpers.reverseServerPort();
-    const fixture = new FixtureBuilder().build();
-    await startFixtureServer(fixtureServer);
-    await loadFixture(fixtureServer, { fixture });
-    await TestHelpers.launchApp({
-      launchArgs: { fixtureServerPort: `${getFixturesServerPort()}` },
+
+describe(
+  SmokeMultiChainPermissions('Relaunch App after Adding Address to Contact Book'),
+  () => {
+    beforeAll(async () => {
+      jest.setTimeout(150000);
+      await TestHelpers.reverseServerPort();
     });
-    await loginToApp();
-  });
 
-  beforeEach(() => {
-    jest.setTimeout(150000);
-  });
+    it('should terminate and relaunch the app after adding a contact', async () => {
+      await withFixtures(
+        {
+          dapp: true,
+          fixture: new FixtureBuilder().withPermissionController().build(),
+          restartDevice: true,
+        },
+        async () => {
+          await loginToApp();
+          await device.disableSynchronization();
+          await TabBarComponent.tapSettings();
+          await SettingsView.tapContacts();
+          await device.enableSynchronization();
 
-  afterAll(async () => {
-    await stopFixtureServer(fixtureServer);
-  });
+          await ContactsView.tapAddContactButton();
+          await AddContactView.typeInName('Curtis');
 
-  it('should go to settings then select contacts', async () => {
-    await device.disableSynchronization();
-    await TabBarComponent.tapSettings();
-    await SettingsView.tapContacts();
-    await device.enableSynchronization();
-
-    await ContactsView.tapAddContactButton();
-    // await Assertions.checkIfVisible(ContactsView.container);
-    await AddContactView.typeInName('Curtis');
-
-    await AddContactView.typeInAddress('curtis.eth');
-        await AddContactView.typeInMemo(MEMO);
-    await AddContactView.tapAddContactButton();
-    // await Assertions.checkIfVisible(ContactsView.container);
-    await ContactsView.isContactAliasVisible('Curtis'); // Check that Ibrahim address is saved in the address book
-    await device.terminateApp();
-    await TestHelpers.launchApp({
-      launchArgs: { fixtureServerPort: `${getFixturesServerPort()}` },
+          await AddContactView.typeInAddress('curtis.eth');
+          await AddContactView.typeInMemo(MEMO);
+          await AddContactView.tapAddContactButton();
+          await ContactsView.isContactAliasVisible('Curtis'); // Check that Ibrahim address is saved in the address book
+          await device.terminateApp();
+          await TestHelpers.launchApp({
+            launchArgs: { fixtureServerPort: `${getFixturesServerPort()}` },
+          });
+          await loginToApp();
+          await TabBarComponent.tapSettings();
+          await SettingsView.tapContacts();
+          await Assertions.checkIfVisible(ContactsView.container);
+          await ContactsView.isContactAliasVisible('Curtis');
+        },
+      );
     });
-    await loginToApp();
-    await TabBarComponent.tapSettings();
-    await SettingsView.tapContacts();
-    await Assertions.checkIfVisible(ContactsView.container);
-    await ContactsView.isContactAliasVisible('Curtis');
-  });
-});
+  },
+);
