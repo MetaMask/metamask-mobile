3605a39585733874236d7622b3ac88eaada38d56
digiwand <20778143+digiwand@users.noreply.github.com>
2025-01-30 21:14:52 +0300

    test:fix: redesign Confirm mock SafeAreaProvider

--- app/components/Views/confirmations/Confirm/Confirm.test.tsx
+++ app/components/Views/confirmations/Confirm/Confirm.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import renderWithProvider from '../../../../util/test/renderWithProvider';
 import {
@@ -7,6 +8,30 @@ import {
   typedSignV1ConfirmationState,
 } from '../../../../util/test/confirm-data-helpers';
 import Confirm from './index';
+
+const mockNavigate = jest.fn();
+const mockGoBack = jest.fn();
+
+jest.mock('@react-navigation/native', () => ({
+  ...jest.requireActual('@react-navigation/native'),
+  useNavigation: () => ({
+    navigate: mockNavigate,
+    goBack: mockGoBack,
+  }),
+}));
+
+jest.mock('react-native-safe-area-context', () => {
+  const inset = { top: 0, right: 0, bottom: 0, left: 0 };
+  const frame = { width: 0, height: 0, x: 0, y: 0 };
+  return {
+    SafeAreaProvider: jest.fn().mockImplementation(({ children }) => children),
+    SafeAreaConsumer: jest
+      .fn()
+      .mockImplementation(({ children }) => children(inset)),
+    useSafeAreaInsets: jest.fn().mockImplementation(() => inset),
+    useSafeAreaFrame: jest.fn().mockImplementation(() => frame),
+  };
+});
 
 jest.mock('../../../../core/Engine', () => ({
   getTotalFiatAccountBalance: () => ({ tokenFiat: 10 }),
@@ -33,10 +58,20 @@ jest.mock('react-native-gzip', () => ({
 }));
 
 describe('Confirm', () => {
+  beforeEach(() => {
+    mockNavigate.mockClear();
+    mockGoBack.mockClear();
+  });
+
   it('should render correct information for personal sign', async () => {
-    const { getAllByRole, getByText } = renderWithProvider(<Confirm />, {
-      state: personalSignatureConfirmationState,
-    });
+    const { getAllByRole, getByText } = renderWithProvider(
+      <SafeAreaProvider>
+        <Confirm />
+      </SafeAreaProvider>,
+      {
+        state: personalSignatureConfirmationState,
+      },
+    );
     expect(getByText('Signature request')).toBeDefined();
     expect(
       getByText('Review request details before you confirm.'),
@@ -49,8 +84,10 @@ describe('Confirm', () => {
   });
 
   it('should render correct information for typed sign v1', async () => {
-    const { getAllByRole, getAllByText, getByText, queryByText } =
-      renderWithProvider(<Confirm />, {
+    const { getAllByRole, getAllByText, getByText, queryByText } = renderWithProvider(
+      <SafeAreaProvider>
+        <Confirm />
+      </SafeAreaProvider>, {
         state: typedSignV1ConfirmationState,
       });
     expect(getByText('Signature request')).toBeDefined();
@@ -63,12 +100,15 @@ describe('Confirm', () => {
   });
 
   it('should render blockaid banner if confirmation has blockaid error response', async () => {
-    const { getByText } = renderWithProvider(<Confirm />, {
-      state: {
-        ...typedSignV1ConfirmationState,
-        signatureRequest: { securityAlertResponse },
-      },
-    });
+    const { getByText } = renderWithProvider(
+      <SafeAreaProvider>
+          <Confirm />
+        </SafeAreaProvider>, {
+        state: {
+          ...typedSignV1ConfirmationState,
+          signatureRequest: { securityAlertResponse },
+        },
+      });
     expect(getByText('Signature request')).toBeDefined();
     expect(getByText('This is a deceptive request')).toBeDefined();
   });
