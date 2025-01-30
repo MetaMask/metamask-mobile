bcf4044606f948edea7cae54d46b1d7b3e1d4218
Curtis David <Curtis.David7@gmail.com>
2025-01-29 19:25:51 -0500

    clean up test

--- e2e/specs/settings/addressbook-relaunch-app.spec.js
+++ e2e/specs/settings/addressbook-relaunch-app.spec.js
@@ -1,12 +1,9 @@
 'use strict';
 import { SmokeCore } from '../../tags';
-import SendView from '../../pages/Send/SendView';
 import SettingsView from '../../pages/Settings/SettingsView';
 import ContactsView from '../../pages/Settings/Contacts/ContactsView';
 import AddContactView from '../../pages/Settings/Contacts/AddContactView';
-import AddAddressModal from '../../pages/Send/AddAddressModal';
 import TabBarComponent from '../../pages/wallet/TabBarComponent';
-import WalletActionsBottomSheet from '../../pages/wallet/WalletActionsBottomSheet';
 import { loginToApp } from '../../viewHelper';
 import FixtureBuilder from '../../fixtures/fixture-builder';
 import {
@@ -17,18 +14,12 @@ import {
 import TestHelpers from '../../helpers';
 import FixtureServer from '../../fixtures/fixture-server';
 import { getFixturesServerPort } from '../../fixtures/utils';
-import CommonView from '../../pages/CommonView';
-import enContent from '../../../locales/languages/en.json';
-import DeleteContactBottomSheet from '../../pages/Settings/Contacts/DeleteContactBottomSheet';
 import Assertions from '../../utils/Assertions';
 
-const INVALID_ADDRESS = '0xB8B4EE5B1b693971eB60bDa15211570df2dB221L';
-const TETHER_ADDRESS = '0xdac17f958d2ee523a2206206994597c13d831ec7';
-const MYTH_ADDRESS = '0x1FDb169Ef12954F20A15852980e1F0C122BfC1D6';
-const MEMO = 'Test adding ENS';
+
 const fixtureServer = new FixtureServer();
 
-describe(SmokeCore('Addressbook Tests'), () => {
+describe(SmokeCore('Addressbook Relaunch'), () => {
   beforeAll(async () => {
     await TestHelpers.reverseServerPort();
     const fixture = new FixtureBuilder().build();
