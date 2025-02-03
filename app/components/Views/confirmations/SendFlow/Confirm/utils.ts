0499860556efe3649eb0372becf0168fd902eae7
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-02-03 16:22:51 +0100

    Fix calculating maxFee by using gasFeeMaxHex to be precise

--- app/components/Views/confirmations/SendFlow/Confirm/utils.ts
+++ app/components/Views/confirmations/SendFlow/Confirm/utils.ts
@@ -1,6 +1,6 @@
 import { BN } from 'ethereumjs-util';
 import { remove0x, add0x } from '@metamask/utils';
-import { toWei } from '../../../../../util/number';
+import { hexToBN } from '@metamask/controller-utils';
 import { updateEditableParams } from '../../../../../util/transaction-controller';
 
 export const updateTransactionToMaxValue = async ({
@@ -14,20 +14,20 @@ export const updateTransactionToMaxValue = async ({
   transactionId: string;
   isEIP1559Transaction: boolean;
   EIP1559GasTransaction: {
-    gasFeeMaxNative: string;
+    gasFeeMaxHex: string;
   };
   legacyGasTransaction: {
-    gasFeeMaxNative: string;
+    gasFeeMaxHex: string;
   };
   accountBalance: string;
   setTransactionValue: (value: string) => void;
 }) => {
-  const { gasFeeMaxNative } = isEIP1559Transaction
+  const { gasFeeMaxHex } = isEIP1559Transaction
     ? EIP1559GasTransaction
     : legacyGasTransaction;
 
   const accountBalanceBN = new BN(remove0x(accountBalance), 16);
-  const transactionFeeMax = new BN(toWei(gasFeeMaxNative, 'ether'), 16);
+  const transactionFeeMax = hexToBN(gasFeeMaxHex);
 
   const maxTransactionValueBN = accountBalanceBN.sub(transactionFeeMax);
 
