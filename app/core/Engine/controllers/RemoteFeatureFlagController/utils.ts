9a31f045cb2be84e4877ac8ee424e9417865282d
Nico MASSART <NicolasMassart@users.noreply.github.com>
2025-02-04 18:24:15 +0000

    chore(runway): cherry-pick feat: customize fetchInterval for remoteFeatureFlagController to 15min (#13341)
    ## **Description**
    
    - add a new app constant for the 15 min fetch interval
    - add fetch interval param using the constant
    - add test that controller builder function passes fetch interval param
    - remove out of scope unit tests (this was testing the controller. This
    is [already tested in controller
    itself](https://github.com/MetaMask/core/blob/main/packages/remote-feature-flag-controller/src/remote-feature-flag-controller.test.ts)
    and adds a dangerous and blocking link to non local code.
    
    ## **Related issues**
    
    Fixes mobile app part of
    https://github.com/MetaMask/MetaMask-planning/issues/4098
    
    ## **Manual testing steps**
    
    (Manual testing only be done by someone with Launch Darkly access)
    1. activate a remote feature flag
    2. Load app
    3. Check feature is activated
    4. disable remote feature flag
    5. Wait for 15 min
    6. Reload the app
    7. Check feature is deactivated
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/core/Engine/controllers/RemoteFeatureFlagController/utils.ts
+++ app/core/Engine/controllers/RemoteFeatureFlagController/utils.ts
@@ -9,6 +9,7 @@ import {
 import Logger from '../../../../util/Logger';
 
 import { RemoteFeatureFlagInitParamTypes } from './types';
+import AppConstants from '../../../AppConstants';
 
 const getFeatureFlagAppEnvironment = () => {
   const env = process.env.METAMASK_ENVIRONMENT;
@@ -34,6 +35,7 @@ export const createRemoteFeatureFlagController = ({
   messenger,
   disabled,
   getMetaMetricsId,
+  fetchInterval = AppConstants.FEATURE_FLAGS_API.DEFAULT_FETCH_INTERVAL,
 }: RemoteFeatureFlagInitParamTypes) => {
   const remoteFeatureFlagController = new RemoteFeatureFlagController({
     messenger,
@@ -48,6 +50,7 @@ export const createRemoteFeatureFlagController = ({
         distribution: getFeatureFlagAppDistribution(),
       },
     }),
+    fetchInterval,
   });
 
   if (disabled) {
