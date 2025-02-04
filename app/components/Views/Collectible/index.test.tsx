c23e3bbf40c5fb5b49f3624a04425b5450aab1b3
salimtb <salim.toubal@outlook.com>
2025-02-04 16:33:34 +0100

    fix: add unit tests

--- app/components/Views/Collectible/index.test.tsx
+++ app/components/Views/Collectible/index.test.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Collectible from './';
+import Collectible from '.';
 import configureMockStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import { backgroundState } from '../../../util/test/initial-root-state';
 import { ThemeContext, mockTheme } from '../../../util/theme';
-import { render } from '@testing-library/react-native';
+import { act, render } from '@testing-library/react-native';
+import Engine from '../../../core/Engine';
+
+jest.mock('../../../core/Engine', () => ({
+  context: {
+    NftController: {
+      addNft: jest.fn(),
+      updateNftMetadata: jest.fn(),
+      checkAndUpdateAllNftsOwnershipStatus: jest.fn(),
+    },
+    NftDetectionController: {
+      detectNfts: jest.fn(),
+    },
+  },
+}));
 
 const mockStore = configureMockStore();
 const initialState = {
@@ -32,8 +46,6 @@ const navigationMock = {
   navigate: jest.fn(),
   push: jest.fn(),
 };
-
-// const toggleModalMock = jest.fn();
 
 const defaultCollectibleContract = {
   address: '0x1',
@@ -75,5 +87,32 @@ describe('Collectible', () => {
       'collectible-contract-information-title',
     );
     expect(modal).toBeTruthy();
+  });
+
+  it('calls detectNfts and resets refreshing state', async () => {
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <ThemeContext.Provider value={mockTheme}>
+          <Collectible
+            navigation={navigationMock}
+            route={{ params: defaultCollectibleContract }}
+          />
+        </ThemeContext.Provider>
+      </Provider>,
+    );
+
+    // Locate the RefreshControl using its testID.
+    const scrollView = getByTestId('refresh-control');
+
+    expect(scrollView).toBeDefined();
+
+    const { refreshControl } = scrollView.props;
+    await act(async () => {
+      await refreshControl.props.onRefresh();
+    });
+
+    expect(
+      Engine.context.NftDetectionController.detectNfts,
+    ).toHaveBeenCalledTimes(1);
   });
 });
