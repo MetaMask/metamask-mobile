ff6f2b77b3138fe026a5b3dd5cbf6847d8f69c75
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-02-04 11:47:14 +0100

    Implement confirm layout

--- app/components/Views/confirmations/Confirm/Confirm.tsx
+++ app/components/Views/confirmations/Confirm/Confirm.tsx
@@ -1,39 +1,67 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { TransactionType } from '@metamask/transaction-controller';
 
 import { useStyles } from '../../../../component-library/hooks';
+import useApprovalRequest from '../hooks/useApprovalRequest';
+import { useConfirmationRedesignEnabled } from '../hooks/useConfirmationRedesignEnabled';
+
 import BottomModal from '../components/UI/BottomModal';
 import AccountNetworkInfo from '../components/Confirm/AccountNetworkInfo';
 import Footer from '../components/Confirm/Footer';
 import Info from '../components/Confirm/Info';
 import SignatureBlockaidBanner from '../components/Confirm/SignatureBlockaidBanner';
 import Title from '../components/Confirm/Title';
-import useApprovalRequest from '../hooks/useApprovalRequest';
-import { useConfirmationRedesignEnabled } from '../hooks/useConfirmationRedesignEnabled';
+
 import styleSheet from './Confirm.styles';
 
-const Confirm = () => {
-  const { isRedesignedEnabled } = useConfirmationRedesignEnabled();
+const FLAT_CONFIRMATIONS: TransactionType[] = [
+  // To be filled with flat confirmations
+];
+
+const ConfirmationLayout = ({ children }: { children: React.ReactNode }) => {
   const { approvalRequest } = useApprovalRequest();
+  const isFlatConfirmation = FLAT_CONFIRMATIONS.includes(
+    approvalRequest?.type as TransactionType,
+  );
+  const { isRedesignedEnabled } = useConfirmationRedesignEnabled();
   const { styles } = useStyles(styleSheet, {});
 
   if (!isRedesignedEnabled) {
     return null;
   }
 
+  if (isFlatConfirmation) {
+    return <SafeAreaView style={styles.mainContainer}>{children}</SafeAreaView>;
+  }
+
   return (
     <BottomModal canCloseOnBackdropClick={false}>
-      <View style={styles.container} testID={approvalRequest?.type}>
-        <View>
-          <Title />
-          {/* TODO: component SignatureBlockaidBanner to be removed once we implement alert system in mobile */}
-          <SignatureBlockaidBanner />
-          <AccountNetworkInfo />
-          <Info />
-        </View>
-        <Footer />
+      <View style={styles.container}>
+        {children}
       </View>
     </BottomModal>
+  );
+};
+
+const Confirm = () => {
+  const { approvalRequest } = useApprovalRequest();
+  const isFlatConfirmation = FLAT_CONFIRMATIONS.includes(
+    approvalRequest?.type as TransactionType,
+  );
+  const ContentDisplay = isFlatConfirmation ? ScrollView : View;
+
+  return (
+    <ConfirmationLayout>
+      <ContentDisplay>
+        <Title />
+        <SignatureBlockaidBanner />
+        <AccountNetworkInfo />
+        <Info />
+      </ContentDisplay>
+      <Footer />
+    </ConfirmationLayout>
   );
 };
 
