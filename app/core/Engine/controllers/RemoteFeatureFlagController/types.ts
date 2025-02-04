23724fe064740502a68ec3d32c63ed04353c5600
metamaskbot <metamaskbot@users.noreply.github.com>
2025-02-04 11:44:53 -0700

    cherry-pick 508a5f7071

--- app/core/Engine/controllers/RemoteFeatureFlagController/types.ts
+++ app/core/Engine/controllers/RemoteFeatureFlagController/types.ts
@@ -1,7 +1,12 @@
-import { RemoteFeatureFlagControllerMessenger, RemoteFeatureFlagControllerState } from '@metamask/remote-feature-flag-controller';
+import {
+  RemoteFeatureFlagControllerMessenger,
+  RemoteFeatureFlagControllerState,
+} from '@metamask/remote-feature-flag-controller';
 
 export interface RemoteFeatureFlagInitParamTypes {
   state?: RemoteFeatureFlagControllerState;
   messenger: RemoteFeatureFlagControllerMessenger;
   disabled: boolean;
+  getMetaMetricsId: () => string;
+  fetchInterval?: number;
 }
--- app/core/Engine/controllers/RemoteFeatureFlagController/utils.ts
+++ app/core/Engine/controllers/RemoteFeatureFlagController/utils.ts
@@ -9,23 +9,31 @@ import {
 import Logger from '../../../../util/Logger';
 
 import { RemoteFeatureFlagInitParamTypes } from './types';
+import AppConstants from '../../../AppConstants';
 
 const getFeatureFlagAppEnvironment = () => {
   const env = process.env.METAMASK_ENVIRONMENT;
   switch (env) {
-    case 'local': return EnvironmentType.Development;
-    case 'pre-release': return EnvironmentType.ReleaseCandidate;
-    case 'production': return EnvironmentType.Production;
-    default: return EnvironmentType.Development;
+    case 'local':
+      return EnvironmentType.Development;
+    case 'pre-release':
+      return EnvironmentType.ReleaseCandidate;
+    case 'production':
+      return EnvironmentType.Production;
+    default:
+      return EnvironmentType.Development;
   }
 };
 
 const getFeatureFlagAppDistribution = () => {
   const dist = process.env.METAMASK_BUILD_TYPE;
   switch (dist) {
-    case 'main': return DistributionType.Main;
-    case 'flask': return DistributionType.Flask;
-    default: return DistributionType.Main;
+    case 'main':
+      return DistributionType.Main;
+    case 'flask':
+      return DistributionType.Flask;
+    default:
+      return DistributionType.Main;
   }
 };
 
@@ -33,6 +41,7 @@ export const createRemoteFeatureFlagController = ({
   state,
   messenger,
   disabled,
+  fetchInterval = AppConstants.FEATURE_FLAGS_API.DEFAULT_FETCH_INTERVAL,
 }: RemoteFeatureFlagInitParamTypes) => {
   const remoteFeatureFlagController = new RemoteFeatureFlagController({
     messenger,
@@ -46,6 +55,7 @@ export const createRemoteFeatureFlagController = ({
         distribution: getFeatureFlagAppDistribution(),
       },
     }),
+    fetchInterval,
   });
 
   if (disabled) {
