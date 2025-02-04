26722c51f04facd336f847a3dc03c10661f5c211
Bryan Fullam <bryan.fullam@consensys.net>
2025-02-04 14:58:14 +0000

    chore(runway): cherry-pick fix: check isNative to determine if a token is a gas token (#13315)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    This PR fixes an issue with sending native gas tokens on chains where
    the gas token is not ETH.
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-mobile/issues/12855
    
    ## **Manual testing steps**
    
    1. Go to Avalanche
    2. Make sure you have some balance of AVAX (the native gas token)
    1. Go to Send
    3. Make sure native gas token is selected
    4. Try to send some AVAX < your total balance
    5. You should not see any warnings about not having enough balance
    
    Do the same thing but on Arbitrum (native gas token is ETH)
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: IF <139582705+infiniteflower@users.noreply.github.com>

--- app/components/Views/confirmations/SendFlow/Amount/index.js
+++ app/components/Views/confirmations/SendFlow/Amount/index.js
@@ -103,6 +103,7 @@ import {
   selectProviderTypeByChainId,
 } from '../../../../../selectors/networkController';
 import { selectContractExchangeRatesByChainId } from '../../../../../selectors/tokenRatesController';
+import { isNativeToken } from '../../utils/generic';
 
 const KEYBOARD_OFFSET = Device.isSmallDevice() ? 80 : 120;
 
@@ -606,7 +607,7 @@ class Amount extends PureComponent {
   hasExchangeRate = () => {
     const { selectedAsset, conversionRate, contractExchangeRates } = this.props;
 
-    if (selectedAsset.isETH) {
+    if (isNativeToken(selectedAsset)) {
       return !!conversionRate;
     }
     const exchangeRate =
@@ -769,7 +770,7 @@ class Amount extends PureComponent {
       transactionObject.data = collectibleTransferTransactionProperties.data;
       transactionObject.to = collectibleTransferTransactionProperties.to;
       transactionObject.value = collectibleTransferTransactionProperties.value;
-    } else if (!selectedAsset.isETH) {
+    } else if (!isNativeToken(selectedAsset)) {
       const tokenAmount = toTokenMinimalUnit(value, selectedAsset.decimals);
       transactionObject.data = generateTransferData('transfer', {
         toAddress: transactionTo,
@@ -783,7 +784,7 @@ class Amount extends PureComponent {
       transactionObject.readableValue = value;
     }
 
-    if (selectedAsset.isETH) {
+    if (isNativeToken(selectedAsset)) {
       transactionObject.data = PREFIX_HEX_STRING;
       transactionObject.to = transactionTo;
     }
@@ -798,8 +799,8 @@ class Amount extends PureComponent {
       transactionState: { transaction, transactionTo },
     } = this.props;
 
-    if (selectedAsset.isETH) {
-      transaction.data = undefined;
+    if (isNativeToken(selectedAsset)) {
+      transaction.data = '0x';
       transaction.to = transactionTo;
       transaction.value = BNToHex(toWei(value));
     } else if (selectedAsset.tokenId) {
@@ -851,7 +852,7 @@ class Amount extends PureComponent {
       }
 
       if (!amountError) {
-        if (selectedAsset.isETH) {
+        if (isNativeToken(selectedAsset)) {
           weiBalance = hexToBN(accounts[selectedAddress].balance);
           weiInput = weiValue.add(estimatedTotalGas);
         } else {
@@ -906,7 +907,7 @@ class Amount extends PureComponent {
     const { internalPrimaryCurrencyIsCrypto, estimatedTotalGas } = this.state;
     const tokenBalance = contractBalances[selectedAsset.address] || '0x0';
     let input;
-    if (selectedAsset.isETH) {
+    if (isNativeToken(selectedAsset)) {
       const balanceBN = hexToBN(accounts[selectedAddress].balance);
       const realMaxValue = balanceBN.sub(estimatedTotalGas);
       const maxValue =
@@ -967,7 +968,7 @@ class Amount extends PureComponent {
       ? handleWeiNumber(inputValue)
       : '0';
     selectedAsset = selectedAsset || this.props.selectedAsset;
-    if (selectedAsset.isETH) {
+    if (isNativeToken(selectedAsset)) {
       // toWei can throw error if input is not a number: Error: while converting number to string, invalid number value
       let weiValue = 0;
 
@@ -1040,22 +1041,22 @@ class Amount extends PureComponent {
   };
 
   handleSelectedAssetBalance = (
-    { address, decimals, symbol, isETH, isNative },
+    selectedAsset,
     renderableBalance,
   ) => {
     const { accounts, selectedAddress, contractBalances } = this.props;
     let currentBalance;
     if (renderableBalance) {
-      currentBalance = `${renderableBalance} ${symbol}`;
-    } else if (isETH || isNative) {
+      currentBalance = `${renderableBalance} ${selectedAsset.symbol}`;
+    } else if (isNativeToken(selectedAsset)) {
       currentBalance = `${renderFromWei(
         accounts[selectedAddress].balance,
-      )} ${symbol}`;
+      )} ${selectedAsset.symbol}`;
     } else {
       currentBalance = `${renderFromTokenMinimalUnit(
-        contractBalances[address],
-        decimals,
-      )} ${symbol}`;
+        contractBalances[selectedAsset.address],
+        selectedAsset.decimals,
+      )} ${selectedAsset.symbol}`;
     }
     this.setState({ currentBalance });
   };
@@ -1098,7 +1099,7 @@ class Amount extends PureComponent {
     const colors = this.context.colors || mockTheme.colors;
     const styles = createStyles(colors);
 
-    if (token.isETH) {
+    if (isNativeToken(token)) {
       balance = renderFromWei(accounts[selectedAddress].balance);
       balanceFiat = weiToFiat(
         hexToBN(accounts[selectedAddress].balance),
@@ -1126,7 +1127,7 @@ class Amount extends PureComponent {
         onPress={() => this.pickSelectedAsset(token)}
       >
         <View style={styles.assetElement}>
-          {token.isETH ? (
+          {isNativeToken(token) ? (
             <NetworkMainAssetLogo big />
           ) : (
             <TokenImage
@@ -1288,7 +1289,7 @@ class Amount extends PureComponent {
     };
 
     const isSwappable =
-      !selectedAsset.isETH &&
+      !isNativeToken(selectedAsset) &&
       AppConstants.SWAPS.ACTIVE &&
       swapsIsLive &&
       isSwapsAllowed(chainId) &&
@@ -1306,7 +1307,10 @@ class Amount extends PureComponent {
             .build(),
         );
         navigateToSwap();
-      } else if (isNetworkBuyNativeTokenSupported && selectedAsset.isETH) {
+      } else if (
+        isNetworkBuyNativeTokenSupported &&
+        isNativeToken(selectedAsset)
+      ) {
         this.props.metrics.trackEvent(
           this.props.metrics
             .createEventBuilder(MetaMetricsEvents.LINK_CLICKED)
@@ -1385,7 +1389,8 @@ class Amount extends PureComponent {
               onPress={navigateToBuyOrSwaps}
               style={styles.errorBuyWrapper}
             >
-              {isNetworkBuyNativeTokenSupported && selectedAsset.isETH ? (
+              {isNetworkBuyNativeTokenSupported &&
+              isNativeToken(selectedAsset) ? (
                 <Text style={[styles.error]}>
                   {strings('transaction.more_to_continue', {
                     ticker: getTicker(ticker),
