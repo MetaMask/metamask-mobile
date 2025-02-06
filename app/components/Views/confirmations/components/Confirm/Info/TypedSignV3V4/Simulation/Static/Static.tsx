f445c01cd0a14588257b602882797fd74f530b7c
digiwand <20778143+digiwand@users.noreply.github.com>
2025-02-07 01:05:03 +0200

    fix: simulation loading color and overflow

--- app/components/Views/confirmations/components/Confirm/Info/TypedSignV3V4/Simulation/Static/Static.tsx
+++ app/components/Views/confirmations/components/Confirm/Info/TypedSignV3V4/Simulation/Static/Static.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 
 import { useStyles } from '../../../../../../../../../component-library/hooks';
 import InfoRow from '../../../../../UI/InfoRow';
 import InfoSection from '../../../../../UI/InfoRow/InfoSection';
-import Loader from '../../../../../../../../../component-library/components-temp/Loader';
 
 const styleSheet = () => StyleSheet.create({
   base: {
     display: 'flex',
     justifyContent: 'space-between',
   },
-  loaderContainer: {
+  activityIndicator: {
     display: 'flex',
     justifyContent: 'center',
   },
@@ -32,7 +31,7 @@ const StaticSimulation: React.FC<{
   isLoading,
   isCollapsed = false,
 }) => {
-  const { styles } = useStyles(styleSheet, {});
+  const { styles, theme } = useStyles(styleSheet, {});
 
   return(
     <View style={isCollapsed ? styles.base : {}}>
@@ -41,9 +40,11 @@ const StaticSimulation: React.FC<{
           {description}
         </InfoRow>
         {isLoading ? (
-          <View style={styles.loaderContainer}>
-            <Loader size={'small'} />
-          </View>
+          <ActivityIndicator
+            style={styles.activityIndicator}
+            size="small"
+            color={theme.colors.warning.default}
+          />
         ) : (
           simulationElements
           )}
