ff9283d3073c1a82e89afd7381a431982a18bc3f
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-02-06 09:40:56 +0100

    Fix unit tests

--- app/components/Views/confirmations/hooks/useTransactionMetadata.test.ts
+++ app/components/Views/confirmations/hooks/useTransactionMetadata.test.ts
@@ -1,81 +1,48 @@
-import { ApprovalType } from '@metamask/controller-utils';
-import {
-  TransactionControllerState,
-  TransactionMeta,
-  TransactionType,
-} from '@metamask/transaction-controller';
+import { TransactionType } from '@metamask/transaction-controller';
+import { merge } from 'lodash';
 
 import { useTransactionMetadata } from './useTransactionMetadata';
 import { renderHookWithProvider } from '../../../../util/test/renderWithProvider';
-
-const ID_MOCK = '123-456-789';
-
-const APPROVAL_REQUEST_MOCK = {
-  id: ID_MOCK,
-  type: ApprovalType.Transaction,
-};
-
-const TRANSACTION_METADATA_MOCK = {
-  id: ID_MOCK,
-  type: TransactionType.contractInteraction,
-};
-
-function renderHook({
-  approvalType,
-  transactionMetadata,
-}: {
-  approvalType?: ApprovalType;
-  transactionMetadata: Partial<TransactionMeta>;
-}) {
-  const { result } = renderHookWithProvider(useTransactionMetadata, {
-    state: {
-      engine: {
-        backgroundState: {
-          ApprovalController: {
-            pendingApprovals: {
-              [ID_MOCK]: {
-                ...APPROVAL_REQUEST_MOCK,
-                type: approvalType ?? ApprovalType.Transaction,
-              },
-            },
-          },
-          TransactionController: {
-            transactions: [transactionMetadata],
-          } as unknown as TransactionControllerState,
-        },
-      },
-    },
-  });
-
-  return result.current;
-}
+import {
+  personalSignatureConfirmationState,
+  stakingDepositConfirmationState,
+} from '../../../../util/test/confirm-data-helpers';
 
 describe('useTransactionMetadata', () => {
   it('returns transaction metadata when approval type is Transaction', () => {
-    const result = renderHook({
-      transactionMetadata: TRANSACTION_METADATA_MOCK,
+    const { result } = renderHookWithProvider(useTransactionMetadata, {
+      state: stakingDepositConfirmationState,
     });
 
-    expect(result).toEqual(TRANSACTION_METADATA_MOCK);
+    expect(result.current).toEqual(
+      stakingDepositConfirmationState.engine.backgroundState
+        .TransactionController.transactions[0],
+    );
   });
 
   it('returns undefined when approval type is not Transaction', () => {
-    const result = renderHook({
-      approvalType: ApprovalType.PersonalSign,
-      transactionMetadata: TRANSACTION_METADATA_MOCK,
+    const { result } = renderHookWithProvider(useTransactionMetadata, {
+      state: personalSignatureConfirmationState,
     });
 
-    expect(result).toBeUndefined();
+    expect(result.current).toBeUndefined();
   });
 
   it('returns undefined when transaction metadata is not found', () => {
-    const result = renderHook({
-      transactionMetadata: {
-        ...TRANSACTION_METADATA_MOCK,
-        id: 'invalid-id',
+    const state = merge(personalSignatureConfirmationState, {
+      engine: {
+        backgroundState: {
+          TransactionController: {
+            transactions: [],
+          },
+        },
       },
     });
 
-    expect(result).toBeUndefined();
+    const { result } = renderHookWithProvider(useTransactionMetadata, {
+      state,
+    });
+
+    expect(result.current).toBeUndefined();
   });
 });
