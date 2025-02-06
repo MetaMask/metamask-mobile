b8a38943e6215d31f56b9cfcc5939db71a15a1bf
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-02-06 10:20:16 +0100

    Fix unit tests

--- app/components/Views/confirmations/components/Confirm/Info/Info.test.tsx
+++ app/components/Views/confirmations/components/Confirm/Info/Info.test.tsx
@@ -4,6 +4,21 @@ import renderWithProvider from '../../../../../../util/test/renderWithProvider';
 import { personalSignatureConfirmationState } from '../../../../../../util/test/confirm-data-helpers';
 import Info from './Info';
 
+jest.mock('../../../../../../core/Engine', () => ({
+  getTotalFiatAccountBalance: () => ({ tokenFiat: 10 }),
+  context: {
+    KeyringController: {
+      state: {
+        keyrings: [],
+      },
+      getOrAddQRKeyring: jest.fn(),
+    },
+  },
+  controllerMessenger: {
+    subscribe: jest.fn(),
+  },
+}));
+
 describe('Info', () => {
   it('should render correctly for personal sign', async () => {
     const { getByText } = renderWithProvider(<Info />, {
