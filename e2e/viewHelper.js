9addd65b3ccb4bec99bce738d31826f626899e24
Curtis David <Curtis.David7@gmail.com>
2025-02-06 13:30:19 -0500

    disable sync for e2e on login

--- e2e/viewHelper.js
+++ e2e/viewHelper.js
@@ -219,8 +219,10 @@ export const switchToSepoliaNetwork = async () => {
 
 export const loginToApp = async () => {
   const PASSWORD = '123123123';
+  await device.disableSynchronization();
   await Assertions.checkIfVisible(LoginView.container);
   await LoginView.enterPassword(PASSWORD);
+  await device.enableSynchronization();
 };
 
 export const waitForTestDappToLoad = async () => {
