a31beb4adbbdb42ece26235913568276f890ecd5
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-02-07 09:55:42 +0100

    Implement staking back navigation header

--- app/components/Views/confirmations/components/Confirm/Info/StakingDeposit/StakingDeposit.tsx
+++ app/components/Views/confirmations/components/Confirm/Info/StakingDeposit/StakingDeposit.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-const StakingDeposit = () => <Text>Staking Deposit</Text>;
+import { strings } from '../../../../../../../../locales/i18n';
+import { useConfirmActions } from '../../../../hooks/useConfirmActions';
+import FlatNavHeader from '../../FlatNavHeader';
+
+const StakingDeposit = () => {
+  const { onReject } = useConfirmActions();
+
+  return (
+    <>
+      <FlatNavHeader title={strings('stake.stake')} onLeftPress={onReject} />
+    </>
+  );
+};
 
 export default StakingDeposit;
