523059437cc149cb6ae6b1dfa1c4f00450385e1b
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-02-11 07:39:16 +0100

    Remove unused import

--- app/components/Views/confirmations/components/Confirm/Info/StakingDeposit/StakingDeposit.tsx
+++ app/components/Views/confirmations/components/Confirm/Info/StakingDeposit/StakingDeposit.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { Text } from 'react-native';
 
 import { strings } from '../../../../../../../../locales/i18n';
-import { useConfirmActions } from '../../../../hooks/useConfirmActions';
 import FlatNavHeader from '../../FlatNavHeader';
 
 const StakingDeposit = () => {
-  const { onReject } = useConfirmActions();
-
   return (
     <>
-      <FlatNavHeader title={strings('stake.stake')} onLeftPress={onReject} />
+      <FlatNavHeader title={strings('stake.stake')} />
     </>
   );
 };
