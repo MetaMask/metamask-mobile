c12c952d762bb3590027aa08d82082dd256761cf
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-02-11 07:48:06 +0100

    Fix lint

--- app/components/Views/confirmations/components/Confirm/Info/StakingDeposit/StakingDeposit.tsx
+++ app/components/Views/confirmations/components/Confirm/Info/StakingDeposit/StakingDeposit.tsx
@@ -3,12 +3,6 @@ import React from 'react';
 import { strings } from '../../../../../../../../locales/i18n';
 import FlatNavHeader from '../../FlatNavHeader';
 
-const StakingDeposit = () => {
-  return (
-    <>
-      <FlatNavHeader title={strings('stake.stake')} />
-    </>
-  );
-};
+const StakingDeposit = () => <FlatNavHeader title={strings('stake.stake')} />;
 
 export default StakingDeposit;
