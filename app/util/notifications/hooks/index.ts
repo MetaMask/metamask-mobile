67d13909d0189a8a736c206b4d56d10c7fd5fe87
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2025-02-11 19:33:27 +0000

    feat: refactor and cleanup push notification effects

--- app/util/notifications/hooks/index.ts
+++ app/util/notifications/hooks/index.ts
@@ -1,74 +1,13 @@
-import { useCallback, useEffect } from 'react';
-import {
-  INotification,
-  TRIGGER_TYPES,
-} from '@metamask/notification-services-controller/notification-services';
-
-import { useSelector } from 'react-redux';
-import { isNotificationsFeatureEnabled } from '../../../util/notifications';
-
-import FCMService from '../services/FCMService';
-import NotificationsService from '../services/NotificationService';
-import { selectIsMetamaskNotificationsEnabled } from '../../../selectors/notifications';
-import { Linking } from 'react-native';
 import { NavigationContainerRef } from '@react-navigation/native';
-import Routes from '../../../constants/navigation/Routes';
+import { useRegisterPushNotificationsEffect } from './useRegisterPushNotificationsEffect';
 
-const useNotificationHandler = (navigation: NavigationContainerRef) => {
-  /**
-   * Handles the action based on the type of notification (sent from the backend & following Notification types) that is opened
-   * @param notification - The notification that is opened
-   */
-
-  const isNotificationEnabled = useSelector(
-    selectIsMetamaskNotificationsEnabled,
-  );
-
-  const handleNotificationCallback = useCallback(
-    async (notification: INotification) => {
-      if (!notification) {
-        return;
-      }
-      if (
-        notification.type === TRIGGER_TYPES.FEATURES_ANNOUNCEMENT &&
-        notification.data.externalLink
-      ) {
-        Linking.openURL(notification.data.externalLink.externalLinkUrl);
-      } else {
-        navigation.navigate(Routes.NOTIFICATIONS.VIEW);
-      }
-    },
-    [navigation],
-  );
-
-  const notificationEnabled =
-    isNotificationsFeatureEnabled() && isNotificationEnabled;
-
-  useEffect(() => {
-    if (!notificationEnabled) return;
-
-    // Firebase Cloud Messaging
-    FCMService.registerAppWithFCM();
-    FCMService.saveFCMToken();
-    FCMService.getFCMToken();
-    FCMService.listenForMessagesBackground();
-
-    // Notifee
-    NotificationsService.onBackgroundEvent(
-      async ({ type, detail }) =>
-        await NotificationsService.handleNotificationEvent({
-          type,
-          detail,
-          callback: handleNotificationCallback,
-        }),
-    );
-
-    const unsubscribeForegroundEvent = FCMService.listenForMessagesForeground();
-
-    return () => {
-      unsubscribeForegroundEvent();
-    };
-  }, [handleNotificationCallback, notificationEnabled]);
+/**
+ * Registers Push Notifications
+ * @todo - clean up props once we finalise integration
+ * @param navigation - page navigation prop
+ */
+const useNotificationHandler = (_navigation: NavigationContainerRef) => {
+  useRegisterPushNotificationsEffect();
 };
 
 export default useNotificationHandler;
---
+++ app/util/notifications/hooks/useRegisterPushNotificationsEffect.ts
@@ -0,0 +1,164 @@
+import { useNavigation, type NavigationProp } from '@react-navigation/native';
+import {
+  INotification,
+  TRIGGER_TYPES,
+} from '@metamask/notification-services-controller/notification-services';
+import Engine from '../../../core/Engine';
+import Routes from '../../../constants/navigation/Routes';
+import NotificationsService from '../services/NotificationService';
+import { PressActionId } from '../types';
+import { useEffect } from 'react';
+import { isNotificationsFeatureEnabled } from '../constants';
+import { useSelector } from 'react-redux';
+import {
+  selectIsMetamaskNotificationsEnabled,
+  selectIsMetaMaskPushNotificationsEnabled,
+} from '../../../selectors/notifications';
+
+// TODO - improve navigation types, so we have Type-Safety for navigation props
+type NavigationParams = Record<string, { notification: INotification }>;
+
+// TODO - improve type inference for notifications we support
+function isINotification(n: unknown): n is INotification {
+  const assumedShape = n as INotification;
+  return Boolean(assumedShape?.type) && Boolean(assumedShape?.data);
+}
+
+/**
+ * Logic for handling a push notification click.
+ * It will publish an event, and attempt navigation to notifications view
+ * @param notification - notification click received from App Start or Background
+ * @param navigation - navigation prop for page navigations
+ * @returns - void
+ */
+function clickPushNotification(
+  notification: INotification,
+  navigation: NavigationProp<NavigationParams>,
+) {
+  // Publish Click Event
+  Engine.controllerMessenger.publish(
+    'NotificationServicesPushController:pushNotificationClicked',
+    notification,
+  );
+
+  // TODO, find a nicer way of abstracting the notifications we do support push notifications for
+  if (notification.type === TRIGGER_TYPES.SNAP) {
+    return;
+  }
+
+  // Navigate
+  navigation.navigate(Routes.NOTIFICATIONS.DETAILS, {
+    notification,
+  });
+}
+
+/**
+ * Android Devices use a `getInitialNotifications` if a push notification cold-starts the application.
+ * @param navigation - navigation prop for page navigations
+ * @returns - void
+ */
+async function onAppOpenNotification(
+  navigation: NavigationProp<NavigationParams>,
+) {
+  const initialNotification =
+    await NotificationsService.getInitialNotification();
+  if (!initialNotification) {
+    return;
+  }
+
+  const { notification, pressAction } = initialNotification;
+  const notificationDataStr = notification?.data?.dataStr;
+
+  if (!notificationDataStr) {
+    return;
+  }
+
+  try {
+    // Notify can only store strings
+    const notificationData = JSON.parse(notificationDataStr as string);
+    if (
+      pressAction?.id === PressActionId.OPEN_NOTIFICATIONS_VIEW &&
+      isINotification(notificationData)
+    ) {
+      clickPushNotification(notificationData, navigation);
+    }
+  } catch {
+    // Do Nothing
+  }
+}
+
+/**
+ * IOS/Anroid devices will use a notifee `backgroundEvent` if a push notification is delivered and clicked on a minimised app.
+ * (IOS also uses this for cold-starts).
+ * @param navigation - navigation prop used for page navigations
+ */
+async function onBackgroundEvent(navigation: NavigationProp<NavigationParams>) {
+  NotificationsService.onBackgroundEvent((event) =>
+    NotificationsService.handleNotificationEvent({
+      ...event,
+      callback: (notification) => {
+        const pressAction = event?.detail?.pressAction;
+        const notificationDataStr = notification?.data?.dataStr;
+
+        if (!notificationDataStr) {
+          return;
+        }
+
+        try {
+          // Notify can only store strings
+          const notificationData = JSON.parse(notificationDataStr as string);
+          if (
+            pressAction?.id === PressActionId.OPEN_NOTIFICATIONS_VIEW &&
+            isINotification(notificationData)
+          ) {
+            clickPushNotification(notificationData, navigation);
+          }
+        } catch {
+          // Do Nothing
+        }
+      },
+    }),
+  );
+}
+
+/**
+ * Effect that registers Notifee Push listeners
+ * - When push notifications are recieved
+ * - When push notifications are clicked
+ */
+export function useRegisterPushNotificationsEffect() {
+  const navigation: NavigationProp<NavigationParams> = useNavigation();
+  const notificationsFlagEnabled = isNotificationsFeatureEnabled();
+  const notificationsControllerEnabled = useSelector(
+    selectIsMetamaskNotificationsEnabled,
+  );
+  const notificationsPushControllerEnabled = useSelector(
+    selectIsMetaMaskPushNotificationsEnabled,
+  );
+  const notificationsEnabled =
+    notificationsFlagEnabled &&
+    notificationsControllerEnabled &&
+    notificationsPushControllerEnabled;
+
+  // App Open Effect
+  useEffect(() => {
+    try {
+      if (notificationsEnabled) {
+        onAppOpenNotification(navigation);
+      }
+    } catch {
+      // Do Nothing
+    }
+  }, [navigation, notificationsEnabled]);
+
+  // On Background and Foreground Events
+  useEffect(() => {
+    try {
+      if (notificationsEnabled) {
+        onBackgroundEvent(navigation);
+      }
+    } catch {
+      // Do Nothing
+    }
+  }, [navigation, notificationsEnabled]);
+}
