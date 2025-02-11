a1c3ba283f1c3f95c5ed96beb987f9e2bb7d9b2c
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-02-11 13:41:00 +0100

    feat: Implement staking deposit `TokenHero` component (#13418)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    This PR aims to add `TokenHero` component for draft staking deposit
    confirmation.
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/MetaMask-planning/issues/4080
    
    ## **Manual testing steps**
    
    N/A
    
    ## **Screenshots/Recordings**
    
    
    
    https://github.com/user-attachments/assets/693e970e-6c3b-4dda-9f68-f85a129dd7f0
    
    
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [X] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [X] I've completed the PR template to the best of my ability
    - [X] I’ve included tests if applicable
    - [X] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [X] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/Views/confirmations/components/Confirm/Info/StakingDeposit/StakingDeposit.tsx
+++ app/components/Views/confirmations/components/Confirm/Info/StakingDeposit/StakingDeposit.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 import { strings } from '../../../../../../../../locales/i18n';
 import FlatNavHeader from '../../FlatNavHeader';
+import TokenHero from '../../TokenHero';
 
-const StakingDeposit = () => <FlatNavHeader title={strings('stake.stake')} />;
-
+const StakingDeposit = () => (
+  <>
+    <FlatNavHeader title={strings('stake.stake')} />
+    <TokenHero />
+  </>
+);
 export default StakingDeposit;
