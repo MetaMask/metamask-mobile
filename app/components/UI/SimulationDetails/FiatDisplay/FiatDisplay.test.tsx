ad70ec86b964b1e7c050eaf5ef1b3cb7f39aed50
digiwand <20778143+digiwand@users.noreply.github.com>
2025-02-13 15:20:21 +0100

    fix: simulation Fiat precision and Fiat flickers different value before decimals are applied (#13371)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    - Fix fiat precision. We should show expected digits instead of 0s. 
      - Replaces areas where value is coerced to number
    - Fix related FiatDisplay test
    - invalid test where we called toBeDefined instead of toBeTruthy from
    queryByText
      - removed useFiatFormatter mock
      - fix invalid showFiatInTestnets mock
    - fix hideFiatForTestnet test related mocks since in
    useHideFiatForTestnet
    ```TEST_NETWORK_IDS.includes(chainId) && !showFiatInTestnets;``` was
    returning true in all cases as chainId was undefined.
    - Update FiatDisplay value to replace $100.00 → $100 and $100.2 →
    $100.20. useFiatFormatter formatted as expected.
    - Fixes fiat flickering issue while data is still being fetched - adds
    placeholder element while token details are pending
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-mobile/issues/13387
    
    ## **Manual testing steps**
    
    1. Go to https://develop.d3bkcslj57l47p.amplifyapp.com/
    2. Trigger Permit Batch
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <img width="320"
    src="https://github.com/user-attachments/assets/30dd5d66-91a3-4fc1-8a6a-c5ede4134c8c">
    
    ### **After example**
    
    <img width="320"
    src="https://github.com/user-attachments/assets/c5ad4c15-bed8-4428-a7f0-bbcf37f5c9c0">
    
    ### **Real after with hidden value if "Unlimited" is shown w/ some token
    icons still loading **
    
    <img width="320"
    src="https://github.com/user-attachments/assets/b8452419-254a-46d8-93af-fd083535e949">
    
    ### **Before flickering issue **
    
    
    https://github.com/user-attachments/assets/52178518-04ed-49fd-8568-d40d1b6c0637
    
    ### **After** 
    
    
    https://github.com/user-attachments/assets/2ef9fc1a-bd0f-457b-bf20-9e5250b1df97
    
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/UI/SimulationDetails/FiatDisplay/FiatDisplay.test.tsx
+++ app/components/UI/SimulationDetails/FiatDisplay/FiatDisplay.test.tsx
@@ -1,82 +1,67 @@
 import React from 'react';
+import BigNumber from 'bignumber.js';
 import { merge } from 'lodash';
+import { CHAIN_IDS } from '@metamask/transaction-controller';
 import renderWithProvider from '../../../../util/test/renderWithProvider';
 import { backgroundState } from '../../../../util/test/initial-root-state';
-
 import { IndividualFiatDisplay, TotalFiatDisplay } from './FiatDisplay';
-import { FIAT_UNAVAILABLE } from '../types';
-import useFiatFormatter from './useFiatFormatter';
 import { mockNetworkState } from '../../../../util/test/network';
-import { CHAIN_IDS } from '@metamask/transaction-controller';
+import { FIAT_UNAVAILABLE, FiatAmount } from '../types';
 
-jest.mock('./useFiatFormatter');
-
-const mockInitialState = {
-  engine: {
-    backgroundState,
-  },
-};
-
-const mockStateWithTestnet = merge({}, mockInitialState, {
-  engine: {
-    backgroundState: {
-      NetworkController: {
-        ...mockNetworkState({
-          chainId: CHAIN_IDS.SEPOLIA,
-          id: 'sepolia',
-          nickname: 'Sepolia',
-          ticker: 'ETH',
-        }),
-      },
+const mockStateWithTestnet = merge(
+  {
+    engine: {
+      backgroundState,
     },
   },
-});
-
-const mockStateWithShowingFiatOnTestnets = merge({}, mockStateWithTestnet, {
-  engine: {
-    backgroundState: {
-      PreferencesController: {
-        showFiatInTestnets: true,
+  {
+    engine: {
+      backgroundState: {
+        CurrencyRateController: {
+          currentCurrency: 'USD',
+        },
+        NetworkController: {
+          ...mockNetworkState({
+            chainId: CHAIN_IDS.SEPOLIA,
+            id: 'sepolia',
+            nickname: 'Sepolia',
+            ticker: 'ETH',
+          }),
+        },
       },
+    },
+    settings: {
+      showFiatOnTestnets: true,
     },
   },
-});
+);
 
-const mockStateWithHidingFiatOnTestnets = merge({}, mockStateWithTestnet, {
-  engine: {
-    backgroundState: {
-      PreferencesController: {
-        showFiatInTestnets: false,
-      },
-    },
+const mockStateWithHideFiatOnTestnets = merge({}, mockStateWithTestnet, {
+  settings: {
+    showFiatOnTestnets: false,
   },
 });
 
 describe('FiatDisplay', () => {
-  const mockUseFiatFormatter = jest.mocked(useFiatFormatter);
-
-  beforeEach(() => {
-    jest.resetAllMocks();
-    mockUseFiatFormatter.mockReturnValue((value: number) => `$${value}`);
-  });
-
   describe('IndividualFiatDisplay', () => {
     it.each([
       [FIAT_UNAVAILABLE, 'Not Available'],
-      [100, '$100'],
-      [-100, '$100'],
-    ])('when fiatAmount is %s it renders %s', (fiatAmount, expected) => {
-      const { queryByText } = renderWithProvider(
-        <IndividualFiatDisplay fiatAmount={fiatAmount} />,
-        { state: mockStateWithShowingFiatOnTestnets },
+      [new BigNumber(100), '$100'],
+      [new BigNumber(-100), '$100'],
+      [new BigNumber(-100.5), '$100.50'],
+      [new BigNumber('987543219876543219876.54321'), '$987,543,219,87...'],
+    ])('when fiatAmount is %s it renders %s', async (fiatAmount, expected) => {
+      const { findByText } = renderWithProvider(
+        <IndividualFiatDisplay fiatAmount={fiatAmount as BigNumber} />,
+        { state: mockStateWithTestnet },
       );
-      expect(queryByText(expected)).toBeDefined();
+      expect(await findByText(expected)).toBeTruthy();
     });
 
     it('does not render anything if hideFiatForTestnet is true', () => {
       const { queryByText } = renderWithProvider(
         <IndividualFiatDisplay fiatAmount={100} />,
-        { state: mockStateWithHidingFiatOnTestnets },
+        { state: mockStateWithHideFiatOnTestnets },
       );
       expect(queryByText('100')).toBe(null);
     });
@@ -86,20 +71,55 @@ describe('FiatDisplay', () => {
     it.each([
       [[FIAT_UNAVAILABLE, FIAT_UNAVAILABLE], 'Not Available'],
       [[], 'Not Available'],
-      [[100, 200, FIAT_UNAVAILABLE, 300], 'Total = $600'],
-      [[-100, -200, FIAT_UNAVAILABLE, -300], 'Total = $600'],
-    ])('when fiatAmounts is %s it renders %s', (fiatAmounts, expected) => {
-      const { queryByText } = renderWithProvider(
-        <TotalFiatDisplay fiatAmounts={fiatAmounts} />,
-        { state: mockStateWithShowingFiatOnTestnets },
-      );
-      expect(queryByText(expected)).toBeDefined();
-    });
+      [
+        [
+          new BigNumber(100),
+          new BigNumber(200),
+          FIAT_UNAVAILABLE,
+          new BigNumber(300),
+        ],
+        'Total = $600',
+      ],
+      [
+        [
+          new BigNumber(-100),
+          new BigNumber(-200),
+          FIAT_UNAVAILABLE,
+          new BigNumber(-300.2),
+        ],
+        'Total = $600.20',
+      ],
+      [
+        [
+          new BigNumber(new BigNumber('987543219876543219876.54321')),
+          new BigNumber(-200),
+          FIAT_UNAVAILABLE,
+          new BigNumber(-300.2),
+        ],
+        'Total = $987,543,219,876,543,219,376.34',
+      ],
+    ])(
+      'when fiatAmounts is %s it renders %s',
+      async (fiatAmounts, expected) => {
+        const { findByText } = renderWithProvider(
+          <TotalFiatDisplay fiatAmounts={fiatAmounts as FiatAmount[]} />,
+          { state: mockStateWithTestnet },
+        );
+
+        expect(await findByText(expected)).toBeTruthy();
+      },
+    );
 
     it('does not render anything if hideFiatForTestnet is true', () => {
+      const mockFiatAmounts = [
+        new BigNumber(100),
+        new BigNumber(200),
+        new BigNumber(300),
+      ] as unknown as FiatAmount[];
+
       const { queryByText } = renderWithProvider(
-        <TotalFiatDisplay fiatAmounts={[100, 200, 300]} />,
-        { state: mockStateWithHidingFiatOnTestnets },
+        <TotalFiatDisplay fiatAmounts={mockFiatAmounts} />,
+        { state: mockStateWithHideFiatOnTestnets },
       );
       expect(queryByText('600')).toBe(null);
     });
