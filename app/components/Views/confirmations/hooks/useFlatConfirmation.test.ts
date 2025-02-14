a02efdf534cb5dd46e1278465bcb85bd085b2c02
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-02-14 13:28:22 +0100

    Update

--- app/components/Views/confirmations/hooks/useFlatConfirmation.test.ts
+++ app/components/Views/confirmations/hooks/useFlatConfirmation.test.ts
@@ -1,18 +1,11 @@
 import { renderHookWithProvider } from '../../../../util/test/renderWithProvider';
 import {
   personalSignatureConfirmationState,
-  stakingDepositConfirmationState,
 } from '../../../../util/test/confirm-data-helpers';
 import { useFlatConfirmation } from './useFlatConfirmation';
 
 describe('useFlatConfirmation', () => {
-  it('returns true for staking confirmation', async () => {
-    const { result } = renderHookWithProvider(useFlatConfirmation, {
-      state: stakingDepositConfirmationState,
-    });
-
-    expect(result.current.isFlatConfirmation).toBe(true);
-  });
+  // TODO: Add unit test for once we have any existing flat confirmations
 
   it('returns false for personal sign request', async () => {
     const { result } = renderHookWithProvider(useFlatConfirmation, {
