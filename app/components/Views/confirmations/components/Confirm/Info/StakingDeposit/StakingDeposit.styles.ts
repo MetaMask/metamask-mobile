70151a253de0b937c303b0ad93afd5a0b1af6b5d
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-02-17 10:21:14 +0100

    Fix background color

--- app/components/Views/confirmations/components/Confirm/Info/StakingDeposit/StakingDeposit.styles.ts
+++ app/components/Views/confirmations/components/Confirm/Info/StakingDeposit/StakingDeposit.styles.ts
@@ -1,9 +1,11 @@
 import { StyleSheet } from 'react-native';
 import Device from '../../../../../../../util/device';
+import { Theme } from '../../../../../../../util/theme/models';
 
-const styleSheet = () =>
+const styleSheet = (params: { theme: Theme }) =>
   StyleSheet.create({
     container: {
+      backgroundColor: params.theme.colors.background.alternative,
       flex: 1,
       paddingHorizontal: 16,
       paddingVertical: 24,
