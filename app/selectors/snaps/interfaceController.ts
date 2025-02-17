b8390477cf3948d88ac683c0d27241a97b19ec88
Daniel Cross <dan.s.cross@icloud.com>
2025-02-17 16:30:58 +0100

    Update interfaceController.ts

--- app/selectors/snaps/interfaceController.ts
+++ app/selectors/snaps/interfaceController.ts
@@ -1,21 +1,46 @@
 import { createSelector } from '@reduxjs/toolkit';
-import { RootState } from '../../reducers';
 import { createDeepEqualSelector } from '../util';
 
-export const selectInterfaceControllerState = (state: RootState) =>
-  state.engine.backgroundState.SnapInterfaceController;
+/**
+ * Get the Snap interfaces from the redux state.
+ *
+ * @param state - Redux state object.
+ * @returns the Snap interfaces.
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getInterfaces = (state: any) =>
+  state?.engine?.backgroundState?.SnapInterfaceController.interfaces;
 
-export const selectInterfaces = createSelector(
-  selectInterfaceControllerState,
-  (controller) => controller.interfaces,
+/**
+ * Input selector providing a way to pass a Snap interface ID as an argument.
+ *
+ * @param _state - Redux state object.
+ * @param interfaceId - ID of a Snap interface.
+ * @returns ID of a Snap Interface that can be used as input selector.
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const selectInterfaceId = (_state: any, interfaceId: string) => interfaceId;
+
+/**
+ * Get a memoized version of the Snap interfaces.
+ */
+export const getMemoizedInterfaces = createDeepEqualSelector(
+  getInterfaces,
+  (interfaces) => interfaces,
 );
 
-export const selectInterface = createSelector(
-  [selectInterfaces, (_state: RootState, interfaceId: string) => interfaceId],
+/**
+ * Get a Snap Interface with a given ID.
+ */
+export const getInterface = createSelector(
+  [getMemoizedInterfaces, selectInterfaceId],
   (interfaces, id) => interfaces[id],
 );
 
-export const selectMemoizedInterface = createDeepEqualSelector(
-  selectInterface,
+/**
+ * Get a memoized version of a Snap interface with a given ID
+ */
+export const getMemoizedInterface = createDeepEqualSelector(
+  getInterface,
   (snapInterface) => snapInterface,
 );
