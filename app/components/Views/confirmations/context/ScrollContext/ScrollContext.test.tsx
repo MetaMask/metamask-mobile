491fcb0bd309b92b9eadf4a3f0583f1d1bf1c30d
jpuri <jyotipuri@gmail.com>
2025-02-17 17:21:23 +0530

    Adding scroll button to confirmation pages

---
+++ app/components/Views/confirmations/context/ScrollContext/ScrollContext.test.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import { ConfirmationFooterSelectorIDs } from '../../../../../../e2e/selectors/Confirmation/ConfirmationView.selectors';
+import renderWithProvider from '../../../../../util/test/renderWithProvider';
+import { personalSignatureConfirmationState } from '../../../../../util/test/confirm-data-helpers';
+import Footer from '../../components/Confirm/Footer';
+import Info from '../../components/Confirm/Info';
+import { ScrollContextProvider, useScrollContext } from './ScrollContext';
+
+jest.mock('@react-navigation/native', () => ({
+  ...jest.requireActual('@react-navigation/native'),
+  useNavigation: () => ({
+    goBack: jest.fn(),
+  }),
+}));
+
+jest.mock('../../../../../core/Engine', () => ({
+  getTotalFiatAccountBalance: () => ({ tokenFiat: 10 }),
+  context: {
+    KeyringController: {
+      state: {
+        keyrings: [],
+      },
+      getOrAddQRKeyring: jest.fn(),
+    },
+  },
+  controllerMessenger: {
+    subscribe: jest.fn(),
+  },
+}));
+
+describe('ScrollContext', () => {
+  it('should not disable confirm button if there is no scroll', () => {
+    const { getByTestId } = renderWithProvider(
+      <ScrollContextProvider
+        scrollableSection={<Info />}
+        staticFooter={<Footer />}
+      />,
+      {
+        state: personalSignatureConfirmationState,
+      },
+    );
+    expect(
+      getByTestId(ConfirmationFooterSelectorIDs.CONFIRM_BUTTON).props.disabled,
+    ).toBe(false);
+  });
+});
+
+describe('useScrollContext', () => {
+  it('should throw error is not wrapped in ScrollContext', () => {
+    expect(() => {
+      useScrollContext();
+    }).toThrow();
+  });
+});
