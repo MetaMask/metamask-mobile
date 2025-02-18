edaa96cea0b7b602360cdf3dbb929406d7a54cb8
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2025-02-18 10:59:09 +0000

    wip

--- app/components/Views/confirmations/components/Confirm/AdvancedDetails/AdvancedDetails.test.tsx
+++ app/components/Views/confirmations/components/Confirm/AdvancedDetails/AdvancedDetails.test.tsx
@@ -20,7 +20,7 @@ describe('AdvancedDetails', () => {
     expect(getByText('0xDc477...0c164')).toBeDefined();
 
     expect(getByText('Interacting with')).toBeDefined();
-    expect(getByText('0x4FEF9...d47Df')).toBeDefined();
+    expect(getByText('MetaMask Pooled Staking')).toBeDefined();
 
     expect(getByText('Network')).toBeDefined();
     expect(getByText('Ethereum Mainnet')).toBeDefined();
