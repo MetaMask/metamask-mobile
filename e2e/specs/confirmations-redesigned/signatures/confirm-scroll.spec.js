d9908b74fc8f00adc4d74e70d2d79fff7f6d81d0
jpuri <jyotipuri@gmail.com>
2025-02-18 19:44:37 +0530

    update

--- e2e/specs/confirmations-redesigned/signatures/confirm-scroll.spec.js
+++ e2e/specs/confirmations-redesigned/signatures/confirm-scroll.spec.js
@@ -14,11 +14,16 @@ import {
 import { SmokeConfirmationsRedesigned } from '../../../tags.js';
 import { mockEvents } from '../../../api-mocking/mock-config/mock-events.js';
 
-describe(SmokeConfirmationsRedesigned('Confirmations Page Scroll'), () => {
-  const testSpecificMock = {
-    GET: [mockEvents.GET.remoteFeatureFlagsReDesignedConfirmations],
-  };
+const permitSignRequestBody = {
+  method: 'eth_signTypedData_v4',
+  params: [
+    '0x76cf1cdd1fcc252442b50d6e97207228aa4aefc3',
+    '{"primaryType":"Permit","types":{"EIP712Domain":[{"name":"name","type":"string"},{"name":"version","type":"string"},{"name":"chainId","type":"uint256"},{"name":"verifyingContract","type":"address"}],"Permit":[{"name":"owner","type":"address"},{"name":"spender","type":"address"},{"name":"value","type":"uint256"},{"name":"nonce","type":"uint256"},{"name":"deadline","type":"uint256"}]},"domain":{"chainId":1,"name":"MyToken","verifyingContract":"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC","version":"1"},"message":{"owner":"0x8eeee1781fd885ff5ddef7789486676961873d12","spender":"0x5B38Da6a701c568545dCfcB03FcB875f56beddC4","value":3000,"nonce":0,"deadline":50000000000}}',
+  ],
+  origin: 'localhost',
+};
 
+describe(SmokeConfirmationsRedesigned('Confirmations Page Scroll'), () => {
   beforeAll(async () => {
     jest.setTimeout(2500000);
     await TestHelpers.reverseServerPort();
@@ -34,7 +39,9 @@ describe(SmokeConfirmationsRedesigned('Confirmations Page Scroll'), () => {
           .build(),
         restartDevice: true,
         ganacheOptions: defaultGanacheOptions,
-        testSpecificMock,
+        testSpecificMock: {
+          GET: [mockEvents.GET.remoteFeatureFlagsReDesignedConfirmations],
+        },
       },
       async () => {
         await loginToApp();
@@ -58,7 +65,22 @@ describe(SmokeConfirmationsRedesigned('Confirmations Page Scroll'), () => {
           .build(),
         restartDevice: true,
         ganacheOptions: defaultGanacheOptions,
-        testSpecificMock,
+        testSpecificMock: {
+          GET: [mockEvents.GET.remoteFeatureFlagsReDesignedConfirmations],
+          POST: [
+            {
+              ...mockEvents.POST.securityAlertApiValidate,
+              requestBody: permitSignRequestBody,
+              response: {
+                block: 20733277,
+                result_type: 'Malicious',
+                reason: 'malicious_domain',
+                description: `You're interacting with a malicious domain. If you approve this request, you might lose your assets.`,
+                features: [],
+              },
+            },
+          ],
+        },
       },
       async () => {
         await loginToApp();
