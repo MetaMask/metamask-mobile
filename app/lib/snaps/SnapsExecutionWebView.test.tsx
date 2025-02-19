f547e828cc5d0847f9809379f83897abdf12f17c
Frederik Bolding <frederik.bolding@gmail.com>
2025-02-19 15:07:43 +0100

    Add basic component test

---
+++ app/lib/snaps/SnapsExecutionWebView.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { createWebView, removeWebView, SnapsExecutionWebView } from './SnapsExecutionWebView';
+
+describe('SnapsExecutionWebView', () => {
+  it('should render correctly', () => {
+    const wrapper = shallow(<SnapsExecutionWebView />);
+    expect(wrapper).toBeDefined();
+  });
+
+  it('should create and remove WebViews correctly', async () => {
+    const wrapper = shallow(<SnapsExecutionWebView />);
+    createWebView('foo')
+    createWebView('bar')
+    wrapper.update();
+    expect(wrapper.find('WebView')).toHaveLength(2);
+
+    removeWebView('foo');
+    wrapper.update();
+    expect(wrapper.find('WebView')).toHaveLength(1);
+  });
+});
