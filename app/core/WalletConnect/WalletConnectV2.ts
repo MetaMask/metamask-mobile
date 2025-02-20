bde66380644883716ce67d5006040189321c6ead
Tamas <soostamas.hu@gmail.com>
2025-02-20 12:04:03 +0100

    fix ui

--- app/core/WalletConnect/WalletConnectV2.ts
+++ app/core/WalletConnect/WalletConnectV2.ts
@@ -25,8 +25,7 @@ import {
   getApprovedSessionMethods,
   getScopedPermissions,
   hideWCLoadingState,
-  parseWalletConnectUri,
-  showWCLoadingState
+  parseWalletConnectUri
 } from './wc-utils';
 
 import WalletConnect2Session from './WalletConnect2Session';
@@ -561,7 +560,7 @@ export class WC2Manager {
       if (rawParams.sessionTopic) {
         const { sessionTopic } = rawParams;
         this.sessions[sessionTopic]?.setDeeplink(true);
-        showWCLoadingState({ navigation: this.navigation });
+        // showWCLoadingState({ navigation: this.navigation });
         return;
       }
 
@@ -583,7 +582,7 @@ export class WC2Manager {
         const cleanUri = wcUri.startsWith('wc://')
           ? wcUri.replace('wc://', 'wc:')
           : wcUri;
-        showWCLoadingState({ navigation: this.navigation });
+        // showWCLoadingState({ navigation: this.navigation });
 
         const paired = await this.web3Wallet.core.pairing.pair({
           uri: cleanUri,
