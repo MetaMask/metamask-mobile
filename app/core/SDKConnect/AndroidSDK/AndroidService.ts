e62ff45343ff3fd813354587e855a8cbfd50a031
Cal-L <cleun007@gmail.com>
2025-02-20 13:09:11 -0800

    Fix accessing navigation

--- app/core/SDKConnect/AndroidSDK/AndroidService.ts
+++ app/core/SDKConnect/AndroidSDK/AndroidService.ts
@@ -36,6 +36,7 @@ import getDefaultBridgeParams from './getDefaultBridgeParams';
 import { AccountsController } from '@metamask/accounts-controller';
 import { toChecksumHexAddress } from '@metamask/controller-utils';
 import Routes from '../../../constants/navigation/Routes';
+import NavigationService from '../../NavigationService';
 
 export default class AndroidService extends EventEmitter2 {
   public communicationClient = NativeModules.CommunicationClient;
@@ -233,7 +234,7 @@ export default class AndroidService extends EventEmitter2 {
               `AndroidService::clients_connected error failed sending jsonrpc error to client`,
             );
           });
-          SDKConnect.getInstance().state.navigation?.navigate(Routes.MODAL.ROOT_MODAL_FLOW, {
+          NavigationService.navigation?.navigate(Routes.MODAL.ROOT_MODAL_FLOW, {
             screen: Routes.SHEET.RETURN_TO_DAPP_MODAL,
           });
           return;
