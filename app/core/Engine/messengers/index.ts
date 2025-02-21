636ba424e788e4069f72b3ea8154564d8c2c255a
IF <139582705+infiniteflower@users.noreply.github.com>
2025-02-21 14:02:33 -0500

    chore: create messenger callback fn for bridge-controller messenger

--- app/core/Engine/messengers/index.ts
+++ app/core/Engine/messengers/index.ts
@@ -1,4 +1,5 @@
 import { getAccountsControllerMessenger } from './accounts-controller-messenger';
+import { getBridgeControllerMessenger } from './bridge-controller-messenger';
 import type { ControllerMessengerByControllerName } from '../types';
 
 /**
@@ -7,5 +8,8 @@ import type { ControllerMessengerByControllerName } from '../types';
 export const CONTROLLER_MESSENGERS: ControllerMessengerByControllerName = {
   AccountsController: {
     getMessenger: getAccountsControllerMessenger,
+  },
+  BridgeController: {
+    getMessenger: getBridgeControllerMessenger,
   },
 } as const;
