3c21df59bd1b9af36ba67b27c67eb3b5927a71a1
IF <139582705+infiniteflower@users.noreply.github.com>
2025-02-21 14:03:18 -0500

    chore: make bridgeController init fn and consume

---
+++ app/core/Engine/controllers/bridge-controller/index.ts
@@ -0,0 +1,44 @@
+import {
+  BridgeClientId,
+  BridgeController,
+  type BridgeControllerMessenger,
+} from '@metamask/bridge-controller';
+import { TransactionParams } from '@metamask/transaction-controller';
+import { ChainId } from '@metamask/controller-utils';
+import { ControllerInitFunction } from '../../types';
+
+/**
+ * Initialize the BridgeController.
+ *
+ * @param request - The request object.
+ * @returns The BridgeController.
+ */
+export const bridgeControllerInit: ControllerInitFunction<
+  BridgeController,
+  BridgeControllerMessenger
+> = (request) => {
+  const { controllerMessenger, getController } = request;
+
+  const transactionController = getController('TransactionController');
+
+  const controller = new BridgeController({
+    messenger: controllerMessenger,
+    clientId: BridgeClientId.MOBILE,
+    // TODO: change getLayer1GasFee type to match transactionController.getLayer1GasFee
+    getLayer1GasFee: async ({
+      transactionParams,
+      chainId,
+    }: {
+      transactionParams: TransactionParams;
+      chainId: ChainId;
+    }) =>
+      await transactionController.getLayer1GasFee({
+        transactionParams,
+        chainId,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      }) as any,
+    fetchFn: fetch,
+  });
+
+  return { controller };
+};
