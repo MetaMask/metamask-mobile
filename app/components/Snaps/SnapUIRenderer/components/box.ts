4776521050edfc6beb0bedc7b32bdb3d0d7d04bd
Daniel Cross <dan.s.cross@icloud.com>
2025-02-21 17:22:08 +0100

    feat: snaps dynamic UI (#12429)
    ## **Description**
    
    This PR integrates the Snaps Dynamic UI controller and components to
    allow different dialog prompts to show on the screen.
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/accounts-planning/issues/738
    
    ## **Manual testing steps**
    
    1. Make a Flask build
    2. Go to: https://metamask.github.io/snaps/test-snaps/latest/ in the in
    app browser
    3. Connect the Snaps Dialog snap
    4. Try to launch the different snaps prompts.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Owen Craston <owengc12@gmail.com>
    Co-authored-by: Frederik Bolding <frederik.bolding@gmail.com>

---
+++ app/components/Snaps/SnapUIRenderer/components/box.ts
@@ -0,0 +1,50 @@
+import { BoxElement, JSXElement, BoxProps } from '@metamask/snaps-sdk/jsx';
+import { getJsxChildren } from '@metamask/snaps-utils';
+import { NonEmptyArray } from '@metamask/utils';
+import { mapToTemplate } from '../utils';
+import { UIComponent, UIComponentFactory } from './types';
+import { TextColor } from '../../../../component-library/components/Texts/Text/Text.types';
+import {
+  AlignItems,
+  FlexDirection,
+  JustifyContent,
+} from '../../../UI/Box/box.types';
+
+function generateJustifyContent(alignment?: BoxProps['alignment']) {
+  switch (alignment) {
+    default:
+    case 'start':
+      return JustifyContent.flexStart;
+
+    case 'center':
+      return JustifyContent.center;
+
+    case 'end':
+      return JustifyContent.flexEnd;
+
+    case 'space-between':
+      return JustifyContent.spaceBetween;
+
+    case 'space-around':
+      return JustifyContent.spaceAround;
+  }
+}
+
+export const box: UIComponentFactory<BoxElement> = ({
+  element: e,
+  ...params
+}) => ({
+  element: 'Box',
+  children: getJsxChildren(e).map((children) =>
+    mapToTemplate({ ...params, element: children as JSXElement }),
+  ) as NonEmptyArray<UIComponent>,
+  props: {
+    flexDirection:
+      e.props.direction === 'horizontal'
+        ? FlexDirection.Row
+        : FlexDirection.Column,
+    justifyContent: generateJustifyContent(e.props.alignment),
+    alignItems: e.props.center && AlignItems.center,
+    color: TextColor.Default,
+  },
+});
