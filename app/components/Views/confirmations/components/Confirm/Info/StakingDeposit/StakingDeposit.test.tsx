fa75d9db43e93545070cb9e7fb3d8c261fb972c1
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-02-24 08:42:52 +0100

    Fix unit test

--- app/components/Views/confirmations/components/Confirm/Info/StakingDeposit/StakingDeposit.test.tsx
+++ app/components/Views/confirmations/components/Confirm/Info/StakingDeposit/StakingDeposit.test.tsx
@@ -4,6 +4,19 @@ import renderWithProvider from '../../../../../../../util/test/renderWithProvide
 import { stakingDepositConfirmationState } from '../../../../../../../util/test/confirm-data-helpers';
 import StakingDeposit from './StakingDeposit';
 
+jest.mock('../../../../../../../core/Engine', () => ({
+  getTotalFiatAccountBalance: () => ({ tokenFiat: 10 }),
+  context: {
+    NetworkController: {
+      getNetworkConfigurationByNetworkClientId: jest.fn(),
+    },
+    GasFeeController: {
+      startPolling: jest.fn(),
+      stopPollingByPollingToken: jest.fn(),
+    },
+  },
+}));
+
 jest.mock('@react-navigation/native', () => {
   const actualNav = jest.requireActual('@react-navigation/native');
   return {
