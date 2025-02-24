6d2f3e0cc875cf1138b236062cb8efe75939e61e
jpuri <jyotipuri@gmail.com>
2025-02-24 10:41:58 +0530

    Fix origin url displayed for signatures

--- app/components/Views/confirmations/components/UI/InfoRow/InfoValue/DisplayURL/DisplayURL.tsx
+++ app/components/Views/confirmations/components/UI/InfoRow/InfoValue/DisplayURL/DisplayURL.tsx
@@ -7,7 +7,7 @@ import Icon, {
   IconSize,
 } from '../../../../../../../../component-library/components/Icons/Icon';
 import Text from '../../../../../../../../component-library/components/Texts/Text';
-// import Logger from '../../../../../../../../util/Logger';
+import Logger from '../../../../../../../../util/Logger';
 import { useStyles } from '../../../../../../../../component-library/hooks';
 import styleSheet from './DisplayURL.styles';
 
@@ -23,9 +23,7 @@ const DisplayURL = ({ url }: DisplayURLProps) => {
     try {
       urlObject = new URL(url);
     } catch (e) {
-      // Commenting out the line below till issue of missing protocol in origin is addressed
-      // https://github.com/MetaMask/metamask-mobile/issues/13580#issuecomment-2671458216
-      // Logger.error(e as Error, `DisplayURL: new URL(url) cannot parse ${url}`);
+      Logger.error(e as Error, `DisplayURL: new URL(url) cannot parse ${url}`);
     }
     setIsHTTP(urlObject?.protocol === 'http:');
   }, [url]);
