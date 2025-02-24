219e91da9028f0a8085d7d8d0d82fbacd4fa0ab6
vinnyhoward <vincenguyenhoward@gmail.com>
2025-02-24 15:14:48 -0700

    fix: removed i18n import from carousel tests due to esm errors - fixed dismiss test

--- e2e/specs/wallet/carousel.spec.js
+++ e2e/specs/wallet/carousel.spec.js
@@ -13,7 +13,6 @@ import FixtureServer from '../../fixtures/fixture-server';
 import Assertions from '../../utils/Assertions';
 import WalletView from '../../pages/wallet/WalletView';
 import Gestures from '../../utils/Gestures';
-import { strings } from '../../../locales/i18n';
 
 const fixtureServer = new FixtureServer();
 
@@ -28,75 +27,69 @@ describe(SmokeCore('Carousel Tests'), () => {
     });
     await loginToApp();
   });
-
   beforeEach(async () => {
     jest.setTimeout(150000);
     // Wait for the Carousel to be rendered
     const carouselContainer = await WalletView.carouselContainer;
-    await Assertions.checkIfVisible(carouselContainer, 30000);
+    await Assertions.checkIfVisible(carouselContainer, 5000);
   });
-
   afterAll(async () => {
     await stopFixtureServer(fixtureServer);
   });
-
   it('should display carousel with correct slides', async () => {
     const carouselContainer = await WalletView.carouselContainer;
     const carouselFirstSlide = await WalletView.carouselFirstSlide;
     const carouselFirstSlideTitle = await WalletView.carouselFirstSlideTitle;
     const carouselProgressDots = await WalletView.carouselProgressDots;
-
-    await Assertions.checkIfVisible(carouselContainer, 30000);
-    await Assertions.checkIfVisible(carouselFirstSlide, 30000);
+    await Assertions.checkIfVisible(carouselContainer, 5000);
+    await Assertions.checkIfVisible(carouselFirstSlide, 5000);
     await Assertions.checkIfElementToHaveText(
       carouselFirstSlideTitle,
-      strings('banner.card.title'),
-      30000,
+      'MetaMask Card',
+      5000,
     );
-    await Assertions.checkIfVisible(carouselProgressDots, 30000);
+    await Assertions.checkIfVisible(carouselProgressDots, 5000);
   });
-
   it('should navigate between slides', async () => {
     const carouselContainer = await WalletView.carouselContainer;
     const carouselSecondSlide = await WalletView.carouselSecondSlide;
     const carouselSecondSlideTitle = await WalletView.carouselSecondSlideTitle;
     const carouselFirstSlideTitle = await WalletView.carouselFirstSlideTitle;
-
     await Gestures.swipe(carouselContainer, 'left', 'slow', 0.7);
-    await Assertions.checkIfVisible(carouselSecondSlide, 30000);
+    await Assertions.checkIfVisible(carouselSecondSlide, 5000);
     await Assertions.checkIfElementToHaveText(
       carouselSecondSlideTitle,
-      strings('banner.fund.title'),
-      30000,
+      'Fund your wallet',
+      5000,
     );
-
     await Gestures.swipe(carouselContainer, 'right', 'slow', 0.7);
     await Assertions.checkIfElementToHaveText(
       carouselFirstSlideTitle,
-      strings('banner.card.title'),
-      30000,
+      'MetaMask Card',
+      5000,
     );
   });
-
-  it('should dismiss a slide', async () => {
-    const carouselFirstSlideTitle = await WalletView.carouselFirstSlideTitle;
-    const closeButton = await WalletView.carouselCloseButton;
-
-    await Assertions.checkIfVisible(closeButton, 30000);
-    await Gestures.tap(closeButton);
-    await Assertions.checkIfElementToHaveText(
-      carouselFirstSlideTitle,
-      strings('banner.fund.title'),
-      30000,
-    );
-  });
-
   it('should handle slide interactions', async () => {
     const carouselFirstSlide = await WalletView.carouselFirstSlide;
     const container = await WalletView.container;
-
-    await Assertions.checkIfVisible(carouselFirstSlide, 30000);
+    await Assertions.checkIfVisible(carouselFirstSlide, 5000);
     await Gestures.tap(carouselFirstSlide);
-    await Assertions.checkIfVisible(container, 30000);
+    await Assertions.checkIfVisible(container, 5000);
+  });
+  it('should dismiss a slide', async () => {
+    await TestHelpers.relaunchApp({
+      launchArgs: { fixtureServerPort: `${getFixturesServerPort()}` },
+    });
+    await loginToApp();
+
+    const carouselFirstSlideTitle = await WalletView.carouselFirstSlideTitle;
+    const closeButton = await WalletView.carouselCloseButton;
+    await Assertions.checkIfVisible(closeButton, 5000);
+    await Gestures.tap(closeButton);
+    await Assertions.checkIfElementToHaveText(
+      carouselFirstSlideTitle,
+      'Fund your wallet',
+      5000,
+    );
   });
 });
