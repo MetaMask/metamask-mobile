b552443a10a9299696d2de9dc7cf2b15152a0e68
Owen Craston <owengc12@gmail.com>
2025-02-25 11:42:12 -0700

    add keyringsMetadata

--- app/core/Engine/Engine.ts
+++ app/core/Engine/Engine.ts
@@ -685,7 +685,6 @@ export class Engine {
         preferencesController,
       ),
       encryptor,
-      // @ts-expect-error TODO: Resolve mismatch between base-controller versions.
       messenger: this.controllerMessenger.getRestricted({
         name: 'KeyringController',
         allowedActions: [],
