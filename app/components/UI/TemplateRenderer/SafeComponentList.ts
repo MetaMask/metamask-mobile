e677905fe40158ee1ced6e6dac28285173f47111
Frederik Bolding <frederik.bolding@gmail.com>
2025-02-25 13:20:26 +0100

    Fix duped import

--- app/components/UI/TemplateRenderer/SafeComponentList.ts
+++ app/components/UI/TemplateRenderer/SafeComponentList.ts
@@ -4,7 +4,7 @@ import Text from '../../../component-library/components/Texts/Text';
 import Icon from '../../../component-library/components/Icons/Icon';
 import BottomSheetFooter from '../../../component-library/components/BottomSheets/BottomSheetFooter';
 import SmartTransactionStatus from '../../Views/SmartTransactionStatus/SmartTransactionStatus';
-import { View } from 'react-native';
+import { View, Text as RNText } from 'react-native';
 import Checkbox from '../../../component-library/components/Checkbox/Checkbox';
 import { SnapUIImage } from '../../UI/Snaps/SnapUIImage/SnapUIImage';
 import { SnapAvatar } from '../Snaps/SnapAvatar/SnapAvatar';
@@ -22,7 +22,6 @@ import { SnapUICheckbox } from '../../Snaps/SnapUICheckbox/SnapUICheckbox';
 import { SnapUIAddress } from '../../UI/Snaps/SnapUIAddress/SnapUIAddress';
 import { SnapUIAvatar } from '../../UI/Snaps/SnapUIAvatar/SnapUIAvatar';
 import InfoRow from '../../Views/confirmations/components/UI/InfoRow';
-import { Text as RNText } from 'react-native';
 
 export const safeComponentList = {
   BottomSheetFooter,
