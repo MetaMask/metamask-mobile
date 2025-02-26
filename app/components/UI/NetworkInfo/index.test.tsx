189ad2dc1ee2dc57798f21da1142cb775aba271d
tommasini <tommasini15@gmail.com>
2025-02-26 15:03:48 +0000

    test coverage for Network info modal

--- app/components/UI/NetworkInfo/index.test.tsx
+++ app/components/UI/NetworkInfo/index.test.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import NetworkInfo from './';
-import configureMockStore from 'redux-mock-store';
-import { Provider } from 'react-redux';
-import { backgroundState } from '../../../util/test/initial-root-state';
 
-const mockStore = configureMockStore();
+import { backgroundState } from '../../../util/test/initial-root-state';
+import renderWithProvider from '../../../util/test/renderWithProvider';
+
+jest.mock('@react-navigation/native', () => {
+  const actualNav = jest.requireActual('@react-navigation/native');
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      navigate: jest.fn(),
+    }),
+  };
+});
+
 const initialState = {
   privacy: {
     approvedHosts: {},
@@ -15,20 +23,44 @@ const initialState = {
   },
 };
 
-const store = mockStore(initialState);
-
 describe('NetworkInfo', () => {
-  it('should render correctly', () => {
-    const wrapper = shallow(
-      <Provider store={store}>
-        <NetworkInfo
-          type={''}
-          onClose={function (): void {
-            throw new Error('Function not implemented.');
-          }}
-          ticker={''}
-        />
-      </Provider>,
+  it('render correctly', () => {
+    const wrapper = renderWithProvider(
+      <NetworkInfo
+        type={''}
+        onClose={function (): void {
+          throw new Error('Function not implemented.');
+        }}
+        ticker={''}
+      />,
+      { state: initialState },
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('render correctly with non-EVM selected', () => {
+    const nonEvmState = {
+      ...initialState,
+      engine: {
+        backgroundState: {
+          ...backgroundState,
+          MultichainNetworkController: {
+            ...backgroundState.MultichainNetworkController,
+            isEvmSelected: false,
+          },
+        },
+      },
+    };
+
+    const wrapper = renderWithProvider(
+      <NetworkInfo
+        type={''}
+        onClose={function (): void {
+          throw new Error('Function not implemented.');
+        }}
+        ticker={''}
+      />,
+      { state: nonEvmState },
     );
     expect(wrapper).toMatchSnapshot();
   });
