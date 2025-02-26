4aebd5736601ce15dd384ae09a67df795b7d2d3f
IF <139582705+infiniteflower@users.noreply.github.com>
2025-02-26 14:39:37 -0500

    chore: remove unneeded tests

--- app/components/UI/Swaps/QuotesView.test.ts
+++ app/components/UI/Swaps/QuotesView.test.ts
@@ -410,40 +410,4 @@ describe('QuotesView', () => {
       expect(mockSubmitSwapsSmartTransaction).not.toHaveBeenCalled();
     });
   });
-
-  describe('polling and error handling', () => {
-    it('should show error when quotes are expired', async () => {
-      const state = merge({}, mockInitialState, {
-        engine: {
-          backgroundState: {
-            SwapsController: {
-              isInPolling: false,
-              error: {key: swapsUtils.SwapsError.QUOTES_EXPIRED_ERROR, description: null},
-            },
-          },
-        },
-      });
-      const wrapper = render(QuotesView, state);
-
-      const error = await wrapper.findByTestId('error-area');
-      expect(error).toBeDefined();
-    });
-
-    it('should not show error when quotes are not expired', async () => {
-      const state = merge({}, mockInitialState, {
-        engine: {
-          backgroundState: {
-            SwapsController: {
-              isInPolling: true,
-              error: null,
-            },
-          },
-        },
-      });
-
-      const wrapper = render(QuotesView, state);
-      const topBar = await wrapper.findByTestId('top-bar');
-      expect(topBar).toBeDefined();
-    });
-  });
 });
