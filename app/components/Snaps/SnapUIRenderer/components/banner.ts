e1786dc241b1ce12b10bc478cf03e2b64b3f6699
david0xd <david@timechaser.org>
2025-02-26 16:00:22 +0100

    Add Snap UI Banner component

---
+++ app/components/Snaps/SnapUIRenderer/components/banner.ts
@@ -0,0 +1,18 @@
+import { BannerElement, JSXElement } from '@metamask/snaps-sdk/jsx';
+import { getJsxChildren } from '@metamask/snaps-utils';
+import { mapToTemplate } from '../utils';
+import { UIComponentFactory } from './types';
+
+export const banner: UIComponentFactory<BannerElement> = ({
+  element: e,
+  ...params
+}) => ({
+    element: 'SnapUIBanner',
+    children: getJsxChildren(e).map((children) =>
+      mapToTemplate({ element: children as JSXElement, ...params }),
+    ),
+    props: {
+      title: e.props.title,
+      severity: e.props.severity,
+    },
+  });
