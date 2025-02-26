fb647eb6eaafd769e279e9952be5168044daf491
Owen Craston <owengc12@gmail.com>
2025-02-26 12:55:21 -0700

    update packages for fix

--- app/components/hooks/useMultichainBalances/useMultichainBalances.ts
+++ app/components/hooks/useMultichainBalances/useMultichainBalances.ts
@@ -70,6 +70,10 @@ const useMultichainBalances = (): UseMultichainBalancesHook => {
   const multichainSelectedAccountCachedBalance = useSelector(
     selectMultichainSelectedAccountCachedBalance,
   );
+  console.log(
+    'multichainSelectedAccountCachedBalance',
+    multichainSelectedAccountCachedBalance,
+  );
   const { symbol } = useSelector(selectMultichainDefaultToken);
   const shouldShowFiat = useSelector(selectMultichainShouldShowFiat);
   const multichainConversionRate = useSelector(selectMultichainConversionRate);
