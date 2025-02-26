cb67ab4df5185dea2b39bb920ea25e0ab976e12e
Frederik Bolding <frederik.bolding@gmail.com>
2025-02-26 10:25:20 +0100

    Add SnapUIAddress tests

---
+++ app/components/UI/Snaps/SnapUIAddress/SnapUIAddress.test.tsx
@@ -0,0 +1,138 @@
+import React from 'react';
+import { SnapUIAddress } from './SnapUIAddress';
+import renderWithProvider from '../../../../util/test/renderWithProvider';
+
+const mockStateWithoutBlockies = {
+    state: {
+      settings: {
+        useBlockieIcon: false,
+      },
+    },
+  };
+
+const mockStateWithBlockies = {
+  state: {
+    settings: {
+      useBlockieIcon: true,
+    },
+  },
+};
+
+describe('SnapUIAddress', () => {
+  it('renders legacy Ethereum address', () => {
+    const { toJSON } = renderWithProvider(
+      <SnapUIAddress address="0xab16a96d359ec26a11e2c2b3d8f8b8942d5bfcdb" />,
+      mockStateWithoutBlockies
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Ethereum address', () => {
+    const { toJSON } = renderWithProvider(
+      <SnapUIAddress address="eip155:1:0xab16a96D359eC26a11e2C2b3d8f8B8942d5Bfcdb" />,
+      mockStateWithoutBlockies
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Ethereum address with blockie', () => {
+    const { toJSON } = renderWithProvider(
+      <SnapUIAddress address="eip155:1:0xab16a96D359eC26a11e2C2b3d8f8B8942d5Bfcdb" />,
+      mockStateWithBlockies,
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Bitcoin address', () => {
+    const { toJSON } = renderWithProvider(
+      <SnapUIAddress address="bip122:000000000019d6689c085ae165831e93:128Lkh3S7CkDTBZ8W7BbpsN3YYizJMp8p6" />,
+      mockStateWithoutBlockies
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Bitcoin address with blockie', () => {
+    const { toJSON } = renderWithProvider(
+      <SnapUIAddress address="bip122:000000000019d6689c085ae165831e93:128Lkh3S7CkDTBZ8W7BbpsN3YYizJMp8p6" />,
+      mockStateWithBlockies,
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Cosmos address', () => {
+    const { toJSON } = renderWithProvider(
+      <SnapUIAddress address="cosmos:cosmoshub-3:cosmos1t2uflqwqe0fsj0shcfkrvpukewcw40yjj6hdc0" />,
+      mockStateWithoutBlockies
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Cosmos address with blockie', () => {
+    const { toJSON } = renderWithProvider(
+      <SnapUIAddress address="cosmos:cosmoshub-3:cosmos1t2uflqwqe0fsj0shcfkrvpukewcw40yjj6hdc0" />,
+      mockStateWithBlockies,
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Polkadot address', () => {
+    const { toJSON } = renderWithProvider(
+      <SnapUIAddress address="polkadot:b0a8d493285c2df73290dfb7e61f870f:5hmuyxw9xdgbpptgypokw4thfyoe3ryenebr381z9iaegmfy" />,
+      mockStateWithoutBlockies
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Polkadot address with blockie', () => {
+    const { toJSON } = renderWithProvider(
+      <SnapUIAddress address="polkadot:b0a8d493285c2df73290dfb7e61f870f:5hmuyxw9xdgbpptgypokw4thfyoe3ryenebr381z9iaegmfy" />,
+      mockStateWithBlockies,
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Starknet address', () => {
+    const { toJSON } = renderWithProvider(
+      <SnapUIAddress address="starknet:SN_GOERLI:0x02dd1b492765c064eac4039e3841aa5f382773b598097a40073bd8b48170ab57" />,
+      mockStateWithoutBlockies
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Starknet address with blockie', () => {
+    const { toJSON } = renderWithProvider(
+      <SnapUIAddress address="starknet:SN_GOERLI:0x02dd1b492765c064eac4039e3841aa5f382773b598097a40073bd8b48170ab57" />,
+      mockStateWithBlockies,
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Hedera address', () => {
+    const { toJSON } = renderWithProvider(
+      <SnapUIAddress address="hedera:mainnet:0.0.1234567890-zbhlt" />,
+      mockStateWithoutBlockies
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Hedera address with blockie', () => {
+    const { toJSON } = renderWithProvider(
+      <SnapUIAddress address="hedera:mainnet:0.0.1234567890-zbhlt" />,
+      mockStateWithBlockies,
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+});
