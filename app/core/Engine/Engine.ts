664e349a6e26ede5345f0da76a461427dbd66971
Owen Craston <owen.craston@consensys.net>
2025-02-26 12:03:05 -0700

    feat: make Snap account creation flow async + displayAccountNameSuggestion flag (#13703)
    ## **Description**
    
    This change the way Snap accounts are being created.
    
    The Snap keyring will now resume the Snap execution right after the user
    accepted and confirmed the account name suggestion.
    
    The main reason of making that flow async was to allow the new
    Multichain*Controller to interact with the Snap (and its account) right
    after the account creation. This was not possible prior to this change,
    cause the Snap was persisting its account only when the extension had
    persisted its own account.
    
    This PR also enables the `displayAccountNameSuggestion` flag such that
    if a pre installed snap wants to skip the account naming dialog they
    can. Currently all of our pre installed snaps do not use this flag so we
    are still going through the account name suggestion flow.
    
    ## **Related issues**
    
    Related to this PR on the extension:
    https://github.com/MetaMask/metamask-extension/pull/30406
    
    Fixes: https://github.com/MetaMask/accounts-planning/issues/845
    
    ## **Manual testing steps**
    
    1. checkout branch
    2. ensure that you are building flask by setting the value of
    `METAMASK_BUILD_TYPE` to `flask` inside the `.js.env` file.
    3. run yarn setup
    4. then run yarn start:ios
    5. import/create a wallet
    6. once your wallet is created, click on the selected account which
    should open the account list
    7. click on "Add account of hardware wallet"
    8. click on the "Add Solana account" button
    9. a popup with the suggested name should show up. Confirm this modal
    and then the account should be added.
    10. your selected account should now be the Solana account
    11. repeat the above steps with the Bitcoin account. Again, the Bitcoin
    accounts should be created and selected upon creation.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    
    https://github.com/user-attachments/assets/4b08a03e-fd22-4d2d-84c1-3e0b15bf9040
    
    
    ### **After**
    
    
    https://github.com/user-attachments/assets/c79a8d69-6ce3-4253-83c2-3e3277a1b13b
    
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/core/Engine/Engine.ts
+++ app/core/Engine/Engine.ts
@@ -672,11 +672,10 @@ export class Engine {
     };
 
     additionalKeyrings.push(
-      snapKeyringBuilder(
-        snapKeyringBuildMessenger,
-        persistAndUpdateAccounts,
-        (address) => this.removeAccount(address),
-      ),
+      snapKeyringBuilder(snapKeyringBuildMessenger, {
+        persistKeyringHelper: () => persistAndUpdateAccounts(),
+        removeAccountHelper: (address) => this.removeAccount(address),
+      }),
     );
 
     ///: END:ONLY_INCLUDE_IF
@@ -686,7 +685,6 @@ export class Engine {
         preferencesController,
       ),
       encryptor,
-      // @ts-expect-error TODO: Resolve mismatch between base-controller versions.
       messenger: this.controllerMessenger.getRestricted({
         name: 'KeyringController',
         allowedActions: [],
@@ -1958,7 +1956,6 @@ export class Engine {
     await removeAccountsFromPermissions([address]);
     // Remove account from the keyring
     await this.keyringController.removeAccount(address as Hex);
-    return address;
   };
   ///: END:ONLY_INCLUDE_IF
 
