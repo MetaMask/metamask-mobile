293cc1c4e00662d96da8ce16ea4bde1d892db4e1
Amitabh Aggarwal <aggarwal.amitabh@gmail.com>
2025-02-27 16:46:15 -0600

    refactor: remove bridge controller messengers from modular messengers

--- app/core/Engine/messengers/index.ts
+++ app/core/Engine/messengers/index.ts
@@ -1,6 +1,4 @@
 import { getAccountsControllerMessenger } from './accounts-controller-messenger';
-import { getBridgeControllerMessenger } from './bridge-controller-messenger';
-import { getBridgeStatusControllerMessenger } from './bridge-status-controller-messenger';
 import type { ControllerMessengerByControllerName } from '../types';
 
 /**
@@ -9,11 +7,5 @@ import type { ControllerMessengerByControllerName } from '../types';
 export const CONTROLLER_MESSENGERS: ControllerMessengerByControllerName = {
   AccountsController: {
     getMessenger: getAccountsControllerMessenger,
-  },
-  BridgeController: {
-    getMessenger: getBridgeControllerMessenger,
-  },
-  BridgeStatusController: {
-    getMessenger: getBridgeStatusControllerMessenger,
   },
 } as const;
