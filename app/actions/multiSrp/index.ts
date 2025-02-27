e72dfd3eef51c70e2efbd97fdf90c2073e11674f
Monte Lai <monte.lai@consensys.net>
2025-02-27 17:07:01 +0800

    refactor: already imported srp logic and remove addNewHdAccount

--- app/actions/multiSrp/index.ts
+++ app/actions/multiSrp/index.ts
@@ -5,7 +5,6 @@ import { wordlist } from '@metamask/scure-bip39/dist/wordlists/english';
 import ExtendedKeyringTypes from '../../constants/keyringTypes';
 import Engine from '../../core/Engine';
 import Logger from '../../util/Logger';
-import { KeyringSelector } from '@metamask/keyring-controller';
 
 export async function importNewSecretRecoveryPhrase(mnemonic: string) {
   const { KeyringController } = Engine.context;
@@ -14,16 +13,16 @@ export async function importNewSecretRecoveryPhrase(mnemonic: string) {
     const hdKeyrings = (await KeyringController.getKeyringsByType(
       ExtendedKeyringTypes.hd,
     )) as HdKeyring[];
-    const alreadyImportedSRP = hdKeyrings.some(
-      (keyring) =>
-        Buffer.from(
-          Buffer.from(
-            Array.from(new Uint16Array(Buffer.from(keyring.mnemonic).buffer))
-              .map((i) => wordlist[i])
-              .join(' '),
-          ),
-        ).toString('utf8') === mnemonic,
-    );
+    const alreadyImportedSRP = hdKeyrings.some((keyring) => {
+      const codePointsToEnglishWords = Buffer.from(
+        Array.from(new Uint16Array(Buffer.from(keyring.mnemonic).buffer))
+          .map((i) => wordlist[i])
+          .join(' '),
+      );
+      return (
+        Buffer.from(codePointsToEnglishWords).toString('utf8') === mnemonic
+      );
+    });
 
     if (alreadyImportedSRP) {
       throw new Error('This mnemonic has already been imported.');
@@ -36,9 +35,10 @@ export async function importNewSecretRecoveryPhrase(mnemonic: string) {
         numberOfAccounts: 1,
       },
     )) as EthKeyring<Json>;
-    const newAccountAddress = (await newKeyring.getAccounts())[0];
+    const [newAccountAddress] = await newKeyring.getAccounts();
     return Engine.setSelectedAddress(newAccountAddress);
   } catch (e: unknown) {
+    Logger.log(e);
     Logger.error(e as Error, 'error while trying to add a new account');
   }
 }
@@ -54,35 +54,5 @@ export async function createNewSecretRecoveryPhrase() {
     return Engine.setSelectedAddress(newAccountAddress);
   } catch (e: unknown) {
     Logger.error(e as Error, 'error while trying to add a new srp');
-  }
-}
-
-export async function addNewHdAccount(
-  keyringId?: string,
-  name?: string,
-): Promise<void> {
-  const { KeyringController } = Engine.context;
-  try {
-    const keyringSelector: KeyringSelector = keyringId
-      ? {
-          id: keyringId,
-        }
-      : {
-          type: ExtendedKeyringTypes.hd,
-        };
-
-    Logger.log('keyringSelector', keyringSelector);
-
-    const addedAccountAddress = await KeyringController.withKeyring(
-      keyringSelector,
-      async (keyring) => (await keyring.addAccounts(1))[0],
-    );
-    Engine.setSelectedAddress(addedAccountAddress);
-
-    if (name) {
-      Engine.setAccountLabel(addedAccountAddress, name);
-    }
-  } catch (e: unknown) {
-    Logger.error(e as Error, 'error while trying to add a new account');
   }
 }
