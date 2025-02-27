63fb9e8356f862b51a7010c6b00e54c627f558ce
Frederik Bolding <frederik.bolding@gmail.com>
2025-02-27 14:52:11 +0100

    Remove unnecesary change

--- app/components/Snaps/SnapUICard/SnapUICard.tsx
+++ app/components/Snaps/SnapUICard/SnapUICard.tsx
@@ -32,7 +32,6 @@ export const SnapUICard: FunctionComponent<SnapUICardProps> = ({
     flexDirection={FlexDirection.Row}
     justifyContent={JustifyContent.spaceBetween}
     alignItems={AlignItems.center}
-    style={{ flex: 1 }}
   >
     <Box
       gap={16}
