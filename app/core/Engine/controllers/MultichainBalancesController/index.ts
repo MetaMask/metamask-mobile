43a6967220c8257503be31e9c8c52d4331711b88
Owen Craston <owengc12@gmail.com>
2025-02-28 19:50:44 -0700

    Move MultichainBalancesController to the new init pattern

---
+++ app/core/Engine/controllers/MultichainBalancesController/index.ts
@@ -0,0 +1,34 @@
+import {
+  MultichainBalancesController,
+  MultichainBalancesControllerState,
+  MultichainBalancesControllerMessenger,
+} from '@metamask/assets-controllers';
+import type { ControllerInitFunction } from '../../types';
+import { defaultMultichainBalancesControllerState } from './constants';
+
+// Export constants
+export * from './constants';
+
+/**
+ * Initialize the MultichainBalancesController.
+ *
+ * @param request - The request object.
+ * @returns The MultichainBalancesController.
+ */
+export const multichainBalancesControllerInit: ControllerInitFunction<
+  MultichainBalancesController,
+  MultichainBalancesControllerMessenger
+> = (request) => {
+  const { controllerMessenger, persistedState } = request;
+
+  const multichainBalancesControllerState =
+    (persistedState.MultichainBalancesController ??
+      defaultMultichainBalancesControllerState) as MultichainBalancesControllerState;
+
+  const controller = new MultichainBalancesController({
+    messenger: controllerMessenger,
+    state: multichainBalancesControllerState,
+  });
+
+  return { controller };
+};
