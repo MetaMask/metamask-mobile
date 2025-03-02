1b91ff673f86f0cd4ebebbc6459ac4f926886a8b
VolodymyrBg <aqdrgg19@gmail.com>
2025-03-02 20:28:24 +0200

    fix(storybook): replace 'any' type with 'StoryFn' in withSafeArea decorator

--- storybook/decorators/withSafeArea.tsx
+++ storybook/decorators/withSafeArea.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StoryFn } from '@storybook/react-native';
 
-// TODO: Replace "any" with type
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const withSafeArea = (story: any) => (
+const withSafeArea = (story: StoryFn) => (
   <SafeAreaProvider>{story()}</SafeAreaProvider>
 );
 
