081e6bf81ab3236a003920f715e692bd90b8a558
david0xd <david@timechaser.org>
2025-03-03 13:19:17 +0100

    Add Snap UI Spinner

---
+++ app/components/Snaps/SnapUISpinner/SnapUISpinner.tsx
@@ -0,0 +1,6 @@
+import React, { FunctionComponent } from 'react';
+import AnimatedSpinner from '../../UI/AnimatedSpinner';
+
+export const SnapUISpinner: FunctionComponent = () => (
+  <AnimatedSpinner></AnimatedSpinner>
+);
