6494eb30a8948cf5e2f0a49dd73c707822f00f4b
Frederik Bolding <frederik.bolding@gmail.com>
2025-03-03 12:50:28 +0100

    Remove unnecesary JSDoc

--- app/components/UI/Snaps/SnapUISelector/SnapUISelector.styles.ts
+++ app/components/UI/Snaps/SnapUISelector/SnapUISelector.styles.ts
@@ -7,7 +7,6 @@ import { colors as importedColors } from '../../../../styles/common';
  *
  * @param params Style sheet params.
  * @param params.theme App theme from ThemeContext.
- * @param params.vars Inputs that the style sheet depends on.
  * @returns StyleSheet object.
  */
 const styleSheet = (params: { theme: Theme }) => {
