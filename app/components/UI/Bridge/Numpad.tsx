5e169b489a454d3968e965bb72c9d9f8f89cf7ca
IF <139582705+infiniteflower@users.noreply.github.com>
2025-03-04 14:55:10 -0500

    chore: add more skeleton for bridge input

---
+++ app/components/UI/Bridge/Numpad.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import Text from '../../../component-library/components/Texts/Text';
+
+export const Numpad = () => (
+    <View>
+      <Text>Numpad</Text>
+    </View>
+  );
---
+++ app/components/UI/Bridge/TokenInputArea.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+import Text from '../../../component-library/components/Texts/Text';
+
+
+export const TokenInputArea = () => (
+    <View>
+      <Text>TokenInputArea</Text>
+    </View>
+  );
