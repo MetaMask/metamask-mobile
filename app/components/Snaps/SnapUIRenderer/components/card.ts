797cfa4f3daa8752ce66870ea74fef255f02ee26
Frederik Bolding <frederik.bolding@gmail.com>
2025-03-04 15:25:35 +0100

    Add test

--- app/components/Snaps/SnapUIRenderer/components/card.ts
+++ app/components/Snaps/SnapUIRenderer/components/card.ts
@@ -3,7 +3,10 @@ import { CardElement } from '@metamask/snaps-sdk/jsx';
 import { UIComponentFactory } from './types';
 import { mapToTemplate } from '../utils';
 
-export const card: UIComponentFactory<CardElement> = ({ element, ...params }) => {
+export const card: UIComponentFactory<CardElement> = ({
+  element,
+  ...params
+}) => {
   if (typeof element.props.title !== 'string') {
     return {
       element: 'SnapUICard',
@@ -19,7 +22,7 @@ export const card: UIComponentFactory<CardElement> = ({ element, ...params }) =>
     };
   }
 
-  return ({
+  return {
     element: 'SnapUICard',
     props: {
       image: element.props.image,
@@ -28,5 +31,5 @@ export const card: UIComponentFactory<CardElement> = ({ element, ...params }) =>
       value: element.props.value,
       extra: element.props.extra,
     },
-  });
+  };
 };
