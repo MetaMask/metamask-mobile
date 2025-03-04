c6549aebc37fc9e54eaeeffa66a7163c0438fbff
david0xd <david@timechaser.org>
2025-03-04 15:18:02 +0100

    Refactor and adjust spinner component

--- app/components/Snaps/SnapUIRenderer/components/spinner.ts
+++ app/components/Snaps/SnapUIRenderer/components/spinner.ts
@@ -2,8 +2,5 @@ import { SpinnerElement } from '@metamask/snaps-sdk/jsx';
 import { UIComponentFactory } from './types';
 
 export const spinner: UIComponentFactory<SpinnerElement> = () => ({
-  element: 'Preloader',
-  props: {
-    className: 'snap-ui-renderer__spinner',
-  },
+  element: 'SnapUISpinner',
 });
