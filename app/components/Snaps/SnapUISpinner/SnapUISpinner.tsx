a5c35d3f350b5550f6a2505526b8fa57c2bcb908
david0xd <david@timechaser.org>
2025-03-04 15:18:02 +0100

    Refactor and adjust spinner component

--- app/components/Snaps/SnapUISpinner/SnapUISpinner.tsx
+++ app/components/Snaps/SnapUISpinner/SnapUISpinner.tsx
@@ -1,6 +1,12 @@
 import React, { FunctionComponent } from 'react';
-import AnimatedSpinner from '../../UI/AnimatedSpinner';
+import { ActivityIndicator } from 'react-native';
+import { lightTheme } from '@metamask/design-tokens';
 
 export const SnapUISpinner: FunctionComponent = () => (
-  <AnimatedSpinner></AnimatedSpinner>
+  <ActivityIndicator
+    size="large"
+    color={lightTheme.colors.primary.default}
+    /* eslint-disable-next-line react-native/no-inline-styles */
+    style={{ alignItems: 'flex-start' }}
+  />
 );
