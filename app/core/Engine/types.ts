18905c90c8df9ba8d8db82c1b5d0651aa5788a2a
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-03-04 01:30:20 +0100

    Initial migration of TransactionController to modularised controller init

--- app/core/Engine/types.ts
+++ app/core/Engine/types.ts
@@ -243,6 +243,10 @@ import {
   EarnControllerEvents,
   EarnControllerState,
 } from '@metamask/earn-controller';
+import { Hex } from '@metamask/utils';
+
+import { CONTROLLER_MESSENGERS } from './messengers';
+import type { RootState } from '../../reducers';
 
 /**
  * Controllers that area always instantiated
@@ -550,12 +554,13 @@ export type ControllersToInitialize =
   ///: END:ONLY_INCLUDE_IF
   | 'CurrencyRateController'
   | 'AccountsController'
-  | 'MultichainNetworkController';
+  | 'MultichainNetworkController'
+  | 'TransactionController';
 
 /**
  * Callback that returns a controller messenger for a specific controller.
  */
-type ControllerMessengerCallback = (
+export type ControllerMessengerCallback = (
   baseControllerMessenger: BaseControllerMessenger,
 ) => BaseRestrictedControllerMessenger;
 
@@ -571,21 +576,13 @@ type ControllerPersistedState = Partial<{
 }>;
 
 /**
- * Map of controller messengers by controller name.
- */
-export type ControllerMessengerByControllerName = {
-  [key in ControllersToInitialize]: {
-    getMessenger: ControllerMessengerCallback;
-  };
-};
-
-/**
  * Request to initialize and return a controller instance.
  * Includes standard data and methods not coupled to any specific controller.
  */
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 export type ControllerInitRequest<
   ControllerMessengerType extends BaseRestrictedControllerMessenger,
+  InitMessengerType extends void | BaseRestrictedControllerMessenger = void,
 > = {
   /**
    * Controller messenger for the client.
@@ -604,6 +601,22 @@ export type ControllerInitRequest<
   ): ControllerByName[Name];
 
   /**
+   * Get the chain id set in the engine.
+   */
+  getCurrentChainId: () => Hex;
+
+  /**
+   * Get the root state of UI.
+   */
+  getRootState: () => RootState;
+
+  /**
+   * Required initialization messenger instance.
+   * Generated using the callback specified in `getInitMessenger`.
+   */
+  initMessenger: InitMessengerType;
+
+  /**
    * The full persisted state for all controllers.
    * Includes controller name properties.
    * e.g. `{ TransactionController: { transactions: [] } }`.
@@ -617,17 +630,22 @@ export type ControllerInitRequest<
 export type ControllerInitFunction<
   ControllerType extends Controller,
   ControllerMessengerType extends BaseRestrictedControllerMessenger,
-> = (request: ControllerInitRequest<ControllerMessengerType>) => {
+  InitMessengerType extends void | BaseRestrictedControllerMessenger = void,
+> = (
+  request: ControllerInitRequest<ControllerMessengerType, InitMessengerType>,
+) => {
   controller: ControllerType;
 };
 
 /**
  * Map of controller init functions by controller name.
  */
-type ControllerInitFunctionByControllerName = {
+export type ControllerInitFunctionByControllerName = {
   [Name in ControllersToInitialize]: ControllerInitFunction<
     ControllerByName[Name],
-    ReturnType<ControllerMessengerByControllerName[Name]['getMessenger']>
+    // @ts-expect-error TODO: Resolve mismatch between base-controller versions.
+    ReturnType<(typeof CONTROLLER_MESSENGERS)[Name]['getMessenger']>,
+    ReturnType<(typeof CONTROLLER_MESSENGERS)[Name]['getInitMessenger']>
   >;
 };
 
@@ -636,9 +654,11 @@ type ControllerInitFunctionByControllerName = {
  */
 export type InitModularizedControllersFunction = (request: {
   controllerInitFunctions: ControllerInitFunctionByControllerName;
-  persistedState: ControllerPersistedState;
-  existingControllersByName?: Partial<ControllerByName>;
   baseControllerMessenger: BaseControllerMessenger;
+  existingControllersByName?: Partial<ControllerByName>;
+  getCurrentChainId: () => Hex;
+  getRootState: () => RootState;
+  persistedState: ControllerPersistedState;
 }) => {
   controllersByName: ControllerByName;
 };
