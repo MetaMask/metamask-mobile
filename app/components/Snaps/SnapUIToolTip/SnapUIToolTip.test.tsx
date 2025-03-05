6917dfdddc30a61cf68518707145fe55ef3c61af
Daniel-Cross <hello@danielcross.dev>
2025-03-05 17:54:19 +0100

    added tests

---
+++ app/components/Snaps/SnapUIToolTip/SnapUIToolTip.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import { SnapUITooltip } from './SnapUIToolTip';
+import Tooltip from '../../Views/confirmations/components/UI/Tooltip';
+import { Text } from 'react-native';
+
+jest.mock('../../Views/confirmations/components/UI/Tooltip', () => jest.fn());
+
+describe('SnapUITooltip', () => {
+  it('should render tooltip with content', () => {
+    const content = 'Test content';
+    render(<SnapUITooltip content={content} />);
+
+    expect(Tooltip).toHaveBeenCalledWith(
+      expect.objectContaining({
+        content: content,
+      }),
+      expect.any(Object),
+    );
+  });
+
+  it('should render tooltip with complex content', () => {
+    const content = <Text>Complex content</Text>;
+    render(<SnapUITooltip content={content} />);
+
+    expect(Tooltip).toHaveBeenCalledWith(
+      expect.objectContaining({
+        content: content,
+      }),
+      expect.any(Object),
+    );
+  });
+});
