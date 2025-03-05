6a406f4c07565e7797aba418356757b31aa79eb9
IF <139582705+infiniteflower@users.noreply.github.com>
2025-03-05 14:17:08 -0500

    Revert "chore: add comment"
    This reverts commit 3d9fb8095f69820f0deaeab2575bd77364b1fbaa.

--- app/components/UI/Swaps/QuotesView.js
+++ app/components/UI/Swaps/QuotesView.js
@@ -1147,7 +1147,6 @@ function SwapsQuotesView({
           Logger.log(LOG_PREFIX, 'Failed to submit smart transaction', e);
           setIsHandlingSwap(false);
 
-          // Fallback to regular transaction submission if STX fails
           await handleCompleteSwap({ hasSTXFailed: true });
         }
       } else {
