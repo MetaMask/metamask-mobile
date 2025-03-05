14461422a62a543c16a76585bb115558d35a5fcf
digiwand <20778143+digiwand@users.noreply.github.com>
2025-03-04 20:33:31 -0800

    test: TypedSignPermit batch

--- app/components/Views/confirmations/components/Confirm/Info/TypedSignV3V4/Simulation/TypedSignPermit/TypedSignPermit.test.tsx
+++ app/components/Views/confirmations/components/Confirm/Info/TypedSignV3V4/Simulation/TypedSignPermit/TypedSignPermit.test.tsx
@@ -58,23 +58,6 @@ describe('PermitSimulation', () => {
     expect(queryByText('Estimated changes')).toBeNull();
   });
 
-  it('renders for Permit NFTs', async () => {
-    const { getByText } = renderWithProvider(<PermitSimulation />, {
-      state: typedSignV4NFTConfirmationState,
-    });
-
-    expect(getByText('Estimated changes')).toBeTruthy();
-    expect(
-      getByText(
-        "You're giving the spender permission to spend this many tokens from your account.",
-      ),
-    ).toBeTruthy();
-    expect(getByText('Withdraw')).toBeTruthy();
-    expect(getByText('0xC3644...1FE88')).toBeTruthy();
-
-    await waitFor(() => expect(getByText('#3606393')).toBeTruthy());
-  });
-
   it('renders for DAI Revoke', async () => {
     const { getByText } = renderWithProvider(<PermitSimulation />, {
       state: generateStateSignTypedData(SignTypedDataMockType.DAI),
@@ -88,5 +71,39 @@ describe('PermitSimulation', () => {
     ).toBeTruthy();
     expect(getByText('Revoke')).toBeTruthy();
     expect(getByText('0x6B175...71d0F')).toBeTruthy();
+  });
+
+  it('renders for Permit Batch', async () => {
+    const { getByText } = renderWithProvider(<PermitSimulation />, {
+      state: generateStateSignTypedData(SignTypedDataMockType.BATCH),
+    });
+
+    expect(
+      getByText(
+        "You're giving the spender permission to spend this many tokens from your account.",
+      ),
+    ).toBeTruthy();
+    expect(getByText('Spending cap')).toBeTruthy();
+    expect(getByText('0xA0b86...6eB48')).toBeTruthy();
+    expect(getByText('0xb0B86...6EB48')).toBeTruthy();
+
+    await waitFor(() => expect(getByText('1,461,501,637,3...')).toBeTruthy());
+    await waitFor(() => expect(getByText('250')).toBeTruthy());
+  });
+
+  it('renders for Permit NFTs', async () => {
+    const { getByText } = renderWithProvider(<PermitSimulation />, {
+      state: typedSignV4NFTConfirmationState,
+    });
+
+    expect(
+      getByText(
+        "You're giving the spender permission to spend this many tokens from your account.",
+      ),
+    ).toBeTruthy();
+    expect(getByText('Withdraw')).toBeTruthy();
+    expect(getByText('0xC3644...1FE88')).toBeTruthy();
+
+    await waitFor(() => expect(getByText('#3606393')).toBeTruthy());
   });
 });
--- app/util/test/confirm-data-helpers.ts
+++ app/util/test/confirm-data-helpers.ts
@@ -675,10 +675,55 @@ export const stakingDepositConfirmationState = {
 };
 
 export enum SignTypedDataMockType {
+  BATCH = 'BATCH',
   DAI = 'DAI',
 }
 
 const SIGN_TYPE_DATA: Record<SignTypedDataMockType, string> = {
+  [SignTypedDataMockType.BATCH]: JSON.stringify({
+    types: {
+      PermitBatch: [
+        { name: 'details', type: 'PermitDetails[]' },
+        { name: 'spender', type: 'address' },
+        { name: 'sigDeadline', type: 'uint256' }
+      ],
+      PermitDetails: [
+        { name: 'token', type: 'address' },
+        { name: 'amount', type: 'uint160' },
+        { name: 'expiration', type: 'uint48' },
+        { name: 'nonce', type: 'uint48' }
+      ],
+      EIP712Domain: [
+        { name: 'name', type: 'string' },
+        { name: 'chainId', type: 'uint256' },
+        { name: 'verifyingContract', type: 'address' }
+      ]
+    },
+    domain: {
+      name: 'Permit2',
+      chainId: '1',
+      verifyingContract: '0x000000000022d473030f116ddee9f6b43ac78ba3'
+    },
+    primaryType: 'PermitBatch',
+    message: {
+      details: [
+        {
+          token: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+          amount: '1461501637330902918203684832716283019655932542975',
+          expiration: '1722887542',
+          nonce: '5'
+        },
+        {
+          token: '0xb0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+          amount: '250',
+          expiration: '1722887642',
+          nonce: '6'
+        }
+      ],
+      spender: '0x3fc91a3afd70395cd496c647d5a6cc9d4b2b7fad',
+      sigDeadline: '1720297342'
+    }
+  }),
   [SignTypedDataMockType.DAI]: JSON.stringify({
     domain: {
       name: 'Dai Stablecoin',
