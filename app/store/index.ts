f6301c3f19c11811f8acfa3a0895cc690a551ca7
Cal-L <cleun007@gmail.com>
2025-03-06 10:22:56 -0800

    Clean up store/middleware

--- app/store/index.ts
+++ app/store/index.ts
@@ -6,14 +6,14 @@ import { rootSaga } from './sagas';
 import rootReducer, { RootState } from '../reducers';
 import ReadOnlyNetworkStore from '../util/test/network-store';
 import { isE2E } from '../util/test/utils';
-import { trace, endTrace, TraceName, TraceOperation } from '../util/trace';
+// import { trace, endTrace, TraceName, TraceOperation } from '../util/trace';
 
 import thunk from 'redux-thunk';
 
 import persistConfig from './persistConfig';
-import getUIStartupSpan from '../core/Performance/UIStartup';
+// import getUIStartupSpan from '../core/Performance/UIStartup';
 import ReduxService, { ReduxStore } from '../core/redux';
-import { onPersistedDataLoaded } from '../actions/user';
+// import { onPersistedDataLoaded } from '../actions/user';
 
 // TODO: Improve type safety by using real Action types instead of `AnyAction`
 const pReducer = persistReducer<RootState, AnyAction>(
@@ -24,11 +24,12 @@ const pReducer = persistReducer<RootState, AnyAction>(
 // eslint-disable-next-line import/no-mutable-exports
 let store: ReduxStore, persistor: Persistor;
 const createStoreAndPersistor = async () => {
-  trace({
-    name: TraceName.StoreInit,
-    parentContext: getUIStartupSpan(),
-    op: TraceOperation.StoreInit,
-  });
+  console.log('createStoreAndPersistor');
+  // trace({
+  //   name: TraceName.StoreInit,
+  //   parentContext: getUIStartupSpan(),
+  //   op: TraceOperation.StoreInit,
+  // });
   // Obtain the initial state from ReadOnlyNetworkStore for E2E tests.
   const initialState = isE2E
     ? await ReadOnlyNetworkStore.getState()
@@ -39,20 +40,20 @@ const createStoreAndPersistor = async () => {
   // Create the store and apply middlewares. In E2E tests, an optional initialState
   // from fixtures can be provided to preload the store; otherwise, it remains undefined.
 
-  const middlewares = [sagaMiddleware, thunk];
+  // const middlewares = [sagaMiddleware, thunk];
 
-  if (__DEV__) {
-    // Add redux flipper middleware for debugging Redux with Flipper
-    // Flipper's client side plugin is https://github.com/jk-gan/flipper-plugin-redux-debugger, which needs to be added as a plugin
-    // flipper-plugin-redux-debugger is named redux-debugger in Flipper's plugin list
-    /* eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
-    const createReduxFlipperDebugger = require('redux-flipper').default;
-    middlewares.push(createReduxFlipperDebugger());
-  }
+  // if (__DEV__) {
+  //   // Add redux flipper middleware for debugging Redux with Flipper
+  //   // Flipper's client side plugin is https://github.com/jk-gan/flipper-plugin-redux-debugger, which needs to be added as a plugin
+  //   // flipper-plugin-redux-debugger is named redux-debugger in Flipper's plugin list
+  //   /* eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
+  //   const createReduxFlipperDebugger = require('redux-flipper').default;
+  //   middlewares.push(createReduxFlipperDebugger());
+  // }
 
   store = configureStore({
     reducer: pReducer,
-    middleware: middlewares,
+    middleware: [sagaMiddleware, thunk],
     preloadedState: initialState,
   });
   // Set the store in the Redux class
@@ -64,9 +65,9 @@ const createStoreAndPersistor = async () => {
    * Initialize services after persist is completed
    */
   const onPersistComplete = () => {
-    endTrace({ name: TraceName.StoreInit });
+    // endTrace({ name: TraceName.StoreInit });
     // Signal that persisted data has been loaded
-    store.dispatch(onPersistedDataLoaded());
+    // store.dispatch(onPersistedDataLoaded());
   };
 
   persistor = persistStore(store, null, onPersistComplete);
