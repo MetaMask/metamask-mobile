62c557a5abbc38b88d5e0d1cf61150838f91253f
Nicholas Gambino <nicholas.gambino@consensys.net>
2025-03-06 11:16:06 -0800

    chore: Init re-render unit test suite for selectAccountTokensAcrossChains selector

--- app/selectors/multichain/evm.test.tsx
+++ app/selectors/multichain/evm.test.tsx
@@ -1,9 +1,31 @@
+import React from 'react';
+import { Text } from 'react-native';
 import { RootState } from '../../reducers';
 import {
   selectedAccountNativeTokenCachedBalanceByChainId,
   selectAccountTokensAcrossChains,
 } from './evm';
 import { SolScope } from '@metamask/keyring-api';
+import { GetByQuery } from '@testing-library/react-native/build/queries/makeQueries';
+import {
+  TextMatch,
+  TextMatchOptions,
+} from '@testing-library/react-native/build/matches';
+import { CommonQueryOptions } from '@testing-library/react-native/build/queries/options';
+import { Store } from 'redux';
+import { useSelector } from 'react-redux';
+import renderWithProvider from '../../util/test/renderWithProvider';
+import Engine, { EngineState } from '../../core/Engine';
+import { act } from '@testing-library/react-native';
+import {
+  AccountTrackerController,
+  TokensController,
+} from '@metamask/assets-controllers';
+import {
+  ETH_CHAIN_ID,
+  POLYGON_CHAIN_ID,
+} from '@metamask/swaps-controller/dist/constants';
+import { AccountsControllerState } from '@metamask/accounts-controller';
 
 describe('Multichain Selectors', () => {
   const mockState: RootState = {
@@ -179,5 +201,179 @@ describe('Multichain Selectors', () => {
         true,
       );
     });
+  });
+});
+
+const mockAccountId = '0xAddress1';
+
+const mockAllTokens = {
+  [ETH_CHAIN_ID]: {
+    [mockAccountId]: [
+      {
+        address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+        symbol: 'USDC',
+        decimals: 6,
+        name: 'USDC',
+      },
+      {
+        address: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+        symbol: 'DAI',
+        decimals: 18,
+        name: 'Dai Stablecoin',
+      },
+    ],
+  },
+  [POLYGON_CHAIN_ID]: {
+    [mockAccountId]: [
+      {
+        address: '0x0D1E753a25eBda689453309112904807625bEFBe',
+        symbol: 'CAKE',
+        decimals: 18,
+        image:
+          'https://static.cx.metamask.io/api/v1/tokenIcons/59144/0x0d1e753a25ebda689453309112904807625befbe.png',
+        aggregators: ['CoinGecko', 'Lifi', 'Rubic'],
+      },
+    ],
+  },
+};
+
+const mockNetworkConfigurationsByChainId = {
+  [ETH_CHAIN_ID]: {
+    chainId: ETH_CHAIN_ID,
+    name: 'Ethereum Mainnet',
+    nativeCurrency: 'ETH',
+  },
+  [POLYGON_CHAIN_ID]: {
+    chainId: POLYGON_CHAIN_ID,
+    name: 'Polygon',
+    nativeCurrency: 'POL',
+  },
+};
+
+const mockAccountsByChainId = {
+  [ETH_CHAIN_ID]: {
+    [mockAccountId]: {
+      balance: '0x1',
+      stakedBalance: '0x2',
+    },
+  },
+  [POLYGON_CHAIN_ID]: {
+    [mockAccountId]: {
+      balance: '0x3',
+    },
+  },
+};
+
+const mockInternalAccounts = {
+  selectedAccount: mockAccountId,
+  accounts: {
+    [mockAccountId]: {
+      address: mockAccountId,
+    },
+  },
+};
+
+// Mock Engine for render tests
+jest.mock('../../core/Engine', () => ({
+  context: {},
+  state: {
+    NetworkController: {
+      networkConfigurationsByChainId: mockNetworkConfigurationsByChainId,
+    },
+    AccountTrackerController: {
+      accountsByChainId: mockAccountsByChainId,
+    } as Partial<AccountTrackerController['state']>,
+    TokensController: {
+      allTokens: mockAllTokens,
+    } as Partial<TokensController['state']>,
+    AccountsController: {
+      internalAccounts: mockInternalAccounts,
+    } as unknown as Partial<AccountsControllerState>,
+  } as EngineState,
+}));
+
+describe('re-renders', () => {
+  const mockState: RootState = {
+    engine: {
+      backgroundState: {
+        NetworkController: {
+          networkConfigurationsByChainId: mockNetworkConfigurationsByChainId,
+        },
+        AccountTrackerController: {
+          accountsByChainId: mockAccountsByChainId,
+        },
+        TokensController: {
+          allTokens: mockAllTokens,
+        },
+        AccountsController: {
+          internalAccounts: mockInternalAccounts,
+        },
+      },
+    },
+  } as unknown as RootState;
+  const mockRenderCall = jest.fn();
+  let getByText: GetByQuery<TextMatch, CommonQueryOptions & TextMatchOptions>;
+  let store: Store;
+
+  beforeEach(() => {
+    mockRenderCall.mockReset();
+    // Clear memoized selectors for each test
+    selectAccountTokensAcrossChains.memoizedResultFunc.clearCache();
+    const MockComponent = () => {
+      const selectedAccountTokensChains = useSelector(
+        selectAccountTokensAcrossChains,
+      );
+      mockRenderCall();
+      return (
+        <>
+          {selectedAccountTokensChains[ETH_CHAIN_ID]?.map((token) => (
+            <Text key={token.address}>{token.name}</Text>
+          ))}
+        </>
+      );
+    };
+    const { store: testStore, getByText: testGetByText } = renderWithProvider(
+      <MockComponent />,
+      {
+        state: mockState,
+      },
+    );
+    getByText = testGetByText;
+    store = testStore;
+  });
+
+  it('re-renders token list once when new token is added', async () => {
+    expect(mockRenderCall).toHaveBeenCalledTimes(1);
+    mockRenderCall.mockReset();
+
+    const newToken = {
+      address: '0xNewTokenAddress',
+      symbol: 'NEW',
+      decimals: 18,
+      name: 'New Token',
+    };
+
+    Engine.state.TokensController.allTokens = {
+      ...mockAllTokens,
+      [ETH_CHAIN_ID]: {
+        ...mockAllTokens[ETH_CHAIN_ID],
+        [mockAccountId]: [
+          ...mockAllTokens[ETH_CHAIN_ID][mockAccountId],
+          newToken,
+        ],
+      },
+    };
+
+    act(() => {
+      store.dispatch({
+        type: 'UPDATE_BG_STATE',
+        payload: {
+          key: 'TokensController',
+        },
+      });
+    });
+
+    expect(mockRenderCall).toHaveBeenCalledTimes(1);
+    expect(getByText(newToken.name)).toBeDefined();
   });
 });
