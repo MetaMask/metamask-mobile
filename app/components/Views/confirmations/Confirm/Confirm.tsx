dcf50476f556aefddf5d90c938486ad870f0a102
digiwand <20778143+digiwand@users.noreply.github.com>
2025-03-07 08:54:14 -0800

    fix: cherry-pick 7.42.0 refactor: Replace redesign confirmation BottomModal with BottomSheet (#13914)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    Cherry-picks 1/3 BottomSheet fix:
    https://github.com/MetaMask/metamask-mobile/pull/13268 into 7.42.0
    
    Todo cherry-picks:
    2/3: https://github.com/MetaMask/metamask-mobile/pull/13783
    3/3: https://github.com/MetaMask/metamask-mobile/pull/13913
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-mobile/issues/13870
    
    ## **Manual testing steps**
    
    Open any redesign signature in Android to test related bug fix
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    Co-authored-by: Brian Nguyen <brianacnguyen@gmail.com>

--- app/components/Views/confirmations/Confirm/Confirm.tsx
+++ app/components/Views/confirmations/Confirm/Confirm.tsx
@@ -6,34 +6,36 @@ import {
   View,
 } from 'react-native';
 
+import BottomSheet from '../../../../component-library/components/BottomSheets/BottomSheet';
 import { useStyles } from '../../../../component-library/hooks';
-import BottomModal from '../components/UI/BottomModal';
-import Footer from '../components/Confirm/Footer';
+import { Footer } from '../components/Confirm/Footer';
 import Info from '../components/Confirm/Info';
 import { LedgerContextProvider } from '../context/LedgerContext';
 import { QRHardwareContextProvider } from '../context/QRHardwareContext/QRHardwareContext';
 import SignatureBlockaidBanner from '../components/Confirm/SignatureBlockaidBanner';
 import Title from '../components/Confirm/Title';
-import useApprovalRequest from '../hooks/useApprovalRequest';
-import { useConfirmActions } from '../hooks/useConfirmActions';
 import { useConfirmationRedesignEnabled } from '../hooks/useConfirmationRedesignEnabled';
 import { useFlatConfirmation } from '../hooks/useFlatConfirmation';
+import useApprovalRequest from '../hooks/useApprovalRequest';
+import { useConfirmActions } from '../hooks/useConfirmActions';
 import styleSheet from './Confirm.styles';
 
 const ConfirmWrapped = ({
   styles,
+  testID,
 }: {
   styles: StyleSheet.NamedStyles<Record<string, unknown>>;
+  testID?: string;
 }) => (
   <QRHardwareContextProvider>
     <LedgerContextProvider>
       <Title />
-      <ScrollView style={styles.scrollable}>
-        <TouchableWithoutFeedback>
-          <View style={styles.scrollableSection}>
+      <ScrollView style={styles.scrollView}>
+        <TouchableWithoutFeedback testID={testID}>
+          <>
             <SignatureBlockaidBanner />
             <Info />
-          </View>
+          </>
         </TouchableWithoutFeedback>
       </ScrollView>
       <Footer />
@@ -62,10 +64,13 @@ export const Confirm = () => {
   }
 
   return (
-    <BottomModal onClose={onReject} testID="modal-confirmation-container">
-      <View style={styles.modalContainer} testID={approvalRequest?.type}>
-        <ConfirmWrapped styles={styles} />
-      </View>
-    </BottomModal>
+    <BottomSheet
+      isInteractable={false}
+      onClose={onReject}
+      style={styles.bottomSheetDialogSheet}
+      testID="modal-confirmation-container"
+    >
+      <ConfirmWrapped testID={approvalRequest?.type} styles={styles} />
+    </BottomSheet>
   );
 };
