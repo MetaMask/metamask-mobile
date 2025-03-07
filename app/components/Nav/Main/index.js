519a356c789a0cc41519e7c940d399f8a2301b5a
Nicholas Gambino <nicholas.gambino@consensys.net>
2025-03-06 18:37:12 -0800

    chore: Remove commented code

--- app/components/Nav/Main/index.js
+++ app/components/Nav/Main/index.js
@@ -87,7 +87,6 @@ import {
 } from '../../../util/transaction-controller';
 import isNetworkUiRedesignEnabled from '../../../util/networks/isNetworkUiRedesignEnabled';
 import { useConnectionHandler } from '../../../util/navigation/useConnectionHandler';
-import { AssetPollingProvider } from '../../hooks/AssetPolling/AssetPollingProvider';
 import { getGlobalEthQuery } from '../../../util/networks/global-network';
 import { selectIsEvmNetworkSelected } from '../../../selectors/multichainNetworkController';
 import { isPortfolioViewEnabled } from '../../../util/networks';
@@ -415,7 +414,6 @@ const Main = (props) => {
 
   return (
     <React.Fragment>
-      {/* <AssetPollingProvider> */}
       <View style={styles.flex}>
         {!forceReload ? (
           <MainNavigator navigation={props.navigation} />
@@ -445,7 +443,6 @@ const Main = (props) => {
         <ProtectYourWalletModal navigation={props.navigation} />
         <RootRPCMethodsUI navigation={props.navigation} />
       </View>
-      {/* </AssetPollingProvider> */}
     </React.Fragment>
   );
 };
