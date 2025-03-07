60283a541052512bb3f373588310cf119ca2781a
Cal-L <cleun007@gmail.com>
2025-03-07 09:49:31 -0800

    Expand unit tests for Root component

--- app/components/Views/Root/index.test.tsx
+++ app/components/Views/Root/index.test.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { View as MockView } from 'react-native';
 import Root from './';
 import { render, waitFor } from '@testing-library/react-native';
 import SecureKeychain from '../../../core/SecureKeychain';
 import EntryScriptWeb3 from '../../../core/EntryScriptWeb3';
+
+const MOCK_CHILD_ID = 'MOCK_CHILD_ID';
+
+jest.mock('react-native-safe-area-context', () => {
+  return {
+    SafeAreaProvider: jest
+      .fn()
+      .mockImplementation(() => <MockView testID={MOCK_CHILD_ID} />),
+  };
+});
 
 jest.mock('../../../core/SecureKeychain', () => ({
   init: jest.fn(),
@@ -15,18 +26,66 @@ jest.mock('../../../core/EntryScriptWeb3', () => ({
 describe('Root', () => {
   it('should render correctly', () => {
     const { toJSON } = render(<Root foxCode="" />);
+
     expect(toJSON()).toMatchSnapshot();
   });
 
   it('should initialize SecureKeychain', async () => {
     render(<Root foxCode="" />);
+
     await waitFor(() => {
       expect(SecureKeychain.init).toHaveBeenCalled();
+      expect(EntryScriptWeb3.init).toHaveBeenCalled();
     });
   });
 
-  it('should initialize EntryScriptWeb3', async () => {
-    render(<Root foxCode="" />);
-    expect(EntryScriptWeb3.init).toHaveBeenCalled();
+  it('should render children if isTest OR isLoading is false', async () => {
+    jest.isolateModules(() => {
+      jest.mock('../../../util/test/utils', () => ({
+        isTest: false,
+      }));
+      jest.mock('react', () => {
+        const mockIsLoading = false;
+        const mockSetIsLoading = jest.fn();
+        return {
+          ...jest.requireActual('react'),
+          // Mock isLoading state to be false
+          useState: jest
+            .fn()
+            .mockImplementation(() => [mockIsLoading, mockSetIsLoading]),
+          useEffect: jest.fn(),
+        };
+      });
+      // Import Root after mocking isTest
+      const Root = require('./index').default;
+
+      const { getByTestId } = render(<Root foxCode="" />);
+      expect(getByTestId(MOCK_CHILD_ID)).toBeDefined();
+    });
+  });
+
+  it('should render children if isTest AND isLoading is true', async () => {
+    jest.isolateModules(() => {
+      jest.mock('../../../util/test/utils', () => ({
+        isTest: true,
+      }));
+      jest.mock('react', () => {
+        const mockIsLoading = true;
+        const mockSetIsLoading = jest.fn();
+        return {
+          ...jest.requireActual('react'),
+          // Mock isLoading state to be true
+          useState: jest
+            .fn()
+            .mockImplementation(() => [mockIsLoading, mockSetIsLoading]),
+          useEffect: jest.fn(),
+        };
+      });
+      // Import Root after mocking isTest
+      const Root = require('./index').default;
+
+      const { toJSON } = render(<Root foxCode="" />);
+      expect(toJSON()).toBeNull();
+    });
   });
 });
