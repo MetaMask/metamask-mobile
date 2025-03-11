4e80c1503dd3b7975438d259b294c332a7316efc
Bryan Fullam <bryan.fullam@consensys.net>
2025-03-11 17:56:30 +0100

    feat: handle Eth USDT special allowance reset case

--- app/util/bridge/hooks/useHandleApprovalTx.ts
+++ app/util/bridge/hooks/useHandleApprovalTx.ts
@@ -1,12 +1,59 @@
 import { TransactionType } from '@metamask/transaction-controller';
 import useHandleTx from './useHandleTx';
 import { QuoteResponse, TxData } from '../../../components/UI/Bridge/types';
+import { FeeType, isEthUsdt, getEthUsdtResetData } from '@metamask/bridge-controller';
+import { decimalToHex } from '../../conversions';
+import { addHexPrefix } from 'ethereumjs-util';
+import { ETH_USDT_ADDRESS } from '@metamask/bridge-controller/dist/constants/bridge.cjs';
+import BigNumber from 'bignumber.js';
+import { Hex } from '@metamask/utils';
+import Engine from '../../../core/Engine';
 
 export const ALLOWANCE_RESET_ERROR = 'Eth USDT allowance reset failed';
 export const APPROVAL_TX_ERROR = 'Approve transaction failed';
 
 export default function useHandleApprovalTx() {
   const { handleTx } = useHandleTx();
+
+  const handleEthUsdtAllowanceReset = async ({
+    approval,
+    quoteResponse,
+    hexChainId,
+  }: {
+    approval: TxData;
+    quoteResponse: QuoteResponse;
+    hexChainId: Hex;
+  }) => {
+    try {
+      const allowance = new BigNumber(
+        await Engine.context.BridgeController.getBridgeERC20Allowance(ETH_USDT_ADDRESS, hexChainId),
+      );
+
+      // quote.srcTokenAmount is actually after the fees
+      // so we need to add fees back in for total allowance to give
+      const sentAmount = new BigNumber(quoteResponse.quote.srcTokenAmount)
+        .plus(quoteResponse.quote.feeData[FeeType.METABRIDGE].amount)
+        .toString();
+
+      const shouldResetApproval = allowance.lt(sentAmount) && allowance.gt(0);
+      if (shouldResetApproval) {
+        const resetData = getEthUsdtResetData();
+        const txParams = {
+          ...approval,
+          data: resetData,
+        };
+        await handleTx({
+          txType: TransactionType.bridgeApproval,
+          txParams,
+          fieldsToAddToTxMeta: {
+            sourceTokenSymbol: quoteResponse.quote.srcAsset.symbol,
+          },
+        });
+      }
+    } catch (e) {
+      throw new Error(`${ALLOWANCE_RESET_ERROR}: ${e}`);
+    }
+  };
 
   const handleApprovalTx = async ({
     approval,
@@ -16,6 +63,17 @@ export default function useHandleApprovalTx() {
     quoteResponse: QuoteResponse;
   }) => {
     try {
+      // On Ethereum, we need to reset the allowance to 0 for USDT first if we need to set a new allowance
+      // https://www.google.com/url?q=https://docs.unizen.io/trade-api/before-you-get-started/token-allowance-management-for-non-updatable-allowance-tokens&sa=D&source=docs&ust=1727386175513609&usg=AOvVaw3Opm6BSJeu7qO0Ve5iLTOh
+      const hexChainId = addHexPrefix(String(decimalToHex(approval.chainId))) as `0x${string}`;
+      if (isEthUsdt(hexChainId, quoteResponse.quote.srcAsset.address)) {
+        await handleEthUsdtAllowanceReset({
+          approval,
+          quoteResponse,
+          hexChainId,
+        });
+      }
+
       const txMeta = await handleTx({
         txType: TransactionType.bridgeApproval,
         txParams: approval,
