1606cf7178a927c4ea570e909d88059a6ca56785
Bryan Fullam <bryan.fullam@consensys.net>
2025-03-12 14:45:36 +0100

    chore: remove unused types

--- app/components/UI/Bridge/types.ts
+++ app/components/UI/Bridge/types.ts
@@ -1,37 +1,4 @@
-import type { CaipChainId, Hex } from '@metamask/utils';
-import type { BigNumber } from 'bignumber.js';
-
-export interface ChainConfiguration {
-  isActiveSrc: boolean;
-  isActiveDest: boolean;
-}
-
-export interface L1GasFees {
-  l1GasFeesInHexWei?: string; // l1 fees for approval and trade in hex wei, appended by controller
-}
-// Values derived from the quote response
-// valueInCurrency values are calculated based on the user's selected currency
-export interface TokenAmountValues {
-  amount: BigNumber;
-  valueInCurrency: BigNumber | null;
-  usd: BigNumber | null;
-}
-export interface QuoteMetadata {
-  gasFee: TokenAmountValues;
-  totalNetworkFee: TokenAmountValues; // estimatedGasFees + relayerFees
-  totalMaxNetworkFee: TokenAmountValues; // maxGasFees + relayerFees
-  toTokenAmount: TokenAmountValues; // destTokenAmount
-  adjustedReturn: Omit<TokenAmountValues, 'amount'>; // destTokenAmount - totalNetworkFee
-  sentAmount: TokenAmountValues; // srcTokenAmount + metabridgeFee
-  swapRate: BigNumber; // destTokenAmount / sentAmount
-  cost: Omit<TokenAmountValues, 'amount'>; // sentAmount - adjustedReturn
-}
-// Sort order set by the user
-
-export enum SortOrder {
-  COST_ASC = 'cost_ascending',
-  ETA_ASC = 'time_descending',
-}
+import type { CaipChainId } from '@metamask/utils';
 
 export type BridgeToken = {
   address: string;
@@ -40,20 +7,6 @@ export type BridgeToken = {
   decimals: number;
   chainId: CaipChainId;
 };
-
-export enum BridgeFlag {
-  EXTENSION_CONFIG = 'extension-config',
-}
-export type GasMultiplierByChainId = Record<string, number>;
-
-export interface FeatureFlagResponse {
-  [BridgeFlag.EXTENSION_CONFIG]: {
-    refreshRate: number;
-    maxRefreshCount: number;
-    support: boolean;
-    chains: Record<number, ChainConfiguration>;
-  };
-}
 
 export interface BridgeAsset {
   chainId: ChainId;
@@ -64,21 +17,6 @@ export interface BridgeAsset {
   icon?: string;
 }
 
-export interface QuoteRequest {
-  walletAddress: string;
-  destWalletAddress?: string;
-  srcChainId: ChainId;
-  destChainId: ChainId;
-  srcTokenAddress: string;
-  destTokenAddress: string;
-  srcTokenAmount: string; // This is the amount sent
-  slippage: number;
-  aggIds?: string[];
-  bridgeIds?: string[];
-  insufficientBal?: boolean;
-  resetApproval?: boolean;
-  refuel?: boolean;
-}
 interface Protocol {
   name: string;
   displayName?: string;
@@ -156,41 +94,4 @@ export interface TxData {
 }
 export enum BridgeFeatureFlagsKey {
   EXTENSION_CONFIG = 'extensionConfig',
-}
-
-export interface BridgeFeatureFlags {
-  [BridgeFeatureFlagsKey.EXTENSION_CONFIG]: {
-    refreshRate: number;
-    maxRefreshCount: number;
-    support: boolean;
-    chains: Record<Hex, ChainConfiguration>;
-  };
-}
-export enum RequestStatus {
-  LOADING,
-  FETCHED,
-  ERROR,
-}
-export enum BridgeUserAction {
-  SELECT_DEST_NETWORK = 'selectDestNetwork',
-  UPDATE_QUOTE_PARAMS = 'updateBridgeQuoteRequestParams',
-}
-export enum BridgeBackgroundAction {
-  SET_FEATURE_FLAGS = 'setBridgeFeatureFlags',
-  RESET_STATE = 'resetState',
-  GET_BRIDGE_ERC20_ALLOWANCE = 'getBridgeERC20Allowance',
-}
-export interface BridgeState {
-  bridgeFeatureFlags: BridgeFeatureFlags;
-  quoteRequest: Partial<QuoteRequest>;
-  quotes: (QuoteResponse & L1GasFees)[];
-  quotesInitialLoadTime?: number;
-  quotesLastFetched?: number;
-  quotesLoadingStatus?: RequestStatus;
-  quoteFetchError?: string;
-  quotesRefreshCount: number;
-}
-
-export interface BridgeControllerState {
-  bridgeState: BridgeState;
 }
