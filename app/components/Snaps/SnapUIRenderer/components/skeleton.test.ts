7fc170fb6cff7fa45eabaea361ade2837fc8ecb1
david0xd <david@timechaser.org>
2025-03-13 10:39:21 +0100

    Add unit tests

---
+++ app/components/Snaps/SnapUIRenderer/components/skeleton.test.ts
@@ -0,0 +1,74 @@
+import { skeleton } from './skeleton';
+import { SkeletonElement } from '@metamask/snaps-sdk/jsx';
+import { mapSnapBorderRadiusToExtensionBorderRadius } from '../utils';
+import { mockTheme } from '../../../../util/theme';
+
+jest.mock('../utils', () => ({
+  mapSnapBorderRadiusToExtensionBorderRadius: jest.fn((value) => value),
+}));
+
+describe('skeleton component', () => {
+  const defaultParams = {
+    map: {},
+    t: jest.fn(),
+    theme: mockTheme,
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should render skeleton with default props', () => {
+    const el: SkeletonElement = {
+      type: 'Skeleton',
+      props: {},
+      key: null,
+    };
+
+    const result = skeleton({ element: el, ...defaultParams });
+
+    expect(result).toEqual({
+      element: 'Skeleton',
+      props: {
+        width: '100%',
+        height: 22,
+        style: {
+          borderRadius: 6,
+        },
+      },
+    });
+  });
+
+  it('should render skeleton with all custom props', () => {
+    const mockBorderRadius = 'full';
+    (mapSnapBorderRadiusToExtensionBorderRadius as jest.Mock).mockReturnValue(
+      9999,
+    );
+
+    const el: SkeletonElement = {
+      type: 'Skeleton',
+      props: {
+        width: '75%',
+        height: 40,
+        borderRadius: mockBorderRadius,
+      },
+      key: null,
+    };
+
+    const result = skeleton({ element: el, ...defaultParams });
+
+    expect(result).toEqual({
+      element: 'Skeleton',
+      props: {
+        width: '75%',
+        height: 40,
+        style: {
+          borderRadius: 9999,
+        },
+      },
+    });
+    expect(mapSnapBorderRadiusToExtensionBorderRadius).toHaveBeenCalledWith(
+      mockBorderRadius,
+    );
+  });
+});
