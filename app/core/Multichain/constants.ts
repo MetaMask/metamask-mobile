0615e5cd10c8deea99b53c0966d5063ac7d8985a
Owen Craston <owengc12@gmail.com>
2025-03-13 18:02:44 -0700

    derive block explorer url for solana

--- app/core/Multichain/constants.ts
+++ app/core/Multichain/constants.ts
@@ -7,6 +7,7 @@ import {
 } from '@metamask/keyring-api';
 import BTC from '../../images/bitcoin-logo.png';
 import SOL from '../../images/solana-logo.png';
+import { MultichainBlockExplorerFormatUrls } from './networks';
 
 // Image imports for React Native rendering
 export const MULTICHAIN_TOKEN_IMAGES = {
@@ -35,17 +36,54 @@ export type MultichainProviderConfig = ProviderConfigWithImageUrl & {
   isAddressCompatible: (address: string) => boolean;
 };
 
+/**
+ * @deprecated TEMPORARY SOURCE OF TRUTH TBD
+ * Non evm network mapping.
+ */
+export enum MultichainNetworks {
+  BITCOIN = 'bip122:000000000019d6689c085ae165831e93',
+  BITCOIN_TESTNET = 'bip122:000000000933ea01ad0ee984209779ba',
+
+  SOLANA = 'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp',
+  SOLANA_DEVNET = 'solana:EtWTRABZaYq6iMfeYKouRu166VU2xqa1',
+  SOLANA_TESTNET = 'solana:4uhcVJyU9pJkvQyS88uRDiswHXSCkY3z',
+}
+
 export const MULTICHAIN_ACCOUNT_TYPE_TO_MAINNET = {
   [BtcAccountType.P2wpkh]: BtcScope.Mainnet,
   [SolAccountType.DataAccount]: SolScope.Mainnet,
 } as const;
 
-export const MULTICHAIN_NETWORK_BLOCK_EXPLORER_URL_MAP = {
-  [BtcScope.Mainnet]: 'https://blockstream.info/address',
-  [BtcScope.Testnet]: 'https://blockstream.info/testnet/address',
-  [SolScope.Mainnet]: 'https://explorer.solana.com/',
-  [SolScope.Devnet]: 'https://explorer.solana.com/?cluster=devnet',
-  [SolScope.Testnet]: 'https://explorer.solana.com/?cluster=testnet',
+export const MULTICHAIN_NETWORK_BLOCK_EXPLORER_FORMAT_URLS_MAP: Record<
+  CaipChainId,
+  MultichainBlockExplorerFormatUrls
+> = {
+  [MultichainNetworks.BITCOIN]: {
+    url: 'https://blockstream.info',
+    address: 'https://blockstream.info/address/{address}',
+    transaction: 'https://blockstream.info/tx/{txId}',
+  },
+  [MultichainNetworks.BITCOIN_TESTNET]: {
+    url: 'https://blockstream.info',
+    address: 'https://blockstream.info/testnet/address/{address}',
+    transaction: 'https://blockstream.info/testnet/tx/{txId}',
+  },
+
+  [MultichainNetworks.SOLANA]: {
+    url: 'https://explorer.solana.com',
+    address: 'https://explorer.solana.com/address/{address}',
+    transaction: 'https://explorer.solana.com/tx/{txId}',
+  },
+  [MultichainNetworks.SOLANA_DEVNET]: {
+    url: 'https://explorer.solana.com',
+    address: 'https://explorer.solana.com/address/{address}?cluster=devnet',
+    transaction: 'https://explorer.solana.com/tx/{txId}?cluster=devnet',
+  },
+  [MultichainNetworks.SOLANA_TESTNET]: {
+    url: 'https://explorer.solana.com',
+    address: 'https://explorer.solana.com/address/{address}?cluster=testnet',
+    transaction: 'https://explorer.solana.com/tx/{txId}?cluster=testnet',
+  },
 } as const;
 
 export const PRICE_API_CURRENCIES = [
