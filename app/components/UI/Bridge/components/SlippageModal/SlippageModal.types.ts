579c29b5065d7590ee97ee26baf5030a720d8a3c
Amitabh Aggarwal <aggarwal.amitabh@gmail.com>
2025-03-14 04:28:21 -0500

    refactor: remove unused SlippageModal types
    This commit cleans up the SlippageModal.types.ts file by removing the SlippageModalRef and SlippageModalProps interfaces, which are no longer in use.

--- app/components/UI/Bridge/components/SlippageModal/SlippageModal.types.ts
+++ app/components/UI/Bridge/components/SlippageModal/SlippageModal.types.ts
@@ -2,13 +2,3 @@ export interface SlippageOption {
   label: string;
   value: string;
 }
-
-export interface SlippageModalRef {
-  show: () => void;
-  hide: () => void;
-}
-
-export interface SlippageModalProps {
-  onSlippageSelected: (slippage: string) => void;
-  selectedSlippage: string;
-}
