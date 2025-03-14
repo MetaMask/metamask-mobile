79148eb013b81a54af419c21662657f89d896c98
kylanhurt <kylan.hurt@gmail.com>
2025-03-14 00:34:15 -0700

    Implement MaxBrowserTabsModal Icon

--- app/components/Views/Browser/MaxBrowserTabsModal.tsx
+++ app/components/Views/Browser/MaxBrowserTabsModal.tsx
@@ -5,10 +5,23 @@ import SheetHeader from '../../../component-library/components/Sheet/SheetHeader
 import Button, { ButtonSize, ButtonVariants, ButtonWidthTypes } from '../../../component-library/components/Buttons/Button';
 import { strings } from '../../../../locales/i18n';
 import Text from '../../../component-library/components/Texts/Text';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useTheme } from '../../../util/theme';
+import { lightTheme } from '@metamask/design-tokens';
 
-const styles = StyleSheet.create({
+const createStyles = (colors) => StyleSheet.create({
   sheet: {
     paddingHorizontal: 20
+  },
+  infoIconWrap: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 8
+  },
+  infoIcon: {
+    color: colors.primary.default,
   },
   dialogDescription: {
     marginBottom: 20,
@@ -16,6 +29,8 @@ const styles = StyleSheet.create({
 });
 
 const MaxBrowserTabsModal = () => {
+  const { colors } = useTheme();
+  const styles = createStyles(colors);
   const modalRef = useRef<BottomSheetRef>(null);
 
   const dismissModal = (): void => {
@@ -24,6 +39,13 @@ const MaxBrowserTabsModal = () => {
 
   return (
     <BottomSheet ref={modalRef}>
+      <View style={styles.infoIconWrap}>
+        <MaterialCommunityIcons
+          name="information"
+          size={28}
+          style={styles.infoIcon}
+        />
+      </View>
       <SheetHeader title={strings('browser.max_tabs_title')} />
       <View style={styles.sheet}>
         <Text style={styles.dialogDescription}>{strings('browser.max_tabs_desc')}</Text>
