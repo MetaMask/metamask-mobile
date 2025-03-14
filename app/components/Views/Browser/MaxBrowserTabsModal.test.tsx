0d98a9cca8b8c736b6823b3b07d74e42f008d9b8
kylanhurt <kylan.hurt@gmail.com>
2025-03-14 13:34:30 -0700

    Add MaxBrowserTabsModal snapshot

--- app/components/Views/Browser/MaxBrowserTabsModal.test.tsx
+++ app/components/Views/Browser/MaxBrowserTabsModal.test.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import MaxBrowserTabsModal from '.';
-import { render } from '@testing-library/react-native';
-import { ThemeContext, mockTheme } from '../../../util/theme';
+import MaxBrowserTabsModal from './MaxBrowserTabsModal';
+import renderWithProvider from '../../../util/test/renderWithProvider';
+
+jest.mock('react-native-safe-area-context', () => {
+  const inset = { top: 0, right: 0, bottom: 0, left: 0 };
+  const frame = { width: 0, height: 0, x: 0, y: 0 };
+  return {
+    SafeAreaProvider: jest.fn().mockImplementation(({ children }) => children),
+    SafeAreaConsumer: jest
+      .fn()
+      .mockImplementation(({ children }) => children(inset)),
+    useSafeAreaInsets: jest.fn().mockImplementation(() => inset),
+    useSafeAreaFrame: jest.fn().mockImplementation(() => frame),
+  };
+});
+
+jest.mock('@react-navigation/native', () => {
+  const actualReactNavigation = jest.requireActual('@react-navigation/native');
+  return {
+    ...actualReactNavigation,
+    useNavigation: () => ({
+      navigate: jest.fn(),
+      setOptions: jest.fn(),
+      goBack: jest.fn(),
+      reset: jest.fn(),
+      dangerouslyGetParent: () => ({
+        pop: jest.fn(),
+      }),
+    }),
+  };
+});
 
 describe('MaxBrowserTabsModal', () => {
-
-  it('should match snapshot when MaxBrowserTabsModal', () => {
-    const { toJSON } = render(
+  it('should render correctly', () => {
+    const { toJSON } = renderWithProvider(
       <MaxBrowserTabsModal />
     );
     expect(toJSON()).toMatchSnapshot();
