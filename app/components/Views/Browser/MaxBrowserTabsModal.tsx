c937f7fcc03706f60e0bc1b62a04718264b06102
kylanhurt <kylan.hurt@gmail.com>
2025-03-14 00:07:03 -0700

    Fix MaxBroserTabsModal transitions

--- app/components/Views/Browser/MaxBrowserTabsModal.tsx
+++ app/components/Views/Browser/MaxBrowserTabsModal.tsx
@@ -3,10 +3,9 @@ import { View, StyleSheet } from 'react-native';
 import BottomSheet, { BottomSheetRef } from '../../../component-library/components/BottomSheets/BottomSheet';
 import SheetHeader from '../../../component-library/components/Sheet/SheetHeader';
 import Button, { ButtonSize, ButtonVariants, ButtonWidthTypes } from '../../../component-library/components/Buttons/Button';
-import ReusableModal from '../../UI/ReusableModal';
 import { strings } from '../../../../locales/i18n';
 import Text from '../../../component-library/components/Texts/Text';
-import { useNavigation } from '@react-navigation/native';
+
 const styles = StyleSheet.create({
   sheet: {
     paddingHorizontal: 20
@@ -23,23 +22,20 @@ const MaxBrowserTabsModal = () => {
     modalRef.current?.onCloseBottomSheet();
   };
 
-
   return (
-    <ReusableModal>
-      <BottomSheet ref={modalRef}>
-        <SheetHeader title={strings('browser.max_tabs_title')} />
-        <View style={styles.sheet}>
-          <Text style={styles.dialogDescription}>{strings('browser.max_tabs_desc')}</Text>
-          <Button
-            variant={ButtonVariants.Primary}
-            label={strings('browser.got_it')}
-            width={ButtonWidthTypes.Full}
-            size={ButtonSize.Lg}
-            onPress={dismissModal}
-          />
-        </View>
-      </BottomSheet>
-    </ReusableModal>
+    <BottomSheet ref={modalRef}>
+      <SheetHeader title={strings('browser.max_tabs_title')} />
+      <View style={styles.sheet}>
+        <Text style={styles.dialogDescription}>{strings('browser.max_tabs_desc')}</Text>
+        <Button
+          variant={ButtonVariants.Primary}
+          label={strings('browser.got_it')}
+          width={ButtonWidthTypes.Full}
+          size={ButtonSize.Lg}
+          onPress={dismissModal}
+        />
+      </View>
+    </BottomSheet>
   )
 };
 
