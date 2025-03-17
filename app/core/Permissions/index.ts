456d7227372a4fc6fb9ad15862fec7482142bc98
Jiexi Luan <jiexiluan@gmail.com>
2025-03-17 15:49:19 -0700

    add newGetPermittedAccounts

--- app/core/Permissions/index.ts
+++ app/core/Permissions/index.ts
@@ -1,9 +1,14 @@
+import { captureException } from '@sentry/react-native';
 import { errorCodes as rpcErrorCodes } from '@metamask/rpc-errors';
 import { RestrictedMethods, CaveatTypes } from './constants';
 import ImportedEngine from '../Engine';
 import Logger from '../../util/Logger';
 import { getUniqueList } from '../../util/general';
 import TransactionTypes from '../TransactionTypes';
+import { Hex } from '@metamask/utils';
+import { InternalAccount } from '@metamask/keyring-internal-api';
+import { Caip25CaveatType, Caip25EndowmentPermissionName, getEthAccounts } from '@metamask/multichain';
+import { PermissionDoesNotExistError } from '@metamask/permission-controller';
 
 const INTERNAL_ORIGINS = [process.env.MM_FOX_CODE, TransactionTypes.MMM];
 
@@ -181,6 +186,90 @@ export const removeAccountsFromPermissions = async (addresses: string[]) => {
 };
 
 /**
+ * Checks that all accounts referenced have a matching InternalAccount. Sends
+ * an error to sentry for any accounts that were expected but are missing from the wallet.
+ *
+ * @param [internalAccounts] - The list of evm accounts the wallet knows about.
+ * @param [accounts] - The list of evm accounts addresses that should exist.
+ */
+const captureKeyringTypesWithMissingIdentities = (
+  internalAccounts: InternalAccount[] = [],
+  accounts: Hex[] = [],
+) => {
+  const accountsMissingIdentities = accounts.filter(
+    (address) =>
+      !internalAccounts.some(
+        (account) => account.address.toLowerCase() === address.toLowerCase(),
+      ),
+  );
+  const keyringTypesWithMissingIdentities = accountsMissingIdentities.map(
+    (address) => Engine.context.KeyringController.getAccountKeyringType(address),
+  );
+
+  const internalAccountCount = internalAccounts.length;
+
+  const accountTrackerCount = Object.keys(
+    Engine.context.AccountTrackerController.state.accounts || {},
+  ).length;
+
+  captureException(
+    new Error(
+      `Attempt to get permission specifications failed because their were ${accounts.length} accounts, but ${internalAccountCount} identities, and the ${keyringTypesWithMissingIdentities} keyrings included accounts with missing identities. Meanwhile, there are ${accountTrackerCount} accounts in the account tracker.`,
+    ),
+  );
+};
+
+  /**
+   * Sorts a list of evm account addresses by most recently selected by using
+   * the lastSelected value for the matching InternalAccount object stored in state.
+   *
+   * @param accounts - The list of evm accounts addresses to sort.
+   * @returns The sorted evm accounts addresses.
+   */
+const sortAccountsByLastSelected = (accounts: Hex[]) => {
+    const internalAccounts: InternalAccount[] = Engine.context.AccountsController.listAccounts();
+
+    return accounts.sort((firstAddress, secondAddress) => {
+      const firstAccount = internalAccounts.find(
+        (internalAccount) =>
+          internalAccount.address.toLowerCase() === firstAddress.toLowerCase(),
+      );
+
+      const secondAccount = internalAccounts.find(
+        (internalAccount) =>
+          internalAccount.address.toLowerCase() === secondAddress.toLowerCase(),
+      );
+
+      if (!firstAccount) {
+        captureKeyringTypesWithMissingIdentities(
+          internalAccounts,
+          accounts,
+        );
+        throw new Error(`Missing identity for address: "${firstAddress}".`);
+      } else if (!secondAccount) {
+        captureKeyringTypesWithMissingIdentities(
+          internalAccounts,
+          accounts,
+        );
+        throw new Error(`Missing identity for address: "${secondAddress}".`);
+      } else if (
+        firstAccount.metadata.lastSelected ===
+        secondAccount.metadata.lastSelected
+      ) {
+        return 0;
+      } else if (firstAccount.metadata.lastSelected === undefined) {
+        return 1;
+      } else if (secondAccount.metadata.lastSelected === undefined) {
+        return -1;
+      }
+
+      return (
+        secondAccount.metadata.lastSelected - firstAccount.metadata.lastSelected
+      );
+    });
+  };
+
+/**
  * Get permitted accounts for the given the host.
  *
  * @param hostname - Subject to check if permissions exists. Ex: A Dapp is a subject.
@@ -216,3 +305,42 @@ export const getPermittedAccounts = async (
     throw error;
   }
 };
+
+
+  /**
+   * Gets the sorted permitted accounts for the specified origin. Returns an empty
+   * array if no accounts are permitted or the wallet is locked. Returns any permitted
+   * accounts if the wallet is locked and `ignoreLock` is true. This lock bypass is needed
+   * for the `eth_requestAccounts` & `wallet_getPermission` handlers both of which
+   * return permissioned accounts to the dapp when the wallet is locked.
+   *
+   * @param {string} origin - The origin whose exposed accounts to retrieve.
+   * @param {object} [options] - The options object
+   * @param {boolean} [options.ignoreLock] - If accounts should be returned even if the wallet is locked.
+   * @returns {Promise<string[]>} The origin's permitted accounts, or an empty
+   * array.
+   */
+  const newGetPermittedAccounts = (origin: string, { ignoreLock }: { ignoreLock?: boolean } = {}) => {
+    let caveat;
+    try {
+      caveat = Engine.context.PermissionController.getCaveat(
+        origin,
+        Caip25EndowmentPermissionName,
+        Caip25CaveatType,
+      );
+    } catch (err) {
+      if (err instanceof PermissionDoesNotExistError) {
+        // suppress expected error in case that the origin
+        // does not have the target permission yet
+        return [];
+      }
+      throw err;
+    }
+
+    if (!Engine.context.KeyringController.isUnlocked() && !ignoreLock) {
+      return [];
+    }
+
+    const ethAccounts = getEthAccounts(caveat.value);
+    return sortAccountsByLastSelected(ethAccounts);
+  };
