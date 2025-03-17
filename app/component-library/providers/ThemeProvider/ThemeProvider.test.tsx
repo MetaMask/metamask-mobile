f5096e7be76fc65ccbb261474094c7c8b0099bfc
Brian Nguyen <brianacnguyen@gmail.com>
2025-03-17 10:07:25 -0700

    fix: Updated ThemeProvider to no longer has wrapping View

--- app/component-library/providers/ThemeProvider/ThemeProvider.test.tsx
+++ app/component-library/providers/ThemeProvider/ThemeProvider.test.tsx
@@ -1,37 +1,53 @@
-// Third party dependencies
 import React from 'react';
 import { render } from '@testing-library/react-native';
 import { View } from 'react-native';
-import configureStore from 'redux-mock-store';
-import { Provider } from 'react-redux';
 
 // External dependencies
-import { mockTheme } from '../../../util/theme';
-
-// Internal dependencies
+import { ThemeContext } from '../../../util/theme';
 import ThemeProvider from './ThemeProvider';
-import { THEMEPROVIDER_TESTID } from './ThemeProvider.constants';
 
-// Create a mock store
-const mockStore = configureStore([]);
-const store = mockStore({
-  user: {
-    appTheme: mockTheme.themeAppearance, // or any other relevant initial state
-  },
-});
+// Mocking useAppTheme
+jest.mock('../../../util/theme', () => ({
+  ...jest.requireActual('../../../util/theme'),
+  useAppTheme: jest.fn(() => ({
+    colors: {
+      background: {
+        alternative: 'white',
+      },
+    },
+  })),
+}));
 
 describe('ThemeProvider', () => {
-  it('should provide the correct theme to its children', () => {
+  it('renders children correctly', () => {
     const { getByTestId } = render(
-      <Provider store={store}>
-        <ThemeProvider>
-          <View />
-        </ThemeProvider>
-      </Provider>,
+      <ThemeProvider>
+        <View testID="child-component" />
+      </ThemeProvider>,
     );
 
-    expect(getByTestId(THEMEPROVIDER_TESTID).props.style.backgroundColor).toBe(
-      mockTheme.colors.background.alternative,
+    expect(getByTestId('child-component')).toBeTruthy();
+  });
+
+  it('provides the correct theme via ThemeContext', () => {
+    let themeValue;
+    const TestComponent = () => {
+      themeValue = React.useContext(ThemeContext);
+      return null;
+    };
+
+    render(
+      <ThemeProvider>
+        <TestComponent />
+      </ThemeProvider>,
     );
+
+    expect(themeValue).toStrictEqual({
+      colors: {
+        background: {
+          alternative: 'white',
+        },
+      },
+    });
   });
 });
