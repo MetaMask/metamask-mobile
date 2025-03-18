0dc3c92ca4f7ccebcd5ec038fe5ec7fc0d3c5f3e
Mathieu Artu <mathieu.artu@consensys.net>
2025-03-18 09:59:25 +0100

    feat: use new identity hooks

--- app/components/Views/OnboardingSuccess/OnboardingGeneralSettings/index.tsx
+++ app/components/Views/OnboardingSuccess/OnboardingGeneralSettings/index.tsx
@@ -10,7 +10,7 @@ import ManageNetworksComponent from '../../../UI/ManageNetworks/ManageNetworks';
 import { useStyles } from '../../../../component-library/hooks';
 import ProfileSyncingComponent from '../../../UI/ProfileSyncing/ProfileSyncing';
 import { selectIsProfileSyncingEnabled } from '../../../../selectors/identity';
-import { enableProfileSyncing } from '../../../../actions/identity';
+import { useEnableProfileSyncing } from '../../../../util/identity/hooks/useProfileSyncing';
 import { isNotificationsFeatureEnabled } from '../../../../util/notifications';
 import { RootState } from '../../../../reducers';
 import { MetaMetricsEvents, useMetrics } from '../../../hooks/useMetrics';
@@ -25,6 +25,7 @@ const GeneralSettings = () => {
     (state: RootState) => state?.settings?.basicFunctionalityEnabled,
   );
   const isProfileSyncingEnabled = useSelector(selectIsProfileSyncingEnabled);
+  const { enableProfileSyncing } = useEnableProfileSyncing();
 
   const handleSwitchToggle = () => {
     navigation.navigate(Routes.MODAL.ROOT_MODAL_FLOW, {
