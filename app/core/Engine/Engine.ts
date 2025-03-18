7cc447388a6f0e193f7d17652365c7ee9842f6d8
gantunesr <17601467+gantunesr@users.noreply.github.com>
2025-03-18 09:58:59 -0300

    chore: change derivation options to DERIVATION_OPTIONS_MINIMUM_OWASP2023

--- app/core/Engine/Engine.ts
+++ app/core/Engine/Engine.ts
@@ -67,7 +67,7 @@ import {
   LedgerMobileBridge,
   LedgerTransportMiddleware,
 } from '@metamask/eth-ledger-bridge-keyring';
-import { Encryptor, DERIVATION_OPTIONS_DEFAULT_OWASP2023, pbkdf2 } from '../Encryptor';
+import { Encryptor, DERIVATION_OPTIONS_MINIMUM_OWASP2023, pbkdf2 } from '../Encryptor';
 import {
   isMainnetByChainId,
   isTestNet,
@@ -210,7 +210,7 @@ import { TransactionControllerInit } from './controllers/transaction-controller'
 const NON_EMPTY = 'NON_EMPTY';
 
 const encryptor = new Encryptor({
-  keyDerivationOptions: DERIVATION_OPTIONS_DEFAULT_OWASP2023,
+  keyDerivationOptions: DERIVATION_OPTIONS_MINIMUM_OWASP2023,
 });
 // TODO: Replace "any" with type
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
