361ec167e2d7f6a5c30425e94ade317527480618
Vinicius Stevam <vinicius.stevam@consensys.net>
2025-03-18 14:11:10 +0000

    fix: apply suggestions

--- app/components/Views/confirmations/components/UI/InfoRow/AlertRow/AlertRow.tsx
+++ app/components/Views/confirmations/components/UI/InfoRow/AlertRow/AlertRow.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import InfoRow, { InfoRowProps } from '../InfoRow';
-import { useAlerts } from '../../../../AlertSystem/context';
-import InlineAlert from '../../../../AlertSystem/InlineAlert/InlineAlert';
 import { TextColor } from '../../../../../../../component-library/components/Texts/Text';
+import InlineAlert from '../../../../AlertSystem/InlineAlert/InlineAlert';
+import { useAlerts } from '../../../../AlertSystem/context';
 import { Severity } from '../../../../types/alerts';
+import InfoRow, { InfoRowProps } from '../InfoRow';
 
 function getAlertTextColors(
   severity?: Severity,
@@ -19,37 +19,37 @@ function getAlertTextColors(
 }
 
 export interface AlertRowProps extends InfoRowProps {
-    alertKey: string;
-    /** Determines whether to display the row only when an alert is present. */
-    isShownWithAlertsOnly?: boolean;
+  alertKey: string;
+  /** Determines whether to display the row only when an alert is present. */
+  isShownWithAlertsOnly?: boolean;
 }
 
 const AlertRow = ({ alertKey, isShownWithAlertsOnly, ...props }: AlertRowProps) => {
-    const { alerts, showAlertModal, setAlertKey } = useAlerts();
-    const alertSelected = alerts.find((a) => a.key === alertKey);
+  const { alerts, showAlertModal, setAlertKey } = useAlerts();
+  const alertSelected = alerts.find((a) => a.key === alertKey);
 
-    const handleInlineAlertClick = () => {
-        setAlertKey(alertKey);
-        showAlertModal();
-      };
+  const handleInlineAlertClick = () => {
+    setAlertKey(alertKey);
+    showAlertModal();
+  };
 
-    if (!alertSelected && isShownWithAlertsOnly) {
-        return null;
-    }
+  if (!alertSelected && isShownWithAlertsOnly) {
+    return null;
+  }
 
-    const alertRowProps = {
-      ...props,
-      variant: getAlertTextColors(alertSelected?.severity),
-    };
+  const alertRowProps = {
+    ...props,
+    variant: getAlertTextColors(alertSelected?.severity),
+  };
 
-    const inlineAlert = alertSelected ? (
-          <InlineAlert
-            onClick={handleInlineAlertClick}
-            severity={alertSelected.severity}
-          />
-      ) : null;
+  const inlineAlert = alertSelected ? (
+    <InlineAlert
+      onClick={handleInlineAlertClick}
+      severity={alertSelected.severity}
+    />
+  ) : null;
 
-    return <InfoRow {...alertRowProps} labelChildren={inlineAlert} />;
+  return <InfoRow {...alertRowProps} labelChildren={inlineAlert} />;
 };
 
 export default AlertRow;
