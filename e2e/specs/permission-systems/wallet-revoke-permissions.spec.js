a94b1290175f530b2dd8fa7a3d695a5198d2624a
ffmcgee <joao.carlos@consensys.net>
2025-03-19 21:42:38 +0100

    we made progress, now figure out whats here and he clean this up

--- e2e/specs/permission-systems/wallet-revoke-permissions.spec.js
+++ e2e/specs/permission-systems/wallet-revoke-permissions.spec.js
@@ -46,7 +46,9 @@ describe(SmokePermissions('Wallet Revoke Permissions'), () => {
         const el0 = await Matchers.getElementByID(
           TestDappSelectorsWebIDs.PERMISSIONS_RESULT,
         );
-
+        const what = await el0.getAttributes();
+        // eslint-disable-next-line no-console
+        console.log(what.text, 'text of el0');
         // eslint-disable-next-line no-console
         console.log({ el0 });
         // await Assertions.checkIfTextIsDisplayed(
@@ -63,7 +65,9 @@ describe(SmokePermissions('Wallet Revoke Permissions'), () => {
 
         // eslint-disable-next-line no-console
         console.log({ el1 });
-
+        const what1 = await el0.getAttributes();
+        // eslint-disable-next-line no-console
+        console.log(what1.text, 'text of el1');
         // await Assertions.checkIfTextIsDisplayed('Permissions result:');
 
         await TestDApp.tapGetPermissionsButton();
@@ -74,13 +78,20 @@ describe(SmokePermissions('Wallet Revoke Permissions'), () => {
           TestDappSelectorsWebIDs.PERMISSIONS_RESULT,
         );
 
+        // eslint-disable-next-line no-console
+        console.log({ el2 });
+        const what2 = await el2.getAttributes();
+        // eslint-disable-next-line no-console
+        console.log(what2.text, 'text of el2');
         const elementId = Matchers.getElementByID(
           TestDappSelectorsWebIDs.PERMISSIONS_RESULT,
         );
 
         // eslint-disable-next-line no-console
         console.log({ el2, elementId: await elementId });
-        expect(true).toBeTruthy();
+        const elIdTetx = await (await elementId).getAttributes();
+        // eslint-disable-next-line no-console
+        console.log(elIdTetx.text, 'elementId text HERE!');
         await Assertions.checkIfElementToHaveText(
           elementId,
           'No permissions found.',
