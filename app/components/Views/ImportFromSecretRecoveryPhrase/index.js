fe3d2052a4d4981bf8b555432b8c51ce5ee266d3
Andre Pimenta <andrepimenta7@gmail.com>
2025-03-19 18:08:53 +0000

    Revert "test TextInput"
    This reverts commit 0e88715676ffff1affb627cbd61942b8600e67e5.

--- app/components/Views/ImportFromSecretRecoveryPhrase/index.js
+++ app/components/Views/ImportFromSecretRecoveryPhrase/index.js
@@ -377,7 +377,7 @@ const ImportFromSecretRecoveryPhrase = ({
 
   const hiddenSRPInput = useCallback(
     () => (
-      <TextInput
+      <OutlinedTextField
         style={styles.input}
         containerStyle={inputWidth}
         inputContainerStyle={styles.padding}
