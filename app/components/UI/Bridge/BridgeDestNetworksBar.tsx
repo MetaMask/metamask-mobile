099e514cb82381d551bca6e4c8233d679e1606c3
IF <139582705+infiniteflower@users.noreply.github.com>
2025-03-19 17:49:27 -0400

    chore: show network icon on selected dest chain and make blue

--- app/components/UI/Bridge/BridgeDestNetworksBar.tsx
+++ app/components/UI/Bridge/BridgeDestNetworksBar.tsx
@@ -6,18 +6,25 @@ import Button, { ButtonVariants } from '../../../component-library/components/Bu
 import { strings } from '../../../../locales/i18n';
 import { useStyles } from '../../../component-library/hooks';
 import { Theme } from '../../../util/theme/models';
-import { StyleSheet, ScrollView } from 'react-native';
+import { StyleSheet, ScrollView, Image } from 'react-native';
 import { IconName } from '../../../component-library/components/Icons/Icon';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectEnabledDestChains, setSelectedDestChainId } from '../../../core/redux/slices/bridge';
+import { selectEnabledDestChains, selectSelectedDestChainId, setSelectedDestChainId } from '../../../core/redux/slices/bridge';
 import { ETH_CHAIN_ID, BASE_CHAIN_ID } from '@metamask/swaps-controller/dist/constants';
 import { Hex } from '@metamask/utils';
+import { Box } from '../Box/Box';
+import { getNetworkImageSource } from '../../../util/networks';
+import { AlignItems, FlexDirection } from '../Box/box.types';
 
 const createStyles = (params: { theme: Theme }) => {
   const { theme } = params;
   return StyleSheet.create({
     networksButton: {
       borderColor: theme.colors.border.muted,
+    },
+    selectedNetworkIcon: {
+      borderColor: theme.colors.primary.muted,
+      backgroundColor: theme.colors.primary.muted,
     },
     scrollView: {
       flexGrow: 0,
@@ -28,6 +35,11 @@ const createStyles = (params: { theme: Theme }) => {
       gap: 4,
       paddingHorizontal: 4,
     },
+    networkIcon: {
+      width: 24,
+      height: 24,
+      borderRadius: 12,
+    },
   });
 };
 
@@ -37,11 +49,16 @@ const ChainPopularity: Record<Hex, number> = {
   [BASE_CHAIN_ID]: 3,
 };
 
+const ShortChainNames: Record<Hex, string> = {
+  [ETH_CHAIN_ID]: 'Ethereum',
+};
+
 export const BridgeDestNetworksBar = () => {
   const navigation = useNavigation();
   const dispatch = useDispatch();
   const { styles } = useStyles(createStyles, {});
   const enabledDestChains = useSelector(selectEnabledDestChains);
+  const selectedDestChainId = useSelector(selectSelectedDestChainId);
 
   const sortedDestChains = enabledDestChains.sort((a, b) => {
     const aPopularity = ChainPopularity[a.chainId] ?? Infinity;
@@ -73,15 +90,25 @@ export const BridgeDestNetworksBar = () => {
         style={styles.networksButton}
         endIconName={IconName.ArrowDown}
       />
-      {sortedDestChains.map((chain) => (
-        <Button
-          key={chain.chainId}
-          variant={ButtonVariants.Secondary}
-          label={<Text>{chain.name}</Text>}
-          style={styles.networksButton}
-          onPress={() => handleSelectNetwork(chain.chainId)}
-        />
-      ))}
+      {sortedDestChains.map((chain) => {
+        // @ts-expect-error - The utils/network file is still JS and this function expects a networkType, and should be optional
+        const networkImage = getNetworkImageSource({ chainId: chain.chainId});
+
+        return (
+          <Button
+            key={chain.chainId}
+            variant={ButtonVariants.Secondary}
+            label={
+              <Box flexDirection={FlexDirection.Row} alignItems={AlignItems.center} gap={4}>
+                {selectedDestChainId === chain.chainId ? <Image source={networkImage} style={styles.networkIcon} /> : null}
+                <Text>{ShortChainNames[chain.chainId] ?? chain.name}</Text>
+              </Box>
+            }
+            style={selectedDestChainId === chain.chainId ? styles.selectedNetworkIcon : styles.networksButton}
+            onPress={() => handleSelectNetwork(chain.chainId)}
+          />
+        );
+      })}
     </ScrollView>
   );
 };
