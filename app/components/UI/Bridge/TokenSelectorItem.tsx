4aecf7d78b4d3a610ae395a066bb0fa514005d29
IF <139582705+infiniteflower@users.noreply.github.com>
2025-03-20 17:56:49 -0400

    feat: show token details when info icon pressed

--- app/components/UI/Bridge/TokenSelectorItem.tsx
+++ app/components/UI/Bridge/TokenSelectorItem.tsx
@@ -15,9 +15,11 @@ import Text, {
 } from '../../../component-library/components/Texts/Text';
 import TokenIcon from '../Swaps/components/TokenIcon';
 import { Box } from '../Box/Box';
-import { FlexDirection } from '../Box/box.types';
+import { AlignItems, FlexDirection, JustifyContent } from '../Box/box.types';
 import { TokenIWithFiatAmount } from './useSourceTokens';
-
+import ButtonIcon, { ButtonIconSizes } from '../../../component-library/components/Buttons/ButtonIcon';
+import { IconColor, IconName } from '../../../component-library/components/Icons/Icon';
+import { useNavigation } from '@react-navigation/native';
 const createStyles = () =>
   StyleSheet.create({
     tokenIcon: {
@@ -27,6 +29,9 @@ const createStyles = () =>
     tokenInfo: {
       flex: 1,
       marginLeft: 8,
+    },
+    infoButton: {
+      marginRight: 12,
     },
   });
 
@@ -46,55 +51,75 @@ export const TokenSelectorItem: React.FC<TokenSelectorItemProps> = ({
   shouldShowBalance = true,
 }) => {
   const styles = createStyles();
-
+  const navigation = useNavigation();
   const fiatValue = token.balanceFiat;
   const balanceWithSymbol = `${token.balance} ${token.symbol}`;
 
+  // Open the asset details screen as a bottom sheet
+  const handleInfoButtonPress = () => {
+     navigation.navigate('Asset', {
+      ...token,
+    });
+  };
+
   return (
-    <AssetElement
-      key={token.address}
-      asset={token}
-      onPress={() => onPress(token)}
-      balance={shouldShowBalance ? fiatValue : undefined}
-      secondaryBalance={shouldShowBalance ? balanceWithSymbol : undefined}
+    <Box
+      flexDirection={FlexDirection.Row}
+      justifyContent={JustifyContent.spaceBetween}
+      alignItems={AlignItems.center}
     >
-      <BadgeWrapper
-        badgePosition={BadgePosition.BottomRight}
-        badgeElement={
-          <Badge
-            variant={BadgeVariant.Network}
-            name={networkName}
-            imageSource={networkImageSource}
-          />
-        }
+      <AssetElement
+        key={token.address}
+        asset={token}
+        onPress={() => onPress(token)}
+        balance={shouldShowBalance ? fiatValue : undefined}
+        secondaryBalance={shouldShowBalance ? balanceWithSymbol : undefined}
       >
-        {token.isNative ? (
-          <TokenIcon
-            symbol={token.symbol}
-            icon={token.image}
-            style={styles.tokenIcon}
-            big={false}
-            biggest={false}
-            testID={`network-logo-${token.symbol}`}
-          />
-        ) : (
-          <AvatarToken
-            name={token.symbol}
-            imageSource={token.image ? { uri: token.image } : undefined}
-            size={AvatarSize.Md}
-          />
-        )}
-      </BadgeWrapper>
-      <Box
-        style={styles.tokenInfo}
-        flexDirection={FlexDirection.Column}
-        gap={4}
-      >
-        <Text variant={TextVariant.BodyLGMedium}>{token.symbol}</Text>
-        <Text variant={TextVariant.BodyMD} color={TextColor.Alternative}>
-          {token.name}
-        </Text>
-      </Box>
-    </AssetElement>
+        <BadgeWrapper
+          badgePosition={BadgePosition.BottomRight}
+          badgeElement={
+            <Badge
+              variant={BadgeVariant.Network}
+              name={networkName}
+              imageSource={networkImageSource}
+            />
+          }
+        >
+          {token.isNative ? (
+            <TokenIcon
+              symbol={token.symbol}
+              icon={token.image}
+              style={styles.tokenIcon}
+              big={false}
+              biggest={false}
+              testID={`network-logo-${token.symbol}`}
+            />
+          ) : (
+            <AvatarToken
+              name={token.symbol}
+              imageSource={token.image ? { uri: token.image } : undefined}
+              size={AvatarSize.Md}
+            />
+          )}
+        </BadgeWrapper>
+        <Box style={styles.tokenInfo} flexDirection={FlexDirection.Column} gap={4}>
+          <Text variant={TextVariant.BodyLGMedium}>
+            {token.symbol}
+          </Text>
+          <Text variant={TextVariant.BodyMD} color={TextColor.Alternative}>
+            {token.name}
+          </Text>
+        </Box>
+      </AssetElement>
+      {!shouldShowBalance && (
+        <ButtonIcon
+          iconName={IconName.Info}
+          size={ButtonIconSizes.Md}
+          onPress={handleInfoButtonPress}
+          iconColor={IconColor.Alternative}
+          style={styles.infoButton}
+        />
+      )}
+    </Box>
   );
 };
