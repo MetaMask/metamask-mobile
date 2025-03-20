ae9fbdbff8f4a4cca58f4267ddda5fe3aebed91e
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-03-20 08:56:48 +0100

    Fix lint

--- app/components/Views/confirmations/components/UI/InfoSectionAccordion/InfoSectionAccordion.styles.ts
+++ app/components/Views/confirmations/components/UI/InfoSectionAccordion/InfoSectionAccordion.styles.ts
@@ -1,6 +1,5 @@
 import { StyleSheet } from 'react-native';
 import { Theme } from '../../../../../../util/theme/models';
-import { fontStyles } from '../../../../../../styles/common';
 
 const styleSheet = (params: { theme: Theme }) => {
   const { theme } = params;
