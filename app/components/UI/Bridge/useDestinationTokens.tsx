2b219f8df504d2d9a1c0c7be59c340c92cb671ae
IF <139582705+infiniteflower@users.noreply.github.com>
2025-03-24 15:11:16 -0400

    chore: filter out opposing token from token lists

--- app/components/UI/Bridge/useDestinationTokens.tsx
+++ app/components/UI/Bridge/useDestinationTokens.tsx
@@ -1,12 +1,15 @@
 import { useTokensWithBalance, TokenIWithFiatAmount } from './useTokensWithBalance';
 import { useSelector } from 'react-redux';
-import { selectSelectedDestChainId } from '../../../core/redux/slices/bridge';
+import { selectSelectedDestChainId, selectSourceToken } from '../../../core/redux/slices/bridge';
 import { Hex } from '@metamask/utils';
 
 // TODO get top tokens from BridgeController
 export const useDestinationTokens: () => TokenIWithFiatAmount[] = () => {
   const selectedDestChainId = useSelector(selectSelectedDestChainId);
+  const selectedSourceToken = useSelector(selectSourceToken);
+
   const tokensWithBalance = useTokensWithBalance({ chainIds: [selectedDestChainId as Hex] });
 
-  return tokensWithBalance;
+  return tokensWithBalance.filter((token) => !(token.address === selectedSourceToken?.address
+    && token.chainId === selectedSourceToken?.chainId));
 };
--- app/components/UI/Bridge/useSourceTokens.ts
+++ app/components/UI/Bridge/useSourceTokens.ts
@@ -1,12 +1,15 @@
 import { TokenIWithFiatAmount, useTokensWithBalance } from './useTokensWithBalance';
 import { useSelector } from 'react-redux';
-import { selectSelectedSourceChainIds } from '../../../core/redux/slices/bridge';
+import { selectSelectedSourceChainIds, selectDestToken } from '../../../core/redux/slices/bridge';
 import { Hex } from '@metamask/utils';
 
 // TODO get top tokens from BridgeController
 export const useSourceTokens: () => TokenIWithFiatAmount[] = () => {
   const selectedSourceChainIds = useSelector(selectSelectedSourceChainIds);
+  const selectedDestToken = useSelector(selectDestToken);
+
   const tokensWithBalance = useTokensWithBalance({ chainIds: selectedSourceChainIds as Hex[] });
 
-  return tokensWithBalance;
+  return tokensWithBalance.filter((token) => !(token.address === selectedDestToken?.address
+    && token.chainId === selectedDestToken?.chainId));
 };
