9aa71c91b220c48fbe8e3088ab97dd188af6189f
António Regadas <antonio.regadas@consensys.net>
2025-03-25 11:25:43 +0000

    chore: adds the multichain transaction view (#14029)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    This PR is part 2 of the work, more to come, it adds the the support for
    the multichain transactions in the activity list, the details modal for
    them and block explorer support for txs as well.
    
    ![Screenshot 2025-03-19 at 17 05
    43](https://github.com/user-attachments/assets/60c5edbb-42c5-4b99-9d42-e7f00660c9c5)
    ![Screenshot 2025-03-19 at 17 05
    53](https://github.com/user-attachments/assets/4ce6f6e8-f2af-485f-9afa-48f5ba843af0)
    
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/util/networks/index.js
+++ app/util/networks/index.js
@@ -14,6 +14,7 @@ import { ChainId, NetworkType, toHex } from '@metamask/controller-utils';
 import { toLowerCaseEquals } from '../general';
 import { fastSplit } from '../number';
 import { regex } from '../../../app/util/regex';
+import { MULTICHAIN_NETWORK_BLOCK_EXPLORER_FORMAT_URLS_MAP } from '../../../app/core/Multichain/constants';
 
 /* eslint-disable */
 const ethLogo = require('../../images/eth-logo-new.png');
@@ -48,7 +49,6 @@ import {
   selectSelectedNonEvmNetworkChainId,
   selectMultichainNetworkControllerState,
 } from '../../selectors/multichainNetworkController';
-import { MULTICHAIN_NETWORK_BLOCK_EXPLORER_FORMAT_URLS_MAP } from '../../core/Multichain/constants';
 import { formatBlockExplorerAddressUrl } from '../../core/Multichain/networks';
 /**
  * List of the supported networks
@@ -309,6 +309,18 @@ export function findBlockExplorerForRpc(rpcTargetUrl, networkConfigurations) {
 
 /**
  * Returns block explorer for non-evm chain id
+ *
+ * @param {object} internalAccount - Internal account object
+ * @returns {string} - Block explorer url or undefined if not found
+ */
+export function findBlockExplorerForNonEvmChainId(chainId) {
+  const blockExplorerUrls =
+    MULTICHAIN_NETWORK_BLOCK_EXPLORER_FORMAT_URLS_MAP[chainId];
+  return blockExplorerUrls?.url;
+}
+
+/**
+ * Returns block explorer for non-evm account
  *
  * @param {object} internalAccount - Internal account object
  * @returns {string} - Block explorer url or undefined if not found
