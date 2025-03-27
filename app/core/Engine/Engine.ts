c9f3211ee2dd2342d80d839cf51583a034be4b34
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2025-03-27 14:23:38 +0000

    feat: upgrade notifications controller to 5.0.0

--- app/core/Engine/Engine.ts
+++ app/core/Engine/Engine.ts
@@ -95,10 +95,8 @@ import { MetaMetricsEvents, MetaMetrics } from '../Analytics';
 ///: BEGIN:ONLY_INCLUDE_IF(preinstalled-snaps,external-snaps)
 import { ExcludedSnapEndowments, ExcludedSnapPermissions } from '../Snaps';
 import { calculateScryptKey } from './controllers/identity/calculate-scrypt-key';
-import { getNotificationServicesControllerMessenger } from './messengers/notifications/notification-services-controller-messenger';
-import { createNotificationServicesController } from './controllers/notifications/create-notification-services-controller';
-import { getNotificationServicesPushControllerMessenger } from './messengers/notifications/notification-services-push-controller-messenger';
-import { createNotificationServicesPushController } from './controllers/notifications/create-notification-services-push-controller';
+import { notificationServicesControllerInit } from './controllers/notifications/notification-services-controller-init';
+import { notificationServicesPushControllerInit } from './controllers/notifications/notification-services-push-controller-init';
 
 import { getAuthenticationControllerMessenger } from './messengers/identity/authentication-controller-messenger';
 import { createAuthenticationController } from './controllers/identity/create-authentication-controller';
@@ -891,23 +889,6 @@ export class Engine {
         isAccountSyncingEnabled: false,
       },
     });
-
-    const notificationServicesControllerMessenger =
-      getNotificationServicesControllerMessenger(this.controllerMessenger);
-    const notificationServicesController = createNotificationServicesController(
-      {
-        messenger: notificationServicesControllerMessenger,
-        initialState: initialState.NotificationServicesController,
-      },
-    );
-
-    const notificationServicesPushControllerMessenger =
-      getNotificationServicesPushControllerMessenger(this.controllerMessenger);
-    const notificationServicesPushController =
-      createNotificationServicesPushController({
-        messenger: notificationServicesPushControllerMessenger,
-        initialState: initialState.NotificationServicesPushController,
-      });
     ///: END:ONLY_INCLUDE_IF
 
     const codefiTokenApiV2 = new CodefiTokenPricesServiceV2();
@@ -1039,6 +1020,9 @@ export class Engine {
         CronjobController: cronjobControllerInit,
         SnapInterfaceController: snapInterfaceControllerInit,
         SnapsRegistry: snapsRegistryInit,
+        NotificationServicesController: notificationServicesControllerInit,
+        NotificationServicesPushController:
+          notificationServicesPushControllerInit,
         ///: END:ONLY_INCLUDE_IF
         ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
         MultichainAssetsController: multichainAssetsControllerInit,
@@ -1070,6 +1054,10 @@ export class Engine {
     const snapController = controllersByName.SnapController;
     const snapInterfaceController = controllersByName.SnapInterfaceController;
     const snapsRegistry = controllersByName.SnapsRegistry;
+    const notificationServicesController =
+      controllersByName.NotificationServicesController;
+    const notificationServicesPushController =
+      controllersByName.NotificationServicesPushController;
     ///: END:ONLY_INCLUDE_IF
 
     ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
@@ -1101,6 +1089,11 @@ export class Engine {
       multichainRatesControllerMessenger,
       multichainRatesController,
     );
+    ///: END:ONLY_INCLUDE_IF
+
+    ///: BEGIN:ONLY_INCLUDE_IF(preinstalled-snaps,external-snaps)
+    // Notification Setup
+    notificationServicesController.init();
     ///: END:ONLY_INCLUDE_IF
 
     const nftController = new NftController({
