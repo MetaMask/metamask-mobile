d02f3fc5da761cc2d43a110877a789c6d165b72f
vrc <vrivera@peersyst.com>
2025-03-28 12:37:30 +0100

    Add XRPLEVM Testnet to metamask mobile

--- app/images/image-icons.js
+++ app/images/image-icons.js
@@ -18,6 +18,7 @@ import KAIA_MAINNET from './kaia.png';
 import FOX_LOGO from '../../app/images/branding/tiny-logo.png';
 import BTC from './bitcoin-logo.png';
 import BASE from './base.png';
+import XRPLEVM_TESTNET from './xrplevm.png';
 
 export default {
   PALM,
@@ -42,4 +43,5 @@ export default {
   FOX_LOGO,
   BTC,
   BASE,
+  'XRPLEVM-TESTNET': XRPLEVM_TESTNET,
 };
