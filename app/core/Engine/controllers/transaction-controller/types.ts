7ee4e551bfd91426ae5a6c541c7b6d97dcd49f39
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-03-28 11:53:53 +0100

    fix: Remove `Transaction Finalized` event tied to STX (#14293)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    We recently added `TransactionController` event handlers by having
    flexibility of "modular initialisation".
    This means that we can get rid of existing `Transaction Finalized`
    events living in the Engine file.
    
    All STX metric properties will be added as expected as before. 
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/MetaMask-planning/issues/4520
    
    ## **Manual testing steps**
    
    No manual testing required - user flows are not affected.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [X] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [X] I've completed the PR template to the best of my ability
    - [X] I’ve included tests if applicable
    - [X] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [X] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/core/Engine/controllers/transaction-controller/types.ts
+++ app/core/Engine/controllers/transaction-controller/types.ts
@@ -1,4 +1,7 @@
 import { JsonMap } from '../../../Analytics/MetaMetrics.types';
+import SmartTransactionsController from '@metamask/smart-transactions-controller';
+import type { RootState } from '../../../../reducers';
+import { TransactionControllerInitMessenger } from '../../messengers/transaction-controller-messenger';
 
 // In order to not import from redux slice, type is defined here
 export interface TransactionMetrics {
@@ -6,6 +9,8 @@ export interface TransactionMetrics {
   sensitiveProperties: JsonMap;
 }
 
-export interface TransactionMetricRequest {
-  getTransactionMetricProperties: (id: string) => TransactionMetrics;
+export interface TransactionEventHandlerRequest {
+  getState: () => RootState;
+  initMessenger: TransactionControllerInitMessenger;
+  smartTransactionsController: SmartTransactionsController;
 }
