79951b7fd438f425c9e482a13371a4701a544119
Ivan Canete <ivan.canete@consensys.net>
2025-03-31 20:39:01 -0300

    add testID and selectors for token search input

--- e2e/pages/Ramps/TokenSelectBottomSheet.js
+++ e2e/pages/Ramps/TokenSelectBottomSheet.js
@@ -1,10 +1,14 @@
 import Matchers from '../../utils/Matchers';
 import Gestures from '../../utils/Gestures';
+import { selectTokenSelectors } from '../../selectors/Ramps/SelectToken.selectors';
 
 class TokenSelectBottomSheet {
+  get tokenSearchInput() {
+    return Matchers.getElementByID(selectTokenSelectors.TOKEN_SELECT_MODAL_SEARCH_INPUT);
+  }
+
   async tapTokenByName(token) {
-    const tokenSearchInput = await Matchers.getElementByLabel(" Search by cryptocurrency");
-    await Gestures.typeTextAndHideKeyboard(tokenSearchInput, token);
+    await Gestures.typeTextAndHideKeyboard(this.tokenSearchInput, token);
     const tokenName = await Matchers.getElementByText(token, 1);
     await Gestures.waitAndTap(tokenName);
   }
