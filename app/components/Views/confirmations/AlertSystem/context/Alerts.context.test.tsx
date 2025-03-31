fccf2aec9ede900e2b70f69755a4b2750587029a
Vinicius Stevam <vinicius.stevam@consensys.net>
2025-03-31 11:47:49 +0100

    fix unit tests

--- app/components/Views/confirmations/AlertSystem/context/Alerts.context.test.tsx
+++ app/components/Views/confirmations/AlertSystem/context/Alerts.context.test.tsx
@@ -13,12 +13,6 @@ jest.mock('../../../../hooks/useAlertsConfirmed', () => ({
   useAlertsConfirmed: jest.fn(),
 }));
 
-jest.mock('../../hooks/useConfirmationAlertMetric', () => ({
-  useConfirmationAlertMetric: jest.fn(() => ({
-    trackAlertRendered: jest.fn(),
-  })),
-}));
-
 describe('AlertsContext', () => {
   const dangerAlertMock: Alert = {
     key: 'alert1',
