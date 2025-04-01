ce796816ceef7de26a52034b0dcc2871132a2e01
digiwand <20778143+digiwand@users.noreply.github.com>
2025-04-01 18:01:32 -0400

    style: reorder utils/signature.ts

--- app/components/Views/confirmations/utils/signature.ts
+++ app/components/Views/confirmations/utils/signature.ts
@@ -15,50 +15,6 @@ import { sanitizeMessage } from '../../../../util/string';
 import { TOKEN_ADDRESS } from '../constants/tokens';
 import BigNumber from 'bignumber.js';
 
-/**
- * The contents of this file have been taken verbatim from
- * metamask-extension/shared/modules/transaction.utils.ts
- *
- * If updating, please be mindful of this or delete this comment.
- */
-
-const REGEX_MESSAGE_VALUE_LARGE =
-  /"message"\s*:\s*\{[^}]*"value"\s*:\s*(\d{15,})/u;
-
-function extractLargeMessageValue(dataToParse: string): string | undefined {
-  if (typeof dataToParse !== 'string') {
-    return undefined;
-  }
-  return dataToParse.match(REGEX_MESSAGE_VALUE_LARGE)?.[1];
-}
-
-/**
- * JSON.parse has a limitation which coerces values to scientific notation if numbers are greater than
- * Number.MAX_SAFE_INTEGER. This can cause a loss in precision.
- *
- * Aside from precision concerns, if the value returned was a large number greater than 15 digits,
- * e.g. 3.000123123123121e+26, passing the value to BigNumber will throw the error:
- * Error: new BigNumber() number type has more than 15 significant digits
- *
- * Note that using JSON.parse reviver cannot help since the value will be coerced by the time it
- * reaches the reviver function.
- *
- * This function has a workaround to extract the large value from the message and replace
- * the message value with the string value.
- *
- * @param dataToParse
- * @returns
- */
-export const parseTypedDataMessage = (dataToParse: string) => {
-  const result = JSON.parse(dataToParse);
-
-  const messageValue = extractLargeMessageValue(dataToParse);
-  if (result.message?.value) {
-    result.message.value = messageValue || String(result.message.value);
-  }
-  return result;
-};
-
 interface TypedSignatureRequest {
   messageParams: MessageParamsTyped;
   type: SignatureRequestType.TypedSign;
@@ -108,6 +64,55 @@ export const isTypedSignV3V4Request = (signatureRequest?: SignatureRequest) => {
   );
 };
 
+const REGEX_MESSAGE_VALUE_LARGE =
+  /"message"\s*:\s*\{[^}]*"value"\s*:\s*(\d{15,})/u;
+
+function extractLargeMessageValue(dataToParse: string): string | undefined {
+  if (typeof dataToParse !== 'string') {
+    return undefined;
+  }
+  return dataToParse.match(REGEX_MESSAGE_VALUE_LARGE)?.[1];
+}
+
+/**
+ * JSON.parse has a limitation which coerces values to scientific notation if numbers are greater than
+ * Number.MAX_SAFE_INTEGER. This can cause a loss in precision.
+ *
+ * Aside from precision concerns, if the value returned was a large number greater than 15 digits,
+ * e.g. 3.000123123123121e+26, passing the value to BigNumber will throw the error:
+ * Error: new BigNumber() number type has more than 15 significant digits
+ *
+ * Note that using JSON.parse reviver cannot help since the value will be coerced by the time it
+ * reaches the reviver function.
+ *
+ * This function has a workaround to extract the large value from the message and replace
+ * the message value with the string value.
+ *
+ * @param dataToParse
+ * @returns
+ */
+export const parseTypedDataMessage = (dataToParse: string) => {
+  const result = JSON.parse(dataToParse);
+
+  const messageValue = extractLargeMessageValue(dataToParse);
+  if (result.message?.value) {
+    result.message.value = messageValue || String(result.message.value);
+  }
+  return result;
+};
+
+export const parseSanitizeTypedDataMessage = (dataToParse: string) => {
+  if (!dataToParse) {
+    return {};
+  }
+
+  const { domain, message, primaryType, types } =
+    parseTypedDataMessage(dataToParse);
+
+  const sanitizedMessage = sanitizeMessage(message, primaryType, types);
+  return { sanitizedMessage, primaryType, domain };
+};
+
 export const parseTypedDataMessageFromSignatureRequest = (
   signatureRequest?: SignatureRequest,
 ) => {
@@ -143,18 +148,6 @@ export const isRecognizedPermit = (request?: SignatureRequest) =>
  */
 export const isRecognizedOrder = (request?: SignatureRequest) =>
   isRecognizedOfType(request, PRIMARY_TYPES_ORDER);
-
-export const parseSanitizeTypedDataMessage = (dataToParse: string) => {
-  if (!dataToParse) {
-    return {};
-  }
-
-  const { domain, message, primaryType, types } =
-    parseTypedDataMessage(dataToParse);
-
-  const sanitizedMessage = sanitizeMessage(message, primaryType, types);
-  return { sanitizedMessage, primaryType, domain };
-};
 
 export interface SIWEMessage {
   address: string;
