449382b6ba37ff3a65c275b150b8572a7d5d2950
George Weiler <george.weiler@consensys.net>
2025-04-01 10:09:26 -0600

    chore: reverts ramp sdk development change

--- app/components/UI/Ramp/sdk/index.tsx
+++ app/components/UI/Ramp/sdk/index.tsx
@@ -47,8 +47,7 @@ let environment = Environment.Production;
 if (isInternalBuild) {
   environment = Environment.Staging;
 } else if (isDevelopment) {
-  environment = Environment.Staging;
-  // environment = Environment.Development;
+  environment = Environment.Development;
 }
 
 let context = Context.Mobile;
