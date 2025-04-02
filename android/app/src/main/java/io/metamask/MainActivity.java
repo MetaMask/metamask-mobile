064d5f83288472f73c2c913b33a49faccb63f55e
metamaskbot <metamaskbot@users.noreply.github.com>
2025-04-01 21:19:44 -0600

    Disable RN's dev loading view since expo is enabled add import

--- android/app/src/main/java/io/metamask/MainActivity.java
+++ android/app/src/main/java/io/metamask/MainActivity.java
@@ -5,6 +5,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import com.facebook.react.devsupport.DevSupportManagerBase;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.util.Log;
