f70430ac65fc4ef7be789a66a1558c57c5837774
Owen Craston <owengc12@gmail.com>
2025-04-02 15:04:44 -0700

    fix AccountNetworkInfoExpanded.test.tsx

--- app/components/Views/confirmations/components/Confirm/AccountNetworkInfo/AccountNetworkInfoExpanded/AccountNetworkInfoExpanded.test.tsx
+++ app/components/Views/confirmations/components/Confirm/AccountNetworkInfo/AccountNetworkInfoExpanded/AccountNetworkInfoExpanded.test.tsx
@@ -3,6 +3,7 @@ import renderWithProvider from '../../../../../../../util/test/renderWithProvide
 import { personalSignatureConfirmationState } from '../../../../../../../util/test/confirm-data-helpers';
 import AccountNetworkInfoExpanded from './AccountNetworkInfoExpanded';
 import { isPortfolioViewEnabled } from '../../../../../../../util/networks';
+import { useMultichainBalances } from '../../../../../../../components/hooks/useMultichainBalances';
 
 jest.mock('../../../../../../../util/networks', () => ({
   ...jest.requireActual('../../../../../../../util/networks'),
@@ -10,15 +11,48 @@ jest.mock('../../../../../../../util/networks', () => ({
 }));
 
 jest.mock('../../../../../../../core/Engine', () => ({
-  getTotalEvmFiatAccountBalance: () => ({ tokenFiat: 10 }),
+  getTotalEvmFiatAccountBalance: jest.fn().mockReturnValue({
+    totalNativeTokenBalance: { amount: '0', unit: 'ETH' },
+    totalBalanceFiat: 0,
+    balances: {
+      '0x0': { amount: '0', unit: 'ETH' },
+    },
+  }),
 }));
+
+jest.mock(
+  '../../../../../../../components/hooks/useMultichainBalances',
+  () => ({
+    useMultichainBalances: jest.fn(),
+  }),
+);
 
 describe('AccountNetworkInfoExpanded', () => {
   const mockIsPortfolioViewEnabled = jest.mocked(isPortfolioViewEnabled);
+  const mockUseMultichainBalances = jest.mocked(useMultichainBalances);
 
   beforeEach(() => {
     jest.clearAllMocks();
     mockIsPortfolioViewEnabled.mockReturnValue(false);
+    mockUseMultichainBalances.mockReturnValue({
+      selectedAccountMultichainBalance: {
+        displayBalance: '$0.00',
+        displayCurrency: 'USD',
+        totalFiatBalance: 0,
+        totalNativeTokenBalance: '0',
+        nativeTokenUnit: 'ETH',
+        tokenFiatBalancesCrossChains: [],
+        shouldShowAggregatedPercentage: false,
+        isPortfolioVieEnabled: true,
+        aggregatedBalance: {
+          ethFiat: 0,
+          tokenFiat: 0,
+          tokenFiat1dAgo: 0,
+          ethFiat1dAgo: 0,
+        },
+      },
+      multichainBalancesForAllAccounts: {},
+    });
   });
 
   it('should match snapshot when isPortfolioViewEnabled is true', () => {
