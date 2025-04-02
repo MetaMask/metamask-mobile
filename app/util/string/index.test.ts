8917e45ac29fa68fb45b2c0ea65188b9a36c8abd
digiwand <20778143+digiwand@users.noreply.github.com>
2025-04-02 12:54:51 -0400

    test: add stripArrayType, stripOneLayerofNesting, isArrayType, and isSolidityType tests

--- app/util/string/index.test.ts
+++ app/util/string/index.test.ts
@@ -1,6 +1,10 @@
 import {
   escapeSpecialUnicode,
+  isArrayType,
+  isSolidityType,
+  stripArrayType,
   stripMultipleNewlines,
+  stripOneLayerofNesting,
 } from '.';
 
 describe('string utils', () => {
@@ -13,6 +17,70 @@ describe('string utils', () => {
     it('escapes all occurences of \u202D and \u202E', () => {
       const result = escapeSpecialUnicode('test \u202D test \u202E test \u202D test');
       expect(result).toEqual('test \\u202D test \\u202E test \\u202D test');
+    });
+  });
+
+  describe('isArrayType', () => {
+    [
+      ['uint256[]', true],
+      ['address[5]', true],
+      ['string[][]', true],
+      ['bytes32[1][2]', true],
+      ['uint256', false],
+      ['address', false],
+      ['string', false],
+      ['bytes32', false],
+    ].forEach(([input, expected]) => {
+      it(`returns ${expected} for ${input}`, () => {
+        const result = isArrayType(input as string);
+        expect(result).toEqual(expected);
+      });
+    });
+  });
+
+  describe('isSolidityType', () => {
+    [
+      ['uint256[]', false],
+      ['address[5]', false],
+      ['string[][]', false],
+      ['bytes32[1][2]', false],
+      ['uint256', true],
+      ['address', true],
+      ['string', true],
+      ['bytes32', true],
+    ].forEach(([input, expected]) => {
+      it(`returns ${expected} for ${input}`, () => {
+        const result = isSolidityType(input as string);
+        expect(result).toEqual(expected);
+      });
+    });
+  });
+
+  describe('stripArrayType', () => {
+    [
+      ['uint256[]', 'uint256'],
+      ['address[5]', 'address'],
+      ['string[][]', 'string'],
+      ['bytes32[1][2]', 'bytes32'],
+    ].forEach(([input, expected]) => {
+      it(`removes the array type from ${input}`, () => {
+        const result = stripArrayType(input);
+        expect(result).toEqual(expected);
+      });
+    });
+  });
+
+  describe('stripOneLayerofNesting', () => {
+    [
+      ['uint256[1]', 'uint256'],
+      ['address[5]', 'address'],
+      ['string[1][2]', 'string[2]'],
+      ['bytes32[1][2]', 'bytes32[2]'],
+    ].forEach(([input, expected]) => {
+      it(`removes one layer of array nesting from ${input}`, () => {
+        const result = stripOneLayerofNesting(input);
+        expect(result).toEqual(expected);
+      });
     });
   });
 
