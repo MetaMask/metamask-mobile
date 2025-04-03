613c593a88400e9f68b089751b24c2f16c880bc3
Cal-L <cleun007@gmail.com>
2025-04-03 11:43:34 -0700

    Remove commented out code

--- app/components/UI/BiometryButton/index.test.tsx
+++ app/components/UI/BiometryButton/index.test.tsx
@@ -1,43 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
-import BiometryButton from './BiometryButton';
 import { Platform } from 'react-native';
 import { BIOMETRY_TYPE } from 'react-native-keychain';
+import BiometryButton from './BiometryButton';
 import AUTHENTICATION_TYPE from '../../../constants/userProperties';
 import { LoginViewSelectors } from '../../../../e2e/selectors/wallet/LoginView.selectors';
 
-// Mock react native
 jest.mock('react-native', () => ({
   ...jest.requireActual('react-native'),
   Platform: { OS: 'ios' },
 }));
-
-// import AUTHENTICATION_TYPE from '../../../../constants/userProperties';
-
-// // Mock image assets
-// jest.mock('../../../../images/ios-face-id.png', () => 'ios-face-id');
-// jest.mock('../../../../images/android-face-recognition.png', () => 'android-face');
-// jest.mock('../../../../images/android-iris.png', () => 'android-iris');
-
-// // Mock the theme hook
-// jest.mock('../../../../util/theme', () => ({
-//   useTheme: () => ({
-//     colors: {
-//       text: {
-//         default: '#000000',
-//       },
-//     },
-//   }),
-// }));
-
-// // Mock vector icons components
-// jest.mock('react-native-vector-icons/MaterialIcons', () => function MockMaterialIcon(props: any) {
-//   return <mock-material-icon {...props} />;
-// });
-
-// jest.mock('react-native-vector-icons/Ionicons', () => function MockIonicons(props: any) {
-//   return <mock-ionicons {...props} />;
-// });
 
 const mockOnPress = jest.fn();
 
