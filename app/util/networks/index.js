3a04378b1e50b6098d8ff567a29c3b5eb9062c93
Nicholas Gambino <nicholas.gambino@consensys.net>
2025-04-04 12:58:41 -0700

    fix: Lint

--- app/util/networks/index.js
+++ app/util/networks/index.js
@@ -27,7 +27,6 @@ import {
   PopularList,
   UnpopularNetworkList,
   CustomNetworkImgMapping,
-  getNonEvmNetworkImageSourceByChainId,
 } from './customNetworks';
 import { strings } from '../../../locales/i18n';
 import {
@@ -52,7 +51,6 @@ import {
 } from '../../selectors/multichainNetworkController';
 import { formatBlockExplorerAddressUrl } from '../../core/Multichain/networks';
 import { CHAIN_IDS } from '@metamask/transaction-controller';
-import { isCaipChainId } from '@metamask/utils';
 
 /**
  * List of the supported networks
@@ -470,7 +468,6 @@ export const getNetworkNameFromProviderConfig = (providerConfig) => {
  *
  * @param {object} params - Params that contains information about the network.
  * @param {string} params.networkType - Type of network from the provider.
- * @param {string} params?.networkType - Type of network from the provider.
  * @param {string} params.chainId - ChainID of the network.
  * @returns {Object} - Image source of the network.
  */
@@ -498,11 +495,6 @@ export const getNetworkImageSource = ({ networkType, chainId }) => {
   if (customNetworkImg) {
     return customNetworkImg;
   }
-
-  if (isCaipChainId(chainId)) {
-    return getNonEvmNetworkImageSourceByChainId(chainId);
-  }
-
   return getTestNetImage(networkType);
 };
 
