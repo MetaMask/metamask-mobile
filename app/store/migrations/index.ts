5aa86233cc6033b253d0bfcb572e1f0f557392ea
Stanley Yuen <102275989+stanleyyconsensys@users.noreply.github.com>
2025-04-08 17:43:37 +0800

    feat: add `MegaETH Testnet` as default network (#14241)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    This PR is to add MegaETH as default network via NetworkController (
    [5527](https://github.com/MetaMask/core/pull/5527),
    [5495](https://github.com/MetaMask/core/pull/5495) )
    
    It also include a migration script to add/replace the MegaETH network
    for existing user
    
    ### **Change**:
    - Add MegaETH Testnet to default network via NetworkController options -
    `additionalDefaultNetworks`
    - Add migration script to replace/add MegaETH Testnet to the
    NetworkController state
    - Decouple the logic on method validateName from `NetworkSetting` to
    Network Utils `isValidNetworkName` for easier maintain and increase test
    coverage
    - Update Unit Test for MegaETH Testnet
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Setup a new wallet
    2. Click on network menu and show test networks
    3. You will see Sepolia , Linea Sepolia and Mega Testnet on test
    networks section
    4. Click on Mega Testnet to switch the network
    5. Send a transaction
    6. Check the transaction result in the explorer
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    <img width="300" alt="Screenshot 2025-03-21 at 12 49 13"
    src="https://github.com/user-attachments/assets/69c95b03-0c95-4cae-bf2f-445a8893d0bf"
    />
    <img width="300" alt="Screenshot 2025-03-21 at 12 49 38"
    src="https://github.com/user-attachments/assets/9e7e2274-b83a-4f72-898a-32a7b19e847f"
    />
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: wantedsystem <amine.harty@consensys.net>
    Co-authored-by: Elliot Winkler <elliot.winkler@gmail.com>
    Co-authored-by: Mathieu Artu <mathieu.artu@consensys.net>

--- app/store/migrations/index.ts
+++ app/store/migrations/index.ts
@@ -72,6 +72,7 @@ import migration68 from './068';
 import migration69 from './069';
 import migration70 from './070';
 import migration71 from './071';
+import migration72 from './072';
 
 // Add migrations above this line
 import { validatePostMigrationState } from '../validateMigration/validateMigration';
@@ -160,6 +161,7 @@ export const migrationList: MigrationsList = {
   69: migration69,
   70: migration70,
   71: migration71,
+  72: migration72,
 };
 
 // Enable both synchronous and asynchronous migrations
