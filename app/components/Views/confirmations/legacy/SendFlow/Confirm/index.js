8305f1d6198b16c0641cc1ea54f69d7b2cd637c0
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-04-08 18:06:04 +0200

    Remove unused condition

--- app/components/Views/confirmations/legacy/SendFlow/Confirm/index.js
+++ app/components/Views/confirmations/legacy/SendFlow/Confirm/index.js
@@ -636,7 +636,7 @@ class Confirm extends PureComponent {
       maxValueMode &&
       selectedAsset.isETH &&
       !isEmpty(gasFeeEstimates) &&
-      (haveGasFeeMaxNativeChanged || isControllerTransactionAdded)
+      haveGasFeeMaxNativeChanged
     ) {
       updateTransactionToMaxValue({
         transactionId,
