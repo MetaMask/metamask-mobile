7e6bcba9b0ffc352105e01c411dab16b7488254c
imblue-dabadee <johnkdev@outlook.com>
2025-04-09 05:08:48 +1000

    add getPhishingTestResultAsync

--- app/util/phishingDetection.ts
+++ app/util/phishingDetection.ts
@@ -1,4 +1,9 @@
-import { PhishingDetectorResult, PhishingDetectorResultType, PhishingController as PhishingControllerClass } from '@metamask/phishing-controller';
+import {
+  PhishingDetectorResult,
+  PhishingDetectorResultType,
+  PhishingController as PhishingControllerClass,
+  RecommendedAction,
+} from '@metamask/phishing-controller';
 import Engine from '../core/Engine';
 import { store } from '../store';
 import { selectProductSafetyDappScanningEnabled } from '../selectors/featureFlagController/productSafetyDappScanning';
@@ -7,7 +12,8 @@ import { selectProductSafetyDappScanningEnabled } from '../selectors/featureFlag
  * Checks if product safety dapp scanning is enabled
  * @returns {boolean} Whether product safety dapp scanning is enabled
  */
-export const isProductSafetyDappScanningEnabled = (): boolean => selectProductSafetyDappScanningEnabled(store.getState());
+export const isProductSafetyDappScanningEnabled = (): boolean =>
+  selectProductSafetyDappScanningEnabled(store.getState());
 
 /**
  * Gets detailed phishing test results for an origin
@@ -21,6 +27,32 @@ export const getPhishingTestResult = (origin: string): PhishingDetectorResult =>
  const { PhishingController } = Engine.context as {
     PhishingController: PhishingControllerClass;
   };
+  PhishingController.maybeUpdateState();
+  return PhishingController.test(origin);
+};
+
+/**
+ * Gets detailed phishing test results for an origin
+ * @param {string} origin - URL origin or hostname to check
+ * @returns {PhishingDetectorResult} Phishing test result object or null if protection is disabled
+ */
+export const getPhishingTestResultAsync = async (
+  origin: string,
+): Promise<PhishingDetectorResult> => {
+  const { PhishingController } = Engine.context as {
+    PhishingController: PhishingControllerClass;
+  };
+
+  if (isProductSafetyDappScanningEnabled()) {
+    const scanResult = await PhishingController.scanUrl(origin);
+    return {
+      result:
+        scanResult.recommendedAction === RecommendedAction.None ||
+        scanResult.recommendedAction === RecommendedAction.Warn,
+      name: 'Product safety dapp scanning is enabled',
+      type: 'DAPP_SCANNING' as PhishingDetectorResultType,
+    };
+  }
   PhishingController.maybeUpdateState();
   return PhishingController.test(origin);
 };
