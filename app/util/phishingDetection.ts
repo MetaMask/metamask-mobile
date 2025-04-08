8e2eff9fb735a2b7559ea729501607d30fe03a2a
imblue-dabadee <johnkdev@outlook.com>
2025-04-09 01:29:55 +1000

    lint: phishing detection util

--- app/util/phishingDetection.ts
+++ app/util/phishingDetection.ts
@@ -1,4 +1,7 @@
-import { PhishingDetectorResult, PhishingDetectorResultType } from '@metamask/phishing-controller';
+import {
+  PhishingDetectorResult,
+  PhishingDetectorResultType,
+} from '@metamask/phishing-controller';
 import Engine from '../core/Engine';
 import { store } from '../store';
 import { selectBasicFunctionalityEnabled } from '../selectors/settings';
@@ -8,29 +11,39 @@ import { selectProductSafetyDappScanningEnabled } from '../selectors/featureFlag
  * Checks if phishing protection is enabled based on the basic functionality toggle
  * @returns {boolean} Whether phishing protection is enabled
  */
-export const isPhishingDetectionEnabled = (): boolean => selectBasicFunctionalityEnabled(store.getState());
+export const isPhishingDetectionEnabled = (): boolean =>
+  selectBasicFunctionalityEnabled(store.getState());
 
 /**
  * Checks if product safety dapp scanning is enabled
  * @returns {boolean} Whether product safety dapp scanning is enabled
  */
-export const isProductSafetyDappScanningEnabled = (): boolean => selectProductSafetyDappScanningEnabled(store.getState());
-
+export const isProductSafetyDappScanningEnabled = (): boolean =>
+  selectProductSafetyDappScanningEnabled(store.getState());
 
 /**
  * Gets detailed phishing test results for an origin
  * @param {string} origin - URL origin or hostname to check
  * @returns {PhishingDetectorResult} Phishing test result object or null if protection is disabled
  */
-export const getPhishingTestResult = (origin: string): PhishingDetectorResult => {
+export const getPhishingTestResult = (
+  origin: string,
+): PhishingDetectorResult => {
   if (!isPhishingDetectionEnabled()) {
-    return { result: false, name: 'Phishing protection is disabled', type: PhishingDetectorResultType.All };
+    return {
+      result: false,
+      name: 'Phishing protection is disabled',
+      type: PhishingDetectorResultType.All,
+    };
   }
 
   if (isProductSafetyDappScanningEnabled()) {
-    return { result: false, name: 'Product safety dapp scanning is enabled', type: PhishingDetectorResultType.All };
+    return {
+      result: false,
+      name: 'Product safety dapp scanning is enabled',
+      type: PhishingDetectorResultType.All,
+    };
   }
-  
 
   const PhishingController = Engine.context.PhishingController;
   PhishingController.maybeUpdateState();
