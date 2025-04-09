ebaed0bd86db63216d7a4ceb5d26611de494846f
EtherWizard33 <eric.lamontagne@consensys.net>
2025-04-09 12:09:17 -0400

    fix: these two feature flags should be functions

--- app/util/networks/index.js
+++ app/util/networks/index.js
@@ -566,9 +566,9 @@ export const isChainPermissionsFeatureEnabled = true;
 export const isPermissionsSettingsV1Enabled =
   process.env.MM_PERMISSIONS_SETTINGS_V1_ENABLED === 'true';
 
-export const isPerDappSelectedNetworkEnabled = true;
+export const isPerDappSelectedNetworkEnabled = () => true;
 
 export const isPortfolioViewEnabled = () =>
   process.env.PORTFOLIO_VIEW === 'true';
 
-export const isMultichainV1Enabled = true;
+export const isMultichainV1Enabled = () => true;
