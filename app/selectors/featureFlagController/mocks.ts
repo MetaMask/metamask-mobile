d6d754cd3b3ba76adc3d4764fc760619b816c83f
Matthew Grainger <matthew.grainger@consensys.net>
2025-04-09 12:45:12 -0400

    feat: added selectors for initial pooled-staking feature flags

--- app/selectors/featureFlagController/mocks.ts
+++ app/selectors/featureFlagController/mocks.ts
@@ -1,5 +1,6 @@
 import { FeatureFlags } from '@metamask/remote-feature-flag-controller';
 import { mockedMinimumAppVersion } from './minimumAppVersion/constants';
+import { mockedPooledStakingFeatureFlagState } from './earnFeatureFlags/constants';
 
 export const mockedState = {
   engine: {
@@ -7,6 +8,7 @@ export const mockedState = {
       RemoteFeatureFlagController: {
         remoteFeatureFlags: {
           ...mockedMinimumAppVersion,
+          ...mockedPooledStakingFeatureFlagState,
           productSafetyDappScanning: true,
         },
         cacheTimestamp: 0,
