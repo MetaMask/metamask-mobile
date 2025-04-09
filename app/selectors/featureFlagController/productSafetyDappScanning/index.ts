301c745274ee7accc341747af9f1b28fa6ff995d
imblue-dabadee <johnkdev@outlook.com>
2025-04-10 01:12:14 +1000

    fix: add back the productSafety feature flag controller

---
+++ app/selectors/featureFlagController/productSafetyDappScanning/index.ts
@@ -0,0 +1,13 @@
+import { createSelector } from 'reselect';
+import { selectRemoteFeatureFlags } from '../';
+// import { hasProperty } from '@metamask/utils';
+
+const DEFAULT_PRODUCT_SAFETY_DAPP_SCANNING_ENABLED = true;
+export const FEATURE_FLAG_NAME = 'productSafetyDappScanning';
+
+export const selectProductSafetyDappScanningEnabled = createSelector(
+  selectRemoteFeatureFlags,
+  () => DEFAULT_PRODUCT_SAFETY_DAPP_SCANNING_ENABLED,
+  // When feature is ready for production, use this implementation:
+  // (remoteFeatureFlags) => hasProperty(remoteFeatureFlags, FEATURE_FLAG_NAME) ? remoteFeatureFlags[FEATURE_FLAG_NAME] as boolean : DEFAULT_PRODUCT_SAFETY_DAPP_SCANNING_ENABLED,
+);
