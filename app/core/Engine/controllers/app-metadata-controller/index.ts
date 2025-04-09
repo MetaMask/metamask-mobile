8bd34be35e1ff5eb64b006a08683f3087f4f669c
Frank von Hoven <141057783+frankvonhoven@users.noreply.github.com>
2025-04-09 12:06:05 -0500

    Fit typing

--- app/core/Engine/controllers/app-metadata-controller/index.ts
+++ app/core/Engine/controllers/app-metadata-controller/index.ts
@@ -1,10 +1,20 @@
-import type { ControllerInitFunction } from '../../types';
 import {
   AppMetadataController,
   type AppMetadataControllerState,
+  type AppMetadataControllerMessenger,
 } from '@metamask/app-metadata-controller';
 import { logAppMetadataControllerCreation } from './utils';
 import { defaultAppMetadataControllerState } from './constants';
+
+interface AppMetadataControllerInitRequest {
+  controllerMessenger: AppMetadataControllerMessenger;
+  persistedState: {
+    AppMetadataController?: AppMetadataControllerState;
+  };
+}
+
+// Export types
+export type { AppMetadataControllerMessenger };
 
 // Export constants
 export * from './constants';
@@ -15,7 +25,9 @@ export * from './constants';
  * @param request - The request object.
  * @returns The AppMetadataController.
  */
-export const appMetadataControllerInit = (request: any) => {
+export const appMetadataControllerInit = (
+  request: AppMetadataControllerInitRequest,
+) => {
   const { controllerMessenger, persistedState } = request;
 
   const appMetadataControllerState = (persistedState.AppMetadataController ??
