d33415678905bb371cb90e12f6f097d123e57562
ffmcgee <joao.carlos@consensys.net>
2025-04-11 17:53:10 +0200

    fix: backgroundBridge.test.ts

--- app/core/__mocks__/MockedEngine.ts
+++ app/core/__mocks__/MockedEngine.ts
@@ -41,6 +41,7 @@ export const mockedEngine = {
       addAndShowApprovalRequest: jest.fn(),
     },
     PermissionController: {
+      createPermissionMiddleware: jest.fn(),
       requestPermissions: jest.fn(),
       getCaveat: jest.fn(),
       updateCaveat: jest.fn(),
