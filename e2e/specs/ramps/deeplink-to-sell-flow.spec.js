eb47b98c1247bac0a170349fda96c378adce7f58
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2025-04-11 17:06:06 -0400

    fix(ramp,test): switch network assertion

--- e2e/specs/ramps/deeplink-to-sell-flow.spec.js
+++ e2e/specs/ramps/deeplink-to-sell-flow.spec.js
@@ -91,7 +91,7 @@ describe(SmokeRamps('Sell Crypto Deeplinks'), () => {
         await NetworkAddedBottomSheet.tapCloseButton();
         await Assertions.checkIfVisible(NetworkEducationModal.container);
         await NetworkEducationModal.tapGotItButton();
-        await Assertions.checkIfTextIsDisplayed('Ether');
+        await Assertions.checkIfTextIsDisplayed('OP Mainnet');
         await Assertions.checkIfTextIsDisplayed(
           PopularNetworksList.Optimism.providerConfig.nickname,
         );
