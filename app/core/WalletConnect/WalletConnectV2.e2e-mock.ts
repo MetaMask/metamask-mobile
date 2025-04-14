e876ab0dd2ef007dbe5c98815bbdbebac952ef11
Christopher Ferreira <christopher.ferreira@consensys.net>
2025-04-14 18:58:57 +0100

    chore: fix unit tests and WalletConnect mock

--- app/core/WalletConnect/WalletConnectV2.e2e-mock.ts
+++ app/core/WalletConnect/WalletConnectV2.e2e-mock.ts
@@ -1,29 +1,41 @@
 import { NavigationContainerRef } from '@react-navigation/native';
-import Client from '@walletconnect/se-sdk';
+import { IWalletKit } from '@reown/walletkit';
 import DevLogger from '../SDKConnect/utils/DevLogger';
 
+import WalletConnect2Session from './WalletConnect2Session';
 export class WC2Manager {
   private constructor(
-    web3Wallet: Client,
+    web3Wallet: IWalletKit,
     deeplinkSessions: {
       [topic: string]: { redirectUrl?: string; origin: string };
     },
     navigation: NavigationContainerRef,
+    sessions: { [topic: string]: WalletConnect2Session } = {},
   ) {
     DevLogger.log(`[MOCK] WC2Manager::constructor()`, {
       web3Wallet,
       deeplinkSessions,
       navigation,
+      sessions,
     });
+
+  }
+
+  protected static async initCore(projectId: string | undefined) {
+    DevLogger.log(`[MOCK] WC2Manager::initCore()`);
+    return;
   }
 
   public static async init({
     navigation,
+    sessions = {},
   }: {
     navigation: NavigationContainerRef;
+    sessions?: { [topic: string]: WalletConnect2Session };
   }) {
-    DevLogger.log(`[MOCK] WC2Manager::init()`, {
+    DevLogger.log(`[MOCK] WC2Manager::init() `, {
       navigation,
+      sessions,
     });
     return;
   }
