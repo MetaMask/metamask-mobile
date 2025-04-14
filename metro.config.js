674c0d789a90eccec7a31088c9a8688aa04987e8
tommasini <tommasini15@gmail.com>
2025-04-14 22:56:24 +0100

    fix lint issue using path at metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -6,9 +6,10 @@
  * @type {import('metro-config').MetroConfig}
  */
 
-
 const { getDefaultConfig } = require('expo/metro-config');
 const { mergeConfig } = require('@react-native/metro-config');
+// We should replace path for react-native-fs
+// eslint-disable-next-line import/no-nodejs-modules
 const path = require('path');
 const {
   wrapWithReanimatedMetroConfig,
@@ -20,35 +21,40 @@ module.exports = function (baseConfig) {
     resolver: { assetExts, sourceExts },
   } = defaultConfig;
 
-  return wrapWithReanimatedMetroConfig(mergeConfig(defaultConfig, {
-    resolver: {
-      assetExts: assetExts.filter((ext) => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg', 'cjs', 'mjs'],
-      resolverMainFields: ['sbmodern', 'react-native', 'browser', 'main'],
-      extraNodeModules: {
-        ...defaultConfig.resolver.extraNodeModules,
-        crypto: require.resolve('react-native-crypto'),
-        stream: require.resolve('stream-browserify'),
-        'images': path.resolve(__dirname, 'app/images'),
-      },
-    },
-    transformer: {
-      babelTransformerPath: require.resolve('./metro.transform.js'),
-      assetPlugins: ['react-native-svg-asset-plugin', 'expo-asset/tools/hashAssetFiles'],
-      svgAssetPlugin: {
-        pngCacheDir: '.png-cache',
-        scales: [1],
-        output: {
-          compressionLevel: 6,
+  return wrapWithReanimatedMetroConfig(
+    mergeConfig(defaultConfig, {
+      resolver: {
+        assetExts: assetExts.filter((ext) => ext !== 'svg'),
+        sourceExts: [...sourceExts, 'svg', 'cjs', 'mjs'],
+        resolverMainFields: ['sbmodern', 'react-native', 'browser', 'main'],
+        extraNodeModules: {
+          ...defaultConfig.resolver.extraNodeModules,
+          crypto: require.resolve('react-native-crypto'),
+          stream: require.resolve('stream-browserify'),
+          images: path.resolve(__dirname, 'app/images'),
         },
       },
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: true,
-          inlineRequires: true,
+      transformer: {
+        babelTransformerPath: require.resolve('./metro.transform.js'),
+        assetPlugins: [
+          'react-native-svg-asset-plugin',
+          'expo-asset/tools/hashAssetFiles',
+        ],
+        svgAssetPlugin: {
+          pngCacheDir: '.png-cache',
+          scales: [1],
+          output: {
+            compressionLevel: 6,
+          },
         },
-      }),
-    },
-    resetCache: true,
-  }));
+        getTransformOptions: async () => ({
+          transform: {
+            experimentalImportSupport: true,
+            inlineRequires: true,
+          },
+        }),
+      },
+      resetCache: true,
+    }),
+  );
 };
