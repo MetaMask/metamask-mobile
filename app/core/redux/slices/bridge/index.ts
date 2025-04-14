83726362198a2b85cb3366f5458cc2f0b57254e7
Bryan Fullam <bryan.fullam@consensys.net>
2025-04-14 17:13:52 +0200

    feat: add destAddress to redux slice

--- app/core/redux/slices/bridge/index.ts
+++ app/core/redux/slices/bridge/index.ts
@@ -16,6 +16,7 @@ import {
 } from '@metamask/bridge-controller';
 import { BridgeToken } from '../../../../components/UI/Bridge/types';
 import { PopularList } from '../../../../util/networks/customNetworks';
+import { Account } from '../../../../components/hooks/useAccounts';
 
 export const selectBridgeControllerState = (state: RootState) =>
   state.engine.backgroundState?.BridgeController;
@@ -25,6 +26,7 @@ export interface BridgeState {
   destAmount: string | undefined;
   sourceToken: BridgeToken | undefined;
   destToken: BridgeToken | undefined;
+  destAddress: string | undefined;
   selectedSourceChainIds: (Hex | CaipChainId)[] | undefined;
   selectedDestChainId: Hex | CaipChainId | undefined;
   slippage: string;
@@ -35,6 +37,7 @@ export const initialState: BridgeState = {
   destAmount: undefined,
   sourceToken: undefined,
   destToken: undefined,
+  destAddress: undefined,
   selectedSourceChainIds: undefined,
   selectedDestChainId: undefined,
   slippage: '0.5',
@@ -67,6 +70,9 @@ const slice = createSlice({
     },
     setDestToken: (state, action: PayloadAction<BridgeToken>) => {
       state.destToken = action.payload;
+    },
+    setDestAddress: (state, action: PayloadAction<string | undefined>) => {
+      state.destAddress = action.payload;
     },
     setSlippage: (state, action: PayloadAction<string>) => {
       state.slippage = action.payload;
@@ -220,6 +226,11 @@ export const selectSlippage = createSelector(
   (bridgeState) => bridgeState.slippage,
 );
 
+export const selectDestAddress = createSelector(
+  selectBridgeState,
+  (bridgeState) => bridgeState.destAddress,
+);
+
 // Actions
 export const {
   setSourceAmount,
@@ -230,4 +241,5 @@ export const {
   setSelectedSourceChainIds,
   setSelectedDestChainId,
   setSlippage,
+  setDestAddress,
 } = actions;
