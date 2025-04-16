87e3559f840cdbfbd1e33b1d495dc77f8d09e08f
Ziad Saab <ziad.saab@gmail.com>
2025-04-16 11:34:15 -0500

    fix urlautocomplete test

--- app/components/UI/UrlAutocomplete/index.test.tsx
+++ app/components/UI/UrlAutocomplete/index.test.tsx
@@ -184,7 +184,7 @@ describe('UrlAutocomplete', () => {
 
     const deleteFavorite = await screen.findByTestId(deleteFavoriteTestId(defaultState.bookmarks[0].url), {includeHiddenElements: true});
     fireEvent.press(deleteFavorite);
-    expect(store.dispatch).toHaveBeenCalledWith(removeBookmark({...defaultState.bookmarks[0], type: 'favorites'}));
+    expect(store.dispatch).toHaveBeenCalledWith(removeBookmark({...defaultState.bookmarks[0], category: 'favorites'}));
   });
 
   it('should show a loading indicator when searching tokens', async () => {
