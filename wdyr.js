f2547e81ae4aa2d172e10bc7ea123e45c3085622
Owen Craston <owengc12@gmail.com>
2025-04-18 12:55:11 -0700

    track hook

--- wdyr.js
+++ wdyr.js
@@ -1,12 +1,19 @@
 // wdyr.js
 import React from 'react';
 
+// adjust the path to wherever your hook lives
+const {
+  default: useMultichainBalances,
+  // eslint-disable-next-line import/no-commonjs
+} = require('./app/components/hooks/useMultichainBalances/useMultichainBalances.ts');
+
 if (__DEV__) {
   // eslint-disable-next-line no-console
   console.log('🐛 why‑did‑you‑render is active');
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   whyDidYouRender(React, {
     trackAllPureComponents: true,
-    // trackHooks: true, // if you want hook-level reporting
+    trackHooks: true, // if you want hook-level reporting
+    trackExtraHooks: [[{ useMultichainBalances }, 'useMultichainBalances']],
   });
 }
