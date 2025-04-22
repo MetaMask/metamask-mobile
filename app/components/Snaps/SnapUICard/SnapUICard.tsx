d4894973627b08adaff1a850a6609ff66364b26f
Frederik Bolding <frederik.bolding@gmail.com>
2025-04-22 13:25:40 +0200

    Fix Card text alignment

--- app/components/Snaps/SnapUICard/SnapUICard.tsx
+++ app/components/Snaps/SnapUICard/SnapUICard.tsx
@@ -54,12 +54,22 @@ export const SnapUICard: FunctionComponent<SnapUICardProps> = ({
         )}
       </Box>
     </Box>
-    <Box flexDirection={FlexDirection.Column} textAlign={TextAlign.right}>
-      <Text variant={TextVariant.BodyMDMedium} ellipsizeMode="tail">
+    <Box flexDirection={FlexDirection.Column}>
+      <Text
+        variant={TextVariant.BodyMDMedium}
+        ellipsizeMode="tail"
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{ textAlign: 'right' }}
+      >
         {value}
       </Text>
       {extra && (
-        <Text color={TextColor.Alternative} ellipsizeMode="tail">
+        <Text
+          color={TextColor.Alternative}
+          ellipsizeMode="tail"
+          // eslint-disable-next-line react-native/no-inline-styles
+          style={{ textAlign: 'right' }}
+        >
           {extra}
         </Text>
       )}
