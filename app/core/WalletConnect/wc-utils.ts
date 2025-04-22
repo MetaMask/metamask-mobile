6d6e56a4117af1b4cb4f1554c6356687c63327f3
Edward Penta <eddie.penta@consensys.net>
2025-04-22 18:29:39 -0400

    fix(wc): move add permitted chain after successful switchToNetwork call

--- app/core/WalletConnect/wc-utils.ts
+++ app/core/WalletConnect/wc-utils.ts
@@ -291,11 +291,6 @@ export const checkWCPermissions = async ({
 
   if (caip2ChainId !== activeCaip2ChainId) {
     try {
-      if (!isAllowedChainId && allowSwitchingToNewChain) {
-        DevLogger.log(`WC::checkWCPermissions adding permitted chain for ${origin}:`, hexChainIdString);
-        await addPermittedChain(getHostname(origin), hexChainIdString);
-      }
-
       await switchToNetwork({
         network: existingNetwork,
         chainId: hexChainIdString,
@@ -305,6 +300,11 @@ export const checkWCPermissions = async ({
         origin,
         isAddNetworkFlow: false,
       });
+      
+      if (!isAllowedChainId && allowSwitchingToNewChain) {
+        DevLogger.log(`WC::checkWCPermissions adding permitted chain for ${origin}:`, hexChainIdString);
+        await addPermittedChain(getHostname(origin), hexChainIdString);
+      }
 
     } catch (error) {
       DevLogger.log(
