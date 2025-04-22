a9016a1d40025b8e1125205de20cd6397dc00e72
IF <139582705+infiniteflower@users.noreply.github.com>
2025-04-22 19:25:46 -0400

    fix: lint errors

--- app/util/transaction-icons/index.test.tsx
+++ app/util/transaction-icons/index.test.tsx
@@ -1,4 +1,6 @@
 import { getTransactionIcon } from './index';
+import { AppThemeKey } from '../theme/models';
+import { ColorSchemeName } from 'react-native';
 
 import transactionIconInteraction from '../../images/transaction-icons/interaction.png';
 import transactionIconSent from '../../images/transaction-icons/send.png';
@@ -10,41 +12,69 @@ import transactionIconReceivedFailed from '../../images/transaction-icons/receiv
 import transactionIconSwapFailed from '../../images/transaction-icons/swap-failed.png';
 
 describe('getTransactionIcon', () => {
+  const defaultAppTheme = AppThemeKey.light;
+  const defaultOsColorScheme: ColorSchemeName = 'light';
+
   test('returns the correct icon for successful send transactions', () => {
-    expect(getTransactionIcon('send', false)).toBe(transactionIconSent);
+    expect(
+      getTransactionIcon('send', false, defaultAppTheme, defaultOsColorScheme),
+    ).toBe(transactionIconSent);
   });
 
   test('returns the correct icon for successful receive transactions', () => {
-    expect(getTransactionIcon('receive', false)).toBe(transactionIconReceived);
+    expect(
+      getTransactionIcon(
+        'receive',
+        false,
+        defaultAppTheme,
+        defaultOsColorScheme,
+      ),
+    ).toBe(transactionIconReceived);
   });
 
   test('returns the correct icon for successful swap transactions', () => {
-    expect(getTransactionIcon('swap', false)).toBe(transactionIconSwap);
+    expect(
+      getTransactionIcon('swap', false, defaultAppTheme, defaultOsColorScheme),
+    ).toBe(transactionIconSwap);
   });
 
   test('returns the interaction icon for bridge transactions', () => {
-    expect(getTransactionIcon('bridge', false)).toBe(
-      transactionIconInteraction,
-    );
+    expect(
+      getTransactionIcon(
+        'bridge',
+        false,
+        defaultAppTheme,
+        defaultOsColorScheme,
+      ),
+    ).toBe(transactionIconInteraction);
   });
 
   test('returns the correct icon for failed send transactions', () => {
-    expect(getTransactionIcon('send', true)).toBe(transactionIconSentFailed);
+    expect(
+      getTransactionIcon('send', true, defaultAppTheme, defaultOsColorScheme),
+    ).toBe(transactionIconSentFailed);
   });
 
   test('returns the correct icon for failed receive transactions', () => {
-    expect(getTransactionIcon('receive', true)).toBe(
-      transactionIconReceivedFailed,
-    );
+    expect(
+      getTransactionIcon(
+        'receive',
+        true,
+        defaultAppTheme,
+        defaultOsColorScheme,
+      ),
+    ).toBe(transactionIconReceivedFailed);
   });
 
   test('returns the correct icon for failed swap transactions', () => {
-    expect(getTransactionIcon('swap', true)).toBe(transactionIconSwapFailed);
+    expect(
+      getTransactionIcon('swap', true, defaultAppTheme, defaultOsColorScheme),
+    ).toBe(transactionIconSwapFailed);
   });
 
   test('returns the failed interaction icon for failed bridge transactions', () => {
-    expect(getTransactionIcon('bridge', true)).toBe(
-      transactionIconInteractionFailed,
-    );
+    expect(
+      getTransactionIcon('bridge', true, defaultAppTheme, defaultOsColorScheme),
+    ).toBe(transactionIconInteractionFailed);
   });
 });
