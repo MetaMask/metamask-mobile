bfe58bf28fecc10daf545bdcdd1227680c50a62f
IF <139582705+infiniteflower@users.noreply.github.com>
2025-04-22 19:35:24 -0400

    fix: lint error

--- app/components/UI/Bridge/components/TransactionDetails/BridgeStepList.test.tsx
+++ app/components/UI/Bridge/components/TransactionDetails/BridgeStepList.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
 import BridgeStepList from './BridgeStepList';
-import { NetworkConfiguration } from '@metamask/network-controller';
 import { TransactionMeta } from '@metamask/transaction-controller';
 import {
   BridgeHistoryItem,
