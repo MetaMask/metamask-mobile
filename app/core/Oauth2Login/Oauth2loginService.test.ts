6143a4a242f06811d4a852fabc95ca381d5b4ce1
ieow <cwlee0@gmail.com>
2025-04-23 15:22:11 +0800

    feat: simple tests

--- app/core/Oauth2Login/Oauth2loginService.test.ts
+++ app/core/Oauth2Login/Oauth2loginService.test.ts
@@ -1,34 +1,40 @@
-// import Oauth2LoginService from './Oauth2loginService';
+import { OAuthProvider } from './Oauth2loginInterface';
+import Oauth2LoginService from './Oauth2loginService';
+import ReduxService, { ReduxStore } from '../redux';
 
-// describe('Oauth2 login', () => {
-//   afterEach(() => {
-//     StorageWrapper.clearAll();
-//     jest.restoreAllMocks();
-//   });
+jest.spyOn(ReduxService, 'store', 'get').mockReturnValue({
+  getState: () => ({ security: { allowLoginWithRememberMe: true } }),
+  dispatch: jest.fn()
+} as unknown as ReduxStore);
+// const actualSeedlessOnboardingController = jest.requireActual('./Oauth2LoginHandler');
 
-//   it('should return a type password', async () => {
-//     try {
-//       const result = await Oauth2LoginService.handleCodeFlow({
-//         provider: 'google',
-//         clientId: AndroidGoogleWebGID,
-//         idToken: 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImMzN2RhNzVjOWZiZTE4YzJjZTkxMjViOWFhMWYzMDBkY2IzMWU4ZDkiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI4ODIzNjMyOTE3NTEta3A5NDBzazNoczFzdWVscDl1dXRsMDI3ajRlYmxwczMuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI4ODIzNjMyOTE3NTEtMmEzN2NjaHJxOW9jMWxmajFwNDE5b3R2YWhuYmhndXYuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDE1NTk2NTkyMzQ1MTg0MzgwMzMiLCJoZCI6InRvci51cyIsImVtYWlsIjoiY2hlcm5nd29laUB0b3IudXMiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibm9uY2UiOiIxMjMxMzEyMzEyMyIsIm5hbWUiOiJDaGVybmcgV29laSBMZWUiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jS0xlWjdKTzl0aF93bi1fQmJ2WnpMZV8xU0JnSTRFWFdCek13c3FScy1XN25xTEd3PXM5Ni1jIiwiZ2l2ZW5fbmFtZSI6IkNoZXJuZyBXb2VpIiwiZmFtaWx5X25hbWUiOiJMZWUiLCJpYXQiOjE3NDQ5ODY3NzMsImV4cCI6MTc0NDk5MDM3M30.RG5U2dFCE2lD5jeO8x6AX4gAiSgqJF8rewzRXsS487K_dzKEcXx2rmsjQWY6YJBiKpb3IAXtMtcPnWC40ctCdtnve0Ox1vv6Copyrz3SEZMzGuZkhQhxGshmVEaSvmr0Vuf7KM-KFMHdj3zDT2sCOqKQ6UdaDHaSDjjx-omuBZNFOFhCrC5weqBQ9E-2Bq1EwNJMnUUiJkNc_sY7SXyAzPlPIfyoRaXt66LA6R2RipQR-2Vf0_tVviScQRkwvtGwDZxgttScoN5CFdSzbpr2olHK3JytqPM9wUc0kE9TrlaYLneKWrWQKwZpauX2uIO2lpGjbn3pTR4GEPdX1TFcWA',
-//         web3AuthNetwork: 'sapphire_devnet',
-//       });
-//       console.log(result);
-//       expect(result).toEqual({ type: 'success', existingUser: false });
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   });
+let mockLoginHandlerResponse = () => undefined;
 
-// });
+jest.mock('./Oauth2LoginHandler', () => ({
+  createLoginHandler: ()=>({
+    login: () => mockLoginHandlerResponse(),
+  }),
+  getByoaTokens: () => ({}),
+}));
 
-export async function testCodeFlow() {
-  // const result = await Oauth2LoginService.handleSeedlessAuthenticate({
-  //   provider: 'google',
-  //   clientId: AndroidGoogleWebGID,
-  //   idToken: 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImMzN2RhNzVjOWZiZTE4YzJjZTkxMjViOWFhMWYzMDBkY2IzMWU4ZDkiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI4ODIzNjMyOTE3NTEta3A5NDBzazNoczFzdWVscDl1dXRsMDI3ajRlYmxwczMuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI4ODIzNjMyOTE3NTEtMmEzN2NjaHJxOW9jMWxmajFwNDE5b3R2YWhuYmhndXYuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDE1NTk2NTkyMzQ1MTg0MzgwMzMiLCJoZCI6InRvci51cyIsImVtYWlsIjoiY2hlcm5nd29laUB0b3IudXMiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibm9uY2UiOiIxMjMxMzEyMzEyMyIsIm5hbWUiOiJDaGVybmcgV29laSBMZWUiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jS0xlWjdKTzl0aF93bi1fQmJ2WnpMZV8xU0JnSTRFWFdCek13c3FScy1XN25xTEd3PXM5Ni1jIiwiZ2l2ZW5fbmFtZSI6IkNoZXJuZyBXb2VpIiwiZmFtaWx5X25hbWUiOiJMZWUiLCJpYXQiOjE3NDQ5ODY3NzMsImV4cCI6MTc0NDk5MDM3M30.RG5U2dFCE2lD5jeO8x6AX4gAiSgqJF8rewzRXsS487K_dzKEcXx2rmsjQWY6YJBiKpb3IAXtMtcPnWC40ctCdtnve0Ox1vv6Copyrz3SEZMzGuZkhQhxGshmVEaSvmr0Vuf7KM-KFMHdj3zDT2sCOqKQ6UdaDHaSDjjx-omuBZNFOFhCrC5weqBQ9E-2Bq1EwNJMnUUiJkNc_sY7SXyAzPlPIfyoRaXt66LA6R2RipQR-2Vf0_tVviScQRkwvtGwDZxgttScoN5CFdSzbpr2olHK3JytqPM9wUc0kE9TrlaYLneKWrWQKwZpauX2uIO2lpGjbn3pTR4GEPdX1TFcWA',
-  //   web3AuthNetwork: 'sapphire_devnet',
-  // });
-  // console.log(result);
-}
+
+describe('Oauth2 login service', () => {
+  it('should return a type dismiss', async () => {
+    jest.mock('./Oauth2LoginHandler', () => ({
+      createLoginHandler: ()=>({
+        login: ()=> undefined,
+      }),
+      getByoaTokens: () => ({}),
+    }));
+    const result = await Oauth2LoginService.handleOauth2Login(OAuthProvider.Google);
+    expect(result.type).toBe('dismiss');
+
+    mockLoginHandlerResponse = () => {
+      throw new Error('unexpecte Error');
+    };
+
+    const result2 = await Oauth2LoginService.handleOauth2Login(OAuthProvider.Google);
+    expect(result2.type).toBe('error');
+  });
+
+});
