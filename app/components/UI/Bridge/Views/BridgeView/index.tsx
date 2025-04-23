dc88fc06acea24a39545dd8acd47047e0e32c01a
Bryan Fullam <bryan.fullam@consensys.net>
2025-04-23 20:56:40 +0200

    chore: lint

--- app/components/UI/Bridge/Views/BridgeView/index.tsx
+++ app/components/UI/Bridge/Views/BridgeView/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import ScreenView from '../../../../Base/ScreenView';
 import Keypad from '../../../../Base/Keypad';
