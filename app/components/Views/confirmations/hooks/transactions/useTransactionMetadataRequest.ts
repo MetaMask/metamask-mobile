1915e1683d4abee06a112fe30b8c50e1c1860530
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-04-28 10:56:09 +0200

    Simulation fixes

--- app/components/Views/confirmations/hooks/transactions/useTransactionMetadataRequest.ts
+++ app/components/Views/confirmations/hooks/transactions/useTransactionMetadataRequest.ts
@@ -1,8 +1,9 @@
+import { useEffect } from 'react';
 import { ApprovalType } from '@metamask/controller-utils';
 import { TransactionMeta } from '@metamask/transaction-controller';
-
 import { useSelector } from 'react-redux';
 
+import Engine from '../../../../../core/Engine';
 import { selectTransactionMetadataById } from '../../../../../selectors/transactionController';
 import { RootState } from '../../../../UI/BasicFunctionality/BasicFunctionalityModal/BasicFunctionalityModal.test';
 import useApprovalRequest from '../useApprovalRequest';
@@ -13,6 +14,16 @@ export function useTransactionMetadataRequest() {
   const transactionMetadata = useSelector((state: RootState) =>
     selectTransactionMetadataById(state, approvalRequest?.id as string),
   );
+
+  useEffect(() => {
+    // This is a temporary solution to force the token list to be fetched for the chainId of the transaction in order to get proper display names for simulation details.
+    // We may remove this once we have a better way to single token information.
+    if (transactionMetadata?.chainId) {
+      Engine.context.TokenListController.fetchTokenList(
+        transactionMetadata.chainId,
+      );
+    }
+  }, [transactionMetadata?.chainId]);
 
   if (
     approvalRequest?.type === ApprovalType.Transaction &&
