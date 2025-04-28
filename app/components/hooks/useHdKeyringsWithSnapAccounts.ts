f16676ea5737d107439c01fbf33268d75e67332c
Monte Lai <monte.lai@consensys.net>
2025-04-28 16:31:30 +0800

    fix: update hook logic

--- app/components/hooks/useHdKeyringsWithSnapAccounts.ts
+++ app/components/hooks/useHdKeyringsWithSnapAccounts.ts
@@ -1,9 +1,9 @@
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { KeyringMetadata, KeyringObject } from '@metamask/keyring-controller';
-import { InternalAccount } from '@metamask/keyring-internal-api';
 import { selectHDKeyrings } from '../../selectors/keyringController';
 import { selectInternalAccounts } from '../../selectors/accountsController';
+import { InternalAccount } from '@metamask/keyring-internal-api';
 
 // TODO: Move this data type to the @metamask/keyring-controller module
 type KeyringObjectWithMetadata = KeyringObject & { metadata: KeyringMetadata };
@@ -16,21 +16,28 @@ type KeyringObjectWithMetadata = KeyringObject & { metadata: KeyringMetadata };
 export const useHdKeyringsWithSnapAccounts = () => {
   const hdKeyrings: KeyringObjectWithMetadata[] = useSelector(selectHDKeyrings);
   const internalAccounts = useSelector(selectInternalAccounts);
-  return useMemo(
-    () =>
-      hdKeyrings.map((keyring) => {
-        const firstPartySnapAccounts = internalAccounts
-          .filter(
-            (account: InternalAccount) =>
-              account.options?.entropySource === keyring.metadata.id,
-          )
-          .map((account: InternalAccount) => account.address);
+  return useMemo(() => {
+    const accountsByEntropySource = new Map<string, string[]>();
+    internalAccounts.forEach((account: InternalAccount) => {
+      const entropySource = account.options?.entropySource as
+        | string
+        | undefined;
+      if (entropySource) {
+        if (!accountsByEntropySource.has(entropySource)) {
+          accountsByEntropySource.set(entropySource, []);
+        }
+        accountsByEntropySource.get(entropySource)?.push(account.address);
+      }
+    });
 
-        return {
-          ...keyring,
-          accounts: [...keyring.accounts, ...firstPartySnapAccounts],
-        };
-      }),
-    [hdKeyrings, internalAccounts],
-  );
+    return hdKeyrings.map((keyring) => {
+      const firstPartySnapAccounts =
+        accountsByEntropySource.get(keyring.metadata.id) || [];
+
+      return {
+        ...keyring,
+        accounts: [...keyring.accounts, ...firstPartySnapAccounts],
+      };
+    });
+  }, [hdKeyrings, internalAccounts]);
 };
