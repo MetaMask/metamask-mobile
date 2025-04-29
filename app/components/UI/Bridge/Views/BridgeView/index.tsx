631660c619e375f2f5edad0bdb65f0252706123e
Bryan Fullam <bryan.fullam@consensys.net>
2025-04-29 15:45:36 +0200

    chore: remove temporary activity tab navigation workaround
    Removes a temporary workaround that was added to solve an issue with post-snap navigation. No longer needed as navigation post-snap now works consistently.

--- app/components/UI/Bridge/Views/BridgeView/index.tsx
+++ app/components/UI/Bridge/Views/BridgeView/index.tsx
@@ -254,11 +254,6 @@ const BridgeView = () => {
   const handleContinue = async () => {
     if (activeQuote) {
       setIsSubmittingTx(true);
-      // TEMPORARY: If tx originates from Solana, navigate to transactions view BEFORE submitting the tx
-      // Necessary because snaps prevents navigation after tx is submitted
-      if (isSolanaSwap || isSolanaToEvm) {
-        navigation.navigate(Routes.TRANSACTIONS_VIEW);
-      }
       await submitBridgeTx({
         quoteResponse: activeQuote,
       });
