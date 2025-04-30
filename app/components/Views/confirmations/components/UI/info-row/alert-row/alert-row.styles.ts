a487a0d05732bb78b1d57d05ea479a4b4e6162c9
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-04-30 13:57:31 +0200

    fix lint

--- app/components/Views/confirmations/components/UI/info-row/alert-row/alert-row.styles.ts
+++ app/components/Views/confirmations/components/UI/info-row/alert-row/alert-row.styles.ts
@@ -1,13 +1,10 @@
 import { StyleSheet } from 'react-native';
 
-const styleSheet = () => {
-
-  return StyleSheet.create({
+const styleSheet = () => StyleSheet.create({
     infoRowOverride: {
       paddingBottom: 4,
       paddingHorizontal: 8,
     },
   });
-};
 
 export default styleSheet;
