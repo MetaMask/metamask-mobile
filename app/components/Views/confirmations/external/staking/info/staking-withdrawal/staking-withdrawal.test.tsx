ed09cd44294245885e1aa1ee84b797e6f170210e
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-04-30 17:01:45 +0200

    feat: Integrate deeplink and dapp initated transfer confirmations (#14916)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    This PR aims to integrate deeplink and dapp initated redesigned transfer
    confirmations.
    
    **Labels:**
    Note that transfer confirmations can only be activated by setting env
    variable. Since this indicates feature is not yet released -
    `no-changelog` `No QA Needed` is added to the PR.
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/MetaMask-planning/issues/4771
    
    ## **Manual testing steps**
    
    - Deeplink and dapp initiated transfer confirmations will show
    redesigned transfer confirmation.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    Deeplink native transfer
    
    
    
    https://github.com/user-attachments/assets/855dfe42-f69d-472e-8625-46460b8e5350
    
    
    Deeplink ETH USDC transfer
    
    
    
    https://github.com/user-attachments/assets/49d28788-e36c-4d4f-bea9-a65bda5ddc7b
    
    
    
    Deeplink Linea USDC transfer
    
    
    
    
    https://github.com/user-attachments/assets/26408d45-6876-46e1-bdf5-ab52e995b336
    
    
    
    DApp initiated transfers
    
    
    
    
    https://github.com/user-attachments/assets/fc79886a-2c4c-49f4-bb79-e54b64007812
    
    
    
    
    ## **Pre-merge author checklist**
    
    - [X] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [X] I've completed the PR template to the best of my ability
    - [X] I’ve included tests if applicable
    - [X] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [X] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Nico MASSART <NicolasMassart@users.noreply.github.com>

--- app/components/Views/confirmations/external/staking/info/staking-withdrawal/staking-withdrawal.test.tsx
+++ app/components/Views/confirmations/external/staking/info/staking-withdrawal/staking-withdrawal.test.tsx
@@ -17,6 +17,9 @@ jest.mock('../../../../../../../core/Engine', () => ({
       startPolling: jest.fn(),
       stopPollingByPollingToken: jest.fn(),
     },
+    TokenListController: {
+      fetchTokenList: jest.fn(),
+    },
   },
 }));
 
