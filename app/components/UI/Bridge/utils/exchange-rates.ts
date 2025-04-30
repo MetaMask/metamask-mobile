d4123df2e5f77153fb0f3882013ead41e3148d0f
IF <139582705+infiniteflower@users.noreply.github.com>
2025-04-30 16:07:58 -0400

    chore: add fetching token rates

--- app/components/UI/Bridge/utils/exchange-rates.ts
+++ app/components/UI/Bridge/utils/exchange-rates.ts
@@ -1,26 +1,39 @@
-import { isSolanaChainId } from '@metamask/bridge-controller';
-import { Hex, CaipAssetType } from '@metamask/utils';
+import { formatChainIdToHex, isSolanaChainId } from '@metamask/bridge-controller';
+import { Hex, CaipAssetType, CaipChainId } from '@metamask/utils';
 import { selectMultichainAssetsRates } from '../../../../selectors/multichain';
-import { addCurrencySymbol, balanceToFiatNumber } from '../../../../util/number';
+import {
+  addCurrencySymbol,
+  balanceToFiatNumber,
+} from '../../../../util/number';
 import { BridgeToken } from '../types';
-
+import { handleFetch } from '@metamask/controller-utils';
+import { CodefiTokenPricesServiceV2, fetchTokenContractExchangeRates } from '@metamask/assets-controllers';
+import { safeToChecksumAddress } from '../../../../util/address';
+import { SolScope } from '@metamask/keyring-api';
+import { toAssetId } from '../hooks/useAssetMetadata/utils';
 
 interface GetDisplayFiatValueParams {
   token: BridgeToken | undefined;
   amount: string | undefined;
-  multiChainMarketData: Record<Hex, Record<Hex, { price: number | undefined; }>> |
-  undefined;
-  networkConfigurationsByChainId: Record<Hex, { nativeCurrency: string; }>;
-  multiChainCurrencyRates: Record<string, { conversionRate: number | null; }> |
-  undefined;
+  multiChainMarketData:
+    | Record<Hex, Record<Hex, { price: number | undefined }>>
+    | undefined;
+  networkConfigurationsByChainId: Record<Hex, { nativeCurrency: string }>;
+  multiChainCurrencyRates:
+    | Record<string, { conversionRate: number | null }>
+    | undefined;
   currentCurrency: string;
   nonEvmMultichainAssetRates: ReturnType<typeof selectMultichainAssetsRates>;
 }
 
 export const getDisplayFiatValue = ({
-  token, amount, multiChainMarketData, // EVM
-  networkConfigurationsByChainId, multiChainCurrencyRates, // EVM
-  currentCurrency, nonEvmMultichainAssetRates, // Non-EVM
+  token,
+  amount,
+  multiChainMarketData, // EVM
+  networkConfigurationsByChainId,
+  multiChainCurrencyRates, // EVM
+  currentCurrency,
+  nonEvmMultichainAssetRates, // Non-EVM
 }: GetDisplayFiatValueParams): string => {
   if (!token || !amount) {
     return addCurrencySymbol('0', currentCurrency);
@@ -34,7 +47,7 @@ export const getDisplayFiatValue = ({
     // We don't need to have an additional conversion from native token to fiat.
     const rate = nonEvmMultichainAssetRates?.[assetId]?.rate || '0';
     balanceFiatCalculation = Number(
-      balanceToFiatNumber(amount, Number(rate), 1)
+      balanceToFiatNumber(amount, Number(rate), 1),
     );
   } else {
     // EVM
@@ -42,15 +55,17 @@ export const getDisplayFiatValue = ({
     const multiChainExchangeRates = multiChainMarketData?.[evmChainId];
     const tokenMarketData = multiChainExchangeRates?.[token.address as Hex];
 
-    const nativeCurrency = networkConfigurationsByChainId[evmChainId]?.nativeCurrency;
-    const multiChainConversionRate = multiChainCurrencyRates?.[nativeCurrency]?.conversionRate ?? 0;
+    const nativeCurrency =
+      networkConfigurationsByChainId[evmChainId]?.nativeCurrency;
+    const multiChainConversionRate =
+      multiChainCurrencyRates?.[nativeCurrency]?.conversionRate ?? 0;
 
     balanceFiatCalculation = Number(
       balanceToFiatNumber(
         amount,
         multiChainConversionRate,
-        tokenMarketData?.price ?? 0
-      )
+        tokenMarketData?.price ?? 0,
+      ),
     );
   }
 
@@ -59,4 +74,58 @@ export const getDisplayFiatValue = ({
   }
 
   return `< ${addCurrencySymbol('0.01', currentCurrency)}`;
+};
+
+export const fetchTokenExchangeRates = async (
+  chainId: Hex | CaipChainId,
+  currency: string,
+  ...tokenAddresses: string[]
+) => {
+  let exchangeRates: Record<string, number | undefined> = {};
+
+  // Solana
+  if (isSolanaChainId(chainId)) {
+    const queryParams = new URLSearchParams({
+      assetIds: tokenAddresses
+        .map((address) => toAssetId(address, SolScope.Mainnet))
+        .join(','),
+      includeMarketData: 'true',
+      vsCurrency: currency,
+    });
+    const url = `https://price.api.cx.metamask.io/v3/spot-prices?${queryParams}`;
+    const tokenV3PriceResponse = (await handleFetch(url)) as Record<
+      string,
+      { price: number }
+    >;
+
+    exchangeRates = Object.entries(tokenV3PriceResponse).reduce(
+      (acc, [k, curr]) => {
+        acc[k] = curr.price;
+        return acc;
+      },
+      {} as Record<string, number>,
+    );
+    return exchangeRates;
+  }
+
+  // EVM chains
+  const checksumAddresses = tokenAddresses.map((address) => safeToChecksumAddress(address));
+  if (checksumAddresses.some((address) => !address)) {
+    return {};
+  }
+
+  exchangeRates = await fetchTokenContractExchangeRates({
+    tokenPricesService: new CodefiTokenPricesServiceV2(),
+    nativeCurrency: currency,
+    tokenAddresses: checksumAddresses as Hex[],
+    chainId: formatChainIdToHex(chainId),
+  });
+
+  return Object.keys(exchangeRates).reduce(
+    (acc: Record<string, number | undefined>, address) => {
+      acc[address] = exchangeRates[address];
+      return acc;
+    },
+    {},
+  );
 };
