86c8524bd192b862bbdff3b2002f48427d92002a
Cal-L <cleun007@gmail.com>
2025-05-01 16:57:23 -0700

    Remove Sentry patch

--- app/util/sentry/index.ts
+++ app/util/sentry/index.ts
@@ -6,6 +6,8 @@ import { ExclusiveEventHintOrCaptureContext } from '@sentry/core/build/types/uti
  * @param exception - The error exception to capture
  * @param hint - The hint to capture the exception with
  * @returns The event ID of the captured exception
+ *
+ * Report Error for better stack tracing - https://docs.sentry.io/platforms/javascript/usage/#capturing-errors
  */
 export function captureErrorException(
   exception: Error,
