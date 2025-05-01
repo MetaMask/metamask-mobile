8cc479559056e436d52f1e0f50cf9879d49b1009
sethkfman <Seth.Kaufman@consensys.net>
2025-05-01 17:09:26 -0600

    lint fix

--- app/components/hooks/AssetPolling/AssetPollingProvider.tsx
+++ app/components/hooks/AssetPolling/AssetPollingProvider.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import useCurrencyRatePolling from './useCurrencyRatePolling';
 import useTokenRatesPolling from './useTokenRatesPolling';
 import useTokenDetectionPolling from './useTokenDetectionPolling';
