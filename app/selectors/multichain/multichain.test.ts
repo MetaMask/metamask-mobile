c6dfa1c826127b21f52616210752ce8821579050
sahar-fehri <sahar.fehri@consensys.net>
2025-05-02 14:42:33 +0200

    fix: cp-7.46.0 improve token list item re renders (#15063)
    ## **Description**
    
    `<TokenListItem>` has been using
    ```
        const nonEvmAsset = useSelector((state: RootState) =>
          selectNonEvmAssetById(state, {
            accountId: selectedAccount?.id,
            assetId: assetKey.address,
          }),
        );
    ```
    To get non EVM asset metadata; even though the selector is a
    deepEqualSelector, each instance of the `<TokenListItem>` were passing a
    different assetId to this selector which breaks reselect memoization and
    the selector had to recompute each time.
    Instead we are using a create selector factory pattern, where each
    instance of `<TokenListItem>` makes their own selector to then be used
    to get the non-evm asset metadata.
    
    Resources:
    [Reselect
    Docs](https://reselect.js.org/introduction/how-does-reselect-work#memoization-with-reselect)
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Nick Gambino <35090461+gambinish@users.noreply.github.com>

--- app/selectors/multichain/multichain.test.ts
+++ app/selectors/multichain/multichain.test.ts
@@ -13,7 +13,7 @@ import {
   selectSelectedAccountMultichainNetworkAggregatedBalance,
   selectSolanaAccountTransactions,
   selectMultichainHistoricalPrices,
-  selectNonEvmAssetById,
+  makeSelectNonEvmAssetById,
 } from './multichain';
 import { InternalAccount } from '@metamask/keyring-internal-api';
 import { CHAIN_IDS } from '@metamask/transaction-controller';
@@ -784,7 +784,8 @@ describe('MultichainNonEvm Selectors', () => {
     });
   });
 
-  describe('selectNonEvmAssetById', () => {
+  describe('makeSelectNonEvmAssetById', () => {
+    const selectNonEvmAssetById = makeSelectNonEvmAssetById();
     const mockAccountId = MOCK_ACCOUNT_BIP122_P2WPKH.id;
     const mockAssetId = MultichainNativeAssets.Bitcoin;
     const mockRate = '25000.00';
