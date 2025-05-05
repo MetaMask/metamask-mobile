5415e465ca0ff899a2cc8d2e216bd31f041fcdcf
Jyoti Puri <jyotipuri@gmail.com>
2025-05-05 20:57:59 +0530

    feat: Add validations for incoming batch request (#14993)
    ## **Description**
    
    Add validations for incoming batch request
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/MetaMask-planning/issues/4785
    
    ## **Manual testing steps**
    
    1. Try different error situations like submit batch request on mainnet
    etc
    2. Ensure that you get validation error back
    
    ## **Screenshots/Recordings**
    
    <img width="400" alt="Screenshot 2025-04-30 at 12 15 38 PM"
    src="https://github.com/user-attachments/assets/52d5bee8-1c0a-489f-bf2f-c3a0cfb7897e"
    />
    
    ## **Pre-merge author checklist**
    
    - [X] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [X] I've completed the PR template to the best of my ability
    - [X] I’ve included tests if applicable
    - [X] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [X] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/core/RPCMethods/createAsyncWalletMiddleware.ts
+++ app/core/RPCMethods/createAsyncWalletMiddleware.ts
@@ -6,15 +6,112 @@ import {
 import { JsonRpcMiddleware } from '@metamask/json-rpc-engine';
 import { JsonRpcParams } from '@metamask/eth-query';
 import { Hex, Json, JsonRpcRequest } from '@metamask/utils';
+import { JsonRpcError, rpcErrors } from '@metamask/rpc-errors';
 import { v4 as uuidv4 } from 'uuid';
 
+import ppomUtil from '../../lib/ppom/ppom-util';
 import Engine from '../Engine';
+
+const VERSION = '2.0.0';
+
+enum EIP5792ErrorCode {
+  UnsupportedNonOptionalCapability = 5700,
+  UnsupportedChainId = 5710,
+  UnknownBundleId = 5730,
+  RejectedUpgrade = 5750,
+}
+
+type JSONRPCRequest = JsonRpcRequest & {
+  networkClientId: string;
+  origin?: string;
+};
 
 export const getAccounts = async () => {
   const { AccountsController } = Engine.context;
   const selectedAddress = AccountsController.getSelectedAccount()?.address;
   return Promise.resolve(selectedAddress ? [selectedAddress] : []);
 };
+
+function validateSendCallsVersion(sendCalls: SendCalls) {
+  const { version } = sendCalls;
+
+  if (version !== VERSION) {
+    throw rpcErrors.invalidInput(
+      `Version not supported: Got ${version}, expected ${VERSION}`,
+    );
+  }
+}
+
+async function validateSendCallsChainId(
+  sendCalls: SendCalls,
+  req: JSONRPCRequest,
+) {
+  const { TransactionController, AccountsController } = Engine.context;
+  const { chainId } = sendCalls;
+  const { networkClientId } = req;
+
+  const dappChainId = Engine.controllerMessenger.call(
+    'NetworkController:getNetworkClientById',
+    networkClientId,
+  ).configuration.chainId;
+
+  if (chainId && chainId.toLowerCase() !== dappChainId.toLowerCase()) {
+    throw rpcErrors.invalidParams(
+      `Chain ID must match the dApp selected network: Got ${chainId}, expected ${dappChainId}`,
+    );
+  }
+
+  const from =
+    sendCalls.from ?? (AccountsController.getSelectedAccount()?.address as Hex);
+
+  const batchSupport = await TransactionController.isAtomicBatchSupported({
+    address: from,
+    chainIds: [dappChainId],
+  });
+
+  const chainBatchSupport = batchSupport?.[0];
+
+  if (!chainBatchSupport) {
+    throw new JsonRpcError(
+      EIP5792ErrorCode.UnsupportedChainId,
+      `EIP-7702 not supported on chain: ${dappChainId}`,
+    );
+  }
+}
+
+function validateCapabilities(sendCalls: SendCalls) {
+  const { calls, capabilities } = sendCalls;
+
+  const requiredTopLevelCapabilities = Object.keys(capabilities ?? {}).filter(
+    (name) => capabilities?.[name].optional !== true,
+  );
+
+  const requiredCallCapabilities = calls.flatMap((call) =>
+    Object.keys(call.capabilities ?? {}).filter(
+      (name) => call.capabilities?.[name].optional !== true,
+    ),
+  );
+
+  const requiredCapabilities = [
+    ...requiredTopLevelCapabilities,
+    ...requiredCallCapabilities,
+  ];
+
+  if (requiredCapabilities.length) {
+    throw new JsonRpcError(
+      EIP5792ErrorCode.UnsupportedNonOptionalCapability,
+      `Unsupported non-optional capabilities: ${requiredCapabilities.join(
+        ', ',
+      )}`,
+    );
+  }
+}
+
+async function validateSendCalls(sendCalls: SendCalls, req: JSONRPCRequest) {
+  validateSendCallsVersion(sendCalls);
+  await validateSendCallsChainId(sendCalls, req);
+  validateCapabilities(sendCalls);
+}
 
 export async function processSendCalls(
   params: SendCalls,
@@ -28,16 +125,20 @@ export async function processSendCalls(
   };
   const transactions = calls.map((call) => ({ params: call }));
 
+  await validateSendCalls(params, req as JSONRPCRequest);
+
   const from =
     paramFrom ?? (AccountsController.getSelectedAccount()?.address as Hex);
+  const securityAlertId = uuidv4();
 
   const { batchId: id } = await TransactionController.addTransactionBatch({
     from,
     networkClientId,
     origin,
-    securityAlertId: uuidv4(),
+    securityAlertId,
     transactions,
-    validateSecurity: () => Promise.resolve(),
+    validateSecurity:
+      ppomUtil.createValidatorForSecurityAlertId(securityAlertId),
   });
 
   return { id };
