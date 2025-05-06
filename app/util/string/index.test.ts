2a9152a7a17246403b2ee6a748ce1947d6170433
digiwand <20778143+digiwand@users.noreply.github.com>
2025-05-06 16:16:14 -0700

    refactor: parse signTypedData logic (#14378)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    Cleanup signTypedData parse and sanitize logic:
    - add related unit tests
    - move sanitizeMessage from util/string to utils/signature
    - memoized parseAndSanitizeSignTypedData
    - renamed methods
    - typedSignData → signTypedData to be consistent with EIP signTypedData
    - TypedDataMessage → signTypedData to avoid confusion as these methods
    are handling signTypedData request messageParams.data. That is message
    data, not data message
      - sanitizeString → escapeSpecialUnicode
    - reduce methods by 1. Combined parseSanitizeTypedDataMessage from
    util/string with the method from utils/signature
    - parseSignTypedDataFromSignatureRequest now returns {} with nullish
    values to be consistent with other parse methods
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-mobile/issues/14377
    
    ## **Manual testing steps**
    
    Test signTypedData confirmations v1 & v3/v4
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/util/string/index.test.ts
+++ app/util/string/index.test.ts
@@ -1,21 +1,86 @@
 import {
-  parseTypedSignDataMessage,
-  sanitizeMessage,
-  sanitizeString,
+  escapeSpecialUnicode,
+  isArrayType,
+  isSolidityType,
+  stripArrayType,
   stripMultipleNewlines,
+  stripOneLayerofNesting,
 } from '.';
-import { mockTypedSignV3Message } from '../test/confirm-data-helpers';
 
 describe('string utils', () => {
-  describe('sanitizeString', () => {
+  describe('escapeSpecialUnicode', () => {
     it('escapes all occurences of \u202E', () => {
-      const result = sanitizeString('test \u202E test \u202E test');
+      const result = escapeSpecialUnicode('test \u202E test \u202E test');
       expect(result).toEqual('test \\u202E test \\u202E test');
     });
 
     it('escapes all occurences of \u202D and \u202E', () => {
-      const result = sanitizeString('test \u202D test \u202E test \u202D test');
+      const result = escapeSpecialUnicode('test \u202D test \u202E test \u202D test');
       expect(result).toEqual('test \\u202D test \\u202E test \\u202D test');
+    });
+  });
+
+  describe('isArrayType', () => {
+    [
+      ['uint256[]', true],
+      ['address[5]', true],
+      ['string[][]', true],
+      ['bytes32[1][2]', true],
+      ['uint256', false],
+      ['address', false],
+      ['string', false],
+      ['bytes32', false],
+    ].forEach(([input, expected]) => {
+      it(`returns ${expected} for ${input}`, () => {
+        const result = isArrayType(input as string);
+        expect(result).toEqual(expected);
+      });
+    });
+  });
+
+  describe('isSolidityType', () => {
+    [
+      ['uint256[]', false],
+      ['address[5]', false],
+      ['string[][]', false],
+      ['bytes32[1][2]', false],
+      ['uint256', true],
+      ['address', true],
+      ['string', true],
+      ['bytes32', true],
+    ].forEach(([input, expected]) => {
+      it(`returns ${expected} for ${input}`, () => {
+        const result = isSolidityType(input as string);
+        expect(result).toEqual(expected);
+      });
+    });
+  });
+
+  describe('stripArrayType', () => {
+    [
+      ['uint256[]', 'uint256'],
+      ['address[5]', 'address'],
+      ['string[][]', 'string'],
+      ['bytes32[1][2]', 'bytes32'],
+    ].forEach(([input, expected]) => {
+      it(`removes the array type from ${input}`, () => {
+        const result = stripArrayType(input);
+        expect(result).toEqual(expected);
+      });
+    });
+  });
+
+  describe('stripOneLayerofNesting', () => {
+    [
+      ['uint256[1]', 'uint256'],
+      ['address[5]', 'address'],
+      ['string[1][2]', 'string[2]'],
+      ['bytes32[1][2]', 'bytes32[2]'],
+    ].forEach(([input, expected]) => {
+      it(`removes one layer of array nesting from ${input}`, () => {
+        const result = stripOneLayerofNesting(input);
+        expect(result).toEqual(expected);
+      });
     });
   });
 
@@ -26,134 +91,15 @@ describe('string utils', () => {
       );
       expect(result).toEqual('Secure ✅ \n');
     });
-  });
 
-  describe('sanitizeMessage', () => {
-    it('should throw an error if types is undefined', () => {
-      const { message, primaryType } = mockTypedSignV3Message;
-      expect(() => sanitizeMessage(message, primaryType, undefined)).toThrow(
-        'Invalid types definition',
-      );
+    it('returns undefined if the input is undefined', async () => {
+      const result = stripMultipleNewlines(undefined);
+      expect(result).toEqual(undefined);
     });
 
-    it('should throw an error if base type is not defined', () => {
-      const { message, types } = mockTypedSignV3Message;
-      expect(() => sanitizeMessage(message, '', types)).toThrow(
-        'Invalid primary type definition',
-      );
-    });
-
-    it('should return message data as expected', () => {
-      const { message, primaryType, types } = mockTypedSignV3Message;
-      const result = sanitizeMessage(message, primaryType, types);
-      expect(result).toStrictEqual({
-        value: {
-          from: {
-            value: {
-              name: { value: 'Cow', type: 'string' },
-              wallet: {
-                value: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
-                type: 'address',
-              },
-            },
-            type: 'Person',
-          },
-          to: {
-            value: {
-              name: { value: 'Bob', type: 'string' },
-              wallet: {
-                value: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
-                type: 'address',
-              },
-            },
-            type: 'Person',
-          },
-          contents: { value: 'Hello, Bob!', type: 'string' },
-        },
-        type: 'Mail',
-      });
-    });
-  });
-
-  describe('parseTypedSignDataMessage', () => {
-    const typedDataMsg =
-      '{"domain":{"chainId":97,"name":"Ether Mail","verifyingContract":"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC","version":"1"},"message":{"contents":"Hello, Bob!","from":{"name":"Cow","wallets":["0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826","0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF","0x06195827297c7A80a443b6894d3BDB8824b43896"]},"to":[{"name":"Bob","wallets":["0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB","0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57","0xB0B0b0b0b0b0B000000000000000000000000000"]}]},"primaryType":"Mail","types":{"EIP712Domain":[{"name":"name","type":"string"},{"name":"version","type":"string"},{"name":"chainId","type":"uint256"},{"name":"verifyingContract","type":"address"}],"Mail":[{"name":"from","type":"Person"},{"name":"to","type":"Person[]"},{"name":"contents","type":"string"}],"Person":[{"name":"name","type":"string"},{"name":"wallets","type":"address[]"}]}}';
-
-    it('should throw an error if types is undefined', () => {
-      const typedDataMsgWithoutTypes =
-        '{"domain":{"chainId":97,"name":"Ether Mail","verifyingContract":"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC","version":"1"},"message":{"contents":"Hello, Bob!","from":{"name":"Cow","wallets":["0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826","0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF","0x06195827297c7A80a443b6894d3BDB8824b43896"]},"to":[{"name":"Bob","wallets":["0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB","0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57","0xB0B0b0b0b0b0B000000000000000000000000000"]}]},"primaryType":"Mail"}';
-      expect(() => parseTypedSignDataMessage(typedDataMsgWithoutTypes)).toThrow(
-        'Invalid types definition',
-      );
-    });
-
-    it('should throw an error if base type is not defined', () => {
-      const typedSignDataWithoutBaseType =
-        '{"domain":{"chainId":97,"name":"Ether Mail","verifyingContract":"0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC","version":"1"},"message":{"contents":"Hello, Bob!","from":{"name":"Cow","wallets":["0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826","0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF","0x06195827297c7A80a443b6894d3BDB8824b43896"]},"to":[{"name":"Bob","wallets":["0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB","0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57","0xB0B0b0b0b0b0B000000000000000000000000000"]}]},"types":{"EIP712Domain":[{"name":"name","type":"string"},{"name":"version","type":"string"},{"name":"chainId","type":"uint256"},{"name":"verifyingContract","type":"address"}],"Mail":[{"name":"from","type":"Person"},{"name":"to","type":"Person[]"},{"name":"contents","type":"string"}],"Person":[{"name":"name","type":"string"},{"name":"wallets","type":"address[]"}]}}';
-      expect(() =>
-        parseTypedSignDataMessage(typedSignDataWithoutBaseType),
-      ).toThrow('Invalid primary type definition');
-    });
-
-    it('should return message data ignoring unknown types and trim new lines', () => {
-      const result = parseTypedSignDataMessage(typedDataMsg);
-      expect(result).toStrictEqual({
-        value: {
-          contents: { value: 'Hello, Bob!', type: 'string' },
-          from: {
-            value: {
-              name: { value: 'Cow', type: 'string' },
-              wallets: {
-                value: [
-                  {
-                    value: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
-                    type: 'address',
-                  },
-                  {
-                    value: '0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF',
-                    type: 'address',
-                  },
-                  {
-                    value: '0x06195827297c7A80a443b6894d3BDB8824b43896',
-                    type: 'address',
-                  },
-                ],
-                type: 'address[]',
-              },
-            },
-            type: 'Person',
-          },
-          to: {
-            value: [
-              {
-                value: {
-                  name: { value: 'Bob', type: 'string' },
-                  wallets: {
-                    value: [
-                      {
-                        value: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
-                        type: 'address',
-                      },
-                      {
-                        value: '0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57',
-                        type: 'address',
-                      },
-                      {
-                        value: '0xB0B0b0b0b0b0B000000000000000000000000000',
-                        type: 'address',
-                      },
-                    ],
-                    type: 'address[]',
-                  },
-                },
-                type: 'Person',
-              },
-            ],
-            type: 'Person[]',
-          },
-        },
-        type: 'Mail',
-      });
+    it('returns the value as is if it is not a string', async () => {
+      const result = stripMultipleNewlines(123);
+      expect(result).toEqual(123);
     });
   });
 });
