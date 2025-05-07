9c669b3458084a0befe73f04a6d131cabd839df8
Matthew Grainger <matthew.grainger@consensys.net>
2025-05-07 16:45:23 -0400

    feat: removed action route param to differentiate between pooled-staking and lending to rely on asset instead

--- app/components/UI/Earn/Views/EarnInputView/EarnInputView.tsx
+++ app/components/UI/Earn/Views/EarnInputView/EarnInputView.tsx
@@ -5,7 +5,7 @@ import {
   useRoute,
 } from '@react-navigation/native';
 import { formatEther } from 'ethers/lib/utils';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { View } from 'react-native';
 import { useSelector } from 'react-redux';
 import { strings } from '../../../../../../locales/i18n';
@@ -41,7 +41,6 @@ import {
   EARN_INPUT_VIEW_ACTIONS,
   EarnInputViewProps,
 } from './EarnInputView.types';
-import { getEarnInputViewTitle } from './utils';
 import { useEarnTokenDetails } from '../../hooks/useEarnTokenDetails';
 import useEarnInputHandlers from '../../hooks/useEarnInput';
 import { selectStablecoinLendingEnabledFlag } from '../../selectors/featureFlags';
@@ -51,12 +50,13 @@ import {
   getErc20SpendingLimit,
 } from '../../utils/tempLending';
 import BigNumber from 'bignumber.js';
+import { isSupportedLendingTokenByChainId } from '../../utils';
 
 const EarnInputView = () => {
   // navigation hooks
   const navigation = useNavigation();
   const route = useRoute<EarnInputViewProps['route']>();
-  const { action, token } = route.params;
+  const { token } = route.params;
 
   // state
   const [
@@ -390,9 +390,16 @@ const EarnInputView = () => {
   const navBarEventOptions = isStablecoinLendingEnabled
     ? earnNavBarEventOptions
     : stakingNavBarEventOptions;
-  const title = isStablecoinLendingEnabled
-    ? getEarnInputViewTitle(action)
-    : strings('stake.stake_eth');
+  const title = useMemo(() => {
+    if (
+      isStablecoinLendingEnabled &&
+      token?.chainId &&
+      isSupportedLendingTokenByChainId(token.symbol, token.chainId)
+    ) {
+      return strings('earn.deposit');
+    }
+    return strings('stake.stake');
+  }, [isStablecoinLendingEnabled, token.chainId, token.symbol]);
 
   useEffect(() => {
     navigation.setOptions(
@@ -406,7 +413,6 @@ const EarnInputView = () => {
     );
   }, [
     navigation,
-    action,
     token,
     theme.colors,
     navBarEventOptions,
