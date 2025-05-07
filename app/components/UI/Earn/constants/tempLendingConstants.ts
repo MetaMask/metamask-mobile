863ae94f63ffe51d454ba84e6591a080b78078b7
Matthew Grainger <matthew.grainger@consensys.net>
2025-05-07 17:41:50 -0400

    feat: fixed incorrect title being displayed on Ddeposit and withdrawal input views for stablecoin lending

---
+++ app/components/UI/Earn/constants/tempLendingConstants.ts
@@ -0,0 +1,3 @@
+// Temp: Will be replaced with earn-controller state
+// We don't want to display "withdraw" and "deposit more" buttons if user isn't lending.
+export const USER_HAS_LENDING_POSITIONS = true;
--- app/components/UI/Earn/utils/token/index.ts
+++ app/components/UI/Earn/utils/token/index.ts
@@ -3,11 +3,21 @@ import { CHAIN_IDS } from '@metamask/transaction-controller';
 import { getDecimalChainId } from '../../../../../util/networks';
 import { TokenI } from '../../../Tokens/types';
 
-// Temporary: Will be replaced with supported vaults from API request
 const HOLESKY_CHAIN_ID_HEX = '0x4268';
 
 const SUPPORTED_STAKING_TOKENS = new Set(['Ethereum']);
 
+// Temporary: Will be replaced with supported markets from API request
+export const SUPPORTED_LENDING_RECEIPT_TOKENS = new Set([
+  // Ethereum mainnet
+  'ADAI',
+  'AETHUSDC',
+  'AUSDT',
+  // Base
+  'aBasUSDC',
+]);
+
+// Temporary: Will be replaced with supported markets from API request
 export const SUPPORTED_LENDING_TOKENS = new Set(['DAI', 'USDC', 'USDT']);
 
 const SUPPORTED_EARN_TOKENS = new Set([
@@ -73,3 +83,11 @@ export const isSupportedLendingTokenByChainId = (
   chainId: string,
 ) =>
   SUPPORTED_LENDING_TOKENS.has(tokenSymbol) && SUPPORTED_CHAIN_IDS.has(chainId);
+
+// TODO: Add tests for this new util
+export const isSupportedLendingReceiptTokenByChainId = (
+  tokenSymbol: string,
+  chainId: string,
+) =>
+  SUPPORTED_LENDING_RECEIPT_TOKENS.has(tokenSymbol) &&
+  SUPPORTED_CHAIN_IDS.has(chainId);
