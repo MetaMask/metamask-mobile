54e9abd70a240de55a9cc3b0f79229e49679a6d9
Xiaoming Wang <7315988+dawnseeker8@users.noreply.github.com>
2025-05-07 18:37:46 +0100

    fix: 15108 create a wrapper for toChecksumAddress to prevent app crash (#15202)
    …dress validation
    
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    ## **Related issues**
    
    Fixes: #15108 
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Nico MASSART <NicolasMassart@users.noreply.github.com>

--- app/components/UI/AddCustomCollectible/index.tsx
+++ app/components/UI/AddCustomCollectible/index.tsx
@@ -4,7 +4,6 @@ import { Alert, Text, TextInput, View, StyleSheet } from 'react-native';
 import { fontStyles } from '../../../styles/common';
 import Engine from '../../../core/Engine';
 import { strings } from '../../../../locales/i18n';
-import { isValidAddress } from 'ethereumjs-util';
 import ActionView from '../ActionView';
 import { isSmartContractAddress } from '../../../util/transactions';
 import Device from '../../../util/device';
@@ -16,6 +15,7 @@ import { selectChainId } from '../../../selectors/networkController';
 import { selectSelectedInternalAccountFormattedAddress } from '../../../selectors/accountsController';
 import { getDecimalChainId } from '../../../util/networks';
 import { useMetrics } from '../../../components/hooks/useMetrics';
+import { isValidHexAddress } from '../../../util/address';
 
 // TODO: Replace "any" with type
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -113,7 +113,7 @@ const AddCustomCollectible = ({
 
   const validateCustomCollectibleAddress = async (): Promise<boolean> => {
     let validated = true;
-    const isValidEthAddress = isValidAddress(address);
+    const isValidEthAddress = isValidHexAddress(address);
     if (address.length === 0) {
       setWarningAddress(strings('collectible.address_cant_be_empty'));
       validated = false;
--- app/components/Views/confirmations/hooks/useAccountInfo.ts
+++ app/components/Views/confirmations/hooks/useAccountInfo.ts
@@ -1,18 +1,20 @@
-import { toChecksumAddress } from 'ethereumjs-util';
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
 import Engine from '../../../../core/Engine';
 import useAddressBalance from '../../../../components/hooks/useAddressBalance/useAddressBalance';
 import { selectInternalAccounts } from '../../../../selectors/accountsController';
-import { renderAccountName } from '../../../../util/address';
+import {
+  renderAccountName,
+  safeToChecksumAddress,
+} from '../../../../util/address';
 import { selectCurrentCurrency } from '../../../../selectors/currencyRateController';
 import { formatWithThreshold } from '../../../../util/assets';
 import I18n from '../../../../../locales/i18n';
 
 const useAccountInfo = (address: string) => {
   const internalAccounts = useSelector(selectInternalAccounts);
-  const activeAddress = toChecksumAddress(address);
+  const activeAddress = safeToChecksumAddress(address);
   const { addressBalance: accountBalance } = useAddressBalance(
     undefined,
     address,
