201506b16840f15899f9633c99adb466584f2d88
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2025-05-08 15:41:32 +0100

    remove todo

--- app/components/UI/DeFiPositions/position-types.ts
+++ app/components/UI/DeFiPositions/position-types.ts
@@ -1,3 +1,2 @@
-// TODO: Export from @metamask/assets-controllers
 export const PositionTypes = ['supply', 'stake', 'borrow', 'reward'] as const;
 export type PositionType = (typeof PositionTypes)[number];
