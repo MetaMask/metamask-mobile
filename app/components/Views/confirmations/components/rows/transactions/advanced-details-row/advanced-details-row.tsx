376aebd52d7c1f0832562e2665de3a0d3b43839e
Jyoti Puri <jyotipuri@gmail.com>
2025-05-09 16:37:21 +0530

    merge

--- app/components/Views/confirmations/components/rows/transactions/advanced-details-row/advanced-details-row.tsx
+++ app/components/Views/confirmations/components/rows/transactions/advanced-details-row/advanced-details-row.tsx
@@ -18,7 +18,7 @@ import { NameType } from '../../../../../../UI/Name/Name.types';
 import { useTransactionMetadataRequest } from '../../../../hooks/transactions/useTransactionMetadataRequest';
 import CustomNonceModal from '../../../../legacy/SendFlow/components/CustomNonceModal';
 import { useSmartAccountSwitchType } from '../../../../hooks/7702/useSmartAccountSwitchType';
-import ExpandableSection from '../../../UI/expandable-section';
+import Expandable from '../../../UI/expandable';
 import InfoRow from '../../../UI/info-row';
 import InfoSection from '../../../UI/info-row/info-section';
 import styleSheet from './advanced-details-row.styles';
