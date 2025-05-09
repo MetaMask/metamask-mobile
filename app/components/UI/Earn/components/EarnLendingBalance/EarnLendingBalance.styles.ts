84d9ad7f7de23df919c756a238d5388100990446
Matthew Grainger <matthew.grainger@consensys.net>
2025-05-09 17:30:36 -0400

    feat: fixing lint error in EarnLendingBalance.styles.ts

--- app/components/UI/Earn/components/EarnLendingBalance/EarnLendingBalance.styles.ts
+++ app/components/UI/Earn/components/EarnLendingBalance/EarnLendingBalance.styles.ts
@@ -1,11 +1,7 @@
 import { StyleSheet } from 'react-native';
-import { Theme } from '../../../../../util/theme/models';
 
-const styleSheet = (params: { theme: Theme }) => {
-  const { theme } = params;
-  const { colors } = theme;
-
-  return StyleSheet.create({
+const styleSheet = () =>
+  StyleSheet.create({
     container: {
       flexDirection: 'row',
       justifyContent: 'space-between',
@@ -18,6 +14,5 @@ const styleSheet = (params: { theme: Theme }) => {
       flex: 1,
     },
   });
-};
 
 export default styleSheet;
