fe6f277063723b95bbbce48e0904a352d31ccb5a
EtherWizard33 <etherwizard22@gmail.com>
2025-05-13 14:43:39 -0400

    style: remove unnecessary line

--- app/components/UI/Tabs/TabThumbnail/TabThumbnail.tsx
+++ app/components/UI/Tabs/TabThumbnail/TabThumbnail.tsx
@@ -59,7 +59,6 @@ const TabThumbnail = ({
     (account) => account.address.toLowerCase() === activeAddress?.toLowerCase(),
   );
   const { networkName, networkImageSource } = useNetworkInfo(tabTitle);
-
   const faviconSource = useFavicon(tab.url);
 
   return (
