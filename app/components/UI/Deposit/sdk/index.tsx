6816d046b943da86cf10e75e9de9b48ed30061cb
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2025-05-14 09:56:18 -0400

    feat(deposit): add feature flags selectors

--- app/components/UI/Deposit/sdk/index.tsx
+++ app/components/UI/Deposit/sdk/index.tsx
@@ -3,16 +3,17 @@ import React, {
   createContext,
   useContext,
   useMemo,
-  useEffect,
-  useState,
 } from 'react';
 import { useSelector } from 'react-redux';
-import { selectRemoteFeatureFlags } from '../../../../selectors/featureFlagController';
+import {
+  selectDepositProviderFrontendAuth,
+  selectDepositProviderApiKey,
+} from '../../../../selectors/featureFlagController/deposit';
 
 export interface DepositSDK {
   // TODO: Add properties and methods to the DepositSDK interface
-  depositsProviderApiKey: string | null;
-  depositsFrontendAuth: string | null;
+  providerApiKey: string | null;
+  providerFrontendAuth: string | null;
 }
 
 const DepositSDKContext = createContext<DepositSDK | undefined>(undefined);
@@ -21,31 +22,16 @@ export const DepositSDKProvider = ({
   value,
   ...props
 }: ProviderProps<DepositSDK>) => {
-  const remoteFeatureFlags = useSelector(selectRemoteFeatureFlags);
-  const [depositsProviderApiKey, setDepositsProviderApiKey] = useState<
-    string | null
-  >(null);
-  const [depositsFrontendAuth, setDepositsFrontendAuth] = useState<
-    string | null
-  >(null);
-
-  useEffect(() => {
-    const apiKey =
-      (remoteFeatureFlags['deposits-provider-api-key'] as string) || null;
-    const frontendAuth =
-      (remoteFeatureFlags['deposits-frontend-auth'] as string) || null;
-
-    setDepositsProviderApiKey(apiKey);
-    setDepositsFrontendAuth(frontendAuth);
-  }, [remoteFeatureFlags]);
+  const providerApiKey = useSelector(selectDepositProviderApiKey);
+  const providerFrontendAuth = useSelector(selectDepositProviderFrontendAuth);
 
   const contextValue = useMemo(
     (): DepositSDK => ({
       // TODO: Add properties and methods to the DepositSDK context value
-      depositsProviderApiKey,
-      depositsFrontendAuth,
+      providerApiKey,
+      providerFrontendAuth,
     }),
-    [depositsProviderApiKey, depositsFrontendAuth],
+    [providerApiKey, providerFrontendAuth],
   );
 
   return (
