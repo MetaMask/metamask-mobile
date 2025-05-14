edf520d174ecb62860be7dd50f4b86129e2c0895
AxelGes <axelges9@gmail.com>
2025-05-13 21:59:12 -0300

    feat: get provider keys from featureFlagsController

--- app/components/UI/Deposit/sdk/index.tsx
+++ app/components/UI/Deposit/sdk/index.tsx
@@ -3,10 +3,16 @@ import React, {
   createContext,
   useContext,
   useMemo,
+  useEffect,
+  useState,
 } from 'react';
+import { useSelector } from 'react-redux';
+import { selectRemoteFeatureFlags } from '../../../../selectors/featureFlagController';
 
 export interface DepositSDK {
   // TODO: Add properties and methods to the DepositSDK interface
+  depositsProviderApiKey: string | null;
+  depositsFrontendAuth: string | null;
 }
 
 const DepositSDKContext = createContext<DepositSDK | undefined>(undefined);
@@ -15,11 +21,31 @@ export const DepositSDKProvider = ({
   value,
   ...props
 }: ProviderProps<DepositSDK>) => {
+  const remoteFeatureFlags = useSelector(selectRemoteFeatureFlags);
+  const [depositsProviderApiKey, setDepositsProviderApiKey] = useState<
+    string | null
+  >(null);
+  const [depositsFrontendAuth, setDepositsFrontendAuth] = useState<
+    string | null
+  >(null);
+
+  useEffect(() => {
+    const apiKey =
+      (remoteFeatureFlags['deposits-provider-api-key'] as string) || null;
+    const frontendAuth =
+      (remoteFeatureFlags['deposits-frontend-auth'] as string) || null;
+
+    setDepositsProviderApiKey(apiKey);
+    setDepositsFrontendAuth(frontendAuth);
+  }, [remoteFeatureFlags]);
+
   const contextValue = useMemo(
     (): DepositSDK => ({
       // TODO: Add properties and methods to the DepositSDK context value
+      depositsProviderApiKey,
+      depositsFrontendAuth,
     }),
-    [],
+    [depositsProviderApiKey, depositsFrontendAuth],
   );
 
   return (
