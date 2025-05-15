b4a77e53d79d6f90e1274c6b008f490c6e703d64
Mathieu Artu <mathieu.artu@consensys.net>
2025-05-15 14:52:21 +0200

    test: multi-srp / multi-auth account syncing (core PR 5753)

--- app/actions/multiSrp/index.ts
+++ app/actions/multiSrp/index.ts
@@ -17,62 +17,74 @@ import { PerformanceEventNames } from '../../core/redux/slices/performance/const
 import { store } from '../../store';
 
 export async function importNewSecretRecoveryPhrase(mnemonic: string) {
-  const { KeyringController } = Engine.context;
+  const { KeyringController, UserStorageController } = Engine.context;
 
-  // Convert input mnemonic to codepoints
-  const mnemonicWords = mnemonic.toLowerCase().split(' ');
-  const inputCodePoints = new Uint16Array(
-    mnemonicWords.map((word) => wordlist.indexOf(word)),
-  );
+  try {
+    await Promise.all([
+      UserStorageController.setHasAccountSyncingSyncedAtLeastOnce(false),
+      UserStorageController.setIsAccountSyncingReadyToBeDispatched(false),
+    ]);
 
-  const hdKeyrings = (await KeyringController.getKeyringsByType(
-    ExtendedKeyringTypes.hd,
-  )) as HdKeyring[];
-
-  // TODO: This is temporary and will be removed once https://github.com/MetaMask/core/issues/5411 is resolved.
-  const alreadyImportedSRP = hdKeyrings.some((keyring) => {
-    // Compare directly with stored codepoints
-    const storedCodePoints = new Uint16Array(
-      // The mnemonic will not be undefined because there will be a keyring.
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      Buffer.from(keyring.mnemonic!).buffer,
+    // Convert input mnemonic to codepoints
+    const mnemonicWords = mnemonic.toLowerCase().split(' ');
+    const inputCodePoints = new Uint16Array(
+      mnemonicWords.map((word) => wordlist.indexOf(word)),
     );
 
-    if (inputCodePoints.length !== storedCodePoints.length) return false;
+    const hdKeyrings = (await KeyringController.getKeyringsByType(
+      ExtendedKeyringTypes.hd,
+    )) as HdKeyring[];
 
-    return inputCodePoints.every(
-      (code, index) => code === storedCodePoints[index],
+    // TODO: This is temporary and will be removed once https://github.com/MetaMask/core/issues/5411 is resolved.
+    const alreadyImportedSRP = hdKeyrings.some((keyring) => {
+      // Compare directly with stored codepoints
+      const storedCodePoints = new Uint16Array(
+        // The mnemonic will not be undefined because there will be a keyring.
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        Buffer.from(keyring.mnemonic!).buffer,
+      );
+
+      if (inputCodePoints.length !== storedCodePoints.length) return false;
+
+      return inputCodePoints.every(
+        (code, index) => code === storedCodePoints[index],
+      );
+    });
+
+    if (alreadyImportedSRP) {
+      throw new Error('This mnemonic has already been imported.');
+    }
+
+    const newKeyring = await KeyringController.addNewKeyring(
+      ExtendedKeyringTypes.hd,
+      {
+        mnemonic,
+        numberOfAccounts: 1,
+      },
     );
-  });
 
-  if (alreadyImportedSRP) {
-    throw new Error('This mnemonic has already been imported.');
+    const [newAccountAddress] = await KeyringController.withKeyring(
+      {
+        id: newKeyring.id,
+      },
+      async ({ keyring }) => keyring.getAccounts(),
+    );
+
+    ///: BEGIN:ONLY_INCLUDE_IF(beta)
+    const multichainClient = MultichainWalletSnapFactory.createClient(
+      WalletClientType.Solana,
+    );
+
+    await multichainClient.addDiscoveredAccounts(newKeyring.id);
+    ///: END:ONLY_INCLUDE_IF
+
+    return Engine.setSelectedAddress(newAccountAddress);
+  } finally {
+    await Promise.all([
+      UserStorageController.setHasAccountSyncingSyncedAtLeastOnce(true),
+      UserStorageController.setIsAccountSyncingReadyToBeDispatched(true),
+    ]);
   }
-
-  const newKeyring = await KeyringController.addNewKeyring(
-    ExtendedKeyringTypes.hd,
-    {
-      mnemonic,
-      numberOfAccounts: 1,
-    },
-  );
-
-  const [newAccountAddress] = await KeyringController.withKeyring(
-    {
-      id: newKeyring.id,
-    },
-    async ({ keyring }) => keyring.getAccounts(),
-  );
-
-  ///: BEGIN:ONLY_INCLUDE_IF(beta)
-  const multichainClient = MultichainWalletSnapFactory.createClient(
-    WalletClientType.Solana,
-  );
-
-  await multichainClient.addDiscoveredAccounts(newKeyring.id);
-  ///: END:ONLY_INCLUDE_IF
-
-  return Engine.setSelectedAddress(newAccountAddress);
 }
 
 export async function createNewSecretRecoveryPhrase() {
