aee9dace65beaa2b7ad118756de9249b43396450
ieow <cwlee0@gmail.com>
2025-05-16 18:01:59 +0800

    feat: disable wizard tour

--- app/components/Views/Wallet/index.tsx
+++ app/components/Views/Wallet/index.tsx
@@ -36,7 +36,6 @@ import NotificationsService from '../../../util/notifications/services/Notificat
 import Engine from '../../../core/Engine';
 import CollectibleContracts from '../../UI/CollectibleContracts';
 import { MetaMetricsEvents } from '../../../core/Analytics';
-import OnboardingWizard from '../../UI/OnboardingWizard';
 import ErrorBoundary from '../ErrorBoundary';
 import { useTheme } from '../../../util/theme';
 import Routes from '../../../constants/navigation/Routes';
@@ -235,12 +234,6 @@ const Wallet = ({
    */
   const ticker = useSelector(selectEvmTicker);
   /**
-   * Current onboarding wizard step
-   */
-  // TODO: Replace "any" with type
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const wizardStep = useSelector((state: any) => state.wizard.step);
-  /**
    * Provider configuration for the current selected network
    */
   const providerConfig = useSelector(selectProviderConfig);
@@ -435,12 +428,11 @@ const Wallet = ({
       networkOnboardingState,
     );
 
-    if (wizardStep > 0 || (!networkOnboarded && prevChainId !== chainId)) {
-      // Do not check since it will conflict with the onboarding wizard and/or network onboarding
+    if (!networkOnboarded && prevChainId !== chainId) {
+      // Do not check since it will conflict with the onboarding and/or network onboarding
       return;
     }
   }, [
-    wizardStep,
     navigation,
     chainId,
     // TODO: Is this providerConfig.rpcUrl needed in this useEffect dependencies?
@@ -787,26 +779,10 @@ const Wallet = ({
     [styles],
   );
 
-  /**
-   * Return current step of onboarding wizard if not step 5 nor 0
-   */
-  const renderOnboardingWizard = useCallback(
-    () =>
-      [1, 2, 3, 4, 5, 6, 7].includes(wizardStep) && (
-        <OnboardingWizard
-          navigation={navigation}
-          coachmarkRef={walletRef.current}
-        />
-      ),
-    [navigation, wizardStep],
-  );
-
   return (
     <ErrorBoundary navigation={navigation} view="Wallet">
       <View style={baseStyles.flexGrow}>
         {selectedInternalAccount ? renderContent() : renderLoader()}
-
-        {renderOnboardingWizard()}
       </View>
     </ErrorBoundary>
   );
