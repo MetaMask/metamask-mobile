9e2cedc1991dda86f18273e412cf40ef546bdba6
infiniteflower <139582705+infiniteflower@users.noreply.github.com>
2025-05-19 23:11:14 +0900

    fix: cp-7.47.0 tokens small amount crashing bridge (#15419)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    This PR fixes an issue where the Bridge would crash if the user selected
    a destination token that had a small balance (e.g. appears as < 0.00001)
    
    ## **Related issues**
    
    Fixes: https://consensyssoftware.atlassian.net/browse/MMS-2454
    
    ## **Manual testing steps**
    
    1. Go to Bridge
    2. Select a dest token that displays "< 0.00001" 
    3. Bridge should not crash
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    
    
    https://github.com/user-attachments/assets/96a8b740-c525-4042-9873-caffd0351c32
    
    
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/UI/Bridge/components/BridgeSourceTokenSelector/BridgeSourceTokenSelector.test.tsx
+++ app/components/UI/Bridge/components/BridgeSourceTokenSelector/BridgeSourceTokenSelector.test.tsx
@@ -131,7 +131,7 @@ describe('BridgeSourceTokenSelector', () => {
 
     expect(setSourceToken).toHaveBeenCalledWith({
       address: '0x0000000000000000000000000000000000000001',
-      balance: '1',
+      balance: '1.0',
       balanceFiat: '$20000',
       chainId: '0x1',
       decimals: 18,
