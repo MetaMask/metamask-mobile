ac133e15fbc9060aea5310acf5d8943e5fceebae
Monte Lai <monte.lai@consensys.net>
2025-05-19 21:37:23 +0800

    fix: restore snap and imported srps when changing password (#15237)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    This PR adds the restoration of first party snap accounts and imported
    srps when changing password.
    
    Changes:
    1. added `restoreImportedSrp` `restoreSnapAccounts` to Vault.js and
    updated the change password logic to use these.
    2. Added new tests to Vault
    3. Fix deadlock that could happen when awaiting a promise in
    MultichainSnapClient.
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    4. What is the improvement/solution?
    -->
    
    ## **Related issues**
    Fixes: https://github.com/MetaMask/metamask-mobile/issues/15258
    Fixes: https://github.com/MetaMask/metamask-mobile/issues/15233
    Resolves:
    https://consensyssoftware.atlassian.net/browse/MMMULTISRP-208?atlOrigin=eyJpIjoiMmM0MDMzNDRlYzdlNDJmZTg3ODNhMzUzNzlkOWZmZDIiLCJwIjoiaiJ9
    
    ## **Manual testing steps**
    
    1. Using a fresh wallet.
    2. Create a new solana account
    3. Import a private key
    5. Import a qr hardware wallet
    6. import a ledger wallet
    7. Go to settings --> security
    8. Go through change password
    9. Login to the wallet and see that accounts are still there. 
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: sethkfman <10342624+sethkfman@users.noreply.github.com>
    Co-authored-by: Gustavo Antunes <17601467+gantunesr@users.noreply.github.com>

--- app/core/Vault.js
+++ app/core/Vault.js
@@ -2,6 +2,16 @@ import Engine from './Engine';
 import Logger from '../util/Logger';
 import { KeyringTypes } from '@metamask/keyring-controller';
 import { withLedgerKeyring } from './Ledger/Ledger';
+import {
+  MultichainWalletSnapFactory,
+  WalletClientType,
+} from './SnapKeyring/MultichainWalletSnapClient';
+import {
+  BtcAccountType,
+  BtcScope,
+  SolAccountType,
+  SolScope,
+} from '@metamask/keyring-api';
 
 /**
  * Restore the given serialized QR keyring.
@@ -36,14 +46,72 @@ export const restoreLedgerKeyring = async (serializedLedgerKeyring) => {
   }
 };
 
+export const restoreImportedSrp = async (seedPhrase, numberOfAccounts) => {
+  const { KeyringController } = Engine.context;
+  try {
+    const { id: keyringId } = await KeyringController.addNewKeyring(
+      KeyringTypes.hd,
+      {
+        mnemonic: seedPhrase,
+      },
+    );
+
+    for (let i = 0; i < numberOfAccounts; i++) {
+      await KeyringController.withKeyring(
+        { id: keyringId },
+        async ({ keyring }) => await keyring.addAccounts(1),
+      );
+    }
+
+    return keyringId;
+  } catch (e) {
+    Logger.error(
+      e,
+      'error while trying to restore imported srp accounts on recreate vault',
+    );
+  }
+};
+
+export const restoreSnapAccounts = async (accountType, entropySource) => {
+  let walletClientType;
+  let scope;
+  switch (accountType) {
+    case SolAccountType.DataAccount: {
+      walletClientType = WalletClientType.Solana;
+      scope = SolScope.Mainnet;
+      break;
+    }
+    case BtcAccountType.P2wpkh: {
+      walletClientType = WalletClientType.Bitcoin;
+      scope = BtcScope.Mainnet;
+      break;
+    }
+    default:
+      throw new Error('Unsupported account type');
+  }
+
+  try {
+    const client = MultichainWalletSnapFactory.createClient(walletClientType);
+    await client.createAccount({
+      entropySource,
+      scope,
+    });
+  } catch (e) {
+    Logger.error(
+      e,
+      'error while trying to restore snap accounts on recreate vault',
+    );
+  }
+};
+
 /**
  * Returns current vault seed phrase
  * It does it using an empty password or a password set by the user
  * depending on the state the app is currently in
  */
-export const getSeedPhrase = async (password = '') => {
+export const getSeedPhrase = async (password = '', keyringId) => {
   const { KeyringController } = Engine.context;
-  return await KeyringController.exportSeedPhrase(password);
+  return await KeyringController.exportSeedPhrase(password, keyringId);
 };
 
 /**
@@ -58,8 +126,35 @@ export const recreateVaultWithNewPassword = async (
   newPassword,
   selectedAddress,
 ) => {
-  const { KeyringController } = Engine.context;
-  const seedPhrase = await getSeedPhrase(password);
+  const { KeyringController, AccountsController } = Engine.context;
+  const hdKeyringsWithMetadata = KeyringController.state.keyrings
+    .map((keyring, _index) => ({
+      ...keyring,
+      ///: BEGIN:ONLY_INCLUDE_IF(multi-srp)
+      metadata: KeyringController.state.keyringsMetadata?.[_index] || {},
+      ///: END:ONLY_INCLUDE_IF
+    }))
+    .filter((keyring) => keyring.type === KeyringTypes.hd);
+
+  const seedPhrases = await Promise.all(
+    hdKeyringsWithMetadata.map(async (keyring) => {
+      try {
+        return await getSeedPhrase(password, keyring.metadata.id);
+      } catch (e) {
+        Logger.error(
+          e,
+          'error while trying to get seed phrase on recreate vault',
+        );
+        return null;
+      }
+    }),
+  );
+  const [primaryKeyringSeedPhrase, ...otherSeedPhrases] = seedPhrases;
+  if (!primaryKeyringSeedPhrase) {
+    throw new Error('error while trying to get seed phrase on recreate vault');
+  }
+
+  // START: Getting accounts to be reimported
 
   let importedAccounts = [];
   try {
@@ -83,9 +178,19 @@ export const recreateVaultWithNewPassword = async (
     );
   }
 
+  const firstPartySnapAccounts =
+    AccountsController.listMultichainAccounts().filter(
+      (account) => account.options?.entropySource,
+    );
+
   // Get props to restore vault
-  const hdKeyring = KeyringController.state.keyrings[0];
-  const existingAccountCount = hdKeyring.accounts.length;
+  const hdKeyringsAccountCount = hdKeyringsWithMetadata.map(
+    (keyring) => keyring.accounts.length,
+  );
+  const [primaryKeyringAccountCount, ...otherKeyringAccountCounts] =
+    hdKeyringsAccountCount;
+
+  // END: Getting accounts to be reimported
 
   const serializedLedgerKeyring = hasKeyringType(
     KeyringController.state,
@@ -101,7 +206,14 @@ export const recreateVaultWithNewPassword = async (
     : undefined;
 
   // Recreate keyring with password given to this method
-  await KeyringController.createNewVaultAndRestore(newPassword, seedPhrase);
+  await KeyringController.createNewVaultAndRestore(
+    newPassword,
+    primaryKeyringSeedPhrase,
+  );
+  const [newPrimaryKeyringMetadata] = KeyringController.state.keyringsMetadata;
+  const newPrimaryKeyringId = newPrimaryKeyringMetadata.id;
+
+  // START: Restoring keyrings
 
   if (serializedQrKeyring !== undefined) {
     await restoreQRKeyring(serializedQrKeyring);
@@ -111,7 +223,7 @@ export const recreateVaultWithNewPassword = async (
   }
 
   // Create previous accounts again
-  for (let i = 0; i < existingAccountCount - 1; i++) {
+  for (let i = 0; i < primaryKeyringAccountCount - 1; i++) {
     await KeyringController.addNewAccount();
   }
 
@@ -125,6 +237,36 @@ export const recreateVaultWithNewPassword = async (
   } catch (e) {
     Logger.error(e, 'error while trying to import accounts on recreate vault');
   }
+
+  // recreate import srp accounts
+  const importedSrpKeyringIds = [];
+  ///: BEGIN:ONLY_INCLUDE_IF(multi-srp)
+  for (const [index, otherSeedPhrase] of otherSeedPhrases.entries()) {
+    const importedSrpKeyring = await restoreImportedSrp(
+      otherSeedPhrase,
+      otherKeyringAccountCounts[index],
+    );
+    importedSrpKeyringIds.push(importedSrpKeyring);
+  }
+  ///: END:ONLY_INCLUDE_IF(multi-srp)
+
+  const newHdKeyringIds = [newPrimaryKeyringId, ...importedSrpKeyringIds];
+  // map old keyring id to new keyring id
+  const keyringIdMap = new Map();
+  for (const [index, keyring] of hdKeyringsWithMetadata.entries()) {
+    keyringIdMap.set(keyring.metadata.id, newHdKeyringIds[index]);
+  }
+
+  // recreate snap accounts
+  for (const snapAccount of firstPartySnapAccounts) {
+    await restoreSnapAccounts(
+      snapAccount.type,
+      keyringIdMap.get(snapAccount.options.entropySource),
+    );
+  }
+
+  // END: Restoring keyrings
+
   const recreatedKeyrings = KeyringController.state.keyrings;
   // Reselect previous selected account if still available
   for (const keyring of recreatedKeyrings) {
