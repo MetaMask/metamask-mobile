dc540b1314f93318d3d68104a096e571d0fb9abd
Patryk Łucka <patryk.lucka@gmail.com>
2025-05-19 20:44:41 +0200

    chore(multi-srp): remove code fences (#15438)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    This PR removes code fences for Multi-SRP feature.
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/constants/navigation/Routes.ts
+++ app/constants/navigation/Routes.ts
@@ -116,9 +116,7 @@ const Routes = {
     TOKEN_SORT: 'TokenSort',
     TOKEN_FILTER: 'TokenFilter',
     CHANGE_IN_SIMULATION_MODAL: 'ChangeInSimulationModal',
-    ///: BEGIN:ONLY_INCLUDE_IF(multi-srp)
     SELECT_SRP: 'SelectSRP',
-    ///: END:ONLY_INCLUDE_IF
   },
   BROWSER: {
     HOME: 'BrowserTabHome',
@@ -196,11 +194,9 @@ const Routes = {
   IDENTITY: {
     TURN_ON_BACKUP_AND_SYNC: 'TurnOnBackupAndSync',
   },
-  ///: BEGIN:ONLY_INCLUDE_IF(multi-srp)
   MULTI_SRP: {
     IMPORT: 'ImportSRPView',
   },
-  ///: END:ONLY_INCLUDE_IF
   ///: BEGIN:ONLY_INCLUDE_IF(external-snaps)
   SNAPS: {
     SNAPS_SETTINGS_LIST: 'SnapsSettingsList',
