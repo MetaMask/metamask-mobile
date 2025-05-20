2a048c1c856aecde5b748399ada454190b43652c
Matthew Grainger <matthew.grainger@consensys.net>
2025-05-20 10:20:10 -0400

    feat: added receipt token balance and abstracted staking and lending balance components into single earn balance entry-point

--- app/components/UI/Earn/components/EarnLendingBalance/index.tsx
+++ app/components/UI/Earn/components/EarnLendingBalance/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { View } from 'react-native';
 import Button, {
   ButtonSize,
@@ -12,13 +12,46 @@ import { useNavigation } from '@react-navigation/native';
 import Routes from '../../../../../constants/navigation/Routes';
 import { useSelector } from 'react-redux';
 import { selectStablecoinLendingEnabledFlag } from '../../selectors/featureFlags';
+import AssetElement from '../../../AssetElement';
+import BadgeWrapper, {
+  BadgePosition,
+} from '../../../../../component-library/components/Badges/BadgeWrapper';
+import Badge, {
+  BadgeVariant,
+} from '../../../../../component-library/components/Badges/Badge';
+import { NetworkBadgeSource } from '../../../AssetOverview/Balance/Balance';
+import NetworkAssetLogo from '../../../NetworkAssetLogo';
+import Text, {
+  TextVariant,
+} from '../../../../../component-library/components/Texts/Text';
+import { selectNetworkConfigurationByChainId } from '../../../../../selectors/networkController';
+import { RootState } from '../../../../../reducers';
+import { Hex } from '@metamask/utils';
+import useLendingTokenPair from '../../hooks/useLendingTokenPair';
+import AvatarToken from '../../../../../component-library/components/Avatars/Avatar/variants/AvatarToken';
+import { AvatarSize } from '../../../../../component-library/components/Avatars/Avatar';
+
+export const EARN_LENDING_BALANCE_TEST_IDS = {
+  RECEIPT_TOKEN_BALANCE_ASSET_LOGO: 'receipt-token-balance-asset-logo',
+  RECEIPT_TOKEN_LABEL: 'receipt-token-label',
+};
 
 export interface EarnLendingBalanceProps {
   asset: TokenI;
+  displayBalance?: boolean;
+  displayButtons?: boolean;
 }
 
-const EarnLendingBalance = ({ asset }: EarnLendingBalanceProps) => {
+const EarnLendingBalance = ({
+  asset,
+  displayBalance = true,
+  displayButtons = true,
+}: EarnLendingBalanceProps) => {
   const { styles } = useStyles(styleSheet, {});
+
+  const networkConfigurationByChainId = useSelector((state: RootState) =>
+    selectNetworkConfigurationByChainId(state, asset.chainId as Hex),
+  );
 
   const isStablecoinLendingEnabled = useSelector(
     selectStablecoinLendingEnabledFlag,
@@ -26,11 +59,36 @@ const EarnLendingBalance = ({ asset }: EarnLendingBalanceProps) => {
 
   const navigation = useNavigation();
 
+  const { receiptToken } = useLendingTokenPair(asset);
+
+  const renderNetworkAvatar = useCallback(() => {
+    if (asset.isNative) {
+      return (
+        <NetworkAssetLogo
+          chainId={asset.chainId as Hex}
+          style={styles.ethLogo}
+          ticker={asset.ticker ?? asset.symbol}
+          big={false}
+          biggest={false}
+          testID={asset.name}
+        />
+      );
+    }
+
+    return (
+      <AvatarToken
+        name={asset.symbol}
+        imageSource={{ uri: asset.image }}
+        size={AvatarSize.Md}
+      />
+    );
+  }, [asset, styles.ethLogo]);
+
   const handleNavigateToWithdrawalInputScreen = () => {
     navigation.navigate('StakeScreens', {
       screen: Routes.STAKING.UNSTAKE,
       params: {
-        token: asset,
+        token: receiptToken,
       },
     });
   };
@@ -47,21 +105,53 @@ const EarnLendingBalance = ({ asset }: EarnLendingBalanceProps) => {
   if (!isStablecoinLendingEnabled) return null;
 
   return (
-    <View style={styles.container}>
-      <Button
-        variant={ButtonVariants.Secondary}
-        style={styles.button}
-        size={ButtonSize.Lg}
-        label={strings('earn.withdraw')}
-        onPress={handleNavigateToWithdrawalInputScreen}
-      />
-      <Button
-        variant={ButtonVariants.Secondary}
-        style={styles.button}
-        size={ButtonSize.Lg}
-        label={strings('earn.deposit_more')}
-        onPress={handleNavigateToDepositInputScreen}
-      />
+    <View>
+      {displayBalance && (
+        <AssetElement
+          asset={receiptToken as TokenI}
+          balance={receiptToken.balanceFiat}
+          secondaryBalance={receiptToken.balanceFormatted}
+        >
+          <BadgeWrapper
+            badgePosition={BadgePosition.BottomRight}
+            style={styles.badgeWrapper}
+            badgeElement={
+              <Badge
+                variant={BadgeVariant.Network}
+                imageSource={NetworkBadgeSource(receiptToken.chainId as Hex)}
+                name={networkConfigurationByChainId?.name}
+              />
+            }
+          >
+            {renderNetworkAvatar()}
+          </BadgeWrapper>
+          <Text
+            style={styles.balances}
+            variant={TextVariant.BodyLGMedium}
+            testID={EARN_LENDING_BALANCE_TEST_IDS.RECEIPT_TOKEN_LABEL}
+          >
+            {receiptToken.name}
+          </Text>
+        </AssetElement>
+      )}
+      {displayButtons && (
+        <View style={styles.container}>
+          <Button
+            variant={ButtonVariants.Secondary}
+            style={styles.button}
+            size={ButtonSize.Lg}
+            label={strings('earn.withdraw')}
+            onPress={handleNavigateToWithdrawalInputScreen}
+          />
+          <Button
+            variant={ButtonVariants.Secondary}
+            style={styles.button}
+            size={ButtonSize.Lg}
+            label={strings('earn.deposit_more')}
+            onPress={handleNavigateToDepositInputScreen}
+          />
+        </View>
+      )}
     </View>
   );
 };
