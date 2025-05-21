5e3a371eacf46bd95341e2b74b814766753cd8ff
Monte Lai <monte.lai@consensys.net>
2025-05-22 04:43:52 +0800

    fix: show accounts in AddNewAccount (#15208)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    This PR fixes the issue where pressing show accounts in the bottom sheet
    of the AddNewAccount does nothing.
    
    Changed:
    1. `AddNewAccount` now uses the SrpListItem when displaying only one
    keyring.
    2.  Css styles of `AddNewAccount` and `SrpListItem`
    3. Add testId prop to `SrpListItem` 
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    4. What is the improvement/solution?
    -->
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-mobile/issues/15403
    Fixes:
    https://consensyssoftware.atlassian.net/browse/MMMULTISRP-174?atlOrigin=eyJpIjoiYWRkYTUzMTUxN2QwNGZjOThhZTY5NTg0OTI5NjFmZDkiLCJwIjoiaiJ9
    
    ## **Manual testing steps**
    
    1.  Have 2 srps in the wallet
    2. AccountActions and click on Ethereum account
    5. click 'show accounts'
    6. See that the account list is expanded. 
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    
    https://github.com/user-attachments/assets/a8b80f61-dc27-4d5d-9f7c-011a05dbffa2
    
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    
    https://github.com/user-attachments/assets/617b8cca-426b-4f7a-9d3c-a6cfd9d93bba
    
    
    <!-- [screenshots/recordings] -->
    
    
    
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/Views/AddNewAccount/AddNewAccount.tsx
+++ app/components/Views/AddNewAccount/AddNewAccount.tsx
@@ -11,9 +11,6 @@ import { SafeAreaView, View } from 'react-native';
 
 // External dependencies.
 import SheetHeader from '../../../component-library/components/Sheet/SheetHeader';
-import Icon, {
-  IconName,
-} from '../../../component-library/components/Icons/Icon';
 import { strings } from '../../../../locales/i18n';
 
 // Internal dependencies
@@ -27,7 +24,6 @@ import Text, {
 import Input from '../../../component-library/components/Form/TextField/foundation/Input';
 import { useStyles } from '../../hooks/useStyles';
 import styleSheet from './AddNewAccount.styles';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 import { useSelector } from 'react-redux';
 import { selectHDKeyrings } from '../../../selectors/keyringController';
 import Button, {
@@ -46,6 +42,7 @@ import BottomSheet, {
 import { useNavigation } from '@react-navigation/native';
 import Routes from '../../../constants/navigation/Routes';
 import { selectInternalAccounts } from '../../../selectors/accountsController';
+import SRPListItem from '../../UI/SRPListItem';
 import { getMultichainAccountName } from '../../../core/SnapKeyring/utils/getMultichainAccountName';
 
 const AddNewAccount = ({ route }: AddNewAccountProps) => {
@@ -67,6 +64,15 @@ const AddNewAccount = ({ route }: AddNewAccountProps) => {
   const hasMultipleSRPs = hdKeyrings.length > 1;
   const [showSRPList, setShowSRPList] = useState(false);
   const [error, setError] = useState<string>('');
+
+  const { keyringToDisplay, keyringIndex } = useMemo(() => {
+    const keyring =
+      hdKeyrings.find((kr) => kr.metadata.id === keyringId) ?? hdKeyrings[0];
+    return {
+      keyringToDisplay: keyring,
+      keyringIndex: hdKeyrings.indexOf(keyring) + 1,
+    };
+  }, [hdKeyrings, keyringId]);
 
   const onBack = () => {
     navigate(Routes.SHEET.ACCOUNT_SELECTOR);
@@ -118,17 +124,6 @@ const AddNewAccount = ({ route }: AddNewAccountProps) => {
     setAccountName(getMultichainAccountName(scope, clientType));
   }, [clientType, scope]);
 
-  const hdKeyringIndex = useMemo(
-    () =>
-      hdKeyrings.findIndex((keyring) => keyring.metadata.id === keyringId) + 1,
-    [hdKeyrings, keyringId],
-  );
-
-  const numberOfAccounts = useMemo(() => {
-    const keyring = hdKeyrings.find((kr) => kr.metadata.id === keyringId);
-    return keyring ? keyring.accounts.length : 0;
-  }, [hdKeyrings, keyringId]);
-
   const addAccountTitle = useMemo(() => {
     switch (clientType) {
       case WalletClientType.Bitcoin:
@@ -171,85 +166,68 @@ const AddNewAccount = ({ route }: AddNewAccountProps) => {
             <SRPList onKeyringSelect={(id) => onKeyringSelection(id)} />
           ) : (
             <View style={styles.base}>
-              <Fragment>
-                <View style={styles.accountInputContainer}>
-                  <Input
-                    testID={AddNewAccountIds.NAME_INPUT}
-                    textVariant={TextVariant.BodyMDMedium}
-                    style={styles.accountInput}
-                    value={accountName}
-                    onChangeText={(newName: string) => {
-                      setAccountName(newName);
-                    }}
-                    placeholder={accountName}
-                    placeholderTextColor={colors.text.default}
-                    onSubmitEditing={onSubmit}
-                  />
-                </View>
-                {hasMultipleSRPs && (
-                  <View style={styles.srpSelectorContainer}>
-                    <Text variant={TextVariant.BodyMDMedium}>
-                      {strings('accounts.select_secret_recovery_phrase')}
-                    </Text>
-                    <TouchableOpacity
-                      style={styles.srpSelector}
-                      onPress={() => setShowSRPList(true)}
-                      testID={AddNewAccountIds.SRP_SELECTOR}
-                    >
-                      <View style={styles.srp}>
-                        <View>
-                          <Text variant={TextVariant.BodyMDMedium}>{`${strings(
-                            'accounts.secret_recovery_phrase',
-                          )} ${hdKeyringIndex}`}</Text>
-                        </View>
-                        <View>
-                          <Text
-                            variant={TextVariant.BodyMD}
-                            color={TextColor.Primary}
-                          >{`${strings(
-                            'accounts.show_accounts',
-                          )} ${numberOfAccounts} ${strings(
-                            'accounts.accounts',
-                          )}`}</Text>
-                        </View>
-                      </View>
-                      <Icon
-                        style={styles.srpArrow}
-                        name={IconName.ArrowRight}
-                      />
-                    </TouchableOpacity>
-                    <Text variant={TextVariant.BodySM}>
-                      {strings('accounts.add_new_hd_account_helper_text')}
-                    </Text>
-                  </View>
-                )}
-                <View style={styles.footerContainer}>
-                  <Button
-                    testID={AddNewAccountIds.CANCEL}
-                    loading={isLoading}
-                    style={styles.button}
-                    variant={ButtonVariants.Secondary}
-                    onPress={onBack}
-                    labelTextVariant={TextVariant.BodyMD}
-                    label={strings('accounts.cancel')}
-                  />
-                  <Button
-                    testID={AddNewAccountIds.CONFIRM}
-                    loading={isLoading}
-                    isDisabled={isLoading || isDuplicateName}
-                    style={styles.button}
-                    variant={ButtonVariants.Primary}
-                    onPress={onSubmit}
-                    labelTextVariant={TextVariant.BodyMD}
-                    label={strings('accounts.add')}
-                  />
-                </View>
-                {error && (
-                  <Text variant={TextVariant.BodySM} color={TextColor.Error}>
-                    {error}
+              <View style={styles.accountInputContainer}>
+                <Input
+                  testID={AddNewAccountIds.NAME_INPUT}
+                  textVariant={TextVariant.BodyMDMedium}
+                  style={styles.accountInput}
+                  value={accountName}
+                  onChangeText={(newName: string) => {
+                    setAccountName(newName);
+                  }}
+                  placeholder={accountName}
+                  placeholderTextColor={colors.text.default}
+                  onSubmitEditing={onSubmit}
+                />
+              </View>
+              {hasMultipleSRPs && (
+                <View style={styles.srpSelectorContainer}>
+                  <Text variant={TextVariant.BodyMDMedium}>
+                    {strings('accounts.select_secret_recovery_phrase')}
                   </Text>
-                )}
-              </Fragment>
+                  <View style={styles.srpSelector}>
+                    <SRPListItem
+                      keyring={keyringToDisplay}
+                      name={`${strings(
+                        'accounts.secret_recovery_phrase',
+                      )} ${keyringIndex}`}
+                      onActionComplete={() => {
+                        setShowSRPList(true);
+                      }}
+                      testID={AddNewAccountIds.SRP_SELECTOR}
+                    />
+                  </View>
+                  <Text variant={TextVariant.BodySM}>
+                    {strings('accounts.add_new_hd_account_helper_text')}
+                  </Text>
+                </View>
+              )}
+              <View style={styles.footerContainer}>
+                <Button
+                  testID={AddNewAccountIds.CANCEL}
+                  loading={isLoading}
+                  style={styles.button}
+                  variant={ButtonVariants.Secondary}
+                  onPress={onBack}
+                  labelTextVariant={TextVariant.BodyMD}
+                  label={strings('accounts.cancel')}
+                />
+                <Button
+                  testID={AddNewAccountIds.CONFIRM}
+                  loading={isLoading}
+                  isDisabled={isLoading || isDuplicateName}
+                  style={styles.button}
+                  variant={ButtonVariants.Primary}
+                  onPress={onSubmit}
+                  labelTextVariant={TextVariant.BodyMD}
+                  label={strings('accounts.add')}
+                />
+              </View>
+              {error && (
+                <Text variant={TextVariant.BodySM} color={TextColor.Error}>
+                  {error}
+                </Text>
+              )}
             </View>
           )}
         </Fragment>
