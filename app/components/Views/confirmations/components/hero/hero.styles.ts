7f09173397a2847390b9c5d48ae602c6e075e0a6
digiwand <20778143+digiwand@users.noreply.github.com>
2025-05-21 08:42:36 -0400

    refactor: abstract Hero from TokenHero

---
+++ app/components/Views/confirmations/components/hero/hero.styles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+import { Theme } from '../../../../../util/theme/models';
+
+const styleSheet = (params: {
+  theme: Theme;
+  vars: { isFlatConfirmation: boolean };
+}) => {
+  const { theme, vars } = params;
+  const { isFlatConfirmation } = vars;
+
+  return StyleSheet.create({
+    base: {
+      paddingBottom: 16,
+      paddingTop: isFlatConfirmation ? 16 : 0,
+    },
+    subtitle: {
+      textAlign: 'center',
+      color: theme.colors.text.alternative,
+    },
+    title: {
+      paddingTop: 4,
+    },
+    titleText: {
+      textAlign: 'center',
+    },
+  });
+};
+
+export default styleSheet;
