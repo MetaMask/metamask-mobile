2cdef33aedf8bd3f6ea30dd22b096a471d57f478
Akhun Akhand <akhun.web3@gmail.com>
2025-05-22 00:37:27 +0600

    Fix incorrect import in ResetPassword test

--- app/components/Views/ResetPassword/index.test.tsx
+++ app/components/Views/ResetPassword/index.test.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import ChoosePassword from './';
-import configureMockStore from 'redux-mock-store';
-import { Provider } from 'react-redux';
+import ResetPassword from './';
+import renderWithProvider from '../../../util/test/renderWithProvider';
 import { backgroundState } from '../../../util/test/initial-root-state';
 
-const mockStore = configureMockStore();
 const initialState = {
   user: {
     passwordSet: true,
@@ -15,15 +12,12 @@ const initialState = {
     backgroundState,
   },
 };
-const store = mockStore(initialState);
 
-describe('ChoosePassword', () => {
+describe('ResetPassword', () => {
   it('should render correctly', () => {
-    const wrapper = shallow(
-      <Provider store={store}>
-        <ChoosePassword />
-      </Provider>,
-    );
-    expect(wrapper).toMatchSnapshot();
+    const { toJSON } = renderWithProvider(<ResetPassword />, {
+      state: initialState,
+    }, false);
+    expect(toJSON()).toMatchSnapshot();
   });
 });
