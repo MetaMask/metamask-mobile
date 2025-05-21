de0fdcda2023e11bff32b9b446a72ad3946480a1
Charly Chevalier <charly.chevalier@consensys.net>
2025-05-21 13:54:29 +0200

    fix: move importAccountFromPrivateKey to its own file to break circular dep

--- app/util/address/index.ts
+++ app/util/address/index.ts
@@ -37,21 +37,17 @@ import type { AddressBookControllerState } from '@metamask/address-book-controll
 import {
   isEqualCaseInsensitive,
   type NetworkType,
-  toChecksumHexAddress,
 } from '@metamask/controller-utils';
 import type {
   NetworkClientId,
   NetworkState,
 } from '@metamask/network-controller';
 import {
-  AccountImportStrategy,
   KeyringObject,
   KeyringTypes,
 } from '@metamask/keyring-controller';
 import { type Hex, isHexString } from '@metamask/utils';
 import PREINSTALLED_SNAPS from '../../lib/snaps/preinstalled-snaps';
-import { endTrace, trace, TraceName, TraceOperation } from '../trace';
-import { getTraceTags } from '../sentry/tags';
 
 const {
   ASSET: { ERC721, ERC1155 },
@@ -153,40 +149,6 @@ export function renderAccountName(
   }
 
   return renderShortAddress(address);
-}
-
-/**
- * Imports an account from a private key
- *
- * @param {String} private_key - String corresponding to a private key
- * @returns {Promise} - Returns a promise
- */
-
-export async function importAccountFromPrivateKey(private_key: string) {
-  trace({
-    name: TraceName.ImportEvmAccount,
-    op: TraceOperation.ImportAccount,
-    tags: getTraceTags(store.getState()),
-  });
-
-  const { KeyringController } = Engine.context;
-  // Import private key
-  let pkey = private_key;
-  // Handle PKeys with 0x
-  if (pkey.length === 66 && pkey.substr(0, 2) === '0x') {
-    pkey = pkey.substr(2);
-  }
-  const importedAccountAddress =
-    await KeyringController.importAccountWithStrategy(
-      AccountImportStrategy.privateKey,
-      [pkey],
-    );
-  const checksummedAddress = toChecksumHexAddress(importedAccountAddress);
-  Engine.setSelectedAddress(checksummedAddress);
-
-  endTrace({
-    name: TraceName.ImportEvmAccount,
-  });
 }
 
 export function isHDOrFirstPartySnapAccount(account: InternalAccount) {
