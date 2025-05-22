92fc3f223b3e3e36f3ae3f92bb8a1f4a497a7f19
Monte Lai <monte.lai@consensys.net>
2025-05-22 22:04:03 +0800

    refactor: extract AddNewAccount component from the bottom sheet to be more reusable

--- app/components/Views/AddNewAccount/AddNewAccount.types.ts
+++ app/components/Views/AddNewAccount/AddNewAccount.types.ts
@@ -1,17 +1,13 @@
 import { CaipChainId } from '@metamask/utils';
 import { WalletClientType } from '../../../core/SnapKeyring/MultichainWalletSnapClient';
+import { InternalAccount } from '@metamask/keyring-internal-api';
 
 /**
  * AddNewAccountProps props.
  */
 export interface AddNewAccountProps {
-  /**
-   * Props that are passed in while navigating to screen.
-   */
-  route: {
-    params?: {
-      scope: CaipChainId;
-      clientType: WalletClientType;
-    };
-  };
+  scope?: CaipChainId;
+  clientType?: WalletClientType;
+  onActionComplete?: (account: InternalAccount) => void;
+  onBack?: () => void;
 }
