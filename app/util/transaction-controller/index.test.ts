d2174699d1a778dd2dc781f6b5c7e77b9a7aa02d
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-05-22 12:38:04 +0200

    Sanitize gas values before transaction updater functions

--- app/util/transaction-controller/index.test.ts
+++ app/util/transaction-controller/index.test.ts
@@ -1,4 +1,7 @@
-import { WalletDevice } from '@metamask/transaction-controller';
+import {
+  WalletDevice,
+  type TransactionMeta,
+} from '@metamask/transaction-controller';
 //eslint-disable-next-line import/no-namespace
 import * as TransactionControllerUtils from './index';
 import Engine from '../../core/Engine';
@@ -21,8 +24,33 @@ jest.mock('../../store', () => ({
   },
 }));
 
-const TRANSACTION_MOCK = { from: '0x0', to: '0x1', value: '0x0' };
+const ID_MOCK = 'testId';
+const EIP_1559_TRANSACTION_PARAMS_MOCK = {
+  from: '0x0',
+  to: '0x1',
+  value: '0x2',
+  type: '0x2',
+  gas: '0x3',
+  maxFeePerGas: '0x4',
+  maxPriorityFeePerGas: '0x5',
+};
+const TRANSACTION_PARAMS_LEGACY_MOCK = {
+  from: '0x9',
+  to: '0x8',
+  value: '0x7',
+  type: '0x0',
+  gas: '0x6',
+  gasPrice: '0x5',
+};
 const NETWORK_CLIENT_ID_MOCK = 'testNetworkClientId';
+const EIP1559_TRANSACTION_META_MOCK = {
+  id: ID_MOCK,
+  txParams: EIP_1559_TRANSACTION_PARAMS_MOCK,
+} as TransactionMeta;
+const LEGACY_TRANSACTION_META_MOCK = {
+  id: ID_MOCK,
+  txParams: TRANSACTION_PARAMS_LEGACY_MOCK,
+} as TransactionMeta;
 
 const TRANSACTION_OPTIONS_MOCK = {
   deviceConfirmedOn: WalletDevice.MM_MOBILE,
@@ -33,6 +61,8 @@ const TRANSACTION_OPTIONS_MOCK = {
 jest.mock('../../core/Engine', () => ({
   context: {
     TransactionController: {
+      getTransactions: jest.fn(),
+
       addTransaction: jest.fn(),
       estimateGas: jest.fn(),
       estimateGasFee: jest.fn(),
@@ -59,35 +89,47 @@ describe('Transaction Controller Util', () => {
 
   describe('addTransaction', () => {
     it('should call addTransaction with correct parameters', async () => {
-      await addTransaction(TRANSACTION_MOCK, TRANSACTION_OPTIONS_MOCK);
+      await addTransaction(
+        EIP_1559_TRANSACTION_PARAMS_MOCK,
+        TRANSACTION_OPTIONS_MOCK,
+      );
 
       expect(
         Engine.context.TransactionController.addTransaction,
-      ).toHaveBeenCalledWith(TRANSACTION_MOCK, TRANSACTION_OPTIONS_MOCK);
+      ).toHaveBeenCalledWith(
+        EIP_1559_TRANSACTION_PARAMS_MOCK,
+        TRANSACTION_OPTIONS_MOCK,
+      );
     });
   });
 
   describe('estimateGas', () => {
     it('should call estimateGas with correct parameters', async () => {
-      await estimateGas(TRANSACTION_MOCK, NETWORK_CLIENT_ID_MOCK);
+      await estimateGas(
+        EIP_1559_TRANSACTION_PARAMS_MOCK,
+        NETWORK_CLIENT_ID_MOCK,
+      );
 
       expect(
         Engine.context.TransactionController.estimateGas,
-      ).toHaveBeenCalledWith(TRANSACTION_MOCK, NETWORK_CLIENT_ID_MOCK);
+      ).toHaveBeenCalledWith(
+        EIP_1559_TRANSACTION_PARAMS_MOCK,
+        NETWORK_CLIENT_ID_MOCK,
+      );
     });
   });
 
   describe('estimateGasFee', () => {
     it('should call estimateGasFee with correct parameters', async () => {
       await estimateGasFee({
-        transactionParams: TRANSACTION_MOCK,
+        transactionParams: EIP_1559_TRANSACTION_PARAMS_MOCK,
         chainId: '0x1',
       });
 
       expect(
         Engine.context.TransactionController.estimateGasFee,
       ).toHaveBeenCalledWith({
-        transactionParams: TRANSACTION_MOCK,
+        transactionParams: EIP_1559_TRANSACTION_PARAMS_MOCK,
         chainId: '0x1',
       });
     });
@@ -198,6 +240,89 @@ describe('Transaction Controller Util', () => {
       );
 
       expect(releaseLockMock).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('Gas property sanitization', () => {
+    function testSanitization<Params extends unknown[]>({
+      testName,
+      updaterFunction,
+      updaterFunctionName,
+      updaterFunctionParams,
+      transactionMock,
+      expectedCallArgumentIndex = 0,
+      paramToSanitize = [],
+    }: {
+      testName: string;
+      updaterFunction: (...args: Params) => void;
+      updaterFunctionName: 'updateTransaction' | 'updateEditableParams';
+      updaterFunctionParams: Params;
+      transactionMock: TransactionMeta;
+      expectedCallArgumentIndex?: number;
+      paramToSanitize?: string[];
+    }) {
+      it(testName, () => {
+        jest
+          .spyOn(Engine.context.TransactionController, 'getTransactions')
+          .mockReturnValue([transactionMock]);
+
+        updaterFunction(...updaterFunctionParams);
+
+        const calledArguments = (
+          Engine.context.TransactionController?.[
+            updaterFunctionName as keyof typeof Engine.context.TransactionController
+          ] as jest.Mock
+        ).mock.calls[0];
+
+        paramToSanitize.forEach((param: string) => {
+          expect(
+            calledArguments[expectedCallArgumentIndex][param],
+          ).not.toBeDefined();
+        });
+      });
+    }
+
+    testSanitization({
+      testName:
+        'updateTransaction removes maxFeePerGas and maxPriorityFeePerGas values for legacy transactions',
+      updaterFunction: TransactionControllerUtils.updateTransaction,
+      updaterFunctionName: 'updateTransaction',
+      updaterFunctionParams: [EIP1559_TRANSACTION_META_MOCK, 'testNote'],
+      transactionMock: LEGACY_TRANSACTION_META_MOCK,
+      expectedCallArgumentIndex: 0,
+      paramToSanitize: ['maxFeePerGas', 'maxPriorityFeePerGas'],
+    });
+
+    testSanitization({
+      testName: 'updateTransaction removes gasPrice for EIP-1559 transactions',
+      updaterFunction: TransactionControllerUtils.updateTransaction,
+      updaterFunctionName: 'updateTransaction',
+      updaterFunctionParams: [EIP1559_TRANSACTION_META_MOCK, 'testNote'],
+      transactionMock: EIP1559_TRANSACTION_META_MOCK,
+      expectedCallArgumentIndex: 0,
+      paramToSanitize: ['gasPrice'],
+    });
+
+    testSanitization({
+      testName:
+        'updateEditableParams removes maxFeePerGas and maxPriorityFeePerGas values for legacy transactions',
+      updaterFunction: TransactionControllerUtils.updateEditableParams,
+      updaterFunctionName: 'updateEditableParams',
+      updaterFunctionParams: [ID_MOCK, EIP_1559_TRANSACTION_PARAMS_MOCK],
+      transactionMock: LEGACY_TRANSACTION_META_MOCK,
+      expectedCallArgumentIndex: 1,
+      paramToSanitize: ['maxFeePerGas', 'maxPriorityFeePerGas'],
+    });
+
+    testSanitization({
+      testName:
+        'updateEditableParams removes gasPrice for EIP-1559 transactions',
+      updaterFunction: TransactionControllerUtils.updateEditableParams,
+      updaterFunctionName: 'updateEditableParams',
+      updaterFunctionParams: [ID_MOCK, EIP_1559_TRANSACTION_PARAMS_MOCK],
+      transactionMock: EIP1559_TRANSACTION_META_MOCK,
+      expectedCallArgumentIndex: 1,
+      paramToSanitize: ['gasPrice'],
     });
   });
 });
