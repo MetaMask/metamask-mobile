f3858f9769e65d5210c949f16b6919baacca8655
Owen Craston <owen.craston@consensys.net>
2025-05-22 20:13:05 +0100

    test srp track event for srp quiz

--- app/components/Views/Quiz/SRPQuiz/SRPQuiz.test.tsx
+++ app/components/Views/Quiz/SRPQuiz/SRPQuiz.test.tsx
@@ -12,8 +12,12 @@ import SRPQuiz, { SRPQuizProps } from './SRPQuiz';
 import Routes from '../../../../constants/navigation/Routes';
 import { MOCK_ACCOUNTS_CONTROLLER_STATE } from '../../../../util/test/accountsControllerTestUtils';
 import { KeyringTypes } from '@metamask/keyring-controller';
+import { useMetrics } from '../../../../components/hooks/useMetrics';
+import { MetaMetricsEvents } from '../../../../core/Analytics';
+import { MetricsEventBuilder } from '../../../../core/Analytics/MetricsEventBuilder';
 
 const mockNavigate = jest.fn();
+const mockTrackEvent = jest.fn();
 
 jest.mock('@react-navigation/native', () => ({
   useNavigation: () => ({
@@ -21,27 +25,71 @@ jest.mock('@react-navigation/native', () => ({
   }),
 }));
 
+jest.mock('../../../../components/hooks/useMetrics', () => ({
+  useMetrics: jest.fn(),
+}));
+
 const renderSRPQuiz = (props: SRPQuizProps, completeQuiz: boolean = true) => {
   const mockStore = configureMockStore();
   const selectedAccount =
     MOCK_ACCOUNTS_CONTROLLER_STATE.internalAccounts.selectedAccount;
+  const selectedAddress =
+    MOCK_ACCOUNTS_CONTROLLER_STATE.internalAccounts.accounts[selectedAccount]
+      .address;
+
+  // Create a second address that will be associated with the second HD keyring
+  const secondAddress = '0x2222222222222222222222222222222222222222';
+
   const initialState = {
     engine: {
       backgroundState: {
-        AccountsController: MOCK_ACCOUNTS_CONTROLLER_STATE,
+        AccountsController: {
+          ...MOCK_ACCOUNTS_CONTROLLER_STATE,
+          internalAccounts: {
+            ...MOCK_ACCOUNTS_CONTROLLER_STATE.internalAccounts,
+            accounts: {
+              ...MOCK_ACCOUNTS_CONTROLLER_STATE.internalAccounts.accounts,
+              [selectedAccount]: {
+                ...MOCK_ACCOUNTS_CONTROLLER_STATE.internalAccounts.accounts[
+                  selectedAccount
+                ],
+                metadata: {
+                  ...MOCK_ACCOUNTS_CONTROLLER_STATE.internalAccounts.accounts[
+                    selectedAccount
+                  ].metadata,
+                  keyring: {
+                    type: KeyringTypes.hd,
+                  },
+                },
+              },
+            },
+          },
+        },
         KeyringController: {
           isUnlocked: false,
           keyrings: [
             {
               type: KeyringTypes.hd,
-              accounts: [
-                MOCK_ACCOUNTS_CONTROLLER_STATE.internalAccounts.accounts[
-                  selectedAccount
-                ].address,
-              ],
+              accounts: ['0x1111111111111111111111111111111111111111'],
               metadata: {
                 id: 'hd-1',
-                name: '',
+                name: 'HD Keyring 1',
+              },
+            },
+            {
+              type: KeyringTypes.hd,
+              accounts: [selectedAddress],
+              metadata: {
+                id: 'hd-2',
+                name: 'HD Keyring 2',
+              },
+            },
+            {
+              type: KeyringTypes.hd,
+              accounts: [secondAddress],
+              metadata: {
+                id: 'hd-3',
+                name: 'HD Keyring 3',
               },
             },
           ],
@@ -91,6 +139,14 @@ const renderSRPQuiz = (props: SRPQuizProps, completeQuiz: boolean = true) => {
 };
 
 describe('SRPQuiz', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    (useMetrics as jest.Mock).mockReturnValue({
+      trackEvent: mockTrackEvent,
+      createEventBuilder: MetricsEventBuilder.createEventBuilder,
+    });
+  });
+
   it('passes the keyringId to the SRPQuiz', async () => {
     const keyringId = '123';
     const props = {
@@ -109,6 +165,37 @@ describe('SRPQuiz', () => {
           shouldUpdateNav: true,
           keyringId,
         },
+      );
+    });
+  });
+
+  it('tracks metrics events with hdEntropyIndex when completing quiz', async () => {
+    const keyringId = '123';
+    const props = {
+      route: {
+        params: { keyringId },
+      },
+    };
+
+    renderSRPQuiz(props, true);
+
+    await waitFor(() => {
+      expect(mockTrackEvent).toHaveBeenCalledWith(
+        MetricsEventBuilder.createEventBuilder(
+          MetaMetricsEvents.REVEAL_SRP_INITIATED,
+        )
+          .addProperties({
+            hd_entropy_index: 1,
+          })
+          .build(),
+      );
+
+      expect(mockTrackEvent).toHaveBeenCalledWith(
+        MetricsEventBuilder.createEventBuilder(MetaMetricsEvents.REVEAL_SRP_CTA)
+          .addProperties({
+            hd_entropy_index: 1,
+          })
+          .build(),
       );
     });
   });
