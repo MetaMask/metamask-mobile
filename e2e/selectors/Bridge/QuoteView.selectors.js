e05dbe756ea0d15ea2710a3f149294f0f92fb88a
Davide Brocchetto <davide.brocchetto@consensys.net>
2025-05-23 14:55:29 +0200

    test: Adding e2e tests for bridge  (#15067)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    This PR adds two Bridge tests to Mobile:
    - Bridge ETH (Mainnet)->ETH (Base)
    - Bridge ETH (Mainnet)->SOL(Solana)
    
    
    
    https://github.com/user-attachments/assets/e1fe81bf-0b7e-4c50-af5a-12e538641e39
    
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1.  yarn && yarn build:test
    2. yarn test:e2e:android:debug:run
    e2e/specs/bridge/bridge-action-smoke.spec.js
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

---
+++ e2e/selectors/Bridge/QuoteView.selectors.js
@@ -0,0 +1,12 @@
+import enContent from '../../../locales/languages/en.json';
+
+export const QuoteViewSelectorText = {
+  QUOTES: enContent.bridge.quote,
+  BRIDGE_TO: 'Bridge to',
+  CONTINUE: 'Continue',
+};
+
+export const QuoteViewSelectorIDs = {
+  TOKEN_SEARCH_INPUT: 'bridge-token-search-input',
+  EXPAND_QUOTE_DETAILS: 'expand-quote-details',
+};
