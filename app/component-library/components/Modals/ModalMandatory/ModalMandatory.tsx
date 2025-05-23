1ed630e4bf9765298b4ad87db0a9258164e9bc1c
ieow <cwlee0@gmail.com>
2025-05-23 17:19:12 +0800

    feat: new onboarding carousel and use terms
    Co-authored-by: smgv <gpatra1996@gmail.com>

--- app/component-library/components/Modals/ModalMandatory/ModalMandatory.tsx
+++ app/component-library/components/Modals/ModalMandatory/ModalMandatory.tsx
@@ -12,16 +12,12 @@ import { WebView } from '@metamask/react-native-webview';
 
 // External dependencies.
 import ButtonPrimary from '../../Buttons/Button/variants/ButtonPrimary';
-import Text from '../../Texts/Text';
+import Text, { TextVariant, TextColor } from '../../Texts/Text';
 import { useStyles } from '../../../hooks';
 import { useTheme } from '../../../../util/theme';
-import ReusableModal, {
-  ReusableModalRef,
-} from '../../../../components/UI/ReusableModal';
 import Checkbox from '../../../../component-library/components/Checkbox';
 import { IconName } from '../../../../component-library/components/Icons/Icon';
 import ButtonIcon from '../../../../component-library/components/Buttons/ButtonIcon';
-
 // Internal dependencies
 import {
   WEBVIEW_SCROLL_END_EVENT,
@@ -34,12 +30,13 @@ import {
 } from './ModalMandatory.types';
 import stylesheet from './ModalMandatory.styles';
 import { TermsOfUseModalSelectorsIDs } from '../../../../../e2e/selectors/Onboarding/TermsOfUseModal.selectors';
+import BottomSheet, { BottomSheetRef } from '../../BottomSheets/BottomSheet';
 
 const ModalMandatory = ({ route }: MandatoryModalProps) => {
   const { colors } = useTheme();
   const { styles } = useStyles(stylesheet, {});
-  const modalRef = useRef<ReusableModalRef>(null);
   const webViewRef = useRef<WebView>(null);
+  const bottomSheetRef = useRef<BottomSheetRef>(null);
 
   const [isWebViewLoaded, setIsWebViewLoaded] = useState<boolean>(false);
   const [isScrollEnded, setIsScrollEnded] = useState<boolean>(false);
@@ -122,13 +119,28 @@ const ModalMandatory = ({ route }: MandatoryModalProps) => {
     };
   }, []);
 
-  const renderHeader = () => (
-    <Text style={styles.headerText}>{headerTitle}</Text>
-  );
-
   const onPress = () => {
-    modalRef.current?.dismissModal(onAccept);
+    bottomSheetRef.current?.onCloseBottomSheet(onAccept);
   };
+
+  const onClose = () => {
+    bottomSheetRef.current?.onCloseBottomSheet();
+  };
+
+  const renderHeader = () => (
+    <View style={styles.headerContainer}>
+      <View style={styles.headerEmpty} />
+      <Text variant={TextVariant.HeadingMD} color={TextColor.Default}>
+        {headerTitle}
+      </Text>
+      <ButtonIcon
+        testID={TermsOfUseModalSelectorsIDs.CLOSE_BUTTON}
+        onPress={onClose}
+        iconName={IconName.Close}
+        hitSlop={12}
+      />
+    </View>
+  );
 
   const onMessage = (event: { nativeEvent: { data: string } }) => {
     if (event.nativeEvent.data === WEBVIEW_SCROLL_END_EVENT) {
@@ -152,6 +164,7 @@ const ModalMandatory = ({ route }: MandatoryModalProps) => {
         testID={TermsOfUseModalSelectorsIDs.SCROLL_ARROW_BUTTON}
         onPress={scrollToEnd}
         iconName={IconName.ArrowDown}
+        iconColor={colors.primary.default}
         hitSlop={12}
       />
     </View>
@@ -231,7 +244,7 @@ const ModalMandatory = ({ route }: MandatoryModalProps) => {
   };
 
   return (
-    <ReusableModal ref={modalRef} style={styles.screen} isInteractable={false}>
+    <BottomSheet ref={bottomSheetRef} shouldNavigateBack>
       <View style={styles.modal} testID={containerTestId}>
         {renderHeader()}
         <View
@@ -247,7 +260,9 @@ const ModalMandatory = ({ route }: MandatoryModalProps) => {
           testID={TermsOfUseModalSelectorsIDs.CHECKBOX}
         >
           <Checkbox onPress={handleSelect} isChecked={isCheckboxSelected} />
-          <Text style={styles.checkboxText}>{checkboxText}</Text>
+          <Text variant={TextVariant.BodySMMedium} color={TextColor.Default}>
+            {checkboxText}
+          </Text>
         </TouchableOpacity>
         <ButtonPrimary
           label={buttonText}
@@ -265,10 +280,16 @@ const ModalMandatory = ({ route }: MandatoryModalProps) => {
         />
         {isScrollToEndNeeded && renderScrollEndButton()}
         {footerHelpText ? (
-          <Text style={styles.footerHelpText}>{footerHelpText}</Text>
+          <Text
+            style={styles.footerHelpText}
+            variant={TextVariant.BodySM}
+            color={TextColor.Alternative}
+          >
+            {footerHelpText}
+          </Text>
         ) : null}
       </View>
-    </ReusableModal>
+    </BottomSheet>
   );
 };
 
