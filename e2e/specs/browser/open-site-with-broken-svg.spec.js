484297a27c693c044e7a02b40596de0392ae6527
Tyschenko <yaroslav.tyschenko@consensys.net>
2025-05-25 22:34:23 +0100

    E2E test for the fix of the issue 15523

---
+++ e2e/specs/browser/open-site-with-broken-svg.spec.js
@@ -0,0 +1,34 @@
+'use strict';
+import Browser from '../../pages/Browser/BrowserView';
+import TabBarComponent from '../../pages/wallet/TabBarComponent';
+import {loginToApp} from '../../viewHelper';
+import FixtureBuilder from '../../fixtures/fixture-builder';
+import {withFixtures} from '../../fixtures/fixture-helper';
+import {SmokeConfirmations} from '../../tags';
+import TestHelpers from '../../helpers';
+
+describe(SmokeConfirmations('Search for a website with broken SVG and open it'), () => {
+  beforeAll(async () => {
+    jest.setTimeout(150000);
+    await TestHelpers.reverseServerPort();
+  });
+
+  it('Open app.ens.domains', async () => {
+    await withFixtures(
+      {
+        dapp: false,
+        fixture: new FixtureBuilder().build(),
+        restartDevice: true,
+        testSpecificMock: false,
+      },
+      async () => {
+        await loginToApp();
+        await TabBarComponent.tapBrowser();
+        await Browser.navigateToURL("https://app.ens.domains");
+        await Browser.searchForUrl("app.ens.domains");
+        await TestHelpers.delay(5000); // Wait for SVG images to load and not crash
+        await expect(element(by.text('E'))).not.toExist(); // Validate that SVG is loaded and text placeholder logo is not displayed
+      },
+    );
+  });
+});
