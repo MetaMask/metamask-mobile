e4ae348e8e97f80e3eb8d7f4367edc86752cd319
Matthew Grainger <matthew.grainger@consensys.net>
2025-05-27 16:33:34 -0400

    feat: fixed incorrect hardcoded token mappings

--- app/components/UI/Earn/utils/token/index.ts
+++ app/components/UI/Earn/utils/token/index.ts
@@ -19,9 +19,9 @@ const SUPPORTED_STAKING_TOKENS = new Set(['Ethereum']);
 // Temporary: Will be replaced with supported markets from API request
 export const SUPPORTED_LENDING_RECEIPT_TOKENS = new Set([
   // Ethereum mainnet
-  'ADAI',
+  'aEthDAI',
   'AETHUSDC',
-  'AUSDT',
+  'aEthUSDT',
   // Base
   'aBasUSDC',
   // Linea
@@ -33,6 +33,37 @@ export const SUPPORTED_LENDING_RECEIPT_TOKENS = new Set([
   'aArbUSDT', // USDT
 ]);
 
+// Temp: To help with internal testing.
+export const TOKEN_ADDRESSES: Record<string, Record<string, string>> = {
+  [CHAIN_IDS.MAINNET]: {
+    DAI: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+    USDT: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+    USDC: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    aEthDAI: '0x018008bfb33d285247A21d44E50697654f754e63',
+    AETHUSDC: '0x98C23E9d8f34FEFb1B7BD6a91B7FF122F4e16F5c',
+    aEthUSDT: '0x23878914EFE38d27C4D67Ab83ed1b93A74D4086a',
+  },
+  [CHAIN_IDS.BASE]: {
+    USDC: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
+    aBasUSDC: '0x4e65fE4DbA92790696d040ac24Aa414708F5c0AB',
+  },
+  [CHAIN_IDS.LINEA_MAINNET]: {
+    USDC: '0x176211869cA2b568f2A7D4EE941E073a821EE1ff',
+    USDT: '0xA219439258ca9da29E9Cc4cE5596924745e12B93',
+    aLinUSDC: '0x374D7860c4f2f604De0191298dD393703Cce84f3',
+    aLinUSDT: '0x88231dfEC71D4FF5c1e466D08C321944A7adC673',
+  },
+  [CHAIN_IDS.ARBITRUM]: {
+    USDC: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
+    DAI: '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1',
+    USDT: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9',
+    'USD₮0': '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9',
+    aArbUSDCn: '0x724dc807b04555b71ed48a6896b6F41593b8C637', // USDC
+    aArbDAI: '0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE', // DAI
+    aArbUSDT: '0x6ab707Aca953eDAeFBc4fD23bA73294241490620', // USDT
+  },
+};
+
 // Temporary: Will be replaced with supported markets from API request
 export const SUPPORTED_LENDING_TOKENS = new Set(['DAI', 'USDC', 'USDT']);
 
@@ -41,9 +72,9 @@ export const LENDING_TOKEN_TO_RECEIPT_TOKEN_MAP: Record<
   Record<string, string>
 > = {
   [CHAIN_IDS.MAINNET]: {
-    DAI: 'ADAI',
+    DAI: 'aEthDAI',
     USDC: 'AETHUSDC',
-    USDT: 'AUSDT',
+    USDT: 'aEthUSDT',
   },
   [CHAIN_IDS.BASE]: {
     USDC: 'aBasUSDC',
@@ -64,9 +95,9 @@ export const RECEIPT_TOKEN_TO_LENDING_TOKEN_MAP: Record<
   Record<string, string>
 > = {
   [CHAIN_IDS.MAINNET]: {
-    ADAI: 'DAI',
+    aEthDAI: 'DAI',
     AETHUSDC: 'USDC',
-    AUSDT: 'USDT',
+    aEthUSDT: 'USDT',
   },
   [CHAIN_IDS.BASE]: {
     aBasUSDC: 'USDC',
