d0d2978fc8abe5177cbbb32b4cf59724d8556a63
hieu-w <hieu@tor.us>
2025-05-27 13:01:48 +0700

    test: add gestures to type text without '\n' at the end

--- e2e/utils/Gestures.js
+++ e2e/utils/Gestures.js
@@ -127,6 +127,17 @@ class Gestures {
     await (await element).typeText(text + '\n');
   }
 
+
+  /**
+   * Type text into an element without hiding the keyboard.
+   *
+   * @param {Promise<Detox.IndexableNativeElement>} element - The element to type into
+   * @param {string} text - Text to be typed into the element
+   */
+  static async typeTextWithoutKeyboard(element, text) {
+    await (await element).typeText(text);
+  }
+
   /**
    * Replace the text in the field of an element identified by ID.
    *
