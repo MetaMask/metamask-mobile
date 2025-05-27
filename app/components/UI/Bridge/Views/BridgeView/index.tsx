a0c50cf50fc12ca90d7d7551bbbe6ebaa0d678c8
IF <139582705+infiniteflower@users.noreply.github.com>
2025-05-27 15:53:54 -0400

    chore: show hw specific text on button on press

--- app/components/UI/Bridge/Views/BridgeView/index.tsx
+++ app/components/UI/Bridge/Views/BridgeView/index.tsx
@@ -67,6 +67,8 @@ import type { BridgeDestTokenSelectorRouteParams } from '../../components/Bridge
 import { useGasFeeEstimates } from '../../../../Views/confirmations/hooks/gas/useGasFeeEstimates';
 import { selectSelectedNetworkClientId } from '../../../../../selectors/networkController';
 import { useMetrics, MetaMetricsEvents } from '../../../../hooks/useMetrics';
+import { isHardwareAccount } from '../../../../../util/address';
+import { selectSelectedInternalAccountFormattedAddress } from '../../../../../selectors/accountsController';
 import { BridgeToken, BridgeViewMode } from '../../types';
 import { useSwitchTokens } from '../../hooks/useSwitchTokens';
 import { ScrollView } from 'react-native';
@@ -103,6 +105,9 @@ const BridgeView = () => {
   const destToken = useSelector(selectDestToken);
   const destChainId = useSelector(selectSelectedDestChainId);
   const destAddress = useSelector(selectDestAddress);
+  const selectedAddress = useSelector(
+    selectSelectedInternalAccountFormattedAddress,
+  );
   const {
     activeQuote,
     isLoading,
@@ -322,6 +327,8 @@ const BridgeView = () => {
 
   const getButtonLabel = () => {
     if (hasInsufficientBalance) return strings('bridge.insufficient_funds');
+    if (isSubmittingTx && selectedAddress && isHardwareAccount(selectedAddress))
+      return strings('bridge.submitting_transaction_hardware_wallet');
     if (isSubmittingTx) return strings('bridge.submitting_transaction');
 
     // Solana uses the continue button since they have a snap confirmation modal
