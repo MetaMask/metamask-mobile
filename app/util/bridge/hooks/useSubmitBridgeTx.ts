16fa5455d9941d30f867a8be83087917a6ab7681
IF <139582705+infiniteflower@users.noreply.github.com>
2025-05-28 15:43:01 -0400

    fix: properly handle evm bridge txs with hardware wallets

--- app/util/bridge/hooks/useSubmitBridgeTx.ts
+++ app/util/bridge/hooks/useSubmitBridgeTx.ts
@@ -3,16 +3,25 @@ import Engine from '../../../core/Engine';
 import { QuoteMetadata } from '@metamask/bridge-controller';
 import { useSelector } from 'react-redux';
 import { selectSmartTransactionsEnabled } from '../../../selectors/smartTransactionsController';
+import { selectSelectedInternalAccountFormattedAddress } from '../../../selectors/accountsController';
+import { isHardwareAccount as getIsHardwareAccount } from '../../address';
 
 export default function useSubmitBridgeTx() {
   const stxEnabled = useSelector(selectSmartTransactionsEnabled);
+  const selectedAddress = useSelector(selectSelectedInternalAccountFormattedAddress);
+  const isHardwareAccount = selectedAddress ? getIsHardwareAccount(selectedAddress) : false;
+  const requireApproval = isHardwareAccount;
 
   const submitBridgeTx = async ({
     quoteResponse,
   }: {
     quoteResponse: QuoteResponse & QuoteMetadata;
   }) => {
-    const txResult = await Engine.context.BridgeStatusController.submitTx(quoteResponse, stxEnabled);
+    const txResult = await Engine.context.BridgeStatusController.submitTx(
+      quoteResponse,
+      stxEnabled,
+      requireApproval,
+    );
 
     return txResult;
   };
