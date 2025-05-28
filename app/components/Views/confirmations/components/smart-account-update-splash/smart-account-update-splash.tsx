8635d03a89efbcb477b0b4ed306e2f17e060eb6e
Jyoti Puri <jyotipuri@gmail.com>
2025-05-28 12:06:22 +0530

    feat: Smart account upgrade splash page should be displayed for the user only once. (#15705)
    ## **Description**
    
    Smart account upgrade splash page should be displayed for the user only
    once.
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/MetaMask-planning/issues/5001
    
    ## **Manual testing steps**
    
    1. Enable 7702 locally
    2. Go to test dapp and submit 5792 send calls
    3. Acknowledge splash page, but cancel confirmation
    4. Submit the request again and check that splash page is not displayed
    
    ## **Screenshots/Recordings**
    NA
    
    ## **Pre-merge author checklist**
    
    - [X] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [X] I've completed the PR template to the best of my ability
    - [X] I’ve included tests if applicable
    - [X] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [X] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/Views/confirmations/components/smart-account-update-splash/smart-account-update-splash.tsx
+++ app/components/Views/confirmations/components/smart-account-update-splash/smart-account-update-splash.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement, useCallback, useState } from 'react';
 import { Image, Linking, View } from 'react-native';
 import { JsonRpcError, serializeError } from '@metamask/rpc-errors';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { strings } from '../../../../../../locales/i18n';
 import { EIP5792ErrorCode } from '../../../../../constants/transaction';
@@ -14,10 +15,12 @@ import Text, {
   TextColor,
   TextVariant,
 } from '../../../../../component-library/components/Texts/Text';
+import { upgradeSplashPageAcknowledgedForAccount } from '../../../../../actions/confirmations';
 import Name from '../../../../UI/Name';
 import { NameType } from '../../../../UI/Name/Name.types';
 import { useTheme } from '../../../../../util/theme';
 import { useStyles } from '../../../../hooks/useStyles';
+import { selectUpgradeSplashPageAcknowledgedForAccounts } from '../../selectors/confirmation';
 import { useConfirmActions } from '../../hooks/useConfirmActions';
 import { useTransactionMetadataRequest } from '../../hooks/transactions/useTransactionMetadataRequest';
 import styleSheet from './smart-account-update-splash.styles';
@@ -59,7 +62,14 @@ const ListItem = ({
 
 export const SmartAccountUpdateSplash = () => {
   const [acknowledged, setAcknowledged] = useState(false);
+  const dispatch = useDispatch();
   const transactionMetadata = useTransactionMetadataRequest();
+  const upgradeSplashPageAcknowledgedForAccounts = useSelector(
+    selectUpgradeSplashPageAcknowledgedForAccounts,
+  );
+  const {
+    txParams: { from },
+  } = transactionMetadata ?? { txParams: {} };
   const { styles } = useStyles(styleSheet, {});
   const { onReject } = useConfirmActions();
 
@@ -74,7 +84,19 @@ export const SmartAccountUpdateSplash = () => {
     onReject(serializedError as unknown as Error);
   }, [onReject]);
 
-  if (!transactionMetadata || acknowledged) {
+  const onConfirm = useCallback(() => {
+    if (!from) {
+      return;
+    }
+    dispatch(upgradeSplashPageAcknowledgedForAccount(from));
+    setAcknowledged(true);
+  }, [dispatch, from, setAcknowledged]);
+
+  if (
+    !transactionMetadata ||
+    acknowledged ||
+    upgradeSplashPageAcknowledgedForAccounts.includes(from as string)
+  ) {
     return null;
   }
 
@@ -145,7 +167,7 @@ export const SmartAccountUpdateSplash = () => {
           size={ButtonSize.Lg}
           style={styles.buttons}
           label={strings('confirm.7702_functionality.splashpage.accept')}
-          onPress={() => setAcknowledged(true)}
+          onPress={onConfirm}
         />
       </View>
     </View>
