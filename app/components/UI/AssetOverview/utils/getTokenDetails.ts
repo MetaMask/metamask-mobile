d5bf26f7281ba1b51994e9fd6255d71ceca84ddc
sahar-fehri <sahar.fehri@consensys.net>
2025-05-28 16:08:25 +0200

    fix: fix display contract address on non evm chains

--- app/components/UI/AssetOverview/utils/getTokenDetails.ts
+++ app/components/UI/AssetOverview/utils/getTokenDetails.ts
@@ -1,6 +1,7 @@
 import { zeroAddress } from 'ethereumjs-util';
 import { TokenI } from '../../Tokens/types';
 import { TokenDetails } from '../TokenDetails/TokenDetails';
+import { parseCaipAssetType } from '@metamask/utils';
 
 export const getTokenDetails = (
   asset: TokenI,
@@ -9,8 +10,12 @@ export const getTokenDetails = (
   tokenMetadata: Record<string, string | number | string[]>,
 ): TokenDetails => {
   if (isNonEvmAsset) {
+    const { assetNamespace, assetReference } = parseCaipAssetType(
+      asset.address as `${string}:${string}/${string}:${string}`,
+    );
+    const isNative = assetNamespace === 'slip44';
     return {
-      contractAddress: asset.address || null,
+      contractAddress: isNative ? null : assetReference || null,
       tokenDecimal: asset.decimals || null,
       tokenList: asset?.aggregators?.join(', ') || null,
     };
