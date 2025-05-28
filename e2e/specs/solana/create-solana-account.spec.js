d740b1ef31eb68cef992dee248aa7136d9a12a5e
Curtis David <Curtis.David7@gmail.com>
2025-05-28 14:36:50 -0400

    test: fix failing assertions for solana e2e

--- e2e/specs/solana/create-solana-account.spec.js
+++ e2e/specs/solana/create-solana-account.spec.js
@@ -63,18 +63,11 @@ describe(SmokeNetworkExpansion('Create Solana account'), () => {
 
   it('should create Solana account directly from new feature announcement sheet', async () => {
     await Assertions.checkIfVisible(
-      SolanaNewFeatureSheetSelectorsIDs.SOLANA_NEW_FEATURE_SHEET,
+      SolanaNewFeatureSheet.sheetContainer,
     );
     await Assertions.checkIfVisible(
-      SolanaNewFeatureSheetSelectorsIDs.SOLANA_LEARN_MORE_BUTTON,
+      SolanaNewFeatureSheet.learnMoreButton,
     );
-    await Assertions.checkIfVisible(
-      SolanaNewFeatureSheetSelectorsIDs.SOLANA_ADD_ACCOUNT_BUTTON_IN_SHEET,
-    );
-    await Assertions.checkIfVisible(
-      SolanaNewFeatureSheetSelectorsIDs.SOLANA_CREATE_ACCOUNT_BUTTON,
-    );
-
     await SolanaNewFeatureSheet.tapCreateAccountButton();
     await AddNewHdAccountComponent.tapConfirm();
     await TestHelpers.delay(4000);
