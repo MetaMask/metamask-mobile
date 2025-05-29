1b6180a7296dec0956be13c00ed2766dbc72fe52
ieow <cwlee0@gmail.com>
2025-05-29 18:46:00 +0800

    feat: oauth service

---
+++ app/core/OAuthService/OAuthLoginHandlers/androidHandlers/apple.ts
@@ -0,0 +1,148 @@
+import {
+  CodeChallengeMethod,
+  ResponseType,
+  AuthRequest,
+} from 'expo-auth-session';
+import {
+  AuthConnection,
+  LoginHandler,
+  LoginHandlerCodeResult,
+} from '../../OAuthInterface';
+import { BaseLoginHandler } from '../baseHandler';
+import { OAuthError, OAuthErrorType } from '../../error';
+export interface AndroidAppleLoginHandlerParams {
+  clientId: string;
+  redirectUri: string;
+  appRedirectUri: string;
+}
+
+/**
+ * AndroidAppleLoginHandler is the login handler for the Apple login on android.
+ */
+export class AndroidAppleLoginHandler
+  extends BaseLoginHandler
+  implements LoginHandler
+{
+  public readonly OAUTH_SERVER_URL = 'https://appleid.apple.com/auth/authorize';
+
+  readonly #scope = ['name', 'email'];
+
+  protected clientId: string;
+  protected redirectUri: string;
+  protected appRedirectUri: string;
+
+  get authConnection() {
+    return AuthConnection.Apple;
+  }
+
+  get scope() {
+    return this.#scope;
+  }
+
+  get authServerPath() {
+    return 'api/v1/oauth/token';
+  }
+
+  /**
+   * AndroidAppleLoginHandler constructor.
+   *
+   * @param params.clientId - The Service ID from the apple developer account for the app.
+   * @param params.redirectUri - The server redirectUri for the Apple login to handle rest api login.
+   * @param params.appRedirectUri - The Android App redirectUri for the customChromeTab to handle auth-session login.
+   */
+  constructor(params: AndroidAppleLoginHandlerParams) {
+    super();
+    const { appRedirectUri, redirectUri, clientId } = params;
+    this.clientId = clientId;
+    this.redirectUri = redirectUri;
+    this.appRedirectUri = appRedirectUri;
+  }
+
+  /**
+   * This method is used to login with apple via customChromeTab via expo-auth-session.
+   * It generates the auth url with server redirect uri and state.
+   * It creates a client auth request instance so that the auth-session can return result on appRedirectUrl.
+   * It then prompts the auth request via the client auth request instance with auth url generated with server redirect uri and state.
+   *
+   * Data flow:
+   * App -> Apple -> AuthServer -> App
+   *
+   * @returns LoginHandlerCodeResult
+   */
+  async login(): Promise<LoginHandlerCodeResult> {
+    const state = JSON.stringify({
+      provider: this.authConnection,
+      client_redirect_back_uri: this.appRedirectUri,
+      redirectUri: this.redirectUri,
+      clientId: this.clientId,
+      random: this.nonce,
+    });
+    const authRequest = new AuthRequest({
+      clientId: this.clientId,
+      redirectUri: this.redirectUri,
+      scopes: this.#scope,
+      responseType: ResponseType.Code,
+      codeChallengeMethod: CodeChallengeMethod.S256,
+      usePKCE: true,
+      state,
+      extraParams: {
+        response_mode: 'form_post',
+      },
+    });
+    // generate the auth url
+    const authUrl = await authRequest.makeAuthUrlAsync({
+      authorizationEndpoint: this.OAUTH_SERVER_URL,
+    });
+
+    // create a client auth request instance so that the auth-session can return result on appRedirectUrl
+    const authRequestClient = new AuthRequest({
+      clientId: this.clientId,
+      redirectUri: this.appRedirectUri,
+      state,
+    });
+
+    // prompt the auth request using generated auth url instead of the client auth request instance
+    const result = await authRequestClient.promptAsync(
+      {
+        authorizationEndpoint: this.OAUTH_SERVER_URL,
+      },
+      {
+        url: authUrl,
+      },
+    );
+    if (result.type === 'success') {
+      return {
+        authConnection: AuthConnection.Apple,
+        code: result.params.code,
+        clientId: this.clientId,
+        redirectUri: this.redirectUri,
+        codeVerifier: authRequest.codeVerifier,
+      };
+    }
+    if (result.type === 'error') {
+      if (result.error) {
+        throw new OAuthError(result.error.message, OAuthErrorType.LoginError);
+      }
+      throw new OAuthError(
+        'handleAndroidAppleLogin: Unknown error',
+        OAuthErrorType.UnknownError,
+      );
+    }
+    if (result.type === 'cancel') {
+      throw new OAuthError(
+        'handleAndroidAppleLogin: User cancelled the login process',
+        OAuthErrorType.UserCancelled,
+      );
+    }
+    if (result.type === 'dismiss') {
+      throw new OAuthError(
+        'handleAndroidAppleLogin: User dismissed the login process',
+        OAuthErrorType.UserDismissed,
+      );
+    }
+    throw new OAuthError(
+      'handleAndroidAppleLogin: Unknown error',
+      OAuthErrorType.UnknownError,
+    );
+  }
+}
---
+++ app/core/OAuthService/OAuthLoginHandlers/constants.ts
@@ -0,0 +1,49 @@
+import { ACTIONS, PROTOCOLS } from '../../../constants/deeplinks';
+import AppConstants from '../../AppConstants';
+
+export const AppRedirectUri = `${PROTOCOLS.HTTPS}://${AppConstants.MM_UNIVERSAL_LINK_HOST}/${ACTIONS.OAUTH_REDIRECT}`;
+
+if (
+  !AppConstants.SEEDLESS_ONBOARDING.WEB3AUTH_NETWORK ||
+  !AppConstants.SEEDLESS_ONBOARDING.AUTH_SERVER_URL ||
+  !AppConstants.SEEDLESS_ONBOARDING.IOS_GOOGLE_CLIENT_ID ||
+  !AppConstants.SEEDLESS_ONBOARDING.IOS_GOOGLE_REDIRECT_URI ||
+  !AppConstants.SEEDLESS_ONBOARDING.IOS_APPLE_CLIENT_ID ||
+  !AppConstants.SEEDLESS_ONBOARDING.ANDROID_WEB_GOOGLE_CLIENT_ID ||
+  !AppConstants.SEEDLESS_ONBOARDING.ANDROID_WEB_APPLE_CLIENT_ID ||
+  !AppConstants.SEEDLESS_ONBOARDING.AUTH_CONNECTION_ID ||
+  !AppConstants.SEEDLESS_ONBOARDING.GROUPED_AUTH_CONNECTION_ID
+) {
+  throw new Error(
+    `Missing environment variables for OAuthLoginHandlers\n
+    WEB3AUTH_NETWORK: ${AppConstants.SEEDLESS_ONBOARDING.WEB3AUTH_NETWORK}\n
+    AUTH_SERVER_URL: ${AppConstants.SEEDLESS_ONBOARDING.AUTH_SERVER_URL}\n
+    IOS_GOOGLE_CLIENT_ID: ${AppConstants.SEEDLESS_ONBOARDING.IOS_GOOGLE_CLIENT_ID}\n
+    IOS_GOOGLE_REDIRECT_URI: ${AppConstants.SEEDLESS_ONBOARDING.IOS_GOOGLE_REDIRECT_URI}\n
+    IOS_APPLE_CLIENT_ID: ${AppConstants.SEEDLESS_ONBOARDING.IOS_APPLE_CLIENT_ID}\n
+    ANDROID_WEB_GOOGLE_CLIENT_ID: ${AppConstants.SEEDLESS_ONBOARDING.ANDROID_WEB_GOOGLE_CLIENT_ID}\n
+    ANDROID_WEB_APPLE_CLIENT_ID: ${AppConstants.SEEDLESS_ONBOARDING.ANDROID_WEB_APPLE_CLIENT_ID}\n`,
+  );
+}
+
+export const web3AuthNetwork =
+  AppConstants.SEEDLESS_ONBOARDING.WEB3AUTH_NETWORK;
+export const AuthServerUrl = AppConstants.SEEDLESS_ONBOARDING.AUTH_SERVER_URL;
+
+export const IosGID = AppConstants.SEEDLESS_ONBOARDING.IOS_GOOGLE_CLIENT_ID;
+export const IosGoogleRedirectUri =
+  AppConstants.SEEDLESS_ONBOARDING.IOS_GOOGLE_REDIRECT_URI;
+export const IosAppleClientId =
+  AppConstants.SEEDLESS_ONBOARDING.IOS_APPLE_CLIENT_ID;
+
+export const AndroidGoogleWebGID =
+  AppConstants.SEEDLESS_ONBOARDING.ANDROID_WEB_GOOGLE_CLIENT_ID;
+export const AppleWebClientId =
+  AppConstants.SEEDLESS_ONBOARDING.ANDROID_WEB_APPLE_CLIENT_ID;
+
+export const AuthConnectionId =
+  AppConstants.SEEDLESS_ONBOARDING.AUTH_CONNECTION_ID;
+export const GroupedAuthConnectionId =
+  AppConstants.SEEDLESS_ONBOARDING.GROUPED_AUTH_CONNECTION_ID;
+
+export const AppleServerRedirectUri = `${AuthServerUrl}/api/v1/oauth/callback`;
