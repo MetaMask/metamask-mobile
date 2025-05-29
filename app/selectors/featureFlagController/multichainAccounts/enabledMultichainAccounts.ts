d182cfb395e40a92b3db81bd899e838670dba3a7
gantunesr <17601467+gantunesr@users.noreply.github.com>
2025-05-29 19:24:19 -0400

    refactor: selectors and tests

--- app/selectors/featureFlagController/multichainAccounts/enabledMultichainAccounts.ts
+++ app/selectors/featureFlagController/multichainAccounts/enabledMultichainAccounts.ts
@@ -1,8 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-// We can ignore the unused vars warning while the flag is not active
-
 import compareVersions from 'compare-versions';
 import { createSelector } from 'reselect';
+import packageJson from '../../../../package.json';
 import { selectRemoteFeatureFlags } from '..';
 
 /**
@@ -14,20 +12,42 @@ export interface MultichainAccountsFeatureFlag {
   minimumVersion: string | null;
 }
 
-// TODO: Update the value to the decided version multichain accounts will be released
-const MINIMUM_SUPPORTED_VERSION = null;
+const APP_VERSION = packageJson.version;
 const FEATURE_VERSION_1 = '1';
 const FEATURE_VERSION_2 = '2';
 
+/**
+ * Asserts that the given value is a valid MultichainAccountsFeatureFlag.
+ * @param value - The value to check.
+ * @returns True if the value is a valid MultichainAccountsFeatureFlag, false otherwise.
+ */
+export const assertMultichainAccountsFeatureFlagType = (value: unknown): value is MultichainAccountsFeatureFlag => (
+  typeof value === 'object' &&
+  value !== null &&
+  'enabled' in value &&
+  typeof value.enabled === 'boolean' &&
+  'featureVersion' in value &&
+  (typeof value.featureVersion === 'string' || value.featureVersion === null) &&
+  'minimumVersion' in value &&
+  (typeof value.minimumVersion === 'string' || value.minimumVersion === null)
+);
+
+/**
+ * Checks if the multichain accounts feature is enabled based on the provided feature flag and version.
+ *
+ * @param enabledMultichainAccounts - The feature flag for multichain accounts.
+ * @param featureVersionToCheck - The version of the feature to check against.
+ * @returns True if the multichain accounts feature is enabled for the specified version, false otherwise.
+ */
 export const isMultichainAccountsFeatureEnabled = (
-  enabledMultichainAccounts: MultichainAccountsFeatureFlag | undefined,
+  enableMultichainAccounts: MultichainAccountsFeatureFlag | undefined,
   featureVersionToCheck: string
 ): boolean => {
-  if (!enabledMultichainAccounts) {
+  if (!enableMultichainAccounts) {
     return false;
   }
 
-  const { enabled, featureVersion, minimumVersion } = enabledMultichainAccounts;
+  const { enabled, featureVersion, minimumVersion } = enableMultichainAccounts;
 
   if (!enabled || !minimumVersion || !featureVersion) {
     return false;
@@ -35,23 +55,36 @@ export const isMultichainAccountsFeatureEnabled = (
 
   return (
     featureVersion === featureVersionToCheck &&
-    // @ts-expect-error - this error can be ignored while the minimum version is not defined
-    compareVersions.compare(minimumVersion, MINIMUM_SUPPORTED_VERSION, '>=')
+    compareVersions.compare(minimumVersion, APP_VERSION, '>=')
   );
 };
 
-const createMultichainAccountsStateSelector = (featureVersion: string) =>
-  createSelector(selectRemoteFeatureFlags, (remoteFeatureFlags): boolean => false);
+/**
+ * Creates a selector to determine if multichain accounts are enabled based on the feature version.
+ * @param featureVersions - The versions of the feature to check against.
+ * @returns Boolean indicating if the multichain accounts feature is enabled for the specified versions.
+ */
+const createMultichainAccountsStateSelector = (featureVersions: string[]) =>
+  createSelector(selectRemoteFeatureFlags, (remoteFeatureFlags): boolean => {
+    const enableMultichainAccounts = remoteFeatureFlags.enableMultichainAccounts;
+    if (!assertMultichainAccountsFeatureFlagType(enableMultichainAccounts)) {
+      return false;
+    }
 
-// TODO: Update selector logic to use remote feature flag.
-//
-// Code:
-// const createMultichainAccountsStateSelector = (featureVersion: string) =>
-//   createSelector(selectRemoteFeatureFlags, (remoteFeatureFlags): boolean => {
-//      const enabledMultichainAccounts = remoteFeatureFlags?.enabledMultichainAccounts;
-//      return isMultichainAccountsFeatureEnabled(enabledMultichainAccounts, featureVersion);
-//   });
+    // Check if the feature is enabled for any of the specified versions
+    return featureVersions.some((featureVersion) =>
+      isMultichainAccountsFeatureEnabled(enableMultichainAccounts, featureVersion)
+    );
+  });
 
+/**
+ * Selector to check if multichain accounts state 1 is enabled.
+ * Returns true if the feature is enabled for state 1 or state 2.
+ */
+export const selectMultichainAccountsState1Enabled = createMultichainAccountsStateSelector([FEATURE_VERSION_1, FEATURE_VERSION_2]);
 
-export const selectMultichainAccountsState1Enabled = createMultichainAccountsStateSelector(FEATURE_VERSION_1);
-export const selectMultichainAccountsState2Enabled = createMultichainAccountsStateSelector(FEATURE_VERSION_2);
+/**
+ * Selector to check if multichain accounts state 2 is enabled.
+ * @returns Boolean indicating if multichain accounts state 2 is enabled.
+ */
+export const selectMultichainAccountsState2Enabled = createMultichainAccountsStateSelector([FEATURE_VERSION_2]);
