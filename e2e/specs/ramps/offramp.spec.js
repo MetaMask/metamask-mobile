edcbe5e6fec875499828201cce2d754e14d11362
Christopher Ferreira <christopher.ferreira@consensys.net>
2025-05-29 21:05:43 +0100

    test: adds event checking coverage to the existing off-ramp flows

--- e2e/specs/ramps/offramp.spec.js
+++ e2e/specs/ramps/offramp.spec.js
@@ -96,22 +96,100 @@ describe(SmokeTrade('Off-Ramp'), () => {
   it('should validate segment/metametric events for a successful offramp flow', async () => {
     const expectedEvents = {
       SELL_BUTTON_CLICKED: 'Sell Button Clicked',
-      SELL_GET_STARTED_CLICKED: 'Sell Get Started Clicked',
-      SELL_QUOTES_REQUESTED: 'Sell Quotes Requested',
-      SELL_QUOTES_RECEIVED: 'Sell Quotes Received',
+      OFFRAMP_CANCELED: 'Off-ramp Canceled',
+      OFFRAMP_GET_STARTED_CLICKED: 'Off-ramp Get Started Clicked',
+      OFFRAMP_QUOTES_REQUESTED: 'Off-ramp Quotes Requested',
+      OFFRAMP_QUOTES_RECEIVED: 'Off-ramp Quotes Received',
+      OFFRAMP_QUOTE_ERROR: 'Off-ramp Quote Error',
     };
 
-    const events = await getEventsPayloads(mockServer);
-    let counter = 0;
-    for (const event of events) {
-      console.log(`${counter}: ${event.event}: ${JSON.stringify(event.properties)}`);
-      counter += 1;
-    }
+    const events = await getEventsPayloads(mockServer, [
+      expectedEvents.SELL_BUTTON_CLICKED,
+      expectedEvents.OFFRAMP_CANCELED,
+      expectedEvents.OFFRAMP_GET_STARTED_CLICKED,
+      expectedEvents.OFFRAMP_QUOTES_REQUESTED,
+      expectedEvents.OFFRAMP_QUOTES_RECEIVED,
+      expectedEvents.OFFRAMP_QUOTE_ERROR,
+    ]);
 
     const softAssert = new SoftAssert();
 
+    const sellButtonClicked = events.find((event) => event.event === expectedEvents.SELL_BUTTON_CLICKED);
     await softAssert.checkAndCollect(async () => {
-      await Assertions.checkIfValueIsPresent(expectedEvents.SELL_BUTTON_CLICKED);
-    });
+      await Assertions.checkIfValueIsPresent(sellButtonClicked);
+    }, 'Sell Button Clicked: Should be present');
+
+    const offRampCanceled = events.find((event) => event.event === expectedEvents.OFFRAMP_CANCELED);
+    await softAssert.checkAndCollect(async () => {
+      await Assertions.checkIfValueIsPresent(offRampCanceled);
+    }, 'Off-ramp Canceled: Should be present');
+
+    const offRampGetStartedClicked = events.find((event) => event.event === expectedEvents.OFFRAMP_GET_STARTED_CLICKED);
+    await softAssert.checkAndCollect(async () => {
+      await Assertions.checkIfValueIsPresent(offRampGetStartedClicked);
+    }, 'Off-ramp Get Started Clicked: Should be present');
+
+    const offRampQuotesRequested = events.find((event) => event.event === expectedEvents.OFFRAMP_QUOTES_REQUESTED);
+    await softAssert.checkAndCollect(async () => {
+      await Assertions.checkIfValueIsPresent(offRampQuotesRequested);
+    }, 'Off-ramp Quotes Requested: Should be present');
+    await softAssert.checkAndCollect(async () => {
+      await Assertions.checkIfObjectHasKeysAndValidValues(offRampQuotesRequested.properties, {
+        payment_method_id: 'string',
+        amount: 'number',
+        location: 'string',
+        currency_destination: 'string',
+        currency_source: 'string',
+        chain_id_source: 'string',
+      });
+    }, 'Off-ramp Quotes Requested: Should have correct properties');
+
+    const offRampQuotesReceived = events.find((event) => event.event === expectedEvents.OFFRAMP_QUOTES_RECEIVED);
+    await softAssert.checkAndCollect(async () => {
+      await Assertions.checkIfValueIsPresent(offRampQuotesReceived);
+    }, 'Off-ramp Quotes Received: Should be present');
+    await softAssert.checkAndCollect(async () => {
+      await Assertions.checkIfObjectHasKeysAndValidValues(offRampQuotesReceived.properties, {
+        amount: 'string',
+        payment_method_id: 'string',
+        refresh_count: 'number',
+        results_count: 'number',
+        average_total_fee: 'number',
+        average_gas_fee: 'number',
+        average_processing_fee: 'number',
+        average_total_fee_of_amount: 'number',
+        quotes_amount_list: 'array',
+        quotes_amount_first: 'number',
+        quotes_amount_last: 'number',
+        currency_destination: 'string',
+        currency_source: 'string',
+        average_fiat_out: 'number',
+        chain_id_source: 'string',
+        provider_offramp_list: 'array',
+        provider_offramp_first: 'string',
+        provider_offramp_last: 'string',
+        provider_offramp_most_reliable: 'string',
+        provider_offramp_best_price: 'string',
+
+      });
+    }, 'Off-ramp Quotes Received: Should have correct properties');
+
+    const offRampQuoteError = events.find((event) => event.event === expectedEvents.OFFRAMP_QUOTE_ERROR);
+    await softAssert.checkAndCollect(async () => {
+      await Assertions.checkIfValueIsPresent(offRampQuoteError);
+    }, 'Off-ramp Quote Error: Should be present');
+    await softAssert.checkAndCollect(async () => {
+      await Assertions.checkIfObjectHasKeysAndValidValues(offRampQuoteError.properties, {
+        amount: 'string',
+        payment_method_id: 'string',
+        error_message: 'string',
+        currency_destination: 'string',
+        currency_source: 'string',
+        provider_offramp: 'string',
+        chain_id_source: 'string',
+      });
+    }, 'Off-ramp Quote Error: Should have correct properties');
+
+    softAssert.throwIfErrors();
   });
 });
