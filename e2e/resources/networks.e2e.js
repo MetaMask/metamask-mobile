ef4ac48cd8cf05d0332dc0db48b0b1e4a10623f4
Vivek <vivek.srilal@consensys.net>
2025-05-30 20:13:54 +0400

    updated networks

--- e2e/resources/networks.e2e.js
+++ e2e/resources/networks.e2e.js
@@ -143,26 +143,6 @@ const CustomNetworks = {
         ticker: 'ETH',
       },
     },
-    MegaTestnet: {
-      providerConfig: {
-        type: 'rpc',
-        chainId: '0x18c6',
-        rpcUrl: 'https://carrot.megaeth.com/rpc',
-        nickname: 'Mega Testnet',
-        ticker: 'MegaETH',
-        BlockExplorerUrl: 'https://megaexplorer.xyz/',
-      },
-    },
-    MonadTestnet: {
-      providerConfig: {
-        type: 'rpc',
-        chainId: '0x279f',
-        rpcUrl: 'https://testnet-rpc.monad.xyz/',
-        nickname: 'Monad Testnet',
-        ticker: 'MON',
-        BlockExplorerUrl: 'https://testnet.monadexplorer.com',
-      },
-    },
   },
   Gnosis: {
     providerConfig: {
@@ -185,5 +165,25 @@ const CustomNetworks = {
       BlockExplorerUrl: 'https://holesky.etherscan.io/',
     },
   },
+  MegaTestnet: {
+      providerConfig: {
+        type: 'rpc',
+        chainId: '0x18c6',
+        rpcUrl: 'https://carrot.megaeth.com/rpc',
+        nickname: 'Mega Testnet',
+        ticker: 'MegaETH',
+        BlockExplorerUrl: 'https://megaexplorer.xyz/',
+      },
+    },
+    MonadTestnet: {
+      providerConfig: {
+        type: 'rpc',
+        chainId: '0x279f',
+        rpcUrl: 'https://testnet-rpc.monad.xyz/',
+        nickname: 'Monad Testnet',
+        ticker: 'MON',
+        BlockExplorerUrl: 'https://testnet.monadexplorer.com',
+      },
+    },
 };
 export { CustomNetworks, PopularNetworksList };
