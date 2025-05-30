e712f8d47f0118f52c070d04389f561d7589515c
Stylianos Panagakos <stylianos.panagakos@consensys.net>
2025-05-30 10:12:49 +0100

    fix: update text color for select amount + fetch quote texts

--- app/components/UI/Bridge/Views/BridgeView/index.tsx
+++ app/components/UI/Bridge/Views/BridgeView/index.tsx
@@ -345,7 +345,7 @@ const BridgeView = () => {
     if (shouldDisplayKeypad && !isLoading) {
       return (
         <Box style={styles.buttonContainer}>
-          <Text color={TextColor.Primary}>
+          <Text color={TextColor.Alternative}>
             {strings('bridge.select_amount')}
           </Text>
         </Box>
@@ -355,7 +355,7 @@ const BridgeView = () => {
     if (isLoading) {
       return (
         <Box style={styles.buttonContainer}>
-          <Text color={TextColor.Primary}>
+          <Text color={TextColor.Alternative}>
             {strings('bridge.fetching_quote')}
           </Text>
         </Box>
