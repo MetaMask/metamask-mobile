1b344172255730648f9b26434cc60badbd9c3d28
infiniteflower <139582705+infiniteflower@users.noreply.github.com>
2025-05-30 17:23:46 +0000

    chore(runway): cherry-pick fix: cp-7.47.0 stuck in submitting during a failed tx, also log errors (#15904)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    This PR fixes an issue where if an error was thrown during Bridge tx
    submission, the user would be stuck on Submitting forever and wouldn't
    be able to get new quotes.
    
    This will also help ongoing investigations into tx submission failures.
    Since you won't have issues getting a fresh Bridge UI screen on exit.
    
    ## **Related issues**
    
    Fixes: https://consensyssoftware.atlassian.net/browse/MMS-2552
    
    Related to https://github.com/MetaMask/metamask-mobile/issues/15850
    
    ## **Manual testing steps**
    
    1. Go to Bridge
    2. Throw an error during tx submission
    3. Button should change back to allow you to submit again
    4. Exit bridge
    5. You should be able to change tokens and get different quotes
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/UI/Bridge/Views/BridgeView/index.tsx
+++ app/components/UI/Bridge/Views/BridgeView/index.tsx
@@ -275,18 +275,22 @@ const BridgeView = () => {
   };
 
   const handleContinue = async () => {
-    if (activeQuote) {
-      dispatch(setIsSubmittingTx(true));
-      // TEMPORARY: If tx originates from Solana, navigate to transactions view BEFORE submitting the tx
-      // Necessary because snaps prevents navigation after tx is submitted
-      if (isSolanaSwap || isSolanaToEvm) {
+    try {
+      if (activeQuote) {
+        dispatch(setIsSubmittingTx(true));
+        // TEMPORARY: If tx originates from Solana, navigate to transactions view BEFORE submitting the tx
+        // Necessary because snaps prevents navigation after tx is submitted
+        if (isSolanaSwap || isSolanaToEvm) {
+          navigation.navigate(Routes.TRANSACTIONS_VIEW);
+        }
+        await submitBridgeTx({
+          quoteResponse: activeQuote,
+        });
         navigation.navigate(Routes.TRANSACTIONS_VIEW);
-        dispatch(setIsSubmittingTx(false));
       }
-      await submitBridgeTx({
-        quoteResponse: activeQuote,
-      });
-      navigation.navigate(Routes.TRANSACTIONS_VIEW);
+    } catch (error) {
+      console.error('Error submitting bridge tx', error);
+    } finally {
       dispatch(setIsSubmittingTx(false));
     }
   };
