d00280d19818ac0215f6a4f8e9c17bb5f5a24eea
ieow <cwlee0@gmail.com>
2025-05-31 22:18:27 +0800

    fix: update coverage

--- app/components/Views/Login/index.test.tsx
+++ app/components/Views/Login/index.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Login from './';
 import renderWithProvider from '../../../util/test/renderWithProvider';
+import { fireEvent } from '@testing-library/react-native';
+import { LoginViewSelectors } from '../../../../e2e/selectors/wallet/LoginView.selectors';
 jest.mock('@react-navigation/native', () => {
   const actualNav = jest.requireActual('@react-navigation/native');
   return {
@@ -19,6 +21,16 @@ jest.mock('@react-navigation/native', () => {
 describe('Login', () => {
   it('should render correctly', () => {
     const { toJSON } = renderWithProvider(<Login />);
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('should render with different logo on password input', () => {
+    const { getByTestId, toJSON } = renderWithProvider(<Login />);
+
+    fireEvent.changeText(
+      getByTestId(LoginViewSelectors.PASSWORD_INPUT),
+      'password',
+    );
     expect(toJSON()).toMatchSnapshot();
   });
 });
