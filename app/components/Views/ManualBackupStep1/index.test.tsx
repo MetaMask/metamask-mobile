e99ca31d1fd9204f6135ae97965ca42aa23d7907
smgv <gpatra1996@gmail.com>
2025-05-31 22:03:13 +0530

    Fix: ManualbackupStep1 Test Case Update

--- app/components/Views/ManualBackupStep1/index.test.tsx
+++ app/components/Views/ManualBackupStep1/index.test.tsx
@@ -16,6 +16,16 @@ const initialState = {
 };
 const store = mockStore(initialState);
 
+const initialStateDark = {
+  user: { appTheme: AppThemeKey.dark },
+};
+const storeDark = mockStore(initialStateDark);
+
+const initialStateOs = {
+  user: { appTheme: AppThemeKey.os },
+};
+const storeOs = mockStore(initialStateOs);
+
 jest.mock('@react-navigation/native', () => {
   const actualNav = jest.requireActual('@react-navigation/native');
   return {
@@ -113,6 +123,88 @@ describe('ManualBackupStep1', () => {
       mockDispatch,
     };
   };
+  const setupTestDark = () => {
+    const mockNavigate = jest.fn();
+    const mockGoBack = jest.fn();
+    const mockSetOptions = jest.fn();
+    const mockDispatch = jest.fn();
+
+    storeDark.dispatch = mockDispatch;
+
+    (useNavigation as jest.Mock).mockReturnValue({
+      navigate: mockNavigate,
+      goBack: mockGoBack,
+      setOptions: mockSetOptions,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+      isFocused: jest.fn(),
+    });
+
+    const wrapper = renderWithProvider(
+      <Provider store={storeDark}>
+        <ManualBackupStep1
+          route={mockRoute}
+          navigation={{
+            navigate: mockNavigate,
+            goBack: mockGoBack,
+            setOptions: mockSetOptions,
+            addListener: jest.fn(),
+            removeListener: jest.fn(),
+            isFocused: jest.fn(),
+          }}
+        />
+      </Provider>,
+    );
+
+    return {
+      wrapper,
+      mockNavigate,
+      mockGoBack,
+      mockSetOptions,
+      mockDispatch,
+    };
+  };
+  const setupTestOs = () => {
+    const mockNavigate = jest.fn();
+    const mockGoBack = jest.fn();
+    const mockSetOptions = jest.fn();
+    const mockDispatch = jest.fn();
+
+    storeOs.dispatch = mockDispatch;
+
+    (useNavigation as jest.Mock).mockReturnValue({
+      navigate: mockNavigate,
+      goBack: mockGoBack,
+      setOptions: mockSetOptions,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+      isFocused: jest.fn(),
+    });
+
+    const wrapper = renderWithProvider(
+      <Provider store={storeOs}>
+        <ManualBackupStep1
+          route={mockRoute}
+          navigation={{
+            navigate: mockNavigate,
+            goBack: mockGoBack,
+            setOptions: mockSetOptions,
+            addListener: jest.fn(),
+            removeListener: jest.fn(),
+            isFocused: jest.fn(),
+          }}
+        />
+      </Provider>,
+    );
+
+    return {
+      wrapper,
+      mockNavigate,
+      mockGoBack,
+      mockSetOptions,
+      mockDispatch,
+    };
+  };
 
   beforeEach(() => {
     jest.clearAllMocks();
@@ -166,6 +258,110 @@ describe('ManualBackupStep1', () => {
       expect(
         wrapper.getByTestId(`${ManualBackUpStepsSelectorsIDs.WORD_ITEM}-0`),
       ).toBeTruthy();
+    });
+  });
+
+  it('should render 2 step confirm password with dark theme', async () => {
+    const { wrapper } = setupTestDark();
+    expect(wrapper.getByText('Step 2 of 3')).toBeTruthy();
+
+    expect(
+      wrapper.getByText(strings('manual_backup_step_1.action')),
+    ).toBeTruthy();
+    expect(
+      wrapper.getByText(strings('manual_backup_step_1.reveal')),
+    ).toBeTruthy();
+
+    const blurButton = wrapper.getByTestId(
+      ManualBackUpStepsSelectorsIDs.BLUR_BUTTON,
+    );
+    fireEvent.press(blurButton);
+
+    await waitFor(() => {
+      expect(
+        wrapper.getByTestId(`${ManualBackUpStepsSelectorsIDs.WORD_ITEM}-0`),
+      ).toBeTruthy();
+    });
+  });
+
+  it('should render 2 step confirm password with os theme', async () => {
+    const { wrapper, mockNavigate } = setupTestOs();
+    expect(wrapper.getByText('Step 2 of 3')).toBeTruthy();
+
+    expect(
+      wrapper.getByText(strings('manual_backup_step_1.action')),
+    ).toBeTruthy();
+    expect(
+      wrapper.getByText(strings('manual_backup_step_1.reveal')),
+    ).toBeTruthy();
+
+    const blurButton = wrapper.getByTestId(
+      ManualBackUpStepsSelectorsIDs.BLUR_BUTTON,
+    );
+    fireEvent.press(blurButton);
+
+    await waitFor(() => {
+      expect(
+        wrapper.getByTestId(`${ManualBackUpStepsSelectorsIDs.WORD_ITEM}-0`),
+      ).toBeTruthy();
+    });
+
+    const srpText = wrapper.getByText(strings('manual_backup_step_1.info-2'));
+
+    fireEvent.press(srpText);
+
+    expect(mockNavigate).toHaveBeenCalledWith('RootModalFlow', {
+      screen: 'SeedphraseModal',
+    });
+
+    const confirmButton = wrapper.getByText(
+      strings('manual_backup_step_1.continue'),
+    );
+
+    fireEvent.press(confirmButton);
+
+    expect(confirmButton).toBeTruthy();
+  });
+
+  it('should render 2 step confirm', async () => {
+    const { wrapper, mockNavigate } = setupTestOs();
+    expect(wrapper.getByText('Step 2 of 3')).toBeTruthy();
+
+    expect(
+      wrapper.getByText(strings('manual_backup_step_1.action')),
+    ).toBeTruthy();
+    expect(
+      wrapper.getByText(strings('manual_backup_step_1.reveal')),
+    ).toBeTruthy();
+
+    const blurButton = wrapper.getByTestId(
+      ManualBackUpStepsSelectorsIDs.BLUR_BUTTON,
+    );
+    fireEvent.press(blurButton);
+
+    await waitFor(() => {
+      expect(
+        wrapper.getByTestId(`${ManualBackUpStepsSelectorsIDs.WORD_ITEM}-0`),
+      ).toBeTruthy();
+    });
+
+    const confirmButton = wrapper.getByText(
+      strings('manual_backup_step_1.continue'),
+    );
+
+    fireEvent.press(confirmButton);
+
+    await waitFor(() => {
+      expect(mockNavigate).toHaveBeenCalledWith('ManualBackupStep2', {
+        words: mockWords,
+        steps: [
+          'Create Password',
+          'Secure wallet',
+          'Confirm Secret Recovery Phrase',
+        ],
+        backupFlow: true,
+        settingsBackup: true,
+      });
     });
   });
 });
