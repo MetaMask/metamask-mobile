30041fbbfa5de3a297a443f0f661c31833648e83
smgv <gpatra1996@gmail.com>
2025-06-01 23:05:56 +0530

    Fix: AccountBackupStep1 Test Case Update

--- app/components/Views/AccountBackupStep1/index.test.tsx
+++ app/components/Views/AccountBackupStep1/index.test.tsx
@@ -1,28 +1,71 @@
-import { ComponentType } from 'react';
+import React from 'react';
 import AccountBackupStep1 from './';
 import { backgroundState } from '../../../util/test/initial-root-state';
-import { renderScreen } from '../../../util/test/renderWithProvider';
+import renderWithProvider from '../../../util/test/renderWithProvider';
+import { useNavigation } from '@react-navigation/native';
 
 // Use fake timers to resolve reanimated issues.
 jest.useFakeTimers();
+
+jest.mock('@react-navigation/native', () => {
+  const actualNav = jest.requireActual('@react-navigation/native');
+  return {
+    ...actualNav,
+    useNavigation: jest.fn(),
+    useFocusEffect: jest.fn(),
+  };
+});
 
 describe('AccountBackupStep1', () => {
   afterEach(() => {
     jest.useFakeTimers({ legacyFakeTimers: true });
   });
 
-  it('should render correctly', () => {
-    const wrapper = renderScreen(
-      AccountBackupStep1 as ComponentType,
-      { name: 'AccountBackupStep1' },
+  const setupTest = () => {
+    const mockNavigate = jest.fn();
+    const mockGoBack = jest.fn();
+    const mockSetOptions = jest.fn();
+
+    const initialState = {
+      engine: {
+        backgroundState,
+      },
+    };
+
+    (useNavigation as jest.Mock).mockReturnValue({
+      navigate: mockNavigate,
+      goBack: mockGoBack,
+      setOptions: mockSetOptions,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+      isFocused: jest.fn(),
+      reset: jest.fn(),
+    });
+
+    const wrapper = renderWithProvider(
+      <AccountBackupStep1
+        navigation={{
+          navigate: mockNavigate,
+          goBack: mockGoBack,
+          setOptions: mockSetOptions,
+        }}
+        route={{}}
+      />,
       {
-        state: {
-          engine: {
-            backgroundState,
-          },
-        },
+        state: initialState,
       },
     );
+
+    return {
+      wrapper,
+      mockNavigate,
+      mockGoBack,
+      mockSetOptions,
+    };
+  };
+
+  it('should render correctly', () => {
+    const { wrapper } = setupTest();
     expect(wrapper).toMatchSnapshot();
   });
 });
