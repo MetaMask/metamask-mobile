e6fc0939d922356ae69cdc9e663780041dfb99f7
Ziad Saab <ziad.saab@gmail.com>
2025-06-01 18:45:22 -0500

    Discovery tab implementation (#15569)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    Start the browser in token discovery mode. This PR is only a chunk of
    the work, so the feature was put behind an environment variable flag.
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    ## **Related issues**
    
    Fixes: MMPD-1588
    
    ## **Manual testing steps**
    
    1. Set `TOKEN_DISCOVERY_BROWSER_ENABLED` to `"true"` in your `.js.env`
    file
    2. Reload the app
    3. Open the mobile browser
    4. Create a new tab
    5. Observe that instead of getting sent to portfolio.metamask, you get a
    screen that says "Token Discovery placeholder"
    
    ## **Screenshots/Recordings**
    
    ### **Before**
    
    
    https://github.com/user-attachments/assets/77b8b71c-1c6a-4521-a9a4-b666ea690b5f
    
    
    ### **After** (with environment variable flag turned on)
    
    
    https://github.com/user-attachments/assets/994e164d-1c66-4bc6-97b4-c755bdf4cd02
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

---
+++ app/components/Views/DiscoveryTab/types.ts
@@ -0,0 +1,8 @@
+import { SharedTabProps } from '../shared/browser-tab-shared-types';
+
+/**
+ * The props for the DiscoveryTab component
+ * Extends shared props that are common to both BrowserTab and DiscoveryTab
+ */
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+export type DiscoveryTabProps = SharedTabProps;
