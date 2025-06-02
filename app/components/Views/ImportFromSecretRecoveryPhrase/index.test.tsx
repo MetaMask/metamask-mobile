86075b524c86e517807fc45460e8575f113f59c0
ieow <cwlee0@gmail.com>
2025-06-02 19:56:27 +0800

    chore: add test case on handle new input

--- app/components/Views/ImportFromSecretRecoveryPhrase/index.test.tsx
+++ app/components/Views/ImportFromSecretRecoveryPhrase/index.test.tsx
@@ -827,4 +827,43 @@ describe('handleOnFocus', () => {
       getByText(strings('import_from_seed.spellcheck_error')),
     ).toBeTruthy();
   });
+
+  it('should handle new word input correctly', () => {
+    const getInput = (index: number) => {
+      return getByTestId(
+        `${ImportFromSeedSelectorsIDs.SEED_PHRASE_INPUT_ID}_${index}`,
+      );
+    };
+
+    const { getByPlaceholderText, getByTestId } = renderScreen(
+      ImportFromSecretRecoveryPhrase,
+      { name: Routes.ONBOARDING.IMPORT_FROM_SECRET_RECOVERY_PHRASE },
+      { state: initialState },
+    );
+
+    // Enter invalid seed phrase
+    const input = getByPlaceholderText(
+      strings('import_from_seed.srp_placeholder'),
+    );
+
+    fireEvent.changeText(input, 'horse ');
+
+    const input0 = getInput(0);
+    const input1 = getInput(1);
+
+    expect(input0).toBeOnTheScreen();
+    expect(input1).toBeOnTheScreen();
+
+    fireEvent.changeText(input1, 'invalid2 ');
+    const input2 = getInput(2);
+    expect(input2).toBeOnTheScreen();
+
+    fireEvent.changeText(input2, 'invalid3 ');
+    const input3 = getInput(3);
+    expect(input3).toBeOnTheScreen();
+
+    fireEvent.changeText(getInput(1), 'invalid4 ');
+    const input4 = getInput(4);
+    expect(input4).toBeOnTheScreen();
+  });
 });
