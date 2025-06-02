e01fd4eca62ebbdb84a8da17df50117f86f02455
Monte Lai <monte.lai@consensys.net>
2025-06-02 18:06:58 +0800

    fix: casing

--- app/components/Views/MultichainAccounts/AccountDetails/AccountDetails.tsx
+++ app/components/Views/MultichainAccounts/AccountDetails/AccountDetails.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react';
 import { InternalAccount } from '@metamask/keyring-internal-api';
-import { BaseAccountDetails } from './2accountTypes/2baseAccountDetails';
+import { BaseAccountDetails } from './AccountTypes/BaseAccountDetails';
 
 interface AccountDetailsProps {
   route: {
