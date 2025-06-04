d38acd12a97a549a6e9c480c0c24df7f86f5dffe
Owen Craston <owen.craston@consensys.net>
2025-06-04 12:08:10 +0100

    edit e2e accounts

--- e2e/specs/identity/account-syncing/mock-data.js
+++ e2e/specs/identity/account-syncing/mock-data.js
@@ -1,6 +1,7 @@
 import { USER_STORAGE_FEATURE_NAMES } from '@metamask/profile-sync-controller/sdk';
 import { createEncryptedResponse } from '../utils/user-storage/generateEncryptedData';
 import { IDENTITY_TEAM_STORAGE_KEY } from '../utils/constants';
+import { toFormattedAddress } from '../../../../app/util/address';
 
 /**
  * This array represents the accounts mock data before it is encrypted and sent to UserStorage.
@@ -14,14 +15,14 @@ import { IDENTITY_TEAM_STORAGE_KEY } from '../utils/constants';
 export const accountsToMockForAccountsSync = [
   {
     v: '1',
-    a: '0xAa4179E7f103701e904D27DF223a39Aa9c27405a'.toLowerCase(),
+    a: toFormattedAddress('0xAa4179E7f103701e904D27DF223a39Aa9c27405a'),
     i: '0000-1111',
     n: 'Hello from account 1',
     nlu: 1738590287,
   },
   {
     v: '1',
-    a: '0xd2a4aFe5c2fF0a16Bf81F77ba4201A8107AA874b'.toLowerCase(),
+    a: toFormattedAddress('0xd2a4aFe5c2fF0a16Bf81F77ba4201A8107AA874b'),
     i: '1111-1111',
     n: 'Hello from account 2',
     nlu: 1738590287,
--- e2e/specs/notifications/utils/constants.js
+++ e2e/specs/notifications/utils/constants.js
@@ -1,4 +1,5 @@
 import { createSHA256Hash } from '@metamask/profile-sync-controller/sdk';
+import { toFormattedAddress } from '../../../../app/util/address';
 
 // As we rely on backup and sync for most of our features, we need to use the same SRP for all of our tests
 export const NOTIFICATIONS_TEAM_SEED_PHRASE =
@@ -8,10 +9,12 @@ export const NOTIFICATIONS_TEAM_PASSWORD = 'notify_password';
 export const NOTIFICATIONS_TEAM_STORAGE_KEY =
   '8e05e4135496f3ea56bd256e8d2a802e8a2d9e37fbdec6c1befd0e53f1114b16';
 
-export const NOTIFICATION_WALLET_ACCOUNT_1 =
-  '0xAa4179E7f103701e904D27DF223a39Aa9c27405a'.toLowerCase();
-export const NOTIFICATION_WALLET_ACCOUNT_2 =
-  '0xd2a4aFe5c2fF0a16Bf81F77ba4201A8107AA874b'.toLowerCase();
+export const NOTIFICATION_WALLET_ACCOUNT_1 = toFormattedAddress(
+  '0xAa4179E7f103701e904D27DF223a39Aa9c27405a',
+);
+export const NOTIFICATION_WALLET_ACCOUNT_2 = toFormattedAddress(
+  '0xd2a4aFe5c2fF0a16Bf81F77ba4201A8107AA874b',
+);
 
 // This key is used to point in the UserStorage path for notifications
 export const NOTIFICATION_STORAGE_HASHED_KEY = createSHA256Hash(
