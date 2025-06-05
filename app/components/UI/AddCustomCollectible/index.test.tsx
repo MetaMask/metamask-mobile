7294222cd0b9e25ca36646987569e78ecfc4feda
sahar-fehri <sahar.fehri@consensys.net>
2025-06-05 10:04:57 +0200

    fix: test

--- app/components/UI/AddCustomCollectible/index.test.tsx
+++ app/components/UI/AddCustomCollectible/index.test.tsx
@@ -4,6 +4,8 @@ import AddCustomCollectible from './';
 import configureMockStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 import initialRootState from '../../../util/test/initial-root-state';
+import renderWithProvider from '../../../util/test/renderWithProvider';
+import { fireEvent } from '@testing-library/react-native';
 
 const mockStore = configureMockStore();
 
@@ -22,5 +24,27 @@ describe('AddCustomCollectible', () => {
       </Provider>,
     );
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('handles address input changes', () => {
+    const { getByTestId } = renderWithProvider(
+      <AddCustomCollectible />,
+      { state: initialRootState },
+    );
+
+    const textfield = getByTestId('input-collectible-address');
+    fireEvent.changeText(textfield, '0xtestAddress');
+    expect(textfield.props.value).toBe('0xtestAddress');
+  });
+
+  it('handles tokenId input changes', () => {
+    const { getByTestId } = renderWithProvider(
+      <AddCustomCollectible />,
+      { state: initialRootState },
+    );
+
+    const textfield = getByTestId('input-collectible-identifier');
+    fireEvent.changeText(textfield, '55');
+    expect(textfield.props.value).toBe('55');
   });
 });
