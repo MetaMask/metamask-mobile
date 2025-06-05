73f76819b84a71e436e766985df818f482a38b6b
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-06-05 09:17:05 +0200

    Rename

--- app/components/Views/confirmations/components/info/switch-account-type/switch-account-type.tsx
+++ app/components/Views/confirmations/components/info/switch-account-type/switch-account-type.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { View } from 'react-native';
 
 import { useConfirmationMetricEvents } from '../../../hooks/metrics/useConfirmationMetricEvents';
-import AccountNetworkInfo from '../../rows/account-network-info-row';
+import AccountNetworkInfoRow from '../../rows/account-network-info-row';
 import AdvancedDetailsRow from '../../rows/transactions/advanced-details-row/advanced-details-row';
 import GasFeesDetailsRow from '../../rows/transactions/gas-fee-details-row';
 import SwitchAccountTypeInfoRow from '../../rows/switch-account-type-info-row';
@@ -13,7 +13,7 @@ const SwitchAccountType = () => {
 
   return (
     <View>
-      <AccountNetworkInfo />
+      <AccountNetworkInfoRow />
       <SwitchAccountTypeInfoRow />
       <GasFeesDetailsRow />
       <AdvancedDetailsRow />
