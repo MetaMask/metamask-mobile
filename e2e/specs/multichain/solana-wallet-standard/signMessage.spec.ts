0805e6c6507b09f309d31083e2bb0a5845386ffe
Baptiste Marchand <75846779+baptiste-marchand@users.noreply.github.com>
2025-06-05 10:54:33 +0200

    feat: switch account e2e test

--- e2e/specs/multichain/solana-wallet-standard/signMessage.spec.ts
+++ e2e/specs/multichain/solana-wallet-standard/signMessage.spec.ts
@@ -2,8 +2,6 @@
 import { SmokeNetworkExpansion } from '../../../tags';
 import SolanaTestDApp from '../../../pages/Browser/SolanaTestDApp';
 import { connectSolanaTestDapp, navigateToSolanaTestDApp } from './testHelpers';
-import Gestures from '../../../utils/Gestures';
-import Matchers from '../../../utils/Matchers';
 import Assertions from '../../../utils/Assertions';
 import { withSolanaAccountSnap } from '../../../common-solana';
 
@@ -25,14 +23,9 @@ describe(
           await signMessageTest.signMessage();
 
           // Confirm the signature
-          // const confirmButton = Matchers.getElementByText('Confirm');
-          const confirmButton = Matchers.getElementByID(
-            'confirm-sign-message-confirm-snap-footer-button',
-          );
-          await Gestures.waitAndTap(confirmButton);
+          await SolanaTestDApp.confirmSignMessage();
 
           const signedMessage = await signMessageTest.getSignedMessage();
-          await Assertions.checkIfTextIsDisplayed('Sign message', 100);
           await Assertions.checkIfTextMatches(
             signedMessage,
             'Kort1JYMAf3dmzKRx4WiYXW9gSfPHzxw0flAka25ymjB4d+UZpU/trFoSPk4DM7emT1c/e6Wk0bsRcLsj/h9BQ==',
