9138d82620731a62adbffd68a6e0dde399365694
George Weiler <georgejweiler@gmail.com>
2025-06-05 09:28:20 -0600

    feat(deposit): fetch user KYC status and route to correct view (#16091)
    When the user clicks the build quote CTA, their account info is fetched
    and the next screen is determined based on their user's authentication
    and KYC status.
    
    - adds "smart routing" to the deposit build quote page. 
    - overhauls `useDepositSdkMethod` to match the pattern used by ramp
    `useSdkMethod`
    - 
    
    depends on
    https://github.com/consensys-vertical-apps/va-mmcx-native-ramps-sdk/pull/11
    
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Pedro Pablo Aste Kompen <wachunei@gmail.com>

--- app/components/UI/Ramp/Deposit/Views/BuildQuote/BuildQuote.test.tsx
+++ app/components/UI/Ramp/Deposit/Views/BuildQuote/BuildQuote.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { fireEvent, screen } from '@testing-library/react-native';
-import { renderScreen } from '../../../../../../util/test/renderWithProvider';
+import { screen } from '@testing-library/react-native';
 import BuildQuote from './BuildQuote';
 import Routes from '../../../../../../constants/navigation/Routes';
-import { backgroundState } from '../../../../../../util/test/initial-root-state';
+import renderDepositTestComponent from '../../utils/renderDepositTestComponent';
 
 const mockNavigate = jest.fn();
 const mockGoBack = jest.fn();
@@ -30,19 +29,7 @@ jest.mock('../../../../Navbar', () => ({
 }));
 
 function render(Component: React.ComponentType) {
-  return renderScreen(
-    Component,
-    {
-      name: Routes.DEPOSIT.BUILD_QUOTE,
-    },
-    {
-      state: {
-        engine: {
-          backgroundState,
-        },
-      },
-    },
-  );
+  return renderDepositTestComponent(Component, Routes.DEPOSIT.BUILD_QUOTE);
 }
 
 describe('BuildQuote Component', () => {
@@ -50,26 +37,8 @@ describe('BuildQuote Component', () => {
     mockNavigate.mockClear();
   });
 
-  it('renders correctly', () => {
+  it('render matches snapshot', () => {
     render(BuildQuote);
-    expect(screen.getByText('Build Quote Page Placeholder')).toBeTruthy();
-  });
-
-  it('navigates to Email screen on "Continue" button press', () => {
-    render(BuildQuote);
-    fireEvent.press(screen.getByRole('button', { name: 'Continue' }));
-    expect(mockNavigate).toHaveBeenCalledWith(
-      Routes.DEPOSIT.ENTER_EMAIL,
-      undefined,
-    );
-  });
-
-  it('calls setOptions when the component mounts', () => {
-    render(BuildQuote);
-    expect(mockSetNavigationOptions).toHaveBeenCalledWith(
-      expect.objectContaining({
-        title: 'Build Quote',
-      }),
-    );
+    expect(screen.toJSON()).toMatchSnapshot();
   });
 });
