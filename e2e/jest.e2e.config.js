08e9bc73751ebcded0cb4fa0c62d3614a460a64a
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-06-06 08:48:35 +0200

    Fix test matcher

--- e2e/jest.e2e.config.js
+++ e2e/jest.e2e.config.js
@@ -3,7 +3,14 @@ require('dotenv').config({ path: '.e2e.env' });
 module.exports = {
   rootDir: '..',
   testMatch: [
-    '<rootDir>/e2e/specs/**/*.spec.{js,ts}',
+    '<rootDir>/e2e/specs/*.spec.js',
+    '<rootDir>/e2e/specs/*.spec.ts',
+    '<rootDir>/e2e/specs/*/*.spec.js',
+    '<rootDir>/e2e/specs/*/*.spec.ts',
+    '<rootDir>/e2e/specs/*/*/*.spec.js',
+    '<rootDir>/e2e/specs/*/*/*.spec.ts',
+    '<rootDir>/e2e/specs/*/*/*/*.spec.js',
+    '<rootDir>/e2e/specs/*/*/*/*.spec.ts',
   ],
   testTimeout: 250000,
   maxWorkers: process.env.CI ? 3 : 1,
