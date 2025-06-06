32fce151f71a4041e98ac93b025eba5effa7db7d
AxelGes <axelges9@gmail.com>
2025-06-05 22:07:14 -0300

    feat: improve providerwebview boilerplate

---
+++ app/components/UI/Ramp/Deposit/Views/ProviderWebview/ProviderWebview.styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const styleSheet = () => {
+  return StyleSheet.create({
+    description: {
+      textAlign: 'center',
+      marginTop: 16,
+    },
+  });
+};
+
+export default styleSheet;
