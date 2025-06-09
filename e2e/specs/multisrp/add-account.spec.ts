fb33a9ee5fb94170cb9165739169a7a477d03aa8
Monte Lai <monte.lai@consensys.net>
2025-06-09 17:05:49 +0800

    fix: tag

--- e2e/specs/multisrp/add-account.spec.ts
+++ e2e/specs/multisrp/add-account.spec.ts
@@ -1,5 +1,5 @@
 'use strict';
-import { SmokeAccounts } from '../../tags';
+import { SmokeWalletPlatform } from '../../tags';
 import FixtureBuilder from '../../fixtures/fixture-builder';
 import {
   loadFixture,
@@ -62,31 +62,34 @@ const addAccountToSrp = async (
   await Assertions.checkIfVisible(AccountListBottomSheet.accountList);
 };
 
-describe(SmokeAccounts('Multi-SRP: Add new account to a specific SRP'), () => {
-  beforeAll(async () => {
-    await TestHelpers.reverseServerPort();
-    const fixture = new FixtureBuilder()
-      .withImportedHdKeyringAndTwoDefaultAccountsOneImportedHdAccountKeyringController()
-      .build();
-    await startFixtureServer(fixtureServer);
-    await loadFixture(fixtureServer, { fixture });
-    await TestHelpers.launchApp({
-      launchArgs: { fixtureServerPort: `${getFixturesServerPort()}` },
+describe(
+  SmokeWalletPlatform('Multi-SRP: Add new account to a specific SRP'),
+  () => {
+    beforeAll(async () => {
+      await TestHelpers.reverseServerPort();
+      const fixture = new FixtureBuilder()
+        .withImportedHdKeyringAndTwoDefaultAccountsOneImportedHdAccountKeyringController()
+        .build();
+      await startFixtureServer(fixtureServer);
+      await loadFixture(fixtureServer, { fixture });
+      await TestHelpers.launchApp({
+        launchArgs: { fixtureServerPort: `${getFixturesServerPort()}` },
+      });
+      await loginToApp();
+      await WalletView.tapIdenticon();
+      await Assertions.checkIfVisible(AccountListBottomSheet.accountList);
     });
-    await loginToApp();
-    await WalletView.tapIdenticon();
-    await Assertions.checkIfVisible(AccountListBottomSheet.accountList);
-  });
 
-  afterAll(async () => {
-    await stopFixtureServer(fixtureServer);
-  });
+    afterAll(async () => {
+      await stopFixtureServer(fixtureServer);
+    });
 
-  it('adds an account to default SRP', async () => {
-    await addAccountToSrp(SRP_1, 'Account 4');
-  });
+    it('adds an account to default SRP', async () => {
+      await addAccountToSrp(SRP_1, 'Account 4');
+    });
 
-  it('adds an account to the imported SRP', async () => {
-    await addAccountToSrp(SRP_2, 'Account 5');
-  });
-});
+    it('adds an account to the imported SRP', async () => {
+      await addAccountToSrp(SRP_2, 'Account 5');
+    });
+  },
+);
