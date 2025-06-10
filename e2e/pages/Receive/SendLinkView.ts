93d237d3c87ead5491527866baa72f869ab03651
Chris Wilcox <chris.wilcox@consensys.net>
2025-06-10 14:21:49 -0700

    feat: add SendLinkView class for link sending interactions
    This commit introduces a new class, SendLinkView, which encapsulates the functionality for interacting with the send link view. It includes methods to access various UI elements such as the QR modal and buttons, enhancing the end-to-end testing capabilities for link sending interactions.

--- e2e/pages/Receive/SendLinkView.ts
+++ e2e/pages/Receive/SendLinkView.ts
@@ -2,34 +2,37 @@ import Matchers from '../../utils/Matchers';
 import Gestures from '../../utils/Gestures';
 import { SendLinkViewSelectorsIDs } from '../../selectors/Receive/SendLinkView.selectors';
 
+type DetoxElement = Promise<Detox.IndexableNativeElement | Detox.NativeElement | Detox.IndexableSystemElement>;
+type TappableElement = Promise<Detox.IndexableNativeElement | Detox.SystemElement>;
+
 class SendLinkView {
-  get container() {
+  get container(): DetoxElement {
     return Matchers.getElementByID(SendLinkViewSelectorsIDs.CONTAINER_ID);
   }
 
-  get qrModal() {
+  get qrModal(): DetoxElement {
     return Matchers.getElementByID(SendLinkViewSelectorsIDs.QR_MODAL);
   }
 
-  get closeSendLinkButton() {
+  get closeSendLinkButton(): TappableElement {
     return Matchers.getElementByID(
       SendLinkViewSelectorsIDs.CLOSE_SEND_LINK_VIEW_BUTTON,
-    );
+    ) as TappableElement;
   }
 
-  get qrCodeButton() {
+  get qrCodeButton(): TappableElement {
     return device.getPlatform() === 'android'
       ? Matchers.getElementByLabel(SendLinkViewSelectorsIDs.QR_CODE_BUTTON)
       : Matchers.getElementByID(SendLinkViewSelectorsIDs.QR_CODE_BUTTON);
   }
 
-  async tapQRCodeButton() {
+  async tapQRCodeButton(): Promise<void> {
     await Gestures.waitAndTap(this.qrCodeButton);
   }
 
-  async tapCloseSendLinkButton() {
+  async tapCloseSendLinkButton(): Promise<void> {
     await Gestures.waitAndTap(this.closeSendLinkButton);
   }
 }
 
-export default new SendLinkView();
+export default new SendLinkView(); 
