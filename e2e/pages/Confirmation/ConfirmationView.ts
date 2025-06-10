279c787a1212e6aeb490781ecb4802f9355c2453
Chris Wilcox <chris.wilcox@consensys.net>
2025-06-10 13:27:30 -0700

    refactor: simplify return types in ConfirmationView class
    This commit refactors the `securityAlertBanner` and `securityAlertResponseFailedBanner` getters in the `ConfirmationView` class to use a new `DetoxElement` type, enhancing code readability and maintainability. This change streamlines the return type definition for better clarity in end-to-end testing scenarios.

--- e2e/pages/Confirmation/ConfirmationView.ts
+++ e2e/pages/Confirmation/ConfirmationView.ts
@@ -1,12 +1,14 @@
 import { ConfirmationTopSheetSelectorsIDs } from '../../selectors/Confirmation/ConfirmationView.selectors';
 import Matchers from '../../utils/Matchers';
 
+type DetoxElement = Promise<Detox.IndexableNativeElement | Detox.NativeElement | Detox.IndexableSystemElement>;
+
 class ConfirmationView {
-  get securityAlertBanner(): Promise<Detox.IndexableNativeElement | Detox.NativeElement | Detox.IndexableSystemElement> {
+  get securityAlertBanner(): DetoxElement {
     return Matchers.getElementByID(ConfirmationTopSheetSelectorsIDs.SECURITY_ALERT_BANNER);
   }
 
-  get securityAlertResponseFailedBanner(): Promise<Detox.IndexableNativeElement | Detox.NativeElement | Detox.IndexableSystemElement> {
+  get securityAlertResponseFailedBanner(): DetoxElement {
     return Matchers.getElementByID(ConfirmationTopSheetSelectorsIDs.SECURITY_ALERT_RESPONSE_FAILED_BANNER);
   }
 }
