6a41329606d06f98d50a94e7c5a7bee81753b715
hieu-w <hieu@tor.us>
2025-06-10 11:38:27 +0700

    test: add waitForElementToBeEnabled instead delay

--- e2e/viewHelper.js
+++ e2e/viewHelper.js
@@ -37,7 +37,6 @@ export const acceptTermOfUse = async () => {
   await Assertions.checkIfVisible(TermsOfUseModal.container);
   await TermsOfUseModal.tapScrollEndButton();
   await TermsOfUseModal.tapAgreeCheckBox();
-  await TestHelpers.delay(3500);
   await TermsOfUseModal.tapAcceptButton();
   await Assertions.checkIfNotVisible(TermsOfUseModal.container);
 };
@@ -118,7 +117,6 @@ export const importWalletWithRecoveryPhrase = async ({
   await OnboardingCarouselView.tapOnGetStartedButton();
   await acceptTermOfUse();
 
-  await TestHelpers.delay(3500);
   await OnboardingView.tapImportWalletFromSeedPhrase();
 
   if (optInToMetrics) {
@@ -140,7 +138,6 @@ export const importWalletWithRecoveryPhrase = async ({
   await CreatePasswordView.enterPassword(password ?? validAccount.password);
   await CreatePasswordView.reEnterPassword(password ?? validAccount.password);
   await CreatePasswordView.tapIUnderstandCheckBox();
-  await TestHelpers.delay(3500);
   await CreatePasswordView.tapCreatePasswordButton();
 
   //'Should dismiss Enable device Notifications checks alert'
