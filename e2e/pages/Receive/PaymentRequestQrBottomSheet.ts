fef22fd10a4188c0c3dcc1af3fb577aeccb5869d
Chris Wilcox <chris.wilcox@consensys.net>
2025-06-10 13:59:52 -0700

    feat: add PaymentRequestQrBottomSheet for QR modal interactions
    This commit introduces a new class, PaymentRequestQrBottomSheet, which encapsulates the functionality for interacting with the QR modal in the payment request flow. It includes methods to access the modal container and close button, as well as a method to tap the close button using defined gestures.

--- e2e/pages/Receive/PaymentRequestQrBottomSheet.ts
+++ e2e/pages/Receive/PaymentRequestQrBottomSheet.ts
@@ -2,20 +2,22 @@ import Matchers from '../../utils/Matchers';
 import Gestures from '../../utils/Gestures';
 import { SendLinkViewSelectorsIDs } from '../../selectors/Receive/SendLinkView.selectors';
 
+type DetoxElement = Promise<Detox.IndexableNativeElement | Detox.NativeElement | Detox.IndexableSystemElement>;
+
 class PaymentRequestQrBottomSheet {
-  get container() {
+  get container(): DetoxElement {
     return Matchers.getElementByID(SendLinkViewSelectorsIDs.QR_MODAL);
   }
 
-  get closeButton() {
+  get closeButton(): DetoxElement {
     return Matchers.getElementByID(
       SendLinkViewSelectorsIDs.CLOSE_QR_MODAL_BUTTON,
     );
   }
 
-  async tapCloseButton() {
+  async tapCloseButton(): Promise<void> {
     await Gestures.waitAndTap(this.closeButton);
   }
 }
 
-export default new PaymentRequestQrBottomSheet();
+export default new PaymentRequestQrBottomSheet(); 
