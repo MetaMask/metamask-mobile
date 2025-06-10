30fb77c8893313835edd7019514e9a2ca2c24845
Chris Wilcox <chris.wilcox@consensys.net>
2025-06-10 14:18:42 -0700

    feat: add RequestPaymentView class for payment request interactions
    This commit introduces a new class, RequestPaymentView, which encapsulates the functionality for interacting with the request payment view. It includes methods to access various UI elements and perform actions such as tapping buttons and entering text, enhancing the end-to-end testing capabilities for payment requests.

--- e2e/pages/Receive/RequestPaymentView.ts
+++ e2e/pages/Receive/RequestPaymentView.ts
@@ -2,51 +2,55 @@ import { RequestPaymentViewSelectors } from '../../selectors/Receive/RequestPaym
 import Matchers from '../../utils/Matchers';
 import Gestures from '../../utils/Gestures';
 
+type DetoxElement = Promise<Detox.IndexableNativeElement | Detox.NativeElement | Detox.IndexableSystemElement>;
+type TappableElement = Promise<Detox.IndexableNativeElement | Detox.SystemElement>;
+type TypableElement = Promise<Detox.IndexableNativeElement>;
+
 class RequestPaymentView {
-  get backButton() {
+  get backButton(): TappableElement {
     return Matchers.getElementByID(RequestPaymentViewSelectors.BACK_BUTTON_ID);
   }
 
-  get tokenSearchInput() {
+  get tokenSearchInput(): TypableElement {
     return Matchers.getElementByID(
       RequestPaymentViewSelectors.TOKEN_SEARCH_INPUT_BOX,
-    );
+    ) as TypableElement;
   }
 
-  get requestAmountInput() {
+  get requestAmountInput(): TypableElement {
     return Matchers.getElementByID(
       RequestPaymentViewSelectors.REQUEST_AMOUNT_INPUT_BOX_ID,
-    );
+    ) as TypableElement;
   }
 
-  get requestPaymentContainer() {
+  get requestPaymentContainer(): DetoxElement {
     return Matchers.getElementByID(
       RequestPaymentViewSelectors.REQUEST_PAYMENT_CONTAINER_ID,
     );
   }
 
-  get requestAssetList() {
+  get requestAssetList(): DetoxElement {
     return Matchers.getElementByID(
       RequestPaymentViewSelectors.REQUEST_ASSET_LIST_ID,
     );
   }
 
-  async tapBackButton() {
+  async tapBackButton(): Promise<void> {
     await Gestures.waitAndTap(this.backButton);
   }
 
-  async searchForToken(token) {
+  async searchForToken(token: string): Promise<void> {
     await Gestures.typeTextAndHideKeyboard(this.tokenSearchInput, token);
   }
 
-  async tapOnToken(token) {
+  async tapOnToken(token: string): Promise<void> {
     const tokenElement = await Matchers.getElementByText(token, 1);
-    await Gestures.waitAndTap(tokenElement);
+    await Gestures.waitAndTap(Promise.resolve(tokenElement));
   }
 
-  async typeInTokenAmount(amount) {
-    await Gestures.typeTextAndHideKeyboard(this.requestAmountInput, amount);
+  async typeInTokenAmount(amount: number | string): Promise<void> {
+    await Gestures.typeTextAndHideKeyboard(this.requestAmountInput, String(amount));
   }
 }
 
-export default new RequestPaymentView();
+export default new RequestPaymentView(); 
