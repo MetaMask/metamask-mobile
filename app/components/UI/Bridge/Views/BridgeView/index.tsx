2690c72d0fd1ab24f73b4e3dc406aa940e7bd661
Bryan Fullam <bryan.fullam@consensys.net>
2025-06-10 13:31:22 +0200

    fix: remove unused ref

--- app/components/UI/Bridge/Views/BridgeView/index.tsx
+++ app/components/UI/Bridge/Views/BridgeView/index.tsx
@@ -86,15 +86,6 @@ const BridgeView = () => {
   const [isErrorBannerVisible, setIsErrorBannerVisible] = useState(true);
   const isSubmittingTx = useSelector(selectIsSubmittingTx);
 
-  // Ref necessary to avoid race condition between Redux state and component state
-  // Without it, the component would reset the bridge state when it shouldn't
-  const isSubmittingTxRef = useRef(isSubmittingTx);
-
-  // Update ref when Redux state changes
-  useEffect(() => {
-    isSubmittingTxRef.current = isSubmittingTx;
-  }, [isSubmittingTx]);
-
   const { styles } = useStyles(createStyles, {});
   const dispatch = useDispatch();
   const navigation = useNavigation();
