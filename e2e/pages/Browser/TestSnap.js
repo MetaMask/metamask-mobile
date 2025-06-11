ee652185398dbc813ca682c1ced63ae34252d027
Yande <andepandy@outlook.com>
2025-06-11 17:05:05 +0100

    Update TestSnap.js

--- e2e/pages/Browser/TestSnap.js
+++ e2e/pages/Browser/TestSnap.js
@@ -749,23 +749,23 @@ class TestSnaps {
   }
 
   async connectToSnap() {
-    await Gestures.waitAndTap(this.getConnectSnapButton, { delayBeforeTap: 4000, timeout: 30000 });
+    await Gestures.waitAndTap(this.getConnectSnapButton, { skipVisibilityCheck: true, delayBeforeTap: 2500 });
   }
 
   async connectToSnapPermissionsRequest() {
-    await Gestures.waitAndTap(this.getConnectSnapPermissionsRequestButton, { delayBeforeTap: 4000 });
+    await Gestures.waitAndTap(this.getConnectSnapPermissionsRequestButton, { skipVisibilityCheck: true, delayBeforeTap: 2500 });
   }
 
   async approveSnapPermissionsRequest() {
-    await Gestures.waitAndTap(this.getApproveSnapPermissionsRequestButton, { delayBeforeTap: 4000 });
+    await Gestures.waitAndTap(this.getApproveSnapPermissionsRequestButton, { skipVisibilityCheck: true, delayBeforeTap: 2500 });
   }
 
   async cancelSnapPermissionsRequest() {
-    await Gestures.waitAndTap(this.getCancelSnapPermissionsRequestButton, { delayBeforeTap: 4000 });
+    await Gestures.waitAndTap(this.getCancelSnapPermissionsRequestButton, { skipVisibilityCheck: true, delayBeforeTap: 2500 });
   }
 
   async connectToSnapInstallOk() {
-    await Gestures.waitAndTap(this.getConnectSnapInstallOkButton, { delayBeforeTap: 4000 });
+    await Gestures.waitAndTap(this.getConnectSnapInstallOkButton, { skipVisibilityCheck: true, delayBeforeTap: 2500 });
   }
 
   async tapPublicKeyBip44Button() {
