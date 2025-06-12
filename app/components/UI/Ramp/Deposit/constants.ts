e9c7d585f0137c7e93d5673ebe91515f355c3031
George Weiler <george.weiler@consensys.net>
2025-06-11 21:40:34 -0600

    feat: mvp integratino of react-phone-input with country picker

---
+++ app/components/UI/Ramp/Deposit/constants.ts
@@ -0,0 +1,148 @@
+import { CountryCode } from 'libphonenumber-js';
+
+export const AVAILABLE_COUNTRIES_PHONE_INPUT: {
+  code: CountryCode;
+  flag: string;
+  name: string;
+}[] = [
+  {
+    code: 'US',
+    flag: '🇺🇸',
+    name: 'United States',
+  },
+  {
+    code: 'AT',
+    flag: '🇦🇹',
+    name: 'Austria',
+  },
+  {
+    code: 'BE',
+    flag: '🇧🇪',
+    name: 'Belgium',
+  },
+  {
+    code: 'BG',
+    flag: '🇧🇬',
+    name: 'Bulgaria',
+  },
+  {
+    code: 'HR',
+    flag: '🇭🇷',
+    name: 'Croatia',
+  },
+  {
+    code: 'CY',
+    flag: '🇨🇾',
+    name: 'Cyprus',
+  },
+  {
+    code: 'CZ',
+    flag: '🇨🇿',
+    name: 'Czech Republic',
+  },
+  {
+    code: 'DK',
+    flag: '🇩🇰',
+    name: 'Denmark',
+  },
+  {
+    code: 'EE',
+    flag: '🇪🇪',
+    name: 'Estonia',
+  },
+  {
+    code: 'FI',
+    flag: '🇫🇮',
+    name: 'Finland',
+  },
+  {
+    code: 'FR',
+    flag: '🇫🇷',
+    name: 'France',
+  },
+  {
+    code: 'DE',
+    flag: '🇩🇪',
+    name: 'Germany',
+  },
+  {
+    code: 'GR',
+    flag: '🇬🇷',
+    name: 'Greece',
+  },
+  {
+    code: 'HU',
+    flag: '🇭🇺',
+    name: 'Hungary',
+  },
+  {
+    code: 'IE',
+    flag: '🇮🇪',
+    name: 'Ireland',
+  },
+  {
+    code: 'IT',
+    flag: '🇮🇹',
+    name: 'Italy',
+  },
+  {
+    code: 'LV',
+    flag: '🇱🇻',
+    name: 'Latvia',
+  },
+  {
+    code: 'LT',
+    flag: '🇱🇹',
+    name: 'Lithuania',
+  },
+  {
+    code: 'LU',
+    flag: '🇱🇺',
+    name: 'Luxembourg',
+  },
+  {
+    code: 'MT',
+    flag: '🇲🇹',
+    name: 'Malta',
+  },
+  {
+    code: 'NL',
+    flag: '🇳🇱',
+    name: 'Netherlands',
+  },
+  {
+    code: 'PL',
+    flag: '🇵🇱',
+    name: 'Poland',
+  },
+  {
+    code: 'PT',
+    flag: '🇵🇹',
+    name: 'Portugal',
+  },
+  {
+    code: 'RO',
+    flag: '🇷🇴',
+    name: 'Romania',
+  },
+  {
+    code: 'SK',
+    flag: '🇸🇰',
+    name: 'Slovakia',
+  },
+  {
+    code: 'SI',
+    flag: '🇸🇮',
+    name: 'Slovenia',
+  },
+  {
+    code: 'ES',
+    flag: '🇪🇸',
+    name: 'Spain',
+  },
+  {
+    code: 'SE',
+    flag: '🇸🇪',
+    name: 'Sweden',
+  },
+];
--- app/components/UI/Ramp/Deposit/utils/index.ts
+++ app/components/UI/Ramp/Deposit/utils/index.ts
@@ -1,4 +1,52 @@
-'use strict';
+import {
+  AsYouType,
+  CountryCode,
+  getCountryCallingCode,
+  parsePhoneNumberFromString,
+} from 'libphonenumber-js';
+
+export const formatPhoneNumber = (text: string, countryCode: CountryCode) => {
+  const formatter = new AsYouType('US');
+  const callingCode = getCountryCallingCode('US');
+  const maxLength = 10;
+
+  if (text.length === 0) {
+    return {
+      formatted: '',
+      nationalNumber: '',
+      isComplete: false,
+      e164Format: `+${callingCode}`,
+    };
+  }
+
+  // do not allow users to start with calling code
+  if (text[0] === callingCode) {
+    text = text.slice(callingCode.length);
+  }
+
+  // special "(" formatting for US numbers
+  if (text.length === 1 && callingCode === '1') {
+    return {
+      formatted: `(${text}`,
+      nationalNumber: text,
+      isComplete: false,
+      e164Format: '',
+    };
+  }
+
+  if (text.length > maxLength) {
+    text = text.slice(0, maxLength);
+  }
+
+  const formatted = formatter.input(text);
+
+  return {
+    formatted,
+    nationalNumber: text,
+    isComplete: text.length === maxLength,
+    e164Format: `+${callingCode}${text}`,
+  };
+};
 
 const emailRegex =
   /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
