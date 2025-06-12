26c96f19c49e6a2517c5f30667fc0b5c7e72a602
Owen Craston <owen.craston@consensys.net>
2025-06-12 21:50:31 +0100

    edit wallet name

---
+++ e2e/selectors/MultichainAccounts/WalletDetails.ts
@@ -0,0 +1,5 @@
+export const WalletDetailsIds = {
+  WALLET_DETAILS_CONTAINER: 'wallet-details-container',
+  BACK_BUTTON: 'wallet-details-back-button',
+  WALLET_NAME: 'wallet-details-wallet-name',
+};
