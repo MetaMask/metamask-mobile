cf589f6ee668bcb24b14f5101767e89280e9ce8f
Vivek <vivek.srilal@consensys.net>
2025-06-12 12:23:28 +0400

    refactor to move into existing spec under confirmations

--- e2e/specs/confirmations/send-erc20-with-dapp.spec.ts
+++ e2e/specs/confirmations/send-erc20-with-dapp.spec.ts
@@ -18,7 +18,11 @@ import TestDApp from '../../pages/Browser/TestDApp';
 import Assertions from '../../utils/Assertions';
 import { mockEvents } from '../../api-mocking/mock-config/mock-events';
 import { buildPermissions } from '../../fixtures/utils';
+import FooterActions from '../../pages/Browser/Confirmations/FooterActions';
+import { CustomNetworks } from '../../resources/networks.e2e';
 
+const MONAD_TESTNET = CustomNetworks.MonadTestnet.providerConfig;
+const MEGAETH_TESTNET = CustomNetworks.MegaTestnet.providerConfig;
 const HST_CONTRACT = SMART_CONTRACTS.HST;
 
 describe(SmokeConfirmations('ERC20 tokens'), () => {
@@ -80,6 +84,91 @@ describe(SmokeConfirmations('ERC20 tokens'), () => {
             (contractConfiguration[HST_CONTRACT] as any).tokenName as string,
           ),
         );
+      },
+    );
+  });
+
+it(`send an ERC20 token from a dapp using ${MONAD_TESTNET.nickname}`, async () => {
+    await withFixtures(
+      {
+        dapp: true,
+        fixture: new FixtureBuilder()
+          .withMonadTestnetNetwork()
+          .withPermissionControllerConnectedToTestDapp(
+            buildPermissions([`${MONAD_TESTNET.chainId}`]),
+          )
+          .build(),
+        restartDevice: true,
+        smartContract: HST_CONTRACT,
+      },
+      // Remove any once withFixtures is typed
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      async ({ contractRegistry }: { contractRegistry: any }) => {
+        const hstAddress = await contractRegistry.getContractAddress(
+          HST_CONTRACT,
+        );
+
+        await loginToApp();
+
+        // Navigate to the browser screen
+        await TabBarComponent.tapBrowser();
+        await TestDApp.navigateToTestDappWithContract({
+          contractAddress: hstAddress,
+        });
+        await TestHelpers.delay(3000);
+
+        // Transfer ERC20 tokens
+        await TestDApp.tapERC20TransferButton();
+        await TestHelpers.delay(3000);
+
+        // Accept confirmation
+        await FooterActions.tapConfirmButton();
+
+        // Check activity tab
+        await TabBarComponent.tapActivity();
+        await Assertions.checkIfTextIsDisplayed('Confirmed');
+      },
+    );
+  });
+
+  it(`send an ERC20 token from a dapp using ${MEGAETH_TESTNET.nickname}`, async () => {
+    await withFixtures(
+      {
+        dapp: true,
+        fixture: new FixtureBuilder()
+          .withMonadTestnetNetwork()
+          .withPermissionControllerConnectedToTestDapp(
+            buildPermissions([`${MEGAETH_TESTNET.chainId}`]),
+          )
+          .build(),
+        restartDevice: true,
+        smartContract: HST_CONTRACT,
+      },
+      // Remove any once withFixtures is typed
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      async ({ contractRegistry }: { contractRegistry: any }) => {
+        const hstAddress = await contractRegistry.getContractAddress(
+          HST_CONTRACT,
+        );
+        await loginToApp();
+
+        // Navigate to the browser screen
+        await TabBarComponent.tapBrowser();
+        await TestDApp.navigateToTestDappWithContract({
+          contractAddress: hstAddress,
+        });
+        await TestHelpers.delay(3000);
+
+        // Transfer ERC20 tokens
+        await TestDApp.tapERC20TransferButton();
+        await TestHelpers.delay(3000);
+
+        // Accept confirmation
+        await FooterActions.tapConfirmButton();
+
+        // Check activity tab
+        await TabBarComponent.tapActivity();
+        await Assertions.checkIfTextIsDisplayed('Confirmed');
       },
     );
   });
