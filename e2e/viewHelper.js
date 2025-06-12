ac727bb47da9e7997d56d613233f3955753fbc4a
ieow <cwlee0@gmail.com>
2025-06-12 12:14:54 +0800

    fix: remove not needed delay

--- e2e/viewHelper.js
+++ e2e/viewHelper.js
@@ -37,7 +37,6 @@ export const acceptTermOfUse = async () => {
   await Assertions.checkIfVisible(TermsOfUseModal.container);
   await TermsOfUseModal.tapScrollEndButton();
   await TermsOfUseModal.tapAgreeCheckBox();
-  await TestHelpers.delay(3500);
   await TermsOfUseModal.tapAcceptButton();
   await Assertions.checkIfNotVisible(TermsOfUseModal.container);
 };
@@ -139,7 +138,6 @@ export const importWalletWithRecoveryPhrase = async ({
   await CreatePasswordView.enterPassword(password ?? validAccount.password);
   await CreatePasswordView.reEnterPassword(password ?? validAccount.password);
   await CreatePasswordView.tapIUnderstandCheckBox();
-  await TestHelpers.delay(3500);
   await CreatePasswordView.tapCreatePasswordButton();
 
   //'Should dismiss Enable device Notifications checks alert'
