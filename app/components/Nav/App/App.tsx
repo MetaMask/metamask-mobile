c7fd2b1bf35f4da456212d9636492f44067599f5
Aslau Mario-Daniel <marioaslau@gmail.com>
2025-06-13 18:56:27 +0300

    feat: Fix linting erros

--- app/components/Nav/App/App.tsx
+++ app/components/Nav/App/App.tsx
@@ -794,8 +794,6 @@ const App: React.FC = () => {
 
   const existingUser = useSelector(selectExistingUser);
 
-  // existingUser is not present in the dependency array because it is not needed to re-run the effect when it changes and it will cause a bug.
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     const appTriggeredAuth = async () => {
       setOnboarded(!!existingUser);
@@ -833,6 +831,8 @@ const App: React.FC = () => {
     appTriggeredAuth().catch((error) => {
       Logger.error(error, 'App: Error in appTriggeredAuth');
     });
+  // existingUser is not present in the dependency array because it is not needed to re-run the effect when it changes and it will cause a bug.
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [navigation, queueOfHandleDeeplinkFunctions]);
 
   const handleDeeplink = useCallback(
@@ -1032,6 +1032,8 @@ const App: React.FC = () => {
     startApp().catch((error) => {
       Logger.error(error, 'Error starting app');
     });
+  // existingUser is not present in the dependency array because it is not needed to re-run the effect when it changes and it will cause a bug.
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
