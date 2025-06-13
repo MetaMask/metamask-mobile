4f87a336307fd7a9c57a846b35361ae610dc143d
Monte Lai <monte.lai@consensys.net>
2025-06-13 18:56:25 +0800

    fix: use assertions

--- e2e/specs/accounts/change-password.spec.ts
+++ e2e/specs/accounts/change-password.spec.ts
@@ -14,12 +14,13 @@ import { loginToApp } from '../../viewHelper.js';
 import TabBarComponent from '../../pages/wallet/TabBarComponent.js';
 import SettingsView from '../../pages/Settings/SettingsView.js';
 import SecurityAndPrivacy from '../../pages/Settings/SecurityAndPrivacy/SecurityAndPrivacyView.js';
-import Assertions from '../../utils/Assertions.js';
 import WalletView from '../../pages/wallet/WalletView.js';
 import ChangePasswordView from '../../pages/Settings/SecurityAndPrivacy/ChangePasswordView.js';
 import LoginView from '../../pages/wallet/LoginView.js';
 import Matchers from '../../utils/Matchers.js';
 import AccountListBottomSheet from '../../pages/wallet/AccountListBottomSheet.js';
+import Gestures from '../../utils/Gestures.js';
+import Assertions from '../../utils/Assertions.js';
 
 const fixtureServer = new FixtureServer();
 // These keys are from the fixture and are used to test the reveal private key functionality
@@ -57,17 +58,17 @@ describe(Regression('change password'), () => {
     await ChangePasswordView.typeInConfirmPasswordInputBox(NEWPASSWORD);
     await ChangePasswordView.reEnterPassword(NEWPASSWORD);
     await ChangePasswordView.tapIUnderstandCheckBox();
-    await ChangePasswordView.tapSubmitButton();
+    await Gestures.waitAndTap(ChangePasswordView.submitButton);
 
     await TabBarComponent.tapWallet();
     await WalletView.tapIdenticon();
 
     // Check if all the accounts are displayed
-    await Matchers.getElementByText('Account 1');
-    await Matchers.getElementByText('Account 2');
-    await Matchers.getElementByText('Account 3');
-    await Matchers.getElementByText('Account 4');
-    await Matchers.getElementByText('QR 1');
+    await Assertions.checkIfVisible(Matchers.getElementByText('Account 1'));
+    await Assertions.checkIfVisible(Matchers.getElementByText('Account 2'));
+    await Assertions.checkIfVisible(Matchers.getElementByText('Account 3'));
+    await Assertions.checkIfVisible(Matchers.getElementByText('Account 4'));
+    await Assertions.checkIfVisible(Matchers.getElementByText('QR 1'));
 
     await AccountListBottomSheet.swipeToDismissAccountsModal();
 
@@ -77,11 +78,10 @@ describe(Regression('change password'), () => {
     await SettingsView.tapYesAlertButton();
     await LoginView.enterPassword(NEWPASSWORD);
 
-    await TabBarComponent.tapWallet();
-    await Matchers.getElementByText('Account 1');
-    await Matchers.getElementByText('Account 2');
-    await Matchers.getElementByText('Account 3');
-    await Matchers.getElementByText('Account 4');
-    await Matchers.getElementByText('QR 1');
+    await WalletView.tapIdenticon();
+    await Assertions.checkIfVisible(Matchers.getElementByText('Account 1'));
+    await Assertions.checkIfVisible(Matchers.getElementByText('Account 2'));
+    await Assertions.checkIfVisible(Matchers.getElementByText('Account 3'));
+    await Assertions.checkIfVisible(Matchers.getElementByText('QR 1'));
   });
 });
