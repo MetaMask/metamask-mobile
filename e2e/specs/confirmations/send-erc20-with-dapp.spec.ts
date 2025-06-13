64c655d1935802df51e36415f36fc318b4a9b1ca
Vivek <vivek.srilal@consensys.net>
2025-06-13 08:40:06 +0400

    revert

--- e2e/specs/confirmations/send-erc20-with-dapp.spec.ts
+++ e2e/specs/confirmations/send-erc20-with-dapp.spec.ts
@@ -31,106 +31,106 @@ describe(SmokeConfirmations('ERC20 tokens'), () => {
     await TestHelpers.reverseServerPort();
   });
 
-//   it('send an ERC20 token from a dapp', async () => {
-//     const testSpecificMock  = {
-//       GET: [
-//         mockEvents.GET.suggestedGasFeesApiGanache,
-//         mockEvents.GET.remoteFeatureFlagsOldConfirmations
-//       ],
-//     };
+  it('send an ERC20 token from a dapp', async () => {
+    const testSpecificMock  = {
+      GET: [
+        mockEvents.GET.suggestedGasFeesApiGanache,
+        mockEvents.GET.remoteFeatureFlagsOldConfirmations
+      ],
+    };
 
-//     await withFixtures(
-//       {
-//         dapp: true,
-//         fixture: new FixtureBuilder()
-//           .withGanacheNetwork()
-//           .withPermissionControllerConnectedToTestDapp(buildPermissions(['0x539']))
-//           .build(),
-//         restartDevice: true,
-//         ganacheOptions: defaultGanacheOptions,
-//         smartContract: HST_CONTRACT,
-//         testSpecificMock,
-//       },
-//       // Remove any once withFixtures is typed
-//       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//       async ({ contractRegistry }: { contractRegistry: any }) => {
-//         const hstAddress = await contractRegistry.getContractAddress(
-//           HST_CONTRACT,
-//         );
-//         await loginToApp();
+    await withFixtures(
+      {
+        dapp: true,
+        fixture: new FixtureBuilder()
+          .withGanacheNetwork()
+          .withPermissionControllerConnectedToTestDapp(buildPermissions(['0x539']))
+          .build(),
+        restartDevice: true,
+        ganacheOptions: defaultGanacheOptions,
+        smartContract: HST_CONTRACT,
+        testSpecificMock,
+      },
+      // Remove any once withFixtures is typed
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      async ({ contractRegistry }: { contractRegistry: any }) => {
+        const hstAddress = await contractRegistry.getContractAddress(
+          HST_CONTRACT,
+        );
+        await loginToApp();
 
-//         // Navigate to the browser screen
-//         await TabBarComponent.tapBrowser();
-//         await TestDApp.navigateToTestDappWithContract({
-//           contractAddress: hstAddress,
-//         });
-//         await TestHelpers.delay(3000);
+        // Navigate to the browser screen
+        await TabBarComponent.tapBrowser();
+        await TestDApp.navigateToTestDappWithContract({
+          contractAddress: hstAddress,
+        });
+        await TestHelpers.delay(3000);
 
-//         // Transfer ERC20 tokens
-//         await TestDApp.tapERC20TransferButton();
-//         await TestHelpers.delay(3000);
+        // Transfer ERC20 tokens
+        await TestDApp.tapERC20TransferButton();
+        await TestHelpers.delay(3000);
 
-//         // Tap confirm button
-//         await TestDApp.tapConfirmButton();
+        // Tap confirm button
+        await TestDApp.tapConfirmButton();
 
-//         // Navigate to the activity screen
-//         await TabBarComponent.tapActivity();
+        // Navigate to the activity screen
+        await TabBarComponent.tapActivity();
 
-//         // Assert "Sent Tokens" transaction is displayed
-//         await Assertions.checkIfTextIsDisplayed(
-//           ActivitiesViewSelectorsText.SENT_TOKENS_MESSAGE_TEXT(
-//             // contractConfiguration is not typed
-//             // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//             (contractConfiguration[HST_CONTRACT] as any).tokenName as string,
-//           ),
-//         );
-//       },
-//     );
-//   });
+        // Assert "Sent Tokens" transaction is displayed
+        await Assertions.checkIfTextIsDisplayed(
+          ActivitiesViewSelectorsText.SENT_TOKENS_MESSAGE_TEXT(
+            // contractConfiguration is not typed
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            (contractConfiguration[HST_CONTRACT] as any).tokenName as string,
+          ),
+        );
+      },
+    );
+  });
 
-// it(`send an ERC20 token from a dapp using ${MONAD_TESTNET.nickname}`, async () => {
-//     await withFixtures(
-//       {
-//         dapp: true,
-//         fixture: new FixtureBuilder()
-//           .withMonadTestnetNetwork()
-//           .withPermissionControllerConnectedToTestDapp(
-//             buildPermissions([`${MONAD_TESTNET.chainId}`]),
-//           )
-//           .build(),
-//         restartDevice: true,
-//         smartContract: HST_CONTRACT,
-//       },
-//       // Remove any once withFixtures is typed
-//       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//       async ({ contractRegistry }: { contractRegistry: any }) => {
-//         const hstAddress = await contractRegistry.getContractAddress(
-//           HST_CONTRACT,
-//         );
+it(`send an ERC20 token from a dapp using ${MONAD_TESTNET.nickname}`, async () => {
+    await withFixtures(
+      {
+        dapp: true,
+        fixture: new FixtureBuilder()
+          .withMonadTestnetNetwork()
+          .withPermissionControllerConnectedToTestDapp(
+            buildPermissions([`${MONAD_TESTNET.chainId}`]),
+          )
+          .build(),
+        restartDevice: true,
+        smartContract: HST_CONTRACT,
+      },
+      // Remove any once withFixtures is typed
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      async ({ contractRegistry }: { contractRegistry: any }) => {
+        const hstAddress = await contractRegistry.getContractAddress(
+          HST_CONTRACT,
+        );
 
-//         await loginToApp();
+        await loginToApp();
 
-//         // Navigate to the browser screen
-//         await TabBarComponent.tapBrowser();
-//         await TestDApp.navigateToTestDappWithContract({
-//           contractAddress: hstAddress,
-//         });
-//         await TestHelpers.delay(3000);
+        // Navigate to the browser screen
+        await TabBarComponent.tapBrowser();
+        await TestDApp.navigateToTestDappWithContract({
+          contractAddress: hstAddress,
+        });
+        await TestHelpers.delay(3000);
 
-//         // Transfer ERC20 tokens
-//         await TestDApp.tapERC20TransferButton();
-//         await TestHelpers.delay(3000);
+        // Transfer ERC20 tokens
+        await TestDApp.tapERC20TransferButton();
+        await TestHelpers.delay(3000);
 
-//         // Accept confirmation
-//         await FooterActions.tapConfirmButton();
-//         await TestHelpers.delay(3000);
+        // Accept confirmation
+        await FooterActions.tapConfirmButton();
+        await TestHelpers.delay(3000);
 
-//         // Check activity tab
-//         await TabBarComponent.tapActivity();
-//         await Assertions.checkIfTextIsDisplayed('Confirmed');
-//       },
-//     );
-//   });
+        // Check activity tab
+        await TabBarComponent.tapActivity();
+        await Assertions.checkIfTextIsDisplayed('Confirmed');
+      },
+    );
+  });
 
   it(`send an ERC20 token from a dapp using ${MEGAETH_TESTNET.nickname}`, async () => {
     await withFixtures(
