fafd14ca6ed0fa1af61b32623b633e82075030c2
ieow <cwlee0@gmail.com>
2025-06-14 11:03:44 +0800

    fix: remove wrong test

--- app/components/Views/ImportFromSecretRecoveryPhrase/index.test.tsx
+++ app/components/Views/ImportFromSecretRecoveryPhrase/index.test.tsx
@@ -781,28 +781,5 @@ describe('ImportFromSecretRecoveryPhrase', () => {
         expect(getByText('Unlock with Face ID?')).toBeOnTheScreen();
       });
     });
-
-    it('Import seed phrase with optin metrics flow', async () => {
-      mockIsEnabled.mockReturnValue(false);
-      const { getByTestId, passwordInput, confirmPasswordInput } =
-        renderStep2WithInputs();
-
-      // Enter valid passwords
-      fireEvent.changeText(passwordInput, 'StrongPass123!');
-      fireEvent.changeText(confirmPasswordInput, 'StrongPass123!');
-
-      // Check learn more checkbox
-      const learnMoreCheckbox = getByTestId(
-        ImportFromSeedSelectorsIDs.CHECKBOX_TEXT_ID,
-      );
-      fireEvent.press(learnMoreCheckbox);
-      // Mock Authentication.newWalletAndRestore
-      jest.spyOn(Authentication, 'newWalletAndRestore').mockResolvedValueOnce();
-      // Try to import
-      const confirmButton = getByTestId(
-        ChoosePasswordSelectorsIDs.SUBMIT_BUTTON_ID,
-      );
-      fireEvent.press(confirmButton);
-    });
   });
 });
