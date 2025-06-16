87fd5e205ca56df1cf4b5638082a09ea9ff63b73
hieu-w <hieu@tor.us>
2025-06-16 13:59:42 +0700

    test: fix onboarding optin tests

--- e2e/specs/onboarding/onboarding-wizard-opt-in.spec.js
+++ e2e/specs/onboarding/onboarding-wizard-opt-in.spec.js
@@ -33,12 +33,10 @@ describe(
       await OnboardingCarouselView.tapOnGetStartedButton();
       await acceptTermOfUse();
       await OnboardingView.tapCreateWallet();
-      await Assertions.checkIfVisible(MetaMetricsOptIn.container);
-      await MetaMetricsOptIn.tapAgreeButton();
-      await Assertions.checkIfVisible(CreatePasswordView.container);
     });
 
     it('should be able to create a new wallet', async () => {
+      await Assertions.checkIfVisible(CreatePasswordView.container);
       await CreatePasswordView.enterPassword(PASSWORD);
       await CreatePasswordView.reEnterPassword(PASSWORD);
       await CreatePasswordView.tapIUnderstandCheckBox();
@@ -51,7 +49,6 @@ describe(
       await ProtectYourWalletView.tapOnRemindMeLaterButton();
       await SkipAccountSecurityModal.tapIUnderstandCheckBox();
       await SkipAccountSecurityModal.tapSkipButton();
-      await OnboardingSuccessView.tapDone();
     });
 
     it('should be able to opt-in of the onboarding-wizard', async () => {
