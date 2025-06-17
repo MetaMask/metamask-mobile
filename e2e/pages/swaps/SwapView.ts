841a54cd8b934966adc06109ddbd4466c2963fe6
Davide Brocchetto <davide.brocchetto@consensys.net>
2025-06-17 15:39:21 -0700

    test: Convert Swap and Stake e2e tests to typescript (#16269)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    This PR convert Swap and Stake e2e tests to typescript and fixes the
    Bridge e2e tests that were disabled because failing due to a UI change
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- e2e/pages/swaps/SwapView.ts
+++ e2e/pages/swaps/SwapView.ts
@@ -5,7 +5,6 @@ import {
 
 import Matchers from '../../utils/Matchers';
 import Gestures from '../../utils/Gestures';
-import TestHelpers from '../..//helpers';
 import { waitFor } from 'detox';
 
 class SwapView {
@@ -17,17 +16,17 @@ class SwapView {
     return Matchers.getElementByID(SwapsViewSelectorsIDs.GAS_FEE);
   }
 
-  get fetchingQuotes() {
+  get fetchingQuotes(): DetoxElement {
     return Matchers.getElementByText(SwapViewSelectorsTexts.FETCHING_QUOTES);
   }
 
-  get swapButton() {
+  get swapButton(): DetoxElement {
     return device.getPlatform() === 'ios'
       ? Matchers.getElementByID(SwapsViewSelectorsIDs.SWAP_BUTTON)
       : Matchers.getElementByLabel(SwapsViewSelectorsIDs.SWAP_BUTTON);
   }
 
-  get iUnderstandLabel() {
+  get iUnderstandLabel(): DetoxElement {
     return Matchers.getElementByText(SwapViewSelectorsTexts.I_UNDERSTAND);
   }
 
@@ -37,25 +36,19 @@ class SwapView {
 
   async isPriceWarningDisplayed() {
     try {
-      const element = await this.iUnderstandLabel;
-      await waitFor(element).toBeVisible().withTimeout(5000);
+      const label = await this.iUnderstandLabel as Detox.NativeElement;
+      await waitFor(label).toBeVisible().withTimeout(5000);
       return true;
     } catch (e) {
       return false;
     }
   }
 
-  generateSwapCompleteLabel(sourceToken, destinationToken) {
+  generateSwapCompleteLabel(sourceToken: string, destinationToken: string): string {
     let title = SwapViewSelectorsTexts.SWAP_CONFIRMED;
     title = title.replace('{{sourceToken}}', sourceToken);
     title = title.replace('{{destinationToken}}', destinationToken);
     return title;
-  }
-
-  // Function to check if the button is enabled
-  async isButtonEnabled(element) {
-    const attributes = await element.getAttributes();
-    return attributes.enabled === true; // Check if enabled is true
   }
 
   async tapSwapButton() {
