10edd56b319bb8588492bb6a46dde5dae15778ce
OGPoyraz <omergoktugpoyraz@gmail.com>
2025-06-17 08:55:02 +0200

    Add unit test for approve transactions in useConfirmationLocation tests

--- app/components/Views/confirmations/hooks/metrics/useConfirmationLocation.test.ts
+++ app/components/Views/confirmations/hooks/metrics/useConfirmationLocation.test.ts
@@ -152,6 +152,25 @@ describe('useConfirmationLocation', () => {
     expect(result.current).toBe(CONFIRMATION_EVENT_LOCATIONS.TRANSFER);
   });
 
+  it.each([
+    [TransactionType.tokenMethodApprove],
+    [TransactionType.tokenMethodSetApprovalForAll],
+  ])('returns APPROVE location for %s transactions', (transactionType) => {
+    mockUseApprovalRequest.mockReturnValue(
+      createApprovalRequestMock({
+        type: ApprovalType.Transaction,
+        requestData: {},
+      }),
+    );
+
+    mockUseTransactionMetadataRequest.mockReturnValue({
+      type: transactionType,
+    } as unknown as TransactionMeta);
+
+    const { result } = renderHook(() => useConfirmationLocation());
+    expect(result.current).toBe(CONFIRMATION_EVENT_LOCATIONS.APPROVE);
+  });
+
   it('returns undefined for transaction approvals with unknown transaction type', () => {
     mockUseApprovalRequest.mockReturnValue(
       createApprovalRequestMock({
