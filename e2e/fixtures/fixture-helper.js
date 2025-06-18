d7fe8dd0737b359e887eaeffe15f6507a01fdd70
Baptiste Marchand <75846779+baptiste-marchand@users.noreply.github.com>
2025-06-18 10:24:16 +0200

    style: fix rebase

--- e2e/fixtures/fixture-helper.js
+++ e2e/fixtures/fixture-helper.js
@@ -200,8 +200,8 @@ export const stopFixtureServer = async (fixtureServer) => {
  * @param {Object} options - An object containing configuration options.
  * @param {Object} options.fixture - The fixture to load.
  * @param {boolean} [options.dapp] - The dapp to load.
- * @param {string} [options.dappPath] - The dapp path to use.
  * @param {boolean} [options.multichainDapp=false]
+ * @param {string} [options.dappPath] - The dapp path to use.
  * @param {Object} [options.ganacheOptions] - The test specific mock to load for test.
  * @param {import('detox/detox').LanguageAndLocale} [options.languageAndLocale] - The language and locale to use for the app.
  * @param {Object} [options.launchArgs] - Additional launch arguments for the app.
