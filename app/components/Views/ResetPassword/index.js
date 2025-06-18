ef6a28fef3fa3b249b106b0e958af0bd63e5aa54
huggingbot <83656073+huggingbot@users.noreply.github.com>
2025-06-18 19:16:57 +0800

    fix: include password strength in password changed event tracking

--- app/components/Views/ResetPassword/index.js
+++ app/components/Views/ResetPassword/index.js
@@ -454,12 +454,15 @@ class ResetPassword extends PureComponent {
       this.props.passwordSet();
 
       // Track password changed event
-      const { biometryChoice } = this.state;
-      const eventBuilder = MetricsEventBuilder.createEventBuilder(MetaMetricsEvents.PASSWORD_CHANGED)
-        .addProperties({
-          biometry_type: this.state.biometryType,
-          biometrics_enabled: Boolean(biometryChoice),
-        });
+      const { biometryChoice, passwordStrength } = this.state;
+      const passwordStrengthWord = getPasswordStrengthWord(passwordStrength);
+      const eventBuilder = MetricsEventBuilder.createEventBuilder(
+        MetaMetricsEvents.PASSWORD_CHANGED,
+      ).addProperties({
+        biometry_type: this.state.biometryType,
+        biometrics_enabled: Boolean(biometryChoice),
+        password_strength: passwordStrengthWord,
+      });
       MetaMetrics.getInstance().trackEvent(eventBuilder.build());
 
       this.setState({ loading: false });
