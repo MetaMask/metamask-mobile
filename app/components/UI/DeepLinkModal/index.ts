65882d6573ad16cdab907a0640b9b9b91d04cc16
Cal-L <cleun007@gmail.com>
2025-06-18 20:32:25 -0700

    Export type

--- app/components/UI/DeepLinkModal/index.ts
+++ app/components/UI/DeepLinkModal/index.ts
@@ -1,4 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export { default as DeepLinkModal } from './DeepLinkModal';
-export { DeepLinkModalLinkType, DeepLinkModalParams } from './types';
+export { DeepLinkModalLinkType, type DeepLinkModalParams } from './types';
 export { createDeepLinkModalNavDetails } from './constant';
