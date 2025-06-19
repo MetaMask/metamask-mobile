c9372f35cff2f38841c022d89d1eed5005f98223
Cal-L <cleun007@gmail.com>
2025-06-18 20:22:06 -0700

    Abstract types

---
+++ app/components/UI/DeepLinkModal/types.ts
@@ -0,0 +1,47 @@
+import { TextStyle, ImageStyle, ViewStyle } from 'react-native';
+
+/**
+ * Deeplink Modal Link Type
+ */
+export enum DeepLinkModalLinkType {
+  PUBLIC = 'public',
+  PRIVATE = 'private',
+  INVALID = 'invalid',
+}
+
+type CommonLinkParams = {
+  linkType: DeepLinkModalLinkType;
+  onBack: () => void;
+};
+
+type PublicLinkParams = CommonLinkParams & {
+  linkType: DeepLinkModalLinkType.PUBLIC;
+  onContinue: () => void;
+  pageTitle: string;
+};
+
+type PrivateLinkParams = CommonLinkParams & {
+  linkType: DeepLinkModalLinkType.PRIVATE;
+  onContinue: () => void;
+  pageTitle: string;
+};
+
+type InvalidLinkParams = CommonLinkParams & {
+  linkType: DeepLinkModalLinkType.INVALID;
+};
+
+/**
+ * Deeplink Modal Params
+ */
+export type DeepLinkModalParams =
+  | PublicLinkParams
+  | PrivateLinkParams
+  | InvalidLinkParams;
+
+/**
+ * Modal Image Props
+ */
+export type ModalImageProps = {
+  linkType: DeepLinkModalLinkType;
+  styles: Record<string, ImageStyle & ViewStyle & TextStyle>;
+};
