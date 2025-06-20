6103316996f19230ed60f437f28007fd37c9ab30
IF <139582705+infiniteflower@users.noreply.github.com>
2025-06-20 16:54:16 -0400

    fix: broken tests

--- app/components/Views/WalletActions/WalletActions.test.tsx
+++ app/components/Views/WalletActions/WalletActions.test.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import {
   EthAccountType,
   SolAccountType,
   SolScope,
 } from '@metamask/keyring-api';
 import { fireEvent } from '@testing-library/react-native';
-import React from 'react';
+import '../../UI/Bridge/_mocks_/initialState';
 import { isSwapsAllowed } from '../../../components/UI/Swaps/utils';
 import {
   selectCanSignTransactions,
