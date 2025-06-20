0b1bc356fbcff74813cb6bf286f1e46a031d87aa
Aslau Mario-Daniel <marioaslau@gmail.com>
2025-06-21 02:20:51 +0300

    feat: sync AppDelegate with main

--- ios/MetaMask/AppDelegate.m
+++ ios/MetaMask/AppDelegate.m
@@ -29,6 +29,7 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{@"foxCode": foxCode};
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
