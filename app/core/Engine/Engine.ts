1b78b8281fda38ef2e59ad2875e7c3224d70781e
IF <139582705+infiniteflower@users.noreply.github.com>
2025-06-20 12:35:04 -0400

    chore: changes for BridgeStatus v30

--- app/core/Engine/Engine.ts
+++ app/core/Engine/Engine.ts
@@ -1195,6 +1195,7 @@ export class Engine {
           'AccountsController:getAccountByAddress',
           'SnapController:handleRequest',
           'TransactionController:getState',
+          'RemoteFeatureFlagController:getState',
         ],
         allowedEvents: [
           'TransactionController:transactionConfirmed',
