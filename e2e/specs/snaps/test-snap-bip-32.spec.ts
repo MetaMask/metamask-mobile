5c12a2079c2e791504d7c68436eb5e9284ba308f
Frederik Bolding <frederik.bolding@gmail.com>
2025-06-23 15:06:51 +0200

    Run Prettier

--- e2e/specs/snaps/test-snap-bip-32.spec.ts
+++ e2e/specs/snaps/test-snap-bip-32.spec.ts
@@ -17,10 +17,11 @@ import TestSnaps from '../../pages/Browser/TestSnaps';
 const fixtureServer = new FixtureServer();
 
 describe(FlaskBuildTests('BIP-32 Snap Tests'), () => {
-
   beforeAll(async () => {
     await TestHelpers.reverseServerPort();
-    const fixture = new FixtureBuilder().withImportedHdKeyringAndTwoDefaultAccountsOneImportedHdAccountKeyringController().build();
+    const fixture = new FixtureBuilder()
+      .withImportedHdKeyringAndTwoDefaultAccountsOneImportedHdAccountKeyringController()
+      .build();
     await startFixtureServer(fixtureServer);
     await loadFixture(fixtureServer, { fixture });
     await TestHelpers.launchApp({
@@ -43,14 +44,17 @@ describe(FlaskBuildTests('BIP-32 Snap Tests'), () => {
     jest.setTimeout(150000);
   });
 
-  it('can connect to BIP-32 snap', async ()=> {
+  it('can connect to BIP-32 snap', async () => {
     await TestSnaps.installSnap('connectBip32Button');
   });
 
   it('can get BIP-32 public key', async () => {
     await TestSnaps.tapButton('getPublicKeyBip32Button');
     await TestHelpers.delay(3000);
-    await TestSnaps.checkResultSpan('bip32PublicKeyResultSpan', '"0x046d718eee8a4e85aac6b6a02c802a60e4d13981ab5cf7d620466c9d23fe24d9cfb2548ed7bdb658ef24d119b70cf49e05a111e8054e53a8d0548e10e684afc40b"');
+    await TestSnaps.checkResultSpan(
+      'bip32PublicKeyResultSpan',
+      '"0x046d718eee8a4e85aac6b6a02c802a60e4d13981ab5cf7d620466c9d23fe24d9cfb2548ed7bdb658ef24d119b70cf49e05a111e8054e53a8d0548e10e684afc40b"',
+    );
   });
 
   it('can sign BIP-32 message using secp256k1', async () => {
@@ -59,7 +63,10 @@ describe(FlaskBuildTests('BIP-32 Snap Tests'), () => {
     await Assertions.checkIfTextIsDisplayed('Signature request');
     await TestSnaps.approveSignRequest();
     await TestSnaps.swipeUpSmall();
-    await TestSnaps.checkResultSpan('bip32MessageResultSecp256k1Span', '"0x304402204b206e3fa1620727c9699ac30869b9c53b3084375531a20196328776b7a315ac02207320be06767c377a1a44ec3729e91455ec700c7c486697c010e7c672a4c3fc6e"');
+    await TestSnaps.checkResultSpan(
+      'bip32MessageResultSecp256k1Span',
+      '"0x304402204b206e3fa1620727c9699ac30869b9c53b3084375531a20196328776b7a315ac02207320be06767c377a1a44ec3729e91455ec700c7c486697c010e7c672a4c3fc6e"',
+    );
   });
 
   it('can sign BIP-32 message using ed25519', async () => {
@@ -68,7 +75,10 @@ describe(FlaskBuildTests('BIP-32 Snap Tests'), () => {
     await Assertions.checkIfTextIsDisplayed('Signature request');
     await TestSnaps.approveSignRequest();
     await TestSnaps.swipeUpSmall();
-    await TestSnaps.checkResultSpan('bip32MessageResultEd25519Span', '"0x52b797a80452402e93d1a53a972a5d22ae77c88c0883233839849f142ab6cef69e91500c4109ff40216cc4327d1914ab05facce372817db532463a38f225e408"');
+    await TestSnaps.checkResultSpan(
+      'bip32MessageResultEd25519Span',
+      '"0x52b797a80452402e93d1a53a972a5d22ae77c88c0883233839849f142ab6cef69e91500c4109ff40216cc4327d1914ab05facce372817db532463a38f225e408"',
+    );
   });
 
   it.skip('can sign BIP-32 message using ed25519Bip32', async () => {
@@ -81,29 +91,43 @@ describe(FlaskBuildTests('BIP-32 Snap Tests'), () => {
   });
 
   it('can sign BIP-32 message using secp256k1 and SRP 1', async () => {
-    await TestSnaps.selectEntropySource('bip32EntropyDropDown', '01JX9NJ15HPNS6RRRYBCKDK33R')
+    await TestSnaps.selectEntropySource(
+      'bip32EntropyDropDown',
+      '01JX9NJ15HPNS6RRRYBCKDK33R',
+    );
     await TestSnaps.fillMessage('messageSecp256k1Input', 'foo');
     await TestSnaps.tapButton('signMessageBip32Secp256k1Button');
     await Assertions.checkIfTextIsDisplayed('Signature request');
     await TestSnaps.approveSignRequest();
     await TestSnaps.swipeUpSmall();
-    await TestSnaps.checkResultSpan('bip32MessageResultSecp256k1Span', '"0x304402204b206e3fa1620727c9699ac30869b9c53b3084375531a20196328776b7a315ac02207320be06767c377a1a44ec3729e91455ec700c7c486697c010e7c672a4c3fc6e"');
+    await TestSnaps.checkResultSpan(
+      'bip32MessageResultSecp256k1Span',
+      '"0x304402204b206e3fa1620727c9699ac30869b9c53b3084375531a20196328776b7a315ac02207320be06767c377a1a44ec3729e91455ec700c7c486697c010e7c672a4c3fc6e"',
+    );
   });
 
   it('can sign BIP-32 message using secp256k1 and SRP 2', async () => {
-    await TestSnaps.selectEntropySource('bip32EntropyDropDown', '01JX9NZWRAVQKES02TWSN8GD91')
+    await TestSnaps.selectEntropySource(
+      'bip32EntropyDropDown',
+      '01JX9NZWRAVQKES02TWSN8GD91',
+    );
     await TestSnaps.fillMessage('messageSecp256k1Input', 'foo');
     await TestSnaps.tapButton('signMessageBip32Secp256k1Button');
     await Assertions.checkIfTextIsDisplayed('Signature request');
     await TestSnaps.approveSignRequest();
     await TestSnaps.swipeUpSmall();
-    await TestSnaps.checkResultSpan('bip32MessageResultSecp256k1Span', '"0x30430220458e02536d6f84029ac13401ac59b9d241255d2e0f58f1c000da7e5d77a76e25021f1a14788d550e9d6b5265a70b17473d18a96aa5c3866efdac87c8bbc6f1db6f"');
+    await TestSnaps.checkResultSpan(
+      'bip32MessageResultSecp256k1Span',
+      '"0x30430220458e02536d6f84029ac13401ac59b9d241255d2e0f58f1c000da7e5d77a76e25021f1a14788d550e9d6b5265a70b17473d18a96aa5c3866efdac87c8bbc6f1db6f"',
+    );
   });
 
   it('fails when choosing the invalid entropy source', async () => {
-    await TestSnaps.selectEntropySource('bip32EntropyDropDown', 'Invalid')
+    await TestSnaps.selectEntropySource('bip32EntropyDropDown', 'Invalid');
     await TestSnaps.fillMessage('messageSecp256k1Input', 'foo');
     await TestSnaps.tapButton('signMessageBip32Secp256k1Button');
-    await Assertions.checkIfTextIsDisplayed('Entropy source with ID "invalid" not found.');
+    await Assertions.checkIfTextIsDisplayed(
+      'Entropy source with ID "invalid" not found.',
+    );
   });
 });
--- e2e/specs/snaps/test-snap-bip-44.spec.ts
+++ e2e/specs/snaps/test-snap-bip-44.spec.ts
@@ -17,16 +17,19 @@ import TestSnaps from '../../pages/Browser/TestSnaps';
 const fixtureServer = new FixtureServer();
 
 describe(FlaskBuildTests('BIP-44 Snap Tests'), () => {
-
-  const EXPECTED_PUBLIC_KEY = '"0x90043dd7faa56b3ed9fe959d25ae17132dc7e0f604663e68a84cf21e0f64390fbdd4781e4baa99c236f3bbf95182bdee"';
-  const EXPECTED_CUSTOM_SIGNATURE = '"0xac396aaaf817d880ceab9b24995818436dee605524ae68061661fd69e340068fd55a24f579017c3591ed60d4738f25720ac3f23964c0209119e8afb2e20088a652b581a32b96002b7a282bf200ef1a463fb0ff56e2c3937f140a5300a5eacbc1"';
+  const EXPECTED_PUBLIC_KEY =
+    '"0x90043dd7faa56b3ed9fe959d25ae17132dc7e0f604663e68a84cf21e0f64390fbdd4781e4baa99c236f3bbf95182bdee"';
+  const EXPECTED_CUSTOM_SIGNATURE =
+    '"0xac396aaaf817d880ceab9b24995818436dee605524ae68061661fd69e340068fd55a24f579017c3591ed60d4738f25720ac3f23964c0209119e8afb2e20088a652b581a32b96002b7a282bf200ef1a463fb0ff56e2c3937f140a5300a5eacbc1"';
   const customMessage = 'This is a custom test message.';
-  const EXPECTED_FOO_BAR_SIGNATURE = '"0x988251404ed60124cc6a4c17bfda9659d59273b675d1cd942478960b0747f1c182076df7bc695ac1459416d0ab7e789f078a3c6f3e438d568f9c05260d95b716c7ba1942661f22f966f2691dc282b40523c073e88b6b43d9dd2983c85cbe6142"';
-
+  const EXPECTED_FOO_BAR_SIGNATURE =
+    '"0x988251404ed60124cc6a4c17bfda9659d59273b675d1cd942478960b0747f1c182076df7bc695ac1459416d0ab7e789f078a3c6f3e438d568f9c05260d95b716c7ba1942661f22f966f2691dc282b40523c073e88b6b43d9dd2983c85cbe6142"';
 
   beforeAll(async () => {
     await TestHelpers.reverseServerPort();
-    const fixture = new FixtureBuilder().withImportedHdKeyringAndTwoDefaultAccountsOneImportedHdAccountKeyringController().build();
+    const fixture = new FixtureBuilder()
+      .withImportedHdKeyringAndTwoDefaultAccountsOneImportedHdAccountKeyringController()
+      .build();
     await startFixtureServer(fixtureServer);
     await loadFixture(fixtureServer, { fixture });
     await TestHelpers.launchApp({
@@ -49,7 +52,7 @@ describe(FlaskBuildTests('BIP-44 Snap Tests'), () => {
     jest.setTimeout(150000);
   });
 
-  it('should connect to BIP-44 snap', async ()=> {
+  it('should connect to BIP-44 snap', async () => {
     await TestSnaps.installSnap('connectBip44Button');
   });
 
@@ -64,21 +67,32 @@ describe(FlaskBuildTests('BIP-44 Snap Tests'), () => {
     await TestSnaps.tapButton('signMessageBip44Button');
     await TestSnaps.approveSignRequest();
     await TestSnaps.swipeUpSmall();
-    await TestSnaps.checkResultSpan('bip44SignResultSpan', EXPECTED_CUSTOM_SIGNATURE);
+    await TestSnaps.checkResultSpan(
+      'bip44SignResultSpan',
+      EXPECTED_CUSTOM_SIGNATURE,
+    );
   });
 
   it('should select an valid entropy source', async () => {
-    await TestSnaps.selectEntropySource('bip44EntropyDropDown', '01JX9NJ15HPNS6RRRYBCKDK33R')
+    await TestSnaps.selectEntropySource(
+      'bip44EntropyDropDown',
+      '01JX9NJ15HPNS6RRRYBCKDK33R',
+    );
     await TestSnaps.fillMessage('messageBip44Input', 'foo bar');
     await TestSnaps.tapButton('signMessageBip44Button');
     await TestSnaps.approveSignRequest();
-    await TestSnaps.checkResultSpan('bip44SignResultSpan', EXPECTED_FOO_BAR_SIGNATURE);
+    await TestSnaps.checkResultSpan(
+      'bip44SignResultSpan',
+      EXPECTED_FOO_BAR_SIGNATURE,
+    );
   });
 
   it('should select an invalid entropy source', async () => {
-    await TestSnaps.selectEntropySource('bip44EntropyDropDown', 'invalid')
+    await TestSnaps.selectEntropySource('bip44EntropyDropDown', 'invalid');
     await TestSnaps.fillMessage('messageBip44Input', customMessage);
     await TestSnaps.tapButton('signMessageBip44Button');
-    await Assertions.checkIfTextIsDisplayed('Entropy source with ID "invalid" not found.');
+    await Assertions.checkIfTextIsDisplayed(
+      'Entropy source with ID "invalid" not found.',
+    );
   });
 });
