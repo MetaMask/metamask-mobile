755483a54dd9e2b7b1c0f7c94fef8281e245814a
Pedro Figueiredo <ganseki.figueiredo@gmail.com>
2025-06-23 17:15:34 +0100

    feat: Add support for gas fees component for batch transactions (#16367)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/MetaMask-planning/issues/5165
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    <img width="487" alt="Screenshot 2025-06-13 at 13 44 01"
    src="https://github.com/user-attachments/assets/8368a31c-b852-4af9-a5d5-fc19d71cc1ca"
    />
    
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/Views/confirmations/hooks/gas/useFeeCalculationsTransactionBatch.ts
+++ app/components/Views/confirmations/hooks/gas/useFeeCalculationsTransactionBatch.ts
@@ -1,5 +1,5 @@
 import { GasFeeEstimates } from '@metamask/gas-fee-controller';
-import { type TransactionMeta } from '@metamask/transaction-controller';
+import { GasFeeEstimateLevel, GasFeeEstimateType, type TransactionBatchMeta } from '@metamask/transaction-controller';
 import { Hex } from '@metamask/utils';
 import { useCallback, useMemo } from 'react';
 import { useSelector } from 'react-redux';
@@ -7,19 +7,19 @@ import { RootState } from '../../../../../reducers';
 import { selectConversionRateByChainId } from '../../../../../selectors/currencyRateController';
 import { selectNetworkConfigurationByChainId } from '../../../../../selectors/networkController';
 import { selectShowFiatInTestnets } from '../../../../../selectors/settings';
+import { hexToDecimal } from '../../../../../util/conversions';
 import { isTestNet } from '../../../../../util/networks';
 import useFiatFormatter from '../../../../UI/SimulationDetails/FiatDisplay/useFiatFormatter';
 import { calculateGasEstimate, getFeesFromHex } from '../../utils/gas';
-import { useSupportsEIP1559 } from '../transactions/useSupportsEIP1559';
-import { useEIP1559TxFees } from './useEIP1559TxFees';
+import { useTransactionBatchSupportsEIP1559 } from '../transactions/useTransactionBatchSupportsEIP1559';
 import { useGasFeeEstimates } from './useGasFeeEstimates';
 
 const HEX_ZERO = '0x0';
 
-export const useFeeCalculations = (transactionMeta: TransactionMeta) => {
-  const { chainId, gasLimitNoBuffer, layer1GasFee, networkClientId } =
-    transactionMeta;
-
+export const useFeeCalculationsTransactionBatch = (transactionBatchesMeta: TransactionBatchMeta) => {
+  const chainId = transactionBatchesMeta.chainId;
+  const gasLimit = transactionBatchesMeta.gas;
+  const networkClientId = transactionBatchesMeta.networkClientId;
   const { nativeCurrency } = useSelector((state: RootState) =>
     selectNetworkConfigurationByChainId(state, chainId as Hex),
   );
@@ -27,17 +27,30 @@ export const useFeeCalculations = (transactionMeta: TransactionMeta) => {
     selectConversionRateByChainId(state, chainId as Hex, true),
   );
   const showFiatOnTestnets = useSelector(selectShowFiatInTestnets);
-
-  const { supportsEIP1559 } = useSupportsEIP1559(transactionMeta);
+  const { supportsEIP1559 } = useTransactionBatchSupportsEIP1559(transactionBatchesMeta);
   const fiatFormatter = useFiatFormatter();
-  const { maxFeePerGas, maxPriorityFeePerGas } =
-    useEIP1559TxFees(transactionMeta);
+
+  let maxFeePerGas = '0';
+  let maxPriorityFeePerGas = '0';
+  if (transactionBatchesMeta?.gasFeeEstimates?.type === GasFeeEstimateType.FeeMarket) {
+    maxFeePerGas = String(hexToDecimal(transactionBatchesMeta?.gasFeeEstimates?.[GasFeeEstimateLevel.Medium]?.maxFeePerGas ?? HEX_ZERO));
+    maxPriorityFeePerGas = String(hexToDecimal(transactionBatchesMeta?.gasFeeEstimates?.[GasFeeEstimateLevel.Medium]?.maxPriorityFeePerGas ?? HEX_ZERO));
+  }
+
   const { gasFeeEstimates } = useGasFeeEstimates(networkClientId);
   const shouldHideFiat = isTestNet(chainId as Hex) && !showFiatOnTestnets;
 
   const estimatedBaseFee = (gasFeeEstimates as GasFeeEstimates)
     ?.estimatedBaseFee;
-  const txParamsGasPrice = transactionMeta.txParams?.gasPrice ?? HEX_ZERO;
+
+  let txParamsGasPrice = HEX_ZERO;
+  if (transactionBatchesMeta?.gasFeeEstimates?.type === GasFeeEstimateType.GasPrice) {
+    txParamsGasPrice = transactionBatchesMeta?.gasFeeEstimates?.gasPrice ?? HEX_ZERO;
+  } else if (transactionBatchesMeta?.gasFeeEstimates?.type === GasFeeEstimateType.Legacy) {
+    txParamsGasPrice = transactionBatchesMeta?.gasFeeEstimates?.[GasFeeEstimateLevel.Medium] ?? HEX_ZERO;
+  } else if (transactionBatchesMeta?.gasFeeEstimates?.type === GasFeeEstimateType.FeeMarket) {
+    txParamsGasPrice = transactionBatchesMeta?.gasFeeEstimates?.[GasFeeEstimateLevel.Medium]?.maxFeePerGas ?? HEX_ZERO;
+  }
 
   const getFeesFromHexCallback = useCallback(
     (hexFee: string) =>
@@ -72,10 +85,10 @@ export const useFeeCalculations = (transactionMeta: TransactionMeta) => {
         gas,
         shouldUseEIP1559FeeLogic,
         estimatedBaseFee,
-        layer1GasFee,
+        // No layer1GasFee for batch transactions
         getFeesFromHexFn: getFeesFromHexCallback,
       }),
-    [estimatedBaseFee, layer1GasFee, getFeesFromHexCallback],
+    [estimatedBaseFee, getFeesFromHexCallback],
   );
 
   // Estimated fee
@@ -84,7 +97,7 @@ export const useFeeCalculations = (transactionMeta: TransactionMeta) => {
       calculateGasEstimateCallback({
         feePerGas: maxFeePerGas,
         priorityFeePerGas: maxPriorityFeePerGas,
-        gas: gasLimitNoBuffer ?? HEX_ZERO,
+        gas: gasLimit ?? HEX_ZERO,
         shouldUseEIP1559FeeLogic: supportsEIP1559,
         gasPrice: txParamsGasPrice,
       }),
@@ -94,7 +107,7 @@ export const useFeeCalculations = (transactionMeta: TransactionMeta) => {
       maxPriorityFeePerGas,
       supportsEIP1559,
       calculateGasEstimateCallback,
-      gasLimitNoBuffer,
+      gasLimit,
     ],
   );
 
