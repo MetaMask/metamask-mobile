e0aec1e8329436c51f34372f2ac3cef66cb96ad0
jiexi <jiexiluan@gmail.com>
2025-06-24 09:36:31 -0700

    fix: Cleanup duplicate wallet permission handlers in BackgroundBridge. Fix incorrect wallet permission handlers in SnapBridge (#16002)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    This is a refactor/cleanup that fixes an issue with the wrong wallet
    permission handlers being used in `SnapBridge`. It also adds missing
    `eth_account` handler to `SnapBridge`. The cleanup also dedupes wallet
    permission handlers that existed in both `RPCMethodMiddleware` and
    `BackgroundBridge`, making `RPCMethodMiddleware` the main source of
    truth again for all Wallet API entrypoints.
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/MetaMask-planning/issues/5073
    
    ## **Manual testing steps**
    
    1. Visit the following pages in the mobile in-app browser and trigger
    the request
    2.
    https://docs.metamask.io/wallet/reference/json-rpc-methods/eth_requestaccounts/
    3.
    https://docs.metamask.io/wallet/reference/json-rpc-methods/wallet_requestpermissions/
    
    Each connect view should only have EVM accounts and EVM networks
    selected by default
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    eth_requestAccounts
    
    https://github.com/user-attachments/assets/99683224-9ebd-431c-8dfb-14e0ccebbda9
    
    wallet_requestPermissions
    
    https://github.com/user-attachments/assets/4411249f-4bd9-4fe7-8e8c-5aff862870d3
    
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/core/RPCMethods/handlers/index.js
+++
@@ -1,9 +0,0 @@
-import requestEthereumAccounts from '../eth-request-accounts';
-import ethAccounts from '../eth_accounts';
-import getProviderState from '../getProviderState';
-
-export const eip1193OnlyHandlers = [ethAccounts, requestEthereumAccounts];
-
-export const multichainLocalHandlers = [
-  getProviderState,
-];
