190206440553162267c79cc9996db4d0e00f6ecd
AxelGes <axelges9@gmail.com>
2025-06-25 03:01:14 -0300

    fix: coverage

--- app/components/UI/Ramp/Deposit/Views/DepositOrderDetails/DepositOrderDetails.test.tsx
+++ app/components/UI/Ramp/Deposit/Views/DepositOrderDetails/DepositOrderDetails.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { screen } from '@testing-library/react-native';
+import { processFiatOrder } from '../../../index';
+import { act, screen, waitFor } from '@testing-library/react-native';
 import DepositOrderDetails from './DepositOrderDetails';
 import renderWithProvider from '../../../../../../util/test/renderWithProvider';
 import { backgroundState } from '../../../../../../util/test/initial-root-state';
@@ -8,10 +9,17 @@ import {
   FIAT_ORDER_PROVIDERS,
 } from '../../../../../../constants/on-ramp';
 import { DepositOrder, DepositOrderType } from '@consensys/native-ramps-sdk';
-import { getOrderById } from '../../../../../../reducers/fiatOrders';
+import { getOrderById, FiatOrder } from '../../../../../../reducers/fiatOrders';
+import AppConstants from '../../../../../../core/AppConstants';
 
 const mockNavigate = jest.fn();
 const mockSetOptions = jest.fn();
+const mockDispatch = jest.fn();
+
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: () => mockDispatch,
+}));
 
 jest.mock('@react-navigation/native', () => {
   const actualNav = jest.requireActual('@react-navigation/native');
@@ -31,6 +39,24 @@ jest.mock('@react-navigation/native', () => {
 
 jest.mock('../../../../../../reducers/fiatOrders', () => ({
   getOrderById: jest.fn(),
+  updateFiatOrder: jest.fn().mockReturnValue({ type: 'FIAT_UPDATE_ORDER' }),
+}));
+
+function mockGetUpdatedOrder(order: FiatOrder) {
+  return {
+    ...order,
+    lastTimeFetched: (order.lastTimeFetched || 0) + 100,
+  };
+}
+
+jest.mock('../../../index', () => ({
+  processFiatOrder: jest.fn().mockImplementation((order, onSuccess) => {
+    const updatedOrder = mockGetUpdatedOrder(order);
+    if (onSuccess) {
+      onSuccess(updatedOrder);
+    }
+    return Promise.resolve();
+  }),
 }));
 
 describe('DepositOrderDetails Component', () => {
@@ -57,6 +83,11 @@ describe('DepositOrderDetails Component', () => {
   beforeEach(() => {
     jest.clearAllMocks();
     (getOrderById as jest.Mock).mockReturnValue(mockOrder);
+    (processFiatOrder as jest.Mock).mockResolvedValue(undefined);
+  });
+
+  afterEach(() => {
+    (processFiatOrder as jest.Mock).mockClear();
   });
 
   it('renders success state correctly', () => {
@@ -109,5 +140,95 @@ describe('DepositOrderDetails Component', () => {
       },
     });
     expect(screen.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders loading state correctly', () => {
+    const loadingOrder = { ...mockOrder, state: FIAT_ORDER_STATES.CREATED };
+    (getOrderById as jest.Mock).mockReturnValue(loadingOrder);
+
+    renderWithProvider(<DepositOrderDetails />, {
+      state: {
+        engine: {
+          backgroundState,
+        },
+      },
+    });
+    expect(screen.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders an error screen if a CREATED order cannot be polled on load', async () => {
+    const createdOrder = { ...mockOrder, state: FIAT_ORDER_STATES.CREATED };
+    (getOrderById as jest.Mock).mockReturnValue(createdOrder);
+    (processFiatOrder as jest.Mock).mockImplementationOnce(() => {
+      throw new Error('Test error message');
+    });
+
+    await waitFor(() => {
+      renderWithProvider(<DepositOrderDetails />, {
+        state: {
+          engine: {
+            backgroundState,
+          },
+        },
+      });
+    });
+
+    expect(screen.toJSON()).toMatchSnapshot();
+  });
+
+  it('calls handleOnRefresh successfully on mount for CREATED orders', async () => {
+    const createdOrder = { ...mockOrder, state: FIAT_ORDER_STATES.CREATED };
+    (getOrderById as jest.Mock).mockReturnValue(createdOrder);
+
+    await waitFor(() => {
+      renderWithProvider(<DepositOrderDetails />, {
+        state: {
+          engine: {
+            backgroundState,
+          },
+        },
+      });
+    });
+
+    await waitFor(() => {
+      expect(processFiatOrder).toHaveBeenCalledWith(
+        createdOrder,
+        expect.any(Function),
+        expect.any(Function),
+        { forced: true },
+      );
+    });
+  });
+
+  it('polls transacted orders using interval', async () => {
+    jest.useFakeTimers();
+    const createdOrder = { ...mockOrder, state: FIAT_ORDER_STATES.CREATED };
+    (getOrderById as jest.Mock).mockReturnValue(createdOrder);
+
+    const intervalCount = 3;
+
+    await waitFor(() => {
+      renderWithProvider(<DepositOrderDetails />, {
+        state: {
+          engine: {
+            backgroundState,
+          },
+        },
+      });
+    });
+
+    await waitFor(() => {
+      expect(processFiatOrder).toHaveBeenCalledTimes(1);
+    });
+
+    act(() => {
+      jest.advanceTimersByTime(
+        AppConstants.FIAT_ORDERS.POLLING_FREQUENCY * intervalCount,
+      );
+      jest.clearAllTimers();
+      jest.useFakeTimers({ legacyFakeTimers: true });
+    });
+
+    expect(processFiatOrder).toHaveBeenCalledTimes(1 + intervalCount);
   });
 });
