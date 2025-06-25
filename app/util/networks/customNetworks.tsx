7d001b884fd54e416f53348a377d8d18c915a335
Ivan Kyuchukov <vaka.stz@gmail.com>
2025-06-25 19:04:31 +0800

    chore: add chiliz chain logo

--- app/util/networks/customNetworks.tsx
+++ app/util/networks/customNetworks.tsx
@@ -281,6 +281,7 @@ export const NETWORK_CHAIN_ID: {
   readonly BERACHAIN: '0x138de';
   readonly EDU: '0xa3c3';
   readonly ABSTRACT: '0xab5';
+  readonly CHILIZ_MAINNET: '0x15b38';
 } & typeof CHAIN_IDS = {
   FLARE_MAINNET: '0xe',
   SONGBIRD_TESTNET: '0x13',
@@ -304,6 +305,7 @@ export const NETWORK_CHAIN_ID: {
   BERACHAIN: '0x138de',
   EDU: '0xa3c3',
   ABSTRACT: '0xab5',
+  CHILIZ_MAINNET: '0x15b38',
   ...CHAIN_IDS,
 };
 
@@ -332,4 +334,5 @@ export const CustomNetworkImgMapping: Record<Hex, string> = {
   [NETWORK_CHAIN_ID.BERACHAIN]: require('../../images/berachain.png'),
   [NETWORK_CHAIN_ID.EDU]: require('../../images/edu.png'),
   [NETWORK_CHAIN_ID.ABSTRACT]: require('../../images/abstract.png'),
+  [NETWORK_CHAIN_ID.CHILIZ_MAINNET]: require('../../images/chiliz.png'),
 };
