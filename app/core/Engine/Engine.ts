3c8da29796a0856c35c5e23ac7b280293bb3c02e
Frederik Bolding <frederik.bolding@gmail.com>
2025-06-26 17:06:46 +0200

    fix: cp-7.50.0 Prevent cronjob state from getting out of sync (#16711)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    Prevent the `CronjobController` state from getting out of sync by
    initializing the controller before the `SnapController` allowing it to
    subscribe to the controller events. Additionally introduce a `init`
    function that handles the side-effects previously present in the
    `CronjobController` constructor.
    
    Since this is a release blocker, this upstream change was patched in:
    https://github.com/MetaMask/snaps/commit/a9f0277aba928d9980f7ff827b1879535f2dda4b
    
    Fixes: https://github.com/MetaMask/metamask-mobile/issues/16700
    
    1. Downgrade Solana Snap to 1.30.3
    2. Create a fresh MetaMask instance
    3. Upgrade Solana Snap to 1.30.4 and reload the app
    4. Wait a couple minutes
    5. Notice that there are no errors in the console

--- app/core/Engine/Engine.ts
+++ app/core/Engine/Engine.ts
@@ -1247,8 +1247,8 @@ export class Engine {
         DeFiPositionsController: defiPositionsControllerInit,
         ///: BEGIN:ONLY_INCLUDE_IF(preinstalled-snaps,external-snaps)
         ExecutionService: executionServiceInit,
-        SnapController: snapControllerInit,
         CronjobController: cronjobControllerInit,
+        SnapController: snapControllerInit,
         SnapInterfaceController: snapInterfaceControllerInit,
         SnapsRegistry: snapsRegistryInit,
         NotificationServicesController: notificationServicesControllerInit,
@@ -1327,6 +1327,7 @@ export class Engine {
     ///: END:ONLY_INCLUDE_IF
 
     ///: BEGIN:ONLY_INCLUDE_IF(preinstalled-snaps,external-snaps)
+    cronjobController.init();
     // Notification Setup
     notificationServicesController.init();
     ///: END:ONLY_INCLUDE_IF
