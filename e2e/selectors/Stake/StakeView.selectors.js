7d02c6014a575ae22e261661b6a5f67158642ea9
Davide Brocchetto <davide.brocchetto@consensys.net>
2025-06-27 11:08:20 -0700

    Fixes

--- e2e/selectors/Stake/StakeView.selectors.js
+++ e2e/selectors/Stake/StakeView.selectors.js
@@ -1,7 +1,7 @@
 import enContent from '../../../locales/languages/en.json';
 
 export const StakeViewSelectors = {
-  STAKE_CONTAINER: enContent.stake.stake_eth,
+  STAKE_CONTAINER: enContent.stake.deposit,
   UNSTAKE_CONTAINER: enContent.stake.unstake_eth,
   CONTINUE: enContent.stake.continue,
   REVIEW_BUTTON: enContent.stake.review,
