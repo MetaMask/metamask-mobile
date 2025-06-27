ee72c3694ed6e583bff76a418d93c0f814a9a208
ieow <cwlee0@gmail.com>
2025-06-27 16:26:52 +0800

    fix: default ios google client bundleid

--- app/core/OAuthService/OAuthLoginHandlers/config.ts
+++ app/core/OAuthService/OAuthLoginHandlers/config.ts
@@ -5,7 +5,7 @@ export const OAUTH_CONFIG: Record<string, Record<string, string>> = {
       ANDROID_APPLE_CLIENT_ID: 'com.web3auth.appleloginextension',
       ANDROID_GOOGLE_CLIENT_ID: '882363291751-2a37cchrq9oc1lfj1p419otvahnbhguv.apps.googleusercontent.com',
       ANDROID_GOOGLE_SERVER_CLIENT_ID: '882363291751-2a37cchrq9oc1lfj1p419otvahnbhguv.apps.googleusercontent.com',
-      IOS_APPLE_CLIENT_ID: getBundleId ? getBundleId() : 'io.metamask.Metamask',
+      IOS_APPLE_CLIENT_ID: getBundleId ? getBundleId() : 'io.metamask.MetaMask',
       IOS_GOOGLE_CLIENT_ID: '882363291751-nbbp9n0o307cfil1lup766g1s99k0932.apps.googleusercontent.com',
       IOS_GOOGLE_REDIRECT_URI: 'com.googleusercontent.apps.882363291751-nbbp9n0o307cfil1lup766g1s99k0932:/oauth2redirect/google',
 
@@ -23,7 +23,7 @@ export const OAUTH_CONFIG: Record<string, Record<string, string>> = {
       ANDROID_APPLE_CLIENT_ID: 'io.metamask.appleloginclient.prod',
       ANDROID_GOOGLE_CLIENT_ID: '795351133007-jcaor637tblrlpuj29shdej3co8bu8kv.apps.googleusercontent.com',
       ANDROID_GOOGLE_SERVER_CLIENT_ID: '795351133007-6d0s31utj13knv440fgjo2ur93241gb6.apps.googleusercontent.com',
-      IOS_APPLE_CLIENT_ID: getBundleId ? getBundleId() : 'io.metamask.Metamask',
+      IOS_APPLE_CLIENT_ID: getBundleId ? getBundleId() : 'io.metamask.MetaMask',
       IOS_GOOGLE_CLIENT_ID: '795351133007-47aohp9j9n7r8fef5n6ejeauhu4kfc9e.apps.googleusercontent.com',
       IOS_GOOGLE_REDIRECT_URI: 'com.googleusercontent.apps.795351133007-47aohp9j9n7r8fef5n6ejeauhu4kfc9e:/oauth2redirect/google',
 
@@ -42,7 +42,7 @@ export const OAUTH_CONFIG: Record<string, Record<string, string>> = {
       ANDROID_APPLE_CLIENT_ID: 'io.metamask.appleloginclient.uat',
       ANDROID_GOOGLE_CLIENT_ID: '387141446914-olajr83p1bbvabh1u8tfglt1k4u6jlcb.apps.googleusercontent.com',
       ANDROID_GOOGLE_SERVER_CLIENT_ID: '387141446914-olajr83p1bbvabh1u8tfglt1k4u6jlcb.apps.googleusercontent.com',
-      IOS_APPLE_CLIENT_ID: getBundleId ? getBundleId() : 'io.metamask.Metamask',
+      IOS_APPLE_CLIENT_ID: getBundleId ? getBundleId() : 'io.metamask.MetaMask',
       IOS_GOOGLE_CLIENT_ID: '387141446914-5ja3p4dfanfkm8uq238fm1b8t1rkscv4.apps.googleusercontent.com',
       IOS_GOOGLE_REDIRECT_URI: 'com.googleusercontent.apps.387141446914-5ja3p4dfanfkm8uq238fm1b8t1rkscv4:/oauth2redirect/google',
 
@@ -61,7 +61,7 @@ export const OAUTH_CONFIG: Record<string, Record<string, string>> = {
       ANDROID_APPLE_CLIENT_ID: 'io.metamask.appleloginclient.dev',
       ANDROID_GOOGLE_CLIENT_ID: '615965109465-i8oeh9kuvl1n6lk1ffkobpvth27bmi41.apps.googleusercontent.com',
       ANDROID_GOOGLE_SERVER_CLIENT_ID: '615965109465-i8oeh9kuvl1n6lk1ffkobpvth27bmi41.apps.googleusercontent.com',
-      IOS_APPLE_CLIENT_ID: getBundleId ? getBundleId() : 'io.metamask.Metamask',
+      IOS_APPLE_CLIENT_ID: getBundleId ? getBundleId() : 'io.metamask.MetaMask',
       IOS_GOOGLE_CLIENT_ID: '615965109465-h6tp2h3crls6hbggispcgovbvk4vabu3.apps.googleusercontent.com',
       IOS_GOOGLE_REDIRECT_URI: 'com.googleusercontent.apps.615965109465-h6tp2h3crls6hbggispcgovbvk4vabu3:/oauth2redirect/google',
 
@@ -79,7 +79,7 @@ export const OAUTH_CONFIG: Record<string, Record<string, string>> = {
       ANDROID_APPLE_CLIENT_ID: 'io.metamask.appleloginclient.flask.prod',
       ANDROID_GOOGLE_CLIENT_ID: '795351133007-0po6dfbepae7klaso18qv61f86u4a2ef.apps.googleusercontent.com',
       ANDROID_GOOGLE_SERVER_CLIENT_ID: '795351133007-gh67d3hot6ib24htu9d7sh01bg90lpdu.apps.googleusercontent.com',
-      IOS_APPLE_CLIENT_ID: getBundleId ? getBundleId() : 'io.metamask.Metamask-Flask',
+      IOS_APPLE_CLIENT_ID: getBundleId ? getBundleId() : 'io.metamask.MetaMask-Flask',
       IOS_GOOGLE_CLIENT_ID: '882363291751-nbbp9n0o307cfil1lup766g1s99k0932.apps.googleusercontent.com',
       IOS_GOOGLE_REDIRECT_URI: '795351133007-gvuagr9t7tfkak3sp2cmng4pdhchlfpd.apps.googleusercontent.com:/oauth2redirect/google',
 
