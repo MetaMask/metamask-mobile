505e2f5e46c5ac6b64391ff5993c9a237ef7fba7
Tyschenko <yaroslav.tyschenko@consensys.net>
2025-06-27 12:36:03 +0100

    Ignore linter errors

--- runNativeAndroidTests.js
+++ runNativeAndroidTests.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line import/no-commonjs
 require('dotenv').config({path: '.e2e.env'});
+// eslint-disable-next-line import/no-nodejs-modules,import/no-commonjs
 const {exec} = require('child_process');
 exec('cd android && ./gradlew connectedFlaskDebugAndroidTest -Pandroid.testInstrumentationRunnerArguments.package=com.metamask.ui', (error, stdout, stderr) => {
   if (error) {
@@ -8,5 +10,6 @@ exec('cd android && ./gradlew connectedFlaskDebugAndroidTest -Pandroid.testInstr
   if (stderr) {
     console.error(`Gradle stderr: ${stderr}`);
   }
+  // eslint-disable-next-line no-console
   console.log(`Gradle output:\n${stdout}`);
 });
