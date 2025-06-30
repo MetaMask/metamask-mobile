6cea7a3e23531a443d5f29cfe8b3437ff3ed9992
ieow <cwlee0@gmail.com>
2025-06-30 11:55:11 +0800

    fix: seedless onboarding runtime env

--- app/core/OAuthService/OAuthLoginHandlers/constants.ts
+++ app/core/OAuthService/OAuthLoginHandlers/constants.ts
@@ -3,6 +3,9 @@ import AppConstants from '../../AppConstants';
 import { AuthConnection } from '../OAuthInterface';
 import { OAUTH_CONFIG } from './config';
 
+export const SEEDLESS_ONBOARDING_ENABLED =
+  process.env.SEEDLESS_ONBOARDING_ENABLED === 'true';
+
 // const BuildType = AppConstants.IS_DEV ? 'development' : AppConstants.METAMASK_BUILD_TYPE || 'development';
 // use development config for now
 const BuildType = 'development';
@@ -11,7 +14,8 @@ const CURRENT_OAUTH_CONFIG = OAUTH_CONFIG[BuildType];
 export const web3AuthNetwork = CURRENT_OAUTH_CONFIG.WEB3AUTH_NETWORK;
 export const AuthServerUrl = CURRENT_OAUTH_CONFIG.AUTH_SERVER_URL;
 export const IosGID = CURRENT_OAUTH_CONFIG.IOS_GOOGLE_CLIENT_ID;
-export const IosGoogleRedirectUri = CURRENT_OAUTH_CONFIG.IOS_GOOGLE_REDIRECT_URI;
+export const IosGoogleRedirectUri =
+  CURRENT_OAUTH_CONFIG.IOS_GOOGLE_REDIRECT_URI;
 export const IosAppleClientId = CURRENT_OAUTH_CONFIG.IOS_APPLE_CLIENT_ID;
 export const AndroidGoogleWebGID =
   CURRENT_OAUTH_CONFIG.ANDROID_GOOGLE_SERVER_CLIENT_ID;
@@ -25,34 +29,36 @@ export enum SupportedPlatforms {
   IOS = 'ios',
 }
 
-export const AuthConnectionConfig: Record<SupportedPlatforms, Record<AuthConnection, {
-  authConnectionId: string;
-  groupedAuthConnectionId?: string;
-}>> = {
+export const AuthConnectionConfig: Record<
+  SupportedPlatforms,
+  Record<
+    AuthConnection,
+    {
+      authConnectionId: string;
+      groupedAuthConnectionId?: string;
+    }
+  >
+> = {
   [SupportedPlatforms.Android]: {
     [AuthConnection.Google]: {
-      authConnectionId:
-        CURRENT_OAUTH_CONFIG.ANDROID_GOOGLE_AUTH_CONNECTION_ID,
+      authConnectionId: CURRENT_OAUTH_CONFIG.ANDROID_GOOGLE_AUTH_CONNECTION_ID,
       groupedAuthConnectionId:
         CURRENT_OAUTH_CONFIG.GOOGLE_GROUPED_AUTH_CONNECTION_ID,
     },
     [AuthConnection.Apple]: {
-      authConnectionId:
-        CURRENT_OAUTH_CONFIG.ANDROID_APPLE_AUTH_CONNECTION_ID,
+      authConnectionId: CURRENT_OAUTH_CONFIG.ANDROID_APPLE_AUTH_CONNECTION_ID,
       groupedAuthConnectionId:
         CURRENT_OAUTH_CONFIG.APPLE_GROUPED_AUTH_CONNECTION_ID,
     },
   },
   [SupportedPlatforms.IOS]: {
     [AuthConnection.Google]: {
-      authConnectionId:
-        CURRENT_OAUTH_CONFIG.IOS_GOOGLE_AUTH_CONNECTION_ID,
+      authConnectionId: CURRENT_OAUTH_CONFIG.IOS_GOOGLE_AUTH_CONNECTION_ID,
       groupedAuthConnectionId:
         CURRENT_OAUTH_CONFIG.GOOGLE_GROUPED_AUTH_CONNECTION_ID,
     },
     [AuthConnection.Apple]: {
-      authConnectionId:
-        CURRENT_OAUTH_CONFIG.IOS_APPLE_AUTH_CONNECTION_ID,
+      authConnectionId: CURRENT_OAUTH_CONFIG.IOS_APPLE_AUTH_CONNECTION_ID,
       groupedAuthConnectionId:
         CURRENT_OAUTH_CONFIG.APPLE_GROUPED_AUTH_CONNECTION_ID,
     },
