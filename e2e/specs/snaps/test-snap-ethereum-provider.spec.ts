05e1cf63567ed154c6c9b1e9d73fcf3b04f5c6e3
Frederik Bolding <frederik.bolding@gmail.com>
2025-07-01 16:07:06 +0200

    Change fixture

--- e2e/specs/snaps/test-snap-ethereum-provider.spec.ts
+++ e2e/specs/snaps/test-snap-ethereum-provider.spec.ts
@@ -23,7 +23,7 @@ describe(FlaskBuildTests('Ethereum Provider Snap Tests'), () => {
   it('can use the Ethereum provider', async () => {
     await withFixtures(
       {
-        fixture: new FixtureBuilder().build(),
+        fixture: new FixtureBuilder().withMultiSRPKeyringController().build(),
         restartDevice: true,
         testSpecificMock: {
           GET: [mockEvents.GET.remoteFeatureFlagsRedesignedConfirmationsFlask],
@@ -50,7 +50,7 @@ describe(FlaskBuildTests('Ethereum Provider Snap Tests'), () => {
         await TestHelpers.delay(500);
         await TestSnaps.checkResultSpanIncludes(
           'ethereumProviderResultSpan',
-          '"0x76cf1CdD1fcC252442b50D6e97207228aA4aefC3"',
+          '"0x5cfe73b6021e818b776b421b1c4db2474086a7e1"',
         );
 
         // Test `personal_sign`.
@@ -60,7 +60,7 @@ describe(FlaskBuildTests('Ethereum Provider Snap Tests'), () => {
         await TestSnaps.approveNativeConfirmation();
         await TestSnaps.checkResultSpan(
           'personalSignResultSpan',
-          '"0xc87c81cc8592936ec9dedfb6040080a0ac100c713231fd3b2ee93942a175efb639ccda7918bd14cbe8ef2f5ec917bba1a35744c06d2d4ff3a8a6b077c3e2a1381c"',
+          '"0xf63c587cd42e7775e2e815a579f9744ea62944f263b3e69fad48535ba98a5ea107bc878088a99942733a59a89ef1d590eafdb467d59cf76564158d7e78351b751b"',
         );
 
         // Test `eth_signTypedData_v4`.
@@ -70,7 +70,7 @@ describe(FlaskBuildTests('Ethereum Provider Snap Tests'), () => {
         await TestSnaps.approveNativeConfirmation();
         await TestSnaps.checkResultSpan(
           'signTypedDataResultSpan',
-          '"0x85094c93975d31ceddddf2894fc7bb96ce6cfdac2559a72417bc214e6e9f24195fa708010da7a52b5df6c75595d0e03f90749b2b5bfbd5605ca799ceb91a36681b"',
+          '"0x7024dc071a7370eee444b2a3edc08d404dd03393694403cdca864653a7e8dd7c583419293d53602666cbe77faa8819fba04f8c57e95df2d4c0190968eece28021c"',
         );
 
         // Check other networks.
