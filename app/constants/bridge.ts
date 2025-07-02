62dfc1930308b36cc3c91985d749884058018ae0
Stanley Yuen <102275989+stanleyyconsensys@users.noreply.github.com>
2025-07-02 17:10:55 +0800

    chore: update endpoint

--- app/constants/bridge.ts
+++ app/constants/bridge.ts
@@ -40,7 +40,4 @@ export const NETWORK_TO_SHORT_NETWORK_NAME_MAP: Record<
   [SolScope.Mainnet]: 'Solana',
 };
 
-export const BRIDGE_API_BASE_URL =
-  process.env.BRIDGE_USE_DEV_APIS === 'true'
-    ? BRIDGE_DEV_API_BASE_URL
-    : BRIDGE_PROD_API_BASE_URL;
+export const BRIDGE_API_BASE_URL = BRIDGE_DEV_API_BASE_URL;
