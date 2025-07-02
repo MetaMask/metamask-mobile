4cf322fb044f7e2eda0add75260152a84b9760be
JP <jp.sainsbury@consensys.net>
2025-07-02 05:02:29 -0500

    feat: fix defi polling

--- app/components/hooks/AssetPolling/AssetPollingProvider.tsx
+++ app/components/hooks/AssetPolling/AssetPollingProvider.tsx
@@ -6,41 +6,45 @@ import useTokenDetectionPolling from './useTokenDetectionPolling';
 import useTokenListPolling from './useTokenListPolling';
 import useTokenBalancesPolling from './useTokenBalancesPolling';
 import useAccountTrackerPolling from './useAccountTrackerPolling';
+import useDeFiPolling from './useDeFiPolling';
 
 // This provider is a step towards making controller polling fully UI based.
 // Eventually, individual UI components will call the use*Polling hooks to
 // poll and return particular data. This polls globally in the meantime.
-export const AssetPollingProvider = memo(({
-  chainId,
-  networkClientId,
-  address,
-}: {
-  chainId?: Hex;
-  networkClientId?: string;
-  address?: Hex;
-}) => {
-  const accountTrackerParams = useMemo(
-    () =>
-      networkClientId ? { networkClientIds: [networkClientId] } : undefined,
-    [networkClientId],
-  );
+export const AssetPollingProvider = memo(
+  ({
+    chainId,
+    networkClientId,
+    address,
+  }: {
+    chainId?: Hex;
+    networkClientId?: string;
+    address?: Hex;
+  }) => {
+    const accountTrackerParams = useMemo(
+      () =>
+        networkClientId ? { networkClientIds: [networkClientId] } : undefined,
+      [networkClientId],
+    );
 
-  const chainParams = useMemo(
-    () => (chainId ? { chainIds: [chainId] } : undefined),
-    [chainId],
-  );
+    const chainParams = useMemo(
+      () => (chainId ? { chainIds: [chainId] } : undefined),
+      [chainId],
+    );
 
-  const tokenDetectionParams = useMemo(
-    () => (chainId && address ? { chainIds: [chainId], address } : undefined),
-    [chainId, address],
-  );
+    const tokenDetectionParams = useMemo(
+      () => (chainId && address ? { chainIds: [chainId], address } : undefined),
+      [chainId, address],
+    );
 
-  useAccountTrackerPolling(accountTrackerParams);
-  useCurrencyRatePolling(chainParams);
-  useTokenRatesPolling(chainParams);
-  useTokenDetectionPolling(tokenDetectionParams);
-  useTokenListPolling(chainParams);
-  useTokenBalancesPolling(chainParams);
+    useAccountTrackerPolling(accountTrackerParams);
+    useCurrencyRatePolling(chainParams);
+    useTokenRatesPolling(chainParams);
+    useTokenDetectionPolling(tokenDetectionParams);
+    useTokenListPolling(chainParams);
+    useTokenBalancesPolling(chainParams);
+    useDeFiPolling();
 
-  return null;
-});
+    return null;
+  },
+);
