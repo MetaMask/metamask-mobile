088be60ebccdb2b2bdda0452580cdfd5b44941e2
hieu-w <maestrokoder@gmail.com>
2025-07-03 11:08:17 +0700

    test: fix flaky scrollToElement util

--- e2e/utils/Gestures.js
+++ e2e/utils/Gestures.js
@@ -269,7 +269,10 @@ class Gestures {
     await waitFor(await destinationElement)
       .toBeVisible()
       .whileElement(await scrollIdentifier)
-      .scroll(scrollAmount, direction);
+      .scroll(scrollAmount, direction)
+      .catch(async (_) => {
+        await waitFor(await destinationElement).toBeVisible().withTimeout(2000);
+      });
   }
 }
 
