b8b1b613b8199efb43626502a7113033aa02bdf2
Cal Leung <cal.leung@consensys.net>
2025-07-08 12:30:16 -0700

    chore: Chore/16093 experimental environment (#16647)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    These changes adds the experimental environment to support nightly
    builds. The changes were made using this previous PR as reference -
    https://github.com/MetaMask/metamask-mobile/pull/15861
    
    ## **Related issues**
    
    Fixes: #16093 
    
    ## **Manual testing steps**
    
    Android exp build -
    https://app.bitrise.io/build/d185f10b-ada7-4687-b02d-251e92719896
    iOS exp build -
    https://app.bitrise.io/build/5de65696-36bb-4404-ae07-49d511498de1
    
    The builds should also be mapping the environment variables according to
    the `exp` environment in `build.sh`
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- metro.transform.js
+++ metro.transform.js
@@ -21,6 +21,7 @@ const availableFeatures = new Set([
   'multi-srp',
   'bitcoin',
   'solana',
+  'experimental',
 ]);
 
 const mainFeatureSet = new Set([
@@ -45,6 +46,8 @@ const flaskFeatureSet = new Set([
   'bitcoin',
   'solana',
 ]);
+// Experimental feature set includes all main features plus experimental
+const experimentalFeatureSet = new Set([...mainFeatureSet, 'experimental']);
 
 /**
  * Gets the features for the current build type, used to determine which code
@@ -57,6 +60,11 @@ function getBuildTypeFeatures() {
   const envType = process.env.METAMASK_ENVIRONMENT ?? 'production';
   switch (buildType) {
     case 'main':
+      // TODO: Refactor this once we've abstracted environment away from build type
+      if (envType === 'exp') {
+        // Only include experimental features in experimental environment
+        return experimentalFeatureSet;
+      }
       return envType === 'beta' ? betaFeatureSet : mainFeatureSet;
     case 'beta':
       return betaFeatureSet;
