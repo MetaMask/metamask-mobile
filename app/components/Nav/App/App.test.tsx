fc6bbfd7f435bfffae9db8648423ec57d0ee6bd7
ieow <cwlee0@gmail.com>
2025-07-09 01:05:08 +0800

    feat: 2.3 seedless oauth flow (#15883)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    ## **Description**
    Integration the following social login via the OAuth Service
    
    Provider supported
    Google
    Apple
    
    Proposed Authentication Flow using seedless onboarding
    ![Screenshot 2025-04-29 at 8 25
    46 PM](https://github.com/user-attachments/assets/99f93c54-ebae-4ac9-a62c-069c4c09beb4)
    
    This PR mainly implement the Login UI Flow for the seedless onboarding
    
    Runtime environment `SEEDLESS_ONBOARDING_ENABLED` will be gating this
    feature and is default to false
    
    
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. 
    copy SEEDLESS_ONBOARDING_ENABLED and respected buildType clientID env in
    js.env
    
    2. 
    - yarn && yarn setup
    - yarn watch:clean 
    
    3.
    Login Social Account
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    
    https://github.com/user-attachments/assets/8fa0a8fb-4763-4f05-9148-1b193253813a
    
    
    https://github.com/user-attachments/assets/ce1f82eb-9224-4ecd-9150-a922af08c3a2
    
    
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: huggingbot <83656073+huggingbot@users.noreply.github.com>
    Co-authored-by: Chaitanya Potti <chaitanya.potti@gmail.com>
    Co-authored-by: hieu-w <maestrokoder@gmail.com>

--- app/components/Nav/App/App.test.tsx
+++ app/components/Nav/App/App.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   DeepPartial,
   renderScreen,
@@ -5,11 +6,16 @@ import {
 import { backgroundState } from '../../../util/test/initial-root-state';
 import App from '.';
 import { MetaMetrics } from '../../../core/Analytics';
-import { waitFor } from '@testing-library/react-native';
+import { cleanup, render, waitFor } from '@testing-library/react-native';
 import { RootState } from '../../../reducers';
 import StorageWrapper from '../../../store/storage-wrapper';
 import { Authentication } from '../../../core';
 import Routes from '../../../constants/navigation/Routes';
+import { strings } from '../../../../locales/i18n';
+import { NavigationContainer } from '@react-navigation/native';
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import { mockTheme, ThemeContext } from '../../../util/theme';
 
 const initialState: DeepPartial<RootState> = {
   user: {
@@ -21,10 +27,42 @@ const initialState: DeepPartial<RootState> = {
   },
 };
 
+jest.mock('react-native-branch', () => ({
+  subscribe: jest.fn(),
+}));
+
+jest.mock('react-native/Libraries/Linking/Linking', () => ({
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+}));
+
+jest.mock('../../../core/DeeplinkManager/SharedDeeplinkManager', () => ({
+  init: jest.fn(),
+  parse: jest.fn(),
+}));
+
+jest.mock('../../../core/SDKConnect/SDKConnect', () => ({
+  getInstance: () => ({
+    init: jest.fn().mockResolvedValue(undefined),
+    postInit: (cb: () => void) => cb(),
+  }),
+}));
+
+jest.mock('../../../../app/core/WalletConnect/WalletConnectV2', () => ({
+  init: jest.fn().mockResolvedValue(undefined),
+}));
+
+jest.mock('../../../lib/ppom/PPOMView', () => ({ PPOMView: () => null }));
+
 jest.mock('../../../core/NavigationService', () => ({
   navigation: {
     reset: jest.fn(),
   },
+}));
+
+// expo library are not supported in jest ( unless using jest-expo as preset ), so we need to mock them
+jest.mock('../../../core/OAuthService/OAuthLoginHandlers', () => ({
+  createLoginHandler: jest.fn(),
 }));
 
 // Mock the navigation hook
@@ -59,9 +97,20 @@ jest.mock(
 (MetaMetrics.getInstance as jest.Mock).mockReturnValue(mockMetrics);
 
 describe('App', () => {
+  jest.useFakeTimers();
+
   beforeEach(() => {
     jest.clearAllMocks();
     mockNavigate.mockClear();
+  });
+
+  afterEach(() => {
+    cleanup();
+    jest.runOnlyPendingTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
   });
 
   it('configures MetaMetrics instance and identifies user on startup', async () => {
@@ -116,6 +165,51 @@ describe('App', () => {
           },
         });
       });
+    });
+  });
+
+  describe('OnboardingRootNav', () => {
+    it('renders the very first onboarding screen when you navigate into OnboardingRootNav', async () => {
+      const routeState = {
+        routes: [
+          {
+            name: Routes.ONBOARDING.ROOT_NAV,
+            state: {
+              index: 0,
+              routes: [
+                {
+                  name: Routes.ONBOARDING.NAV,
+                  state: {
+                    index: 0,
+                    routes: [
+                      {
+                        name: 'OnboardingCarousel',
+                        params: {}
+                      }
+                    ]
+                  }
+                }
+              ],
+            },
+          },
+        ],
+      };
+      const mockStore = configureMockStore();
+      const store = mockStore(initialState);
+
+      const Providers = ({ children }: { children: React.ReactElement }) => (
+        <NavigationContainer initialState={routeState}>
+          <Provider store={store}>
+            <ThemeContext.Provider value={mockTheme}>
+              {children}
+            </ThemeContext.Provider>
+          </Provider>
+        </NavigationContainer>
+      );
+
+      const { getByText } = render(<App />, { wrapper: Providers });
+
+      expect(getByText(strings('onboarding_carousel.get_started'))).toBeTruthy();
     });
   });
 });
