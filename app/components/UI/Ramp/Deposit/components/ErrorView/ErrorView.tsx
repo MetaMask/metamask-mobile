1aed7fce2537e343927c82d5afbe240501d42dad
AxelGes <axelges9@gmail.com>
2025-07-08 23:43:35 -0300

    feat: cleanup errorview

--- app/components/UI/Ramp/Deposit/components/ErrorView/ErrorView.tsx
+++ app/components/UI/Ramp/Deposit/components/ErrorView/ErrorView.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import { strings } from '../../../../../../../locales/i18n';
 import {
@@ -20,18 +20,14 @@ import { useStyles } from '../../../../../../component-library/hooks';
 import styleSheet from './ErrorView.styles';
 
 interface Props {
-  description?: string; // The error description (Optional)
-  title?: string; //  The error title, default will be "Error" if not provided (Optional)
-  ctaLabel?: string; // The CTA button label, default will be "Try again" (Optional)
-  ctaOnPress?: () => void; // The optional callback to be invoked when pressing the CTA button (Optional)
+  description?: string;
+  title?: string;
+  ctaLabel?: string;
+  ctaOnPress?: () => void;
 }
 
 function ErrorView({ description, title, ctaLabel, ctaOnPress }: Props) {
   const { styles } = useStyles(styleSheet, {});
-
-  const ctaOnPressCallback = useCallback(() => {
-    ctaOnPress?.();
-  }, [ctaOnPress]);
 
   return (
     <View style={styles.content}>
@@ -66,7 +62,7 @@ function ErrorView({ description, title, ctaLabel, ctaOnPress }: Props) {
           <Button
             style={styles.button}
             variant={ButtonVariants.Primary}
-            onPress={ctaOnPressCallback}
+            onPress={ctaOnPress}
             label={ctaLabel || strings('deposit.error_view.try_again')}
             size={ButtonSize.Lg}
           />
