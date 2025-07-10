50375e335dff33297d18bfe8f0cd1948ee957ad3
Monte Lai <monte.lai@consensys.net>
2025-07-10 16:00:34 +0800

    fix: update logic of vault to use change password from keyring controller

--- app/core/Vault.js
+++ app/core/Vault.js
@@ -1,4 +1,8 @@
 import Engine from './Engine';
+import Logger from '../util/Logger';
+import { KeyringTypes } from '@metamask/keyring-controller';
+import ReduxService from './redux';
+import { selectSeedlessOnboardingLoginFlow } from '../selectors/seedlessOnboardingController';
 
 /**
  * Returns current vault seed phrase
@@ -22,10 +26,37 @@ export const recreateVaultWithNewPassword = async (
   newPassword,
   selectedAddress,
 ) => {
-  const { KeyringController } = Engine.context;
+  const { KeyringController, SeedlessOnboardingController } = Engine.context;
   const { setSelectedAddress } = Engine;
-  await KeyringController.verifyPassword(password);
+  const [primaryKeyring] = KeyringController.state.keyrings.filter(
+    (keyring) => keyring.type === KeyringTypes.hd,
+  );
+
+  const primaryKeyringSeedPhrase = await getSeedPhrase(
+    password,
+    primaryKeyring.metadata.id,
+  );
+
+  if (!primaryKeyringSeedPhrase) {
+    throw new Error('error while trying to get seed phrase on recreate vault');
+  }
+
   await KeyringController.changePassword(password, newPassword);
+
+  let seedlessChangePasswordError = null;
+  if (selectSeedlessOnboardingLoginFlow(ReduxService.store.getState())) {
+    try {
+      await SeedlessOnboardingController.changePassword(newPassword, password);
+    } catch (error) {
+      Logger.error(error);
+      await KeyringController.createNewVaultAndRestore(
+        password,
+        primaryKeyringSeedPhrase,
+      );
+      seedlessChangePasswordError = error;
+    }
+  }
+
   setSelectedAddress(selectedAddress);
 };
 
