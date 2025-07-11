d84839c3f9ea09ab3f8ffc7013bb834c1e347146
ieow <cwlee0@gmail.com>
2025-07-11 16:50:02 +0800

    fix: update authtokenhandler

--- app/core/OAuthService/AuthTokenHandler.ts
+++ app/core/OAuthService/AuthTokenHandler.ts
@@ -1,50 +1,63 @@
+import { Platform } from 'react-native';
+import { AuthConnection } from './OAuthInterface';
+import { createLoginHandler } from './OAuthLoginHandlers';
+
 class AuthTokenHandler {
-  private config?: {
-    authServerUrl: string;
-    authConnectionId: string;
-    groupedAuthConnectionId?: string;
-  };
+  protected readonly AUTH_SERVER_TOKEN_PATH = '/api/v1/oauth/token';
 
-  setAuthServerConfig(config: {
-    authServerUrl: string;
-    authConnectionId: string;
-    groupedAuthConnectionId?: string;
+  protected readonly AUTH_SERVER_REVOKE_PATH = '/api/v1/oauth/revoke';
+
+  async refreshJWTToken(params: {
+    connection: AuthConnection;
+    refreshToken: string;
   }) {
-    this.config = config;
-  }
+    const { connection, refreshToken } = params;
+    const loginHandler = createLoginHandler(Platform.OS, connection);
 
-  clearAuthServerConfig() {
-    this.config = undefined;
-  }
+    const requestData = {
+      client_id: loginHandler.options.clientId,
+      login_provider: connection,
+      network: loginHandler.options.web3AuthNetwork,
+      refresh_token: refreshToken,
+      grant_type: 'refresh_token', // specify refresh token flow
+    };
 
-  async refreshJWTToken() {
-    if (!this.config) {
-      throw new Error('Auth server config not set');
-    }
-    const response = await fetch(`${this.config.authServerUrl}/refresh-jwt`, {
-      method: 'POST',
-      body: JSON.stringify({
-        authConnectionId: this.config.authConnectionId,
-        groupedAuthConnectionId: this.config.groupedAuthConnectionId,
-      }),
-    });
+    const response = await fetch(
+      `${loginHandler.options.authServerUrl}${this.AUTH_SERVER_TOKEN_PATH}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestData),
+      },
+    );
+
     return response.json();
   }
 
-  async revokeRefreshToken() {
-    if (!this.config) {
-      throw new Error('Auth server config not set');
-    }
+  async revokeRefreshToken(params: {
+    connection: AuthConnection;
+    revokeToken: string;
+  }) {
+    const { connection, revokeToken } = params;
+    const loginHandler = createLoginHandler(Platform.OS, connection);
+
+    const requestData = {
+      revoke_token: revokeToken,
+    };
+
     const response = await fetch(
-      `${this.config.authServerUrl}/revoke-refresh-token`,
+      `${loginHandler.options.authServerUrl}${this.AUTH_SERVER_REVOKE_PATH}`,
       {
         method: 'POST',
-        body: JSON.stringify({
-          authConnectionId: this.config.authConnectionId,
-          groupedAuthConnectionId: this.config.groupedAuthConnectionId,
-        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestData),
       },
     );
+
     return response.json();
   }
 }
