a841ae0731b723c4d624abd14a90d0bb772bc274
Bruno Nascimento <bruno.nascimento@consensys.net>
2025-07-14 17:34:09 -0300

    refactor(card): useGetAllowances using wrong text for logging errors

--- app/components/UI/Card/hooks/useGetAllowances.tsx
+++ app/components/UI/Card/hooks/useGetAllowances.tsx
@@ -64,7 +64,7 @@ export const useGetAllowances = (address?: string, autoFetch = false) => {
 
         setAllowances(filteredAllowances);
       } catch (error) {
-        console.error('Error fetching priority token:', error);
+        console.error('Error fetching priority allowances:', error);
       } finally {
         setIsLoading(false);
       }
