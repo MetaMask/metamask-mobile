8500c2db930dc20675cca941f9d853ede2d759f2
Monte Lai <monte.lai@consensys.net>
2025-07-15 07:57:45 +0800

    fix: test

--- app/core/Vault.test.ts
+++ app/core/Vault.test.ts
@@ -1,8 +1,11 @@
-import { EthAccountType } from '@metamask/keyring-api';
+import { EthAccountType, SolAccountType } from '@metamask/keyring-api';
 import Engine from './Engine';
 
 import { KeyringTypes } from '@metamask/keyring-controller';
-import { createMockInternalAccount } from '../util/test/accountsControllerTestUtils';
+import {
+  createMockInternalAccount,
+  createMockSnapInternalAccount,
+} from '../util/test/accountsControllerTestUtils';
 import { recreateVaultWithNewPassword } from './Vault';
 import ReduxService, { ReduxStore } from './redux';
 
@@ -17,6 +20,119 @@ const mockHdAccount1 = createMockInternalAccount(
   KeyringTypes.hd,
   EthAccountType.Eoa,
 );
+const mockHdAccount2 = createMockInternalAccount(
+  '0xf57E323fD8C7Bb908A13557b1cE4441c8213824c',
+  'Hd Account 2',
+  KeyringTypes.hd,
+  EthAccountType.Eoa,
+);
+const mockHdAccount3 = createMockInternalAccount(
+  '0x67B2fAf7959fB61eb9746571041476Bbd0672569',
+  'Hd Account 3',
+  KeyringTypes.hd,
+  EthAccountType.Eoa,
+);
+const mockHdAccount4 = createMockInternalAccount(
+  '0xf57E323fD8C7Bb908A13557b1cE4441c8213824c',
+  'Hd Account 4',
+  KeyringTypes.hd,
+  EthAccountType.Eoa,
+);
+const mockQrAccount = createMockInternalAccount(
+  '0xB6ce536fe74d9d5a25356B249424E1D3BDaADC57',
+  'Qr Account',
+  KeyringTypes.qr,
+  EthAccountType.Eoa,
+);
+const mockLedgerAccount = createMockInternalAccount(
+  '0xE3D19DCfE5255C3448CB80299906ac15Dee2cF29',
+  'Ledger Account',
+  KeyringTypes.ledger,
+  EthAccountType.Eoa,
+);
+const mockSolanaAccount = createMockSnapInternalAccount(
+  '',
+  'Solana Account 1',
+  SolAccountType.DataAccount,
+  'hd-keyring-id-2',
+);
+const mockThirdPartySnapAccount = createMockSnapInternalAccount(
+  '0x571CE5f203f93662301D43A5020aDB895f842cC9',
+  'Third Party Snap Account',
+  EthAccountType.Eoa,
+);
+
+const mockPrivateKeyAccount = createMockInternalAccount(
+  '0x15fB9d189aEa6233bE7487294702A431a7656810',
+  'Private Key Account',
+  KeyringTypes.simple,
+  EthAccountType.Eoa,
+);
+const mockHdKeyringMetadata = {
+  id: 'hd-keyring-id',
+  name: '',
+};
+
+const mockHdKeyringMetadata2 = {
+  id: 'hd-keyring-id-2',
+  name: '',
+};
+
+const mockQrKeyringMetadata = {
+  id: 'qr-keyring-id',
+  name: '',
+};
+
+const mockLedgerKeyringMetadata = {
+  id: 'ledger-keyring-id',
+  name: '',
+};
+
+const mockSnapKeyringMetadata = {
+  id: 'snap-keyring-id',
+  name: '',
+};
+
+const mockSimpleKeyringMetadata = {
+  id: 'simple-keyring-id',
+  name: '',
+};
+
+const mockHdKeyring = {
+  type: KeyringTypes.hd,
+  accounts: [mockHdAccount1.address, mockHdAccount2.address],
+  metadata: mockHdKeyringMetadata,
+};
+
+const mockHdKeyring2 = {
+  type: KeyringTypes.hd,
+  accounts: [mockHdAccount3.address, mockHdAccount4.address],
+  metadata: mockHdKeyringMetadata2,
+};
+
+const mockQrKeyring = {
+  type: KeyringTypes.qr,
+  accounts: [mockQrAccount.address],
+  metadata: mockQrKeyringMetadata,
+};
+
+const mockLedgerKeyring = {
+  type: KeyringTypes.ledger,
+  accounts: [mockLedgerAccount.address],
+  metadata: mockLedgerKeyringMetadata,
+};
+
+const mockSnapKeyring = {
+  type: KeyringTypes.snap,
+  accounts: [mockSolanaAccount.address, mockThirdPartySnapAccount.address],
+  metadata: mockSnapKeyringMetadata,
+};
+
+const mockSimpleKeyring = {
+  type: KeyringTypes.simple,
+  accounts: [mockPrivateKeyAccount.address],
+  metadata: mockSimpleKeyringMetadata,
+};
 
 jest.mock('./Engine', () => ({
   context: {
@@ -27,6 +143,18 @@ jest.mock('./Engine', () => ({
       exportSeedPhrase: (password: string) => mockExportSeedPhrase(password),
       createNewVaultAndRestore: (password: string, seedPhrases: string[]) =>
         mockCreateNewVaultAndRestore(password, seedPhrases),
+      state: {
+        get keyrings() {
+          return [
+            mockHdKeyring,
+            mockHdKeyring2,
+            mockQrKeyring,
+            mockLedgerKeyring,
+            mockSnapKeyring,
+            mockSimpleKeyring,
+          ];
+        },
+      },
     },
     SeedlessOnboardingController: {
       changePassword: jest.fn(),
