d96898c843dc8e0821bb69e34957f50bae034fd5
smgv <gpatra1996@gmail.com>
2025-07-14 22:56:06 +0530

    fix: test case update

--- app/components/Views/QRScanner/index.test.tsx
+++ app/components/Views/QRScanner/index.test.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { waitFor } from '@testing-library/react-native';
+import { useCameraPermission } from 'react-native-vision-camera';
 
 import renderWithProvider from '../../../util/test/renderWithProvider';
 import QrScanner from './';
@@ -18,6 +20,20 @@ jest.mock('@react-navigation/native', () => {
   };
 });
 
+jest.mock('react-native-vision-camera', () => ({
+  Camera: () => null,
+  useCameraDevice: jest.fn(() => ({ id: 'back' })),
+  useCameraPermission: jest.fn(),
+  useCodeScanner: jest.fn(() => ({
+    codeTypes: ['qr'],
+    onCodeScanned: jest.fn(),
+  })),
+}));
+
+const mockUseCameraPermission = useCameraPermission as jest.MockedFunction<
+  typeof useCameraPermission
+>;
+
 const initialState = {
   engine: {
     backgroundState,
@@ -25,15 +41,51 @@ const initialState = {
 };
 
 describe('QrScanner', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockUseCameraPermission.mockReturnValue({
+      hasPermission: true,
+      requestPermission: jest.fn().mockResolvedValue('granted'),
+    });
+  });
+
   it('render matches snapshot', () => {
     const { toJSON } = renderWithProvider(
-      <QrScanner
-        onScanSuccess={() => {
-          //unused
-        }}
-      />,
+      <QrScanner onScanSuccess={jest.fn()} />,
       { state: initialState },
     );
     expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('should request permission when hasPermission is false', async () => {
+    const mockRequestPermission = jest.fn().mockResolvedValue('granted');
+    mockUseCameraPermission.mockReturnValue({
+      hasPermission: false,
+      requestPermission: mockRequestPermission,
+    });
+
+    renderWithProvider(<QrScanner onScanSuccess={jest.fn()} />, {
+      state: initialState,
+    });
+
+    await waitFor(() => {
+      expect(mockRequestPermission).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it('should not request permission when hasPermission is true', async () => {
+    const mockRequestPermission = jest.fn();
+    mockUseCameraPermission.mockReturnValue({
+      hasPermission: true,
+      requestPermission: mockRequestPermission,
+    });
+
+    renderWithProvider(<QrScanner onScanSuccess={jest.fn()} />, {
+      state: initialState,
+    });
+
+    await waitFor(() => {
+      expect(mockRequestPermission).not.toHaveBeenCalled();
+    });
   });
 });
