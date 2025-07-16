92e9d4622859c5dab1c7ac5540f1e88ce507cd1d
cmd-ob <ola.bale@consensys.net>
2025-07-16 19:05:11 +0100

    fix: update import path for Assertions in salt generation test

--- e2e/specs/accounts/aes/salt-generation.spec.ts
+++ e2e/specs/accounts/aes/salt-generation.spec.ts
@@ -1,6 +1,6 @@
 import { SmokeAccounts } from '../../../tags';
 import TestHelpers from '../../../helpers';
-import Assertions from '../../../utils/Assertions';
+import Assertions from '../../../framework/Assertions';
 import TabBarComponent from '../../../pages/wallet/TabBarComponent';
 import SettingsView from '../../../pages/Settings/SettingsView';
 import { loginToApp } from '../../../viewHelper';
