5330d974e9abc7cae4ab01e2094c3f44cf86dc9c
smilingkylan <kylan.hurt@gmail.com>
2025-07-15 18:51:19 -0700

    Add try / catch to BrowserTab->SendActiveAccount in case of invalid URL parse attempts

--- app/components/Views/BrowserTab/BrowserTab.tsx
+++ app/components/Views/BrowserTab/BrowserTab.tsx
@@ -979,18 +979,22 @@ export const BrowserTab: React.FC<BrowserTabProps> = React.memo(({
       return;
     }
     
-    // Get permitted accounts for the target URL
-    const permissionsControllerState = Engine.context.PermissionController.state;
-    const hostname = new URLParse(urlToCheck).hostname;
-    const permittedAcc = getPermittedEvmAddressesByHostname(
-      permissionsControllerState,
-      hostname,
-    );
-
-    notifyAllConnections({
-      method: NOTIFICATION_NAMES.accountsChanged,
-      params: permittedAcc,
-    });
+    try {
+      // Get permitted accounts for the target URL
+      const permissionsControllerState = Engine.context.PermissionController.state;
+      const hostname = new URLParse(urlToCheck).hostname;
+      const permittedAcc = getPermittedEvmAddressesByHostname(
+        permissionsControllerState,
+        hostname,
+      );
+  
+      notifyAllConnections({
+        method: NOTIFICATION_NAMES.accountsChanged,
+        params: permittedAcc,
+      });
+    } catch (err) {
+      console.warn(err);
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [notifyAllConnections]);
 
