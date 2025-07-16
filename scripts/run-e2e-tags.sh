67dbc1d18673737caff3ca3a2360c5d1ce49dd86
João <castrofjoao@gmail.com>
2025-07-16 15:26:29 +0100

    feat(INFRA-2766): run many tests

--- scripts/run-e2e-tags.sh
+++ scripts/run-e2e-tags.sh
@@ -40,11 +40,9 @@ TEST_FILES="${matching_files[*]}"
 if [[ "$BITRISE_TRIGGERED_WORKFLOW_ID" == *"ios"* ]]; then
     echo "Detected iOS workflow"
     IGNORE_BOXLOGS_DEVELOPMENT="true" \
-    #yarn test:e2e:ios:run:qa-release $TEST_FILES
-    yarn test:e2e:ios:run:qa-release e2e/specs/confirmations/send-to-contract-address.spec.ts
+    yarn test:e2e:ios:run:qa-release $TEST_FILES
 else
     echo "Detected Android workflow"
     IGNORE_BOXLOGS_DEVELOPMENT="true" \
-    #yarn test:e2e:android:run:qa-release $TEST_FILES
-    yarn test:e2e:android:run:qa-release e2e/specs/confirmations/send-to-contract-address.spec.ts
+    yarn test:e2e:android:run:qa-release $TEST_FILES
 fi
