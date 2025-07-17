c22ce04e1d6d128308bf018d7efd7c07568b1949
IF <139582705+infiniteflower@users.noreply.github.com>
2025-07-17 15:00:17 -0400

    chore: format

--- app/components/UI/Bridge/Views/BridgeView/BridgeView.test.tsx
+++ app/components/UI/Bridge/Views/BridgeView/BridgeView.test.tsx
@@ -427,7 +427,8 @@ describe('BridgeView', () => {
           address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', // USDC token address
           chainId: '0x1' as Hex,
           decimals: 6,
-          image: 'https://static.cx.metamask.io/api/v1/tokenIcons/1/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png',
+          image:
+            'https://static.cx.metamask.io/api/v1/tokenIcons/1/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png',
           name: 'USD Coin',
           symbol: 'USDC',
         },
