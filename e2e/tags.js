6c7a6f0494375eeb629dd2c4e9d1ce28d4150f40
Christopher Ferreira <christopher.ferreira@consensys.net>
2025-07-17 16:28:29 +0100

    fix: reverts the changes in tags.js in favor of an hardcoded tag in the test

--- e2e/tags.js
+++ e2e/tags.js
@@ -18,7 +18,6 @@ const tags = {
   smokeAnalytics: 'SmokeAnalytics:',
   smokeMultiChainAPI: 'SmokeMultiChainAPI:',
   FlaskBuildTests: 'FlaskBuildTests:',
-  GHMigration: 'GHMigration:',
 };
 
 const Regression = (testName) => `${tags.regression} ${testName}`;
@@ -51,7 +50,6 @@ const SmokeNetworkExpansion = (testName) =>
   `${tags.SmokeNetworkExpansion} ${testName}`;
 const FlaskBuildTests = (testName) =>
   `${tags.FlaskBuildTests} ${testName}`;
-const GHMigration = (testName) => `${tags.GHMigration} ${testName}`;
 
 export {
   FlaskBuildTests,
@@ -73,5 +71,4 @@ export {
   SmokeNotifications,
   SmokeAnalytics,
   SmokeMultiChainAPI,
-  GHMigration,
 };
