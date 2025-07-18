5f11618efcedbb16dd0f39670745387067059d5c
Jyoti Puri <jyotipuri@gmail.com>
2025-07-18 22:18:57 +0530

    Adding a very basic send page for new send implementation.

---
+++ app/components/Views/confirmations/components/send/send.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import Text from '../../../../../component-library/components/Texts/Text';
+
+const Send = () => {
+  return (
+    <View>
+      <Text>SEND IMPLEMENTATION TO COME HERE</Text>
+    </View>
+  );
+};
+
+export default Send;
