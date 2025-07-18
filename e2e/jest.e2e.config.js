ceaa642e52c90fd677a754a75a59b461fef34aab
João <castrofjoao@gmail.com>
2025-07-18 12:19:27 +0100

    feat(INFRA-2766): monitoring + no parellelization in jest

--- e2e/jest.e2e.config.js
+++ e2e/jest.e2e.config.js
@@ -4,7 +4,7 @@ module.exports = {
   rootDir: '..',
   testMatch: ['<rootDir>/e2e/specs/**/*.spec.{js,ts}'],
   testTimeout: 500000,
-  maxWorkers: process.env.CI ? 3 : 1,
+  maxWorkers: 1, //process.env.CI ? 3 : 1,
   setupFilesAfterEnv: ['<rootDir>/e2e/init.js'],
   globalSetup: 'detox/runners/jest/globalSetup',
   globalTeardown: 'detox/runners/jest/globalTeardown',
