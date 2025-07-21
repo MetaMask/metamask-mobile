28458cd368f2e49da01c9326fbd5086f05f1c358
tommasini <tommasini15@gmail.com>
2025-07-21 13:49:17 +0100

    fix default acocunt fixture builder

--- e2e/fixtures/fixture-builder.js
+++ e2e/fixtures/fixture-builder.js
@@ -20,7 +20,7 @@ import {
 } from './constants';
 
 export const DEFAULT_FIXTURE_ACCOUNT =
-  '0x76cf1cdd1fcc252442b50d6e97207228aa4aefc3';
+  '0x76cf1CdD1fcC252442b50D6e97207228aA4aefC3';
 
 export const DEFAULT_FIXTURE_ACCOUNT_2 =
   '0xcdd74c6eb517f687aa2c786bc7484eb2f9bae1da';
