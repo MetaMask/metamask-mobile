320381bf616a1ec5025fedacb01dd9d1cb576f10
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2025-07-21 09:24:50 -0400

    refactor(deposit): reorder payment methods (#17394)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    This PR reorders the payment methods, moving Apple Pay to the top 
    
    ## **Changelog**
    
    <!--
    If this PR is not End-User-Facing and should not show up in the
    CHANGELOG, you can choose to either:
    1. Write `CHANGELOG entry: null`
    2. Label with `no-changelog`
    
    If this PR is End-User-Facing, please write a short User-Facing
    description in the past tense like:
    `CHANGELOG entry: Added a new tab for users to see their NFTs`
    `CHANGELOG entry: Fixed a bug that was causing some NFTs to flicker`
    
    (This helps the Release Engineer do their job more quickly and
    accurately)
    -->
    
    CHANGELOG entry:
    
    ## **Related issues**
    
    Fixes: https://consensyssoftware.atlassian.net/browse/TRAM-2541
    
    ## **Manual testing steps**
    
    1. Go to Deposit feature
    2. on iOS Apple Pay must be the default option
    3. on Android Debit Credit must be default option
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/UI/Ramp/Deposit/Views/BuildQuote/BuildQuote.tsx
+++ app/components/UI/Ramp/Deposit/Views/BuildQuote/BuildQuote.tsx
@@ -66,7 +66,7 @@ import {
   USD_CURRENCY,
   DepositFiatCurrency,
   EUR_CURRENCY,
-  DEBIT_CREDIT_PAYMENT_METHOD,
+  APPLE_PAY_PAYMENT_METHOD,
 } from '../../constants';
 import { useDepositRouting } from '../../hooks/useDepositRouting';
 import Logger from '../../../../../../util/Logger';
@@ -83,7 +83,7 @@ const BuildQuote = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const [paymentMethod, setPaymentMethod] = useState<DepositPaymentMethod>(
-    DEBIT_CREDIT_PAYMENT_METHOD,
+    APPLE_PAY_PAYMENT_METHOD,
   );
   const [cryptoCurrency, setCryptoCurrency] =
     useState<DepositCryptoCurrency>(USDC_TOKEN);
