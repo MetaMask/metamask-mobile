829a05ca4ac5161c666d4ed060a2238540a288d0
Cal-L <cleun007@gmail.com>
2025-07-22 18:01:39 -0700

    Add commands for test and e2e environments

--- scripts/build.sh
+++ scripts/build.sh
@@ -167,6 +167,23 @@ remapEnvVariableE2E() {
 	remapEnvVariable "MAIN_ANDROID_GOOGLE_SERVER_CLIENT_ID_UAT" "ANDROID_GOOGLE_SERVER_CLIENT_ID"
 }
 
+# Mapping for environmental values in the test environment. 
+# This is the same as the QA mapping since e2e used QA values before. Subject to change as build configs are updated.
+remapEnvVariableTest() {
+  	echo "Remapping E2E env variable names to match E2E values"
+  	remapEnvVariable "SEGMENT_WRITE_KEY_QA" "SEGMENT_WRITE_KEY"
+  	remapEnvVariable "SEGMENT_PROXY_URL_QA" "SEGMENT_PROXY_URL"
+  	remapEnvVariable "SEGMENT_DELETE_API_SOURCE_ID_QA" "SEGMENT_DELETE_API_SOURCE_ID"
+  	remapEnvVariable "SEGMENT_REGULATIONS_ENDPOINT_QA" "SEGMENT_REGULATIONS_ENDPOINT"
+  	remapEnvVariable "MM_SENTRY_DSN_TEST" "MM_SENTRY_DSN"
+
+	remapEnvVariable "MAIN_IOS_GOOGLE_CLIENT_ID_UAT" "IOS_GOOGLE_CLIENT_ID"
+	remapEnvVariable "MAIN_IOS_GOOGLE_REDIRECT_URI_UAT" "IOS_GOOGLE_REDIRECT_URI"
+	remapEnvVariable "MAIN_ANDROID_APPLE_CLIENT_ID_UAT" "ANDROID_APPLE_CLIENT_ID"
+	remapEnvVariable "MAIN_ANDROID_GOOGLE_CLIENT_ID_UAT" "ANDROID_GOOGLE_CLIENT_ID"
+	remapEnvVariable "MAIN_ANDROID_GOOGLE_SERVER_CLIENT_ID_UAT" "ANDROID_GOOGLE_SERVER_CLIENT_ID"
+}
+
 remapEnvVariableProduction() {
   	echo "Remapping release env variable names to match production values"
   	remapEnvVariable "SEGMENT_WRITE_KEY_PROD" "SEGMENT_WRITE_KEY"
@@ -746,6 +763,8 @@ if [ "$MODE" == "main" ]; then
 		remapEnvVariableReleaseCandidate
 	elif [ "$ENVIRONMENT" == "exp" ]; then
 		remapEnvVariableExperimental
+	elif [ "$ENVIRONMENT" == "test" ]; then
+		remapEnvVariableTest
 	elif [ "$ENVIRONMENT" == "e2e" ]; then
 		remapEnvVariableE2E
 	fi
