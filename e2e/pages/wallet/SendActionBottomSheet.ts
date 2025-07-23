0ea2f62bc9cf0c58b692c0329d90b2f8f021a4c2
Christopher Ferreira <christopher.ferreira@consensys.net>
2025-07-23 20:04:19 +0100

    feat: convert solana account tests to ts, remove flaky swap assertion

--- e2e/pages/wallet/SendActionBottomSheet.ts
+++ e2e/pages/wallet/SendActionBottomSheet.ts
@@ -1,57 +1,62 @@
 import { SendActionViewSelectorsIDs } from '../../selectors/SendFlow/SendActionView.selectors';
-import Gestures from '../../utils/Gestures';
-import Matchers from '../../utils/Matchers';
+import Gestures from '../../framework/Gestures';
+import Matchers from '../../framework/Matchers';
 
 class SendActionBottomSheet {
-  get solanaAddressInputField() {
+  get solanaAddressInputField(): DetoxElement {
     return Matchers.getElementByID(
       SendActionViewSelectorsIDs.SOLANA_INPUT_ADDRESS_FIELD,
     );
   }
 
-  get solanaAmountInputField() {
+  get solanaAmountInputField(): DetoxElement {
     return Matchers.getElementByID(
       SendActionViewSelectorsIDs.SOLANA_INPUT_AMOUNT_FIELD,
     );
   }
 
-  get invalidAddressError() {
+  get invalidAddressError(): DetoxElement {
     return Matchers.getElementByID(
       SendActionViewSelectorsIDs.INVALID_ADDRESS_ERROR,
     );
   }
 
-  get continueButton() {
+  get continueButton(): DetoxElement {
     return Matchers.getElementByID(SendActionViewSelectorsIDs.CONTINUE_BUTTON);
   }
 
-  get cancelButton() {
+  get cancelButton(): DetoxElement {
     return Matchers.getElementByID(SendActionViewSelectorsIDs.CANCEL_BUTTON);
   }
 
-  get closeModalButton() {
+  get closeModalButton(): DetoxElement {
     return Matchers.getElementByID(SendActionViewSelectorsIDs.CLOSE_BUTTON);
   }
 
-  get sendSOLTransactionButton() {
+  get sendSOLTransactionButton(): DetoxElement {
     return Matchers.getElementByID(
       SendActionViewSelectorsIDs.SEND_TRANSACTION_BUTTON,
     );
   }
 
-  async sendActionInputAddress(address) {
-    await Gestures.typeTextAndHideKeyboard(
-      this.solanaAddressInputField,
-      address,
-    );
+  async sendActionInputAddress(address: string) {
+    await Gestures.typeText(this.solanaAddressInputField, address, {
+      hideKeyboard: true,
+      elemDescription: 'Solana address input field',
+    });
   }
 
-  async sendActionInputAmount(amount) {
-    await Gestures.typeTextAndHideKeyboard(this.solanaAmountInputField, amount);
+  async sendActionInputAmount(amount: string) {
+    await Gestures.typeText(this.solanaAmountInputField, amount, {
+      hideKeyboard: true,
+    });
   }
 
   async tapSendSOLTransactionButton() {
-    await Gestures.waitAndTap(this.sendSOLTransactionButton);
+    await Gestures.waitAndTap(this.sendSOLTransactionButton, {
+      delay: 1500,
+      elemDescription: 'Send SOL transaction button',
+    });
   }
 
   async tapCancelButton() {
@@ -60,7 +65,7 @@ class SendActionBottomSheet {
 
   async tapContinueButton() {
     await Gestures.waitAndTap(this.continueButton, {
-      skipVisibilityCheck: true,
+      delay: 1000,
     });
   }
 
