27f6624e0407065d995cfe24cc04531348cdb201
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-07-23 14:22:57 +0200

    test(e2e): Add E2E test for `snap_manageState` (#17418)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    This adds an E2E test for `snap_manageState` and the newer `snap_*state`
    functions.
    
    ## **Changelog**
    
    <!--
    If this PR is not End-User-Facing and should not show up in the
    CHANGELOG, you can choose to either:
    1. Write `CHANGELOG entry: null`
    2. Label with `no-changelog`
    
    If this PR is End-User-Facing, please write a short User-Facing
    description in the past tense like:
    `CHANGELOG entry: Added a new tab for users to see their NFTs`
    `CHANGELOG entry: Fixed a bug that was causing some NFTs to flicker`
    
    (This helps the Release Engineer do their job more quickly and
    accurately)
    -->
    
    CHANGELOG entry:
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    1. Go to this page...
    2.
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

---
+++ e2e/specs/snaps/test-snap-manage-state.spec.ts
@@ -0,0 +1,139 @@
+import TestHelpers from '../../helpers';
+import { FlaskBuildTests } from '../../tags';
+import { loginToApp } from '../../viewHelper';
+import FixtureBuilder from '../../fixtures/fixture-builder';
+import {
+  loadFixture,
+  startFixtureServer,
+  stopFixtureServer,
+} from '../../fixtures/fixture-helper';
+import { getFixturesServerPort } from '../../fixtures/utils';
+import FixtureServer from '../../fixtures/fixture-server';
+import TabBarComponent from '../../pages/wallet/TabBarComponent';
+import TestSnaps from '../../pages/Browser/TestSnaps';
+
+const fixtureServer = new FixtureServer();
+
+describe(FlaskBuildTests('Manage State Snap Tests'), () => {
+  beforeAll(async () => {
+    await TestHelpers.reverseServerPort();
+    const fixture = new FixtureBuilder()
+      .withMultiSRPKeyringController()
+      .build();
+    await startFixtureServer(fixtureServer);
+    await loadFixture(fixtureServer, { fixture });
+    await TestHelpers.launchApp({
+      launchArgs: { fixtureServerPort: `${getFixturesServerPort()}` },
+    });
+    await loginToApp();
+
+    // Navigate to test snaps URL once for all tests
+    await TabBarComponent.tapBrowser();
+    await TestSnaps.navigateToTestSnap();
+  });
+
+  afterAll(async () => {
+    await stopFixtureServer(fixtureServer);
+  });
+
+  beforeEach(() => {
+    jest.setTimeout(150_000);
+  });
+
+  describe('new state functions', () => {
+    it('connects to the State Snap', async () => {
+      await TestSnaps.installSnap('connectStateButton');
+    });
+
+    describe('encrypted state', () => {
+      it('sets and retrieves state', async () => {
+        await TestSnaps.fillMessage('dataStateInput', '"bar"');
+        await TestSnaps.fillMessage('setStateKeyInput', 'foo');
+        await TestSnaps.tapButton('sendStateButton');
+        await TestSnaps.checkResultSpan(
+          'encryptedStateResultSpan',
+          JSON.stringify({ foo: 'bar' }, null, 2),
+        );
+
+        await TestSnaps.fillMessage('getStateInput', 'foo');
+        await TestSnaps.tapButton('sendGetStateButton');
+        await TestSnaps.checkResultSpan('getStateResultSpan', '"bar"');
+      });
+
+      it('clears the state', async () => {
+        await TestSnaps.tapButton('clearStateButton');
+        await TestSnaps.checkResultSpan('encryptedStateResultSpan', 'null');
+      });
+    });
+
+    describe('unencrypted state', () => {
+      it('sets and retrieves unencrypted state', async () => {
+        await TestSnaps.fillMessage('dataUnencryptedStateInput', '"bar"');
+        await TestSnaps.fillMessage('setStateKeyUnencryptedInput', 'foo');
+        await TestSnaps.tapButton('sendUnencryptedStateButton');
+        await TestSnaps.checkResultSpan(
+          'unencryptedStateResultSpan',
+          JSON.stringify({ foo: 'bar' }, null, 2),
+        );
+
+        await TestSnaps.fillMessage('getUnencryptedStateInput', 'foo');
+        await TestSnaps.tapButton('sendGetUnencryptedStateButton');
+        await TestSnaps.checkResultSpan(
+          'getStateUnencryptedResultSpan',
+          '"bar"',
+        );
+      });
+
+      it('clears the unencrypted state', async () => {
+        await TestSnaps.tapButton('clearStateUnencryptedButton');
+        await TestSnaps.checkResultSpan('unencryptedStateResultSpan', 'null');
+      });
+    });
+  });
+
+  describe('legacy state functions', () => {
+    describe('encrypted state', () => {
+      it('sets and retrieves state', async () => {
+        await TestSnaps.fillMessage('dataManageStateInput', '23');
+        await TestSnaps.tapButton('sendManageStateButton');
+        await TestSnaps.checkResultSpan('sendManageStateResultSpan', 'true');
+        await TestSnaps.checkResultSpan(
+          'retrieveManageStateResultSpan',
+          JSON.stringify({ items: ['23'] }, null, 2),
+        );
+      });
+
+      it('clears the state', async () => {
+        await TestSnaps.tapButton('clearManageStateButton');
+        await TestSnaps.checkResultSpan('clearManageStateResultSpan', 'true');
+        await TestSnaps.checkResultSpan(
+          'retrieveManageStateResultSpan',
+          JSON.stringify({ items: [] }, null, 2),
+        );
+      });
+    });
+
+    describe('unencrypted state', () => {
+      it('sets and retrieves unencrypted state', async () => {
+        await TestSnaps.fillMessage('dataUnencryptedManageStateInput', '23');
+        await TestSnaps.tapButton('sendUnencryptedManageStateButton');
+        await TestSnaps.checkResultSpan(
+          'retrieveManageStateUnencryptedResultSpan',
+          JSON.stringify({ items: ['23'] }, null, 2),
+        );
+      });
+
+      it('clears the unencrypted state', async () => {
+        await TestSnaps.tapButton('clearUnencryptedManageStateButton');
+        await TestSnaps.checkResultSpan(
+          'clearUnencryptedManageStateResultSpan',
+          'true',
+        );
+        await TestSnaps.checkResultSpan(
+          'retrieveManageStateUnencryptedResultSpan',
+          JSON.stringify({ items: [] }, null, 2),
+        );
+      });
+    });
+  });
+});
