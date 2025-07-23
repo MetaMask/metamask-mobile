1fd7a9798be6449970cf62b8b54a89a147ba3809
Jyoti Puri <jyotipuri@gmail.com>
2025-07-23 18:36:05 +0530

    update

--- app/components/Views/confirmations/components/send/send.test.tsx
+++ app/components/Views/confirmations/components/send/send.test.tsx
@@ -8,6 +8,7 @@ import { backgroundState } from '../../../../../util/test/initial-root-state';
 import * as TransactionUtils from '../../../../../util/transaction-controller';
 import { SendContextProvider } from '../../context/send-context';
 import Send from './send';
+import { ParamListBase, RouteProp, useRoute } from '@react-navigation/native';
 
 const mockGoBack = jest.fn();
 const mockNavigate = jest.fn();
@@ -82,7 +83,7 @@ describe('Send', () => {
     expect(mockGoBack).toHaveBeenCalledTimes(1);
   });
 
-  it('when confirm is clicked create transaction', async () => {
+  it('when confirm is clicked create transaction for ERC20 token', async () => {
     const mockAddTransaction = jest
       .spyOn(TransactionUtils, 'addTransaction')
       .mockImplementation(() =>
@@ -94,6 +95,59 @@ describe('Send', () => {
     const { getByText, getByTestId } = renderComponent();
     fireEvent.changeText(getByTestId('send_to_address'), '0x123');
     fireEvent.changeText(getByTestId('send_amount'), '1');
+    fireEvent.press(getByText('Confirm'));
+    expect(mockAddTransaction).toHaveBeenCalledTimes(1);
+  });
+
+  it('when confirm is clicked create transaction for native token', async () => {
+    (useRoute as jest.MockedFn<typeof useRoute>).mockReturnValue({
+      params: {
+        asset: {
+          name: 'Ethereum',
+          address: '0x123',
+          isNative: true,
+          chainId: '0x1',
+        },
+      },
+    } as RouteProp<ParamListBase, string>);
+
+    const mockAddTransaction = jest
+      .spyOn(TransactionUtils, 'addTransaction')
+      .mockImplementation(() =>
+        Promise.resolve({
+          result: Promise.resolve('123'),
+          transactionMeta: { id: '123' } as TransactionMeta,
+        }),
+      );
+    const { getByText, getByTestId } = renderComponent();
+    fireEvent.changeText(getByTestId('send_to_address'), '0x123');
+    fireEvent.changeText(getByTestId('send_amount'), '1');
+    fireEvent.press(getByText('Confirm'));
+    expect(mockAddTransaction).toHaveBeenCalledTimes(1);
+  });
+
+  it('when confirm is clicked create transaction for NFT', async () => {
+    (useRoute as jest.MockedFn<typeof useRoute>).mockReturnValue({
+      params: {
+        asset: {
+          name: 'MyNFT',
+          address: '0x123',
+          chainId: '0x1',
+          tokenId: '0x1',
+        },
+      },
+    } as RouteProp<ParamListBase, string>);
+
+    const mockAddTransaction = jest
+      .spyOn(TransactionUtils, 'addTransaction')
+      .mockImplementation(() =>
+        Promise.resolve({
+          result: Promise.resolve('123'),
+          transactionMeta: { id: '123' } as TransactionMeta,
+        }),
+      );
+    const { getByText, getByTestId } = renderComponent();
+    fireEvent.changeText(getByTestId('send_to_address'), '0x123');
     fireEvent.press(getByText('Confirm'));
     expect(mockAddTransaction).toHaveBeenCalledTimes(1);
   });
