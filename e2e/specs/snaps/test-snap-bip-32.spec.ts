f22e59f3e6ecea5cffc3d6ce87fa96d11b2afbda
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-07-23 14:25:41 +0200

    Disable broken Snaps E2E tests

--- e2e/specs/snaps/test-snap-bip-32.spec.ts
+++ e2e/specs/snaps/test-snap-bip-32.spec.ts
@@ -110,7 +110,9 @@ describe(FlaskBuildTests('BIP-32 Snap Tests'), () => {
     );
   });
 
-  it('fails when choosing the invalid entropy source', async () => {
+  // This test is skipped because of a bug causing the app to crash when the
+  // alert is displayed.
+  it.skip('fails when choosing the invalid entropy source', async () => {
     await TestSnaps.selectInDropdown('bip32EntropyDropDown', 'Invalid');
     await TestSnaps.fillMessage('messageSecp256k1Input', 'bar baz');
     await TestSnaps.tapButton('signMessageBip32Secp256k1Button');
