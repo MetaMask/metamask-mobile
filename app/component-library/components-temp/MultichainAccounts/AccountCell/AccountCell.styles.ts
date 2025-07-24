99a3aa1d4cc06d49d88bbdd2fb606c9affc364ef
Monte Lai <monte.lai@consensys.net>
2025-07-24 14:02:59 +0800

    fix: move to temp

--- app/component-library/components-temp/MultichainAccounts/AccountCell/AccountCell.styles.ts
+++ app/component-library/components-temp/MultichainAccounts/AccountCell/AccountCell.styles.ts
@@ -16,27 +16,40 @@ const styleSheet = (params: {
       paddingRight: 8,
       paddingTop: 16,
       paddingBottom: 16,
-      borderLeftWidth: isSelected ? 4 : 0,
-      borderLeftColor: isSelected
-        ? colors.info.default
-        : colors.background.default,
-      backgroundColor: isSelected
-        ? colors.info.muted
-        : colors.background.default,
     },
     avatar: {
       borderRadius: 8,
       width: 32,
       height: 32,
       backgroundColor: colors.background.muted,
+      borderWidth: isSelected ? 2 : 0,
+      borderColor: isSelected ? colors.info.default : colors.background.default,
     },
     accountName: {
       display: 'flex',
       flexDirection: 'row',
       justifyContent: 'center',
+      alignItems: 'center',
+    },
+    endContainer: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      gap: 8,
     },
     balance: {
       verticalAlign: 'middle',
+    },
+    menuButton: {
+      backgroundColor: colors.background.muted,
+      borderRadius: 8,
+      height: 28,
+      width: 28,
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
     },
   });
 };
