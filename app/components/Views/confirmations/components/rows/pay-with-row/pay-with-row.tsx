b02051cdcea886361a6b5b26652b93bacd8fdb5a
Matthew Walsh <matthew.walsh@consensys.net>
2025-07-24 23:21:40 +0100

    Move hook

--- app/components/Views/confirmations/components/rows/pay-with-row/pay-with-row.tsx
+++ app/components/Views/confirmations/components/rows/pay-with-row/pay-with-row.tsx
@@ -1,27 +1,36 @@
 import React, { useCallback } from 'react';
 import InfoRow from '../../UI/info-row';
 import InfoSection from '../../UI/info-row/info-section';
-import { Box } from '@metamask/design-system-react-native';
 import { useNavigation } from '@react-navigation/native';
 import Routes from '../../../../../../constants/navigation/Routes';
 import { TokenPill } from '../../token-pill/token-pill';
 import { useTransactionPayToken } from '../../../hooks/pay/useTransactionPayToken';
+import { useTransactionBridgeQuotes } from '../../../hooks/pay/useTransactionBridgeQuotes';
+import Icon, {
+  IconName,
+} from '../../../../../../component-library/components/Icons/Icon';
+import { Box } from '../../../../../UI/Box/Box';
+import { AlignItems, FlexDirection } from '../../../../../UI/Box/box.types';
 
 export function PayWithRow() {
   const navigation = useNavigation();
   const { payToken } = useTransactionPayToken();
+  const { quotes } = useTransactionBridgeQuotes();
 
   const handleClick = useCallback(() => {
-    navigation.navigate(Routes.CONFIRMATION_PAY_WITH_MODAL, {
-      minimumFiatBalance: 2,
-    });
+    navigation.navigate(Routes.CONFIRMATION_PAY_WITH_MODAL);
   }, [navigation]);
 
   return (
     <>
       <InfoSection>
-        <InfoRow label="Pay with" tooltip="Test 1 2 3">
-          <Box onTouchEnd={handleClick}>
+        <InfoRow label="Pay with">
+          <Box
+            onTouchEnd={handleClick}
+            flexDirection={FlexDirection.Row}
+            alignItems={AlignItems.center}
+          >
+            {quotes?.length && <Icon name={IconName.CheckBold} />}
             <TokenPill address={payToken.address} chainId={payToken.chainId} />
           </Box>
         </InfoRow>
