0b636a8cdebbca956eb02f3585296268e29801e1
IF <139582705+infiniteflower@users.noreply.github.com>
2025-07-24 20:13:07 -0400

    chore: add tests

--- app/components/UI/Bridge/hooks/useIsOnBridgeRoute/index.ts
+++ app/components/UI/Bridge/hooks/useIsOnBridgeRoute/index.ts
@@ -2,6 +2,6 @@ import { useNavigationState } from '@react-navigation/native';
 import Routes from '../../../../../constants/navigation/Routes';
 
 export const useIsOnBridgeRoute = () => {
-  const routes = useNavigationState((state) => state.routes[0].state?.routes);
-  return routes?.some((route) => route?.name === Routes.BRIDGE.ROOT);
+  const routes = useNavigationState((state) => state?.routes[0]?.state?.routes);
+  return routes ? routes.some((route) => route?.name === Routes.BRIDGE.ROOT) : false;
 };
