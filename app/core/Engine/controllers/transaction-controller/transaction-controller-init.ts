4821aa1674401305e46de6aefa4e8e9e4409ec58
Matthew Walsh <matthew.walsh@consensys.net>
2025-07-25 12:50:32 +0100

    Add initial hook

--- app/core/Engine/controllers/transaction-controller/transaction-controller-init.ts
+++ app/core/Engine/controllers/transaction-controller/transaction-controller-init.ts
@@ -39,6 +39,7 @@ import {
   handleTransactionFinalizedEventForMetrics,
 } from './event-handlers/metrics';
 import { handleShowNotification } from './event-handlers/notification';
+import { PayHook } from '../../../../util/transactions/hooks/pay-hook';
 
 export const TransactionControllerInit: ControllerInitFunction<
   TransactionController,
@@ -152,8 +153,13 @@ function publishHook({
   signedTransactionInHex: Hex;
 }): Promise<{ transactionHash: string }> {
   const state = getState();
+
   const { shouldUseSmartTransaction, featureFlags } =
     getSmartTransactionCommonParams(state, transactionMeta.chainId);
+
+  new PayHook({
+    messenger: initMessenger,
+  }).getHook()(transactionMeta, signedTransactionInHex);
 
   // @ts-expect-error - TransactionController expects transactionHash to be defined but submitSmartTransactionHook could return undefined
   return submitSmartTransactionHook({
