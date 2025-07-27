9e1a8d787f59975c5616911dfc56b7007017de68
Jyoti Puri <jyotipuri@gmail.com>
2025-07-27 13:57:00 +0530

    update

--- app/components/Views/confirmations/utils/send.ts
+++ app/components/Views/confirmations/utils/send.ts
@@ -3,26 +3,14 @@ import { Nft } from '@metamask/assets-controllers';
 import Routes from '../../../../constants/navigation/Routes';
 import { AssetType } from '../types/token';
 
-interface NavigateType {
-  <RouteName extends string>(
-    ...args: [RouteName] | [RouteName, object | undefined]
-  ): void;
-  <RouteName extends string>(
-    route:
-      | { key: string; params?: object | undefined }
-      | {
-          name: RouteName;
-          key?: string | undefined;
-          params: object | undefined;
-        },
-  ): void;
-}
-
 export const isSendRedesignEnabled = () =>
   process.env.MM_SEND_REDESIGN_ENABLED === 'true';
 
 export const handleSendPageNavigation = (
-  navigate: NavigateType,
+  navigate: <RouteName extends string>(
+    screenName: RouteName,
+    params?: object,
+  ) => void,
   asset: AssetType | Nft,
 ) => {
   if (isSendRedesignEnabled()) {
