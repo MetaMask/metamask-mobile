8444ad750171384b55b58c3ae86a116b4d502a7e
ieow <cwlee0@gmail.com>
2025-07-29 17:26:56 +0800

    fix: testcase

--- app/core/Vault.test.ts
+++ app/core/Vault.test.ts
@@ -1,19 +1,7 @@
-import {
-  EthAccountType,
-  SolAccountType,
-  SolScope,
-} from '@metamask/keyring-api';
-import Logger from '../util/Logger';
+import { EthAccountType, SolAccountType } from '@metamask/keyring-api';
 import Engine from './Engine';
-import { withLedgerKeyring } from './Ledger/Ledger';
 
-import {
-  restoreLedgerKeyring,
-  restoreQRKeyring,
-  restoreSnapAccounts,
-  restoreImportedSrp,
-  recreateVaultWithNewPassword,
-} from './Vault';
+import { recreateVaultWithNewPassword } from './Vault';
 import { KeyringSelector, KeyringTypes } from '@metamask/keyring-controller';
 import {
   createMockInternalAccount,
@@ -29,8 +17,6 @@ const mockExportSeedPhrase = jest.fn();
 const mockListMultichainAccounts = jest.fn();
 const mockCreateNewVaultAndRestore = jest.fn();
 const mockAddNewAccount = jest.fn();
-const mockRestoreQRKeyring = jest.fn();
-const mockRestoreLedgerKeyring = jest.fn();
 const mockExportAccount = jest.fn();
 const mockImportAccountWithStrategy = jest.fn();
 
@@ -170,10 +156,6 @@ jest.mock('./Engine', () => ({
       createNewVaultAndRestore: (password: string, seedPhrase: string) =>
         mockCreateNewVaultAndRestore(password, seedPhrase),
       addNewAccount: () => mockAddNewAccount(),
-      restoreQRKeyring: (serializedKeyring: string) =>
-        mockRestoreQRKeyring(serializedKeyring),
-      restoreLedgerKeyring: (serializedKeyring: string) =>
-        mockRestoreLedgerKeyring(serializedKeyring),
       exportAccount: (password: string, account: string) =>
         mockExportAccount(password, account),
       importAccountWithStrategy: (strategy: string, accounts: string[]) =>
@@ -210,7 +192,6 @@ const mockEngine = jest.mocked(Engine);
 jest.mock('./Ledger/Ledger', () => ({
   withLedgerKeyring: jest.fn(),
 }));
-const mockWithLedgerKeyring = jest.mocked(withLedgerKeyring);
 
 jest.mock('../util/Logger', () => ({
   error: jest.fn(),
@@ -238,136 +219,6 @@ jest.mock('./SnapKeyring/MultichainWalletSnapClient', () => ({
 describe('Vault', () => {
   beforeEach(() => {
     jest.clearAllMocks();
-  });
-  describe('restoreQRKeyring', () => {
-    it('should restore QR keyring if it exists', async () => {
-      const mockSerializedQrKeyring = 'serialized-keyring';
-
-      await restoreQRKeyring(mockSerializedQrKeyring);
-
-      expect(mockRestoreQRKeyring).toHaveBeenCalled();
-      expect(mockRestoreQRKeyring).toHaveBeenCalledWith(
-        mockSerializedQrKeyring,
-      );
-    });
-
-    it('should log error if an exception is thrown', async () => {
-      const error = new Error('Test error');
-      mockRestoreQRKeyring.mockRejectedValue(error);
-      const mockSerializedQrKeyring = 'serialized-keyring';
-
-      await restoreQRKeyring(mockSerializedQrKeyring);
-
-      expect(Logger.error).toHaveBeenCalledWith(
-        error,
-        'error while trying to get qr accounts on recreate vault',
-      );
-    });
-  });
-
-  describe('restoreLedgerKeyring', () => {
-    it('should restore ledger keyring if it exists', async () => {
-      const mockLedgerKeyringInstance = {
-        deserialize: jest.fn(),
-      };
-      mockWithLedgerKeyring.mockImplementation(
-        // @ts-expect-error The Ledger keyring is not compatible with our keyring type yet
-        (operation) => operation(mockLedgerKeyringInstance),
-      );
-      const mockSerializedLedgerKeyring = 'serialized-keyring';
-
-      await restoreLedgerKeyring(mockSerializedLedgerKeyring);
-
-      expect(mockLedgerKeyringInstance.deserialize).toHaveBeenCalledWith(
-        mockSerializedLedgerKeyring,
-      );
-    });
-
-    it('should log error if the Ledger keyring throws an error', async () => {
-      const error = new Error('Test error');
-      const mockLedgerKeyringInstance = {
-        deserialize: jest.fn().mockRejectedValue(error),
-      };
-      mockWithLedgerKeyring.mockImplementation(
-        // @ts-expect-error The Ledger keyring is not compatible with our keyring type yet
-        (operation) => operation(mockLedgerKeyringInstance),
-      );
-      const mockSerializedLedgerKeyring = 'serialized-keyring';
-
-      await restoreLedgerKeyring(mockSerializedLedgerKeyring);
-
-      expect(Logger.error).toHaveBeenCalledWith(
-        error,
-        'error while trying to restore Ledger accounts on recreate vault',
-      );
-    });
-
-    it('should log error if the KeyringController throws an error', async () => {
-      const error = new Error('Test error');
-      mockWithLedgerKeyring.mockRejectedValue(error);
-      const mockSerializedLedgerKeyring = 'serialized-keyring';
-
-      await restoreLedgerKeyring(mockSerializedLedgerKeyring);
-
-      expect(Logger.error).toHaveBeenCalledWith(
-        error,
-        'error while trying to restore Ledger accounts on recreate vault',
-      );
-    });
-  });
-
-  describe('restoreSnapAccounts', () => {
-    it('creates a snap account if it exists', async () => {
-      await restoreSnapAccounts(SolAccountType.DataAccount, 'entropy-source');
-
-      expect(mockMultichainWalletSnapClient.createAccount).toHaveBeenCalledWith(
-        {
-          entropySource: 'entropy-source',
-          scope: SolScope.Mainnet,
-        },
-      );
-    });
-
-    it('logs error if snap account creation fails', async () => {
-      const error = new Error('Test error');
-      mockMultichainWalletSnapClient.createAccount.mockRejectedValue(error);
-
-      await restoreSnapAccounts(SolAccountType.DataAccount, 'entropy-source');
-
-      expect(Logger.error).toHaveBeenCalledWith(
-        error,
-        'error while trying to restore snap accounts on recreate vault',
-      );
-    });
-  });
-  describe('restoreImportedSrp', () => {
-    it('restore imported srp accounts if they exist', async () => {
-      mockAddNewKeyring.mockResolvedValue({ id: 'keyring-id' });
-      mockWithKeyring.mockResolvedValue(null);
-      await restoreImportedSrp('seed-phrase', 5);
-
-      expect(mockAddNewKeyring).toHaveBeenCalledWith(KeyringTypes.hd, {
-        mnemonic: 'seed-phrase',
-      });
-
-      expect(mockWithKeyring).toHaveBeenCalledTimes(5);
-      expect(mockWithKeyring).toHaveBeenCalledWith(
-        { id: 'keyring-id' },
-        expect.any(Function),
-      );
-    });
-
-    it('logs error if srp account creation fails', async () => {
-      const error = new Error('Test error');
-      mockAddNewKeyring.mockRejectedValue(error);
-
-      await restoreImportedSrp('seed-phrase', 5);
-
-      expect(Logger.error).toHaveBeenCalledWith(
-        error,
-        'error while trying to restore imported srp accounts on recreate vault',
-      );
-    });
   });
 
   describe('recreateVaultWithNewPassword', () => {
