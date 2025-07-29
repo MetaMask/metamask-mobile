77e8b2b841d448302552af5553c60d3eacc27ddd
Guillaume Roux <guillaumeroux123@gmail.com>
2025-07-29 12:40:54 +0200

    add JSON-PRC E2E test

---
+++ e2e/specs/snaps/test-snap-rpc.spec.ts
@@ -0,0 +1,53 @@
+import FixtureBuilder from '../../fixtures/fixture-builder';
+import {
+  loadFixture,
+  startFixtureServer,
+  stopFixtureServer,
+} from '../../fixtures/fixture-helper';
+import FixtureServer from '../../fixtures/fixture-server';
+import { getFixturesServerPort } from '../../fixtures/utils';
+import TestHelpers from '../../helpers';
+import TestSnaps from '../../pages/Browser/TestSnaps';
+import TabBarComponent from '../../pages/wallet/TabBarComponent';
+import { FlaskBuildTests } from '../../tags';
+import { loginToApp } from '../../viewHelper';
+
+const fixtureServer = new FixtureServer();
+
+describe(FlaskBuildTests('Snap RPC Tests'), () => {
+  beforeAll(async () => {
+    await TestHelpers.reverseServerPort();
+    const fixture = new FixtureBuilder()
+      .withMultiSRPKeyringController()
+      .build();
+    await startFixtureServer(fixtureServer);
+    await loadFixture(fixtureServer, { fixture });
+    await TestHelpers.launchApp({
+      launchArgs: { fixtureServerPort: `${getFixturesServerPort()}` },
+    });
+    await loginToApp();
+
+    // Navigate to test snaps URL once for all tests
+    await TabBarComponent.tapBrowser();
+    await TestSnaps.navigateToTestSnap();
+  });
+
+  afterAll(async () => {
+    await stopFixtureServer(fixtureServer);
+  });
+
+  beforeEach(() => {
+    jest.setTimeout(150_000);
+  });
+
+  it('can use the cross-snap RPC endowment and produce a public key', async () => {
+    await TestSnaps.installSnap('connectBip32Button');
+    await TestSnaps.installSnap('connectJsonRpcButton');
+
+    await TestSnaps.tapButton('sendRpcButton');
+    await TestSnaps.checkResultSpan(
+      'rpcResultSpan',
+      '"0x033e98d696ae15caef75fa8dd204a7c5c08d1272b2218ba3c20feeb4c691eec366"',
+    );
+  });
+});
