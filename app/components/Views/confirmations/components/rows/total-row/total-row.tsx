facfde5473bbf8364321c77eda3c9505e2b0a7da
Matthew Walsh <matthew.walsh@consensys.net>
2025-07-30 11:05:00 +0100

    Add decimals to useTransactionPayToken
    Add quotes to context.
    Add spinners.
    Fix navigation bar.

--- app/components/Views/confirmations/components/rows/total-row/total-row.tsx
+++ app/components/Views/confirmations/components/rows/total-row/total-row.tsx
@@ -5,17 +5,26 @@ import Text from '../../../../../../component-library/components/Texts/Text';
 import { useTransactionRequiredFiat } from '../../../hooks/pay/useTransactionRequiredFiat';
 import useFiatFormatter from '../../../../../UI/SimulationDetails/FiatDisplay/useFiatFormatter';
 import { BigNumber } from 'bignumber.js';
+import { useConfirmationContext } from '../../../context/confirmation-context';
+import AnimatedSpinner, {
+  SpinnerSize,
+} from '../../../../../UI/AnimatedSpinner';
 
 export function TotalRow() {
   const formatFiat = useFiatFormatter();
   const { totalWithBalanceFiat } = useTransactionRequiredFiat();
+  const { quotesLoading } = useConfirmationContext();
 
   const formattedTotal = formatFiat(new BigNumber(totalWithBalanceFiat));
 
   return (
     <InfoSection>
       <InfoRow label="Total">
-        <Text>{formattedTotal}</Text>
+        {quotesLoading ? (
+          <AnimatedSpinner size={SpinnerSize.SM} />
+        ) : (
+          <Text>{formattedTotal}</Text>
+        )}
       </InfoRow>
     </InfoSection>
   );
