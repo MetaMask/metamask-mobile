13c6ff88aa8780662d7b0c40f2b4b578f269d47d
IF <139582705+infiniteflower@users.noreply.github.com>
2025-08-01 14:55:27 -0400

    fix: swap on solana appearing as bridge

--- app/components/UI/MultichainBridgeTransactionListItem/MultichainBridgeTransactionListItem.tsx
+++ app/components/UI/MultichainBridgeTransactionListItem/MultichainBridgeTransactionListItem.tsx
@@ -19,7 +19,6 @@ import Routes from '../../../constants/navigation/Routes';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../reducers';
 import { getSwapBridgeTxActivityTitle } from '../Bridge/utils/transaction-history';
-import { strings } from '../../../../locales/i18n';
 
 const MultichainBridgeTransactionListItem = ({
   transaction,
@@ -35,13 +34,11 @@ const MultichainBridgeTransactionListItem = ({
   const { colors, typography } = useTheme();
   const osColorScheme = useColorScheme();
   const appTheme = useSelector((state: RootState) => state.user.appTheme);
-
-  const isBridgeComplete = Boolean(
-    bridgeHistoryItem?.status.srcChain.txHash &&
-      bridgeHistoryItem.status.destChain?.txHash,
-  );
-
   const style = styles(colors, typography);
+
+  const isSwap =
+    bridgeHistoryItem.quote.srcAsset.chainId ===
+    bridgeHistoryItem.quote.destAsset.chainId;
 
   const handlePress = () => {
     navigation.navigate(Routes.BRIDGE.BRIDGE_TRANSACTION_DETAILS, {
@@ -52,13 +49,18 @@ const MultichainBridgeTransactionListItem = ({
   const renderTxElementIcon = () => {
     const isFailedTransaction = transaction.status === 'failed';
     const icon = getTransactionIcon(
-      'bridge',
+      isSwap ? 'swap' : 'bridge',
       isFailedTransaction,
       appTheme,
       osColorScheme,
     );
     return <Image source={icon} style={style.icon} resizeMode="stretch" />;
   };
+
+  const isBridgeComplete = Boolean(
+    bridgeHistoryItem?.status.srcChain.txHash &&
+      bridgeHistoryItem.status.destChain?.txHash,
+  );
 
   return (
     <>
@@ -84,16 +86,15 @@ const MultichainBridgeTransactionListItem = ({
                 numberOfLines={1}
                 style={style.listItemTitle as TextStyle}
               >
-                {getSwapBridgeTxActivityTitle(bridgeHistoryItem) ??
-                  strings('bridge.title')}
+                {getSwapBridgeTxActivityTitle(bridgeHistoryItem)}
               </ListItem.Title>
-              {!isBridgeComplete && (
+              {!isBridgeComplete && !isSwap && (
                 <BridgeActivityItemTxSegments
                   bridgeTxHistoryItem={bridgeHistoryItem}
                   transactionStatus={transaction.status}
                 />
               )}
-              {isBridgeComplete && (
+              {(isBridgeComplete || isSwap) && (
                 <StatusText
                   testID={`transaction-status-${transaction.id}`}
                   status={transaction.status}
