bd317b1c0ec44c175130d4722859cef36a4e66c9
IF <139582705+infiniteflower@users.noreply.github.com>
2025-08-01 17:52:57 -0400

    chore: handle default slippage for solana, evm swaps, evm stablecoin swaps, and bridge

--- app/components/UI/Bridge/hooks/useInitialSlippage/index.ts
+++ app/components/UI/Bridge/hooks/useInitialSlippage/index.ts
@@ -1,18 +1,66 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
+  selectDestToken,
+  selectIsBridge,
+  selectIsEvmSwap,
   selectIsSolanaSwap,
+  selectSourceToken,
   setSlippage,
 } from '../../../../../core/redux/slices/bridge';
+import { handleEvmStablecoinSlippage } from '../../../Swaps/useStablecoinsDefaultSlippage';
+import { isHex } from 'viem';
+import usePrevious from '../../../../hooks/usePrevious';
 
 export const useInitialSlippage = () => {
   const dispatch = useDispatch();
   const isSolanaSwap = useSelector(selectIsSolanaSwap);
+  const isBridge = useSelector(selectIsBridge);
+  const isEvmSwap = useSelector(selectIsEvmSwap);
+  const sourceToken = useSelector(selectSourceToken);
+  const destToken = useSelector(selectDestToken);
 
-  // Set slippage to undefined for Solana swaps
+  const prevSourceTokenAddress = usePrevious(sourceToken?.address);
+  const prevDestTokenAddress = usePrevious(destToken?.address);
+
   useEffect(() => {
+    // Solana Swaps
     if (isSolanaSwap) {
-      dispatch(setSlippage(undefined));
+      dispatch(setSlippage('0.5'));
+      return;
     }
-  }, [isSolanaSwap, dispatch]);
+    // EVM Swaps
+    if (
+      isEvmSwap &&
+      isHex(sourceToken?.chainId) &&
+      sourceToken?.address &&
+      destToken?.address
+    ) {
+      handleEvmStablecoinSlippage({
+        sourceTokenAddress: sourceToken?.address,
+        destTokenAddress: destToken?.address,
+        chainId: sourceToken?.chainId,
+        setSlippage: (slippage: number) =>
+          dispatch(setSlippage(slippage.toString())),
+        prevSourceTokenAddress,
+        prevDestTokenAddress,
+      });
+    }
+
+    // Bridge
+    if (isBridge) {
+      dispatch(setSlippage('0.5'));
+      return;
+    }
+  }, [
+    isSolanaSwap,
+    isBridge,
+    dispatch,
+    sourceToken?.address,
+    destToken?.address,
+    sourceToken?.chainId,
+    isEvmSwap,
+    prevSourceTokenAddress,
+    prevDestTokenAddress,
+  ]);
 };
