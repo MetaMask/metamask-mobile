b83de27c23e8619496424b0285d5cb101704bcf3
Curtis David <Curtis.David7@gmail.com>
2025-08-04 12:00:33 -0400

    Revert "add check to see if tests are running against browserstack, do not use dynamic ports"
    This reverts commit 227d0625b88de01e1111404c8e4f49b43d8d3b44.

--- e2e/framework/fixtures/FixtureUtils.ts
+++ e2e/framework/fixtures/FixtureUtils.ts
@@ -21,10 +21,6 @@ function transformToValidPort(defaultPort: number, pid: number) {
 
 function getServerPort(defaultPort: number) {
   if (process.env.CI) {
-    if (process.env.BROWSERSTACK_LOCAL) {
-      // if running on browserstack, do not use dynamic ports
-      return defaultPort;
-    }
     return transformToValidPort(defaultPort, process.pid);
   }
   return defaultPort;
