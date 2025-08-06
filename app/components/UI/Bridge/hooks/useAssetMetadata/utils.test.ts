f3df9d3faa635cea866b22b51288ca7e3ae66888
IF <139582705+infiniteflower@users.noreply.github.com>
2025-08-06 18:48:14 -0400

    fix: tests

--- app/components/UI/Bridge/hooks/useAssetMetadata/utils.test.ts
+++ app/components/UI/Bridge/hooks/useAssetMetadata/utils.test.ts
@@ -150,6 +150,7 @@ describe('asset-utils', () => {
       expect(result).toStrictEqual({
         symbol: 'SOL',
         decimals: 9,
+        name: 'Solana Token',
         image:
           'https://static.cx.metamask.io/api/v2/tokenIcons/assets/solana/5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp/token/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v.png',
         assetId: solanaAssetId,
@@ -179,8 +180,9 @@ describe('asset-utils', () => {
       expect(result).toStrictEqual({
         symbol: 'TEST',
         decimals: 18,
+        name: 'Test Token',
         image:
-          'https://static.cx.metamask.io/api/v2/tokenIcons/assets/eip155/1/erc20/0x123.png',
+          'https://static.cx.metamask.io/api/v2/tokenIcons/assets/eip155/1/erc20/0xbd3Afb0bB76683eCb4225F9DBc91f998713C3b01.png',
         assetId: mockAssetId,
         address: mockAddress,
         chainId: mockHexChainId,
@@ -208,8 +210,9 @@ describe('asset-utils', () => {
       expect(result).toStrictEqual({
         symbol: 'TEST',
         decimals: 18,
+        name: 'Test Token',
         image:
-          'https://static.cx.metamask.io/api/v2/tokenIcons/assets/eip155/1/erc20/0x123.png',
+          'https://static.cx.metamask.io/api/v2/tokenIcons/assets/eip155/1/erc20/0xbd3Afb0bB76683eCb4225F9DBc91f998713C3b01.png',
         assetId: mockAssetId,
         address: mockAddress,
         chainId: mockHexChainId,
@@ -251,8 +254,9 @@ describe('asset-utils', () => {
       expect(result).toStrictEqual({
         symbol: 'TEST',
         decimals: 18,
+        name: 'Test Token',
         image:
-          'https://static.cx.metamask.io/api/v2/tokenIcons/assets/eip155/1/erc20/0x123.png',
+          'https://static.cx.metamask.io/api/v2/tokenIcons/assets/eip155/1/erc20/0xbd3Afb0bB76683eCb4225F9DBc91f998713C3b01.png',
         assetId: mockAssetId,
         address: mockAddress,
         chainId: mockHexChainId,
