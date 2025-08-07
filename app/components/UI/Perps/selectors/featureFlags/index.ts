d0b3d793e18c703b2091015be9fdad95509fe614
Nicholas Gambino <nicholas.gambino@consensys.net>
2025-08-07 08:10:52 -0700

    chore: Revert ff

--- app/components/UI/Perps/selectors/featureFlags/index.ts
+++ app/components/UI/Perps/selectors/featureFlags/index.ts
@@ -1,38 +1,28 @@
 import { createSelector } from 'reselect';
 import { selectRemoteFeatureFlags } from '../../../../../selectors/featureFlagController';
-// import { hasMinimumRequiredVersion } from '../../../../../util/remoteFeatureFlag';
-// import { PerpsLaunchDarklyFlag } from '../../types';
+import { hasMinimumRequiredVersion } from '../../../../../util/remoteFeatureFlag';
+import { PerpsLaunchDarklyFlag } from '../../types';
 
-// const perpsRemoteFeatureFlag = (remoteFlag: PerpsLaunchDarklyFlag) =>
-//   Boolean(remoteFlag?.enabled) &&
-//   hasMinimumRequiredVersion(remoteFlag?.minimumVersion);
-
-// export const selectPerpsEnabledFlag = createSelector(
-//   selectRemoteFeatureFlags,
-//   (remoteFeatureFlags) => {
-//     const remoteFlag =
-//       remoteFeatureFlags?.perpsPerpTradingEnabled as unknown as PerpsLaunchDarklyFlag;
-
-//     return perpsRemoteFeatureFlag(remoteFlag);
-//   },
-// );
-
-// export const selectPerpsServiceInterruptionBannerEnabledFlag = createSelector(
-//   selectRemoteFeatureFlags,
-//   (remoteFeatureFlags) => {
-//     const remoteFlag =
-//       remoteFeatureFlags?.perpsPerpTradingServiceInterruptionBannerEnabled as unknown as PerpsLaunchDarklyFlag;
-
-//     return perpsRemoteFeatureFlag(remoteFlag);
-//   },
-// );
+const perpsRemoteFeatureFlag = (remoteFlag: PerpsLaunchDarklyFlag) =>
+  Boolean(remoteFlag?.enabled) &&
+  hasMinimumRequiredVersion(remoteFlag?.minimumVersion);
 
 export const selectPerpsEnabledFlag = createSelector(
   selectRemoteFeatureFlags,
-  () => true,
+  (remoteFeatureFlags) => {
+    const remoteFlag =
+      remoteFeatureFlags?.perpsPerpTradingEnabled as unknown as PerpsLaunchDarklyFlag;
+
+    return perpsRemoteFeatureFlag(remoteFlag);
+  },
 );
 
 export const selectPerpsServiceInterruptionBannerEnabledFlag = createSelector(
   selectRemoteFeatureFlags,
-  () => true,
+  (remoteFeatureFlags) => {
+    const remoteFlag =
+      remoteFeatureFlags?.perpsPerpTradingServiceInterruptionBannerEnabled as unknown as PerpsLaunchDarklyFlag;
+
+    return perpsRemoteFeatureFlag(remoteFlag);
+  },
 );
