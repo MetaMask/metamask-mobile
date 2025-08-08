38eb2751f3a98d3fda84aa5858cf77bbb5baa086
smilingkylan <kylan.hurt@gmail.com>
2025-08-08 07:36:13 -0700

    Remove browser replaceState override

--- app/util/browserScripts.ts
+++ app/util/browserScripts.ts
@@ -17,53 +17,7 @@ const getWindowInformation = `
 `;
 
 export const SPA_urlChangeListener = `(function () {
-  var __mmHistory = window.history;
-  var __mmPushState = __mmHistory.pushState;
-  var __mmReplaceState = __mmHistory.replaceState;
-  function __mm__updateUrl(){
-    const siteName = document.querySelector('head > meta[property="og:site_name"]');
-    const title = siteName || document.querySelector('head > meta[name="title"]') || document.title;
-    const height = Math.max(document.documentElement.clientHeight, document.documentElement.scrollHeight, document.body.clientHeight, document.body.scrollHeight);
 
-    window.ReactNativeWebView && window.ReactNativeWebView.postMessage(JSON.stringify(
-      {
-        type: 'NAV_CHANGE',
-        payload: {
-          url: location.href,
-          title: title,
-        }
-      }
-    ));
-
-    setTimeout(() => {
-      const height = Math.max(document.documentElement.clientHeight, document.documentElement.scrollHeight, document.body.clientHeight, document.body.scrollHeight);
-      window.ReactNativeWebView && window.ReactNativeWebView.postMessage(JSON.stringify(
-      {
-        type: 'GET_HEIGHT',
-        payload: {
-          height: height
-        }
-      }))
-    }, 500);
-  }
-
-  __mmHistory.pushState = function(state) {
-    setTimeout(function () {
-      __mm__updateUrl();
-    }, 100);
-    return __mmPushState.apply(history, arguments);
-  };
-
-  __mmHistory.replaceState = function(state) {
-    setTimeout(function () {
-      __mm__updateUrl();
-    }, 100);
-    return __mmReplaceState.apply(history, arguments);
-  };
-
-  window.onpopstate = function(event) {
-    __mm__updateUrl();
-  };
   })();
 `;
 
