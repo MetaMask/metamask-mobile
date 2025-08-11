52cf4f83e8f67a9b6a84f1d2d4b1054c879ca39f
Elliot Winkler <elliot.winkler@gmail.com>
2025-08-11 15:27:22 -0600

    Add more tests

--- app/core/Engine/controllers/network-controller/messenger-action-handlers.test.ts
+++ app/core/Engine/controllers/network-controller/messenger-action-handlers.test.ts
@@ -24,7 +24,7 @@ describe('onRpcEndpointUnavailable', () => {
 
   describe('if the Segment event should be created', () => {
     describe('if the error is a JSON-RPC error', () => {
-      it('creates a Segment event for a JSON-RPC error, hiding the API from the URL and including the HTTP status code and error code', () => {
+      it('creates a Segment event given an Infura endpoint URL, removing the path and including the HTTP status code', () => {
         shouldCreateRpcServiceEventsMock.mockReturnValue(true);
         const trackEvent = jest.fn();
 
@@ -52,10 +52,66 @@ describe('onRpcEndpointUnavailable', () => {
         });
         /* eslint-enable @typescript-eslint/naming-convention */
       });
+
+      it('creates a Segment event given a custom endpoint URL, removing the path and including the HTTP status code', () => {
+        shouldCreateRpcServiceEventsMock.mockReturnValue(true);
+        const trackEvent = jest.fn();
+
+        onRpcEndpointUnavailable({
+          chainId: '0xaa36a7',
+          endpointUrl: 'https://flare-api.flare.network/ext/C/rpc',
+          error: new HttpError(420),
+          trackEvent,
+          metaMetricsId:
+            '0x86bacb9b2bf9a7e8d2b147eadb95ac9aaa26842327cd24afc8bd4b3c1d136420',
+        });
+
+        // The case of the Segment properties are intentional.
+        /* eslint-disable @typescript-eslint/naming-convention */
+        expect(trackEvent).toHaveBeenCalledWith({
+          event: {
+            category: 'RPC Service Unavailable',
+          },
+          properties: {
+            chain_id_caip: 'eip155:11155111',
+            http_status: 420,
+            rpc_endpoint_url: 'flare-api.flare.network',
+          },
+        });
+        /* eslint-enable @typescript-eslint/naming-convention */
+      });
+
+      it('creates a Segment event given a Quicknode endpoint URL, removing the path and including the HTTP status code', () => {
+        shouldCreateRpcServiceEventsMock.mockReturnValue(true);
+        const trackEvent = jest.fn();
+
+        onRpcEndpointUnavailable({
+          chainId: '0xaa36a7',
+          endpointUrl: 'https://some-subdomain.quiknode.pro/abc123456',
+          error: new HttpError(420),
+          trackEvent,
+          metaMetricsId:
+            '0x86bacb9b2bf9a7e8d2b147eadb95ac9aaa26842327cd24afc8bd4b3c1d136420',
+        });
+
+        // The case of the Segment properties are intentional.
+        /* eslint-disable @typescript-eslint/naming-convention */
+        expect(trackEvent).toHaveBeenCalledWith({
+          event: {
+            category: 'RPC Service Unavailable',
+          },
+          properties: {
+            chain_id_caip: 'eip155:11155111',
+            http_status: 420,
+            rpc_endpoint_url: 'some-subdomain.quiknode.pro',
+          },
+        });
+        /* eslint-enable @typescript-eslint/naming-convention */
+      });
     });
 
     describe('when the error is not a JSON-RPC error', () => {
-      it('creates a Segment event, hiding the API from the URL and not including the HTTP status code', () => {
+      it('creates a Segment event given an Infura endpoint URL, removing the path and not including the HTTP status code', () => {
         shouldCreateRpcServiceEventsMock.mockReturnValue(true);
         const trackEvent = jest.fn();
 
@@ -78,6 +134,60 @@ describe('onRpcEndpointUnavailable', () => {
           properties: {
             chain_id_caip: 'eip155:11155111',
             rpc_endpoint_url: 'some-subdomain.infura.io',
+          },
+        });
+        /* eslint-enable @typescript-eslint/naming-convention */
+      });
+
+      it('creates a Segment event given a custom endpoint URL, removing the path and not including the HTTP status code', () => {
+        shouldCreateRpcServiceEventsMock.mockReturnValue(true);
+        const trackEvent = jest.fn();
+
+        onRpcEndpointUnavailable({
+          chainId: '0xaa36a7',
+          endpointUrl: 'https://flare-api.flare.network/ext/C/rpc',
+          error: new Error('some error'),
+          trackEvent,
+          metaMetricsId:
+            '0x86bacb9b2bf9a7e8d2b147eadb95ac9aaa26842327cd24afc8bd4b3c1d136420',
+        });
+
+        // The case of the Segment properties are intentional.
+        /* eslint-disable @typescript-eslint/naming-convention */
+        expect(trackEvent).toHaveBeenCalledWith({
+          event: {
+            category: 'RPC Service Unavailable',
+          },
+          properties: {
+            chain_id_caip: 'eip155:11155111',
+            rpc_endpoint_url: 'flare-api.flare.network',
+          },
+        });
+        /* eslint-enable @typescript-eslint/naming-convention */
+      });
+
+      it('creates a Segment event given a Quicknode endpoint URL, removing the path and not including the HTTP status code', () => {
+        shouldCreateRpcServiceEventsMock.mockReturnValue(true);
+        const trackEvent = jest.fn();
+
+        onRpcEndpointUnavailable({
+          chainId: '0xaa36a7',
+          endpointUrl: 'https://some-subdomain.quiknode.pro/abc123456',
+          error: new Error('some error'),
+          trackEvent,
+          metaMetricsId:
+            '0x86bacb9b2bf9a7e8d2b147eadb95ac9aaa26842327cd24afc8bd4b3c1d136420',
+        });
+
+        // The case of the Segment properties are intentional.
+        /* eslint-disable @typescript-eslint/naming-convention */
+        expect(trackEvent).toHaveBeenCalledWith({
+          event: {
+            category: 'RPC Service Unavailable',
+          },
+          properties: {
+            chain_id_caip: 'eip155:11155111',
+            rpc_endpoint_url: 'some-subdomain.quiknode.pro',
           },
         });
         /* eslint-enable @typescript-eslint/naming-convention */
@@ -122,7 +232,7 @@ describe('onRpcEndpointDegraded', () => {
 
   describe('if the Segment event should be created', () => {
     describe('when the error is a JSON-RPC error', () => {
-      it('creates a Segment event, hiding the API from the URL and including the HTTP status code and error code', () => {
+      it('creates a Segment event given an Infura endpoint URL, removing the path and including the HTTP status code', () => {
         shouldCreateRpcServiceEventsMock.mockReturnValue(true);
         const trackEvent = jest.fn();
 
@@ -150,10 +260,66 @@ describe('onRpcEndpointDegraded', () => {
         });
         /* eslint-enable @typescript-eslint/naming-convention */
       });
+
+      it('creates a Segment event given a custom endpoint URL, removing the path and including the HTTP status code', () => {
+        shouldCreateRpcServiceEventsMock.mockReturnValue(true);
+        const trackEvent = jest.fn();
+
+        onRpcEndpointDegraded({
+          chainId: '0xaa36a7',
+          endpointUrl: 'https://flare-api.flare.network/ext/C/rpc',
+          error: new HttpError(420),
+          trackEvent,
+          metaMetricsId:
+            '0x86bacb9b2bf9a7e8d2b147eadb95ac9aaa26842327cd24afc8bd4b3c1d136420',
+        });
+
+        // The case of the Segment properties are intentional.
+        /* eslint-disable @typescript-eslint/naming-convention */
+        expect(trackEvent).toHaveBeenCalledWith({
+          event: {
+            category: 'RPC Service Degraded',
+          },
+          properties: {
+            chain_id_caip: 'eip155:11155111',
+            http_status: 420,
+            rpc_endpoint_url: 'flare-api.flare.network',
+          },
+        });
+        /* eslint-enable @typescript-eslint/naming-convention */
+      });
+
+      it('creates a Segment event given a Quicknode endpoint URL, removing the path and including the HTTP status code', () => {
+        shouldCreateRpcServiceEventsMock.mockReturnValue(true);
+        const trackEvent = jest.fn();
+
+        onRpcEndpointDegraded({
+          chainId: '0xaa36a7',
+          endpointUrl: 'https://some-subdomain.quiknode.pro/abc123456',
+          error: new HttpError(420),
+          trackEvent,
+          metaMetricsId:
+            '0x86bacb9b2bf9a7e8d2b147eadb95ac9aaa26842327cd24afc8bd4b3c1d136420',
+        });
+
+        // The case of the Segment properties are intentional.
+        /* eslint-disable @typescript-eslint/naming-convention */
+        expect(trackEvent).toHaveBeenCalledWith({
+          event: {
+            category: 'RPC Service Degraded',
+          },
+          properties: {
+            chain_id_caip: 'eip155:11155111',
+            http_status: 420,
+            rpc_endpoint_url: 'some-subdomain.quiknode.pro',
+          },
+        });
+        /* eslint-enable @typescript-eslint/naming-convention */
+      });
     });
 
     describe('when the error is not a JSON-RPC error', () => {
-      it('creates a Segment event, hiding the API from the URL and not including the HTTP status code', () => {
+      it('creates a Segment event given an Infura endpoint URL, removing the path and not including the HTTP status code', () => {
         shouldCreateRpcServiceEventsMock.mockReturnValue(true);
         const trackEvent = jest.fn();
 
@@ -180,10 +346,64 @@ describe('onRpcEndpointDegraded', () => {
         });
         /* eslint-enable @typescript-eslint/naming-convention */
       });
+
+      it('creates a Segment event given a custom endpoint URL, removing the path and not including the HTTP status code', () => {
+        shouldCreateRpcServiceEventsMock.mockReturnValue(true);
+        const trackEvent = jest.fn();
+
+        onRpcEndpointDegraded({
+          chainId: '0xaa36a7',
+          endpointUrl: 'https://flare-api.flare.network/ext/C/rpc',
+          error: new Error('some error'),
+          trackEvent,
+          metaMetricsId:
+            '0x86bacb9b2bf9a7e8d2b147eadb95ac9aaa26842327cd24afc8bd4b3c1d136420',
+        });
+
+        // The case of the Segment properties are intentional.
+        /* eslint-disable @typescript-eslint/naming-convention */
+        expect(trackEvent).toHaveBeenCalledWith({
+          event: {
+            category: 'RPC Service Degraded',
+          },
+          properties: {
+            chain_id_caip: 'eip155:11155111',
+            rpc_endpoint_url: 'flare-api.flare.network',
+          },
+        });
+        /* eslint-enable @typescript-eslint/naming-convention */
+      });
+
+      it('creates a Segment event given a Quicknode endpoint URL, removing the path and not including the HTTP status code', () => {
+        shouldCreateRpcServiceEventsMock.mockReturnValue(true);
+        const trackEvent = jest.fn();
+
+        onRpcEndpointDegraded({
+          chainId: '0xaa36a7',
+          endpointUrl: 'https://some-subdomain.quiknode.pro/abc123456',
+          error: new Error('some error'),
+          trackEvent,
+          metaMetricsId:
+            '0x86bacb9b2bf9a7e8d2b147eadb95ac9aaa26842327cd24afc8bd4b3c1d136420',
+        });
+
+        // The case of the Segment properties are intentional.
+        /* eslint-disable @typescript-eslint/naming-convention */
+        expect(trackEvent).toHaveBeenCalledWith({
+          event: {
+            category: 'RPC Service Degraded',
+          },
+          properties: {
+            chain_id_caip: 'eip155:11155111',
+            rpc_endpoint_url: 'some-subdomain.quiknode.pro',
+          },
+        });
+        /* eslint-enable @typescript-eslint/naming-convention */
+      });
     });
 
     describe('when there is no error', () => {
-      it('creates a Segment event, hiding the API from the URL', () => {
+      it('creates a Segment event given an Infura endpoint URL, removing the path', () => {
         shouldCreateRpcServiceEventsMock.mockReturnValue(true);
         const trackEvent = jest.fn();
 
@@ -206,6 +426,60 @@ describe('onRpcEndpointDegraded', () => {
           properties: {
             chain_id_caip: 'eip155:11155111',
             rpc_endpoint_url: 'some-subdomain.infura.io',
+          },
+        });
+        /* eslint-enable @typescript-eslint/naming-convention */
+      });
+
+      it('creates a Segment event given a custom endpoint URL, removing the path', () => {
+        shouldCreateRpcServiceEventsMock.mockReturnValue(true);
+        const trackEvent = jest.fn();
+
+        onRpcEndpointDegraded({
+          chainId: '0xaa36a7',
+          endpointUrl: 'https://flare-api.flare.network/ext/C/rpc',
+          error: undefined,
+          trackEvent,
+          metaMetricsId:
+            '0x86bacb9b2bf9a7e8d2b147eadb95ac9aaa26842327cd24afc8bd4b3c1d136420',
+        });
+
+        // The case of the Segment properties are intentional.
+        /* eslint-disable @typescript-eslint/naming-convention */
+        expect(trackEvent).toHaveBeenCalledWith({
+          event: {
+            category: 'RPC Service Degraded',
+          },
+          properties: {
+            chain_id_caip: 'eip155:11155111',
+            rpc_endpoint_url: 'flare-api.flare.network',
+          },
+        });
+        /* eslint-enable @typescript-eslint/naming-convention */
+      });
+
+      it('creates a Segment event given a Quicknode endpoint URL, removing the path', () => {
+        shouldCreateRpcServiceEventsMock.mockReturnValue(true);
+        const trackEvent = jest.fn();
+
+        onRpcEndpointDegraded({
+          chainId: '0xaa36a7',
+          endpointUrl: 'https://some-subdomain.quiknode.pro/abc123456',
+          error: undefined,
+          trackEvent,
+          metaMetricsId:
+            '0x86bacb9b2bf9a7e8d2b147eadb95ac9aaa26842327cd24afc8bd4b3c1d136420',
+        });
+
+        // The case of the Segment properties are intentional.
+        /* eslint-disable @typescript-eslint/naming-convention */
+        expect(trackEvent).toHaveBeenCalledWith({
+          event: {
+            category: 'RPC Service Degraded',
+          },
+          properties: {
+            chain_id_caip: 'eip155:11155111',
+            rpc_endpoint_url: 'some-subdomain.quiknode.pro',
           },
         });
         /* eslint-enable @typescript-eslint/naming-convention */
