982df8a8c7020ef027c3b6b21e12135196510872
IF <139582705+infiniteflower@users.noreply.github.com>
2025-08-11 18:51:23 -0400

    chore: use AvatarGroup component instead

--- app/components/UI/Bridge/components/BridgeSourceNetworksBar.tsx
+++ app/components/UI/Bridge/components/BridgeSourceNetworksBar.tsx
@@ -1,10 +1,7 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { StyleSheet } from 'react-native';
 import { Box } from '../../Box/Box';
-import Text, {
-  TextColor,
-  TextVariant,
-} from '../../../../component-library/components/Texts/Text';
+import Text from '../../../../component-library/components/Texts/Text';
 import { useStyles } from '../../../../component-library/hooks';
 import { Theme } from '../../../../util/theme/models';
 import { CaipChainId, Hex } from '@metamask/utils';
@@ -19,32 +16,17 @@ import Button, {
 } from '../../../../component-library/components/Buttons/Button';
 import Routes from '../../../../constants/navigation/Routes';
 import { useNavigation } from '@react-navigation/native';
-import AvatarNetwork from '../../../../component-library/components/Avatars/Avatar/variants/AvatarNetwork/AvatarNetwork';
-import { AvatarSize } from '../../../../component-library/components/Avatars/Avatar';
-
-const NETWORK_GAP = 2;
-const NETWORK_OVERFLOW_OVERLAY_SIZE = 16;
-const NETWORK_OVERFLOW_OVERLAY_MARGIN_LEFT =
-  -NETWORK_OVERFLOW_OVERLAY_SIZE - NETWORK_GAP;
+import {
+  AvatarSize,
+  AvatarVariant,
+} from '../../../../component-library/components/Avatars/Avatar';
+import AvatarGroup from '../../../../component-library/components/Avatars/AvatarGroup';
 
 const createStyles = (params: { theme: Theme }) => {
   const { theme } = params;
   return StyleSheet.create({
     networksButton: {
       borderColor: theme.colors.border.muted,
-    },
-    avatarContainer: {},
-    avatarNetwork: {
-      marginRight: 0,
-    },
-    networkOverflowOverlay: {
-      backgroundColor: theme.colors.overlay.alternative,
-      width: NETWORK_OVERFLOW_OVERLAY_SIZE,
-      height: NETWORK_OVERFLOW_OVERLAY_SIZE,
-      borderRadius: 4,
-      marginLeft: NETWORK_OVERFLOW_OVERLAY_MARGIN_LEFT,
-      lineHeight: NETWORK_OVERFLOW_OVERLAY_SIZE,
-      paddingHorizontal: 0.5,
     },
   });
 };
@@ -86,21 +68,13 @@ export const BridgeSourceNetworksBar: React.FC<SourceNetworksButtonProps> = ({
     });
   };
 
-  const renderSourceNetworks = useCallback(
-    () =>
-      networksToShow.map(({ chainId }) => (
-        <Box key={chainId} style={styles.avatarContainer}>
-          <AvatarNetwork
-            key={chainId}
-            imageSource={getNetworkImageSource({ chainId })}
-            name={networkConfigurations[chainId]?.name}
-            size={AvatarSize.Xs}
-            style={styles.avatarNetwork}
-          />
-        </Box>
-      )),
-    [networkConfigurations, styles, networksToShow],
-  );
+  const networkAvatars = networksToShow.map(({ chainId }) => ({
+    imageSource: getNetworkImageSource({ chainId }),
+    name: networkConfigurations[chainId]?.name,
+    size: AvatarSize.Xs,
+    variant: AvatarVariant.Network as const,
+    includesBorder: true,
+  }));
 
   return (
     <Button
@@ -112,21 +86,11 @@ export const BridgeSourceNetworksBar: React.FC<SourceNetworksButtonProps> = ({
           alignItems={AlignItems.center}
           gap={4}
         >
-          <Box
-            flexDirection={FlexDirection.Row}
-            alignItems={AlignItems.center}
-            gap={NETWORK_GAP}
-          >
-            {renderSourceNetworks()}
-            {selectedSourceChainIds.length > MAX_NETWORK_ICONS && (
-              <Text
-                variant={TextVariant.BodyXS}
-                color={TextColor.Inverse}
-                style={styles.networkOverflowOverlay}
-              >
-                +{selectedSourceChainIds.length - MAX_NETWORK_ICONS}
-              </Text>
-            )}
+          <Box flexDirection={FlexDirection.Row} alignItems={AlignItems.center}>
+            <AvatarGroup
+              avatarPropsList={networkAvatars}
+              size={AvatarSize.Xs}
+            />
           </Box>
           <Text>{networkText}</Text>
         </Box>
