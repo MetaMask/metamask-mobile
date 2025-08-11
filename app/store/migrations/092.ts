840656227df8f51a26224839409bc6507dabe3fc
Stanley Yuen <102275989+stanleyyconsensys@users.noreply.github.com>
2025-08-11 09:30:20 +0800

    chore: update sei network name

---
+++ app/store/migrations/092.ts
@@ -0,0 +1,66 @@
+import { hasProperty, isObject } from '@metamask/utils';
+import { captureException } from '@sentry/react-native';
+import { ensureValidState } from './util';
+import { NetworkConfiguration } from '@metamask/network-controller';
+import { NETWORK_CHAIN_ID } from '../../util/networks/customNetworks';
+
+/**
+ * Migration 092: Update Sei Network Name
+ *
+ * This migration updates the SEI network name from `Sei Network` to `Sei Mainnet`.
+ */
+export default function migrate(state: unknown): unknown {
+  const migrationVersion = 92;
+  const fromName = 'Sei Network';
+  const toName = 'Sei Mainnet';
+  const seiChainId = NETWORK_CHAIN_ID.SEI_MAINNET;
+
+  if (!ensureValidState(state, migrationVersion)) {
+    return state;
+  }
+
+  try {
+    // We only update the network name if it exists in the state
+    // and matches the expected chain ID and name.
+    if (
+      hasProperty(state, 'NetworkController') &&
+      isObject(state.NetworkController) &&
+      isObject(state.NetworkController.networkConfigurationsByChainId) &&
+      hasProperty(
+        state.NetworkController.networkConfigurationsByChainId,
+        seiChainId,
+      ) &&
+      isObject(
+        state.NetworkController.networkConfigurationsByChainId[seiChainId],
+      ) &&
+      hasProperty(
+        state.NetworkController.networkConfigurationsByChainId[
+          seiChainId
+        ] as NetworkConfiguration,
+        'name',
+      ) &&
+      (
+        state.NetworkController.networkConfigurationsByChainId[
+          seiChainId
+        ] as NetworkConfiguration
+      ).name === fromName
+    ) {
+      (
+        state.NetworkController.networkConfigurationsByChainId[
+          seiChainId
+        ] as NetworkConfiguration
+      ).name = toName;
+    }
+    return state;
+  } catch (error) {
+    captureException(
+      new Error(
+        `Migration ${migrationVersion}: Failed to update Sei network name: ${String(
+          error,
+        )}`,
+      ),
+    );
+    // Return the original state if migration fails to avoid breaking the app
+    return state;
+  }
+}
