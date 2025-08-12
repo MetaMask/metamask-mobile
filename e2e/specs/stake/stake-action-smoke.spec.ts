da9bcfe42b9e68f8b063ad991c82ebd7b6ecc57f
Brian Nguyen <brianacnguyen@gmail.com>
2025-08-11 20:02:53 -0700

    Removed unused imports

--- e2e/specs/stake/stake-action-smoke.spec.ts
+++ e2e/specs/stake/stake-action-smoke.spec.ts
@@ -27,7 +27,6 @@ import Assertions from '../../framework/Assertions';
 import StakeView from '../../pages/Stake/StakeView';
 import StakeConfirmView from '../../pages/Stake/StakeConfirmView';
 import SendView from '../../pages/Send/SendView';
-import WalletActionsBottomSheet from '../../pages/wallet/WalletActionsBottomSheet';
 import AmountView from '../../pages/Send/AmountView';
 import TransactionConfirmationView from '../../pages/Send/TransactionConfirmView';
 import AccountListBottomSheet from '../../pages/wallet/AccountListBottomSheet';
