2aea40358a7b6fd10a7efb0900ef520ceabb7791
Jiexi Luan <jiexiluan@gmail.com>
2025-08-13 11:17:11 -0700

    lint

--- e2e/specs/connections/evm-provider-events.spec.ts
+++ e2e/specs/connections/evm-provider-events.spec.ts
@@ -188,7 +188,7 @@ describe(SmokeWalletPlatform('EVM Provider Events'), () => {
     );
   });
 
-  it('notifies a permitted dapp of the new chain ID when the network changes.', async () => {
+  it('notifies a permitted dapp of the new chain ID when the network changes', async () => {
     await withFixtures(
       {
         dapps: [
