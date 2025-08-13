31fe3de423acfa74ecf127f87d57bd67a7068cc3
Matthew Walsh <matthew.walsh@consensys.net>
2025-08-13 23:07:18 +0100

    Update design of pay with row

--- app/components/Views/confirmations/components/token-pill/token-pill.styles.ts
+++ app/components/Views/confirmations/components/token-pill/token-pill.styles.ts
@@ -1,26 +1,19 @@
 import { Theme } from '@metamask/design-tokens';
 import { StyleSheet, ViewStyle } from 'react-native';
 
-const styleSheet = (params: { theme: Theme }) => {
-  const { theme } = params;
-  const { colors } = theme;
-
+const styleSheet = (_params: { theme: Theme }) => {
   const baseStyle: ViewStyle = {
-    backgroundColor: colors.background.alternative,
     borderRadius: 99,
-    paddingVertical: 4,
-    paddingInline: 8,
-    gap: 5,
   };
 
   const tokenIconStyle: ViewStyle = {
-    width: 16,
-    height: 16,
-    borderRadius: 8,
+    width: 34,
+    height: 34,
+    borderRadius: 99,
   };
 
   const networkIconStyle: ViewStyle = {
-    paddingTop: 4,
+    marginTop: 4,
   };
 
   return StyleSheet.create({
