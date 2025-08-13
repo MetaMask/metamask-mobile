655b1c648b59edb6c699629df9eed791bc63631d
Guillaume Roux <guillaumeroux123@gmail.com>
2025-08-13 11:57:17 +0200

    [WIP] Snaps UI link E2E

---
+++ e2e/api-mocking/mock-responses/empty-page-mock.js
@@ -0,0 +1,14 @@
+/**
+ * An empty HTML page.
+ */
+export const emptyHtmlPage = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>E2E Test Page</title>
+    <link rel="icon" href="data:image/png;base64,iVBORw0KGgo=">
+  </head>
+  <body data-testid="empty-page-body">
+    Empty page by MetaMask
+  </body>
+</html>`;
---
+++ e2e/specs/snaps/test-snap-uilinks.spec.ts
@@ -0,0 +1,45 @@
+import FixtureBuilder from '../../framework/fixtures/FixtureBuilder';
+import { withFixtures } from '../../framework/fixtures/FixtureHelper';
+import TestSnaps from '../../pages/Browser/TestSnaps';
+import TabBarComponent from '../../pages/wallet/TabBarComponent';
+import { FlaskBuildTests } from '../../tags';
+import { loginToApp } from '../../viewHelper';
+import { Assertions, Gestures, Matchers } from '../../framework';
+import { emptyHtmlPage } from '../../api-mocking/mock-responses/empty-page-mock';
+
+jest.setTimeout(150_000);
+
+describe(FlaskBuildTests('UI Links Snap Test'), () => {
+  it('opens a link in the browser', async () => {
+    await withFixtures(
+      {
+        fixture: new FixtureBuilder().build(),
+        restartDevice: true,
+        testSpecificMock: {
+          GET: [
+            {
+              urlEndpoint: 'https://snaps.metamask.io/',
+              responseCode: 200,
+              response: emptyHtmlPage,
+            },
+          ],
+        },
+      },
+      async () => {
+        await loginToApp();
+        await TabBarComponent.tapBrowser();
+        await TestSnaps.navigateToTestSnap();
+
+        await TestSnaps.installSnap('connectDialogSnapButton');
+
+        await TestSnaps.tapButton('sendConfirmationButton');
+        await Assertions.expectTextDisplayed('Confirmation Dialog');
+
+        const link = Matchers.getElementByID('snaps-ui-link');
+        await Gestures.waitAndTap(link);
+
+        await Assertions.expectTextDisplayed('Empty page by MetaMask');
+      },
+    );
+  });
+});
