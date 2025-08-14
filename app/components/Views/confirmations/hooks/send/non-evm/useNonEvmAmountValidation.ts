17efda4577829b21a3f192ae59752cec92b2fe32
Jyoti Puri <jyotipuri@gmail.com>
2025-08-14 13:09:25 +0530

    feat: fix arithmetic used for non-evm send on amount page (#18296)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    Use floating point arithmetic for non-evm send on amount page
    
    ## **Changelog**
    
    <!--
    If this PR is not End-User-Facing and should not show up in the
    CHANGELOG, you can choose to either:
    1. Write `CHANGELOG entry: null`
    2. Label with `no-changelog`
    
    If this PR is End-User-Facing, please write a short User-Facing
    description in the past tense like:
    `CHANGELOG entry: Added a new tab for users to see their NFTs`
    `CHANGELOG entry: Fixed a bug that was causing some NFTs to flicker`
    
    (This helps the Release Engineer do their job more quickly and
    accurately)
    -->
    
    CHANGELOG entry:
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-mobile/issues/18294
    
    ## **Manual testing steps**
    
    ```gherkin
    Feature: my feature name
    
      Scenario: user [verb for user action]
        Given [describe expected initial app state]
    
        When user [verb for user action]
        Then [describe expected outcome]
    ```
    
    ## **Screenshots/Recordings**
    NA
    
    ## **Pre-merge author checklist**
    
    - [X] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [X] I've completed the PR template to the best of my ability
    - [X] I’ve included tests if applicable
    - [X] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [X] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/Views/confirmations/hooks/send/non-evm/useNonEvmAmountValidation.ts
+++ app/components/Views/confirmations/hooks/send/non-evm/useNonEvmAmountValidation.ts
@@ -3,8 +3,10 @@ import { useCallback } from 'react';
 import { strings } from '../../../../../../../locales/i18n';
 import { isDecimal } from '../../../../../../util/number';
 import { AssetType } from '../../../types/token';
+import { toBNWithDecimals } from '../../../utils/send';
 import { useSendContext } from '../../../context/send-context';
 
+// todo: once we integrate with solana snap for validations this can be fully / partially removed
 export const validateAmountFn = ({
   amount,
   asset,
@@ -18,7 +20,11 @@ export const validateAmountFn = ({
   if (!isDecimal(amount) || Number(amount) < 0) {
     return strings('send.invalid_value');
   }
-  if (parseFloat(amount) > parseFloat(asset.balance)) {
+  if (
+    toBNWithDecimals(amount, asset.decimals).gt(
+      toBNWithDecimals(asset.balance, asset.decimals),
+    )
+  ) {
     return strings('send.insufficient_funds');
   }
 };
