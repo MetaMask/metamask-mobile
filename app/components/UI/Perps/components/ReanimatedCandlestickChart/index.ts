23fe1e3c526369b0667e8df7e2bd048e61c51c9e
Nicholas Gambino <nicholas.gambino@consensys.net>
2025-08-15 16:02:01 -0700

    chore: Remove reanimated

--- app/components/UI/Perps/components/ReanimatedCandlestickChart/index.ts
+++
@@ -1,4 +0,0 @@
-// Reanimated Smooth Zoom CandleStick Chart Components
-export { default as ReanimatedCandlestickChart } from './ReanimatedCandlestickChart';
-export { default as ReanimatedCandlestickChartIntegration } from './ReanimatedCandlestickChartIntegration';
-export { default as ReanimatedCandlestickChartTest } from './ReanimatedCandlestickChartTest';
