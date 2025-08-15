dc752b34fc1ff4d9489f84e35ae9c61c917ef238
Brian Nguyen <brianacnguyen@gmail.com>
2025-08-14 20:03:20 -0700

    Polished tabbar

--- app/component-library/components/Navigation/TabBar/TabBar.constants.ts
+++ app/component-library/components/Navigation/TabBar/TabBar.constants.ts
@@ -2,6 +2,8 @@
 
 // Third party dependencies.
 import { IconName } from '../../Icons/Icon';
+// External dependencies.
+import { strings } from '../../../../../locales/i18n';
 // Internal dependencies.
 import { IconByTabBarIconKey, TabBarIconKey } from './TabBar.types';
 
@@ -12,5 +14,13 @@ export const ICON_BY_TAB_BAR_ICON_KEY: IconByTabBarIconKey = {
   [TabBarIconKey.Activity]: IconName.Activity,
   [TabBarIconKey.Setting]: IconName.Setting,
 };
+
+export const LABEL_BY_TAB_BAR_ICON_KEY = {
+  [TabBarIconKey.Wallet]: strings('bottom_nav.home'),
+  [TabBarIconKey.Browser]: strings('bottom_nav.browser'),
+  [TabBarIconKey.Actions]: '',
+  [TabBarIconKey.Activity]: strings('bottom_nav.activity'),
+  [TabBarIconKey.Setting]: strings('bottom_nav.settings'),
+} as const;
 
 export const TAB_BAR_HEIGHT = 54; // 22px text line height + 20px icon height + 12px padding top
