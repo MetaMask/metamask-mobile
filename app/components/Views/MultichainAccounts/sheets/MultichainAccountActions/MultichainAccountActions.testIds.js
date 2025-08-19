0e1fe933c099650889a294a73b98a821ed8de947
Patryk Łucka <5708018+PatrykLucka@users.noreply.github.com>
2025-08-19 11:01:02 +0200

    test: remove unused test ID constants from MultichainAccountActions

--- app/components/Views/MultichainAccounts/sheets/MultichainAccountActions/MultichainAccountActions.testIds.js
+++
@@ -1,8 +0,0 @@
-export const MULTICHAIN_ACCOUNT_ACTIONS_ACCOUNT_DETAILS =
-  'multichain-account-actions-account-details';
-
-export const MULTICHAIN_ACCOUNT_ACTIONS_EDIT_NAME =
-  'multichain-account-actions-edit-name';
-
-export const MULTICHAIN_ACCOUNT_ACTIONS_ADDRESSES =
-  'multichain-account-actions-addresses';
