1cb327e03f2ac0f14bb60c852fda44e4bc6ca399
Matthew Walsh <matthew.walsh@consensys.net>
2025-08-19 18:08:49 +0100

    Add hero component

--- app/components/Views/confirmations/components/activity/transaction-details-pay-with-row/transaction-details-pay-with-row.tsx
+++ app/components/Views/confirmations/components/activity/transaction-details-pay-with-row/transaction-details-pay-with-row.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 import { TransactionDetailsRow } from '../transaction-details-row/transaction-details-row';
 import Text from '../../../../../../component-library/components/Texts/Text';
 import { Box } from '../../../../../UI/Box/Box';
-import { FlexDirection } from '../../../../../UI/Box/box.types';
+import { AlignItems, FlexDirection } from '../../../../../UI/Box/box.types';
+import { TokenIcon, TokenIconVariant } from '../../token-icon';
 
 export function TransactionDetailsPayWithRow() {
+  const chainId = '0x1';
+  const USDC = '0xA0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
+
   return (
     <TransactionDetailsRow label={'Paid with'}>
-      <Box flexDirection={FlexDirection.Row} gap={6}>
+      <Box
+        flexDirection={FlexDirection.Row}
+        gap={6}
+        alignItems={AlignItems.center}
+      >
+        <TokenIcon
+          chainId={chainId}
+          address={USDC}
+          variant={TokenIconVariant.Row}
+        />
         <Text>USDC</Text>
       </Box>
     </TransactionDetailsRow>
