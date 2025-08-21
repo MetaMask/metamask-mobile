96cd55c95efea56123f9239a34a7c932ade7dae4
vinnyhoward <vincenguyenhoward@gmail.com>
2025-08-21 11:24:23 -0600

    fix: update test type failing lint job

--- app/components/UI/NetworkMultiSelector/NetworkMultiSelector.test.tsx
+++ app/components/UI/NetworkMultiSelector/NetworkMultiSelector.test.tsx
@@ -167,6 +167,7 @@ describe('NetworkMultiSelector', () => {
       toggleNetwork: jest.fn(),
       isNetworkEnabled: jest.fn(),
       hasOneEnabledNetwork: false,
+      tryEnableEvmNetwork: jest.fn(),
     });
 
     mockUseNetworksByNamespace.mockReturnValue({
@@ -340,6 +341,7 @@ describe('NetworkMultiSelector', () => {
         toggleNetwork: jest.fn(),
         isNetworkEnabled: jest.fn(),
         hasOneEnabledNetwork: false,
+        tryEnableEvmNetwork: jest.fn(),
       });
 
       const { getByTestId } = render(
@@ -397,6 +399,7 @@ describe('NetworkMultiSelector', () => {
         toggleNetwork: jest.fn(),
         isNetworkEnabled: jest.fn(),
         hasOneEnabledNetwork: false,
+        tryEnableEvmNetwork: jest.fn(),
       });
 
       const { getByTestId } = render(
