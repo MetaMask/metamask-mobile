f86a79eeecff3fdad098d63f03eb93770a9420ba
Christopher Ferreira <christopher.ferreira@consensys.net>
2025-08-21 18:05:53 +0100

    test: tests flakiness detection

---
+++ e2e/specs/card/dummy-test.spec.ts
@@ -0,0 +1,20 @@
+import { withFixtures } from '../../framework/fixtures/FixtureHelper';
+import { SmokeCard } from '../../tags';
+import FixtureBuilder from '../../framework/fixtures/FixtureBuilder';
+import { loginToApp } from '../../viewHelper';
+
+describe(SmokeCard('Dummy Test'), () => {
+  it('should pass', async () => {
+    await withFixtures(
+      {
+        fixture: new FixtureBuilder().build(),
+      },
+      async () => {
+        await loginToApp();
+
+        // Adding a sleep
+        await new Promise((resolve) => setTimeout(resolve, 10000));
+      },
+    );
+  });
+});
