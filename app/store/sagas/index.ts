3774ef51c0eff16bd8125886e3de91bc6f74cf3c
Cal-L <cleun007@gmail.com>
2025-08-22 13:20:54 -0700

    Simplify SDK initialization

--- app/store/sagas/index.ts
+++ app/store/sagas/index.ts
@@ -138,48 +138,22 @@ export function* basicFunctionalityToggle() {
   }
 }
 
-export function* handleSDKInit() {
-  yield take([NavigationActionType.ON_NAVIGATION_READY]);
-
-  // Initialize SDKConnect
-  while (true) {
-    const action = (yield take([
-      UserActionType.LOGIN,
-      SET_COMPLETED_ONBOARDING,
-    ])) as LoginAction | SetCompletedOnboardingAction;
-
-    let completedOnboarding: boolean;
-    if (action.type === SET_COMPLETED_ONBOARDING) {
-      completedOnboarding = action.completedOnboarding;
-    } else {
-      completedOnboarding = yield select(selectCompletedOnboarding);
-    }
-
-    const { KeyringController } = Engine.context;
-    const isUnlocked = KeyringController.isUnlocked();
-
-    if (
-      action.type === UserActionType.LOGIN ||
-      (completedOnboarding && isUnlocked)
-    ) {
-      try {
-        yield call(SDKConnect.init, { context: 'Nav/App' });
-      } catch (e) {
-        Logger.log('Failed to initialize SDKConnect', e);
-      }
-      break;
-    }
-  }
-
-  // Initialize WalletConnect V2
+export function* initializeSDKServices() {
   try {
+    // Initialize WalletConnect
     yield call(WC2Manager.init, {});
+    // Initialize SDKConnect
+    yield call(SDKConnect.init, { context: 'Nav/App' });
   } catch (e) {
     Logger.log('Failed to initialize services', e);
   }
 }
 
 export function* handleDeeplinkSaga() {
+  // TODO: This is only needed because SDKConnect does some weird stuff when it's initialized.
+  // Once that's refactored and the singleton is simply initialized, we should be able to remove this.
+  let hasInitializedSDKServices = false;
+
   while (true) {
     // Handle parsing deeplinks after login or when the lock manager is resolved
     const value = (yield take([
@@ -203,6 +177,11 @@ export function* handleDeeplinkSaga() {
     // App is locked or onboarding is not yet complete
     if (!isUnlocked || !completedOnboarding) {
       continue;
+    }
+
+    // Initialize SDK services
+    if (!hasInitializedSDKServices) {
+      yield call(initializeSDKServices);
     }
 
     const deeplink = AppStateEventProcessor.pendingDeeplink;
@@ -245,6 +224,5 @@ export function* rootSaga() {
   yield fork(startAppServices);
   yield fork(authStateMachine);
   yield fork(basicFunctionalityToggle);
-  yield fork(handleSDKInit);
   yield fork(handleDeeplinkSaga);
 }
