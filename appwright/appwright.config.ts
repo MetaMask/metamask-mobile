246ca7775feb915b30a3923a06ae88474f942760
Curtis David <Curtis.David7@gmail.com>
2025-08-21 21:19:35 -0400

    Revert "quick check to see if appwright is working as expected"
    This reverts commit 5017be84a2492ac6fe4197061e3aa2eadb78bbe6.

--- appwright/appwright.config.ts
+++ appwright/appwright.config.ts
@@ -3,45 +3,13 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '.e2e.env' });
 import { defineConfig, Platform } from 'appwright';
 export default defineConfig({
-  testMatch: '**/tests/*.spec.js',
+  testMatch: '**/specs/*.spec.js',
   timeout: 7 * 60 * 1000, //7 minutes until we introduce fixtures
   expect: {
     timeout: 30 * 1000, //30 seconds
   },
-  // reporter: [
-  //   // The default HTML reporter from Appwright
-  //   [
-  //     'html',
-  //     { open: 'never', outputFolder: './test-reports/appwright-report' },
-  //   ],
-  //   ['./reporters/custom-reporter.js'],
-  //   ['list'],
-  // ],
 
   projects: [
-    {
-      name: 'android',
-      use: {
-        platform: Platform.ANDROID,
-        device: {
-          provider: 'emulator', // or 'local-device' or 'browserstack'
-          name: 'Samsung Galaxy S24 Ultra', // this can changed
-          osVersion: '14', // this can changed
-        },
-        buildPath: '/Users/javi/Downloads/app-qa-release.apk', // Path to your .apk file
-      },
-    },
-    {
-      name: 'ios',
-      use: {
-        platform: Platform.IOS,
-        device: {
-          provider: 'emulator', // or 'local-device' or 'browserstack'
-          osVersion: '16.0', // this can changed
-        },
-        buildPath: 'Metamask-QA.app', // Path to your .app file
-      },
-    },
     {
       name: 'browserstack-android',
       use: {
@@ -63,7 +31,7 @@ export default defineConfig({
           name: process.env.BROWSERSTACK_DEVICE || 'iPhone 14 Pro Max',
           osVersion: process.env.BROWSERSTACK_OS_VERSION || '16.0',
         },
-        buildPath: 'bs://b287e329f47b58e49c96b6947575c6114f5f6f63', // Path to Browserstack url bs:// link //release-7.53.0-7.53.0-2223.ipa
+        buildPath: process.env.BROWSERSTACK_IOS_APP_URL, // Path to Browserstack url bs:// link //release-7.53.0-7.53.0-2223.ipa
       },
     },
   ],
