93f7645b7099ca57baa95bf7b163f498dca9ecbd
Kylan Hurt <6249205+smilingkylan@users.noreply.github.com>
2025-08-21 19:56:19 -0700

    fix: Allow most WebView options on all pages (including homepage) (#18477)
    ## **Description**
    
    The purpose of this task is to enable **all** applicable WebView options
    to be displayed on the homepage (currently
    https://portfolio.metamask.io)
    
    ## **Changelog**
    
    `CHANGELOG entry: Show all applicable options on WebView homepage`
    
    CHANGELOG entry:
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-mobile/issues/18201
    
    ## **Manual testing steps**
    
    ```gherkin
    Feature: options display in WebView on all web pages including homepage (currently portfolio.metamask.io)
    
      Scenario: user opens the WebView in MetaMask mobile app
        Given they are in the WebView on the homepage (currently portfolio.metamask.io)
    
        When user clicks on the three dots in the bottom-right corner (for options)
        Then see several options, including "Add to favorites" and "Reload"
    ```
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <img width="200" alt="IMG_4431"
    src="https://github.com/user-attachments/assets/fcacedc5-ff7e-4507-882b-213d5fda3720"
    />
    
    
    ### **After**
    
    <img width="200" alt="IMG_4431"
    src="https://github.com/user-attachments/assets/461b2af5-a6f6-49e2-9932-c4111101b152"
    />
    
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/Views/BrowserTab/BrowserTab.tsx
+++ app/components/Views/BrowserTab/BrowserTab.tsx
@@ -1554,7 +1554,6 @@ export const BrowserTab: React.FC<BrowserTabProps> = React.memo(
                 onNewTabPress={onNewTabPress}
                 toggleOptionsIfNeeded={toggleOptionsIfNeeded}
                 activeUrl={resolvedUrlRef.current}
-                isHomepage={isHomepage}
                 getMaskedUrl={getMaskedUrl}
                 title={titleRef}
                 reload={reload}
