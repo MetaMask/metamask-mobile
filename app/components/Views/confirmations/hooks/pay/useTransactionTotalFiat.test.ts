69940ff7e118439bc41bf1dca373485001d3507d
Matthew Walsh <matthew.walsh@consensys.net>
2025-08-23 01:29:32 +0100

    Remove explorer icon
    Fix transaction controller util unit tests.
    Fix QR context unit tests.
    Fix pay hook unit tests.
    Fix useTransactionTotalFiat unit tests.
    Fix smart account splash unit tests.
    Fix ledger modal unit tests.
    Fix ledger context unit tests.

--- app/components/Views/confirmations/hooks/pay/useTransactionTotalFiat.test.ts
+++ app/components/Views/confirmations/hooks/pay/useTransactionTotalFiat.test.ts
@@ -202,6 +202,6 @@ describe('useTransactionTotalFiat', () => {
       ] as TransactionBridgeQuote[],
     });
 
-    expect(result.current.bridgeFeeFormatted).toBe('$30.00');
+    expect(result.current.bridgeFeeFormatted).toBe('$30');
   });
 });
