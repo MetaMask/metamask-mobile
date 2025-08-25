dd72e1434ac7c18b0679b0ab81489b25e0904c53
salimtb <salim.toubal@consensys.net>
2025-08-25 16:56:04 +0200

    fix: hide defi tab for testnet

--- app/components/Views/Wallet/index.tsx
+++ app/components/Views/Wallet/index.tsx
@@ -5,6 +5,7 @@ import React, {
   useContext,
   useMemo,
 } from 'react';
+
 import {
   ActivityIndicator,
   StyleSheet as RNStyleSheet,
@@ -105,10 +106,7 @@ import { PortfolioBalance } from '../../UI/Tokens/TokenList/PortfolioBalance';
 import useCheckNftAutoDetectionModal from '../../hooks/useCheckNftAutoDetectionModal';
 import useCheckMultiRpcModal from '../../hooks/useCheckMultiRpcModal';
 import { useAccountsWithNetworkActivitySync } from '../../hooks/useAccountsWithNetworkActivitySync';
-import {
-  selectTokenNetworkFilter,
-  selectUseTokenDetection,
-} from '../../../selectors/preferencesController';
+import { selectUseTokenDetection } from '../../../selectors/preferencesController';
 import { TokenI } from '../../UI/Tokens/types';
 import { Hex } from '@metamask/utils';
 import { Nft, Token } from '@metamask/assets-controllers';
@@ -346,6 +344,12 @@ const Wallet = ({
    */
   const providerConfig = useSelector(selectProviderConfig);
   const chainId = useSelector(selectChainId);
+  const enabledNetworks = useSelector(selectEVMEnabledNetworks);
+
+  const enabledNetworksHasTestNet = useMemo(
+    () => enabledNetworks.some((network) => isTestNet(network)),
+    [enabledNetworks],
+  );
 
   const prevChainId = usePrevious(chainId);
 
@@ -607,7 +611,6 @@ const Wallet = ({
     networkConfigurations?.[chainId]?.name ?? selectedNetworkName;
 
   const networkImageSource = useSelector(selectNetworkImageSource);
-  const tokenNetworkFilter = useSelector(selectTokenNetworkFilter);
   const enabledEVMNetworks = useSelector(selectEVMEnabledNetworks);
 
   const isAllNetworks = useSelector(selectIsAllNetworks);
@@ -659,19 +662,13 @@ const Wallet = ({
   const handleNetworkFilter = useCallback(() => {
     // TODO: Come back possibly just add the chain id of the eth
     // network as the default state instead of doing this
-    const { PreferencesController } = Engine.context;
-    if (Object.keys(tokenNetworkFilter).length === 0) {
-      PreferencesController.setTokenNetworkFilter({
-        [chainId]: true,
-      });
-    }
     if (
       isRemoveGlobalNetworkSelectorEnabled() &&
       enabledEVMNetworks.length === 0
     ) {
       selectNetwork(chainId);
     }
-  }, [chainId, tokenNetworkFilter, selectNetwork, enabledEVMNetworks]);
+  }, [chainId, selectNetwork, enabledEVMNetworks]);
 
   useEffect(() => {
     handleNetworkFilter();
@@ -953,7 +950,7 @@ const Wallet = ({
 
   const defiEnabled =
     isEvmSelected &&
-    !isTestNet(chainId) &&
+    !enabledNetworksHasTestNet &&
     basicFunctionalityEnabled &&
     assetsDefiPositionsEnabled;
 
