b9b1f1e838215cd3fdc72929e5027e8a761d2c52
Matthew Walsh <matthew.walsh@consensys.net>
2025-08-25 21:05:27 +0100

    Fix linting

--- app/components/Views/confirmations/external/perps-temp/components/deposit/deposit.test.tsx
+++ app/components/Views/confirmations/external/perps-temp/components/deposit/deposit.test.tsx
@@ -68,6 +68,7 @@ describe('PerpsDeposit', () => {
 
     usePerpsDepositViewMock.mockReturnValue({
       isFullView: false,
+      isPayTokenSelected: false,
     });
 
     useTokenAssetMock.mockReturnValue({
