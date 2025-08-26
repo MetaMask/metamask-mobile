afd1a01c5554fa5eac15f622454b95315e218667
Curtis David <Curtis.David7@gmail.com>
2025-08-26 16:19:22 -0400

    address PR comments and add documentation in testing readme

--- appwright/appwright.config.ts
+++ appwright/appwright.config.ts
@@ -24,11 +24,11 @@ export default defineConfig({
       use: {
         platform: Platform.ANDROID,
         device: {
-          provider: 'emulator', // or 'local-device' or 'browserstack'
-          name: 'Samsung Galaxy S24 Ultra', // this can changed
-          osVersion: '14', // this can changed
+          provider: 'emulator',
+          name: 'Samsung Galaxy S24 Ultra', // this can be changed to your emulator name
+          osVersion: '14', // this can be changed to your emulator version
         },
-        buildPath: '/Users/javi/Downloads/app-qa-release.apk', // Path to your .apk file
+        buildPath: 'PATH-TO-BUILD', // Path to your .apk file
       },
     },
     {
@@ -36,10 +36,10 @@ export default defineConfig({
       use: {
         platform: Platform.IOS,
         device: {
-          provider: 'emulator', // or 'local-device' or 'browserstack'
-          osVersion: '16.0', // this can changed
+          provider: 'emulator',
+          osVersion: '16.0', // this can be changed to your simulator version
         },
-        buildPath: 'Metamask-QA.app', // Path to your .app file
+        buildPath: 'PATH-TO-BUILD', // Path to your .app file
       },
     },
     {
@@ -47,11 +47,11 @@ export default defineConfig({
       use: {
         platform: Platform.ANDROID,
         device: {
-          provider: 'browserstack', // or 'local-device' or 'browserstack'
+          provider: 'browserstack',
           name: process.env.BROWSERSTACK_DEVICE || 'Samsung Galaxy S23 Ultra', // this can changed
           osVersion: process.env.BROWSERSTACK_OS_VERSION || '13.0', // this can changed
         },
-        buildPath: process.env.BROWSERSTACK_ANDROID_APP_URL, // Path to Browserstack url bs:// link release-7.53.0-7.53.0-2223.apk
+        buildPath: process.env.BROWSERSTACK_ANDROID_APP_URL, // Path to Browserstack url
       },
     },
     {
@@ -63,7 +63,7 @@ export default defineConfig({
           name: process.env.BROWSERSTACK_DEVICE || 'iPhone 14 Pro Max',
           osVersion: process.env.BROWSERSTACK_OS_VERSION || '16.0',
         },
-        buildPath: process.env.BROWSERSTACK_IOS_APP_URL, // Path to Browserstack url bs:// link //release-7.53.0-7.53.0-2223.ipa
+        buildPath: process.env.BROWSERSTACK_IOS_APP_URL, // Path to Browserstack url
       },
     },
   ],
