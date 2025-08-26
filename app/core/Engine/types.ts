2cf8f40496d7a5662e18dc1c78cdf5a5dfb88155
Caainã Jeronimo <caainaje@gmail.com>
2025-08-26 17:14:05 -0300

    chore: remove duplicated type

--- app/core/Engine/types.ts
+++ app/core/Engine/types.ts
@@ -564,7 +564,6 @@ export type Controllers = {
   RewardsController: RewardsController;
   RewardsDataService: RewardsDataService;
   SeedlessOnboardingController: SeedlessOnboardingController<EncryptionKey>;
-  PredictController: PredictController;
 };
 
 /**
