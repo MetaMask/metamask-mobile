0eb9e187f842ae8bc11ecfb6bd1cceda23944e1a
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2025-08-27 11:05:52 +0100

    add tests for coverage

--- app/selectors/tokenList.test.ts
+++ app/selectors/tokenList.test.ts
@@ -1,4 +1,7 @@
-import { selectSortedTokenKeys } from './tokenList';
+import {
+  selectAssetsBySelectedAccountGroup,
+  selectSortedTokenKeys,
+} from './tokenList';
 import { selectTokenSortConfig } from './preferencesController';
 import { selectIsEvmNetworkSelected } from './multichainNetworkController';
 ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
@@ -22,6 +25,9 @@ jest.mock('./accountsController');
 jest.mock('./multichain');
 jest.mock('../store', () => ({
   store: { getState: jest.fn() },
+}));
+jest.mock('@metamask/assets-controllers', () => ({
+  selectAssetsBySelectedAccountGroup: jest.fn(),
 }));
 
 // This selector consumes many selectors and is very hard to create exact state
@@ -171,5 +177,76 @@ describe('selectSortedTokenKeys', () => {
     const result2 = selectSortedTokenKeys(mockState());
 
     expect(result1).not.toBe(result2);
+  });
+});
+
+describe('selectAssetsBySelectedAccountGroup', () => {
+  it('builds the initial state object', () => {
+    const {
+      selectAssetsBySelectedAccountGroup:
+        mockSelectAssetsBySelectedAccountGroup,
+    } = jest.requireMock('@metamask/assets-controllers');
+
+    const mockResult = {};
+    mockSelectAssetsBySelectedAccountGroup.mockReturnValue(mockResult);
+
+    const mockState = () =>
+      ({
+        engine: {
+          backgroundState: {
+            AccountTreeController: {
+              accountTree: {},
+            },
+            AccountsController: {
+              accounts: {},
+            },
+            TokensController: {
+              allTokens: {},
+            },
+            TokenBalancesController: {
+              tokenBalances: {},
+            },
+            TokenRatesController: {
+              tokenRates: {},
+            },
+            MultichainAssetsController: {
+              allAssets: {},
+            },
+            MultichainBalancesController: {
+              allBalances: {},
+            },
+            MultichainAssetsRatesController: {
+              allAssetsRates: {},
+            },
+            CurrencyRateController: {
+              currencyRates: {},
+            },
+            NetworkController: {
+              networks: {},
+            },
+            AccountTrackerController: {
+              accountsByChainId: {},
+            },
+          },
+        },
+      } as unknown as RootState);
+
+    const result = selectAssetsBySelectedAccountGroup(mockState());
+
+    expect(mockSelectAssetsBySelectedAccountGroup).toHaveBeenCalledWith({
+      accountTree: {},
+      accounts: {},
+      allTokens: {},
+      tokenBalances: {},
+      tokenRates: {},
+      allAssets: {},
+      allBalances: {},
+      allAssetsRates: {},
+      currencyRates: {},
+      networks: {},
+      accountsByChainId: {},
+    });
+
+    expect(result).toBe(mockResult);
   });
 });
