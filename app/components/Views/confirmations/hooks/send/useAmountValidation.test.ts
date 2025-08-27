d30def521aa1e550cce205397fa3b3ffad4b10e0
Jyoti Puri <jyotipuri@gmail.com>
2025-08-27 17:49:20 +0530

    Adding unit tests

--- app/components/Views/confirmations/hooks/send/useAmountValidation.test.ts
+++ app/components/Views/confirmations/hooks/send/useAmountValidation.test.ts
@@ -1,10 +1,28 @@
 import { renderHookWithProvider } from '../../../../../util/test/renderWithProvider';
+import { Nft } from '../../types/token';
 import { evmSendStateMock } from '../../__mocks__/send.mock';
-import { useAmountValidation } from './useAmountValidation';
+import {
+  useAmountValidation,
+  validateERC1155Balance,
+} from './useAmountValidation';
 
 const mockState = {
   state: evmSendStateMock,
 };
+
+describe('validateERC1155Balance', () => {
+  it('return error if amount is greater than balance and not otherwise', () => {
+    expect(
+      validateERC1155Balance({ balance: 5 } as unknown as Nft, '5'),
+    ).toEqual(undefined);
+    expect(
+      validateERC1155Balance({ balance: 5 } as unknown as Nft, '1'),
+    ).toEqual(undefined);
+    expect(
+      validateERC1155Balance({ balance: 5 } as unknown as Nft, '10'),
+    ).toEqual('Insufficient funds');
+  });
+});
 
 describe('useAmountValidation', () => {
   it('return field for amount error', () => {
