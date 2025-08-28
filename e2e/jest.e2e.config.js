25e88abfee4234603f03bb2c6885deebeb3555b5
Javier Briones <1674192+jvbriones@users.noreply.github.com>
2025-08-28 10:31:19 +0200

    test: add custom ci properties for the test reports

--- e2e/jest.e2e.config.js
+++ e2e/jest.e2e.config.js
@@ -31,7 +31,7 @@ module.exports = {
         outputDirectory: './e2e/reports',
         classNameTemplate: '{filepath}',
         addFileAttribute: 'true',
-        testCasePropertiesFile: './e2e/junitProperties.js',
+        testCasePropertiesFile: './e2e/junitProperties.js'
       },
     ],
   ],
