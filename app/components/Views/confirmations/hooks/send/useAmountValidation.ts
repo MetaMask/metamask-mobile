b3f14f9dc04cc6bb353873e63982ce4ac00116e8
Jyoti Puri <jyotipuri@gmail.com>
2025-08-28 15:26:27 +0530

    Fix in amount validation on send page

--- app/components/Views/confirmations/hooks/send/useAmountValidation.ts
+++ app/components/Views/confirmations/hooks/send/useAmountValidation.ts
@@ -1,6 +1,7 @@
 import { useCallback } from 'react';
 
 import { strings } from '../../../../../../locales/i18n';
+import { isDecimal } from '../../../../../util/number';
 import { useAsyncResult } from '../../../../hooks/useAsyncResult';
 import { Nft, TokenStandard } from '../../types/token';
 import { useSendContext } from '../../context/send-context';
@@ -24,6 +25,12 @@ export const useAmountValidation = () => {
   const { validateNonEvmAmount } = useNonEvmAmountValidation();
 
   const validateAmount = useCallback(async () => {
+    if (value === undefined || value === null || value === '') {
+      return undefined;
+    }
+    if (!isDecimal(value) || Number(value) < 0) {
+      return strings('invalidValue');
+    }
     if (asset?.standard === TokenStandard.ERC1155) {
       return validateERC1155Balance(asset as Nft, value);
     }
