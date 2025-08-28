0d62f7af7d41d03dcce8ba58eb721835d7e2e669
Goktug Poyraz <omergoktugpoyraz@gmail.com>
2025-08-28 15:36:17 +0200

    Update solana send flow tests

--- e2e/specs/solana/solana-send-flow.spec.ts
+++ e2e/specs/solana/solana-send-flow.spec.ts
@@ -44,20 +44,30 @@ describe(SmokeNetworkExpansion('Solana Token Transfer Functionality'), () => {
   });
 
   it('should import wallet with a Solana account', async () => {
-    await importWalletWithRecoveryPhrase({
-      seedPhrase: process.env.MM_SOLANA_E2E_TEST_SRP,
-    });
+    await withFixtures(
+      {
+        fixture: new FixtureBuilder().build(),
+        restartDevice: false,
+      },
+      async () => {
+        await importWalletWithRecoveryPhrase({
+          seedPhrase: process.env.MM_SOLANA_E2E_TEST_SRP,
+        });
 
-    await WalletView.tapCurrentMainWalletAccountActions();
-    await AccountListBottomSheet.tapAddAccountButton();
-    await AddAccountBottomSheet.tapAddSolanaAccount();
-    await AddNewHdAccountComponent.tapConfirm();
+        await WalletView.tapCurrentMainWalletAccountActions();
+        await AccountListBottomSheet.tapAddAccountButton();
+        await AddAccountBottomSheet.tapAddSolanaAccount();
+        await AddNewHdAccountComponent.tapConfirm();
 
-    await Assertions.expectElementToBeVisible(NetworkEducationModal.container);
-    await NetworkEducationModal.tapGotItButton();
+        await Assertions.expectElementToBeVisible(
+          NetworkEducationModal.container,
+        );
+        await NetworkEducationModal.tapGotItButton();
 
-    await WalletView.tapCurrentMainWalletAccountActions();
-    await AccountListBottomSheet.tapToSelectActiveAccountAtIndex(1);
+        await WalletView.tapCurrentMainWalletAccountActions();
+        await AccountListBottomSheet.tapToSelectActiveAccountAtIndex(1);
+      },
+    );
   });
 
   it('should validate recipient address format correctly', async () => {
