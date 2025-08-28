8b981a98c1b4fcf9929ebb5ff4df034308cd1576
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2025-08-28 14:55:39 -0400

    test: add address selector

--- app/components/Views/AddressSelector/AddressSelector.test.tsx
+++ app/components/Views/AddressSelector/AddressSelector.test.tsx
@@ -1,38 +1,15 @@
 import React from 'react';
+import Routes from '../../../constants/navigation/Routes';
 import { screen } from '@testing-library/react-native';
 import { renderScreen } from '../../../util/test/renderWithProvider';
-import Routes from '../../../constants/navigation/Routes';
 
-import { createMockInternalAccount } from '../../../util/test/accountsControllerTestUtils';
-import AddressSelector from './AddressSelector';
-import initialRootState from '../../../util/test/initial-root-state';
 import { AccountGroupId, AccountWalletId } from '@metamask/account-api';
 import { EthScope, SolAccountType, SolScope } from '@metamask/keyring-api';
-
-// const mockAccounts = [
-//   {
-//     id: internalAccount1.id,
-//     address: internalAccount1.address,
-//     balance: '0x0',
-//     name: internalAccount1.metadata.name,
-//   },
-//   {
-//     id: internalSolanaAccount1.id,
-//     address: internalSolanaAccount1.address,
-//     balance: '0x0',
-//     name: internalSolanaAccount1.metadata.name,
-//   },
-//   {
-//     id: internalAccount2.id,
-//     address: internalAccount2.address,
-//     balance: '0x0',
-//     name: internalAccount2.metadata.name,
-//   },
-// ];
-
-// const mockEnsByAccountAddress = {
-//   [internalAccount2.address]: 'test.eth',
-// };
+import { createMockInternalAccount } from '../../../util/test/accountsControllerTestUtils';
+import initialRootState from '../../../util/test/initial-root-state';
+import AddressSelector from './AddressSelector';
+import { AddressSelectorParams } from './AddressSelector.types';
+import { setReloadAccounts } from '../../../actions/accounts';
 
 const ACCOUNT_WALLET_ID = 'entropy:wallet-id-1' as AccountWalletId;
 const ACCOUNT_GROUP_ID = 'entropy:wallet-id-1/1' as AccountGroupId;
@@ -60,26 +37,6 @@ const mockInitialState = {
   engine: {
     backgroundState: {
       ...initialRootState.engine.backgroundState,
-      NetworkController: {
-        networkConfigurationsByChainId: {
-          0x1: {
-            chainId: '0x1',
-            name: 'Ethereum',
-          },
-          0xaa36a7: {
-            chainId: '0xaa36a7',
-            name: 'Sepolia Test Network',
-          },
-          0x2105: {
-            chainId: '0x2105',
-            name: 'Base',
-          },
-          0xa4b1: {
-            chainId: '0xa4b1',
-            name: 'Arbitrum One',
-          },
-        },
-      },
       AccountsController: {
         internalAccounts: {
           accounts: {
@@ -90,6 +47,7 @@ const mockInitialState = {
       },
       AccountTreeController: {
         accountTree: {
+          selectedAccountGroup: ACCOUNT_GROUP_ID,
           wallets: {
             [ACCOUNT_WALLET_ID]: {
               id: ACCOUNT_WALLET_ID,
@@ -104,25 +62,6 @@ const mockInitialState = {
           },
         },
       },
-      MultichainNetworkController: {
-        multichainNetworkConfigurationsByChainId: {
-          [SolScope.Mainnet]: {
-            name: 'Solana Mainnet',
-            chainId: SolScope.Mainnet,
-            isTestnet: false,
-          },
-          [SolScope.Testnet]: {
-            name: 'Solana Testnet',
-            chainId: SolScope.Testnet,
-            isTestnet: true,
-          },
-          [SolScope.Devnet]: {
-            name: 'Solana Devnet',
-            chainId: SolScope.Devnet,
-            isTestnet: true,
-          },
-        },
-      },
     },
   },
   accounts: {
@@ -130,65 +69,22 @@ const mockInitialState = {
   },
 };
 
-// Mock the Redux dispatch
-// const mockDispatch = jest.fn();
+jest.mock('../../hooks/useAccountName', () => ({
+  useAccountName: () => 'Mock Account Name',
+}));
 
-// jest.mock('react-redux', () => ({
-//   ...jest.requireActual('react-redux'),
-//   useDispatch: () => mockDispatch,
-//   useSelector: (selector: unknown) => {
-//     // Default mock state for selectors
-//     const mockState = {
-//       engine: {
-//         backgroundState: {
-//           KeyringController: MOCK_KEYRING_CONTROLLER_STATE_WITH_SOLANA,
-//           AccountsController: MOCK_ACCOUNTS_CONTROLLER_STATE_WITH_SOLANA,
-//           AccountTreeController: {
-//             accountTree: {
-//               wallets: {},
-//             },
-//           },
-//           PreferencesController: {
-//             privacyMode: false,
-//           },
-//         },
-//       },
-//       accounts: {
-//         reloadAccounts: false,
-//       },
-//       settings: {
-//         useBlockieIcon: false,
-//       },
-//     };
-//     return (selector as (mockState: unknown) => unknown)(mockState);
-//   },
-// }));
+const mockDispatch = jest.fn();
 
-// jest.mock('../../../core/Engine', () => {
-//   const {
-//     MOCK_ACCOUNTS_CONTROLLER_STATE: AccountsControllerState,
-//     MOCK_KEYRING_CONTROLLER_STATE: KeyringControllerState,
-//   } = jest.requireActual('../../../util/test/accountsControllerTestUtils');
-//   return {
-//     context: {
-//       KeyringController: {
-//         state: KeyringControllerState,
-//         importAccountWithStrategy: jest.fn(),
-//       },
-//       AccountsController: {
-//         state: {
-//           internalAccounts: AccountsControllerState.internalAccounts,
-//         },
-//       },
-//       AccountTreeController: {
-//         setSelectedAccountGroup: jest.fn(),
-//       },
-//     },
-//     setSelectedAddress: jest.fn(),
-//   };
-// });
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: () => mockDispatch,
+}));
 
-function render(Component: React.ComponentType, state = mockInitialState) {
+function render(
+  Component: React.ComponentType,
+  state = mockInitialState,
+  params: AddressSelectorParams | undefined = undefined,
+) {
   return renderScreen(
     Component,
     {
@@ -197,6 +93,7 @@ function render(Component: React.ComponentType, state = mockInitialState) {
     {
       state,
     },
+    params,
   );
 }
 
@@ -205,89 +102,45 @@ describe('AccountSelector', () => {
     jest.clearAllMocks();
   });
 
-  it('should render correctly', () => {
+  it('renders correctly and matches snapshot', () => {
     render(AddressSelector);
     expect(screen.toJSON()).toMatchSnapshot();
   });
 
-  // it('includes all accounts', () => {
-  //   const { queryByText } = renderScreen(
-  //     AccountSelectorWrapper,
-  //     {
-  //       name: Routes.SHEET.ACCOUNT_SELECTOR,
-  //     },
-  //     {
-  //       state: mockInitialState,
-  //     },
-  //     mockRoute.params,
-  //   );
+  it('includes only EVM addresses if isEvmOnly', () => {
+    const { queryAllByTestId } = render(AddressSelector, mockInitialState, {
+      isEvmOnly: true,
+    });
 
-  //   const accountsList = screen.getByTestId(
-  //     AccountListBottomSheetSelectorsIDs.ACCOUNT_LIST_ID,
-  //   );
+    const networkNames = queryAllByTestId(
+      'multichain-address-row-network-name',
+    ).map((node) => node.props.children);
 
-  //   expect(accountsList).toBeDefined();
-  //   expect(queryByText(internalAccount1.metadata.name)).toBeDefined();
-  //   expect(queryByText(internalSolanaAccount1.metadata.name)).toBeDefined();
-  //   expect(queryByText(internalAccount2.metadata.name)).toBeDefined();
-  // });
+    expect(networkNames).toEqual(['Ethereum Mainnet', 'Linea', 'Base Mainnet']);
+    expect(networkNames).not.toContain('Solana');
+  });
 
-  // it('includes only EVM accounts if isEvmOnly', () => {
-  //   const { queryByText } = renderScreen(
-  //     AccountSelectorWrapper,
-  //     {
-  //       name: Routes.SHEET.ACCOUNT_SELECTOR,
-  //     },
-  //     {
-  //       state: mockInitialState,
-  //     },
-  //     { ...mockRoute.params, isEvmOnly: true },
-  //   );
+  it('includes only addresses from specified CAIP chain IDs', () => {
+    const { queryAllByTestId } = render(AddressSelector, mockInitialState, {
+      displayOnlyCaipChainIds: ['eip155:59144', 'eip155:8453'],
+    });
 
-  //   const accountsList = screen.getByTestId(
-  //     AccountListBottomSheetSelectorsIDs.ACCOUNT_LIST_ID,
-  //   );
+    const networkNames = queryAllByTestId(
+      'multichain-address-row-network-name',
+    ).map((node) => node.props.children);
+    expect(networkNames).toEqual(['Linea', 'Base Mainnet']);
+    expect(networkNames).not.toContain('Ethereum Mainnet');
+    expect(networkNames).not.toContain('Solana');
+  });
 
-  //   expect(accountsList).toBeDefined();
-  //   expect(queryByText(internalAccount1.metadata.name)).toBeDefined();
-  //   expect(queryByText(internalSolanaAccount1.metadata.name)).toBeNull();
-  //   expect(queryByText(internalAccount2.metadata.name)).toBeDefined();
-  // });
-
-  // it('should display add account button', () => {
-  //   renderScreen(
-  //     AccountSelectorWrapper,
-  //     {
-  //       name: Routes.SHEET.ACCOUNT_SELECTOR,
-  //     },
-  //     {
-  //       state: mockInitialState,
-  //     },
-  //     mockRoute.params,
-  //   );
-
-  //   const addButton = screen.getByTestId(
-  //     AccountListBottomSheetSelectorsIDs.ACCOUNT_LIST_ADD_BUTTON_ID,
-  //   );
-  //   expect(addButton).toBeDefined();
-  // });
-
-  // it('renders account selector with multichain support', () => {
-  //   renderScreen(
-  //     AccountSelectorWrapper,
-  //     {
-  //       name: Routes.SHEET.ACCOUNT_SELECTOR,
-  //     },
-  //     {
-  //       state: mockInitialState,
-  //     },
-  //     mockRoute.params,
-  //   );
-
-  //   // Should render the account list (either multichain or EVM based on feature flag)
-  //   const accountsList = screen.getByTestId(
-  //     AccountListBottomSheetSelectorsIDs.ACCOUNT_LIST_ID,
-  //   );
-  //   expect(accountsList).toBeDefined();
-  // });
+  it('dispatches setReloadAccounts(false) if reloadAccounts is true in state', () => {
+    const stateWithReload = {
+      ...mockInitialState,
+      accounts: {
+        reloadAccounts: true,
+      },
+    };
+    render(AddressSelector, stateWithReload);
+    expect(mockDispatch).toHaveBeenCalledWith(setReloadAccounts(false));
+  });
 });
