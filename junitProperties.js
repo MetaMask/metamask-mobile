14019a3245cfef385f279f3283fa58fddceaea9c
Javier Briones <1674192+jvbriones@users.noreply.github.com>
2025-08-29 17:22:15 +0200

    test: add custom ci properties for the test reports

--- junitProperties.js
+++ junitProperties.js
@@ -2,6 +2,6 @@ module.exports = () => {
   return {
     JOB_NAME: process.env.JOB_NAME || '',
     RUN_ID: process.env.RUN_ID || '',
-    PR_NUMBER: process.env.PR_NUMBER || ''
+    PR_NUMBER: process.env.PR_NUMBER || '',
   };
 };
