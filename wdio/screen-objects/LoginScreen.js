661485911e8fb58d2a1065ef62c9ec934a7adb42
Curtis David <Curtis.David7@gmail.com>
2025-08-29 20:21:21 -0400

    fix lint

--- wdio/screen-objects/LoginScreen.js
+++ wdio/screen-objects/LoginScreen.js
@@ -1,35 +1,55 @@
-import Gestures from '../helpers/Gestures';
-import Selectors from '../helpers/Selectors';
-import { LoginViewSelectors } from '../../e2e/selectors/wallet/LoginView.selectors';
+import Gestures from "../helpers/Gestures";
+import Selectors from "../helpers/Selectors";
+import AppwrightSelectors from "../helpers/AppwrightSelectors";
+import { LoginViewSelectors } from "../../e2e/selectors/wallet/LoginView.selectors";
 
 class LoginScreen {
+  get device() {
+    return this._device;
+  }
+
+  set device(device) {
+    this._device = device;
+  }
+
   get loginScreen() {
     return Selectors.getXpathElementByResourceId(LoginViewSelectors.CONTAINER);
   }
 
   get resetWalletButton() {
-    return Selectors.getXpathElementByResourceId(LoginViewSelectors.RESET_WALLET);
+    return Selectors.getXpathElementByResourceId(
+      LoginViewSelectors.RESET_WALLET,
+    );
   }
 
   get passwordInput() {
+    // Always return the same selector for backward compatibility
+    // The actual element resolution will be handled in async methods
+    return Selectors.getXpathElementByResourceId(
+      LoginViewSelectors.PASSWORD_INPUT,
+    );
+  }
+
+  async getPasswordInputElement() {
     if (!this._device) {
-      return Selectors.getXpathElementByResourceId(LoginViewSelectors.PASSWORD_INPUT);
+      return Selectors.getXpathElementByResourceId(
+        LoginViewSelectors.PASSWORD_INPUT,
+      );
     } else {
-if (AppwrightSelectors.isAndroid(this._device)) {
-  return AppwrightSelectors.getElementByID(this._device, LoginViewSelectors.PASSWORD_INPUT);
-
+      if (AppwrightSelectors.isAndroid(this._device)) {
+        return await AppwrightSelectors.getElementByID(
+          this._device,
+          LoginViewSelectors.PASSWORD_INPUT,
+        );
       } else {
-        return  AppwrightSelectors.getElementByID(this._device, 'textfield');
-            
+        return await AppwrightSelectors.getElementByID(this._device, "textfield");
       }
-
-
     }
   }
 
   get unlockButton() {
     // TODO: update the component to have a testID property and use that instead of text
-    return Selectors.getXpathElementByText('Unlock');
+    return Selectors.getXpathElementByText("Unlock");
   }
 
   get title() {
@@ -37,7 +57,9 @@ if (AppwrightSelectors.isAndroid(this._device)) {
   }
 
   get rememberMeToggle() {
-    return Selectors.getXpathElementByResourceId(LoginViewSelectors.REMEMBER_ME_SWITCH);
+    return Selectors.getXpathElementByResourceId(
+      LoginViewSelectors.REMEMBER_ME_SWITCH,
+    );
   }
 
   async isLoginScreenVisible() {
@@ -54,7 +76,7 @@ if (AppwrightSelectors.isAndroid(this._device)) {
   }
 
   async typePassword(password) {
-    await this.isLoginScreenVisible()
+    await this.isLoginScreenVisible();
     await Gestures.typeText(this.passwordInput, password);
   }
 
