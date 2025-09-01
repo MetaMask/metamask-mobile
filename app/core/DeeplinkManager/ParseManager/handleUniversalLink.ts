854e006d53aa1bae3b21cc9b33ea0b63e35ecbad
João Loureiro <175489935+joaoloureirop@users.noreply.github.com>
2025-09-01 14:16:36 +0100

    add deeplink route handler back

--- app/core/DeeplinkManager/ParseManager/handleUniversalLink.ts
+++ app/core/DeeplinkManager/ParseManager/handleUniversalLink.ts
@@ -167,6 +167,9 @@ async function handleUniversalLink({
     // loops back to open the link with the right protocol
     instance.parse(deeplinkUrl, { origin: source });
     return;
+  } else if (action === SUPPORTED_ACTIONS.CREATE_ACCOUNT) {
+    const deeplinkUrl = urlObj.href.replace(BASE_URL_ACTION, '');
+    instance._handleCreateAccount(deeplinkUrl);
   } else if (action === SUPPORTED_ACTIONS.WC) {
     const { params } = extractURLParams(urlObj.href);
     const wcURL = params?.uri;
