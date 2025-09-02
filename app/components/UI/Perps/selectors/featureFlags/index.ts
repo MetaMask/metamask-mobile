344913283e9b733abe3cebe04ab7167f3bbdc242
Matthew Grainger <matthew.grainger@consensys.net>
2025-09-01 23:58:30 -0400

    feat: added Perps feature flag fallback to local flags in the event of connection or incorrectly configured flags

--- app/components/UI/Perps/selectors/featureFlags/index.ts
+++ app/components/UI/Perps/selectors/featureFlags/index.ts
@@ -3,27 +3,55 @@ import { selectRemoteFeatureFlags } from '../../../../../selectors/featureFlagCo
 import { hasMinimumRequiredVersion } from '../../../../../util/remoteFeatureFlag';
 import { PerpsLaunchDarklyFlag } from '../../types';
 
-const perpsRemoteFeatureFlag = (remoteFlag: PerpsLaunchDarklyFlag) =>
-  Boolean(remoteFlag?.enabled) &&
-  hasMinimumRequiredVersion(remoteFlag?.minimumVersion);
+// TODO: Add tests for fallback logic
+const perpsRemoteFeatureFlag = (remoteFlag: PerpsLaunchDarklyFlag) => {
+  // If failed to fetch remote flag or flag is misconfigured, return undefined to trigger fallback
+  if (
+    !remoteFlag ||
+    typeof remoteFlag.enabled !== 'boolean' ||
+    typeof remoteFlag.minimumVersion !== 'string'
+  ) {
+    return undefined;
+  }
+
+  return (
+    remoteFlag.enabled && hasMinimumRequiredVersion(remoteFlag.minimumVersion)
+  );
+};
+
+export const prioritizeFlagsByEnv = (
+  localFlag: boolean,
+  remoteFlag: PerpsLaunchDarklyFlag,
+) => {
+  if (process.env.METAMASK_ENVIRONMENT === 'production') {
+    // In production, use remote flag if available, otherwise fall back to local
+    return perpsRemoteFeatureFlag(remoteFlag) ?? localFlag;
+  }
+
+  // In development, use local flag.
+  return localFlag;
+};
 
 export const selectPerpsEnabledFlag = createSelector(
   selectRemoteFeatureFlags,
   (remoteFeatureFlags) => {
+    const localFlag = process.env.MM_PERPS_ENABLED === 'true';
     const remoteFlag =
       remoteFeatureFlags?.perpsPerpTradingEnabled as unknown as PerpsLaunchDarklyFlag;
 
-    return perpsRemoteFeatureFlag(remoteFlag);
+    return prioritizeFlagsByEnv(localFlag, remoteFlag);
   },
 );
 
 export const selectPerpsServiceInterruptionBannerEnabledFlag = createSelector(
   selectRemoteFeatureFlags,
   (remoteFeatureFlags) => {
+    const localFlag =
+      process.env.MM_PERPS_SERVICE_INTERRUPTION_BANNER_ENABLED === 'true';
     const remoteFlag =
       remoteFeatureFlags?.perpsPerpTradingServiceInterruptionBannerEnabled as unknown as PerpsLaunchDarklyFlag;
 
-    return perpsRemoteFeatureFlag(remoteFlag);
+    return prioritizeFlagsByEnv(localFlag, remoteFlag);
   },
 );
 
