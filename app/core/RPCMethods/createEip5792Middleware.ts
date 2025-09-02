72efaabfa08bc08100ad909f864ab7243f66c6e3
ffmcgee <joao.carlos@consensys.net>
2025-09-02 13:49:24 +0200

    docs: types for eip5792 middleware

--- app/core/RPCMethods/createEip5792Middleware.ts
+++ app/core/RPCMethods/createEip5792Middleware.ts
@@ -1,11 +1,21 @@
-import { createWalletMiddleware } from '@metamask/eth-json-rpc-middleware';
+import {
+  createWalletMiddleware,
+  ProcessSendCallsHook,
+  GetCapabilitiesHook,
+  GetCallsStatusHook,
+} from '@metamask/eth-json-rpc-middleware';
+import { JsonRpcRequest } from '@metamask/utils';
 
-// TODO: [ffmcgee] export types from package to simplify function typing here
 export function createEip5792Middleware({
   getAccounts,
   getCallsStatus,
   getCapabilities,
   processSendCalls,
+}: {
+  getAccounts: (req: JsonRpcRequest) => Promise<string[]>;
+  getCallsStatus: GetCallsStatusHook;
+  getCapabilities: GetCapabilitiesHook;
+  processSendCalls: ProcessSendCallsHook;
 }) {
   return createWalletMiddleware({
     getAccounts,
