59b1da665f336992b5a02ba63b1590f12fa0072f
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2025-09-02 12:22:37 -0400

    fix(ramp): adjust navbar ui cp-7.55.0 (#19068)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    This PR removes the selected network from order details and send
    transaction screens in buy/sell orders.
    
    ## **Changelog**
    
    <!--
    If this PR is not End-User-Facing and should not show up in the
    CHANGELOG, you can choose to either:
    1. Write `CHANGELOG entry: null`
    2. Label with `no-changelog`
    
    If this PR is End-User-Facing, please write a short User-Facing
    description in the past tense like:
    `CHANGELOG entry: Added a new tab for users to see their NFTs`
    `CHANGELOG entry: Fixed a bug that was causing some NFTs to flicker`
    
    (This helps the Release Engineer do their job more quickly and
    accurately)
    -->
    
    CHANGELOG entry:
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    ```gherkin
    Feature: my feature name
    
      Scenario: user [verb for user action]
        Given [describe expected initial app state]
    
        When user [verb for user action]
        Then [describe expected outcome]
    ```
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/UI/Ramp/Aggregator/Views/SendTransaction/SendTransaction.tsx
+++ app/components/UI/Ramp/Aggregator/Views/SendTransaction/SendTransaction.tsx
@@ -112,6 +112,7 @@ function SendTransaction() {
             'fiat_on_ramp_aggregator.send_transaction.sell_crypto',
           ),
           showCancel: false,
+          showNetwork: false,
         },
         colors,
       ),
