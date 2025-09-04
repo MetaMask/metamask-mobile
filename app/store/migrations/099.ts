97ee0dff82d7a98e7a8b0535fee31da4896d0fd2
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-09-04 14:03:14 -0700

    chore: add migration for old txHistory entries

---
+++ app/store/migrations/099.ts
@@ -0,0 +1,69 @@
+import { captureException } from '@sentry/react-native';
+import { ensureValidState } from './util';
+import { isObject } from '@metamask/utils';
+import { BridgeStatusControllerState } from '@metamask/bridge-status-controller';
+import { isSolanaChainId } from '@metamask/bridge-controller';
+
+/**
+ * Migration: Update bridge txHistory for solana to use txHash as key and txMetaId
+ */
+const migration = async (state: unknown): Promise<unknown> => {
+  const migrationVersion = 99;
+
+  // Ensure the state is valid for migration
+  if (!ensureValidState(state, migrationVersion)) {
+    return state;
+  }
+
+  try {
+    const bridgeStatusControllerState =
+      state.engine.backgroundState.BridgeStatusController;
+
+    if (!isObject(bridgeStatusControllerState)) {
+      return state;
+    }
+
+    if (!isObject(bridgeStatusControllerState.txHistory)) {
+      return state;
+    }
+
+    const { txHistory } =
+      bridgeStatusControllerState as BridgeStatusControllerState;
+
+    if (!isObject(txHistory)) {
+      return state;
+    }
+
+    Object.entries(txHistory).forEach(([key, historyItem]) => {
+      const srcChainId =
+        historyItem.status?.srcChain?.chainId ?? historyItem.quote?.srcChainId;
+      const isSolanaTx = isSolanaChainId(srcChainId);
+      if (isSolanaTx) {
+        // If solana tx, use the src chain tx hash as the key
+        const newId = isSolanaTx
+          ? historyItem.status?.srcChain?.txHash
+          : undefined;
+
+        if (newId) {
+          // If txHash is defined, update the txHistory with the new key and txMetaId
+          txHistory[newId] = {
+            ...historyItem,
+            txMetaId: newId,
+          };
+          delete txHistory[key];
+        }
+      }
+    });
+
+    return state;
+  } catch (error) {
+    captureException(
+      new Error(
+        `Migration ${migrationVersion}: Failed to update bridge txHistory for solana to use txHash as key and txMetaId. Error: ${error}`,
+      ),
+    );
+    return state;
+  }
+};
+
+export default migration;
