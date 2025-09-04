876106cdfd80bbea198e3a86718f9a827601b091
Charly Chevalier <charly.chevalier@consensys.net>
2025-09-04 21:20:54 +0200

    fix: force Snap keyring instantiation

--- app/util/discovery.ts
+++ app/util/discovery.ts
@@ -4,6 +4,9 @@ import { MultichainAccountWallet } from '@metamask/multichain-account-service';
 import Engine from '../core/Engine';
 
 export async function discoverAndCreateAccountsFor(entropySources: EntropySourceId[]): Promise<void> {
+  // HACK: Force Snap keyring instantiation.
+  await Engine.getSnapKeyring();
+
   await Promise.all(entropySources.map(async (entropySource) => {
     const wallet = Engine.context.MultichainAccountService.getMultichainAccountWallet({ entropySource }) as MultichainAccountWallet<Bip44Account<KeyringAccount>>;
 
