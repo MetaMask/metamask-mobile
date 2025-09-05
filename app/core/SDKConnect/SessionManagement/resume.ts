79c96f9deec689e756c62e8c65073974da0687be
Javier Ribó <javi.ribo@consensys.net>
2025-09-05 14:47:13 +0200

    fix: improve pausing and resuming of websocket sessions + proper deeplink management

--- app/core/SDKConnect/SessionManagement/resume.ts
+++ app/core/SDKConnect/SessionManagement/resume.ts
@@ -1,6 +1,5 @@
 import SDKConnect from '../SDKConnect';
 import DevLogger from '../utils/DevLogger';
-import { wait } from '../utils/wait.util';
 
 async function resume({
   channelId,
@@ -9,32 +8,16 @@ async function resume({
   channelId: string;
   instance: SDKConnect;
 }) {
-  const session = instance.state.connected[channelId]?.remote;
-  const alreadyResumed = instance.state.connected[channelId].isResumed ?? false;
-
-  DevLogger.log(
-    `SDKConnect::resume channel=${channelId} alreadyResumed=${alreadyResumed} session=${session} paused=${session.isPaused()} connected=${session?.isConnected()} connecting=${
-      instance.state.connecting[channelId]
-    }`,
-  );
-  if (
-    session &&
-    !session?.isConnected() &&
-    !alreadyResumed &&
-    !instance.state.connecting[channelId]
-  ) {
-    instance.state.connected[channelId].resume();
-    await wait(500); // Some devices (especially android) need time to update socket status after resuming.
+  const connection = instance.state.connected[channelId];
+  if (!connection) {
     DevLogger.log(
-      `SDKConnect::_handleAppState - done resuming - socket_connected=${instance.state.connected[
-        channelId
-      ].remote.isConnected()}`,
+      `SDKConnect::resume - No connection found for channel ${channelId}, skipping resume.`,
     );
-  } else {
-    DevLogger.log(
-      `SDKConnect::_handleAppState - SKIP - connection.resumed=${instance.state.connected[channelId]?.isResumed}`,
-    );
+    return;
   }
+  DevLogger.log(`SDKConnect::resume - Resuming connection ${channelId}`);
+  // Unconditionally call resume on the connection object.
+  connection.resume();
 }
 
 export default resume;
