78b5ca7167e2eae2262f29f84de59d60bd7dba01
Pavel Dvorkin <pavel.dvorkin@consensys.net>
2025-09-05 20:08:15 -0400

    INFRA-2911: Hardcoded parameters values for create release pull request

--- .github/scripts/resolve-previous-ref.sh
+++ .github/scripts/resolve-previous-ref.sh
@@ -35,12 +35,11 @@ fetch_matching_refs() {
   echo "$temp_file"
 }
 
-# Fetch for each prefix
-version_v_file=$(fetch_matching_refs "Version-v")
+# Fetch only release/ branches
 release_file=$(fetch_matching_refs "release/")
 
-# Combine and process: extract {name, semver} for matches, sort desc by semver
-jq -s 'add | [ .[] | .ref | ltrimstr("refs/heads/") as $name | select($name | test("^Version-v[0-9]+\\.[0-9]+\\.[0-9]+$") or test("^release/[0-9]+\\.[0-9]+\\.[0-9]+$")) | {name: $name, semver: (if $name | test("^Version-v") then $name | ltrimstr("Version-v") else $name | ltrimstr("release/") end) } ] | sort_by( .semver | split(".") | map(tonumber) ) | reverse' "$version_v_file" "$release_file" > all_versions.json
+# Process: extract {name, semver} for matches, sort desc by semver
+jq -s 'add | [ .[] | .ref | ltrimstr("refs/heads/") as $name | select($name | test("^release/[0-9]+\\.[0-9]+\\.[0-9]+$")) | {name: $name, semver: $name | ltrimstr("release/") } ] | sort_by( .semver | split(".") | map(tonumber) ) | reverse' "$release_file" > all_versions.json
 
 # Filter to those with semver strictly lower than current and non-hotfix (patch==0)
 jq --arg semver "$semver" '[ .[] | select( .semver as $v | $semver | split(".") as $c | $v | split(".") as $p | ( ($p[0] | tonumber) < ($c[0] | tonumber) or (($p[0] | tonumber) == ($c[0] | tonumber) and (($p[1] | tonumber) < ($c[1] | tonumber) or (($p[1] | tonumber) == ($c[1] | tonumber) and ($p[2] | tonumber) < ($c[2] | tonumber)))) ) and (($p[2] | tonumber) == 0) ) ]' all_versions.json > filtered_versions.json
