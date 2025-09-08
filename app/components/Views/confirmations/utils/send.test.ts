406c594fe2f2df0508b8b5a5537434a13b73c03c
Jyoti Puri <jyotipuri@gmail.com>
2025-09-08 16:20:46 +0530

    update

--- app/components/Views/confirmations/utils/send.test.ts
+++ app/components/Views/confirmations/utils/send.test.ts
@@ -290,6 +290,5 @@ describe('convertCurrency', () => {
     expect(convertCurrency('120.75', 0.5, 4, 2)).toEqual('60.37');
     expect(convertCurrency('120.75', 0.25, 4, 4)).toEqual('30.1875');
     expect(convertCurrency('0.01', 10, 4, 0)).toEqual('60.37');
-    expect(convertCurrency('0.01', 10, 4, 0)).toEqual('0');
   });
 });
