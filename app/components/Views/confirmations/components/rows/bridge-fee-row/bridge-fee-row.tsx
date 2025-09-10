dd05ebf395fffe1b12aef4cf35ac7c75089731fc
Matthew Walsh <matthew.walsh@consensys.net>
2025-09-10 21:39:10 +0100

    Add transaction fee row

--- app/components/Views/confirmations/components/rows/bridge-fee-row/bridge-fee-row.tsx
+++ app/components/Views/confirmations/components/rows/bridge-fee-row/bridge-fee-row.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import AnimatedSpinner, {
   SpinnerSize,
 } from '../../../../../UI/AnimatedSpinner';
@@ -10,14 +10,21 @@ import {
 } from '../../../../../../core/redux/slices/confirmationMetrics';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../../../../reducers';
-import Text from '../../../../../../component-library/components/Texts/Text';
+import Text, {
+  TextColor,
+} from '../../../../../../component-library/components/Texts/Text';
 import { useTransactionTotalFiat } from '../../../hooks/pay/useTransactionTotalFiat';
 import { strings } from '../../../../../../../locales/i18n';
 import { TransactionType } from '@metamask/transaction-controller';
+import useFiatFormatter from '../../../../../UI/SimulationDetails/FiatDisplay/useFiatFormatter';
+import { BigNumber } from 'bignumber.js';
+import { Box } from '../../../../../UI/Box/Box';
+import { FlexDirection, JustifyContent } from '../../../../../UI/Box/box.types';
 
 export function BridgeFeeRow() {
   const { id: transactionId, type } = useTransactionMetadataOrThrow();
-  const { totalBridgeFeeFormatted } = useTransactionTotalFiat();
+  const { totalTransactionFeeFormatted } = useTransactionTotalFiat();
+  const fiatFormatter = useFiatFormatter();
 
   const isQuotesLoading = useSelector((state: RootState) =>
     selectIsTransactionBridgeQuotesLoadingById(state, transactionId),
@@ -34,24 +41,61 @@ export function BridgeFeeRow() {
   }
 
   return (
-    <InfoRow
-      label={strings('confirm.label.bridge_fee')}
-      tooltip={getTooltip(type)}
-    >
-      {isQuotesLoading ? (
-        <AnimatedSpinner size={SpinnerSize.SM} />
-      ) : (
-        <Text>{totalBridgeFeeFormatted}</Text>
-      )}
-    </InfoRow>
+    <>
+      <InfoRow
+        label={strings('confirm.label.bridge_fee')}
+        tooltip={getTooltip(type)}
+        tooltipTitle="Fees"
+      >
+        {isQuotesLoading ? (
+          <AnimatedSpinner size={SpinnerSize.SM} />
+        ) : (
+          <Text>{totalTransactionFeeFormatted}</Text>
+        )}
+      </InfoRow>
+      <InfoRow label={strings('confirm.label.metamask_fee')}>
+        {isQuotesLoading ? (
+          <AnimatedSpinner size={SpinnerSize.SM} />
+        ) : (
+          <Text>{fiatFormatter(new BigNumber(0))}</Text>
+        )}
+      </InfoRow>
+    </>
   );
 }
 
-function getTooltip(type?: TransactionType) {
+function getTooltip(type?: TransactionType): ReactNode {
   switch (type) {
     case TransactionType.perpsDeposit:
-      return strings('confirm.tooltip.perps_deposit.bridge_fee');
+      return <FeesTooltip />;
     default:
       return undefined;
   }
+}
+
+function FeesTooltip() {
+  const { totalBridgeFeeFormatted, totalNativeEstimatedFormatted } =
+    useTransactionTotalFiat();
+
+  return (
+    <Box gap={14}>
+      <Text>{strings('confirm.tooltip.perps_deposit.bridge_fee')}</Text>
+      <Box
+        flexDirection={FlexDirection.Row}
+        justifyContent={JustifyContent.spaceBetween}
+      >
+        <Text color={TextColor.Alternative}>Network fee</Text>
+        <Text color={TextColor.Alternative}>
+          {totalNativeEstimatedFormatted}
+        </Text>
+      </Box>
+      <Box
+        flexDirection={FlexDirection.Row}
+        justifyContent={JustifyContent.spaceBetween}
+      >
+        <Text color={TextColor.Alternative}>Bridge provider fee</Text>
+        <Text color={TextColor.Alternative}>{totalBridgeFeeFormatted}</Text>
+      </Box>
+    </Box>
+  );
 }
