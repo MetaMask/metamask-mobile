044f0e287af5f8511c447024b2d7f0579fac1c3d
Curtis David <Curtis.David7@gmail.com>
2025-09-11 18:49:20 -0400

    remove commented code

--- appwright/tests/performance/scenario2.spec.js
+++ appwright/tests/performance/scenario2.spec.js
@@ -57,16 +57,6 @@ test('Onboarding new wallet, SRP 1 + SRP 2 + SRP 3', async ({
     'Time since the user clicks on "Done" button until Solana feature sheet is visible',
   );
 
-  // timer1.start();
-  // await WelcomeScreen.clickGetStartedButton();
-  // await TermOfUseScreen.isDisplayed();
-  // timer1.stop();
-  // await TermOfUseScreen.tapAgreeCheckBox();
-  // await TermOfUseScreen.tapScrollEndButton();
-  // timer2.start();
-  // await TermOfUseScreen.tapAcceptButton();
-  // await OnboardingScreen.isScreenTitleVisible();
-  // timer2.stop();
   timer3.start();
   await OnboardingScreen.tapCreateNewWalletButton();
   await OnboardingSheet.isVisible();
