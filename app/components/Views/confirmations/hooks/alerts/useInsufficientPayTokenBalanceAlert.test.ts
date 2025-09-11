85ff4b82f1dbe8034ecf3a757775b7c412457265
Matthew Walsh <matthew.walsh@consensys.net>
2025-09-12 00:03:22 +0100

    Add alternate message to balance alert

--- app/components/Views/confirmations/hooks/alerts/useInsufficientPayTokenBalanceAlert.test.ts
+++ app/components/Views/confirmations/hooks/alerts/useInsufficientPayTokenBalanceAlert.test.ts
@@ -45,6 +45,41 @@ describe('useInsufficientPayTokenBalanceAlert', () => {
     } as ReturnType<typeof useTransactionPayTokenAmounts>);
 
     useTransactionPayTokenMock.mockReturnValue({
+      payToken: { balance: '123.455', symbol: 'TST' },
+    } as ReturnType<typeof useTransactionPayToken>);
+
+    const { result } = runHook();
+
+    expect(result.current).toEqual([
+      {
+        key: AlertKeys.InsufficientPayTokenBalance,
+        field: RowAlertKey.Amount,
+        message: strings(
+          'alert_system.insufficient_pay_token_balance_fees.message',
+          {
+            symbol: 'TST',
+          },
+        ),
+        title: strings(
+          'alert_system.insufficient_pay_token_balance_fees.title',
+        ),
+        severity: Severity.Danger,
+        isBlocking: true,
+      },
+    ]);
+  });
+
+  it('returns alert if balance less than token amount', () => {
+    useTransactionPayTokenAmountsMock.mockReturnValue({
+      amounts: [
+        {
+          amountHumanOriginal: '123.456',
+        },
+      ],
+      totalHuman: '123.457',
+    } as ReturnType<typeof useTransactionPayTokenAmounts>);
+
+    useTransactionPayTokenMock.mockReturnValue({
       payToken: { balance: '123.455' },
     } as ReturnType<typeof useTransactionPayToken>);
 
