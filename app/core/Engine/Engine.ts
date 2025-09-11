c6aed31d42365fababbcd470b37251508eb08619
Vince Howard <vincenguyenhoward@gmail.com>
2025-09-11 17:15:57 -0600

    fix: cp-7.55.0 initialize network enablement controller (#19204)
    Initialize network enablement controller to get correct namespaces based
    off of multichain network controller configurations
    
    **Problem:** When running the Flask build we get a `Missing chain id`
    error. This is because the network enablement controller state for
    `bip22` is an empty object and is missing the
    `bip122:000000000019d6689c085ae165831e93` caip chain id.
    
    **Solution:** Initialize the network enablement controller based off of
    the multichain network controller config. There is a separate PR
    [here](https://github.com/MetaMask/core/pull/6455) for core that enabled
    that. For now we will just use a patch
    
    **Update** Controller has been released and patch is no longer needed
    
    CHANGELOG entry: null
    
    Fixes: https://github.com/MetaMask/metamask-mobile/issues/19201
    
    ```gherkin
    Feature: Network Enablement Controller
    
      Scenario: user logs in
        Given they are on Flask build and has a bitcoin address
        Then user switches to bitcoin
        Then nothing happens and app doesn't crash
    ```
    
    `~`
    
    <img width="350" alt="image"
    src="https://github.com/user-attachments/assets/276a990d-e6ce-4231-ab40-29b69e2a66a2"
    />
    
    <img width="350"
    alt="simulator_screenshot_25C53C66-6164-40F9-A881-C2F262FE9AFE"
    src="https://github.com/user-attachments/assets/d508d640-b12b-4edd-8b3c-ea2c9e7a42c4"
    />
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: Antonio Regadas <antonio.regadas@consensys.net>
    Co-authored-by: Alejandro Garcia Anglada <aganglada@gmail.com>

--- app/core/Engine/Engine.ts
+++ app/core/Engine/Engine.ts
@@ -1265,6 +1265,10 @@ export class Engine {
     const multichainAccountService = controllersByName.MultichainAccountService;
     ///: END:ONLY_INCLUDE_IF
 
+    const networkEnablementController =
+      controllersByName.NetworkEnablementController;
+    networkEnablementController.init();
+
     ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
     const multichainRatesControllerMessenger =
       this.controllerMessenger.getRestricted({
@@ -1277,9 +1281,6 @@ export class Engine {
       messenger: multichainRatesControllerMessenger,
       initialState: initialState.RatesController,
     });
-
-    const networkEnablementController =
-      controllersByName.NetworkEnablementController;
 
     // Set up currency rate sync
     setupCurrencyRateSync(
