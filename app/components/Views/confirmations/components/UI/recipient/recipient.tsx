ad7a0064bd23fcd2bab8b5ae7132a993850fd4e1
sethkfman <Seth.Kaufman@consensys.net>
2025-09-17 14:03:37 -0600

    remove the double wrap of the button element

--- app/components/Views/confirmations/components/UI/recipient/recipient.tsx
+++ app/components/Views/confirmations/components/UI/recipient/recipient.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback } from 'react';
-import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 
 import {
   Box,
@@ -17,7 +16,6 @@ import { AvatarAccountType } from '../../../../../../component-library/component
 import { formatAddress } from '../../../../../../util/address';
 import styleSheet from './recipient.styles';
 import { useStyles } from '../../../../../hooks/useStyles';
-import { noop } from 'lodash';
 
 export interface RecipientType {
   address: string;
@@ -48,57 +46,51 @@ export function Recipient({
     onPress?.(recipient);
   }, [recipient, onPress]);
 
-  const tap = Gesture.Tap()
-    .runOnJS(true)
-    .onEnd(() => handlePressRecipient());
-
   return (
-    <GestureDetector gesture={tap}>
-      <ButtonBase
-        testID={
-          isSelected
-            ? `selected-${recipient.address}`
-            : `recipient-${recipient.address}`
-        }
-        style={tw.style(
-          'w-full flex-row items-center justify-between py-2 px-4',
-          isSelected ? 'bg-pressed' : 'bg-transparent',
-        )}
-        onPress={noop}
-        label={
-          <Box twClassName="flex-row items-center">
-            <Box twClassName="h-12 justify-center">
-              <Avatar
-                variant={AvatarVariant.Account}
-                type={accountAvatarType}
-                accountAddress={recipient.address}
-                size={AvatarSize.Md}
-              />
-            </Box>
-
-            <Box twClassName="ml-4 h-12 justify-center">
-              <Text
-                testID={`recipient-name-${recipient.address}`}
-                variant={TextVariant.BodyMd}
-                fontWeight={FontWeight.Medium}
-                numberOfLines={1}
-              >
-                {isBIP44
-                  ? recipient.accountGroupName || recipient.contactName
-                  : recipient.accountName || recipient.contactName}
-              </Text>
-              <Text
-                testID={`recipient-address-${recipient.address}`}
-                variant={TextVariant.BodyMd}
-                style={styles.recipientAddress}
-                numberOfLines={1}
-              >
-                {formatAddress(recipient.address, 'short')}
-              </Text>
-            </Box>
+    <ButtonBase
+      testID={
+        isSelected
+          ? `selected-${recipient.address}`
+          : `recipient-${recipient.address}`
+      }
+      style={tw.style(
+        'w-full flex-row items-center justify-between py-2 px-4',
+        isSelected ? 'bg-pressed' : 'bg-transparent',
+      )}
+      onPress={handlePressRecipient}
+      label={
+        <Box twClassName="flex-row items-center">
+          <Box twClassName="h-12 justify-center">
+            <Avatar
+              variant={AvatarVariant.Account}
+              type={accountAvatarType}
+              accountAddress={recipient.address}
+              size={AvatarSize.Md}
+            />
           </Box>
-        }
-      />
-    </GestureDetector>
+
+          <Box twClassName="ml-4 h-12 justify-center">
+            <Text
+              testID={`recipient-name-${recipient.address}`}
+              variant={TextVariant.BodyMd}
+              fontWeight={FontWeight.Medium}
+              numberOfLines={1}
+            >
+              {isBIP44
+                ? recipient.accountGroupName || recipient.contactName
+                : recipient.accountName || recipient.contactName}
+            </Text>
+            <Text
+              testID={`recipient-address-${recipient.address}`}
+              variant={TextVariant.BodyMd}
+              style={styles.recipientAddress}
+              numberOfLines={1}
+            >
+              {formatAddress(recipient.address, 'short')}
+            </Text>
+          </Box>
+        </Box>
+      }
+    />
   );
 }
