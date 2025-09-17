2c0fd32567c63ae41213c35af608f10f3402ba60
Edouard Bougon <15703023+EdouardBougon@users.noreply.github.com>
2025-09-17 12:53:27 +0200

    feat: fix "return to app" modal

--- app/constants/navigation/Routes.ts
+++ app/constants/navigation/Routes.ts
@@ -162,7 +162,6 @@ const Routes = {
     CONNECTION_DETAILS: 'ConnectionDetails',
     PERMITTED_NETWORKS_INFO_SHEET: 'PermittedNetworksInfoSheet',
     NETWORK_SELECTOR: 'NetworkSelector',
-    RETURN_TO_DAPP_MODAL: 'ReturnToDappModal',
     ACCOUNT_ACTIONS: 'AccountActions',
     FIAT_ON_TESTNETS_FRICTION: 'SettingsAdvancedFiatOnTestnetsFriction',
     SHOW_IPFS: 'ShowIpfs',
@@ -331,6 +330,9 @@ const Routes = {
     ASSET: 'Asset',
     AMOUNT: 'Amount',
     DEFAULT: 'Send',
+  },
+  SDK: {
+    RETURN_TO_DAPP_TOAST: 'ReturnToDappToast',
   },
 };
 
