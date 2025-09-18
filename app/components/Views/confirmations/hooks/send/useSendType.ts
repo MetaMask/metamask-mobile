db2553ad9348436d3ae947da4338f7e976b4a676
Alejandro Garcia <alejandro.garcia@consensys.net>
2025-09-18 13:33:16 +0200

    feat: bitcoin bip44 integration

--- app/components/Views/confirmations/hooks/send/useSendType.ts
+++ app/components/Views/confirmations/hooks/send/useSendType.ts
@@ -1,8 +1,14 @@
 import { isAddress as isEvmAddress } from 'ethers/lib/utils';
-import { isSolanaChainId } from '@metamask/bridge-controller';
+import {
+  /// BEGIN:ONLY_INCLUDE_IF(bitcoin)
+  isBitcoinChainId,
+  /// END:ONLY_INCLUDE_IF
+  isSolanaChainId,
+} from '@metamask/bridge-controller';
 import { useMemo } from 'react';
 
 import { useSendContext } from '../../context/send-context';
+import { isNonEvmChainId } from '../../../../../core/Multichain/utils';
 
 export const useSendType = () => {
   const { asset } = useSendContext();
@@ -10,10 +16,23 @@ export const useSendType = () => {
     () => (asset?.address ? isEvmAddress(asset.address) : undefined),
     [asset?.address],
   );
+  const isNonEvmSendType = useMemo(
+    () => (asset?.chainId ? isNonEvmChainId(asset.chainId) : undefined),
+    [asset?.chainId],
+  );
+
   const isSolanaSendType = useMemo(
     () => (asset?.chainId ? isSolanaChainId(asset.chainId) : undefined),
     [asset?.chainId],
   );
+
+  /// BEGIN:ONLY_INCLUDE_IF(bitcoin)
+  const isBitcoinSendType = useMemo(
+    () => (asset?.chainId ? isBitcoinChainId(asset.chainId) : undefined),
+    [asset?.chainId],
+  );
+  /// END:ONLY_INCLUDE_IF
+
   const assetIsNative =
     asset && 'isNative' in asset ? Boolean(asset.isNative) : undefined;
 
@@ -21,10 +40,21 @@ export const useSendType = () => {
     () => ({
       isEvmSendType,
       isEvmNativeSendType: isEvmSendType && assetIsNative,
-      isNonEvmSendType: isSolanaSendType,
-      isNonEvmNativeSendType: isSolanaSendType && assetIsNative,
+      isNonEvmNativeSendType: isNonEvmSendType && assetIsNative,
+      isNonEvmSendType,
       isSolanaSendType,
+      /// BEGIN:ONLY_INCLUDE_IF(bitcoin)
+      isBitcoinSendType,
+      /// END:ONLY_INCLUDE_IF
     }),
-    [isEvmSendType, isSolanaSendType, assetIsNative],
+    [
+      isEvmSendType,
+      isNonEvmSendType,
+      assetIsNative,
+      isSolanaSendType,
+      /// BEGIN:ONLY_INCLUDE_IF(bitcoin)
+      isBitcoinSendType,
+      /// END:ONLY_INCLUDE_IF
+    ],
   );
 };
--- app/core/Engine/controllers/multichain-account-service/multichain-account-service-init.ts
+++ app/core/Engine/controllers/multichain-account-service/multichain-account-service-init.ts
@@ -3,6 +3,9 @@ import {
   MultichainAccountServiceMessenger,
 } from '@metamask/multichain-account-service';
 import { ControllerInitFunction } from '../../types';
+/// BEGIN:ONLY_INCLUDE_IF(bitcoin)
+import { BitcoinAccountProvider } from './providers/BitcoinAccountProvider';
+/// END:ONLY_INCLUDE_IF
 
 /**
  * Initialize the multichain account service.
@@ -17,6 +20,9 @@ export const multichainAccountServiceInit: ControllerInitFunction<
 > = ({ controllerMessenger }) => {
   const controller = new MultichainAccountService({
     messenger: controllerMessenger,
+    /// BEGIN:ONLY_INCLUDE_IF(bitcoin)
+    providers: [new BitcoinAccountProvider(controllerMessenger)],
+    /// END:ONLY_INCLUDE_IF
   });
 
   return { controller, memStateKey: null, persistedStateKey: null };
