69812d041a5d7451d1112ab3a0778d1b5260c3b6
Wei Sun <wei.sun@consensys.net>
2025-09-17 17:55:48 -0700

    fix Row styling

--- app/components/Views/confirmations/components/UI/recipient/recipient.tsx
+++ app/components/Views/confirmations/components/UI/recipient/recipient.tsx
@@ -5,14 +5,13 @@ import {
   FontWeight,
   Text,
   TextVariant,
-} from '@metamask/design-system-react-native';
+ ButtonBase } from '@metamask/design-system-react-native';
 import { useTailwind } from '@metamask/design-system-twrnc-preset';
 import Avatar, {
   AvatarSize,
   AvatarVariant,
 } from '../../../../../../component-library/components/Avatars/Avatar';
 import { AvatarAccountType } from '../../../../../../component-library/components/Avatars/Avatar/variants/AvatarAccount';
-import ButtonBase from '../../../../../../component-library/components/Buttons/Button/foundation/ButtonBase';
 import { formatAddress } from '../../../../../../util/address';
 import styleSheet from './recipient.styles';
 import { useStyles } from '../../../../../hooks/useStyles';
@@ -53,44 +52,45 @@ export function Recipient({
           ? `selected-${recipient.address}`
           : `recipient-${recipient.address}`
       }
-      style={tw.style(
-        'w-full flex-row items-center justify-between py-2 px-4',
-        isSelected ? 'bg-pressed' : 'bg-transparent',
-      )}
-      onPress={handlePressRecipient}
-      label={
-        <Box twClassName="flex-row items-center">
-          <Box twClassName="h-12 justify-center">
-            <Avatar
-              variant={AvatarVariant.Account}
-              type={accountAvatarType}
-              accountAddress={recipient.address}
-              size={AvatarSize.Md}
-            />
-          </Box>
-
-          <Box twClassName="ml-4 h-12 justify-center">
-            <Text
-              testID={`recipient-name-${recipient.address}`}
-              variant={TextVariant.BodyMd}
-              fontWeight={FontWeight.Medium}
-              numberOfLines={1}
-            >
-              {isBIP44
-                ? recipient.accountGroupName || recipient.contactName
-                : recipient.accountName || recipient.contactName}
-            </Text>
-            <Text
-              testID={`recipient-address-${recipient.address}`}
-              variant={TextVariant.BodyMd}
-              style={styles.recipientAddress}
-              numberOfLines={1}
-            >
-              {formatAddress(recipient.address, 'short')}
-            </Text>
-          </Box>
-        </Box>
+      style={({ pressed }) =>
+        tw.style(
+          'w-full flex-row items-center justify-between h-18',
+          pressed || isSelected ? 'bg-pressed' : 'bg-transparent',
+        )
       }
-    />
+      onPress={handlePressRecipient}
+    >
+      <Box twClassName="flex-row items-center h-12">
+        <Box twClassName="h-12 justify-center">
+          <Avatar
+            variant={AvatarVariant.Account}
+            type={accountAvatarType}
+            accountAddress={recipient.address}
+            size={AvatarSize.Md}
+          />
+        </Box>
+
+        <Box twClassName="ml-4 h-12 justify-center">
+          <Text
+            testID={`recipient-name-${recipient.address}`}
+            variant={TextVariant.BodyMd}
+            fontWeight={FontWeight.Medium}
+            numberOfLines={1}
+          >
+            {isBIP44
+              ? recipient.accountGroupName || recipient.contactName
+              : recipient.accountName || recipient.contactName}
+          </Text>
+          <Text
+            testID={`recipient-address-${recipient.address}`}
+            variant={TextVariant.BodyMd}
+            style={styles.recipientAddress}
+            numberOfLines={1}
+          >
+            {formatAddress(recipient.address, 'short')}
+          </Text>
+        </Box>
+      </Box>
+    </ButtonBase>
   );
 }
