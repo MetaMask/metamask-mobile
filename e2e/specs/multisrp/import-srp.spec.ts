81bd65cc6aa6679b24f5ba988b4fc860ebe11085
Davide Brocchetto <davide.brocchetto@consensys.net>
2025-09-17 21:49:15 -0700

    Refactor code and rename file

---
+++ e2e/specs/multisrp/import-srp.spec.ts
@@ -0,0 +1,52 @@
+import { SmokeWalletPlatform } from '../../tags';
+import FixtureBuilder from '../../framework/fixtures/FixtureBuilder';
+import { withFixtures } from '../../framework/fixtures/FixtureHelper';
+import WalletView from '../../pages/wallet/WalletView';
+import { loginToApp } from '../../viewHelper';
+import Assertions from '../../framework/Assertions';
+import ImportSrpView from '../../pages/importSrp/ImportSrpView';
+import { Mockttp } from 'mockttp';
+import { remoteFeatureMultichainAccountsAccountDetailsV2 } from '../../api-mocking/mock-responses/feature-flags-mocks';
+import { setupRemoteFeatureFlagsMock } from '../../api-mocking/helpers/remoteFeatureFlagsHelper';
+import { goToImportSrp, inputSrp } from './utils';
+import { IDENTITY_TEAM_SEED_PHRASE } from '../identity/utils/constants';
+
+const IMPORTED_ACCOUNT_NAME = 'Account 4';
+
+const testSpecificMock = async (mockServer: Mockttp) => {
+  await setupRemoteFeatureFlagsMock(
+    mockServer,
+    remoteFeatureMultichainAccountsAccountDetailsV2(true),
+  );
+};
+
+describe(
+  SmokeWalletPlatform('Multi-SRP: Add new account to a specific SRP'),
+  () => {
+    it('adds an account to default SRP and one to the imported SRP', async () => {
+      await withFixtures(
+        {
+          fixture: new FixtureBuilder()
+            .withImportedHdKeyringAndTwoDefaultAccountsOneImportedHdAccountKeyringController()
+            .ensureMultichainIntroModalSuppressed()
+            .build(),
+          restartDevice: true,
+          testSpecificMock,
+        },
+        async () => {
+          await loginToApp();
+          await goToImportSrp();
+          await inputSrp(IDENTITY_TEAM_SEED_PHRASE);
+          await ImportSrpView.tapImportButton();
+          await Assertions.expectElementToHaveText(
+            WalletView.accountName,
+            IMPORTED_ACCOUNT_NAME,
+            {
+              description: `Expect selected account to be ${IMPORTED_ACCOUNT_NAME}`,
+            },
+          );
+        },
+      );
+    });
+  },
+);
