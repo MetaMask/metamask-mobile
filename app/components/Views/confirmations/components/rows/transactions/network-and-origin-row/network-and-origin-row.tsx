0fdcee4273af9a19cd18016528f9f27321b36439
Goktug Poyraz <omergoktugpoyraz@gmail.com>
2025-09-19 11:37:10 +0200

    Fix e2e

--- app/components/Views/confirmations/components/rows/transactions/network-and-origin-row/network-and-origin-row.tsx
+++ app/components/Views/confirmations/components/rows/transactions/network-and-origin-row/network-and-origin-row.tsx
@@ -14,18 +14,20 @@ import { useStyles } from '../../../../../../../component-library/hooks';
 import { getNetworkImageSource } from '../../../../../../../util/networks';
 import { strings } from '../../../../../../../../locales/i18n';
 import { RootState } from '../../../../../../../reducers';
-import InfoSection from '../../../UI/info-row/info-section';
-import InfoRow from '../../../UI/info-row/info-row';
-import { MMM_ORIGIN } from '../../../../constants/confirmations';
-import styleSheet from './network-and-origin-row.styles';
 import AvatarNetwork from '../../../../../../../component-library/components/Avatars/Avatar/variants/AvatarNetwork/AvatarNetwork';
 import { AvatarSize } from '../../../../../../../component-library/components/Avatars/Avatar/Avatar.types';
+import { useApprovalInfo } from '../../../../hooks/useApprovalInfo';
+import { MMM_ORIGIN } from '../../../../constants/confirmations';
+import InfoSection from '../../../UI/info-row/info-section';
+import InfoRow from '../../../UI/info-row/info-row';
+import Address from '../../../UI/info-row/info-value/address';
+import styleSheet from './network-and-origin-row.styles';
 
 export const NetworkAndOriginRow = () => {
   const { styles } = useStyles(styleSheet, {});
   const transactionMetadata = useTransactionMetadataRequest();
   const signatureRequest = useSignatureRequest();
-
+  const { fromAddress, isSIWEMessage } = useApprovalInfo() ?? {};
   const chainId = transactionMetadata?.chainId || signatureRequest?.chainId;
   const origin =
     transactionMetadata?.origin || signatureRequest?.messageParams?.origin;
@@ -64,6 +66,14 @@ export const NetworkAndOriginRow = () => {
           style={styles.infoRowOverride}
         >
           <Text variant={TextVariant.BodyMD}>{origin}</Text>
+        </InfoRow>
+      )}
+      {signatureRequest && isSIWEMessage && (
+        <InfoRow
+          label={strings('confirm.label.signing_in_with')}
+          testID={ConfirmationRowComponentIDs.SIWE_SIGNING_ACCOUNT_INFO}
+        >
+          <Address address={fromAddress ?? ''} chainId={chainId ?? ''} />
         </InfoRow>
       )}
     </InfoSection>
