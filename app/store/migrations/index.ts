358d193ad13f828ad56baaa83cf99d1dae4febde
Mathieu Artu <mathieu.artu@consensys.net>
2025-09-19 12:58:29 +0200

    fix: remove old and unused UserStorageController state properties (#20026)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    This PR adds a migration that deletes old and unused state properties
    from `UserStorageController`.
    
    ## **Changelog**
    
    <!--
    If this PR is not End-User-Facing and should not show up in the
    CHANGELOG, you can choose to either:
    1. Write `CHANGELOG entry: null`
    2. Label with `no-changelog`
    
    If this PR is End-User-Facing, please write a short User-Facing
    description in the past tense like:
    `CHANGELOG entry: Added a new tab for users to see their NFTs`
    `CHANGELOG entry: Fixed a bug that was causing some NFTs to flicker`
    
    (This helps the Release Engineer do their job more quickly and
    accurately)
    -->
    
    CHANGELOG entry: null
    
    ## **Related issues**
    
    Fixes: https://consensyssoftware.atlassian.net/browse/MUL-861
    
    ## **Manual testing steps**
    
    ```gherkin
    Feature: my feature name
    
      Scenario: user [verb for user action]
        Given [describe expected initial app state]
    
        When user [verb for user action]
        Then [describe expected outcome]
    ```
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/store/migrations/index.ts
+++ app/store/migrations/index.ts
@@ -100,6 +100,7 @@ import migration96 from './096';
 import migration97 from './097';
 import migration98 from './098';
 import migration99 from './099';
+import migration100 from './100';
 
 // Add migrations above this line
 import { validatePostMigrationState } from '../validateMigration/validateMigration';
@@ -216,6 +217,7 @@ export const migrationList: MigrationsList = {
   97: migration97,
   98: migration98,
   99: migration99,
+  100: migration100,
 };
 
 // Enable both synchronous and asynchronous migrations
