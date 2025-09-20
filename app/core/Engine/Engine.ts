6925e479bb70a16ffd9c1766b34671422e4d6dc4
salimtb <salim.toubal@consensys.net>
2025-09-20 15:05:05 +0200

    fix: clean up

--- app/core/Engine/Engine.ts
+++ app/core/Engine/Engine.ts
@@ -1247,10 +1247,7 @@ export class Engine {
 
     const networkEnablementController =
       controllersByName.NetworkEnablementController;
-
-    ///: BEGIN:ONLY_INCLUDE_IF(bitcoin)
     networkEnablementController.init();
-    ///: END:ONLY_INCLUDE_IF
 
     ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
     const multichainRatesControllerMessenger =
