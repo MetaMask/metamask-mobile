4d193759e250508447c9db9174a9005ab1eb7bb3
vinnyhoward <vincenguyenhoward@gmail.com>
2025-09-22 15:09:25 -0600

    fix: resolved issue where header would flicker when showing address list

--- app/components/UI/Navbar/index.js
+++ app/components/UI/Navbar/index.js
@@ -2355,3 +2355,35 @@ export function getDeFiProtocolPositionDetailsNavbarOptions(navigation) {
     ),
   };
 }
+
+/**
+ * Function that returns the navigation options for the Address List screen
+ *
+ * @param {Object} navigation - Navigation object required to push new views
+ * @param {string} title - Title in string format
+ * @param {string} testID - Test ID for the back button
+ * @returns {Object} - Corresponding navbar options
+ */
+export function getAddressListNavbarOptions(navigation, title, testID) {
+  const innerStyles = StyleSheet.create({
+    headerLeft: {
+      marginHorizontal: 8,
+    },
+  });
+  return {
+    headerTitle: () => (
+      <MorphText variant={TextVariant.BodyMDBold}>{title}</MorphText>
+    ),
+    headerLeft: () => (
+      <View style={innerStyles.headerLeft}>
+        <ButtonIcon
+          testID={testID}
+          iconName={IconName.ArrowLeft}
+          size={ButtonIconSize.Md}
+          iconProps={{ color: MMDSIconColor.IconDefault }}
+          onPress={() => navigation.goBack()}
+        />
+      </View>
+    ),
+  };
+}
