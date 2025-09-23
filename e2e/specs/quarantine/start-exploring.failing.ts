40a501614b1c2316cd8155cc46cafc0e762cb7e6
Gaurav Goel <grvgoel19@gmail.com>
2025-09-23 15:17:56 +0400

    code cleanup

--- e2e/specs/quarantine/start-exploring.failing.ts
+++ e2e/specs/quarantine/start-exploring.failing.ts
@@ -4,7 +4,6 @@ import OnboardingView from '../../pages/Onboarding/OnboardingView';
 import OnboardingCarouselView from '../../pages/Onboarding/OnboardingCarouselView';
 import ProtectYourWalletView from '../../pages/Onboarding/ProtectYourWalletView';
 import CreatePasswordView from '../../pages/Onboarding/CreatePasswordView';
-import MetaMetricsOptIn from '../../pages/Onboarding/MetaMetricsOptInView';
 import OnboardingSuccessView from '../../pages/Onboarding/OnboardingSuccessView';
 import SkipAccountSecurityModal from '../../pages/Onboarding/SkipAccountSecurityModal';
 import { acceptTermOfUse } from '../../viewHelper';
@@ -37,13 +36,6 @@ describe(RegressionWalletPlatform('Start Exploring'), () => {
     await OnboardingCarouselView.tapOnGetStartedButton();
     await acceptTermOfUse();
     await Assertions.expectElementToBeVisible(OnboardingView.container);
-  });
-
-  it('should be able to opt-out of the onboarding-wizard', async () => {
-    await OnboardingView.tapCreateWallet();
-    await Assertions.expectElementToBeVisible(MetaMetricsOptIn.container);
-    await MetaMetricsOptIn.tapAgreeButton();
-    await Assertions.expectElementToBeVisible(CreatePasswordView.container);
   });
 
   it('should be able to create a new wallet', async () => {
