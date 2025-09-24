2563794ca937ef619ce84a0e5eac6af3703ef670
gantunesr <17601467+gantunesr@users.noreply.github.com>
2025-09-24 16:18:06 -0300

    test: fix unit test

--- app/multichain-accounts/remote-feature-flag.test.ts
+++ app/multichain-accounts/remote-feature-flag.test.ts
@@ -121,7 +121,7 @@ describe('Multichain Accounts Feature Flag', () => {
         },
         mockState2FeatureVersionsToCheck,
       );
-      expect(result).toBe(false);
+      expect(result).toBe(true);
     });
 
     it('returns true when the feature flag meets all conditions', () => {
