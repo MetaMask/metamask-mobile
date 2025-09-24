b65bc5f927be39181ab6e5893fabb2a4eb6c6ba2
Jean-Baptiste Blanc <jb.blanc@consensys.net>
2025-09-24 16:35:26 +0200

    fix: refactoring and adding tests

--- app/core/Engine/controllers/rewards-controller/services/rewards-data-service.ts
+++ app/core/Engine/controllers/rewards-controller/services/rewards-data-service.ts
@@ -26,6 +26,7 @@ import type {
 import { getSubscriptionToken } from '../utils/multi-subscription-token-vault';
 import Logger from '../../../../../util/Logger';
 import { successfulFetch } from '@metamask/controller-utils';
+import { getDefaultRewardsApiBaseUrlForMetaMaskEnv } from '../utils/rewards-api-url';
 
 const SERVICE_NAME = 'RewardsDataService';
 
@@ -256,25 +257,9 @@ export class RewardsDataService {
     // always using url from env var if set
     if (process.env.REWARDS_API_URL) return process.env.REWARDS_API_URL;
     // otherwise using default per-env url
-    return this.getDefaultRewardsApiBaseUrlForMetaMaskEnv();
-  }
-
-  private getDefaultRewardsApiBaseUrlForMetaMaskEnv() {
-    const env = process.env.METAMASK_ENVIRONMENT;
-    switch (env) {
-      case 'e2e':
-      case 'exp':
-      case 'rc':
-      case 'pre-release':
-        return AppConstants.REWARDS_API_URL.UAT;
-      case 'production':
-      case 'beta':
-        return AppConstants.REWARDS_API_URL.PRD;
-      case 'dev':
-      case 'local':
-      default:
-        return AppConstants.REWARDS_API_URL.UAT; // temporary for v7.57, should be moved to DEV later
-    }
+    return getDefaultRewardsApiBaseUrlForMetaMaskEnv(
+      process.env.METAMASK_ENVIRONMENT,
+    );
   }
 
   /**
