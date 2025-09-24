3143c90393baecd19629baf45b8765c954603ebf
António Regadas <antonio.regadas@consensys.net>
2025-09-24 09:58:36 +0100

    chore: Add Solana devnet support (#20155)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    
    We are enabling Solana devnet support, its under the FF
    `solanaTestnetsEnabled` (already in main).
    
    
    https://github.com/user-attachments/assets/11b04e4d-43f8-49a2-8d15-8d9d9a686d9f
    
    <img height="700" alt="Simulator Screenshot - iPhone 15 - 2025-09-23 at
    11 00 32"
    src="https://github.com/user-attachments/assets/7d79f6aa-c6cd-46ae-a203-44f30506cd1c"
    />
    <img height="700" alt="Simulator Screenshot - iPhone 15 - 2025-09-23 at
    11 00 22"
    src="https://github.com/user-attachments/assets/e04f02cc-0909-40ee-9952-0c42b3e77c3d"
    />
    <img height="700" alt="Simulator Screenshot - iPhone 15 - 2025-09-23 at
    11 06 58"
    src="https://github.com/user-attachments/assets/d7e33955-1e5c-4161-b9e8-9e685a1abc74"
    />
    
    ## **Changelog**
    
    <!--
    If this PR is not End-User-Facing and should not show up in the
    CHANGELOG, you can choose to either:
    1. Write `CHANGELOG entry: null`
    2. Label with `no-changelog`
    
    If this PR is End-User-Facing, please write a short User-Facing
    description in the past tense like:
    `CHANGELOG entry: Added a new tab for users to see their NFTs`
    `CHANGELOG entry: Fixed a bug that was causing some NFTs to flicker`
    
    (This helps the Release Engineer do their job more quickly and
    accurately)
    -->
    
    CHANGELOG entry: Adds Solana devnet support, under FF.
    
    ## **Related issues**
    
    Fixes:
    
    ## **Manual testing steps**
    
    ```gherkin
    Feature: my feature name
    
      Scenario: user [verb for user action]
        Given [describe expected initial app state]
    
        When user [verb for user action]
        Then [describe expected outcome]
    ```
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [ ] I’ve followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs) and [MetaMask Mobile
    Coding
    Standards](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/CODING_GUIDELINES.md).
    - [ ] I've completed the PR template to the best of my ability
    - [ ] I’ve included tests if applicable
    - [ ] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-mobile/blob/main/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [x] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [x] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.

--- app/components/UI/CollectibleContracts/index.js
+++ app/components/UI/CollectibleContracts/index.js
@@ -27,7 +27,6 @@ import {
   multichainCollectibleContractsSelector,
   multichainCollectiblesSelector,
   multichainCollectiblesByEnabledNetworksSelector,
-  multichainCollectibleContractsByEnabledNetworksSelector,
 } from '../../../reducers/collectibles';
 import { removeFavoriteCollectible } from '../../../actions/collectibles';
 import AppConstants from '../../../core/AppConstants';
@@ -669,11 +668,7 @@ const CollectibleContracts = ({
               </>
             }
             isDisabled={isDisabled}
-            onPress={
-              isEvmSelected || isMultichainAccountsState2Enabled
-                ? showFilterControls
-                : () => null
-            }
+            onPress={showFilterControls}
             endIconName={
               isEvmSelected || isMultichainAccountsState2Enabled
                 ? IconName.ArrowDown
