ed93ff80da4ef141a8365a65a755f1d4ce902ecb
Owen Craston <owen.craston@consensys.net>
2025-09-25 18:51:59 -0400

    refine styles

--- app/component-library/components-temp/MultichainAccounts/AccountCell/AccountCell.styles.ts
+++ app/component-library/components-temp/MultichainAccounts/AccountCell/AccountCell.styles.ts
@@ -1,37 +1,13 @@
 import { StyleSheet } from 'react-native';
 import { Theme } from '../../../../util/theme/models';
-import { colors as staticColors } from '../../../../styles/common';
 
-const styleSheet = (params: {
-  theme: Theme;
-  vars: { isSelected: boolean };
-}) => {
-  const { theme, vars } = params;
-  const { isSelected } = vars;
+const styleSheet = (params: { theme: Theme }) => {
+  const { theme } = params;
   const { colors } = theme;
 
   return StyleSheet.create({
     container: {
       gap: 16,
-      paddingTop: 16,
-      paddingBottom: 16,
-    },
-    avatarWrapper: {
-      borderRadius: 8,
-      width: isSelected ? 40 : 36, // 36 (avatar size) + 2*2 (border width) when selected, 32 + 2*2 when not
-      height: isSelected ? 40 : 36, // 36 (avatar size) + 2*2 (border width) when selected, 32 + 2*2 when not
-      borderWidth: 2,
-      borderColor: isSelected ? colors.info.default : staticColors.transparent,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    avatar: {
-      borderRadius: 6, // Slightly smaller to account for wrapper border
-      width: isSelected ? 36 : 32, // Increase size to maintain 32x32 content area with 2px border
-      height: isSelected ? 36 : 32, // Increase size to maintain 32x32 content area with 2px border
-      backgroundColor: colors.background.muted,
-      borderWidth: isSelected ? 2 : 0,
-      borderColor: isSelected ? staticColors.white : staticColors.transparent,
     },
     accountName: {
       display: 'flex',
