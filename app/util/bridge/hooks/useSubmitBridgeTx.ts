4d54d3b4f7374a917b990d71ca3c1b5230c82db0
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-09-26 13:18:38 -0700

    fix: include fromAddress in swap submitTx call

--- app/util/bridge/hooks/useSubmitBridgeTx.ts
+++ app/util/bridge/hooks/useSubmitBridgeTx.ts
@@ -2,17 +2,24 @@ import { QuoteResponse } from '../../../components/UI/Bridge/types';
 import Engine from '../../../core/Engine';
 import { QuoteMetadata } from '@metamask/bridge-controller';
 import { useSelector } from 'react-redux';
+import { selectSourceWalletAddress } from '../../../selectors/bridge';
 import { selectShouldUseSmartTransaction } from '../../../selectors/smartTransactionsController';
 
 export default function useSubmitBridgeTx() {
   const stxEnabled = useSelector(selectShouldUseSmartTransaction);
+  const walletAddress = useSelector(selectSourceWalletAddress);
 
   const submitBridgeTx = async ({
     quoteResponse,
   }: {
     quoteResponse: QuoteResponse & QuoteMetadata;
   }) => {
+    if (!walletAddress) {
+      throw new Error('Wallet address is not set');
+    }
+
     const txResult = await Engine.context.BridgeStatusController.submitTx(
+      walletAddress,
       {
         ...quoteResponse,
         approval: quoteResponse.approval ?? undefined,
