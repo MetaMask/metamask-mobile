fe91220220450209883c3b33136b17b0dd21013f
Jyoti Puri <jyotipuri@gmail.com>
2025-09-26 17:50:08 +0530

    Update

--- app/components/Views/confirmations/hooks/send/useToAddressValidation.ts
+++ app/components/Views/confirmations/hooks/send/useToAddressValidation.ts
@@ -1,13 +1,12 @@
 import { Hex } from '@metamask/utils';
 import { isAddress as isSolanaAddress } from '@solana/addresses';
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { strings } from '../../../../../../locales/i18n';
 import { isENS, isValidHexAddress } from '../../../../../util/address';
 import { selectAddressBook } from '../../../../../selectors/addressBookController';
 import { selectInternalAccounts } from '../../../../../selectors/accountsController';
-import { useAsyncResult } from '../../../../hooks/useAsyncResult';
 import {
   shouldSkipValidation,
   validateHexAddress,
@@ -17,12 +16,21 @@ import { useSendContext } from '../../context/send-context';
 import { useSendType } from './useSendType';
 import { useNameValidation } from './useNameValidation';
 
+interface ValidationResult {
+  toAddressValidated?: string;
+  error?: string;
+  warning?: string;
+  resolvedAddress?: string;
+}
+
 export const useToAddressValidation = () => {
   const internalAccounts = useSelector(selectInternalAccounts);
   const addressBook = useSelector(selectAddressBook);
   const { chainId, to } = useSendContext();
   const { isEvmSendType, isSolanaSendType } = useSendType();
   const { validateName } = useNameValidation();
+  const [result, setResult] = useState<ValidationResult>({});
+  const [loading, setLoading] = useState(false);
 
   const validateToAddress = useCallback(
     async (toAddress?: string) => {
@@ -68,25 +76,33 @@ export const useToAddressValidation = () => {
     ],
   );
 
-  const { value, pending } = useAsyncResult<{
-    toAddressValidated?: string;
-    error?: string;
-    warning?: string;
-    resolvedAddress?: string;
-  }>(async () => {
-    const result = await validateToAddress(to);
-    return { ...result, toAddressValidated: to };
-  }, [validateToAddress, to]);
+  useEffect(() => {
+    let cancel = false;
+
+    (async () => {
+      setLoading(true);
+      const result = await validateToAddress(to);
+
+      if (!cancel) {
+        setResult({ ...result, toAddressValidated: to });
+        setLoading(false);
+      }
+    })();
+
+    return () => {
+      cancel = true;
+    };
+  }, [setLoading, to, validateToAddress]);
 
   const {
     toAddressValidated,
     error: toAddressError,
     warning: toAddressWarning,
     resolvedAddress,
-  } = value ?? {};
+  } = result ?? {};
 
   return {
-    loading: pending,
+    loading,
     resolvedAddress,
     toAddressError,
     toAddressValidated,
