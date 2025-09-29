7996e275ff8d8b723ada2a10e52bf21d2172f2a8
Gaurav Goel <grvgoel19@gmail.com>
2025-09-29 13:50:16 +0400

    enable synchronization in e2e test

--- e2e/viewHelper.ts
+++ e2e/viewHelper.ts
@@ -240,7 +240,6 @@ export const CreateNewWallet = async ({ optInToMetrics = true } = {}) => {
   });
   await ProtectYourWalletView.tapOnRemindMeLaterButton();
 
-
   await Assertions.expectElementToBeVisible(MetaMetricsOptIn.container, {
     description: 'MetaMetrics Opt-In should be visible',
   });
@@ -251,7 +250,7 @@ export const CreateNewWallet = async ({ optInToMetrics = true } = {}) => {
   }
 
   await device.disableSynchronization(); // Detox is hanging after wallet creation
-
+  await device.enableSynchronization();
   await Assertions.expectElementToBeVisible(OnboardingSuccessView.container, {
     description: 'Onboarding Success View should be visible',
   });
