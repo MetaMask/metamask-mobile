520e22ae109fb08e5d3c069847723d62a993b074
Matthew Walsh <matthew.walsh@consensys.net>
2025-09-30 14:01:44 +0100

    Create CustomAmountInfo component

--- app/components/Views/confirmations/hooks/pay/useAutomaticTransactionPayToken.ts
+++ app/components/Views/confirmations/hooks/pay/useAutomaticTransactionPayToken.ts
@@ -3,7 +3,6 @@ import { useTokensWithBalance } from '../../../../UI/Bridge/hooks/useTokensWithB
 import { selectEnabledSourceChains } from '../../../../../core/redux/slices/bridge';
 import { useTransactionRequiredTokens } from './useTransactionRequiredTokens';
 import { NATIVE_TOKEN_ADDRESS } from '../../constants/tokens';
-import { useTransactionRequiredFiat } from './useTransactionRequiredFiat';
 import { useTransactionMetadataRequest } from '../transactions/useTransactionMetadataRequest';
 import { orderBy } from 'lodash';
 import { useEffect, useMemo, useRef } from 'react';
@@ -12,6 +11,11 @@ import { createProjectLogger } from '@metamask/utils';
 import { useTransactionPayToken } from './useTransactionPayToken';
 import { BridgeToken } from '../../../../UI/Bridge/types';
 import { isHardwareAccount } from '../../../../../util/address';
+import {
+  TransactionMeta,
+  TransactionType,
+} from '@metamask/transaction-controller';
+import { PERPS_MINIMUM_DEPOSIT } from '../../constants/perps';
 
 const log = createProjectLogger('transaction-pay');
 
@@ -22,22 +26,22 @@ export interface BalanceOverride {
 }
 
 export function useAutomaticTransactionPayToken({
-  balanceOverrides,
   countOnly = false,
 }: {
-  balanceOverrides?: BalanceOverride[];
   countOnly?: boolean;
 } = {}) {
   const isUpdated = useRef(false);
   const supportedChains = useSelector(selectEnabledSourceChains);
   const { setPayToken } = useTransactionPayToken();
-  const { totalFiat } = useTransactionRequiredFiat();
   const requiredTokens = useTransactionRequiredTokens({ log: true });
+
+  const transactionMeta =
+    useTransactionMetadataRequest() ?? ({ txParams: {} } as TransactionMeta);
 
   const {
     chainId,
     txParams: { from },
-  } = useTransactionMetadataRequest() ?? { txParams: {} };
+  } = transactionMeta;
 
   const chainIds = useMemo(
     () => (!isUpdated.current ? supportedChains.map((c) => c.chainId) : []),
@@ -46,6 +50,7 @@ export function useAutomaticTransactionPayToken({
 
   const tokens = useTokensWithBalance({ chainIds });
   const isHardwareWallet = isHardwareAccount(from ?? '');
+  const requiredBalance = getRequiredBalance(transactionMeta);
 
   let automaticToken: { address: string; chainId?: string } | undefined;
   let count = 0;
@@ -54,14 +59,6 @@ export function useAutomaticTransactionPayToken({
     const targetToken =
       requiredTokens.find((token) => token.address !== NATIVE_TOKEN_ADDRESS) ??
       requiredTokens[0];
-
-    const balanceOverride = balanceOverrides?.find(
-      (token) =>
-        token.address.toLowerCase() === targetToken?.address?.toLowerCase() &&
-        token.chainId === chainId,
-    );
-
-    const requiredBalance = balanceOverride?.balance ?? totalFiat;
 
     const sufficientBalanceTokens = orderBy(
       tokens.filter((token) =>
@@ -142,4 +139,13 @@ function isTokenSupported(
   const hasNativeBalance = (nativeToken?.tokenFiatAmount ?? 0) > 0;
 
   return isTokenBalanceSufficient && hasNativeBalance;
+}
+
+function getRequiredBalance(transactionMeta: TransactionMeta): number {
+  switch (transactionMeta?.type) {
+    case TransactionType.perpsDeposit:
+      return PERPS_MINIMUM_DEPOSIT;
+    default:
+      return 0;
+  }
 }
---
+++ app/components/Views/confirmations/hooks/transactions/useTransactionCustomAmount.ts
@@ -0,0 +1,62 @@
+import { useCallback, useState } from 'react';
+import { MAX_LENGTH } from '../../components/edit-amount-2/edit-amount-2';
+import { Hex } from '@metamask/utils';
+import { useTokenFiatRate } from '../tokens/useTokenFiatRates';
+import { BigNumber } from 'bignumber.js';
+import { useTransactionMetadataRequest } from './useTransactionMetadataRequest';
+import { TransactionMeta } from '@metamask/transaction-controller';
+import { useTokenAmount } from '../useTokenAmount';
+import { setTransactionBridgeQuotesLoading } from '../../../../../core/redux/slices/confirmationMetrics';
+import { useDispatch } from 'react-redux';
+
+export function useTransactionCustomAmount() {
+  const dispatch = useDispatch();
+  const [amountFiat, setAmountFiat] = useState('0');
+  const [isInputChanged, setInputChanged] = useState(false);
+
+  const transactionMeta = useTransactionMetadataRequest() as TransactionMeta;
+  const { chainId, id: transactionId } = transactionMeta;
+
+  const tokenAddress = getTokenAddress(transactionMeta);
+  const tokenFiatRate = useTokenFiatRate(tokenAddress, chainId);
+  const { updateTokenAmount: updateTokenAmountCallback } = useTokenAmount();
+
+  const amountHuman = new BigNumber(amountFiat || '0')
+    .dividedBy(tokenFiatRate ?? 1)
+    .toString(10);
+
+  const updatePendingAmount = useCallback((value: string) => {
+    let newAmount = value.replace(/^0+/, '') || '0';
+
+    if (newAmount.startsWith('.') || newAmount.startsWith(',')) {
+      newAmount = '0' + newAmount;
+    }
+
+    if (newAmount.length >= MAX_LENGTH) {
+      return;
+    }
+
+    setAmountFiat(newAmount);
+    setInputChanged(true);
+  }, []);
+
+  const updateTokenAmount = useCallback(() => {
+    dispatch(
+      setTransactionBridgeQuotesLoading({ transactionId, isLoading: true }),
+    );
+
+    updateTokenAmountCallback(amountHuman);
+  }, [amountHuman, dispatch, transactionId, updateTokenAmountCallback]);
+
+  return {
+    amountFiat,
+    amountHuman,
+    isInputChanged,
+    updatePendingAmount,
+    updateTokenAmount,
+  };
+}
+
+function getTokenAddress(transactionMeta: TransactionMeta | undefined): Hex {
+  return transactionMeta?.txParams?.to as Hex;
+}
