16b1d523f515a9c417aa8aceb2dacf9e7db1905d
Cal-L <cal.leung@consensys.net>
2025-09-29 17:28:00 -0700

    Fix unit tests and update snapshots

--- app/components/Views/SimpleWebview/index.test.tsx
+++ app/components/Views/SimpleWebview/index.test.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import SimpleWebview from './';
 
+jest.mock('../../UI/Navbar', () => ({
+  getWebviewNavbar: jest.fn(),
+}));
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: jest.fn(),
+  useRoute: jest.fn(() => ({
+    params: { url: 'https://etherscan.io', title: 'etherscan' },
+  })),
+}));
+
 describe('SimpleWebview', () => {
   it('should render correctly', () => {
-    const wrapper = shallow(
-      <SimpleWebview
-        navigation={{
-          setParams: () => {
-            ('');
-          },
-          setOptions: () => null,
-        }}
-        route={{ params: { url: 'https://etherscan.io', title: 'etherscan' } }}
-      />,
-    );
+    const wrapper = shallow(<SimpleWebview />);
     expect(wrapper).toMatchSnapshot();
   });
 });
