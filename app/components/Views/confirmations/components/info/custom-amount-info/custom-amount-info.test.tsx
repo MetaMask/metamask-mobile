e065b468000ae9abee58937c0057eaab4b389d3b
Matthew Walsh <matthew.walsh@consensys.net>
2025-10-01 00:26:22 +0100

    Add unit tests

---
+++ app/components/Views/confirmations/components/info/custom-amount-info/custom-amount-info.test.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { merge, noop } from 'lodash';
+import renderWithProvider from '../../../../../../util/test/renderWithProvider';
+import { CustomAmountInfo } from './custom-amount-info';
+import { simpleSendTransactionControllerMock } from '../../../__mocks__/controllers/transaction-controller-mock';
+import { transactionApprovalControllerMock } from '../../../__mocks__/controllers/approval-controller-mock';
+import { otherControllersMock } from '../../../__mocks__/controllers/other-controllers-mock';
+import { useTransactionPayToken } from '../../../hooks/pay/useTransactionPayToken';
+import { useTransactionCustomAmount } from '../../../hooks/transactions/useTransactionCustomAmount';
+import { useConfirmationContext } from '../../../context/confirmation-context';
+
+jest.mock('../../../hooks/ui/useClearConfirmationOnBackSwipe');
+jest.mock('../../../hooks/pay/useAutomaticTransactionPayToken');
+jest.mock('../../../hooks/pay/useTransactionPayToken');
+jest.mock('../../../hooks/pay/useTransactionBridgeQuotes');
+jest.mock('../../../hooks/transactions/useTransactionCustomAmount');
+jest.mock('../../../context/confirmation-context');
+
+jest.mock('@react-navigation/native', () => ({
+  ...jest.requireActual('@react-navigation/native'),
+  useNavigation: jest.fn(),
+}));
+
+function render() {
+  return renderWithProvider(<CustomAmountInfo />, {
+    state: merge(
+      {},
+      simpleSendTransactionControllerMock,
+      transactionApprovalControllerMock,
+      otherControllersMock,
+    ),
+  });
+}
+
+describe('CustomAmountInfo', () => {
+  const useTransactionPayTokenMock = jest.mocked(useTransactionPayToken);
+  const useConfirmationContextMock = jest.mocked(useConfirmationContext);
+
+  const useTransactionCustomAmountMock = jest.mocked(
+    useTransactionCustomAmount,
+  );
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+
+    useTransactionPayTokenMock.mockReturnValue({
+      payToken: {
+        address: '0x123',
+        balance: '0',
+        balanceFiat: '0',
+        balanceRaw: '0',
+        chainId: '0x1',
+        decimals: 18,
+        symbol: 'TST',
+        tokenFiatAmount: 0,
+      },
+      setPayToken: noop,
+    });
+
+    useTransactionCustomAmountMock.mockReturnValue({
+      amountFiat: '123.45',
+      amountHuman: '0',
+      isInputChanged: false,
+      updatePendingAmount: noop,
+      updateTokenAmount: noop,
+    });
+
+    useConfirmationContextMock.mockReturnValue({
+      setIsFooterVisible: noop,
+    } as ReturnType<typeof useConfirmationContext>);
+  });
+
+  it('renders', () => {
+    const { getByTestId } = render();
+
+    expect(getByTestId('custom-amount-input')).toHaveProp(
+      'defaultValue',
+      '123.45',
+    );
+  });
+});
