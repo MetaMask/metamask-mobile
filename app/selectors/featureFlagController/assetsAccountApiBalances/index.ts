c9bd4c8369199e92b63f8969898571562f96748d
salimtb <salim.toubal@consensys.net>
2025-10-01 18:16:40 +0200

    fix: branch for perf e2e tests using account api

--- app/selectors/featureFlagController/assetsAccountApiBalances/index.ts
+++ app/selectors/featureFlagController/assetsAccountApiBalances/index.ts
@@ -1,13 +1,19 @@
 import { createSelector } from 'reselect';
 import { selectRemoteFeatureFlags } from '..';
-import { hasProperty } from '@metamask/utils';
 
 export const FEATURE_FLAG_NAME = 'assetsAccountApiBalances';
 
 export const selectAssetsAccountApiBalancesEnabled = createSelector(
   selectRemoteFeatureFlags,
-  (remoteFeatureFlags) =>
-    hasProperty(remoteFeatureFlags, FEATURE_FLAG_NAME)
-      ? remoteFeatureFlags[FEATURE_FLAG_NAME]
-      : [],
+  () => [
+    '0x1',
+    '0xe708',
+    '0x38',
+    '0x89',
+    '0x2105',
+    '0xa',
+    '0xa4b1',
+    '0x531',
+    '0x82750',
+  ],
 );
