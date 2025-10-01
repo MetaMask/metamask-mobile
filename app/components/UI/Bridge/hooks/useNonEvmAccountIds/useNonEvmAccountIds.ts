8ccea7535676a0136e7887c3d3f590f3dc5a48ce
IF <139582705+infiniteflower@users.noreply.github.com>
2025-10-01 13:34:58 -0400

    chore: add hook for non evm account ids

---
+++ app/components/UI/Bridge/hooks/useNonEvmAccountIds/useNonEvmAccountIds.ts
@@ -0,0 +1,24 @@
+import { useSelector } from 'react-redux';
+import { CaipChainId } from '@metamask/utils';
+import { BtcScope, SolScope } from '@metamask/keyring-api';
+import { selectSelectedAccountGroupInternalAccounts } from '../../../../../selectors/multichainAccounts/accountTreeController';
+
+const NON_EVM_CHAIN_IDS: CaipChainId[] = [SolScope.Mainnet, BtcScope.Mainnet];
+
+/**
+ * Hook to get account IDs for non-EVM accounts
+ * @returns {string[]} Array of account IDs for accounts that have non-EVM scopes
+ */
+export const useNonEvmAccountIds = (): string[] => {
+  const selectedAccountGroupInternalAccounts = useSelector(
+    selectSelectedAccountGroupInternalAccounts,
+  );
+
+  const nonEvmAccountIds = selectedAccountGroupInternalAccounts
+    .filter((account) =>
+      account.scopes.some((scope) => NON_EVM_CHAIN_IDS.includes(scope)),
+    )
+    .map((account) => account.id);
+
+  return nonEvmAccountIds;
+};
