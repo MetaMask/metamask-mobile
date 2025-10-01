5d8d88f2431baacdc14daaa0bacc7896158a6526
Matthew Walsh <matthew.walsh@consensys.net>
2025-10-01 11:20:26 +0100

    Force currency

--- app/components/Views/confirmations/components/info/perps-deposit-info/perps-deposit-info.tsx
+++ app/components/Views/confirmations/components/info/perps-deposit-info/perps-deposit-info.tsx
@@ -3,10 +3,11 @@ import { strings } from '../../../../../../../locales/i18n';
 import useNavbar from '../../../hooks/ui/useNavbar';
 import { CustomAmountInfo } from '../custom-amount-info';
 import { usePerpsDepositToken } from '../../../hooks/perps-deposit/usePerpsDepositToken';
+import { PERPS_CURRENCY } from '../../../constants/perps';
 
 export function PerpsDepositInfo() {
   useNavbar(strings('confirm.title.perps_deposit'));
   usePerpsDepositToken();
 
-  return <CustomAmountInfo />;
+  return <CustomAmountInfo currency={PERPS_CURRENCY} />;
 }
