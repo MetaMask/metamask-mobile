04317976b1ff7de46083b2db3f2c065a36b5ee4b
Jyoti Puri <jyotipuri@gmail.com>
2025-10-02 11:35:24 +0530

    update

--- app/components/Views/confirmations/utils/send-address-validations.ts
+++ app/components/Views/confirmations/utils/send-address-validations.ts
@@ -55,7 +55,7 @@ export const validateHexAddress = async (
           warning: strings('send.token_contract_warning'),
         };
       }
-    } catch (e) {
+    } catch {
       // Not a token address
     }
   }
