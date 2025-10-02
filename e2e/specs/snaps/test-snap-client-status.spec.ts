126abce7a33f1773a3faec5d15592f555c37d82d
cmd-ob <ola.bale@consensys.net>
2025-10-02 13:32:18 +0100

    test: skip React Native reload for Client Status Snap tests

--- e2e/specs/snaps/test-snap-client-status.spec.ts
+++ e2e/specs/snaps/test-snap-client-status.spec.ts
@@ -13,6 +13,7 @@ describe(FlaskBuildTests('Client Status Snap Tests'), () => {
       {
         fixture: new FixtureBuilder().build(),
         restartDevice: true,
+        skipReactNativeReload: true,
       },
       async () => {
         await loginToApp();
