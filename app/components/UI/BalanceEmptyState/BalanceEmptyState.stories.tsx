acda5817642b1f8312cf8fcf584755ae3ad687e7
georgewrmarshall <george.marshall@consensys.net>
2025-10-02 12:39:14 -0700

    chore: adding balance empty state component

---
+++ app/components/UI/BalanceEmptyState/BalanceEmptyState.stories.tsx
@@ -0,0 +1,10 @@
+import BalanceEmptyState from './BalanceEmptyState';
+
+const BalanceEmptyStateMeta = {
+  title: 'Components / UI / BalanceEmptyState',
+  component: BalanceEmptyState,
+};
+
+export default BalanceEmptyStateMeta;
+
+export const Default = {};
