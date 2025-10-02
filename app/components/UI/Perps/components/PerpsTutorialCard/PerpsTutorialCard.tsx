4ac04df193338baab54f2ed9eaa47307a263e7d0
Nicholas Gambino <nicholas.gambino@consensys.net>
2025-10-02 09:23:46 -0700

    chore: Add missing i18n string

--- app/components/UI/Perps/components/PerpsTutorialCard/PerpsTutorialCard.tsx
+++ app/components/UI/Perps/components/PerpsTutorialCard/PerpsTutorialCard.tsx
@@ -13,6 +13,7 @@ import { TouchableOpacity } from '../../../../../component-library/components/Bu
 import { useNavigation } from '@react-navigation/native';
 import Routes from '../../../../../constants/navigation/Routes';
 import { PerpsTutorialSelectorsIDs } from '../../../../../../e2e/selectors/Perps/Perps.selectors';
+import { strings } from '../../../../../../locales/i18n';
 
 const PerpsTutorialCard = () => {
   const { styles } = useStyles(styleSheet, {});
@@ -29,7 +30,9 @@ const PerpsTutorialCard = () => {
       onPress={handlePress}
       testID={PerpsTutorialSelectorsIDs.TUTORIAL_CARD}
     >
-      <Text variant={TextVariant.BodyMd}>Learn the basics of perps</Text>
+      <Text variant={TextVariant.BodyMd}>
+        {strings('perps.tutorial.card.title')}
+      </Text>
       <Icon
         name={IconName.ArrowRight}
         size={IconSize.Md}
