6758f3d581e155099f23884fcecbd702e07589e5
Matthew Walsh <matthew.walsh@consensys.net>
2025-10-03 01:28:03 +0100

    Split test into functions

--- e2e/specs/confirmations/transaction-pay.spec.ts
+++ e2e/specs/confirmations/transaction-pay.spec.ts
@@ -22,6 +22,127 @@ import {
   BRIDGE_STATUS_RESPONSE,
 } from '../../api-mocking/mock-responses/transaction-pay';
 
+describe(SmokeConfirmationsRedesigned('MetaMask Pay'), () => {
+  it(`bridges funds for perps deposit`, async () => {
+    await withFixtures(
+      {
+        fixture: new FixtureBuilder()
+          .withPerpsProfile('no-funds')
+          .withPerpsFirstTimeUser(false)
+          .withDisabledSmartTransactions()
+          .withPopularNetworks()
+          .withTokenRates(
+            CHAIN_IDS.BASE,
+            '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+            2.0,
+          )
+          .withTokenRates(
+            CHAIN_IDS.ARBITRUM,
+            '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
+            2.0,
+          )
+          .withTokensForAllPopularNetworks([
+            {
+              address: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+              symbol: 'DAI',
+              decimals: 18,
+              name: 'Dai Stablecoin',
+              balance: '0.012345',
+            },
+          ])
+          .build(),
+        restartDevice: true,
+        testSpecificMock,
+      },
+      async () => {
+        await loginToApp();
+        await device.disableSynchronization();
+        await startDeposit();
+        await confirmTransaction();
+        await checkTransactionDetails();
+        await checkActivity();
+      },
+    );
+  });
+});
+
+async function startDeposit() {
+  await WalletView.tapOnPerpsTab();
+  await PerpsTabView.tapBalanceButton();
+  await PerpsTabView.tapAddFundsButton();
+}
+
+async function confirmTransaction() {
+  await Assertions.expectElementToHaveText(
+    TransactionPayView.payWithSymbol,
+    'DAI',
+  );
+
+  await Assertions.expectElementToHaveText(
+    TransactionPayView.payWithFiat,
+    '$123.45',
+  );
+
+  await Assertions.expectElementToHaveText(
+    TransactionPayView.payWithBalance,
+    '0.0123 DAI',
+  );
+
+  await TransactionPayView.tapPayWithRow();
+
+  await Assertions.expectElementToBeVisible(
+    TransactionPayView.availableToken(
+      '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+      CHAIN_IDS.BASE,
+    ),
+  );
+
+  await Assertions.expectElementToBeVisible(
+    TransactionPayView.availableToken(
+      '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
+      CHAIN_IDS.ARBITRUM,
+    ),
+  );
+
+  await TransactionPayView.tapPayWithModalCloseButton();
+  await TransactionPayView.tapKeyboardAmount('10.12');
+  await TransactionPayView.tapKeyboardContinueButton();
+  await FooterActions.tapConfirmButton();
+}
+
+async function checkTransactionDetails() {
+  await Gestures.waitAndTap(Matchers.getElementByText('Track'));
+
+  await Assertions.expectElementToHaveText(
+    TransactionDetailsModal.paidWithSymbol,
+    'DAI',
+  );
+
+  await Assertions.expectElementToHaveText(
+    TransactionDetailsModal.summaryLine(0),
+    'Bridge from DAI to USDC',
+  );
+
+  await Assertions.expectElementToHaveText(
+    TransactionDetailsModal.summaryLine(1),
+    'Add funds',
+  );
+}
+
+async function checkActivity() {
+  await TabBarComponent.tapWallet();
+  await TabBarComponent.tapActivity();
+
+  await Assertions.expectElementToHaveText(
+    ActivitiesView.transactionStatus(0),
+    ActivitiesViewSelectorsText.CONFIRM_TEXT,
+  );
+
+  await Assertions.expectElementToNotBeVisible(
+    ActivitiesView.transactionItem(1),
+  );
+}
+
 async function testSpecificMock(mockServer: Mockttp) {
   await NetworkMock(CHAIN_IDS.ARBITRUM)(mockServer);
   await NetworkMock(CHAIN_IDS.BASE)(mockServer);
@@ -74,111 +195,3 @@ async function testSpecificMock(mockServer: Mockttp) {
     responseCode: 200,
   });
 }
-
-describe(SmokeConfirmationsRedesigned('MetaMask Pay'), () => {
-  it(`bridges funds for perps deposit`, async () => {
-    await withFixtures(
-      {
-        fixture: new FixtureBuilder()
-          .withPerpsProfile('no-funds')
-          .withPerpsFirstTimeUser(false)
-          .withDisabledSmartTransactions()
-          .withPopularNetworks()
-          .withTokenRates(
-            CHAIN_IDS.BASE,
-            '0x6B175474E89094C44Da98b954EedeAC495271d0F',
-            2.0,
-          )
-          .withTokenRates(
-            CHAIN_IDS.ARBITRUM,
-            '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
-            2.0,
-          )
-          .withTokensForAllPopularNetworks([
-            {
-              address: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
-              symbol: 'DAI',
-              decimals: 18,
-              name: 'Dai Stablecoin',
-              balance: '0.012345',
-            },
-          ])
-          .build(),
-        restartDevice: true,
-        testSpecificMock,
-      },
-      async () => {
-        await loginToApp();
-        await device.disableSynchronization();
-        await WalletView.tapOnPerpsTab();
-        await PerpsTabView.tapBalanceButton();
-        await PerpsTabView.tapAddFundsButton();
-
-        await Assertions.expectElementToHaveText(
-          TransactionPayView.payWithSymbol,
-          'DAI',
-        );
-
-        await Assertions.expectElementToHaveText(
-          TransactionPayView.payWithFiat,
-          '$123.45',
-        );
-
-        await Assertions.expectElementToHaveText(
-          TransactionPayView.payWithBalance,
-          '0.0123 DAI',
-        );
-
-        await TransactionPayView.tapPayWithRow();
-
-        await Assertions.expectElementToBeVisible(
-          TransactionPayView.availableToken(
-            '0x6B175474E89094C44Da98b954EedeAC495271d0F',
-            CHAIN_IDS.BASE,
-          ),
-        );
-
-        await Assertions.expectElementToBeVisible(
-          TransactionPayView.availableToken(
-            '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
-            CHAIN_IDS.ARBITRUM,
-          ),
-        );
-
-        await TransactionPayView.tapPayWithModalCloseButton();
-        await TransactionPayView.tapKeyboardAmount('10.12');
-        await TransactionPayView.tapKeyboardContinueButton();
-        await FooterActions.tapConfirmButton();
-
-        await Gestures.waitAndTap(Matchers.getElementByText('Track'));
-
-        await Assertions.expectElementToHaveText(
-          TransactionDetailsModal.paidWithSymbol,
-          'DAI',
-        );
-
-        await Assertions.expectElementToHaveText(
-          TransactionDetailsModal.summaryLine(0),
-          'Bridge from DAI to USDC',
-        );
-
-        await Assertions.expectElementToHaveText(
-          TransactionDetailsModal.summaryLine(1),
-          'Add funds',
-        );
-
-        await TabBarComponent.tapWallet();
-        await TabBarComponent.tapActivity();
-
-        await Assertions.expectElementToHaveText(
-          ActivitiesView.transactionStatus(0),
-          ActivitiesViewSelectorsText.CONFIRM_TEXT,
-        );
-
-        await Assertions.expectElementToNotBeVisible(
-          ActivitiesView.transactionItem(1),
-        );
-      },
-    );
-  });
-});
