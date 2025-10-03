caf43cff449bd10fc4568e4aeac3572318e61226
Kevin Bluer <kevin@bluer.com>
2025-10-03 15:22:19 -0500

    chore: manually overriding feature flag for test build (not to be merged)

--- app/components/UI/Predict/selectors/featureFlags/index.ts
+++ app/components/UI/Predict/selectors/featureFlags/index.ts
@@ -1,14 +1,13 @@
 import { createSelector } from 'reselect';
-import { hasProperty } from '@metamask/utils';
-import { selectRemoteFeatureFlags } from '../../../../../selectors/featureFlagController';
 
 export const OVERRIDE_PREDICT_ENABLED_VALUE = false;
 export const FEATURE_FLAG_NAME = 'predictEnabled';
 
 export const selectPredictEnabledFlag = createSelector(
-  selectRemoteFeatureFlags,
-  (remoteFlags) =>
-    hasProperty(remoteFlags, FEATURE_FLAG_NAME)
-      ? Boolean(remoteFlags[FEATURE_FLAG_NAME])
-      : OVERRIDE_PREDICT_ENABLED_VALUE,
+  // selectRemoteFeatureFlags,
+  // (remoteFlags) =>
+  //   hasProperty(remoteFlags, FEATURE_FLAG_NAME)
+  //     ? Boolean(remoteFlags[FEATURE_FLAG_NAME])
+  //     : OVERRIDE_PREDICT_ENABLED_VALUE,
+  () => true, // NOTE: manually overriding for browserstack build
 );
