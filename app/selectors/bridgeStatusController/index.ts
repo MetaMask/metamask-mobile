47c33581d1d409bb2a173117e47990cb45250423
IF <139582705+infiniteflower@users.noreply.github.com>
2025-10-07 12:46:07 -0400

    fix: not loading tx details if currently selected account was on btc network

--- app/selectors/bridgeStatusController/index.ts
+++ app/selectors/bridgeStatusController/index.ts
@@ -1,24 +1,38 @@
 import { createSelector } from 'reselect';
 import { BridgeHistoryItem } from '@metamask/bridge-status-controller';
-import { selectSelectedInternalAccountAddress } from '../accountsController';
+import { selectSelectedAccountGroupWithInternalAccountsAddresses } from '../multichainAccounts/accountTreeController';
 import { RootState } from '../../reducers';
+import { isEthAddress } from '../../util/address';
 
 export const selectBridgeStatusState = (state: RootState) =>
   state.engine.backgroundState.BridgeStatusController;
 
 /**
- * Returns a mapping of srcTxMetaId to txHistoryItem for the selected address
+ * Returns a mapping of srcTxMetaId to txHistoryItem for any account in the current account group
  */
 export const selectBridgeHistoryForAccount = createSelector(
-  [selectSelectedInternalAccountAddress, selectBridgeStatusState],
-  (selectedAddress, bridgeStatusState) => {
+  [
+    selectSelectedAccountGroupWithInternalAccountsAddresses,
+    selectBridgeStatusState,
+  ],
+  (accountGroupAddresses, bridgeStatusState) => {
     // Handle the case when bridgeStatusState is undefined
     const { txHistory = {} } = bridgeStatusState || {};
+
+    // Convert addresses to lowercase Set for efficient lookup
+    const addressesSet = new Set(
+      accountGroupAddresses
+        .filter((address): address is string => address != null)
+        .map((address) => address.toLowerCase()),
+    );
 
     return Object.keys(txHistory).reduce<Record<string, BridgeHistoryItem>>(
       (acc, txMetaId) => {
         const txHistoryItem = txHistory[txMetaId];
-        if (txHistoryItem.account === selectedAddress) {
+        const txHistoryItemAccount = isEthAddress(txHistoryItem.account)
+          ? txHistoryItem.account?.toLowerCase()
+          : txHistoryItem.account;
+        if (addressesSet.has(txHistoryItemAccount)) {
           acc[txMetaId] = txHistoryItem;
         }
         return acc;
