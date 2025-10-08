fab231b7c527c4d7f831f1f113d92a85d9a5e49c
Matthew Walsh <matthew.walsh@consensys.net>
2025-10-08 13:35:57 +0100

    Use temporary screen

--- app/components/Nav/Main/MainNavigator.js
+++ app/components/Nav/Main/MainNavigator.js
@@ -488,6 +488,14 @@ const SettingsFlow = () => (
     {
       ///: END:ONLY_INCLUDE_IF
     }
+    <Stack.Screen
+      name={Routes.FULL_SCREEN_CONFIRMATIONS.REDESIGNED_CONFIRMATIONS}
+      component={RedesignedConfirm}
+      options={{
+        title: '',
+        headerLeft: () => null,
+      }}
+    />
   </Stack.Navigator>
 );
 
--- app/components/Views/Settings/DeveloperOptions/PredictDepositTest.tsx
+++ app/components/Views/Settings/DeveloperOptions/PredictDepositTest.tsx
@@ -42,7 +42,6 @@ export function PredictDepositTest() {
   const handleDeposit = useCallback(async () => {
     navigateToConfirmation({
       loader: ConfirmationLoader.CustomAmount,
-      stack: Routes.PERPS.ROOT,
     });
 
     addTransactionBatch({
