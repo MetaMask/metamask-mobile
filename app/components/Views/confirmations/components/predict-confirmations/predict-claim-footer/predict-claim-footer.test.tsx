81a3c9bd35fb3bef5c625b9a62e78d431ca3fdb9
Matthew Walsh <matthew.walsh@consensys.net>
2025-10-10 00:17:04 +0100

    Fix linting

--- app/components/Views/confirmations/components/predict-confirmations/predict-claim-footer/predict-claim-footer.test.tsx
+++ app/components/Views/confirmations/components/predict-confirmations/predict-claim-footer/predict-claim-footer.test.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import renderWithProvider from '../../../../../../util/test/renderWithProvider';
 import { PredictClaimFooter } from './predict-claim-footer';
-import { merge } from 'lodash';
+import { merge, noop } from 'lodash';
 import { simpleSendTransactionControllerMock } from '../../../__mocks__/controllers/transaction-controller-mock';
 import { transactionApprovalControllerMock } from '../../../__mocks__/controllers/approval-controller-mock';
 import { otherControllersMock } from '../../../__mocks__/controllers/other-controllers-mock';
 import { strings } from '../../../../../../../locales/i18n';
+import { fireEvent } from '@testing-library/react-native';
 
-function render() {
-  return renderWithProvider(<PredictClaimFooter />, {
+function render({ onPress }: { onPress?: () => void } = {}) {
+  return renderWithProvider(<PredictClaimFooter onPress={onPress ?? noop} />, {
     state: merge(
       {},
       simpleSendTransactionControllerMock,
@@ -30,5 +31,14 @@ describe('PredictClaimFooter', () => {
   it('renders market images', () => {
     const { getAllByTestId } = render();
     expect(getAllByTestId('token-avatar-image')).toHaveLength(3);
+  });
+
+  it('calls onPress when button is pressed', () => {
+    const onPressMock = jest.fn();
+    const { getByText } = render({ onPress: onPressMock });
+
+    fireEvent.press(getByText(strings('confirm.predict_claim.button_label')));
+
+    expect(onPressMock).toHaveBeenCalled();
   });
 });
