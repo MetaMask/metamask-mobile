d11a4cf29095c33a3cc46934fc73ad36a9ce1d8f
Bryan Fullam <bryanfullam@gmail.com>
2025-10-09 13:48:09 +0200

    chore: lint

--- app/core/redux/slices/bridge/index.ts
+++ app/core/redux/slices/bridge/index.ts
@@ -212,12 +212,7 @@ export const selectBridgeFeatureFlags = createSelector(
       },
     });
 
-    if (
-      hasMinimumRequiredVersion(
-        featureFlags.minimumVersion,
-        process.env.MM_BRIDGE_ENABLED === 'true',
-      )
-    ) {
+    if (hasMinimumRequiredVersion(featureFlags.minimumVersion)) {
       return featureFlags;
     }
 
