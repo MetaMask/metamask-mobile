088a14d03e081d491d4735ec4d540ed5843abc40
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2025-10-10 22:46:57 +0100

    chore: remove 'primary_currency_toggle' event

--- app/core/Analytics/MetaMetrics.events.ts
+++ app/core/Analytics/MetaMetrics.events.ts
@@ -484,7 +484,6 @@ enum EVENT_NAME {
   NFT_AUTO_DETECTION_DISBLED_MODAL = 'Nft Autodetection Disabled from modal',
   // NFT auto detection events
   NFT_AUTO_DETECTION_ENABLED = 'nft_autodetection_enabled',
-  PRIMARY_CURRENCY_TOGGLE = 'primary_currency_toggle',
   LOGIN_DOWNLOAD_LOGS = 'Download State Logs Button Clicked',
 
   // Backup and sync
@@ -1131,7 +1130,6 @@ const events = {
   NFT_AUTO_DETECTION_ENABLED: generateOpt(
     EVENT_NAME.NFT_AUTO_DETECTION_ENABLED,
   ),
-  PRIMARY_CURRENCY_TOGGLE: generateOpt(EVENT_NAME.PRIMARY_CURRENCY_TOGGLE),
   LOGIN_DOWNLOAD_LOGS: generateOpt(EVENT_NAME.LOGIN_DOWNLOAD_LOGS),
   // Backup and sync
   ACCOUNTS_SYNC_ADDED: generateOpt(EVENT_NAME.ACCOUNTS_SYNC_ADDED),
