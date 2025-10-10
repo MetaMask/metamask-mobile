a4a7b91a360ef27ad33b00637ee12eac78af8ed8
ieow <4881057+ieow@users.noreply.github.com>
2025-10-10 17:16:56 +0800

    fix: add headerShown: false

--- app/components/Nav/App/App.tsx
+++ app/components/Nav/App/App.tsx
@@ -181,6 +181,9 @@ const OnboardingSuccessFlow = () => (
     <Stack.Screen
       name={Routes.ONBOARDING.SUCCESS}
       component={OnboardingSuccess} // Used in SRP flow
+      options={{
+        headerShown: false,
+      }}
     />
     <Stack.Screen
       name={Routes.ONBOARDING.DEFAULT_SETTINGS} // This is being used in import wallet flow
