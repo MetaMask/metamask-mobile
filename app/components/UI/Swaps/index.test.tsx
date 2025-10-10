d8f95d47888afd60bd7f1b092aa1125e8d89f3af
ghgoodreau <hunter.goodreau@consensys.net>
2025-10-10 14:55:36 -0500

    chore: add tests

--- app/components/UI/Swaps/index.test.tsx
+++ app/components/UI/Swaps/index.test.tsx
@@ -7,6 +7,7 @@ import SwapsAmountView from './';
 import { backgroundState } from '../../../util/test/initial-root-state';
 import { RootState } from '../../../reducers';
 import { QuoteViewSelectorIDs } from '../../../../e2e/selectors/swaps/QuoteView.selectors';
+import { renderFromTokenMinimalUnit } from '../../../util/number';
 
 jest.mock('@react-navigation/native', () => {
   const actualNav = jest.requireActual('@react-navigation/native');
@@ -47,5 +48,19 @@ describe('SwapsAmountView', () => {
     expect(
       getByTestId(QuoteViewSelectorIDs.SOURCE_TOKEN_SELECTOR),
     ).toBeDefined();
+  });
+
+  it('truncates max amount to 5 decimals when using renderFromTokenMinimalUnit', () => {
+    // Given a balance with many decimal places (typical ETH balance in wei)
+    const balanceInWei = '1234567890123456789'; // 1.234567890123456789 ETH
+
+    // When converting to readable format with renderFromTokenMinimalUnit
+    const result = renderFromTokenMinimalUnit(balanceInWei, 18);
+
+    // Then it should be truncated to 5 decimals
+    expect(result).toBe('1.23457');
+
+    // Verify it doesn't show all 18 decimals
+    expect(result).not.toContain('234567890123456789');
   });
 });
