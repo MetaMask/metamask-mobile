6b6c4dbc2163899a5b67831aab01069969abcd42
Matthew Walsh <matthew.walsh@consensys.net>
2025-10-10 21:03:31 +0100

    Add confirmation
    Add selector.
    Add balance component.

--- app/components/Views/confirmations/components/info/custom-amount-info/custom-amount-info.tsx
+++ app/components/Views/confirmations/components/info/custom-amount-info/custom-amount-info.tsx
@@ -1,4 +1,10 @@
-import React, { memo, useCallback, useEffect, useState } from 'react';
+import React, {
+  ReactNode,
+  memo,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 import { PayTokenAmount, PayTokenAmountSkeleton } from '../../pay-token-amount';
 import InfoSection from '../../UI/info-row/info-section';
 import { PayWithRow, PayWithRowSkeleton } from '../../rows/pay-with-row';
@@ -31,11 +37,13 @@ import { useTransactionMetadataRequest } from '../../../hooks/transactions/useTr
 import { useIsTransactionPayLoading } from '../../../hooks/pay/useIsTransactionPayLoading';
 
 export interface CustomAmountInfoProps {
+  children?: ReactNode;
   currency?: string;
+  disablePay?: boolean;
 }
 
 export const CustomAmountInfo: React.FC<CustomAmountInfoProps> = memo(
-  ({ currency }) => {
+  ({ children, currency, disablePay }) => {
     useClearConfirmationOnBackSwipe();
     useAutomaticTransactionPayToken();
 
@@ -86,7 +94,8 @@ export const CustomAmountInfo: React.FC<CustomAmountInfoProps> = memo(
             hasAlert={Boolean(keyboardAlertMessage)}
             onPress={handleAmountPress}
           />
-          <PayTokenAmount amountHuman={amountHuman} />
+          {disablePay !== true && <PayTokenAmount amountHuman={amountHuman} />}
+          {children}
           {!isKeyboardVisible && (
             <AlertBanner
               blockingOnly
@@ -95,9 +104,11 @@ export const CustomAmountInfo: React.FC<CustomAmountInfoProps> = memo(
               inline
             />
           )}
-          <InfoSection>
-            <PayWithRow />
-          </InfoSection>
+          {disablePay !== true && (
+            <InfoSection>
+              <PayWithRow />
+            </InfoSection>
+          )}
           {isKeyboardVisible && <AlertMessage alertMessage={alertMessage} />}
           {isResultReady && (
             <>
