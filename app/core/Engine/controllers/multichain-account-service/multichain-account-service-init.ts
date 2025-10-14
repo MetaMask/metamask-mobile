12a40d004481bc5d45a12eeff8aabde85289ebc5
Alejandro Garcia <alejandro.garcia@consensys.net>
2025-10-14 12:06:10 +0200

    chore: move logic out

--- app/core/Engine/controllers/multichain-account-service/multichain-account-service-init.ts
+++ app/core/Engine/controllers/multichain-account-service/multichain-account-service-init.ts
@@ -57,23 +57,23 @@ export const multichainAccountServiceInit: ControllerInitFunction<
   // - Basic Functionality toggle at runtime (same pattern as Solana)
   if (selectIsBitcoinAccountsEnabled(ReduxService.store.getState())) {
     btcProvider.setEnabled(true);
-
-    // TODO: Move this logic to the SnapKeyring directly.
-    initMessenger.subscribe(
-      'MultichainAccountService:multichainAccountGroupUpdated',
-      (group) => {
-        const { AccountTreeController } = Engine.context;
-
-        // If the current group gets updated, then maybe there are more accounts being "selected"
-        // now, so we have to forward them to the Snap keyring too!
-        if (AccountTreeController.getSelectedAccountGroup() === group.id) {
-          // eslint-disable-next-line no-void
-          void forwardSelectedAccountGroupToSnapKeyring(group.id);
-        }
-      },
-    );
   }
   /// END:ONLY_INCLUDE_IF
+
+  // TODO: Move this logic to the SnapKeyring directly.
+  initMessenger.subscribe(
+    'MultichainAccountService:multichainAccountGroupUpdated',
+    (group) => {
+      const { AccountTreeController } = Engine.context;
+
+      // If the current group gets updated, then maybe there are more accounts being "selected"
+      // now, so we have to forward them to the Snap keyring too!
+      if (AccountTreeController.getSelectedAccountGroup() === group.id) {
+        // eslint-disable-next-line no-void
+        void forwardSelectedAccountGroupToSnapKeyring(group.id);
+      }
+    },
+  );
 
   return { controller, memStateKey: null, persistedStateKey: null };
 };
