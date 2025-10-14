effad8eb02719d3109e8c527047668b0a406873e
Javier Briones <1674192+jvbriones@users.noreply.github.com>
2025-10-14 18:31:03 +0200

    chore: clean e2e stuff

--- e2e/jest.e2e.config.js
+++ e2e/jest.e2e.config.js
@@ -18,7 +18,7 @@ if (process.env.BITRISE_TRIGGERED_WORKFLOW_ID) {
 module.exports = {
   rootDir: '..',
   testMatch: ['<rootDir>/e2e/specs/**/*.spec.{js,ts}'],
-  testTimeout: 500000,
+  testTimeout: 300000,
   maxWorkers: workers,
   setupFilesAfterEnv: ['<rootDir>/e2e/init.js'],
   globalSetup: 'detox/runners/jest/globalSetup',
