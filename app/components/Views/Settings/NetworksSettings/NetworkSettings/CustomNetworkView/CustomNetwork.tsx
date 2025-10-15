22b652c50f3a60c842af17a8bde9872beb226ca4
Frederic HENG <frederic.heng@consensys.net>
2025-10-15 19:31:27 +0200

    feat: update no network fees label in networks list

--- app/components/Views/Settings/NetworksSettings/NetworkSettings/CustomNetworkView/CustomNetwork.tsx
+++ app/components/Views/Settings/NetworksSettings/NetworkSettings/CustomNetworkView/CustomNetwork.tsx
@@ -24,13 +24,13 @@ import { NetworkConfiguration } from '@metamask/network-controller';
 import { MultichainNetworkConfiguration } from '@metamask/multichain-network-controller';
 import Text, {
   TextVariant,
+  TextColor,
 } from '../../../../../../component-library/components/Texts/Text';
 import Icon, {
   IconSize,
   IconName,
 } from '../../../../../../component-library/components/Icons/Icon';
 import { getGasFeesSponsoredNetworkEnabled } from '../../../../../../selectors/featureFlagController/gasFeesSponsored';
-import Tag from '../../../../../../component-library/components/Tags/Tag';
 
 const CustomNetwork = ({
   showPopularNetworkModal,
@@ -135,15 +135,22 @@ const CustomNetwork = ({
                 }
               />
             </View>
-            <CustomText bold={!isNetworkUiRedesignEnabled()}>
-              {networkConfiguration.nickname}
-            </CustomText>
-            {isGasFeesSponsoredNetworkEnabled(networkConfiguration.chainId) ? (
-              <Tag
-                label={strings('networks.no_network_fee')}
-                style={networkSettingsStyles.tagLabelSpacing}
-              />
-            ) : null}
+            <View style={customNetworkStyles.nameAndTagContainer}>
+              <CustomText bold={!isNetworkUiRedesignEnabled()}>
+                {networkConfiguration.nickname}
+              </CustomText>
+              {isGasFeesSponsoredNetworkEnabled(
+                networkConfiguration.chainId,
+              ) ? (
+                <Text
+                  variant={TextVariant.BodySM}
+                  color={TextColor.Alternative}
+                  style={customNetworkStyles.tagLabelBelowName}
+                >
+                  {strings('networks.no_network_fee')}
+                </Text>
+              ) : null}
+            </View>
           </View>
 
           <View style={networkSettingsStyles.popularWrapper}>
