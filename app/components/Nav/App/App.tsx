54c34865c5dade3970431ca2b7f346f899a7be13
ieow <4881057+ieow@users.noreply.github.com>
2025-10-16 21:06:59 +0800

    fix: revert App.tsx changes

--- app/components/Nav/App/App.tsx
+++ app/components/Nav/App/App.tsx
@@ -234,7 +234,6 @@ const OnboardingNav = () => (
     <Stack.Screen
       name={Routes.ONBOARDING.SUCCESS}
       component={OnboardingSuccess} // Used in SRP flow
-      options={{ headerShown: false }}
     />
     <Stack.Screen
       name={Routes.ONBOARDING.DEFAULT_SETTINGS} // This is being used in import wallet flow
@@ -890,6 +889,7 @@ const AppFlow = () => {
         <Stack.Screen
           name={Routes.ONBOARDING.SUCCESS_FLOW}
           component={OnboardingSuccessFlow}
+          options={{ headerShown: false }}
         />
         <Stack.Screen
           name={Routes.VAULT_RECOVERY.RESTORE_WALLET}
