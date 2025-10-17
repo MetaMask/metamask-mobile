d5cf526663d34e8560b4ea5be09a4fcc8d805d34
Patryk Łucka <5708018+PatrykLucka@users.noreply.github.com>
2025-10-17 15:47:43 +0200

    test: try to fix/workaround e2e

--- e2e/specs/accounts/create-wallet-account.spec.ts
+++ e2e/specs/accounts/create-wallet-account.spec.ts
@@ -7,11 +7,10 @@ import AccountDetails from '../../pages/MultichainAccounts/AccountDetails';
 import AddressList from '../../pages/MultichainAccounts/AddressList';
 import { defaultGanacheOptions } from '../../framework/Constants';
 import { completeSrpQuiz } from '../multisrp/utils';
-import TestHelpers from '../../helpers.js';
 
 describe(SmokeAccounts('Create wallet accounts'), () => {
   const FIRST = 0;
-  const LAST = 2;
+  const LAST = 3;
 
   it('should be able to add a new account and verify it is working', async () => {
     await withMultichainAccountDetailsV2EnabledFixtures(async () => {
@@ -23,17 +22,20 @@ describe(SmokeAccounts('Create wallet accounts'), () => {
       );
       await AccountListBottomSheet.tapCreateAccount(FIRST);
 
-      // FIXME: Creating account seems to take a bit of time, and the following assertion might not
-      // be correct either. Checking for "Account 3" is not enough, since there's already another
-      // account named that way in another wallet.
-      // For now, we just add a delay to make sure account indexing is correct, but we should
-      // probably change this and use the "Creating accounts..." status instead.
-      await TestHelpers.delay(2000);
+      // FIXME: tapAccountEllipsisButtonV2 seems to click wrong account, so we create
+      // 2nd account to use click the account by name
+      // We need to create "Account 4", as "Account 3" is already there from the beginning
+      await AccountListBottomSheet.tapCreateAccount(FIRST);
 
       // Account names are now per wallet, thus other accounts from the fixture (that are not associated
       // with the primary HD keyring) are not considered for account index. Current fixture uses 2 HD
       // accounts, thus the next one is: "Account 3".
-      const visibleAccounts = ['Account 1', 'Account 2', 'Account 3'];
+      const visibleAccounts = [
+        'Account 1',
+        'Account 2',
+        'Account 3',
+        'Account 4',
+      ];
       for (const accountName of visibleAccounts) {
         await Assertions.expectElementToBeVisible(
           AccountListBottomSheet.getAccountElementByAccountNameV2(accountName),
@@ -43,7 +45,7 @@ describe(SmokeAccounts('Create wallet accounts'), () => {
         );
       }
 
-      await AccountListBottomSheet.tapAccountEllipsisButtonV2(LAST);
+      await AccountListBottomSheet.tapAccountByNameV2('Account 4');
       await AccountDetails.tapNetworksLink();
 
       const visibleNetworks = [
