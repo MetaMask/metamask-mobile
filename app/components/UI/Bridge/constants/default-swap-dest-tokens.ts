77a344a1b9bb5987ea961015337624d2ab42ecdd
Vivek <106310394+vivek-consensys@users.noreply.github.com>
2025-10-21 14:37:28 +0400

    Update app/components/UI/Bridge/constants/default-swap-dest-tokens.ts

--- app/components/UI/Bridge/constants/default-swap-dest-tokens.ts
+++ app/components/UI/Bridge/constants/default-swap-dest-tokens.ts
@@ -88,7 +88,7 @@ export const DefaultSwapDestTokens: Record<Hex | CaipChainId, BridgeToken> = {
   [CHAIN_IDS.SEI]: {
     symbol: 'USDC',
     name: 'USD Coin',
-    address: '0xe15fC38F6D8c56aF07bbCBe3BAf5708A2Bf42392',
+    address: '0xe15fc38f6d8c56af07bbcbe3baf5708a2bf42392',
     decimals: 6,
     image:
       'https://static.cx.metamask.io/api/v1/tokenIcons/1329/0xe15fc38f6d8c56af07bbcbe3baf5708a2bf42392.png',
