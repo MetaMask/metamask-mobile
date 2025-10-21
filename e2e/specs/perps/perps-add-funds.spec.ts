consensys-test/metamask-mobile-test-workflow

fix: all test and remove skip

e2e/specs/perps/perps-add-funds.spec.ts
@@ -6,113 +6,120 @@ import { RegressionTrade } from '../../tags';
 import { loginToApp } from '../../viewHelper';
 import { PERPS_ARBITRUM_MOCKS } from '../../api-mocking/mock-responses/perps-arbitrum-mocks';
 import Assertions from '../../framework/Assertions';
-import Matchers from '../../framework/Matchers';
 import PerpsTabView from '../../pages/Perps/PerpsTabView';
 import { PerpsHelpers } from './helpers/perps-helpers';
 import WalletView from '../../pages/wallet/WalletView';
 import PerpsDepositView from '../../pages/Perps/PerpsDepositView';
 import PerpsE2E from '../../framework/PerpsE2E';
-import Gestures from '../../framework/Gestures';
 import ToastModal from '../../pages/wallet/ToastModal';
 import Utilities from '../../framework/Utilities';
 
 // We reuse the confirmation screen keyboard and continue button by text/selectors
 
-describe(RegressionTrade('Perps - Add funds (has funds, not first time)'), () => {
-  beforeEach(async () => {
-    jest.setTimeout(150000);
-  });
+describe(
+  RegressionTrade('Perps - Add funds (has funds, not first time)'),
+  () => {
+    beforeEach(async () => {
+      jest.setTimeout(150000);
+    });
 
-  it('deposits $100 from Add funds and verifies updated balance', async () => {
-    await withFixtures(
-      {
-        fixture: new FixtureBuilder()
-          .withPerpsProfile('no-positions')
-          .withPerpsFirstTimeUser(false)
-          .withKeyringControllerOfMultipleAccounts()
-          .withNetworkController({
-            providerConfig: {
-              type: 'rpc',
-              chainId: '0xa4b1',
-              rpcUrl: 'https://arb1.arbitrum.io/rpc',
-              nickname: 'Arbitrum One',
-              ticker: 'ETH',
-            },
-          })
-          .withTokensForAllPopularNetworks([
+    it('deposits $80 from Add funds and verifies updated balance', async () => {
+      await withFixtures(
+        {
+          fixture: new FixtureBuilder()
+            .withPerpsProfile('no-positions')
+            .withPerpsFirstTimeUser(false)
+            .withKeyringControllerOfMultipleAccounts()
+            .withNetworkController({
+              providerConfig: {
+                type: 'rpc',
+                chainId: '0xa4b1',
+                rpcUrl: 'https://arb1.arbitrum.io/rpc',
+                nickname: 'Arbitrum One',
+                ticker: 'ETH',
+              },
+            })
+            .withTokensForAllPopularNetworks([
+              {
+                address: '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8',
+                symbol: 'USDC',
+                decimals: 6,
+                name: 'USD Coin',
+                type: 'erc20',
+              },
+            ])
+            .build(),
+          restartDevice: true,
+          testSpecificMock: PERPS_ARBITRUM_MOCKS,
+          localNodeOptions: [
             {
-              address: '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8',
-              symbol: 'USDC',
-              decimals: 6,
-              name: 'USD Coin',
-              type: 'erc20',
+              type: LocalNodeType.anvil,
+              options: { hardfork: 'prague' as Hardfork },
             },
-          ])
-          .build(),
-        restartDevice: true,
-        testSpecificMock: PERPS_ARBITRUM_MOCKS,
-        localNodeOptions: [
-          {
-            type: LocalNodeType.anvil,
-            options: { hardfork: 'prague' as Hardfork },
-          },
-        ],
-      },
-      async () => {
-        await loginToApp();
+          ],
+        },
+        async () => {
+          await loginToApp();
 
-        await Assertions.expectElementToBeVisible(WalletView.container as DetoxElement, {
-          description: 'Wallet view visible',
-        });
-
-        // Go to Perps tab
-        await PerpsHelpers.navigateToPerpsTab();
-
-        // Read initial balance text for later comparison
-        const initialBalance = await PerpsTabView.getBalance();
-
-        // Open Add Funds from balance menu
-        await PerpsTabView.tapBalanceButton();
-        await PerpsTabView.tapAddFundsButton();
-
-        // If a network-added toast appears, wait for it to disappear before interacting
-        await Assertions.expectElementToNotBeVisible(
-          ToastModal.container as DetoxElement,
-          { description: 'No toast visible before entering amount', timeout: 10000 },
-        );
-
-        // Ensure deposit UI visible
-        await PerpsDepositView.expectLoaded();
-
-        // Open Pay with selector and choose USDC
-        await PerpsDepositView.selectUSDC();
-
-        // Focus and type 80 using keypad helpers
-        await PerpsDepositView.focusAmount();
-        await PerpsDepositView.typeUSD('80');
-
-        // Continue and Confirm
-        await PerpsDepositView.tapContinue();
-        await PerpsDepositView.tapConfirm();
-
-        // Apply deposit mock and wait for wallet balance to increase
-        await PerpsE2E.applyDepositUSD('80');
-        await Utilities.waitUntil(
-          async () => {
-            const current = await PerpsTabView.getBalance();
-            return current > initialBalance;
-          },
-          { interval: 500, timeout: 15000 },
-        );
-
-        // Back on Perps tab, read balance and assert increment by 80
-        const updatedBalance = await PerpsTabView.getBalance();
-        if (!(updatedBalance > initialBalance)) {
-          throw new Error(
-            `Balance did not increase: before=${initialBalance}, after=${updatedBalance}`,
+          await Assertions.expectElementToBeVisible(
+            WalletView.container as DetoxElement,
+            {
+              description: 'Wallet view visible',
+            },
           );
-        }
-      },
-    );
-  });
-});
+
+          // Go to Perps tab
+          await PerpsHelpers.navigateToPerpsTab();
+
+          // Read initial balance text for later comparison
+          const initialBalance = await PerpsTabView.getBalance();
+
+          // Open Add Funds from balance menu
+          await PerpsTabView.tapBalanceButton();
+          await PerpsTabView.tapAddFundsButton();
+
+          // If a network-added toast appears, wait for it to disappear before interacting
+          await Assertions.expectElementToNotBeVisible(
+            ToastModal.container as DetoxElement,
+            {
+              description: 'No toast visible before entering amount',
+              timeout: 10000,
+            },
+          );
+
+          // Ensure deposit UI visible
+          await PerpsDepositView.expectLoaded();
+
+          // Open Pay with selector and choose USDC
+          await PerpsDepositView.selectUSDC();
+
+          // Focus and type 80 using keypad helpers
+          await PerpsDepositView.focusAmount();
+          await PerpsDepositView.typeUSD('80');
+
+          // Continue and Confirm
+          await PerpsDepositView.tapContinue();
+          await PerpsDepositView.tapConfirm();
+
+          // Apply deposit mock and wait for wallet balance to increase
+          await PerpsE2E.applyDepositUSD('80');
+          await Utilities.waitUntil(
+            async () => {
+              const current = await PerpsTabView.getBalance();
+              return current > initialBalance;
+            },
+            { interval: 500, timeout: 15000 },
+          );
+
+          // Back on Perps tab, read balance and assert increment by 80
+          const updatedBalance = await PerpsTabView.getBalance();
+          if (!(updatedBalance > initialBalance)) {
+            throw new Error(
+              `Balance did not increase: before=${initialBalance}, after=${updatedBalance}`,
+            );
+          }
+        },
+      );
+    });
+  },
+);

e2e/specs/perps/perps-position-liquidation.spec.ts
@@ -21,7 +21,7 @@ const logger = createLogger({
 });
 
 describe(RegressionTrade('Perps Position'), () => {
-  it.skip('should open a long position with custom profit and close it', async () => {
+  it('should open a long position with custom profit and close it', async () => {
     await withFixtures(
       {
         fixture: new FixtureBuilder().build(),
@@ -32,17 +32,13 @@ describe(RegressionTrade('Perps Position'), () => {
         logger.info('💰 Using E2E mock balance - no wallet import needed');
         logger.info('🎯 Mock account: $10,000 total, $8,000 available');
         await loginToApp();
-
         // Navigate to Perps tab using manual sync management
         await PerpsHelpers.navigateToPerpsTab();
 
-        // Navigate to actions
-        await TabBarComponent.tapActions();
-
-        await WalletActionsBottomSheet.tapPerpsButton();
+        await WalletActionsBottomSheet.tapStartANewTradeButton();
 
         await device.disableSynchronization();
-        await PerpsMarketListView.tapFirstMarketRowItem();
+        await PerpsMarketListView.tapMarketRowItem('ETH');
         await PerpsMarketDetailsView.tapLongButton();
 
         await PerpsView.tapPlaceOrderButton();
@@ -51,27 +47,28 @@ describe(RegressionTrade('Perps Position'), () => {
         logger.info('💎 E2E Mock: Position created with mock data');
 
         await PerpsView.tapBackButtonPositionSheet();
-        await PerpsView.tapBackButtonMarketList();
+        // Next line is a workaround to go to wallet from Perps
+        await TabBarComponent.tapHome();
 
         // add price change and liquidation -> not yet liquidated
-        await PerpsE2E.updateMarketPrice('BTC', '80000.00');
+        await PerpsE2E.updateMarketPrice('BTC', '50000.00');
         await PerpsE2E.triggerLiquidation('BTC');
         logger.info('🔥 E2E Mock: Liquidation triggered. Not yet liquidated');
 
         // Assertion 1: still have 2 positions (the default and the recently opened)
         await PerpsView.ensurePerpsTabPositionVisible('BTC', 5, 'long', 0);
-        await PerpsView.ensurePerpsTabPositionVisible('BTC', 3, 'long', 1);
+        await PerpsView.ensurePerpsTabPositionVisible('ETH', 3, 'long', 1);
 
         // add price change and force liquidation - BTC below 30k triggers default BTC liquidation
-        await PerpsE2E.updateMarketPrice('BTC', '30000.00');
+        await PerpsE2E.updateMarketPrice('BTC', '10000.00');
         await PerpsE2E.triggerLiquidation('BTC');
         logger.info('🔥 E2E Mock: Liquidation triggered. Liquidated');
 
         // Assertion 2: only BTC 3x is visible
         // 1) The expected (first item) exists and is visible
         await Assertions.expectElementToBeVisible(
-          PerpsView.getPositionItem('BTC', 3, 'long', 0),
-          { description: 'BTC 3x long en índice 0' },
+          PerpsView.getPositionItem('ETH', 3, 'long', 0),
+          { description: 'ETH 3x long en índice 0' },
         );
 
         // 2) There is no second item of position (verification by index with base ID)

MetaMask/metamask-mobile

e2e/specs/perps/perps-add-funds.spec.ts
@@ -16,101 +16,103 @@ import Utilities from '../../framework/Utilities';
 
 // We reuse the confirmation screen keyboard and continue button by text/selectors
 
-describe(RegressionTrade('Perps - Add funds (has funds, not first time)'), () => {
-  beforeEach(async () => {
-    jest.setTimeout(150000);
-  });
+describe(
+  RegressionTrade('Perps - Add funds (has funds, not first time)'),
+  () => {
+    beforeEach(async () => {
+      jest.setTimeout(150000);
+    });
 
-  it('deposits $100 from Add funds and verifies updated balance', async () => {
-    await withFixtures(
-      {
-        fixture: new FixtureBuilder()
-          .withPerpsProfile('no-positions')
-          .withPerpsFirstTimeUser(false)
-          .withKeyringControllerOfMultipleAccounts()
-          .withNetworkController({
-            providerConfig: {
-              type: 'rpc',
-              chainId: '0xa4b1',
-              rpcUrl: 'https://arb1.arbitrum.io/rpc',
-              nickname: 'Arbitrum One',
-              ticker: 'ETH',
-            },
-          })
-          .withTokensForAllPopularNetworks([
+    it('deposits $80 from Add funds and verifies updated balance', async () => {
+      await withFixtures(
+        {
+          fixture: new FixtureBuilder()
+            .withPerpsProfile('no-positions')
+            .withPerpsFirstTimeUser(false)
+            .withKeyringControllerOfMultipleAccounts()
+            .withNetworkController({
+              providerConfig: {
+                type: 'rpc',
+                chainId: '0xa4b1',
+                rpcUrl: 'https://arb1.arbitrum.io/rpc',
+                nickname: 'Arbitrum One',
+                ticker: 'ETH',
+              },
+            })
+            .withTokensForAllPopularNetworks([
+              {
+                address: '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8',
+                symbol: 'USDC',
+                decimals: 6,
+                name: 'USD Coin',
+                type: 'erc20',
+              },
+            ])
+            .build(),
+          restartDevice: true,
+          testSpecificMock: PERPS_ARBITRUM_MOCKS,
+          localNodeOptions: [
             {
-              address: '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8',
-              symbol: 'USDC',
-              decimals: 6,
-              name: 'USD Coin',
-              type: 'erc20',
+              type: LocalNodeType.anvil,
+              options: { hardfork: 'prague' as Hardfork },
             },
-          ])
-          .build(),
-        restartDevice: true,
-        testSpecificMock: PERPS_ARBITRUM_MOCKS,
-        localNodeOptions: [
-          {
-            type: LocalNodeType.anvil,
-            options: { hardfork: 'prague' as Hardfork },
-          },
-        ],
-      },
-      async () => {
-        await loginToApp();
+          ],
+        },
+        async () => {
+          await loginToApp();
+          
+          // Go to Perps tab
+          await PerpsHelpers.navigateToPerpsTab();
 
-        await Assertions.expectElementToBeVisible(WalletView.container as DetoxElement, {
-          description: 'Wallet view visible',
-        });
+          // Read initial balance text for later comparison
+          const initialBalance = await PerpsTabView.getBalance();
 
-        // Go to Perps tab
-        await PerpsHelpers.navigateToPerpsTab();
+          // Open Add Funds from balance menu
+          await PerpsTabView.tapBalanceButton();
+          await PerpsTabView.tapAddFundsButton();
 
-        // Read initial balance text for later comparison
-        const initialBalance = await PerpsTabView.getBalance();
-
-        // Open Add Funds from balance menu
-        await PerpsTabView.tapBalanceButton();
-        await PerpsTabView.tapAddFundsButton();
-
-        // If a network-added toast appears, wait for it to disappear before interacting
-        await Assertions.expectElementToNotBeVisible(
-          ToastModal.container as DetoxElement,
-          { description: 'No toast visible before entering amount', timeout: 10000 },
-        );
-
-        // Ensure deposit UI visible
-        await PerpsDepositView.expectLoaded();
-
-        // Open Pay with selector and choose USDC
-        await PerpsDepositView.selectUSDC();
-
-        // Focus and type 80 using keypad helpers
-        await PerpsDepositView.focusAmount();
-        await PerpsDepositView.typeUSD('80');
-
-        // Continue and Confirm
-        await PerpsDepositView.tapContinue();
-        await PerpsDepositView.tapConfirm();
-
-        // Apply deposit mock and wait for wallet balance to increase
-        await PerpsE2EModifiers.applyDepositUSD('80');
-        await Utilities.waitUntil(
-          async () => {
-            const current = await PerpsTabView.getBalance();
-            return current > initialBalance;
-          },
-          { interval: 500, timeout: 15000 },
-        );
-
-        // Back on Perps tab, read balance and assert increment by 80
-        const updatedBalance = await PerpsTabView.getBalance();
-        if (!(updatedBalance > initialBalance)) {
-          throw new Error(
-            `Balance did not increase: before=${initialBalance}, after=${updatedBalance}`,
+          // If a network-added toast appears, wait for it to disappear before interacting
+          await Assertions.expectElementToNotBeVisible(
+            ToastModal.container as DetoxElement,
+            {
+              description: 'No toast visible before entering amount',
+              timeout: 10000,
+            },
           );
-        }
-      },
-    );
-  });
-});
+
+          // Ensure deposit UI visible
+          await PerpsDepositView.expectLoaded();
+
+          // Open Pay with selector and choose USDC
+          await PerpsDepositView.selectUSDC();
+
+          // Focus and type 80 using keypad helpers
+          await PerpsDepositView.focusAmount();
+          await PerpsDepositView.typeUSD('80');
+
+          // Continue and Confirm
+          await PerpsDepositView.tapContinue();
+          await PerpsDepositView.tapConfirm();
+
+          // Apply deposit mock and wait for wallet balance to increase
+          await PerpsE2EModifiers.applyDepositUSD('80');
+          await Utilities.waitUntil(
+            async () => {
+              const current = await PerpsTabView.getBalance();
+              return current > initialBalance;
+            },
+            { interval: 500, timeout: 15000 },
+          );
+
+          // Back on Perps tab, read balance and assert increment by 80
+          const updatedBalance = await PerpsTabView.getBalance();
+          if (!(updatedBalance > initialBalance)) {
+            throw new Error(
+              `Balance did not increase: before=${initialBalance}, after=${updatedBalance}`,
+            );
+          }
+        },
+      );
+    });
+  },
+);

e2e/specs/perps/perps-position-liquidation.spec.ts
@@ -21,7 +21,7 @@ const logger = createLogger({
 });
 
 describe(RegressionTrade('Perps Position'), () => {
-  it.skip('should open a long position with custom profit and close it', async () => {
+  it('should open a long position with custom profit and close it', async () => {
     await withFixtures(
       {
         fixture: new FixtureBuilder().build(),
@@ -32,17 +32,13 @@ describe(RegressionTrade('Perps Position'), () => {
         logger.info('💰 Using E2E mock balance - no wallet import needed');
         logger.info('🎯 Mock account: $10,000 total, $8,000 available');
         await loginToApp();
-
         // Navigate to Perps tab using manual sync management
         await PerpsHelpers.navigateToPerpsTab();
 
-        // Navigate to actions
-        await TabBarComponent.tapActions();
-
-        await WalletActionsBottomSheet.tapPerpsButton();
+        await WalletActionsBottomSheet.tapStartANewTradeButton();
 
         await device.disableSynchronization();
-        await PerpsMarketListView.tapFirstMarketRowItem();
+        await PerpsMarketListView.tapMarketRowItem('ETH');
         await PerpsMarketDetailsView.tapLongButton();
 
         await PerpsView.tapPlaceOrderButton();
@@ -51,27 +47,28 @@ describe(RegressionTrade('Perps Position'), () => {
         logger.info('💎 E2E Mock: Position created with mock data');
 
         await PerpsView.tapBackButtonPositionSheet();
-        await PerpsView.tapBackButtonMarketList();
+        // Next line is a workaround to go to wallet from Perps
+        await TabBarComponent.tapHome();
 
         // add price change and liquidation -> not yet liquidated
-        await PerpsE2EModifiers.updateMarketPrice('BTC', '80000.00');
+        await PerpsE2EModifiers.updateMarketPrice('BTC', '50000.00');
         await PerpsE2EModifiers.triggerLiquidation('BTC');
         logger.info('🔥 E2E Mock: Liquidation triggered. Not yet liquidated');
 
         // Assertion 1: still have 2 positions (the default and the recently opened)
         await PerpsView.ensurePerpsTabPositionVisible('BTC', 5, 'long', 0);
-        await PerpsView.ensurePerpsTabPositionVisible('BTC', 3, 'long', 1);
+        await PerpsView.ensurePerpsTabPositionVisible('ETH', 3, 'long', 1);
 
         // add price change and force liquidation - BTC below 30k triggers default BTC liquidation
-        await PerpsE2EModifiers.updateMarketPrice('BTC', '30000.00');
+        await PerpsE2EModifiers.updateMarketPrice('BTC', '10000.00');
         await PerpsE2EModifiers.triggerLiquidation('BTC');
         logger.info('🔥 E2E Mock: Liquidation triggered. Liquidated');
 
         // Assertion 2: only BTC 3x is visible
         // 1) The expected (first item) exists and is visible
         await Assertions.expectElementToBeVisible(
-          PerpsView.getPositionItem('BTC', 3, 'long', 0),
-          { description: 'BTC 3x long en índice 0' },
+          PerpsView.getPositionItem('ETH', 3, 'long', 0),
+          { description: 'ETH 3x long en índice 0' },
         );
 
         // 2) There is no second item of position (verification by index with base ID)
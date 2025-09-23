2ec940952c49dc41d34c472a3c8d9ebf3e8e89b3
Racitores <ramon.acitores@consensys.net>
2025-09-23 16:12:40 +0200

    fix: all test and remove skip

--- e2e/selectors/wallet/TabBar.selectors.js
+++ e2e/selectors/wallet/TabBar.selectors.js
@@ -2,6 +2,7 @@ export const TabBarSelectorIDs = {
   WALLET: 'tab-bar-item-Wallet',
   BROWSER: 'tab-bar-item-Browser',
   ACTIONS: 'trade-tab-bar-item-icon-container',
+  TRADE: 'tab-bar-item-Trade',
   SETTING: 'tab-bar-item-Setting',
   ACTIVITY: 'tab-bar-item-Activity',
   TRADE: 'tab-bar-item-Trade',
