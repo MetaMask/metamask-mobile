1cfa9eafbee6438717293405cb1a59cb99738f21
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2025-10-22 12:26:39 +0200

    Use fixture for Snap lifecycle test

--- e2e/specs/snaps/test-snap-lifecycle.spec.ts
+++ e2e/specs/snaps/test-snap-lifecycle.spec.ts
@@ -1,7 +1,5 @@
 import FixtureBuilder from '../../framework/fixtures/FixtureBuilder';
 import { withFixtures } from '../../framework/fixtures/FixtureHelper';
-import { getFixturesServerPort } from '../../framework/fixtures/FixtureUtils';
-import TestHelpers from '../../helpers';
 import TestSnaps from '../../pages/Browser/TestSnaps';
 import TabBarComponent from '../../pages/wallet/TabBarComponent';
 import { FlaskBuildTests } from '../../tags';
@@ -34,21 +32,14 @@ describe(FlaskBuildTests('Lifecycle hooks Snap Tests'), () => {
   it('runs the onStart lifecycle hook when the client is started', async () => {
     await withFixtures(
       {
-        fixture: new FixtureBuilder().build(),
+        fixture: new FixtureBuilder()
+          .withSnapControllerOnStartLifecycleSnap()
+          .build(),
+        restartDevice: true,
         skipReactNativeReload: true,
       },
       async () => {
-        await TestHelpers.terminateApp();
-        await TestHelpers.launchApp({
-          launchArgs: { fixtureServerPort: `${getFixturesServerPort()}` },
-        });
-
-        try {
-          await loginToApp();
-        } catch {
-          // The assertions inside may fail due to the ongoing test.
-        }
-
+        await loginToApp();
         await Assertions.checkIfTextIsDisplayed(
           'The client was started successfully, and the "onStart" handler was called.',
         );
