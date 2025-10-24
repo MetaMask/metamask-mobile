dc0df0578332a48f00c7b913241e128a9b36c78c
tommasini <46944231+tommasini@users.noreply.github.com>
2025-10-24 16:36:11 +0100

    fix conflicts

--- app/components/Views/MultichainAccounts/AccountGroupDetails/components/SecretRecoveryPhrase.tsx
+++ app/components/Views/MultichainAccounts/AccountGroupDetails/components/SecretRecoveryPhrase.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useMemo } from 'react';
+import { TouchableOpacity } from 'react-native';
 import { useSelector } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 import { InternalAccount } from '@metamask/keyring-internal-api';
@@ -20,7 +21,6 @@ import { areAddressesEqual } from '../../../../../util/address';
 import { AccountDetailsIds } from '../../../../../../e2e/selectors/MultichainAccounts/AccountDetails.selectors';
 import { useStyles } from '../../../../hooks/useStyles';
 import styleSheet from '../AccountGroupDetails.styles';
-import TempTouchableOpacity from '../../../../../component-library/components-temp/TempTouchableOpacity';
 
 export interface SecretRecoveryPhraseProps {
   account: InternalAccount;
@@ -59,11 +59,10 @@ export const SecretRecoveryPhrase = ({
   }, [navigation, account?.options.entropySource]);
 
   return (
-    <TempTouchableOpacity
+    <TouchableOpacity
       style={styles.secretRecoveryPhrase}
       testID={AccountDetailsIds.SECRET_RECOVERY_PHRASE_LINK}
       onPress={onExportMnemonic}
-      shouldEnableAndroidPressIn
     >
       <Text variant={TextVariant.BodyMDMedium}>
         {strings('multichain_accounts.account_details.secret_recovery_phrase')}
@@ -87,6 +86,6 @@ export const SecretRecoveryPhrase = ({
           color={colors.text.alternative}
         />
       </Box>
-    </TempTouchableOpacity>
+    </TouchableOpacity>
   );
 };
