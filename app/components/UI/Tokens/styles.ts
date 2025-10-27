cd854541cb34cf2e40297262e797e1885c753058
Brian Nguyen <brianacnguyen@gmail.com>
2025-10-27 11:54:05 -0700

    Reverting extra unnecessary changes

--- app/components/UI/Tokens/styles.ts
+++ app/components/UI/Tokens/styles.ts
@@ -13,36 +13,6 @@ const createStyles = (colors: Colors) =>
     bottomSheetText: {
       width: '100%',
     },
-    bottomModal: {
-      justifyContent: 'flex-end',
-      margin: 0,
-    },
-    box: {
-      backgroundColor: colors.background.default,
-      paddingHorizontal: 8,
-      paddingBottom: 20,
-      borderWidth: 0,
-      padding: 0,
-    },
-    boxContent: {
-      backgroundColor: colors.background.default,
-      paddingBottom: 21,
-      paddingTop: 0,
-      borderWidth: 0,
-    },
-    notch: {
-      width: 40,
-      height: 4,
-      borderRadius: 2,
-      backgroundColor: colors.border.muted,
-      alignSelf: 'center',
-      marginTop: 4,
-    },
-    editNetworkButton: {
-      width: '100%',
-    },
-
-    // Empty State Styles
     emptyView: {
       backgroundColor: colors.background.default,
       justifyContent: 'center',
@@ -56,8 +26,6 @@ const createStyles = (colors: Colors) =>
     emptyTokensViewText: {
       fontFamily: 'Geist Medium',
     },
-
-    // Token List Item Styles
     ethLogo: {
       width: 40,
       height: 40,
@@ -98,7 +66,6 @@ const createStyles = (colors: Colors) =>
       alignItems: 'center',
       alignContent: 'center',
     },
-
     stakeButton: {
       flexDirection: 'row',
     },
@@ -106,10 +73,36 @@ const createStyles = (colors: Colors) =>
       marginLeft: 2,
       marginRight: 2,
     },
-
-    // Portfolio Balance Styles
     portfolioBalance: {
       marginHorizontal: 16,
+    },
+    bottomModal: {
+      justifyContent: 'flex-end',
+      margin: 0,
+    },
+    box: {
+      backgroundColor: colors.background.default,
+      paddingHorizontal: 8,
+      paddingBottom: 20,
+      borderWidth: 0,
+      padding: 0,
+    },
+    boxContent: {
+      backgroundColor: colors.background.default,
+      paddingBottom: 21,
+      paddingTop: 0,
+      borderWidth: 0,
+    },
+    editNetworkButton: {
+      width: '100%',
+    },
+    notch: {
+      width: 40,
+      height: 4,
+      borderRadius: 2,
+      backgroundColor: colors.border.muted,
+      alignSelf: 'center',
+      marginTop: 4,
     },
     balanceContainer: {
       flexDirection: 'row',
@@ -118,13 +111,9 @@ const createStyles = (colors: Colors) =>
     loaderWrapper: {
       paddingLeft: 40,
     },
-
-    // Control Bar Styles
     controlIconButton: {
       backgroundColor: colors.background.default,
     },
-
-    // Network Image Styles
     networkImageContainer: {
       position: 'absolute',
       right: 0,
