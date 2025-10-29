d509c3c4ec15b759bd223fb371ab6008097a715d
João <castrofjoao@gmail.com>
2025-10-29 16:11:06 +0000

    rock config

---
+++ rock.config.mjs
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-default-export */
+
+// @ts-nocheck - Rock CLI packages only available in CI environment
+import { platformIOS } from '@rock-js/platform-ios';
+import { pluginMetro } from '@rock-js/plugin-metro';
+
+/** @type {import('rock').Config} */
+export default {
+  bundler: pluginMetro(),
+  platforms: {
+    ios: platformIOS({
+      scheme: 'MetaMask',
+      configuration: 'Release',
+    }),
+  },
+  remoteCacheProvider: 'github-actions',
+  fingerprint: {
+    extraSources: [
+      'METAMASK_ENVIRONMENT',
+      'METAMASK_BUILD_TYPE',
+      'E2E',
+    ],
+  },
+};
+
