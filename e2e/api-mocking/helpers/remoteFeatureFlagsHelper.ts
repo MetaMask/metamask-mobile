f454b02e4b1deb3313b3508c72a1b0bfafe55506
Florin Dzeladini <florin.dzeladini@consensys.net>
2025-10-29 08:46:27 +0100

    feat: add blacklist custom network feature flag

--- e2e/api-mocking/helpers/remoteFeatureFlagsHelper.ts
+++ e2e/api-mocking/helpers/remoteFeatureFlagsHelper.ts
@@ -267,6 +267,9 @@ const DEFAULT_FEATURE_FLAGS_ARRAY: Record<string, unknown>[] = [
   {
     predictEnabled: false,
   },
+  {
+    additionalNetworkBlacklist: [], // Empty by default, can be overridden in tests
+  },
 ];
 
 /**
