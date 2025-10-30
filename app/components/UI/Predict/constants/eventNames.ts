a21d59a4f4b253df8d57b8552866d741f90740bc
Luis Taniça <matallui@gmail.com>
2025-10-30 12:45:43 -0600

    fix(predict): unit tests and tsc errors

--- app/components/UI/Predict/constants/eventNames.ts
+++ app/components/UI/Predict/constants/eventNames.ts
@@ -20,7 +20,7 @@ export const PredictEventProperties = {
   FAILURE_REASON: 'failure_reason',
 
   // Sensitive properties
-  USD_AMOUNT: 'usc_amount',
+  USD_AMOUNT: 'usd_amount',
   SHARE_PRICE: 'share_price',
   ORDER_ID: 'order_id',
   USER_ADDRESS: 'user_address',
