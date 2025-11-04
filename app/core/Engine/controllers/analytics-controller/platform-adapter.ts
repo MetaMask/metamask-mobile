55280ce07a6bc9c470277646be191a795c8d9fc2
Nicolas MASSART <nicolas.massart@consensys.net>
2025-11-04 18:24:28 +0100

    format: prettier pass

--- app/core/Engine/controllers/analytics-controller/platform-adapter.ts
+++ app/core/Engine/controllers/analytics-controller/platform-adapter.ts
@@ -8,18 +8,15 @@ import type {
  * TODO: Implement the adapter to integrate with MetaMetrics.
  */
 export const createPlatformAdapter = (): AnalyticsPlatformAdapter => ({
-    trackEvent(
-      _eventName: string,
-      _properties: AnalyticsEventProperties,
-    ): void {
-      // TODO: Implement trackEvent integration with MetaMetrics
-    },
+  trackEvent(_eventName: string, _properties: AnalyticsEventProperties): void {
+    // TODO: Implement trackEvent integration with MetaMetrics
+  },
 
-    identify(_userId: string, _traits?: AnalyticsEventProperties): void {
-      // TODO: Implement identify integration with MetaMetrics
-    },
+  identify(_userId: string, _traits?: AnalyticsEventProperties): void {
+    // TODO: Implement identify integration with MetaMetrics
+  },
 
-    trackPage(_pageName: string, _properties?: AnalyticsEventProperties): void {
-      // TODO: Implement trackPage integration with MetaMetrics
-    },
-  });
+  trackPage(_pageName: string, _properties?: AnalyticsEventProperties): void {
+    // TODO: Implement trackPage integration with MetaMetrics
+  },
+});
