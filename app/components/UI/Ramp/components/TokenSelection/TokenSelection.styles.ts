291b2bafea66cf8cc8db375484efe6e128e8dbf6
George Weiler <george.weiler@consensys.net>
2025-11-05 06:54:28 -0700

    chore: formatting

--- app/components/UI/Ramp/components/TokenSelection/TokenSelection.styles.ts
+++ app/components/UI/Ramp/components/TokenSelection/TokenSelection.styles.ts
@@ -1,7 +1,8 @@
 import { StyleSheet } from 'react-native';
 import { Theme } from '../../../../../util/theme/models';
 
-const styleSheet = (params: { theme: Theme }) => StyleSheet.create({
+const styleSheet = (params: { theme: Theme }) =>
+  StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: params.theme.colors.background.default,
