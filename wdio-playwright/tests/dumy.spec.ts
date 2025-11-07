258e8fabbb5d744d89a158d6dee861e74f279c4e
Christopher Ferreira <christopher.ferreira@consensys.net>
2025-11-07 11:10:49 +0000

    test: wip

--- wdio-playwright/tests/dumy.spec.ts
+++ wdio-playwright/tests/dumy.spec.ts
@@ -2,13 +2,20 @@ import { test } from '../fixture';
 import { expect } from '@playwright/test';
 import LoginView from '../../e2e/pages/wallet/LoginView';
 
+const W3C = 'element-6066-11e4-a52e-4f735466cecf';
+
 test('dummy test', async ({ driver }) => {
-  const element = await driver.findElement(
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const el = await driver.findElement(
     'xpath',
-    '//*[@text="Welcome Back!"]',
+    '//*[@resource-id="login-password-input"]',
   );
-  expect(element).toBeDefined();
-  // Use existing Detox-style PO with Appium-backed compat layer
-  await LoginView.enterPassword('Secret123');
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  // const elId = (el as any)[W3C] ?? (el as any).ELEMENT;
+  await driver.elementSendKeys(el['element-6066-11e4-a52e-4f735466cecf'], 'Secret123');
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  // expect(element).toBeDefined();
+  // // Use existing Detox-style PO with Appium-backed compat layer
+  // await LoginView.enterPassword('Secret123');
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
 });
