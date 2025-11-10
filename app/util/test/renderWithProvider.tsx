d53252df1c71cadbd745204c13f8e60ed5fb3513
sallem-consensys <sallem.ahmed@consensys.net>
2025-11-10 17:58:03 -0500

    remove Feature Flag Wrapper

--- app/util/test/renderWithProvider.tsx
+++ app/util/test/renderWithProvider.tsx
@@ -16,7 +16,6 @@ import { mockTheme, ThemeContext } from '../theme';
 import { Theme } from '../theme/models';
 import configureStore from './configureStore';
 import { RootState } from '../../reducers';
-import { FeatureFlagOverrideProvider } from '../../contexts/FeatureFlagOverrideContext';
 
 // DeepPartial is a generic type that recursively makes all properties of a given type T optional
 export type DeepPartial<T> = T extends (...args: unknown[]) => unknown
@@ -47,9 +46,7 @@ export default function renderWithProvider(
 
   const InnerProvider = ({ children }: { children: React.ReactElement }) => (
     <Provider store={store}>
-      <ThemeContext.Provider value={theme}>
-        <FeatureFlagOverrideProvider>{children}</FeatureFlagOverrideProvider>
-      </ThemeContext.Provider>
+      <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
     </Provider>
   );
 
