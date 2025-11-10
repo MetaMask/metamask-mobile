01a328ab83fe53e81b7b67ea9c1eefa6645c797b
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2025-11-10 17:59:56 -0300

    feat(ramp): add buy settings modal event

--- app/components/UI/Ramp/Aggregator/Views/Modals/Settings/SettingsModal.test.tsx
+++ app/components/UI/Ramp/Aggregator/Views/Modals/Settings/SettingsModal.test.tsx
@@ -3,14 +3,19 @@ import { fireEvent } from '@testing-library/react-native';
 
 // Internal dependencies.
 import SettingsModal from './SettingsModal';
-import { renderScreen } from '../../../../../../../util/test/renderWithProvider';
+import {
+  DeepPartial,
+  renderScreen,
+} from '../../../../../../../util/test/renderWithProvider';
 import { backgroundState } from '../../../../../../../util/test/initial-root-state';
 import Routes from '../../../../../../../constants/navigation/Routes';
 import { createDepositNavigationDetails } from '../../../../Deposit/routes/utils';
+import { RampSDK } from '../../../sdk';
 
 const mockNavigate = jest.fn();
 const mockGoBack = jest.fn();
 const mockDangerouslyGetParent = jest.fn();
+const mockTrackEvent = jest.fn();
 
 jest.mock('@react-navigation/native', () => {
   const actualReactNavigation = jest.requireActual('@react-navigation/native');
@@ -24,6 +29,17 @@ jest.mock('@react-navigation/native', () => {
     }),
   };
 });
+
+jest.mock('../../../../hooks/useAnalytics', () => () => mockTrackEvent);
+
+const mockUseRampSDKValues: DeepPartial<RampSDK> = {
+  selectedRegion: { id: 'us' },
+};
+
+jest.mock('../../../sdk', () => ({
+  ...jest.requireActual('../../../sdk'),
+  useRampSDK: () => mockUseRampSDKValues,
+}));
 
 function render() {
   return renderScreen(
@@ -145,6 +161,19 @@ describe('SettingsModal', () => {
 
       expect(mockNavigate).not.toHaveBeenCalled();
       expect(mockDangerouslyGetParent).not.toHaveBeenCalled();
+    });
+
+    it('tracks event when deposit is pressed', () => {
+      const { getByText } = render();
+      const newBuyExperienceButton = getByText('Use new buy experience');
+
+      fireEvent.press(newBuyExperienceButton);
+
+      expect(mockTrackEvent).toHaveBeenCalledWith('RAMPS_BUTTON_CLICKED', {
+        location: 'Settings Modal',
+        ramp_type: 'DEPOSIT',
+        region: 'us',
+      });
     });
   });
 });
