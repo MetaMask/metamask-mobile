0798b034d1a5faa47593c01162df534c106cde4e
sallem-consensys <sallem.ahmed@consensys.net>
2025-11-12 18:45:11 -0500

    add use-feature-flag

---
+++ app/components/hooks/useFeatureFlag.ts
@@ -0,0 +1,16 @@
+import { useSelector } from 'react-redux';
+import { useFeatureFlagOverride } from '../../../contexts/FeatureFlagOverrideContext';
+import { selectBasicFunctionalityEnabled } from '../../../selectors/settings';
+
+export enum FeatureFlagNames {}
+
+export const useFeatureFlag = (key: FeatureFlagNames) => {
+  const { getFeatureFlag } = useFeatureFlagOverride();
+  const isBasicFunctionalityEnabled = useSelector(
+    selectBasicFunctionalityEnabled,
+  );
+  if (!isBasicFunctionalityEnabled) {
+    return false;
+  }
+  return getFeatureFlag(key);
+};
