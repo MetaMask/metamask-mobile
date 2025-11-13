c051862592a76e3f173a3e7f3d567ae5ca65f143
Patryk Lucka <5708018+PatrykLucka@users.noreply.github.com>
2025-11-13 16:21:10 +0100

    style: Update NetworkVerificationInfo styles and tests

--- app/components/UI/NetworkVerificationInfo/NetworkVerificationInfo.test.tsx
+++ app/components/UI/NetworkVerificationInfo/NetworkVerificationInfo.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NetworkVerificationInfo from './NetworkVerificationInfo';
-import { render, fireEvent } from '@testing-library/react-native';
+import { render } from '@testing-library/react-native';
 import { BannerAlertSeverity } from '../../../component-library/components/Banners/Banner';
 import { strings } from '../../../../locales/i18n';
 import { useSelector } from 'react-redux';
@@ -128,8 +128,9 @@ describe('NetworkVerificationInfo', () => {
     ).toThrow('Unable to find an element with text');
   });
 
-  it('should render chainId as a decimal', () => {
+  it('renders chainId as a decimal', () => {
     (useSelector as jest.Mock).mockReturnValue(true);
+
     const { getByText } = render(
       <NetworkVerificationInfo
         customNetworkInformation={mockNetworkInfo}
@@ -137,12 +138,6 @@ describe('NetworkVerificationInfo', () => {
         onConfirm={() => undefined}
       />,
     );
-
-    // Accordion content is hidden by default, so we need to expand it
-    const accordionButton = getByText(
-      strings('spend_limit_edition.view_details'),
-    );
-    fireEvent.press(accordionButton);
 
     expect(getByText('10')).toBeTruthy();
   });
