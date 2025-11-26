0fd0b07e58ee00446314e121f9e2800d63c3991b
sahar-fehri <sahar.fehri@consensys.net>
2025-11-26 14:55:10 +0100

    fix: rm sites section

--- app/components/Views/TrendingView/ExploreSearchScreen/components/ExploreSearchResults/ExploreSearchResults.tsx
+++ app/components/Views/TrendingView/ExploreSearchScreen/components/ExploreSearchResults/ExploreSearchResults.tsx
@@ -84,9 +84,7 @@ const ExploreSearchResults: React.FC<ExploreSearchResultsProps> = ({
     const result: FlatListItem[] = [];
 
     // Filter sections based on basic functionality toggle
-    const sectionsToShow = isBasicFunctionalityEnabled
-      ? SECTIONS_ARRAY
-      : [SECTIONS_CONFIG.sites];
+    const sectionsToShow = isBasicFunctionalityEnabled ? SECTIONS_ARRAY : [];
 
     sectionsToShow.forEach((section) => {
       const items = data[section.id];
