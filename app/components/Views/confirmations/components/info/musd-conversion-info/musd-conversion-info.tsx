a58c35d20c71cde020eb7f9b0eb3033950adeed7
Matthew Grainger <matthew.grainger@consensys.net>
2025-11-25 19:24:10 -0500

    feat: fixed outputChainId mismatch bug

--- app/components/Views/confirmations/components/info/musd-conversion-info/musd-conversion-info.tsx
+++ app/components/Views/confirmations/components/info/musd-conversion-info/musd-conversion-info.tsx
@@ -7,24 +7,28 @@ import {
   MUSD_TOKEN_ADDRESS_BY_CHAIN,
 } from '../../../../../UI/Earn/constants/musd';
 import { useAddToken } from '../../../hooks/tokens/useAddToken';
-import { useRoute, RouteProp } from '@react-navigation/native';
 import { MusdConversionConfig } from '../../../../../UI/Earn/hooks/useMusdConversion';
 import { CHAIN_IDS } from '@metamask/transaction-controller';
+import { useParams } from '../../../../../../util/navigation/navUtils';
 
 export const MusdConversionInfo = () => {
-  const route =
-    useRoute<RouteProp<Record<string, MusdConversionConfig>, string>>();
   // TEMP: Will be brought back in subsequent PR.
   // const preferredPaymentToken = route.params?.preferredPaymentToken;
-  const outputChainId = route.params?.outputChainId ?? CHAIN_IDS.MAINNET;
+  const { outputChainId } = useParams<MusdConversionConfig>({
+    outputChainId: CHAIN_IDS.MAINNET,
+  });
 
   useNavbar(strings('earn.musd_conversion.earn_rewards_with'));
 
   const { decimals, name, symbol } = MUSD_TOKEN;
 
-  const tokenToAddAddress =
-    MUSD_TOKEN_ADDRESS_BY_CHAIN[outputChainId] ??
-    MUSD_TOKEN_ADDRESS_BY_CHAIN[CHAIN_IDS.MAINNET];
+  const tokenToAddAddress = MUSD_TOKEN_ADDRESS_BY_CHAIN[outputChainId];
+
+  if (!tokenToAddAddress) {
+    throw new Error(
+      `mUSD token address not found for chain ID: ${outputChainId}`,
+    );
+  }
 
   useAddToken({
     chainId: outputChainId,
