3d8b94840afc84e04894e257ac0f3e0fc6ef53a7
Michele Esposito <michele@esposito.codes>
2025-11-27 12:24:49 +0100

    update `profile-metrics-controller-init.test.ts`

--- app/core/Engine/controllers/profile-metrics-controller-init.test.ts
+++ app/core/Engine/controllers/profile-metrics-controller-init.test.ts
@@ -11,37 +11,58 @@ import { buildControllerInitRequestMock } from '../utils/test-utils';
 
 jest.mock('@metamask/profile-metrics-controller');
 
-function getInitRequestMock(): jest.Mocked<
-  ControllerInitRequest<ProfileMetricsControllerMessenger>
-> {
+function getInitRequestMock({
+  metaMetricsId,
+  remoteFeatureFlag,
+}: {
+  metaMetricsId: string;
+  remoteFeatureFlag: boolean;
+}): jest.Mocked<ControllerInitRequest<ProfileMetricsControllerMessenger>> {
   const baseMessenger = new ExtendedMessenger<MockAnyNamespace, never, never>({
     namespace: MOCK_ANY_NAMESPACE,
+  });
+
+  const mockGetController = jest.fn().mockReturnValue({
+    state: {
+      remoteFeatureFlags: { extensionUxPna25: remoteFeatureFlag },
+    },
   });
 
   const requestMock = {
     ...buildControllerInitRequestMock(baseMessenger),
     controllerMessenger: getProfileMetricsControllerMessenger(baseMessenger),
     initMessenger: undefined,
+    metaMetricsId,
+    getController: mockGetController,
   };
 
   return requestMock;
 }
 
-describe('profileMetricsControllerInit', () => {
-  it('initializes the controller', () => {
-    const { controller } = profileMetricsControllerInit(getInitRequestMock());
-    expect(controller).toBeInstanceOf(ProfileMetricsController);
-  });
+describe.each([
+  { metaMetricsId: 'mock-id', remoteFeatureFlag: true },
+  { metaMetricsId: 'mock-id-2', remoteFeatureFlag: false },
+])('profileMetricsControllerInit', ({ metaMetricsId, remoteFeatureFlag }) => {
+  describe(`when metaMetricsId is ${metaMetricsId} and the feature flag value is ${remoteFeatureFlag}`, () => {
+    it('initializes the controller', () => {
+      const { controller } = profileMetricsControllerInit(
+        getInitRequestMock({ metaMetricsId, remoteFeatureFlag }),
+      );
+      expect(controller).toBeInstanceOf(ProfileMetricsController);
+    });
 
-  it('passes the proper arguments to the controller', () => {
-    profileMetricsControllerInit(getInitRequestMock());
+    it('passes the proper arguments to the controller', () => {
+      profileMetricsControllerInit(
+        getInitRequestMock({ metaMetricsId, remoteFeatureFlag }),
+      );
 
-    const controllerMock = jest.mocked(ProfileMetricsController);
-    expect(controllerMock).toHaveBeenCalledWith({
-      messenger: expect.any(Object),
-      state: undefined,
-      assertUserOptedIn: expect.any(Function),
-      getMetaMetricsId: expect.any(Function),
+      const controllerMock = jest.mocked(ProfileMetricsController);
+      expect(controllerMock).toHaveBeenCalledWith({
+        messenger: expect.any(Object),
+        state: undefined,
+        assertUserOptedIn: expect.any(Function),
+        getMetaMetricsId: expect.any(Function),
+      });
     });
   });
 });
