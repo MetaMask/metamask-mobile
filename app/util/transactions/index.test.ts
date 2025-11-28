2af135117a56546edc1b4b352ad9566a15705790
IF <139582705+infiniteflower@users.noreply.github.com>
2025-11-29 01:27:12 +0900

    chore: replace swaps-controller getSwapsContractAddress, isValidSwapsContractAddress with bridge-controller equivalents

--- app/util/transactions/index.test.ts
+++ app/util/transactions/index.test.ts
@@ -1,9 +1,9 @@
-import { swapsUtils } from '@metamask/swaps-controller';
+/* eslint-disable import/no-namespace */
 import BN from 'bnjs4';
 
-/* eslint-disable-next-line import/no-namespace */
 import * as controllerUtilsModule from '@metamask/controller-utils';
 import { ERC721, ERC1155, ORIGIN_METAMASK } from '@metamask/controller-utils';
+import * as bridgeControllerModule from '@metamask/bridge-controller';
 
 import { handleMethodData } from '../../util/transaction-controller';
 
@@ -608,7 +608,7 @@ describe('Transactions utils :: isTransactionIncomplete', () => {
 describe('Transactions utils :: getActionKey', () => {
   beforeEach(() => {
     jest
-      .spyOn(swapsUtils, 'getSwapsContractAddress')
+      .spyOn(bridgeControllerModule, 'getSwapsContractAddress')
       .mockImplementation(() => 'SWAPS_CONTRACT_ADDRESS');
   });
 
