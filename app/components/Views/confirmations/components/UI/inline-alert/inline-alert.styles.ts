ef347d81f96883111173cf622695fabe82556e7e
augmentedmode <jacob.lebowitz@consensys.net>
2025-12-01 15:24:46 -0500

    fix: icon padding to match other icons

--- app/components/Views/confirmations/components/UI/inline-alert/inline-alert.styles.ts
+++ app/components/Views/confirmations/components/UI/inline-alert/inline-alert.styles.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 const styleSheet = () =>
   StyleSheet.create({
     iconContainer: {
-      marginLeft: 8,
+      marginLeft: 4,
       marginTop: 1, // Slight adjustment for visual centering with text
     },
   });
