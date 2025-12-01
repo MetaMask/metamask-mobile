57608daf312485b6f7754d9f3881a25ecbc81e86
vinnyhoward <vincenguyenhoward@gmail.com>
2025-12-01 10:38:02 -0700

    fix: lint issues

--- app/components/hooks/useBuildPortfolioUrl.test.ts
+++ app/components/hooks/useBuildPortfolioUrl.test.ts
@@ -43,7 +43,6 @@ describe('useBuildPortfolioUrl', () => {
       isDataRecorded: jest.fn(),
       getMetaMetricsId: jest.fn(),
       createEventBuilder: jest.fn(),
-      enableSocialLogin: jest.fn(),
     });
   });
 
