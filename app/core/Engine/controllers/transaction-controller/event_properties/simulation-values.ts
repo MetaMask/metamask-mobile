18f9c174681c82401704d67144025692121d30f3
dan437 <80175477+dan437@users.noreply.github.com>
2025-12-02 14:15:07 +0100

    chore: Add missing properties for transaction events
    Signed-off-by: dan437 <80175477+dan437@users.noreply.github.com>

---
+++ app/core/Engine/controllers/transaction-controller/event_properties/simulation-values.ts
@@ -0,0 +1,31 @@
+import { TransactionMetricsBuilder } from '../types';
+import { JsonMap } from '../../../../Analytics/MetaMetrics.types';
+
+/**
+ * Gets simulation asset fiat values for transaction metrics from TransactionMeta.assetsFiatValues.
+ *
+ * @param transactionMeta - The transaction metadata
+ * @returns Object with simulation_sending_assets_total_value and simulation_receiving_assets_total_value properties
+ */
+export const getSimulationValuesProperties: TransactionMetricsBuilder = ({
+  transactionMeta,
+}) => {
+  const properties: JsonMap = {};
+  const sensitiveProperties: JsonMap = {};
+  const { assetsFiatValues } = transactionMeta;
+
+  if (!assetsFiatValues) {
+    return { properties, sensitiveProperties };
+  }
+
+  if (assetsFiatValues.sending !== undefined) {
+    properties.simulation_sending_assets_total_value = assetsFiatValues.sending;
+  }
+
+  if (assetsFiatValues.receiving !== undefined) {
+    properties.simulation_receiving_assets_total_value =
+      assetsFiatValues.receiving;
+  }
+
+  return { properties, sensitiveProperties };
+};
