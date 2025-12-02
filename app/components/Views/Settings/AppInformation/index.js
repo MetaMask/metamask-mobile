a069c2f0b2f6752e111bcaa4be73126f60eb02ce
Wei Sun <wei.sun@consensys.net>
2025-12-02 15:43:56 -0800

    7.62.88 ota v1

--- app/components/Views/Settings/AppInformation/index.js
+++ app/components/Views/Settings/AppInformation/index.js
@@ -223,6 +223,7 @@ class AppInformation extends PureComponent {
             <Text style={styles.versionInfo}>
               {getFullVersion(this.state.appInfo)}
             </Text>
+            <Text style={styles.versionInfo}>This is OTA update v1 text</Text>
             {isQa ? (
               <Text style={styles.branchInfo}>
                 {`Branch: ${process.env['GIT_BRANCH']}`}
