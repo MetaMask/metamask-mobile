a73bb9f96294f1ff46aed16f9a132cf5a38a1394
sahar-fehri <sahar.fehri@consensys.net>
2025-12-02 13:48:11 +0100

    fix: revert trending list

--- app/components/UI/Trending/utils/trendingNetworksList.ts
+++ app/components/UI/Trending/utils/trendingNetworksList.ts
@@ -1,59 +1,125 @@
-import { CaipChainId } from '@metamask/utils';
 import {
-  ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
-  SolScope,
-  ///: END:ONLY_INCLUDE_IF
   ///: BEGIN:ONLY_INCLUDE_IF(tron)
   TrxScope,
   ///: END:ONLY_INCLUDE_IF
 } from '@metamask/keyring-api';
 import { ProcessedNetwork } from '../../../hooks/useNetworksByNamespace/useNetworksByNamespace';
 import { getNetworkImageSource } from '../../../../util/networks';
+import { NetworkToCaipChainId } from '../../NetworkMultiSelector/NetworkMultiSelector.constants';
 
 /**
- * Helper function to create a ProcessedNetwork object
- */
-const createNetworkEntry = (
-  caipChainId: CaipChainId,
-  name: string,
-): ProcessedNetwork => ({
-  id: caipChainId,
-  name,
-  caipChainId,
-  isSelected: false,
-  imageSource: getNetworkImageSource({ chainId: caipChainId }),
-});
-
-/**
- * Network configurations for trending features
+ * Static list of popular networks for trending features.
+ * Returns ProcessedNetwork objects similar to usePopularNetworks hook.
+ * This is a static constant that doesn't depend on Redux state.
  */
 // Before adding a network, you MUST make sure it is supported on both `searchAPI` and `trendingAPI`
-const TRENDING_NETWORKS_CONFIG: {
-  caipChainId: CaipChainId;
-  name: string;
-}[] = [
-  { caipChainId: 'eip155:1', name: 'Ethereum' },
-  { caipChainId: 'eip155:59144', name: 'Linea' },
-  { caipChainId: 'eip155:8453', name: 'Base' },
-  { caipChainId: 'eip155:42161', name: 'Arbitrum' },
-  { caipChainId: 'eip155:56', name: 'BNB Chain' },
-  { caipChainId: 'eip155:10', name: 'OP' },
-  { caipChainId: 'eip155:137', name: 'Polygon' },
-  { caipChainId: 'eip155:1329', name: 'Sei' },
-  { caipChainId: 'eip155:43114', name: 'Avalanche' },
-  { caipChainId: 'eip155:324', name: 'zkSync Era' },
-  { caipChainId: SolScope.Mainnet, name: 'Solana' },
+export const TRENDING_NETWORKS_LIST: ProcessedNetwork[] = [
+  {
+    id: NetworkToCaipChainId.ETHEREUM,
+    name: 'Ethereum',
+    caipChainId: NetworkToCaipChainId.ETHEREUM,
+    isSelected: false,
+    imageSource: getNetworkImageSource({
+      chainId: NetworkToCaipChainId.ETHEREUM,
+    }),
+  },
+  {
+    id: NetworkToCaipChainId.LINEA,
+    name: 'Linea',
+    caipChainId: NetworkToCaipChainId.LINEA,
+    isSelected: false,
+    imageSource: getNetworkImageSource({
+      chainId: NetworkToCaipChainId.LINEA,
+    }),
+  },
+  {
+    id: NetworkToCaipChainId.BASE,
+    name: 'Base',
+    caipChainId: NetworkToCaipChainId.BASE,
+    isSelected: false,
+    imageSource: getNetworkImageSource({
+      chainId: NetworkToCaipChainId.BASE,
+    }),
+  },
+  {
+    id: NetworkToCaipChainId.ARBITRUM,
+    name: 'Arbitrum',
+    caipChainId: NetworkToCaipChainId.ARBITRUM,
+    isSelected: false,
+    imageSource: getNetworkImageSource({
+      chainId: NetworkToCaipChainId.ARBITRUM,
+    }),
+  },
+  {
+    id: NetworkToCaipChainId.BNB,
+    name: 'BNB Chain',
+    caipChainId: NetworkToCaipChainId.BNB,
+    isSelected: false,
+    imageSource: getNetworkImageSource({ chainId: NetworkToCaipChainId.BNB }),
+  },
+  {
+    id: NetworkToCaipChainId.OPTIMISM,
+    name: 'OP',
+    caipChainId: NetworkToCaipChainId.OPTIMISM,
+    isSelected: false,
+    imageSource: getNetworkImageSource({
+      chainId: NetworkToCaipChainId.OPTIMISM,
+    }),
+  },
+  {
+    id: NetworkToCaipChainId.POLYGON,
+    name: 'Polygon',
+    caipChainId: NetworkToCaipChainId.POLYGON,
+    isSelected: false,
+    imageSource: getNetworkImageSource({
+      chainId: NetworkToCaipChainId.POLYGON,
+    }),
+  },
+  {
+    id: NetworkToCaipChainId.SEI,
+    name: 'Sei',
+    caipChainId: NetworkToCaipChainId.SEI,
+    isSelected: false,
+    imageSource: getNetworkImageSource({
+      chainId: NetworkToCaipChainId.SEI,
+    }),
+  },
+  {
+    id: NetworkToCaipChainId.AVALANCHE,
+    name: 'Avalanche',
+    caipChainId: NetworkToCaipChainId.AVALANCHE,
+    isSelected: false,
+    imageSource: getNetworkImageSource({
+      chainId: NetworkToCaipChainId.AVALANCHE,
+    }),
+  },
+  {
+    id: NetworkToCaipChainId.ZKSYNC_ERA,
+    name: 'zkSync Era',
+    caipChainId: NetworkToCaipChainId.ZKSYNC_ERA,
+    isSelected: false,
+    imageSource: getNetworkImageSource({
+      chainId: NetworkToCaipChainId.ZKSYNC_ERA,
+    }),
+  },
+  ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
+  {
+    id: NetworkToCaipChainId.SOLANA,
+    name: 'Solana',
+    caipChainId: NetworkToCaipChainId.SOLANA,
+    isSelected: false,
+    imageSource: getNetworkImageSource({
+      chainId: NetworkToCaipChainId.SOLANA,
+    }),
+  },
+  ///: END:ONLY_INCLUDE_IF
   ///: BEGIN:ONLY_INCLUDE_IF(tron)
-  { caipChainId: TrxScope.Mainnet, name: 'Tron' },
+  {
+    id: TrxScope.Mainnet,
+    name: 'Tron',
+    caipChainId: TrxScope.Mainnet,
+    isSelected: false,
+    imageSource: getNetworkImageSource({ chainId: TrxScope.Mainnet }),
+  },
   ///: END:ONLY_INCLUDE_IF
 ];
-
-/**
- * Static list of popular networks for trending features
- * Returns ProcessedNetwork objects similar to usePopularNetworks hook
- * This is a static constant that doesn't depend on Redux state
- */
-export const TRENDING_NETWORKS_LIST: ProcessedNetwork[] =
-  TRENDING_NETWORKS_CONFIG.map(({ caipChainId, name }) =>
-    createNetworkEntry(caipChainId, name),
-  );
