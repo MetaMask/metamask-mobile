e34ce22e4599c17756ea78cd4ab9847f6ef7b12d
georgewrmarshall <george.marshall@consensys.net>
2025-12-03 14:18:51 +1300

    chore: removing unused font preloader code

--- app/components/Views/Root/index.tsx
+++ app/components/Views/Root/index.tsx
@@ -14,7 +14,6 @@ import { RootProps } from './types';
 import NavigationProvider from '../../Nav/NavigationProvider';
 import ControllersGate from '../../Nav/ControllersGate';
 import { isTest } from '../../../util/test/utils';
-import FontLoadingGate from './FontLoadingGate';
 import { FeatureFlagOverrideProvider } from '../../../contexts/FeatureFlagOverrideContext';
 ///: BEGIN:ONLY_INCLUDE_IF(preinstalled-snaps,external-snaps)
 import { SnapsExecutionWebView } from '../../../lib/snaps';
@@ -80,10 +79,8 @@ const Root = ({ foxCode }: RootProps) => {
                 <ControllersGate>
                   <ToastContextWrapper>
                     <ErrorBoundary view="Root">
-                      <FontLoadingGate>
-                        <ReducedMotionConfig mode={ReduceMotion.Never} />
-                        <App />
-                      </FontLoadingGate>
+                      <ReducedMotionConfig mode={ReduceMotion.Never} />
+                      <App />
                     </ErrorBoundary>
                   </ToastContextWrapper>
                 </ControllersGate>
