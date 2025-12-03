a770cd8670f0dfce6f7a850da9d43a41e0f2d54e
Amanda Yeoh <amanda.yeoh@consensys.net>
2025-12-03 11:41:44 +0800

    chore: refine spacing and avatar size

--- app/components/UI/DeFiPositions/DeFiProtocolPositionDetails.styles.ts
+++ app/components/UI/DeFiPositions/DeFiProtocolPositionDetails.styles.ts
@@ -7,11 +7,13 @@ const styleSheet = () =>
   StyleSheet.create({
     detailsWrapper: {
       paddingHorizontal: 16,
+      paddingTop: 8,
       flexDirection: 'row',
       justifyContent: 'space-between',
     },
     separatorWrapper: {
       paddingHorizontal: 16,
+      paddingVertical: 16,
     },
     protocolPositionDetailsWrapper: {
       flex: 1,
