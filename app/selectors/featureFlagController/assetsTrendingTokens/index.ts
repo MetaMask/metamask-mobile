f0cccf68ae34aeb3ab794218b59c881ce11eae51
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2025-12-03 13:06:16 +0000

    revert: remove force FF flag enabled

--- app/selectors/featureFlagController/assetsTrendingTokens/index.ts
+++ app/selectors/featureFlagController/assetsTrendingTokens/index.ts
@@ -93,11 +93,9 @@ export const selectAssetsTrendingTokensEnabled = createSelector(
     const envOverride =
       process.env.OVERRIDE_REMOTE_FEATURE_FLAGS &&
       process.env.ASSETS_TRENDING_TOKENS_ENABLED;
-    return (
-      isAssetsTrendingTokensFeatureEnabled(
-        assetsTrendingTokensEnabled,
-        envOverride || undefined,
-      ) || true
+    return isAssetsTrendingTokensFeatureEnabled(
+      assetsTrendingTokensEnabled,
+      envOverride || undefined,
     );
   },
 );
