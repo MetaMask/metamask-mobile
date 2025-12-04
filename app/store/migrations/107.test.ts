4c6794e6b699358ba87df15835db43ff8b60d1e9
Florin Dzeladini <florin.dzeladini@consensys.net>
2025-12-04 11:27:07 +0100

    chore: test naming

--- app/store/migrations/107.test.ts
+++ app/store/migrations/107.test.ts
@@ -176,7 +176,7 @@ describe(`migration #${migrationVersion}`, () => {
   ];
 
   it.each(invalidStates)(
-    'should capture exception if $scenario',
+    'captures exception if $scenario',
     ({ errorMessage, state }) => {
       const orgState = cloneDeep(state);
       mockedEnsureValidState.mockReturnValue(true);
