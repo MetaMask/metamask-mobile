529a62cf7337924499ea33e122b41955e870202e
Gaurav Goel <grvgoel19@gmail.com>
2025-12-05 12:39:21 +0700

    code refactored to use specific input mode

--- app/util/test/testSetup.js
+++ app/util/test/testSetup.js
@@ -568,10 +568,6 @@ jest.mock('react-native-safe-area-context', () => ({
   useSafeAreaInsets: () => ({ top: 0, bottom: 0, left: 0, right: 0 }),
 }));
 
-jest.mock('react-native-keyboard-aware-scroll-view', () => ({
-  KeyboardAwareScrollView: require('react-native').ScrollView,
-}));
-
 jest.mock(
   'react-native-keyboard-controller',
   () => ({
