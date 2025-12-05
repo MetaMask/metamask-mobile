ab122977db18871e0d392375c1d9f0ac950a5135
Bryan Fullam <bryan.fullam@consensys.net>
2025-12-05 13:52:36 -0800

    fix: set default pairs before swap navigation

--- app/components/UI/Bridge/hooks/useSwapBridgeNavigation/index.ts
+++ app/components/UI/Bridge/hooks/useSwapBridgeNavigation/index.ts
@@ -2,7 +2,7 @@ import { useCallback } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import Routes from '../../../../../constants/navigation/Routes';
 import { Hex, CaipChainId } from '@metamask/utils';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { BridgeToken, BridgeViewMode } from '../../types';
 import {
   formatChainIdToHex,
@@ -20,11 +20,18 @@ import {
   ActionPosition,
 } from '../../../../../util/analytics/actionButtonTracking';
 import { useAddNetwork } from '../../../../hooks/useAddNetwork';
-import { selectIsBridgeEnabledSourceFactory } from '../../../../../core/redux/slices/bridge';
+import {
+  selectIsBridgeEnabledSourceFactory,
+  setSourceToken,
+  setDestToken,
+} from '../../../../../core/redux/slices/bridge';
 import { trace, TraceName } from '../../../../../util/trace';
 import { useCurrentNetworkInfo } from '../../../../hooks/useCurrentNetworkInfo';
 import { strings } from '../../../../../../locales/i18n';
-import { getNativeSourceToken } from '../../utils/tokenUtils';
+import {
+  getNativeSourceToken,
+  getDefaultDestToken,
+} from '../../utils/tokenUtils';
 
 export enum SwapBridgeNavigationLocation {
   TabBar = 'TabBar',
@@ -49,6 +56,7 @@ export const useSwapBridgeNavigation = ({
   sourceToken?: BridgeToken;
 }) => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
   const { trackEvent, createEventBuilder } = useMetrics();
   const getIsBridgeEnabledSource = useSelector(
     selectIsBridgeEnabledSourceFactory,
@@ -118,6 +126,28 @@ export const useSwapBridgeNavigation = ({
         sourceToken = getNativeSourceToken(EthScope.Mainnet);
       }
 
+      // Pre-populate Redux state before navigation to prevent empty button flash
+      dispatch(setSourceToken(sourceToken));
+
+      const defaultDestToken = getDefaultDestToken(sourceToken.chainId);
+      // Make sure source and dest tokens are different
+      if (
+        defaultDestToken &&
+        sourceToken.address?.toLowerCase() !==
+          defaultDestToken.address?.toLowerCase()
+      ) {
+        dispatch(setDestToken(defaultDestToken));
+      } else {
+        // Fall back to native token if default dest is same as source
+        const nativeDestToken = getNativeSourceToken(sourceToken.chainId);
+        if (
+          sourceToken.address?.toLowerCase() !==
+          nativeDestToken.address?.toLowerCase()
+        ) {
+          dispatch(setDestToken(nativeDestToken));
+        }
+      }
+
       const params: BridgeRouteParams = {
         sourceToken,
         sourcePage,
@@ -159,6 +189,7 @@ export const useSwapBridgeNavigation = ({
     },
     [
       navigation,
+      dispatch,
       tokenBase,
       sourcePage,
       trackEvent,
