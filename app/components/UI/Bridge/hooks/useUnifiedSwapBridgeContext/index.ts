f453c5510b79da96561711bfd6ef45d28c0cccff
IF <139582705+infiniteflower@users.noreply.github.com>
2025-12-10 15:37:12 -0500

    chore: consume calcTokenFiatValue in the unified context

--- app/components/UI/Bridge/hooks/useUnifiedSwapBridgeContext/index.ts
+++ app/components/UI/Bridge/hooks/useUnifiedSwapBridgeContext/index.ts
@@ -3,13 +3,53 @@ import { useSelector } from 'react-redux';
 import {
   selectDestToken,
   selectSourceToken,
+  selectSourceAmount,
 } from '../../../../../core/redux/slices/bridge';
 import { selectShouldUseSmartTransaction } from '../../../../../selectors/smartTransactionsController';
+import { selectCurrencyRates } from '../../../../../selectors/currencyRateController';
+import { selectTokenMarketData } from '../../../../../selectors/tokenRatesController';
+import { selectNetworkConfigurations } from '../../../../../selectors/networkController';
+///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
+import { selectMultichainAssetsRates } from '../../../../../selectors/multichain';
+///: END:ONLY_INCLUDE_IF(keyring-snaps)
+import { calcTokenFiatValue } from '../../utils/exchange-rates';
 
 export const useUnifiedSwapBridgeContext = () => {
   const smartTransactionsEnabled = useSelector(selectShouldUseSmartTransaction);
   const fromToken = useSelector(selectSourceToken);
   const toToken = useSelector(selectDestToken);
+  const sourceAmount = useSelector(selectSourceAmount);
+
+  const evmMultiChainMarketData = useSelector(selectTokenMarketData);
+  const evmMultiChainCurrencyRates = useSelector(selectCurrencyRates);
+  const networkConfigurationsByChainId = useSelector(
+    selectNetworkConfigurations,
+  );
+
+  ///: BEGIN:ONLY_INCLUDE_IF(keyring-snaps)
+  const nonEvmMultichainAssetRates = useSelector(selectMultichainAssetsRates);
+  ///: END:ONLY_INCLUDE_IF(keyring-snaps)
+
+  // TODO this is not actually the USD amount source yet, it's the amount source in the user's selected currency
+  const usdAmountSource = useMemo(
+    () =>
+      calcTokenFiatValue({
+        token: fromToken ?? undefined,
+        amount: sourceAmount,
+        evmMultiChainMarketData,
+        networkConfigurationsByChainId,
+        evmMultiChainCurrencyRates,
+        nonEvmMultichainAssetRates,
+      }),
+    [
+      fromToken,
+      sourceAmount,
+      evmMultiChainMarketData,
+      networkConfigurationsByChainId,
+      evmMultiChainCurrencyRates,
+      nonEvmMultichainAssetRates,
+    ],
+  );
 
   return useMemo(
     () => ({
@@ -18,7 +58,8 @@ export const useUnifiedSwapBridgeContext = () => {
       token_symbol_destination: toToken?.symbol ?? '',
       security_warnings: [], // TODO
       warnings: [], // TODO
+      usd_amount_source: usdAmountSource,
     }),
-    [smartTransactionsEnabled, fromToken, toToken],
+    [smartTransactionsEnabled, fromToken, toToken, usdAmountSource],
   );
 };
