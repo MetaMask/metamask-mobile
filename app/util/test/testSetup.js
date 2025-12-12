a098d44b0f1c20356396bea765562934f38503e0
juanmigdr <juanmi.gdr@gmail.com>
2025-12-12 15:23:42 +0100

    fix: broken tests

--- app/util/test/testSetup.js
+++ app/util/test/testSetup.js
@@ -563,6 +563,11 @@ jest.mock('../../core/Engine', () =>
   require('../../core/__mocks__/MockedEngine'),
 );
 
+jest.mock('../assets', () => ({
+  ...jest.requireActual('../assets'),
+  prepareNftDetectionEvents: jest.fn(() => []),
+}));
+
 jest.mock('react-native-safe-area-context', () => ({
   ...jest.requireActual('react-native-safe-area-context'),
   useSafeAreaInsets: () => ({ top: 0, bottom: 0, left: 0, right: 0 }),
