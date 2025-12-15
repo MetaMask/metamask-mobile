a56fbb7dfb12189d5234d8d0bc53fd8ca735a23d
augmentedmode <jacob.lebowitz@consensys.net>
2025-12-15 10:19:11 -0500

    chore: cleanup

--- app/components/Views/BrowserTab/index.test.tsx
+++ app/components/Views/BrowserTab/index.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { screen, waitFor } from '@testing-library/react-native';
-import { PhishingDetectorResultType } from '@metamask/phishing-controller';
 import AppConstants from '../../../core/AppConstants';
 import renderWithProvider from '../../../util/test/renderWithProvider';
 import { backgroundState } from '../../../util/test/initial-root-state';
@@ -54,10 +53,6 @@ const mockInitialState = {
 
 jest.mock('../../../core/Engine', () => ({
   context: {
-    PhishingController: {
-      maybeUpdateState: jest.fn(),
-      test: () => ({ result: true, name: 'test' }),
-    },
     AccountsController: {
       listMultichainAccounts: () => [],
     },
@@ -203,34 +198,6 @@ describe('BrowserTab', () => {
           url: 'javascript://example.com',
         }),
       ).toBe(false);
-    });
-
-    it('allows initial page load and checks phishing status asynchronously', async () => {
-      const { getPhishingTestResultAsync } = jest.requireMock(
-        '../../../util/phishingDetection',
-      );
-      getPhishingTestResultAsync.mockResolvedValue({
-        result: true,
-        name: 'phishing-site',
-        type: PhishingDetectorResultType.Blocklist,
-      });
-
-      renderWithProvider(<BrowserTab {...mockProps} />, {
-        state: mockInitialState,
-      });
-
-      await waitFor(() =>
-        expect(screen.getByTestId('browser-webview')).toBeVisible(),
-      );
-
-      const webView = screen.getByTestId('browser-webview');
-      const onShouldStartLoadWithRequest =
-        webView.props.onShouldStartLoadWithRequest;
-
-      const phishingResult = onShouldStartLoadWithRequest({
-        url: 'https://phishing-site.com',
-      });
-      expect(phishingResult).toBe(true);
     });
   });
 });
