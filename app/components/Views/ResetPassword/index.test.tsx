acb33953b948e6f8e19c95013f6e9e2199445768
Wei Sun <wei.sun@consensys.net>
2025-12-16 10:00:42 -0800

    update reauthenticate mock

--- app/components/Views/ResetPassword/index.test.tsx
+++ app/components/Views/ResetPassword/index.test.tsx
@@ -85,11 +85,9 @@ jest.mock('../../../core/Authentication', () => ({
     ),
   checkIsSeedlessPasswordOutdated: jest.fn().mockResolvedValue(false),
   lockApp: jest.fn().mockResolvedValue(undefined),
-  reauthenticate: jest
-    .fn()
-    .mockImplementation((password?: string) =>
-      Promise.resolve(password ?? 'CurrentPassword123'),
-    ),
+  reauthenticate: jest.fn((password) =>
+    Promise.resolve({ password: password ?? 'CurrentPassword123' }),
+  ),
   authData: {
     currentAuthType: 'passcode',
   },
