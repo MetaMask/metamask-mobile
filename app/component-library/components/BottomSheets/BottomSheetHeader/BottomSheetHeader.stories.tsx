4595764311fb9f5f5e8d0776af442ae69a11781c
Brian Nguyen <brianacnguyen@gmail.com>
2025-12-15 16:41:32 -0800

    Updated snapshots

--- app/component-library/components/BottomSheets/BottomSheetHeader/BottomSheetHeader.stories.tsx
+++ app/component-library/components/BottomSheets/BottomSheetHeader/BottomSheetHeader.stories.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 // Internal dependencies.
 import { default as BottomSheetHeader } from './BottomSheetHeader';
 import { BottomSheetHeaderVariant } from './BottomSheetHeader.types';
-
+import Text, { TextVariant } from '../../Texts/Text';
 const BottomSheetHeaderMeta = {
   title: 'Component Library / BottomSheets / BottomSheetHeader',
   component: BottomSheetHeader,
@@ -29,33 +29,12 @@ export const Variant = {
   render: function Render() {
     return (
       <>
-        <BottomSheetHeader variant={BottomSheetHeaderVariant.Compact}>
-          BottomSheetHeader Variant: Compact with a long title
-        </BottomSheetHeader>
         <BottomSheetHeader
-          variant={BottomSheetHeaderVariant.Compact}
-          onBack={() => {
-            console.log('Back button clicked');
-          }}
           onClose={() => {
             console.log('Close button clicked');
           }}
         >
-          BottomSheetHeader Variant: Compact with accessories
-        </BottomSheetHeader>
-        <BottomSheetHeader variant={BottomSheetHeaderVariant.Display}>
-          BottomSheetHeader Variant: Display with a long title
-        </BottomSheetHeader>
-        <BottomSheetHeader
-          variant={BottomSheetHeaderVariant.Display}
-          onBack={() => {
-            console.log('Back button clicked');
-          }}
-          onClose={() => {
-            console.log('Close button clicked');
-          }}
-        >
-          BottomSheetHeader Variant: Display with accessories
+          <Text variant={TextVariant.HeadingMD}>Modify</Text>
         </BottomSheetHeader>
       </>
     );
