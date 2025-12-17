9a887bd0916e22a09fc7e557c04a3df482b23b36
Antonio Regadas <antonio.regadas@consensys.net>
2025-12-17 18:13:39 +0000

    chore: small update

--- app/components/UI/Earn/components/Tron/TronStakingButtons/TronStakingCta.tsx
+++ app/components/UI/Earn/components/Tron/TronStakingButtons/TronStakingCta.tsx
@@ -7,43 +7,37 @@ import Text, {
 import Button, {
   ButtonVariants,
 } from '../../../../../../component-library/components/Buttons/Button';
+import { useStyles } from '../../../../../../component-library/hooks';
 import { strings } from '../../../../../../../locales/i18n';
-
-const styles = {
-  container: { alignItems: 'center', marginTop: 16 },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    marginTop: 4,
-    marginBottom: 8,
-  },
-} as const;
+import styleSheet from './TronStakingCta.styles';
 
 interface TronStakingCtaProps extends Pick<ViewProps, 'style'> {
   aprText?: string;
   onEarn?: () => void;
 }
 
-const TronStakingCta = ({ style, aprText, onEarn }: TronStakingCtaProps) => (
-  <View style={[styles.container, style]}>
-    <Text variant={TextVariant.HeadingMD}>
-      {strings('stake.stake_your_trx_cta.title')}
-    </Text>
-    <View style={styles.row}>
-      <Text>{strings('stake.stake_your_trx_cta.description_start')}</Text>
-      {aprText ? <Text color={TextColor.Success}>{aprText}</Text> : null}
-      <Text>{strings('stake.stake_your_trx_cta.description_end')}</Text>
+const TronStakingCta = ({ style, aprText, onEarn }: TronStakingCtaProps) => {
+  const { styles } = useStyles(styleSheet, {});
+
+  return (
+    <View style={[styles.container, style]}>
+      <Text variant={TextVariant.HeadingMD}>
+        {strings('stake.stake_your_trx_cta.title')}
+      </Text>
+      <View style={styles.row}>
+        <Text>{strings('stake.stake_your_trx_cta.description_start')}</Text>
+        {aprText ? <Text color={TextColor.Success}>{aprText}</Text> : null}
+        <Text>{strings('stake.stake_your_trx_cta.description_end')}</Text>
+      </View>
+      {onEarn ? (
+        <Button
+          label={strings('stake.stake_your_trx_cta.learn_more')}
+          variant={ButtonVariants.Link}
+          onPress={onEarn}
+        />
+      ) : null}
     </View>
-    {onEarn ? (
-      <Button
-        label={strings('stake.stake_your_trx_cta.learn_more')}
-        variant={ButtonVariants.Link}
-        onPress={onEarn}
-      />
-    ) : null}
-  </View>
-);
+  );
+};
 
 export default TronStakingCta;
