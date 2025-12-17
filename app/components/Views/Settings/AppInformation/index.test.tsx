edb0c6439ba5744719e01653c959c6fe3a44366c
Wei Sun <wei.sun@consensys.net>
2025-12-16 16:16:31 -0800

    fix tests in AppInformation

--- app/components/Views/Settings/AppInformation/index.test.tsx
+++ app/components/Views/Settings/AppInformation/index.test.tsx
@@ -456,11 +456,9 @@ describe('AppInformation', () => {
         { state: MOCK_STATE },
       );
 
-      expect(queryByText(/Expo Project ID:/)).toBeNull();
       expect(queryByText(/Update ID:/)).toBeNull();
       expect(queryByText(/OTA Update Channel:/)).toBeNull();
       expect(queryByText(/OTA Update runtime version:/)).toBeNull();
-      expect(queryByText(/OTA Update URL:/)).toBeNull();
       expect(queryByText(/Check Automatically:/)).toBeNull();
       expect(queryByText(/OTA Update status:/)).toBeNull();
     });
@@ -485,7 +483,6 @@ describe('AppInformation', () => {
         expect(getByText(/Update ID: mock-update-id/)).toBeTruthy();
         expect(getByText(/OTA Update Channel: test-channel/)).toBeTruthy();
         expect(getByText(/OTA Update runtime version: 1.0.0/)).toBeTruthy();
-        expect(getByText(/OTA Update URL: https:\/\/example.com/)).toBeTruthy();
         expect(getByText(/Check Automatically: NEVER/)).toBeTruthy();
         expect(getByText(/OTA Update status:/)).toBeTruthy();
       });
