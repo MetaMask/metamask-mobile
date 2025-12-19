8b6eeec6c39f3e134e4c05d2724d7425acfa2b34
Matthew Grainger <matthew.grainger@consensys.net>
2025-12-19 13:56:45 -0500

    feat: replaced receive button on home page with quick convert link

--- app/components/Views/AssetDetails/AssetDetailsActions/AssetDetailsActions.tsx
+++ app/components/Views/AssetDetails/AssetDetailsActions/AssetDetailsActions.tsx
@@ -137,6 +137,15 @@ export const AssetDetailsActions: React.FC<AssetDetailsActionsProps> = ({
     withNavigationLock(onReceive);
   }, [withNavigationLock, onReceive]);
 
+  // Navigate to mUSD Quick Convert view
+  const handleMusdQuickConvertPress = useCallback(() => {
+    withNavigationLock(() => {
+      navigate(Routes.EARN.ROOT, {
+        screen: Routes.EARN.MUSD.QUICK_CONVERT,
+      });
+    });
+  }, [withNavigationLock, navigate]);
+
   return (
     <View style={styles.activitiesButton}>
       {displayBuyButton && (
@@ -172,10 +181,10 @@ export const AssetDetailsActions: React.FC<AssetDetailsActionsProps> = ({
       </View>
       <View style={styles.buttonContainer}>
         <MainActionButton
-          iconName={IconName.Received}
-          label={strings('asset_overview.receive_button')}
-          onPress={handleReceivePress}
-          isDisabled={false}
+          iconName={IconName.Coin}
+          label={strings('earn.musd_conversion.quick_convert_button')}
+          onPress={handleMusdQuickConvertPress}
+          isDisabled={!canSignTransactions}
           testID={receiveButtonActionID}
         />
       </View>
