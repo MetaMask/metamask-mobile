03991cd9ea3c175274e9a88be5059f9a8df4cc5d
vinnyhoward <vincenguyenhoward@gmail.com>
2026-01-06 11:26:43 -0700

    fix: remove unused test var

--- e2e/selectors/ImportAccount/ImportAccountFromPrivateKey.selectors.ts
+++ e2e/selectors/ImportAccount/ImportAccountFromPrivateKey.selectors.ts
@@ -2,6 +2,5 @@ export const ImportAccountFromPrivateKeyIDs = {
   CONTAINER: 'import-account-screen',
   PRIVATE_KEY_INPUT_BOX: 'input-private-key',
   IMPORT_BUTTON: 'import-button',
-  BACK_BUTTON: 'back-button-on-account-screen',
   CLOSE_BUTTON: 'close-button-on-account-screen',
 };
