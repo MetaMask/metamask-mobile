680fcaab1776c6c5e07a790e0bbe940e4ca73041
vinnyhoward <vincenguyenhoward@gmail.com>
2026-01-06 11:48:20 -0700

    fix: remove unused var

--- e2e/selectors/MultichainAccounts/EditAccountName.selectors.ts
+++ e2e/selectors/MultichainAccounts/EditAccountName.selectors.ts
@@ -2,5 +2,4 @@ export const EditAccountNameIds = {
   EDIT_ACCOUNT_NAME_CONTAINER: 'edit-account-name-container',
   ACCOUNT_NAME_INPUT: 'edit-account-name-input',
   SAVE_BUTTON: 'edit-account-name-save-button',
-  BACK_BUTTON: 'edit-account-name-back-button',
 };
