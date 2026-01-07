6251422d0b502971782b2ac57a970a63743ec3a0
Brian Nguyen <brianacnguyen@gmail.com>
2026-01-07 15:21:45 -0800

    Updated snapshots

--- app/components/Views/TooltipModal/ToolTipModal.styles.ts
+++ app/components/Views/TooltipModal/ToolTipModal.styles.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 
 const styleSheet = () =>
   StyleSheet.create({
@@ -8,7 +8,7 @@ const styleSheet = () =>
     footerContainer: {
       paddingHorizontal: 16,
       paddingTop: 24,
-      paddingBottom: 16,
+      paddingBottom: Platform.OS === 'android' ? 0 : 16,
     },
   });
 
