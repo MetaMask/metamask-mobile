a84b0c8d93f4acddbac053ea9de89d5123c60431
Matthew Grainger <matthew.grainger@consensys.net>
2026-01-07 10:34:16 -0500

    feat: reverted mUSD conersion token filter name in pay-with-modal

--- app/components/Views/confirmations/components/modals/pay-with-modal/pay-with-modal.tsx
+++ app/components/Views/confirmations/components/modals/pay-with-modal/pay-with-modal.tsx
@@ -20,7 +20,7 @@ export function PayWithModal() {
   const requiredTokens = useTransactionPayRequiredTokens();
   const transactionMeta = useTransactionMetadataRequest();
   const bottomSheetRef = useRef<BottomSheetRef>(null);
-  const { filterAllowedTokens } = useMusdConversionTokens();
+  const { filterAllowedTokens: musdTokenFilter } = useMusdConversionTokens();
 
   const handleClose = useCallback(() => {
     bottomSheetRef.current?.onCloseBottomSheet();
@@ -49,12 +49,12 @@ export function PayWithModal() {
       if (
         hasTransactionType(transactionMeta, [TransactionType.musdConversion])
       ) {
-        return filterAllowedTokens(availableTokens);
+        return musdTokenFilter(availableTokens);
       }
 
       return availableTokens;
     },
-    [filterAllowedTokens, payToken, requiredTokens, transactionMeta],
+    [musdTokenFilter, payToken, requiredTokens, transactionMeta],
   );
 
   return (
