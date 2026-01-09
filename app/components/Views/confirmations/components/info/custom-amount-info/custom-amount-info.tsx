fe3a8b42da11b30456d6c4e2f8afe732bfc78b07
Matthew Grainger <matthew.grainger@consensys.net>
2026-01-08 19:04:35 -0500

    feat: updated mUSD conversion convert button label

--- app/components/Views/confirmations/components/info/custom-amount-info/custom-amount-info.tsx
+++ app/components/Views/confirmations/components/info/custom-amount-info/custom-amount-info.tsx
@@ -300,7 +300,7 @@ function useButtonLabel() {
   }
 
   if (hasTransactionType(transaction, [TransactionType.musdConversion])) {
-    return strings('earn.musd_conversion.convert_to_musd');
+    return strings('earn.musd_conversion.convert');
   }
 
   return strings('confirm.deposit_edit_amount_done');
