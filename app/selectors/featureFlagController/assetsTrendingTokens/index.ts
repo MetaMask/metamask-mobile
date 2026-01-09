d0bc3e2775487def44591e7ab9d86caba3b2e15a
IF <139582705+infiniteflower@users.noreply.github.com>
2026-01-09 14:03:09 -0500

    refactor: remove temporary feature flag enabling for assets trending tokens

--- app/selectors/featureFlagController/assetsTrendingTokens/index.ts
+++ app/selectors/featureFlagController/assetsTrendingTokens/index.ts
@@ -89,9 +89,6 @@ export const isAssetsTrendingTokensFeatureEnabled = (
 export const selectAssetsTrendingTokensEnabled = createSelector(
   selectRemoteFeatureFlags,
   (remoteFeatureFlags): boolean => {
-    // TEMP: Force enable explore/trending feature for testing
-    return true;
-
     const { trendingTokens: assetsTrendingTokensEnabled } = remoteFeatureFlags;
     const envOverride =
       process.env.OVERRIDE_REMOTE_FEATURE_FLAGS &&
