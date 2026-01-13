15f768051c8aa67aa7ac62ae028de0df9bf83772
Nikos Fotiadis <nikosfotiadis0@gmail.com>
2026-01-13 09:19:07 +0200

    test: tron test dapp selectors

---
+++ e2e/pages/Browser/TronTestDApp.ts
@@ -0,0 +1,197 @@
+import { dataTestIds } from '@metamask/test-dapp-tron';
+import { getDappUrl } from '../../framework/fixtures/FixtureUtils';
+import Matchers from '../../framework/Matchers';
+import { BrowserViewSelectorsIDs } from '../../selectors/Browser/BrowserView.selectors';
+import Browser from './BrowserView';
+import Gestures from '../../framework/Gestures';
+import { waitFor } from 'detox';
+import { TronTestDappSelectorsWebIDs } from '../../selectors/Browser/TronTestDapp.selectors';
+
+/**
+ * Get a test element by data-testid
+ * @param dataTestId - The data-testid of the element
+ * @param options.tag - The tag of the element having the data-testid attribute (e.g. 'div', 'input', etc.). Defaults to 'div'
+ * @param options.extraXPath - The extra xpath to the element (e.g. '/div/button'), useful for accessing elements we aren't able to assign a data-testid to
+ * @returns The test element
+ */
+function getTestElement(
+  dataTestId: string,
+  options: { extraXPath?: string; tag?: string } = {},
+): Promise<DetoxElement | WebElement> {
+  const { tag = 'div', extraXPath = '' } = options;
+  const xpath = `//${tag}[@data-testid="${dataTestId}"]${extraXPath}`;
+
+  return Matchers.getElementByXPath(
+    BrowserViewSelectorsIDs.BROWSER_WEBVIEW_ID,
+    xpath,
+  );
+}
+
+/**
+ * Class to interact with the Multichain Test DApp via the WebView
+ */
+class TronTestDApp {
+  get connectButtonSelector(): WebElement {
+    return getTestElement(dataTestIds.testPage.header.connect, {
+      tag: 'button',
+    });
+  }
+
+  get disconnectButtonDropdownSelector(): WebElement {
+    return getTestElement(dataTestIds.testPage.header.disconnect, {
+      tag: 'button',
+    });
+  }
+
+  get disconnectButtonSelector(): WebElement {
+    return Matchers.getElementByCSS(
+      BrowserViewSelectorsIDs.BROWSER_WEBVIEW_ID,
+      TronTestDappSelectorsWebIDs.DISCONNECT_BUTTON,
+      2,
+    );
+  }
+
+  get endpointSelector(): WebElement {
+    return getTestElement(dataTestIds.testPage.header.endpoint, {
+      tag: 'input',
+    });
+  }
+
+  get walletButtonSelector(): WebElement {
+    return Matchers.getElementByCSS(
+      BrowserViewSelectorsIDs.BROWSER_WEBVIEW_ID,
+      TronTestDappSelectorsWebIDs.WALLET_BUTTON,
+      2,
+    );
+  }
+
+  get confirmTransactionButtonSelector(): WebElement {
+    return Matchers.getElementByID(
+      TronTestDappSelectorsWebIDs.CONFIRM_TRANSACTION_BUTTON,
+    );
+  }
+
+  get confirmSignMessageButtonSelector(): WebElement {
+    return Matchers.getElementByID(
+      TronTestDappSelectorsWebIDs.CONFIRM_SIGN_MESSAGE_BUTTON,
+    );
+  }
+
+  get cancelButtonSelector() {
+    return Matchers.getElementByText('Cancel');
+  }
+
+  async navigateToTronTestDApp(): Promise<void> {
+    await Browser.tapUrlInputBox();
+
+    await Browser.navigateToURL(getDappUrl(0));
+
+    await waitFor(element(by.id(BrowserViewSelectorsIDs.BROWSER_WEBVIEW_ID)))
+      .toBeVisible()
+      .withTimeout(10000);
+  }
+
+  async reloadTronTestDApp(): Promise<void> {
+    await Browser.reloadTab();
+  }
+
+  /**
+   * Tap a button in the WebView
+   */
+  async tapButton(webElement: WebElement): Promise<void> {
+    await Gestures.scrollToWebViewPort(webElement);
+    await Gestures.tap(webElement);
+  }
+
+  getHeader() {
+    return {
+      connect: async () => {
+        await this.tapButton(this.connectButtonSelector);
+      },
+      disconnect: async () => {
+        await this.tapButton(this.disconnectButtonDropdownSelector);
+
+        await this.tapButton(this.disconnectButtonSelector);
+      },
+      selectMetaMask: async () => {
+        await this.tapButton(this.walletButtonSelector);
+      },
+      getConnectionStatus: async () => {
+        const connectionStatusDiv = await getTestElement(
+          dataTestIds.testPage.header.connectionStatus,
+        );
+        return await connectionStatusDiv.getText();
+      },
+      getAccount: async () => {
+        const account = await getTestElement(
+          dataTestIds.testPage.header.account,
+          { extraXPath: '/div/a' },
+        );
+        return await account.getText();
+      },
+    };
+  }
+
+  getSignMessageTest() {
+    return {
+      signMessage: async () => {
+        await this.tapButton(
+          getTestElement(dataTestIds.testPage.signMessage.signMessage, {
+            tag: 'button',
+          }),
+        );
+      },
+      getSignedMessage: async () =>
+        (
+          await getTestElement(dataTestIds.testPage.signMessage.signedMessage, {
+            tag: 'pre',
+          })
+        ).getText(),
+    };
+  }
+
+  getSendTrxTest() {
+    return {
+      signTransaction: async () => {
+        await this.tapButton(
+          getTestElement(dataTestIds.testPage.sendTRX.signTransaction, {
+            tag: 'button',
+          }),
+        );
+      },
+      sendTransaction: async () => {
+        await this.tapButton(
+          getTestElement(dataTestIds.testPage.sendTRX.sendTransaction, {
+            tag: 'button',
+          }),
+        );
+      },
+      getSignedTransaction: async () =>
+        (
+          await getTestElement(dataTestIds.testPage.sendTRX.signedTransaction, {
+            tag: 'pre',
+          })
+        ).getText(),
+      getTransactionHash: async () =>
+        (
+          await getTestElement(dataTestIds.testPage.sendTRX.transactionHash, {
+            tag: 'pre',
+          })
+        ).getText(),
+    };
+  }
+
+  async confirmTransaction(): Promise<void> {
+    await Gestures.waitAndTap(this.confirmTransactionButtonSelector);
+  }
+
+  async confirmSignMessage(): Promise<void> {
+    await Gestures.waitAndTap(this.confirmSignMessageButtonSelector);
+  }
+
+  async tapCancelButton(): Promise<void> {
+    await Gestures.waitAndTap(this.cancelButtonSelector);
+  }
+}
+
+export default new TronTestDApp();
