c44df2296ca791bc501ecda20f8fc553a313e892
cmd-ob <ola.bale@consensys.net>
2026-01-13 07:49:42 +0000

    fix: Remove TODO comment after testing smart E2E selection fallback

--- app/core/Engine/Engine.ts
+++ app/core/Engine/Engine.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-shadow */
-// TODO: Remove this comment after testing smart-e2e-selection fallback
 ///: BEGIN:ONLY_INCLUDE_IF(sample-feature)
 import { samplePetnamesControllerInit } from '../../features/SampleFeature/controllers/sample-petnames-controller-init';
 ///: END:ONLY_INCLUDE_IF
