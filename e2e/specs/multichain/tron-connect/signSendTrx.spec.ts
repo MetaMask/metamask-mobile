6397cde55f5b1de2a77b4d8d37e31f578cf2e120
Nikos Fotiadis <nikosfotiadis0@gmail.com>
2026-01-13 09:29:23 +0200

    test: tron sign trx e2e

---
+++ e2e/specs/multichain/tron-connect/signSendTrx.spec.ts
@@ -0,0 +1,47 @@
+import { SmokeNetworkExpansion } from '../../../tags';
+import Assertions from '../../../framework/Assertions';
+import TronTestDApp from '../../../pages/Browser/TronTestDApp';
+import {
+  account1Short,
+  connectTronTestDapp,
+  navigateToTronTestDApp,
+} from './testHelpers';
+import { withTronAccountEnabled } from '../../../common-tron';
+
+describe(SmokeNetworkExpansion('Tron Connect E2E - Sign/Send TRX'), () => {
+  beforeAll(async () => {
+    jest.setTimeout(150000);
+  });
+
+  it('Should be able to sign a TRX transaction', async () => {
+    await withTronAccountEnabled(
+      {
+        tronAccountPermitted: true,
+      },
+      async () => {
+        await navigateToTronTestDApp();
+
+        // 1. Connect
+        await connectTronTestDapp();
+
+        const header = TronTestDApp.getHeader();
+
+        // Verify we are connected
+        const account = await header.getAccount();
+        await Assertions.checkIfTextMatches(account, account1Short);
+        const connectionStatus = await header.getConnectionStatus();
+        await Assertions.checkIfTextMatches(connectionStatus, 'Connected');
+
+        // 2. Sign transaction
+        await TronTestDApp.getSendTrxTest().signTransaction();
+
+        // Approve the signature
+        await TronTestDApp.confirmTransaction();
+
+        const signedTransaction =
+          await TronTestDApp.getSendTrxTest().getSignedTransaction();
+        console.log(signedTransaction);
+      },
+    );
+  });
+});
