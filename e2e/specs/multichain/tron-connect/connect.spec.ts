dbd65b1b3a8a511137c5d34fcddccb099522b058
Nikos Fotiadis <nikosfotiadis0@gmail.com>
2026-01-14 12:52:04 +0200

    test: add tests for page reload

--- e2e/specs/multichain/tron-connect/connect.spec.ts
+++ e2e/specs/multichain/tron-connect/connect.spec.ts
@@ -39,8 +39,6 @@ describe(SmokeNetworkExpansion('Tron Connect E2E - Connect'), () => {
         'Not connected',
       );
 
-      await new Promise((resolve) => setTimeout(resolve, 3000));
-
       // 3. Connect again
       await connectTronTestDapp();
 
@@ -50,6 +48,37 @@ describe(SmokeNetworkExpansion('Tron Connect E2E - Connect'), () => {
         connectionStatusAfterConnect,
         'Connected',
       );
+    });
+  });
+
+  it('Should restore the session afte refreshing the page', async () => {
+    await withTronAccountEnabled({}, async () => {
+      await navigateToTronTestDApp();
+
+      // 1. Connect
+      await connectTronTestDapp();
+
+      const header = TronTestDApp.getHeader();
+
+      // Verify we are connected
+      const account = await header.getAccount();
+      await Assertions.checkIfTextMatches(account, account1Short);
+      const connectionStatus = await header.getConnectionStatus();
+      await Assertions.checkIfTextMatches(connectionStatus, 'Connected');
+
+      // Refresh the page
+      await TronTestDApp.reloadTronTestDApp();
+
+      // Verify we are still connected
+      const connectionStatusAfterRefresh = await header.getConnectionStatus();
+      await Assertions.checkIfTextMatches(
+        connectionStatusAfterRefresh,
+        'Connected',
+      );
+
+      // Verify we are still connected
+      const accountAfterRefresh = await header.getAccount();
+      await Assertions.checkIfTextMatches(accountAfterRefresh, account1Short);
     });
   });
 });
