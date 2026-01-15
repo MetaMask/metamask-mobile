e9240171a04fd893c45f4bba3f7bb670cc961530
gantunesr <17601467+gantunesr@users.noreply.github.com>
2026-01-15 18:05:45 -0300

    chore: hardcode flag to return True for state 2

--- app/multichain-accounts/remote-feature-flag.ts
+++ app/multichain-accounts/remote-feature-flag.ts
@@ -1,5 +1,3 @@
-import compareVersions from 'compare-versions';
-import packageJson from '../../package.json';
 import { FeatureFlags } from '@metamask/remote-feature-flag-controller';
 import Engine from '../core/Engine';
 
@@ -12,7 +10,6 @@ export interface MultichainAccountsFeatureFlag {
   minimumVersion: string | null;
 }
 
-const APP_VERSION = packageJson.version;
 const enableMultichainAccountsState2Local =
   process.env.MM_ENABLE_MULTICHAIN_ACCOUNTS_STATE_2;
 
@@ -21,22 +18,6 @@ export const MULTICHAIN_ACCOUNTS_FEATURE_VERSION_2 = '2';
 
 export const STATE_1_FLAG = 'enableMultichainAccounts';
 export const STATE_2_FLAG = 'enableMultichainAccountsState2';
-/**
- * Asserts that the given value is a valid MultichainAccountsFeatureFlag.
- * @param value - The value to check.
- * @returns True if the value is a valid MultichainAccountsFeatureFlag, false otherwise.
- */
-export const assertMultichainAccountsFeatureFlagType = (
-  value: unknown,
-): value is MultichainAccountsFeatureFlag =>
-  typeof value === 'object' &&
-  value !== null &&
-  'enabled' in value &&
-  typeof value.enabled === 'boolean' &&
-  'featureVersion' in value &&
-  (typeof value.featureVersion === 'string' || value.featureVersion === null) &&
-  'minimumVersion' in value &&
-  (typeof value.minimumVersion === 'string' || value.minimumVersion === null);
 
 /**
  * Checks if the multichain accounts feature is enabled based on remote feature flags.
@@ -46,56 +27,13 @@ export const assertMultichainAccountsFeatureFlagType = (
  * @returns True if the multichain accounts feature is enabled for any specified versions, false otherwise.
  */
 export const isMultichainAccountsRemoteFeatureEnabled = (
-  remoteFeatureFlags: FeatureFlags,
-  featureVersionsToCheck: {
+  _remoteFeatureFlags: FeatureFlags,
+  _featureVersionsToCheck: {
     version: string;
     featureKey: keyof FeatureFlags;
   }[],
   _override?: string,
-): boolean => {
-  const overrideForVersion2 = featureVersionsToCheck.some(
-    ({ version }) => version === MULTICHAIN_ACCOUNTS_FEATURE_VERSION_2,
-  );
-
-  if (overrideForVersion2) {
-    return true;
-  }
-
-  const enableMultichainAccounts = remoteFeatureFlags.enableMultichainAccounts;
-  const enableMultichainAccountsState2 =
-    remoteFeatureFlags.enableMultichainAccountsState2;
-
-  const isState1Undefined =
-    !enableMultichainAccounts ||
-    !assertMultichainAccountsFeatureFlagType(enableMultichainAccounts);
-  const isState2Undefined =
-    !enableMultichainAccountsState2 ||
-    !assertMultichainAccountsFeatureFlagType(enableMultichainAccountsState2);
-  if (isState1Undefined && isState2Undefined) {
-    return true;
-  }
-
-  for (const { version, featureKey } of featureVersionsToCheck) {
-    const featureFlag = remoteFeatureFlags[featureKey];
-
-    if (!assertMultichainAccountsFeatureFlagType(featureFlag)) {
-      continue;
-    }
-
-    const { enabled, featureVersion, minimumVersion } = featureFlag;
-
-    if (
-      enabled &&
-      minimumVersion !== null &&
-      featureVersion === version &&
-      compareVersions.compare(minimumVersion, APP_VERSION, '<=')
-    ) {
-      return true;
-    }
-  }
-
-  return false;
-};
+): boolean => true;
 
 /**
  * Get remote feature flags from its controller's state.
