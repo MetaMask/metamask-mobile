2839ad16550b636103676625adffc37d1d91bfb3
Matthew Grainger <matthew.grainger@consensys.net>
2026-01-16 12:58:44 -0500

    chore: post-rebase cleanup

--- app/components/UI/Tokens/TokenList/TokenListItem/TokenListItem.tsx
+++ app/components/UI/Tokens/TokenList/TokenListItem/TokenListItem.tsx
@@ -151,7 +151,7 @@ export const TokenListItem = React.memo(
 
     const earnToken = getEarnToken(asset as TokenI);
 
-    const { initiateConversion, hasSeenConversionEducationScreen } =
+    const { initiateCustomConversion, hasSeenConversionEducationScreen } =
       useMusdConversion();
 
     const shouldShowConvertToMusdCta = useMemo(
@@ -231,7 +231,7 @@ export const TokenListItem = React.memo(
 
         const assetChainId = toHex(asset.chainId);
 
-        await initiateConversion({
+        await initiateCustomConversion({
           preferredPaymentToken: {
             address: toHex(asset.address),
             chainId: assetChainId,
@@ -251,7 +251,7 @@ export const TokenListItem = React.memo(
       chainId,
       createEventBuilder,
       hasSeenConversionEducationScreen,
-      initiateConversion,
+      initiateCustomConversion,
       networkName,
       trackEvent,
     ]);
