861cf222869e27f69df697c5dccb80c8f334c546
gantunesr <17601467+gantunesr@users.noreply.github.com>
2026-01-21 22:44:51 -0300

    test: undo E2E changes

--- e2e/specs/send/send-solana-token.spec.ts
+++ e2e/specs/send/send-solana-token.spec.ts
@@ -9,21 +9,16 @@ const RECIPIENT = '4Nd1mZyJY5ZqzR3n8bQF7h5L2Q9gY1yTtM6nQhc7P1Dp';
 
 describe(SmokeConfirmationsRedesigned('Send SOL token'), () => {
   it('should send solana to an address', async () => {
-    await withSolanaAccountEnabled(
-      {
-        numberOfAccounts: 0,
-      },
-      async () => {
-        await device.disableSynchronization();
-        await WalletView.tapOnToken('Solana', 1);
-        await TokenOverview.tapSendButton();
-        // using 0 value as balance of SOL is not loaded at times making test flaky
-        await SendView.enterZeroAmount();
-        await SendView.pressContinueButton();
-        await SendView.inputRecipientAddress(RECIPIENT);
-        await SendView.pressReviewButton();
-        await SolanaTestDApp.tapCancelButton();
-      },
-    );
+    await withSolanaAccountEnabled({}, async () => {
+      await device.disableSynchronization();
+      await WalletView.tapOnToken('Solana', 1);
+      await TokenOverview.tapSendButton();
+      // using 0 value as balance of SOL is not loaded at times making test flaky
+      await SendView.enterZeroAmount();
+      await SendView.pressContinueButton();
+      await SendView.inputRecipientAddress(RECIPIENT);
+      await SendView.pressReviewButton();
+      await SolanaTestDApp.tapCancelButton();
+    });
   });
 });
