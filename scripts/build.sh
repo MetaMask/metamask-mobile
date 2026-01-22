e62cf0862641939874153617f2988faed3cd63ed
Gaurav Goel <grvgoel19@gmail.com>
2026-01-22 17:48:46 +0700

    code cleanup

--- scripts/build.sh
+++ scripts/build.sh
@@ -29,9 +29,11 @@ loadJSEnv(){
 # Load JS env variables
 loadJSEnv
 
-# Always apply ENVIRONMENT arg if provided (overrides .js.env)
-export METAMASK_BUILD_TYPE=${MODE:-"$METAMASK_BUILD_TYPE"}
-export METAMASK_ENVIRONMENT=${ENVIRONMENT:-"$METAMASK_ENVIRONMENT"}
+if [ "$PLATFORM" != "watcher" ]; then
+	# Use the values from the environment variables when platform is watcher
+	export METAMASK_BUILD_TYPE=${MODE:-"$METAMASK_BUILD_TYPE"}
+	export METAMASK_ENVIRONMENT=${ENVIRONMENT:-"$METAMASK_ENVIRONMENT"}
+fi
 
 # Enable Sentry to auto upload source maps and debug symbols
 export SENTRY_DISABLE_AUTO_UPLOAD=${SENTRY_DISABLE_AUTO_UPLOAD:-"true"}
