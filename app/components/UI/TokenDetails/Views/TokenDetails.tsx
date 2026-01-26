156e9f2cb4558d776be946d677fecc36f1a446b3
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2026-01-26 15:30:52 +0000

    create wrapper

--- app/components/UI/TokenDetails/Views/TokenDetails.tsx
+++ app/components/UI/TokenDetails/Views/TokenDetails.tsx
@@ -1,27 +1,111 @@
 import React from 'react';
-import { View } from 'react-native';
-import Text from '../../../../component-library/components/Texts/Text';
+import { View, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 import { selectTokenDetailsV2Enabled } from '../../../../selectors/featureFlagController/tokenDetailsV2';
 import Asset from '../../../Views/Asset';
+import { TokenI } from '../../Tokens/types';
+import { Theme } from '@metamask/design-tokens';
+import { useStyles } from '../../../hooks/useStyles';
+import { RootState } from '../../../../reducers';
+import { selectNetworkConfigurationByChainId } from '../../../../selectors/networkController';
+import { useNavigation } from '@react-navigation/native';
+import Routes from '../../../../constants/navigation/Routes';
+import { isMainnetByChainId } from '../../../../util/networks';
+import useBlockExplorer from '../../../hooks/useBlockExplorer';
+import { AssetInlineHeader } from '../components/AssetInlineHeader';
 
-const TokenDetailsFeatureFlagWrapper: React.FC = (props: any) => {
+interface TokenDetailsProps {
+  route: {
+    params: TokenI;
+  };
+}
+
+const styleSheet = (params: { theme: Theme }) => {
+  const { theme } = params;
+  const { colors } = theme;
+  return StyleSheet.create({
+    wrapper: {
+      backgroundColor: colors.background.default,
+      flex: 1,
+    },
+  });
+};
+
+const TokenDetails: React.FC<{ token: TokenI }> = ({ token }) => {
+  const { styles } = useStyles(styleSheet, {});
+  const networkConfigurationByChainId = useSelector((state: RootState) =>
+    selectNetworkConfigurationByChainId(state, token.chainId),
+  );
+  const navigation = useNavigation();
+
+  const isNativeToken = token.isNative ?? token.isETH;
+  const isMainnet = isMainnetByChainId(token.chainId);
+  const { getBlockExplorerUrl } = useBlockExplorer(token.chainId);
+
+  const shouldShowMoreOptionsInNavBar =
+    isMainnet ||
+    !isNativeToken ||
+    (isNativeToken && getBlockExplorerUrl(token.address, token.chainId));
+
+  const openAssetOptions = () => {
+    navigation.navigate(Routes.MODAL.ROOT_MODAL_FLOW, {
+      screen: 'AssetOptions',
+      params: {
+        isNativeCurrency: isNativeToken,
+        address: token.address,
+        chainId: token.chainId,
+        asset: token,
+      },
+    });
+  };
+
+  return (
+    <View style={styles.wrapper}>
+      <AssetInlineHeader
+        title={token.symbol}
+        networkName={networkConfigurationByChainId.name}
+        onBackPress={() => navigation.goBack()}
+        onOptionsPress={
+          shouldShowMoreOptionsInNavBar ? openAssetOptions : () => undefined
+        }
+      />
+    </View>
+  );
+};
+
+const TokenDetailsFeatureFlagWrapper: React.FC<TokenDetailsProps> = (props) => {
   const isTokenDetailsV2Enabled = useSelector(selectTokenDetailsV2Enabled);
+  // const isTokenDetailsV2Enabled = true;
 
   return isTokenDetailsV2Enabled ? (
-    <TokenDetails {...props} />
+    <TokenDetails token={props.route.params} />
   ) : (
     <Asset {...props} />
   );
 };
 
-const TokenDetails: React.FC = () => {
-  console.log('TokenDetails');
-  return (
-    <View>
-      <Text>Token Details</Text>
-    </View>
-  );
-};
-
 export { TokenDetailsFeatureFlagWrapper as TokenDetails };
+
+// {
+//   key: 'Asset-kPogXX-PuEGiLDNqIlEyz',
+//   name: 'Asset',
+//   params: {
+//     accountType: 'eip155:eoa',
+//     address: '0x176211869cA2b568f2A7D4EE941E073a821EE1ff',
+//     aggregators: [],
+//     balance: '114.12913',
+//     balanceFiat: 'US$114.06',
+//     chainId: '0xe708',
+//     decimals: 6,
+//     image:
+//       'https://static.cx.metamask.io/api/v1/tokenIcons/59144/0x176211869ca2b568f2a7d4ee941e073a821ee1ff.png',
+//     isETH: false,
+//     isNative: false,
+//     isStaked: false,
+//     logo: 'https://static.cx.metamask.io/api/v1/tokenIcons/59144/0x176211869ca2b568f2a7d4ee941e073a821ee1ff.png',
+//     name: 'USDC',
+//     scrollToMerklRewards: undefined,
+//     symbol: 'USDC',
+//     ticker: 'USDC',
+//   },
+// };
---
+++ app/components/UI/TokenDetails/components/AssetInlineHeader.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Text, {
+  TextVariant,
+  TextColor,
+} from '../../../../component-library/components/Texts/Text';
+import { Theme } from '@metamask/design-tokens';
+import { useStyles } from '../../../hooks/useStyles';
+import {
+  ButtonIcon,
+  ButtonIconSize,
+  IconName,
+} from '@metamask/design-system-react-native';
+import { EdgeInsets, useSafeAreaInsets } from 'react-native-safe-area-context';
+
+const inlineHeaderStyles = (params: {
+  theme: Theme;
+  vars: { insets: EdgeInsets };
+}) => {
+  const {
+    theme,
+    vars: { insets },
+  } = params;
+  const { colors } = theme;
+  return StyleSheet.create({
+    container: {
+      backgroundColor: colors.background.default,
+      flexDirection: 'row',
+      alignItems: 'center',
+      height: 48,
+      gap: 16,
+      marginTop: insets.top,
+    },
+    leftButton: {
+      marginLeft: 16,
+    },
+    titleWrapper: {
+      flex: 1,
+      alignItems: 'center',
+    },
+    rightButton: {
+      marginRight: 16,
+    },
+    rightPlaceholder: {
+      marginRight: 16,
+      width: 24,
+    },
+  });
+};
+
+export const AssetInlineHeader = ({
+  title,
+  networkName,
+  onBackPress,
+  onOptionsPress,
+}: {
+  title: string;
+  networkName: string;
+  onBackPress: () => void;
+  onOptionsPress: () => void;
+}) => {
+  const insets = useSafeAreaInsets();
+  const { styles } = useStyles(inlineHeaderStyles, { insets });
+  return (
+    <View style={styles.container}>
+      <ButtonIcon
+        style={styles.leftButton}
+        onPress={onBackPress}
+        size={ButtonIconSize.Lg}
+        iconName={IconName.ArrowLeft}
+        testID="back-arrow-button"
+      />
+      <View style={styles.titleWrapper}>
+        <Text variant={TextVariant.HeadingSM} numberOfLines={1}>
+          {title}
+        </Text>
+        {networkName ? (
+          <Text
+            variant={TextVariant.BodySM}
+            color={TextColor.Alternative}
+            numberOfLines={1}
+          >
+            {networkName}
+          </Text>
+        ) : null}
+      </View>
+      {onOptionsPress ? (
+        <ButtonIcon
+          style={styles.rightButton}
+          onPress={onOptionsPress}
+          size={ButtonIconSize.Lg}
+          iconName={IconName.MoreVertical}
+        />
+      ) : (
+        <View style={styles.rightPlaceholder} />
+      )}
+    </View>
+  );
+};
