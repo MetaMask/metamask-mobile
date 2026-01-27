828ec1113e06556a12dce21304cfac59b6838e1c
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2026-01-27 12:40:20 +0000

    remove barrel files

--- app/components/UI/TokenDetails/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { AssetInlineHeader } from './AssetInlineHeader';
-export { default as AssetOverviewContent } from './AssetOverviewContent';
-export type { AssetOverviewContentProps } from './AssetOverviewContent';
