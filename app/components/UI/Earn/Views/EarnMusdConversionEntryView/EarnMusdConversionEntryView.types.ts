c6ddcd965d810dd0d724c0d4f26e6b7ebe733541
Nicholas Smith <nick.smith@consensys.net>
2026-01-27 13:35:25 -0600

    chore: remove ununsed directory

--- app/components/UI/Earn/Views/EarnMusdConversionEntryView/EarnMusdConversionEntryView.types.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Test IDs for the EarnMusdConversionEntryView component
- */
-export const EarnMusdConversionEntryViewSelectorsIDs = {
-  CONTAINER: 'earn-musd-conversion-entry-view-container',
-  LOADING_INDICATOR: 'earn-musd-conversion-entry-view-loading-indicator',
-};
