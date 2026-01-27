385085f5214cda0bdd214d64e8efb934a1e43f71
Goktug Poyraz <omergoktugpoyraz@gmail.com>
2026-01-27 19:40:46 +0100

    Fix unit test and e2e imports

--- e2e/specs/confirmations/transactions/transaction-pay.spec.ts
+++ e2e/specs/confirmations/transactions/transaction-pay.spec.ts
@@ -1,28 +1,28 @@
-import { withFixtures } from '../../../tests/framework/fixtures/FixtureHelper';
-import FixtureBuilder from '../../../tests/framework/fixtures/FixtureBuilder';
-import { SmokeConfirmationsRedesigned } from '../../tags';
-import { loginToApp } from '../../viewHelper';
+import { withFixtures } from '../../../../tests/framework/fixtures/FixtureHelper';
+import FixtureBuilder from '../../../../tests/framework/fixtures/FixtureBuilder';
+import { SmokeConfirmationsRedesigned } from '../../../tags';
+import { loginToApp } from '../../../viewHelper';
 import {
   remoteFeatureEip7702,
   remoteFeatureFlagPredictEnabled,
-} from '../../../tests/api-mocking/mock-responses/feature-flags-mocks';
+} from '../../../../tests/api-mocking/mock-responses/feature-flags-mocks';
 import { Mockttp } from 'mockttp';
-import { setupRemoteFeatureFlagsMock } from '../../../tests/api-mocking/helpers/remoteFeatureFlagsHelper';
-import { POLYMARKET_COMPLETE_MOCKS } from '../../../tests/api-mocking/mock-responses/polymarket/polymarket-mocks';
-import PredictAddFunds from '../../../tests/page-objects/Predict/PredictAddFunds';
+import { setupRemoteFeatureFlagsMock } from '../../../../tests/api-mocking/helpers/remoteFeatureFlagsHelper';
+import { POLYMARKET_COMPLETE_MOCKS } from '../../../../tests/api-mocking/mock-responses/polymarket/polymarket-mocks';
+import PredictAddFunds from '../../../../tests/page-objects/Predict/PredictAddFunds';
 import {
   mockRelayQuote,
   mockRelayStatus,
-} from '../../../tests/api-mocking/mock-responses/transaction-pay';
+} from '../../../../tests/api-mocking/mock-responses/transaction-pay';
 import { CHAIN_IDS } from '@metamask/transaction-controller';
-import TransactionPayConfirmation from '../../pages/Confirmation/TransactionPayConfirmation';
-import FooterActions from '../../pages/Browser/Confirmations/FooterActions';
-import { Gestures } from '../../../tests/framework';
-import TransactionDetailsModal from '../../pages/Transactions/TransactionDetailsModal';
-import TabBarComponent from '../../pages/wallet/TabBarComponent';
-import WalletActionsBottomSheet from '../../pages/wallet/WalletActionsBottomSheet';
-import ActivitiesView from '../../pages/Transactions/ActivitiesView';
-import PredictMarketList from '../../../tests/page-objects/Predict/PredictMarketList';
+import TransactionPayConfirmation from '../../../pages/Confirmation/TransactionPayConfirmation';
+import FooterActions from '../../../pages/Browser/Confirmations/FooterActions';
+import { Gestures } from '../../../../tests/framework';
+import TransactionDetailsModal from '../../../pages/Transactions/TransactionDetailsModal';
+import TabBarComponent from '../../../pages/wallet/TabBarComponent';
+import WalletActionsBottomSheet from '../../../pages/wallet/WalletActionsBottomSheet';
+import ActivitiesView from '../../../pages/Transactions/ActivitiesView';
+import PredictMarketList from '../../../../tests/page-objects/Predict/PredictMarketList';
 
 describe(SmokeConfirmationsRedesigned('Transaction Pay'), () => {
   it('deposits to predict balance', async () => {
