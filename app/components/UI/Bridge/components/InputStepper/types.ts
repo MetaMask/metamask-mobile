d2b620f126e7777cb1edaa7af553dd17f824f4d1
GeorgeGkas <georgegkas@gmail.com>
2026-01-28 12:48:46 +0200

    refactor: remove unused type

--- app/components/UI/Bridge/components/InputStepper/types.ts
+++ app/components/UI/Bridge/components/InputStepper/types.ts
@@ -4,14 +4,12 @@ import {
   IconSize,
   TextColor,
 } from '@metamask/design-system-react-native';
-import { InputStepperDescriptionType } from './constants';
 
 export interface InputStepperProps {
   value: string;
   onIncrease: () => void;
   onDecrease: () => void;
   description?: {
-    type: InputStepperDescriptionType;
     message: string;
     color: TextColor;
     icon?: {
