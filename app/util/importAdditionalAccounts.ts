0540454e07de0d50f14c041e6c23eed8e0fe2f07
gantunesr <17601467+gantunesr@users.noreply.github.com>
2026-01-27 22:12:41 -0300

    chore: remove importAdditionalAccounts

--- app/util/importAdditionalAccounts.ts
+++
@@ -1,9 +0,0 @@
-const MAX = 20;
-
-/**
- * Add additional accounts in the wallet based on balance
- */
-// eslint-disable-next-line arrow-body-style
-export default async (_maxAccounts: number = MAX, _index: number = 0) => {
-  return;
-};
