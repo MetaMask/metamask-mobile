0a1f62d5ea260be7b81ff69f43cc6113ef675b54
augmentedmode <jacob.lebowitz@consensys.net>
2026-01-28 10:19:52 -0500

    fix: pr comments

--- e2e/specs/confirmations-redesigned/signatures/alert-system.spec.ts
+++ e2e/specs/confirmations-redesigned/signatures/alert-system.spec.ts
@@ -245,7 +245,7 @@ describe(SmokeConfirmationsRedesigned('Alert System - Signature'), () => {
             AlertSystem.alertMismatchTitle,
           );
           await AlertSystem.tapAcknowledgeAlertModal();
-          await AlertSystem.tapGotItAlertModalButton();
+          await AlertSystem.tapAcknowledgeAlertModalButton();
           // Confirm request
           await FooterActions.tapConfirmButton();
           await Assertions.expectElementToBeVisible(
