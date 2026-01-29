3d68089708d1f225f2694afef4691650c67794c9
Michal Szorad <michal.szorad@consensys.net>
2026-01-29 10:27:22 +0100

    chore: fix ts

--- app/components/Views/confirmations/components/confirm/confirm-component.styles.ts
+++ app/components/Views/confirmations/components/confirm/confirm-component.styles.ts
@@ -3,7 +3,10 @@ import { Theme } from '../../../../../util/theme/models';
 
 const styleSheet = (params: {
   theme: Theme;
-  vars: { isFullScreenConfirmation: boolean; disableSafeArea: boolean };
+  vars: {
+    isFullScreenConfirmation: boolean;
+    disableSafeArea?: boolean;
+  };
 }) => {
   const { theme, vars } = params;
 
@@ -22,7 +25,7 @@ const styleSheet = (params: {
       justifyContent: 'space-between',
     },
     scrollView: {
-      paddingHorizontal: vars.disableSafeArea ? 0 : 16,
+      paddingHorizontal: vars.disableSafeArea === true ? 0 : 16,
     },
     scrollViewContent: {
       flex: vars.isFullScreenConfirmation ? 1 : undefined,
