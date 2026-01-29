9b37ff9f65ea9c321768c4a9d88482d0b251af21
ieow <4881057+ieow@users.noreply.github.com>
2026-01-29 21:40:41 +0800

    fix: disable backpress on LockScreen

--- app/components/Views/LockScreen/index.tsx
+++ app/components/Views/LockScreen/index.tsx
@@ -1,10 +1,23 @@
 /* eslint-disable import/no-commonjs */
-import React from 'react';
+import React, { useCallback } from 'react';
+import { BackHandler } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 import FoxLoader from '../../UI/FoxLoader';
 
 /**
  * View that displays a loading animation when the app is locked.
  */
-const LockScreen: React.FC = () => <FoxLoader />;
+const LockScreen: React.FC = () => {
+  useFocusEffect(
+    useCallback(() => {
+      const onBackPress = () => true; // Returning true prevents default back behavior
+      BackHandler.addEventListener('hardwareBackPress', onBackPress);
+      return () =>
+        BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+    }, []),
+  );
+
+  return <FoxLoader />;
+};
 
 export default LockScreen;
