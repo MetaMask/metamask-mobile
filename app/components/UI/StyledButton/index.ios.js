7aadd7ab0b9156570308468d7d5231f54b2372e5
georgewrmarshall <george.marshall@consensys.net>
2026-02-01 18:02:37 -0800

    chore: update deprecated component messages to point to design system
    Update outdated deprecation messages that incorrectly point to
    app/component-library when those components are themselves deprecated.
    All messages now correctly reference @metamask/design-system-react-native.
    
    Updated components:
    - Button components (StyledButton, GenericButton, Button)
    - Text component
    - WebsiteIcon (now points to AvatarFavicon specifically)
    - CheckBox component
    - All icon library components (13 variants)
    
    This provides clearer guidance for developers migrating away from
    deprecated components.

--- app/components/UI/StyledButton/index.ios.js
+++ app/components/UI/StyledButton/index.ios.js
@@ -9,12 +9,8 @@ import {
 } from 'deprecated-react-native-prop-types';
 
 /**
- * @deprecated The `<StyledButton>` component has been deprecated in favor of the new `<Button>` component from the component-library.
- * Please update your code to use the new `<Button>` component instead, which can be found at app/component-library/components/Buttons/Button/Button.tsx.
- * You can find documentation for the new Button component in the README:
- * {@link https://github.com/MetaMask/metamask-mobile/tree/main/app/component-library/components/Buttons/Button/README.md}
- * If you would like to help with the replacement of the old `Button` component, please submit a pull request against this GitHub issue:
- * {@link https://github.com/MetaMask/metamask-mobile/issues/8106}
+ * @deprecated The `<StyledButton>` component has been deprecated in favor of the `<Button>` component from `@metamask/design-system-react-native`.
+ * Please update your code to use the new `<Button>` component from `@metamask/design-system-react-native`.
  */
 export default class StyledButton extends PureComponent {
   static propTypes = {
--- app/components/UI/WebsiteIcon/index.js
+++ app/components/UI/WebsiteIcon/index.js
@@ -33,8 +33,8 @@ const createStyles = (colors) =>
  * View that renders a website logo depending of the context
  */
 /**
- * @deprecated This `<WebsiteIcon>` component has been deprecated, any new usage of it should use Avatar with the favicon variant instead:
- * https://github.com/MetaMask/metamask-mobile/blob/34f9da127435053a32e5f4e9c69ce8aa1e37c394/app/component-library/components/Avatars/Avatar/README.md#L1
+ * @deprecated The `<WebsiteIcon>` component has been deprecated in favor of the `<AvatarFavicon>` component from `@metamask/design-system-react-native`.
+ * Please update your code to use the new `<AvatarFavicon>` component from `@metamask/design-system-react-native`.
  */
 class WebsiteIcon extends PureComponent {
   static propTypes = {
