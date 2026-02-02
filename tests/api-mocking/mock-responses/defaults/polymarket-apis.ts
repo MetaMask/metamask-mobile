f96af0a036193f4e4a1e19b948d2f0f5fe9a95b9
Christopher Ferreira <christopher.ferreira@consensys.net>
2026-02-02 12:57:52 +0000

    test: removes duplicated mock

--- tests/api-mocking/mock-responses/defaults/polymarket-apis.ts
+++ tests/api-mocking/mock-responses/defaults/polymarket-apis.ts
@@ -11,28 +11,6 @@ export const POLYMARKET_API_MOCKS = {
         blocked: false, // Set to false to allow access in tests
       },
     },
-    {
-      urlEndpoint:
-        'https://tx-sentinel-polygon-mainnet.api.cx.metamask.io/network',
-      responseCode: 200,
-      response: {
-        name: 'Polygon Mainnet',
-        group: 'polygon',
-        chainID: 137,
-        nativeCurrency: {
-          name: 'POL',
-          symbol: 'POL',
-          decimals: 18,
-        },
-        network: 'polygon-mainnet',
-        explorer: 'https://polygonscan.com/',
-        confirmations: true,
-        smartTransactions: false,
-        relayTransactions: true,
-        hidden: false,
-        sendBundle: false,
-      },
-    },
   ],
   POST: [],
   PUT: [],
