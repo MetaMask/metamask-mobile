e19dfddd93a895d8a4d2958af84cebfd525e47c2
George Weiler <george.weiler@consensys.net>
2026-02-02 09:32:27 -0700

    chore: install ramps controller preview build

--- app/components/UI/Ramp/hooks/useHydrateRampsController.ts
+++ app/components/UI/Ramp/hooks/useHydrateRampsController.ts
@@ -12,7 +12,7 @@ import useRampsUnifiedV2Enabled from './useRampsUnifiedV2Enabled';
  * Should be called from a top-level component like Wallet that mounts early.
  */
 export function useHydrateRampsController(): void {
-  const userRegion = useSelector(selectUserRegion);
+  const { data: userRegion } = useSelector(selectUserRegion);
   const isV2UnifiedEnabled = useRampsUnifiedV2Enabled();
 
   useEffect(() => {
