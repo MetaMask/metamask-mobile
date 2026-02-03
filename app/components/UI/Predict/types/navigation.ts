dba1aec7cb274e8db0a9561d4a3c1a9505cc1e3e
sallem-consensys <sallem.ahmed@consensys.net>
2026-02-03 16:00:54 -0500

    fix predict typing

--- app/components/UI/Predict/types/navigation.ts
+++ app/components/UI/Predict/types/navigation.ts
@@ -2,6 +2,29 @@
  * Predict navigation parameters
  */
 
+import { ParamListBase } from '@react-navigation/native';
+import {
+  PredictActivityItem,
+  PredictMarket,
+  PredictOutcome,
+  PredictOutcomeToken,
+  PredictPosition,
+} from '.';
+import { PredictEventValues } from '../constants/eventNames';
+
+export type PredictEntryPoint =
+  | typeof PredictEventValues.ENTRY_POINT.CAROUSEL
+  | typeof PredictEventValues.ENTRY_POINT.PREDICT_FEED
+  | typeof PredictEventValues.ENTRY_POINT.PREDICT_MARKET_DETAILS
+  | typeof PredictEventValues.ENTRY_POINT.SEARCH
+  | typeof PredictEventValues.ENTRY_POINT.HOMEPAGE_POSITIONS
+  | typeof PredictEventValues.ENTRY_POINT.HOMEPAGE_NEW_PREDICTION
+  | typeof PredictEventValues.ENTRY_POINT.HOMEPAGE_BALANCE
+  | typeof PredictEventValues.ENTRY_POINT.MAIN_TRADE_BUTTON
+  | typeof PredictEventValues.ENTRY_POINT.BACKGROUND
+  | typeof PredictEventValues.ENTRY_POINT.TRENDING_SEARCH
+  | typeof PredictEventValues.ENTRY_POINT.TRENDING;
+
 /** Predict market list parameters */
 export interface PredictMarketListParams {
   entryPoint?: string;
@@ -10,7 +33,7 @@ export interface PredictMarketListParams {
 /** Predict market details parameters */
 export interface PredictMarketDetailsParams {
   marketId?: string;
-  entryPoint?: string;
+  entryPoint?: PredictEntryPoint;
   title?: string;
   image?: string;
   isGame?: boolean;
@@ -19,21 +42,30 @@ export interface PredictMarketDetailsParams {
 
 /** Predict activity detail parameters */
 export interface PredictActivityDetailParams {
-  activity: Record<string, unknown>;
+  activity: PredictActivityItem;
 }
 
 /** Predict buy preview parameters */
 export interface PredictBuyPreviewParams {
-  market: Record<string, unknown>;
-  outcome: Record<string, unknown>;
-  outcomeToken: Record<string, unknown>;
-  entryPoint?: string;
+  market: PredictMarket;
+  outcome: PredictOutcome;
+  outcomeToken: PredictOutcomeToken;
+  entryPoint?: PredictEntryPoint;
 }
 
 /** Predict sell preview parameters */
 export interface PredictSellPreviewParams {
-  market: Record<string, unknown>;
-  position: Record<string, unknown>;
-  outcome: Record<string, unknown>;
-  entryPoint?: string;
+  market: PredictMarket;
+  position: PredictPosition;
+  outcome: PredictOutcome;
+  entryPoint?: PredictEntryPoint;
+}
+
+export interface PredictNavigationParamList extends ParamListBase {
+  Predict: undefined;
+  PredictMarketList: PredictMarketListParams;
+  PredictMarketDetails: PredictMarketDetailsParams;
+  PredictSellPreview: PredictSellPreviewParams;
+  PredictBuyPreview: PredictBuyPreviewParams;
+  PredictActivityDetail: PredictActivityDetailParams;
 }
