33f83adde5fe74f55aac1397c2266769fa945bef
georgewrmarshall <george.marshall@consensys.net>
2026-02-04 13:46:56 -0800

    chore: reverting bottomsheetfooter styles and updating snapshots

--- app/component-library/components/BottomSheets/BottomSheetFooter/BottomSheetFooter.styles.ts
+++ app/component-library/components/BottomSheets/BottomSheetFooter/BottomSheetFooter.styles.ts
@@ -35,7 +35,7 @@ const styleSheet = (params: {
         flexDirection:
           buttonsAlignment === ButtonsAlignment.Horizontal ? 'row' : 'column',
         paddingVertical: 4,
-        paddingHorizontal: 16,
+        paddingHorizontal: 8,
       } as ViewStyle,
       style,
     ) as ViewStyle,
