318bde9111ac1e905c50d91000375f2f1bd0b0dd
Cal-L <cal.leung@consensys.net>
2026-02-04 15:44:43 -0800

    Add unit tests for osAuthEnabled action

--- app/reducers/security/index.test.ts
+++ app/reducers/security/index.test.ts
@@ -1,4 +1,9 @@
-import { ActionType } from '../../actions/security';
+import {
+  ActionType,
+  type AllowLoginWithRememberMeUpdated,
+  type SetDataCollectionForMarketing,
+  type SetOsAuthEnabled,
+} from '../../actions/security';
 import { SecuritySettingsState } from '../../actions/security/state';
 import securityReducer from '.';
 
@@ -7,12 +12,11 @@ describe('securityReducer', () => {
     allowLoginWithRememberMe: false,
     dataCollectionForMarketing: null,
     isNFTAutoDetectionModalViewed: false,
+    osAuthEnabled: true,
   };
 
   it('should set allowLoginWithRememberMe to true', () => {
-    // TODO: Replace "any" with type
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const action: any = {
+    const action: AllowLoginWithRememberMeUpdated = {
       type: ActionType.SET_ALLOW_LOGIN_WITH_REMEMBER_ME,
       enabled: true,
     };
@@ -30,7 +34,7 @@ describe('securityReducer', () => {
   it('should set dataCollectionForMarketing to true', () => {
     // TODO: Replace "any" with type
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const action: any = {
+    const action: SetDataCollectionForMarketing = {
       type: ActionType.SET_DATA_COLLECTION_FOR_MARKETING,
       enabled: true,
     };
@@ -38,6 +42,22 @@ describe('securityReducer', () => {
     const expectedState = {
       ...initialState,
       dataCollectionForMarketing: true,
+    };
+
+    const newState = securityReducer(initialState, action);
+
+    expect(newState).toEqual(expectedState);
+  });
+
+  it('should set osAuthEnabled to true', () => {
+    const action: SetOsAuthEnabled = {
+      type: ActionType.SET_OS_AUTH_ENABLED,
+      enabled: true,
+    };
+
+    const expectedState = {
+      ...initialState,
+      osAuthEnabled: true,
     };
 
     const newState = securityReducer(initialState, action);
