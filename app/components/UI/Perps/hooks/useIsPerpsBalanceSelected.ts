3fda58e3d95a2c653ef61afec3b05e6ded0877bb
Michal Szorad <michal.szorad@consensys.net>
2026-02-04 11:53:30 +0100

    wip

--- app/components/UI/Perps/hooks/useIsPerpsBalanceSelected.ts
+++ app/components/UI/Perps/hooks/useIsPerpsBalanceSelected.ts
@@ -1,13 +1,10 @@
 import type { Hex } from '@metamask/utils';
 import { useSelector } from 'react-redux';
-import { USDC_ARBITRUM_MAINNET_ADDRESS } from '../constants/hyperLiquidConfig';
 import { ARBITRUM_CHAIN_ID } from '@metamask/swaps-controller/dist/constants';
 import { selectIsPerpsBalanceSelected } from '../selectors/perpsController';
 
 /** Address used to represent "Perps balance" as the payment token (synthetic option). */
-export const PERPS_BALANCE_PLACEHOLDER_ADDRESS =
-  USDC_ARBITRUM_MAINNET_ADDRESS ||
-  ('0x0000000000000000000000000000000000000001' as Hex);
+export const PERPS_BALANCE_PLACEHOLDER_ADDRESS = ('0x0000000000000000000000000000000000000000' as Hex);
 
 /** Chain id used for the "Perps balance" payment option. */
 export const PERPS_BALANCE_CHAIN_ID = ARBITRUM_CHAIN_ID;
