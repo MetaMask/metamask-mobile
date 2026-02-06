f4bc9269816d51c1084c862259d83329c660d3f3
dan437 <80175477+dan437@users.noreply.github.com>
2026-02-06 13:53:35 +0100

    Temporarily enable withdraw to any token
    Signed-off-by: dan437 <80175477+dan437@users.noreply.github.com>

--- app/components/Views/confirmations/hooks/pay/useTransactionPayWithdraw.ts
+++ app/components/Views/confirmations/hooks/pay/useTransactionPayWithdraw.ts
@@ -5,8 +5,9 @@ import { isTransactionPayWithdraw } from '../../utils/transaction';
  * Whether the withdraw token picker feature is enabled.
  * When false, withdrawals always use the default Polygon USDC.E.
  */
-const isWithdrawTokenPickerEnabled =
-  process.env.MM_PREDICT_WITHDRAW_ANY_TOKEN === 'true';
+// TODO: Revert before merging - temporarily enabled for preview build
+const isWithdrawTokenPickerEnabled = true;
+// process.env.MM_PREDICT_WITHDRAW_ANY_TOKEN === 'true';
 
 export interface UseTransactionPayWithdrawResult {
   /** Whether this transaction is a withdraw type */
