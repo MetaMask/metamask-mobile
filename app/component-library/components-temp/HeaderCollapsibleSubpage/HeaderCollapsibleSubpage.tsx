402a7eee1e6c19930359cd80b9aea54bc434079c
Brian Nguyen <brianacnguyen@gmail.com>
2026-02-05 17:02:38 -0800

    Added all headers to components temp

---
+++ app/component-library/components-temp/HeaderCollapsibleSubpage/HeaderCollapsibleSubpage.tsx
@@ -0,0 +1,69 @@
+// Third party dependencies.
+import React from 'react';
+
+// Internal dependencies.
+import HeaderCollapsible from '../HeaderCollapsible';
+import TitleSubpage from '../TitleSubpage';
+import { HeaderCollapsibleSubpageProps } from './HeaderCollapsibleSubpage.types';
+
+/**
+ * HeaderCollapsibleSubpage is a collapsing header component that combines
+ * HeaderCollapsible with TitleSubpage as the expanded content.
+ *
+ * @example
+ * ```tsx
+ * const { onScroll, scrollY, expandedHeight, setExpandedHeight } = useHeaderCollapsible();
+ *
+ * return (
+ *   <View style={{ flex: 1 }}>
+ *     <HeaderCollapsibleSubpage
+ *       title="Token Name"
+ *       onBack={handleBack}
+ *       titleSubpageProps={{
+ *         startAccessory: <AvatarToken />,
+ *         title: "Token Name",
+ *         bottomLabel: "$1,234.56",
+ *       }}
+ *       scrollY={scrollY}
+ *       onExpandedHeightChange={setExpandedHeight}
+ *     />
+ *     <ScrollView
+ *       onScroll={onScroll}
+ *       scrollEventThrottle={16}
+ *       contentContainerStyle={{ paddingTop: expandedHeight }}
+ *     >
+ *       <Content />
+ *     </ScrollView>
+ *   </View>
+ * );
+ * ```
+ */
+const HeaderCollapsibleSubpage: React.FC<HeaderCollapsibleSubpageProps> = ({
+  titleSubpage,
+  titleSubpageProps,
+  titleSectionTestID,
+  ...props
+}) => {
+  // Render title section content
+  const renderExpandedContent = () => {
+    if (titleSubpage) {
+      return titleSubpage;
+    }
+    if (titleSubpageProps) {
+      return (
+        <TitleSubpage
+          {...titleSubpageProps}
+          testID={titleSectionTestID}
+          twClassName={`px-4 pt-1 pb-3 ${titleSubpageProps?.twClassName ?? ''}`.trim()}
+        />
+      );
+    }
+    return null;
+  };
+
+  return (
+    <HeaderCollapsible {...props} expandedContent={renderExpandedContent()} />
+  );
+};
+
+export default HeaderCollapsibleSubpage;
---
+++ app/component-library/components-temp/HeaderStackedStandard/index.ts
@@ -0,0 +1,3 @@
+export { default } from './HeaderStackedStandard';
+export { default as getHeaderStackedStandardNavbarOptions } from './getHeaderStackedStandardNavbarOptions';
+export type { HeaderStackedStandardProps } from './HeaderStackedStandard.types';
