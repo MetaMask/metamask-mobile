2eaf74d1bc35aea7daefb44d1c1e495b7aaf3758
Brian Nguyen <brianacnguyen@gmail.com>
2026-02-09 21:41:08 -0800

    Updated headerstackedstandard index

--- app/component-library/components-temp/HeaderStackedStandard/index.ts
+++ app/component-library/components-temp/HeaderStackedStandard/index.ts
@@ -1,3 +1,2 @@
 export { default } from './HeaderStackedStandard';
-export { default as getHeaderStackedStandardNavbarOptions } from './getHeaderStackedStandardNavbarOptions';
 export type { HeaderStackedStandardProps } from './HeaderStackedStandard.types';
