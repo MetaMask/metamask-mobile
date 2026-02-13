6e0a6a7819acd59fbdd79cfcb99536bec8a92e82
Patryk Lucka <5708018+PatrykLucka@users.noreply.github.com>
2026-02-13 12:36:37 +0100

    refactor(MusdClaimInfo): simplify component structure and enhance chainId handling

--- app/components/Views/confirmations/components/info/musd-claim-info/musd-claim-info.tsx
+++ app/components/Views/confirmations/components/info/musd-claim-info/musd-claim-info.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Hex } from '@metamask/utils';
 
 import { strings } from '../../../../../../../locales/i18n';
 import InfoSection from '../../UI/info-row/info-section';
@@ -10,26 +9,15 @@ import AccountRow from '../../rows/transactions/account-row';
 import NetworkRow from '../../rows/transactions/network-row';
 import GasFeesDetailsRow from '../../rows/transactions/gas-fee-details-row';
 import { ConfirmationInfoComponentIDs } from '../../../constants/info-ids';
-import { useTransactionMetadataRequest } from '../../../hooks/transactions/useTransactionMetadataRequest';
 
-export const MusdClaimInfo = () => {
-  const transactionMetadata = useTransactionMetadataRequest();
-
-  if (!transactionMetadata) {
-    return null;
-  }
-
-  const chainId = transactionMetadata.chainId as Hex;
-
-  return (
-    <View testID={ConfirmationInfoComponentIDs.MUSD_CLAIM}>
-      <HeroRow />
-      <InfoSection>
-        <AccountRow label={strings('stake.claiming_to')} chainId={chainId} />
-        <InfoRowDivider />
-        <NetworkRow chainId={chainId} />
-      </InfoSection>
-      <GasFeesDetailsRow disableUpdate />
-    </View>
-  );
-};
+export const MusdClaimInfo = () => (
+  <View testID={ConfirmationInfoComponentIDs.MUSD_CLAIM}>
+    <HeroRow />
+    <InfoSection>
+      <AccountRow label={strings('stake.claiming_to')} />
+      <InfoRowDivider />
+      <NetworkRow />
+    </InfoSection>
+    <GasFeesDetailsRow disableUpdate />
+  </View>
+);
