3d7d8382dcb6ae20371f5e364237628e189b7974
Ganesh Suresh Patra <ganesh.patra@consensys.net>
2026-02-13 10:46:30 +0530

    fix: updated srpquiz image for dark and light theme

--- app/components/Views/RevealPrivateCredential/components/SRPQuizIntroduction.tsx
+++ app/components/Views/RevealPrivateCredential/components/SRPQuizIntroduction.tsx
@@ -13,59 +13,70 @@ import {
   FlexDirection,
   JustifyContent,
 } from '../../../UI/Box/box.types';
-import SecurityQuizLockImage from '../../../../images/reveal_srp.png';
+import SecurityQuizLockImageLight from '../../../../images/reveal_srp_light.png';
+import SecurityQuizLockImageDark from '../../../../images/reveal_srp.png';
 import { strings } from '../../../../../locales/i18n';
 import { ExportCredentialsIds } from '../../MultichainAccounts/AccountDetails/ExportCredentials.testIds';
 import { SrpQuizGetStartedSelectorsIDs } from '../../Quiz/SRPQuiz/SrpQuizModal.testIds';
 import { SRPQuizIntroductionProps } from '../types';
+import { useTheme } from '../../../../util/theme';
+import { AppThemeKey } from '../../../../util/theme/models';
 
 const SRPQuizIntroduction = ({
   onGetStarted,
   onLearnMore,
   styles,
-}: SRPQuizIntroductionProps) => (
-  <Box
-    testID={ExportCredentialsIds.CONTAINER}
-    style={styles.quizContainer}
-    flexDirection={FlexDirection.Column}
-    alignItems={AlignItems.center}
-    justifyContent={JustifyContent.spaceBetween}
-  >
+}: SRPQuizIntroductionProps) => {
+  const theme = useTheme();
+  const { themeAppearance } = theme;
+  const SecurityQuizLockImage =
+    themeAppearance === AppThemeKey.dark
+      ? SecurityQuizLockImageDark
+      : SecurityQuizLockImageLight;
+  return (
     <Box
+      testID={ExportCredentialsIds.CONTAINER}
+      style={styles.quizContainer}
       flexDirection={FlexDirection.Column}
       alignItems={AlignItems.center}
-      justifyContent={JustifyContent.center}
+      justifyContent={JustifyContent.spaceBetween}
     >
-      <Image source={SecurityQuizLockImage} height={220} width={190} />
-      <Text
-        variant={TextVariant.BodyMD}
-        color={TextColor.Alternative}
-        style={styles.quizDescription}
+      <Box
+        flexDirection={FlexDirection.Column}
+        alignItems={AlignItems.center}
+        justifyContent={JustifyContent.center}
       >
-        {strings('multichain_accounts.reveal_srp.description')}
-      </Text>
+        <Image source={SecurityQuizLockImage} height={220} width={190} />
+        <Text
+          variant={TextVariant.BodyMD}
+          color={TextColor.Alternative}
+          style={styles.quizDescription}
+        >
+          {strings('multichain_accounts.reveal_srp.description')}
+        </Text>
+      </Box>
+      <Box
+        flexDirection={FlexDirection.Column}
+        alignItems={AlignItems.center}
+        style={styles.quizButtonContainer}
+        gap={16}
+      >
+        <ButtonPrimary
+          onPress={onGetStarted}
+          size={ButtonSize.Lg}
+          label={strings('multichain_accounts.reveal_srp.get_started')}
+          testID={SrpQuizGetStartedSelectorsIDs.BUTTON}
+          style={styles.button}
+        />
+        <ButtonLink
+          onPress={onLearnMore}
+          label={strings('multichain_accounts.reveal_srp.learn_more')}
+          testID={ExportCredentialsIds.LEARN_MORE_BUTTON}
+          style={styles.button}
+        />
+      </Box>
     </Box>
-    <Box
-      flexDirection={FlexDirection.Column}
-      alignItems={AlignItems.center}
-      style={styles.quizButtonContainer}
-      gap={16}
-    >
-      <ButtonPrimary
-        onPress={onGetStarted}
-        size={ButtonSize.Lg}
-        label={strings('multichain_accounts.reveal_srp.get_started')}
-        testID={SrpQuizGetStartedSelectorsIDs.BUTTON}
-        style={styles.button}
-      />
-      <ButtonLink
-        onPress={onLearnMore}
-        label={strings('multichain_accounts.reveal_srp.learn_more')}
-        testID={ExportCredentialsIds.LEARN_MORE_BUTTON}
-        style={styles.button}
-      />
-    </Box>
-  </Box>
-);
+  );
+};
 
 export default SRPQuizIntroduction;
