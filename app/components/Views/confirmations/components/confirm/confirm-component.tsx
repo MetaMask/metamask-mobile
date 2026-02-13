499b8b78794ac0842db959f4f441a749d457f13d
Matthew Grainger <matthew.grainger@consensys.net>
2026-02-13 16:22:16 -0500

    fix: fixed fullscreen default loading spinner rendering after confirming max convert transaction

--- app/components/Views/confirmations/components/confirm/confirm-component.tsx
+++ app/components/Views/confirmations/components/confirm/confirm-component.tsx
@@ -194,9 +194,15 @@ function ConfirmationAlerts({ children }: { children: ReactNode }) {
 }
 
 function Loader() {
-  const { styles } = useStyles(styleSheet, { isFullScreenConfirmation: true });
+  const { isFullScreenConfirmation } = useFullScreenConfirmation();
+  const { styles } = useStyles(styleSheet, { isFullScreenConfirmation });
   const params = useParams<ConfirmationParams>();
   const loader = params?.loader ?? ConfirmationLoader.Default;
+
+  // Don't show full screen loader for bottom sheet confirmations
+  if (!isFullScreenConfirmation) {
+    return null;
+  }
 
   if (loader === ConfirmationLoader.CustomAmount) {
     return (
