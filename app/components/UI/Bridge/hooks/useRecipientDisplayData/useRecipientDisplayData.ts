fcf3f04a42e0d7f2471375c3fded00bd0f856686
gantunesr <17601467+gantunesr@users.noreply.github.com>
2026-02-18 16:22:40 -0300

    refactor: remove BIP-44 flag from bridge code

--- app/components/UI/Bridge/hooks/useRecipientDisplayData/useRecipientDisplayData.ts
+++ app/components/UI/Bridge/hooks/useRecipientDisplayData/useRecipientDisplayData.ts
@@ -6,7 +6,6 @@ import {
   selectAccountToWalletMap,
   selectWalletsMap,
 } from '../../../../../selectors/multichainAccounts/accountTreeController';
-import { selectMultichainAccountsState2Enabled } from '../../../../../selectors/featureFlagController/multichainAccounts';
 import { useMemo } from 'react';
 import { areAddressesEqual } from '../../../../../util/address';
 
@@ -25,9 +24,6 @@ export const useRecipientDisplayData = () => {
   const internalAccounts = useSelector(selectInternalAccounts);
   const accountToGroupMap = useSelector(selectAccountToGroupMap);
   const accountToWalletMap = useSelector(selectAccountToWalletMap);
-  const isMultichainAccountsState2Enabled = useSelector(
-    selectMultichainAccountsState2Enabled,
-  );
   const walletsMap = useSelector(selectWalletsMap);
 
   return useMemo(() => {
@@ -54,17 +50,13 @@ export const useRecipientDisplayData = () => {
     let displayName = internalAccount.metadata.name;
     let walletName: string | undefined;
 
-    // Use account group name if available, otherwise use account name
-    if (isMultichainAccountsState2Enabled) {
-      const accountGroup = accountToGroupMap[internalAccount.id];
-      displayName =
-        accountGroup?.metadata.name || internalAccount.metadata.name;
+    const accountGroup = accountToGroupMap[internalAccount.id];
+    displayName = accountGroup?.metadata.name || internalAccount.metadata.name;
 
-      if (walletsMap) {
-        const walletId = accountToWalletMap[internalAccount.id];
-        if (walletId && walletsMap[walletId]) {
-          walletName = walletsMap[walletId].metadata.name;
-        }
+    if (walletsMap) {
+      const walletId = accountToWalletMap[internalAccount.id];
+      if (walletId && walletsMap[walletId]) {
+        walletName = walletsMap[walletId].metadata.name;
       }
     }
 
@@ -77,7 +69,6 @@ export const useRecipientDisplayData = () => {
     destAddress,
     internalAccounts,
     accountToGroupMap,
-    isMultichainAccountsState2Enabled,
     accountToWalletMap,
     walletsMap,
   ]);
