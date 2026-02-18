402db208cdc081bc72105d1431298edf7f6f9610
Cal-L <cal.leung@consensys.net>
2026-02-17 20:30:03 -0800

    Add device authentication as authentication type

--- app/constants/userProperties.ts
+++ app/constants/userProperties.ts
@@ -1,6 +1,7 @@
 enum AUTHENTICATION_TYPE {
   BIOMETRIC = 'biometrics',
   PASSCODE = 'device_passcode',
+  DEVICE_AUTHENTICATION = 'device_authentication',
   REMEMBER_ME = 'remember_me',
   PASSWORD = 'password',
   UNKNOWN = 'unknown',
