647212d53c9b1bbf5afbb17e4886765c352d1e3e
Brian Nguyen <brianacnguyen@gmail.com>
2026-02-18 11:30:02 -0800

    Fixed lint issues

--- app/components/Views/Settings/AppInformation/index.test.tsx
+++ app/components/Views/Settings/AppInformation/index.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { waitFor, fireEvent } from '@testing-library/react-native';
 import { Image, TouchableOpacity } from 'react-native';
 import renderWithProvider, {
