059d355b0585a970d0752c3e5daa14e0708e75ed
Cal-L <cal.leung@consensys.net>
2026-02-17 22:31:02 -0800

    Add deprecation doc on remember me

--- app/constants/userProperties.ts
+++ app/constants/userProperties.ts
@@ -2,9 +2,12 @@ enum AUTHENTICATION_TYPE {
   BIOMETRIC = 'biometrics',
   PASSCODE = 'device_passcode',
   DEVICE_AUTHENTICATION = 'device_authentication',
-  REMEMBER_ME = 'remember_me',
   PASSWORD = 'password',
   UNKNOWN = 'unknown',
+  /**
+   * @deprecated Legacy authentication type used for keychain storage
+   */
+  REMEMBER_ME = 'remember_me',
 }
 
 export default AUTHENTICATION_TYPE;
