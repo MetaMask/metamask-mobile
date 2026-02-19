5087f2d026a1f94e4fcd5cab47396fe6aa8c73fe
Matthew Grainger <matthew.grainger@consensys.net>
2026-02-19 14:48:56 -0500

    feat: fix invalid redirect_to property for mUSD conversion education screen when quick convert is enabled

--- app/components/UI/Earn/Views/EarnMusdConversionEducationView/index.tsx
+++ app/components/UI/Earn/Views/EarnMusdConversionEducationView/index.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useMemo, useRef } from 'react';
 import { Hex } from '@metamask/utils';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { View, Image, useColorScheme, Linking } from 'react-native';
 import { setMusdConversionEducationSeen } from '../../../../../actions/user';
 import Logger from '../../../../../util/Logger';
@@ -39,6 +39,7 @@ import { RampIntent } from '../../../Ramp/types';
 import { EARN_TEST_IDS } from '../../constants/testIds';
 import AppConstants from '../../../../../core/AppConstants';
 import { MUSD_CONVERSION_NAVIGATION_OVERRIDE } from '../../types/musd.types';
+import { selectMusdQuickConvertEnabledFlag } from '../../selectors/featureFlags';
 interface EarnMusdConversionEducationViewRouteParams {
   /**
    * Indicates if this navigation originated from a deeplink
@@ -63,6 +64,8 @@ interface EarnMusdConversionEducationViewRouteParams {
  */
 const EarnMusdConversionEducationView = () => {
   const dispatch = useDispatch();
+
+  const isQuickConvertEnabled = useSelector(selectMusdQuickConvertEnabledFlag);
 
   const { initiateCustomConversion } = useMusdConversion();
   const { goToBuy } = useRampNavigation();
@@ -168,7 +171,9 @@ const EarnMusdConversionEducationView = () => {
   }, [submitScreenViewedEvent]);
 
   const submitContinuePressedEvent = useCallback(() => {
-    let redirectsTo = EVENT_LOCATIONS.CUSTOM_AMOUNT_SCREEN;
+    let redirectsTo = isQuickConvertEnabled
+      ? EVENT_LOCATIONS.QUICK_CONVERT_HOME_SCREEN
+      : EVENT_LOCATIONS.CUSTOM_AMOUNT_SCREEN;
     if (deeplinkState?.action === 'navigate_home') {
       redirectsTo = EVENT_LOCATIONS.HOME_SCREEN;
     } else if (deeplinkState?.action === 'buy') {
@@ -188,15 +193,17 @@ const EarnMusdConversionEducationView = () => {
         .build(),
     );
   }, [
+    isQuickConvertEnabled,
+    EVENT_LOCATIONS.QUICK_CONVERT_HOME_SCREEN,
+    EVENT_LOCATIONS.CUSTOM_AMOUNT_SCREEN,
+    EVENT_LOCATIONS.CONVERSION_EDUCATION_SCREEN,
+    EVENT_LOCATIONS.HOME_SCREEN,
+    EVENT_LOCATIONS.BUY_SCREEN,
+    deeplinkState?.action,
     trackEvent,
     createEventBuilder,
-    EVENT_LOCATIONS.CONVERSION_EDUCATION_SCREEN,
-    EVENT_LOCATIONS.CUSTOM_AMOUNT_SCREEN,
-    EVENT_LOCATIONS.BUY_SCREEN,
-    EVENT_LOCATIONS.HOME_SCREEN,
     BUTTON_TYPES.PRIMARY,
     primaryButtonText,
-    deeplinkState,
   ]);
 
   const submitGoBackPressedEvent = () => {
