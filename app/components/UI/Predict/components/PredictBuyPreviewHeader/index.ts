1fc22db91a016539a3bf7b4f532310eb66bf4d8c
Caainã Jeronimo <caainaje@gmail.com>
2026-02-19 11:02:21 -0300

    fix: predict info header

--- app/components/UI/Predict/components/PredictBuyPreviewHeader/index.ts
+++ app/components/UI/Predict/components/PredictBuyPreviewHeader/index.ts
@@ -1,1 +1,6 @@
 export { default as PredictBuyPreviewHeader } from './PredictBuyPreviewHeader';
+export {
+  PredictBuyPreviewHeaderTitle,
+  PredictBuyPreviewHeaderBack,
+} from './PredictBuyPreviewHeader';
+export type { PredictBuyPreviewHeaderTitleProps } from './PredictBuyPreviewHeader';
