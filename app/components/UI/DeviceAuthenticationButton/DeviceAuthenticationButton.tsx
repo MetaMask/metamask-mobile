e88036596c7ecd94b8f54e0f96b39678facf3c5d
Cal-L <cal.leung@consensys.net>
2026-02-19 18:55:17 -0800

    Use ButtonIcon for device auth button

--- app/components/UI/DeviceAuthenticationButton/DeviceAuthenticationButton.tsx
+++ app/components/UI/DeviceAuthenticationButton/DeviceAuthenticationButton.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
-import styles from './styles';
-import Icon, {
-  IconName,
-  IconSize,
-  IconColor,
-} from '../../../component-library/components/Icons/Icon';
 import { LoginViewSelectors } from '../../Views/Login/LoginView.testIds';
+import {
+  ButtonIcon,
+  ButtonIconProps,
+} from '@metamask/design-system-react-native';
 
 type DeviceAuthenticationButtonProps = {
   hidden: boolean;
-} & TouchableOpacityProps;
+} & ButtonIconProps;
 
 const DeviceAuthenticationButton = ({
   hidden,
@@ -19,19 +16,10 @@ const DeviceAuthenticationButton = ({
   if (hidden) return null;
 
   return (
-    <TouchableOpacity
-      testID={LoginViewSelectors.BIOMETRY_BUTTON}
-      hitSlop={styles.hitSlop}
+    <ButtonIcon
       {...props}
-    >
-      <Icon
-        color={IconColor.Default}
-        style={styles.fixCenterIcon}
-        size={IconSize.Lg}
-        name={IconName.SecurityKey}
-        testID={LoginViewSelectors.DEVICE_AUTHENTICATION_ICON}
-      />
-    </TouchableOpacity>
+      testID={LoginViewSelectors.DEVICE_AUTHENTICATION_ICON}
+    />
   );
 };
 
--- app/components/UI/DeviceAuthenticationButton/index.test.tsx
+++ app/components/UI/DeviceAuthenticationButton/index.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { fireEvent, render } from '@testing-library/react-native';
 import DeviceAuthenticationButton from './DeviceAuthenticationButton';
 import { LoginViewSelectors } from '../../Views/Login/LoginView.testIds';
+import { ButtonIconSize, IconName } from '@metamask/design-system-react-native';
 
 const mockOnPress = jest.fn();
 
@@ -12,7 +13,12 @@ describe('DeviceAuthenticationButton', () => {
 
   it('hides when hidden is true', () => {
     const { toJSON } = render(
-      <DeviceAuthenticationButton onPress={mockOnPress} hidden />,
+      <DeviceAuthenticationButton
+        iconName={IconName.SecurityKey}
+        size={ButtonIconSize.Md}
+        onPress={mockOnPress}
+        hidden
+      />,
     );
 
     expect(toJSON()).toBeNull();
@@ -20,10 +26,17 @@ describe('DeviceAuthenticationButton', () => {
 
   it('renders button with device authentication icon when visible', () => {
     const { getByTestId } = render(
-      <DeviceAuthenticationButton onPress={mockOnPress} hidden={false} />,
+      <DeviceAuthenticationButton
+        iconName={IconName.SecurityKey}
+        size={ButtonIconSize.Md}
+        onPress={mockOnPress}
+        hidden={false}
+      />,
     );
 
-    expect(getByTestId(LoginViewSelectors.BIOMETRY_BUTTON)).toBeOnTheScreen();
+    expect(
+      getByTestId(LoginViewSelectors.DEVICE_AUTHENTICATION_ICON),
+    ).toBeOnTheScreen();
     expect(
       getByTestId(LoginViewSelectors.DEVICE_AUTHENTICATION_ICON),
     ).toBeOnTheScreen();
@@ -31,10 +44,15 @@ describe('DeviceAuthenticationButton', () => {
 
   it('calls onPress when pressed', () => {
     const { getByTestId } = render(
-      <DeviceAuthenticationButton onPress={mockOnPress} hidden={false} />,
+      <DeviceAuthenticationButton
+        iconName={IconName.SecurityKey}
+        size={ButtonIconSize.Md}
+        onPress={mockOnPress}
+        hidden={false}
+      />,
     );
 
-    fireEvent.press(getByTestId(LoginViewSelectors.BIOMETRY_BUTTON));
+    fireEvent.press(getByTestId(LoginViewSelectors.DEVICE_AUTHENTICATION_ICON));
     expect(mockOnPress).toHaveBeenCalledTimes(1);
   });
 });
