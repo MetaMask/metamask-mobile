aac5e50c35a068125a3d116ddd739341047355be
Prithpal Sooriya <prithpal.sooriya@consensys.net>
2026-02-20 18:22:01 +0000

    chore: remove process.env spread

--- app/util/environment.ts
+++ app/util/environment.ts
@@ -4,9 +4,7 @@
 // This needs to be updated to check for the METAMASK_ENVIRONMENT environment variable instead of NODE_ENV
 // Once this is updated, verify that e2e smoke tests are working as expected
 export const isProduction = (): boolean =>
-  // TODO: process.env.NODE_ENV === 'production' doesn't work with tests yet. Once we make it work,
-  // we can remove the following line and use the code above instead.
-  ({ ...process.env })?.METAMASK_ENVIRONMENT === 'production';
+  process.env.METAMASK_ENVIRONMENT === 'production';
 
 export const isGatorPermissionsFeatureEnabled = (): boolean =>
   process.env.GATOR_PERMISSIONS_ENABLED?.toString() === 'true';
