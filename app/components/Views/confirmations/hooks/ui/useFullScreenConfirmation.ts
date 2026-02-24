57f0b264a263ab36c9ad86caab172a1bc109302d
Matthew Grainger <matthew.grainger@consensys.net>
2026-02-24 17:10:22 -0500

    feat: fixing bugbot issue

--- app/components/Views/confirmations/hooks/ui/useFullScreenConfirmation.ts
+++ app/components/Views/confirmations/hooks/ui/useFullScreenConfirmation.ts
@@ -7,7 +7,7 @@ import { useTransactionMetadataRequest } from '../transactions/useTransactionMet
 import useApprovalRequest from '../useApprovalRequest';
 import { hasTransactionType } from '../../utils/transaction';
 import { useParams } from '../../../../../util/navigation/navUtils';
-import { ConfirmationParams } from '../../components/confirm/confirm-component';
+import type { ConfirmationParams } from '../../components/confirm/confirm-component';
 
 const getIsFullScreenConfirmation = (
   approvalRequest: ApprovalRequest<TransactionMeta> | undefined,
