4c4e64d54a8925a947e6c09a70fa2c6c12fc25de
kirillzyusko <zyusko.kirik@gmail.com>
2026-02-24 15:50:28 +0100

    fix: prettier

--- app/components/UI/Earn/components/Earnings/EarningsHistory/EarningsHistoryList/EarningsHistoryList.tsx
+++ app/components/UI/Earn/components/Earnings/EarningsHistory/EarningsHistoryList/EarningsHistoryList.tsx
@@ -8,7 +8,11 @@ import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
 import { strings } from '../../../../../../../../locales/i18n';
 import { EarningsHistoryListProps } from './EarningsHistoryList.types';
 import { EARN_EXPERIENCES } from '../../../../constants/experiences';
-import { FontWeight, Label, TextColor } from '@metamask/design-system-react-native';
+import {
+  FontWeight,
+  Label,
+  TextColor,
+} from '@metamask/design-system-react-native';
 
 const EarningsHistoryList = ({
   type,
@@ -30,18 +34,29 @@ const EarningsHistoryList = ({
           <View key={`earning-history-list-item-${index}`}>
             {isGroupHeaderVisible && (
               <View style={styles.lineItemGroupHeaderContainer}>
-                <Label color={TextColor.TextDefault} fontWeight={FontWeight.Medium}>
+                <Label
+                  color={TextColor.TextDefault}
+                  fontWeight={FontWeight.Medium}
+                >
                   {earning.groupHeader}
                 </Label>
               </View>
             )}
             <View style={styles.lineItemContainer}>
               <View style={styles.leftLineItemBox}>
-                <Label color={TextColor.TextDefault} fontWeight={FontWeight.Medium}>{earning.label}</Label>
+                <Label
+                  color={TextColor.TextDefault}
+                  fontWeight={FontWeight.Medium}
+                >
+                  {earning.label}
+                </Label>
               </View>
               <View style={styles.rightLineItemContainer}>
                 <View style={styles.rightLineItemBox}>
-                  <Label color={TextColor.SuccessDefault} fontWeight={FontWeight.Medium}>
+                  <Label
+                    color={TextColor.SuccessDefault}
+                    fontWeight={FontWeight.Medium}
+                  >
                     + {earning.amount} {earning.ticker}
                   </Label>
                 </View>
