d53c002c0a63e2e98de5a6cfd68e6cb1c3778f96
Matthew Grainger <matthew.grainger@consensys.net>
2026-02-25 12:38:32 -0500

    feat: revert useFullScreenConfirmation changes since variant path is not used anymore

--- app/components/Views/confirmations/hooks/ui/useFullScreenConfirmation.ts
+++ app/components/Views/confirmations/hooks/ui/useFullScreenConfirmation.ts
@@ -1,10 +1,7 @@
 import { ApprovalRequest } from '@metamask/approval-controller';
 import { ApprovalType } from '@metamask/controller-utils';
 import { TransactionMeta } from '@metamask/transaction-controller';
-import {
-  FORCE_BOTTOM_SHEET_BY_VARIANT,
-  FULL_SCREEN_CONFIRMATIONS,
-} from '../../constants/confirmations';
+import { FULL_SCREEN_CONFIRMATIONS } from '../../constants/confirmations';
 import { useIsInternalConfirmation } from '../transactions/useIsInternalConfirmation';
 import { useTransactionMetadataRequest } from '../transactions/useTransactionMetadataRequest';
 import useApprovalRequest from '../useApprovalRequest';
@@ -16,31 +13,23 @@ const getIsFullScreenConfirmation = (
   approvalRequest: ApprovalRequest<TransactionMeta> | undefined,
   transactionMetadata: TransactionMeta | undefined,
   isWalletInitiated: boolean,
-  variant?: string,
 ): boolean => {
   if (!isWalletInitiated) {
     return false;
+  }
+
+  if (
+    approvalRequest?.type === ApprovalType.Transaction &&
+    transactionMetadata
+  ) {
+    return hasTransactionType(transactionMetadata, FULL_SCREEN_CONFIRMATIONS);
   }
 
   if (approvalRequest?.type === ApprovalType.TransactionBatch) {
     return true;
   }
 
-  if (
-    approvalRequest?.type !== ApprovalType.Transaction ||
-    !transactionMetadata?.type
-  ) {
-    return false;
-  }
-
-  if (
-    variant &&
-    FORCE_BOTTOM_SHEET_BY_VARIANT[transactionMetadata.type]?.[variant] === true
-  ) {
-    return false;
-  }
-
-  return hasTransactionType(transactionMetadata, FULL_SCREEN_CONFIRMATIONS);
+  return false;
 };
 
 export const useFullScreenConfirmation = () => {
