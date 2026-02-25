bfc93759921b73d34ce545da37a83c8737556107
Matthew Grainger <matthew.grainger@consensys.net>
2026-02-25 18:07:19 -0500

    feat: added test coverage

--- app/components/UI/Earn/components/EarnTransactionMonitor.test.tsx
+++ app/components/UI/Earn/components/EarnTransactionMonitor.test.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import EarnTransactionMonitor from './EarnTransactionMonitor';
 import { useMusdConversionStatus } from '../hooks/useMusdConversionStatus';
+import { useMusdConversionForegroundApprovalCleanup } from '../hooks/useMusdConversionForegroundApprovalCleanup';
 import { useMerklClaimStatus } from '../hooks/useMerklClaimStatus';
 
 jest.mock('../hooks/useMusdConversionStatus');
+jest.mock('../hooks/useMusdConversionForegroundApprovalCleanup');
 jest.mock('../hooks/useMerklClaimStatus');
 
 describe('EarnTransactionMonitor', () => {
   const mockUseMusdConversionStatus = jest.mocked(useMusdConversionStatus);
+  const mockUseMusdConversionForegroundApprovalCleanup = jest.mocked(
+    useMusdConversionForegroundApprovalCleanup,
+  );
   const mockUseMerklClaimStatus = jest.mocked(useMerklClaimStatus);
 
   beforeEach(() => {
@@ -29,6 +34,14 @@ describe('EarnTransactionMonitor', () => {
     render(<EarnTransactionMonitor />);
 
     expect(mockUseMusdConversionStatus).toHaveBeenCalledTimes(1);
+  });
+
+  it('calls useMusdConversionForegroundApprovalCleanup hook', () => {
+    render(<EarnTransactionMonitor />);
+
+    expect(
+      mockUseMusdConversionForegroundApprovalCleanup,
+    ).toHaveBeenCalledTimes(1);
   });
 
   it('calls useMerklClaimStatus hook', () => {
