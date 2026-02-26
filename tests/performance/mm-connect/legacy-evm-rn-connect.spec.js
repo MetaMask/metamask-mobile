60444caa8f88626a8a25c625414067f2c5e9313d
Alex Donesky <adonesky@gmail.com>
2026-02-26 15:53:06 -0600

    cleanup

--- tests/performance/mm-connect/legacy-evm-rn-connect.spec.js
+++ tests/performance/mm-connect/legacy-evm-rn-connect.spec.js
@@ -1,4 +1,4 @@
-import { test, expect } from 'appwright';
+import { test, expect } from '../../framework/fixtures/performance';
 
 import { login } from '../../framework/utils/Flows.js';
 import RNPlaygroundDapp from '../../../wdio/screen-objects/RNPlaygroundDapp.js';
