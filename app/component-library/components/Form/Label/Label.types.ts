835a5cb45b1e396e932eceef9d9796e658077a46
kirillzyusko <zyusko.kirik@gmail.com>
2026-02-27 14:07:07 +0100

    fix: remove outdated comment

--- app/component-library/components/Form/Label/Label.types.ts
+++ app/component-library/components/Form/Label/Label.types.ts
@@ -3,6 +3,5 @@ import { TextProps } from '../../Texts/Text/Text.types';
 
 /**
  * Label component props.
- * @deprecated Use `Label` from `@metamask/design-system-react-native` instead.
  */
 export type LabelProps = TextProps;
