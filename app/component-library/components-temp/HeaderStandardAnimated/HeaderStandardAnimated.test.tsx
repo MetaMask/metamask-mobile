e06f5c5b46617a00b9b19d6d21d3afbbf38bda0b
Brian Nguyen <brianacnguyen@gmail.com>
2026-02-27 15:34:06 -0800

    Removed unused vars

--- app/component-library/components-temp/HeaderStandardAnimated/HeaderStandardAnimated.test.tsx
+++ app/component-library/components-temp/HeaderStandardAnimated/HeaderStandardAnimated.test.tsx
@@ -1,7 +1,6 @@
 // Third party dependencies.
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
-import { Text } from 'react-native';
 
 // External dependencies.
 import { IconName } from '@metamask/design-system-react-native';
