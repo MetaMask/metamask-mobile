996145d2c20e59158fa08253c4db41611d0a7039
Christopher Ferreira <christopher.ferreira@consensys.net>
2026-03-06 22:02:10 +0000

    test: fix duplicated testIds

--- app/components/UI/Identity/BackupAndSyncFeaturesToggles/BackupAndSyncFeaturesToggles.testIds.ts
+++ app/components/UI/Identity/BackupAndSyncFeaturesToggles/BackupAndSyncFeaturesToggles.testIds.ts
@@ -1,4 +1,4 @@
 export const BACKUP_AND_SYNC_FEATURES_TOGGLES_TEST_IDS = {
-  TOGGLE_ACCOUNT_SYNCING: 'toggle-contactSyncing',
+  TOGGLE_ACCOUNT_SYNCING: 'toggle-accountSyncing',
   TOGGLE_CONTACT_SYNCING: 'toggle-contactSyncing',
 } as const;
