8919526a45a45abaa71bec1d05a3482c2398bc8d
Christopher Ferreira <christopher.ferreira@consensys.net>
2026-03-06 17:10:19 +0000

    test: moves hardcoded testId strings into testIds file

---
+++ app/components/UI/Identity/BackupAndSyncFeaturesToggles/BackupAndSyncFeaturesToggles.testIds.ts
@@ -0,0 +1,3 @@
+export const BACKUP_AND_SYNC_FEATURES_TOGGLES_TEST_IDS = {
+  TOGGLE_ACCOUNT_SYNCING: 'toggle-contactSyncing',
+} as const;
---
+++ app/components/UI/Identity/BackupAndSyncToggle/BackupAndSyncToggle.testIds.ts
@@ -0,0 +1,3 @@
+export const BACKUP_AND_SYNC_TOGGLE_TEST_IDS = {
+  TOGGLE: 'toggle-backupAndSync',
+};
