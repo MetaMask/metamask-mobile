7f2bb7e68c5964dbc3064a67d4bfc645e43e5fdd
georgewrmarshall <george.marshall@consensys.net>
2026-03-05 17:16:21 -0800

    chore: eslint rc update

--- .eslintrc.js
+++ .eslintrc.js
@@ -122,7 +122,6 @@ module.exports = {
         'app/components/Snaps/**/*.{js,jsx,ts,tsx}',
         'app/components/UI/Predict/**/*.{js,jsx,ts,tsx}',
         'app/components/UI/Ramp/**/*.{js,jsx,ts,tsx}',
-        'app/components/UI/Rewards/**/*.{js,jsx,ts,tsx}',
       ],
       rules: {
         '@metamask/design-tokens/color-no-hex': 'error',
