b6ba45d3b3259a7137c3e8f86a5c9865c5b67c43
Alex Donesky <adonesky@gmail.com>
2026-03-09 13:37:11 -0500

    bump @metamask/mobile-wallet-procol-* to lastest versions and adapt breaking changes

--- app/core/SDKConnectV2/services/key-manager.ts
+++ app/core/SDKConnectV2/services/key-manager.ts
@@ -1,13 +1,17 @@
 import { IKeyManager, KeyPair } from '@metamask/mobile-wallet-protocol-core';
-import { PrivateKey, encrypt, decrypt } from 'eciesjs';
+import { PrivateKey, PublicKey, encrypt, decrypt } from 'eciesjs';
 
 export class KeyManager implements IKeyManager {
   generateKeyPair(): KeyPair {
     const privateKey = new PrivateKey();
     return {
       privateKey: new Uint8Array(privateKey.secret),
-      publicKey: privateKey.publicKey.toBytes(true),
+      publicKey: new Uint8Array(privateKey.publicKey.compressed),
     };
+  }
+
+  validatePeerKey(key: Uint8Array): void {
+    PublicKey.fromHex(Buffer.from(key).toString('hex'));
   }
 
   async encrypt(
@@ -15,7 +19,10 @@ export class KeyManager implements IKeyManager {
     theirPublicKey: Uint8Array,
   ): Promise<string> {
     const plaintextBuffer = Buffer.from(plaintext, 'utf8');
-    const encryptedBuffer = encrypt(theirPublicKey, plaintextBuffer);
+    const encryptedBuffer = encrypt(
+      Buffer.from(theirPublicKey),
+      plaintextBuffer,
+    );
     return Buffer.from(encryptedBuffer).toString('base64');
   }
 
@@ -24,7 +31,7 @@ export class KeyManager implements IKeyManager {
     myPrivateKey: Uint8Array,
   ): Promise<string> {
     const encryptedBuffer = Buffer.from(encryptedB64, 'base64');
-    const decryptedBuffer = decrypt(myPrivateKey, encryptedBuffer);
+    const decryptedBuffer = decrypt(Buffer.from(myPrivateKey), encryptedBuffer);
     return Buffer.from(decryptedBuffer).toString('utf8');
   }
 }
