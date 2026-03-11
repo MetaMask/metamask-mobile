57e8b732153e06dbfd9ee567e901717e1675fb58
Caainã Jeronimo <caainaje@gmail.com>
2026-03-11 14:31:28 -0300

    fix: remove type for now

--- app/components/UI/Predict/hooks/usePredictBalanceTokenFilter.ts
+++ app/components/UI/Predict/hooks/usePredictBalanceTokenFilter.ts
@@ -1,7 +1,6 @@
 import { BigNumber } from 'bignumber.js';
 import { useCallback } from 'react';
 import useFiatFormatter from '../../SimulationDetails/FiatDisplay/useFiatFormatter';
-import { PREDICT_DEPOSIT_AND_ORDER_TYPE } from '../../../Views/confirmations/constants/predict';
 import { useTransactionMetadataRequest } from '../../../Views/confirmations/hooks/transactions/useTransactionMetadataRequest';
 import { AssetType } from '../../../Views/confirmations/types/token';
 import { hasTransactionType } from '../../../Views/confirmations/utils/transaction';
@@ -11,6 +10,10 @@ import {
 } from '../constants/transactions';
 import { usePredictBalance } from './usePredictBalance';
 import { usePredictPaymentToken } from './usePredictPaymentToken';
+import { TransactionType } from '@metamask/transaction-controller';
+
+//TODO: Remove this once the predictDepositAndOrder type is added to the transaction controller
+const PREDICT_DEPOSIT_AND_ORDER_TYPE = 'predictDepositAndOrder';
 
 export function usePredictBalanceTokenFilter(
   forceEnabled = false,
@@ -24,7 +27,10 @@ export function usePredictBalanceTokenFilter(
     (tokens: AssetType[]): AssetType[] => {
       if (
         !forceEnabled &&
-        !hasTransactionType(transactionMeta, [PREDICT_DEPOSIT_AND_ORDER_TYPE])
+        !hasTransactionType(transactionMeta, [
+          // TODO: Remove this once the predictDepositAndOrder type is added to the transaction controller
+          PREDICT_DEPOSIT_AND_ORDER_TYPE as TransactionType,
+        ])
       ) {
         return tokens;
       }
