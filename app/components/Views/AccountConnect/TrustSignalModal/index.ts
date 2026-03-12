ad079fbdea1c0cb977be4121dfd8143551785f1d
Alex Mendonca <alex.mendonca@consensys.net>
2026-03-12 10:09:05 +0000

    remove missing variable

--- app/components/Views/AccountConnect/TrustSignalModal/index.ts
+++ app/components/Views/AccountConnect/TrustSignalModal/index.ts
@@ -3,7 +3,6 @@ export { TrustSignalModalSelectorsIDs } from './TrustSignalModal.testIds';
 export type { TrustSignalModalProps } from './TrustSignalModal.types';
 export {
   DEV_TRUST_SIGNAL_OVERRIDES,
-  TRUST_SIGNAL_VARIANT,
   useTrustSignalState,
   useTrustSignalGateControl,
 } from './useTrustSignalGate';
