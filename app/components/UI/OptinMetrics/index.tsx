b85f6fd074091a6c13976cda32a66a5a96b37ef7
Gaurav Goel <grvgoel19@gmail.com>
2026-03-17 18:33:07 +0530

    addressed cursor comments

--- app/components/UI/OptinMetrics/index.tsx
+++ app/components/UI/OptinMetrics/index.tsx
@@ -351,7 +351,7 @@ const OptinMetrics = () => {
   return (
     <SafeAreaView edges={{ bottom: 'additive' }} style={rootStyle}>
       <ScrollView
-        style={tw.style('flex-1 bg-default')}
+        style={rootStyle}
         scrollEventThrottle={150}
         onContentSizeChange={onContentSizeChange}
         onLayout={onLayout}
