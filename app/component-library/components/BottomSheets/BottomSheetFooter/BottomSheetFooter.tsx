3c5984452cb76db4f4c9063fa5995b9c33a2d1d1
sallem-consensys <sallem.ahmed@consensys.net>
2026-03-26 17:08:35 -0400

    undo bottomsheetfooter fallback

--- app/component-library/components/BottomSheets/BottomSheetFooter/BottomSheetFooter.tsx
+++ app/component-library/components/BottomSheets/BottomSheetFooter/BottomSheetFooter.tsx
@@ -33,20 +33,18 @@ const BottomSheetFooter: React.FC<BottomSheetFooterProps> = ({
 
   return (
     <View style={styles.base} testID={TESTID_BOTTOMSHEETFOOTER}>
-      {buttonPropsArray.map((buttonProp, index) => {
-        const fallbackTestId =
-          index > 0
-            ? TESTID_BOTTOMSHEETFOOTER_BUTTON_SUBSEQUENT
-            : TESTID_BOTTOMSHEETFOOTER_BUTTON;
-        return (
-          <Button
-            key={index}
-            style={index > 0 ? styles.subsequentButton : styles.button}
-            {...buttonProp}
-            testID={buttonProp.testID ?? fallbackTestId}
-          />
-        );
-      })}
+      {buttonPropsArray.map((buttonProp, index) => (
+        <Button
+          key={index}
+          style={index > 0 ? styles.subsequentButton : styles.button}
+          testID={
+            index > 0
+              ? TESTID_BOTTOMSHEETFOOTER_BUTTON_SUBSEQUENT
+              : TESTID_BOTTOMSHEETFOOTER_BUTTON
+          }
+          {...buttonProp}
+        />
+      ))}
     </View>
   );
 };
