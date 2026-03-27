a8f130b1a01d04f5681190fdb49f7fb04d90ffda
Jyoti Puri <jyotipuri@gmail.com>
2026-03-27 20:19:13 +0530

    update

--- app/components/Views/confirmations/components/info/money-account-deposit-info/money-account-deposit-info.tsx
+++ app/components/Views/confirmations/components/info/money-account-deposit-info/money-account-deposit-info.tsx
@@ -3,8 +3,10 @@ import useNavbar from '../../../hooks/ui/useNavbar';
 import { CustomAmountInfo } from '../custom-amount-info';
 import { strings } from '../../../../../../../locales/i18n';
 
+export const MONEY_ACCOUNT_CURRENCY = 'usd';
+
 export function MoneyAccountDepositInfo() {
   useNavbar(strings('confirm.title.money_account_deposit'));
 
-  return <CustomAmountInfo />;
+  return <CustomAmountInfo currency={MONEY_ACCOUNT_CURRENCY} />;
 }
