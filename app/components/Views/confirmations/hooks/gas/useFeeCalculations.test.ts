0e0cfca5fab4c0b11c318343448ef485be439f46
Maxime OUAIRY <maxime.ouairy-ext@consensys.net>
2026-03-27 17:14:54 +0100

    fix: ensure Send max is full balance if gas sponsored

--- app/components/Views/confirmations/hooks/gas/useFeeCalculations.test.ts
+++ app/components/Views/confirmations/hooks/gas/useFeeCalculations.test.ts
@@ -54,6 +54,25 @@ describe('useFeeCalculations', () => {
     expect(result.current.calculateGasEstimate).toBeDefined();
   });
 
+  it('returns zero fee when gas is sponsored', () => {
+    const { result } = renderHookWithProvider(
+      () =>
+        useFeeCalculations({
+          ...transactionMeta,
+          isGasFeeSponsored: true,
+        }),
+      {
+        state: stakingDepositConfirmationState,
+      },
+    );
+
+    expect(result.current.estimatedFeeFiat).toBe('$0');
+    expect(result.current.estimatedFeeNative).toBe('0');
+    expect(result.current.estimatedFeeFiatPrecise).toBe('0');
+    expect(result.current.preciseNativeFeeInHex).toBe('0x0');
+    expect(result.current.calculateGasEstimate).toBeDefined();
+  });
+
   it('returns fee calculations but hides fiat on testnets when showFiatOnTestnets is false', () => {
     mockIsTestNet.mockReturnValue(true);
     const clonedStakingDepositConfirmationState = cloneDeep(
