e93244cd3c6ed4547ff8ddc7ef23c29ac6eb29f9
Jyoti Puri <jyotipuri@gmail.com>
2026-03-30 18:50:12 +0530

    feat: add recipient account selection option on money account deposit page

--- app/components/Views/confirmations/components/developer/confirmations-developer-options/confirmations-developer-options.tsx
+++ app/components/Views/confirmations/components/developer/confirmations-developer-options/confirmations-developer-options.tsx
@@ -145,6 +145,7 @@ function MoneyAccountDeposit() {
     addTransactionBatchAndNavigate({
       loader: ConfirmationLoader.CustomAmount,
       transactionType: TransactionType.moneyAccountDeposit,
+      recipient: PROXY_ADDRESS,
     });
   }, [addTransactionBatchAndNavigate]);
 
@@ -201,10 +202,12 @@ function useAddTransactionBatch() {
       headerShown,
       loader,
       transactionType,
+      recipient = POLYGON_USDCE_ADDRESS,
     }: {
       headerShown?: boolean;
       loader?: ConfirmationLoader;
       transactionType: TransactionType;
+      recipient?: Hex;
     }) => {
       navigateToConfirmation({
         headerShown,
@@ -227,7 +230,7 @@ function useAddTransactionBatch() {
           },
           {
             params: {
-              to: POLYGON_USDCE_ADDRESS,
+              to: recipient,
               data: transferData,
             },
             type: transactionType,
