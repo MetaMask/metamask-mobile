e2fb8f2a487d3500e2426a262d481f796775685e
IF <139582705+infiniteflower@users.noreply.github.com>
2026-03-30 20:49:00 -0400

    chore: clean up

--- app/components/UI/Bridge/Views/BridgeView/BridgeView.test.tsx
+++ app/components/UI/Bridge/Views/BridgeView/BridgeView.test.tsx
@@ -1296,8 +1296,6 @@ describe('BridgeView', () => {
     });
   });
 
-  // TODO: This test suite is temporary and will be replaced by another behavior once geolocation detection will be implemented.
-
   describe('deep link parameter handling', () => {
     const mockDeepLinkSourceToken = {
       address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
