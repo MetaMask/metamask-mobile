da37ac3b4a24b21e5d26905b6a9c8d4a7eaafd6d
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2026-03-31 13:53:07 +0200

    Remove more unused types

--- app/core/Engine/controllers/multichain-router/constants.ts
+++
@@ -1,9 +0,0 @@
-import {
-  MultichainRoutingServiceGetSupportedAccountsAction as MultichainRoutingServiceGetSupportedAccountsActionType,
-  MultichainRoutingServiceIsSupportedScopeAction as MultichainRoutingServiceIsSupportedScopeActionType,
-} from '@metamask/snaps-controllers';
-
-export const MultichainRoutingServiceIsSupportedScopeAction: MultichainRoutingServiceIsSupportedScopeActionType['type'] =
-  'MultichainRoutingService:isSupportedScope';
-export const MultichainRoutingServiceGetSupportedAccountsAction: MultichainRoutingServiceGetSupportedAccountsActionType['type'] =
-  'MultichainRoutingService:getSupportedAccounts';
