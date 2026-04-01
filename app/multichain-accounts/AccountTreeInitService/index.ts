1a44c8eacc098c291dc6456b53a45b197917aada
Charly Chevalier <charly.chevalier@consensys.net>
2026-04-01 19:14:00 +0200

    feat: init MoneyAccountController

--- app/multichain-accounts/AccountTreeInitService/index.ts
+++ app/multichain-accounts/AccountTreeInitService/index.ts
@@ -3,10 +3,19 @@ import { forwardSelectedAccountGroupToSnapKeyring } from '../../core/SnapKeyring
 
 export class AccountTreeInitService {
   initializeAccountTree = async (): Promise<void> => {
-    const { AccountTreeController, AccountsController } = Engine.context;
+    const {
+      AccountTreeController,
+      AccountsController,
+      MoneyAccountController,
+    } = Engine.context;
 
     await AccountsController.updateAccounts();
+
     AccountTreeController.init();
+
+    // Money accounts are not part of the account-tree nor the accounts controller, so
+    // we need to initialize them separately.
+    await MoneyAccountController.init();
 
     // Forward initial selected accounts.
     await forwardSelectedAccountGroupToSnapKeyring(
