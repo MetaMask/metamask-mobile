58e40bbf9f0f3acaa27ce0417b09df2735375c9f
Tyler Chong <tyler.chong@consensys.net>
2026-04-07 11:41:49 +0800

    refactor(analytics): use identify for social login traits

--- app/components/Views/Onboarding/index.tsx
+++ app/components/Views/Onboarding/index.tsx
@@ -458,7 +458,7 @@ const Onboarding = () => {
       track(MetaMetricsEvents.SOCIAL_LOGIN_COMPLETED, {
         account_type: accountType,
       });
-      metrics.addTraitsToUser({
+      metrics.identify({
         login_method: 'social',
         social_provider: provider,
       });
