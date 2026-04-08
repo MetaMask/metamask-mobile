1ddc3aba49f0fe2914f28e03fa29a09ba1db045b
kevin <kevin.andres@epitech.eu>
2026-04-08 17:23:18 +0200

    prettier
    Signed-off-by: kevin <kevin.andres@epitech.eu>

--- app/components/UI/Rewards/components/Benefits/TheMiracleFooter.tsx
+++ app/components/UI/Rewards/components/Benefits/TheMiracleFooter.tsx
@@ -9,12 +9,12 @@ import TheMiracleLogo from 'images/benefits/themiracle-logo.svg';
 import { strings } from '../../../../../../locales/i18n';
 
 const TheMiracleFooter = () => (
-    <Box twClassName="flex flex-row gap-1 items-center justify-center">
-      <Text variant={TextVariant.BodyXs} color={TextColor.TextAlternative}>
-        {strings('rewards.benefits.powered_by')}
-      </Text>
-      <TheMiracleLogo name="TheMiracleLogo" width={90} height={26} />
-    </Box>
-  );
+  <Box twClassName="flex flex-row gap-1 items-center justify-center">
+    <Text variant={TextVariant.BodyXs} color={TextColor.TextAlternative}>
+      {strings('rewards.benefits.powered_by')}
+    </Text>
+    <TheMiracleLogo name="TheMiracleLogo" width={90} height={26} />
+  </Box>
+);
 
 export default TheMiracleFooter;
