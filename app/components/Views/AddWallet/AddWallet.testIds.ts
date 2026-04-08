144fa6d17672297418fdac0b3675b4f77dd9eaf4
Monte Lai <monte.lai@consensys.net>
2026-04-08 18:54:03 +0800

    fix:test ids

--- app/components/Views/AddWallet/AddWallet.testIds.ts
+++ app/components/Views/AddWallet/AddWallet.testIds.ts
@@ -1,7 +1,7 @@
 export const AddWalletTestIds = {
   SCREEN: 'add-wallet-screen',
   BACK_BUTTON: 'add-wallet-back-button',
-  IMPORT_WALLET_BUTTON: 'add-wallet-import-wallet-button',
-  IMPORT_ACCOUNT_BUTTON: 'add-wallet-import-account-button',
+  IMPORT_WALLET_BUTTON: 'add-account-srp-account',
+  IMPORT_ACCOUNT_BUTTON: 'add-account-import-account',
   CONNECT_HARDWARE_BUTTON: 'add-wallet-connect-hardware-button',
 } as const;
