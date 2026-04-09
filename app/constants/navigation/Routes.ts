c10d6a3cc512764755aafc99fe3510f2a192741c
Bigshmow <devin.stewart@consensys.net>
2026-04-08 20:02:12 -0600

    chore: add TraderProfileView route and i18n strings

--- app/constants/navigation/Routes.ts
+++ app/constants/navigation/Routes.ts
@@ -341,6 +341,7 @@ const Routes = {
   SOCIAL_LEADERBOARD: {
     ROOT: 'SocialLeaderboard',
     VIEW: 'TopTradersView',
+    PROFILE: 'TraderProfileView',
   },
   PREDICT: {
     ROOT: 'Predict',
