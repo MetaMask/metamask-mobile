0ee1127b38f8dcf97bbf677726ee076e02213522
Curtis David <Curtis.David7@gmail.com>
2026-04-09 14:35:49 -0400

    remove unused snapshot

--- app/components/Views/LockScreen/index.test.tsx
+++ app/components/Views/LockScreen/index.test.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react-native';
-import { ActivityIndicator } from 'react-native';
 import LockScreen from './';
+import { FoxLoaderSelectorsIDs } from '../../UI/FoxLoader/FoxLoader.testIds';
 
 describe('LockScreen', () => {
   it('should render correctly', () => {
     render(<LockScreen />);
-    expect(screen.UNSAFE_getByType(ActivityIndicator)).toBeTruthy();
+    expect(
+      screen.getByTestId(FoxLoaderSelectorsIDs.CONTAINER),
+    ).toBeOnTheScreen();
   });
 });
