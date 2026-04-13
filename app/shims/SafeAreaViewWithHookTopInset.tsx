5d6f8ce4d77da805f1d29b493d7e7100cc8a0da2
Wei Sun <wei.sun@consensys.net>
2026-04-13 14:44:45 -0700

    update snapshots

--- app/shims/SafeAreaViewWithHookTopInset.tsx
+++ app/shims/SafeAreaViewWithHookTopInset.tsx
@@ -46,23 +46,23 @@ export const SafeAreaView = forwardRef<
       edges == null
         ? { ...defaultEdges }
         : (() => {
-          const edgesObj: EdgeRecord = Array.isArray(edges)
-            ? (edges as readonly Edge[]).reduce<EdgeRecord>(
-              (acc, edge: Edge) => {
-                acc[edge] = 'additive';
-                return acc;
-              },
-              {},
-            )
-            : (edges as EdgeRecord);
+            const edgesObj: EdgeRecord = Array.isArray(edges)
+              ? (edges as readonly Edge[]).reduce<EdgeRecord>(
+                  (acc, edge: Edge) => {
+                    acc[edge] = 'additive';
+                    return acc;
+                  },
+                  {},
+                )
+              : (edges as EdgeRecord);
 
-          return {
-            top: edgesObj.top ?? 'off',
-            right: edgesObj.right ?? 'off',
-            bottom: edgesObj.bottom ?? 'off',
-            left: edgesObj.left ?? 'off',
-          };
-        })();
+            return {
+              top: edgesObj.top ?? 'off',
+              right: edgesObj.right ?? 'off',
+              bottom: edgesObj.bottom ?? 'off',
+              left: edgesObj.left ?? 'off',
+            };
+          })();
 
     // Defers top inset to be applied via styles instead
     const topMode = nativeEdgesInternal.top;
