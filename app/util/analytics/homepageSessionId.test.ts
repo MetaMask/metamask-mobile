3fa0ec3c5058bdf34746bda2c43db627452335ec
vinnyhoward <vincenguyenhoward@gmail.com>
2026-04-13 09:54:43 -0700

    fix: remove redudant test

--- app/util/analytics/homepageSessionId.test.ts
+++ app/util/analytics/homepageSessionId.test.ts
@@ -4,11 +4,6 @@ const UUID_V4_REGEX =
   /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 describe('HOMEPAGE_APP_SESSION_ID', () => {
-  it('is a non-empty string', () => {
-    expect(typeof HOMEPAGE_APP_SESSION_ID).toBe('string');
-    expect(HOMEPAGE_APP_SESSION_ID.length).toBeGreaterThan(0);
-  });
-
   it('is a valid UUID v4', () => {
     expect(HOMEPAGE_APP_SESSION_ID).toMatch(UUID_V4_REGEX);
   });
