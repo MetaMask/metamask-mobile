d129222cf144c5170483dc21a3025ffbfc6555fc
saraantole <sara.antole@gmail.com>
2026-04-13 16:48:02 +0200

    fix: import

--- app/components/UI/Rewards/components/Onboarding/constants.ts
+++ app/components/UI/Rewards/components/Onboarding/constants.ts
@@ -1,3 +1,3 @@
-export { REWARDS_LEARN_MORE_URL as REWARDS_ONBOARD_OPTIN_LEGAL_LEARN_MORE_URL } from '../../../../../../constants/urls';
+export { REWARDS_LEARN_MORE_URL as REWARDS_ONBOARD_OPTIN_LEGAL_LEARN_MORE_URL } from '../../../../../constants/urls';
 
 export const REWARDS_ONBOARD_TERMS_URL = 'https://go.metamask.io/rewards-terms';
