2c6262e6f7551fa20194b8f6d673efb7125ef320
Jyoti Puri <jyotipuri@gmail.com>
2026-04-13 21:38:53 +0530

    update

--- app/components/UI/Money/selectors/featureFlags.ts
+++ app/components/UI/Money/selectors/featureFlags.ts
@@ -8,14 +8,13 @@ import { isMoneyAccountEnabled } from '../../../../lib/Money/feature-flags';
 
 export const selectMoneyHomeScreenEnabledFlag = createSelector(
   selectRemoteFeatureFlags,
-  (remoteFeatureFlags) => 
-    // const localFlag = process.env.MM_MONEY_HOME_SCREEN_ENABLED === 'true';
-    // const remoteFlag =
-    //   remoteFeatureFlags?.moneyHomeScreenEnabled as unknown as VersionGatedFeatureFlag;
+  (remoteFeatureFlags) => {
+    const localFlag = process.env.MM_MONEY_HOME_SCREEN_ENABLED === 'true';
+    const remoteFlag =
+      remoteFeatureFlags?.moneyHomeScreenEnabled as unknown as VersionGatedFeatureFlag;
 
-    // return validatedVersionGatedFeatureFlag(remoteFlag) ?? localFlag;
-     true
-  ,
+    return validatedVersionGatedFeatureFlag(remoteFlag) ?? localFlag;
+  },
 );
 
 /** Temporary flag: remote value is a boolean only. */
