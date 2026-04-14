a20139683cf89149be819ba3304ae763e7a15efd
georgewrmarshall <george.marshall@consensys.net>
2026-04-14 15:04:16 -0700

    fix: add @metamask-previews scope to Jest transform and fix IconName.Question
    - Update transformIgnorePatterns to handle @metamask-previews/* packages
    - Use LegacyIconName.Question for ModalType.Confirmation in RewardsBottomSheetModal

--- app/components/UI/Rewards/components/RewardsBottomSheetModal.tsx
+++ app/components/UI/Rewards/components/RewardsBottomSheetModal.tsx
@@ -22,6 +22,7 @@ import {
 import BottomSheet, {
   BottomSheetRef,
 } from '../../../../component-library/components/BottomSheets/BottomSheet';
+import { IconName as LegacyIconName } from '../../../../component-library/components/Icons/Icon/Icon.types';
 
 export enum ModalType {
   Danger = 'danger',
@@ -129,7 +130,7 @@ const RewardsBottomSheetModal = ({ route }: RewardsBottomSheetModalProps) => {
     }
 
     // Default icon handling
-    let iconName = IconName.Danger;
+    let iconName: IconName = IconName.Danger;
     let iconStyle = 'text-warning-default';
 
     switch (type) {
@@ -137,7 +138,7 @@ const RewardsBottomSheetModal = ({ route }: RewardsBottomSheetModalProps) => {
         iconName = IconName.Danger;
         break;
       case ModalType.Confirmation:
-        iconName = IconName.Question;
+        iconName = LegacyIconName.Question as unknown as IconName;
         iconStyle = 'text-primary-default';
         break;
     }
