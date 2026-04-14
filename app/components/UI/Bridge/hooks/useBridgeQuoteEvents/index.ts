de1ee0e082f48b11cc959cf44edae695b38e67d3
rarquevaux <r.arquevaux@gmail.com>
2026-04-14 11:01:53 -0700

    feat(STX-473): add hook useTokenBalanceInUsd

--- app/components/UI/Bridge/hooks/useBridgeQuoteEvents/index.ts
+++ app/components/UI/Bridge/hooks/useBridgeQuoteEvents/index.ts
@@ -8,26 +8,10 @@ import { useEffect, useMemo } from 'react';
 import Engine from '../../../../../core/Engine';
 import {
   getQuotesReceivedProperties,
-  isNonEvmChainId,
   QuoteWarning,
   UnifiedSwapBridgeEventName,
 } from '@metamask/bridge-controller';
-import { Hex, isCaipChainId } from '@metamask/utils';
-import { selectCurrencyRates } from '../../../../../selectors/currencyRateController';
-import { selectTokenMarketData } from '../../../../../selectors/tokenRatesController';
-import { selectNetworkConfigurations } from '../../../../../selectors/networkController';
-import {
-  selectMultichainAssetsRates,
-  selectMultichainBalances,
-} from '../../../../../selectors/multichain';
-import { selectSelectedInternalAccountAddress } from '../../../../../selectors/accountsController';
-import { selectSelectedInternalAccountByScope } from '../../../../../selectors/multichainAccounts/accounts';
-import { selectTokensBalances } from '../../../../../selectors/tokenBalancesController';
-import { fromTokenMinimalUnitString } from '../../../../../util/number';
-import {
-  calcTokenFiatValue,
-  calcUsdAmountFromFiat,
-} from '../../utils/exchange-rates';
+import { useTokenBalanceInUsd } from '../useTokenBalanceInUsd';
 
 /**
  * Hook for publishing the QuotesReceived event.
@@ -55,75 +39,7 @@ export const useBridgeQuoteEvents = ({
     useSelector(selectBridgeQuotes);
 
   const sourceToken = useSelector(selectSourceToken);
-  const evmMultiChainMarketData = useSelector(selectTokenMarketData);
-  const evmMultiChainCurrencyRates = useSelector(selectCurrencyRates);
-  const networkConfigurationsByChainId = useSelector(
-    selectNetworkConfigurations,
-  );
-  const nonEvmMultichainAssetRates = useSelector(selectMultichainAssetsRates);
-
-  const getAccountByScope = useSelector(selectSelectedInternalAccountByScope);
-  const selectedAccountAddress = useSelector(
-    selectSelectedInternalAccountAddress,
-  );
-  const tokenBalances = useSelector(selectTokensBalances);
-  const multichainBalances = useSelector(selectMultichainBalances);
-
-  const resolvedBalance = useMemo(() => {
-    if (!sourceToken) return undefined;
-
-    if (
-      isCaipChainId(sourceToken.chainId) &&
-      isNonEvmChainId(sourceToken.chainId)
-    ) {
-      const scopedAccount = getAccountByScope(sourceToken.chainId);
-      if (!scopedAccount) return undefined;
-      return multichainBalances?.[scopedAccount.id]?.[sourceToken.address]
-        ?.amount;
-    }
-
-    const hexBalance =
-      tokenBalances?.[selectedAccountAddress as Hex]?.[
-        sourceToken.chainId as Hex
-      ]?.[sourceToken.address as Hex];
-    if (!hexBalance) return undefined;
-    try {
-      return fromTokenMinimalUnitString(hexBalance, sourceToken.decimals);
-    } catch {
-      return undefined;
-    }
-  }, [
-    sourceToken,
-    tokenBalances,
-    selectedAccountAddress,
-    multichainBalances,
-    getAccountByScope,
-  ]);
-
-  const fromTokenBalanceInUsd = useMemo(() => {
-    const balanceFiat = calcTokenFiatValue({
-      token: sourceToken ?? undefined,
-      amount: resolvedBalance,
-      evmMultiChainMarketData,
-      networkConfigurationsByChainId,
-      evmMultiChainCurrencyRates,
-      nonEvmMultichainAssetRates,
-    });
-    if (!balanceFiat) return undefined;
-    return calcUsdAmountFromFiat({
-      tokenFiatValue: balanceFiat,
-      chainId: sourceToken?.chainId,
-      networkConfigurationsByChainId,
-      evmMultiChainCurrencyRates,
-    });
-  }, [
-    resolvedBalance,
-    sourceToken,
-    evmMultiChainMarketData,
-    networkConfigurationsByChainId,
-    evmMultiChainCurrencyRates,
-    nonEvmMultichainAssetRates,
-  ]);
+  const fromTokenBalanceInUsd = useTokenBalanceInUsd(sourceToken ?? undefined);
 
   const warnings = useMemo(() => {
     const latestWarnings: QuoteWarning[] = [];
