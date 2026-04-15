4a65d6e38bbf3e3b460dfb372c5f4307b2add1a7
Patryk Lucka <5708018+PatrykLucka@users.noreply.github.com>
2026-04-15 13:08:47 +0200

    feat: implement wallet home onboarding steps functionality

--- app/components/Views/Wallet/index.tsx
+++ app/components/Views/Wallet/index.tsx
@@ -590,6 +590,10 @@ const Wallet = ({
   const isMountedRef = useRef(true);
   const refreshInProgressRef = useRef(false);
   const [refreshing, setRefreshing] = useState(false);
+  const [
+    hideWalletMainActionsForPostOnboardingSteps,
+    setHideWalletMainActionsForPostOnboardingSteps,
+  ] = useState(false);
   const { refreshBalance } = useBalanceRefresh();
   const theme = useTheme();
 
@@ -685,6 +689,13 @@ const Wallet = ({
 
   const displayBuyButton = true;
   const displaySwapsButton = AppConstants.SWAPS.ACTIVE;
+
+  const handlePostOnboardingStepsSurfaceActiveChange = useCallback(
+    (active: boolean) => {
+      setHideWalletMainActionsForPostOnboardingSteps(active);
+    },
+    [],
+  );
 
   const onReceive = useCallback(() => {
     trackActionButtonClick(trackEvent, createEventBuilder, {
@@ -1324,19 +1335,25 @@ const Wallet = ({
         <NetworkConnectionBanner />
       </View>
       <>
-        <AccountGroupBalance />
-
-        <AssetDetailsActions
-          displayBuyButton={displayBuyButton}
-          displaySwapsButton={displaySwapsButton}
-          goToSwaps={goToSwaps}
-          onReceive={onReceive}
-          onSend={onSend}
-          buyButtonActionID={WalletViewSelectorsIDs.WALLET_BUY_BUTTON}
-          swapButtonActionID={WalletViewSelectorsIDs.WALLET_SWAP_BUTTON}
-          sendButtonActionID={WalletViewSelectorsIDs.WALLET_SEND_BUTTON}
-          receiveButtonActionID={WalletViewSelectorsIDs.WALLET_RECEIVE_BUTTON}
+        <AccountGroupBalance
+          onPostOnboardingStepsSurfaceActiveChange={
+            handlePostOnboardingStepsSurfaceActiveChange
+          }
         />
+
+        {!hideWalletMainActionsForPostOnboardingSteps ? (
+          <AssetDetailsActions
+            displayBuyButton={displayBuyButton}
+            displaySwapsButton={displaySwapsButton}
+            goToSwaps={goToSwaps}
+            onReceive={onReceive}
+            onSend={onSend}
+            buyButtonActionID={WalletViewSelectorsIDs.WALLET_BUY_BUTTON}
+            swapButtonActionID={WalletViewSelectorsIDs.WALLET_SWAP_BUTTON}
+            sendButtonActionID={WalletViewSelectorsIDs.WALLET_SEND_BUTTON}
+            receiveButtonActionID={WalletViewSelectorsIDs.WALLET_RECEIVE_BUTTON}
+          />
+        ) : null}
 
         {isCarouselBannersEnabled && <Carousel style={styles.carousel} />}
 
