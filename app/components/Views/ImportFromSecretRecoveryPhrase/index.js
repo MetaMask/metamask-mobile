f7eb2d0256fd51aa868f60298c9f65582f91f933
kirillzyusko <zyusko.kirik@gmail.com>
2026-04-15 16:17:31 +0200

    feat: migrate `TextField` component (web3auth)

--- app/components/Views/ImportFromSecretRecoveryPhrase/index.js
+++ app/components/Views/ImportFromSecretRecoveryPhrase/index.js
@@ -60,6 +60,7 @@ import {
   Text,
   TextColor,
   TextVariant,
+  TextField,
 } from '@metamask/design-system-react-native';
 import { useTailwind } from '@metamask/design-system-twrnc-preset';
 import { Authentication } from '../../../core';
@@ -82,7 +83,6 @@ import Icon, {
 } from '../../../component-library/components/Icons/Icon';
 import { ToastContext } from '../../../component-library/components/Toast/Toast.context';
 import { ToastVariants } from '../../../component-library/components/Toast/Toast.types';
-import TextField from '../../../component-library/components/Form/TextField/TextField';
 import { CommonActions } from '@react-navigation/native';
 import { SRP_LENGTHS, SPACE_CHAR, PASSCODE_NOT_SET_ERROR } from './constant';
 import { useAnalytics } from '../../hooks/useAnalytics/useAnalytics';
