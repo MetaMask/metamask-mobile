c366553829bd85ff9f4b4cf421c7e44501ffe17e
vinnyhoward <vincenguyenhoward@gmail.com>
2026-04-16 13:08:45 -0600

    chore: update entrypoint event for predict homescreen flow

--- app/components/UI/Predict/types/navigation.ts
+++ app/components/UI/Predict/types/navigation.ts
@@ -26,7 +26,8 @@ export type PredictEntryPoint =
   | typeof PredictEventValues.ENTRY_POINT.MAIN_TRADE_BUTTON
   | typeof PredictEventValues.ENTRY_POINT.BACKGROUND
   | typeof PredictEventValues.ENTRY_POINT.TRENDING_SEARCH
-  | typeof PredictEventValues.ENTRY_POINT.TRENDING;
+  | typeof PredictEventValues.ENTRY_POINT.TRENDING
+  | typeof PredictEventValues.ENTRY_POINT.HOME_SECTION;
 
 /** Predict market list parameters */
 export interface PredictMarketListParams {
