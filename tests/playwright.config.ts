d923cb5005ae29e89127b5aac36badf479bde93b
Christopher Ferreira <christopher.ferreira@consensys.net>
2026-04-17 19:52:58 +0100

    test: revert unwanted changes and remove non used projects

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -130,41 +130,8 @@ export default defineConfig({
       },
     },
     {
-      name: 'mm-connect-ios-browserstack',
-      testMatch: '**/performance/mm-connect/**/*.spec.ts',
-      timeout: 12 * 60 * 1000, // BrowserStack session creation (~1min) counts against test time until driver fixture is worker-scoped
-      use: {
-        platform: Platform.IOS,
-        device: {
-          provider: 'browserstack',
-          name: process.env.BROWSERSTACK_DEVICE || 'iPhone 14 Pro Max',
-          osVersion: process.env.BROWSERSTACK_OS_VERSION || '16.0',
-        },
-        app: {
-          appId: 'io.metamask.MetaMask',
-        },
-        buildPath: 'bs://a0ea40650b0a1108e32b27ec93ac73af3b393855', // Just a demo, CI will take care of this
-      },
-    },
-    {
-      name: 'mm-connect-ios-local',
-      testMatch: '**/performance/mm-connect/**/*.spec.ts',
-      timeout: 12 * 60 * 1000,
-      use: {
-        platform: Platform.IOS,
-        device: {
-          provider: 'emulator',
-          osVersion: '16.0', // this can be changed to your simulator version
-        },
-        app: {
-          appId: 'io.metamask.MetaMask',
-        },
-        buildPath: 'PATH-TO-BUILD', // Path to your .app file
-      },
-    },
-    {
       name: 'mm-connect-android-browserstack',
-      testMatch: 'tests/performance/mm-connect/multichain-rn-solana.spec.ts',
+      testMatch: '**/performance/mm-connect/**/*.spec.ts',
       timeout: 12 * 60 * 1000,
       use: {
         platform: Platform.ANDROID,
@@ -181,7 +148,7 @@ export default defineConfig({
           packageName: 'io.metamask',
           launchableActivity: 'io.metamask.MainActivity',
         },
-        buildPath: 'bs://b539a5ed79b77fca9978253852afc7c5dc337e45', // Path to Browserstack url
+        buildPath: process.env.BROWSERSTACK_ANDROID_APP_URL, // Path to Browserstack url
       },
     },
     {
