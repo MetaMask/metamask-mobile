6711b41412a14ab06a47e407dafd92ee81495032
Christopher Ferreira <christopher.ferreira@consensys.net>
2026-04-20 15:35:33 +0100

    test: remove event that is not fired on the tested flow

--- tests/helpers/analytics/expectations/predict-claim-positions.analytics.ts
+++ tests/helpers/analytics/expectations/predict-claim-positions.analytics.ts
@@ -1,16 +1,11 @@
 import type { AnalyticsExpectations } from '../../../framework';
 
-const POSITION_VIEWED = 'Predict Position Viewed';
 const ACTIVITY_VIEWED = 'Predict Activity Viewed';
 
 export const predictClaimPositionsAnalyticsExpectations: AnalyticsExpectations =
   {
-    eventNames: [POSITION_VIEWED, ACTIVITY_VIEWED],
+    eventNames: [ACTIVITY_VIEWED],
     events: [
-      {
-        name: POSITION_VIEWED,
-        requiredDefinedPropertyKeys: ['open_positions_count'],
-      },
       {
         name: ACTIVITY_VIEWED,
         requiredDefinedPropertyKeys: ['activity_type'],
