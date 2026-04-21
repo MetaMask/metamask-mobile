0abb6f608ffcf90af6eaf21906575adce4558a1c
Priya Narayanaswamy <priya.narayanaswamy@consensys.net>
2026-04-21 17:39:23 +0200

    test:revert address review

--- tests/selectors/Earn/EarnLendingView.selectors.ts
+++ tests/selectors/Earn/EarnLendingView.selectors.ts
@@ -1,22 +1,16 @@
 import enContent from '../../../locales/languages/en.json';
-import { EARN_LENDING_BALANCE_TEST_IDS } from '../../../app/components/UI/Earn/components/EarnLendingBalance';
-import {
-  CONFIRMATION_FOOTER_TEST_ID,
-  CONFIRMATION_FOOTER_BUTTON_TEST_IDS,
-} from '../../../app/components/UI/Earn/Views/EarnLendingDepositConfirmationView/components/ConfirmationFooter';
-import { DEPOSIT_DETAILS_SECTION_TEST_ID } from '../../../app/components/UI/Earn/Views/EarnLendingDepositConfirmationView/components/DepositInfoSection';
-import { DEPOSIT_RECEIVE_SECTION_TEST_ID } from '../../../app/components/UI/Earn/Views/EarnLendingDepositConfirmationView/components/DepositReceiveSection';
-import { PROGRESS_STEPPER_TEST_IDS } from '../../../app/components/UI/Earn/Views/EarnLendingDepositConfirmationView/components/ProgressStepper';
+
+const CONFIRMATION_FOOTER_PREFIX = 'earn-lending-confirmation-footer';
 
 export const EarnLendingViewSelectorsIDs = {
-  WITHDRAW_BUTTON: EARN_LENDING_BALANCE_TEST_IDS.WITHDRAW_BUTTON,
-  DEPOSIT_BUTTON: EARN_LENDING_BALANCE_TEST_IDS.DEPOSIT_BUTTON,
-  CONFIRMATION_FOOTER: CONFIRMATION_FOOTER_TEST_ID,
-  CONFIRM_BUTTON: CONFIRMATION_FOOTER_BUTTON_TEST_IDS.CONFIRM_BUTTON,
-  CANCEL_BUTTON: CONFIRMATION_FOOTER_BUTTON_TEST_IDS.CANCEL_BUTTON,
-  DEPOSIT_INFO_SECTION: DEPOSIT_DETAILS_SECTION_TEST_ID,
-  DEPOSIT_RECEIVE_SECTION: DEPOSIT_RECEIVE_SECTION_TEST_ID,
-  PROGRESS_BAR: PROGRESS_STEPPER_TEST_IDS.PROGRESS_BAR,
+  WITHDRAW_BUTTON: 'withdraw-button',
+  DEPOSIT_BUTTON: 'deposit-button',
+  CONFIRMATION_FOOTER: CONFIRMATION_FOOTER_PREFIX,
+  CONFIRM_BUTTON: `${CONFIRMATION_FOOTER_PREFIX}-confirm-button`,
+  CANCEL_BUTTON: `${CONFIRMATION_FOOTER_PREFIX}-cancel-button`,
+  DEPOSIT_INFO_SECTION: 'depositDetailsSection',
+  DEPOSIT_RECEIVE_SECTION: 'depositReceiveSection',
+  PROGRESS_BAR: 'progress-stepper-progress-bar',
   REVIEW_BUTTON: 'review-button',
 };
 
