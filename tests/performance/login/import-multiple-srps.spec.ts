a2ba1580c144f2f824aca1db803fe6703c34ca5b
Javier Garcia Vera <javier.vera@consensys.net>
2026-04-21 12:33:24 +0200

    test: added smart selection for performance tests

--- tests/performance/login/import-multiple-srps.spec.ts
+++ tests/performance/login/import-multiple-srps.spec.ts
@@ -6,18 +6,11 @@ import ImportWalletView from '../../page-objects/Onboarding/ImportWalletView';
 import AddAccountBottomSheet from '../../page-objects/wallet/AddAccountBottomSheet';
 import AccountListBottomSheet from '../../page-objects/wallet/AccountListBottomSheet';
 import WalletView from '../../page-objects/wallet/WalletView';
-import {
-  Performance,
-  System,
-  PerformanceAccountList,
-  PerformanceLogin,
-} from '../../tags.performance.js';
+import { PerformanceAccountList } from '../../tags.performance.js';
 import PlaywrightGestures from '../../framework/PlaywrightGestures';
 /* Scenario 4: Import SRP with +50 accounts, SRP 1, SRP 2, SRP 3 */
-perfTest.describe(
-  `${Performance} ${System} ${PerformanceLogin} ${PerformanceAccountList}`,
-  () => {
-    perfTest.setTimeout(30 * 60 * 1000);
+perfTest.describe(PerformanceAccountList, () => {
+  perfTest.setTimeout(30 * 60 * 1000);
 
     perfTest.skip(
       // skipped because of the account sync issue
