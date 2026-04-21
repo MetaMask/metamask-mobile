b409d9947c3c7ab3c8a58966b2edc36d495039ec
John Whiles <john.whiles@consensys.net>
2026-04-21 12:16:55 +0100

    feat: init money controller

--- app/core/Engine/controllers/money-account-upgrade-controller-init.ts
+++ app/core/Engine/controllers/money-account-upgrade-controller-init.ts
@@ -2,7 +2,12 @@ import {
   MoneyAccountUpgradeController,
   type MoneyAccountUpgradeControllerMessenger,
 } from '@metamask/money-account-upgrade-controller';
+import { type Hex, hexToNumber } from '@metamask/utils';
 import type { MessengerClientInitFunction } from '../types';
+import { getDeleGatorEnvironment } from '../../Delegation/environment';
+
+const ZERO_ADDRESS: Hex = '0x0000000000000000000000000000000000000000';
+const DEFAULT_CHAIN_ID: Hex = '0xa4b1';
 
 /**
  * Initialize the MoneyAccountUpgradeController.
@@ -18,6 +23,26 @@ export const moneyAccountUpgradeControllerInit: MessengerClientInitFunction<
   const controller = new MoneyAccountUpgradeController({
     messenger: controllerMessenger,
   });
+
+  (async () => {
+    const serviceDetails = await controllerMessenger.call(
+      'ChompApiService:getServiceDetails',
+      [DEFAULT_CHAIN_ID],
+    );
+    const chain = serviceDetails.chains[DEFAULT_CHAIN_ID];
+    const musdTokenAddress =
+      chain?.protocol.vedaProtocol?.supportedTokens[0]?.tokenAddress ??
+      ZERO_ADDRESS;
+
+    const environment = getDeleGatorEnvironment(hexToNumber(DEFAULT_CHAIN_ID));
+
+    await controller.init(DEFAULT_CHAIN_ID, {
+      musdTokenAddress,
+      delegatorImplAddress: environment.EIP7702StatelessDeleGatorImpl,
+      redeemerEnforcer: environment.caveatEnforcers.RedeemerEnforcer,
+      valueLteEnforcer: environment.caveatEnforcers.ValueLteEnforcer,
+    });
+  })();
 
   return { controller };
 };
