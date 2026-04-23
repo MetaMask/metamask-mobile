0d0a9884545a62dff4379e53f669f0a3f65705f2
Curtis David <Curtis.David7@gmail.com>
2026-04-23 15:53:32 -0400

    fix

--- app/components/Views/confirmations/components/blockaid-banner/BlockaidBanner.test.tsx
+++ app/components/Views/confirmations/components/blockaid-banner/BlockaidBanner.test.tsx
@@ -4,9 +4,7 @@ import { fireEvent } from '@testing-library/react-native';
 
 import { TESTID_ACCORDION_CONTENT } from '../../../../../component-library/components/Accordions/Accordion/Accordion.constants';
 import { TESTID_ACCORDIONHEADER } from '../../../../../component-library/components/Accordions/Accordion/foundation/AccordionHeader/AccordionHeader.constants';
-import { BANNERALERT_TEST_ID } from '../../../../../component-library/components/Banners/Banner/variants/BannerAlert/BannerAlert.constants';
 import BlockaidBanner from './BlockaidBanner';
-import { FALSE_POSITIVE_REPOST_LINE_TEST_ID } from './BlockaidBanner.constants';
 import { ResultType, Reason } from './BlockaidBanner.types';
 import renderWithProvider, {
   DeepPartial,
@@ -64,7 +62,7 @@ describe('BlockaidBanner', () => {
     expect(wrapper.getByTestId('security-alert-banner')).toBeOnTheScreen();
   });
 
-  it('should render correctly with reason "raw_signature_farming"', async () => {
+  it('should render correctly with reason "raw_signature_farming"', () => {
     const wrapper = renderWithProvider(
       <BlockaidBanner
         securityAlertResponse={{
@@ -77,20 +75,16 @@ describe('BlockaidBanner', () => {
     );
 
     expect(wrapper.getByTestId('security-alert-banner')).toBeOnTheScreen();
+    expect(wrapper.getByTestId(TESTID_ACCORDIONHEADER)).toBeOnTheScreen();
+    expect(wrapper.getByText('This is a suspicious request')).toBeOnTheScreen();
     expect(
-      await wrapper.queryByTestId(TESTID_ACCORDIONHEADER),
-    ).toBeOnTheScreen();
-    expect(
-      await wrapper.getByText('This is a suspicious request'),
-    ).toBeOnTheScreen();
-    expect(
-      await wrapper.getByText(
+      wrapper.getByText(
         'If you approve this request, you might lose your assets.',
       ),
     ).toBeOnTheScreen();
   });
 
-  it('should render correctly with list attack details', async () => {
+  it('should render correctly with list attack details', () => {
     const wrapper = renderWithProvider(
       <BlockaidBanner
         securityAlertResponse={{
@@ -103,38 +97,25 @@ describe('BlockaidBanner', () => {
     );
 
     expect(wrapper.getByTestId('security-alert-banner')).toBeOnTheScreen();
-    expect(
-      await wrapper.queryByTestId(TESTID_ACCORDIONHEADER),
-    ).toBeOnTheScreen();
-    expect(await wrapper.queryByTestId(TESTID_ACCORDION_CONTENT)).toBeNull();
+    expect(wrapper.getByTestId(TESTID_ACCORDIONHEADER)).toBeOnTheScreen();
+    expect(wrapper.queryByTestId(TESTID_ACCORDION_CONTENT)).toBeNull();
 
-    fireEvent.press(await wrapper.getByText('See details'));
+    fireEvent.press(wrapper.getByText('See details'));
 
+    expect(wrapper.getByTestId(TESTID_ACCORDION_CONTENT)).toBeOnTheScreen();
     expect(
-      await wrapper.queryByTestId(TESTID_ACCORDION_CONTENT),
+      wrapper.getByText(/We found attack vectors in this request/),
     ).toBeOnTheScreen();
     expect(
-      await wrapper.queryByText('We found attack vectors in this request'),
+      wrapper.getByText(/This request shows a fake token name and icon\./),
     ).toBeOnTheScreen();
+    expect(wrapper.getByText(/Operator is an EOA/)).toBeOnTheScreen();
     expect(
-      await wrapper.queryByText(
-        'This request shows a fake token name and icon.',
-      ),
-    ).toBeOnTheScreen();
-    expect(
-      await wrapper.queryByText(
-        'If you approve this request, a third party known for scams might take all your assets.',
-      ),
-    ).toBeOnTheScreen();
-    expect(await wrapper.queryByText('Operator is an EOA')).toBeOnTheScreen();
-    expect(
-      await wrapper.queryByText(
-        'Operator is untrusted according to previous activity',
-      ),
+      wrapper.getByText(/Operator is untrusted according to previous activity/),
     ).toBeOnTheScreen();
   });
 
-  it('should render something does not look right with contact us link when expanded', async () => {
+  it('should render something does not look right with contact us link when expanded', () => {
     const wrapper = renderWithProvider(
       <BlockaidBanner
         securityAlertResponse={{
@@ -147,52 +128,45 @@ describe('BlockaidBanner', () => {
     );
 
     expect(wrapper.getByTestId('security-alert-banner')).toBeOnTheScreen();
-    expect(
-      await wrapper.queryByTestId(TESTID_ACCORDIONHEADER),
-    ).toBeOnTheScreen();
-    expect(await wrapper.queryByTestId(TESTID_ACCORDION_CONTENT)).toBeNull();
+    expect(wrapper.getByTestId(TESTID_ACCORDIONHEADER)).toBeOnTheScreen();
+    expect(wrapper.queryByTestId(TESTID_ACCORDION_CONTENT)).toBeNull();
 
-    fireEvent.press(await wrapper.getByText('See details'));
+    fireEvent.press(wrapper.getByText('See details'));
 
+    expect(wrapper.getByTestId(TESTID_ACCORDION_CONTENT)).toBeOnTheScreen();
     expect(
-      await wrapper.queryByTestId(TESTID_ACCORDION_CONTENT),
-    ).toBeOnTheScreen();
-    expect(
-      await wrapper.queryByTestId(FALSE_POSITIVE_REPOST_LINE_TEST_ID),
-    ).toBeOnTheScreen();
-    expect(
-      await wrapper.queryByText('Something doesn’t look right?'),
+      wrapper.getByText(/Something doesn\u2019t look right\?/),
     ).toBeOnTheScreen();
   });
 
-  it('should not render if securityAlertResponse is undefined', async () => {
+  it('should not render if securityAlertResponse is undefined', () => {
     const wrapper = renderWithProvider(<BlockaidBanner />, {
       state: mockState,
     });
 
-    expect(await wrapper.queryByTestId(TESTID_ACCORDIONHEADER)).toBeNull();
-    expect(await wrapper.queryByTestId(TESTID_ACCORDION_CONTENT)).toBeNull();
+    expect(wrapper.queryByTestId(TESTID_ACCORDIONHEADER)).toBeNull();
+    expect(wrapper.queryByTestId(TESTID_ACCORDION_CONTENT)).toBeNull();
   });
 
-  it('should not render if blockaid does not support network', async () => {
+  it('should not render if blockaid does not support network', () => {
     const wrapper = renderWithProvider(<BlockaidBanner />, {
       state: mockState,
     });
 
-    expect(await wrapper.queryByTestId(TESTID_ACCORDIONHEADER)).toBeNull();
-    expect(await wrapper.queryByTestId(TESTID_ACCORDION_CONTENT)).toBeNull();
+    expect(wrapper.queryByTestId(TESTID_ACCORDIONHEADER)).toBeNull();
+    expect(wrapper.queryByTestId(TESTID_ACCORDION_CONTENT)).toBeNull();
   });
 
-  it('should not render if user has not enabled blockaid', async () => {
+  it('should not render if user has not enabled blockaid', () => {
     const wrapper = renderWithProvider(<BlockaidBanner />, {
       state: mockState,
     });
 
-    expect(await wrapper.queryByTestId(TESTID_ACCORDIONHEADER)).toBeNull();
-    expect(await wrapper.queryByTestId(TESTID_ACCORDION_CONTENT)).toBeNull();
+    expect(wrapper.queryByTestId(TESTID_ACCORDIONHEADER)).toBeNull();
+    expect(wrapper.queryByTestId(TESTID_ACCORDION_CONTENT)).toBeNull();
   });
 
-  it('should render loader if reason is requestInProgress', async () => {
+  it('should render loader if reason is requestInProgress', () => {
     const wrapper = renderWithProvider(
       <BlockaidBanner
         securityAlertResponse={{
@@ -206,11 +180,11 @@ describe('BlockaidBanner', () => {
     );
 
     expect(wrapper.getByTestId('blockaid-banner-loader')).toBeOnTheScreen();
-    expect(await wrapper.queryByTestId(TESTID_ACCORDIONHEADER)).toBeNull();
-    expect(await wrapper.queryByTestId(BANNERALERT_TEST_ID)).toBeNull();
+    expect(wrapper.queryByTestId(TESTID_ACCORDIONHEADER)).toBeNull();
+    expect(wrapper.queryByTestId('security-alert-banner')).toBeNull();
   });
 
-  it('should not render if resultType is benign', async () => {
+  it('should not render if resultType is benign', () => {
     const wrapper = renderWithProvider(
       <BlockaidBanner
         securityAlertResponse={{
@@ -222,24 +196,24 @@ describe('BlockaidBanner', () => {
       { state: mockState },
     );
 
-    expect(await wrapper.queryByTestId(TESTID_ACCORDIONHEADER)).toBeNull();
-    expect(await wrapper.queryByTestId(TESTID_ACCORDION_CONTENT)).toBeNull();
+    expect(wrapper.queryByTestId(TESTID_ACCORDIONHEADER)).toBeNull();
+    expect(wrapper.queryByTestId(TESTID_ACCORDION_CONTENT)).toBeNull();
   });
 
-  it('should render normal banner alert if resultType is failed', async () => {
+  it('should render normal banner alert if resultType is failed', () => {
     const wrapper = renderWithProvider(
       <BlockaidBanner securityAlertResponse={securityAlertResponse} />,
       { state: mockState },
     );
 
-    expect(await wrapper.queryByTestId(TESTID_ACCORDIONHEADER)).toBeNull();
-    expect(await wrapper.queryByTestId(TESTID_ACCORDION_CONTENT)).toBeNull();
-    expect(await wrapper.queryByTestId(BANNERALERT_TEST_ID)).toBeOnTheScreen();
+    expect(wrapper.queryByTestId(TESTID_ACCORDIONHEADER)).toBeNull();
+    expect(wrapper.queryByTestId(TESTID_ACCORDION_CONTENT)).toBeNull();
     expect(
-      await wrapper.queryByText('Request may not be safe'),
+      wrapper.getByTestId('security-alert-response-failed-banner'),
     ).toBeOnTheScreen();
+    expect(wrapper.getByText('Request may not be safe')).toBeOnTheScreen();
     expect(
-      await wrapper.queryByText(
+      wrapper.getByText(
         'Because of an error, this request was not verified by the security provider. Proceed with caution.',
       ),
     ).toBeOnTheScreen();
