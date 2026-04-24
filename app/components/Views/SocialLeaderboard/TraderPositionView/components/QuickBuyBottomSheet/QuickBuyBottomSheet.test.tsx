74b79d0c44c08dfe6736dfe7a4e86ace3963f0b8
Joao Santos <jrmsantos15@gmail.com>
2026-04-24 08:00:21 +0200

    chore: lint

--- app/components/Views/SocialLeaderboard/TraderPositionView/components/QuickBuyBottomSheet/QuickBuyBottomSheet.test.tsx
+++ app/components/Views/SocialLeaderboard/TraderPositionView/components/QuickBuyBottomSheet/QuickBuyBottomSheet.test.tsx
@@ -153,6 +153,7 @@ const buildHookResult = (
   hasError: false,
   hasValidAmount: false,
   isConfirmDisabled: true,
+  confirmButtonState: 'idle',
   getButtonLabel: () => 'social_leaderboard.trader_position.buy',
   handleClose: jest.fn(),
   handlePresetPress: jest.fn(),
