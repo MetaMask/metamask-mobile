c7b42b3ff9bf96754ea5e5fd768cb7e32e3724a8
adnxy <adnanpoviolabs@gmail.com>
2026-04-27 17:16:30 +0200

    fix: improve test config

--- jest.config.view.js
+++ jest.config.view.js
@@ -4,12 +4,7 @@ const baseConfig = require('./jest.config.js');
 module.exports = {
   ...baseConfig,
   setupFilesAfterEnv: ['<rootDir>/app/util/test/testSetupView.js'],
-  testPathIgnorePatterns: (baseConfig.testPathIgnorePatterns || []).filter(
-    (pattern) => !pattern.includes('view'),
-  ),
-  testMatch: ['**/*.view.test.ts?(x)'],
   testTimeout: 30000,
   forceExit: true,
-  maxWorkers: process.env.CI ? 2 : 1,
-  workerIdleMemoryLimit: '512MB',
+  maxWorkers: 1,
 };
