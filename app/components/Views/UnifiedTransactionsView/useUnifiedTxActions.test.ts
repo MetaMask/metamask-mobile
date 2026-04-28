f5b2613ddf08f35a0a45f9314c5b1b1c3db197e6
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2026-04-28 16:30:13 +0100

    test fix

--- app/components/Views/UnifiedTransactionsView/useUnifiedTxActions.test.ts
+++ app/components/Views/UnifiedTransactionsView/useUnifiedTxActions.test.ts
@@ -66,6 +66,7 @@ jest.mock('../../UI/QRHardware/QRSigningTransactionModal', () => ({
 }));
 
 jest.mock('@metamask/rpc-errors', () => ({
+  ...jest.requireActual('@metamask/rpc-errors'),
   providerErrors: {
     ...jest.requireActual('@metamask/rpc-errors').providerErrors,
     userRejectedRequest: jest.fn(() => ({ code: 4001 })),
