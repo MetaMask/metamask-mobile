142048a6b2bf9882d2a61a13d75fcbd4d04f4e51
Jyoti Puri <jyotipuri@gmail.com>
2026-05-05 15:31:56 +0530

    updated for testing

--- app/components/UI/Money/hooks/useMoneyAccountBalance.ts
+++ app/components/UI/Money/hooks/useMoneyAccountBalance.ts
@@ -193,9 +193,9 @@ const useMoneyAccountBalance = (
     isAggregatedBalanceLoading,
     musdFiatFormatted,
     musdSHFvdFiatFormatted,
-    tokenTotal,
-    totalFiatFormatted,
-    totalFiatRaw,
+    tokenTotal: new BigNumber(10),
+    totalFiatFormatted: '$10',
+    totalFiatRaw: 10,
     // TODO: Remove __DEV__ values before launch. This is temporary to circumvent the Vault's current 0% APY.
     apyDecimal: __DEV__ ? DEV_APY.decimal : apyDecimal,
     apyPercent: __DEV__ ? DEV_APY.percent : apyPercent,
