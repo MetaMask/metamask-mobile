f7edace5cb02913121a271a11998393062b018d5
Alexey Kureev <a.g.kureev@gmail.com>
2026-05-07 00:36:16 +0200

    fix(design): regenerate icon types/assets

--- app/component-library/components/Icons/Icon/Icon.assets.ts
+++ app/component-library/components/Icons/Icon/Icon.assets.ts
@@ -85,8 +85,6 @@ import descriptionSVG from './assets/description.svg';
 import detailsSVG from './assets/details.svg';
 import diagramSVG from './assets/diagram.svg';
 import documentcodeSVG from './assets/document-code.svg';
-import musdSVG from './assets/musd.svg';
-import musdfilledSVG from './assets/musd-filled.svg';
 import downloadSVG from './assets/download.svg';
 import draftSVG from './assets/draft.svg';
 import ecoleafSVG from './assets/eco-leaf.svg';
@@ -169,6 +167,8 @@ import monitorSVG from './assets/monitor.svg';
 import morehorizontalSVG from './assets/more-horizontal.svg';
 import moreverticalSVG from './assets/more-vertical.svg';
 import mountainflagSVG from './assets/mountain-flag.svg';
+import musdfilledSVG from './assets/musd-filled.svg';
+import musdSVG from './assets/musd.svg';
 import musicnoteSVG from './assets/music-note.svg';
 import nophotographySVG from './assets/no-photography.svg';
 import notificationSVG from './assets/notification.svg';
@@ -178,13 +178,13 @@ import passwordcheckSVG from './assets/password-check.svg';
 import pendingSVG from './assets/pending.svg';
 import peopleSVG from './assets/people.svg';
 import personcancelSVG from './assets/person-cancel.svg';
+import piechartSVG from './assets/pie-chart.svg';
 import pinSVG from './assets/pin.svg';
 import plantSVG from './assets/plant.svg';
-import pieChartSVG from './assets/pie-chart.svg';
-import predictionsSVG from './assets/predictions.svg';
 import plugSVG from './assets/plug.svg';
 import plusandminusSVG from './assets/plus-and-minus.svg';
 import policyalertSVG from './assets/policy-alert.svg';
+import predictionsSVG from './assets/predictions.svg';
 import printSVG from './assets/print.svg';
 import priorityhighSVG from './assets/priority-high.svg';
 import privacytipSVG from './assets/privacy-tip.svg';
@@ -374,8 +374,6 @@ export const assetByIconName: AssetByIconName = {
   [IconName.Details]: detailsSVG,
   [IconName.Diagram]: diagramSVG,
   [IconName.DocumentCode]: documentcodeSVG,
-  [IconName.Musd]: musdSVG,
-  [IconName.MusdFilled]: musdfilledSVG,
   [IconName.Download]: downloadSVG,
   [IconName.Draft]: draftSVG,
   [IconName.EcoLeaf]: ecoleafSVG,
@@ -458,6 +456,8 @@ export const assetByIconName: AssetByIconName = {
   [IconName.MoreHorizontal]: morehorizontalSVG,
   [IconName.MoreVertical]: moreverticalSVG,
   [IconName.MountainFlag]: mountainflagSVG,
+  [IconName.MusdFilled]: musdfilledSVG,
+  [IconName.Musd]: musdSVG,
   [IconName.MusicNote]: musicnoteSVG,
   [IconName.NoPhotography]: nophotographySVG,
   [IconName.Notification]: notificationSVG,
@@ -467,13 +467,13 @@ export const assetByIconName: AssetByIconName = {
   [IconName.Pending]: pendingSVG,
   [IconName.People]: peopleSVG,
   [IconName.PersonCancel]: personcancelSVG,
+  [IconName.PieChart]: piechartSVG,
   [IconName.Pin]: pinSVG,
   [IconName.Plant]: plantSVG,
-  [IconName.Portfolio]: pieChartSVG,
-  [IconName.Predictions]: predictionsSVG,
   [IconName.Plug]: plugSVG,
   [IconName.PlusAndMinus]: plusandminusSVG,
   [IconName.PolicyAlert]: policyalertSVG,
+  [IconName.Predictions]: predictionsSVG,
   [IconName.Print]: printSVG,
   [IconName.PriorityHigh]: priorityhighSVG,
   [IconName.PrivacyTip]: privacytipSVG,
