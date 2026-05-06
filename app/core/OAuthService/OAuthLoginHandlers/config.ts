c3844b5a0974aa7728f8da3fc9b188ff9116ce01
ieow <4881057+ieow@users.noreply.github.com>
2026-05-06 14:53:52 +0800

    feat: update client id

--- app/core/OAuthService/OAuthLoginHandlers/config.ts
+++ app/core/OAuthService/OAuthLoginHandlers/config.ts
@@ -1,4 +1,10 @@
 interface OAUTH_CONFIG_TYPE {
+  IOS_GOOGLE_CLIENT_ID: string;
+  IOS_GOOGLE_REDIRECT_URI: string;
+  ANDROID_GOOGLE_CLIENT_ID: string;
+  ANDROID_GOOGLE_SERVER_CLIENT_ID: string;
+  ANDROID_APPLE_CLIENT_ID: string;
+
   AUTH_SERVER_URL: string;
   WEB3AUTH_NETWORK: string;
 
@@ -22,6 +28,16 @@ enum BUILD_TYPE {
 
 export const OAUTH_CONFIG: Record<BUILD_TYPE, OAUTH_CONFIG_TYPE> = {
   development: {
+    IOS_GOOGLE_CLIENT_ID:
+      '615965109465-h6tp2h3crls6hbggispcgovbvk4vabu3.apps.googleusercontent.com',
+    IOS_GOOGLE_REDIRECT_URI:
+      'com.googleusercontent.apps.615965109465-h6tp2h3crls6hbggispcgovbvk4vabu3:/oauth2redirect/google',
+    ANDROID_GOOGLE_CLIENT_ID:
+      '615965109465-laapla9g0klg2p7rp5mn66fcl6jc4fes.apps.googleusercontent.com',
+    ANDROID_GOOGLE_SERVER_CLIENT_ID:
+      '615965109465-i8oeh9kuvl1n6lk1ffkobpvth27bmi41.apps.googleusercontent.com',
+    ANDROID_APPLE_CLIENT_ID: 'io.metamask.appleloginclient.dev',
+
     GOOGLE_GROUPED_AUTH_CONNECTION_ID: 'mm-seedless-onboarding',
     APPLE_GROUPED_AUTH_CONNECTION_ID: 'mm-seedless-onboarding',
     AUTH_SERVER_URL: 'https://api-develop-torus-byoa.web3auth.io',
@@ -33,6 +49,16 @@ export const OAUTH_CONFIG: Record<BUILD_TYPE, OAUTH_CONFIG_TYPE> = {
     IOS_APPLE_AUTH_CONNECTION_ID: 'byoa-server',
   },
   main_prod: {
+    IOS_GOOGLE_CLIENT_ID:
+      '795351133007-jcaor637tblrlpuj29shdej3co8bu8kv.apps.googleusercontent.com',
+    IOS_GOOGLE_REDIRECT_URI:
+      'com.googleusercontent.apps.795351133007-47aohp9j9n7r8fef5n6ejeauhu4kfc9e:/oauth2redirect/google',
+    ANDROID_GOOGLE_CLIENT_ID:
+      '795351133007-jcaor637tblrlpuj29shdej3co8bu8kv.apps.googleusercontent.com',
+    ANDROID_GOOGLE_SERVER_CLIENT_ID:
+      '795351133007-6d0s31utj13knv440fgjo2ur93241gb6.apps.googleusercontent.com',
+    ANDROID_APPLE_CLIENT_ID: 'io.metamask.appleloginclient.prod',
+
     GOOGLE_GROUPED_AUTH_CONNECTION_ID: 'mm-google-main',
     APPLE_GROUPED_AUTH_CONNECTION_ID: 'mm-apple-main',
     AUTH_SERVER_URL: 'https://auth-service.api.cx.metamask.io',
@@ -44,6 +70,16 @@ export const OAUTH_CONFIG: Record<BUILD_TYPE, OAUTH_CONFIG_TYPE> = {
     IOS_APPLE_AUTH_CONNECTION_ID: 'mm-apple-main-common',
   },
   main_uat: {
+    IOS_GOOGLE_CLIENT_ID:
+      '387141446914-5ja3p4dfanfkm8uq238fm1b8t1rkscv4.apps.googleusercontent.com',
+    IOS_GOOGLE_REDIRECT_URI:
+      'com.googleusercontent.apps.387141446914-5ja3p4dfanfkm8uq238fm1b8t1rkscv4:/oauth2redirect/google',
+    ANDROID_GOOGLE_CLIENT_ID:
+      '387141446914-7rl5s9s1uv82fgb03f93eqc0n8jq7t6k.apps.googleusercontent.com',
+    ANDROID_GOOGLE_SERVER_CLIENT_ID:
+      '387141446914-olajr83p1bbvabh1u8tfglt1k4u6jlcb.apps.googleusercontent.com',
+    ANDROID_APPLE_CLIENT_ID: 'io.metamask.appleloginclient.uat',
+
     GOOGLE_GROUPED_AUTH_CONNECTION_ID: 'mm-google-uat',
     APPLE_GROUPED_AUTH_CONNECTION_ID: 'mm-apple-uat',
     AUTH_SERVER_URL: 'https://auth-service.uat-api.cx.metamask.io',
@@ -55,6 +91,16 @@ export const OAUTH_CONFIG: Record<BUILD_TYPE, OAUTH_CONFIG_TYPE> = {
     IOS_APPLE_AUTH_CONNECTION_ID: 'mm-apple-uat-common',
   },
   main_dev: {
+    IOS_GOOGLE_CLIENT_ID:
+      '615965109465-h6tp2h3crls6hbggispcgovbvk4vabu3.apps.googleusercontent.com',
+    IOS_GOOGLE_REDIRECT_URI:
+      'com.googleusercontent.apps.615965109465-h6tp2h3crls6hbggispcgovbvk4vabu3:/oauth2redirect/google',
+    ANDROID_GOOGLE_CLIENT_ID:
+      '615965109465-laapla9g0klg2p7rp5mn66fcl6jc4fes.apps.googleusercontent.com',
+    ANDROID_GOOGLE_SERVER_CLIENT_ID:
+      '615965109465-i8oeh9kuvl1n6lk1ffkobpvth27bmi41.apps.googleusercontent.com',
+    ANDROID_APPLE_CLIENT_ID: 'io.metamask.appleloginclient.dev',
+
     GOOGLE_GROUPED_AUTH_CONNECTION_ID: 'mm-google-dev',
     APPLE_GROUPED_AUTH_CONNECTION_ID: 'mm-apple-dev',
     AUTH_SERVER_URL: 'https://auth-service.dev-api.cx.metamask.io',
@@ -66,6 +112,16 @@ export const OAUTH_CONFIG: Record<BUILD_TYPE, OAUTH_CONFIG_TYPE> = {
     IOS_APPLE_AUTH_CONNECTION_ID: 'mm-apple-dev-common',
   },
   flask_prod: {
+    IOS_GOOGLE_CLIENT_ID:
+      '795351133007-gvuagr9t7tfkak3sp2cmng4pdhchlfpd.apps.googleusercontent.com',
+    IOS_GOOGLE_REDIRECT_URI:
+      'com.googleusercontent.apps.795351133007-gvuagr9t7tfkak3sp2cmng4pdhchlfpd:/oauth2redirect/google',
+    ANDROID_GOOGLE_CLIENT_ID:
+      '795351133007-0po6dfbepae7klaso18qv61f86u4a2ef.apps.googleusercontent.com',
+    ANDROID_GOOGLE_SERVER_CLIENT_ID:
+      '795351133007-gh67d3hot6ib24htu9d7sh01bg90lpdu.apps.googleusercontent.com',
+    ANDROID_APPLE_CLIENT_ID: 'io.metamask.appleloginclient.flask.prod',
+
     GOOGLE_GROUPED_AUTH_CONNECTION_ID: 'mm-google-flask-main',
     APPLE_GROUPED_AUTH_CONNECTION_ID: 'mm-apple-flask-main',
     AUTH_SERVER_URL: 'https://auth-service.api.cx.metamask.io',
@@ -77,6 +133,16 @@ export const OAUTH_CONFIG: Record<BUILD_TYPE, OAUTH_CONFIG_TYPE> = {
     IOS_APPLE_AUTH_CONNECTION_ID: 'mm-apple-flask-main-common',
   },
   flask_uat: {
+    IOS_GOOGLE_CLIENT_ID:
+      '387141446914-1tdlsrare1jtjd2tgal9bi1ilb4qro5d.apps.googleusercontent.com',
+    IOS_GOOGLE_REDIRECT_URI:
+      'com.googleusercontent.apps.387141446914-1tdlsrare1jtjd2tgal9bi1ilb4qro5d:/oauth2redirect/google',
+    ANDROID_GOOGLE_CLIENT_ID:
+      '387141446914-ki5586faf9qmlghop8g07f4a10scdevi.apps.googleusercontent.com',
+    ANDROID_GOOGLE_SERVER_CLIENT_ID:
+      '387141446914-f03k9ivc2jrmi1s53lne88mh529372kj.apps.googleusercontent.com',
+    ANDROID_APPLE_CLIENT_ID: 'io.metamask.appleloginclient.flask.uat',
+
     GOOGLE_GROUPED_AUTH_CONNECTION_ID: 'mm-google-flask-uat',
     APPLE_GROUPED_AUTH_CONNECTION_ID: 'mm-apple-flask-uat',
     AUTH_SERVER_URL: 'https://auth-service.api.cx.metamask.io',
@@ -88,6 +154,16 @@ export const OAUTH_CONFIG: Record<BUILD_TYPE, OAUTH_CONFIG_TYPE> = {
     IOS_APPLE_AUTH_CONNECTION_ID: 'mm-apple-flask-uat-common',
   },
   flask_dev: {
+    IOS_GOOGLE_CLIENT_ID:
+      '615965109465-89b2lmqgm5ka8j8t403qhooouv57id9b.apps.googleusercontent.com',
+    IOS_GOOGLE_REDIRECT_URI:
+      'com.googleusercontent.apps.615965109465-89b2lmqgm5ka8j8t403qhooouv57id9b:/oauth2redirect/google',
+    ANDROID_GOOGLE_CLIENT_ID:
+      '615965109465-9nn2i74feqs3v9ps4lb61ha0v34eo382.apps.googleusercontent.com',
+    ANDROID_GOOGLE_SERVER_CLIENT_ID:
+      '615965109465-ab20kuqbls6fj5s50fvmvbnket8nv1sh.apps.googleusercontent.com',
+    ANDROID_APPLE_CLIENT_ID: 'io.metamask.appleloginclient.flask.dev',
+
     GOOGLE_GROUPED_AUTH_CONNECTION_ID: 'mm-google-flask-dev',
     APPLE_GROUPED_AUTH_CONNECTION_ID: 'mm-apple-flask-dev',
     AUTH_SERVER_URL: 'https://auth-service.dev-api.cx.metamask.io',
