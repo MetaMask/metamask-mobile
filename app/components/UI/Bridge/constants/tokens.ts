403fe7f036517e7dc8f17754cbf43281053a580a
IF <139582705+infiniteflower@users.noreply.github.com>
2026-05-07 16:04:16 -0400

    chore: add musd to chain 1 stablecoins

--- app/components/UI/Bridge/constants/tokens.ts
+++ app/components/UI/Bridge/constants/tokens.ts
@@ -11,4 +11,13 @@ export const BridgeTokenMetadata: Record<CaipAssetType, BridgeToken> = {
       'https://static.cx.metamask.io/api/v2/tokenIcons/assets/eip155/1/erc20/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png',
     chainId: '0x1',
   },
+  'eip155:1/erc20:0xaca92e438df0b2401ff60da7e4337b687a2435da': {
+    symbol: 'MUSD',
+    name: 'MetaMask USD',
+    address: '0xaca92e438df0b2401ff60da7e4337b687a2435da',
+    decimals: 6,
+    image:
+      'https://static.cx.metamask.io/api/v2/tokenIcons/assets/eip155/1/erc20/0xaca92e438df0b2401ff60da7e4337b687a2435da.png',
+    chainId: '0x1',
+  },
 };
