8269b67b3fdcc6670d100ee84e45b30978eee606
Matthew Grainger <matthew.grainger@consensys.net>
2026-05-08 13:24:00 -0400

    feat: update tests

--- app/core/Engine/controllers/money-account-balance-service-init.test.ts
+++ app/core/Engine/controllers/money-account-balance-service-init.test.ts
@@ -1,4 +1,3 @@
-import { CHAIN_IDS } from '@metamask/transaction-controller';
 import { buildMessengerClientInitRequestMock } from '../utils/test-utils';
 import { ExtendedMessenger } from '../../ExtendedMessenger';
 import { getMoneyAccountBalanceServiceMessenger } from '../messengers/money-account-balance-service-messenger';
@@ -33,18 +32,13 @@ describe('moneyAccountBalanceServiceInit', () => {
     expect(controller).toBeInstanceOf(MoneyAccountBalanceService);
   });
 
-  it('passes vault configuration and messenger to the service', () => {
+  it('passes messenger to the service', () => {
     moneyAccountBalanceServiceInit(getInitRequestMock());
 
     const serviceMock = jest.mocked(MoneyAccountBalanceService);
     expect(serviceMock).toHaveBeenCalledWith(
       expect.objectContaining({
         messenger: expect.any(Object),
-        vaultAddress: '0xB5F07d769dD60fE54c97dd53101181073DDf21b2',
-        vaultChainId: CHAIN_IDS.ARBITRUM,
-        accountantAddress: '0x800ebc3B74F67EaC27C9CCE4E4FF28b17CdCA173',
-        underlyingTokenAddress: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
-        underlyingTokenDecimals: 6,
       }),
     );
   });
