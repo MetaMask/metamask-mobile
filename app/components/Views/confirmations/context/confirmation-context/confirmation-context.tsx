ac25bd31c13ab49618c8b740833995a5951bf984
Goktug Poyraz <omergoktugpoyraz@gmail.com>
2026-05-08 12:47:39 +0200

    Fix mmpay fiat ramp loading state

--- app/components/Views/confirmations/context/confirmation-context/confirmation-context.tsx
+++ app/components/Views/confirmations/context/confirmation-context/confirmation-context.tsx
@@ -3,9 +3,11 @@ import React, { useContext, useMemo, useState } from 'react';
 
 export interface ConfirmationContextParams {
   isFooterVisible?: boolean;
+  isHeadlessBuyInProgress: boolean;
   isTransactionValueUpdating: boolean;
   isTransactionDataUpdating: boolean;
   setIsFooterVisible: (isFooterVisible: boolean) => void;
+  setIsHeadlessBuyInProgress: (isHeadlessBuyInProgress: boolean) => void;
   setIsTransactionValueUpdating: (isTransactionValueUpdating: boolean) => void;
   setIsTransactionDataUpdating: (isTransactionDataUpdating: boolean) => void;
 }
@@ -14,9 +16,11 @@ export interface ConfirmationContextParams {
 // that are used to render the confirmation
 const ConfirmationContext = React.createContext<ConfirmationContextParams>({
   isFooterVisible: true,
+  isHeadlessBuyInProgress: false,
   isTransactionDataUpdating: false,
   isTransactionValueUpdating: false,
   setIsFooterVisible: noop,
+  setIsHeadlessBuyInProgress: noop,
   setIsTransactionDataUpdating: noop,
   setIsTransactionValueUpdating: noop,
 });
@@ -33,23 +37,29 @@ export const ConfirmationContextProvider: React.FC<
 
   const [isFooterVisible, setIsFooterVisible] = useState<boolean>();
 
+  const [isHeadlessBuyInProgress, setIsHeadlessBuyInProgress] = useState(false);
+
   const [isTransactionDataUpdating, setIsTransactionDataUpdating] =
     useState<boolean>(false);
 
   const contextValue = useMemo(
     () => ({
       isFooterVisible,
+      isHeadlessBuyInProgress,
       isTransactionDataUpdating,
       isTransactionValueUpdating,
       setIsFooterVisible,
+      setIsHeadlessBuyInProgress,
       setIsTransactionDataUpdating,
       setIsTransactionValueUpdating,
     }),
     [
       isFooterVisible,
+      isHeadlessBuyInProgress,
       isTransactionDataUpdating,
       isTransactionValueUpdating,
       setIsFooterVisible,
+      setIsHeadlessBuyInProgress,
       setIsTransactionDataUpdating,
       setIsTransactionValueUpdating,
     ],
