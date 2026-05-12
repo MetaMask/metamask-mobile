42b4b44686d8891bac689bf290da6c8908dc993e
Luis Taniça <matallui@gmail.com>
2026-05-12 14:21:53 -0600

    feat(predict): add World Cup feature flag config

---
+++ app/components/UI/Predict/queries/worldCup.ts
@@ -0,0 +1,29 @@
+import {
+  buildPredictWorldCupAllQuery,
+  buildPredictWorldCupLiveQuery,
+  buildPredictWorldCupPropsQuery,
+  buildPredictWorldCupStageEventsQuery,
+} from '../utils/worldCup';
+import type {
+  PredictWorldCupConfig,
+  PredictWorldCupStageConfig,
+} from '../types/flags';
+
+export const predictWorldCupKeys = {
+  all: () => ['predict', 'worldCup'] as const,
+  tab: (queryParams: string) =>
+    [...predictWorldCupKeys.all(), 'tab', queryParams] as const,
+  stage: (stageKey: string, eventIds: string[]) =>
+    [...predictWorldCupKeys.all(), 'stage', stageKey, ...eventIds] as const,
+};
+
+export const predictWorldCupQueries = {
+  all: (config: Pick<PredictWorldCupConfig, 'tagSlug'>) =>
+    buildPredictWorldCupAllQuery(config),
+  props: (config: Pick<PredictWorldCupConfig, 'tagSlug' | 'gamesTagId'>) =>
+    buildPredictWorldCupPropsQuery(config),
+  live: (config: Pick<PredictWorldCupConfig, 'seriesId' | 'gamesTagId'>) =>
+    buildPredictWorldCupLiveQuery(config),
+  stage: (stage: Pick<PredictWorldCupStageConfig, 'eventIds'>) =>
+    buildPredictWorldCupStageEventsQuery(stage),
+};
