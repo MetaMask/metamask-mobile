5aa5a8a5ccc3b90b699ebd639e7d76f55cd087cb
sahar-fehri <sahar.fehri@consensys.net>
2026-05-20 10:03:00 +0200

    feat: add ut

--- app/components/UI/TokenDetails/components/TokenDetailsInlineHeader.test.tsx
+++ app/components/UI/TokenDetails/components/TokenDetailsInlineHeader.test.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
 import { TokenDetailsInlineHeader } from './TokenDetailsInlineHeader';
+import { AMBIENT_NEGATIVE_COLOR } from './abTestConfig';
+
+jest.mock('@metamask/design-system-react-native', () => {
+  const actual = jest.requireActual('@metamask/design-system-react-native');
+  const { View } = jest.requireActual('react-native');
+  return {
+    ...actual,
+    ButtonIcon: (props: Record<string, unknown>) => (
+      <View {...props} testID={props.testID as string} />
+    ),
+  };
+});
 
 describe('TokenDetailsInlineHeader', () => {
   const mockOnBackPress = jest.fn();
@@ -25,5 +37,28 @@ describe('TokenDetailsInlineHeader', () => {
     fireEvent.press(getByTestId('back-arrow-button'));
 
     expect(mockOnBackPress).toHaveBeenCalledTimes(1);
+  });
+
+  it('passes iconProps with color when iconColorClass is provided', () => {
+    const { getByTestId } = render(
+      <TokenDetailsInlineHeader
+        onBackPress={mockOnBackPress}
+        iconColorClass={`text-[${AMBIENT_NEGATIVE_COLOR}]`}
+      />,
+    );
+
+    const button = getByTestId('back-arrow-button');
+    expect(button.props.iconProps).toEqual({
+      color: `text-[${AMBIENT_NEGATIVE_COLOR}]`,
+    });
+  });
+
+  it('does not pass iconProps when iconColorClass is undefined', () => {
+    const { getByTestId } = render(
+      <TokenDetailsInlineHeader onBackPress={mockOnBackPress} />,
+    );
+
+    const button = getByTestId('back-arrow-button');
+    expect(button.props.iconProps).toBeUndefined();
   });
 });
