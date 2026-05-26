89a45df00feae2178d40c93986fb4e137f0dba11
Samir Mehta <12882259+samir-acle@users.noreply.github.com>
2026-05-26 11:32:16 -0400

    fix: move notification developer options to shared UI

--- app/components/Views/Settings/DeveloperOptions/index.tsx
+++ app/components/Views/Settings/DeveloperOptions/index.tsx
@@ -23,7 +23,7 @@ import { MusdDeveloperOptionsSection } from '../../../UI/Earn/components/MusdDev
 import { CardDeveloperOptionsSection } from '../../../UI/Card/components/CardDeveloperOptionsSection';
 import { selectMoneyHomeScreenEnabledFlag } from '../../../UI/Money/selectors/featureFlags';
 import { MoneyUiDeveloperOptionsSection } from '../../../UI/Money/components/MoneyUiDeveloperOptionsSection';
-import NotificationsDeveloperOptionsSection from '../../Notifications/DeveloperOptions/NotificationsDeveloperOptionsSection';
+import NotificationsDeveloperOptionsSection from '../../../UI/Notification/DeveloperOptionsSection/NotificationsDeveloperOptionsSection';
 
 const DeveloperOptions = () => {
   const navigation = useNavigation();
