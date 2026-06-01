63eb99d2796ee93504426777eaf17449b98fd2cd
geositta <matthew.denton@consensys.net>
2026-06-01 17:23:11 -0500

    chore: bump compliance controller to 2.1.0

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ process.env.MM_FOX_CODE = 'EXAMPLE_FOX_CODE';
 
 process.env.MM_SECURITY_ALERTS_API_ENABLED = 'true';
 process.env.SECURITY_ALERTS_API_URL = 'https://example.com';
+process.env.COMPLIANCE_API_URL = 'https://compliance.example.com';
 
 process.env.LAUNCH_DARKLY_URL =
   'https://client-config.dev-api.cx.metamask.io/v1';
