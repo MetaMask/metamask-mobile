722f8c585665d9c1cec2ef3919b5d1789f68982f
Javier Garcia Vera <javier.vera@consensys.net>
2026-06-01 15:18:25 +0200

    test: fixed tags for asset-view.spec.ts

--- tests/performance/login/asset-view.spec.ts
+++ tests/performance/login/asset-view.spec.ts
@@ -4,15 +4,24 @@ import { loginToAppPlaywright } from '../../flows/wallet.flow';
 import { asPlaywrightElement, PlaywrightAssertions } from '../../framework';
 import WalletView from '../../page-objects/wallet/WalletView';
 import TokenOverview from '../../page-objects/wallet/TokenOverview';
-import { PerformanceAssetLoading } from '../../tags.performance.js';
+import {
+  Performance,
+  PerformanceLogin,
+  PerformanceAssetLoading,
+} from '../../tags.performance.js';
 
 /* Scenario 8: Asset View, SRP 1 + SRP 2 + SRP 3 */
-perfTest.describe(PerformanceAssetLoading, () => {
-  perfTest(
-    'Asset View, SRP 1 + SRP 2 + SRP 3',
-    { tag: '@assets-dev-team' },
-    async ({ currentDeviceDetails, driver, performanceTracker }, testInfo) => {
-      await loginToAppPlaywright();
+perfTest.describe(
+  `${Performance} ${PerformanceLogin} ${PerformanceAssetLoading}`,
+  () => {
+    perfTest(
+      'Asset View, SRP 1 + SRP 2 + SRP 3',
+      { tag: '@assets-dev-team' },
+      async (
+        { currentDeviceDetails, driver, performanceTracker },
+        testInfo,
+      ) => {
+        await loginToAppPlaywright();
 
         const assetViewScreen = new TimerHelper(
           'Time since the user clicks on the asset view button until the user sees the token overview screen',
