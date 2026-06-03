16222f4af8e59b1658abce716acd318b1f5dc0a0
Goktug Poyraz <omergoktugpoyraz@gmail.com>
2026-06-03 11:54:29 +0200

    chore: add mUSD token on Monad + apply TPC patch in package.json

--- app/components/Views/confirmations/components/info/money-account-deposit-info/money-account-deposit-info.tsx
+++ app/components/Views/confirmations/components/info/money-account-deposit-info/money-account-deposit-info.tsx
@@ -3,6 +3,12 @@ import { CustomAmountInfo } from '../custom-amount-info';
 import { useMoneyAccountDepositNavbar } from '../../../../../UI/Money/hooks/useMoneyAccountDepositNavbar';
 import { useParams } from '../../../../../../util/navigation/navUtils';
 import { ConfirmationParams } from '../../confirm/confirm-component';
+import { useAddToken } from '../../../hooks/tokens/useAddToken';
+import { CHAIN_IDS } from '@metamask/transaction-controller';
+import {
+  MUSD_TOKEN,
+  MUSD_TOKEN_ADDRESS_BY_CHAIN,
+} from '../../../../../UI/Earn/constants/musd';
 
 export const MONEY_ACCOUNT_CURRENCY = 'usd';
 
@@ -12,6 +18,14 @@ export function MoneyAccountDepositInfo() {
 
   const params = useParams<ConfirmationParams>();
   const autoFiat = params?.autoSelectFiatPayment;
+
+  useAddToken({
+    chainId: CHAIN_IDS.MONAD,
+    decimals: MUSD_TOKEN.decimals,
+    name: MUSD_TOKEN.name,
+    symbol: MUSD_TOKEN.symbol,
+    tokenAddress: MUSD_TOKEN_ADDRESS_BY_CHAIN[CHAIN_IDS.MONAD],
+  });
 
   return (
     <CustomAmountInfo
