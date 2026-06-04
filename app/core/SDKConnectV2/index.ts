db0be4bad513cff975f1ca0f640e5925cd206cf6
ieow <4881057+ieow@users.noreply.github.com>
2026-06-04 11:40:43 +0800

    fix: revert relayUrl changes

--- app/core/SDKConnectV2/index.ts
+++ app/core/SDKConnectV2/index.ts
@@ -2,9 +2,8 @@ import { HostApplicationAdapter } from './adapters/host-application-adapter';
 import { ConnectionStore } from './store/connection-store';
 import { ConnectionRegistry } from './services/connection-registry';
 import { KeyManager } from './services/key-manager';
-import { getMwpRelayUrl } from './utils/relayUrl';
 
-const relayURL = getMwpRelayUrl();
+const relayURL = 'wss://mm-sdk-relay.api.cx.metamask.io/connection/websocket';
 const keymanager = new KeyManager();
 const hostapp = new HostApplicationAdapter();
 const store = new ConnectionStore('sdk-connect-v2/connections');
