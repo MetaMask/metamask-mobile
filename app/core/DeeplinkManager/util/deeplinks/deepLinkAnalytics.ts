28a5494ceea3975f5a7bedb87d9dccf47ab16869
Stylianos Panagakos <stylianos.panagakos@consensys.net>
2026-06-05 15:41:54 +0100

    fix: lint

--- app/core/DeeplinkManager/util/deeplinks/deepLinkAnalytics.ts
+++ app/core/DeeplinkManager/util/deeplinks/deepLinkAnalytics.ts
@@ -461,6 +461,7 @@ const routeExtractors: Record<
   (urlParams: UrlParamValues, sensitiveProps: Record<string, string>) => void
 > = {
   [DeepLinkRoute.SWAP]: extractSwapProperties,
+  [DeepLinkRoute.BATCH_SELL]: extractInvalidProperties,
   [DeepLinkRoute.PERPS]: extractPerpsProperties,
   [DeepLinkRoute.TRANSACTION]: extractTransactionProperties,
   [DeepLinkRoute.BUY]: extractBuyProperties,
@@ -644,6 +645,8 @@ export const extractRouteFromUrl = (url: string): DeepLinkRoute => {
     switch (route) {
       case 'swap':
         return DeepLinkRoute.SWAP;
+      case 'batch-sell':
+        return DeepLinkRoute.BATCH_SELL;
       case 'perps':
         return DeepLinkRoute.PERPS;
       case 'deposit':
