14a59271912576158d59544ccd70b027973f95cc
Wei Sun <wei.sun@consensys.net>
2026-06-05 10:15:00 -0700

    fix e2e snap goBack buttons

--- app/components/Views/Snaps/SnapSettings/SnapSettings.constants.ts
+++ app/components/Views/Snaps/SnapSettings/SnapSettings.constants.ts
@@ -1,8 +1,6 @@
 ///: BEGIN:ONLY_INCLUDE_IF(snaps)
-import { CommonSelectorsIDs } from '../../../../util/Common.testIds';
-
 export const SNAP_SETTINGS_HEADER = 'snap-settings-header';
-export const SNAP_SETTINGS_BACK_BUTTON = CommonSelectorsIDs.BACK_ARROW_BUTTON;
+export const SNAP_SETTINGS_BACK_BUTTON = 'snap-settings-back-button';
 export const SNAP_SETTINGS_REMOVE_BUTTON = 'snap-settings-remove-button';
 export const SNAP_SETTINGS_SCROLLVIEW = 'snap-settings-scrollview';
 ///: END:ONLY_INCLUDE_IF
--- app/components/Views/Snaps/SnapsSettingsList/SnapsSettingsList.constants.ts
+++ app/components/Views/Snaps/SnapsSettingsList/SnapsSettingsList.constants.ts
@@ -1,10 +1,9 @@
 ///: BEGIN:ONLY_INCLUDE_IF(snaps)
 import { TextColor } from '@metamask/design-system-react-native';
-import { CommonSelectorsIDs } from '../../../../util/Common.testIds';
 
 export const SNAPS_SETTINGS_LIST_HEADER = 'snaps-settings-list-header';
 export const SNAPS_SETTINGS_LIST_BACK_BUTTON =
-  CommonSelectorsIDs.BACK_ARROW_BUTTON;
+  'snaps-settings-list-back-button';
 
 /** Matches legacy `getNavigationOptionsTitle` primary header title color. */
 export const SNAPS_HEADER_TITLE_PROPS = {
