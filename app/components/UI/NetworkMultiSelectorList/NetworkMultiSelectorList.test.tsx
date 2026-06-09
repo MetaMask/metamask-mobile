45f77d30a154e249501e212c053084931ee051d9
Frederic HENG <frederic.heng@consensys.net>
2026-06-09 11:09:29 +0200

    style: fix Prettier formatting in NetworkMultiSelectorList test

--- app/components/UI/NetworkMultiSelectorList/NetworkMultiSelectorList.test.tsx
+++ app/components/UI/NetworkMultiSelectorList/NetworkMultiSelectorList.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet , Text } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { render, fireEvent, act } from '@testing-library/react-native';
 import { useSelector } from 'react-redux';
 import { parseCaipChainId, CaipChainId } from '@metamask/utils';
