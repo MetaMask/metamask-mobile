94480d80b96fbd36bdf999d247d9f0fe231c9def
0xEdouard <15703023+0xEdouardEth@users.noreply.github.com>
2026-06-11 14:45:52 +0200

    fix: remove unused BitcoinTestDappSelectorsWebIDs

--- tests/selectors/Browser/BitcoinTestDapp.selectors.ts
+++ tests/selectors/Browser/BitcoinTestDapp.selectors.ts
@@ -5,11 +5,3 @@ export interface BitcoinTestDappWebIDs {
   WALLET_BUTTON: string;
   CONFIRM_SIGN_MESSAGE_BUTTON: string;
 }
-
-/**
- * Web element IDs for the Bitcoin Test Dapp
- */
-export const BitcoinTestDappSelectorsWebIDs: BitcoinTestDappWebIDs = {
-  WALLET_BUTTON: '.wallet-adapter-modal-list .wallet-adapter-button', // Important space between classes to indicate a parent-child relationship
-  CONFIRM_SIGN_MESSAGE_BUTTON: 'confirmation-confirm-snap-footer-button',
-};
