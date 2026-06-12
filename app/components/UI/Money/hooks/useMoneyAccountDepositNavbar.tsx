8f1fdd95370ccea25620bf11d45c5a8dc765cfd5
Alexey Kureev <a.g.kureev@gmail.com>
2026-06-12 10:39:35 +0200

    fix(money): inline single-use deposit navbar hook (MUSD-945)

--- app/components/UI/Money/hooks/useMoneyAccountDepositNavbar.tsx
+++
@@ -1,6 +0,0 @@
-import { strings } from '../../../../../locales/i18n';
-import useNavbar from '../../../Views/confirmations/hooks/ui/useNavbar';
-
-export function useMoneyAccountDepositNavbar() {
-  useNavbar(strings('confirm.title.money_account_add_money'), true);
-}
