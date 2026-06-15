e86ab087d0f644b32dc31586d015a925f2f884f5
Charly Chevalier <charly.chevalier@consensys.net>
2026-06-15 10:15:25 +0200

    fix: remove conflict sections

--- app/core/Engine/controllers/snap-account-service/snap-account-service-init.ts
+++ app/core/Engine/controllers/snap-account-service/snap-account-service-init.ts
@@ -1,11 +1,7 @@
 import { SnapAccountService } from '@metamask/snap-account-service';
 import type { MessengerClientInitFunction } from '../../types';
 import type { SnapAccountServiceMessenger } from '../../messengers/snap-account-service-messenger/snap-account-service-messenger';
-<<<<<<< HEAD
-import { createEnsureOnboardingCompleteCallback } from '../../utils/ensureOnboardingComplete';
-=======
 import { ensureOnboardingComplete } from '../../utils/ensureOnboardingComplete';
->>>>>>> main
 
 /**
  * Initialize the Snap account service.
@@ -22,11 +18,7 @@ export const snapAccountServiceInit: MessengerClientInitFunction<
     messenger: controllerMessenger,
     config: {
       snapPlatformWatcher: {
-<<<<<<< HEAD
-        ensureOnboardingComplete: createEnsureOnboardingCompleteCallback(),
-=======
         ensureOnboardingComplete,
->>>>>>> main
       },
     },
   });
