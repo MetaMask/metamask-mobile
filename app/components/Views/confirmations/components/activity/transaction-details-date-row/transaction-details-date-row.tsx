2461fd90b20a2e41b1f8352aa16ee62b37289c82
Patryk Lucka <5708018+PatrykLucka@users.noreply.github.com>
2026-06-24 16:57:20 +0200

    chore: revert changes in old transaction details row

--- app/components/Views/confirmations/components/activity/transaction-details-date-row/transaction-details-date-row.tsx
+++ app/components/Views/confirmations/components/activity/transaction-details-date-row/transaction-details-date-row.tsx
@@ -2,15 +2,29 @@ import React from 'react';
 import { useTransactionDetails } from '../../../hooks/activity/useTransactionDetails';
 import { TransactionDetailsRow } from '../transaction-details-row/transaction-details-row';
 import Text from '../../../../../../component-library/components/Texts/Text';
-import { formatTimestampToDateTime } from '../../../../../../util/date';
-import { strings } from '../../../../../../../locales/i18n';
+import { getIntlDateTimeFormatter } from '../../../../../../util/intl';
+import I18n, { strings } from '../../../../../../../locales/i18n';
 
 export function TransactionDetailsDateRow() {
   const { transactionMeta } = useTransactionDetails();
 
+  const date = new Date(transactionMeta.time);
+
+  const month = getIntlDateTimeFormatter(I18n.locale, {
+    month: 'short',
+  }).format(date);
+
+  const timeString = getIntlDateTimeFormatter(I18n.locale, {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  }).format(date);
+
+  const dateString = `${month} ${date.getDate()}, ${date.getFullYear()} at ${timeString}`;
+
   return (
     <TransactionDetailsRow label={strings('transactions.date')}>
-      <Text>{formatTimestampToDateTime(transactionMeta.time)}</Text>
+      <Text>{dateString}</Text>
     </TransactionDetailsRow>
   );
 }
