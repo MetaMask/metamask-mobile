d94e9fbb46fd37079efbbbd8e9863c08f5519700
Wei Sun <wei.sun@consensys.net>
2026-06-29 12:51:55 -0700

    chore(navigation): complete RootStackParamList for market insights and perps routes

--- app/core/NavigationService/types.ts
+++ app/core/NavigationService/types.ts
@@ -59,6 +59,7 @@ import type { OnboardingCryptoExperienceQuestionnaireRouteParams } from '../../c
 // Perps navigation params
 import type { PerpsNavigationParamList } from '../../components/UI/Perps/types/navigation';
 import type { TrendingTokensFullViewParams } from '../../components/UI/Trending/Views/TrendingTokensFullView/TrendingTokensFullView';
+import type { MarketInsightsRouteParams } from '../../components/UI/MarketInsights/Views/MarketInsightsView/MarketInsightsView';
 
 // QR Scanner params
 import type { QRScannerParams } from '../../components/Views/QRScanner/QRScanner.types';
@@ -421,6 +422,7 @@ export type RootStackParamList = {
     | undefined;
   SitesFullView: { mode?: 'favorites' } | undefined;
   ExploreSearch: undefined;
+  MarketInsightsView: MarketInsightsRouteParams;
   RewardsOnboardingFlow: undefined;
   RewardsOnboardingIntro: undefined;
   BenefitFullView: BenefitFullViewRouteParams;
@@ -650,6 +652,7 @@ export type RootStackParamList = {
   // leaderboard into PerpsMarketDetails).
   Perps: NestedNavigationParams | PerpsNavigationParamList['Perps'];
   PerpsTradingView: PerpsNavigationParamList['PerpsTradingView'];
+  PerpsOrderRedirect: PerpsNavigationParamList['PerpsOrderRedirect'];
   PerpsWithdraw: PerpsNavigationParamList['PerpsWithdraw'];
   PerpsPositions: PerpsNavigationParamList['PerpsPositions'];
   PerpsMarketListView: PerpsNavigationParamList['PerpsMarketListView'];
@@ -675,6 +678,7 @@ export type RootStackParamList = {
   PerpsCancelAllOrders: undefined;
   PerpsTooltip: undefined;
   PerpsCrossMarginWarning: undefined;
+  PerpsSelectProvider: PerpsNavigationParamList['PerpsSelectProvider'];
   PerpsPositionTransaction: PerpsNavigationParamList['PerpsPositionTransaction'];
   PerpsOrderTransaction: PerpsNavigationParamList['PerpsOrderTransaction'];
   PerpsFundingTransaction: PerpsNavigationParamList['PerpsFundingTransaction'];
