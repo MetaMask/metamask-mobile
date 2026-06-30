7476cdf0466e8a0fe4e81ceb6a15b14ecbb6c136
Caainã Jeronimo <caainaje@gmail.com>
2026-06-30 14:34:23 -0300

    chore: move const

--- app/components/UI/Predict/utils/resolvePredictFeatureFlags.test.ts
+++ app/components/UI/Predict/utils/resolvePredictFeatureFlags.test.ts
@@ -840,7 +840,6 @@ describe('resolvePredictFeatureFlags', () => {
         'soccer_team_totals',
         'basketball_team_to_score_first',
         'soccer_exact_score',
-        'soccer_team_to_advance',
       ]);
     });
 
