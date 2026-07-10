0ec962d75e8329aebf43b9929dd38c5fe06cefe7
Baptiste Marchand <75846779+baptiste-marchand@users.noreply.github.com>
2026-07-10 12:28:30 +0200

    fix: replace legacy push notification keys

--- app/core/Engine/controllers/notifications/create-push-message.test.ts
+++ app/core/Engine/controllers/notifications/create-push-message.test.ts
@@ -61,8 +61,8 @@ describe('notification-message tests', () => {
     );
     const result = createNotificationMessage(notification);
 
-    expect(result?.title).toBe('Swap completed');
-    expect(result?.description).toContain('Your MetaMask Swap was successful');
+    expect(result?.title).toBe('Swap complete');
+    expect(result?.description).toContain('Your swap is complete.');
   });
 
   test('displays erc721 sent notification', () => {
@@ -111,9 +111,9 @@ describe('notification-message tests', () => {
     );
     const result = createNotificationMessage(notification);
 
-    expect(result?.title).toBe('Stake complete');
+    expect(result?.title).toBe('Stake deposited');
     expect(result?.description).toContain(
-      'Your RocketPool stake was successful',
+      'Your Rocket Pool stake deposit is complete.',
     );
   });
 
@@ -125,7 +125,7 @@ describe('notification-message tests', () => {
 
     expect(result?.title).toBe('Unstake complete');
     expect(result?.description).toContain(
-      'Your RocketPool unstake was successful',
+      'Your Rocket Pool unstake is complete.',
     );
   });
 
@@ -135,8 +135,10 @@ describe('notification-message tests', () => {
     );
     const result = createNotificationMessage(notification);
 
-    expect(result?.title).toBe('Stake complete');
-    expect(result?.description).toContain('Your Lido stake was successful');
+    expect(result?.title).toBe('Stake deposited');
+    expect(result?.description).toContain(
+      'Your Lido stake deposit is complete.',
+    );
   });
 
   test('displays lido stake ready to be withdrawn notification', () => {
@@ -145,9 +147,9 @@ describe('notification-message tests', () => {
     );
     const result = createNotificationMessage(notification);
 
-    expect(result?.title).toBe('Stake ready for withdrawal');
+    expect(result?.title).toBe('Stake ready to withdraw');
     expect(result?.description).toContain(
-      'Your Lido stake is now ready to be withdrawn',
+      'Your Lido stake is ready to withdraw.',
     );
   });
 
@@ -159,7 +161,7 @@ describe('notification-message tests', () => {
 
     expect(result?.title).toBe('Withdrawal requested');
     expect(result?.description).toContain(
-      'Your Lido withdrawal request was submitted',
+      'Your Lido withdrawal request was submitted.',
     );
   });
 
@@ -169,9 +171,7 @@ describe('notification-message tests', () => {
     );
     const result = createNotificationMessage(notification);
 
-    expect(result?.title).toBe('Withdrawal completed');
-    expect(result?.description).toContain(
-      'Your Lido withdrawal was successful',
-    );
+    expect(result?.title).toBe('Withdrawal complete');
+    expect(result?.description).toContain('Your Lido withdrawal is complete.');
   });
 });
