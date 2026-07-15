3cc7495e57e3d713fc96ac61f70114d9b1a7f0cd
IF <139582705+infiniteflower@users.noreply.github.com>
2026-07-15 19:10:31 -0400

    chore: memoize quote row view

--- app/components/UI/Bridge/components/QuoteSelectorView/QuoteRow.tsx
+++ app/components/UI/Bridge/components/QuoteSelectorView/QuoteRow.tsx
@@ -43,108 +43,113 @@ export interface QuoteRowViewProps extends QuoteRowProps {
   destTokenSymbol?: string;
 }
 
-export const QuoteRowView = ({
-  selected,
-  provider,
-  isLowestCost,
-  formattedTotalCost,
-  formattedReceiveAmountFiat,
-  destTokenSymbol,
-  onPress,
-  quoteRequestId,
-  loading,
-  receiveAmount,
-}: QuoteRowViewProps) => {
-  const theme = useTheme();
-  const formattedReceiveAmount = useMemo(
-    () =>
-      receiveAmount && receiveAmount !== '0' && destTokenSymbol
-        ? formatAmountWithLocaleSeparators(
-            limitToMaximumDecimalPlaces(parseFloat(receiveAmount)),
-          )
-        : receiveAmount,
-    [receiveAmount, destTokenSymbol],
-  );
+export const QuoteRowView = React.memo(
+  ({
+    selected,
+    provider,
+    isLowestCost,
+    formattedTotalCost,
+    formattedReceiveAmountFiat,
+    destTokenSymbol,
+    onPress,
+    quoteRequestId,
+    loading,
+    receiveAmount,
+  }: QuoteRowViewProps) => {
+    const theme = useTheme();
+    const formattedReceiveAmount = useMemo(
+      () =>
+        receiveAmount && receiveAmount !== '0' && destTokenSymbol
+          ? formatAmountWithLocaleSeparators(
+              limitToMaximumDecimalPlaces(parseFloat(receiveAmount)),
+            )
+          : receiveAmount,
+      [receiveAmount, destTokenSymbol],
+    );
 
-  return (
-    <TouchableOpacity onPress={() => onPress(quoteRequestId)}>
-      <Box
-        paddingVertical={3}
-        paddingHorizontal={4}
-        backgroundColor={
-          selected
-            ? BoxBackgroundColor.BackgroundMuted
-            : BoxBackgroundColor.BackgroundDefault
-        }
-        gap={2}
-        flexDirection={BoxFlexDirection.Row}
-        justifyContent={BoxJustifyContent.Between}
-        alignItems={BoxAlignItems.Center}
-      >
-        <Box gap={1}>
+    return (
+      <TouchableOpacity onPress={() => onPress(quoteRequestId)}>
+        <Box
+          paddingVertical={3}
+          paddingHorizontal={4}
+          backgroundColor={
+            selected
+              ? BoxBackgroundColor.BackgroundMuted
+              : BoxBackgroundColor.BackgroundDefault
+          }
+          gap={2}
+          flexDirection={BoxFlexDirection.Row}
+          justifyContent={BoxJustifyContent.Between}
+          alignItems={BoxAlignItems.Center}
+        >
+          <Box gap={1}>
+            <Box
+              gap={1}
+              flexDirection={BoxFlexDirection.Row}
+              alignItems={BoxAlignItems.Center}
+            >
+              <Skeleton hideChildren={loading}>
+                <View>
+                  <Text
+                    fontWeight={FontWeight.Medium}
+                    variant={TextVariantDS.BodyMd}
+                  >
+                    {provider.name}
+                  </Text>
+                </View>
+              </Skeleton>
+              <View>
+                {isLowestCost && !loading && (
+                  <TagBase
+                    shape={TagShape.Rectangle}
+                    textProps={{
+                      variant: TextVariant.BodySM,
+                      style: { fontWeight: 500 },
+                      color: theme.colors.success.default,
+                    }}
+                    severity={TagSeverity.Success}
+                  >
+                    {strings('bridge.lowest_cost')}
+                  </TagBase>
+                )}
+              </View>
+            </Box>
+            <Skeleton hideChildren={loading}>
+              <Text
+                variant={TextVariantDS.BodySm}
+                color={TextColor.TextAlternative}
+              >
+                {strings('bridge.total_cost')}: {formattedTotalCost}
+              </Text>
+            </Skeleton>
+          </Box>
           <Box
+            justifyContent={BoxJustifyContent.Center}
+            alignItems={BoxAlignItems.End}
             gap={1}
-            flexDirection={BoxFlexDirection.Row}
-            alignItems={BoxAlignItems.Center}
           >
             <Skeleton hideChildren={loading}>
-              <View>
-                <Text
-                  fontWeight={FontWeight.Medium}
-                  variant={TextVariantDS.BodyMd}
-                >
-                  {provider.name}
-                </Text>
-              </View>
+              <Text
+                variant={TextVariantDS.BodyMd}
+                fontWeight={FontWeight.Medium}
+              >
+                ~ {formattedReceiveAmount} {destTokenSymbol}
+              </Text>
             </Skeleton>
-            <View>
-              {isLowestCost && !loading && (
-                <TagBase
-                  shape={TagShape.Rectangle}
-                  textProps={{
-                    variant: TextVariant.BodySM,
-                    style: { fontWeight: 500 },
-                    color: theme.colors.success.default,
-                  }}
-                  severity={TagSeverity.Success}
-                >
-                  {strings('bridge.lowest_cost')}
-                </TagBase>
-              )}
-            </View>
+            <Skeleton hideChildren={loading}>
+              <Text
+                variant={TextVariantDS.BodySm}
+                color={TextColor.TextAlternative}
+              >
+                ~ {formattedReceiveAmountFiat}
+              </Text>
+            </Skeleton>
           </Box>
-          <Skeleton hideChildren={loading}>
-            <Text
-              variant={TextVariantDS.BodySm}
-              color={TextColor.TextAlternative}
-            >
-              {strings('bridge.total_cost')}: {formattedTotalCost}
-            </Text>
-          </Skeleton>
         </Box>
-        <Box
-          justifyContent={BoxJustifyContent.Center}
-          alignItems={BoxAlignItems.End}
-          gap={1}
-        >
-          <Skeleton hideChildren={loading}>
-            <Text variant={TextVariantDS.BodyMd} fontWeight={FontWeight.Medium}>
-              ~ {formattedReceiveAmount} {destTokenSymbol}
-            </Text>
-          </Skeleton>
-          <Skeleton hideChildren={loading}>
-            <Text
-              variant={TextVariantDS.BodySm}
-              color={TextColor.TextAlternative}
-            >
-              ~ {formattedReceiveAmountFiat}
-            </Text>
-          </Skeleton>
-        </Box>
-      </Box>
-    </TouchableOpacity>
-  );
-};
+      </TouchableOpacity>
+    );
+  },
+);
 
 export const QuoteRow = (props: QuoteRowProps) => {
   const destToken = useSelector(selectDestToken);
