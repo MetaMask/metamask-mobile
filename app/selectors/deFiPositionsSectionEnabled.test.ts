e2adfd239a1c2178bcfb940d851c6a95603b6914
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2026-07-29 11:02:30 +0100

    fix test

--- app/selectors/deFiPositionsSectionEnabled.test.ts
+++ app/selectors/deFiPositionsSectionEnabled.test.ts
@@ -1,21 +1,27 @@
 import { selectDeFiPositionsSectionEnabled } from './deFiPositionsSectionEnabled';
 
 describe('selectDeFiPositionsSectionEnabled', () => {
-  it('returns true when V2 is disabled and basic functionality is enabled', () => {
-    expect(selectDeFiPositionsSectionEnabled.resultFunc(false, true)).toBe(
-      true,
-    );
+  it('returns true when assets DeFi is enabled, V2 is disabled, and basic functionality is enabled', () => {
+    expect(
+      selectDeFiPositionsSectionEnabled.resultFunc(true, false, true),
+    ).toBe(true);
   });
 
   it('returns false when V2 is enabled', () => {
-    expect(selectDeFiPositionsSectionEnabled.resultFunc(true, true)).toBe(
+    expect(selectDeFiPositionsSectionEnabled.resultFunc(true, true, true)).toBe(
       false,
     );
   });
 
   it('returns false when basic functionality is disabled', () => {
-    expect(selectDeFiPositionsSectionEnabled.resultFunc(false, false)).toBe(
-      false,
-    );
+    expect(
+      selectDeFiPositionsSectionEnabled.resultFunc(true, false, false),
+    ).toBe(false);
+  });
+
+  it('returns false when assets DeFi positions is disabled', () => {
+    expect(
+      selectDeFiPositionsSectionEnabled.resultFunc(false, false, true),
+    ).toBe(false);
   });
 });
