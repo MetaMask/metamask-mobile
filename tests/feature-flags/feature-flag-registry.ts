fabf5de6e547bdd1c36e077318d1dae0d2673d15
Caainã Jeronimo <caainaje@gmail.com>
2026-07-29 16:08:15 -0300

    fix: flag registry

--- tests/feature-flags/feature-flag-registry.ts
+++ tests/feature-flags/feature-flag-registry.ts
@@ -800,7 +800,7 @@ export const FEATURE_FLAG_REGISTRY: Record<string, FeatureFlagRegistryEntry> = {
     type: FeatureFlagType.Remote,
     inProd: true,
     productionDefault: {
-      enabled: false,
+      enabled: true,
       minimumVersion: '0.0.0',
     },
     status: FeatureFlagStatus.Active,
@@ -5574,7 +5574,7 @@ export const FEATURE_FLAG_REGISTRY: Record<string, FeatureFlagRegistryEntry> = {
     type: FeatureFlagType.Remote,
     inProd: true,
     productionDefault: {
-      enabled: false,
+      enabled: true,
       minimumVersion: '0.0.0',
     },
     status: FeatureFlagStatus.Active,
