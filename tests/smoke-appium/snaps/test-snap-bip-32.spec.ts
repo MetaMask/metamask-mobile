eaf88073502dade332f7c0f1a0217239734775d2
cmd-ob <ola.bale@consensys.net>
2026-08-01 08:09:43 +0100

    test(e2e): configure BIP-32 Snap tests to run in serial mode

--- tests/smoke-appium/snaps/test-snap-bip-32.spec.ts
+++ tests/smoke-appium/snaps/test-snap-bip-32.spec.ts
@@ -11,7 +11,7 @@ const multiSrpFixture = new FixtureBuilder()
   .build();
 
 appiumTest.describe(SmokeSnaps('BIP-32 Snap Tests'), () => {
-  appiumTest.describe.configure({ timeout: 150_000 });
+  appiumTest.describe.configure({ mode: 'serial', timeout: 150_000 });
 
   appiumTest(
     'can connect to BIP-32 snap',
