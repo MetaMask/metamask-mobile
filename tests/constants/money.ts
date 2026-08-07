3976465a279757f5d6cddf3d16d26822b691f16d
Matthew Grainger <matthew.grainger@consensys.net>
2026-08-07 13:42:42 -0400

    feat: changed MONEY_FUNDED_USER_SRP to point to TEST_SRP_3

--- tests/constants/money.ts
+++ tests/constants/money.ts
@@ -1,2 +1,2 @@
 export const MONEY_NEW_USER_SRP = process.env.TEST_SRP_1 ?? '';
-export const MONEY_FUNDED_USER_SRP = process.env.TEST_SRP_2 ?? '';
+export const MONEY_FUNDED_USER_SRP = process.env.TEST_SRP_3 ?? '';
