b5a9c7d31d142682a7da607901375ef1e5b62a6d
Arthur Breton <arthur.breton@consensys.net>
2026-08-21 23:37:37 +0800

    cleanup(pr-TAT-3639): remove reproduction marker

--- app/components/UI/Perps/Views/PerpsOrderView/PerpsOrderView.tsx
+++ app/components/UI/Perps/Views/PerpsOrderView/PerpsOrderView.tsx
@@ -1169,11 +1169,6 @@ const PerpsOrderViewContentBase: React.FC<PerpsOrderViewContentProps> = ({
   const hasInsufficientFundsError =
     hasInsufficientPayTokenBalance ||
     filteredErrors.some((error) => error.startsWith(insufficientBalancePrefix));
-  if (hasInsufficientFundsError) {
-    DevLogger.log(
-      '[PR-TAT-3639] BUG_MARKER: duplicate insufficient-funds surfaces',
-    );
-  }
 
   // Handlers
   const handleTPSLPress = useCallback(() => {
