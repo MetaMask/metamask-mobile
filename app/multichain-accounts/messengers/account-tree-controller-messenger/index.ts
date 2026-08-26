b46c5621e9c4a82b85ba35a6adb1787bb28ed715
gantunesr <17601467+gantunesr@users.noreply.github.com>
2026-08-26 18:15:20 -0300

    chore: remove redundant comment

--- app/multichain-accounts/messengers/account-tree-controller-messenger/index.ts
+++ app/multichain-accounts/messengers/account-tree-controller-messenger/index.ts
@@ -42,7 +42,6 @@ export function getAccountTreeControllerMessenger(
       'AuthenticationController:getSessionProfile',
       'MultichainAccountService:createMultichainAccountGroup',
       'MultichainAccountService:createMultichainAccountGroups',
-      // account-tree-controller ^8.0.0: required for :{import,export}State
       'MultichainAccountService:createMultichainAccountWallet',
       'SnapController:getSnap',
       'KeyringController:getState',
