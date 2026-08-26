bb4aec8fa3e20a798fc36fcd70399fc7429c4dd7
micaelae <micaela.estabillo@consensys.net>
2026-08-26 14:36:33 -0700

    refactor: move useQuoteData

--- app/components/UI/Bridge/hooks/useSwapQuotes/SwapQuotesContext.tsx
+++ app/components/UI/Bridge/hooks/useSwapQuotes/SwapQuotesContext.tsx
@@ -1,6 +1,12 @@
 import React, { createContext, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+
 import { type UseQuoteRequestParams, useQuoteRequest } from './useQuoteRequest';
-import { useQuoteData, type UseQuoteDataParams } from './useQuoteData';
+import { selectBridgeControllerState } from '../../../../../core/redux/slices/bridge';
+import { useValidQuotes } from '../useValidQuotes';
+import { useBlockaidError } from '../useBlockaidError';
+import { useFormattedQuoteData } from '../useFormattedQuoteData';
+import type { UseBridgeQuotesParams } from './types';
 
 export type SwapQuotesContextValue = ReturnType<typeof useQuoteRequest> &
   ReturnType<typeof useQuoteData>;
@@ -14,6 +20,78 @@ interface SwapQuotesProviderProps
     UseQuoteDataParams {
   children: React.ReactNode;
 }
+
+export interface UseQuoteDataParams extends UseBridgeQuotesParams {}
+
+const useQuoteData = ({
+  latestSourceAtomicBalance,
+  quoteParams,
+}: UseQuoteDataParams) => {
+  const { quoteFetchError, quotesLoadingStatus } = useSelector(
+    selectBridgeControllerState,
+  );
+
+  // Resolve active quote and availability status
+  const {
+    activeQuote,
+    bestQuote,
+    isActiveQuoteForCurrentTokenPair,
+    isExpired,
+    isLoading,
+    isNoQuotesAvailable,
+    needsNewQuote,
+    validQuotes,
+    willRefresh,
+  } = useValidQuotes({ latestSourceAtomicBalance, quoteParams });
+
+  // Validate solana quotes
+  const blockaidError = useBlockaidError({ activeQuote });
+
+  // Format quote data
+  const { destTokenAmount, formattedQuoteData, shouldShowPriceImpactWarning } =
+    useFormattedQuoteData({
+      activeQuote,
+      isActiveQuoteForCurrentTokenPair,
+      quoteParams,
+    });
+
+  return useMemo(
+    () => ({
+      activeQuote,
+      bestQuote,
+      blockaidError,
+      destTokenAmount,
+      formattedQuoteData,
+      isActiveQuoteForCurrentTokenPair,
+      isExpired,
+      isLoading,
+      isNoQuotesAvailable,
+      needsNewQuote,
+      quoteFetchError,
+      quotesLoadingStatus,
+      shouldShowPriceImpactWarning,
+      validQuotes,
+      willRefresh,
+    }),
+    [
+      activeQuote,
+      bestQuote,
+      blockaidError,
+      destTokenAmount,
+      formattedQuoteData,
+      isActiveQuoteForCurrentTokenPair,
+      isExpired,
+      isLoading,
+      isNoQuotesAvailable,
+      needsNewQuote,
+      quoteFetchError,
+      quotesLoadingStatus,
+      shouldShowPriceImpactWarning,
+      validQuotes,
+      willRefresh,
+    ],
+  );
+};
 
 export function SwapQuotesProvider({
   children,
