f5d28482b698f07ceaa8c9546722f00eb1adffca
Frederik Bolding <frederik.bolding@gmail.com>
2026-09-02 12:23:25 +0200

    Use binds

--- app/core/RPCMethods/RPCMethodMiddleware.ts
+++ app/core/RPCMethods/RPCMethodMiddleware.ts
@@ -568,7 +568,10 @@ export const getRpcMethodMiddleware = ({
               resolve(undefined);
             },
             {
-              getAccounts: () => getPermittedAccounts(origin),
+              getAccounts: getPermittedAccounts.bind(
+                getPermittedAccounts,
+                origin,
+              ),
               getPermissionsForOrigin:
                 Engine.context.PermissionController.getPermissions.bind(
                   Engine.context.PermissionController,
@@ -596,7 +599,10 @@ export const getRpcMethodMiddleware = ({
                 resolve(undefined);
               },
               {
-                getAccounts: (...args) => getPermittedAccounts(origin, ...args),
+                getAccounts: getPermittedAccounts.bind(
+                  getPermittedAccounts,
+                  origin,
+                ),
                 getCaip25PermissionFromLegacyPermissionsForOrigin: (
                   requestedPermissions?: RequestedPermissions,
                 ) =>
@@ -678,8 +684,10 @@ export const getRpcMethodMiddleware = ({
                 resolve(undefined);
               },
               {
-                getAccounts: (opts?: { ignoreLock?: boolean }) =>
-                  getPermittedAccounts(origin, opts),
+                getAccounts: getPermittedAccounts.bind(
+                  getPermittedAccounts,
+                  origin,
+                ),
                 getCaip25PermissionFromLegacyPermissionsForOrigin: (
                   requestedPermissions?: RequestedPermissions,
                 ) =>
