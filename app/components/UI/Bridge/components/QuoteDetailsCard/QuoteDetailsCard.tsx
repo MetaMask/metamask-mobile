35ec8bc2b7ce5c8deccbe443a397de22b994e91f
Bryan Fullam <bryan.fullam@consensys.net>
2026-09-10 16:54:42 +0300

    feat: surface relayer fee in quote details

--- app/components/UI/Bridge/components/QuoteDetailsCard/QuoteDetailsCard.tsx
+++ app/components/UI/Bridge/components/QuoteDetailsCard/QuoteDetailsCard.tsx
@@ -1,5 +1,7 @@
 import React, { useMemo } from 'react';
 import { TouchableOpacity, Platform, UIManager } from 'react-native';
+import { BigNumber } from 'bignumber.js';
+import { sumAmounts } from '@metamask/bridge-controller';
 import { useNavigation } from '@react-navigation/native';
 import type { AppNavigationProp } from '../../../../../core/NavigationService/types';
 import { strings } from '../../../../../../locales/i18n';
@@ -56,6 +58,8 @@ import KeyValueRowLabel from '../../../../../component-library/components-temp/K
 import { usePriceImpactViewData } from '../../hooks/usePriceImpactViewData';
 import AppConstants from '../../../../../core/AppConstants';
 import { parsePriceImpact } from '../../utils/getPriceImpactViewData';
+import formatFiat from '../../../../../util/formatFiat';
+import { selectCurrentCurrency } from '../../../../../selectors/currencyRateController';
 
 if (
   Platform.OS === 'android' &&
@@ -82,6 +86,7 @@ const QuoteDetailsCard: React.FC<QuoteDetailsCardProps> = ({
   const sourceToken = useSelector(selectSourceToken);
   const destToken = useSelector(selectDestToken);
   const sourceAmount = useSelector(selectSourceAmount);
+  const currency = useSelector(selectCurrentCurrency);
   const {
     estimatedPoints,
     isLoading: isRewardsLoading,
@@ -142,6 +147,21 @@ const QuoteDetailsCard: React.FC<QuoteDetailsCardProps> = ({
   };
 
   const isGasless = isGaslessQuote(activeQuote?.quote);
+
+  const relayerFee = activeQuote?.quote?.feeData?.relayer;
+  const formattedRelayerFee = useMemo(() => {
+    if (!relayerFee) {
+      return '-';
+    }
+
+    const fee = sumAmounts(relayerFee);
+    const valueInCurrency = fee?.valueInCurrency;
+    if (valueInCurrency == null) {
+      return '-';
+    }
+
+    return formatFiat(new BigNumber(valueInCurrency), currency);
+  }, [currency, relayerFee]);
 
   const formattedMinToTokenAmount = formatMinimumReceived(
     activeQuote?.quote.dest?.minAmountNormalized || '0',
@@ -301,6 +321,25 @@ const QuoteDetailsCard: React.FC<QuoteDetailsCardProps> = ({
             value={{
               label: {
                 text: formattedQuoteData.networkFee,
+                variant: TextVariantLegacy.BodyMD,
+                color: TextColorLegacy.Default,
+              },
+            }}
+          />
+        )}
+
+        {relayerFee && (
+          <KeyValueRow
+            field={{
+              label: {
+                text: toSentenceCase(strings('bridge.relayer_fee')),
+                variant: TextVariantLegacy.BodyMD,
+                color: TextColorLegacy.Alternative,
+              },
+            }}
+            value={{
+              label: {
+                text: formattedRelayerFee,
                 variant: TextVariantLegacy.BodyMD,
                 color: TextColorLegacy.Default,
               },
