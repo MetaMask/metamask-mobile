fac42156ec93f933dafa822e010c62c4b22bb26a
vinnyhoward <vincenguyenhoward@gmail.com>
2026-09-11 08:36:31 -0600

    fix: unit test

--- app/components/Nav/Main/MainNavigator.test.tsx
+++ app/components/Nav/Main/MainNavigator.test.tsx
@@ -1454,7 +1454,7 @@ describe('MainNavigator', () => {
       expect(rootStackScreenNames(container)).toContain(Routes.REWARDS_VIEW);
 
       const tabs = homeTabNames(container, state);
-      expect(tabs).toContain(Routes.SOCIAL_LEADERBOARD.TAB);
+      expect(tabs).toContain(Routes.SOCIAL.TAB);
       expect(tabs).not.toContain(Routes.REWARDS_VIEW);
       expect(tabs).not.toContain(Routes.MODAL.TRADE_WALLET_ACTIONS);
     });
@@ -1470,7 +1470,7 @@ describe('MainNavigator', () => {
       const tabs = homeTabNames(container, state);
       expect(tabs).toContain(Routes.REWARDS_VIEW);
       expect(tabs).toContain(Routes.MODAL.TRADE_WALLET_ACTIONS);
-      expect(tabs).not.toContain(Routes.SOCIAL_LEADERBOARD.TAB);
+      expect(tabs).not.toContain(Routes.SOCIAL.TAB);
     });
   });
 
