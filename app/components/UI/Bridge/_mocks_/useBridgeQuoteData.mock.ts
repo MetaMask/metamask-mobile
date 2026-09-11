92feb5da4a1f0c50d2fb1e57c6f7e47e84f90b65
micaelae <micaela.estabillo@consensys.net>
2026-09-10 22:06:12 -0700

    fix: import

--- app/components/UI/Bridge/_mocks_/useBridgeQuoteData.mock.ts
+++ app/components/UI/Bridge/_mocks_/useBridgeQuoteData.mock.ts
@@ -22,7 +22,6 @@ export const mockUseBridgeQuoteData: ReturnType<
     priceImpact: '1%',
     priceImpactFiat: '1',
     slippage: '0.5%',
-    priceImpactFiat: '1',
   },
   quotesLoadingStatus: RequestStatus.FETCHED,
   blockaidError: null,
