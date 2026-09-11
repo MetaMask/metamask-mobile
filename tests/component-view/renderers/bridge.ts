d651f3a9175b9d809ad05db30ea314d695ebb6c0
micaelae <micaela.estabillo@consensys.net>
2026-09-10 19:11:21 -0700

    Revert "chore: rm route wrap"
    This reverts commit a6891ab0ab4b7e31b73cd2e06ce5b2038fc34385.

--- tests/component-view/renderers/bridge.ts
+++ tests/component-view/renderers/bridge.ts
@@ -20,6 +20,18 @@ import BlockExplorersModal from '../../../app/components/UI/Bridge/components/Tr
 import { initialStateBridge } from '../presets/bridge';
 import type { TransactionMeta } from '@metamask/transaction-controller';
 import type { Transaction } from '@metamask/keyring-api';
+import { BridgeSessionProvider } from '../../../app/components/UI/Bridge/providers/BridgeSessionProvider';
+
+export const withBridgeSession = (Component: React.ComponentType) =>
+  function BridgeViewWithSession() {
+    return (
+      <BridgeSessionProvider>
+        <Component />
+      </BridgeSessionProvider>
+    );
+  };
+
+export const BridgeViewWithSession = withBridgeSession(BridgeView);
 
 interface RenderBridgeViewOptions {
   overrides?: DeepPartial<RootState>;
@@ -55,7 +67,7 @@ export function renderBridgeView(
   const state = builder.build();
 
   return renderComponentViewScreen(
-    BridgeView as unknown as React.ComponentType,
+    BridgeViewWithSession as unknown as React.ComponentType,
     { name: Routes.BRIDGE.BRIDGE_VIEW },
     { state },
   );
@@ -72,7 +84,7 @@ export function renderBridgeViewWithModals(
   const state = builder.build();
 
   return renderScreenWithRoutes(
-    BridgeView as unknown as React.ComponentType,
+    BridgeViewWithSession as unknown as React.ComponentType,
     { name: Routes.BRIDGE.BRIDGE_VIEW },
     [
       {
@@ -95,7 +107,7 @@ export function renderBridgeViewWithRecurringOrderDetails(
   const state = builder.build();
 
   return renderScreenWithRoutes(
-    BridgeView as unknown as React.ComponentType,
+    BridgeViewWithSession as unknown as React.ComponentType,
     { name: Routes.BRIDGE.BRIDGE_VIEW },
     [
       {
