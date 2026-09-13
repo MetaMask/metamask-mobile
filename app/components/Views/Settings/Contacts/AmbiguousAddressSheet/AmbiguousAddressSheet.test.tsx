98f27db910d86ff5ad2e3f8d32161b3f0c8d6ee8
georgewrmarshall <george.marshall@consensys.net>
2026-09-13 14:55:54 -0700

    chore: updating test coverage

--- app/components/Views/Settings/Contacts/AmbiguousAddressSheet/AmbiguousAddressSheet.test.tsx
+++ app/components/Views/Settings/Contacts/AmbiguousAddressSheet/AmbiguousAddressSheet.test.tsx
@@ -1,14 +1,70 @@
+import React from 'react';
 import AmbiguousAddressSheet from './AmbiguousAddressSheet';
 import { renderScreen } from '../../../../../util/test/renderWithProvider';
-import { screen } from '@testing-library/react-native';
+import { fireEvent, screen } from '@testing-library/react-native';
 import Routes from '../../../../../constants/navigation/Routes';
 import { strings } from '../../../../../../locales/i18n';
+
+jest.mock('@metamask/design-system-react-native', () => {
+  const ReactModule = jest.requireActual<typeof import('react')>('react');
+  const ReactNative = jest.requireActual<typeof import('react-native')>(
+    'react-native',
+  );
+  const actual = jest.requireActual(
+    '@metamask/design-system-react-native',
+  ) as Record<string, unknown>;
+
+  return {
+    ...actual,
+    BottomSheet: ReactModule.forwardRef(
+      (
+        {
+          children,
+          goBack,
+        }: { children: React.ReactNode; goBack: () => void },
+        ref: React.Ref<{ onCloseBottomSheet: () => void }>,
+      ) => {
+        ReactModule.useImperativeHandle(ref, () => ({
+          onCloseBottomSheet: goBack,
+        }));
+        return <ReactNative.View>{children}</ReactNative.View>;
+      },
+    ),
+  };
+});
 
 describe('AmbiguousAddressSheet', () => {
   it('should render correctly', () => {
     renderScreen(AmbiguousAddressSheet, {
       name: Routes.SHEET.AMBIGUOUS_ADDRESS,
     });
+    expect(
+      screen.getByText(strings('duplicate_address.title')),
+    ).toBeOnTheScreen();
+    expect(
+      screen.getByText(strings('duplicate_address.body')),
+    ).toBeOnTheScreen();
+  });
+
+  it('closes when the confirmation button is pressed', () => {
+    renderScreen(AmbiguousAddressSheet, {
+      name: Routes.SHEET.AMBIGUOUS_ADDRESS,
+    });
+
+    fireEvent.press(screen.getByText(strings('duplicate_address.button')));
+
+    expect(
+      screen.getByText(strings('duplicate_address.title')),
+    ).toBeOnTheScreen();
+  });
+
+  it('closes when the header close button is pressed', () => {
+    renderScreen(AmbiguousAddressSheet, {
+      name: Routes.SHEET.AMBIGUOUS_ADDRESS,
+    });
+
+    fireEvent.press(screen.getByText(strings('duplicate_address.title')));
+
     expect(
       screen.getByText(strings('duplicate_address.title')),
     ).toBeOnTheScreen();
