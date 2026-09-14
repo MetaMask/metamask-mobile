64df7440a327c4bd15ae857e205a25882853816b
Repro <omergoktugpoyraz@gmail.com>
2026-09-14 09:38:51 +0300

    Update modals

--- app/components/Views/confirmations/components/modals/advanced-gas-price-modal/advanced-gas-price-modal.styles.ts
+++ app/components/Views/confirmations/components/modals/advanced-gas-price-modal/advanced-gas-price-modal.styles.ts
@@ -1,20 +1,7 @@
 import { StyleSheet } from 'react-native';
-import { Theme } from '../../../../../../util/theme/models';
 
-const styleSheet = (params: { theme: Theme }) => {
-  const { theme } = params;
-  const { colors } = theme;
-
+const styleSheet = () => {
   return StyleSheet.create({
-    container: {
-      backgroundColor: theme.colors.background.elevated1,
-      borderWidth: 1,
-      borderColor: colors.border.alternative,
-      padding: 16,
-      paddingBottom: 36,
-      borderTopRightRadius: 16,
-      borderTopLeftRadius: 16,
-    },
     button: {
       width: '100%',
     },
