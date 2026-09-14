0089ff588034780a25174f07afeaf69c18f96109
Bernardo Garces Chapero <bernardo.chapero@consensys.net>
2026-09-14 13:00:10 +0100

    reverts

--- app/components/UI/PredictNext/services/PredictMarketDataService.test.ts
+++ app/components/UI/PredictNext/services/PredictMarketDataService.test.ts
@@ -17,7 +17,6 @@ import {
   type PredictVenueId,
 } from '../types';
 import {
-  PREDICT_MARKET_DATA_SERVICE_NAME,
   PredictMarketDataService,
   type PredictMarketDataServiceMessenger,
 } from './PredictMarketDataService';
@@ -38,7 +37,7 @@ jest.mock('../../../../util/trace', () => ({
 
 const createService = (marketData: VenueMarketDataAdapter) => {
   const messenger: PredictMarketDataServiceMessenger = new Messenger({
-    namespace: PREDICT_MARKET_DATA_SERVICE_NAME,
+    namespace: 'PredictMarketDataService',
   });
   return new PredictMarketDataService({
     messenger,
