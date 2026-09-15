4064b8b2712f7efac2bc1e1980c883469554ac33
Matthew Grainger <matthew.grainger@consensys.net>
2026-09-15 18:00:52 -0400

    fix: replaced money onboarding rive animation. Updated text overlay to depend on currentStep view model property

--- app/components/UI/Money/Views/MoneyOnboardingView/MoneyOnboardingView.testIds.ts
+++ app/components/UI/Money/Views/MoneyOnboardingView/MoneyOnboardingView.testIds.ts
@@ -1,5 +1,4 @@
 export const MoneyOnboardingViewTestIds = {
-  RIVE_INPUT_CONTAINER: 'money-onboarding-rive-input-container',
   RIVE_ANIMATION: 'money-onboarding-rive-animation',
   OVERLAY_TITLE: 'money-onboarding-overlay-title',
   OVERLAY_CONTENT: 'money-onboarding-overlay-content',
