713da0a39473c665e94369fe675d4fa9ed3cdc9b
Arthur Breton <arthur.breton@consensys.net>
2026-09-15 22:50:08 +0800

    cleanup(tat-3960): remove reproduction marker

--- app/components/UI/Perps/Views/PerpsProMarketView/components/PerpsProTwapCard.tsx
+++ app/components/UI/Perps/Views/PerpsProMarketView/components/PerpsProTwapCard.tsx
@@ -24,7 +24,6 @@ import {
 import React from 'react';
 import { Pressable } from 'react-native';
 import { useSelector } from 'react-redux';
-import DevLogger from '../../../../../../core/SDKConnect/utils/DevLogger';
 import { strings } from '../../../../../../../locales/i18n';
 import { selectPrivacyMode } from '../../../../../../selectors/preferencesController';
 import PerpsTokenLogo from '../../../components/PerpsTokenLogo';
@@ -142,9 +141,6 @@ const PerpsProTwapCard = ({
   const executedSize = formatPositionSize(twapOrder.executedSize);
   const elapsedMinutes = Math.floor(
     twapOrder.elapsedTimeMilliseconds / MILLISECONDS_PER_MINUTE,
-  );
-  DevLogger.log(
-    `[TAT-3960] BUG_MARKER: active TWAP elapsed rendered from a snapshot-only value, floored to whole minutes; status=${twapOrder.status} elapsedMs=${twapOrder.elapsedTimeMilliseconds} renderedMinutes=${elapsedMinutes}`,
   );
 
   const handlePress = onPress ? () => onPress(twapOrder) : undefined;
