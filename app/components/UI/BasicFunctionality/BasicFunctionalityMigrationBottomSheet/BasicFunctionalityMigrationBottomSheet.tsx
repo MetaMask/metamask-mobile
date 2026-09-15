1cb663c5c44ec08fa47dbd4aa07e45440e11677d
Gaurav Goel <grvgoel19@gmail.com>
2026-09-15 12:08:19 +0700

    feat: point mobile legal URLs at metamask.com

--- app/components/UI/BasicFunctionality/BasicFunctionalityMigrationBottomSheet/BasicFunctionalityMigrationBottomSheet.tsx
+++ app/components/UI/BasicFunctionality/BasicFunctionalityMigrationBottomSheet/BasicFunctionalityMigrationBottomSheet.tsx
@@ -23,7 +23,7 @@ import { strings } from '../../../../../locales/i18n';
 export const BASIC_FUNCTIONALITY_MIGRATION_BLOG_POST_LINK =
   'https://metamask.io/news/updating-metamask-analytics';
 export const BASIC_FUNCTIONALITY_MIGRATION_PRIVACY_NOTICE_LINK =
-  'https://consensys.io/privacy-notice';
+  'https://metamask.com/privacy-notice';
 
 const BasicFunctionalityMigrationBottomSheet = () => {
   const dispatch = useDispatch();
