9dd9d2d53df48980161b343a347ea2e09993b361
Arthur Breton <arthur.breton@consensys.net>
2026-09-21 17:28:03 +0800

    cleanup(pr-tat-3979): remove reproduction marker

--- app/components/UI/Perps/Views/PerpsProMarketView/components/PerpsProPositionsPanel.tsx
+++ app/components/UI/Perps/Views/PerpsProMarketView/components/PerpsProPositionsPanel.tsx
@@ -97,7 +97,6 @@ import {
   CHASE_METAMETRICS_PROPERTY,
 } from '../../../constants/chaseAnalytics';
 import usePerpsToasts from '../../../hooks/usePerpsToasts';
-import DevLogger from '../../../../../../core/SDKConnect/utils/DevLogger';
 import { registerVisibleChaseOrderHandles } from '../../../services/ChaseOrderVisibility';
 import PerpsTokenLogo from '../../../components/PerpsTokenLogo';
 import PerpsProActivityFilterSheet from './PerpsProActivityFilterSheet';
@@ -441,16 +440,6 @@ const PerpsProPositionsPanel = ({
         : historicalTwapOrders,
     [historicalTwapOrders, isFilledOnly, twapView],
   );
-
-  useEffect(() => {
-    if (!terminatingTwapOrder) {
-      return;
-    }
-    DevLogger.log(
-      '[PR-TAT-3979] BUG_MARKER: TWAP terminate sheet opened inline inside the scrollable positions panel; activeTwapOrders=' +
-        activeTwapOrders.length,
-    );
-  }, [activeTwapOrders.length, terminatingTwapOrder]);
 
   useEffect(() => {
     if (
