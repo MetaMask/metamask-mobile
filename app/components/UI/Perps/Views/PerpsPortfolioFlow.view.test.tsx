da723417d6e06dd47f30f2e00aec7e4bdba187f3
tommasini <tommasini15@gmail.com>
2026-09-22 18:12:55 +0100

    view tests fix

--- app/components/UI/Perps/Views/PerpsPortfolioFlow.view.test.tsx
+++ app/components/UI/Perps/Views/PerpsPortfolioFlow.view.test.tsx
@@ -19,6 +19,10 @@ import {
   renderPerpsSelectAdjustMarginActionView,
   defaultPositionForViews,
 } from '../../../../../tests/component-view/renderers/perpsViewRenderer';
+import {
+  clearPerpsOutreachApiMocks,
+  setupPerpsOutreachApiMock,
+} from '../../../../../tests/component-view/api-mocking/perpsOutreach';
 import { PerpsPositionsViewSelectorsIDs } from '../Perps.testIds';
 
 const TIMEOUT_MS = 3000;
@@ -36,6 +40,15 @@ describe('Portfolio & Account Flow', () => {
     EMPTY_TITLE = strings('perps.position.list.empty_title');
     ADD_MARGIN = strings('perps.adjust_margin.add_margin');
     REDUCE_MARGIN = strings('perps.adjust_margin.reduce_margin');
+  });
+
+  beforeEach(() => {
+    setupPerpsOutreachApiMock(null);
+  });
+
+  afterEach(() => {
+    cleanup();
+    clearPerpsOutreachApiMocks();
   });
 
   it('browses positions, handles geo-restrictions, and adjusts margin', async () => {
